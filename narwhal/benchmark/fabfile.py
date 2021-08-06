fef5fbc68bf65a0d17fcd750a988c3c5d7df3e08
Alberto Sonnino <asonnino@fb.com>
2021-08-06 15:34:15 +0100

    Fix lef comments

--- narwhal/benchmark/fabfile.py
+++ narwhal/benchmark/fabfile.py
@@ -99,7 +99,7 @@ def remote(ctx, debug=False):
         'nodes': [10],
         'workers': 1,
         'collocate': True,
-        'rate': [100_000],
+        'rate': [50_000],
         'tx_size': 512, 
         'duration': 300,
         'runs': 1,
