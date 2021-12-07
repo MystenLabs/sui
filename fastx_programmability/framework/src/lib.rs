cedef69ed994b4efba3e71b282ce1f66c9225851
Xun Li <lxfind@gmail.com>
2021-12-07 15:49:53 -0800

    [NFC] Better code style for creating BuildConfig

--- fastx_programmability/framework/src/lib.rs
+++ fastx_programmability/framework/src/lib.rs
@@ -9,8 +9,10 @@ fn check_that_move_code_can_be_built() {
     use std::path::PathBuf;
 
     let framework_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let mut build_config = BuildConfig::default();
-    build_config.dev_mode = true;
+    let build_config = BuildConfig {
+        dev_mode: true,
+        ..Default::default()
+    };
     build_config
         .compile_package(&framework_dir, &mut Vec::new())
         .unwrap();
