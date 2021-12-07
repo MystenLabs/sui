7392261690938d150c5327521278730cc026310a
Xun Li <lxfind@gmail.com>
2021-12-06 18:32:14 -0800

    rustfmt

--- fastx_programmability/framework/src/lib.rs
+++ fastx_programmability/framework/src/lib.rs
@@ -5,11 +5,13 @@ pub mod natives;
 
 #[test]
 fn check_that_move_code_can_be_built() {
-    use std::path::PathBuf;
     use move_package::BuildConfig;
+    use std::path::PathBuf;
 
     let framework_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let mut build_config = BuildConfig::default();
     build_config.dev_mode = true;
-    build_config.compile_package(&framework_dir, &mut Vec::new()).unwrap();
+    build_config
+        .compile_package(&framework_dir, &mut Vec::new())
+        .unwrap();
 }
