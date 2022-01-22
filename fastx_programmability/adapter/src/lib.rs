6d48c5c4a506d31e18cce8c2f1d12571c131b7fc
Sam Blackshear <sam.blackshear@gmail.com>
2022-01-21 17:01:05 -0800

    [cleanup] remove adapter CLI and related code
    The adapter CLI has been broken for awhile, and I think it will be a fair amount of work to fix. Focusing on having one CLI that works really well + does everything a Move dev would wish for seems more high-pri at the moment.
    
    This deletes all the code related to the adapter CLI so we don't need to maintain it.

--- fastx_programmability/adapter/src/lib.rs
+++ fastx_programmability/adapter/src/lib.rs
@@ -4,25 +4,3 @@
 pub mod adapter;
 pub mod bytecode_rewriter;
 pub mod genesis;
-pub mod state_view;
-
-use move_core_types::account_address::AccountAddress;
-
-/// Extract + return an address from the first `authenticator.length()` bytes of `object`.
-/// Replace theses bytes with `authenticator`.
-/// copy the first authenticator.length() bytes out of `object`, turn them into
-/// an address. and return them. then, replace the first authenicator.length()
-/// bytes of `object` with `authenticator`
-pub(crate) fn swap_authenticator_and_id(
-    authenticator: AccountAddress,
-    object: &mut Vec<u8>,
-) -> AccountAddress {
-    assert!(object.len() > authenticator.len());
-
-    let authenticator_bytes = authenticator.into_bytes();
-    let mut id_bytes = [0u8; AccountAddress::LENGTH];
-
-    id_bytes[..authenticator.len()].clone_from_slice(&object[..authenticator.len()]);
-    object[..authenticator.len()].clone_from_slice(&authenticator_bytes[..authenticator.len()]);
-    AccountAddress::new(id_bytes)
-}
