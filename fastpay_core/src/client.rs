12552d132583b46250fda7a6c9e8c2328568819c
patrick <patrickkuo@me.com>
2022-01-25 19:25:05 +0000

    * remove unknownError and imp From<anyhow::Error> for FastPayError

--- fastpay_core/src/client.rs
+++ fastpay_core/src/client.rs
@@ -483,7 +483,7 @@ where
         known_certificates: Vec<CertifiedOrder>,
         target_sequence_number: SequenceNumber,
         action: F,
-    ) -> Result<(Vec<OrderInfoResponse>, Vec<V>), FastPayError>
+    ) -> Result<(Vec<OrderInfoResponse>, Vec<V>), anyhow::Error>
     where
         F: Fn(AuthorityName, &'a mut A) -> AsyncResult<'a, V, FastPayError> + Send + Sync + Copy,
         V: Copy,
@@ -496,6 +496,7 @@ where
                 expected_sequence: next_sequence_number,
                 received_sequence: target_sequence_number,
             }
+            .into()
         );
         let requester = CertificateRequester::new(
             self.committee.clone(),
@@ -580,7 +581,7 @@ where
         object_id: ObjectID,
         known_certificates: Vec<CertifiedOrder>,
         target_sequence_number: SequenceNumber,
-    ) -> Result<Vec<OrderInfoResponse>, FastPayError> {
+    ) -> Result<Vec<OrderInfoResponse>, anyhow::Error> {
         self.broadcast_and_execute(
             sender,
             object_id,
@@ -727,10 +728,10 @@ where
         &mut self,
         order: Order,
         with_confirmation: bool,
-    ) -> Result<CertifiedOrder, FastPayError> {
+    ) -> Result<CertifiedOrder, anyhow::Error> {
         fp_ensure!(
             self.pending_transfer == None || self.pending_transfer.as_ref() == Some(&order),
-            FastPayError::ConcurrentTransferError
+            FastPayError::ConcurrentTransferError.into()
         );
         fp_ensure!(
             order.sequence_number() == self.next_sequence_number(order.object_id())?,
@@ -739,6 +740,7 @@ where
                 expected_sequence: self.next_sequence_number(order.object_id())?,
                 received_sequence: order.sequence_number()
             }
+            .into()
         );
         self.pending_transfer = Some(order.clone());
         let result = self
