35fa4d398d78a452b0a1275efd9b90174a8fddb7
Xun Li <lxfind@gmail.com>
2022-01-27 13:40:56 -0800

    Add trasfer_to_object API in Transfer.move

--- fastx_programmability/framework/src/natives/transfer.rs
+++ fastx_programmability/framework/src/natives/transfer.rs
@@ -15,12 +15,12 @@ use smallvec::smallvec;
 use std::collections::VecDeque;
 
 /// Implementation of Move native function
-/// `transfer_internal<T: key>(event: TransferEvent<T>)`
+/// `transfer_internal<T: key>(obj: T, recipient: vector<u8>, should_freeze: bool)`
 /// Here, we simply emit this event. The fastX adapter
 /// treats this as a special event that is handled
-/// differently from user events--for each `TransferEvent`,
+/// differently from user events:
 /// the adapter will change the owner of the object
-/// in question to `TransferEvent.recipient`
+/// in question to `recipient`.
 pub fn transfer_internal(
     context: &mut NativeContext,
     mut ty_args: Vec<Type>,
@@ -33,19 +33,44 @@ pub fn transfer_internal(
     let should_freeze = pop_arg!(args, bool);
     let recipient = pop_arg!(args, Vec<u8>);
     let transferred_obj = args.pop_back().unwrap();
-
     let event_type = if should_freeze {
-        EventType::TransferAndFreeze
+        EventType::TransferToAddressAndFreeze
     } else {
-        EventType::Transfer
-    } as u64;
+        EventType::TransferToAddress
+    };
+    transfer_common(context, ty, transferred_obj, recipient, event_type)
+}
 
+/// Implementation of Move native function
+/// `transfer_to_object_id<T: key>(obj: T, id: IDBytes)`
+pub fn transfer_to_object_id(
+    context: &mut NativeContext,
+    mut ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(ty_args.len() == 1);
+    debug_assert!(args.len() == 2);
+
+    let ty = ty_args.pop().unwrap();
+    let recipient = pop_arg!(args, Vec<u8>);
+    let transferred_obj = args.pop_back().unwrap();
+    let event_type = EventType::TransferToObject;
+    transfer_common(context, ty, transferred_obj, recipient, event_type)
+}
+
+fn transfer_common(
+    context: &mut NativeContext,
+    ty: Type,
+    transferred_obj: Value,
+    recipient: Vec<u8>,
+    event_type: EventType,
+) -> PartialVMResult<NativeResult> {
     // Charge a constant native gas cost here, since
     // we will charge it properly when processing
     // all the events in adapter.
     // TODO: adjust native_gas cost size base.
     let cost = native_gas(context.cost_table(), NativeCostIndex::EMIT_EVENT, 1);
-    if !context.save_event(recipient, event_type, ty, transferred_obj)? {
+    if !context.save_event(recipient, event_type as u64, ty, transferred_obj)? {
         return Ok(NativeResult::err(cost, 0));
     }
 
