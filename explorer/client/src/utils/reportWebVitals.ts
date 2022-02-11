72de8f8dbbb4eff01334b12d0a7b7beaba60a436
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-02-11 11:40:22 +0000

    explorer-client: initialise client app
    * project init with `create-react-app` with typescript template
    * added suport for scss
    * updated readme with some info for development
    * added linting/formating support with prettier, eslint and stylelint
    * using `.env` to disable reporting `web-vitals` in prod mode and `console.log` them in dev
    * disabled `react-scripts` from opening the browser with env variable in `.env.development` (can be overridden in a local env file)
    * added classnames lib

---
+++ explorer/client/src/utils/reportWebVitals.ts
@@ -0,0 +1,37 @@
+import type { ReportHandler } from 'web-vitals';
+
+enum ReportMethod {
+    console = 'console',
+    request = 'request',
+}
+
+let onPerfEntry: ReportHandler;
+if (process.env.REACT_APP_REPORT_VITALS === 'true') {
+    switch (process.env.REACT_APP_REPORT_VITALS_METHOD) {
+        case ReportMethod.console:
+            onPerfEntry = console.log;
+            break;
+        case ReportMethod.request:
+            // nothing for now
+            break;
+        default:
+            // do nothing by default
+            break;
+    }
+}
+
+const reportWebVitals = () => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        import('web-vitals').then(
+            ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+                getCLS(onPerfEntry);
+                getFID(onPerfEntry);
+                getFCP(onPerfEntry);
+                getLCP(onPerfEntry);
+                getTTFB(onPerfEntry);
+            }
+        );
+    }
+};
+
+export default reportWebVitals;
