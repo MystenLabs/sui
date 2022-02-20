fb23af9e68dfd0eabb7e2a181de3b2e6ec37542e
François Garillot <francois@garillot.net>
2022-02-20 12:15:19 -0500

    chore: fix license headers, add checking to CI

---
+++ mysten-infra/scripts/license_check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# shellcheck disable=SC2044
+# This script checks each file starts with a license comment
+set -e
+set -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOPLEVEL="${DIR}/../"
+
+# Iterate over rust files not in the target directory
+for i in $(find "$TOPLEVEL" -path "$TOPLEVEL/target" -prune -o -iname "*.rs" -print) 
+do
+  CNT=$(head -n3 "$i" | grep -oEe '// (Copyright \(c\) 2022, Mysten Labs, Inc.|SPDX-License-Identifier: Apache-2.0)' | wc -l)
+  # echo "$i $CNT"
+  if [ "$CNT" -lt 2 ]
+  then
+     echo "File $i has an incorrect license header"
+     exit 1
+  fi
+done
