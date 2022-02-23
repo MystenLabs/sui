9413942cbb1970533f149ec5de49e7ca51edc69f
ade <ade@mystenlabs.com>
2022-02-22 19:57:14 -0500

    added license

--- sui_types/src/move_call_type_check.rs
+++ sui_types/src/move_call_type_check.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::{
     base_types::{ObjectID, TxContext, TX_CONTEXT_MODULE_NAME, TX_CONTEXT_STRUCT_NAME},
     error::SuiError,
