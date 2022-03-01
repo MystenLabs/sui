be46f494e9dafffc5cb84f6fb766db8a8a8fea56
patrick <patrickkuo@me.com>
2022-03-01 13:41:27 +0000

    Add license header

--- sui/src/keystore.rs
+++ sui/src/keystore.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use anyhow::anyhow;
 use ed25519_dalek::ed25519::signature;
 use ed25519_dalek::{ed25519, Signer};
