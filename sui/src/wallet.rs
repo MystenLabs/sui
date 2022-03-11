9abba10e33aa22384ab454d6fc22158b8f9002ef
Patrick Kuo <patrickkuo@me.com>
2022-03-11 23:44:16 +0000

    [wallet] Interactive shell completion enhancement and more unit tests (#704)
    * * added `CompletionCache` for reusing queried data for command line completion
    
    * * add tests for shell input completer, env variable substitution
    
    * * add some rust doc to CacheKey Ord
    
    * remove loop label

--- sui/src/wallet.rs
+++ sui/src/wallet.rs
@@ -1,16 +1,22 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
+
+use std::io::Write;
+use std::io::{stderr, stdout};
+use std::path::PathBuf;
+
 use async_trait::async_trait;
 use colored::Colorize;
-use std::io;
-use std::path::PathBuf;
 use structopt::clap::{App, AppSettings};
 use structopt::StructOpt;
-use sui::config::{Config, WalletConfig};
-use sui::shell::{install_shell_plugins, AsyncHandler, CommandStructure, Shell};
-use sui::wallet_commands::*;
 use tracing::error;
 use tracing_subscriber::EnvFilter;
+
+use sui::config::{Config, WalletConfig};
+use sui::shell::{
+    install_shell_plugins, AsyncHandler, CacheKey, CommandStructure, CompletionCache, Shell,
+};
+use sui::wallet_commands::*;
 
 const SUI: &str = "   _____       _    _       __      ____     __
   / ___/__  __(_)  | |     / /___ _/ / /__  / /_
@@ -70,25 +76,30 @@ async fn main() -> Result<(), anyhow::Error> {
             .await?;
     }
 
+    let mut out = stdout();
+
     if !options.no_shell {
         let app: App = WalletCommands::clap();
-        println!("{}", SUI.cyan().bold());
-        print!("--- ");
-        app.write_long_version(&mut io::stdout())?;
-        println!(" ---");
-        println!("{}", context.config);
-        println!();
-        println!("Welcome to the Sui interactive shell.");
-        println!();
+        writeln!(out, "{}", SUI.cyan().bold())?;
+        let version = app
+            .p
+            .meta
+            .long_version
+            .unwrap_or_else(|| app.p.meta.version.unwrap_or("unknown"));
+        writeln!(out, "--- sui wallet {} ---", version)?;
+        writeln!(out)?;
+        writeln!(out, "{}", context.config)?;
+        writeln!(out, "Welcome to the Sui interactive shell.")?;
+        writeln!(out)?;
 
-        let mut shell = Shell {
-            prompt: "sui>-$ ".bold().green(),
-            state: context,
-            handler: ClientCommandHandler,
-            command: CommandStructure::from_clap(&install_shell_plugins(app)),
-        };
+        let mut shell = Shell::new(
+            "sui>-$ ".bold().green(),
+            context,
+            ClientCommandHandler,
+            CommandStructure::from_clap(&install_shell_plugins(app)),
+        );
 
-        shell.run_async().await?;
+        shell.run_async(&mut out, &mut stderr()).await?;
     } else if let Some(mut cmd) = options.cmd {
         cmd.execute(&mut context).await?.print(!options.json);
     }
@@ -99,8 +110,13 @@ struct ClientCommandHandler;
 
 #[async_trait]
 impl AsyncHandler<WalletContext> for ClientCommandHandler {
-    async fn handle_async(&self, args: Vec<String>, context: &mut WalletContext) -> bool {
-        if let Err(e) = handle_command(get_command(args), context).await {
+    async fn handle_async(
+        &self,
+        args: Vec<String>,
+        context: &mut WalletContext,
+        completion_cache: CompletionCache,
+    ) -> bool {
+        if let Err(e) = handle_command(get_command(args), context, completion_cache).await {
             error!("{}", e.to_string().red());
         }
         false
@@ -115,12 +131,35 @@ fn get_command(args: Vec<String>) -> Result<WalletOpts, anyhow::Error> {
 async fn handle_command(
     wallet_opts: Result<WalletOpts, anyhow::Error>,
     context: &mut WalletContext,
+    completion_cache: CompletionCache,
 ) -> Result<(), anyhow::Error> {
     let mut wallet_opts = wallet_opts?;
-    wallet_opts
-        .command
-        .execute(context)
-        .await?
-        .print(!wallet_opts.json);
+    let result = wallet_opts.command.execute(context).await?;
+
+    // Update completion cache
+    // TODO: Completion data are keyed by strings, are there ways to make it more error proof?
+    if let Ok(mut cache) = completion_cache.write() {
+        match result {
+            WalletCommandResult::Addresses(ref addresses) => {
+                let addresses = addresses
+                    .iter()
+                    .map(|addr| format!("{}", addr))
+                    .collect::<Vec<_>>();
+                cache.insert(CacheKey::flag("--address"), addresses.clone());
+                cache.insert(CacheKey::flag("--to"), addresses);
+            }
+            WalletCommandResult::Objects(ref objects) => {
+                let objects = objects
+                    .iter()
+                    .map(|(object_id, _, _)| format!("{}", object_id))
+                    .collect::<Vec<_>>();
+                cache.insert(CacheKey::new("object", "--id"), objects.clone());
+                cache.insert(CacheKey::flag("--gas"), objects.clone());
+                cache.insert(CacheKey::flag("--object-id"), objects);
+            }
+            _ => {}
+        }
+    }
+    result.print(!wallet_opts.json);
     Ok(())
 }
