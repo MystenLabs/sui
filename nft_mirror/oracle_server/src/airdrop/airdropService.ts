ac243d9aed704081be5591afbb1557f904966137
Chris Li <76067158+666lcz@users.noreply.github.com>
2022-03-11 16:33:42 -0800

    [NFT Mirroring] set up CORS policy

--- nft_mirror/oracle_server/src/airdrop/airdropService.ts
+++ nft_mirror/oracle_server/src/airdrop/airdropService.ts
@@ -1,7 +1,9 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-interface AirdropClaimMessage {
+import { ValidateError } from 'tsoa';
+
+interface AirdropClaimInfo {
     /**
      * Name of the source chain
      * @pattern ethereum
@@ -31,21 +33,15 @@ interface AirdropClaimMessage {
  *
  *
  * @example {
- *  "message": {
- *      "source_chain": "ethereum",
- *      "source_contract_address": "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
- *      "source_token_id": "101",
- *      "source_owner_address": "0x529f501ceb3ab599274a38f2aee41a7eba1fcead",
- *      "destination_sui_address": "0x10"
- *   },
- *   "signature": "0x21fbf0696d5e0aa2ef41a2b4ffb623bcaf070461d61cf7251c74161f82fec3a4370854bc0a34b3ab487c1bc021cd318c734c51ae29374f2beb0e6f2dd49b4bf41c"
+ *   "wallet_message": "{\"domain\":{\"chainId\":1,\"name\":\"SuiDrop\",\"version\":\"1\"},\"message\":{\"source_chain\":\"ethereum\",\"source_contract_address\":\"0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D\",\"source_token_id\":\"8937\",\"source_owner_address\":\"0x09dbc4a902199bbe7f7ec29b3714731786f2e878\",\"destination_sui_address\":\"0xa5e6dbcf33730ace6ec8b400ff4788c1f150ff7e\"},\"primaryType\":\"ClaimRequest\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"}],\"ClaimRequest\":[{\"name\":\"source_chain\",\"type\":\"string\"},{\"name\":\"source_contract_address\",\"type\":\"string\"},{\"name\":\"source_token_id\",\"type\":\"string\"},{\"name\":\"source_owner_address\",\"type\":\"string\"},{\"name\":\"destination_sui_address\",\"type\":\"string\"}]}}",
+ *   "signature": "abc"
  * }
  */
 export interface AirdropClaimRequest {
     /**
-     * unsigned message
+     * An EIP-712 compliant message
      */
-    message: AirdropClaimMessage;
+    wallet_message: string;
     /**
      * Digital signature of `message` signed by the private key of `source_owner_address`
      */
@@ -87,12 +83,40 @@ export class AirdropService {
     public async claim(
         claimMessage: AirdropClaimRequest
     ): Promise<AirdropClaimResponse> {
-        const message = claimMessage.message;
+        const { wallet_message } = claimMessage;
+        const data = JSON.parse(wallet_message);
+        const claimInfo = this.parseClaimInfo(data);
         return {
-            source_chain: message.source_chain,
-            source_contract_address: message.source_contract_address,
-            source_token_id: message.source_token_id,
-            sui_explorer_link: 'www.mystenlabs.com',
+            source_chain: claimInfo.source_chain,
+            source_contract_address: claimInfo.source_contract_address,
+            source_token_id: claimInfo.source_token_id,
+            sui_explorer_link:
+                'https://djgd7fpxio1yh.cloudfront.net/objects/7bc832ec31709638cd8d9323e90edf332gff4389',
         };
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    parseClaimInfo(data: any): AirdropClaimInfo {
+        if (isAirdropClaimInfo(data['message'])) {
+            return data['message'] as AirdropClaimInfo;
+        }
+        throw new ValidateError(
+            { messages: { message: 'Wrong format', value: data['message'] } },
+            'Wrong format for wallet message'
+        );
+    }
+}
+
+/**
+ * User Defined Type Guard for AirdropClaimInfo
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isAirdropClaimInfo(arg: any): arg is AirdropClaimInfo {
+    return (
+        arg.source_chain &&
+        arg.source_contract_address &&
+        arg.source_token_id &&
+        arg.source_owner_address &&
+        arg.destination_sui_address
+    );
 }
