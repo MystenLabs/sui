cb25c08f5dc9ce82408387792830a6264fa42175
patrick <patrickkuo@me.com>
2022-03-15 16:22:55 +0000

    add license

--- sui/src/unit_tests/rest_server_tests.rs
+++ sui/src/unit_tests/rest_server_tests.rs
@@ -1,3 +1,5 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 use anyhow::anyhow;
 use dropshot::test_util::{LogContext, TestContext};
 use dropshot::{ConfigDropshot, ConfigLogging, ConfigLoggingLevel};
