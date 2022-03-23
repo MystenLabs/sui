57121e0503eb9d99eaf5d122a6dbed43b6d2a507
Andrew Burnie <apburnie@hotmail.co.uk>
2022-03-23 14:42:19 +0000

    prevents display of Address Results pages with no objects

--- explorer/client/src/utils/utility_functions.tsx
+++ explorer/client/src/utils/utility_functions.tsx
@@ -3,21 +3,40 @@ import { DefaultRpcClient as rpc } from './rpc';
 
 const navigateWithUnknown = async (input: string, navigate: Function) => {
     // feels crude to just search each category for an ID, but works for now
-    const addrPromise = rpc
-        .getAddressObjects(input)
-        .then((data) => {
-            navigate(`../addresses/${input}`, { state: data });
-        })
-        .catch((error) => console.log(error));
-
-    const objInfoPromise = rpc.getObjectInfo(input).then((data) => {
-        navigate(`../objects/${input}`, { state: data });
+    const addrPromise = rpc.getAddressObjects(input).then((data) => {
+        if (data.length > 0) {
+            return {
+                category: 'addresses',
+                data: data,
+            };
+        } else {
+            throw new Error('No objects for Address');
+        }
     });
 
+    const objInfoPromise = rpc.getObjectInfo(input).then((data) => ({
+        category: 'objects',
+        data: data,
+    }));
+
     //if none of the queries find a result, show missing page
-    Promise.any([addrPromise, objInfoPromise]).catch((_) =>
-        navigate(`../missing/${input}`)
-    );
+    Promise.any([objInfoPromise, addrPromise])
+        .then((pac) => {
+            if (
+                pac?.data &&
+                (pac?.category === 'objects' || pac?.category === 'addresses')
+            ) {
+                navigate(`../${pac.category}/${input}`, { state: pac.data });
+            } else {
+                throw new Error(
+                    'Something wrong with navigateWithUnknown function'
+                );
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            navigate(`../missing/${input}`);
+        });
 };
 
 const findDataFromID = (targetID: string | undefined, state: any) =>
