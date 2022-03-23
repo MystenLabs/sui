94d75cd176d4a4988d845a1dd382e66d48a2d256
Andrew Burnie <apburnie@hotmail.co.uk>
2022-03-23 13:53:40 +0000

    removes conversion scripts folder

--- explorer/client/conversion-scripts/move-json-converter.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const contents = fs.readFileSync('./Example.move', { encoding: 'utf8' });
-
-const data = JSON.stringify({ data: contents });
-
-fs.writeFileSync('moveExample.json', data);
