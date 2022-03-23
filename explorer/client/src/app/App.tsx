d82b99b83b267cffdae5e1ee2e04acdcb9f8eb26
Stella Cannefax <stella@mystenlabs.com>
2022-03-22 19:08:25 -0700

    restore copyright to App.tsx

--- explorer/client/src/app/App.tsx
+++ explorer/client/src/app/App.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import Footer from '../components/footer/Footer';
 import Search from '../components/search/Search';
 import AppRoutes from '../pages/config/AppRoutes';
