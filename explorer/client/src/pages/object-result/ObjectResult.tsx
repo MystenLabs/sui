4ddc67548062d93b6efd13f07b9f1983d8db8027
Andrew Burnie <apburnie@hotmail.co.uk>
2022-03-24 18:55:45 +0000

    static object data displays only when tests are run

--- explorer/client/src/pages/object-result/ObjectResult.tsx
+++ explorer/client/src/pages/object-result/ObjectResult.tsx
@@ -1,7 +1,7 @@
 //import 'ace-builds/src-noconflict/theme-github';
 import React, { useEffect, useState } from 'react';
 //import AceEditor from 'react-ace';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 import ErrorResult from '../../components/error-result/ErrorResult';
 import theme from '../../styles/theme.module.css';
@@ -23,9 +23,20 @@ const DATATYPE_DEFAULT: DataType = {
     loadState: 'pending',
 };
 
-const ObjectResult = (): JSX.Element => {
-    const { id: objID } = useParams();
+function instanceOfDataType(object: any): object is DataType {
+    return object && ['id', 'version', 'objType'].every((x) => x in object);
+}
 
+const Fail = ({ objID }: { objID: string | undefined }): JSX.Element => {
+    return (
+        <ErrorResult
+            id={objID}
+            errorMsg="There was an issue with the data on the following object"
+        />
+    );
+};
+
+const ObjectResultInternetAPI = ({ objID }: { objID: string }): JSX.Element => {
     const [showObjectState, setObjectState] = useState(DATATYPE_DEFAULT);
     const rpc = DefaultRpcClient;
 
@@ -52,15 +63,40 @@ const ObjectResult = (): JSX.Element => {
         );
     }
     if (showObjectState.loadState === 'fail') {
-        return (
-            <ErrorResult
-                id={objID}
-                errorMsg="There was an issue with the data on the following object"
-            />
-        );
+        return <Fail objID={objID} />;
     }
 
     return <div>"Something went wrong"</div>;
+};
+
+const ObjectResultStatic = ({ objID }: { objID: string }): JSX.Element => {
+    const { findDataFromID } = require('../../utils/static/utility_functions');
+    const data = findDataFromID(objID, undefined);
+
+    if (instanceOfDataType(data)) {
+        return <ObjectLoaded data={data} />;
+    } else {
+        return <Fail objID={objID} />;
+    }
+};
+
+const ObjectResult = (): JSX.Element => {
+    const { id: objID } = useParams();
+    const { state } = useLocation();
+
+    if (instanceOfDataType(state)) {
+        return <ObjectLoaded data={state} />;
+    }
+
+    if (objID !== undefined) {
+        if (process.env.REACT_APP_DATA !== 'static') {
+            return <ObjectResultInternetAPI objID={objID} />;
+        } else {
+            return <ObjectResultStatic objID={objID} />;
+        }
+    }
+
+    return <Fail objID={objID} />;
 };
 
 export { ObjectResult };
