8f1a9837d828fee98d63f56391e17271f15071b5
Chris Li <76067158+666lcz@users.noreply.github.com>
2022-03-24 15:41:49 -0700

    Add license check for move, typescript, javascript files (#1051)

--- scripts/license_check.sh
+++ scripts/license_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2044
+# shellcheck disable=SC2044,SC2086,SC2016
 # This script checks each file starts with a license comment
 set -e
 set -o pipefail
@@ -7,14 +7,15 @@ set -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOPLEVEL="${DIR}/../"
 
-# Iterate over rust files not in the target directory
-for i in $(find "$TOPLEVEL" -path "$TOPLEVEL/target" -prune -o -iname "*.rs" -print) 
+# Iterate over files in the repo that satisfy the following rules
+# 1. File extension is one of .(move | rs | tsx | ts | js)
+# 2. File directory is not '$TOPLEVEL/target' or "**/build" or "**/node_modules"
+for i in $(find $TOPLEVEL  -type d \( -path '$TOPLEVEL/target' -o -name 'node_modules' -o -name 'build' \) -prune -o \( -iname '*.rs' -o -iname '*.move' -o -iname '*.tsx' -o -iname '*.ts' -o -iname '*.js' \) -print)
 do
-  CNT=$(head -n3 "$i" | grep -oEe '// (Copyright \(c\) 2022, Mysten Labs, Inc.|SPDX-License-Identifier: Apache-2.0)' | wc -l)
-  # echo "$i $CNT"
+  CNT=$(head -n3 "$i" | grep -oEe '// (Copyright \(c\) 2022, Mysten Labs, Inc.|SPDX-License-Identifier: Apache-2.0)' | wc -l) || true
   if [ "$CNT" -lt 2 ]
   then
-     echo "File $i has an incorrect license header"
-     exit 1
+    echo "File $i has an incorrect license header"
+    exit 1
   fi
 done
--- sui_programmability/examples/defi/tests/EscrowTests.move
+++ sui_programmability/examples/defi/tests/EscrowTests.move
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 #[test_only]
 module DeFi::EscrowTests {
     use Sui::ID::{Self, VersionedID};
