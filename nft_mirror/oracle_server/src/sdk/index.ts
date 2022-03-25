95e30449b72ac7b65641a2bafe575eb54fe714a6
Stella Cannefax <stella@mystenlabs.com>
2022-03-25 14:46:47 -0700

    add licenses for nft_mirror dir

--- nft_mirror/oracle_server/src/sdk/index.ts
+++ nft_mirror/oracle_server/src/sdk/index.ts
@@ -1,4 +1,7 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 export * from './gateway';
