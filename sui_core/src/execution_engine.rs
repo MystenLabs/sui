72e0279a2ee7143cac5b7a7f7570bbd2a3387112
George Danezis <gdanezis@users.noreply.github.com>
2022-03-29 21:54:22 +0100

    [Authority + Adapter] Add package caching, and remove redundant digest checks + clones (#963)
    * Added cache and keep digest checks
    
    Co-authored-by: George Danezis <george@danez.is>

--- sui_core/src/execution_engine.rs
+++ sui_core/src/execution_engine.rs
@@ -23,8 +23,8 @@ pub fn execute_transaction_to_effects<S: BackingPackageStore>(
     transaction: Transaction,
     transaction_digest: TransactionDigest,
     objects_by_kind: Vec<(InputObjectKind, Object)>,
-    move_vm: &Arc<adapter::MoveVM>,
-    native_functions: NativeFunctionTable,
+    move_vm: &Arc<adapter::SuiMoveVM>,
+    native_functions: &NativeFunctionTable,
 ) -> SuiResult<TransactionEffects> {
     let mut transaction_dependencies: BTreeSet<_> = objects_by_kind
         .iter()
@@ -64,8 +64,8 @@ fn execute_transaction<S: BackingPackageStore>(
     transaction: Transaction,
     mut objects_by_kind: Vec<(InputObjectKind, Object)>,
     tx_ctx: &mut TxContext,
-    move_vm: &Arc<adapter::MoveVM>,
-    native_functions: NativeFunctionTable,
+    move_vm: &Arc<adapter::SuiMoveVM>,
+    native_functions: &NativeFunctionTable,
 ) -> SuiResult<ExecutionStatus> {
     // unwraps here are safe because we built `inputs`
     let mut gas_object = objects_by_kind.pop().unwrap().1;
@@ -90,8 +90,8 @@ fn execute_transaction<S: BackingPackageStore>(
                 adapter::execute(
                     move_vm,
                     temporary_store,
-                    native_functions.clone(),
-                    package,
+                    native_functions,
+                    &package,
                     &c.module,
                     &c.function,
                     c.type_arguments.clone(),
