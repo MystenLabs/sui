7c30c0a99102e46c47d7009d700e0617b60e7662
Patrick Kuo <patrickkuo@me.com>
2022-04-05 01:11:32 +0100

    [rest gateway] - Align rest api to GatewayAPI + RestAPI rust wrapper + Lot of refactoring (#1123)
    * made bytes encoding consistent - encoding IDs with Hex and data bytes with Base64
    * added split-coin end point
    * added merge-coin end point
    * new `RestGatewayClient` - a rust wrapper for the RestAPI
    * add support of Rest gateway to the wallet - wallet can be configured to connect to the network directly, or via the rest gateway
    * removed start/stop/genesis from rest server
    * removed wallet functionalities from rest server
    * Genesis now creates a new `gateway.conf`, which can be used to start the rest gateway server.
    * CORS can be configured using env variable e.g. `ACCESS_CONTROL_ALLOW_ORIGIN=* ./rest_server`

---
+++ sui/src/unit_tests/sui_network.rs
@@ -0,0 +1,69 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::path::Path;
+
+use sui::config::{AuthorityPrivateInfo, Config, GenesisConfig, WalletConfig};
+use sui::gateway::{GatewayConfig, GatewayType};
+use sui::keystore::KeystoreType;
+use sui::sui_commands::{genesis, SuiNetwork};
+use sui::{SUI_NETWORK_CONFIG, SUI_WALLET_CONFIG};
+
+pub async fn start_test_network(
+    working_dir: &Path,
+    genesis_config: Option<GenesisConfig>,
+) -> Result<SuiNetwork, anyhow::Error> {
+    let working_dir = working_dir.to_path_buf();
+    let network_path = working_dir.join(SUI_NETWORK_CONFIG);
+    let wallet_path = working_dir.join(SUI_WALLET_CONFIG);
+    let keystore_path = working_dir.join("wallet.key");
+    let db_folder_path = working_dir.join("client_db");
+
+    let mut genesis_config =
+        genesis_config.unwrap_or(GenesisConfig::default_genesis(&working_dir)?);
+    let authorities = genesis_config
+        .authorities
+        .iter()
+        .map(|info| AuthorityPrivateInfo {
+            key_pair: info.key_pair.copy(),
+            host: info.host.clone(),
+            port: 0,
+            db_path: info.db_path.clone(),
+            stake: info.stake,
+        })
+        .collect();
+    genesis_config.authorities = authorities;
+
+    let (network_config, accounts, keystore) = genesis(genesis_config).await?;
+    let network = SuiNetwork::start(&network_config).await?;
+
+    let network_config = network_config.persisted(&network_path);
+    network_config.save()?;
+    keystore.save(&keystore_path)?;
+
+    let authorities = network_config.get_authority_infos();
+    let authorities = authorities
+        .into_iter()
+        .zip(&network.spawned_authorities)
+        .map(|(mut info, server)| {
+            info.base_port = server.get_port();
+            info
+        })
+        .collect();
+
+    // Create wallet config with stated authorities port
+    WalletConfig {
+        accounts,
+        keystore: KeystoreType::File(keystore_path),
+        gateway: GatewayType::Embedded(GatewayConfig {
+            db_folder_path,
+            authorities,
+            ..Default::default()
+        }),
+    }
+    .persisted(&wallet_path)
+    .save()?;
+
+    // Return network handle
+    Ok(network)
+}
