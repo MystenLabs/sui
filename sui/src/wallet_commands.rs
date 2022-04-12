MystenLabs/sui

commit 21ef23ea03e12150017a9a120ce0370e2e46c9d4

Switch to Clap. Bump Move version (#1312)
* Switch to Clap. Bump Move version

- Switch from structopt to clap v3
- Bump Move version

sui/src/wallet_commands.rs
@@ -8,13 +8,12 @@ use std::sync::{Arc, RwLock};
 use std::time::Instant;
 
 use anyhow::anyhow;
+use clap::*;
 use colored::Colorize;
 use move_core_types::identifier::Identifier;
 use move_core_types::language_storage::TypeTag;
 use move_core_types::parser::parse_type_tag;
 use serde::Serialize;
-use structopt::clap::AppSettings;
-use structopt::StructOpt;
 use tracing::info;
 
 use sui_core::gateway_state::gateway_responses::{
@@ -33,169 +32,167 @@ use crate::config::{Config, PersistedConfig, WalletConfig};
 use crate::keystore::Keystore;
 use crate::sui_json::{resolve_move_function_args, SuiJsonValue};
 
-#[derive(StructOpt)]
-#[structopt(name = "", rename_all = "kebab-case")]
-#[structopt(setting(AppSettings::NoBinaryName))]
+#[derive(Parser)]
+#[clap(name = "", rename_all = "kebab-case", no_binary_name = true)]
 pub struct WalletOpts {
-    #[structopt(subcommand)]
+    #[clap(subcommand)]
     pub command: WalletCommands,
     /// Returns command outputs in JSON format.
-    #[structopt(long, global = true)]
+    #[clap(long, global = true)]
     pub json: bool,
 }
 
 #[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
-#[structopt(setting(AppSettings::NoBinaryName))]
+#[clap(rename_all = "kebab-case", no_binary_name = true)]
 pub enum WalletCommands {
     /// Switch active address
-    #[structopt(name = "switch")]
+    #[clap(name = "switch")]
     Switch {
         /// Address to switch wallet commands to
-        #[structopt(long, parse(try_from_str = decode_bytes_hex))]
+        #[clap(long, parse(try_from_str = decode_bytes_hex))]
         address: SuiAddress,
     },
 
     /// Default address used for commands when none specified
-    #[structopt(name = "active-address")]
+    #[clap(name = "active-address")]
     ActiveAddress {},
 
     /// Get obj info
-    #[structopt(name = "object")]
+    #[clap(name = "object")]
     Object {
         /// Object ID of the object to fetch
-        #[structopt(long)]
+        #[clap(long)]
         id: ObjectID,
     },
 
     /// Publish Move modules
-    #[structopt(name = "publish")]
+    #[clap(name = "publish")]
     Publish {
         /// Path to directory containing a Move package
-        #[structopt(long)]
+        #[clap(long)]
         path: String,
 
         /// ID of the gas object for gas payment, in 20 bytes Hex string
         /// If not provided, a gas object with at least gas_budget value will be selected
-        #[structopt(long)]
+        #[clap(long)]
         gas: Option<ObjectID>,
 
         /// Gas budget for running module initializers
-        #[structopt(long)]
+        #[clap(long)]
         gas_budget: u64,
     },
 
     /// Call Move function
-    #[structopt(name = "call")]
+    #[clap(name = "call")]
     Call {
         /// Object ID of the package, which contains the module
-        #[structopt(long)]
+        #[clap(long)]
         package: ObjectID,
         /// The name of the module in the package
-        #[structopt(long)]
+        #[clap(long)]
         module: Identifier,
         /// Function name in module
-        #[structopt(long)]
+        #[clap(long)]
         function: Identifier,
         /// Function name in module
-        #[structopt(long, parse(try_from_str = parse_type_tag))]
+        #[clap(long, parse(try_from_str = parse_type_tag))]
         type_args: Vec<TypeTag>,
         /// Simplified ordered args like in the function syntax
         /// ObjectIDs, Addresses must be hex strings
-        #[structopt(long)]
+        #[clap(long)]
         args: Vec<SuiJsonValue>,
         /// ID of the gas object for gas payment, in 20 bytes Hex string
-        #[structopt(long)]
+        #[clap(long)]
         /// If not provided, a gas object with at least gas_budget value will be selected
-        #[structopt(long)]
+        #[clap(long)]
         gas: Option<ObjectID>,
         /// Gas budget for this call
-        #[structopt(long)]
+        #[clap(long)]
         gas_budget: u64,
     },
 
     /// Transfer an object
-    #[structopt(name = "transfer")]
+    #[clap(name = "transfer")]
     Transfer {
         /// Recipient address
-        #[structopt(long, parse(try_from_str = decode_bytes_hex))]
+        #[clap(long, parse(try_from_str = decode_bytes_hex))]
         to: SuiAddress,
 
         /// Object to transfer, in 20 bytes Hex string
-        #[structopt(long)]
+        #[clap(long)]
         object_id: ObjectID,
 
         /// ID of the gas object for gas payment, in 20 bytes Hex string
         /// If not provided, a gas object with at least gas_budget value will be selected
-        #[structopt(long)]
+        #[clap(long)]
         gas: Option<ObjectID>,
 
         /// Gas budget for this transfer
-        #[structopt(long)]
+        #[clap(long)]
         gas_budget: u64,
     },
     /// Synchronize client state with authorities.
-    #[structopt(name = "sync")]
+    #[clap(name = "sync")]
     SyncClientState {
-        #[structopt(long, parse(try_from_str = decode_bytes_hex))]
+        #[clap(long, parse(try_from_str = decode_bytes_hex))]
         address: Option<SuiAddress>,
     },
 
     /// Obtain the Addresses managed by the wallet.
-    #[structopt(name = "addresses")]
+    #[clap(name = "addresses")]
     Addresses,
 
     /// Generate new address and keypair.
-    #[structopt(name = "new-address")]
+    #[clap(name = "new-address")]
     NewAddress,
 
     /// Obtain all objects owned by the address.
-    #[structopt(name = "objects")]
+    #[clap(name = "objects")]
     Objects {
         /// Address owning the objects
-        #[structopt(long, parse(try_from_str = decode_bytes_hex))]
+        #[clap(long, parse(try_from_str = decode_bytes_hex))]
         address: Option<SuiAddress>,
     },
 
     /// Obtain all gas objects owned by the address.
-    #[structopt(name = "gas")]
+    #[clap(name = "gas")]
     Gas {
         /// Address owning the objects
-        #[structopt(long, parse(try_from_str = decode_bytes_hex))]
+        #[clap(long, parse(try_from_str = decode_bytes_hex))]
         address: Option<SuiAddress>,
     },
 
     /// Split a coin object into multiple coins.
     SplitCoin {
         /// Coin to Split, in 20 bytes Hex string
-        #[structopt(long)]
+        #[clap(long)]
         coin_id: ObjectID,
         /// Amount to split out from the coin
-        #[structopt(long)]
+        #[clap(long)]
         amounts: Vec<u64>,
         /// ID of the gas object for gas payment, in 20 bytes Hex string
         /// If not provided, a gas object with at least gas_budget value will be selected
-        #[structopt(long)]
+        #[clap(long)]
         gas: Option<ObjectID>,
         /// Gas budget for this call
-        #[structopt(long)]
+        #[clap(long)]
         gas_budget: u64,
     },
 
     /// Merge two coin objects into one coin
     MergeCoin {
         /// Coin to merge into, in 20 bytes Hex string
-        #[structopt(long)]
+        #[clap(long)]
         primary_coin: ObjectID,
         /// Coin to be merged, in 20 bytes Hex string
-        #[structopt(long)]
+        #[clap(long)]
         coin_to_merge: ObjectID,
         /// ID of the gas object for gas payment, in 20 bytes Hex string
         /// If not provided, a gas object with at least gas_budget value will be selected
-        #[structopt(long)]
+        #[clap(long)]
         gas: Option<ObjectID>,
         /// Gas budget for this call
-        #[structopt(long)]
+        #[clap(long)]
         gas_budget: u64,
     },
 }

scripts/move_dependency.py
@@ -25,7 +25,7 @@ def parse_args():
     )
     subparser.add_parser("local")
     upgrade = subparser.add_parser("upgrade")
-    upgrade.add_argument("--repo", required=False, default="diem")
+    upgrade.add_argument("--repo", required=False, default="move-language")
     upgrade_group = upgrade.add_mutually_exclusive_group(required=True)
     upgrade_group.add_argument("--rev")
     upgrade_group.add_argument("--branch")

sui/Cargo.toml
@@ -13,7 +13,6 @@ bytes = "1.1.0"
 futures = "0.3.21"
 serde = { version = "1.0.136", features = ["derive"] }
 serde_json = "1.0.79"
-structopt = "0.3.26"
 tempfile = "3.3.0"
 tokio = { version = "1.17.0", features = ["full"] }
 rand = "0.8.4"
@@ -33,7 +32,7 @@ serde-value = "0.7.0"
 serde-name = "0.2.0"
 log = "0.4.14"
 dirs = "4.0.0"
-clap = { version = "3", features = ["derive"] }
+clap = { version = "3.1.8", features = ["derive"] }
 telemetry_subscribers = { git = "https://github.com/MystenLabs/mysten-infra", rev = "a45af6dc28aa12c8cc13521d118c24aadd4c6adf" }
 
 bcs = "0.1.3"
@@ -54,12 +53,12 @@ http = "0.2.6"
 hyper = "0.14.17"
 schemars = "0.8.8"
 
-move-package = { git = "https://github.com/diem/move", rev = "63bd52e0f5b02711d29a678e6f946b3d429f2090" }
-move-core-types = { git = "https://github.com/diem/move", rev = "63bd52e0f5b02711d29a678e6f946b3d429f2090", features = ["address20"] }
-move-bytecode-verifier = { git = "https://github.com/diem/move", rev = "63bd52e0f5b02711d29a678e6f946b3d429f2090" }
-move-binary-format = { git = "https://github.com/diem/move", rev = "63bd52e0f5b02711d29a678e6f946b3d429f2090" }
-move-bytecode-utils = { git = "https://github.com/diem/move", rev = "63bd52e0f5b02711d29a678e6f946b3d429f2090" }
-move-unit-test = { git = "https://github.com/diem/move", rev = "63bd52e0f5b02711d29a678e6f946b3d429f2090" }
+move-package = { git = "https://github.com/move-language/move", rev = "2e7c37edada44436805e047dd26724a26c07635a" }
+move-core-types = { git = "https://github.com/move-language/move", rev = "2e7c37edada44436805e047dd26724a26c07635a", features = ["address20"] }
+move-bytecode-verifier = { git = "https://github.com/move-language/move", rev = "2e7c37edada44436805e047dd26724a26c07635a" }
+move-binary-format = { git = "https://github.com/move-language/move", rev = "2e7c37edada44436805e047dd26724a26c07635a" }
+move-bytecode-utils = { git = "https://github.com/move-language/move", rev = "2e7c37edada44436805e047dd26724a26c07635a" }
+move-unit-test = { git = "https://github.com/move-language/move", rev = "2e7c37edada44436805e047dd26724a26c07635a" }
 
 once_cell = "1.9.0"
 reqwest = { version = "0.11.10", features=["json","serde_json", "blocking"]}

sui/src/microbench.rs
@@ -6,13 +6,13 @@
 
 use anyhow::Error;
 use bytes::Bytes;
+use clap::*;
 use futures::stream::StreamExt;
 use move_core_types::account_address::AccountAddress;
 use move_core_types::ident_str;
 use rayon::prelude::*;
 use std::collections::{HashSet, VecDeque};
 use std::time::{Duration, Instant};
-use structopt::StructOpt;
 use sui_adapter::genesis;
 use sui_core::authority_client::AuthorityClient;
 use sui_core::{authority::*, authority_server::AuthorityServer};
@@ -38,52 +38,52 @@ use std::sync::Arc;
 use std::thread;
 use strum_macros::EnumString;
 
-#[derive(Debug, Clone, StructOpt)]
-#[structopt(
+#[derive(Debug, Clone, Parser)]
+#[clap(
     name = "Sui Microbenchmark",
     about = "Local test and microbenchmark of the Sui authorities"
 )]
 struct ClientServerBenchmark {
     /// Hostname
-    #[structopt(long, default_value = "127.0.0.1")]
+    #[clap(long, default_value = "127.0.0.1")]
     host: String,
     /// Path to the database
-    #[structopt(long, default_value = "")]
+    #[clap(long, default_value = "")]
     db_dir: String,
     /// Base port number
-    #[structopt(long, default_value = "9555")]
+    #[clap(long, default_value = "9555")]
     port: u16,
     /// Size of the Sui committee. Minimum size is 4 to tolerate one fault
-    #[structopt(long, default_value = "10")]
+    #[clap(long, default_value = "10")]
     committee_size: usize,
     /// Forces sending transactions one at a time
-    #[structopt(long)]
+    #[clap(long)]
     single_operation: bool,
     /// Number of transactions to be sent in the benchmark
-    #[structopt(long, default_value = "100000")]
+    #[clap(long, default_value = "100000")]
     num_transactions: usize,
     /// Timeout for sending queries (us)
-    #[structopt(long, default_value = "40000000")]
+    #[clap(long, default_value = "40000000")]
     send_timeout_us: u64,
     /// Timeout for receiving responses (us)
-    #[structopt(long, default_value = "40000000")]
+    #[clap(long, default_value = "40000000")]
     recv_timeout_us: u64,
     /// Maximum size of datagrams received and sent (bytes)
-    #[structopt(long, default_value = transport::DEFAULT_MAX_DATAGRAM_SIZE_STR)]
+    #[clap(long, default_value = transport::DEFAULT_MAX_DATAGRAM_SIZE_STR)]
     buffer_size: usize,
     /// Which execution path to track. TransactionsAndCerts or TransactionsOnly or CertsOnly
-    #[structopt(long, default_value = "TransactionsAndCerts")]
+    #[clap(long, default_value = "TransactionsAndCerts")]
     benchmark_type: BenchmarkType,
     /// Number of connections to the server
-    #[structopt(long, default_value = "0")]
+    #[clap(long, default_value = "0")]
     tcp_connections: usize,
     /// Number of database cpus
-    #[structopt(long, default_value = "1")]
+    #[clap(long, default_value = "1")]
     db_cpus: usize,
     /// Use Move orders
-    #[structopt(long)]
+    #[clap(long)]
     use_move: bool,
-    #[structopt(long, default_value = "2000")]
+    #[clap(long, default_value = "2000")]
     batch_size: usize,
 }
 #[derive(Debug, Clone, Copy, PartialEq, EnumString)]
@@ -107,7 +107,7 @@ fn main() {
         tracing_subscriber::fmt::Subscriber::builder().with_env_filter(env_filter);
     let subscriber = subscriber_builder.with_writer(std::io::stderr).finish();
     set_global_default(subscriber).expect("Failed to set subscriber");
-    let benchmark = ClientServerBenchmark::from_args();
+    let benchmark = ClientServerBenchmark::parse();
     assert!(
         benchmark.committee_size >= MIN_COMMITTEE_SIZE,
         "Found committee size of {:?}, but minimum committee size is {:?}",

sui/src/microbench_latency.rs
@@ -5,6 +5,7 @@
 
 use anyhow::Error;
 use bytes::Bytes;
+use clap::*;
 use futures::channel::mpsc::{channel as MpscChannel, Sender as MpscSender};
 use futures::stream::StreamExt;
 use futures::SinkExt;
@@ -12,7 +13,6 @@ use move_core_types::account_address::AccountAddress;
 use move_core_types::ident_str;
 use rayon::prelude::*;
 use std::time::{Duration, Instant};
-use structopt::StructOpt;
 use sui_adapter::genesis;
 use sui_core::{authority::*, authority_server::AuthorityServer};
 use sui_network::{network::NetworkClient, transport};
@@ -35,48 +35,48 @@ use std::path::Path;
 use std::sync::Arc;
 use std::thread;
 
-#[derive(Debug, Clone, StructOpt)]
-#[structopt(
+#[derive(Debug, Clone, Parser)]
+#[clap(
     name = "Sui Latency Microbenchmark",
     about = "Local test and latency microbenchmark of the Sui authorities"
 )]
 struct FixedRateBenchmark {
     /// Hostname
-    #[structopt(long, default_value = "127.0.0.1")]
+    #[clap(long, default_value = "127.0.0.1")]
     host: String,
     /// Base port number
-    #[structopt(long, default_value = "9550")]
+    #[clap(long, default_value = "9550")]
     port: u16,
     /// Size of the Sui committee. Minimum size is 4 to tolerate one fault
-    #[structopt(long, default_value = "10")]
+    #[clap(long, default_value = "10")]
     committee_size: usize,
 
     /// Timeout for sending queries (us)
-    #[structopt(long, default_value = "10000000")]
+    #[clap(long, default_value = "10000000")]
     send_timeout_us: u64,
     /// Timeout for receiving responses (us)
-    #[structopt(long, default_value = "10000000")]
+    #[clap(long, default_value = "10000000")]
     recv_timeout_us: u64,
     /// Maximum size of datagrams received and sent (bytes)
-    #[structopt(long, default_value = transport::DEFAULT_MAX_DATAGRAM_SIZE_STR)]
+    #[clap(long, default_value = transport::DEFAULT_MAX_DATAGRAM_SIZE_STR)]
     buffer_size: usize,
     /// Number of connections to the server
-    #[structopt(long, default_value = "0")]
+    #[clap(long, default_value = "0")]
     tcp_connections: usize,
-    #[structopt(long, default_value = "1")]
+    #[clap(long, default_value = "1")]
     db_cpus: usize,
     /// Use Move orders
-    #[structopt(long)]
+    #[clap(long)]
     use_move: bool,
 
     /// Number of chunks to send
-    #[structopt(long, default_value = "100")]
+    #[clap(long, default_value = "100")]
     num_chunks: usize,
     /// Size of chunks per tick
-    #[structopt(long, default_value = "1000")]
+    #[clap(long, default_value = "1000")]
     chunk_size: usize,
     /// The time between each tick. Default 10ms
-    #[structopt(long, default_value = "10000")]
+    #[clap(long, default_value = "10000")]
     period_us: u64,
 }
 
@@ -89,7 +89,7 @@ fn main() {
         tracing_subscriber::fmt::Subscriber::builder().with_env_filter(env_filter);
     let subscriber = subscriber_builder.with_writer(std::io::stderr).finish();
     set_global_default(subscriber).expect("Failed to set subscriber");
-    let benchmark = FixedRateBenchmark::from_args();
+    let benchmark = FixedRateBenchmark::parse();
     assert!(
         benchmark.committee_size >= MIN_COMMITTEE_SIZE,
         "Found committee size of {:?}, but minimum committee size is {:?}",

sui/src/rest_server.rs
@@ -7,6 +7,7 @@ use std::str::FromStr;
 use std::sync::Arc;
 
 use anyhow::anyhow;
+use clap::*;
 use dropshot::{endpoint, Query, TypedBody};
 use dropshot::{
     ApiDescription, ConfigDropshot, ConfigLogging, ConfigLoggingLevel, HttpError,
@@ -20,7 +21,6 @@ use move_core_types::parser::parse_type_tag;
 use move_core_types::value::MoveStructLayout;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use structopt::StructOpt;
 
 use sui::config::PersistedConfig;
 use sui::gateway::GatewayConfig;
@@ -49,26 +49,26 @@ const DEFAULT_REST_SERVER_ADDR_IPV4: &str = "127.0.0.1";
 #[cfg(test)]
 mod rest_server_tests;
 
-#[derive(StructOpt)]
-#[structopt(
+#[derive(Parser)]
+#[clap(
     name = "Sui Rest Gateway",
     about = "A Byzantine fault tolerant chain with low-latency finality and high throughput",
     rename_all = "kebab-case"
 )]
 struct RestServerOpt {
-    #[structopt(long)]
+    #[clap(long)]
     config: Option<PathBuf>,
 
-    #[structopt(long, default_value = DEFAULT_REST_SERVER_PORT)]
+    #[clap(long, default_value = DEFAULT_REST_SERVER_PORT)]
     port: u16,
 
-    #[structopt(long, default_value = DEFAULT_REST_SERVER_ADDR_IPV4)]
+    #[clap(long, default_value = DEFAULT_REST_SERVER_ADDR_IPV4)]
     host: Ipv4Addr,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let options: RestServerOpt = RestServerOpt::from_args();
+    let options: RestServerOpt = RestServerOpt::parse();
 
     let config_dropshot: ConfigDropshot = ConfigDropshot {
         bind_address: SocketAddr::from((options.host, options.port)),

sui/src/shell.rs
@@ -9,6 +9,7 @@ use std::io::Write;
 use std::sync::{Arc, RwLock};
 
 use async_trait::async_trait;
+use clap::*;
 use colored::Colorize;
 use rustyline::completion::{Completer, Pair};
 use rustyline::error::ReadlineError;
@@ -17,7 +18,6 @@ use rustyline::hint::Hinter;
 use rustyline::validate::Validator;
 use rustyline::{Config, Context, Editor};
 use rustyline_derive::Helper;
-use structopt::clap::{App, SubCommand};
 use unescape::unescape;
 
 #[path = "unit_tests/shell_tests.rs"]
@@ -171,16 +171,16 @@ fn substitute_env_variables(s: String) -> String {
     s
 }
 
-pub fn install_shell_plugins<'a>(clap: App<'a, 'a>) -> App<'a, 'a> {
+pub fn install_shell_plugins(clap: Command) -> Command {
     clap.subcommand(
-        SubCommand::with_name("exit")
+        Command::new("exit")
             .alias("quit")
             .about("Exit the interactive shell"),
     )
-    .subcommand(SubCommand::with_name("clear").about("Clear screen"))
-    .subcommand(SubCommand::with_name("echo").about("Write arguments to the console output"))
-    .subcommand(SubCommand::with_name("env").about("Print environment"))
-    .subcommand(SubCommand::with_name("history").about("Print history"))
+    .subcommand(Command::new("clear").about("Clear screen"))
+    .subcommand(Command::new("echo").about("Write arguments to the console output"))
+    .subcommand(Command::new("env").about("Print environment"))
+    .subcommand(Command::new("history").about("Print history"))
 }
 
 #[derive(Helper)]
@@ -260,21 +260,20 @@ pub struct CommandStructure {
 }
 
 impl CommandStructure {
-    /// Create CommandStructure using structopt::clap::App, currently only support 1 level of subcommands
-    pub fn from_clap(app: &App) -> Self {
+    /// Create CommandStructure using clap::Command, currently only support 1 level of subcommands
+    pub fn from_clap(app: &Command) -> Self {
         let subcommands = app
-            .p
-            .subcommands
-            .iter()
+            .get_subcommands()
             .map(|it| {
                 let name = it.get_name();
                 CommandStructure {
                     name: name.to_string(),
                     completions: it
-                        .p
-                        .opts
-                        .iter()
-                        .map(|it| format!("--{}", it.b.name))
+                        .get_opts()
+                        .map(|it| match it.get_long() {
+                            Some(long) => format!("--{}", long),
+                            None => format!("--{}", name),
+                        })
                         .collect::<Vec<_>>(),
                     children: vec![],
                 }

sui/src/sui.rs
@@ -2,21 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 extern crate core;
 
-use structopt::StructOpt;
+use clap::*;
 use sui::sui_commands::SuiCommand;
 
 #[cfg(test)]
 #[path = "unit_tests/cli_tests.rs"]
 mod cli_tests;
 
-#[derive(StructOpt)]
-#[structopt(
+#[derive(Parser)]
+#[clap(
     name = "Sui Local",
     about = "A Byzantine fault tolerant chain with low-latency finality and high throughput",
     rename_all = "kebab-case"
 )]
 struct SuiOpt {
-    #[structopt(subcommand)]
+    #[clap(subcommand)]
     command: SuiCommand,
 }
 
@@ -31,6 +31,6 @@ async fn main() -> Result<(), anyhow::Error> {
     #[allow(unused)]
     let guard = telemetry_subscribers::init(config);
 
-    let options: SuiOpt = SuiOpt::from_args();
+    let options: SuiOpt = SuiOpt::parse();
     options.command.execute().await
 }

commit 4dfac81ed481b8b4e7d2c1d77c5618f2ca7b6acb

[wallet] Fix clap commands
- Fix multiple values issue caused by #1312

sui/src/wallet_commands.rs
@@ -95,11 +95,16 @@ pub enum WalletCommands {
         #[clap(long)]
         function: Identifier,
         /// Function name in module
-        #[clap(long, parse(try_from_str = parse_type_tag))]
+        #[clap(
+            long,
+            parse(try_from_str = parse_type_tag),
+            multiple_occurrences = false,
+            multiple_values = true
+        )]
         type_args: Vec<TypeTag>,
         /// Simplified ordered args like in the function syntax
         /// ObjectIDs, Addresses must be hex strings
-        #[clap(long)]
+        #[clap(long, multiple_occurrences = false, multiple_values = true)]
         args: Vec<SuiJsonValue>,
         /// ID of the gas object for gas payment, in 20 bytes Hex string
         #[clap(long)]
@@ -168,7 +173,7 @@ pub enum WalletCommands {
         #[clap(long)]
         coin_id: ObjectID,
         /// Amount to split out from the coin
-        #[clap(long)]
+        #[clap(long, multiple_occurrences = false, multiple_values = true)]
         amounts: Vec<u64>,
         /// ID of the gas object for gas payment, in 20 bytes Hex string
         /// If not provided, a gas object with at least gas_budget value will be selected