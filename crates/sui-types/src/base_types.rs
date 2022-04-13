ab263c08a749acde4c45a5cb0b4412f16eca073b
George Danezis <george@danez.is>
2022-04-13 15:32:56 +0100

    Rebased to latest

--- crates/sui-types/src/base_types.rs
+++ crates/sui-types/src/base_types.rs
@@ -6,37 +6,36 @@ use std::collections::{HashMap, HashSet};
 
 use crate::crypto::PublicKeyBytes;
 use crate::error::SuiError;
-use ed25519_dalek::Digest;
 
 use hex::FromHex;
 use rand::Rng;
-use serde::{de::Error as _, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use std::borrow::Borrow;
-use std::collections::HashSet;
 
 use std::convert::{TryFrom, TryInto};
 use std::fmt;
 use std::str::FromStr;
 
-use crate::crypto::PublicKeyBytes;
-use crate::error::SuiError;
 use crate::sui_serde::Base64;
 use crate::sui_serde::Hex;
 use crate::sui_serde::Readable;
+
 use anyhow::anyhow;
 use base64ct::Encoding;
 use digest::Digest;
-use hex::FromHex;
+
 use move_core_types::account_address::AccountAddress;
 use move_core_types::ident_str;
 use move_core_types::identifier::IdentStr;
 use opentelemetry::{global, Context};
-use rand::Rng;
+
 use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
+
 use serde_with::serde_as;
 use serde_with::Bytes;
+
 use sha3::Sha3_256;
+
 
 #[cfg(test)]
 #[path = "unit_tests/base_types_tests.rs"]
@@ -207,6 +206,7 @@ pub struct TransactionEffectsDigest(
     #[serde_as(as = "Readable<Base64, Bytes>")] 
     pub [u8; TRANSACTION_DIGEST_LENGTH]
 );
+
 
 
 pub const TX_CONTEXT_MODULE_NAME: &IdentStr = ident_str!("TxContext");
