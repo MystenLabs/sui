9ca0158d2b57ac5036426667994ae5b7c9b0e9c3
Chris Li <666lcz@gmail.com>
2022-04-19 02:10:37 -0700

    Use into() for NamedObjectRef

--- sui/src/rest_gateway/responses.rs
+++ sui/src/rest_gateway/responses.rs
@@ -40,20 +40,22 @@ pub struct NamedObjectRef {
 }
 
 impl NamedObjectRef {
-    pub fn from((object_id, version, digest): ObjectRef) -> Self {
-        Self {
-            object_id: object_id.to_hex(),
-            version: version.value(),
-            digest: Base64::encode_string(digest.as_ref()),
-        }
-    }
-
     pub fn to_object_ref(self) -> Result<ObjectRef, anyhow::Error> {
         Ok((
             ObjectID::try_from(self.object_id)?,
             SequenceNumber::from(self.version),
             ObjectDigest::try_from(&*Base64::decode_vec(&self.digest).map_err(|e| anyhow!(e))?)?,
         ))
+    }
+}
+
+impl From<ObjectRef> for NamedObjectRef {
+    fn from((object_id, version, digest): ObjectRef) -> Self {
+        Self {
+            object_id: object_id.to_hex(),
+            version: version.value(),
+            digest: Base64::encode_string(digest.as_ref()),
+        }
     }
 }
 
@@ -163,7 +165,7 @@ impl TryFrom<ObjectRead> for GetObjectInfoResponse {
         match obj {
             ObjectRead::Exists(object_ref, object, layout) => {
                 Ok(Self::Exists(ObjectExistsResponse {
-                    object_ref: NamedObjectRef::from(object_ref),
+                    object_ref: object_ref.into(),
                     object: object.to_json(&layout).map_err(|e| {
                         custom_http_error(StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
                     })?,
@@ -172,7 +174,7 @@ impl TryFrom<ObjectRead> for GetObjectInfoResponse {
             ObjectRead::NotExists(object_id) => Ok(Self::NotExists(ObjectNotExistsResponse {
                 object_id: object_id.to_hex(),
             })),
-            ObjectRead::Deleted(obj_ref) => Ok(Self::Deleted(NamedObjectRef::from(obj_ref))),
+            ObjectRead::Deleted(obj_ref) => Ok(Self::Deleted(obj_ref.into())),
         }
     }
 }
