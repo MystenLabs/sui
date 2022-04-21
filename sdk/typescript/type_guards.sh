e1102f98a73b3b08e6430013f58c51d9dac4f90c
Stella Cannefax <stella@mystenlabs.com>
2022-04-21 04:03:03 -0700

    make type guards script less repetitive

--- sdk/typescript/type_guards.sh
+++ sdk/typescript/type_guards.sh
@@ -3,28 +3,25 @@
 # generate TS type guards for project
 npx ts-auto-guard --export-all src/rpc/client.ts src/**.ts
 
-second_arg=", _argumentName?: string"
-if [[ `uname` == "Darwin" ]]; then
-    # fix import of BN.js types
-    sed -i '' 's/"..\/node_modules\/@types\/bn";/"bn.js";/g' src/index.guard.ts
-    # remove the unsupported second argument from generated type guards
-    sed -i '' "s/${second_arg}//g" src/index.guard.ts
-    sed -i '' "s/${second_arg}//g" src/rpc/client.guard.ts
-else
-    # linux / GNU sed versions of same thing
-    sed -i 's/"..\/node_modules\/@types\/bn";/"bn.js";/g' src/index.guard.ts
-    sed -i "s/${second_arg}//g" src/index.guard.ts
-    sed -i "s/${second_arg}//g" src/rpc/client.guard.ts
-fi
-
 LICENSE="// Copyright (c) 2022, Mysten Labs, Inc.\n// SPDX-License-Identifier: Apache-2.0\n";
-index="src/index.guard.ts"
-# add license header to generated files
-echo -e ${LICENSE} | cat - ${index} > src/index.guard.temp.ts
-rm ${index}
-mv src/index.guard.temp.ts ${index}
+second_arg=", _argumentName?: string"
 
-client="src/rpc/client.guard.ts"
-echo -e ${LICENSE} | cat - ${client} > src/client.guard.temp.ts
-rm ${client}
-mv src/client.guard.temp.ts ${client}
+for FILE in src/index.guard.ts src/rpc/client.guard.ts
+do
+    if [[ `uname` == "Darwin" ]]; then
+        # fix import of BN.js types
+        sed -i '' 's/"..\/node_modules\/@types\/bn";/"bn.js";/g' ${FILE}
+        # remove the unsupported second argument from generated type guards
+        sed -i '' "s/${second_arg}//g" ${FILE}
+    else
+        # linux / GNU sed versions of same thing
+        sed -i 's/"..\/node_modules\/@types\/bn";/"bn.js";/g' ${FILE}
+        sed -i "s/${second_arg}//g" ${FILE}
+    fi
+
+    TEMP=${FILE}.temp
+    # add license header to generated file
+    echo -e ${LICENSE} | cat - ${FILE} > ${TEMP}
+    rm ${FILE}
+    mv ${TEMP} ${FILE}
+done
