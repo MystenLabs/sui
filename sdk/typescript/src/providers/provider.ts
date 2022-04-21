003193138c2999e2a59c5f15d64f14be5aa9c5c7
Chris Li <76067158+666lcz@users.noreply.github.com>
2022-04-21 00:24:10 -0700

    [ts-sdk] Implement getOwnedObjects (#1482)

--- sdk/typescript/src/providers/provider.ts
+++ sdk/typescript/src/providers/provider.ts
@@ -1,13 +1,19 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import type {
+  GetObjectInfoResponse,
+  ObjectRef,
+} from '../types/objects';
+import type {
+  CertifiedTransaction,
+  GatewayTxSeqNumber,
+  GetTxnDigestsResponse,
+  TransactionDigest,
+} from '../types/transactions';
+
 ///////////////////////////////
 // Exported Types
-export interface ObjectRef {
-  objectDigest: string;
-  objectId: string;
-  version: string;
-}
 
 export interface SignedTransaction {
   txBytes: string;
@@ -22,9 +28,48 @@ export type TransactionResponse = string;
 // Exported Abstracts
 export abstract class Provider {
   // Objects
-  abstract getObjectRefs(address: string): Promise<ObjectRef[]>;
+  /**
+   * Get all objects owned by an address
+   */
+  abstract getOwnedObjectRefs(address: string): Promise<ObjectRef[]>;
+
+  /**
+   * Get information about an object
+   */
+  abstract getObjectInfo(objectId: string): Promise<GetObjectInfoResponse>;
 
   // Transactions
+
+  /**
+   * Get Transaction Details from a digest
+   */
+  abstract getTransaction(
+    digest: TransactionDigest
+  ): Promise<CertifiedTransaction>;
+
+  /**
+   * Get transaction digests for a given range
+   *
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getTransactionDigestsInRange(
+    start: GatewayTxSeqNumber,
+    end: GatewayTxSeqNumber
+  ): Promise<GetTxnDigestsResponse>;
+
+  /**
+   * Get the latest `count` transactions
+   *
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getRecentTransactions(count: number): Promise<GetTxnDigestsResponse>;
+
+  /**
+   * Get total number of transactions
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getTotalTransactionNumber(): Promise<number>;
+
   abstract executeTransaction(
     txn: SignedTransaction
   ): Promise<TransactionResponse>;
