0f827c7d0b00a65b02c33780896ef067b41aacd0
Chris Li <76067158+666lcz@users.noreply.github.com>
2022-04-21 00:24:10 -0700

    [ts-sdk] Implement getOwnedObjects (#1482)

--- sdk/typescript/src/providers/json-rpc-provider.ts
+++ sdk/typescript/src/providers/json-rpc-provider.ts
@@ -1,16 +1,26 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { Provider, SignedTransaction, TransactionResponse } from './provider';
+import { JsonRpcClient } from '../rpc/client';
+import { array, number, type as pick } from 'superstruct';
 import {
-  Provider,
+  GetObjectInfoResponse,
+  GetObjectInfoResponseSchema,
   ObjectRef,
-  SignedTransaction,
-  TransactionResponse,
-} from './provider';
+  ObjectRefSchema,
+} from '../types/objects';
+import {
+  CertifiedTransaction,
+  CertifiedTransactionSchema,
+  GatewayTxSeqNumber,
+  GetTxnDigestsResponse,
+  GetTxnDigestsResponseSchema,
+  TransactionDigest,
+} from '../types/transactions';
 
 export class JsonRpcProvider extends Provider {
-  //@ts-ignore
-  private endpointURL: string;
+  private client: JsonRpcClient;
 
   /**
    * Establish a connection to a Sui Gateway endpoint
@@ -19,20 +29,102 @@ export class JsonRpcProvider extends Provider {
    */
   constructor(endpoint: string) {
     super();
-    this.endpointURL = endpoint;
+    this.client = new JsonRpcClient(endpoint);
   }
 
   // Objects
-  async getObjectRefs(_address: string): Promise<ObjectRef[]> {
-    // TODO: implement the function with a RPC client
-    return [];
+  async getOwnedObjectRefs(address: string): Promise<ObjectRef[]> {
+    try {
+      const resp = await this.client.requestWithValidation(
+        'sui_getOwnedObjects',
+        [address],
+        pick({ objects: array(ObjectRefSchema) })
+      );
+      return resp.objects;
+    } catch (err) {
+      throw new Error(
+        `Error fetching owned object refs: ${err} for address ${address}`
+      );
+    }
+  }
+
+  async getObjectInfo(objectId: string): Promise<GetObjectInfoResponse> {
+    try {
+      const resp = await this.client.requestWithValidation(
+        'sui_getObjectTypedInfo',
+        [objectId],
+        GetObjectInfoResponseSchema
+      );
+      return resp;
+    } catch (err) {
+      throw new Error(`Error fetching object info: ${err} for id ${objectId}`);
+    }
   }
 
   // Transactions
+  async getTransaction(
+    digest: TransactionDigest
+  ): Promise<CertifiedTransaction> {
+    try {
+      const resp = await this.client.requestWithValidation(
+        'sui_getTransaction',
+        [digest],
+        CertifiedTransactionSchema
+      );
+      return resp;
+    } catch (err) {
+      throw new Error(`Error getting transaction: ${err} for digest ${digest}`);
+    }
+  }
+
   async executeTransaction(
     _txn: SignedTransaction
   ): Promise<TransactionResponse> {
     throw new Error('Method not implemented.');
+  }
+
+  async getTotalTransactionNumber(): Promise<number> {
+    try {
+      const resp = await this.client.requestWithValidation(
+        'sui_getTotalTransactionNumber',
+        [],
+        number()
+      );
+      return resp;
+    } catch (err) {
+      throw new Error(`Error fetching total transaction number: ${err}`);
+    }
+  }
+
+  async getTransactionDigestsInRange(
+    start: GatewayTxSeqNumber,
+    end: GatewayTxSeqNumber
+  ): Promise<GetTxnDigestsResponse> {
+    try {
+      return await this.client.requestWithValidation(
+        'sui_getTransactionsInRange',
+        [start, end],
+        GetTxnDigestsResponseSchema
+      );
+    } catch (err) {
+      throw new Error(
+        `Error fetching transaction digests in range: ${err} for range ${start}-${end}`
+      );
+    }
+  }
+
+  async getRecentTransactions(count: number): Promise<GetTxnDigestsResponse> {
+    try {
+      return await this.client.requestWithValidation(
+        'sui_getRecentTransactions',
+        [count],
+        GetTxnDigestsResponseSchema
+      );
+    } catch (err) {
+      throw new Error(
+        `Error fetching recent transactions: ${err} for count ${count}`
+      );
+    }
   }
 
   // TODO: add more interface methods
--- sdk/typescript/src/providers/void-provider.ts
+++ sdk/typescript/src/providers/void-provider.ts
@@ -1,24 +1,51 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { GetObjectInfoResponse, ObjectRef } from '../types/objects';
 import {
-  Provider,
-  ObjectRef,
-  SignedTransaction,
-  TransactionResponse,
-} from './provider';
+  CertifiedTransaction,
+  GatewayTxSeqNumber,
+  GetTxnDigestsResponse,
+  TransactionDigest,
+} from '../types/transactions';
+import { Provider, SignedTransaction, TransactionResponse } from './provider';
 
 export class VoidProvider extends Provider {
   // Objects
-  async getObjectRefs(_address: string): Promise<ObjectRef[]> {
-    throw this.newError('getObjectRefs');
+  async getOwnedObjectRefs(_address: string): Promise<ObjectRef[]> {
+    throw this.newError('getOwnedObjectRefs');
+  }
+
+  async getObjectInfo(_objectId: string): Promise<GetObjectInfoResponse> {
+    throw this.newError('getObjectInfo');
   }
 
   // Transactions
+  async getTransaction(
+    _digest: TransactionDigest
+  ): Promise<CertifiedTransaction> {
+    throw this.newError('getTransaction');
+  }
+
   async executeTransaction(
     _txn: SignedTransaction
   ): Promise<TransactionResponse> {
     throw this.newError('executeTransaction');
+  }
+
+  async getTotalTransactionNumber(): Promise<number> {
+    throw this.newError('getTotalTransactionNumber');
+  }
+
+  async getTransactionDigestsInRange(
+    _start: GatewayTxSeqNumber,
+    _end: GatewayTxSeqNumber
+  ): Promise<GetTxnDigestsResponse> {
+    throw this.newError('getTransactionDigestsInRange');
+  }
+
+  async getRecentTransactions(_count: number): Promise<GetTxnDigestsResponse> {
+    throw this.newError('getRecentTransactions');
   }
 
   private newError(operation: string): Error {
