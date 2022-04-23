8b1779e0b04019bb6765e9b0ff1c8814ac8a2626
Xun Li <lxfind@gmail.com>
2022-04-23 16:19:20 -0700

    Make get_parent_iterator() return iterator (#1559)

--- sui_core/src/authority/authority_store.rs
+++ sui_core/src/authority/authority_store.rs
@@ -339,7 +339,7 @@ impl<const ALL_OBJ_VER: bool, S: Eq + Serialize + for<'de> Deserialize<'de>>
         &self,
         object_id: ObjectID,
         seq: Option<SequenceNumber>,
-    ) -> Result<Vec<(ObjectRef, TransactionDigest)>, SuiError> {
+    ) -> Result<impl Iterator<Item = (ObjectRef, TransactionDigest)> + '_, SuiError> {
         let seq_inner = seq.unwrap_or_else(|| SequenceNumber::from(0));
         let obj_dig_inner = ObjectDigest::new([0; 32]);
 
@@ -348,14 +348,13 @@ impl<const ALL_OBJ_VER: bool, S: Eq + Serialize + for<'de> Deserialize<'de>>
             .iter()
             // The object id [0; 16] is the smallest possible
             .skip_to(&(object_id, seq_inner, obj_dig_inner))?
-            .take_while(|((id, iseq, _digest), _txd)| {
+            .take_while(move |((id, iseq, _digest), _txd)| {
                 let mut flag = id == &object_id;
-                if seq.is_some() {
-                    flag &= seq_inner == *iseq;
+                if let Some(seq_num) = seq {
+                    flag &= seq_num == *iseq;
                 }
                 flag
-            })
-            .collect())
+            }))
     }
 
     /// Read a lock for a specific (transaction, shared object) pair.
