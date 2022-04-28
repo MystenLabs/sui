27dff728a4c9cb65cd5d92a574105df20cb51887
Xun Li <lxfind@gmail.com>
2022-04-27 21:48:38 -0700

    Support take immutable and shared in separate APIs in test scenario (#1558)

--- sui_programmability/framework/src/natives/test_scenario.rs
+++ sui_programmability/framework/src/natives/test_scenario.rs
@@ -28,21 +28,23 @@ type Event = (Vec<u8>, u64, Type, MoveTypeLayout, Value);
 const WRAPPED_OBJECT_EVENT: u64 = 255;
 const UPDATE_OBJECT_EVENT: u64 = 254;
 
-/// Trying to transfer an unowned object, which is not allowed.
-/// This won't be detected right away when a transfer is happening.
-/// Instead, it's detected when processing the inventory events.
-const ETRANSFER_UNOWNED_OBJECT: u64 = 100;
-
-/// Trying to mutating an immutable object.
-/// This is detected when returning an immutable object back to
-/// the inventory.
-const EMUTATING_IMMUTABLE_OBJECT: u64 = 101;
+/// When transfer an object to a parent object, the parent object
+/// is not found in the inventory.
+const EPARENT_OBJECT_NOT_FOUND: u64 = 100;
 
 #[derive(Debug)]
 struct OwnedObj {
     value: Value,
     type_: Type,
+    /// Owner is the direct owner of the object.
     owner: Owner,
+    /// Signer is the ultimate owner of the object potentially through
+    /// chains of object ownership.
+    /// e.g. If account A ownd object O1, O1 owns O2. Then
+    /// O2's owner is O1, and signer is A.
+    /// signer will always be set eventually, but it needs to be optional first
+    /// since we may not know its signer initially.
+    signer: Option<Owner>,
 }
 
 /// Set of all live objects in the current test scenario
@@ -74,10 +76,22 @@ fn get_object_id_from_event(event_type_byte: u64, val: &Value) -> Option<ObjectI
     Some(ObjectID::try_from(address.value_as::<AccountAddress>().unwrap().as_slice()).unwrap())
 }
 
+fn account_to_sui_address(address: AccountAddress) -> SuiAddress {
+    SuiAddress::try_from(address.as_slice()).unwrap()
+}
+
 /// Process the event log to determine the global set of live objects
 /// Returns the abort_code if an error is encountered.
 fn get_global_inventory(events: &[Event]) -> Result<Inventory, u64> {
     let mut inventory = Inventory::new();
+    // Since we allow transfer object to ID, it's possible that when we transfer
+    // an object to a parenet object, the parent object does not yet exist in the event log.
+    // And without the parent object we cannot know the ultimate signer.
+    // To handle this, for child objects whose parent is not yet known, we add them
+    // to the unresolved_signer_parents map, which maps from parent object ID
+    // to the list of child objects it has. Whenever a new object is seen, we check the map
+    // and resolve if the object is an unresolved parent.
+    let mut unresolved_signer_parents: BTreeMap<ObjectID, BTreeSet<ObjectID>> = BTreeMap::new();
     for (recipient, event_type_byte, type_, _layout, val) in events {
         let obj_id = if let Some(obj_id) = get_object_id_from_event(*event_type_byte, val) {
             obj_id
@@ -93,9 +107,6 @@ fn get_global_inventory(events: &[Event]) -> Result<Inventory, u64> {
         if *event_type_byte == UPDATE_OBJECT_EVENT {
             if let Some(cur) = inventory.get_mut(&obj_id) {
                 let new_value = val.copy_value().unwrap();
-                if cur.owner == Owner::Immutable && !cur.value.equals(&new_value).unwrap() {
-                    return Err(EMUTATING_IMMUTABLE_OBJECT);
-                }
                 // Update the object content since it may have been mutated.
                 cur.value = new_value;
             }
@@ -108,7 +119,28 @@ fn get_global_inventory(events: &[Event]) -> Result<Inventory, u64> {
             | EventType::TransferToObject
             | EventType::FreezeObject
             | EventType::ShareObject => {
-                let owner = get_new_owner(&inventory, &obj_id, event_type, recipient.clone())?;
+                let owner = get_new_owner(&event_type, recipient.clone());
+                let signer = if event_type == EventType::TransferToObject {
+                    let parent_id = ObjectID::try_from(recipient.as_slice()).unwrap();
+                    if let Some(parent_obj) = inventory.get(&parent_id) {
+                        parent_obj.signer
+                    } else {
+                        unresolved_signer_parents
+                            .entry(parent_id)
+                            .or_default()
+                            .insert(obj_id);
+                        None
+                    }
+                } else {
+                    Some(owner)
+                };
+                if signer.is_some() {
+                    if let Some(children) = unresolved_signer_parents.remove(&obj_id) {
+                        for child in children {
+                            inventory.get_mut(&child).unwrap().signer = signer;
+                        }
+                    }
+                }
                 // note; may overwrite older values of the object, which is intended
                 inventory.insert(
                     obj_id,
@@ -116,6 +148,7 @@ fn get_global_inventory(events: &[Event]) -> Result<Inventory, u64> {
                         value: Value::copy_value(val).unwrap(),
                         type_: type_.clone(),
                         owner,
+                        signer,
                     },
                 );
             }
@@ -126,22 +159,16 @@ fn get_global_inventory(events: &[Event]) -> Result<Inventory, u64> {
             EventType::User => (),
         }
     }
-    Ok(inventory)
+    if unresolved_signer_parents.is_empty() {
+        Ok(inventory)
+    } else {
+        Err(EPARENT_OBJECT_NOT_FOUND)
+    }
 }
 
-fn get_new_owner(
-    inventory: &Inventory,
-    obj_id: &ObjectID,
-    event_type: EventType,
-    recipient: Vec<u8>,
-) -> Result<Owner, u64> {
-    if let Some(existing) = inventory.get(obj_id) {
-        if !existing.owner.is_owned() {
-            // Unowned objects are not allowed to be transferred.
-            return Err(ETRANSFER_UNOWNED_OBJECT);
-        }
-    }
-    Ok(match event_type {
+/// Return the new owner of the object after the transfer event.
+fn get_new_owner(event_type: &EventType, recipient: Vec<u8>) -> Owner {
+    match event_type {
         EventType::FreezeObject => Owner::Immutable,
         EventType::ShareObject => Owner::Shared,
         EventType::TransferToAddress => {
@@ -149,35 +176,33 @@ fn get_new_owner(
         }
         EventType::TransferToObject => Owner::ObjectOwner(SuiAddress::try_from(recipient).unwrap()),
         _ => panic!("Unrecognized event_type"),
-    })
+    }
 }
 
 /// Get the objects of type `type_` that can be spent by `addr`
 /// Returns the abort_code if an error is encountered.
 fn get_inventory_for(
-    addr: &AccountAddress,
+    signer: Owner,
+    parent_object: Option<AccountAddress>,
     type_: &Type,
     tx_end_index: usize,
     events: &[Event],
 ) -> Result<Vec<Value>, u64> {
     let inventory = get_global_inventory(&events[..tx_end_index])?;
-    let sui_addr = SuiAddress::try_from(addr.to_vec()).unwrap();
     Ok(inventory
         .into_iter()
-        .filter_map(|(_, obj)| {
-            // TODO: We should also be able to include objects indirectly owned by the
-            // requested address through owning other objects.
-            // https://github.com/MystenLabs/sui/issues/673
-            if (obj.owner == Owner::AddressOwner(sui_addr)
-                || obj.owner == Owner::ObjectOwner(sui_addr)
-                || !obj.owner.is_owned())
-                && &obj.type_ == type_
-            {
-                Some(obj.value)
-            } else {
-                None
-            }
+        .filter(|(_, obj)| {
+            &obj.type_ == type_
+                && if let Some(parent) = parent_object {
+                    let obj_signer = obj.signer.unwrap();
+                    obj.owner
+                        == Owner::ObjectOwner(SuiAddress::try_from(parent.as_slice()).unwrap())
+                        && (!obj_signer.is_owned() || obj_signer == signer)
+                } else {
+                    obj.owner == signer
+                }
         })
+        .map(|(_, obj)| obj.value)
         .collect())
 }
 
@@ -241,7 +266,7 @@ pub fn num_events(
 }
 
 /// Return all the values of type `T` in the inventory of `owner_address`
-pub fn get_inventory(
+pub fn get_account_owned_inventory(
     context: &mut NativeContext,
     ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
@@ -253,7 +278,67 @@ pub fn get_inventory(
     let owner_address = pop_arg!(args, AccountAddress);
 
     let cost = native_gas(context.cost_table(), NativeCostIndex::EMIT_EVENT, 0);
-    match get_inventory_for(&owner_address, &ty_args[0], tx_end_index, context.events()) {
+    match get_inventory_for(
+        Owner::AddressOwner(account_to_sui_address(owner_address)),
+        None,
+        &ty_args[0],
+        tx_end_index,
+        context.events(),
+    ) {
+        Ok(inventory) => Ok(NativeResult::ok(
+            cost,
+            smallvec![Value::vector_for_testing_only(inventory)],
+        )),
+        Err(abort_code) => Ok(NativeResult::err(cost, abort_code)),
+    }
+}
+
+pub fn get_unowned_inventory(
+    context: &mut NativeContext,
+    ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert_eq!(ty_args.len(), 1);
+    debug_assert_eq!(args.len(), 2);
+
+    let tx_end_index = pop_arg!(args, u64) as usize;
+    let immutable = pop_arg!(args, bool);
+    let owner = if immutable {
+        Owner::Immutable
+    } else {
+        Owner::Shared
+    };
+
+    let cost = native_gas(context.cost_table(), NativeCostIndex::EMIT_EVENT, 0);
+    match get_inventory_for(owner, None, &ty_args[0], tx_end_index, context.events()) {
+        Ok(inventory) => Ok(NativeResult::ok(
+            cost,
+            smallvec![Value::vector_for_testing_only(inventory)],
+        )),
+        Err(abort_code) => Ok(NativeResult::err(cost, abort_code)),
+    }
+}
+
+pub fn get_object_owned_inventory(
+    context: &mut NativeContext,
+    ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert_eq!(ty_args.len(), 1);
+    debug_assert_eq!(args.len(), 3);
+
+    let tx_end_index = pop_arg!(args, u64) as usize;
+    let parent_object = pop_arg!(args, AccountAddress);
+    let signer_address = pop_arg!(args, AccountAddress);
+
+    let cost = native_gas(context.cost_table(), NativeCostIndex::EMIT_EVENT, 0);
+    match get_inventory_for(
+        Owner::AddressOwner(account_to_sui_address(signer_address)),
+        Some(parent_object),
+        &ty_args[0],
+        tx_end_index,
+        context.events(),
+    ) {
         Ok(inventory) => Ok(NativeResult::ok(
             cost,
             smallvec![Value::vector_for_testing_only(inventory)],
