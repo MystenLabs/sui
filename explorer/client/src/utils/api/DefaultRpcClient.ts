d7407983995c61c187a781fe0fd8b5b3aabdcdd3
Stella Cannefax <stella@mystenlabs.com>
2022-05-04 01:17:34 -0500

    lint changes

--- explorer/client/src/utils/api/DefaultRpcClient.ts
+++ explorer/client/src/utils/api/DefaultRpcClient.ts
@@ -10,9 +10,7 @@ const LOCAL = 'http://127.0.0.1:5001';
 const DEVNET = 'https://gateway.devnet.sui.io:9000';
 
 let rpcUrl;
-if(useLocal)
-    rpcUrl = LOCAL;
-else
-    rpcUrl = tryGetRpcSetting() ?? DEVNET;
+if (useLocal) rpcUrl = LOCAL;
+else rpcUrl = tryGetRpcSetting() ?? DEVNET;
 
 export const DefaultRpcClient = new JsonRpcProvider(rpcUrl);
