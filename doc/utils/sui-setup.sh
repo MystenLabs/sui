5d593efa9ba08e3cf172b5933f96b95bb013d568
Clay-Mysten <100217682+Clay-Mysten@users.noreply.github.com>
2022-05-10 16:24:39 -0700

    Create sui-setup.sh startup script for Cargo install through genesis (#1819)
    * Create sui-setup.sh
    
    startup script for Cargo install through genesis
    
    * Update sui-setup.sh
    
    Replace cargo installation with confirmation and link to doc for install
    
    * Update sui-setup.sh
    
    Add directory removal
    
    * Update sui-setup.sh
    
    Make path to script explicit
    
    * Update sui-setup.sh
    
    Make doc comments better still
    
    * Update sui-setup.sh
    
    Display recommendation for manual install of VSCode extension in output
    
    * Update sui-setup.sh
    
    Attempting to add standard license header

---
+++ doc/utils/sui-setup.sh
@@ -0,0 +1,33 @@
+# Copyright (c) 2022, Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#!/bin/bash
+# Prereqs: Rust Cargo, Git CLI, and GitHub account
+# Usage: set up environment for Sui development
+# Run `sui-setup.sh` in the directory to download source
+shopt -s nullglob
+set -e
+set -o pipefail
+
+## Confirm or get Cargo with Rust toolchain
+command -v cargo >/dev/null 2>&1 || { echo "Cargo (https://doc.rust-lang.org/cargo/getting-started/installation.html) is not installed or missing from PATH, exiting."; return 1; }
+
+## Remove sui directory if it already exists
+rm -rf sui/
+
+## Build and install Sui binaries
+cargo install --locked --git https://github.com/MystenLabs/sui.git --branch "devnet" sui
+
+## Install Move Analyzer language server plugin
+cargo install --git https://github.com/move-language/move move-analyzer
+
+## Download Sui source code
+git clone https://github.com/MystenLabs/sui.git
+
+## Create Wallet configuration
+sui genesis --force
+
+## Recommend manual install of VSCode extension
+echo "Now install the Move Analyzer VSCode extension per: https://marketplace.visualstudio.com/items?itemName=move.move-analyzer"
+
+# unset it now
+shopt -u nullglob
