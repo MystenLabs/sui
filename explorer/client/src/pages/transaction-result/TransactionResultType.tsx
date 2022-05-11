e14ed88404d1c875dd458519fa8c92acce1580d5
Andrew Burnie <apburnie@hotmail.co.uk>
2022-05-11 11:08:19 +0100

    implements license headers

--- explorer/client/src/pages/transaction-result/TransactionResultType.tsx
+++ explorer/client/src/pages/transaction-result/TransactionResultType.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import type {
     CertifiedTransaction,
     ExecutionStatusType,
--- explorer/client/src/utils/envUtil.ts
+++ explorer/client/src/utils/envUtil.ts
@@ -1,1 +1,4 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 export const IS_STATIC_ENV = process.env.REACT_APP_DATA === 'static';
