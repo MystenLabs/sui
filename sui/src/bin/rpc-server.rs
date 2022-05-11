48ea2fbc591dfacde5713f7221c0570d9027387b
Evan Chan <velvia@gmail.com>
2022-05-10 21:34:49 -0700

    Add RPC server API metrics

--- sui/src/bin/rpc-server.rs
+++ sui/src/bin/rpc-server.rs
@@ -6,10 +6,15 @@ use jsonrpsee::{
     http_server::{AccessControlBuilder, HttpServerBuilder},
     RpcModule,
 };
+use jsonrpsee_core::middleware::Middleware;
+use prometheus_exporter::prometheus::{
+    register_histogram_vec, register_int_counter_vec, HistogramVec, IntCounterVec,
+};
 use std::{
     env,
     net::{IpAddr, Ipv4Addr, SocketAddr},
     path::PathBuf,
+    time::Instant,
 };
 use sui::{
     config::sui_config_dir,
@@ -72,6 +77,7 @@ async fn main() -> anyhow::Result<()> {
 
     let server = server_builder
         .set_access_control(acl)
+        .set_middleware(JsonRpcMetrics::new())
         .build(SocketAddr::new(IpAddr::V4(options.host), options.port))
         .await?;
 
@@ -95,4 +101,58 @@ async fn main() -> anyhow::Result<()> {
 
     server_handle.await;
     Ok(())
+}
+
+#[derive(Clone)]
+struct JsonRpcMetrics {
+    /// Counter of requests, route is a label (ie separate timeseries per route)
+    requests_by_route: IntCounterVec,
+    /// Request latency, route is a label
+    req_latency_by_route: HistogramVec,
+    /// Failed requests by route
+    errors_by_route: IntCounterVec,
+}
+
+impl JsonRpcMetrics {
+    pub fn new() -> Self {
+        Self {
+            requests_by_route: register_int_counter_vec!(
+                "rpc_requests_by_route",
+                "Number of requests by route",
+                &["route"]
+            )
+            .unwrap(),
+            req_latency_by_route: register_histogram_vec!(
+                "req_latency_by_route",
+                "Latency of a request by route",
+                &["route"]
+            )
+            .unwrap(),
+            errors_by_route: register_int_counter_vec!(
+                "errors_by_route",
+                "Number of errors by route",
+                &["route"]
+            )
+            .unwrap(),
+        }
+    }
+}
+
+impl Middleware for JsonRpcMetrics {
+    type Instant = Instant;
+
+    fn on_request(&self) -> Instant {
+        Instant::now()
+    }
+
+    fn on_result(&self, name: &str, success: bool, started_at: Instant) {
+        self.requests_by_route.with_label_values(&[name]).inc();
+        let req_latency_secs = (Instant::now() - started_at).as_secs_f64();
+        self.req_latency_by_route
+            .with_label_values(&[name])
+            .observe(req_latency_secs);
+        if !success {
+            self.errors_by_route.with_label_values(&[name]).inc();
+        }
+    }
 }
