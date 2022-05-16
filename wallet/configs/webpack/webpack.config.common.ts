505518ca2d17b1a4791c1a5ddd4e91872c59967e
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-05-16 11:56:11 +0100

    wallet-ext: path aliases

--- wallet/configs/webpack/webpack.config.common.ts
+++ wallet/configs/webpack/webpack.config.common.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { exec } from 'child_process';
 import CopyPlugin from 'copy-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
@@ -15,83 +16,136 @@ const PROJECT_ROOT = resolve(__dirname, '..', '..');
 const CONFIGS_ROOT = resolve(PROJECT_ROOT, 'configs');
 const SRC_ROOT = resolve(PROJECT_ROOT, 'src');
 const OUTPUT_ROOT = resolve(PROJECT_ROOT, 'dist');
+const TS_CONFIGS_ROOT = resolve(CONFIGS_ROOT, 'ts');
+const TS_CONFIG_FILE = resolve(
+    TS_CONFIGS_ROOT,
+    `tsconfig.${process.env.NODE_ENV === 'development' ? 'dev' : 'prod'}.json`
+);
 
-const tsConfigFilename = `tsconfig.${
-    process.env.NODE_ENV === 'development' ? 'dev' : 'prod'
-}.json`;
+function loadTsConfig(tsConfigFilePath: string) {
+    return new Promise<string>((res, rej) => {
+        exec(
+            `${resolve(
+                PROJECT_ROOT,
+                'node_modules',
+                '.bin',
+                'tsc'
+            )} -p ${tsConfigFilePath} --showConfig`,
+            (error, stdout, stderr) => {
+                if (error || stderr) {
+                    rej(error || stderr);
+                }
+                res(stdout);
+            }
+        );
+    }).then(
+        (tsContent) => JSON.parse(tsContent),
+        (e) => {
+            // eslint-disable-next-line no-console
+            console.error(e);
+            throw e;
+        }
+    );
+}
 
-const commonConfig: Configuration = {
-    context: SRC_ROOT,
-    entry: {
-        background: './background',
-        ui: './ui',
-    },
-    output: {
-        path: OUTPUT_ROOT,
-        clean: true,
-    },
-    stats: {
-        preset: 'summary',
-        timings: true,
-        errors: true,
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(t|j)sx?$/,
-                loader: 'ts-loader',
-                options: {
-                    configFile: resolve(CONFIGS_ROOT, 'ts', tsConfigFilename),
-                },
-            },
-            {
-                test: /\.(s)?css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            chunks: ['ui'],
-            filename: 'ui.html',
-            template: resolve(SRC_ROOT, 'ui', 'index.template.html'),
-            title: APP_NAME,
-        }),
-        new CopyPlugin({
-            patterns: [
+async function generateAliasFromTs() {
+    const tsConfigJSON = await loadTsConfig(TS_CONFIG_FILE);
+    const {
+        compilerOptions: { paths, baseUrl = './' },
+    } = tsConfigJSON;
+    const alias: Record<string, string> = {};
+    if (paths) {
+        Object.keys(paths).forEach((anAlias) => {
+            const aliasPath = paths[anAlias][0];
+            const adjAlias = anAlias.replace(/\/\*$/gi, '');
+            const adjPath = (
+                aliasPath.startsWith('./') || aliasPath.startsWith('../')
+                    ? resolve(TS_CONFIGS_ROOT, baseUrl, aliasPath)
+                    : aliasPath
+            ).replace(/\/\*$/, '');
+            alias[adjAlias] = adjPath;
+        });
+    }
+    return alias;
+}
+
+const commonConfig: () => Promise<Configuration> = async () => {
+    const alias = await generateAliasFromTs();
+    return {
+        context: SRC_ROOT,
+        entry: {
+            background: './background',
+            ui: './ui',
+        },
+        output: {
+            path: OUTPUT_ROOT,
+            clean: true,
+        },
+        stats: {
+            preset: 'summary',
+            timings: true,
+            errors: true,
+            warnings: true,
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+            alias,
+        },
+        module: {
+            rules: [
                 {
-                    from: resolve(SRC_ROOT, 'manifest', 'icons', '**', '*'),
-                },
-                {
-                    from: resolve(SRC_ROOT, 'manifest', 'manifest.json'),
-                    to: resolve(OUTPUT_ROOT, '[name][ext]'),
-                    transform: (content) => {
-                        const { description, version } = packageJson;
-                        const manifestJson = {
-                            ...JSON.parse(content.toString()),
-                            name: APP_NAME,
-                            description,
-                            version,
-                        };
-                        return JSON.stringify(manifestJson, null, 4);
+                    test: /\.(t|j)sx?$/,
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: TS_CONFIG_FILE,
                     },
                 },
+                {
+                    test: /\.(s)?css$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif)$/,
+                    type: 'asset/resource',
+                },
             ],
-        }),
-    ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin({
+                chunks: ['ui'],
+                filename: 'ui.html',
+                template: resolve(SRC_ROOT, 'ui', 'index.template.html'),
+                title: APP_NAME,
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: resolve(SRC_ROOT, 'manifest', 'icons', '**', '*'),
+                    },
+                    {
+                        from: resolve(SRC_ROOT, 'manifest', 'manifest.json'),
+                        to: resolve(OUTPUT_ROOT, '[name][ext]'),
+                        transform: (content) => {
+                            const { description, version } = packageJson;
+                            const manifestJson = {
+                                ...JSON.parse(content.toString()),
+                                name: APP_NAME,
+                                description,
+                                version,
+                            };
+                            return JSON.stringify(manifestJson, null, 4);
+                        },
+                    },
+                ],
+            }),
+        ],
+    };
 };
 
 export default commonConfig;
