2b66e6e95caa3b776f2b29668aa588afb7a8a737
François Garillot <francois@garillot.net>
2022-05-16 14:38:40 -0400

    test: Add a test to start nodes with basic setup

---
+++ narwhal/node/tests/node_smoke_test.rs
@@ -0,0 +1,106 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::time::{Duration, Instant};
+
+use config::Export;
+use test_utils::{committee, keys, temp_dir};
+
+#[test]
+fn test_primary_no_consensus() {
+    let db_path = temp_dir().into_os_string().into_string().unwrap();
+    let config_path = temp_dir().into_os_string().into_string().unwrap();
+    let now = Instant::now();
+    let duration = Duration::from_secs(5);
+
+    let keys = keys(None);
+    let keys_file_path = format!("{config_path}/smoke_test_keys.json");
+    keys[0].export(&keys_file_path).unwrap();
+
+    let committee = committee(None);
+    let committee_file_path = format!("{config_path}/smoke_test_committee.json");
+    committee.export(&committee_file_path).unwrap();
+
+    let mut child = std::process::Command::new("cargo")
+        .current_dir("..")
+        .args(&["run", "--bin", "node", "--"])
+        .args(&[
+            "run",
+            "--committee",
+            &committee_file_path,
+            "--keys",
+            &keys_file_path,
+            "--store",
+            &db_path,
+            "primary",
+            "--consensus-disabled",
+        ])
+        .spawn()
+        .expect("failed to launch primary process w/o consensus");
+
+    while now.elapsed() < duration {
+        match child.try_wait() {
+            Ok(Some(status)) => {
+                if !status.success() {
+                    panic!("node panicked with: {:?}", child.stderr.take().unwrap());
+                }
+                assert!(status.success());
+                break;
+            }
+            Ok(None) => continue,
+            Err(e) => {
+                panic!("error waiting for child process: {}", e);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_primary_with_consensus() {
+    let db_path = temp_dir().into_os_string().into_string().unwrap();
+    let config_path = temp_dir().into_os_string().into_string().unwrap();
+    let now = Instant::now();
+    let duration = Duration::from_secs(5);
+
+    let keys = keys(None);
+    let keys_file_path = format!("{config_path}/smoke_test_keys.json");
+    keys[0].export(&keys_file_path).unwrap();
+
+    let committee = committee(None);
+    let committee_file_path = format!("{config_path}/smoke_test_committee.json");
+    committee.export(&committee_file_path).unwrap();
+
+    let mut child = std::process::Command::new("cargo")
+        .current_dir("..")
+        .args(&["run", "--bin", "node", "--"])
+        .args(&[
+            "run",
+            "--committee",
+            &committee_file_path,
+            "--keys",
+            &keys_file_path,
+            "--store",
+            &db_path,
+            "primary",
+            //no arg : default of with_consensus
+        ])
+        .spawn()
+        .expect("failed to launch primary process w/o consensus");
+
+    while now.elapsed() < duration {
+        match child.try_wait() {
+            Ok(Some(status)) => {
+                if !status.success() {
+                    panic!("node panicked with: {:?}", child.stderr.take().unwrap());
+                }
+                assert!(status.success());
+                break;
+            }
+            // This is expected to run indefinitely => will hit the timeout
+            Ok(None) => continue,
+            Err(e) => {
+                panic!("error waiting for child process: {}", e);
+            }
+        }
+    }
+}
