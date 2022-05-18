45a06ece0016e06ec80a7eff21159801670d3120
Stella Cannefax <stella@mystenlabs.com>
2022-05-18 12:51:45 -0500

    fix import in common.ts

--- sdk/typescript/src/types/common.ts
+++ sdk/typescript/src/types/common.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { Base64DataBuffer } from "../serialization/base64";
+
 /** Base64 string representing the object digest */
 export type TransactionDigest = string;
 export type SuiAddress = string;
@@ -19,14 +21,14 @@ const VALID_BASE64_REGEX =
   /^(?:[a-zA-Z0-9+\/]{4})*(?:|(?:[a-zA-Z0-9+\/]{3}=)|(?:[a-zA-Z0-9+\/]{2}==)|(?:[a-zA-Z0-9+\/]{1}===))$/;
 
 export function isValidTransactionDigest(value: string): value is TransactionDigest {
-  return (new Base64DataBuffer(value)).getLength() === TX_DIGEST_LENGTH
+  return new Base64DataBuffer(value).getLength() === TX_DIGEST_LENGTH
     && VALID_BASE64_REGEX.test(value);
 }
 
 // TODO - can we automatically sync this with rust length definition?
 // Source of truth is
-// https://github.com/MystenLabs/sui/blob/acb2b97ae21f47600e05b0d28127d88d0725561d/crates/sui-types/src/base_types.rs#L67 
-// which uses the Move account address length 
+// https://github.com/MystenLabs/sui/blob/acb2b97ae21f47600e05b0d28127d88d0725561d/crates/sui-types/src/base_types.rs#L67
+// which uses the Move account address length
 // https://github.com/move-language/move/blob/67ec40dc50c66c34fd73512fcc412f3b68d67235/language/move-core/types/src/account_address.rs#L23 .
 
 const SUI_ADDRESS_LENGTH = 20;
