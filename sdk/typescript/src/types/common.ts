baf0bec2e4239d940d7947ba403494630f2331de
Stella Cannefax <stella@mystenlabs.com>
2022-05-17 19:48:41 -0500

    add string type checks to typescript sdk

--- sdk/typescript/src/types/common.ts
+++ sdk/typescript/src/types/common.ts
@@ -9,3 +9,35 @@ export type ObjectOwner =
   | { ObjectOwner: SuiAddress }
   | 'Shared'
   | 'Immutable';
+
+
+const TX_DIGEST_BASE64_LENGTH = 44;
+// taken from https://rgxdb.com/r/1NUN74O6
+const VALID_BASE64_REGEX =
+  /^(?:[a-zA-Z0-9+\/]{4})*(?:|(?:[a-zA-Z0-9+\/]{3}=)|(?:[a-zA-Z0-9+\/]{2}==)|(?:[a-zA-Z0-9+\/]{1}===))$/;
+
+export function isTransactionDigest(value: string): value is TransactionDigest {
+  return value.length === TX_DIGEST_BASE64_LENGTH
+    && VALID_BASE64_REGEX.test(value);
+}
+
+// TODO - can we automatically sync this with rust length definition?
+const SUI_ADDRESS_LENGTH = 16;
+export function isValidSuiAddress(value: string): value is SuiAddress {
+  return isHex(value) && getHexByteLength(value) === SUI_ADDRESS_LENGTH;
+}
+
+// TODO - which type should this guard ?
+export function isValidSuiObjectId(value: string): boolean {
+  return isValidSuiAddress(value);
+}
+
+function isHex(value: string): boolean {
+  return /^(0x|0X)?[a-fA-F0-9]+$/.test(value) && value.length % 2 === 0;
+}
+
+function getHexByteLength(value: string): number {
+  return /^(0x|0X)/.test(value)
+    ? (value.length - 2) / 2
+    : value.length / 2;
+}
