afca79e5bcae2a151b1930760aa181707e142ad0
Stella Cannefax <stella@mystenlabs.com>
2022-05-17 20:29:51 -0500

    add search format error page

--- explorer/client/src/pages/config/AppRoutes.tsx
+++ explorer/client/src/pages/config/AppRoutes.tsx
@@ -4,6 +4,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import AddressResult from '../address-result/AddressResult';
+import FormatError from '../format-error/FormatError';
 import Home from '../home/Home';
 import MissingResource from '../missing-resource/MissingResource';
 import { ObjectResult } from '../object-result/ObjectResult';
@@ -17,6 +18,7 @@ const AppRoutes = () => {
             <Route path="/transactions/:id" element={<TransactionResult />} />
             <Route path="/addresses/:id" element={<AddressResult />} />
             <Route path="/missing/:id" element={<MissingResource />} />
+            <Route path="/searcherror/:id" element={<FormatError />} />
             <Route path="*" element={<Navigate to="/" replace={true} />} />
         </Routes>
     );
