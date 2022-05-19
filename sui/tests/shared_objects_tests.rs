2aecb6a5bafc551cf585760e3518c6caf2b5714c
Alberto Sonnino <alberto@sonnino.com>
2022-05-19 14:15:25 -0400

    Clearer consensus errors

--- sui/tests/shared_objects_tests.rs
+++ sui/tests/shared_objects_tests.rs
@@ -72,10 +72,10 @@ async fn submit_shared_object_transaction(
         let replies: Vec<_> = futures::future::join_all(futures)
             .await
             .into_iter()
-            // Remove all `ConsensusConnectionBroken` replies. Note that the original Sui error type
-            // `SuiError::ConsensusConnectionBroken(..)` is lost when the message is sent through the
+            // Remove all `FailedToHearBackFromConsensus` replies. Note that the original Sui error type
+            // `SuiError::FailedToHearBackFromConsensus(..)` is lost when the message is sent through the
             // network (it is replaced by `RpcError`). As a result, the following filter doesn't work:
-            // `.filter(|result| !matches!(result, Err(SuiError::ConsensusConnectionBroken(..))))`.
+            // `.filter(|result| !matches!(result, Err(SuiError::FailedToHearBackFromConsensus(..))))`.
             .filter(|result| match result {
                 Err(e) => !e.to_string().contains("deadline has elapsed"),
                 _ => true,
