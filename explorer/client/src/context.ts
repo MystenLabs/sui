c42010e03bf9b94ba3134cc64e09b15c3d876935
Andrew Burnie <apburnie@hotmail.co.uk>
2022-05-19 12:00:54 +0100

    adds licenses

--- explorer/client/src/context.ts
+++ explorer/client/src/context.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import { createContext, type Dispatch, type SetStateAction } from 'react';
 
 import { Network } from './utils/api/DefaultRpcClient';
