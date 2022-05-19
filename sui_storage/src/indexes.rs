7d69554aeeeefd94b510e9f6f6fd4f91bf838613
Mark Logan <mark@mystenlabs.com>
2022-05-19 10:32:10 -0700

    fixup indexes

--- sui_storage/src/indexes.rs
+++ sui_storage/src/indexes.rs
@@ -36,9 +36,8 @@ impl IndexStore {
     pub fn open<P: AsRef<Path>>(path: P, db_options: Option<Options>) -> Self {
         let mut options = db_options.unwrap_or_default();
 
-        /* The table cache is locked for updates and this determines the number
-           of shareds, ie 2^10. Increase in case of lock contentions.
-        */
+        // The table cache is locked for updates and this determines the number
+        // of shareds, ie 2^10. Increase in case of lock contentions.
         let row_cache = rocksdb::Cache::new_lru_cache(1_000_000).expect("Cache is ok");
         options.set_row_cache(&row_cache);
         options.set_table_cache_num_shard_bits(10);
