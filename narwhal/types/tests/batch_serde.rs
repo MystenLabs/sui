a2175b6475ec2b4e6f8c8f313242a19707f7512d
François Garillot <francois@garillot.net>
2022-05-21 00:05:48 -0400

    feat: backwards-compatibility bincode test

--- narwhal/types/tests/batch_serde.rs
+++ narwhal/types/tests/batch_serde.rs
@@ -1,11 +1,12 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use bincode::Options;
 use serde_test::{assert_tokens, Token};
 use types::Batch;
 
 #[test]
-fn test_ser_de() {
+fn test_serde_batch() {
     let tx = || vec![1; 5];
 
     let txes: Batch = Batch((0..2).map(|_| tx()).collect());
@@ -31,5 +32,34 @@ fn test_ser_de() {
             Token::SeqEnd,
             Token::SeqEnd,
         ],
+    );
+}
+
+#[test]
+fn test_bincode_serde_batch() {
+    let tx = || vec![1; 5];
+
+    let txes: Batch = Batch((0..2).map(|_| tx()).collect());
+
+    let config = bincode::DefaultOptions::new()
+        .with_big_endian()
+        .with_fixint_encoding();
+
+    let txes_bytes = config.serialize(&txes).unwrap();
+
+    // Length as u64: 0000000000000002,
+    let bytes: [u8; 8] = hex::decode("0000000000000002").unwrap().try_into().unwrap();
+    assert_eq!(u64::from_be_bytes(bytes), 2u64);
+
+    // Length-prefix 2, length-prefix 5, 11111, length-prefix 5, 11111
+    let expected_bytes =
+        hex::decode("00000000000000020000000000000005010101010100000000000000050101010101")
+            .unwrap();
+
+    assert_eq!(
+        txes_bytes.clone(),
+        expected_bytes,
+        "received {}",
+        hex::encode(txes_bytes)
     );
 }
