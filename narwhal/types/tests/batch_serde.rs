535b121d7a3539b5f8d3e6bd7b39fcd4c634ef29
François Garillot <francois@garillot.net>
2022-05-21 00:05:57 -0400

    feat: add test for serialized batch message contents

--- narwhal/types/tests/batch_serde.rs
+++ narwhal/types/tests/batch_serde.rs
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use bincode::Options;
+use crypto::ed25519::Ed25519PublicKey;
 use serde_test::{assert_tokens, Token};
-use types::Batch;
+use types::{Batch, WorkerMessage};
 
 #[test]
 fn test_serde_batch() {
@@ -54,6 +55,35 @@ fn test_bincode_serde_batch() {
     // Length-prefix 2, length-prefix 5, 11111, length-prefix 5, 11111
     let expected_bytes =
         hex::decode("00000000000000020000000000000005010101010100000000000000050101010101")
+            .unwrap();
+
+    assert_eq!(
+        txes_bytes.clone(),
+        expected_bytes,
+        "received {}",
+        hex::encode(txes_bytes)
+    );
+}
+
+#[test]
+fn test_bincode_serde_batch_message() {
+    let tx = || vec![1; 5];
+
+    let txes: WorkerMessage<Ed25519PublicKey> =
+        WorkerMessage::Batch(Batch((0..2).map(|_| tx()).collect()));
+
+    let config = bincode::DefaultOptions::new()
+        .with_big_endian()
+        .with_fixint_encoding();
+
+    let txes_bytes = config.serialize(&txes).unwrap();
+
+    // We expect the difference with the above test will be the enum variant above on 4 bytes,
+    // see https://github.com/bincode-org/bincode/blob/75a2e0bc9d35cfa7537633b07a9307bf71da84b5/src/features/serde/ser.rs#L212-L224
+
+    // Variant index 0 (4 bytes), Length-prefix 2, length-prefix 5, 11111, length-prefix 5, 11111
+    let expected_bytes =
+        hex::decode("0000000000000000000000020000000000000005010101010100000000000000050101010101")
             .unwrap();
 
     assert_eq!(
