b57bce36b285ba843d3de3ae26bb1f1c0d97ed5f
François Garillot <francois@garillot.net>
2022-05-21 00:06:05 -0400

    fix: simplify serialization tests, default to LE

--- narwhal/types/tests/batch_serde.rs
+++ narwhal/types/tests/batch_serde.rs
@@ -1,8 +1,5 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-
-use bincode::Options;
-
 use crypto::{ed25519::Ed25519PublicKey, Hash};
 use proptest::arbitrary::Arbitrary;
 use serde_test::{assert_tokens, Token};
@@ -44,19 +41,15 @@ fn test_bincode_serde_batch() {
 
     let txes: Batch = Batch((0..2).map(|_| tx()).collect());
 
-    let config = bincode::DefaultOptions::new()
-        .with_big_endian()
-        .with_fixint_encoding();
-
-    let txes_bytes = config.serialize(&txes).unwrap();
+    let txes_bytes = bincode::serialize(&txes).unwrap();
 
     // Length as u64: 0000000000000002,
-    let bytes: [u8; 8] = hex::decode("0000000000000002").unwrap().try_into().unwrap();
-    assert_eq!(u64::from_be_bytes(bytes), 2u64);
+    let bytes: [u8; 8] = hex::decode("0200000000000000").unwrap().try_into().unwrap();
+    assert_eq!(u64::from_le_bytes(bytes), 2u64);
 
     // Length-prefix 2, length-prefix 5, 11111, length-prefix 5, 11111
     let expected_bytes =
-        hex::decode("00000000000000020000000000000005010101010100000000000000050101010101")
+        hex::decode("02000000000000000500000000000000010101010105000000000000000101010101")
             .unwrap();
 
     assert_eq!(
@@ -74,18 +67,14 @@ fn test_bincode_serde_batch_message() {
     let txes: WorkerMessage<Ed25519PublicKey> =
         WorkerMessage::Batch(Batch((0..2).map(|_| tx()).collect()));
 
-    let config = bincode::DefaultOptions::new()
-        .with_big_endian()
-        .with_fixint_encoding();
-
-    let txes_bytes = config.serialize(&txes).unwrap();
+    let txes_bytes = bincode::serialize(&txes).unwrap();
 
     // We expect the difference with the above test will be the enum variant above on 4 bytes,
     // see https://github.com/bincode-org/bincode/blob/75a2e0bc9d35cfa7537633b07a9307bf71da84b5/src/features/serde/ser.rs#L212-L224
 
     // Variant index 0 (4 bytes), Length-prefix 2, length-prefix 5, 11111, length-prefix 5, 11111
     let expected_bytes =
-        hex::decode("0000000000000000000000020000000000000005010101010100000000000000050101010101")
+        hex::decode("0000000002000000000000000500000000000000010101010105000000000000000101010101")
             .unwrap();
 
     assert_eq!(
