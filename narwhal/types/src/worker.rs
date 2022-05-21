a4884a466c219f2c29d43938f1d905239a78131f
François Garillot <francois@garillot.net>
2022-05-21 00:06:08 -0400

    feat: add new function + micro-benchmark for serialized batch hashing
    It's a perf non-regression:
    
    Batch digests/serialized batch digest/100
                            time:   [48.326 us 48.407 us 48.495 us]
                            thrpt:  [1006.9 MiB/s 1008.7 MiB/s 1010.4 MiB/s]
    Found 9 outliers among 100 measurements (9.00%)
      8 (8.00%) high mild
      1 (1.00%) high severe
    Batch digests/iterating through serialized batch digest/100
                            time:   [48.269 us 48.396 us 48.535 us]
                            thrpt:  [1006.0 MiB/s 1008.9 MiB/s 1011.6 MiB/s]
    Found 5 outliers among 100 measurements (5.00%)
      4 (4.00%) high mild
      1 (1.00%) high severe
    Batch digests/serialized batch digest/500
                            time:   [241.82 us 242.18 us 242.56 us]
                            thrpt:  [1006.5 MiB/s 1008.1 MiB/s 1009.6 MiB/s]
    Found 16 outliers among 100 measurements (16.00%)
      13 (13.00%) high mild
      3 (3.00%) high severe
    Batch digests/iterating through serialized batch digest/500
                            time:   [241.52 us 241.90 us 242.31 us]
                            thrpt:  [1007.5 MiB/s 1009.3 MiB/s 1010.8 MiB/s]
    Found 7 outliers among 100 measurements (7.00%)
      4 (4.00%) high mild
      3 (3.00%) high severe
    Batch digests/serialized batch digest/1000
                            time:   [483.09 us 483.89 us 484.75 us]
                            thrpt:  [1007.3 MiB/s 1009.1 MiB/s 1010.8 MiB/s]
    Found 14 outliers among 100 measurements (14.00%)
      10 (10.00%) high mild
      4 (4.00%) high severe
    Batch digests/iterating through serialized batch digest/1000
                            time:   [483.66 us 484.59 us 485.59 us]
                            thrpt:  [1005.5 MiB/s 1007.6 MiB/s 1009.6 MiB/s]
    Found 2 outliers among 100 measurements (2.00%)
      2 (2.00%) high mild
    Batch digests/serialized batch digest/5000
                            time:   [2.4250 ms 2.4300 ms 2.4353 ms]
                            thrpt:  [1002.5 MiB/s 1004.7 MiB/s 1006.8 MiB/s]
    Found 1 outliers among 100 measurements (1.00%)
      1 (1.00%) high mild
    Batch digests/iterating through serialized batch digest/5000
                            time:   [2.4429 ms 2.4491 ms 2.4556 ms]
                            thrpt:  [994.20 MiB/s 996.85 MiB/s 999.38 MiB/s]
    Found 3 outliers among 100 measurements (3.00%)
      3 (3.00%) high mild

--- narwhal/types/src/worker.rs
+++ narwhal/types/src/worker.rs
@@ -1,11 +1,10 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-
-
 use blake2::digest::Update;
 use crypto::traits::VerifyingKey;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 use crate::{Batch, BatchDigest};
 
@@ -27,4 +26,41 @@ pub type SerializedBatchMessage = Vec<u8>;
 
 pub fn serialized_batch_digest<K: AsRef<[u8]>>(sbm: K) -> BatchDigest {
     BatchDigest::new(crypto::blake2b_256(|hasher| hasher.update(&sbm)))
+}
+
+#[derive(Debug, Error)]
+#[non_exhaustive]
+pub enum DigestError {
+    #[error("Invalid argument: invalid byte at {0}")]
+    InvalidArgumentError(usize),
+}
+
+fn read_one_transaction(sbm: &[u8], offset: usize) -> Result<(&[u8], usize), DigestError> {
+    let length = u64::from_le_bytes(
+        sbm[offset..offset + 8]
+            .try_into()
+            .map_err(|_| DigestError::InvalidArgumentError(offset))?,
+    );
+    let length = usize::try_from(length).map_err(|_| DigestError::InvalidArgumentError(offset))?;
+    let end = offset + 8 + length;
+    Ok((&sbm[offset + 8..end], end))
+}
+
+pub fn hash_all_transactions(sbm: &SerializedBatchMessage) -> Result<BatchDigest, DigestError> {
+    let mut offset = 4; // skip the enum variant selector
+    let num_transactions = u64::from_le_bytes(
+        sbm[offset..offset + 8]
+            .try_into()
+            .map_err(|_| DigestError::InvalidArgumentError(offset))?,
+    );
+    offset += 8;
+    let mut transactions = Vec::new();
+    for _i in 0..num_transactions {
+        let (tx_ref, new_offset) = read_one_transaction(sbm, offset)?;
+        transactions.push(tx_ref);
+        offset = new_offset;
+    }
+    Ok(BatchDigest::new(crypto::blake2b_256(|hasher| {
+        transactions.iter().for_each(|tx| hasher.update(tx))
+    })))
 }
