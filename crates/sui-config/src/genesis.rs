8509bc74240e074eb2dde287f9f970382ca68fa6
Brandon Williams <brandon@mystenlabs.com>
2022-05-23 10:32:58 -0700

    config: use pre-compiled sui-framework

--- crates/sui-config/src/genesis.rs
+++ crates/sui-config/src/genesis.rs
@@ -5,8 +5,6 @@ use base64ct::Encoding;
 use move_binary_format::CompiledModule;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde_with::{serde_as, DeserializeAs, SerializeAs};
-use std::path::PathBuf;
-use sui_framework::DEFAULT_FRAMEWORK_PATH;
 use sui_types::{base_types::TxContext, crypto::PublicKeyBytes, object::Object};
 use tracing::info;
 
@@ -80,8 +78,8 @@ impl<'de> DeserializeAs<'de, CompiledModule> for SerdeCompiledModule {
 }
 
 pub struct Builder {
-    sui_framework: PathBuf,
-    move_framework: PathBuf,
+    sui_framework: Option<Vec<CompiledModule>>,
+    move_framework: Option<Vec<CompiledModule>>,
     move_modules: Vec<Vec<CompiledModule>>,
     objects: Vec<Object>,
     genesis_ctx: TxContext,
@@ -91,10 +89,8 @@ pub struct Builder {
 impl Builder {
     pub fn new(genesis_ctx: TxContext) -> Self {
         Self {
-            sui_framework: PathBuf::from(DEFAULT_FRAMEWORK_PATH),
-            move_framework: PathBuf::from(DEFAULT_FRAMEWORK_PATH)
-                .join("deps")
-                .join("move-stdlib"),
+            sui_framework: None,
+            move_framework: None,
             move_modules: vec![],
             objects: vec![],
             genesis_ctx,
@@ -102,13 +98,13 @@ impl Builder {
         }
     }
 
-    pub fn sui_framework(mut self, path: PathBuf) -> Self {
-        self.sui_framework = path;
+    pub fn sui_framework(mut self, sui_framework: Vec<CompiledModule>) -> Self {
+        self.sui_framework = Some(sui_framework);
         self
     }
 
-    pub fn move_framework(mut self, path: PathBuf) -> Self {
-        self.move_framework = path;
+    pub fn move_framework(mut self, move_framework: Vec<CompiledModule>) -> Self {
+        self.move_framework = Some(move_framework);
         self
     }
 
@@ -144,7 +140,9 @@ impl Builder {
 
         // Load Move Framework
         info!("Loading Move framework lib from {:?}", self.move_framework);
-        let move_modules = sui_framework::get_move_stdlib_modules(&self.move_framework).unwrap();
+        let move_modules = self
+            .move_framework
+            .unwrap_or_else(sui_framework::get_move_stdlib);
         // let move_framework =
         //     Object::new_package(move_modules.clone(), TransactionDigest::genesis());
         modules.push(move_modules);
@@ -152,7 +150,9 @@ impl Builder {
 
         // Load Sui Framework
         info!("Loading Sui framework lib from {:?}", self.sui_framework);
-        let sui_modules = sui_framework::get_sui_framework_modules(&self.sui_framework).unwrap();
+        let sui_modules = self
+            .sui_framework
+            .unwrap_or_else(sui_framework::get_sui_framework);
         // let sui_framework = Object::new_package(sui_modules.clone(), TransactionDigest::genesis());
         modules.push(sui_modules);
         // objects.push(sui_framework);
@@ -170,14 +170,11 @@ impl Builder {
 
 #[cfg(test)]
 mod test {
-    use sui_framework::DEFAULT_FRAMEWORK_PATH;
-
     use super::Genesis;
 
     #[test]
     fn roundtrip() {
-        let sui_lib =
-            sui_framework::get_sui_framework_modules(DEFAULT_FRAMEWORK_PATH.as_ref()).unwrap();
+        let sui_lib = sui_framework::get_sui_framework();
 
         let genesis = Genesis {
             modules: vec![sui_lib],
