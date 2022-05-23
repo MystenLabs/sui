ad492fa47b2d0e1871c19d217922045bbec4ea49
Andrew Burnie <apburnie@hotmail.co.uk>
2022-05-23 11:58:55 +0100

    network stored in browser

--- explorer/client/src/app/App.tsx
+++ explorer/client/src/app/App.tsx
@@ -1,24 +1,18 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Footer from '../components/footer/Footer';
 import NetworkSelect from '../components/network/Network';
 import Search from '../components/search/Search';
-import { NetworkContext } from '../context';
+import { NetworkContext, useNetwork } from '../context';
 import AppRoutes from '../pages/config/AppRoutes';
-import { Network } from '../utils/api/DefaultRpcClient';
-import { IS_LOCAL_ENV } from '../utils/envUtil';
 
 import styles from './App.module.css';
 
 function App() {
-    const [network, setNetwork] = useState<Network | string>(
-        IS_LOCAL_ENV ? Network.Local : Network.Devnet
-    );
-
+    const [network, setNetwork] = useNetwork();
     return (
         <NetworkContext.Provider value={[network, setNetwork]}>
             <div className={styles.app}>
--- explorer/client/src/context.ts
+++ explorer/client/src/context.ts
@@ -1,10 +1,39 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { createContext, type Dispatch, type SetStateAction } from 'react';
+import {
+    createContext,
+    useEffect,
+    useState,
+    type Dispatch,
+    type SetStateAction,
+} from 'react';
 
-import { type Network } from './utils/api/DefaultRpcClient';
+import { Network } from './utils/api/DefaultRpcClient';
+import { IS_LOCAL_ENV } from './utils/envUtil';
 
 export const NetworkContext = createContext<
     [Network | string, Dispatch<SetStateAction<Network | string>>]
 >(['', () => null]);
+
+const LOCALSTORE_RPC_KEY = 'sui-explorer-rpc';
+
+export function useNetwork(): [
+    string,
+    Dispatch<SetStateAction<Network | string>>
+] {
+    //Get storage network from browser:
+    const storageNetwork = window.localStorage.getItem(LOCALSTORE_RPC_KEY);
+
+    // Start value is that in storage or, if null, the website's default:
+    const [network, setNetwork] = useState<Network | string>(
+        storageNetwork || (IS_LOCAL_ENV ? Network.Local : Network.Devnet)
+    );
+
+    useEffect(() => {
+        // If network in UI changes, change network in storage:
+        window.localStorage.setItem(LOCALSTORE_RPC_KEY, network);
+    }, [network]);
+
+    return [network, setNetwork];
+}
