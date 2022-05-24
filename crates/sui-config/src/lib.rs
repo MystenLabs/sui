08e81cbf76c04290733c2aadaea5b2c5312cf305
longbowlu <zl9394@gmail.com>
2022-05-24 16:08:28 -0700

    gateway port 9000 -> 443

--- crates/sui-config/src/lib.rs
+++ crates/sui-config/src/lib.rs
@@ -228,7 +228,7 @@ const SUI_CONFIG_DIR: &str = "sui_config";
 pub const SUI_NETWORK_CONFIG: &str = "network.conf";
 pub const SUI_WALLET_CONFIG: &str = "wallet.conf";
 pub const SUI_GATEWAY_CONFIG: &str = "gateway.conf";
-pub const SUI_DEV_NET_URL: &str = "https://gateway.devnet.sui.io:9000";
+pub const SUI_DEV_NET_URL: &str = "https://gateway.devnet.sui.io:443";
 
 pub const AUTHORITIES_DB_NAME: &str = "authorities_db";
 pub const DEFAULT_STARTING_PORT: u16 = 10000;
