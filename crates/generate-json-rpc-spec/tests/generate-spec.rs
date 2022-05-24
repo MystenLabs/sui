58c72cf972599da1139d800b5e14fcb674262aa9
Brandon Williams <brandon@mystenlabs.com>
2022-05-23 20:40:16 -0700

    generate-json-rpc-spec: move into own crate and fix when run at repo root

--- crates/generate-json-rpc-spec/tests/generate-spec.rs
+++ crates/generate-json-rpc-spec/tests/generate-spec.rs
@@ -1,13 +1,13 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+const GENERATE_JSON_RPC_SPEC_BIN: &str = env!("CARGO_BIN_EXE_generate-json-rpc-spec");
+
 #[test]
 fn test_json_rpc_spec() {
     // If this test breaks and you intended a json rpc schema change, you need to run to get the fresh schema:
-    // # cargo -q run --example generate-json-rpc-spec -- record
-    let status = std::process::Command::new("cargo")
-        .current_dir("..")
-        .args(&["run", "--example", "generate-json-rpc-spec", "--"])
+    // # cargo -q run --bin generate-json-rpc-spec -- record
+    let status = std::process::Command::new(GENERATE_JSON_RPC_SPEC_BIN)
         .arg("test")
         .status()
         .expect("failed to execute process");
