fbfbe5422bcd46ab40ad23a92e07f6ce9710497f
François Garillot <francois@garillot.net>
2022-05-25 23:02:25 -0400

    add a workspace hack package

--- narwhal/scripts/license_check.sh
+++ narwhal/scripts/license_check.sh
@@ -10,11 +10,15 @@ TOPLEVEL="${DIR}/../"
 # Iterate over rust files not in the target directory
 for i in $(find "$TOPLEVEL" -path "$TOPLEVEL/target" -prune -o -iname "*.rs" -print) 
 do
-	echo "hello $i"
+  echo "Checking $i"
   CNT=$(head -n3 "$i" | grep -oEe '// (Copyright \(c\) 2022, Mysten Labs, Inc.|SPDX-License-Identifier: Apache-2.0)' | wc -l)
   if [ "$CNT" -lt 2 ]
   then
      echo "File $i has an incorrect license header"
      exit 1
+  else
+     echo "File $i is OK"
   fi
 done
+
+exit 0
