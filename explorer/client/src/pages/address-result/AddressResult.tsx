f413ca3f00d8f3958a5dc28e40e1721d27d14a2c
Andrew Burnie <apburnie@hotmail.co.uk>
2022-05-27 17:13:29 +0100

    lists transactions sent from/to address

--- explorer/client/src/pages/address-result/AddressResult.tsx
+++ explorer/client/src/pages/address-result/AddressResult.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ErrorResult from '../../components/error-result/ErrorResult';
@@ -8,6 +9,8 @@ import Longtext from '../../components/longtext/Longtext';
 import OwnedObjects from '../../components/ownedobjects/OwnedObjects';
 import TxForID from '../../components/transactions-for-id/TxForID';
 import theme from '../../styles/theme.module.css';
+import { DefaultRpcClient as rpc } from '../../utils/api/DefaultRpcClient';
+import { IS_STATIC_ENV } from '../../utils/envUtil';
 
 type DataType = {
     id: string;
@@ -21,6 +24,64 @@ type ResponseType = {
 
 function instanceOfDataType(object: any): object is DataType {
     return object !== undefined && ['id', 'objects'].every((x) => x in object);
+}
+const DATATYPE_DEFAULT = {
+    to: [],
+    from: [],
+    loadState: 'pending',
+};
+
+function TxForAddress({ id }: { id: string }) {
+    const [showData, setData] = useState(DATATYPE_DEFAULT);
+    const [network] = useContext(NetworkContext);
+
+    useEffect(() => {
+        rpc(network)
+            .getTransactionsForAddress(id)
+            .then((data) =>
+                setData({
+                    ...(data as typeof DATATYPE_DEFAULT),
+                    loadState: 'loaded',
+                })
+            )
+            .catch((error) => {
+                console.log(error);
+                setData({ ...DATATYPE_DEFAULT, loadState: 'fail' });
+            });
+    }, [id, network]);
+
+    if (showData.loadState === 'pending') {
+        return <div>Loading ...</div>;
+    }
+
+    if (showData.loadState === 'loaded') {
+        return (
+            <>
+                <div>
+                    <div>From</div>
+                    <div>
+                        {showData.from.map((x) => (
+                            <div key={`from-${x[0]}`}>{x[1]}</div>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <div>To</div>
+                    <div>
+                        {showData.to.map((x) => (
+                            <div key={`from-${x[0]}`}>{x[1]}</div>
+                        ))}
+                    </div>
+                </div>
+            </>
+        );
+    }
+    return (
+        <ErrorResult
+            id={id}
+            errorMsg="Transactions could not be extracted on the following specified transaction ID"
+        />
+    );
 }
 
 function AddressResult() {
@@ -39,6 +100,7 @@ function AddressResult() {
                         />
                     </div>
                 </div>
+                {!IS_STATIC_ENV && <TxForAddress id={addressID} />}
                 <div>
                     <div>Owned Objects</div>
                     <div>
