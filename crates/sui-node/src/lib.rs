d4965ba3f304e94dd3721ea902892ce142396261
Brandon Williams <brandon@mystenlabs.com>
2022-05-26 19:47:22 -0700

    sui-node: better merging of validator and fullnode logic
    Instead of having a single branch determining if we start a validator or
    fullnode, break out the individual components and enable turning them
    each on/off individually.
    
    Closes: #2068

--- crates/sui-node/src/lib.rs
+++ crates/sui-node/src/lib.rs
@@ -2,116 +2,167 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
+use futures::TryFutureExt;
+use parking_lot::Mutex;
 use std::{collections::BTreeMap, sync::Arc, time::Duration};
 use sui_config::NodeConfig;
+use sui_core::authority_server::ValidatorService;
 use sui_core::{
     authority::{AuthorityState, AuthorityStore},
     authority_active::{gossip::gossip_process, ActiveAuthority},
     authority_client::NetworkAuthorityClient,
+    checkpoints::CheckpointStore,
 };
 use sui_gateway::json_rpc::JsonRpcServerBuilder;
 use sui_gateway::read_api::{FullNodeApi, ReadApi};
+use sui_network::api::ValidatorServer;
 use sui_storage::IndexStore;
 use tracing::info;
 
 // TODO extract the important bits from AuthorityServer and FullNode so that we can have a single
 // unified node. See https://github.com/MystenLabs/sui/issues/2068 for more info.
-pub struct SuiNode;
+pub struct SuiNode {
+    grpc_server: tokio::task::JoinHandle<Result<()>>,
+    _json_rpc_service: Option<jsonrpsee::http_server::HttpServerHandle>,
+    _batch_subsystem_handle: tokio::task::JoinHandle<Result<()>>,
+    _gossip_handle: Option<tokio::task::JoinHandle<()>>,
+    state: Arc<AuthorityState>,
+}
 
 impl SuiNode {
-    pub async fn start(config: &NodeConfig) -> Result<()> {
-        if config.consensus_config().is_some() {
-            // Validator
-            let server = sui_core::make::make_server(config).await?.spawn().await?;
+    pub async fn start(config: &NodeConfig) -> Result<SuiNode> {
+        info!(node =? config.public_key(),
+            "Initializing sui-node listening on {}", config.network_address
+        );
 
-            info!(node =? config.public_key(),
-                "Initializing sui-node listening on {}", config.network_address
-            );
-
-            server.join().await?;
+        let secret = Arc::pin(config.key_pair().copy());
+        let store = Arc::new(AuthorityStore::open(config.db_path().join("store"), None));
+        let checkpoint_store = if config.consensus_config().is_some() {
+            Some(Arc::new(Mutex::new(CheckpointStore::open(
+                config.db_path().join("checkpoints"),
+                None,
+                config.public_key(),
+                config.committee_config().committee(),
+                secret.clone(),
+            )?)))
         } else {
-            // Fullnode
-            let fullnode = FullNode::start(config).await?;
+            None
+        };
 
-            let mut server = JsonRpcServerBuilder::new()?;
-            server.register_module(ReadApi::new(fullnode.state.clone()))?;
-            server.register_module(FullNodeApi::new(fullnode.state))?;
-
-            let server_handle = server.start(config.json_rpc_address).await?;
-
-            server_handle.await;
-        }
-
-        Ok(())
-    }
-}
-
-//TODO remove the separate FullNode type and merge with the SuiNode type above
-pub struct FullNode {
-    pub state: Arc<AuthorityState>,
-}
-
-impl FullNode {
-    pub async fn start(config: &NodeConfig) -> anyhow::Result<Self> {
-        // TODO use ReplicaStore, or (more likely) get rid of ReplicaStore and
-        // use run-time configuration to determine how much state AuthorityStore
-        // keeps
-        let store = Arc::new(AuthorityStore::open(config.db_path(), None));
-
-        let index_path = config.db_path().join("indexes");
-        let indexes = Arc::new(IndexStore::open(index_path, None));
+        let index_store = if config.consensus_config().is_some() {
+            None
+        } else {
+            Some(Arc::new(IndexStore::open(
+                config.db_path().join("indexes"),
+                None,
+            )))
+        };
 
         let state = Arc::new(
             AuthorityState::new(
                 config.committee_config().committee(),
                 config.public_key(),
-                Arc::pin(config.key_pair().copy()),
+                secret,
                 store,
-                Some(indexes),
-                None,
+                index_store,
+                checkpoint_store,
                 config.genesis(),
             )
             .await,
         );
 
-        let mut net_config = mysten_network::config::Config::new();
-        net_config.connect_timeout = Some(Duration::from_secs(5));
-        net_config.request_timeout = Some(Duration::from_secs(5));
+        let gossip_handle = if config.consensus_config().is_some() {
+            None
+        } else {
+            let mut net_config = mysten_network::config::Config::new();
+            net_config.connect_timeout = Some(Duration::from_secs(5));
+            net_config.request_timeout = Some(Duration::from_secs(5));
 
-        let mut authority_clients = BTreeMap::new();
-        for validator in config.committee_config().validator_set() {
-            let channel = net_config
-                .connect_lazy(validator.network_address())
-                .unwrap();
-            let client = NetworkAuthorityClient::new(channel);
-            authority_clients.insert(validator.public_key(), client);
-        }
+            let mut authority_clients = BTreeMap::new();
+            for validator in config.committee_config().validator_set() {
+                let channel = net_config
+                    .connect_lazy(validator.network_address())
+                    .unwrap();
+                let client = NetworkAuthorityClient::new(channel);
+                authority_clients.insert(validator.public_key(), client);
+            }
 
-        let active_authority = ActiveAuthority::new(state.clone(), authority_clients)?;
+            let active_authority = ActiveAuthority::new(state.clone(), authority_clients)?;
 
-        // Start following validators
-        tokio::task::spawn(async move {
-            gossip_process(
-                &active_authority,
-                // listen to all authorities (note that gossip_process caps this to total minus 1.)
-                active_authority.state.committee.voting_rights.len(),
-            )
-            .await;
-        });
+            // Start following validators
+            Some(tokio::task::spawn(async move {
+                gossip_process(
+                    &active_authority,
+                    // listen to all authorities (note that gossip_process caps this to total minus 1.)
+                    active_authority.state.committee.voting_rights.len(),
+                )
+                .await;
+            }))
+        };
 
-        // Start batch system so the full node can be followed - currently only the
-        // tests use this, in order to wait until the full node has seen a tx.
-        // However, there's no reason full nodes won't want to follow other full nodes
-        // eventually.
-        let batch_state = state.clone();
-        tokio::task::spawn(async move {
-            batch_state
-                .run_batch_service(1000, Duration::from_secs(1))
-                .await
-        });
+        let batch_subsystem_handle = {
+            // Start batch system so that this node can be followed
+            let batch_state = state.clone();
+            tokio::task::spawn(async move {
+                batch_state
+                    .run_batch_service(1000, Duration::from_secs(1))
+                    .await
+                    .map_err(Into::into)
+            })
+        };
 
-        info!("Started full node ");
+        let validator_service = if config.consensus_config().is_some() {
+            Some(ValidatorService::new(config, state.clone()).await?)
+        } else {
+            None
+        };
 
-        Ok(Self { state })
+        let grpc_server = {
+            let mut server_builder = mysten_network::config::Config::new().server_builder();
+
+            if let Some(validator_service) = validator_service {
+                server_builder =
+                    server_builder.add_service(ValidatorServer::new(validator_service));
+            }
+
+            let server = server_builder.bind(config.network_address()).await?;
+            let local_addr = server.local_addr();
+            info!("Listening to traffic on {local_addr}");
+            tokio::spawn(server.serve().map_err(Into::into))
+        };
+
+        let json_rpc_service = if config.consensus_config().is_some() {
+            None
+        } else {
+            let mut server = JsonRpcServerBuilder::new()?;
+            server.register_module(ReadApi::new(state.clone()))?;
+            server.register_module(FullNodeApi::new(state.clone()))?;
+
+            let server_handle = server.start(config.json_rpc_address).await?;
+            Some(server_handle)
+        };
+
+        let node = Self {
+            grpc_server,
+            _json_rpc_service: json_rpc_service,
+            _gossip_handle: gossip_handle,
+            _batch_subsystem_handle: batch_subsystem_handle,
+            state,
+        };
+
+        info!("SuiNode started!");
+
+        Ok(node)
+    }
+
+    pub fn state(&self) -> Arc<AuthorityState> {
+        self.state.clone()
+    }
+
+    //TODO watch/wait on all the components
+    pub async fn wait(self) -> Result<()> {
+        self.grpc_server.await??;
+
+        Ok(())
     }
 }
--- crates/sui/tests/full_node_tests.rs
+++ crates/sui/tests/full_node_tests.rs
@@ -9,7 +9,7 @@ use sui::{
 };
 use sui_config::{NetworkConfig, PersistedConfig};
 use sui_core::authority::AuthorityState;
-use sui_node::FullNode;
+use sui_node::SuiNode;
 use sui_types::{
     base_types::{ObjectID, SuiAddress, TransactionDigest},
     batch::UpdateItem,
@@ -21,13 +21,13 @@ use tokio::time::{sleep, Duration};
 use tracing::info;
 
 async fn transfer_coin(
-    node: &FullNode,
+    node: &SuiNode,
     context: &mut WalletContext,
 ) -> Result<(ObjectID, SuiAddress, SuiAddress, TransactionDigest), anyhow::Error> {
     let sender = context.config.accounts.get(0).cloned().unwrap();
     let receiver = context.config.accounts.get(1).cloned().unwrap();
 
-    let object_refs = node.state.get_owned_objects(sender).await?;
+    let object_refs = node.state().get_owned_objects(sender).await?;
     let gas_object = object_refs.get(0).unwrap().0;
     let object_to_send = object_refs.get(1).unwrap().0;
 
@@ -123,13 +123,13 @@ async fn test_full_node_follows_txes() -> Result<(), anyhow::Error> {
     config.db_path = working_dir.path().join("fullnode");
     config.consensus_config = None;
 
-    let node = FullNode::start(&config).await?;
+    let node = SuiNode::start(&config).await?;
 
     let (transfered_object, _, receiver, digest) = transfer_coin(&node, &mut context).await?;
-    wait_for_tx(digest, node.state.clone()).await;
+    wait_for_tx(digest, node.state().clone()).await;
 
     // verify that the node has seen the transfer
-    let object_info = node.state.get_object_info(&transfered_object).await?;
+    let object_info = node.state().get_object_info(&transfered_object).await?;
     let object = object_info.into_object()?;
 
     assert_eq!(object.owner.get_owner_address().unwrap(), receiver);
@@ -157,37 +157,37 @@ async fn test_full_node_indexes() -> Result<(), anyhow::Error> {
     config.db_path = working_dir.path().join("fullnode");
     config.consensus_config = None;
 
-    let node = FullNode::start(&config).await?;
+    let node = SuiNode::start(&config).await?;
 
     let (transfered_object, sender, receiver, digest) = transfer_coin(&node, &mut context).await?;
 
-    wait_for_tx(digest, node.state.clone()).await;
+    wait_for_tx(digest, node.state().clone()).await;
 
     let txes = node
-        .state
+        .state()
         .get_transactions_by_input_object(transfered_object)
         .await?;
     assert_eq!(txes[0].1, digest);
 
     let txes = node
-        .state
+        .state()
         .get_transactions_by_mutated_object(transfered_object)
         .await?;
     assert_eq!(txes[0].1, digest);
 
-    let txes = node.state.get_transactions_from_addr(sender).await?;
+    let txes = node.state().get_transactions_from_addr(sender).await?;
     assert_eq!(txes[0].1, digest);
 
-    let txes = node.state.get_transactions_to_addr(receiver).await?;
+    let txes = node.state().get_transactions_to_addr(receiver).await?;
     assert_eq!(txes[0].1, digest);
 
     // Note that this is also considered a tx to the sender, because it mutated
     // one or more of the sender's objects.
-    let txes = node.state.get_transactions_to_addr(sender).await?;
+    let txes = node.state().get_transactions_to_addr(sender).await?;
     assert_eq!(txes[0].1, digest);
 
     // No transactions have originated from the receiver
-    let txes = node.state.get_transactions_from_addr(receiver).await?;
+    let txes = node.state().get_transactions_from_addr(receiver).await?;
     assert_eq!(txes.len(), 0);
 
     Ok(())
