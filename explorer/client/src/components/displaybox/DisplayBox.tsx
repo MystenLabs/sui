a7392af5f8f4a63a14b75ba8b1bb74f003e1abe4
Stella Cannefax <stella@mystenlabs.com>
2022-05-28 03:08:06 -0500

    DisplayBox changes for image mod work

--- explorer/client/src/components/displaybox/DisplayBox.tsx
+++ explorer/client/src/components/displaybox/DisplayBox.tsx
@@ -1,15 +1,19 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useContext } from 'react';
 
 import { transformURL } from '../../utils/stringUtils';
+import { ImageModClient } from '../../utils/imageModeratorClient';
 
 import styles from './DisplayBox.module.css';
 
 function DisplayBox({ display }: { display: string }) {
     const [hasDisplayLoaded, setHasDisplayLoaded] = useState(false);
     const [hasFailedToLoad, setHasFailedToLoad] = useState(false);
+
+    const [hasImgBeenChecked, setHasImgBeenChecked] = useState(false);
+    const [imgAllowState, setImgAllowState] = useState(false);
 
     const imageStyle = hasDisplayLoaded ? {} : { display: 'none' };
     const handleImageLoad = useCallback(
@@ -19,6 +23,23 @@ function DisplayBox({ display }: { display: string }) {
 
     useEffect(() => {
         setHasFailedToLoad(false);
+        setHasImgBeenChecked(false);
+        setImgAllowState(false);
+
+        new ImageModClient()
+            .checkImage(transformURL(display))
+            .then(({ ok }) => {
+                //setHasImgBeenChecked(true);
+                setImgAllowState(ok);
+            })
+            .catch((error) => {
+                //setHasImgBeenChecked(true);
+                // default to allow, so a broken img check service doesn't break NFT display
+                setImgAllowState(true);
+            })
+            .finally(() => {
+                setHasImgBeenChecked(true);
+            });
     }, [display]);
 
     const handleImageFail = useCallback(
@@ -30,13 +51,34 @@ function DisplayBox({ display }: { display: string }) {
         [setHasFailedToLoad]
     );
 
+    console.log(
+        `img allow state: ${imgAllowState}`,
+        `checked state: ${imgAllowState}, has display loaded: ${hasDisplayLoaded}`
+    );
+
+    // if we've loaded the display image but the check hasn't returned, display a blurry version
+    const shouldBlur = hasDisplayLoaded && !hasImgBeenChecked && !imgAllowState;
+    if (shouldBlur)
+        console.log(
+            'SHOULD BLUR???',
+            hasDisplayLoaded,
+            hasImgBeenChecked,
+            imgAllowState
+        );
+
+    const imgClass = shouldBlur ? styles.imageboxblur : styles.imagebox;
+    console.log('img class', imgClass);
+
+    const imgSrc = processDisplayValue(display);
+
     return (
         <div className={styles['display-container']}>
-            {!hasDisplayLoaded && (
-                <div className={styles.imagebox} id="pleaseWaitImage">
-                    Please wait for display to load
-                </div>
-            )}
+            {!hasDisplayLoaded ||
+                (hasDisplayLoaded && !imgAllowState && (
+                    <div className={styles.imagebox} id="pleaseWaitImage">
+                        image loading...
+                    </div>
+                ))}
             {hasFailedToLoad ? (
                 <div className={styles.imagebox} id="noImage">
                     No Image was Found
@@ -44,7 +86,7 @@ function DisplayBox({ display }: { display: string }) {
             ) : (
                 <img
                     id="loadedImage"
-                    className={styles.imagebox}
+                    className={imgClass}
                     style={imageStyle}
                     alt="NFT"
                     src={transformURL(display)}
