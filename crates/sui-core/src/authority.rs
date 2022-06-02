5ee97157bc71f83a99fac2739b719275eaea0248
Mark Logan <mark@mystenlabs.com>
2022-06-02 08:20:44 -0700

    fix clippy

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -13,7 +13,6 @@ use crate::{
     query_helpers::QueryHelpers,
     transaction_input_checker,
 };
-use anyhow::anyhow;
 use arc_swap::ArcSwap;
 use async_trait::async_trait;
 use itertools::Itertools;
