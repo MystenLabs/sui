bea7d940b48b38839af90167ac7953078179d304
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-06-02 14:14:15 +0100

    wallet-ext: setting view
    * link to ToS
    * mint demo NFT
    * logout (clear mnemonic)

--- wallet/src/ui/app/redux/slices/sui-objects/index.ts
+++ wallet/src/ui/app/redux/slices/sui-objects/index.ts
@@ -8,6 +8,8 @@ import {
     createSlice,
 } from '@reduxjs/toolkit';
 
+import { ExampleNFT } from './NFT';
+
 import type { SuiObject } from '@mysten/sui.js';
 import type { RootState } from '_redux/RootReducer';
 import type { AppThunkConfig } from '_store/thunk-extras';
@@ -40,6 +42,16 @@ export const fetchAllOwnedObjects = createAsyncThunk<
     }
     return allSuiObjects;
 });
+
+export const mintDemoNFT = createAsyncThunk<void, void, AppThunkConfig>(
+    'mintDemoNFT',
+    async (_, { extra: { api, keypairVault }, dispatch }) => {
+        await ExampleNFT.mintExampleNFT(
+            api.getSignerInstance(keypairVault.getKeyPair())
+        );
+        await dispatch(fetchAllOwnedObjects());
+    }
+);
 
 interface SuiObjectsManualState {
     loading: boolean;
