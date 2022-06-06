a2d67ad1e32d018ab06508002aa6c9d796400cb0
Jibz1 <godwinjibs@gmail.com>
2022-06-06 03:27:37 -0400

    Explorer TX pagination (#2377)
    * pagination of tx by sequence number
    
    * max cal
    
    * update
    
    * update mock total txcount
    
    * prettier fix
    
    * fixed edge case where last pag number does not show
    
    * mobile optimazation
    
    * fix
    
    * lint fix

--- explorer/client/src/pages/home/Home.tsx
+++ explorer/client/src/pages/home/Home.tsx
@@ -1,18 +1,86 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { useEffect, useState, useContext } from 'react';
+
+import ErrorResult from '../../components/error-result/ErrorResult';
 import LastestTxCard from '../../components/transaction-card/RecentTxCard';
 import TxCountCard from '../../components/transaction-count/TxCountCard';
+import { NetworkContext } from '../../context';
+import {
+    DefaultRpcClient as rpc,
+    type Network,
+} from '../../utils/api/DefaultRpcClient';
+import { IS_STATIC_ENV } from '../../utils/envUtil';
 
 import styles from './Home.module.css';
 
-function Home() {
+const initState = { count: 0, loadState: 'pending' };
+
+// Moved this method to the Home.tsx file so getTotalTransactionNumber can be called once across the entire component.
+async function getTransactionCount(network: Network | string): Promise<number> {
+    return rpc(network).getTotalTransactionNumber();
+}
+
+function HomeStatic() {
+    const [count] = useState(500);
     return (
         <div data-testid="home-page" id="home" className={styles.home}>
-            <LastestTxCard />
-            <TxCountCard />
+            <LastestTxCard count={count} />
+            <TxCountCard count={count} />
         </div>
     );
 }
+
+function HomeAPI() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [results, setResults] = useState(initState);
+    const [network] = useContext(NetworkContext);
+    useEffect(() => {
+        let isMounted = true;
+        getTransactionCount(network)
+            .then((resp: number) => {
+                if (isMounted) {
+                    setIsLoaded(true);
+                }
+                setResults({
+                    loadState: 'loaded',
+                    count: resp,
+                });
+            })
+            .catch((err) => {
+                setResults({
+                    ...initState,
+                    loadState: 'fail',
+                });
+                setIsLoaded(false);
+            });
+
+        return () => {
+            isMounted = false;
+        };
+    }, [network]);
+
+    if (results.loadState === 'pending') {
+        return <div className={styles.gray}>loading...</div>;
+    }
+
+    if (!isLoaded && results.loadState === 'fail') {
+        return (
+            <ErrorResult
+                id=""
+                errorMsg="Error getting total transaction count"
+            />
+        );
+    }
+    return (
+        <div data-testid="home-page" id="home" className={styles.home}>
+            <LastestTxCard count={results.count} />
+            <TxCountCard count={results.count} />
+        </div>
+    );
+}
+
+const Home = () => (IS_STATIC_ENV ? <HomeStatic /> : <HomeAPI />);
 
 export default Home;
