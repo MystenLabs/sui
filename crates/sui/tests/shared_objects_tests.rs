44e907fb7ad46f27bbdae14564703f7fb9eb8d47
Tom Harbert <tom@mystenlabs.com>
2022-06-17 14:26:05 -0700

    clean up after merging origin/main

--- crates/sui/tests/shared_objects_tests.rs
+++ crates/sui/tests/shared_objects_tests.rs
@@ -357,7 +357,7 @@ async fn replay_shared_object_transaction() {
 }
 
 #[tokio::test]
-#[ignore]
+//#[ignore] // cargo test gateway -p sui --test shared_objects_tests -- --nocapture
 async fn shared_object_on_gateway() {
     let mut gas_objects = test_gas_objects();
 
