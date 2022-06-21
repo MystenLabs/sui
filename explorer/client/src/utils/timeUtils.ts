34be5a9ccee5fb188ccb108637c6b409e642893a
Andrew Burnie <apburnie@hotmail.co.uk>
2022-06-21 15:56:29 +0100

    replaces milliseconds with time ago

--- explorer/client/src/utils/timeUtils.ts
+++ explorer/client/src/utils/timeUtils.ts
@@ -1,13 +1,15 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { IS_STATIC_ENV } from './envUtil';
+
 const stdToN = (original: number, length: number) =>
     String(original).padStart(length, '0');
 
-export const convertNumberToDate = (epochTimeSecs: number | null): string => {
-    if (!epochTimeSecs) return 'Not Available';
-    //API returns epoch time in seconds, Date expects milliseconds:
-    const date = new Date(epochTimeSecs);
+export const convertNumberToDate = (epochMilliSecs: number | null): string => {
+    if (!epochMilliSecs) return 'Not Available';
+
+    const date = new Date(epochMilliSecs);
 
     const MONTHS = [
         'Jan',
@@ -29,8 +31,32 @@ export const convertNumberToDate = (epochTimeSecs: number | null): string => {
     } ${date.getUTCFullYear()} ${stdToN(date.getUTCHours(), 2)}:${stdToN(
         date.getUTCMinutes(),
         2
-    )}:${stdToN(date.getUTCSeconds(), 2)}.${stdToN(
-        date.getUTCMilliseconds(),
-        3
-    )} (UTC)`;
+    )}:${stdToN(date.getUTCSeconds(), 2)} UTC`;
+};
+
+export const timeAgo = (epochMilliSecs: number | null): string => {
+    if (!epochMilliSecs) return 'Not Available';
+
+    //In static mode the time is fixed at 1 Jan 2025 01:13:10 UTC for testing purposes
+    const timeNow = IS_STATIC_ENV ? 1735693990000 : Date.now();
+
+    const timeDiff = timeNow - epochMilliSecs;
+
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    if (days >= 1) return `${days} day${days === 1 ? '' : 's'}`;
+
+    const hours = Math.floor(timeDiff / (1000 * 60 * 60));
+
+    if (hours >= 1) return `${hours} hour${hours === 1 ? '' : 's'}`;
+
+    const mins = Math.floor(timeDiff / (1000 * 60));
+
+    if (mins >= 1) return `${mins} min${mins === 1 ? '' : 's'}`;
+
+    const secs = Math.floor(timeDiff / 1000);
+
+    if (secs >= 1) return `${secs} sec${secs === 1 ? '' : 's'}`;
+
+    return `< 1 sec`;
 };
