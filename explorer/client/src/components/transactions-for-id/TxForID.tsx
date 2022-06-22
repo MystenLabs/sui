9e379e197dcfe89f103f410b50f985c6f944ce46
Andrew Burnie <apburnie@hotmail.co.uk>
2022-06-22 16:13:43 +0100

    get live data to display values 'ago'

--- explorer/client/src/components/transactions-for-id/TxForID.tsx
+++ explorer/client/src/components/transactions-for-id/TxForID.tsx
@@ -61,9 +61,7 @@ function TxForIDView({ showData }: { showData: TxnData[] | undefined }) {
         <div id="tx" className={styles.txresults}>
             <div className={styles.txheader}>
                 <div className={styles.txid}>TxId</div>
-                {showData[0].timestamp_ms && (
-                    <div className={styles.txage}>Age</div>
-                )}
+                <div className={styles.txage}>Age</div>
                 <div className={styles.txtype}>TxType</div>
                 <div className={styles.txstatus}>Status</div>
                 <div className={styles.txadd}>Addresses</div>
@@ -79,11 +77,9 @@ function TxForIDView({ showData }: { showData: TxnData[] | undefined }) {
                             alttext={truncate(x.txId, 26, '...')}
                         />
                     </div>
-                    {showData[0].timestamp_ms && (
-                        <div className={styles.txage}>
-                            {timeAgo(x.timestamp_ms)}
-                        </div>
-                    )}
+                    <div className={styles.txage}>
+                        {`${timeAgo(x.timestamp_ms)} ago`}
+                    </div>
                     <div className={styles.txtype}>{x.kind}</div>
                     <div
                         className={cl(
@@ -160,6 +156,7 @@ function TxForIDAPI({ id, category }: { id: string; category: categoryType }) {
                             kind: el!.kind,
                             From: el!.From,
                             To: el!.To,
+                            timestamp_ms: el!.timestamp_ms,
                         }));
                         setData({
                             data: subData,
