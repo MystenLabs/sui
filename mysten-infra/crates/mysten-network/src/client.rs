04ce47763364b3bfdeb2c94f5f7e17fd1510a46c
Brandon Williams <brandon@mystenlabs.com>
2022-06-21 22:01:04 -0700

    network: fix build on windows

--- mysten-infra/crates/mysten-network/src/client.rs
+++ mysten-infra/crates/mysten-network/src/client.rs
@@ -3,7 +3,7 @@
 
 use crate::{
     config::Config,
-    multiaddr::{parse_dns, parse_ip4, parse_ip6, parse_unix},
+    multiaddr::{parse_dns, parse_ip4, parse_ip6},
 };
 use anyhow::{anyhow, Context, Result};
 use multiaddr::{Multiaddr, Protocol};
@@ -57,7 +57,7 @@ fn endpoint_from_multiaddr(addr: &Multiaddr) -> Result<MyEndpoint> {
         // Protocol::Memory(_) => todo!(),
         #[cfg(unix)]
         Protocol::Unix(_) => {
-            let (path, http_or_https) = parse_unix(addr)?;
+            let (path, http_or_https) = crate::multiaddr::parse_unix(addr)?;
             let uri = format!("{http_or_https}://localhost");
             MyEndpoint::try_from_uri(uri)?.with_uds_connector(path.as_ref().into())
         }
@@ -69,6 +69,7 @@ fn endpoint_from_multiaddr(addr: &Multiaddr) -> Result<MyEndpoint> {
 
 struct MyEndpoint {
     endpoint: Endpoint,
+    #[cfg(unix)]
     uds_connector: Option<PathBuf>,
 }
 
@@ -76,6 +77,7 @@ impl MyEndpoint {
     fn new(endpoint: Endpoint) -> Self {
         Self {
             endpoint,
+            #[cfg(unix)]
             uds_connector: None,
         }
     }
@@ -88,6 +90,7 @@ impl MyEndpoint {
         Ok(Self::new(endpoint))
     }
 
+    #[cfg(unix)]
     fn with_uds_connector(self, path: PathBuf) -> Self {
         Self {
             endpoint: self.endpoint,
@@ -101,22 +104,26 @@ impl MyEndpoint {
     }
 
     fn connect_lazy(self) -> Channel {
+        #[cfg(unix)]
         if let Some(path) = self.uds_connector {
-            self.endpoint
+            return self
+                .endpoint
                 .connect_with_connector_lazy(tower::service_fn(move |_: Uri| {
                     let path = path.clone();
 
                     // Connect to a Uds socket
                     tokio::net::UnixStream::connect(path)
-                }))
-        } else {
-            self.endpoint.connect_lazy()
+                }));
         }
+
+        self.endpoint.connect_lazy()
     }
 
     async fn connect(self) -> Result<Channel> {
+        #[cfg(unix)]
         if let Some(path) = self.uds_connector {
-            self.endpoint
+            return self
+                .endpoint
                 .connect_with_connector(tower::service_fn(move |_: Uri| {
                     let path = path.clone();
 
@@ -124,10 +131,10 @@ impl MyEndpoint {
                     tokio::net::UnixStream::connect(path)
                 }))
                 .await
-                .map_err(Into::into)
-        } else {
-            self.endpoint.connect().await.map_err(Into::into)
+                .map_err(Into::into);
         }
+
+        self.endpoint.connect().await.map_err(Into::into)
     }
 }
 
