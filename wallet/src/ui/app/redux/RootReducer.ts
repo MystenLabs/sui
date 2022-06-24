ec299aef12f0dee95a2dcba96515e3dfdac52612
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-06-24 16:18:36 +0100

    wallet-ext: connect site view
    * allows the user to accept/reject a dapp connection
    * using mock permissions for now and background client

--- wallet/src/ui/app/redux/RootReducer.ts
+++ wallet/src/ui/app/redux/RootReducer.ts
@@ -5,6 +5,7 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import account from './slices/account';
 import app from './slices/app';
+import permissions from './slices/permissions';
 import suiObjects from './slices/sui-objects';
 import transactions from './slices/transactions';
 import txresults from './slices/txresults';
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
     suiObjects,
     transactions,
     txresults,
+    permissions,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
