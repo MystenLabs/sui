e460a17ecf87a21ca884216be772b76d41fc56ba
George Danezis <george@danez.is>
2022-06-27 17:47:06 +0300

    Added reordering tests

--- crates/sui-core/src/checkpoints/causal_order.rs
+++ crates/sui-core/src/checkpoints/causal_order.rs
@@ -45,15 +45,15 @@ pub trait EffectsStore {
         transactions: &[ExecutionDigests],
         ckpt_store: &mut CheckpointStore,
     ) -> SuiResult<Vec<ExecutionDigests>> {
-        let effetcs = self.get_effects(transactions)?;
+        let effects = self.get_effects(transactions)?;
 
         // Ensure all transactions included are executed (static property). This should be true since we should not
         // be signing a checkpoint unless we have processed all transactions within it.
-        debug_assert!(effetcs.iter().all(|e| e.is_some()));
+        debug_assert!(effects.iter().all(|e| e.is_some()));
 
         // Include in the checkpoint the computed effects,  rather than the effects provided.
         // (Which could have been provided by < f+1 byz validators and be incorrect).
-        let digests = effetcs
+        let digests = effects
             .iter()
             .map(|e| {
                 let e = &e.as_ref().unwrap();
@@ -70,7 +70,7 @@ pub trait EffectsStore {
             .collect();
 
         // Index the effects by transaction digest, as we will need to look them up.
-        let mut effect_map: BTreeMap<TransactionDigest, &TransactionEffects> = effetcs
+        let mut effect_map: BTreeMap<TransactionDigest, &TransactionEffects> = effects
             .iter()
             .map(|e| {
                 let e = e.as_ref().unwrap();
@@ -150,18 +150,32 @@ pub trait EffectsStore {
             let next_transaction = *candidates.iter().next().unwrap();
             candidates.remove(next_transaction);
 
+            // If nothing depends on this tx move on to the next
+            let forward_deps = forward_index.get(next_transaction);
+            if forward_deps.is_none() {
+                continue;
+            }
+
             // Check all transactions that depend on it, to see if all
             // dependencies are  satisfied.
-            for dep in forward_index.get(next_transaction).unwrap() {
-                // The candidate is its parent. If it is the last parent the above will be true
-                // but only once, so we should not already have sequenced it.
-                debug_assert!(!master_set.contains(dep));
+            for dep in forward_deps.unwrap() {
+                // If the forward dependency is not included in the current checkpoint, ignore.
+                if !effect_map.contains_key(*dep) {
+                    continue;
+                }
+
+                // We have already included this into the candidate set once.
+                if master_set.contains(dep) {
+                    continue;
+                }
 
                 if effect_map
                     .get(*dep)
                     .unwrap()
                     .dependencies
                     .iter()
+                    // All dependencies sequenced in the master seq or in previous checkpoint
+                    .filter(|item| tx_not_in_checkpoint.contains(*item))
                     .all(|item| master_set.contains(item))
                 {
                     // It seems like all dependencies are satisfied for dep, so sequence it.
@@ -221,5 +235,187 @@ impl CausalOrder for Arc<AuthorityStore> {
         _ckpt_store: &mut CheckpointStore,
     ) -> SuiResult<Vec<ExecutionDigests>> {
         self.causal_order_from_effects(transactions, _ckpt_store)
+    }
+}
+
+impl EffectsStore for BTreeMap<TransactionDigest, TransactionEffects> {
+    fn get_effects(
+        &self,
+        transactions: &[ExecutionDigests],
+    ) -> SuiResult<Vec<Option<TransactionEffects>>> {
+        Ok(transactions
+            .iter()
+            .map(|item| self.get(&item.transaction).cloned())
+            .collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::BTreeMap, env, fs, sync::Arc};
+
+    use crate::checkpoints::causal_order::EffectsStore;
+    use crate::checkpoints::CheckpointStore;
+    use rand::{prelude::StdRng, SeedableRng};
+    use sui_types::{
+        base_types::{ExecutionDigests, ObjectDigest, ObjectID, SequenceNumber, TransactionDigest},
+        gas::GasCostSummary,
+        messages::{ExecutionStatus, TransactionEffects},
+        object::Owner,
+        utils::make_committee_key,
+    };
+    use typed_store::Map;
+
+    fn effects_from(
+        transaction_digest: TransactionDigest,
+        dependencies: Vec<TransactionDigest>,
+    ) -> TransactionEffects {
+        TransactionEffects {
+            // The only fields that matter
+            transaction_digest,
+            dependencies,
+
+            // Other fields do not really matter here
+            status: ExecutionStatus::Success,
+            gas_used: GasCostSummary {
+                computation_cost: 0,
+                storage_cost: 0,
+                storage_rebate: 0,
+            },
+            shared_objects: vec![],
+            created: vec![],
+            mutated: vec![],
+            unwrapped: vec![],
+            deleted: vec![],
+            wrapped: vec![],
+            // Nonsense is ok for the purposes of these tests
+            gas_object: (
+                (
+                    ObjectID::random(),
+                    SequenceNumber::from(0),
+                    ObjectDigest::random(),
+                ),
+                Owner::Immutable,
+            ),
+            events: vec![],
+        }
+    }
+
+    #[test]
+    #[allow(clippy::redundant_clone)]
+    fn causal_just_reorder() {
+        let mut rng = StdRng::from_seed([1; 32]);
+        let (keys, committee) = make_committee_key(&mut rng);
+        let k = keys[0].copy();
+
+        // Setup
+
+        let dir = env::temp_dir();
+        let path = dir.join(format!("SC_{:?}", ObjectID::random()));
+        fs::create_dir(&path).unwrap();
+
+        // Create an authority
+        // Open store first time
+
+        let mut cps = CheckpointStore::open(
+            path,
+            None,
+            committee.epoch,
+            *k.public_key_bytes(),
+            Arc::pin(k.copy()),
+        )
+        .unwrap();
+
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+
+        // Make some transactions
+        let t0 = TransactionDigest::random();
+        let t1 = TransactionDigest::random();
+        let t2 = TransactionDigest::random();
+        let t3 = TransactionDigest::random();
+
+        let e0 = effects_from(t0, vec![]);
+        let e1 = effects_from(t1, vec![t0]);
+        let e2 = effects_from(t2, vec![t1]);
+        let e3 = effects_from(t3, vec![t2]);
+
+        let mut effect_map = BTreeMap::new();
+        effect_map.extend([
+            (t0, e0),
+            (t1, e1.clone()),
+            (t2, e2.clone()),
+            (t3, e3.clone()),
+        ]);
+
+        let input: Vec<_> = vec![e2.clone(), e1.clone(), e3.clone()]
+            .iter()
+            .map(|item| ExecutionDigests::new(item.transaction_digest, item.digest()))
+            .collect();
+
+        // TEST 1
+        // None are recorded as new transactions in the checkpoint DB so the end sequence is empty
+        let x = effect_map.causal_order_from_effects(&input, &mut cps);
+        assert_eq!(x.unwrap().len(), 0);
+
+        cps.extra_transactions.insert(&input[0], &0).unwrap();
+        cps.extra_transactions.insert(&input[1], &1).unwrap();
+        cps.extra_transactions.insert(&input[2], &2).unwrap();
+
+        // TEST 2
+        // The two transactions are recorded as new so they are re-ordered and sequenced
+        let x = effect_map.causal_order_from_effects(&input[..2], &mut cps);
+        assert!(x.clone().unwrap().len() == 2);
+        // Its in the correct order
+        assert!(x.unwrap() == vec![input[1], input[0]]);
+
+        // TEST3
+        // Skip t2. and order [t3, t1]
+        let input: Vec<_> = vec![e3, e1.clone()]
+            .iter()
+            .map(|item| ExecutionDigests::new(item.transaction_digest, item.digest()))
+            .collect();
+
+        let x = effect_map.causal_order_from_effects(&input[..2], &mut cps);
+
+        assert!(x.clone().unwrap().len() == 1);
+        // Its in the correct order
+        assert!(x.unwrap() == vec![input[1]]);
+
+        // Test4
+        // Many dependencies
+        println!("Test 4");
+
+        // Make some transactions
+        let tx = TransactionDigest::random();
+        let ty = TransactionDigest::random();
+
+        let ex = effects_from(tx, vec![t0, t1]);
+        let ey = effects_from(ty, vec![tx, t2]);
+
+        effect_map.extend([(tx, ex.clone()), (ty, ey.clone())]);
+
+        let input: Vec<_> = vec![e2.clone(), ex.clone(), ey.clone(), e1.clone()]
+            .iter()
+            .map(|item| ExecutionDigests::new(item.transaction_digest, item.digest()))
+            .collect();
+
+        cps.extra_transactions.insert(&input[1], &3).unwrap();
+        cps.extra_transactions.insert(&input[2], &4).unwrap();
+
+        assert!(input[1..].len() == 3);
+        let x = effect_map.causal_order_from_effects(&input[1..], &mut cps);
+
+        println!("result: {:?}", x);
+        assert_eq!(x.clone().unwrap().len(), 2);
+        // Its in the correct order
+        assert_eq!(x.unwrap(), vec![input[3], input[1]]);
+
+        // TESt 5 all
+
+        let x = effect_map.causal_order_from_effects(&input, &mut cps);
+
+        println!("result: {:?}", x);
+        assert_eq!(x.clone().unwrap().len(), 4);
     }
 }
