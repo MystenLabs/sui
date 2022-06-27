10a195188c1f99b9bff84a01d7d8c6ce671073bd
Patrick Kuo <patrickkuo@me.com>
2022-06-27 11:13:43 +0100

    [pub sub] - Event subscription using event filter (#2589)
    * filter impl and improvement for pub sub
    
    * * Add sender and digest to delete and publish event
    * some refactoring
    
    * * Add sender, digest, module, package, function and recipient to new_object event
    
    * remove tx digest from Event
    
    * added unit test for filter and event_handler
    
    * pubsub docs
    
    * Make direct edits to new pubsub page
    
    * address PR comment
    Fix test
    
    * rename instigator -> sender
    
    * remove destination_addr
    
    * address PR comment
    fix test
    
    * remove function from Event and event filters
    address PR comments
    
    * return EventEnvelope in subscription
    
    * fixup after rebase
    
    * populate transaction digest in event envelope
    
    * fix test
    
    * Make second round of edits to new pubsub doc
    
    * address PR comment
    
    Co-authored-by: Clay-Mysten <100217682+Clay-Mysten@users.noreply.github.com>

--- crates/sui-core/src/event_handler.rs
+++ crates/sui-core/src/event_handler.rs
@@ -1,13 +1,17 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::collections::BTreeMap;
 use std::sync::Arc;
 
 use move_bytecode_utils::module_cache::SyncModuleCache;
+use serde_json::Value;
+use sui_json_rpc_api::rpc_types::{SuiMoveStruct, SuiMoveValue};
 use tokio_stream::Stream;
 use tracing::{debug, error};
 
-use sui_types::object::ObjectFormatOptions;
+use sui_types::base_types::TransactionDigest;
+use sui_types::object::{MoveObject, ObjectFormatOptions};
 use sui_types::{
     error::{SuiError, SuiResult},
     event::{Event, EventEnvelope},
@@ -15,8 +19,12 @@ use sui_types::{
 };
 
 use crate::authority::{AuthorityStore, ResolverWrapper};
-use crate::event_filter::EventFilter;
 use crate::streamer::Streamer;
+use sui_types::event_filter::EventFilter;
+
+#[cfg(test)]
+#[path = "unit_tests/event_handler_tests.rs"]
+mod event_handler_tests;
 
 pub const EVENT_DISPATCH_BUFFER_SIZE: usize = 1000;
 
@@ -37,21 +45,33 @@ impl EventHandler {
     pub async fn process_events(&self, effects: &TransactionEffects, timestamp_ms: u64) {
         // serially dispatch event processing to honor events' orders.
         for event in &effects.events {
-            if let Err(e) = self.process_event(event, timestamp_ms).await {
+            if let Err(e) = self
+                .process_event(event, timestamp_ms, effects.transaction_digest)
+                .await
+            {
                 error!(error =? e, "Failed to send EventEnvelope to dispatch");
             }
         }
     }
 
-    pub async fn process_event(&self, event: &Event, timestamp_ms: u64) -> SuiResult {
+    pub async fn process_event(
+        &self,
+        event: &Event,
+        timestamp_ms: u64,
+        digest: TransactionDigest,
+    ) -> SuiResult {
         let json_value = match event {
-            Event::MoveEvent(event_obj) => {
+            Event::MoveEvent {
+                type_, contents, ..
+            } => {
                 debug!(event =? event, "Process MoveEvent.");
-                let move_struct = event_obj.to_move_struct_with_resolver(
-                    ObjectFormatOptions::default(),
-                    &self.module_cache,
-                )?;
-                Some(serde_json::to_value(&move_struct).map_err(|e| {
+                // Piggyback on MoveObject's conversion logic.
+                let move_object = MoveObject::new(type_.clone(), contents.clone());
+                let layout =
+                    move_object.get_layout(ObjectFormatOptions::default(), &self.module_cache)?;
+                // Convert into `SuiMoveStruct` which is a mirror of MoveStruct but with additional type supports, (e.g. ascii::String).
+                let move_struct = move_object.to_move_struct(&layout)?.into();
+                Some(to_json_value(move_struct).map_err(|e| {
                     SuiError::ObjectSerializationError {
                         error: e.to_string(),
                     }
@@ -59,7 +79,7 @@ impl EventHandler {
             }
             _ => None,
         };
-        let envelope = EventEnvelope::new(timestamp_ms, None, event.clone(), json_value);
+        let envelope = EventEnvelope::new(timestamp_ms, digest, event.clone(), json_value);
         // TODO store events here
         self.event_streamer.send(envelope).await
     }
@@ -67,4 +87,39 @@ impl EventHandler {
     pub fn subscribe(&self, filter: EventFilter) -> impl Stream<Item = EventEnvelope> {
         self.event_streamer.subscribe(filter)
     }
+}
+
+fn to_json_value(move_struct: SuiMoveStruct) -> Result<Value, serde_json::Error> {
+    // Unwrap MoveStructs
+    let unwrapped = match move_struct {
+        SuiMoveStruct::Runtime(values) => {
+            let values = values
+                .into_iter()
+                .map(|value| match value {
+                    SuiMoveValue::Struct(move_struct) => to_json_value(move_struct),
+                    SuiMoveValue::Vector(values) => to_json_value(SuiMoveStruct::Runtime(values)),
+                    _ => serde_json::to_value(&value),
+                })
+                .collect::<Result<Vec<_>, _>>()?;
+            serde_json::to_value(&values)
+        }
+        // We only care about values here, assuming struct type information is known at the client side.
+        SuiMoveStruct::WithTypes { type_: _, fields } | SuiMoveStruct::WithFields(fields) => {
+            let fields = fields
+                .into_iter()
+                .map(|(key, value)| {
+                    let value = match value {
+                        SuiMoveValue::Struct(move_struct) => to_json_value(move_struct),
+                        SuiMoveValue::Vector(values) => {
+                            to_json_value(SuiMoveStruct::Runtime(values))
+                        }
+                        _ => serde_json::to_value(&value),
+                    };
+                    value.map(|value| (key, value))
+                })
+                .collect::<Result<BTreeMap<_, _>, _>>()?;
+            serde_json::to_value(&fields)
+        }
+    }?;
+    serde_json::to_value(&unwrapped)
 }
