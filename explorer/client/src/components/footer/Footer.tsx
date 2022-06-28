b509f432523e6a403fbd31cb0deaca89987d475d
Andrew Burnie <apburnie@hotmail.co.uk>
2022-06-28 20:27:39 +0100

    adds links to footer

--- explorer/client/src/components/footer/Footer.tsx
+++ explorer/client/src/components/footer/Footer.tsx
@@ -1,8 +1,6 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Link } from 'react-router-dom';
-
 import { CookiesConsent } from '../cookies-consent/CookiesConsent';
 import ExternalLink from '../external-link/ExternalLink';
 
@@ -12,6 +10,77 @@ function Footer() {
     return (
         <div className={styles.footer}>
             <nav className={styles.links}>
+                <div>
+                    <h6>Read</h6>
+                    <ul>
+                        <li>
+                            <ExternalLink
+                                href="https://medium.com/mysten-labs"
+                                label="Blog"
+                            />
+                        </li>
+                        <li>
+                            <ExternalLink
+                                href="https://github.com/MystenLabs/sui/blob/main/doc/paper/sui.pdf"
+                                label="Whitepaper"
+                            />
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h6>Build</h6>
+                    <ul>
+                        <li>
+                            <ExternalLink
+                                href="https://docs.sui.io/"
+                                label="Docs"
+                            />
+                        </li>
+                        <li>
+                            <ExternalLink
+                                href="https://github.com/MystenLabs"
+                                label="GitHub"
+                            />
+                        </li>
+                        <li>
+                            <ExternalLink
+                                href="https://discord.gg/sui"
+                                label="Discord"
+                            />
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h6>Follow</h6>
+                    <ul>
+                        <li>
+                            <ExternalLink
+                                href="https://mystenlabs.com/#community"
+                                label="Press"
+                            />
+                        </li>
+                        <li>
+                            <ExternalLink
+                                href="https://twitter.com/mysten_labs"
+                                label="Twitter"
+                            />
+                        </li>
+                        <li>
+                            <ExternalLink
+                                href="https://www.linkedin.com/company/mysten-labs/"
+                                label="LinkedIn"
+                            />
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <CookiesConsent />
+        </div>
+    );
+}
+
+export default Footer;
+/*
                 <Link to="/" id="homeBtn">
                     Home
                 </Link>
@@ -24,10 +93,4 @@ function Footer() {
                     href="https://docs.sui.io/"
                     label="Developer Hub"
                 />
-            </nav>
-            <CookiesConsent />
-        </div>
-    );
-}
-
-export default Footer;
+                */
