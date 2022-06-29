2647086ba41d7894e6678fa42f5aeabf4f539bc8
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-06-29 23:37:41 +0100

    wallet-ext: content script, ui connections to background service
    * adds a demo method to the dapp to get the user's account
    * requests user's permission if not already given

--- wallet/src/shared/messaging/messages/payloads/permissions/PermissionType.ts
+++ wallet/src/shared/messaging/messages/payloads/permissions/PermissionType.ts
@@ -1,4 +1,4 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export type PermissionType = 'viewAccount';
+export type PermissionType = 'viewAccount' | 'suggestTransactions';
