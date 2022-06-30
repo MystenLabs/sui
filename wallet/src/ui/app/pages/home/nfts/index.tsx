d7ac84c6ba9ddf180554324ff613453328a04a7f
Chris Li <666lcz@gmail.com>
2022-06-30 16:39:24 -0700

    wallet-ext: check has_public_transfer

--- wallet/src/ui/app/pages/home/nfts/index.tsx
+++ wallet/src/ui/app/pages/home/nfts/index.tsx
@@ -11,11 +11,7 @@ function NftsPage() {
     return (
         <ObjectsLayout totalItems={nfts.length} emptyMsg="No NFTs found">
             {nfts.map((anNft) => (
-                <SuiObject
-                    obj={anNft}
-                    sendNFT={true}
-                    key={anNft.reference.objectId}
-                />
+                <SuiObject obj={anNft} key={anNft.reference.objectId} />
             ))}
         </ObjectsLayout>
     );
