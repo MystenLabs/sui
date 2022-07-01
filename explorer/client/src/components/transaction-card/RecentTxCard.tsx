f2a33a2a5442c4ff824ba87fefc5c221d870bd0f
Andrew Burnie <apburnie@hotmail.co.uk>
2022-07-01 22:50:06 +0100

    [explorer] Displays Transaction Timestamps (#2630)
    * displays timestamp as a number in the Tx Results and to Tx tables in Object and Address Results

--- explorer/client/src/components/transaction-card/RecentTxCard.tsx
+++ explorer/client/src/components/transaction-card/RecentTxCard.tsx
@@ -16,6 +16,7 @@ import {
 import { IS_STATIC_ENV } from '../../utils/envUtil';
 import { getAllMockTransaction } from '../../utils/static/searchUtil';
 import { truncate } from '../../utils/stringUtils';
+import { timeAgo } from '../../utils/timeUtils';
 import ErrorResult from '../error-result/ErrorResult';
 import Pagination from '../pagination/Pagination';
 
@@ -42,6 +43,7 @@ type TxnData = {
     txGas: number;
     kind: TransactionKindName | undefined;
     From: string;
+    timestamp_ms?: number;
 };
 
 function generateStartEndRange(
@@ -116,6 +118,9 @@ function LatestTxView({
                         )}
                     >
                         <div className={styles.txcardgridlarge}>TxId</div>
+                        {results.latestTx[0].timestamp_ms && (
+                            <div className={styles.txage}>Time</div>
+                        )}
                         <div className={styles.txtype}>TxType</div>
                         <div className={styles.txstatus}>Status</div>
                         <div className={styles.txgas}>Gas</div>
@@ -134,6 +139,11 @@ function LatestTxView({
                                     alttext={truncate(tx.txId, TRUNCATE_LENGTH)}
                                 />
                             </div>
+                            {tx.timestamp_ms && (
+                                <div className={styles.txage}>{`${timeAgo(
+                                    tx.timestamp_ms
+                                )} ago`}</div>
+                            )}
                             <div className={styles.txtype}> {tx.kind}</div>
                             <div
                                 className={cl(
