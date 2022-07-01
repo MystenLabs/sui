0fdd77016778c5d4116b67ac88da87ce4222ec7c
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-07-01 16:32:34 +0100

    wallet-ext: demo dapp
    * a demo dapp that uses the DappInterface to mint a devnet_nft

---
+++ wallet/examples/demo-nft-dapp/next.config.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
