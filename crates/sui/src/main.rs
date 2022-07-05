16fe689e760944e0610160c4172fe9e61e9bb63e
Patrick Kuo <patrickkuo@me.com>
2022-07-06 00:57:45 +0100

    windows cli color fix (#2987)

--- crates/sui/src/main.rs
+++ crates/sui/src/main.rs
@@ -13,6 +13,9 @@ mod cli_tests;
 
 #[tokio::main]
 async fn main() {
+    #[cfg(windows)]
+    colored::control::set_virtual_terminal(true).unwrap();
+
     let bin_name = env!("CARGO_BIN_NAME");
     let cmd: SuiCommand = SuiCommand::parse();
     let _guard = match cmd {
