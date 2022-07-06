b67a957815bd0024b3da5d0d2096f49bcc6d3fa6
Emma Zhong <emma@mystenlabs.com>
2022-07-06 11:26:12 -0700

    [CLI] make sui move prove call move prover with custom options

--- crates/sui/src/sui_move/prove.rs
+++ crates/sui/src/sui_move/prove.rs
@@ -6,6 +6,8 @@ use move_cli::base::{self, prove};
 use move_package::BuildConfig;
 use std::path::{Path, PathBuf};
 
+const SUI_NATIVE_TEMPLATE: &[u8] = include_bytes!("sui-natives.bpl");
+
 #[derive(Parser)]
 pub struct Prove {
     #[clap(flatten)]
@@ -30,16 +32,33 @@ impl Prove {
             args.push(format!("--config={}", prover_toml.to_string_lossy()));
         }
         args.extend(opts.iter().cloned());
-        let options = move_prover::cli::Options::create_from_args(&args)?;
 
-        prove::run_move_prover(
-            build_config,
-            &rerooted_path,
-            &target_filter,
-            for_test,
-            options,
-        )?;
+        let mut options = move_prover::cli::Options::create_from_args(&args)?;
 
-        Ok(())
+        // provide Sui-specific Boogie template for the native functions to the prover.
+        options.backend.custom_natives =
+            Some(move_prover_boogie_backend::options::CustomNativeOptions {
+                template_bytes: SUI_NATIVE_TEMPLATE.to_vec(),
+                module_instance_names: vec![
+                    (
+                        "0x2::transfer".to_string(),
+                        "transfer_instances".to_string(),
+                    ),
+                    ("0x2::id".to_string(), "id_instances".to_string()),
+                    ("0x2::event".to_string(), "sui_event_instances".to_string()),
+                ],
+            });
+
+        let prover_result = std::thread::spawn(move || {
+            prove::run_move_prover(
+                build_config,
+                &rerooted_path,
+                &target_filter,
+                for_test,
+                options,
+            )
+        });
+
+        prover_result.join().expect("move prover thread panicked")
     }
 }
