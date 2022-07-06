d889bc5b70294bb859b59e0031266c4bc3fea33e
patrick <patrickkuo@me.com>
2022-07-06 15:14:52 +0100

    upgrade jsonrpsee to 0.14.0

--- crates/sui-json-rpc/src/lib.rs
+++ crates/sui-json-rpc/src/lib.rs
@@ -76,31 +76,24 @@ impl JsonRpcServerBuilder {
         use_websocket: bool,
         prometheus_registry: &prometheus::Registry,
     ) -> anyhow::Result<Self> {
-        let (ac_builder, allow_list) = match env::var("ACCESS_CONTROL_ALLOW_ORIGIN") {
-            Ok(value) => {
-                let owned_list: Vec<String> = value
-                    .split(',')
-                    .into_iter()
-                    .map(|s| s.into())
-                    .collect::<Vec<_>>();
-                (
-                    AccessControlBuilder::default().set_allowed_origins(&owned_list)?,
-                    owned_list,
-                )
-            }
-            _ => (AccessControlBuilder::default(), vec![]),
-        };
-
         let server_builder = if use_websocket {
-            let mut builder = WsServerBuilder::default()
-                .set_middleware(ApiMetrics::WebsocketMetrics(WebsocketMetrics {}));
-            if !allow_list.is_empty() {
-                info!("Setting ACCESS_CONTROL_ALLOW_ORIGIN to : {:?}", allow_list);
-                builder = builder.set_allowed_origins(allow_list)?;
-            }
-            ServerBuilder::WsBuilder(builder)
+            ServerBuilder::WsBuilder(
+                WsServerBuilder::default()
+                    .set_middleware(ApiMetrics::WebsocketMetrics(WebsocketMetrics {})),
+            )
         } else {
-            let acl = ac_builder.build();
+            let acl = match env::var("ACCESS_CONTROL_ALLOW_ORIGIN") {
+                Ok(value) => {
+                    let owned_list: Vec<String> = value
+                        .split(',')
+                        .into_iter()
+                        .map(|s| s.into())
+                        .collect::<Vec<_>>();
+                    AccessControlBuilder::default().set_allowed_origins(&owned_list)?
+                }
+                _ => AccessControlBuilder::default(),
+            }
+            .build();
             info!(?acl);
             ServerBuilder::HttpBuilder(
                 HttpServerBuilder::default()
