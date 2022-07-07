f12bd32ccce62b7ca3c68a1e802b13503acdd86e
Adam Welc <adam@mystenlabs.com>
2022-07-06 17:31:26 -0700

    [Move CLI] Updated move CLI to work with flattened pkg core Move command (#3019)
    * [Move CLI] Updated move CLI to work with flattened pkg core Move command
    
    * Fixed linter warnings
    
    * Addressed review comments

--- crates/sui/src/sui_move/build.rs
+++ crates/sui/src/sui_move/build.rs
@@ -1,19 +1,38 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use clap::Parser;
+use move_cli::base::{self, build};
 use move_package::BuildConfig;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-pub fn execute(
-    path: &Path,
-    dump_bytecode_as_base64: bool,
-    build_config: BuildConfig,
-) -> anyhow::Result<()> {
-    if dump_bytecode_as_base64 {
-        let compiled_modules = sui_framework::build_move_package_to_base64(path, build_config)?;
-        println!("{:?}", compiled_modules);
-    } else {
-        sui_framework::build_and_verify_package(path, build_config)?;
+#[derive(Parser)]
+pub struct Build {
+    #[clap(flatten)]
+    pub build: build::Build,
+    /// Whether we are printing in base64.
+    #[clap(long, global = true)]
+    pub dump_bytecode_as_base64: bool,
+}
+
+impl Build {
+    pub fn execute(&self, path: Option<PathBuf>, build_config: BuildConfig) -> anyhow::Result<()> {
+        let rerooted_path = base::reroot_path(path)?;
+        Self::execute_internal(&rerooted_path, build_config, self.dump_bytecode_as_base64)
     }
-    Ok(())
+    pub fn execute_internal(
+        rerooted_path: &Path,
+        build_config: BuildConfig,
+        dump_bytecode_as_base64: bool,
+    ) -> anyhow::Result<()> {
+        // find manifest file directory from a given path or (if missing) from current dir
+        if dump_bytecode_as_base64 {
+            let compiled_modules =
+                sui_framework::build_move_package_to_base64(rerooted_path, build_config)?;
+            println!("{:?}", compiled_modules);
+        } else {
+            sui_framework::build_and_verify_package(rerooted_path, build_config)?;
+        }
+        Ok(())
+    }
 }
