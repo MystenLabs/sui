67c2d13a475ad6f77ac49ac0357e26ba9d4e49ee
Adam Welc <adam@mystenlabs.com>
2022-07-07 11:52:12 -0700

    Create prover options before calling into core Move

--- crates/sui/src/sui_move/prove.rs
+++ crates/sui/src/sui_move/prove.rs
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use clap::Parser;
-use move_cli::base::prove;
+use move_cli::base::{self, prove};
 use move_package::BuildConfig;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser)]
 pub struct Prove {
@@ -14,7 +14,32 @@ pub struct Prove {
 
 impl Prove {
     pub fn execute(self, path: Option<PathBuf>, build_config: BuildConfig) -> anyhow::Result<()> {
-        self.prove.execute(path, build_config)?;
+        let rerooted_path = base::reroot_path(path)?;
+        let prove::Prove {
+            target_filter,
+            for_test,
+            options,
+        } = self.prove;
+        let opts = match options {
+            Some(prove::ProverOptions::Options(opts)) => opts,
+            _ => vec![],
+        };
+        let mut args = vec!["package".to_string()];
+        let prover_toml = Path::new(&rerooted_path).join("Prover.toml");
+        if prover_toml.exists() {
+            args.push(format!("--config={}", prover_toml.to_string_lossy()));
+        }
+        args.extend(opts.iter().cloned());
+        let options = move_prover::cli::Options::create_from_args(&args)?;
+
+        prove::run_move_prover(
+            build_config,
+            &rerooted_path,
+            &target_filter,
+            for_test,
+            options,
+        )?;
+
         Ok(())
     }
 }
