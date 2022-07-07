a60bd88169fdc7c385272b5ce239d28ff9dbd56f
Todd Nowacki <tmn@mystenlabs.com>
2022-07-07 11:39:33 -0700

    [sui-verifier] Small cleanup + comments
    - Removed id_immutable pass, it is covered by `VersionedID` not having `drop`
    - Removed check for types with `key` not having `drop`, it is covered by `VersionedID`
    - Added doc comment to `struct_with_key_verifier`

--- crates/sui-verifier/src/lib.rs
+++ crates/sui-verifier/src/lib.rs
@@ -5,7 +5,6 @@ pub mod verifier;
 
 pub mod entry_points_verifier;
 pub mod global_storage_access_verifier;
-pub mod id_immutable_verifier;
 pub mod id_leak_verifier;
 pub mod private_generics;
 pub mod struct_with_key_verifier;
