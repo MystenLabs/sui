039aaa27cf9b02cc8f596665e677369a8b2b5d57
Evan Chan <velvia@gmail.com>
2022-07-12 11:43:43 -0700

    Restore original setting

--- narwhal/benchmark/fabfile.py
+++ narwhal/benchmark/fabfile.py
@@ -21,7 +21,7 @@ def local(ctx, debug=True):
         'workers': 1,
         'rate': 50_000,
         'tx_size': 512,
-        'duration': 10,
+        'duration': 20,
         'mem_profiling': False
     }
     node_params = {
