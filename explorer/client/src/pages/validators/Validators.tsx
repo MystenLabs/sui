d616459e85e467f06b5c62f69f15dd94cd2aeb70
Stella Cannefax <stella@mystenlabs.com>
2022-07-13 21:55:58 -0500

    add license to validators

--- explorer/client/src/pages/validators/Validators.tsx
+++ explorer/client/src/pages/validators/Validators.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import { Base64DataBuffer } from '@mysten/sui.js';
 import { useState, useContext, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
