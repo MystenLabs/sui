2bf026e180688992544a6ec10f651c57950a9675
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-07-14 20:19:44 +0300

    wallet-ext: updated font with new icons
    * new icons:
      * arrow-left
      * clipboard
      * globe
      * logout
      * person
      * sui-chevron-right
    * used icomoon to optimize all the svg iamges

--- wallet/font-icons/output/sui-icons.ts
+++ wallet/font-icons/output/sui-icons.ts
@@ -3,13 +3,19 @@
 
 export enum SuiIcons {
   Apps = "sui-icons-apps",
+  ArrowLeft = "sui-icons-arrow-left",
+  Clipboard = "sui-icons-clipboard",
+  Globe = "sui-icons-globe",
   History = "sui-icons-history",
+  Logout = "sui-icons-logout",
   Nfts = "sui-icons-nfts",
+  Person = "sui-icons-person",
+  SuiChevronRight = "sui-icons-sui-chevron-right",
   SuiLogoIcon = "sui-icons-sui-logo-icon",
   SuiLogoTxt = "sui-icons-sui-logo-txt",
   Tokens = "sui-icons-tokens"
 }
 
-export type SuiIconsClassname = "sui-icons-apps" | "sui-icons-history" | "sui-icons-nfts" | "sui-icons-sui-logo-icon" | "sui-icons-sui-logo-txt" | "sui-icons-tokens"
-export type SuiIconsIcon = "apps" | "history" | "nfts" | "sui-logo-icon" | "sui-logo-txt" | "tokens"
+export type SuiIconsClassname = "sui-icons-apps" | "sui-icons-arrow-left" | "sui-icons-clipboard" | "sui-icons-globe" | "sui-icons-history" | "sui-icons-logout" | "sui-icons-nfts" | "sui-icons-person" | "sui-icons-sui-chevron-right" | "sui-icons-sui-logo-icon" | "sui-icons-sui-logo-txt" | "sui-icons-tokens"
+export type SuiIconsIcon = "apps" | "arrow-left" | "clipboard" | "globe" | "history" | "logout" | "nfts" | "person" | "sui-chevron-right" | "sui-logo-icon" | "sui-logo-txt" | "tokens"
 export const SuiIconsPrefix = "sui-icons-"
