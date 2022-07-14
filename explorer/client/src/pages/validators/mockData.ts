c5b6b7c60ce081485d215cc26effb46bfd474ec4
Stella Cannefax <stella@mystenlabs.com>
2022-07-13 20:16:25 -0500

    move mock validator data to own file

---
+++ explorer/client/src/pages/validators/mockData.ts
@@ -0,0 +1,62 @@
+export const validators = [
+    {
+        name: 'Jump Crypto',
+        stake: 9_220_000,
+        stakePercent: '5.2%',
+        position: 1,
+    },
+    {
+        name: 'Blockdaemon',
+        stake: 8_220_000,
+        stakePercent: '4.2%',
+        position: 2,
+    },
+    {
+        name: 'Kraken',
+        stake: 4_650_000,
+        stakePercent: '2.69%',
+        position: 3,
+    },
+    {
+        name: 'Coinbase',
+        stake: 4_550_000,
+        stakePercent: '2.63%',
+        position: 4,
+    },
+    {
+        name: 'a16z',
+        stake: 2_860_000,
+        stakePercent: '1.58%',
+        position: 5,
+    },
+    {
+        name: 'Figment',
+        stake: 2_840_000,
+        stakePercent: '1.63%',
+        position: 6,
+    },
+    {
+        name: '0x813e...d21f07',
+        stake: 2_730_000,
+        stakePercent: '1.58%',
+        position: 7,
+    },
+    {
+        name: '0x813e...d21f07',
+        stake: 2_730_000,
+        stakePercent: '1.58%',
+        position: 8,
+    },
+    {
+        name: '0x813e...d21f07',
+        stake: 2_730_000,
+        stakePercent: '1.58%',
+        position: 9,
+    },
+    {
+        name: '0x813e...d21f07',
+        stake: 2_730_000,
+        stakePercent: '1.58%',
+        position: 10,
+    },
+];
