e9be29517bb6e6076edca47ef265b0c06815ad99
Stella Cannefax <stella@mystenlabs.com>
2022-07-13 22:08:02 -0500

    Update mockData.ts

--- explorer/client/src/pages/validators/mockData.ts
+++ explorer/client/src/pages/validators/mockData.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import { Base64DataBuffer } from '@mysten/sui.js';
 
 import { type Validator, type ValidatorState } from './Validators';
