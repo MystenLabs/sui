45f071e075c3bf07eea0507f81e02115d36b66dd
Andrew Burnie <apburnie@hotmail.co.uk>
2022-07-18 11:36:54 +0100

    places stats on same line as pagination

--- explorer/client/src/components/pagination/PaginationWrapper.tsx
+++ explorer/client/src/components/pagination/PaginationWrapper.tsx
@@ -2,14 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 import { useState, useCallback } from 'react';
 
+import TabFooter from '../../components/tabs/TabFooter';
+
 import styles from './PaginationWrapper.module.css';
 
 export default function PaginationWrapper({
     results,
     viewComponentFn,
+    stats,
 }: {
     results: any;
     viewComponentFn: Function;
+    stats?: {
+        stats_text: string;
+        count: number;
+    };
 }) {
     const [pageIndex, setPageIndex] = useState(0);
 
@@ -76,25 +83,34 @@ export default function PaginationWrapper({
         return (
             <>
                 {viewComponentFn(objectSample)}
-                {FirstButton}
-                {Array(FINAL_PAGE_NO)
-                    .fill(0)
-                    .map((_: number, arrayIndex: number) => (
-                        <button
-                            key={`page-${arrayIndex}`}
-                            className={
-                                pageIndex === arrayIndex
-                                    ? styles.pagenumber
-                                    : styles.btncontainer
-                            }
-                            id="firstBtn"
-                            onClick={handleBtnClick(arrayIndex)}
-                            disabled={pageIndex === arrayIndex}
-                        >
-                            {arrayIndex + 1}
-                        </button>
-                    ))}
-                {LastButton}
+                <div className={styles.footer}>
+                    <div>
+                        {FirstButton}
+                        {Array(FINAL_PAGE_NO)
+                            .fill(0)
+                            .map((_: number, arrayIndex: number) => (
+                                <button
+                                    key={`page-${arrayIndex}`}
+                                    className={
+                                        pageIndex === arrayIndex
+                                            ? styles.pagenumber
+                                            : styles.btncontainer
+                                    }
+                                    id="firstBtn"
+                                    onClick={handleBtnClick(arrayIndex)}
+                                    disabled={pageIndex === arrayIndex}
+                                >
+                                    {arrayIndex + 1}
+                                </button>
+                            ))}
+                        {LastButton}
+                    </div>
+                    {stats && (
+                        <div>
+                            <TabFooter stats={stats} />
+                        </div>
+                    )}
+                </div>
             </>
         );
     }
@@ -102,76 +118,89 @@ export default function PaginationWrapper({
     return (
         <>
             {viewComponentFn(objectSample)}
-            {FINAL_PAGE_NO > 1 && (
-                <>
-                    {FirstButton}
-                    <button
-                        className={
-                            pageIndex === 0
-                                ? styles.pagenumber
-                                : styles.btncontainer
-                        }
-                        id="firstBtn"
-                        onClick={handleBtnClick(0)}
-                        disabled={pageIndex === 0}
-                    >
-                        1
-                    </button>
+            <div className={styles.footer}>
+                <div>
+                    {FINAL_PAGE_NO > 1 && (
+                        <>
+                            {FirstButton}
+                            <button
+                                className={
+                                    pageIndex === 0
+                                        ? styles.pagenumber
+                                        : styles.btncontainer
+                                }
+                                id="firstBtn"
+                                onClick={handleBtnClick(0)}
+                                disabled={pageIndex === 0}
+                            >
+                                1
+                            </button>
 
-                    <button
-                        className={
-                            pageIndex === 1
-                                ? styles.pagenumber
-                                : styles.btncontainer
-                        }
-                        id="secondBtn"
-                        onClick={handleBtnClick(1)}
-                        disabled={pageIndex === 1}
-                    >
-                        2
-                    </button>
-                    {pageIndex > 2 && (
-                        <button className={styles.nointeract}>...</button>
+                            <button
+                                className={
+                                    pageIndex === 1
+                                        ? styles.pagenumber
+                                        : styles.btncontainer
+                                }
+                                id="secondBtn"
+                                onClick={handleBtnClick(1)}
+                                disabled={pageIndex === 1}
+                            >
+                                2
+                            </button>
+                            {pageIndex > 2 && (
+                                <button className={styles.nointeract}>
+                                    ...
+                                </button>
+                            )}
+
+                            {pageIndex > 1 && pageIndex < FINAL_PAGE_NO - 2 && (
+                                <button className={styles.pagenumber}>
+                                    {pageIndex + 1}
+                                </button>
+                            )}
+
+                            {pageIndex < FINAL_PAGE_NO - 3 && (
+                                <button className={styles.nointeract}>
+                                    ...
+                                </button>
+                            )}
+
+                            <button
+                                className={
+                                    pageIndex === FINAL_PAGE_NO - 2
+                                        ? styles.pagenumber
+                                        : styles.btncontainer
+                                }
+                                id="secondLastBtn"
+                                onClick={handleBtnClick(FINAL_PAGE_NO - 2)}
+                                disabled={pageIndex === FINAL_PAGE_NO - 2}
+                            >
+                                {FINAL_PAGE_NO - 1}
+                            </button>
+                            <button
+                                id="lastBtn"
+                                disabled={pageIndex === FINAL_PAGE_NO - 1}
+                                onClick={handleBtnClick(FINAL_PAGE_NO - 1)}
+                                className={
+                                    pageIndex === FINAL_PAGE_NO - 1
+                                        ? styles.pagenumber
+                                        : styles.btncontainer
+                                }
+                            >
+                                {FINAL_PAGE_NO}
+                            </button>
+
+                            {LastButton}
+                        </>
                     )}
-
-                    {pageIndex > 1 && pageIndex < FINAL_PAGE_NO - 2 && (
-                        <button className={styles.pagenumber}>
-                            {pageIndex + 1}
-                        </button>
-                    )}
-
-                    {pageIndex < FINAL_PAGE_NO - 3 && (
-                        <button className={styles.nointeract}>...</button>
-                    )}
-
-                    <button
-                        className={
-                            pageIndex === FINAL_PAGE_NO - 2
-                                ? styles.pagenumber
-                                : styles.btncontainer
-                        }
-                        id="secondLastBtn"
-                        onClick={handleBtnClick(FINAL_PAGE_NO - 2)}
-                        disabled={pageIndex === FINAL_PAGE_NO - 2}
-                    >
-                        {FINAL_PAGE_NO - 1}
-                    </button>
-                    <button
-                        id="lastBtn"
-                        disabled={pageIndex === FINAL_PAGE_NO - 1}
-                        onClick={handleBtnClick(FINAL_PAGE_NO - 1)}
-                        className={
-                            pageIndex === FINAL_PAGE_NO - 1
-                                ? styles.pagenumber
-                                : styles.btncontainer
-                        }
-                    >
-                        {FINAL_PAGE_NO}
-                    </button>
-
-                    {LastButton}
-                </>
-            )}
+                </div>
+                {stats && (
+                    <div>
+                        <TabFooter stats={stats} />
+                    </div>
+                )}
+            </div>
         </>
     );
 }
