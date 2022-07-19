c90fdfaf7eb500f057c426b99156d3443b8e9bac
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-07-19 13:41:24 +0300

    wallet-ext: home/tokens view ui update
    * main buttons are disabled until we implement the required functionality
    * left send and stake buttons until we implement the new required views
    * show stake button only for SUI (not available for other coins)

---
+++ wallet/src/ui/app/pages/home/tokens/coin-balance/index.tsx
@@ -0,0 +1,71 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import cl from 'classnames';
+import { memo, useMemo } from 'react';
+import { useIntl } from 'react-intl';
+import { Link } from 'react-router-dom';
+
+import { useMiddleEllipsis } from '_hooks';
+import { Coin, GAS_TYPE_ARG } from '_redux/slices/sui-objects/Coin';
+import { balanceFormatOptions } from '_shared/formatting';
+
+import st from './CoinBalance.module.scss';
+
+export type CoinProps = {
+    type: string;
+    balance: bigint;
+    hideStake?: boolean;
+    mode?: 'row-item' | 'standalone';
+};
+
+function CoinBalance({
+    type,
+    balance,
+    hideStake = false,
+    mode = 'row-item',
+}: CoinProps) {
+    const symbol = useMemo(() => Coin.getCoinSymbol(type), [type]);
+    const intl = useIntl();
+    const balanceFormatted = useMemo(
+        () => intl.formatNumber(balance, balanceFormatOptions),
+        [intl, balance]
+    );
+    const sendUrl = useMemo(
+        () => `/send?${new URLSearchParams({ type }).toString()}`,
+        [type]
+    );
+    const stakeUrl = useMemo(
+        () => `/stake?${new URLSearchParams({ type }).toString()}`,
+        [type]
+    );
+    const showStake = !hideStake && GAS_TYPE_ARG === type;
+    const shortenType = useMiddleEllipsis(type, 30);
+    return (
+        <div className={cl(st.container, st[mode])}>
+            <div className={cl(st.valuesContainer, st[mode])}>
+                <span className={cl(st.value, st[mode])}>
+                    {balanceFormatted}
+                </span>
+                <span className={cl(st.symbol, st[mode])}>{symbol}</span>
+            </div>
+            <div className={cl(st.typeActionsContainer, st[mode])}>
+                {mode === 'row-item' ? (
+                    <span className={st.type} title={type}>
+                        {shortenType}
+                    </span>
+                ) : null}
+                <Link className={cl('btn', st.action)} to={sendUrl}>
+                    Send
+                </Link>
+                {showStake ? (
+                    <Link className={cl('btn', st.action)} to={stakeUrl}>
+                        Stake
+                    </Link>
+                ) : null}
+            </div>
+        </div>
+    );
+}
+
+export default memo(CoinBalance);
