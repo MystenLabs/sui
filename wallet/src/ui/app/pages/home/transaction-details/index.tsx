aa6c272a1558369a9590c68bb54b25e42110d0d5
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-07-19 15:07:31 +0300

    wallet-ext: fix ts error after sdk update

--- wallet/src/ui/app/pages/home/transaction-details/index.tsx
+++ wallet/src/ui/app/pages/home/transaction-details/index.tsx
@@ -28,6 +28,8 @@ const txKindToTxt: Record<TransactionKindName, string> = {
     TransferObject: 'Object transfer',
     Call: 'Call',
     Publish: 'Publish',
+    TransferSui: 'Sui transfer',
+    ChangeEpoch: 'Change epoch',
 };
 
 function TransactionDetailsPage() {
