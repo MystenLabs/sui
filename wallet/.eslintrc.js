e7a7c001e37288b8d79829f488e9ab8078b95a33
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-07-21 11:37:58 +0300

    wallet-ext: update deps

--- wallet/.eslintrc.js
+++ wallet/.eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
                 ],
                 pathGroups: [
                     {
-                        pattern: '{.,..,_*}/**/*.?(s)css',
+                        pattern: '{.,..,_*,*}/**/*.?(s)css',
                         group: 'type',
                         position: 'after',
                     },
