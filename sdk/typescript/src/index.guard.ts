4acb7f20ab606bab2cfbf7c2a1577aef4102dc67
punwai <pun@mystenlabs.com>
2022-07-21 00:04:20 -0700

    fix error on guards

--- sdk/typescript/src/index.guard.ts
+++ sdk/typescript/src/index.guard.ts
@@ -1,6 +1,9 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 /*
  * Generated type guards for "index.ts".
  * WARNING: Do not manually change this file.
@@ -436,8 +439,8 @@ export function isAuthorityQuorumSignInfo(obj: any, _argumentName?: string): obj
             typeof obj === "object" ||
             typeof obj === "function") &&
         isSequenceNumber(obj.epoch) as boolean &&
-        Array.isArray(obj.signatures) &&
-        obj.signatures.every((e: any) =>
+        Array.isArray(obj.signature) &&
+        obj.signature.every((e: any) =>
             isTransactionDigest(e) as boolean
         )
     )
