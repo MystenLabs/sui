32d30479c7a2b7f8e6aa3f16f5492ce1bdabfcfb
daoauth <57783762+daoauth@users.noreply.github.com>
2022-07-22 23:39:33 +0900

    Add Ed25519Keypair.fromDerivePath method (#2900)
    * Add Ed25519Keypair.fromDerivePath method
    
    * add ed25519-hd-key
    
    * lint
    
    * update: bip39-light.d.ts
    
    * Update ed25519-keypair.ts
    
    * chore: run prettier
    
    * wallet-ext: add events polyfill
    
    * fixes error when building the wallet
    
    Co-authored-by: François Garillot <francois@garillot.net>
    Co-authored-by: Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>

---
+++ sdk/typescript/src/types/bip39-light.d.ts
@@ -0,0 +1,20 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+declare module 'bip39-light' {
+  export function entropyToMnemonic(
+    entropy: Buffer | string,
+    wordlist?: string[]
+  ): string;
+  export function generateMnemonic(
+    strength?: number,
+    rng?: (size: number) => Buffer,
+    wordlist?: string[]
+  ): string;
+  export function mnemonicToSeed(mnemonic: string, password?: string): Buffer;
+  export function mnemonicToSeedHex(
+    mnemonic: string,
+    password?: string
+  ): string;
+  export function validateMnemonic(mnemonic: string): boolean;
+}
