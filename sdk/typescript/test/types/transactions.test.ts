5dabe6492e55c41cbb3f5a9e34db81dece1cd415
Stella Cannefax <stella@mystenlabs.com>
2022-07-22 22:38:11 +0200

    Revert "fix transaction definition test"
    This reverts commit 29613801ac4ddac7f5edaa887797735a317b8539.

--- sdk/typescript/test/types/transactions.test.ts
+++ sdk/typescript/test/types/transactions.test.ts
@@ -8,10 +8,10 @@ import { isTransactionResponse } from '../../src/index.guard';
 describe('Test Transaction Definition', () => {
   it('Test against different transaction definitions', () => {
     const txns = mockTransactionData;
-
-    expect(isTransactionResponse(txns['move_call']['EffectResponse'])).toBeTruthy();
-    expect(isTransactionResponse(txns['transfer']['EffectResponse'])).toBeTruthy();
-    expect(isTransactionResponse(txns['coin_split']['SplitCoinResponse'])).toBeTruthy();
+    
+    expect(isTransactionResponse(txns['move_call'])).toBeTruthy();
+    expect(isTransactionResponse(txns['transfer'])).toBeTruthy();
+    expect(isTransactionResponse(txns['coin_split'])).toBeTruthy();
     // TODO: add mock data for failed transaction
     // expect(
     //   isTransactionEffectsResponse(txns['fail'])
