d621d3f5ef8e25d6d706ea567d6c5d69aef31039
damirka <damirka.ru@gmail.com>
2022-07-25 19:59:11 +0300

    [sdk] adds experimental section

---
+++ sdk/typescript/experimental/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    sdk: './src/index.ts'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: false,
+    library: 'sui',
+    libraryTarget: 'window'
+  },
+};
