522a787471a28280c1d09876b6b0c94d15edbed1
Stella Cannefax <stella@mystenlabs.com>
2022-07-29 01:26:22 +0200

    add missing licenses

--- sdk/typescript/src/types/events.ts
+++ sdk/typescript/src/types/events.ts
@@ -1,14 +1,5 @@
-/*
-"newObject": {
-    "packageId": "0x0000000000000000000000000000000000000002",
-    "transactionModule": "coin",
-    "sender": "0x9b7a3fb24a52703456a16cb0a5b3ebc481ba53a2",
-    "recipient": {
-        "AddressOwner": "0x9b7a3fb24a52703456a16cb0a5b3ebc481ba53a2"
-    },
-    "objectId": "0xa3f1a121797295c6ed5669eb2a59d7893899e671"
-}
-*/
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 
 import { SuiAddress, ObjectOwner } from "./common";
 import { ObjectId, SequenceNumber } from "./objects";
