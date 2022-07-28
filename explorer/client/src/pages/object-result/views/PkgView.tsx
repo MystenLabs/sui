3b73db8bf4be2a9c908979445e8f160728e995fe
Andrew Burnie <apburnie@hotmail.co.uk>
2022-07-28 17:54:47 +0100

    Package Description is under a tab

--- explorer/client/src/pages/object-result/views/PkgView.tsx
+++ explorer/client/src/pages/object-result/views/PkgView.tsx
@@ -1,5 +1,6 @@
 import Longtext from '../../../components/longtext/Longtext';
 import ModulesWrapper from '../../../components/module/ModulesWrapper';
+import Tabs from '../../../components/tabs/Tabs';
 import TxForID from '../../../components/transactions-for-id/TxForID';
 import theme from '../../../styles/theme.module.css';
 import { getOwnerStr } from '../../../utils/objectUtils';
@@ -17,7 +18,6 @@ function PkgView({ data }: { data: DataType }) {
         owner: getOwnerStr(data.owner),
     };
 
-    const descriptionTitle = 'Package Description';
     const isPublisherGenesis =
         viewedData.objType === 'Move Package' &&
         viewedData?.publisherAddress === 'Genesis';
@@ -29,51 +29,58 @@ function PkgView({ data }: { data: DataType }) {
         .filter(([key, _]) => key !== 'name')
         .filter(([_, value]) => checkIsPropertyType(value));
 
+    const defaultactivetab = 0;
+
     return (
         <div className={styles.resultbox}>
             <div className={`${styles.textbox} ${styles.noaccommodate}`}>
                 {viewedData.name && <h1>{viewedData.name}</h1>}{' '}
-                <h2 className={styles.header}>{descriptionTitle}</h2>
-                <div className={theme.textresults} id="descriptionResults">
-                    <div>
-                        <div>Object ID</div>
-                        <div id="objectID">
-                            <Longtext
-                                text={viewedData.id}
-                                category="objects"
-                                isLink={false}
-                            />
-                        </div>
-                    </div>
-                    {data.data?.tx_digest && !isPublisherGenesis && (
+                <Tabs selected={defaultactivetab}>
+                    <div
+                        title="Details"
+                        className={theme.textresults}
+                        id="descriptionResults"
+                    >
                         <div>
-                            <div>Last Transaction ID</div>
-                            <div id="lasttxID">
+                            <div>Object ID</div>
+                            <div id="objectID">
                                 <Longtext
-                                    text={data.data?.tx_digest}
-                                    category="transactions"
-                                    isLink={true}
+                                    text={viewedData.id}
+                                    category="objects"
+                                    isLink={false}
                                 />
                             </div>
                         </div>
-                    )}
-                    <div>
-                        <div>Version</div>
-                        <div>{viewedData.version}</div>
-                    </div>
-                    {viewedData?.publisherAddress && (
-                        <div>
-                            <div>Publisher</div>
-                            <div id="lasttxID">
-                                <Longtext
-                                    text={viewedData.publisherAddress}
-                                    category="addresses"
-                                    isLink={!isPublisherGenesis}
-                                />
+                        {data.data?.tx_digest && !isPublisherGenesis && (
+                            <div>
+                                <div>Last Transaction ID</div>
+                                <div id="lasttxID">
+                                    <Longtext
+                                        text={data.data?.tx_digest}
+                                        category="transactions"
+                                        isLink={true}
+                                    />
+                                </div>
                             </div>
+                        )}
+                        <div>
+                            <div>Version</div>
+                            <div>{viewedData.version}</div>
                         </div>
-                    )}
-                </div>
+                        {viewedData?.publisherAddress && (
+                            <div>
+                                <div>Publisher</div>
+                                <div id="lasttxID">
+                                    <Longtext
+                                        text={viewedData.publisherAddress}
+                                        category="addresses"
+                                        isLink={!isPublisherGenesis}
+                                    />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </Tabs>
                 <ModulesWrapper
                     data={{
                         title: 'Modules',
