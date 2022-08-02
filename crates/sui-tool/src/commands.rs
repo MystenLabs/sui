5a6df0590af60903637f7e80e5ab1fd439a9ef61
Xun Li <lxfind@gmail.com>
2022-08-02 11:44:44 -0700

    Add fetch-transaction to sui-tool (#3717)

--- crates/sui-tool/src/commands.rs
+++ crates/sui-tool/src/commands.rs
@@ -63,6 +63,14 @@ pub enum ToolCommand {
         #[clap(long = "no-header", help = "don't show header in concise output")]
         no_header: bool,
     },
+    #[clap(name = "fetch-transaction")]
+    FetchTransaction {
+        #[clap(long = "genesis")]
+        genesis: PathBuf,
+
+        #[clap(long, help = "The object ID to fetch")]
+        digest: TransactionDigest,
+    },
     /// Tool to read validator & gateway db.
     #[clap(name = "db-tool")]
     DbTool {
@@ -422,6 +430,20 @@ impl ToolCommand {
                 } else {
                     println!("{}", VerboseObjectOutput(output));
                 }
+            }
+            ToolCommand::FetchTransaction { genesis, digest } => {
+                let clients = make_clients(genesis)?;
+
+                let responses = join_all(clients.iter().map(|(name, client)| async {
+                    let result = client
+                        .handle_transaction_info_request(TransactionInfoRequest {
+                            transaction_digest: digest,
+                        })
+                        .await;
+                    (*name, result)
+                }))
+                .await;
+                println!("{:#?}", responses);
             }
             ToolCommand::DbTool { db_path, cmd } => {
                 let path = PathBuf::from(db_path);
