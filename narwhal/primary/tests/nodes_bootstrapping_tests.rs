321d458b8faa82001bf45f0b2c408e33331850e0
Anastasios Kichidis <akihidis@gmail.com>
2022-08-04 17:19:52 +0100

    [test] loss of liveness end to end test (MystenLabs/narwhal#652)

--- narwhal/primary/tests/nodes_bootstrapping_tests.rs
+++ narwhal/primary/tests/nodes_bootstrapping_tests.rs
@@ -1,10 +1,8 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-use itertools::Itertools;
-use std::{collections::HashMap, time::Duration};
+use std::time::Duration;
 use telemetry_subscribers::TelemetryGuards;
 use test_utils::cluster::Cluster;
-use tracing::info;
 
 /// Nodes will be started in a staggered fashion. This is simulating
 /// a real world scenario where nodes across validators will not start
@@ -26,24 +24,16 @@ async fn test_node_staggered_starts() {
 
     tokio::time::sleep(node_staggered_delay).await;
 
-    let rounds = authorities_latest_commit_round(&cluster).await;
-    assert_eq!(
-        rounds.len(),
-        0,
-        "No node should be able to commit, no reported round was expected"
-    );
+    // No node should be able to commit, no reported round was expected
+    cluster.assert_progress(0, 0).await;
 
     // ==== Start second authority ====
     cluster.authority(1).start(false, Some(1)).await;
 
     tokio::time::sleep(node_staggered_delay).await;
 
-    let rounds = authorities_latest_commit_round(&cluster).await;
-    assert_eq!(
-        rounds.len(),
-        0,
-        "No node should be able to commit, no reported round was expected"
-    );
+    // No node should be able to commit, no reported round was expected
+    cluster.assert_progress(0, 0).await;
 
     // ==== Start third authority ====
     // Now 2f + 1 nodes are becoming available and we expect all the nodes to
@@ -52,19 +42,8 @@ async fn test_node_staggered_starts() {
 
     tokio::time::sleep(node_staggered_delay).await;
 
-    let rounds = authorities_latest_commit_round(&cluster).await;
-    assert_eq!(
-        rounds.len(),
-        3,
-        "Expected to have received commit metrics from only three nodes"
-    );
-    assert!(rounds.values().all(|v| *v > 1f64), "We have only (f) unavailable nodes, so all should have made progress and committed at least after the first round");
-
-    // We expect all the nodes to have managed to catch up by now
-    // and be pretty much in similar rounds. The threshold here is
-    // not defined by some rule but more of an expectation.
-    let (min, max) = rounds.values().into_iter().minmax().into_option().unwrap();
-    assert!(max - min <= 2.0, "Nodes shouldn't be that behind");
+    // We have only (f) unavailable nodes, so all should have made progress and committed at least after the first round
+    cluster.assert_progress(3, 2).await;
 
     // ==== Start fourth authority ====
     // Now 3f + 1 nodes are becoming available (the whole network) and all the nodes
@@ -73,19 +52,8 @@ async fn test_node_staggered_starts() {
 
     tokio::time::sleep(node_staggered_delay).await;
 
-    let rounds = authorities_latest_commit_round(&cluster).await;
-    assert_eq!(
-        rounds.len(),
-        4,
-        "Expected to have received commit metrics from all nodes"
-    );
-    assert!(rounds.values().all(|v| v > &1.0), "All nodes are available so all should have made progress and committed at least after the first round");
-
-    // We expect all the nodes to have managed to catch up by now
-    // and be pretty much in similar rounds. The threshold here is
-    // not defined by some rule but more of an expectation.
-    let (min, max) = rounds.values().into_iter().minmax().into_option().unwrap();
-    assert!(max - min <= 2.0, "Nodes shouldn't be that behind");
+    // All nodes are available so all should have made progress and committed at least after the first round
+    cluster.assert_progress(4, 2).await;
 }
 
 #[ignore]
@@ -114,19 +82,7 @@ async fn test_second_node_restart() {
     tokio::time::sleep(node_advance_delay).await;
 
     // Ensure that nodes have made progress
-    let rounds = authorities_latest_commit_round(&cluster).await;
-    assert_eq!(
-        rounds.len(),
-        4,
-        "Expected to have received commit metrics from all nodes"
-    );
-    assert!(rounds.values().all(|v| v > &1.0), "All nodes are available so all should have made progress and committed at least after the first round");
-
-    // We expect all the nodes to have managed to catch up by now
-    // and be pretty much in similar rounds. The threshold here is
-    // not defined by some rule but more of an expectation.
-    let (min, max) = rounds.values().into_iter().minmax().into_option().unwrap();
-    assert!(max - min <= 2.0, "Nodes shouldn't be that behind");
+    cluster.assert_progress(4, 2).await;
 
     // Now restart node 3 with some delay between
     cluster.authority(3).restart(true, restart_delay).await;
@@ -135,39 +91,122 @@ async fn test_second_node_restart() {
     tokio::time::sleep(node_advance_delay).await;
 
     // Ensure that nodes have made progress
-    let rounds = authorities_latest_commit_round(&cluster).await;
-    assert_eq!(
-        rounds.len(),
-        4,
-        "Expected to have received commit metrics from all nodes"
-    );
-    assert!(rounds.values().all(|v| v > &1.0), "All nodes are available so all should have made progress and committed at least after the first round");
-
-    // We expect all the nodes to have managed to catch up by now
-    // and be pretty much in similar rounds. The threshold here is
-    // not defined by some rule but more of an expectation.
-    let (min, max) = rounds.values().into_iter().minmax().into_option().unwrap();
-    assert!(max - min <= 2.0, "Nodes shouldn't be that behind");
+    cluster.assert_progress(4, 2).await;
 }
 
-async fn authorities_latest_commit_round(cluster: &Cluster) -> HashMap<usize, f64> {
-    let mut authorities_latest_commit = HashMap::new();
+#[ignore]
+#[tokio::test]
+/// We are testing the loss of liveness of a healthy cluster. While 3f+1 nodes run
+/// we are shutting down f+1 nodes. Then we are bringing the f+1 nodes back again
+/// but we are not expecting now the cluster to be able to make progress. We expect
+/// the restarted nodes to not be able to make new proposals and effectively make
+/// the system stall. This is due to the issue that is described here
+/// https://github.com/MystenLabs/narwhal/issues/664 .
+/// Once this is fixed we would expect this test to fail as all the nodes should be
+/// able to propose from where they left of at last round.
+async fn test_loss_of_liveness_without_recovery() {
+    // Enabled debug tracing so we can easily observe the
+    // nodes logs.
+    let _guard = setup_tracing();
 
-    for authority in cluster.authorities().await {
-        let primary = authority.primary().await;
-        if let Some(metric) = primary.metric("narwhal_primary_last_committed_round") {
-            let value = metric.get_gauge().get_value();
+    let node_advance_delay = Duration::from_secs(60);
 
-            authorities_latest_commit.insert(primary.id, value);
+    // A cluster of 4 nodes will be created
+    let mut cluster = Cluster::new(None, None, true);
 
-            info!(
-                "[Node {}] Metric narwhal_primary_last_committed_round -> {value}",
-                primary.id
-            );
-        }
-    }
+    // ===== Start the cluster ====
+    cluster.start(Some(4), Some(1), None).await;
 
-    authorities_latest_commit
+    // Let the nodes advance a bit
+    tokio::time::sleep(node_advance_delay).await;
+
+    // Ensure that nodes have made progress
+    cluster.assert_progress(4, 2).await;
+
+    // Now stop node 2 & 3
+    cluster.authority(2).stop_all().await;
+    cluster.authority(3).stop_all().await;
+
+    // wait and fetch the latest commit round
+    tokio::time::sleep(node_advance_delay).await;
+    let rounds_1 = cluster.assert_progress(2, 0).await;
+
+    // wait and fetch again the rounds
+    tokio::time::sleep(node_advance_delay).await;
+    let rounds_2 = cluster.assert_progress(2, 0).await;
+
+    // We assert that nodes haven't advanced at all
+    assert_eq!(rounds_1, rounds_2);
+
+    // Now bring up nodes
+    cluster.authority(2).start(true, Some(1)).await;
+    cluster.authority(3).start(true, Some(1)).await;
+
+    // wait and fetch the latest commit round
+    tokio::time::sleep(node_advance_delay).await;
+    let rounds_3 = cluster.assert_progress(2, 0).await;
+
+    assert_eq!(rounds_2, rounds_3);
+}
+
+#[ignore]
+#[tokio::test]
+/// We are testing the loss of liveness of a healthy cluster. While 3f+1 nodes run
+/// we are shutting down f+1 nodes one by one with some delay between them.
+/// Then we are bringing the f+1 nodes back again. We expect the cluster to
+/// recover and effectively make progress.
+async fn test_loss_of_liveness_with_recovery() {
+    // Enabled debug tracing so we can easily observe the
+    // nodes logs.
+    let _guard = setup_tracing();
+
+    let node_advance_delay = Duration::from_secs(60);
+
+    // A cluster of 4 nodes will be created
+    let mut cluster = Cluster::new(None, None, true);
+
+    // ===== Start the cluster ====
+    cluster.start(Some(4), Some(1), None).await;
+
+    // Let the nodes advance a bit
+    tokio::time::sleep(node_advance_delay).await;
+
+    // Ensure that nodes have made progress
+    cluster.assert_progress(4, 2).await;
+
+    // Now stop node 2
+    cluster.authority(2).stop_all().await;
+
+    // allow other nodes to advance
+    tokio::time::sleep(node_advance_delay).await;
+
+    // Now stop node 3
+    cluster.authority(3).stop_all().await;
+
+    // wait and fetch the latest commit round
+    tokio::time::sleep(node_advance_delay).await;
+    let rounds_1 = cluster.assert_progress(2, 0).await;
+
+    // wait and fetch again the rounds
+    tokio::time::sleep(node_advance_delay).await;
+    let rounds_2 = cluster.assert_progress(2, 0).await;
+
+    // We assert that nodes haven't advanced at all
+    assert_eq!(rounds_1, rounds_2);
+
+    // Now bring up nodes
+    cluster.authority(2).start(true, Some(1)).await;
+    cluster.authority(3).start(true, Some(1)).await;
+
+    // wait and fetch the latest commit round
+    tokio::time::sleep(node_advance_delay).await;
+    let rounds_3 = cluster.assert_progress(4, 0).await;
+
+    let round_2_max = rounds_2.values().into_iter().max().unwrap();
+    assert!(
+        rounds_3.values().all(|v| v > round_2_max),
+        "All the nodes should have advanced more from the previous round"
+    );
 }
 
 fn setup_tracing() -> TelemetryGuards {
