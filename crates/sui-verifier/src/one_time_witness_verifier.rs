a523c40a5c47abeeeeab2c1cede9a7155c9941cd
Adam Welc <adam@mystenlabs.com>
2022-08-10 17:08:12 -0700

    [Move] Implemented one-type witness checking (#3771)
    * [Move] Implemented one-type witness checking
    
    * Added missing files
    
    * Adjusted number of object created in init functions
    
    * Renamed char(acteristic) type to one-time witness
    
    * Changed location of one-time witness checking function
    
    * All coins now use one-time witness
    
    * Added a new line
    
    * Updated test output
    
    * Adjusted tests
    
    * Updated snapshot test file
    
    * Removed check to make the test more robust
    
    * Updated snapshot test file
    
    * one-timeness check moved to Coin (#3894)
    
    * Removed a function that could compromise one-time witness safety and adjusted tests
    
    Co-authored-by: Damir Shamanaev <damirka.ru@gmail.com>

--- crates/sui-verifier/src/one_time_witness_verifier.rs
+++ crates/sui-verifier/src/one_time_witness_verifier.rs
@@ -1,9 +1,9 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-//! A module can define a "characteristic type", that is a type that is instantiated only once, and
-//! this property is enforced by the system. We define a characteristic type as a struct type that
-//! has the same name as the module that defines it but with all the letter capitalized, and
+//! A module can define a one-time witness type, that is a type that is instantiated only once, and
+//! this property is enforced by the system. We define a one-time witness type as a struct type that
+//! has the same name as the module that defines it but with all the letters capitalized, and
 //! possessing certain special properties specified below (please note that by convention, "regular"
 //! struct type names are expressed in camel case).  In other words, if a module defines a struct
 //! type whose name is the same as the module name, this type MUST possess these special properties,
@@ -15,11 +15,6 @@
 //! - its definition does not involve type parameters
 //! - its only instance in existence is passed as an argument to the module initializer
 //! - it is never instantiated anywhere in its defining module
-//!
-//! A characteristic type is one way of implementing a one-time witness pattern, where we want to
-//! restrict the number of times a given type (instance) is used. Another way could be to have a set
-//! data structure that can store types and use it to guarantee uniqueness.
-
 use move_binary_format::{
     access::ModuleAccess,
     binary_views::BinaryIndexedView,
@@ -38,10 +33,11 @@ use sui_types::{
 use crate::{verification_failure, INIT_FN_NAME};
 
 pub fn verify_module(module: &CompiledModule) -> Result<(), ExecutionError> {
-    // In Sui's framework code there is an exception to the characteristic type rule - we have a SUI
-    // type in the sui module but it is instantiated and the module has no initializer (the reason
-    // for it is that the SUI coin is only instantiated during genesis). It is easiest to simply
-    // special-case this module particularly that this is framework code and thus deemed correct.
+    // In Sui's framework code there is an exception to the one-time witness type rule - we have a
+    // SUI type in the sui module but it is instantiated outside of the module initializer (in fact,
+    // the module has no initializer). The reason for it is that the SUI coin is only instantiated
+    // during genesis. It is easiest to simply special-case this module particularly that this is
+    // framework code and thus deemed correct.
     if ModuleId::new(SUI_FRAMEWORK_ADDRESS, ident_str!("sui").to_owned()) == module.self_id() {
         return Ok(());
     }
@@ -50,18 +46,18 @@ pub fn verify_module(module: &CompiledModule) -> Result<(), ExecutionError> {
     let mod_handle = view.module_handle_at(module.self_module_handle_idx);
     let mod_name = view.identifier_at(mod_handle.name).as_str();
     let struct_defs = &module.struct_defs;
-    let mut char_type_candidate = None;
-    // find structs that can potentially represent a characteristic type
+    let mut one_time_witness_candidate = None;
+    // find structs that can potentially represent a one-time witness type
     for def in struct_defs {
         let struct_handle = module.struct_handle_at(def.struct_handle);
         let struct_name = view.identifier_at(struct_handle.name).as_str();
         if mod_name.to_ascii_uppercase() == struct_name {
-            verify_char_type(module, struct_name, struct_handle, def)
+            verify_one_time_witness(module, struct_name, struct_handle, def)
                 .map_err(verification_failure)?;
-            // if we reached this point, it means we have a legitimate characteristic type candidate
-            // and we have to make sure that both the init function's signature reflects this and
-            // that this type is not instantiated in any function of the module
-            char_type_candidate = Some((struct_name, struct_handle, def));
+            // if we reached this point, it means we have a legitimate one-time witness type
+            // candidate and we have to make sure that both the init function's signature reflects
+            // this and that this type is not instantiated in any function of the module
+            one_time_witness_candidate = Some((struct_name, struct_handle, def));
             break; // no reason to look any further
         }
     }
@@ -69,21 +65,20 @@ pub fn verify_module(module: &CompiledModule) -> Result<(), ExecutionError> {
         let fn_handle = module.function_handle_at(fn_def.function);
         let fn_name = module.identifier_at(fn_handle.name);
         if fn_name == INIT_FN_NAME {
-            if let Some((candidate_name, candidate_handle, _)) = char_type_candidate {
-                // only verify if init function conforms to characteristic types requirements if we
-                // have a characteristic type candidate
-                verify_init_function_char_type(module, fn_handle, candidate_name, candidate_handle)
+            if let Some((candidate_name, candidate_handle, _)) = one_time_witness_candidate {
+                // only verify if init function conforms to one-time witness type requirements if we
+                // have a one-time witness type candidate
+                verify_init_one_time_witness(module, fn_handle, candidate_name, candidate_handle)
                     .map_err(verification_failure)?;
             } else {
-                // if there is no characteristic type candidate than the init function should have
+                // if there is no one-time witness type candidate than the init function should have
                 // only one parameter of TxContext type
-                verify_init_function_single_param(module, fn_handle)
-                    .map_err(verification_failure)?;
+                verify_init_single_param(module, fn_handle).map_err(verification_failure)?;
             }
         }
-        if let Some((candidate_name, _, def)) = char_type_candidate {
-            // only verify lack of characteristic types instantiations if we have a
-            // characteristic type candidate
+        if let Some((candidate_name, _, def)) = one_time_witness_candidate {
+            // only verify lack of one-time witness type instantiations if we have a one-time
+            // witness type candidate
             verify_no_instantiations(module, fn_def, candidate_name, def)
                 .map_err(verification_failure)?;
         }
@@ -92,9 +87,9 @@ pub fn verify_module(module: &CompiledModule) -> Result<(), ExecutionError> {
     Ok(())
 }
 
-// Verifies all required properties of a characteristic type candidate (that is a type whose name is
-// the same as the name of a
-fn verify_char_type(
+// Verifies all required properties of a one-time witness type candidate (that is a type whose name
+// is the same as the name of a
+fn verify_one_time_witness(
     module: &CompiledModule,
     candidate_name: &str,
     candidate_handle: &StructHandle,
@@ -105,14 +100,14 @@ fn verify_char_type(
     let abilities = candidate_handle.abilities;
     if abilities != drop_set {
         return Err(format!(
-            "characteristic type candidate {}::{} must have a single ability: drop",
+            "one-time witness type candidate {}::{} must have a single ability: drop",
             module.self_id(),
             candidate_name,
         ));
     }
     let field_count = candidate_def.declared_field_count().map_err(|_| {
         format!(
-            "characteristic type candidate {}::{} cannot be a native structure",
+            "one-time witness type candidate {}::{} cannot be a native structure",
             module.self_id(),
             candidate_name
         )
@@ -122,7 +117,7 @@ fn verify_char_type(
     // successful
     if field_count != 1 || candidate_def.field(0).unwrap().signature.0 != SignatureToken::Bool {
         return Err(format!(
-            "characteristic type candidate {}::{} must have a single bool field only (or no fields)",
+            "one-time witness type candidate {}::{} must have a single bool field only (or no fields)",
             module.self_id(),
             candidate_name,
         ));
@@ -130,7 +125,7 @@ fn verify_char_type(
 
     if !candidate_handle.type_parameters.is_empty() {
         return Err(format!(
-            "characteristic type candidate {}::{} cannot have type parameters",
+            "one-time witness type candidate {}::{} cannot have type parameters",
             module.self_id(),
             candidate_name,
         ));
@@ -138,8 +133,8 @@ fn verify_char_type(
     Ok(())
 }
 
-/// Checks if this module's `init` function conformant with the characteristic type
-fn verify_init_function_char_type(
+/// Checks if this module's `init` function conformant with the one-time witness type
+fn verify_init_one_time_witness(
     module: &CompiledModule,
     fn_handle: &FunctionHandle,
     candidate_name: &str,
@@ -147,11 +142,11 @@ fn verify_init_function_char_type(
 ) -> Result<(), String> {
     let view = &BinaryIndexedView::Module(module);
     let fn_sig = view.signature_at(fn_handle.parameters);
-    if fn_sig.len() != 2 || !is_char_type(view, &fn_sig.0[0], candidate_handle) {
+    if fn_sig.len() != 2 || !is_one_time_witness(view, &fn_sig.0[0], candidate_handle) {
         // check only the first parameter - the other one is checked in entry_points verification
         // pass
         return Err(format!(
-            "init function of a module containing characteristic type candidate must have {}::{} as the first parameter",
+            "init function of a module containing one-time witness type candidate must have {}::{} as the first parameter",
             module.self_id(),
             candidate_name,
         ));
@@ -160,8 +155,8 @@ fn verify_init_function_char_type(
     Ok(())
 }
 
-// Checks if a given SignatureToken represents a characteristic type struct
-fn is_char_type(
+// Checks if a given SignatureToken represents a one-time witness type struct
+fn is_one_time_witness(
     view: &BinaryIndexedView,
     tok: &SignatureToken,
     candidate_handle: &StructHandle,
@@ -170,7 +165,7 @@ fn is_char_type(
 }
 
 /// Checks if this module's `init` function has a single parameter of TxContext type only
-fn verify_init_function_single_param(
+fn verify_init_single_param(
     module: &CompiledModule,
     fn_handle: &FunctionHandle,
 ) -> Result<(), String> {
@@ -190,7 +185,7 @@ fn verify_init_function_single_param(
     Ok(())
 }
 
-/// Checks if this module function does not contain instantiation of the characteristic type
+/// Checks if this module function does not contain instantiation of the one-time witness type
 fn verify_no_instantiations(
     module: &CompiledModule,
     fn_def: &FunctionDefinition,
@@ -212,7 +207,7 @@ fn verify_no_instantiations(
             let fn_handle = module.function_handle_at(fn_def.function);
             let fn_name = module.identifier_at(fn_handle.name);
             return Err(format!(
-                "characteristic type {}::{} is instantiated \
+                "one-time witness type {}::{} is instantiated \
                          in the {}::{} function and must never be",
                 module.self_id(),
                 struct_name,
