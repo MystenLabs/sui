523494e5956abde6b77fdbda4bac660c8f0d0f45
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-08-12 15:57:08 +0300

    wallet-ext: disable stake coins
    * allow users to navigate to stake home view but disable staking new coins
    * see https://github.com/MystenLabs/sui/pull/3616/commits/e929c1a420876216c9f14699693b521f48e3827b and https://github.com/MystenLabs/sui/pull/3616 for more details on why this was disabled

--- wallet/src/ui/app/staking/index.tsx
+++ wallet/src/ui/app/staking/index.tsx
@@ -6,9 +6,13 @@ import { Route } from 'react-router-dom';
 import StakeHome from './home';
 import StakeNew from './stake';
 
+export const STAKING_ENABLED = process.env.STAKING_ENABLED === 'true';
+
 export const routes = (
     <>
         <Route path="stake" element={<StakeHome />} />
-        <Route path="stake/new" element={<StakeNew />} />
+        {STAKING_ENABLED ? (
+            <Route path="stake/new" element={<StakeNew />} />
+        ) : null}
     </>
 );
