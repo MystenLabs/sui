f67ce2272684ea12f910c995e6b047293d7293ea
Andrew Burnie <apburnie@hotmail.co.uk>
2022-08-15 17:28:19 +0100

    splits out SingleCoinView

--- explorer/client/src/components/ownedobjects/views/OwnedCoinView.tsx
+++ explorer/client/src/components/ownedobjects/views/OwnedCoinView.tsx
@@ -12,28 +12,81 @@ import { type DataType, ITEMS_PER_PAGE } from '../OwnedObjectConstants';
 
 import styles from '../styles/OwnedCoin.module.css';
 
-export default function OwnedCoinView({ results }: { results: DataType }) {
-    const ALL_CLOSED = null;
+function SingleCoinView({
+    results,
+    coinLabel,
+    currentPage,
+}: {
+    results: DataType;
+    coinLabel: string;
+    currentPage: number;
+}) {
+    const [isOpen, setIsOpen] = useState(false);
 
-    const [openedType, setOpenedType] = useState<string | null>(ALL_CLOSED);
-
-    const [currentPage, setCurrentPage] = useState(1);
-
-    const openThisType = useCallback(
-        (thisType: string) => () => {
-            setOpenedType(thisType);
-        },
+    const switchIsOpen = useCallback(
+        () => setIsOpen((prevOpen) => !prevOpen),
         []
     );
 
-    const goBack = useCallback(() => setOpenedType(ALL_CLOSED), []);
-
-    const uniqueTypes = Array.from(new Set(results.map(({ Type }) => Type)));
-
     // Switching the page closes any open group:
     useEffect(() => {
-        setOpenedType(ALL_CLOSED);
+        setIsOpen(false);
     }, [currentPage]);
+
+    const subObjList = results.filter(({ Type }) => Type === coinLabel);
+
+    return (
+        <div className={isOpen ? styles.openedgroup : styles.closedgroup}>
+            <div onClick={switchIsOpen} className={styles.summary}>
+                <div className={isOpen ? styles.openicon : styles.closedicon}>
+                    <ContentIcon />
+                </div>
+                <div>{handleCoinType(coinLabel)}</div>
+                <div>{subObjList.length}</div>
+                <div>
+                    {subObjList[0]._isCoin &&
+                    subObjList.every((el) => el.balance !== undefined)
+                        ? `${subObjList.reduce(
+                              (prev, current) => prev.add(current.balance!),
+                              Coin.getZero()
+                          )}`
+                        : ''}
+                </div>
+                <div />
+            </div>
+            <div className={styles.openbody}>
+                {isOpen &&
+                    subObjList.map((subObj, index) => (
+                        <React.Fragment key={index}>
+                            <div className={styles.objectid}>
+                                <div />
+                                <div>Object ID</div>
+                                <div>
+                                    <Longtext
+                                        text={subObj.id}
+                                        category="objects"
+                                        isCopyButton={false}
+                                    />
+                                </div>
+                                <div />
+                            </div>
+                            <div className={styles.balance}>
+                                <div />
+                                <div>Balance</div>
+                                <div>{subObj.balance?.toString()}</div>
+                                <div />
+                            </div>
+                        </React.Fragment>
+                    ))}
+            </div>
+        </div>
+    );
+}
+
+export default function OwnedCoinView({ results }: { results: DataType }) {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const uniqueTypes = Array.from(new Set(results.map(({ Type }) => Type)));
 
     return (
         <>
@@ -49,94 +102,14 @@ export default function OwnedCoinView({ results }: { results: DataType }) {
                             (currentPage - 1) * ITEMS_PER_PAGE,
                             currentPage * ITEMS_PER_PAGE
                         )
-                        .map((typeV) => {
-                            const subObjList = results.filter(
-                                ({ Type }) => Type === typeV
-                            );
-                            return (
-                                <div
-                                    key={typeV}
-                                    className={
-                                        openedType === typeV
-                                            ? styles.openedgroup
-                                            : styles.closedgroup
-                                    }
-                                >
-                                    <div
-                                        onClick={
-                                            openedType === typeV
-                                                ? goBack
-                                                : openThisType(typeV)
-                                        }
-                                        className={styles.summary}
-                                    >
-                                        <div
-                                            className={
-                                                openedType === typeV
-                                                    ? styles.openicon
-                                                    : styles.closedicon
-                                            }
-                                        >
-                                            <ContentIcon />
-                                        </div>
-                                        <div>{handleCoinType(typeV)}</div>
-                                        <div>{subObjList.length}</div>
-                                        <div>
-                                            {subObjList[0]._isCoin &&
-                                            subObjList.every(
-                                                (el) => el.balance !== undefined
-                                            )
-                                                ? `${subObjList.reduce(
-                                                      (prev, current) =>
-                                                          prev.add(
-                                                              current.balance!
-                                                          ),
-                                                      Coin.getZero()
-                                                  )}`
-                                                : ''}
-                                        </div>
-                                        <div />
-                                    </div>
-                                    <div className={styles.openbody}>
-                                        {openedType === typeV &&
-                                            subObjList.map((subObj, index) => (
-                                                <React.Fragment key={index}>
-                                                    <div
-                                                        className={
-                                                            styles.objectid
-                                                        }
-                                                    >
-                                                        <div />
-                                                        <div>Object ID</div>
-                                                        <div>
-                                                            <Longtext
-                                                                text={subObj.id}
-                                                                category="objects"
-                                                                isCopyButton={
-                                                                    false
-                                                                }
-                                                            />
-                                                        </div>
-                                                        <div />
-                                                    </div>
-                                                    <div
-                                                        className={
-                                                            styles.balance
-                                                        }
-                                                    >
-                                                        <div />
-                                                        <div>Balance</div>
-                                                        <div>
-                                                            {subObj.balance?.toString()}
-                                                        </div>
-                                                        <div />
-                                                    </div>
-                                                </React.Fragment>
-                                            ))}
-                                    </div>
-                                </div>
-                            );
-                        })}
+                        .map((typeV) => (
+                            <SingleCoinView
+                                key={typeV}
+                                results={results}
+                                coinLabel={typeV}
+                                currentPage={currentPage}
+                            />
+                        ))}
                 </div>
             </div>
             <Pagination
