d3e912fca3eaeeb145fe667334fc2fbb4cbbf65e
Jordan Gensler <jordan@mystenlabs.com>
2022-08-18 14:14:59 -0700

    Migrate bcs off of tsdx

--- sdk/typescript/bcs/tests/serde.test.ts
+++ sdk/typescript/bcs/tests/serde.test.ts
@@ -1,10 +1,11 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { describe, it, expect } from 'vitest';
 import { bcs } from './../src/index';
 
 describe('serde', () => {
-    test('it should serialize primitives in both directions', () => {
+    it('should serialize primitives in both directions', () => {
         let u8 = 100;
         let u64 = '18446744073709551615';
         let u128 = '1844674407370955161518446744073709551';
@@ -19,7 +20,7 @@ describe('serde', () => {
         assert(bool === bcs.de(bcs.BOOL, bcs.ser(bcs.BOOL, bool).toBytes()))
     });
 
-    test('it should serde structs', () => {
+    it('should serde structs', () => {
         bcs.registerAddressType('address', 20, 'hex');
         bcs.registerStructType('Beep', { id: 'address', value: 'u64' });
 
@@ -30,7 +31,7 @@ describe('serde', () => {
         assert(struct.value.toString(10) === '10000000');
     });
 
-    test('it should serde enums', () => {
+    it('should serde enums', () => {
         bcs.registerAddressType('address', 20, 'hex');
         bcs.registerEnumType('Enum', {
             with_value: 'address',
