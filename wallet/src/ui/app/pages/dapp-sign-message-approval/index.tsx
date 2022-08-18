a037cf8bcf438cf8f9503bdf39ecf26b01c69f0c
Josh Hundley <josh.hundley@gmail.com>
2022-08-18 11:57:27 -0600

    [wallet] slight cleanup on sign message page

--- wallet/src/ui/app/pages/dapp-sign-message-approval/index.tsx
+++ wallet/src/ui/app/pages/dapp-sign-message-approval/index.tsx
@@ -47,8 +47,6 @@ export function DappSignMessageApprovalPage() {
                     })
                 );
             }
-
-            return true;
         },
         [dispatch, signMessageRequest]
     );
