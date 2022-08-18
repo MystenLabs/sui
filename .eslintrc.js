763cfd834e389003c70a342a570cef3afdc82093
Jordan Gensler <jordan@mystenlabs.com>
2022-08-18 16:37:37 -0700

    Add license header to lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 module.exports = {
   root: true,
   extends: ["react-app", "prettier", "plugin:prettier/recommended"],
