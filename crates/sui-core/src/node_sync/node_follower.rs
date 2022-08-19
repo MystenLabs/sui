9b97dc55b52fc55731afef611be3f3b3cc906f37
Mark Logan <mark@mystenlabs.com>
2022-08-19 15:33:30 -0700

    Add license

--- crates/sui-core/src/node_sync/node_follower.rs
+++ crates/sui-core/src/node_sync/node_follower.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::{
     authority_active::gossip::GossipMetrics, authority_aggregator::AuthorityAggregator,
     authority_client::AuthorityAPI, safe_client::SafeClient,
