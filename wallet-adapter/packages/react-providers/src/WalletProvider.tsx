b6e9c9eaf9b2ab3909387cef1b61d2c8b9eb16e0
Jordan Gensler <jordan@mystenlabs.com>
2022-08-22 17:22:54 -0700

    Resolve rebase issues

--- wallet-adapter/packages/react-providers/src/WalletProvider.tsx
+++ wallet-adapter/packages/react-providers/src/WalletProvider.tsx
@@ -7,23 +7,24 @@ import { WalletCapabilities } from "@mysten/wallet-adapter-base";
 import { Wallet, WalletContext } from './useWallet';
 
 export interface WalletAdapter {
-  adapter: WalletCapabilities;
+    adapter: WalletCapabilities;
 }
 
 export interface WalletProviderProps {
-  children: ReactNode;
-  // Pass this through props to add list of supported wallets
-  supportedWallets: Wallet[];
+    children: ReactNode;
+    // Pass this through props to add list of supported wallets
+    supportedWallets: Wallet[]
 }
 
 export const WalletProvider: FC<WalletProviderProps> = ({
-  children,
-  supportedWallets,
+    children,
+    supportedWallets
 }) => {
-  // Wallet that user chose
-  const [wallet, setWallet] = useState<WalletAdapter | null>(null);
-  const [connected, setConnected] = useState(false);
-  const [connecting, setConnecting] = useState(false);
+    // Wallet that user chose
+    const [wallet, setWallet] = useState<WalletAdapter | null>(null);
+    const [connected, setConnected] = useState(false)
+    const [connecting, setConnecting] = useState(false)
+
 
     const connect = useCallback(
         async () => {
@@ -43,60 +44,44 @@ export const WalletProvider: FC<WalletProviderProps> = ({
     const disconnect = async () => {
         setConnected(false);
         setWalletAndUpdateStorage(null);
-
-    try {
-      setConnecting(true);
-      await wallet.adapter.connect();
-      setConnected(true);
-    } catch (e) {
-      setConnected(false);
     }
-    setConnecting(false);
-  }, [wallet]);
 
-  const disconnect = async () => {
-    setConnected(false);
-    setWalletAndUpdateStorage(null);
-  };
+    // Use this to update wallet so that the chosen wallet persists after reload.
+    const setWalletAndUpdateStorage = useCallback((selectedWallet: Wallet | null) => {
+        setWallet(selectedWallet);
+        if (selectedWallet != null) {
+            localStorage.setItem('suiWallet', selectedWallet.adapter.name);
+        } else {
+            localStorage.removeItem('suiWallet');
+        }
+    }, []);
 
-  // Use this to update wallet so that the chosen wallet persists after reload.
-  const setWalletAndUpdateStorage = useCallback(
-    (selectedWallet: Wallet | null) => {
-      setWallet(selectedWallet);
-      if (selectedWallet != null) {
-        localStorage.setItem("suiWallet", selectedWallet.adapter.name);
-      } else {
-        localStorage.removeItem("suiWallet");
-      }
-    },
-    []
-  );
+    // Changes the selected wallet
+    const choose = useCallback((name: string) => {
+        let newWallet = supportedWallets.find(wallet => wallet.adapter.name === name);
+        if (newWallet) {
+            setWalletAndUpdateStorage(newWallet);
+        }
+        connect();
+    }, [supportedWallets, connect, setWalletAndUpdateStorage]);
 
-  // Changes the selected wallet
-  const choose = useCallback(
-    (name: string) => {
-      let newWallet = supportedWallets.find(
-        (wallet) => wallet.adapter.name === name
-      );
-      if (newWallet) {
-        setWalletAndUpdateStorage(newWallet);
-      }
-      connect();
-    },
-    [supportedWallets, connect, setWalletAndUpdateStorage]
-  );
+    // If the wallet is null, check if there isn't anything in local storage
+    // Note: Optimize this.
+    useEffect(() => {
+        if (!wallet && !connected && !connecting) {
+            let walletItem = localStorage.getItem('suiWallet');
+            if (typeof walletItem === 'string') {
+                const items = walletItem;
+                choose(items);
+            }
+        }
+    }, [choose, connected, connecting, wallet]);
 
-  // If the wallet is null, check if there isn't anything in local storage
-  // Note: Optimize this.
-  useEffect(() => {
-    if (!wallet && !connected && !connecting) {
-      let walletItem = localStorage.getItem("suiWallet");
-      if (typeof walletItem === "string") {
-        const items = walletItem;
-        choose(items);
-      }
+    // Returns all accounts (i.e. public keys) managed by the selected wallet
+    const getAccounts = async (): Promise<SuiAddress[]> => {
+        if (wallet == null) throw Error('Wallet Not Connected');
+        return await wallet.adapter.getAccounts();
     }
-  }, [choose, connected, connecting, wallet]);
 
     // Requests wallet for signature and executes if signed
     const executeMoveCall = async (transaction: MoveCallTransaction): Promise<SuiTransactionResponse> => {
@@ -138,5 +123,4 @@ export const WalletProvider: FC<WalletProviderProps> = ({
             {children}
         </WalletContext.Provider>
     );
-
 };
