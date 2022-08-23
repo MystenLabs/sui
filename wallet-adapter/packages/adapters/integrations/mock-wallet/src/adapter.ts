c78a703b123c953870d84eeb9e94f0c0ac777df0
Jordan Gensler <jordan@mystenlabs.com>
2022-08-22 21:47:38 -0700

    [wallet-adapter] Migrate wallet adapter demo to Vite (#4179)
    * Migrate wallet adapter demo to Vite
    
    * Resolve rebase issues

--- wallet-adapter/packages/adapters/integrations/mock-wallet/src/adapter.ts
+++ wallet-adapter/packages/adapters/integrations/mock-wallet/src/adapter.ts
@@ -1,76 +1,85 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { MoveCallTransaction, SuiAddress, SuiTransactionResponse } from "@mysten/sui.js";
+import {
+  MoveCallTransaction,
+  SuiAddress,
+  SuiTransactionResponse,
+} from "@mysten/sui.js";
 import { WalletCapabilities } from "@mysten/wallet-adapter-base";
 
-const ALL_PERMISSION_TYPES = [
-    'viewAccount',
-    'suggestTransactions',
-];
+const ALL_PERMISSION_TYPES = ["viewAccount", "suggestTransactions"];
 type AllPermissionsType = typeof ALL_PERMISSION_TYPES;
 type PermissionType = AllPermissionsType[number];
 
 interface SuiWallet {
-    hasPermissions(permissions: readonly PermissionType[]): Promise<boolean>;
-    requestPermissions(): Promise<boolean>;
-    getAccounts(): Promise<SuiAddress[]>;
-    executeMoveCall: (transaction: MoveCallTransaction) => Promise<SuiTransactionResponse>;
-    executeSerializedMoveCall: (transactionBytes: Uint8Array) => Promise<SuiTransactionResponse>;
+  hasPermissions(permissions: readonly PermissionType[]): Promise<boolean>;
+  requestPermissions(): Promise<boolean>;
+  getAccounts(): Promise<SuiAddress[]>;
+  executeMoveCall: (
+    transaction: MoveCallTransaction
+  ) => Promise<SuiTransactionResponse>;
+  executeSerializedMoveCall: (
+    transactionBytes: Uint8Array
+  ) => Promise<SuiTransactionResponse>;
 }
 interface SuiWalletWindow {
-    suiWallet: SuiWallet
+  suiWallet: SuiWallet;
 }
 
 declare const window: SuiWalletWindow;
 
 // Stored as state somewhere (Probably in a place with generics )
-export class MockWalletAdapter implements WalletCapabilities{
-    connecting: boolean;
-    connected: boolean;
+export class MockWalletAdapter implements WalletCapabilities {
+  connecting: boolean;
+  connected: boolean;
 
-    getAccounts(): Promise<string[]> {
-        return window.suiWallet.getAccounts();
-    }
-    executeMoveCall(transaction: MoveCallTransaction): Promise<SuiTransactionResponse> {
-        return window.suiWallet.executeMoveCall(transaction);
-    }
-    executeSerializedMoveCall(transactionBytes: Uint8Array): Promise<SuiTransactionResponse> {
-        return window.suiWallet.executeSerializedMoveCall(transactionBytes);
-    }
+  getAccounts(): Promise<string[]> {
+    return window.suiWallet.getAccounts();
+  }
+  executeMoveCall(
+    transaction: MoveCallTransaction
+  ): Promise<SuiTransactionResponse> {
+    return window.suiWallet.executeMoveCall(transaction);
+  }
+  executeSerializedMoveCall(
+    transactionBytes: Uint8Array
+  ): Promise<SuiTransactionResponse> {
+    return window.suiWallet.executeSerializedMoveCall(transactionBytes);
+  }
 
-    name: string;
+  name: string;
 
-    async connect(): Promise<void> {
-        this.connecting = true;
-        if (window.suiWallet) {
-            const wallet = window.suiWallet;
-            try {
-                let given = await wallet.requestPermissions();
-                const newLocal: readonly PermissionType[] = ['viewAccount']
-                let perms = await wallet.hasPermissions(newLocal);
-                console.log(perms);
-                console.log(given);
-                this.connected = true;
-            } catch (err) {
-                console.error(err);
-            } finally {
-                this.connecting = false;
-            }
-        }
-    }
-
-    // Come back to this later
-    async disconnect(): Promise<void> {
-        if (this.connected == true) {
-            this.connected = false;
-        }
-        console.log("disconnected");
-    }
-
-    constructor(name: string) {
-        this.connected = false;
+  async connect(): Promise<void> {
+    this.connecting = true;
+    if (window.suiWallet) {
+      const wallet = window.suiWallet;
+      try {
+        let given = await wallet.requestPermissions();
+        const newLocal: readonly PermissionType[] = ["viewAccount"];
+        let perms = await wallet.hasPermissions(newLocal);
+        console.log(perms);
+        console.log(given);
+        this.connected = true;
+      } catch (err) {
+        console.error(err);
+      } finally {
         this.connecting = false;
-        this.name = name;
+      }
     }
+  }
+
+  // Come back to this later
+  async disconnect(): Promise<void> {
+    if (this.connected == true) {
+      this.connected = false;
+    }
+    console.log("disconnected");
+  }
+
+  constructor(name: string) {
+    this.connected = false;
+    this.connecting = false;
+    this.name = name;
+  }
 }
--- wallet-adapter/packages/adapters/integrations/sui-wallet/src/adapter.ts
+++ wallet-adapter/packages/adapters/integrations/sui-wallet/src/adapter.ts
@@ -1,75 +1,84 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { MoveCallTransaction, SuiAddress, SuiTransactionResponse } from "@mysten/sui.js";
+import {
+  MoveCallTransaction,
+  SuiAddress,
+  SuiTransactionResponse,
+} from "@mysten/sui.js";
 import { WalletCapabilities } from "@mysten/wallet-adapter-base";
 
-const ALL_PERMISSION_TYPES = [
-    'viewAccount',
-    'suggestTransactions',
-] as const;
+const ALL_PERMISSION_TYPES = ["viewAccount", "suggestTransactions"] as const;
 type AllPermissionsType = typeof ALL_PERMISSION_TYPES;
 type PermissionType = AllPermissionsType[number];
 
 interface SuiWallet {
-    hasPermissions(permissions: readonly PermissionType[]): Promise<boolean>;
-    requestPermissions(): Promise<boolean>;
-    getAccounts(): Promise<SuiAddress[]>;
-    executeMoveCall: (transaction: MoveCallTransaction) => Promise<SuiTransactionResponse>;
-    executeSerializedMoveCall: (transactionBytes: Uint8Array) => Promise<SuiTransactionResponse>;
+  hasPermissions(permissions: readonly PermissionType[]): Promise<boolean>;
+  requestPermissions(): Promise<boolean>;
+  getAccounts(): Promise<SuiAddress[]>;
+  executeMoveCall: (
+    transaction: MoveCallTransaction
+  ) => Promise<SuiTransactionResponse>;
+  executeSerializedMoveCall: (
+    transactionBytes: Uint8Array
+  ) => Promise<SuiTransactionResponse>;
 }
 interface SuiWalletWindow {
-    suiWallet: SuiWallet
+  suiWallet: SuiWallet;
 }
 
 declare const window: SuiWalletWindow;
 
 // Stored as state somewhere (Probably in a place with generics )
-export class SuiWalletAdapter implements WalletCapabilities{
-    connecting: boolean;
-    connected: boolean;
+export class SuiWalletAdapter implements WalletCapabilities {
+  connecting: boolean;
+  connected: boolean;
 
-    getAccounts(): Promise<string[]> {
-        return window.suiWallet.getAccounts();
-    }
-    executeMoveCall(transaction: MoveCallTransaction): Promise<SuiTransactionResponse> {
-        return window.suiWallet.executeMoveCall(transaction);
-    }
-    executeSerializedMoveCall(transactionBytes: Uint8Array): Promise<SuiTransactionResponse> {
-        return window.suiWallet.executeSerializedMoveCall(transactionBytes);
-    }
+  getAccounts(): Promise<string[]> {
+    return window.suiWallet.getAccounts();
+  }
+  executeMoveCall(
+    transaction: MoveCallTransaction
+  ): Promise<SuiTransactionResponse> {
+    return window.suiWallet.executeMoveCall(transaction);
+  }
+  executeSerializedMoveCall(
+    transactionBytes: Uint8Array
+  ): Promise<SuiTransactionResponse> {
+    return window.suiWallet.executeSerializedMoveCall(transactionBytes);
+  }
 
-    name = "Sui Wallet";
+  name = "Sui Wallet";
 
-    async connect(): Promise<void> {
-        this.connecting = true;
-        if (window.suiWallet) {
-            const wallet = window.suiWallet;
-            try {
-                let given = await wallet.requestPermissions();
-                const newLocal: readonly PermissionType[] = ['viewAccount']
-                let perms = await wallet.hasPermissions(newLocal);
-                console.log(perms);
-                console.log(given);
-                this.connected = true;
-            } catch (err) {
-                console.error(err);
-            } finally {
-                this.connecting = false;
-            }
-        }
-    }
-
-    // Come back to this later
-    async disconnect(): Promise<void> {
-        if (this.connected == true) {
-            this.connected = false;
-        }
-        console.log("disconnected");
-    }
-
-    constructor() {
-        this.connected = false;
+  async connect(): Promise<void> {
+    this.connecting = true;
+    if (window.suiWallet) {
+      const wallet = window.suiWallet;
+      try {
+        let given = await wallet.requestPermissions();
+        const newLocal: readonly PermissionType[] = ["viewAccount"];
+        let perms = await wallet.hasPermissions(newLocal);
+        console.log(perms);
+        console.log(given);
+        this.connected = true;
+      } catch (err) {
+        console.error(err);
+      } finally {
         this.connecting = false;
+      }
     }
+  }
+
+  // Come back to this later
+  async disconnect(): Promise<void> {
+    if (this.connected == true) {
+      this.connected = false;
+    }
+    console.log("disconnected");
+  }
+
+  constructor() {
+    this.connected = false;
+    this.connecting = false;
+  }
 }
