522523751ed0ab28b81f6485c6dfd2ab1c8bda14
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-08-24 20:45:56 +0300

    wallet-ext: change executeSerializedMoveCall signature to accept base64 or Uint8Array

--- wallet/src/shared/messaging/messages/payloads/transactions/ExecuteTransactionRequest.ts
+++ wallet/src/shared/messaging/messages/payloads/transactions/ExecuteTransactionRequest.ts
@@ -6,10 +6,13 @@ import { isBasePayload } from '_payloads';
 import type { MoveCallTransaction } from '@mysten/sui.js';
 import type { BasePayload, Payload } from '_payloads';
 
+export type TransactionDataType =
+    | { type: 'move-call'; data: MoveCallTransaction }
+    | { type: 'serialized-move-call'; data: string };
+
 export interface ExecuteTransactionRequest extends BasePayload {
     type: 'execute-transaction-request';
-    transaction?: MoveCallTransaction;
-    transactionBytes?: Uint8Array;
+    transaction: TransactionDataType;
 }
 
 export function isExecuteTransactionRequest(
