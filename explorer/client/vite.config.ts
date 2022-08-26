bd4727a7b2f60e6329f4677284713a72e4775d27
Jordan Gensler <jordan@mystenlabs.com>
2022-08-26 15:43:37 -0700

    Fix all builds

--- explorer/client/vite.config.ts
+++ explorer/client/vite.config.ts
@@ -2,15 +2,27 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr(), tsconfigPaths()],
+    plugins: [react(), svgr()],
     build: {
         // Set the output directory to match what CRA uses:
         outDir: 'build',
+    },
+    resolve: {
+        alias: {
+            '@mysten/sui.js': path.resolve(
+                __dirname,
+                '../../sdk/typescript/src/'
+            ),
+            '@mysten/bcs': path.resolve(
+                __dirname,
+                '../../sdk/typescript/bcs/src/'
+            ),
+        },
     },
 });
