8c709bcf48bf66649ec7edd177adb5ffa2f46af0
Mark Logan <mark@marklgn.com>
2022-08-31 11:33:16 -0700

    Fix hash container non-determinism

--- crates/sui-macros/src/lib.rs
+++ crates/sui-macros/src/lib.rs
@@ -4,6 +4,46 @@
 use proc_macro::TokenStream;
 use quote::quote;
 
+#[proc_macro_attribute]
+pub fn init_static_initializers(_args: TokenStream, item: TokenStream) -> TokenStream {
+    let mut input = syn::parse_macro_input!(item as syn::ItemFn);
+
+    let body = &input.block;
+    input.block = syn::parse2(quote! {
+        {
+            // We have some lazily-initialized static state in the program. The initializers
+            // alter the thread-local hash container state any time they create a new hash
+            // container. Therefore, we need to ensure that these initializers are run in a
+            // separate thread before the first test thread is launched. Otherwise, they would
+            // run inside of the first test thread, but not subsequent ones.
+            //
+            // Note that none of this has any effect on process-level determinism. Without this
+            // code, we can still get the same test results from two processes started with the
+            // same seed.
+            //
+            // However, when using sim_test(check_determinism) or MADSIM_TEST_CHECK_DETERMINISM=1,
+            // we want the same test invocation to be deterministic when run twice
+            // _in the same process_, so we need to take care of this. This will also
+            // be very important for being able to reproduce a failure that occurs in the Nth
+            // iteration of a multi-iteration test run.
+            std::thread::spawn(|| {
+                ::telemetry_subscribers::init_for_testing();
+                ::sui_framework::get_move_stdlib();
+                ::sui_framework::get_sui_framework();
+            }).join().unwrap();
+
+            #body
+        }
+    })
+    .expect("Parsing failure");
+
+    let result = quote! {
+        #input
+    };
+
+    result.into()
+}
+
 /// The sui_test macro will invoke either #[madsim::test] or #[tokio::test],
 /// depending on whether the simulator config var is enabled.
 ///
@@ -16,10 +56,13 @@ pub fn sui_test(args: TokenStream, item: TokenStream) -> TokenStream {
     let header = if cfg!(madsim) {
         quote! {
             #[::sui_simulator::sim_test(crate = "sui_simulator", #(#args)*)]
+            #[init_static_initializers]
         }
     } else {
         quote! {
             #[::tokio::test(#(#args)*)]
+            // though this is not required for tokio, we do it to get logs as well.
+            #[init_static_initializers]
         }
     };
 
@@ -45,6 +88,7 @@ pub fn sim_test(args: TokenStream, item: TokenStream) -> TokenStream {
     let result = if cfg!(madsim) {
         quote! {
             #[::sui_simulator::sim_test(crate = "sui_simulator", #(#args)*)]
+            #[init_static_initializers]
             #input
         }
     } else {
