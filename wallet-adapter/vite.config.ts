ebe028c8fe8a6d072245a06de4283b91fad194cb
Jordan Gensler <jordan@mystenlabs.com>
2022-09-01 11:38:20 -0700

    Fix build

--- wallet-adapter/vite.config.ts
+++ wallet-adapter/vite.config.ts
@@ -3,7 +3,7 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
