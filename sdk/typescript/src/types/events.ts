f24cf9b5c23e8bfc0053af01b35e7c8d91034fc4
Evan Chan <velvia@gmail.com>
2022-09-02 11:12:25 -0700

    Fix test and type in TS

--- sdk/typescript/src/types/events.ts
+++ sdk/typescript/src/types/events.ts
@@ -28,6 +28,7 @@ export type TransferObjectEvent = {
     objectId: ObjectId;
     version: SequenceNumber;
     type: string; // TODO - better type
+    amount: number | null;
 };
 
 export type DeleteObjectEvent = {
