89bfb0f05d59031773099553087ea547948d9c08
Brandon Williams <brandon@mystenlabs.com>
2022-09-02 16:53:06 -0700

    primary: convert certificate tests

--- narwhal/primary/src/tests/certificate_tests.rs
+++ narwhal/primary/src/tests/certificate_tests.rs
@@ -3,154 +3,142 @@
 
 // This test file tests the validity of the 'certificates' implementation.
 
-use fastcrypto::{
-    traits::{KeyPair, Signer},
-    Hash, SignatureService,
-};
-use test_utils::{
-    committee, fixture_batch_with_transactions, fixture_header_builder, keys, keys_with_len,
-    pure_committee_from_keys_with_mock_ports, shared_worker_cache,
-};
-use types::{Certificate, Header, Vote};
-
-fn generate_header() -> Header {
-    let batch = fixture_batch_with_transactions(10);
-    let proposer = &keys(None).pop().unwrap();
-
-    fixture_header_builder()
-        .with_payload_batch(batch, 0)
-        .build(proposer)
-        .unwrap()
-}
+use crypto::KeyPair;
+use fastcrypto::traits::KeyPair as _;
+use std::num::NonZeroUsize;
+use test_utils::CommitteeFixture;
+use types::{Certificate, Vote};
 
 #[test]
 fn test_empty_certificate_verification() {
-    let header = generate_header();
-    // You should not be allowed to create a certificate that does not satisfying quorum requirements
-    assert!(Certificate::new(&committee(None), header.clone(), Vec::new()).is_err());
+    let fixture = CommitteeFixture::builder().build();
 
-    let certificate = Certificate::new_unsigned(&committee(None), header, Vec::new()).unwrap();
+    let committee = fixture.committee();
+    let header = fixture.header();
+    // You should not be allowed to create a certificate that does not satisfying quorum requirements
+    assert!(Certificate::new(&committee, header.clone(), Vec::new()).is_err());
+
+    let certificate = Certificate::new_unsigned(&committee, header, Vec::new()).unwrap();
     assert!(certificate
-        .verify(&committee(None), shared_worker_cache(None))
+        .verify(&committee, fixture.worker_cache().into())
         .is_err());
 }
 
-#[tokio::test]
-async fn test_valid_certificate_verification() {
-    let header = generate_header();
+#[test]
+fn test_valid_certificate_verification() {
+    let fixture = CommitteeFixture::builder().build();
+    let committee = fixture.committee();
+    let header = fixture.header();
 
     let mut signatures = Vec::new();
 
     // 3 Signers satisfies the 2F + 1 signed stake requirement
-    for key in &keys(None)[..3] {
-        let mut signature_service = SignatureService::new(key.copy());
-        let vote = Vote::new(&header, key.public(), &mut signature_service).await;
+    for authority in fixture.authorities().take(3) {
+        let vote = authority.vote(&header);
         signatures.push((vote.author.clone(), vote.signature.clone()));
     }
 
-    let certificate = Certificate::new(&committee(None), header, signatures).unwrap();
+    let certificate = Certificate::new(&committee, header, signatures).unwrap();
 
     assert!(certificate
-        .verify(&committee(None), shared_worker_cache(None))
+        .verify(&committee, fixture.worker_cache().into())
         .is_ok());
 }
 
-#[tokio::test]
-async fn test_certificate_insufficient_signatures() {
-    let header = generate_header();
+#[test]
+fn test_certificate_insufficient_signatures() {
+    let fixture = CommitteeFixture::builder().build();
+    let committee = fixture.committee();
+    let header = fixture.header();
 
     let mut signatures = Vec::new();
 
     // 2 Signatures. This is less than 2F + 1 (3).
-    for key in &keys(None)[..2] {
-        let mut signature_service = SignatureService::new(key.copy());
-        let vote = Vote::new(&header, key.public(), &mut signature_service).await;
+    for authority in fixture.authorities().take(2) {
+        let vote = authority.vote(&header);
         signatures.push((vote.author.clone(), vote.signature.clone()));
     }
 
-    assert!(Certificate::new(&committee(None), header.clone(), signatures.clone()).is_err());
+    assert!(Certificate::new(&committee, header.clone(), signatures.clone()).is_err());
 
-    let certificate = Certificate::new_unsigned(&committee(None), header, signatures).unwrap();
+    let certificate = Certificate::new_unsigned(&committee, header, signatures).unwrap();
 
     assert!(certificate
-        .verify(&committee(None), shared_worker_cache(None))
+        .verify(&committee, fixture.worker_cache().into())
         .is_err());
 }
 
-#[tokio::test]
-async fn test_certificate_validly_repeated_public_keys() {
-    let header = generate_header();
+#[test]
+fn test_certificate_validly_repeated_public_keys() {
+    let fixture = CommitteeFixture::builder().build();
+    let committee = fixture.committee();
+    let header = fixture.header();
 
     let mut signatures = Vec::new();
 
-    // 2 Signatures. This is less than 2F + 1 (3).
-    for key in &keys(None)[..3] {
+    // 3 Signers satisfies the 2F + 1 signed stake requirement
+    for authority in fixture.authorities().take(3) {
+        let vote = authority.vote(&header);
         // We double every (pk, signature) pair - these should be ignored when forming the certificate.
-        let mut signature_service = SignatureService::new(key.copy());
-        let vote = Vote::new(&header, key.public(), &mut signature_service).await;
         signatures.push((vote.author.clone(), vote.signature.clone()));
         signatures.push((vote.author.clone(), vote.signature.clone()));
     }
 
-    let certificate_res = Certificate::new(&committee(None), header, signatures);
+    let certificate_res = Certificate::new(&committee, header, signatures);
     assert!(certificate_res.is_ok());
     let certificate = certificate_res.unwrap();
 
     assert!(certificate
-        .verify(&committee(None), shared_worker_cache(None))
+        .verify(&committee, fixture.worker_cache().into())
         .is_ok());
 }
 
-#[tokio::test]
-async fn test_unknown_signature_in_certificate() {
-    let header = generate_header();
+#[test]
+fn test_unknown_signature_in_certificate() {
+    let fixture = CommitteeFixture::builder().build();
+    let committee = fixture.committee();
+    let header = fixture.header();
 
     let mut signatures = Vec::new();
 
     // 2 Signatures. This is less than 2F + 1 (3).
-    for key in &keys(None)[..2] {
-        // We double every (pk, signature) pair - these should be ignored when forming the certificate.
-        let mut signature_service = SignatureService::new(key.copy());
-        let vote = Vote::new(&header, key.public(), &mut signature_service).await;
+    for authority in fixture.authorities().take(2) {
+        let vote = authority.vote(&header);
         signatures.push((vote.author.clone(), vote.signature.clone()));
     }
 
-    let malicious_key = keys(Some(2)).pop().unwrap();
-    let mut signature_service = SignatureService::new(malicious_key.copy());
-    let vote = Vote::new(&header, malicious_key.public(), &mut signature_service).await;
+    let malicious_key = KeyPair::generate(&mut rand::rngs::OsRng);
+
+    let vote = Vote::new_with_signer(&header, malicious_key.public(), &malicious_key);
     signatures.push((vote.author.clone(), vote.signature));
 
-    assert!(Certificate::new(&committee(None), header, signatures).is_err());
+    assert!(Certificate::new(&committee, header, signatures).is_err());
 }
 
-//
-// This takes really long to run due to an optimization we didn't make (we should cache the genesis committee).
-//
 proptest::proptest! {
-    #[ignore]
     #[test]
     fn test_certificate_verification(
         committee_size in 4..35_usize
     ) {
-        let keys = &keys_with_len(None, committee_size);
-        let committee = &pure_committee_from_keys_with_mock_ports(&keys[..]);
-        let header = generate_header();
+        let fixture = CommitteeFixture::builder()
+            .committee_size(NonZeroUsize::new(committee_size).unwrap())
+            .build();
+        let committee = fixture.committee();
+        let header = fixture.header();
 
         let mut signatures = Vec::new();
 
         let quorum_threshold = committee.quorum_threshold() as usize;
 
-        let unsigned_certificate = Certificate::new_unsigned(committee, header.clone(), vec![]).unwrap();
-
-        for key in &keys[..quorum_threshold] {
-            let signature = key.sign(unsigned_certificate.digest().as_ref());
-            signatures.push((key.public().clone(), signature));
+        for authority in fixture.authorities().take(quorum_threshold) {
+            let vote = authority.vote(&header);
+            signatures.push((vote.author.clone(), vote.signature.clone()));
         }
 
-        let certificate = Certificate::new(committee, header, signatures).unwrap();
+        let certificate = Certificate::new(&committee, header, signatures).unwrap();
 
         assert!(certificate
-            .verify(committee, shared_worker_cache(None))
+            .verify(&committee, fixture.worker_cache().into())
             .is_ok());
     }
 }
