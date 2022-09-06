9fdc118cdfee0cc3c30518fe299a4c8b01bea11f
Andrey Chursin <andrey@mystenlabs.com>
2022-09-06 13:49:46 -0700

    Count in-flight GRPC requests via request lifecycle metrics
    This is a fix on top of #4489 that uses more reliable lifecycle metrics introduced in MystenLabs/mysten-infra#142
    
    Also adds request counter to see total number of requests

--- crates/sui-node/src/metrics.rs
+++ crates/sui-node/src/metrics.rs
@@ -3,7 +3,10 @@
 
 use axum::{extract::Extension, http::StatusCode, routing::get, Router};
 use mysten_network::metrics::MetricsCallbackProvider;
-use prometheus::{register_int_gauge_vec_with_registry, IntGaugeVec, Registry, TextEncoder};
+use prometheus::{
+    register_int_counter_vec_with_registry, register_int_gauge_vec_with_registry, IntCounterVec,
+    IntGaugeVec, Registry, TextEncoder,
+};
 use std::net::SocketAddr;
 use std::time::Duration;
 use sui_network::tonic::Code;
@@ -41,6 +44,7 @@ async fn metrics(Extension(registry): Extension<Registry>) -> (StatusCode, Strin
 #[derive(Clone)]
 pub struct GrpcMetrics {
     inflight_grpc: IntGaugeVec,
+    grpc_requests: IntCounterVec,
 }
 
 impl GrpcMetrics {
@@ -48,7 +52,14 @@ impl GrpcMetrics {
         Self {
             inflight_grpc: register_int_gauge_vec_with_registry!(
                 "inflight_grpc",
-                "Total in-flight GRPC per route",
+                "Total in-flight GRPC requests per route",
+                &["path"],
+                registry,
+            )
+            .unwrap(),
+            grpc_requests: register_int_counter_vec_with_registry!(
+                "grpc_requests",
+                "Total GRPC requests per route",
                 &["path"],
                 registry,
             )
@@ -58,11 +69,22 @@ impl GrpcMetrics {
 }
 
 impl MetricsCallbackProvider for GrpcMetrics {
-    fn on_request(&self, path: String) {
-        self.inflight_grpc.with_label_values(&[&path]).inc();
+    fn on_request(&self, _path: String) {}
+    fn on_response(
+        &self,
+        _path: String,
+        _latency: Duration,
+        _status: u16,
+        _grpc_status_code: Code,
+    ) {
     }
 
-    fn on_response(&self, path: String, _latency: Duration, _status: u16, _grpc_status_code: Code) {
-        self.inflight_grpc.with_label_values(&[&path]).dec();
+    fn on_start(&self, path: &str) {
+        self.inflight_grpc.with_label_values(&[path]).inc();
+        self.grpc_requests.with_label_values(&[path]).inc();
+    }
+
+    fn on_drop(&self, path: &str) {
+        self.inflight_grpc.with_label_values(&[path]).dec();
     }
 }
