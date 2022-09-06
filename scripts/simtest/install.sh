3d0f675ad0751908fc1d000a1e05c717672df869
Mark Logan <mark@marklgn.com>
2022-09-06 10:38:01 -0700

    Make the installed cargo-simtest script invoke the one in version control

--- scripts/simtest/install.sh
+++ scripts/simtest/install.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 BIN_DIR="$HOME/.cargo/bin"
+SOURCE_DIR=$(pwd)
 
 if [ ! -d "$BIN_DIR" ]; then
   echo "$BIN_DIR not found."
@@ -10,6 +11,16 @@ if [ ! -d "$BIN_DIR" ]; then
   exit 1
 fi
 
-cp cargo-simtest "$BIN_DIR" || exit 1
-echo "Installed cargo-simtest to $BIN_DIR"
+SIMTEST=$BIN_DIR/cargo-simtest
+
+cat <<EOF > "$SIMTEST" || exit 1
+#!/bin/bash
+
+REPO_ROOT=\$(git rev-parse --show-toplevel)
+source "\$REPO_ROOT/scripts/simtest/cargo-simtest"
+EOF
+
+chmod +x "$SIMTEST"
+
+echo "Installed cargo-simtest to $SIMTEST"
 echo "You can now run simulator tests via \`cargo simtest\`"
