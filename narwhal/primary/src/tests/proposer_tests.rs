59d0e55e4836569423dd4d71b7a04c322757a120
Brandon Williams <brandon@mystenlabs.com>
2022-09-07 13:37:34 -0700

    primary: convert proposer tests to use a CommitteeFixture

--- narwhal/primary/src/tests/proposer_tests.rs
+++ narwhal/primary/src/tests/proposer_tests.rs
@@ -4,16 +4,19 @@
 use super::*;
 use fastcrypto::traits::KeyPair;
 use prometheus::Registry;
-use test_utils::{committee, keys, shared_worker_cache};
+use test_utils::CommitteeFixture;
 
 #[tokio::test]
 async fn propose_empty() {
-    let kp = keys(None).pop().unwrap();
-    let name = kp.public().clone();
-    let signature_service = SignatureService::new(kp);
+    let fixture = CommitteeFixture::builder().build();
+    let committee = fixture.committee();
+    let shared_worker_cache = fixture.shared_worker_cache();
+    let primary = fixture.authorities().next().unwrap();
+    let name = primary.public_key();
+    let signature_service = SignatureService::new(primary.keypair().copy());
 
     let (_tx_reconfigure, rx_reconfigure) =
-        watch::channel(ReconfigureNotification::NewEpoch(committee(None)));
+        watch::channel(ReconfigureNotification::NewEpoch(committee.clone()));
     let (_tx_parents, rx_parents) = test_utils::test_channel!(1);
     let (_tx_our_digests, rx_our_digests) = test_utils::test_channel!(1);
     let (tx_headers, mut rx_headers) = test_utils::test_channel!(1);
@@ -23,7 +26,7 @@ async fn propose_empty() {
     // Spawn the proposer.
     let _proposer_handle = Proposer::spawn(
         name,
-        committee(None),
+        committee.clone(),
         signature_service,
         /* header_size */ 1_000,
         /* max_header_delay */ Duration::from_millis(20),
@@ -39,19 +42,20 @@ async fn propose_empty() {
     let header = rx_headers.recv().await.unwrap();
     assert_eq!(header.round, 1);
     assert!(header.payload.is_empty());
-    assert!(header
-        .verify(&committee(None), shared_worker_cache(None))
-        .is_ok());
+    assert!(header.verify(&committee, shared_worker_cache).is_ok());
 }
 
 #[tokio::test]
 async fn propose_payload() {
-    let kp = keys(None).pop().unwrap();
-    let name = kp.public().clone();
-    let signature_service = SignatureService::new(kp);
+    let fixture = CommitteeFixture::builder().build();
+    let committee = fixture.committee();
+    let shared_worker_cache = fixture.shared_worker_cache();
+    let primary = fixture.authorities().next().unwrap();
+    let name = primary.public_key();
+    let signature_service = SignatureService::new(primary.keypair().copy());
 
     let (_tx_reconfigure, rx_reconfigure) =
-        watch::channel(ReconfigureNotification::NewEpoch(committee(None)));
+        watch::channel(ReconfigureNotification::NewEpoch(committee.clone()));
     let (_tx_parents, rx_parents) = test_utils::test_channel!(1);
     let (tx_our_digests, rx_our_digests) = test_utils::test_channel!(1);
     let (tx_headers, mut rx_headers) = test_utils::test_channel!(1);
@@ -61,7 +65,7 @@ async fn propose_payload() {
     // Spawn the proposer.
     let _proposer_handle = Proposer::spawn(
         name.clone(),
-        committee(None),
+        committee.clone(),
         signature_service,
         /* header_size */ 32,
         /* max_header_delay */
@@ -86,7 +90,5 @@ async fn propose_payload() {
     let header = rx_headers.recv().await.unwrap();
     assert_eq!(header.round, 1);
     assert_eq!(header.payload.get(&digest), Some(&worker_id));
-    assert!(header
-        .verify(&committee(None), shared_worker_cache(None))
-        .is_ok());
+    assert!(header.verify(&committee, shared_worker_cache).is_ok());
 }
