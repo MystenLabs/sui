a51edcb4be5a7a0c3e99728b1b27128c07fda5de
Jibz <godwinjibs@gmail.com>
2022-09-07 20:47:29 -0400

    limit number validators on homepage to 5 and fix error display

--- explorer/client/src/components/top-validators-card/TopValidatorsCard.tsx
+++ explorer/client/src/components/top-validators-card/TopValidatorsCard.tsx
@@ -88,8 +88,10 @@ function TopValidatorsCard({ state }: { state: ValidatorState }): JSX.Element {
     );
 
     // map the above data to match the table - combine stake and stake percent
+    // limit validators to 5 elements
+    // TODO: add ordering
     const tableData = {
-        data: validatorsData.map((validator) => ({
+        data: validatorsData.splice(0, 5).map((validator) => ({
             name: validator.name,
             position: validator.position,
             address: (
