b2e03b466b86eaa8eef51a85e55c961e9f60dcd5
Sam Blackshear <sam.blackshear@gmail.com>
2022-09-10 16:02:10 -0700

    [doc] fix Move package tutorial
    The package tutorial is currently broken, which is confusing some folks on Discord and in builder's groups. Let's take this opportunity to fix and simplify the tutorial:
    
    * Use `sui move new` to create the package skeleton
    * Fix `sui move new` to create a Move.toml that depends on the devnet branch + has a named alias for `sui` by default
    * Fix unused imports in the example code
    * Expand the example code and explain the different parts of the module

--- crates/sui/src/sui_move/new.rs
+++ crates/sui/src/sui_move/new.rs
@@ -4,9 +4,12 @@
 use clap::Parser;
 use move_cli::base::new;
 use std::path::PathBuf;
+use sui_types::SUI_FRAMEWORK_ADDRESS;
 
 const SUI_PKG_NAME: &str = "Sui";
-const SUI_PKG_PATH: &str = "{ git = \"https://github.com/MystenLabs/sui.git\", subdir = \"crates/sui-framework\", rev = \"main\" }";
+
+// Use devnet by default. Probably want to add options to make this configurable later
+const SUI_PKG_PATH: &str = "{ git = \"https://github.com/MystenLabs/sui.git\", subdir = \"crates/sui-framework\", rev = \"devnet\" }";
 
 #[derive(Parser)]
 pub struct New {
@@ -21,7 +24,13 @@ impl New {
             path,
             "0.0.1",
             [(SUI_PKG_NAME, SUI_PKG_PATH)],
-            [(name, "0x0")],
+            [
+                (name, "0x0"),
+                (
+                    &SUI_PKG_NAME.to_lowercase(),
+                    &SUI_FRAMEWORK_ADDRESS.to_string(),
+                ),
+            ],
             "",
         )?;
         Ok(())
