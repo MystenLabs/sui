f648f1c3422b226ca4481ba4523a1790e6c662a7
Brandon Williams <brandon@mystenlabs.com>
2022-09-11 15:54:38 -0700

    types: bring back serialized Batch benchmark

--- narwhal/types/src/tests/batch_serde.rs
+++ narwhal/types/src/tests/batch_serde.rs
@@ -1,9 +1,10 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
+
+use crate::{serialized_batch_digest, Batch, WorkerMessage};
 use fastcrypto::Hash;
 use proptest::arbitrary::Arbitrary;
 use serde_test::{assert_tokens, Token};
-use types::{Batch, WorkerMessage};
 
 #[test]
 fn test_serde_batch() {
@@ -82,4 +83,18 @@ fn test_bincode_serde_batch_message() {
         "received {}",
         hex::encode(txes_bytes)
     );
+}
+
+proptest::proptest! {
+
+    #[test]
+    fn test_batch_and_serialized(
+        batch in Batch::arbitrary()
+    ) {
+        let digest = batch.digest();
+        let message = WorkerMessage::Batch(batch);
+        let serialized = bincode::serialize(&message).expect("Failed to serialize our own batch");
+        let digest_from_serialized = serialized_batch_digest(&serialized).expect("Failed to hash serialized batch");
+        assert_eq!(digest, digest_from_serialized);
+    }
 }
