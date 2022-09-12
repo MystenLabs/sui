3d6eb4609bf888f55eb2ba086b1212f9340a7975
Jordan Gensler <jordan@mystenlabs.com>
2022-09-12 09:26:52 -0700

    Fixing storybook

--- explorer/client/.storybook/main.js
+++ explorer/client/.storybook/main.js
@@ -4,9 +4,8 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    // "@storybook/addon-links",
+    "@storybook/addon-a11y",
     "@storybook/addon-essentials",
-    // "@storybook/addon-interactions"
   ],
   "framework": "@storybook/react",
   "core": {
