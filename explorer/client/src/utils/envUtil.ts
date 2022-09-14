5186e0bec6b2e9c943637b8f3f93be6b86a3af8f
Jordan Gensler <jordan@mystenlabs.com>
2022-09-14 15:27:57 -0700

    Support unit tests

--- explorer/client/src/utils/envUtil.ts
+++ explorer/client/src/utils/envUtil.ts
@@ -13,7 +13,10 @@ const HOST_TO_NETWORK: Record<string, Network> = {
 export let CURRENT_ENV: Network = Network.Local;
 if (import.meta.env.VITE_NETWORK) {
     CURRENT_ENV = HOST_TO_NETWORK[import.meta.env.VITE_NETWORK];
-} else if (window.location.hostname.includes('.sui.io')) {
+} else if (
+    typeof window !== 'undefined' &&
+    window.location.hostname.includes('.sui.io')
+) {
     const host = window.location.hostname.split('.').at(-3) || 'devnet';
     CURRENT_ENV = HOST_TO_NETWORK[host] || Network.Devnet;
 }
