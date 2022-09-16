68140e8f03e4b6f33e718fc5958e2a65b509ccb7
Jordan Gensler <jordan@mystenlabs.com>
2022-09-16 12:24:00 -0700

    [explorer] Remove targeted builds for explorer (#4631)
    * Remove targeted builds for explorer
    
    * Fix targeted builds for static
    
    * Support unit tests
    
    * Update docs + remove dev:local command
    
    * Update explorer/client/README.md
    
    Co-authored-by: Randall-Mysten <109545725+randall-Mysten@users.noreply.github.com>
    
    * Use prod instead of dev
    
    * Lint fix
    
    Co-authored-by: Randall-Mysten <109545725+randall-Mysten@users.noreply.github.com>

--- explorer/client/src/utils/api/rpcSetting.ts
+++ explorer/client/src/utils/api/rpcSetting.ts
@@ -3,14 +3,19 @@
 
 export enum Network {
     Local = 'Local',
+    Static = 'Static',
     Devnet = 'Devnet',
     Staging = 'Staging',
+    CI = 'CI',
 }
 
-const ENDPOINTS = {
+const ENDPOINTS: Record<Network, string> = {
     [Network.Local]: 'http://127.0.0.1:9000',
+    [Network.CI]: 'https://fullnode.devnet.sui.io:443',
     [Network.Devnet]: 'https://fullnode.devnet.sui.io:443',
     [Network.Staging]: 'https://fullnode.staging.sui.io:443',
+    // NOTE: Static is pointed to devnet, but it shouldn't actually fetch data.
+    [Network.Static]: 'https://fullnode.devnet.sui.io:443',
 };
 
 export function getEndpoint(network: Network | string): string {
