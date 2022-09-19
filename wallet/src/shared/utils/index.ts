a3a332afac799d9a4b47c63215fe8f5e41303bf8
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2022-09-19 09:26:23 -0700

    add page views to wallet

--- wallet/src/shared/utils/index.ts
+++ wallet/src/shared/utils/index.ts
@@ -1,9 +1,24 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Browser from 'webextension-polyfill';
+
+import { WALLET_URL, plausible } from '_shared/constants';
 
 export function openInNewTab() {
     const url = Browser.runtime.getURL('ui.html');
     return Browser.tabs.create({ url });
+}
+
+export function usePageView() {
+    const location = useLocation();
+    useEffect(() => {
+        if (process.env.NODE_ENV !== 'development') {
+            plausible.trackPageview({
+                url: WALLET_URL + location.pathname,
+            });
+        }
+    }, [location]);
 }
