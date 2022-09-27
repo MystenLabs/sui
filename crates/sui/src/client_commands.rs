1d2fc23c9ba8ccfc15a0c218ef43b7aec497e443
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2022-09-27 14:09:04 -0400

    Add Pay to gateway and fullnode APIs

--- crates/sui/src/client_commands.rs
+++ crates/sui/src/client_commands.rs
@@ -9,7 +9,7 @@ use std::{
     time::Instant,
 };
 
-use anyhow::anyhow;
+use anyhow::{anyhow, ensure};
 use bip32::DerivationPath;
 use clap::*;
 use colored::Colorize;
@@ -179,6 +179,31 @@ pub enum SuiClientCommands {
         /// The amount to transfer, if not specified, the entire coin object will be transferred.
         #[clap(long)]
         amount: Option<u64>,
+    },
+    /// Pay SUI to recipients following specified amounts, with input coins.
+    /// Length of recipients must be the same as that of amounts.
+    #[clap(name = "pay")]
+    Pay {
+        /// The input coins to be used for pay recipients, following the specified amounts.
+        #[clap(long)]
+        input_coins: Vec<ObjectID>,
+
+        /// The recipient addresses, must be of same length as amounts
+        #[clap(long)]
+        recipients: Vec<SuiAddress>,
+
+        /// The amounts to be transferred, following the order of recipients.
+        #[clap(long)]
+        amounts: Vec<u64>,
+
+        /// ID of the gas object for gas payment, in 20 bytes Hex string
+        /// If not provided, a gas object with at least gas_budget value will be selected
+        #[clap(long)]
+        gas: Option<ObjectID>,
+
+        /// Gas budget for this transfer
+        #[clap(long)]
+        gas_budget: u64,
     },
     /// Synchronize client state with authorities.
     #[clap(name = "sync")]
@@ -381,6 +406,50 @@ impl SuiClientCommands {
                     return Err(anyhow!("Error transferring SUI: {:#?}", effects.status));
                 }
                 SuiClientCommandResult::TransferSui(cert, effects)
+            }
+
+            SuiClientCommands::Pay {
+                input_coins,
+                recipients,
+                amounts,
+                gas,
+                gas_budget,
+            } => {
+                ensure!(
+                    !input_coins.is_empty(),
+                    "Pay transaction requires a non-empty list of input coins"
+                );
+                ensure!(
+                    !recipients.is_empty(),
+                    "Pay transaction requires a non-empty list of recipient addresses"
+                );
+                ensure!(
+                    recipients.len() == amounts.len(),
+                    format!(
+                        "Found {:?} recipient addresses, but {:?} recipient amounts",
+                        recipients.len(),
+                        amounts.len()
+                    ),
+                );
+                let from = context.get_object_owner(&input_coins[0]).await?;
+                let data = context
+                    .client
+                    .transaction_builder()
+                    .pay(from, input_coins, recipients, amounts, gas, gas_budget)
+                    .await?;
+                let signature = context.keystore.sign(&from, &data.to_bytes())?;
+                let response = context
+                    .execute_transaction(Transaction::new(data, signature))
+                    .await?;
+                let cert = response.certificate;
+                let effects = response.effects;
+                if matches!(effects.status, SuiExecutionStatus::Failure { .. }) {
+                    return Err(anyhow!(
+                        "Error executing Pay transaction: {:#?}",
+                        effects.status
+                    ));
+                }
+                SuiClientCommandResult::Pay(cert, effects)
             }
 
             SuiClientCommands::Addresses => {
@@ -750,6 +819,9 @@ impl Display for SuiClientCommandResult {
             SuiClientCommandResult::TransferSui(cert, effects) => {
                 write!(writer, "{}", write_cert_and_effects(cert, effects)?)?;
             }
+            SuiClientCommandResult::Pay(cert, effects) => {
+                write!(writer, "{}", write_cert_and_effects(cert, effects)?)?;
+            }
             SuiClientCommandResult::Addresses(addresses) => {
                 writeln!(writer, "Showing {} results.", addresses.len())?;
                 for address in addresses {
@@ -955,6 +1027,7 @@ pub enum SuiClientCommandResult {
         SuiTransactionEffects,
     ),
     TransferSui(SuiCertifiedTransaction, SuiTransactionEffects),
+    Pay(SuiCertifiedTransaction, SuiTransactionEffects),
     Addresses(Vec<SuiAddress>),
     Objects(Vec<SuiObjectInfo>),
     SyncClientState,
