2412c21b05dc3c5cfb15efc2669e6076e3ebc644
Jordan Gensler <jordan@mystenlabs.com>
2022-09-30 13:04:31 -0700

    Start adding signAndSendTransaction to dapp interrface

--- apps/wallet/src/shared/messaging/messages/payloads/transactions/ExecuteTransactionRequest.ts
+++ apps/wallet/src/shared/messaging/messages/payloads/transactions/ExecuteTransactionRequest.ts
@@ -3,10 +3,11 @@
 
 import { isBasePayload } from '_payloads';
 
-import type { MoveCallTransaction } from '@mysten/sui.js';
+import type { MoveCallTransaction, SignableTransaction } from '@mysten/sui.js';
 import type { BasePayload, Payload } from '_payloads';
 
 export type TransactionDataType =
+    | { type: 'v2'; data: SignableTransaction }
     | { type: 'move-call'; data: MoveCallTransaction }
     | { type: 'serialized-move-call'; data: string };
 
