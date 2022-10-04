2bf08d4fd19931d04c319b48452549f42749901b
Arun Koshy <97870774+arun-koshy@users.noreply.github.com>
2022-10-03 22:53:17 -0700

    Update connectivity.rs license

--- narwhal/network/src/connectivity.rs
+++ narwhal/network/src/connectivity.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
-// SPDX-License-Identifier: A&pache-2.0
+// SPDX-License-Identifier: Apache-2.0
 use tokio::task::JoinHandle;
 
 use crate::metrics::NetworkConnectionMetrics;
