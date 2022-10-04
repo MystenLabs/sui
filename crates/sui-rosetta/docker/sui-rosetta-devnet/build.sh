bf2f6163afdd628b267daf8c6cbb4f489204ae02
patrick <patrickkuo@me.com>
2022-10-04 10:20:32 +0100

    rename image name for different env

--- crates/sui-rosetta/docker/sui-rosetta-devnet/build.sh
+++ crates/sui-rosetta/docker/sui-rosetta-devnet/build.sh
@@ -20,7 +20,7 @@ echo "git revision: \t$GIT_REVISION"
 echo
 
 docker build -f "$DOCKERFILE" "$REPO_ROOT" \
-  -t mysten/sui-rosetta \
+  -t mysten/sui-rosetta-devnet \
 	--build-arg GIT_REVISION="$GIT_REVISION" \
 	--build-arg BUILD_DATE="$BUILD_DATE" \
 	"$@"
