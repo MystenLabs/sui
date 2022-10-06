6214130b4e47b781b298089b852af665de196aa3
Brandon Williams <brandon@mystenlabs.com>
2022-10-05 17:25:00 -0700

    chore: update tonic to 0.8.2

--- narwhal/examples/build.rs
+++ narwhal/examples/build.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("PROTOC", protobuf_src::protoc());
     tonic_build::compile_protos("protos/narwhal.proto")?;
     Ok(())
 }
