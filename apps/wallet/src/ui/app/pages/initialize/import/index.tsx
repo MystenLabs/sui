e462611f06c7ba93604fad0545c51baad14995ae
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-10-08 03:40:56 +0100

    wallet-ext: import mnemonic flow step one (input mnemonic)

--- apps/wallet/src/ui/app/pages/initialize/import/index.tsx
+++ apps/wallet/src/ui/app/pages/initialize/import/index.tsx
@@ -1,103 +1,67 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useFormik } from 'formik';
-import { useCallback } from 'react';
-import * as Yup from 'yup';
+import { useCallback, useState } from 'react';
 
-import Button from '_app/shared/button';
-import Icon, { SuiIcons } from '_components/icon';
-import { useAppDispatch, useAppSelector } from '_hooks';
+import StepOne from './steps/StepOne';
+import { WALLET_ENCRYPTION_ENABLED } from '_app/wallet/constants';
+import { useAppDispatch } from '_hooks';
+import CardLayout from '_pages/initialize/shared/card-layout';
 import { createMnemonic } from '_redux/slices/account';
-import { normalizeMnemonics, validateMnemonics } from '_src/shared/utils/bip39';
-
-import type { FocusEventHandler } from 'react';
 
 import st from './Import.module.scss';
 
-const validationSchema = Yup.object({
-    mnemonic: Yup.string()
-        .ensure()
-        .required()
-        .trim()
-        .transform((mnemonic) => normalizeMnemonics(mnemonic))
-        .test('mnemonic-valid', 'Recovery Passphrase is invalid', (mnemonic) =>
-            validateMnemonics(mnemonic)
-        )
-        .label('Recovery Passphrase'),
-});
-
 const initialValues = {
     mnemonic: '',
+    password: '',
+    confirmPassword: '',
 };
-type ValuesType = typeof initialValues;
+
+const allSteps = [StepOne];
+
+if (WALLET_ENCRYPTION_ENABLED) {
+    // TODO: add more steps
+}
+
+export type ImportValuesType = typeof initialValues;
 
 const ImportPage = () => {
-    const createInProgress = useAppSelector(({ account }) => account.creating);
+    const [data, setData] = useState<ImportValuesType>(initialValues);
+    const [step, setStep] = useState(0);
     const dispatch = useAppDispatch();
     const onHandleSubmit = useCallback(
-        async ({ mnemonic }: ValuesType) => {
-            await dispatch(createMnemonic({ existingMnemonic: mnemonic }));
+        async ({ mnemonic, password }: ImportValuesType) => {
+            //TODO
+            await new Promise((r) => setTimeout(r, 3000));
+            await dispatch(
+                createMnemonic({ importedMnemonic: mnemonic, password })
+            );
         },
         [dispatch]
     );
-    const {
-        handleBlur,
-        handleChange,
-        values: { mnemonic },
-        isSubmitting,
-        isValid,
-        errors,
-        touched,
-        handleSubmit,
-        setFieldValue,
-    } = useFormik({
-        initialValues,
-        onSubmit: onHandleSubmit,
-        validationSchema,
-        validateOnMount: true,
-    });
-    const onHandleMnemonicBlur = useCallback<
-        FocusEventHandler<HTMLTextAreaElement>
-    >(
-        async (e) => {
-            const adjMnemonic = await validationSchema.fields.mnemonic.cast(
-                mnemonic
-            );
-            await setFieldValue('mnemonic', adjMnemonic, false);
-            handleBlur(e);
-        },
-        [setFieldValue, mnemonic, handleBlur]
-    );
+    const totalSteps = allSteps.length;
+    const StepForm = step < totalSteps ? allSteps[step] : null;
     return (
-        <>
-            <h1 className={st.headerTitle}>Import wallet</h1>
-            <form onSubmit={handleSubmit} noValidate autoComplete="off">
-                <textarea
-                    onChange={handleChange}
-                    value={mnemonic}
-                    onBlur={onHandleMnemonicBlur}
-                    className={st.mnemonic}
-                    placeholder="Paste your 12-word passphrase"
-                    name="mnemonic"
-                    disabled={createInProgress || isSubmitting}
+        <CardLayout>
+            <h3 className={st.headerSubTitle}>Wallet Setup</h3>
+            <h1 className={st.headerTitle}>Import an Existing Wallet</h1>
+            {StepForm ? (
+                <StepForm
+                    next={async (data, stepIncrement) => {
+                        const nextStep = step + stepIncrement;
+                        if (nextStep >= totalSteps) {
+                            await onHandleSubmit(data);
+                        }
+                        setData(data);
+                        if (nextStep < 0) {
+                            return;
+                        }
+                        setStep(nextStep);
+                    }}
+                    data={data}
                 />
-                {touched.mnemonic && errors?.mnemonic && (
-                    <div className={st.error}>{errors?.mnemonic}</div>
-                )}
-
-                <Button
-                    type="submit"
-                    disabled={isSubmitting || createInProgress || !isValid}
-                    mode="primary"
-                    className={st.btn}
-                    size="large"
-                >
-                    Import wallet Now
-                    <Icon icon={SuiIcons.ArrowRight} className={st.next} />
-                </Button>
-            </form>
-        </>
+            ) : null}
+        </CardLayout>
     );
 };
 
---
+++ apps/wallet/src/ui/app/pages/initialize/import/steps/StepOne.tsx
@@ -0,0 +1,93 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+
+import Button from '_app/shared/button';
+import { WALLET_ENCRYPTION_ENABLED } from '_app/wallet/constants';
+import Alert from '_components/alert';
+import Icon, { SuiIcons } from '_components/icon';
+import Loading from '_components/loading';
+import { mnemonicValidation } from '_pages/initialize/import/validation';
+
+import type { ImportValuesType } from '_pages/initialize/import/index';
+
+import st from './StepOne.module.scss';
+
+const validationSchema = Yup.object({
+    mnemonic: mnemonicValidation,
+});
+
+export type StepOneProps = {
+    next: (data: ImportValuesType, step: 1 | -1) => Promise<void>;
+    data: ImportValuesType;
+};
+
+export default function StepOne({ next, data }: StepOneProps) {
+    const btnTxt = WALLET_ENCRYPTION_ENABLED ? 'Continue' : 'Import wallet Now';
+    return (
+        <Formik
+            initialValues={data}
+            validationSchema={validationSchema}
+            validateOnMount={true}
+            onSubmit={async (values) => {
+                await next(values, 1);
+            }}
+            enableReinitialize={true}
+        >
+            {({
+                isSubmitting,
+                touched,
+                errors,
+                values: { mnemonic },
+                isValid,
+                handleChange,
+                setFieldValue,
+                handleBlur,
+            }) => (
+                <Form className={st.form}>
+                    <label className={st.label} htmlFor="importMnemonicTxt">
+                        Enter Recovery Phrase
+                    </label>
+                    <textarea
+                        id="importMnemonicTxt"
+                        onChange={handleChange}
+                        value={mnemonic}
+                        onBlur={async (e) => {
+                            const adjMnemonic =
+                                await validationSchema.fields.mnemonic.cast(
+                                    mnemonic
+                                );
+                            await setFieldValue('mnemonic', adjMnemonic, false);
+                            handleBlur(e);
+                        }}
+                        className={st.mnemonic}
+                        placeholder="Enter your 12-word recovery phrase"
+                        name="mnemonic"
+                        disabled={isSubmitting}
+                    />
+                    {touched.mnemonic && errors?.mnemonic && (
+                        <Alert>{errors?.mnemonic}</Alert>
+                    )}
+                    <div className={st.fill} />
+                    <Button
+                        type="submit"
+                        disabled={isSubmitting || !isValid}
+                        mode="primary"
+                        className={st.btn}
+                        size="large"
+                    >
+                        <Loading loading={isSubmitting}>
+                            {btnTxt}
+                            <Icon
+                                icon={SuiIcons.ArrowRight}
+                                className={st.next}
+                            />
+                        </Loading>
+                    </Button>
+                </Form>
+            )}
+        </Formik>
+    );
+}
