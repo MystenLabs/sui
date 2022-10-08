a3473acb22666c3734c2c01bdc5716d725873b3e
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-10-08 11:21:47 +0100

    wallet-ext: onboarding views reuse card-layout

--- apps/wallet/src/ui/app/pages/initialize/shared/card-layout/index.tsx
+++ apps/wallet/src/ui/app/pages/initialize/shared/card-layout/index.tsx
@@ -3,15 +3,47 @@
 
 import cn from 'classnames';
 
+import Icon, { SuiIcons } from '_components/icon';
+
 import type { ReactNode } from 'react';
 
 import st from './CardLayout.module.scss';
 
 export type CardLayoutProps = {
+    title?: string;
+    subtitle?: string;
+    headerCaption?: string;
+    icon?: 'success';
     children: ReactNode | ReactNode[];
     className?: string;
 };
 
-export default function CardLayout({ className, children }: CardLayoutProps) {
-    return <div className={cn(className, st.container)}>{children}</div>;
+export default function CardLayout({
+    className,
+    children,
+    title,
+    subtitle,
+    headerCaption,
+    icon,
+}: CardLayoutProps) {
+    return (
+        <div className={cn(className, st.container)}>
+            {icon === 'success' ? (
+                <div className={st.successIcon}>
+                    <div className={st.successBg}>
+                        <Icon
+                            icon={SuiIcons.ThumbsUp}
+                            className={st.thumbsUp}
+                        />
+                    </div>
+                </div>
+            ) : null}
+            {headerCaption ? (
+                <h3 className={st.caption}>{headerCaption}</h3>
+            ) : null}
+            {title ? <h1 className={st.headerTitle}>{title}</h1> : null}
+            {subtitle ? <h1 className={st.subTitle}>{subtitle}</h1> : null}
+            {children}
+        </div>
+    );
 }
