1d58c5fd67a3de5a24b240e12e679576d7e13232
Brandon Williams <brandon@mystenlabs.com>
2022-10-09 16:41:17 -0700

    config: add external_address to p2p config

--- crates/sui-config/src/p2p.rs
+++ crates/sui-config/src/p2p.rs
@@ -3,13 +3,19 @@
 
 use std::net::SocketAddr;
 
+use multiaddr::Multiaddr;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct P2pConfig {
+    /// The address that the p2p network will bind on.
     #[serde(default = "default_listen_address")]
     pub listen_address: SocketAddr,
+    /// The external address other nodes can use to reach this node.
+    /// This will be shared with other peers through the discovery service
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub external_address: Option<Multiaddr>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub anemo_config: Option<anemo::Config>,
 }
@@ -22,6 +28,7 @@ impl Default for P2pConfig {
     fn default() -> Self {
         Self {
             listen_address: default_listen_address(),
+            external_address: Default::default(),
             anemo_config: Default::default(),
         }
     }
