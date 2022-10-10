b2133314fbe7a86d6204659609bfee3b4a664323
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-10-10 05:39:37 +0100

    wallet-ext: check keyring status to access pages
    * guard hook for locked status
    * update store for keyring status updates
    * send message now throws if payload is an error (to avoid checking it every time)

--- apps/wallet/src/ui/app/wallet/locked-page/index.tsx
+++ apps/wallet/src/ui/app/wallet/locked-page/index.tsx
@@ -13,7 +13,9 @@ import FieldLabel from '_app/shared/field-label';
 import PasswordInput from '_app/shared/input/password';
 import PageMainLayout from '_app/shared/page-main-layout';
 import { unlockWallet } from '_app/wallet/actions';
-import { useAppDispatch } from '_hooks';
+import { useLockedGuard } from '_app/wallet/hooks';
+import Loading from '_components/loading';
+import { useAppDispatch, useInitializedGuard } from '_hooks';
 import PageLayout from '_pages/layout';
 
 import st from './LockedPage.module.scss';
@@ -23,67 +25,76 @@ const validation = Yup.object({
 });
 
 export default function LockedPage() {
+    const initGuardLoading = useInitializedGuard(true);
+    const lockedGuardLoading = useLockedGuard(true);
+    const guardsLoading = initGuardLoading || lockedGuardLoading;
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const nextUrl = searchParams.get('url') || '/';
     return (
-        <PageLayout limitToPopUpSize={true}>
-            <PageMainLayout className={st.main}>
-                <CardLayout
-                    icon="sui"
-                    headerCaption="Hello There"
-                    title="Welcome Back"
-                    mode="plain"
-                >
-                    <Formik
-                        initialValues={{ password: '' }}
-                        validationSchema={validation}
-                        validateOnMount={true}
-                        onSubmit={async ({ password }, { setFieldError }) => {
-                            try {
-                                await dispatch(
-                                    unlockWallet({ password })
-                                ).unwrap();
-                                navigate(nextUrl);
-                            } catch (e) {
-                                setFieldError(
-                                    'password',
-                                    (e as Error).message || 'Incorrect password'
-                                );
-                            }
-                        }}
+        <Loading loading={guardsLoading}>
+            <PageLayout limitToPopUpSize={true}>
+                <PageMainLayout className={st.main}>
+                    <CardLayout
+                        icon="sui"
+                        headerCaption="Hello There"
+                        title="Welcome Back"
+                        mode="plain"
                     >
-                        {({ touched, errors, isSubmitting, isValid }) => (
-                            <Form className={st.form}>
-                                <FieldLabel txt="Enter Password">
-                                    <Field
-                                        name="password"
-                                        component={PasswordInput}
-                                        disabled={isSubmitting}
-                                    />
-                                    {touched.password && errors.password ? (
-                                        <Alert>{errors.password}</Alert>
-                                    ) : null}
-                                </FieldLabel>
-                                <div className={st.fill} />
-                                <Button
-                                    type="submit"
-                                    disabled={isSubmitting || !isValid}
-                                    mode="primary"
-                                    size="large"
-                                >
-                                    <Icon
-                                        icon={SuiIcons.CheckFill}
-                                        className={st.btnIcon}
-                                    />
-                                    Unlock
-                                </Button>
-                            </Form>
-                        )}
-                    </Formik>
-                </CardLayout>
-            </PageMainLayout>
-        </PageLayout>
+                        <Formik
+                            initialValues={{ password: '' }}
+                            validationSchema={validation}
+                            validateOnMount={true}
+                            onSubmit={async (
+                                { password },
+                                { setFieldError }
+                            ) => {
+                                try {
+                                    await dispatch(
+                                        unlockWallet({ password })
+                                    ).unwrap();
+                                    navigate(nextUrl);
+                                } catch (e) {
+                                    setFieldError(
+                                        'password',
+                                        (e as Error).message ||
+                                            'Incorrect password'
+                                    );
+                                }
+                            }}
+                        >
+                            {({ touched, errors, isSubmitting, isValid }) => (
+                                <Form className={st.form}>
+                                    <FieldLabel txt="Enter Password">
+                                        <Field
+                                            name="password"
+                                            component={PasswordInput}
+                                            disabled={isSubmitting}
+                                        />
+                                        {touched.password && errors.password ? (
+                                            <Alert>{errors.password}</Alert>
+                                        ) : null}
+                                    </FieldLabel>
+                                    <div className={st.fill} />
+                                    <Button
+                                        type="submit"
+                                        disabled={isSubmitting || !isValid}
+                                        mode="primary"
+                                        size="large"
+                                    >
+                                        <Icon
+                                            icon={SuiIcons.CheckFill}
+                                            className={st.btnIcon}
+                                        />
+                                        Unlock
+                                    </Button>
+                                </Form>
+                            )}
+                        </Formik>
+                    </CardLayout>
+                </PageMainLayout>
+            </PageLayout>
+        </Loading>
     );
 }
