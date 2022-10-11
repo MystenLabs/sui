01369cfd9cd2447962b16b69cbd9558a22b54769
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-10-11 13:17:19 +0100

    wallet-ext: clear storage when the extension gets updated
    * when the previous versions is less or equal to 0.1.1 clear extension storage
    * update package version

--- apps/wallet/src/background/index.ts
+++ apps/wallet/src/background/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { lte } from 'semver';
 import Browser from 'webextension-polyfill';
 
 import Alarms, { LOCK_ALARM_NAME } from './Alarms';
@@ -9,9 +10,18 @@ import Permissions from './Permissions';
 import { Connections } from './connections';
 import { openInNewTab } from '_shared/utils';
 
-Browser.runtime.onInstalled.addListener((details) => {
-    if (details.reason === 'install') {
+Browser.runtime.onInstalled.addListener(async ({ reason, previousVersion }) => {
+    if (reason === 'install') {
         openInNewTab();
+    } else if (
+        reason === 'update' &&
+        previousVersion &&
+        lte(previousVersion, '0.1.1')
+    ) {
+        // clear everything in the storage
+        // mainly done to clear the mnemonic that was stored
+        // as plain text
+        await Browser.storage.local.clear();
     }
 });
 
