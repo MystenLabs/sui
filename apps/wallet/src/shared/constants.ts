a33c85a70fa00300143bdf4d36984c02c117ddca
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-10-11 01:58:37 +0100

    wallet-ext: auto-lock timer setting

--- apps/wallet/src/shared/constants.ts
+++ apps/wallet/src/shared/constants.ts
@@ -3,3 +3,6 @@
 
 export const ToS_LINK = 'https://sui.io/terms';
 export const PRIVACY_POLICY_LINK = 'https://sui.io/policy/';
+
+export const AUTO_LOCK_TIMER_STORAGE_KEY = 'auto-lock-timer-interval';
+export const AUTO_LOCK_TIMER_DEFAULT_INTERVAL_MINUTES = 5;
