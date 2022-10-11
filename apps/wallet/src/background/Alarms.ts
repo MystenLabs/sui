6bd0add66a17769f793dfebfffc6a31ceb4db953
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-10-11 01:58:37 +0100

    wallet-ext: auto-lock timer setting

--- apps/wallet/src/background/Alarms.ts
+++ apps/wallet/src/background/Alarms.ts
@@ -4,6 +4,10 @@
 import Browser from 'webextension-polyfill';
 
 import Keyring from './Keyring';
+import {
+    AUTO_LOCK_TIMER_DEFAULT_INTERVAL_MINUTES,
+    AUTO_LOCK_TIMER_STORAGE_KEY,
+} from '_src/shared/constants';
 
 export const LOCK_ALARM_NAME = 'lock-keyring-alarm';
 
@@ -12,7 +16,12 @@ class Alarms {
         if (Keyring.isLocked) {
             return;
         }
-        const delayInMinutes = 5;
+        const delayInMinutes = (
+            await Browser.storage.local.get({
+                [AUTO_LOCK_TIMER_STORAGE_KEY]:
+                    AUTO_LOCK_TIMER_DEFAULT_INTERVAL_MINUTES,
+            })
+        )[AUTO_LOCK_TIMER_STORAGE_KEY];
         Browser.alarms.create(LOCK_ALARM_NAME, { delayInMinutes });
     }
 
