e6c043edc2744b3bcb8c59e22224a472554e88f9
Andrew Burnie <apburnie@hotmail.co.uk>
2022-10-17 18:28:08 +0100

    restricts .placeholder to just be background gradient

--- apps/explorer/src/ui/Placeholder.tsx
+++ apps/explorer/src/ui/Placeholder.tsx
@@ -1,7 +1,20 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { cva } from 'class-variance-authority';
+
 import styles from './utils/customStyles.module.css';
+
+const placeholder = cva([styles.placeholder], {
+    variants: {
+        variant: {
+            default: 'rounded-[3px] animate-shimmer',
+        },
+    },
+    defaultVariants: {
+        variant: 'default',
+    },
+});
 
 export function Placeholder({
     width,
@@ -12,7 +25,7 @@ export function Placeholder({
 }) {
     return (
         <div
-            className={styles.placeholder}
+            className={placeholder()}
             style={{
                 width,
                 height,
