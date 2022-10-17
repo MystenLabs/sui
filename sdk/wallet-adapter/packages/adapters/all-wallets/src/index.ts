d92e017d4327de69d2e95339e938c5af86d7d279
MacOMNI <414294494@qq.com>
2022-10-17 10:34:13 +0800

    #sdk update

--- sdk/wallet-adapter/packages/adapters/all-wallets/src/index.ts
+++ sdk/wallet-adapter/packages/adapters/all-wallets/src/index.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import exp from "constants";
+
 export * from "@mysten/wallet-adapter-sui-wallet";
 export * from "@mysten/wallet-adapter-mock-wallet";
 export * from "@mysten/wallet-adapter-wallet-standard";
+export * from "@mysten/wallet-adapter-hyperpay-wallet";
