f9d512084b0320ec466555ca922055ba9116d0e3
Anastasios Kichidis <akihidis@gmail.com>
2022-10-21 15:55:16 +0100

    address comment after review

--- narwhal/Docker/build.sh
+++ narwhal/Docker/build.sh
@@ -5,7 +5,7 @@
 # fast fail.
 set -e
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 DOCKERFILE="$DIR/Dockerfile"
 GIT_REVISION="$(git describe --always --dirty --exclude '*')"
