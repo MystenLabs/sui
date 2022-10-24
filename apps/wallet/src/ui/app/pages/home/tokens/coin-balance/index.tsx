6adc9bd79825431c047218d9f242b548dcc9690e
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-10-24 22:44:30 +0100

    wallet-ext: update coin items in tokens view
    * show a hardcoded icon for sui
    * show a generic icon for all other coins

--- apps/wallet/src/ui/app/pages/home/tokens/coin-balance/index.tsx
+++ apps/wallet/src/ui/app/pages/home/tokens/coin-balance/index.tsx
@@ -4,7 +4,9 @@
 import cl from 'classnames';
 import { memo } from 'react';
 
-import { useMiddleEllipsis, useFormatCoin } from '_hooks';
+import Icon, { SuiIcons } from '_components/icon';
+import { useFormatCoin } from '_hooks';
+import { GAS_TYPE_ARG } from '_redux/slices/sui-objects/Coin';
 
 import st from './CoinBalance.module.scss';
 
@@ -17,20 +19,28 @@ export type CoinProps = {
 
 function CoinBalance({ type, balance, mode = 'row-item' }: CoinProps) {
     const [formatted, symbol] = useFormatCoin(balance, type);
-
-    const shortenType = useMiddleEllipsis(type, 30);
+    const icon = type === GAS_TYPE_ARG ? SuiIcons.SuiLogoIcon : SuiIcons.Tokens;
     return (
         <div className={cl(st.container, st[mode])}>
-            <div className={cl(st.valuesContainer, st[mode])}>
+            {mode === 'row-item' ? (
+                <>
+                    <Icon
+                        icon={icon}
+                        className={cl(st.coinIcon, {
+                            [st.sui]: type === GAS_TYPE_ARG,
+                        })}
+                    />
+                    <div className={cl(st.coinNameContainer, st[mode])}>
+                        <span className={st.coinName}>
+                            {symbol.toLocaleLowerCase()}
+                        </span>
+                        <span className={st.coinSymbol}>{symbol}</span>
+                    </div>
+                </>
+            ) : null}
+            <div className={cl(st.valueContainer, st[mode])}>
                 <span className={cl(st.value, st[mode])}>{formatted}</span>
                 <span className={cl(st.symbol, st[mode])}>{symbol}</span>
-            </div>
-            <div className={cl(st.typeActionsContainer, st[mode])}>
-                {mode === 'row-item' ? (
-                    <span className={st.type} title={type}>
-                        {shortenType}
-                    </span>
-                ) : null}
             </div>
         </div>
     );
