dd8d5802564ef9f784cd3c1309a9a3dc7a1898fd
Andrew Burnie <apburnie@hotmail.co.uk>
2022-10-26 14:31:19 +0100

    adds LShape SVG and splits out logic

--- apps/explorer/src/pages/transaction-result/SendReceiveView.tsx
+++ apps/explorer/src/pages/transaction-result/SendReceiveView.tsx
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import cl from 'clsx';
 
+import { ReactComponent as LShapeIcon} from '../../assets/LShape.svg';
 import { ReactComponent as DoneIcon } from '../../assets/SVGIcons/Done.svg';
 import { ReactComponent as StartIcon } from '../../assets/SVGIcons/Start.svg';
 import Longtext from '../../components/longtext/Longtext';
@@ -14,15 +15,38 @@ type TxAddress = {
 };
 //TODO: Add date format function
 function SendRecieveView({ data }: { data: TxAddress }) {
+    if (data?.recipient && data.recipient.length === 1) {
+        return (
+            <div className={styles.txaddress} data-testid="transaction-sender">
+                <h4 className={styles.oneheading}>Sender &#x26; Recipient</h4>
+                <div className={styles.oneaddress}>
+                    <StartIcon />
+                    <Longtext
+                        text={data.sender}
+                        category="addresses"
+                        isLink={true}
+                    />
+                </div>
+                <div>
+                    {data.recipient.map((add: string, idx: number) => (
+                        <div className={styles.oneaddress} key={idx}>
+                            <DoneIcon />
+                            <Longtext
+                                text={add}
+                                category="addresses"
+                                isLink={true}
+                                alttext={add}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
     return (
         <div className={styles.txaddress} data-testid="transaction-sender">
             <div className={styles.senderbox}>
-                <div>
-                    <h4>
-                        Sender{' '}
-                        {data.recipient?.length === 1 ? '& Recipient' : ''}{' '}
-                    </h4>
-                </div>
+                <h4>Sender</h4>
                 <div className={styles.oneaddress}>
                     <StartIcon />
                     <Longtext
@@ -36,18 +60,13 @@ function SendRecieveView({ data }: { data: TxAddress }) {
                 className={cl([
                     styles.txaddresssender,
                     data.recipient?.length ? styles.recipient : '',
-                    data.recipient?.length === 1
-                        ? styles.txaddresssenderonerecipent
-                        : '',
                 ])}
             >
                 {data.recipient && (
                     <div className={styles.recipientbox}>
-                        {data.recipient.length > 1 && (
-                            <div>
-                                <h4>Recipients</h4>
-                            </div>
-                        )}
+                        <div>
+                            <h4>Recipients</h4>
+                        </div>
                         {data.recipient.map((add: string, idx: number) => (
                             <div className={styles.oneaddress} key={idx}>
                                 <DoneIcon />
