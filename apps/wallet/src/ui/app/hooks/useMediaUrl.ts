cd09e9e081b245fe3e5cb379707ecbde5b84dda7
Origin Byte Developer <devs@originbyte.io>
2022-10-26 10:39:34 +0300

    [wallet] Add Origin Byte NFTs support

--- apps/wallet/src/ui/app/hooks/useMediaUrl.ts
+++ apps/wallet/src/ui/app/hooks/useMediaUrl.ts
@@ -6,13 +6,16 @@ import { useMemo } from 'react';
 
 import type { SuiData } from '@mysten/sui.js';
 
+export const prepareImageUrl = (mediaUrl: string) =>
+    mediaUrl.replace(/^ipfs:\/\//, 'https://ipfs.io/ipfs/');
+
 export default function useMediaUrl(objData: SuiData) {
     const { fields } = (isSuiMoveObject(objData) && objData) || {};
     return useMemo(() => {
         if (fields) {
-            const mediaUrl = fields.url || fields.metadata?.fields.url;
+            const mediaUrl = fields.url;
             if (typeof mediaUrl === 'string') {
-                return mediaUrl.replace(/^ipfs:\/\//, 'https://ipfs.io/ipfs/');
+                return prepareImageUrl(mediaUrl);
             }
         }
         return null;
--- apps/wallet/src/ui/app/hooks/useNFTBasicData.ts
+++ apps/wallet/src/ui/app/hooks/useNFTBasicData.ts
@@ -2,18 +2,25 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { isSuiMoveObject, getObjectId, getObjectFields } from '@mysten/sui.js';
+import { isArtNft, parseNftdata } from '@originbyte/js-sdk';
 
 import useFileExtentionType from './useFileExtentionType';
-import useMediaUrl from './useMediaUrl';
+import useMediaUrl, { prepareImageUrl } from './useMediaUrl';
 
 import type { SuiObject } from '@mysten/sui.js';
 
 export default function useNFTBasicData(nftObj: SuiObject) {
     const nftObjectID = getObjectId(nftObj.reference);
-    const filePath = useMediaUrl(nftObj.data);
+    let filePath = useMediaUrl(nftObj.data);
     let objType = null;
     let nftFields = null;
-    if (isSuiMoveObject(nftObj.data)) {
+    if (isArtNft(nftObj)) {
+        objType = nftObj.data.type;
+        console.log('nftObj.data', nftObj.data);
+        nftFields = parseNftdata(nftObj.data.fields);
+        filePath = prepareImageUrl(nftFields.url);
+        console.log('result:', nftFields, nftFields, filePath);
+    } else if (isSuiMoveObject(nftObj.data)) {
         objType = nftObj.data.type;
         nftFields = getObjectFields(nftObj.data);
     }
