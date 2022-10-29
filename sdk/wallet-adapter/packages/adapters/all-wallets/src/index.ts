3ead1eefb5e61f592a64a973df202f2ea9a74023
Jordan Gensler <jordan@mystenlabs.com>
2022-10-29 16:00:42 -0700

    [wallet-adapter] Remove legacy sui wallet adapter (#5671)

--- sdk/wallet-adapter/packages/adapters/all-wallets/src/index.ts
+++ sdk/wallet-adapter/packages/adapters/all-wallets/src/index.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export * from "@mysten/wallet-adapter-sui-wallet";
 export * from "@mysten/wallet-adapter-mock-wallet";
 export * from "@mysten/wallet-adapter-wallet-standard";
