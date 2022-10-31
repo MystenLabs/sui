b0e648c405b08a94e34233e8c0ea21a595b726c0
Jordan Gensler <jordan@mystenlabs.com>
2022-10-31 16:01:10 +0000

    Fix build

--- sdk/typescript/src/utils/ed25519-hd-key.ts
+++ sdk/typescript/src/utils/ed25519-hd-key.ts
@@ -7,7 +7,7 @@
 import { sha512 } from '@noble/hashes/sha512';
 import { hmac } from '@noble/hashes/hmac';
 import nacl from 'tweetnacl';
-import { fromHEX, toB64 } from '@mysten/bcs';
+import { fromHEX } from '@mysten/bcs';
 
 type Hex = string;
 type Path = string;
