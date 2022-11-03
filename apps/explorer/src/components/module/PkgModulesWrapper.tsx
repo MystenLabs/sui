24367f2b4b2f3f467ac5739aeea8e766987362e2
Andrew Burnie <apburnie@hotmail.co.uk>
2022-11-03 14:29:32 +0000

    reduces breakpoint

--- apps/explorer/src/components/module/PkgModulesWrapper.tsx
+++ apps/explorer/src/components/module/PkgModulesWrapper.tsx
@@ -47,9 +47,9 @@ function PkgModuleViewWrapper({ id, modules }: Props) {
 
     const submitSearch = useCallback(() => {
         setSelectedModule((prev: string) =>
-            (filteredModules.length === 1) ? filteredModules[0] : prev 
+            filteredModules.length === 1 ? filteredModules[0] : prev
         );
-    }, [filteredModules ]);
+    }, [filteredModules]);
 
     return (
         <div
@@ -57,11 +57,8 @@ function PkgModuleViewWrapper({ id, modules }: Props) {
                 'flex flex-wrap border-0 border-y border-solid border-sui-grey-45'
             }
         >
-            <div className={'w-full lg:w-[15vw] pr-[20px]'}>
-                <Combobox
-                    value={selectedModule}
-                    onChange={setSelectedModule}
-                >
+            <div className={'w-full md:w-[15vw] pr-[20px]'}>
+                <Combobox value={selectedModule} onChange={setSelectedModule}>
                     <div className="box-border border border-sui-grey-50 border-solid rounded-[6px] h-[34px] shadow-sm placeholder-sui-grey-65 pl-3 w-full flex my-[10px] justify-between  ml-[2px]">
                         <Combobox.Input
                             onChange={(event) => setQuery(event.target.value)}
@@ -79,7 +76,7 @@ function PkgModuleViewWrapper({ id, modules }: Props) {
                     <Combobox.Options
                         static
                         as="div"
-                        className="w-full h-[209px] lg:h-[571px] overflow-auto mr-[-20px] pr-[20px]"
+                        className="w-full h-[209px] md:h-[571px] overflow-auto mr-[-20px] pr-[20px]"
                     >
                         <VerticalList>
                             {filteredModules.map((name, idx) => (
@@ -105,14 +102,14 @@ function PkgModuleViewWrapper({ id, modules }: Props) {
                     </Combobox.Options>
                 </Combobox>
             </div>
-            <div className="border-0 lg:border-l border-solid border-sui-grey-45 lg:pl-[30px] pt-[20px]">
+            <div className="border-0 md:border-l border-solid border-sui-grey-45 md:pl-[30px] pt-[20px]">
                 <TabGroup size="md">
                     <TabList>
                         <Tab>Bytecode</Tab>
                     </TabList>
                     <TabPanels>
                         <TabPanel>
-                            <div className="overflow-auto h-[555px] w-[87vw] lg:w-[75vw]">
+                            <div className="overflow-auto h-[555px] w-[87vw] md:w-[70vw] lg:w-[75vw]">
                                 {modules
                                     .filter(([name]) => name === selectedModule)
                                     .map(([name, code], idx) => (
