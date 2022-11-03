2b208b30a7874267685635745bbb7b6148289175
Andrey Chursin <andrey@mystenlabs.com>
2022-11-03 09:19:28 -0700

    [RFC] disable gossip on validators (#5795)
    As part of new [checkpoint](#5763) narwhal becomes a new data dissemination mechanism for all certificates between validators, and therefor we can disable gossip.
    
    This change slightly increase data dissemination latency, since currently certificates are disseminated on narwhal commit.
    
    In the future when we have payload verification we can also take certificates directly from narwhal worker without waiting for it to make into commit sequence.

--- crates/sui-node/src/lib.rs
+++ crates/sui-node/src/lib.rs
@@ -256,10 +256,6 @@ impl SuiNode {
             }
             active_authority.clone().spawn_node_sync_process().await;
             None
-        } else if config.enable_gossip {
-            // TODO: get degree from config file.
-            let degree = 4;
-            Some(active_authority.clone().spawn_gossip_process(degree).await)
         } else {
             None
         };
