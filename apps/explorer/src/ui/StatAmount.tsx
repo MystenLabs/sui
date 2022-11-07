83f4870bcb257515fc3a7927797154b3c0399d58
Jibz <godwinjibs@gmail.com>
2022-11-07 18:46:40 -0500

    update

--- apps/explorer/src/ui/StatAmount.tsx
+++ apps/explorer/src/ui/StatAmount.tsx
@@ -1,45 +1,36 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { SUI_TYPE_ARG, Coin } from '@mysten/sui.js';
-
-import { numberSuffix } from '../utils/numberUtil';
-
-import { CoinFormat, formatBalance } from '~/hooks/useFormatCoin';
 import { Heading } from '~/ui/Heading';
 import { Text } from '~/ui/Text';
+import { formatDate } from '~/utils/timeUtils';
 
 export interface StatAmountProps {
-    amount: bigint | string | number;
+    amount: number | string;
     currency?: string;
-    dollarAmount?: number;
-    date?: string;
-    full?: boolean;
+    dollarAmount?: number | string;
+    date?: Date | number;
 }
-
-const SUI_DECIMALS = 9;
 
 export function StatAmount({
     amount,
-    currency = SUI_TYPE_ARG,
+    currency = 'SUI',
     dollarAmount,
     date,
-    full,
 }: StatAmountProps) {
-    const formattedAmount = formatBalance(
-        amount,
-        SUI_DECIMALS,
-        full ? CoinFormat.FULL : CoinFormat.ROUNDED
-    );
-    const coinSymbol =
-        currency === SUI_TYPE_ARG ? Coin.getCoinSymbol(SUI_TYPE_ARG) : currency;
     return (
         <div className="flex flex-col justify-start h-full text-sui-grey-75 gap-2">
             <div className="text-sui-grey-100 flex flex-col items-baseline gap-2.5">
                 {date && (
                     <div className="text-sui-grey-75">
                         <Text variant="bodySmall" weight="semibold">
-                            {date}
+                            {formatDate(date, [
+                                'month',
+                                'day',
+                                'year',
+                                'hour',
+                                'minute',
+                            ])}
                         </Text>
                     </div>
                 )}
@@ -49,16 +40,16 @@ export function StatAmount({
 
                 <div className="flex flex-row items-start gap-1">
                     <Heading as="h2" variant="heading2" weight="bold">
-                        {formattedAmount}
+                        {amount}
                     </Heading>
                     <div className="text-sui-grey-80 ">
-                        <Text variant="bodySmall">{coinSymbol}</Text>
+                        <Text variant="bodySmall">{currency}</Text>
                     </div>
                 </div>
             </div>
             {dollarAmount && (
                 <Text variant="bodySmall" weight="semibold">
-                    ${numberSuffix(dollarAmount)}
+                    ${dollarAmount}
                 </Text>
             )}
         </div>
--- apps/explorer/src/ui/stories/StatAmount.stories.tsx
+++ apps/explorer/src/ui/stories/StatAmount.stories.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { SUI_TYPE_ARG } from '@mysten/sui.js';
 import { type Meta, type StoryObj } from '@storybook/react';
 
 import { StatAmount, type StatAmountProps } from '../StatAmount';
@@ -11,10 +10,10 @@ export default {
 } as Meta;
 
 const data = {
-    amount: 9007199254740991n,
-    currency: SUI_TYPE_ARG,
+    amount: 9740991,
+    currency: 'SUI',
     dollarAmount: 123.56,
-    date: 'June 24, 2012, 2:34 PM',
+    date: Date.now(),
 };
 
 export const Default: StoryObj<StatAmountProps> = {
