4e964605b456a2c003094b1925e8a07188898e19
Jibz <godwinjibs@gmail.com>
2022-11-07 17:58:02 -0500

    explorer Sender Recipient card

---
+++ apps/explorer/src/ui/SenderRecipient.tsx
@@ -0,0 +1,160 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { cva } from 'class-variance-authority';
+
+//TODO: Switch to using ButtonOrLink component after support for sui explorer links
+import Longtext from '~/components/longtext/Longtext';
+import { Heading } from '~/ui/Heading';
+import { Text } from '~/ui/Text';
+
+const isCoinTransfer = cva(
+    ['flex flex-col ml-6 gap-[15px] justify-center  relative'],
+    {
+        variants: {
+            senderRecipient: {
+                true: 'before:content-[url()] before:border-2 before:border-[#a0b6c3] before:overflow-y-hidden before:absolute before:h-[calc(55%)] before:w-[15px] before:border-r-[transparent] before:border-t-[transparent] before:mt-1 before:ml-[-16px] before:rounded-l before:border-dotted',
+                false: '',
+            },
+        },
+    }
+);
+
+const senderRecipientAddressStyles = cva(
+    [
+        'break-all before:ml-[-23px] before:absolute relative before:top-[50%] before:translate-y-[-50%]',
+    ],
+    {
+        variants: {
+            isSender: {
+                true: 'before:content-[url(~/assets/SVGIcons/Start.svg)] before:mt-[2px]',
+                false: 'before:content-[url(~/assets/SVGIcons/CheckFill.svg)] before:mt-[3px]',
+            },
+            isCoinTransfer: {
+                true: 'ml-[20px]',
+            },
+        },
+
+        defaultVariants: {
+            isSender: false,
+        },
+    }
+);
+
+function SenderRecipientAddress({
+    isSender,
+    address,
+    isCoin,
+}: {
+    isSender?: boolean;
+    address: string;
+    isCoin?: boolean;
+}) {
+    const isCoinTransfer = isCoin && !isSender ? true : false;
+    return (
+        <div
+            className={senderRecipientAddressStyles({
+                isSender,
+                isCoinTransfer,
+            })}
+        >
+            <Longtext
+                text={address}
+                category="addresses"
+                isLink
+                alttext={address}
+            />
+        </div>
+    );
+}
+
+function Amount({ amount, symbol }: { amount: number; symbol?: string }) {
+    return (
+        <div className="flex flex-row items-end gap-1 text-sui-grey-100">
+            <Heading as="h6" variant="heading6">
+                {amount}
+            </Heading>
+            {symbol && (
+                <div className="text-sui-grey-80 ">
+                    <Text variant="bodySmall">{symbol}</Text>
+                </div>
+            )}
+        </div>
+    );
+}
+
+type Recipient = {
+    address: string;
+    amount?: {
+        value: number;
+        unit?: string;
+    };
+};
+
+export interface SenderRecipientProps {
+    sender: string;
+    transferCoin?: boolean;
+    recipients?: Recipient[];
+}
+
+export function SenderRecipient({
+    sender,
+    recipients,
+    transferCoin = true,
+}: SenderRecipientProps) {
+    const multipleRecipients =
+        recipients?.length && recipients?.length > 1 ? true : false;
+    const senderRecipient = !multipleRecipients && transferCoin ? true : false;
+    const primaryRecipient = senderRecipient && recipients?.[0];
+    const multipleRecipientsList = primaryRecipient
+        ? recipients?.filter((_, i) => i !== 0)
+        : recipients;
+
+    return (
+        <div className="flex flex-col justify-start h-full text-sui-grey-75 gap-2">
+            <Heading as="h4" variant="heading4" weight="semibold">
+                {' '}
+                Sender {senderRecipient && '& Recipient'}{' '}
+            </Heading>
+            <div className={isCoinTransfer({ senderRecipient })}>
+                <SenderRecipientAddress isSender address={sender} />
+                {primaryRecipient && (
+                    <SenderRecipientAddress
+                        address={primaryRecipient.address}
+                        isCoin
+                    />
+                )}
+                {multipleRecipientsList?.length ? (
+                    <div className="mt-2 flex flex-col gap-2">
+                        <div className="ml-[-24px] mt-[10px] mb-[10px]">
+                            <Heading
+                                as="h4"
+                                variant="heading4"
+                                weight="semibold"
+                            >
+                                Recipient
+                                {multipleRecipientsList.length > 1 && 's'}{' '}
+                            </Heading>
+                        </div>
+                        {multipleRecipientsList?.map((recipient, index) => (
+                            <div
+                                className="flex flex-col gap-2.5 mb-2"
+                                key={index}
+                            >
+                                <SenderRecipientAddress
+                                    address={recipient?.address}
+                                />
+                                {recipient?.amount && (
+                                    <Amount
+                                        amount={recipient.amount?.value}
+                                        symbol={recipient.amount?.unit}
+                                    />
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                ) : null}
+            </div>
+        </div>
+    );
+}
---
+++ apps/explorer/src/ui/stories/SenderRecipient.stories.tsx
@@ -0,0 +1,80 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { type Meta, type StoryObj } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { SenderRecipient, type SenderRecipientProps } from '../SenderRecipient';
+
+const data = {
+    transferCoin: false,
+    sender: '0x813f1adee5abb1e00dfa653bb827856106e56764',
+    recipients: [
+        {
+            address: '0x955d8ddc4a17670bda6b949cbdbc8f5aac820cc7',
+            amount: {
+                value: 1000,
+                unit: 'SUI',
+            },
+        },
+    ],
+};
+
+export default {
+    component: SenderRecipient,
+    decorators: [
+        (Story) => (
+            <MemoryRouter>
+                <Story />
+            </MemoryRouter>
+        ),
+    ],
+
+    args: {
+        ...data,
+        transferCoin: true,
+    },
+} as Meta;
+
+export const Default: StoryObj<SenderRecipientProps> = {
+    render: (props) => <SenderRecipient {...props} />,
+};
+
+export const noRecipient: StoryObj<SenderRecipientProps> = {
+    args: {
+        sender: '0x813f1adee5abb1e00dfa653bb827856106e56764',
+        transferCoin: false,
+        recipients: [],
+    },
+};
+
+export const nonCoin: StoryObj<SenderRecipientProps> = {
+    ...Default,
+    args: {
+        ...data,
+        transferCoin: false,
+        recipients: [
+            {
+                address: '0x955d8ddc4a17670bda6b949cbdbc8f5aac820cc7',
+                amount: {
+                    value: 400,
+                    unit: 'SUI',
+                },
+            },
+            {
+                address: '0x9798852b55fcbf352052c9414920ebf7811ce05e',
+                amount: {
+                    value: 1850,
+                    unit: 'COIN',
+                },
+            },
+            {
+                address: '0x813f1adee5abb1e00dfa653bb827856106e56764',
+                amount: {
+                    value: 950,
+                    unit: 'SUI',
+                },
+            },
+        ],
+    },
+};
