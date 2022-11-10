b043306ac879fbd791f77df41bf39ca15732b861
Jibz <godwinjibs@gmail.com>
2022-11-10 11:33:43 -0500

    lint

--- apps/explorer/src/components/tx-time/TxTimeType.tsx
+++ apps/explorer/src/components/tx-time/TxTimeType.tsx
@@ -12,9 +12,7 @@ export function TxTimeType({ timestamp }: Prop) {
 
     return (
         <section>
-            <div
-                className="text-caption w-20"
-            >{`${timeAgo}`}</div>
+            <div className="text-caption w-20">{`${timeAgo}`}</div>
         </section>
     );
 }
