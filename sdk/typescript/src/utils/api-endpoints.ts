75cc2f240d7842fa18e892265a7d166920f171e3
Lutwidse <lutwidse@m0n4.com>
2022-11-14 00:20:49 +0900

    [ts-sdk] add testnet endpoint to api-endpoints.ts

--- sdk/typescript/src/utils/api-endpoints.ts
+++ sdk/typescript/src/utils/api-endpoints.ts
@@ -4,6 +4,7 @@
 export enum Network {
   LOCAL = 'LOCAL',
   DEVNET = 'DEVNET',
+  TESTNET = 'TESTNET',
 }
 
 export type ApiEndpoints = {
@@ -20,4 +21,8 @@ export const NETWORK_TO_API: Record<Network, ApiEndpoints> = {
     fullNode: 'https://fullnode.devnet.sui.io/',
     faucet: 'https://faucet.devnet.sui.io/gas',
   },
+  [Network.TESTNET]: {
+    fullNode: 'https://fullnode.testnet.sui.io/',
+    faucet: 'https://faucet.testnet.sui.io/gas'
+  }
 };
