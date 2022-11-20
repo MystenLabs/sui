5226f7c2e99af19449593300cca83717ec4d0775
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-11-20 13:31:26 +0000

    wallet-ext: collapse component
    * also adds base tailwind rules to fix rotate transform

--- apps/core/tailwind.config.js
+++ apps/core/tailwind.config.js
@@ -131,6 +131,9 @@ module.exports = {
       height: {
         header: "68px",
       },
+      transitionTimingFunction: {
+        'ease-in-out-cubic': 'cubic-bezier(0.65, 0, 0.35, 1)'
+      }
     },
   },
   corePlugins: {
