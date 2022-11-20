4e0ea099249c4d096447c3a01aa9382b2d83ecdf
Jordan Gensler <jordan@mystenlabs.com>
2022-11-20 15:29:45 -0500

    Better component composition

--- apps/explorer/src/ui/stories/StatAmount.stories.tsx
+++ apps/explorer/src/ui/stories/StatAmount.stories.tsx
@@ -2,25 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { type Meta, type StoryObj } from '@storybook/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { StatAmount, type StatAmountProps } from '../StatAmount';
 
 export default {
     component: StatAmount,
-    decorators: [
-        (Story) => (
-            <QueryClientProvider client={new QueryClient()}>
-                <Story />
-            </QueryClientProvider>
-        ),
-    ],
 } as Meta;
 
 export const defaultAmount: StoryObj<StatAmountProps> = {
     args: {
         amount: 9740991,
-        currency: 'SUI',
+        symbol: 'SUI',
         dollarAmount: 123.56,
         date: 1667942429177,
     },
