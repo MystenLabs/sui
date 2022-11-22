c202b2dcd0cb05a1ede4868a3883f9497465fabf
Jibz <godwinjibs@gmail.com>
2022-11-22 09:36:17 -0500

    update

--- apps/explorer/src/ui/DescriptionList.tsx
+++ apps/explorer/src/ui/DescriptionList.tsx
@@ -7,36 +7,26 @@ import type { ReactNode } from 'react';
 
 import { Text } from '~/ui/Text';
 
-
-
-
-
 export type LabelProps = ExtractProps<typeof Text>;
 
 export function Label(props: LabelProps) {
-  return (
-    <dt>
-        <Text {...props}>
-            {props.children}
-        </Text>
-    </dt>
-  );
+    return (
+        <dt className="col-span-1">
+            <Text {...props}>{props.children}</Text>
+        </dt>
+    );
 }
 
 export function Value({ children }: { children: ReactNode }) {
-    return (
-      <dd>
-          {children}
-      </dd>
-    );
-  }
+    return <dd className="ml-0 col-span-2">{children}</dd>;
+}
 
-export type  DescriptionListProps = ExtractProps<typeof Label>;
+export type DescriptionListProps = {
+    children: ReactNode[];
+};
 
-export function DescriptionList(children : ReactNode) {
+export function DescriptionList({ children }: DescriptionListProps) {
     return (
-        <dl>
-            {children}
-        </dl>
+        <dl className="grid grid-cols-1 md:grid-cols-3 gap-2">{children}</dl>
     );
 }
