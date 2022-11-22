9f1753eb3ef14deffdb7fc4b845de7b72f02e4fd
sadhan <sadhan@mystenlabs.com>
2022-11-21 18:39:41 -0800

    Add sally api and reference implementation

--- crates/typed-store-derive/src/lib.rs
+++ crates/typed-store-derive/src/lib.rs
@@ -288,10 +288,13 @@ pub fn derive_dbmap_utils_general(input: TokenStream) -> TokenStream {
     let generics = &input.generics;
     let generics_names = extract_generics_names(generics);
 
-    let allowed_types_with_post_process_fn: BTreeMap<_, _> =
-        [("DBMap", ""), ("Store", "typed_store::Store::new")]
-            .into_iter()
-            .collect();
+    let allowed_types_with_post_process_fn: BTreeMap<_, _> = [
+        ("SallyColumn", ""),
+        ("DBMap", ""),
+        ("Store", "typed_store::Store::new"),
+    ]
+    .into_iter()
+    .collect();
     let allowed_strs = allowed_types_with_post_process_fn
         .keys()
         .map(|s| s.to_string())
@@ -629,6 +632,372 @@ pub fn derive_dbmap_utils_general(input: TokenStream) -> TokenStream {
                     self.table_summary(table_name.as_str())
                 }
 
+
+        }
+
+    })
+}
+
+#[proc_macro_derive(SallyDB, attributes(default_options_override_fn))]
+pub fn derive_sallydb_general(input: TokenStream) -> TokenStream {
+    //log_syntax!("here");
+    let input = parse_macro_input!(input as ItemStruct);
+    let name = &input.ident;
+    let generics = &input.generics;
+    let generics_names = extract_generics_names(generics);
+
+    let allowed_types_with_post_process_fn: BTreeMap<_, _> =
+        [("SallyColumn", "")].into_iter().collect();
+    let allowed_strs = allowed_types_with_post_process_fn
+        .keys()
+        .map(|s| s.to_string())
+        .collect();
+
+    // TODO: use `parse_quote` over `parse()`
+    // TODO: Eventually this should return a Vec<Vec<GeneralTableOptions>> to capture default table options for each column type i.e. RockDB, TestDB, etc
+    let (field_names, inner_types, derived_table_options, simple_field_type_name_str) =
+        extract_struct_info(input.clone(), allowed_strs);
+
+    let (key_names, value_names): (Vec<_>, Vec<_>) = inner_types
+        .iter()
+        .map(|q| (q.args.first().unwrap(), q.args.last().unwrap()))
+        .unzip();
+
+    // This is the actual name of the type which was found
+    let post_process_fn_str = allowed_types_with_post_process_fn
+        .get(&simple_field_type_name_str.as_str())
+        .unwrap();
+    let post_process_fn: proc_macro2::TokenStream = post_process_fn_str.parse().unwrap();
+
+    let default_options_override_fn_names: Vec<proc_macro2::TokenStream> = derived_table_options
+        .iter()
+        .map(|q| {
+            let GeneralTableOptions::OverrideFunction(fn_name) = q;
+            fn_name.parse().unwrap()
+        })
+        .collect();
+
+    let generics_bounds =
+        "std::fmt::Debug + serde::Serialize + for<'de> serde::de::Deserialize<'de>";
+    let generics_bounds_token: proc_macro2::TokenStream = generics_bounds.parse().unwrap();
+
+    let config_struct_name_str = format!("{}SallyConfigurator", name);
+    let sally_config_struct_name: proc_macro2::TokenStream =
+        config_struct_name_str.parse().unwrap();
+
+    let intermediate_db_map_struct_name_str = format!("{}Primary", name);
+    let intermediate_db_map_struct_name: proc_macro2::TokenStream =
+        intermediate_db_map_struct_name_str.parse().unwrap();
+
+    let secondary_db_map_struct_name_str = format!("{}ReadOnly", name);
+    let secondary_db_map_struct_name: proc_macro2::TokenStream =
+        secondary_db_map_struct_name_str.parse().unwrap();
+
+    TokenStream::from(quote! {
+
+        // <----------- This section generates the configurator struct -------------->
+
+        /// Create config structs for configuring SallyColumns
+        pub struct #sally_config_struct_name {
+            #(
+                pub #field_names : typed_store::sally::SallyColumnOptions,
+            )*
+        }
+
+        impl #sally_config_struct_name {
+            /// Initialize to defaults
+            pub fn init() -> Self {
+                Self {
+                    #(
+                        #field_names : typed_store::sally::default_column_options(),
+                    )*
+                }
+            }
+
+            /// Build a config
+            pub fn build(&self) -> typed_store::sally::SallyDBConfigMap {
+                typed_store::sally::SallyDBConfigMap::new([
+                    #(
+                        (stringify!(#field_names).to_owned(), self.#field_names.clone()),
+                    )*
+                ].into_iter().collect())
+            }
+        }
+
+
+        impl <
+                #(
+                    #generics_names: #generics_bounds_token,
+                )*
+            > #name #generics {
+
+                pub fn configurator() -> #sally_config_struct_name {
+                    #sally_config_struct_name::init()
+                }
+        }
+
+
+        // <----------- This section generates the core open logic for opening sally columns -------------->
+
+        /// Create an intermediate struct used to open the DBMap tables in primary mode
+        /// This is only used internally
+        struct #intermediate_db_map_struct_name #generics {
+                #(
+                    pub #field_names : SallyColumn #inner_types,
+                )*
+        }
+
+
+        impl <
+                #(
+                    #generics_names: #generics_bounds_token,
+                )*
+            > #intermediate_db_map_struct_name #generics {
+            /// Opens a set of tables in read-write mode
+            /// If as_secondary_with_path is set, the DB is opened in read only mode with the path specified
+            pub fn init(db_options: typed_store::sally::SallyDBOptions) -> Self {
+                match db_options {
+                    typed_store::sally::SallyDBOptions::TestDB => {
+                        let (
+                            #(
+                                #field_names
+                            ),*
+                        ) = (#(
+                            SallyColumn::TestDB((typed_store::test_db::TestDB::#inner_types::open(), typed_store::sally::SallyConfig::default()))
+                            ),*);
+
+                        Self {
+                            #(
+                                #field_names,
+                            )*
+                        }
+                    },
+                    typed_store::sally::SallyDBOptions::RocksDB((path, access_type, global_db_options_override, tables_db_options_override)) => {
+                        let path = &path;
+                        let db = {
+                            let opt_cfs = match tables_db_options_override {
+                                None => [
+                                    #(
+                                        (stringify!(#field_names).to_owned(), #default_options_override_fn_names().clone()),
+                                    )*
+                                ],
+                                Some(o) => [
+                                    #(
+                                        (stringify!(#field_names).to_owned(), o.to_map().get(stringify!(#field_names)).unwrap().clone()),
+                                    )*
+                                ]
+                            };
+                            // Safe to call unwrap because we will have atleast one field_name entry in the struct
+                            let opt_cfs: Vec<_> = opt_cfs.iter().map(|q| (q.0.as_str(), &q.1.options)).collect();
+                            let db = match access_type {
+                                RocksDBAccessType::Secondary(Some(p)) => typed_store::rocks::open_cf_opts_secondary(path, Some(&p), global_db_options_override, &opt_cfs),
+                                _ => typed_store::rocks::open_cf_opts(path, global_db_options_override, &opt_cfs)
+                            };
+                            db
+                        }.expect("Cannot open DB.");
+                        let (
+                            #(
+                                #field_names
+                            ),*
+                        ) = (#(
+                            SallyColumn::RocksDB((DBMap::#inner_types::reopen(&db, Some(stringify!(#field_names))).expect(&format!("Cannot open {} CF.", stringify!(#field_names))[..]), typed_store::sally::SallyConfig::default()))
+                            ),*);
+
+                        Self {
+                            #(
+                                #field_names,
+                            )*
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // <----------- This section generates the read-write open logic and other common utils -------------->
+        impl <
+                #(
+                    #generics_names: #generics_bounds_token,
+                )*
+            > #name #generics {
+            /// Opens a set of tables in read-write mode
+            /// Only one process is allowed to do this at a time
+            /// `global_db_options_override` apply to the whole DB
+            /// `tables_db_options_override` apply to each table. If `None`, the attributes from `default_options_override_fn` are used if any
+            #[allow(unused_parens)]
+            pub fn init(
+                db_options: typed_store::sally::SallyDBOptions
+            ) -> Self {
+                let inner = #intermediate_db_map_struct_name::init(db_options);
+                Self {
+                    #(
+                        #field_names: #post_process_fn(inner.#field_names),
+                    )*
+                }
+            }
+
+            /// Returns a list of the tables name and type pairs
+            pub fn describe_tables() -> std::collections::BTreeMap<String, (String, String)> {
+                vec![#(
+                    (stringify!(#field_names).to_owned(), (stringify!(#key_names).to_owned(), stringify!(#value_names).to_owned())),
+                )*].into_iter().collect()
+            }
+
+            /// This opens the DB in read only mode and returns a struct which exposes debug features
+            pub fn get_read_only_handle (
+                db_options: typed_store::sally::SallyReadOnlyDBOptions
+                ) -> #secondary_db_map_struct_name #generics {
+                #secondary_db_map_struct_name::init_read_only(db_options)
+            }
+        }
+
+        // <----------- This section generates the features that use read-only open logic -------------->
+        /// Create an intermediate struct used to open the DBMap tables in secondary mode
+        /// This is only used internally
+        pub struct #secondary_db_map_struct_name #generics {
+            #(
+                pub #field_names : SallyColumn #inner_types,
+            )*
+        }
+
+        impl <
+                #(
+                    #generics_names: #generics_bounds_token,
+                )*
+            > #secondary_db_map_struct_name #generics {
+            /// Open in read only mode. No limitation on number of processes to do this
+            pub fn init_read_only(
+                db_options: typed_store::sally::SallyReadOnlyDBOptions,
+            ) -> Self {
+                match db_options {
+                    typed_store::sally::SallyReadOnlyDBOptions::TestDB => {
+                        let inner = #intermediate_db_map_struct_name::init(SallyDBOptions::TestDB);
+                        Self {
+                            #(
+                                #field_names: inner.#field_names,
+                            )*
+                        }
+                    },
+                    typed_store::sally::SallyReadOnlyDBOptions::RocksDB((primary_path, with_secondary_path, global_db_options_override)) => {
+                        let inner = match with_secondary_path {
+                            Some(q) => #intermediate_db_map_struct_name::init(SallyDBOptions::RocksDB((primary_path, RocksDBAccessType::Secondary(Some(q)), global_db_options_override, None))),
+                            None => {
+                                let p: std::path::PathBuf = tempfile::tempdir()
+                                    .expect("Failed to open temporary directory")
+                                    .into_path();
+                                #intermediate_db_map_struct_name::init(SallyDBOptions::RocksDB((primary_path, RocksDBAccessType::Secondary(Some(p)), global_db_options_override, None)))
+                            }
+                        };
+                        Self {
+                            #(
+                                #field_names: inner.#field_names,
+                            )*
+                        }
+                    }
+                }
+            }
+
+            /// Dump all key-value pairs in the page at the given table name
+            /// Tables must be opened in read only mode using `open_tables_read_only`
+            pub fn dump(&self, table_name: &str, page_size: u16,
+                page_number: usize) -> eyre::Result<std::collections::BTreeMap<String, String>> {
+                Ok(match table_name {
+                    #(
+                        stringify!(#field_names) => {
+                            match &self.#field_names {
+                                SallyColumn::RocksDB((db_map, typed_store::sally::SallyConfig { mode: typed_store::sally::SallyRunMode::FallbackToDB })) => {
+                                    typed_store::traits::Map::try_catch_up_with_primary(db_map)?;
+                                    typed_store::traits::Map::iter(db_map)
+                                        .skip((page_number * (page_size) as usize))
+                                        .take(page_size as usize)
+                                        .map(|(k, v)| (format!("{:?}", k), format!("{:?}", v)))
+                                        .collect::<std::collections::BTreeMap<_, _>>()
+                                }
+                                _ => unimplemented!(),
+                            }
+                        }
+                    )*
+                    _ => eyre::bail!("No such table name: {}", table_name),
+                })
+            }
+
+            pub fn table_summary(&self, table_name: &str) -> eyre::Result<typed_store::traits::TableSummary> {
+                let mut count = 0;
+                let mut key_bytes = 0;
+                let mut value_bytes = 0;
+                match table_name {
+                    #(
+                        stringify!(#field_names) => {
+                            match &self.#field_names {
+                                SallyColumn::RocksDB((db_map, typed_store::sally::SallyConfig { mode: typed_store::sally::SallyRunMode::FallbackToDB })) => {
+                                    typed_store::traits::Map::try_catch_up_with_primary(db_map)?;
+                                    db_map.table_summary()
+                                }
+                                _ => unimplemented!(),
+                            }
+                        }
+                    )*
+
+                    _ => eyre::bail!("No such table name: {}", table_name),
+                }
+            }
+
+            /// Count the keys in this table
+            /// Tables must be opened in read only mode using `open_tables_read_only`
+            pub fn count_keys(&self, table_name: &str) -> eyre::Result<usize> {
+                Ok(match table_name {
+                    #(
+                        stringify!(#field_names) => {
+                            match &self.#field_names {
+                                SallyColumn::RocksDB((db_map, typed_store::sally::SallyConfig { mode: typed_store::sally::SallyRunMode::FallbackToDB })) => {
+                                    typed_store::traits::Map::try_catch_up_with_primary(db_map)?;
+                                    typed_store::traits::Map::iter(db_map).count()
+                                }
+                                _ => unimplemented!(),
+                            }
+                        }
+                    )*
+
+                    _ => eyre::bail!("No such table name: {}", table_name),
+                })
+            }
+
+            pub fn describe_tables() -> std::collections::BTreeMap<String, (String, String)> {
+                vec![#(
+                    (stringify!(#field_names).to_owned(), (stringify!(#key_names).to_owned(), stringify!(#value_names).to_owned())),
+                )*].into_iter().collect()
+            }
+        }
+
+
+        impl <
+                #(
+                    #generics_names: #generics_bounds_token,
+                )*
+            > TypedStoreDebug for #secondary_db_map_struct_name #generics {
+                fn dump_table(
+                    &self,
+                    table_name: String,
+                    page_size: u16,
+                    page_number: usize,
+                ) -> eyre::Result<std::collections::BTreeMap<String, String>> {
+                    self.dump(table_name.as_str(), page_size, page_number)
+                }
+
+                fn primary_db_name(&self) -> String {
+                    stringify!(#name).to_owned()
+                }
+
+                fn describe_all_tables(&self) -> std::collections::BTreeMap<String, (String, String)> {
+                    Self::describe_tables()
+                }
+
+                fn count_table_keys(&self, table_name: String) -> eyre::Result<usize> {
+                    self.count_keys(table_name.as_str())
+                }
+                fn table_summary(&self, table_name: String) -> eyre::Result<TableSummary> {
+                    self.table_summary(table_name.as_str())
+                }
 
         }
 
--- crates/typed-store/src/rocks/mod.rs
+++ crates/typed-store/src/rocks/mod.rs
@@ -1,9 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 mod errors;
-mod iter;
-mod keys;
-mod values;
+pub(crate) mod iter;
+pub(crate) mod keys;
+pub(crate) mod values;
 
 use crate::{
     metrics::{DBMetrics, RocksDBPerfContext, SamplingInterval},
@@ -18,7 +18,12 @@ use rocksdb::{
 };
 use serde::{de::DeserializeOwned, Serialize};
 use std::{
-    borrow::Borrow, collections::BTreeMap, env, marker::PhantomData, path::Path, sync::Arc,
+    borrow::Borrow,
+    collections::BTreeMap,
+    env,
+    marker::PhantomData,
+    path::{Path, PathBuf},
+    sync::Arc,
     time::Duration,
 };
 use tap::TapFallible;
@@ -872,6 +877,66 @@ impl DBBatch {
     }
 
     /// Deletes a set of keys given as an iterator
+    pub fn delete_batch_non_consuming<J: Borrow<K>, K: Serialize, V>(
+        &mut self,
+        db: &DBMap<K, V>,
+        purged_vals: impl IntoIterator<Item = J>,
+    ) -> Result<(), TypedStoreError> {
+        if !Arc::ptr_eq(&db.rocksdb, &self.rocksdb) {
+            return Err(TypedStoreError::CrossDBBatch);
+        }
+
+        purged_vals
+            .into_iter()
+            .try_for_each::<_, Result<_, TypedStoreError>>(|k| {
+                let k_buf = be_fix_int_ser(k.borrow())?;
+                self.batch.delete_cf(&db.cf(), k_buf);
+
+                Ok(())
+            })?;
+        Ok(())
+    }
+    /// Deletes a range of keys between `from` (inclusive) and `to` (non-inclusive)
+    pub fn delete_range_non_consuming<'a, K: Serialize, V>(
+        &mut self,
+        db: &'a DBMap<K, V>,
+        from: &K,
+        to: &K,
+    ) -> Result<(), TypedStoreError> {
+        if !Arc::ptr_eq(&db.rocksdb, &self.rocksdb) {
+            return Err(TypedStoreError::CrossDBBatch);
+        }
+
+        let from_buf = be_fix_int_ser(from)?;
+        let to_buf = be_fix_int_ser(to)?;
+
+        self.batch.delete_range_cf(&db.cf(), from_buf, to_buf)
+    }
+
+    /// inserts a range of (key, value) pairs given as an iterator
+    pub fn insert_batch_non_consuming<J: Borrow<K>, K: Serialize, U: Borrow<V>, V: Serialize>(
+        &mut self,
+        db: &DBMap<K, V>,
+        new_vals: impl IntoIterator<Item = (J, U)>,
+    ) -> Result<(), TypedStoreError> {
+        if !Arc::ptr_eq(&db.rocksdb, &self.rocksdb) {
+            return Err(TypedStoreError::CrossDBBatch);
+        }
+
+        new_vals
+            .into_iter()
+            .try_for_each::<_, Result<_, TypedStoreError>>(|(k, v)| {
+                let k_buf = be_fix_int_ser(k.borrow())?;
+                let v_buf = bincode::serialize(v.borrow())?;
+                self.batch.put_cf(&db.cf(), k_buf, v_buf);
+                Ok(())
+            })?;
+        Ok(())
+    }
+}
+
+// TODO: Remove this entire implementation once we switch to sally
+impl DBBatch {
     pub fn delete_batch<J: Borrow<K>, K: Serialize, V>(
         mut self,
         db: &DBMap<K, V>,
@@ -1703,4 +1768,9 @@ impl DBMapTableConfigMap {
     pub fn to_map(&self) -> BTreeMap<String, DBOptions> {
         self.0.clone()
     }
+}
+
+pub enum RocksDBAccessType {
+    Primary,
+    Secondary(Option<PathBuf>),
 }
