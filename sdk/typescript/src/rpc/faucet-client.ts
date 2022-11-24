21c26ce6a5d4e3448abd74323e3164286d3deba6
Chris Li <76067158+666lcz@users.noreply.github.com>
2022-11-23 19:33:37 -0500

    [TypeScript SDK] Add retry for the faucet to e2e tests (#6362)

--- sdk/typescript/src/rpc/faucet-client.ts
+++ sdk/typescript/src/rpc/faucet-client.ts
@@ -6,6 +6,8 @@ import fetch from 'cross-fetch';
 import { FaucetResponse, SuiAddress } from '../types';
 import { HttpHeaders } from './client';
 
+export class FaucetRateLimitError extends Error {}
+
 export async function requestSuiFromFaucet(
   endpoint: string,
   recipient: SuiAddress,
@@ -23,8 +25,20 @@ export async function requestSuiFromFaucet(
       ...(httpHeaders || {}),
     },
   });
-  const parsed = await res.json();
 
+  if (res.status === 429) {
+    throw new FaucetRateLimitError(
+      `Too many requests from this client have been sent to the faucet. Please retry later`
+    );
+  }
+  let parsed;
+  try {
+    parsed = await res.json();
+  } catch (e) {
+    throw new Error(
+      `Ecountered error when parsing response from faucet, error: ${e}, status ${res.status}, response ${res}`
+    );
+  }
   if (parsed.error) {
     throw new Error(`Faucet returns error: ${parsed.error}`);
   }
