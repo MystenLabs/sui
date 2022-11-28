75b780ebe4acb6d69ead7820293607f9171ffcc4
Jordan Gensler <jordan@mystenlabs.com>
2022-11-28 18:01:41 -0500

    [fe] Use new tsconfig paths (#6438)

--- apps/explorer/vite.config.ts
+++ apps/explorer/vite.config.ts
@@ -1,18 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { pathAlias } from '@mysten/core/vite.config';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-
-import tsconfig from './tsconfig.json';
-
-const alias = (folder: string) => new URL(folder, import.meta.url).pathname;
-
-const tsconfigPaths = {};
-Object.entries(tsconfig.compilerOptions.paths).forEach(([key, [value]]) => {
-    tsconfigPaths[key] = alias(value);
-});
 
 // Assign the Vercel Analytics ID into a vite-safe name:
 process.env.VITE_VERCEL_ANALYTICS_ID = process.env.VERCEL_ANALYTICS_ID;
@@ -26,8 +18,8 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '~': alias('./src'),
-            ...tsconfigPaths,
+            '~': new URL('./src', import.meta.url).pathname,
+            ...pathAlias(import.meta.url),
         },
     },
 });
--- sdk/wallet-adapter/vite.config.ts
+++ sdk/wallet-adapter/vite.config.ts
@@ -1,18 +1,15 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { pathAlias } from "@mysten/core/vite.config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfig from './tsconfig.json';
 
-// TODO: Make an internal helper for this:
-const alias = {};
-Object.entries(tsconfig.compilerOptions.paths).forEach(([key, [value]]) => {
-  alias[key] = new URL(value, import.meta.url).pathname + '/';
-});
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias },
+  resolve: {
+    alias: {
+      ...pathAlias(import.meta.url),
+    },
+  },
 });
