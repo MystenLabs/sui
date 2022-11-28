284d45c55ead5c9e08aa97923c8f2a89578db8a4
Jibz <godwinjibs@gmail.com>
2022-11-28 11:20:38 -0500

    check values

--- apps/explorer/src/components/pagination/__test__/Pagination.test.tsx
+++ apps/explorer/src/components/pagination/__test__/Pagination.test.tsx
@@ -7,8 +7,36 @@ import { describe, it, expect } from 'vitest';
 import Pagination from '../Pagination';
 
 describe('Pagination unit test', () => {
-    it('check pagination buttons and value', () => {
-        const { container, getByRole, getByTestId } = render(
+    it('check pagination buttons', () => {
+        const { getByRole, getByTestId } = render(
+            <Pagination totalItems={105} itemsPerPage={10} currentPage={1} />
+        );
+        const nextBtn = getByRole('button', { name: /→/i });
+        const prevBtn = getByRole('button', { name: /←/i });
+        const secondBtn = getByTestId('secondBtn');
+        const secondLastBtn = getByTestId('secondLastBtn');
+
+        expect(prevBtn).toBeDefined();
+        expect(nextBtn).toBeDefined();
+        expect(secondBtn).toBeDefined();
+        expect(secondLastBtn).toBeDefined();
+        expect(nextBtn).not.toHaveProperty('disabled', true);
+        expect(prevBtn).toHaveProperty('disabled', true);
+    });
+
+    it('check button values for last page', () => {
+        const { getByRole } = render(
+            <Pagination totalItems={105} itemsPerPage={10} currentPage={11} />
+        );
+        const nextBtn = getByRole('button', { name: /→/i });
+        const prevBtn = getByRole('button', { name: /←/i });
+
+        expect(nextBtn).toHaveProperty('disabled', true);
+        expect(prevBtn).not.toHaveProperty('disabled', true);
+    });
+
+    it('check pagination values', () => {
+        const { container } = render(
             <Pagination totalItems={105} itemsPerPage={10} currentPage={1} />
         );
 
@@ -20,31 +48,9 @@ describe('Pagination unit test', () => {
             'button[data-testid="lastBtn"]:not([style*="display:none"])'
         );
 
-        const nextBtn = getByRole('button', { name: /→/i });
-        const prevBtn = getByRole('button', { name: /←/i });
-        const secondBtn = getByTestId('secondBtn');
-        const secondLastBtn = getByTestId('secondLastBtn');
-        expect(prevBtn).toBeDefined();
-        expect(nextBtn).toBeDefined();
-
         expect(firstBtn).toBeDefined();
         expect(lastBtn).toBeDefined();
-
-        expect(secondBtn).toBeDefined();
-        expect(secondLastBtn).toBeDefined();
         expect(firstBtn?.textContent).toBe('1');
         expect(lastBtn?.textContent).toBe('11');
-        expect(prevBtn).toHaveProperty('disabled', true);
-    });
-    it('check button values for last page', () => {
-        const { getByRole } = render(
-            <Pagination totalItems={105} itemsPerPage={10} currentPage={11} />
-        );
-        // Handle multiple elements with the same test id:
-
-        const nextBtn = getByRole('button', { name: /→/i });
-        const prevBtn = getByRole('button', { name: /←/i });
-        expect(nextBtn).toHaveProperty('disabled', true);
-        expect(prevBtn).not.toHaveProperty('disabled', true);
     });
 });
