47a20e08bcb8da819c5fe2bc160b6f006032257d
William Smith <williamprincesmith@gmail.com>
2022-11-29 08:44:23 -0800

    Parallelize execution - first pass approach

--- crates/sui-core/src/authority_active/checkpoint_executor/mod.rs
+++ crates/sui-core/src/authority_active/checkpoint_executor/mod.rs
@@ -3,6 +3,8 @@
 
 use std::{sync::Arc, time::Duration};
 
+use futures::stream::FuturesOrdered;
+use sui_metrics::spawn_monitored_task;
 use sui_types::{
     base_types::{AuthorityName, ExecutionDigests, TransactionDigest},
     committee::StakeUnit,
@@ -10,7 +12,14 @@ use sui_types::{
     messages::{TransactionEffects, VerifiedCertificate},
     messages_checkpoint::{CheckpointSequenceNumber, VerifiedCheckpoint},
 };
-use tokio::sync::broadcast::{self, error::RecvError};
+use tokio::{
+    sync::{
+        broadcast::{self, error::RecvError},
+        Semaphore,
+    },
+    task::JoinHandle,
+};
+use tokio_stream::StreamExt;
 use tracing::{error, info, warn};
 use typed_store::rocks::TypedStoreError;
 
@@ -48,88 +57,115 @@ impl CheckpointExecutor {
     }
 
     pub async fn run(mut self) {
+        let mut finished: FuturesOrdered<JoinHandle<(u64, VerifiedCheckpoint)>> =
+            FuturesOrdered::new();
+        let limit = Arc::new(Semaphore::new(num_cpus::get()));
+
         loop {
-            match self.mailbox.recv().await {
-                Ok(checkpoint) => {
-                    let last_to_exec = checkpoint.sequence_number() as u64;
-                    let next_to_exec = self
-                        .highest_executed_checkpoint
-                        .map(|highest| highest + 1)
-                        .unwrap_or(0);
+            tokio::select! {
+                // execution worker consumer branch
+                Some(Ok((seq_num, checkpoint))) = finished.next() => {
+                    *self.highest_executed_checkpoint = Some(seq_num as CheckpointSequenceNumber);
+                    self.metrics.last_executed_checkpoint.set(seq_num as i64);
+                    self.update_lag_metric(seq_num)
+                        .unwrap_or_else(|err| error!("Update lag metric error: {:?}", err));
 
-                    for seq_num in next_to_exec..=last_to_exec {
-                        let next_checkpoint = match self
-                            .checkpoint_store
-                            .get_checkpoint_by_sequence_number(seq_num)
-                        {
-                            Ok(Some(new_checkpoint)) => new_checkpoint,
-                            Ok(None) => {
-                                error!(
-                                    "Checkpoint {:?} does not exist in checkpoint store",
-                                    seq_num,
-                                );
-                                self.metrics.checkpoint_exec_errors.inc();
-                                continue;
-                            }
-                            Err(err) => {
-                                error!("Failed to fetch checkpoint {:?}: {:?}", seq_num, err);
-                                self.metrics.checkpoint_exec_errors.inc();
-                                continue;
-                            }
-                        };
+                    self.checkpoint_store
+                        .update_highest_executed_checkpoint(&checkpoint)
+                        .unwrap();
+                },
+                // execution worker producer branch
+                received = self.mailbox.recv() => match received {
+                    Ok(checkpoint) => {
+                        let last_to_exec = checkpoint.sequence_number() as u64;
+                        let next_to_exec = self
+                            .highest_executed_checkpoint
+                            .map(|highest| highest + 1)
+                            .unwrap_or(0);
 
-                        // Since StateSync is guaranteed to produce checkpoint sync messages
-                        // in monotonically increasing order, then a mismatch of epoch would
-                        // mean that we failed to execute reconfig at the epoch boundary.
-                        assert!(
-                            next_checkpoint.epoch()
-                                != self
-                                    .authority_state
-                                    .committee_store()
-                                    .get_latest_committee()
-                                    .epoch()
-                        );
+                        for seq_num in next_to_exec..=last_to_exec {
+                            let next_checkpoint = match self
+                                .checkpoint_store
+                                .get_checkpoint_by_sequence_number(seq_num)
+                            {
+                                Ok(Some(new_checkpoint)) => new_checkpoint,
+                                Ok(None) => {
+                                    error!(
+                                        "Checkpoint {:?} does not exist in checkpoint store",
+                                        seq_num,
+                                    );
+                                    self.metrics.checkpoint_exec_errors.inc();
+                                    continue;
+                                }
+                                Err(err) => {
+                                    error!("Failed to fetch checkpoint {:?}: {:?}", seq_num, err);
+                                    self.metrics.checkpoint_exec_errors.inc();
+                                    continue;
+                                }
+                            };
 
-                        while let Err(err) = self.execute_checkpoint(&next_checkpoint).await {
-                            error!(
-                                "Error while executing checkpoint, will retry in 1s: {:?}",
-                                err
+                            // Since StateSync is guaranteed to produce checkpoint sync messages
+                            // in monotonically increasing order, then a mismatch of epoch would
+                            // mean that we failed to execute reconfig at the epoch boundary.
+                            assert!(
+                                next_checkpoint.epoch()
+                                    != self
+                                        .authority_state
+                                        .committee_store()
+                                        .get_latest_committee()
+                                        .epoch()
                             );
-                            tokio::time::sleep(Duration::from_secs(1)).await;
-                            self.metrics.checkpoint_exec_errors.inc();
-                            continue;
-                        }
-                        *self.highest_executed_checkpoint =
-                            Some(seq_num as CheckpointSequenceNumber);
-                        self.metrics.last_executed_checkpoint.set(seq_num as i64);
-                        self.update_lag_metric(seq_num)
-                            .unwrap_or_else(|err| error!("Update lag metric error: {:?}", err));
 
-                        // Last checkpoint of epoch
-                        if let Some(next_committee) = next_checkpoint.next_epoch_committee() {
-                            // Unwrap ok because reconfig failure is unrecoverable
-                            self.reconfig(next_committee).await.unwrap();
+                            // TODO get semaphore here to rate limit number of spawned tasks
+                            // Unfortunately blocking on the semaphore here in the case where
+                            // we are rate limited on threads means we also blocking from
+                            // consuming from the finished queue (first branch).. Need to
+                            // think through this
+                            let limit = limit.clone();
+                            // hold semaphore permit until task completes. unwrap ok because we never close
+                            // the semaphore in this context.
+                            let permit = limit.acquire_owned().await.unwrap();
+
+                            finished.push_back(spawn_monitored_task!(async move {
+                                let _guard = permit;
+                                while let Err(err) = self.execute_checkpoint(&next_checkpoint).await {
+                                    error!(
+                                        "Error while executing checkpoint, will retry in 1s: {:?}",
+                                        err
+                                    );
+                                    tokio::time::sleep(Duration::from_secs(1)).await;
+                                    self.metrics.checkpoint_exec_errors.inc();
+                                }
+
+                                (seq_num, next_checkpoint)
+                            }));
+
+                            // Last checkpoint of epoch
+                            if let Some(next_committee) = next_checkpoint.next_epoch_committee() {
+                                // Unwrap ok because reconfig failure is unrecoverable
+                                self.reconfig(next_committee).await.unwrap();
+                            }
                         }
                     }
-                }
-                // In this case, messages in the mailbox have been overwritten
-                // as a result of lagging too far behind. We can simply continue the loop,
-                // as the next call to recv() should return the overwritten checkpoint and we
-                // will attempt to execute all checkpoints from (highest_executed_checkpoint + 1)
-                // up to the result of the next recv() call.
-                Err(RecvError::Lagged(num_skipped)) => {
-                    warn!(
-                        "Checkpoint Execution Recv channel overflowed {:?} messages",
-                        num_skipped,
-                    );
-                    self.metrics
-                        .checkpoint_exec_recv_channel_overflow
-                        .inc_by(num_skipped);
-                }
-                Err(RecvError::Closed) => {
-                    info!("Checkpoint Execution Sender (StateSync) closed channel");
-                    break;
-                }
+                    // In this case, messages in the mailbox have been overwritten
+                    // as a result of lagging too far behind. We can simply continue the loop,
+                    // as the next call to recv() should return the overwritten checkpoint and we
+                    // will attempt to execute all checkpoints from (highest_executed_checkpoint + 1)
+                    // up to the result of the next recv() call.
+                    Err(RecvError::Lagged(num_skipped)) => {
+                        warn!(
+                            "Checkpoint Execution Recv channel overflowed {:?} messages",
+                            num_skipped,
+                        );
+                        self.metrics
+                            .checkpoint_exec_recv_channel_overflow
+                            .inc_by(num_skipped);
+                    }
+                    Err(RecvError::Closed) => {
+                        info!("Checkpoint Execution Sender (StateSync) closed channel");
+                        break;
+                    }
+                },
             }
         }
     }
@@ -152,10 +188,6 @@ impl CheckpointExecutor {
             );
             Vec::new()
         };
-
-        self.checkpoint_store
-            .update_highest_executed_checkpoint(checkpoint)
-            .map_err(SuiError::from)?;
         Ok(effects)
     }
 
