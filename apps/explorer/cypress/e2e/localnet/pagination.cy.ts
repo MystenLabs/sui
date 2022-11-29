973e2782b0bede73ad42e70468d13d2885012654
Jibz <godwinjibs@gmail.com>
2022-11-28 19:07:14 -0500

    test

--- apps/explorer/cypress/e2e/localnet/pagination.cy.ts
+++ apps/explorer/cypress/e2e/localnet/pagination.cy.ts
@@ -3,30 +3,21 @@
 
 Cypress.config('baseUrl', 'http://localhost:3000');
 
-const nftObject = (num: number) => `div#ownedObjects > div:nth-child(${num}) a`;
-
 describe('PaginationWrapper has buttons', () => {
-    const paginationContext = '#NFTSection';
-
-    it('to go to the next page', () => {
-        cy.task('faucet')
-            .then((address) => cy.task('mint', address))
-            .then((tx) => {
-                if (!('EffectsCert' in tx)) {
-                    throw new Error('Missing effects cert');
-                }
-                cy.get(paginationContext).within(() => {
-                    cy.get('[data-testid=nextBtn]:visible').click();
-                    cy.get(nftObject(1)).click();
-                });
-                cy.get('#objectID').contains('Image2');
-            });
+    const nftObject = (num: number) =>
+        `div#ownedObjects > div:nth-child(${num}) a`;
+    it('to go to the last page', () => {
+        cy.visit('/addresses/ownsAllAddress');
+        cy.get('#NFTSection').within(() => {
+            cy.get('[data-testid=lastBtn]:visible').click();
+            cy.get(nftObject(1)).click();
+        });
+        cy.get('#objectID').contains('CollectionObject');
     });
 
     it('to go to the last page', () => {
-        const address = 'ownsAllAddress';
-        cy.visit(`/addresses/${address}`);
-        cy.get(paginationContext).within(() => {
+        cy.visit('/addresses/ownsAllAddress');
+        cy.get('#NFTSection').within(() => {
             cy.get('[data-testid=lastBtn]:visible').click();
             cy.get(nftObject(1)).click();
         });
@@ -34,9 +25,8 @@ describe('PaginationWrapper has buttons', () => {
     });
 
     it('to go back a page', () => {
-        const address = 'ownsAllAddress';
-        cy.visit(`/addresses/${address}`);
-        cy.get(paginationContext).within(() => {
+        cy.visit('/addresses/ownsAllAddress');
+        cy.get('#NFTSection').within(() => {
             cy.get('[data-testid=lastBtn]:visible').click();
             cy.get('[data-testid=backBtn]:visible').click();
             cy.get(nftObject(1)).click();
@@ -45,9 +35,8 @@ describe('PaginationWrapper has buttons', () => {
     });
 
     it('to go to first page', () => {
-        const address = 'ownsAllAddress';
-        cy.visit(`/addresses/${address}`);
-        cy.get(paginationContext).within(() => {
+        cy.visit('/addresses/ownsAllAddress');
+        cy.get('#NFTSection').within(() => {
             cy.get('[data-testid=lastBtn]:visible').click();
             cy.get('[data-testid=backBtn]:visible').click();
             cy.get('[data-testid=firstBtn]:visible').click();
