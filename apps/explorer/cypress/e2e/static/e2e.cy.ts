50a62262d210edbc10af44f439fe4002b776ebe3
Jibz <godwinjibs@gmail.com>
2022-11-29 06:15:18 -0500

    restored some e2e

--- apps/explorer/cypress/e2e/static/e2e.cy.ts
+++ apps/explorer/cypress/e2e/static/e2e.cy.ts
@@ -74,6 +74,53 @@ describe('End-to-end Tests', () => {
         });
     });
 
+    describe('PaginationWrapper has buttons', () => {
+        const paginationContext = '#NFTSection';
+
+        it('to go to the next page', () => {
+            const address = 'ownsAllAddress';
+            cy.visit(`/address/${address}`);
+            cy.get(paginationContext).within(() => {
+                cy.get('[data-testid=nextBtn]:visible').click();
+                cy.get(nftObject(1)).click();
+            });
+            cy.get('#objectID').contains('Image2');
+        });
+
+        it('to go to the last page', () => {
+            const address = 'ownsAllAddress';
+            cy.visit(`/address/${address}`);
+            cy.get(paginationContext).within(() => {
+                cy.get('[data-testid=lastBtn]:visible').click();
+                cy.get(nftObject(1)).click();
+            });
+            cy.get('#objectID').contains('CollectionObject');
+        });
+
+        it('to go back a page', () => {
+            const address = 'ownsAllAddress';
+            cy.visit(`/address/${address}`);
+            cy.get(paginationContext).within(() => {
+                cy.get('[data-testid=lastBtn]:visible').click();
+                cy.get('[data-testid=backBtn]:visible').click();
+                cy.get(nftObject(1)).click();
+            });
+            cy.get('#objectID').contains('player5');
+        });
+
+        it('to go to first page', () => {
+            const address = 'ownsAllAddress';
+            cy.visit(`/address/${address}`);
+            cy.get(paginationContext).within(() => {
+                cy.get('[data-testid=lastBtn]:visible').click();
+                cy.get('[data-testid=backBtn]:visible').click();
+                cy.get('[data-testid=firstBtn]:visible').click();
+            });
+            cy.get(nftObject(1)).click();
+            cy.get('#objectID').contains('ChildObjectWBrokenImage');
+        });
+    });
+
     describe('Group View', () => {
         it('evaluates balance', () => {
             const address = 'ownsAllAddress';
