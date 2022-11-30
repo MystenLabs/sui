2047ef96869157f2ca7bca113b568b4dc5a6f546
Jordan Gensler <jordan@mystenlabs.com>
2022-11-30 11:48:06 -0500

    [wallet-ext] Remove demo nft example (#6442)

--- apps/wallet/examples/demo-nft-dapp/next.config.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-};
-
-module.exports = nextConfig;
