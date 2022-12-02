fb6270a1916fa3508d61c142a2f015d87f6a578b
MW Tian <mingwei@mystenlabs.com>
2022-12-01 16:05:40 -0800

    Remove dhat feature and imports

--- narwhal/benchmark/benchmark/commands.py
+++ narwhal/benchmark/benchmark/commands.py
@@ -17,13 +17,8 @@ class CommandMaker:
         return f'rm -r {PathMaker.logs_path()} ; mkdir -p {PathMaker.logs_path()}'
 
     @staticmethod
-    def compile(mem_profiling):
-        if mem_profiling:
-            params = ["--profile", "bench-profiling",
-                      "--features", "benchmark dhat-heap"]
-        else:
-            params = ["--release", "--features", "benchmark"]
-        return ["cargo", "build", "--quiet"] + params
+    def compile():
+        return ["cargo", "build", "--quiet", "--release", "--features", "benchmark"]
 
     @staticmethod
     def generate_key(filename):
