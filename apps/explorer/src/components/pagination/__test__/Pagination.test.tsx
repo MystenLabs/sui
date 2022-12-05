6fa29f4376c98e124fb34eef56c196fd4fa88a58
Jibz <godwinjibs@gmail.com>
2022-12-05 14:27:10 -0500

    added test for onPageChangeFn

--- apps/explorer/src/components/pagination/__test__/Pagination.test.tsx
+++ apps/explorer/src/components/pagination/__test__/Pagination.test.tsx
@@ -2,17 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { render } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import userEvent from '@testing-library/user-event'
+import { describe, it, expect, vi  } from 'vitest';
 
 import Pagination from '../Pagination';
 
-describe('Pagination unit test', () => {
-    it('check pagination buttons', () => {
-        const { getByRole, getByTestId } = render(
+
+describe('Pagination test', () => {
+    it('check pagination buttons exist and check disabled behavior for the nextBtn and prevBtn', () => {
+        const { getByTestId, container } = render(
             <Pagination totalItems={105} itemsPerPage={10} currentPage={1} />
         );
-        const nextBtn = getByRole('button', { name: /→/i });
-        const prevBtn = getByRole('button', { name: /←/i });
+        const nextBtn = container.querySelector(
+            'button[data-testid="nextBtn"]:not([style*="display:none"])'
+        );
+        const prevBtn = container.querySelector(
+            'button[data-testid="backBtn"]:not([style*="display:none"])'
+        );
         const secondBtn = getByTestId('secondBtn');
         const secondLastBtn = getByTestId('secondLastBtn');
 
@@ -24,13 +30,17 @@ describe('Pagination unit test', () => {
         expect(prevBtn).toHaveProperty('disabled', true);
     });
 
-    it('check button values for last page', () => {
-        const { getByRole } = render(
+    it('check active state for backBtn and nextBtn on the last page', () => {
+        const { container } = render(
             <Pagination totalItems={105} itemsPerPage={10} currentPage={11} />
         );
-        const nextBtn = getByRole('button', { name: /→/i });
-        const prevBtn = getByRole('button', { name: /←/i });
 
+        const nextBtn = container.querySelector(
+            'button[data-testid="nextBtn"]:not([style*="display:none"])'
+        );
+        const prevBtn = container.querySelector(
+            'button[data-testid="backBtn"]:not([style*="display:none"])'
+        );
         expect(nextBtn).toHaveProperty('disabled', true);
         expect(prevBtn).not.toHaveProperty('disabled', true);
     });
@@ -44,6 +54,7 @@ describe('Pagination unit test', () => {
         const firstBtn = container.querySelector(
             'button[data-testid="firstBtn"]:not([style*="display:none"])'
         );
+
         const lastBtn = container.querySelector(
             'button[data-testid="lastBtn"]:not([style*="display:none"])'
         );
@@ -53,4 +64,28 @@ describe('Pagination unit test', () => {
         expect(firstBtn?.textContent).toBe('1');
         expect(lastBtn?.textContent).toBe('11');
     });
+
+    it('onPageChangeFn should match clicked button page number', async () => {
+ 
+        const setPageNumChange = vi.fn((number) => number)
+
+        const {  getByTestId, } = render(
+            <Pagination totalItems={105} itemsPerPage={10} currentPage={1} onPagiChangeFn={setPageNumChange} />
+        );
+            
+        const secondBtn = getByTestId('secondBtn');
+        const secondBtnPageNum = parseInt(secondBtn.textContent || '0', 10);
+        const secondLastBtn = getByTestId('secondLastBtn');
+        const secondLastBtnNumber = parseInt(secondLastBtn?.textContent || '0', 10);
+
+        await userEvent.click(secondBtn)
+        expect(setPageNumChange).toHaveReturnedWith(2);
+
+        await userEvent.click(secondBtn);
+        expect(setPageNumChange).toHaveReturnedWith(secondBtnPageNum);
+
+        await userEvent.click(secondLastBtn)
+        expect(setPageNumChange).toHaveReturnedWith(secondLastBtnNumber);
+    });
+ 
 });
