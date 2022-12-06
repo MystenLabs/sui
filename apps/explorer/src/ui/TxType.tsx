22036a0d06cc47bf67437218295b8693a4f8ae3a
Andrew Burnie <apburnie@hotmail.co.uk>
2022-12-06 14:50:24 +0000

    adds symbol

--- apps/explorer/src/ui/TxType.tsx
+++ apps/explorer/src/ui/TxType.tsx
@@ -3,6 +3,9 @@
 
 import { type ReactNode } from 'react';
 
+import { ReactComponent as ContentSuccessStatus } from '../assets/SVGIcons/12px/Check.svg';
+import { ReactComponent as ContentFailedStatus } from '../assets/SVGIcons/12px/X.svg';
+
 export type TxTypeProps = {
     isFail?: boolean;
     count?: string;
@@ -11,8 +14,12 @@ export type TxTypeProps = {
 
 export function TxType({ isFail, count, children }: TxTypeProps) {
     return (
-        <div className="flex">
-            <div>{isFail ? 'Fail' : 'Success'}</div>
+        <div className="flex items-center justify-center gap-1.5">
+            {isFail ? (
+                <ContentFailedStatus className="fill-issue-dark" />
+            ) : (
+                <ContentSuccessStatus className="fill-success" />
+            )}
             {children}
             {count && <div>{count}</div>}
         </div>
