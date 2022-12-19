0d77150bff4fd6bafdcd2fa95a36a2973183a4d8
Mark Logan <mark@mystenlabs.com>
2022-12-19 09:00:14 -0800

    fix rebase error

--- crates/sui-node/src/lib.rs
+++ crates/sui-node/src/lib.rs
@@ -319,7 +319,7 @@ impl SuiNode {
 
         // TODO only configure validators as seed/preferred peers for validators and not for
         // fullnodes once we've had a chance to re-work fullnode configuration generation.
-        let mut p2p_config = config.p2p_config.clone();
+        let mut p2p_config = p2p_config.clone();
         let our_network_public_key = config.network_key_pair.public();
         let other_validators = config
             .genesis()?
