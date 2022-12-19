fc3723aba0a96f1c18f113495914f40810e82f6b
Jibz <godwinjibs@gmail.com>
2022-12-19 13:06:39 -0500

    update

--- apps/explorer/src/pages/validator/ValidatorDetails.tsx
+++ apps/explorer/src/pages/validator/ValidatorDetails.tsx
@@ -161,7 +161,7 @@ function ValidatorDetails() {
                     variant="square"
                     size="xl"
                 />
-                <div className="flex flex-col gap-2.5 md:gap-3.5 mt-1">
+                <div className="mt-1 flex flex-col gap-2.5 md:gap-3.5">
                     <Heading
                         as="h1"
                         variant="heading2"
