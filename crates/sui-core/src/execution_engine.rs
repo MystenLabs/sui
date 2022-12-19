b8eb0bbc896a59d39c0a5857cb95606ece675857
Xun Li <lxfind@gmail.com>
2022-12-18 20:47:45 -0800

    [tokenomics] Pass storage reinvest rate to advance epoch system call

--- crates/sui-core/src/execution_engine.rs
+++ crates/sui-core/src/execution_engine.rs
@@ -10,6 +10,7 @@ use sui_types::base_types::SequenceNumber;
 use tracing::{debug, instrument};
 
 use sui_adapter::adapter;
+use sui_protocol_constants::STORAGE_FUND_REINVEST_RATE;
 use sui_types::coin::{transfer_coin, update_input_coins, Coin};
 use sui_types::committee::EpochId;
 use sui_types::error::{ExecutionError, ExecutionErrorKind};
@@ -317,6 +318,7 @@ fn execution_loop<
                         CallArg::Pure(bcs::to_bytes(&storage_charge).unwrap()),
                         CallArg::Pure(bcs::to_bytes(&computation_charge).unwrap()),
                         CallArg::Pure(bcs::to_bytes(&storage_rebate).unwrap()),
+                        CallArg::Pure(bcs::to_bytes(&STORAGE_FUND_REINVEST_RATE).unwrap()),
                     ],
                     gas_status.create_move_gas_status(),
                     tx_ctx,
