a82166bd286bb1860e497457230ce92acd200a72
Jibz <godwinjibs@gmail.com>
2022-12-19 10:22:21 -0500

    design update

--- apps/explorer/src/ui/ImageIcon.tsx
+++ apps/explorer/src/ui/ImageIcon.tsx
@@ -3,26 +3,30 @@
 
 import { cva, type VariantProps } from 'class-variance-authority';
 
-const imageStyle = cva(['bg-gray-45 text-gray-65 capitalize overflow-hidden'], {
-    variants: {
-        //TODO: verify sizes with design especially for mobile
-        size: {
-            sm: 'w-6 h-6 font-medium text-bodySmall',
-            md: 'w-8 h-8 font-medium text-body',
-            lg: 'md:w-10 md:h-10 w-8 h-8 font-medium text-heading1',
-            xl: 'md:w-32 md:h-32 w-16 font-medium text-heading1',
+const imageStyle = cva(
+    [
+        'bg-gradient-to-r from-gradient-blue-start to-gradient-blue-end text-white capitalize overflow-hidden',
+    ],
+    {
+        variants: {
+            size: {
+                sm: 'w-6 h-6 font-medium text-subtitleSmallExtra',
+                md: 'w-8 h-8 font-medium text-body',
+                lg: 'md:w-10 md:h-10 w-8 h-8 font-medium text-heading1',
+                xl: 'md:w-31.5 md:h-31.5 w-16 font-medium text-heading1',
+            },
+            variant: {
+                circle: 'rounded-full',
+                square: 'rounded-md',
+            },
         },
-        variant: {
-            circle: 'rounded-full',
-            square: 'rounded-md',
-        },
-    },
 
-    defaultVariants: {
-        variant: 'circle',
-        size: 'md',
-    },
-});
+        defaultVariants: {
+            variant: 'circle',
+            size: 'md',
+        },
+    }
+);
 
 export interface ImageIconProps extends VariantProps<typeof imageStyle> {
     src?: string | null;
