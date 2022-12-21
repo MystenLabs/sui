8544157edf11511c0c35bf7f3a788a8572aab1c7
Brandon Williams <brandon@mystenlabs.com>
2022-12-20 17:03:05 -0800

    node: load narwhal committee from system object
    Load narwhal committee information directly from the on-chain system
    object instead of constructing it based on metadata included in the
    genesis object.

--- crates/sui-config/tests/snapshot_tests.rs
+++ crates/sui-config/tests/snapshot_tests.rs
@@ -90,7 +90,6 @@ fn populated_genesis_snapshot_matches() {
         .build();
     assert_yaml_snapshot!(genesis.validator_set());
     assert_yaml_snapshot!(genesis.committee().unwrap());
-    assert_yaml_snapshot!(genesis.narwhal_committee());
     assert_yaml_snapshot!(genesis.sui_system_object());
     // Serialized `genesis` is not static and cannot be snapshot tested.
 }
