4277fb899c82ba138f032ba5de25933813e78142
Krešimir Klas <github@kklas.net>
2022-12-29 23:46:34 +0000

    [sdk] implement parsing of nested types in TypeTagSerializer

--- sdk/typescript/src/signers/txn-data-serializers/type-tag-serializer.ts
+++ sdk/typescript/src/signers/txn-data-serializers/type-tag-serializer.ts
@@ -1,11 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { normalizeSuiAddress, TypeTag } from '../../types';
+import { TypeTag } from '../../types';
 
 const VECTOR_REGEX = /^vector<(.+)>$/;
-const STRUCT_REGEX = /^([^:]+)::([^:]+)::(.+)/;
-const STRUCT_TYPE_TAG_REGEX = /^[^<]+<(.+)>$/;
+const STRUCT_REGEX = /^([^:]+)::([^:]+)::([^<]+)(<(.+)>)?/;
 
 export class TypeTagSerializer {
   parseFromStr(str: string): TypeTag {
@@ -35,33 +34,47 @@ export class TypeTagSerializer {
 
     const structMatch = str.match(STRUCT_REGEX);
     if (structMatch) {
-      try {
-        return {
-          struct: {
-            address: normalizeSuiAddress(structMatch[1]),
-            module: structMatch[2],
-            name: structMatch[3].match(/^([^<]+)/)![1],
-            typeParams: this.parseStructTypeTag(structMatch[3]),
-          },
-        };
-      } catch (e) {
-        throw new Error(`Encounter error parsing type args for ${str}`);
-      }
+      return {
+        struct: {
+          address: structMatch[1],
+          module: structMatch[2],
+          name: structMatch[3],
+          typeParams:
+            structMatch[5] === undefined
+              ? []
+              : this.parseStructTypeArgs(structMatch[5]),
+        },
+      };
     }
 
     throw new Error(
-      `Encounter unexpected token when parsing type args for ${str}`
+      `Encountered unexpected token when parsing type args for ${str}`
     );
   }
 
-  parseStructTypeTag(str: string): TypeTag[] {
-    const typeTagsMatch = str.match(STRUCT_TYPE_TAG_REGEX);
-    if (!typeTagsMatch) {
-      return [];
+  parseStructTypeArgs(str: string): TypeTag[] {
+    // split `str` by all `,` outside angle brackets
+    const tok: Array<string> = [];
+    let word = '';
+    let nestedAngleBrackets = 0;
+    for (let i = 0; i < str.length; i++) {
+      const char = str[i];
+      if (char === '<') {
+        nestedAngleBrackets++;
+      }
+      if (char === '>') {
+        nestedAngleBrackets--;
+      }
+      if (nestedAngleBrackets === 0 && char === ',') {
+        tok.push(word.trim());
+        word = '';
+        continue;
+      }
+      word += char;
     }
-    // TODO: This will fail if the struct has nested type args with commas. Need
-    // to implement proper parsing for this case
-    const typeTags = typeTagsMatch[1].split(',');
-    return typeTags.map((tag) => this.parseFromStr(tag));
+
+    tok.push(word.trim());
+
+    return tok.map((tok) => this.parseFromStr(tok));
   }
 }
