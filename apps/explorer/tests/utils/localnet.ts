e7ba4708911360bb6992bd705bfbada1fe20f1b7
Jordan Gensler <jordan@mystenlabs.com>
2023-01-04 01:06:41 -0800

    tsconfig paths

--- apps/explorer/tests/utils/localnet.ts
+++ apps/explorer/tests/utils/localnet.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+// eslint-disable-next-line import/order
+import 'tsconfig-paths/register';
 import {
     Ed25519Keypair,
     JsonRpcProvider,
