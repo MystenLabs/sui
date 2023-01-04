f1d249b2978d1d14e4bafc2f3857fa2dfb2c0e7d
Chris Li <chris@mystenlabs.com>
2023-01-04 14:39:40 -0500

    [TypeScript SDK] Add method for DevInspectMoveCall

---
+++ .changeset/nasty-rocks-itch.md
@@ -0,0 +1,5 @@
+---
+"@mysten/sui.js": patch
+---
+
+Add devInspectMoveCall, which is similar to devInspectTransaction, but lets you call any Move function without a gas object and budget
--- sdk/typescript/src/providers/void-provider.ts
+++ sdk/typescript/src/providers/void-provider.ts
@@ -4,6 +4,7 @@
 import { PublicKey, SignatureScheme } from '../cryptography/publickey';
 import { HttpHeaders } from '../rpc/client';
 import { Base64DataBuffer } from '../serialization/base64';
+import { RawMoveCall } from '../signers/txn-data-serializers/txn-data-serializer';
 import {
   CertifiedTransaction,
   TransactionDigest,
@@ -34,7 +35,7 @@ import {
   Order,
   TransactionEffects,
   CoinMetadata,
-  DevInspectResults
+  DevInspectResults,
 } from '../types';
 import { Provider } from './provider';
 
@@ -119,6 +120,13 @@ export class VoidProvider extends Provider {
 
   devInspectTransaction(_txBytes: string): Promise<DevInspectResults> {
     throw this.newError('devInspectTransaction');
+  }
+
+  async devInspectMoveCall(
+    _sender: SuiAddress,
+    _moveCall: RawMoveCall
+  ): Promise<DevInspectResults> {
+    throw this.newError('devInspectMoveCall');
   }
 
   dryRunTransaction(_txBytes: string): Promise<TransactionEffects> {
