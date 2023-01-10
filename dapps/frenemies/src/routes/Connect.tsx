2ceb1ec91ede6c6d97b0130af4b1d61028e8f2b0
Jordan Gensler <jordan@mystenlabs.com>
2023-01-10 13:22:46 -0800

    Scaffold frenemies

---
+++ dapps/frenemies/src/routes/Connect.tsx
@@ -0,0 +1,5 @@
+import { ConnectButton } from '@mysten/wallet-kit';
+
+export function Connect() {
+	return <ConnectButton />
+}
---
+++ dapps/frenemies/src/routes/Home.tsx
@@ -0,0 +1,3 @@
+export function Home() {
+  return <h1 className="text-3xl font-bold underline">home page</h1>;
+}
