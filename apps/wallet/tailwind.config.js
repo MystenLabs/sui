0bdee8c252b6a952645fc21f20c4d91eab6b3b13
Jibz <godwinjibs@gmail.com>
2023-01-10 09:01:26 -0500

    update

--- apps/wallet/tailwind.config.js
+++ apps/wallet/tailwind.config.js
@@ -24,21 +24,17 @@ module.exports = {
             boxShadow: {
                 'wallet-content': '0px -5px 20px 5px rgba(160, 182, 195, 0.15)',
                 button: '0px 1px 2px rgba(16, 24, 40, 0.05)',
+                notification: '0px 0px 20px rgba(29, 55, 87, 0.11)',
             },
             borderRadius: {
                 20: '1.25rem',
                 15: '0.9375rem',
+                '2lg': '0.625rem',
             },
             height: {
                 header: '68px',
             },
-            borderRadius: {
-                '2lg': '0.625rem',
-            },
-            boxShadow: {
-                notification: '0px 0px 20px rgba(29, 55, 87, 0.11)',
-            },
-            maxWidth: {
+            width: {
                 'popup-width': '360px',
             },
         },
