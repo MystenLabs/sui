e4d8a9487a810dbdf181d6475de9930e1dd63e4d
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2023-01-10 20:03:12 -0700

    indexer: add basic prometheus metrics

--- crates/sui-indexer/src/processors/address_processor.rs
+++ crates/sui-indexer/src/processors/address_processor.rs
@@ -1,10 +1,12 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use prometheus::Registry;
 use std::sync::Arc;
-use sui_indexer::errors::IndexerError;
 use tracing::info;
 
+use sui_indexer::errors::IndexerError;
+use sui_indexer::metrics::IndexerAddressProcessorMetrics;
 use sui_indexer::models::address_logs::{commit_address_log, read_address_log};
 use sui_indexer::models::addresses::{commit_addresses, transaction_to_address, NewAddress};
 use sui_indexer::models::transactions::read_transactions;
@@ -14,11 +16,19 @@ const TRANSACTION_BATCH_SIZE: usize = 100;
 
 pub struct AddressProcessor {
     pg_connection_pool: Arc<PgConnectionPool>,
+    pub address_processor_metrics: IndexerAddressProcessorMetrics,
 }
 
 impl AddressProcessor {
-    pub fn new(pg_connection_pool: Arc<PgConnectionPool>) -> AddressProcessor {
-        Self { pg_connection_pool }
+    pub fn new(
+        pg_connection_pool: Arc<PgConnectionPool>,
+        prometheus_registry: &Registry,
+    ) -> AddressProcessor {
+        let address_processor_metrics = IndexerAddressProcessorMetrics::new(prometheus_registry);
+        Self {
+            pg_connection_pool,
+            address_processor_metrics,
+        }
     }
 
     pub async fn start(&self) -> Result<(), IndexerError> {
@@ -38,6 +48,9 @@ impl AddressProcessor {
 
             commit_addresses(&mut pg_pool_conn, addr_vec)?;
             commit_address_log(&mut pg_pool_conn, last_processed_id)?;
+            self.address_processor_metrics
+                .total_address_batch_processed
+                .inc();
         }
     }
 }
