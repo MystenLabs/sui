5f467ddf64d47b43def6dd82ab6ad40dbce27322
Jibz <godwinjibs@gmail.com>
2023-01-10 20:47:16 -0500

    rm Base64DataBuffer

--- apps/explorer/src/utils/getName.ts
+++ apps/explorer/src/utils/getName.ts
@@ -1,12 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Base64DataBuffer } from '@mysten/sui.js';
-
 // TODO : Import from  SDK
 import { VALDIATOR_NAME } from '~/pages/validator/ValidatorDataTypes';
-
-const textDecoder = new TextDecoder();
 
 export function getName(rawName: string | number[]) {
     let name: string;
@@ -14,7 +10,7 @@ export function getName(rawName: string | number[]) {
     if (Array.isArray(rawName)) {
         name = String.fromCharCode(...rawName);
     } else {
-        name = textDecoder.decode(new Base64DataBuffer(rawName).getData());
+        name = decodeURIComponent(atob(rawName));
         if (!VALDIATOR_NAME.test(name)) {
             name = rawName;
         }
