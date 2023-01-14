293d45597c855ac496516951a4b0713405b1eb5c
Jibz1 <godwinjibs@gmail.com>
2023-01-14 06:44:41 -0800

    update activities to include events coinBalance (#6953)
    <img width="395" alt="Screenshot 2022-12-20 at 5 15 04 PM"
    src="https://user-images.githubusercontent.com/8676844/208778061-b4366d61-353c-4d8c-8723-15ec2d6316e3.png">

--- apps/wallet/src/ui/app/helpers/index.ts
+++ apps/wallet/src/ui/app/helpers/index.ts
@@ -4,3 +4,5 @@
 export { default as formatDate } from './formatDate';
 export { default as notEmpty } from './notEmptyCheck';
 export { parseAmount } from './parseAmount';
+export { getEventsSummary } from './getEventsSummary';
+export { getAmount } from './getAmount';
