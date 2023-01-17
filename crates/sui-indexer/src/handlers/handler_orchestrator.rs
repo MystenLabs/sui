a8336715e83ff90dea1dd4ef76efa6404b9c4f39
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2023-01-17 15:30:18 -0700

    indexer: temp event handlers for new objects and new modules

--- crates/sui-indexer/src/handlers/handler_orchestrator.rs
+++ crates/sui-indexer/src/handlers/handler_orchestrator.rs
@@ -15,6 +15,8 @@ use crate::handlers::checkpoint_handler::CheckpointHandler;
 use crate::handlers::event_handler::EventHandler;
 use crate::handlers::transaction_handler::TransactionHandler;
 
+use crate::handlers::object_event_handler::ObjectEventHandler;
+use crate::handlers::publish_event_handler::PublishEventHandler;
 const BACKOFF_MAX_INTERVAL_IN_SECS: u64 = 600;
 
 #[derive(Clone)]
@@ -45,6 +47,16 @@ impl HandlerOrchestrator {
             &self.prometheus_registry,
         );
         let event_handler = EventHandler::new(
+            self.rpc_client.clone(),
+            self.pg_connection_pool.clone(),
+            &self.prometheus_registry,
+        );
+        let obj_event_handler = ObjectEventHandler::new(
+            self.rpc_client.clone(),
+            self.pg_connection_pool.clone(),
+            &self.prometheus_registry,
+        );
+        let publish_event_handler = PublishEventHandler::new(
             self.rpc_client.clone(),
             self.pg_connection_pool.clone(),
             &self.prometheus_registry,
@@ -133,8 +145,66 @@ impl HandlerOrchestrator {
                 );
             }
         });
-        try_join_all(vec![checkpoint_handle, txn_handle, event_handle])
-            .await
-            .expect("Handler orchestrator shoult not run into errors.");
+        let object_event_handle = tokio::task::spawn(async move {
+            let backoff_config = ExponentialBackoffBuilder::new()
+                .with_max_interval(std::time::Duration::from_secs(BACKOFF_MAX_INTERVAL_IN_SECS))
+                .build();
+            let object_event_res = retry(backoff_config, || async {
+                let object_event_handler_exec_res = obj_event_handler.start().await;
+                if let Err(e) = object_event_handler_exec_res.clone() {
+                    obj_event_handler
+                        .event_handler_metrics
+                        .total_object_event_handler_error
+                        .inc();
+                    warn!(
+                        "Indexer object event handler failed with error: {:?}, retrying...",
+                        e
+                    );
+                }
+                Ok(object_event_handler_exec_res?)
+            })
+            .await;
+            if let Err(e) = object_event_res {
+                error!(
+                    "Indexer object event handler failed after retrials with error: {:?}",
+                    e
+                );
+            }
+        });
+        let publish_event_handle = tokio::task::spawn(async move {
+            let backoff_config = ExponentialBackoffBuilder::new()
+                .with_max_interval(std::time::Duration::from_secs(BACKOFF_MAX_INTERVAL_IN_SECS))
+                .build();
+            let publish_event_res = retry(backoff_config, || async {
+                let publish_event_handler_exec_res = publish_event_handler.start().await;
+                if let Err(e) = publish_event_handler_exec_res.clone() {
+                    publish_event_handler
+                        .event_handler_metrics
+                        .total_publish_event_handler_error
+                        .inc();
+                    warn!(
+                        "Indexer publish event handler failed with error: {:?}, retrying...",
+                        e
+                    );
+                }
+                Ok(publish_event_handler_exec_res?)
+            })
+            .await;
+            if let Err(e) = publish_event_res {
+                error!(
+                    "Indexer publish event handler failed after retrials with error: {:?}",
+                    e
+                );
+            }
+        });
+        try_join_all(vec![
+            txn_handle,
+            event_handle,
+            checkpoint_handle,
+            object_event_handle,
+            publish_event_handle,
+        ])
+        .await
+        .expect("Handler orchestrator shoult not run into errors.");
     }
 }
--- crates/sui-indexer/src/models/mod.rs
+++ crates/sui-indexer/src/models/mod.rs
@@ -14,3 +14,8 @@ pub mod package_logs;
 pub mod packages;
 pub mod transaction_logs;
 pub mod transactions;
+// TODO: remove below after wave 2
+pub mod object_event_logs;
+pub mod object_events;
+pub mod publish_event_logs;
+pub mod publish_events;
