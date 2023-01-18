bae60917bbee8b0a5c8685d2dc4132e91129370f
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2023-01-17 18:43:23 -0700

    indexer: event handlers for new objects and new modules (#7455)
    b/c of throughput regression along with table size, indexer was not able
    to stay updated on private testnet.
    This PR adds separate event handlers that only reads new modules / new
    objects.
    
    Co-authored-by: Chris Li <76067158+666lcz@users.noreply.github.com>

--- crates/sui-indexer/src/schema.rs
+++ crates/sui-indexer/src/schema.rs
@@ -66,6 +66,25 @@ diesel::table! {
 }
 
 diesel::table! {
+    object_event_logs (id) {
+        id -> Int4,
+        next_cursor_tx_dig -> Nullable<Text>,
+        next_cursor_event_seq -> Nullable<Int8>,
+    }
+}
+
+diesel::table! {
+    object_events (id) {
+        id -> Int8,
+        transaction_digest -> Nullable<Varchar>,
+        event_sequence -> Int8,
+        event_time -> Nullable<Timestamp>,
+        event_type -> Varchar,
+        event_content -> Varchar,
+    }
+}
+
+diesel::table! {
     object_logs (last_processed_id) {
         last_processed_id -> Int8,
     }
@@ -97,6 +116,25 @@ diesel::table! {
         author -> Text,
         module_names -> Array<Nullable<Text>>,
         package_content -> Text,
+    }
+}
+
+diesel::table! {
+    publish_event_logs (id) {
+        id -> Int4,
+        next_cursor_tx_dig -> Nullable<Text>,
+        next_cursor_event_seq -> Nullable<Int8>,
+    }
+}
+
+diesel::table! {
+    publish_events (id) {
+        id -> Int8,
+        transaction_digest -> Nullable<Varchar>,
+        event_sequence -> Int8,
+        event_time -> Nullable<Timestamp>,
+        event_type -> Varchar,
+        event_content -> Varchar,
     }
 }
 
@@ -139,10 +177,14 @@ diesel::allow_tables_to_appear_in_same_query!(
     error_logs,
     event_logs,
     events,
+    object_event_logs,
+    object_events,
     object_logs,
     objects,
     package_logs,
     packages,
+    publish_event_logs,
+    publish_events,
     transaction_logs,
     transactions,
 );
