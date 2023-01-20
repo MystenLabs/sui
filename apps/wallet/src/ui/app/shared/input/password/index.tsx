3caa7171bfcb1950cd1fe6e12f99e9bd4c29e075
Jibz <godwinjibs@gmail.com>
2023-01-20 13:42:09 -0500

    wip

--- apps/wallet/src/ui/app/shared/input/password/index.tsx
+++ apps/wallet/src/ui/app/shared/input/password/index.tsx
@@ -21,7 +21,7 @@ function PasswordInput({ field, meta, form, ...props }: PasswordInputProps) {
                 {...field}
                 {...props}
                 className={cl(
-                    'h-11 w-full text-body text-steel-dark font-medium',
+                    'h-11 w-full text-body text-steel-dark font-medium flex items-center gap-5 bg-white py-2 pr-0 pl-3 border border-solid border-gray-50 ',
                     st.input
                 )}
                 placeholder="Password"
