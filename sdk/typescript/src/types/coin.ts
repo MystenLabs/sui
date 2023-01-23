d0aa61266ba5f59ef9abe6c74814b50a9c55d35d
Andreas Xydas <andreas@mystenlabs.com>
2023-01-23 13:28:26 +0200

    add previousTransaction in CoinStruct

--- sdk/typescript/src/types/coin.ts
+++ sdk/typescript/src/types/coin.ts
@@ -11,6 +11,7 @@ export const CoinStruct = object({
     digest: TransactionDigest,
     balance: number(),
     lockedUntilEpoch: nullable(number()),
+    previousTransaction: TransactionDigest,
 });
 
   
