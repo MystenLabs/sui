10d4e56266e13fa180a59432e4406b2ee4f751e1
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2023-01-23 15:28:14 -0700

    indexer: read events from txn effects

--- crates/sui-indexer/migrations/2022-11-18-220045_events/up.sql
+++ crates/sui-indexer/migrations/2022-11-18-220045_events/up.sql
@@ -1,11 +1,9 @@
 CREATE TABLE EVENTS (
     id BIGSERIAL PRIMARY KEY,
-    -- below 2 are from Event ID, tx_digest and event_seq
     transaction_digest VARCHAR(255) NOT NULL,
     event_sequence BIGINT NOT NULL,
     event_time TIMESTAMP,
     event_type VARCHAR NOT NULL,
     event_content VARCHAR NOT NULL,
-    next_cursor_transaction_digest VARCHAR(255),
-    next_cursor_event_sequence BIGINT
+    next_cursor_transaction_digest VARCHAR(255)
 );
