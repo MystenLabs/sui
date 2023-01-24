f8fb4f3235e4ff1ac894d52bae15cc85dc26e4fc
Jordan Gensler <jordan@mystenlabs.com>
2023-01-24 13:47:22 -0800

    Autoconnect and leaderboard

---
+++ dapps/frenemies/src/utils/useAutoconnect.ts
@@ -0,0 +1,27 @@
+import { useWalletKit } from "@mysten/wallet-kit";
+import { useEffect, useRef } from "react";
+
+const AUTOCONNECT_KEY = "frenemies:wallet";
+
+export function useAutoconnect() {
+  const { wallets, currentWallet, connect } = useWalletKit();
+  const previousWallet = useRef(currentWallet);
+
+  useEffect(() => {
+    const storedWallet = localStorage.getItem(AUTOCONNECT_KEY);
+    // Initial load:
+    if (!currentWallet && !previousWallet.current && storedWallet) {
+      connect(storedWallet);
+    }
+    // Disconnect:
+    if (!currentWallet && previousWallet.current) {
+      localStorage.removeItem(AUTOCONNECT_KEY);
+    }
+    // Connect:
+    if (currentWallet) {
+      localStorage.setItem(AUTOCONNECT_KEY, currentWallet.name);
+    }
+
+    previousWallet.current = currentWallet;
+  }, [wallets, currentWallet]);
+}
