f4a004ffbcff1fbaf07b73389b43422cb9b28230
Arun Koshy <97870774+arun-koshy@users.noreply.github.com>
2023-01-23 22:38:15 -0800

    Address final review comments

--- crates/sui-benchmark/src/drivers/mod.rs
+++ crates/sui-benchmark/src/drivers/mod.rs
@@ -82,7 +82,7 @@ impl StressStats {
         table
             .set_content_arrangement(ContentArrangement::Dynamic)
             .set_width(200)
-            .set_header(vec!["-", "p50", "p99"]);
+            .set_header(vec!["metric", "p50", "p99"]);
 
         let mut row = Row::new();
         row.add_cell(Cell::new("cpu usage"));
@@ -121,14 +121,9 @@ impl BenchmarkStats {
                 "duration(s)",
                 "tps",
                 "error%",
-                "min",
-                "p25",
-                "p50",
-                "p75",
-                "p90",
-                "p99",
-                "p99.9",
-                "max",
+                "latency (min)",
+                "latency (p50)",
+                "latency (p99)",
             ]);
         let mut row = Row::new();
         row.add_cell(Cell::new(self.duration.as_secs()));
@@ -137,15 +132,8 @@ impl BenchmarkStats {
             (100 * self.num_error) as f32 / (self.num_error + self.num_success) as f32,
         ));
         row.add_cell(Cell::new(self.latency_ms.histogram.min()));
-        row.add_cell(Cell::new(self.latency_ms.histogram.value_at_quantile(0.25)));
         row.add_cell(Cell::new(self.latency_ms.histogram.value_at_quantile(0.5)));
-        row.add_cell(Cell::new(self.latency_ms.histogram.value_at_quantile(0.75)));
-        row.add_cell(Cell::new(self.latency_ms.histogram.value_at_quantile(0.9)));
         row.add_cell(Cell::new(self.latency_ms.histogram.value_at_quantile(0.99)));
-        row.add_cell(Cell::new(
-            self.latency_ms.histogram.value_at_quantile(0.999),
-        ));
-        row.add_cell(Cell::new(self.latency_ms.histogram.max()));
         table.add_row(row);
         table
     }
