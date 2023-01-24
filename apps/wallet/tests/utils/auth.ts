3d229199c14727ad1039ee4a699e4f49dcb11725
Jordan Gensler <jordan@mystenlabs.com>
2023-01-24 10:08:07 -0800

    Fix e2e tests for wallet

--- apps/wallet/tests/utils/auth.ts
+++ apps/wallet/tests/utils/auth.ts
@@ -11,12 +11,12 @@ export async function createWallet(page: Page, extensionUrl: string) {
     await page.getByRole('link', { name: /Create a New Wallet/ }).click();
     await page.getByLabel('Create Password').fill('mystenlabs');
     await page.getByLabel('Confirm Password').fill('mystenlabs');
-    // TODO: Clicking checkbox should be improved:
     await page
         .locator('label', { has: page.locator('input[type=checkbox]') })
-        .locator('span')
-        .nth(0)
         .click();
     await page.getByRole('button', { name: /Create Wallet/ }).click();
+    await page
+        .locator('label', { has: page.locator('input[type=checkbox]') })
+        .click();
     await page.getByRole('button', { name: /Open Sui Wallet/ }).click();
 }
