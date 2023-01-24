ca7deeae3ddf46e04b6e24232b3b3831789fabbf
Jordan Gensler <jordan@mystenlabs.com>
2023-01-23 21:42:01 -0800

    Fetch sui balance

--- dapps/frenemies/src/components/Validators/Balance.tsx
+++ dapps/frenemies/src/components/Validators/Balance.tsx
@@ -1,7 +1,26 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import BigNumber from 'bignumber.js';
+import { SUI_TYPE_ARG } from "@mysten/sui.js";
+import { useWalletKit } from "@mysten/wallet-kit";
+import { useQuery } from "@tanstack/react-query";
+import provider from "../../network/provider";
+
 export function Balance() {
+  const { currentAccount } = useWalletKit();
+  const { data } = useQuery(
+    ["account", "balance"],
+    async () => {
+      const { decimals }= await provider.getCoinMetadata(SUI_TYPE_ARG);
+      const { totalBalance } = await provider.getBalance(currentAccount!, SUI_TYPE_ARG);
+      return new BigNumber(totalBalance).shiftedBy(-1 * decimals).toFormat();
+    },
+    {
+      enabled: !!currentAccount,
+    }
+  );
+
   return (
     <div className="rounded-full shadow-notification bg-white px-4 py-1 flex items-center gap-11">
       <div>
@@ -9,10 +28,12 @@ export function Balance() {
           In your wallet
         </div>
         <div className="text-steel-dark">
-          <span className="font-semibold">1000</span> SUI
+          <span className="font-semibold">{data ?? '--'}</span> SUI
         </div>
       </div>
-      <div className="rounded-full h-7 w-7 bg-sui"></div>
+      <div>
+        <img src="/sui_icon.svg" />
+      </div>
     </div>
   );
 }
