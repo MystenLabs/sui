b5d3afbb20d14edd0941c816a5aafabd8a67ceb8
Jibz <godwinjibs@gmail.com>
2023-01-23 19:56:45 -0500

    update

--- apps/explorer/src/components/top-validators-card/TopValidatorsCard.tsx
+++ apps/explorer/src/components/top-validators-card/TopValidatorsCard.tsx
@@ -50,7 +50,12 @@ const validatorsTable = (
     const validators = processValidators(
         validatorsData.validators.fields.active_validators,
         totalStake
-    ).sort((a, b) => (a.name > b.name ? 1 : -1));
+    ).sort((a, b) =>
+        a.name.localeCompare(b.name, 'en', {
+            sensitivity: 'base',
+            numeric: true,
+        })
+    );
 
     const validatorsItems = limit ? validators.splice(0, limit) : validators;
 
