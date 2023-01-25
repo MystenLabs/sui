b9f6edb9c7059e9ffa49f68f7c598258109c7534
Andrey Chursin <andrey@mystenlabs.com>
2023-01-25 15:11:14 -0800

    [RWLocks] Implement immutable shared object reference
    This PR implements read-only shared object references.
    
    We modify scheduling of transactions with shared object arguments where `mutable: false` flag set so that they do not 'conflict' with each other and can be executed in parallel.
    
    Note that transaction dependency tree is also affected by shared object mutability:
    
    * Transaction with read-only reference to shared object depends on the transaction that created the version of shared object that this read-only transaction uses.
    
    * Read-only transactions do not depend on each other
    
    * Next write transaction depends on transaction that previously updated an object, but not on read-only transaction(s) that read this object.
    
    See updated unit tests for details.
    
    More changes in future PRs:
    
    * Better integration with pruner

--- crates/sui-types/src/unit_tests/messages_tests.rs
+++ crates/sui-types/src/unit_tests/messages_tests.rs
@@ -733,10 +733,11 @@ fn test_change_epoch_transaction() {
     assert!(tx.contains_shared_object());
     assert_eq!(
         tx.shared_input_objects().next().unwrap(),
-        (
-            &SUI_SYSTEM_STATE_OBJECT_ID,
-            &SUI_SYSTEM_STATE_OBJECT_SHARED_VERSION
-        )
+        SharedInputObject {
+            id: SUI_SYSTEM_STATE_OBJECT_ID,
+            initial_shared_version: SUI_SYSTEM_STATE_OBJECT_SHARED_VERSION,
+            mutable: true,
+        }
     );
     assert!(tx.is_system_tx());
     assert_eq!(
