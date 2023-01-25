d4f3eae67a166a219630b90cf8302ff3097885e6
Anastasios Kichidis <akihidis@gmail.com>
2023-01-25 21:03:01 +0000

    [narwhal] tune connectivity parameters for primary & worker (#7649)
    Reducing anemo connectivity properties to ensure that both primary & worker nodes will attempt to reconnect faster.

--- narwhal/worker/src/worker.rs
+++ narwhal/worker/src/worker.rs
@@ -235,6 +235,9 @@ impl Worker {
             config.inbound_request_timeout_ms = Some(300_000);
             config.outbound_request_timeout_ms = Some(300_000);
             config.shutdown_idle_timeout_ms = Some(1_000);
+            config.connectivity_check_interval_ms = Some(2_000);
+            config.connection_backoff_ms = Some(1_000);
+            config.max_connection_backoff_ms = Some(20_000);
             config
         };
 
