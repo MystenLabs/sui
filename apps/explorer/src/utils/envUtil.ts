e65b67904b0f310e639ea9cecbf95bb206d92723
Jordan Gensler <jordan@mystenlabs.com>
2023-01-25 11:46:46 -0800

    [explorer] Default network to testnet (#7560)
    This changes the default network to testnet. Do not merge until Testnet
    is live.

--- apps/explorer/src/utils/envUtil.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-import { Network } from './api/rpcSetting';
-
-export const DEFAULT_NETWORK =
-    import.meta.env.VITE_NETWORK ||
-    (import.meta.env.DEV ? Network.LOCAL : Network.DEVNET);
