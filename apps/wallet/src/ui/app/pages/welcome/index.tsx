547a513a602fc0b13e90659d4f02851b3a7f991d
mamos-mysten <122397493+mamos-mysten@users.noreply.github.com>
2023-01-26 12:06:21 -0800

    [wallet] - add connected network name to wallet logo (#7700)
    add connected network name below the sui logo
    
    went through a few different iterations with this... this version ended
    up being the simplest with the least amount of code changes. the other
    iterations included:
    
    - add a `public` folder and serve the SVGs as static assets. this breaks
    down with the blue color treatment on the landing page
    - import logos as SVG components. required adding an svg-loader to
    wallet
    
    I can revive those branches if we think either one is a better approach.
    
    
    
    ![image](https://user-images.githubusercontent.com/122397493/214705046-4045d575-aa3e-4987-8729-769faf18e755.png)
    
    ![image](https://user-images.githubusercontent.com/122397493/214705164-d09cd934-d4d9-485c-834e-57a4382092b4.png)

--- apps/wallet/src/ui/app/pages/welcome/index.tsx
+++ apps/wallet/src/ui/app/pages/welcome/index.tsx
@@ -24,11 +24,9 @@ const WelcomePage = () => {
                     <div className="pt-6 rounded-20 bg-alice-blue shadow-wallet-content flex flex-col flex-nowrap items-center justify-center w-popup-width h-popup-height">
                         <BottomMenuLayout>
                             <Content className="flex flex-col flex-nowrap items-center p-7.5 pb-0">
-                                <Logo
-                                    size="normal"
-                                    className="text-hero mt-7.5"
-                                    txt={true}
-                                />
+                                <div className="mt-7.5 text-hero">
+                                    <Logo />
+                                </div>
                                 <div className="mx-auto text-center mt-12">
                                     <Heading
                                         variant="heading2"
