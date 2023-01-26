f4165364654adcb2773b5a0db9a081f77ebe0c28
Jordan Gensler <jordan@mystenlabs.com>
2023-01-25 16:59:10 -0800

    [wallet-ext] Resolve all attributes always (#7706)
    This resolves all attributes whenever we set them. This lets us resolve
    features consistently.

--- apps/wallet/src/shared/utils/index.ts
+++ apps/wallet/src/shared/utils/index.ts
@@ -7,10 +7,9 @@ import Browser from 'webextension-polyfill';
 
 import { trackPageview, trackEvent } from '../plausible';
 import { useAppSelector } from '_hooks';
-import { growthbook } from '_src/ui/app/experimentation/feature-gating';
+import { setAttributes } from '_src/ui/app/experimentation/feature-gating';
 
 export const MAIN_UI_URL = Browser.runtime.getURL('ui.html');
-const WALLET_VERSION = Browser.runtime.getManifest().version;
 
 export function openInNewTab() {
     return Browser.tabs.create({ url: MAIN_UI_URL });
@@ -24,15 +23,7 @@ export function usePageView() {
         customRPC && apiEnv === 'customRPC' ? customRPC : apiEnv.toUpperCase();
 
     useEffect(() => {
-        // NOTE: This is a hack to work around hook timing issues with the Growthbook SDK.
-        // Issue: https://github.com/growthbook/growthbook/issues/915
-        setTimeout(() => {
-            growthbook.setAttributes({
-                network: activeNetwork,
-                version: WALLET_VERSION,
-                beta: process.env.WALLET_BETA || false,
-            });
-        }, 0);
+        setAttributes(activeNetwork);
 
         trackPageview({
             url: location.pathname,
