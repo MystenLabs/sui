7ce20ea4c5706e5f50908690545f220cd3de451a
Jordan Gensler <jordan@mystenlabs.com>
2023-01-25 16:36:23 -0800

    Rework attributes

--- apps/wallet/src/ui/app/experimentation/feature-gating.ts
+++ apps/wallet/src/ui/app/experimentation/feature-gating.ts
@@ -2,5 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { GrowthBook } from '@growthbook/growthbook';
+import Browser from 'webextension-polyfill';
 
 export const growthbook = new GrowthBook();
+
+const WALLET_VERSION = Browser.runtime.getManifest().version;
+
+export function setAttributes(network?: string | null) {
+    growthbook.setAttributes({
+        network,
+        version: WALLET_VERSION,
+        beta: process.env.WALLET_BETA || false,
+    });
+}
