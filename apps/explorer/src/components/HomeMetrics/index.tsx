01fa843b89870b2ca0af5dbfde84f3cdabfa6a8b
Jordan Gensler <jordan@mystenlabs.com>
2023-01-26 12:06:00 -0800

    [explorer] Homepage UI tweaks (#7726)

--- apps/explorer/src/components/HomeMetrics/index.tsx
+++ apps/explorer/src/components/HomeMetrics/index.tsx
@@ -11,7 +11,7 @@ import { useAppsBackend } from '~/hooks/useAppsBackend';
 import { useGetSystemObject } from '~/hooks/useGetObject';
 import { Card } from '~/ui/Card';
 import { Heading } from '~/ui/Heading';
-import { Stats } from '~/ui/Stats';
+import { Stats, type StatsProps } from '~/ui/Stats';
 import { formatAmount } from '~/utils/formatAmount';
 import { GROWTHBOOK_FEATURES } from '~/utils/growthbook';
 
@@ -29,6 +29,15 @@ interface TPSCheckpointResponse {
 
 function roundFloat(number: number, decimals: number) {
     return parseFloat(number.toFixed(decimals));
+}
+
+// Simple wrapper around stats to avoid text wrapping:
+function StatsWrapper(props: StatsProps) {
+    return (
+        <div className="flex-shrink-0">
+            <Stats {...props} />
+        </div>
+    );
 }
 
 export function HomeMetrics() {
@@ -60,50 +69,50 @@ export function HomeMetrics() {
 
             <div className="mt-8 space-y-7">
                 <MetricGroup label="Current">
-                    <Stats label="TPS" tooltip="Transactions per second">
+                    <StatsWrapper label="TPS" tooltip="Transactions per second">
                         {tpsData?.tps ? roundFloat(tpsData.tps, 2) : null}
-                    </Stats>
-                    <Stats label="Gas Price" tooltip="Current gas price">
+                    </StatsWrapper>
+                    <StatsWrapper label="Gas Price" tooltip="Current gas price">
                         {systemData?.reference_gas_price
                             ? `${systemData?.reference_gas_price} MIST`
                             : null}
-                    </Stats>
-                    <Stats label="Epoch" tooltip="The current epoch">
+                    </StatsWrapper>
+                    <StatsWrapper label="Epoch" tooltip="The current epoch">
                         {systemData?.epoch}
-                    </Stats>
-                    <Stats
+                    </StatsWrapper>
+                    <StatsWrapper
                         label="Checkpoint"
                         tooltip="The current checkpoint (updates every one min)"
                     >
                         {tpsData?.checkpoint}
-                    </Stats>
+                    </StatsWrapper>
                 </MetricGroup>
 
                 <MetricGroup label="Total">
-                    <Stats
+                    <StatsWrapper
                         label="Packages"
                         tooltip="Total packages counter (updates every one min)"
                     >
                         {formatAmount(countsData?.packages)}
-                    </Stats>
-                    <Stats
+                    </StatsWrapper>
+                    <StatsWrapper
                         label="Objects"
                         tooltip="Total objects counter (updates every one min)"
                     >
                         {formatAmount(countsData?.objects)}
-                    </Stats>
-                    <Stats
+                    </StatsWrapper>
+                    <StatsWrapper
                         label="Transactions"
                         tooltip="Total transactions counter (updates every one min)"
                     >
                         {formatAmount(countsData?.transactions)}
-                    </Stats>
-                    <Stats
+                    </StatsWrapper>
+                    <StatsWrapper
                         label="Addresses"
                         tooltip="Total addresses counter (updates every one min)"
                     >
                         {formatAmount(countsData?.addresses)}
-                    </Stats>
+                    </StatsWrapper>
                 </MetricGroup>
             </div>
         </Card>
