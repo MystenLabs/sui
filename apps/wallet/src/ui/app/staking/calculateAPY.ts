735bd4d0bd867bf7df9ae35c1d4c114bb9f68787
Jibz1 <godwinjibs@gmail.com>
2023-01-26 18:21:18 -0800

    Set APYs greater than 100_000 percent to 0 (#7709)
    <img width="744" alt="Screenshot 2023-01-26 at 1 44 09 AM"
    src="https://user-images.githubusercontent.com/8676844/214773578-96b47784-9871-4c2c-90b6-e10db85f3c3a.png">

--- apps/wallet/src/ui/app/staking/calculateAPY.ts
+++ apps/wallet/src/ui/app/staking/calculateAPY.ts
@@ -12,11 +12,17 @@ export function calculateAPY(validators: ActiveValidator, epoch: number) {
         validators.fields.delegation_staking_pool.fields;
 
     const num_epochs_participated = +epoch - +starting_epoch;
-    const apy = Math.pow(
-        1 +
-            (+sui_balance - +delegation_token_supply.fields.value) /
-                +delegation_token_supply.fields.value,
-        365 / num_epochs_participated - 1
-    );
-    return apy ? roundFloat(apy, APY_DECIMALS) : 0;
+    const apy =
+        Math.pow(
+            1 +
+                (+sui_balance - +delegation_token_supply.fields.value) /
+                    +delegation_token_supply.fields.value,
+            365 / num_epochs_participated
+        ) - 1;
+
+    //guard against NaN
+    const apyReturn = apy ? roundFloat(apy, APY_DECIMALS) : 0;
+
+    // guard against very large numbers (e.g. 1e+100)
+    return apyReturn > 100_000 ? 0 : apyReturn;
 }
