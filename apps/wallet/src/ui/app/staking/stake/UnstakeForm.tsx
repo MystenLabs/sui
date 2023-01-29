407fc209e97e5e61dd40a9554e5682a7b595ac5a
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2023-01-29 21:23:08 +0000

    wallet-ext: take into account the gas price when calculating gas budget

--- apps/wallet/src/ui/app/staking/stake/UnstakeForm.tsx
+++ apps/wallet/src/ui/app/staking/stake/UnstakeForm.tsx
@@ -5,7 +5,8 @@ import { SUI_TYPE_ARG } from '@mysten/sui.js';
 import { ErrorMessage, Form, useFormikContext } from 'formik';
 import { useEffect, useRef } from 'react';
 
-import { Content } from '_app/shared/bottom-menu-layout';
+import LoadingIndicator from '../../components/loading/LoadingIndicator';
+import { useGasBudgetInMist } from '../../hooks/useGasBudgetInMist';
 import { Card } from '_app/shared/card';
 import { Text } from '_app/shared/text';
 import Alert from '_components/alert';
@@ -34,10 +35,10 @@ export function UnStakeForm({
     const onClearRef = useRef(onClearSubmitError);
     onClearRef.current = onClearSubmitError;
 
-    const [gasBudgetEstimation, symbol] = useFormatCoin(
-        DEFAULT_GAS_BUDGET_FOR_STAKE,
-        SUI_TYPE_ARG
+    const { gasBudget, isLoading } = useGasBudgetInMist(
+        DEFAULT_GAS_BUDGET_FOR_STAKE
     );
+    const [gasBudgetFormatted, symbol] = useFormatCoin(gasBudget, SUI_TYPE_ARG);
 
     const [rewards, rewardSymbol] = useFormatCoin(stakingReward, SUI_TYPE_ARG);
 
@@ -54,106 +55,104 @@ export function UnStakeForm({
             autoComplete="off"
             noValidate
         >
-            <Content>
-                <Card
-                    variant="gray"
-                    titleDivider
-                    header={
-                        <div className="px-4 py-3 w-full flex bg-white justify-between">
+            <Card
+                variant="gray"
+                titleDivider
+                header={
+                    <div className="px-4 py-3 w-full flex bg-white justify-between">
+                        <Text
+                            variant="body"
+                            weight="medium"
+                            color="steel-darker"
+                        >
+                            Your Stake
+                        </Text>
+                        <div className="flex gap-0.5 items-end">
                             <Text
-                                variant="body"
-                                weight="medium"
-                                color="steel-darker"
-                            >
-                                Your Stake
-                            </Text>
-                            <div className="flex gap-0.5 items-end">
-                                <Text
-                                    variant="heading4"
-                                    weight="semibold"
-                                    color="steel-darker"
-                                >
-                                    {tokenBalance}
-                                </Text>
-                                <Text
-                                    variant="bodySmall"
-                                    weight="medium"
-                                    color="steel-dark"
-                                >
-                                    {symbol}
-                                </Text>
-                            </div>
-                        </div>
-                    }
-                    footer={
-                        <div className="py-px flex justify-between w-full">
-                            <Text
-                                variant="body"
-                                weight="medium"
-                                color="steel-darker"
-                            >
-                                Gas Fees
-                            </Text>
-                            <Text
-                                variant="body"
-                                weight="medium"
-                                color="steel-darker"
-                            >
-                                {gasBudgetEstimation} {symbol}
-                            </Text>
-                        </div>
-                    }
-                >
-                    <div className="pb-3.75 flex flex-col  w-full gap-2">
-                        <div className="flex gap-0.5 justify-between w-full">
-                            <Text
-                                variant="body"
+                                variant="heading4"
                                 weight="semibold"
                                 color="steel-darker"
                             >
-                                Staking Rewards
+                                {tokenBalance}
                             </Text>
                             <Text
-                                variant="body"
+                                variant="bodySmall"
                                 weight="medium"
-                                color="steel-darker"
+                                color="steel-dark"
                             >
-                                {rewards} {rewardSymbol}
-                            </Text>
-                        </div>
-                        <div className="w-2/3">
-                            <Text
-                                variant="p2"
-                                weight="medium"
-                                color="steel-darker"
-                            >
-                                Distributed at end of current Epoch
+                                {symbol}
                             </Text>
                         </div>
                     </div>
-                </Card>
-                <ErrorMessage name="amount" component="div">
-                    {(msg) => (
-                        <div className="mt-2 flex flex-col flex-nowrap">
-                            <Alert mode="warning" className="text-body">
-                                {msg}
-                            </Alert>
-                        </div>
-                    )}
-                </ErrorMessage>
-
-                {submitError ? (
+                }
+                footer={
+                    <div className="py-px flex justify-between w-full">
+                        <Text
+                            variant="body"
+                            weight="medium"
+                            color="steel-darker"
+                        >
+                            Gas Fees
+                        </Text>
+                        <Text
+                            variant="body"
+                            weight="medium"
+                            color="steel-darker"
+                        >
+                            {isLoading ? (
+                                <LoadingIndicator />
+                            ) : (
+                                `${gasBudgetFormatted} ${symbol}`
+                            )}
+                        </Text>
+                    </div>
+                }
+            >
+                <div className="pb-3.75 flex flex-col  w-full gap-2">
+                    <div className="flex gap-0.5 justify-between w-full">
+                        <Text
+                            variant="body"
+                            weight="semibold"
+                            color="steel-darker"
+                        >
+                            Staking Rewards
+                        </Text>
+                        <Text
+                            variant="body"
+                            weight="medium"
+                            color="steel-darker"
+                        >
+                            {rewards} {rewardSymbol}
+                        </Text>
+                    </div>
+                    <div className="w-2/3">
+                        <Text variant="p2" weight="medium" color="steel-darker">
+                            Distributed at end of current Epoch
+                        </Text>
+                    </div>
+                </div>
+            </Card>
+            <ErrorMessage name="amount" component="div">
+                {(msg) => (
                     <div className="mt-2 flex flex-col flex-nowrap">
-                        <Alert mode="warning">
-                            <strong>Unstake failed</strong>
-
-                            <div>
-                                <small>{submitError}</small>
-                            </div>
+                        <Alert mode="warning" className="text-body">
+                            {msg}
                         </Alert>
                     </div>
-                ) : null}
-            </Content>
+                )}
+            </ErrorMessage>
+
+            {submitError ? (
+                <div className="mt-2 flex flex-col flex-nowrap">
+                    <Alert mode="warning">
+                        <strong>Unstake failed</strong>
+
+                        <div>
+                            <small>{submitError}</small>
+                        </div>
+                    </Alert>
+                </div>
+            ) : null}
         </Form>
     );
 }
