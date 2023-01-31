d3629ebd14c04f28b4c318b013c095428edff6a6
Jibz <godwinjibs@gmail.com>
2023-01-30 21:10:36 -0500

    transaction/receipt update

---
+++ apps/wallet/src/ui/app/components/receipt-card/TxnGasSummery.tsx
@@ -0,0 +1,60 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useFormatCoin } from '_hooks';
+import { GAS_TYPE_ARG } from '_redux/slices/sui-objects/Coin';
+import { Text } from '_src/ui/app/shared/text';
+
+import type { GasCostSummary } from '@mysten/sui.js';
+
+type TxnGasSummeryProps = {
+    gasSummary?: GasCostSummary;
+    totalGas: number;
+    transferAmount: number | null;
+};
+
+//TODO add gas breakdown
+export function TxnGasSummery({
+    gasSummary,
+    totalGas,
+    transferAmount,
+}: TxnGasSummeryProps) {
+    const [totalAmount, totalAmountSymbol] = useFormatCoin(
+        totalGas + (transferAmount || 0),
+        GAS_TYPE_ARG
+    );
+    const [gas, symbol] = useFormatCoin(totalGas, GAS_TYPE_ARG);
+
+    return (
+        <div className="flex flex-col w-full items-center pt-3.5 gap-3.75 border-t border-solid border-steel/20 border-x-0 border-b-0 ">
+            <div className="flex justify-between items-center w-full">
+                <Text variant="body" weight="medium" color="steel-darker">
+                    Gas Fees
+                </Text>
+
+                <div className="flex gap-1 items-center">
+                    <Text variant="body" weight="medium" color="steel-darker">
+                        {gas} {symbol}
+                    </Text>
+                </div>
+            </div>
+            {transferAmount ? (
+                <div className="flex justify-between items-center w-full">
+                    <Text variant="body" weight="medium" color="steel-darker">
+                        Total Amount
+                    </Text>
+
+                    <div className="flex gap-1 items-center">
+                        <Text
+                            variant="body"
+                            weight="medium"
+                            color="steel-darker"
+                        >
+                            {totalAmount} {totalAmountSymbol}
+                        </Text>
+                    </div>
+                </div>
+            ) : null}
+        </div>
+    );
+}
--- apps/wallet/src/ui/app/components/transactions-card/TxnImage.tsx
+++ apps/wallet/src/ui/app/components/transactions-card/TxnImage.tsx
@@ -5,37 +5,49 @@ import { Text } from '_app/shared/text';
 import { useGetNFTMeta } from '_hooks';
 
 //TODO merge all NFT image displays
-export function TxnImage({ id }: { id: string }) {
+export function TxnImage({ id, label }: { id: string; label?: string }) {
     const nftMeta = useGetNFTMeta(id);
 
     return nftMeta?.url ? (
-        <div className="flex w-full gap-2">
-            <img
-                src={nftMeta.url.replace(/^ipfs:\/\//, 'https://ipfs.io/ipfs/')}
-                className="w-10 h-10 rounded"
-                alt={nftMeta.name || 'Nft image'}
-            />
-            <div className="flex flex-col gap-1 justify-center break-all w-56">
-                {nftMeta.name && (
-                    <Text
-                        color="gray-90"
-                        weight="semibold"
-                        variant="subtitleSmall"
-                        truncate
-                    >
-                        {nftMeta.name}
-                    </Text>
-                )}
-                {nftMeta.description && (
-                    <Text
-                        color="steel-darker"
-                        weight="medium"
-                        variant="subtitleSmall"
-                        truncate
-                    >
-                        {nftMeta.description}
-                    </Text>
-                )}
+        <div className="flex gap-2 flex-col">
+            {label && (
+                <Text variant="body" weight="medium" color="steel-darker">
+                    {label}
+                </Text>
+            )}
+            <div className="flex w-full gap-2">
+                <div className="flex gap-2">
+                    <img
+                        src={nftMeta.url.replace(
+                            /^ipfs:\/\//,
+                            'https://ipfs.io/ipfs/'
+                        )}
+                        className="w-10 h-10 rounded"
+                        alt={nftMeta.name || 'Nft image'}
+                    />
+                </div>
+                <div className="flex flex-col gap-1 justify-center break-all w-56">
+                    {nftMeta.name && (
+                        <Text
+                            color="gray-90"
+                            weight="semibold"
+                            variant="subtitleSmall"
+                            truncate
+                        >
+                            {nftMeta.name}
+                        </Text>
+                    )}
+                    {nftMeta.description && (
+                        <Text
+                            color="steel-darker"
+                            weight="medium"
+                            variant="subtitleSmall"
+                            truncate
+                        >
+                            {nftMeta.description}
+                        </Text>
+                    )}
+                </div>
             </div>
         </div>
     ) : null;
