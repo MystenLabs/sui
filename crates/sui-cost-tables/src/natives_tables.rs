c6652fc20e881b0b6c2684f6e3f7cc691c1ddccf
oxade <93547199+oxade@users.noreply.github.com>
2023-02-02 08:39:37 -0500

    Event count limits (#7804)
    Limits the number of events to 256 using the object runtime.

--- crates/sui-cost-tables/src/natives_tables.rs
+++ crates/sui-cost-tables/src/natives_tables.rs
@@ -1,9 +1,6 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::units_types::GasCost;
-use sui_protocol_constants::*;
-
 //
 // Native function costs
 //
@@ -25,32 +22,4 @@ pub enum SuiNativeCostIndex {
 
     TX_CONTEXT_DERIVE_ID = 7,
     TX_CONTEXT_NEW_SIGNER_FROM_ADDR = 8,
-}
-
-// Native costs are currently flat
-// TODO recalibrate wrt bytecode costs
-pub fn _native_cost_schedule() -> Vec<GasCost> {
-    use SuiNativeCostIndex as N;
-
-    let mut native_table = vec![
-        // This is artificially chosen to limit too many event emits
-        // We will change this in https://github.com/MystenLabs/sui/issues/3341
-        (
-            N::EVENT_EMIT,
-            GasCost::new(MAX_TX_GAS / MAX_NUM_EVENT_EMIT, 1),
-        ),
-        (N::OBJECT_BYTES_TO_ADDR, GasCost::new(30, 1)),
-        (N::OBJECT_BORROW_UUID, GasCost::new(150, 1)),
-        (N::OBJECT_DELETE_IMPL, GasCost::new(100, 1)),
-        (N::TRANSFER_TRANSFER_INTERNAL, GasCost::new(80, 1)),
-        (N::TRANSFER_FREEZE_OBJECT, GasCost::new(80, 1)),
-        (N::TRANSFER_SHARE_OBJECT, GasCost::new(80, 1)),
-        (N::TX_CONTEXT_DERIVE_ID, GasCost::new(110, 1)),
-        (N::TX_CONTEXT_NEW_SIGNER_FROM_ADDR, GasCost::new(200, 1)),
-    ];
-    native_table.sort_by_key(|cost| cost.0 as u64);
-    native_table
-        .into_iter()
-        .map(|(_, cost)| cost)
-        .collect::<Vec<_>>()
 }
