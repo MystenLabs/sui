e86737689bee79c4e9bb51a83bf034f8881ac207
Jordan Gensler <jordan@mystenlabs.com>
2023-02-07 09:40:37 -0800

    Add autoconnect to wallet kit

---
+++ sdk/wallet-adapter/wallet-kit-core/src/storage.ts
@@ -0,0 +1,25 @@
+export interface StorageAdapter {
+  set(key: string, value: string): Promise<void>;
+  get(key: string): Promise<string | undefined | null>;
+  del(key: string): Promise<void>;
+}
+
+export const localStorageAdapter: StorageAdapter = {
+  async set(key, value) {
+    return localStorage.setItem(key, value);
+  },
+  async get(key) {
+    return localStorage.getItem(key);
+  },
+  async del(key) {
+    localStorage.removeItem(key);
+  },
+};
+
+export const noopStorageAdapter: StorageAdapter = {
+  async set() {},
+  async get() {
+    return null;
+  },
+  async del() {},
+};
