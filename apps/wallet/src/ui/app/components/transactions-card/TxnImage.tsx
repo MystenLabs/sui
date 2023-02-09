cda21a450bd8502321c0c5c2307ec99bd5a6ea08
Jibz <godwinjibs@gmail.com>
2023-02-09 13:11:06 -0500

    change background color for brokenimage

--- apps/wallet/src/ui/app/components/transactions-card/TxnImage.tsx
+++ apps/wallet/src/ui/app/components/transactions-card/TxnImage.tsx
@@ -18,27 +18,36 @@ export function TxnImage({ id }: { id: string }) {
                 src={nftMeta.url}
             />
 
-            <div className="flex flex-col gap-1 justify-center break-all w-56">
-                {nftMeta.name && (
-                    <Text
-                        color="gray-90"
-                        weight="semibold"
-                        variant="subtitleSmall"
-                        truncate
-                    >
-                        {nftMeta.name}
-                    </Text>
-                )}
-                {nftMeta.description && (
-                    <Text
-                        color="steel-darker"
-                        weight="medium"
-                        variant="subtitleSmall"
-                        truncate
-                    >
-                        {nftMeta.description}
-                    </Text>
-                )}
+            <div className="flex w-full gap-2">
+                <NftImage
+                    borderRadius="sm"
+                    size="xs"
+                    name={nftMeta.name}
+                    src={nftMeta.url}
+                />
+
+                <div className="flex flex-col gap-1 justify-center break-all w-56">
+                    {nftMeta.name && (
+                        <Text
+                            color="gray-90"
+                            weight="semibold"
+                            variant="subtitleSmall"
+                            truncate
+                        >
+                            {nftMeta.name}
+                        </Text>
+                    )}
+                    {nftMeta.description && (
+                        <Text
+                            color="steel-darker"
+                            weight="medium"
+                            variant="subtitleSmall"
+                            truncate
+                        >
+                            {nftMeta.description}
+                        </Text>
+                    )}
+                </div>
             </div>
         </div>
     ) : null;
