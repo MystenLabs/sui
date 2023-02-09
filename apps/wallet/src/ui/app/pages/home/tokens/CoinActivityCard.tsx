e06b388301fd749090f1108a8026771515a0edb2
Jibz <godwinjibs@gmail.com>
2023-02-09 14:42:54 -0500

    rebase update

--- apps/wallet/src/ui/app/pages/home/tokens/CoinActivityCard.tsx
+++ apps/wallet/src/ui/app/pages/home/tokens/CoinActivityCard.tsx
@@ -45,10 +45,7 @@ export function CoinActivitiesCard({ coinType }: { coinType: string }) {
     return (
         <div className="flex flex-col flex-nowrap ">
             <div className="flex-grow overflow-y-auto px-5 -mx-5 divide-y divide-solid divide-gray-45 divide-x-0">
-                <Loading
-                    loading={isLoading}
-                    className="flex justify-center items-center h-full"
-                >
+                <Loading loading={isLoading}>
                     {txnByCoinType?.length && activeAddress
                         ? txnByCoinType.map((txn) => (
                               <ErrorBoundary
