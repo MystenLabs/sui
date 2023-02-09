42bb7a5f6a29d8b155c17bd5d7b353d580c88e7f
Jordan Gensler <jordan@mystenlabs.com>
2023-02-09 14:21:13 -0800

    Copy update

--- dapps/frenemies/src/routes/Offline.tsx
+++ dapps/frenemies/src/routes/Offline.tsx
@@ -2,15 +2,19 @@ import { Card } from "../components/Card";
 
 export function Offline() {
   return (
-    <div className="absolute top-1/2 left-1/2 max-w-2xl w-full -translate-x-1/2 -translate-y-1/2 px-4">
+    <div className="absolute top-1/2 left-1/2 max-w-4xl w-full -translate-x-1/2 -translate-y-1/2 px-4">
       <Card spacing="xl" variant="error">
         <div className="flex flex-col md:flex-row gap-16 items-center">
           <img src="/capy_cry.svg" alt="Sad Capy" className="flex-1" />
-          <div className="text-heading2">
+          <div className="text-heading2 leading-tight">
             <div className="font-bold">
-              We're aware of issues and are working to fix them.
+              Frenemies is currently offline while we upgrade the app to provide
+              players with the best experience.
             </div>
-            <div className="mt-4">Please check back soon.</div>
+            <div className="mt-4">
+              We expect Frenemies to be available again by Friday, February 10
+              at 12 p.m. PST.
+            </div>
           </div>
         </div>
       </Card>
