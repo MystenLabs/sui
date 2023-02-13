188452897843127bca6dd65f31b981366a7a2480
Jordan Gensler <jordan@mystenlabs.com>
2023-02-13 14:55:40 -0800

    Add license

--- dapps/frenemies/src/components/GameEnding.tsx
+++ dapps/frenemies/src/components/GameEnding.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { GAME_END_DATE, gameIsOver } from "../config";
--- dapps/frenemies/src/routes/Claim.tsx
+++ dapps/frenemies/src/routes/Claim.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import { ConnectButton } from "@mysten/wallet-kit";
 import { useWalletKit } from "@mysten/wallet-kit";
 import { Card } from "../components/Card";
