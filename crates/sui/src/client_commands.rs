2fa5c81eab8ff0b34a405f8b7b661d01306fcbaa
Andreas Xydas <andreas@mystenlabs.com>
2023-02-13 16:40:43 +0200

    Fix --bcs to get raw bytes

--- crates/sui/src/client_commands.rs
+++ crates/sui/src/client_commands.rs
@@ -32,7 +32,8 @@ use sui_types::error::SuiError;
 use sui_framework_build::compiled_package::BuildConfig;
 use sui_json::SuiJsonValue;
 use sui_json_rpc_types::{
-    DynamicFieldPage, GetObjectDataResponse, SuiObjectInfo, SuiParsedObject, SuiTransactionResponse,
+    DynamicFieldPage, GetObjectDataResponse, SuiObjectInfo, SuiParsedObject, SuiRawData,
+    SuiTransactionResponse,
 };
 use sui_json_rpc_types::{GetRawObjectDataResponse, SuiData};
 use sui_json_rpc_types::{SuiCertifiedTransaction, SuiExecutionStatus, SuiTransactionEffects};
@@ -530,8 +531,13 @@ impl SuiClientCommands {
             SuiClientCommands::Object { id, bcs } => {
                 // Fetch the object ref
                 let client = context.get_client().await?;
-                let object_read = client.read_api().get_parsed_object(id).await?;
-                SuiClientCommandResult::Object(object_read, bcs)
+                if !bcs {
+                    let object_read = client.read_api().get_parsed_object(id).await?;
+                    SuiClientCommandResult::Object(object_read)
+                } else {
+                    let raw_object_read = client.read_api().get_object(id).await?;
+                    SuiClientCommandResult::RawObject(raw_object_read)
+                }
             }
 
             SuiClientCommands::DynamicFieldQuery { id, cursor, limit } => {
@@ -1222,19 +1228,29 @@ impl Display for SuiClientCommandResult {
                     writeln!(writer, "{}", parsed_resp)?;
                 }
             }
-            SuiClientCommandResult::Object(object_read, bcs) => {
-                let object = if *bcs {
-                    match object_read.object() {
-                        Ok(v) => {
-                            let bcs_bytes = bcs::to_bytes(v).unwrap();
-                            format!("{:?}\nNumber of bytes: {}", bcs_bytes, bcs_bytes.len())
-                        }
-                        Err(err) => format!("{err}").red().to_string(),
-                    }
-                } else {
-                    unwrap_err_to_string(|| Ok(object_read.object()?))
-                };
+            SuiClientCommandResult::Object(object_read) => {
+                let object = unwrap_err_to_string(|| Ok(object_read.object()?));
                 writeln!(writer, "{}", object)?;
+            }
+            SuiClientCommandResult::RawObject(raw_object_read) => {
+                let raw_object = match raw_object_read.object() {
+                    Ok(v) => match &v.data {
+                        SuiRawData::MoveObject(o) => {
+                            format!("{:?}\nNumber of bytes: {}", o.bcs_bytes, o.bcs_bytes.len())
+                        }
+                        SuiRawData::Package(p) => {
+                            let mut temp = String::new();
+                            let mut bcs_bytes = 0usize;
+                            for m in &p.module_map {
+                                temp.push_str(&format!("{:?}\n", m));
+                                bcs_bytes += m.1.len()
+                            }
+                            format!("{}Number of bytes: {}", temp, bcs_bytes)
+                        }
+                    },
+                    Err(err) => format!("{err}").red().to_string(),
+                };
+                writeln!(writer, "{}", raw_object)?;
             }
             SuiClientCommandResult::Call(cert, effects) => {
                 write!(writer, "{}", write_cert_and_effects(cert, effects)?)?;
@@ -1498,13 +1514,13 @@ fn write_cert_and_effects(
 impl Debug for SuiClientCommandResult {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let s = unwrap_err_to_string(|| match self {
-            SuiClientCommandResult::Object(object_read, bcs) => {
+            SuiClientCommandResult::Object(object_read) => {
                 let object = object_read.object()?;
-                if *bcs {
-                    Ok(serde_json::to_string_pretty(&bcs::to_bytes(&object)?)?)
-                } else {
-                    Ok(serde_json::to_string_pretty(&object)?)
-                }
+                Ok(serde_json::to_string_pretty(&object)?)
+            }
+            SuiClientCommandResult::RawObject(raw_object_read) => {
+                let raw_object = raw_object_read.object()?;
+                Ok(serde_json::to_string_pretty(&raw_object)?)
             }
             _ => Ok(serde_json::to_string_pretty(self)?),
         });
@@ -1540,7 +1556,8 @@ impl SuiClientCommandResult {
 pub enum SuiClientCommandResult {
     Publish(SuiTransactionResponse),
     VerifySource,
-    Object(GetObjectDataResponse, bool),
+    Object(GetObjectDataResponse),
+    RawObject(GetRawObjectDataResponse),
     Call(SuiCertifiedTransaction, SuiTransactionEffects),
     Transfer(
         // Skipping serialisation for elapsed time.
--- crates/sui/src/unit_tests/cli_tests.rs
+++ crates/sui/src/unit_tests/cli_tests.rs
@@ -596,15 +596,12 @@ async fn test_native_transfer() -> Result<(), anyhow::Error> {
     }
     .execute(context)
     .await?;
-    let mut_obj1 = if let SuiClientCommandResult::Object(
-        GetObjectDataResponse::Exists(object),
-        false,
-    ) = resp
-    {
-        object
-    } else {
-        panic!()
-    };
+    let mut_obj1 =
+        if let SuiClientCommandResult::Object(GetObjectDataResponse::Exists(object)) = resp {
+            object
+        } else {
+            panic!()
+        };
 
     let resp = SuiClientCommands::Object {
         id: mut_obj2,
@@ -612,15 +609,12 @@ async fn test_native_transfer() -> Result<(), anyhow::Error> {
     }
     .execute(context)
     .await?;
-    let mut_obj2 = if let SuiClientCommandResult::Object(
-        GetObjectDataResponse::Exists(object),
-        false,
-    ) = resp
-    {
-        object
-    } else {
-        panic!()
-    };
+    let mut_obj2 =
+        if let SuiClientCommandResult::Object(GetObjectDataResponse::Exists(object)) = resp {
+            object
+        } else {
+            panic!()
+        };
 
     let (gas, obj) = if mut_obj1.owner.get_owner_address().unwrap() == address {
         (mut_obj1, mut_obj2)
@@ -669,8 +663,7 @@ async fn test_native_transfer() -> Result<(), anyhow::Error> {
 #[test]
 // Test for issue https://github.com/MystenLabs/sui/issues/1078
 fn test_bug_1078() {
-    let read =
-        SuiClientCommandResult::Object(GetObjectDataResponse::NotExists(ObjectID::random()), false);
+    let read = SuiClientCommandResult::Object(GetObjectDataResponse::NotExists(ObjectID::random()));
     let mut writer = String::new();
     // fmt ObjectRead should not fail.
     write!(writer, "{}", read).unwrap();
