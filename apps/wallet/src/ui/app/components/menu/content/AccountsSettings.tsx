237c4d46f0badf933b7d28a768a2dc8b4973e82d
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-02-14 22:24:34 +0000

    wallet-ext: account settings view support multiple accounts
    * display all the accounts of the user
    * update the ui

--- apps/wallet/src/ui/app/components/menu/content/AccountsSettings.tsx
+++ apps/wallet/src/ui/app/components/menu/content/AccountsSettings.tsx
@@ -1,25 +1,20 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { Account } from './Account';
 import { MenuLayout } from './MenuLayout';
-import AccountAddress from '_components/account-address';
 import { useNextMenuUrl } from '_components/menu/hooks';
-import { Text } from '_src/ui/app/shared/text';
+import { useAccounts } from '_src/ui/app/hooks/useAccounts';
 
 export function AccountsSettings() {
     const backUrl = useNextMenuUrl(true, '/');
+    const accounts = useAccounts();
     return (
         <MenuLayout title="Accounts" back={backUrl}>
             <div className="flex flex-col gap-3">
-                <Text color="gray-90" weight="medium" variant="p1">
-                    Address
-                </Text>
-                <AccountAddress
-                    mode="faded"
-                    shorten={true}
-                    showLink={false}
-                    copyable
-                />
+                {accounts.map(({ address }) => (
+                    <Account address={address} key={address} />
+                ))}
             </div>
         </MenuLayout>
     );
