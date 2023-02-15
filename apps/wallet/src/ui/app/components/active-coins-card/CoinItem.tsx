14199d0290ed7bd28af71ee0bd0cfb596920bc8d
Jibz <godwinjibs@gmail.com>
2023-02-15 08:47:27 -0500

    use coin meta information

--- apps/wallet/src/ui/app/components/active-coins-card/CoinItem.tsx
+++ apps/wallet/src/ui/app/components/active-coins-card/CoinItem.tsx
@@ -1,10 +1,14 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { useQuery } from '@tanstack/react-query';
+
+import Loading from '../loading';
 import { Text } from '_app/shared/text';
 import { CoinIcon } from '_components/coin-icon';
-import { useFormatCoin } from '_hooks';
+import { useRpc, useFormatCoin } from '_hooks';
 
+// Get coin metadata from the coin type
 export function CoinItem({
     coinType,
     balance,
@@ -12,31 +16,45 @@ export function CoinItem({
     coinType: string;
     balance: bigint;
 }) {
+    const rpc = useRpc();
+
+    // Get coin metadata from the coin type
+    // Include icon url and name of the coin
+    const { data, isLoading } = useQuery(
+        ['get-coin-meta', coinType],
+        async () => {
+            return rpc.getCoinMetadata(coinType);
+        },
+        { enabled: !!coinType }
+    );
+
     const [formatted, symbol] = useFormatCoin(balance, coinType);
 
     return (
-        <div className="flex gap-2.5 w-full" role="button">
-            <CoinIcon coinType={coinType} size="md" />
-            <div className="flex flex-col flex-1 gap-1.5">
-                <div className="flex flex-row justify-between">
-                    <Text variant="body" color="gray-90" weight="semibold">
-                        {symbol}
-                    </Text>
+        <Loading loading={isLoading}>
+            <div className="flex gap-2.5 w-full" role="button">
+                <CoinIcon coinType={coinType} size="md" />
+                <div className="flex flex-col flex-1 gap-1.5">
+                    <div className="flex flex-row justify-between">
+                        <Text variant="body" color="gray-90" weight="semibold">
+                            {data?.name || symbol}
+                        </Text>
 
-                    <Text variant="body" color="gray-90" weight="semibold">
-                        {formatted} {symbol}
-                    </Text>
-                </div>
-                <div className="flex flex-row">
-                    <Text
-                        variant="subtitle"
-                        color="steel-dark"
-                        weight="semibold"
-                    >
-                        {symbol}
-                    </Text>
+                        <Text variant="body" color="gray-90" weight="semibold">
+                            {formatted} {symbol}
+                        </Text>
+                    </div>
+                    <div className="flex flex-row">
+                        <Text
+                            variant="subtitle"
+                            color="steel-dark"
+                            weight="semibold"
+                        >
+                            {symbol}
+                        </Text>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Loading>
     );
 }
