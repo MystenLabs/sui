a1edaaab38783eb4760e9c615d0680fc2f824945
Brandon Williams <brandon@mystenlabs.com>
2023-02-16 17:04:02 -0800

    sui-tls: introduce SelfSignedCertificate type

--- crates/sui-tls/src/certgen.rs
+++ crates/sui-tls/src/certgen.rs
@@ -5,9 +5,42 @@ use fastcrypto::ed25519::Ed25519PrivateKey;
 use pkcs8::EncodePrivateKey;
 use rcgen::{CertificateParams, KeyPair, SignatureAlgorithm};
 
-pub fn generate_self_signed_tls_certificate(
+pub struct SelfSignedCertificate {
+    inner: rcgen::Certificate,
+}
+
+impl SelfSignedCertificate {
+    pub fn new(private_key: Ed25519PrivateKey, server_name: &str) -> Self {
+        Self {
+            inner: generate_self_signed_tls_certificate(private_key, server_name),
+        }
+    }
+
+    pub fn rustls_certificate(&self) -> rustls::Certificate {
+        let cert_bytes = self.inner.serialize_der().unwrap();
+        rustls::Certificate(cert_bytes)
+    }
+
+    pub fn rustls_private_key(&self) -> rustls::PrivateKey {
+        let private_key_bytes = self.inner.serialize_private_key_der();
+        rustls::PrivateKey(private_key_bytes)
+    }
+
+    pub fn reqwest_identity(&self) -> reqwest::tls::Identity {
+        let pem = self.inner.serialize_pem().unwrap() + &self.inner.serialize_private_key_pem();
+        reqwest::tls::Identity::from_pem(pem.as_ref()).unwrap()
+    }
+
+    pub fn reqwest_certificate(&self) -> reqwest::tls::Certificate {
+        let cert = self.inner.serialize_der().unwrap();
+        reqwest::tls::Certificate::from_der(&cert).unwrap()
+    }
+}
+
+fn generate_self_signed_tls_certificate(
     private_key: Ed25519PrivateKey,
-) -> (rustls::Certificate, rustls::PrivateKey) {
+    server_name: &str,
+) -> rcgen::Certificate {
     let keypair = ed25519::KeypairBytes {
         secret_key: private_key.0.to_bytes(),
         // ring cannot handle the optional public key that would be legal der here
@@ -15,23 +48,19 @@ pub fn generate_self_signed_tls_certificate(
         public_key: None,
     };
 
-    generate_cert(&keypair, crate::TLS_CERTIFICATE_SERVER_NAME)
+    generate_cert(&keypair, server_name)
 }
 
-fn generate_cert(
-    keypair: &ed25519::KeypairBytes,
-    server_name: &str,
-) -> (rustls::Certificate, rustls::PrivateKey) {
+fn generate_cert(keypair: &ed25519::KeypairBytes, server_name: &str) -> rcgen::Certificate {
     let pkcs8 = keypair.to_pkcs8_der().unwrap();
     let key_der = rustls::PrivateKey(pkcs8.as_bytes().to_vec());
-    let certificate = private_key_to_certificate(vec![server_name.to_owned()], &key_der).unwrap();
-    (certificate, key_der)
+    private_key_to_certificate(vec![server_name.to_owned()], &key_der).unwrap()
 }
 
 fn private_key_to_certificate(
     subject_names: impl Into<Vec<String>>,
     private_key: &rustls::PrivateKey,
-) -> Result<rustls::Certificate, anyhow::Error> {
+) -> Result<rcgen::Certificate, anyhow::Error> {
     let alg = &rcgen::PKCS_ED25519;
 
     let certificate = gen_certificate(subject_names, (private_key.0.as_ref(), alg))?;
@@ -41,7 +70,7 @@ fn private_key_to_certificate(
 fn gen_certificate(
     subject_names: impl Into<Vec<String>>,
     key_pair: (&[u8], &'static SignatureAlgorithm),
-) -> Result<rustls::Certificate, anyhow::Error> {
+) -> Result<rcgen::Certificate, anyhow::Error> {
     let kp = KeyPair::from_der_and_sign_algo(key_pair.0, key_pair.1)?;
 
     let mut cert_params = CertificateParams::new(subject_names);
@@ -52,6 +81,5 @@ fn gen_certificate(
     let cert = rcgen::Certificate::from_params(cert_params).expect(
         "unreachable! from_params should only fail if the key is incompatible with params.algo",
     );
-    let cert_bytes = cert.serialize_der()?;
-    Ok(rustls::Certificate(cert_bytes))
+    Ok(cert)
 }
