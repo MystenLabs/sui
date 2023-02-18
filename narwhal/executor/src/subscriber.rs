d12092e1869c3e8a5b90f180c6dffae86e821a0c
Arun Koshy <97870774+arun-koshy@users.noreply.github.com>
2023-02-17 18:14:11 -0800

    Increase max concurrent payload fetch requests
    32 -> 1000

--- narwhal/executor/src/subscriber.rs
+++ narwhal/executor/src/subscriber.rs
@@ -152,7 +152,7 @@ async fn create_and_run_subscriber(
 
 impl<Network: SubscriberNetwork> Subscriber<Network> {
     /// Returns the max amount of pending consensus messages we should expect.
-    const MAX_PENDING_PAYLOADS: usize = 32;
+    const MAX_PENDING_PAYLOADS: usize = 1000;
 
     /// Main loop connecting to the consensus to listen to sequence messages.
     async fn run(
