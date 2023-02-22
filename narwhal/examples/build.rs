ec4af52fe8d023c5a13a1e7651fe0db6defd827c
MW Tian <mingwei@mystenlabs.com>
2023-02-22 10:40:01 -0800

    Remove example RPC definition

--- narwhal/examples/build.rs
+++ narwhal/examples/build.rs
@@ -3,6 +3,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(not(target_env = "msvc"))]
     std::env::set_var("PROTOC", protobuf_src::protoc());
-    tonic_build::compile_protos("protos/narwhal.proto")?;
+    // tonic_build::compile_protos("protos/narwhal.proto")?;
     Ok(())
 }
