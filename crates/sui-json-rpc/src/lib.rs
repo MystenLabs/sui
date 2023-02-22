6818d67bfd46b28d55c985ccb39c91ecfdf6ac27
patrick <patrickkuo@me.com>
2023-02-22 19:39:19 +0000

    [jsonrpc] - use jsonrpsee logger instead of tower layer for metrics logging (#8442)
    Using tower layer requires multiple serialise/deserialise to extract
    method and errors from the jsonrpc payload, which is inefficient and
    error prone, this PR uses jsonrpsee's logger api which exposes most of
    the metrics we needed without extra serde.
    
    This PR also added 2 new metrics:
    * `rpc_client` for tracking the number of connections made by different
    SDK and versions.
    * `rpc_inflight_connection` for tracking connected client
    
    Run sui node locally and check Prometheus endpoint
    `127.0.0.1:58429/metrics` for metrics

--- crates/sui-json-rpc/src/lib.rs
+++ crates/sui-json-rpc/src/lib.rs
@@ -18,7 +18,7 @@ use tracing::{info, warn};
 
 use sui_open_rpc::{Module, Project};
 
-use crate::metrics::MetricsLayer;
+use crate::metrics::MetricsLogger;
 
 pub mod api;
 pub mod bcs_api;
@@ -120,16 +120,15 @@ impl JsonRpcServerBuilder {
             })
             .unwrap_or(u32::MAX);
 
-        let metrics_layer = MetricsLayer::new(&self.registry, &methods_names);
-        let middleware = tower::ServiceBuilder::new()
-            .layer(cors)
-            .layer(metrics_layer);
+        let metrics_logger = MetricsLogger::new(&self.registry, &methods_names);
+        let middleware = tower::ServiceBuilder::new().layer(cors);
 
         let server = ServerBuilder::default()
             .max_response_body_size(2 << 30)
             .max_connections(max_connection)
             .set_host_filtering(AllowHosts::Any)
             .set_middleware(middleware)
+            .set_logger(metrics_logger)
             .build(listen_address)
             .await?;
         let addr = server.local_addr()?;
