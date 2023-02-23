ce524a0c157ac4db50452b58353fe76b0ff4db52
SplittyDev <splittydev@gmail.com>
2023-02-23 16:21:31 +0200

    Partially remove metrics from `narwhal/node`

--- narwhal/node/src/metrics.rs
+++ narwhal/node/src/metrics.rs
@@ -1,9 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 use axum::{http::StatusCode, routing::get, Extension, Router};
-use config::{AuthorityIdentifier, WorkerId};
-use mysten_metrics::spawn_logged_monitored_task;
-use mysten_network::multiaddr::{to_socket_addr, Multiaddr};
+use config::WorkerId;
+use crypto::PublicKey;
+use multiaddr::Multiaddr;
+use mysten_network::multiaddr::to_socket_addr;
 use prometheus::{Registry, TextEncoder};
 use std::collections::HashMap;
 use tokio::task::JoinHandle;
@@ -39,14 +40,13 @@ pub fn start_prometheus_server(addr: Multiaddr, registry: &Registry) -> JoinHand
 
     let socket_addr = to_socket_addr(&addr).expect("failed to convert Multiaddr to SocketAddr");
 
-    spawn_logged_monitored_task!(
+    tokio::spawn(
         async move {
             axum::Server::bind(&socket_addr)
                 .serve(app.into_make_service())
                 .await
                 .unwrap();
-        },
-        "MetricsServerTask"
+        }
     )
 }
 
