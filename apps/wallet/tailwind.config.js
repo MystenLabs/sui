ac57e4d470cdfc777b1150dbd34a70f6a1a3e3e4
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2023-02-23 12:47:51 +0000

    wallet-ext: logout confirmation dialog (#8489)
    * make sure user actually wants to logout using a modal to avoid
    accidental logouts
    
    
    
    https://user-images.githubusercontent.com/10210143/220646312-f8c1fca6-5d17-4d3c-b74c-edb49fc489c5.mov
    
    
    
    https://user-images.githubusercontent.com/10210143/220646355-a9ff9611-5e27-4cd6-b19b-9e6209ae3176.mov
    
    
    closes APPS-501

--- apps/wallet/tailwind.config.js
+++ apps/wallet/tailwind.config.js
@@ -34,6 +34,7 @@ module.exports = {
                 'wallet-content': '0px -5px 20px 5px rgba(160, 182, 195, 0.15)',
                 button: '0px 1px 2px rgba(16, 24, 40, 0.05)',
                 notification: '0px 0px 20px rgba(29, 55, 87, 0.11)',
+                'wallet-modal': '0px 0px 44px 0px rgba(0, 0, 0, 0.15)',
             },
             borderRadius: {
                 20: '1.25rem',
