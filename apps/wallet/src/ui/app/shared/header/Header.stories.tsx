6da7006ef32b31df49914b98571a379edefba2bb
William Robertson <williamrobertson@mystenlabs.com>
2023-02-23 14:23:44 -0500

    re-arrange

--- apps/wallet/src/ui/app/shared/header/Header.stories.tsx
+++ apps/wallet/src/ui/app/shared/header/Header.stories.tsx
@@ -11,29 +11,29 @@ export default {
 
 export const Default: StoryObj<typeof Header> = {};
 
-export const WithMiddleContent: StoryObj<typeof Header> = {
+export const Full: StoryObj<typeof Header> = {
     args: {
         middleContent: (
             <div className="text-ellipsis whitespace-nowrap overflow-hidden">
                 Connected to some dapp
             </div>
         ),
-    },
-};
-
-export const WithRightContent: StoryObj<typeof Header> = {
-    args: {
         rightContent: <div>Menu</div>,
     },
 };
 
-export const WithAll: StoryObj<typeof Header> = {
+export const WithMiddleContentOnly: StoryObj<typeof Header> = {
     args: {
         middleContent: (
             <div className="text-ellipsis whitespace-nowrap overflow-hidden">
                 Connected to some dapp
             </div>
         ),
+    },
+};
+
+export const WithRightContentOnly: StoryObj<typeof Header> = {
+    args: {
         rightContent: <div>Menu</div>,
     },
 };
