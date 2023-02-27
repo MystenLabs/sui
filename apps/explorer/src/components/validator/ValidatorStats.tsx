7b899fb966647b09e64591d4f83297b4be226333
Jordan Gensler <jordan@mystenlabs.com>
2023-02-27 15:13:08 -0800

    Improve UX

--- apps/explorer/src/components/validator/ValidatorStats.tsx
+++ apps/explorer/src/components/validator/ValidatorStats.tsx
@@ -42,124 +42,133 @@ export function ValidatorStats({
 
     return (
         <div className="flex flex-col items-stretch gap-5 md:flex-row">
-            <Card spacing="lg">
-                <div className="flex basis-full flex-col gap-8 md:basis-1/3">
-                    <Heading
-                        as="div"
-                        variant="heading4/semibold"
-                        color="steel-darker"
-                    >
-                        SUI Staked on Validator
-                    </Heading>
-                    <div className="flex flex-col gap-8 lg:flex-row">
-                        <Stats
-                            label="Staking APY"
-                            tooltip="This is the Annualized Percentage Yield of the a specific validator’s past operations. Note there is no guarantee this APY will be true in the future."
-                            unavailable={apy <= 0}
+            <div className="flex-grow">
+                <Card spacing="lg">
+                    <div className="flex basis-full flex-col gap-8 md:basis-1/3">
+                        <Heading
+                            as="div"
+                            variant="heading4/semibold"
+                            color="steel-darker"
                         >
-                            {apy}%
-                        </Stats>
-                        <Stats
-                            label="Commission"
-                            tooltip="Coming soon"
-                            unavailable={commission <= 0}
-                        >
-                            <Heading
-                                as="h3"
-                                variant="heading2/semibold"
-                                color="steel-darker"
+                            SUI Staked on Validator
+                        </Heading>
+                        <div className="flex flex-col gap-8 lg:flex-row">
+                            <Stats
+                                label="Staking APY"
+                                tooltip="This is the Annualized Percentage Yield of the a specific validator’s past operations. Note there is no guarantee this APY will be true in the future."
+                                unavailable={apy <= 0}
                             >
-                                {commission}%
-                            </Heading>
-                        </Stats>
-                        <Stats
-                            label="Total SUI Staked"
-                            tooltip="The total SUI staked on the network by validators and delegators to validate the network and earn rewards."
-                            unavailable={totalStake <= 0}
-                        >
-                            <DelegationAmount amount={totalStake} isStats />
-                        </Stats>
-                    </div>
-                    <div className="flex flex-col gap-8 lg:flex-row">
-                        <Stats
-                            label="Delegators"
-                            tooltip="The number of active delegators"
-                            unavailable
-                        />
-                    </div>
-                </div>
-            </Card>
-
-            <Card spacing="lg">
-                <div className="flex basis-full flex-col items-stretch gap-8 md:basis-80">
-                    <Heading
-                        as="div"
-                        variant="heading4/semibold"
-                        color="steel-darker"
-                    >
-                        Validator Staking Rewards
-                    </Heading>
-                    <div className="flex flex-col gap-8">
-                        <Stats
-                            label="Last Epoch SUI Rewards"
-                            tooltip="The stake rewards collected during the last epoch."
-                            unavailable={+epochRewards <= 0}
-                        >
-                            <DelegationAmount amount={epochRewards} isStats />
-                        </Stats>
-
-                        <Stats
-                            label="Reward Pool"
-                            tooltip="Amount currently in this validator’s reward pool"
-                            unavailable={+rewardsPoolBalance <= 0}
-                        >
-                            <DelegationAmount
-                                amount={rewardsPoolBalance}
-                                isStats
-                            />
-                        </Stats>
-                    </div>
-                </div>
-            </Card>
-
-            <Card spacing="lg">
-                <div className="flex max-w-full flex-col gap-8">
-                    <Heading
-                        as="div"
-                        variant="heading4/semibold"
-                        color="steel-darker"
-                    >
-                        Network Participation
-                    </Heading>
-                    <div className="flex flex-col gap-8">
-                        <div className="flex flex-col gap-8 lg:flex-row">
+                                {apy}%
+                            </Stats>
                             <Stats
-                                label="Checkpoint Participation"
-                                tooltip="The percentage of checkpoints certified thus far by this validator."
-                                unavailable
-                            />
-
-                            <Stats
-                                label="Voted Last Round"
-                                tooltip="Did this validator vote in the latest round."
-                                unavailable
-                            />
+                                label="Total SUI Staked"
+                                tooltip="The total SUI staked on the network by validators and delegators to validate the network and earn rewards."
+                                unavailable={totalStake <= 0}
+                            >
+                                <DelegationAmount amount={totalStake} isStats />
+                            </Stats>
                         </div>
                         <div className="flex flex-col gap-8 lg:flex-row">
                             <Stats
-                                label="Tallying Score"
-                                tooltip="A score generated by validators to evaluate each other’s performance throughout Sui’s regular operations."
-                                unavailable
-                            />
+                                label="Commission"
+                                tooltip="Coming soon"
+                                unavailable={commission <= 0}
+                            >
+                                <Heading
+                                    as="h3"
+                                    variant="heading2/semibold"
+                                    color="steel-darker"
+                                >
+                                    {commission}%
+                                </Heading>
+                            </Stats>
                             <Stats
-                                label="Last Narwhal Round"
-                                tooltip="Latest Narwhal round for this epoch."
+                                label="Delegators"
+                                tooltip="The number of active delegators"
                                 unavailable
                             />
                         </div>
                     </div>
-                </div>
-            </Card>
+                </Card>
+            </div>
+
+            <div className="flex-grow">
+                <Card spacing="lg">
+                    <div className="flex basis-full flex-col items-stretch gap-8 md:basis-80">
+                        <Heading
+                            as="div"
+                            variant="heading4/semibold"
+                            color="steel-darker"
+                        >
+                            Validator Staking Rewards
+                        </Heading>
+                        <div className="flex flex-col gap-8">
+                            <Stats
+                                label="Last Epoch SUI Rewards"
+                                tooltip="The stake rewards collected during the last epoch."
+                                unavailable={+epochRewards <= 0}
+                            >
+                                <DelegationAmount
+                                    amount={epochRewards}
+                                    isStats
+                                />
+                            </Stats>
+
+                            <Stats
+                                label="Reward Pool"
+                                tooltip="Amount currently in this validator’s reward pool"
+                                unavailable={+rewardsPoolBalance <= 0}
+                            >
+                                <DelegationAmount
+                                    amount={rewardsPoolBalance}
+                                    isStats
+                                />
+                            </Stats>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+
+            <div className="flex-grow">
+                <Card spacing="lg">
+                    <div className="flex max-w-full flex-col gap-8">
+                        <Heading
+                            as="div"
+                            variant="heading4/semibold"
+                            color="steel-darker"
+                        >
+                            Network Participation
+                        </Heading>
+                        <div className="flex flex-col gap-8">
+                            <div className="flex flex-col gap-8 lg:flex-row">
+                                <Stats
+                                    label="Checkpoint Participation"
+                                    tooltip="The percentage of checkpoints certified thus far by this validator."
+                                    unavailable
+                                />
+
+                                <Stats
+                                    label="Voted Last Round"
+                                    tooltip="Did this validator vote in the latest round."
+                                    unavailable
+                                />
+                            </div>
+                            <div className="flex flex-col gap-8 lg:flex-row">
+                                <Stats
+                                    label="Tallying Score"
+                                    tooltip="A score generated by validators to evaluate each other’s performance throughout Sui’s regular operations."
+                                    unavailable
+                                />
+                                <Stats
+                                    label="Last Narwhal Round"
+                                    tooltip="Latest Narwhal round for this epoch."
+                                    unavailable
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
         </div>
     );
 }
