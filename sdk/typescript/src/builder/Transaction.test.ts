4f196e9d66f47e0e2181f559bac9ec31bf40d298
Jordan Gensler <jordan@mystenlabs.com>
2023-02-27 09:55:40 -0800

    Add transaction response

--- sdk/typescript/src/builder/Transaction.test.ts
+++ sdk/typescript/src/builder/Transaction.test.ts
@@ -2,25 +2,8 @@ import { Transaction, Commands } from './';
 
 const tx = new Transaction({ inputs: ['amount', 'address'] });
 const coin = tx.add(Commands.Split(tx.gas(), tx.input('amount')));
-
-// NOTE: Do this:
-// Option 1, coin is directly usable
-tx.add(Commands.TransferObjects([coin], tx.input('address')));
-// This also allows us to destructure out variable returns:
-const [gameNFT, gameAccount] = tx.add(
-  Commands.MoveCall({
-    package: '0x2',
-    function: 'game',
-    module: 'register',
-    arguments: [],
-    typeArguments: [],
-  }),
-);
-
-// Option 2, `.result()` method:
 tx.add(Commands.TransferObjects([coin.result()], tx.input('address')));
-// This requires calling result with an index:
-const register = tx.add(
+tx.add(
   Commands.MoveCall({
     package: '0x2',
     function: 'game',
@@ -29,13 +12,14 @@ const register = tx.add(
     typeArguments: [],
   }),
 );
-register.result(0);
-register.result(1);
 
-console.log(tx);
+tx.provideInputs({
+  amount: 100,
+  address: '0x2',
+});
 
 const serialized = tx.serialize();
 console.log(serialized);
 
 const tx2 = Transaction.from(serialized);
-console.log(tx2);
+console.log(tx2.serialize());
