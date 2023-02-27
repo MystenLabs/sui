301df809814677b90e7c821eec8e2df2b95c6169
Jian Lu <jian@mystenlabs.com>
2023-02-27 12:28:54 -0500

    adding comparison between txns
    adding license
    
    adding some logging

--- crates/sui-indexer/src/bin/checkpoint_check.rs
+++ crates/sui-indexer/src/bin/checkpoint_check.rs
@@ -1,46 +1,57 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use anyhow::Result;
 use clap::Parser;
-// use prometheus::Registry;
 use rand::Rng;
+use std::cmp::min;
 use sui_indexer::new_rpc_client;
 use sui_json_rpc_types::CheckpointId;
 use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    info!("Running benchmark setup in remote mode ..");
+    info!("Running correctness check for indexer...");
     let test_config = TestConfig::parse();
-    let rpc_client = new_rpc_client(test_config.rpc_client_url.clone()).await?;
+    let fn_rpc_client = new_rpc_client(test_config.fn_rpc_client_url.clone()).await?;
+    let indexer_rpc_client = new_rpc_client(test_config.indexer_rpc_client_url.clone()).await?;
 
-    let latest_checkpoint = rpc_client
+    let latest_checkpoint = indexer_rpc_client
         .read_api()
         .get_latest_checkpoint_sequence_number()
         .await?;
 
-    let num = rand::thread_rng().gen_range(0..100);
+    let num = rand::thread_rng().gen_range(10..100);
 
-    let target_checkpoint = latest_checkpoint - num;
-    println!("{0:?}", target_checkpoint);
+    let target_checkpoint = min(latest_checkpoint - num, 1);
 
-    let checkpoint = rpc_client
+    let fn_checkpoint = fn_rpc_client
         .read_api()
         .get_checkpoint(CheckpointId::SequenceNumber(target_checkpoint))
         .await?;
 
-    // let checkpoint = rpc_client
-    //     .read_api()
-    //     .get_checkpoint_summary(target_checkpoint)
-    //     .await?;
+    let indexer_checkpoint = indexer_rpc_client
+        .read_api()
+        .get_checkpoint(CheckpointId::SequenceNumber(target_checkpoint))
+        .await?;
 
-    // let transactions = rpc_client.read_api()
-    // TODO: grab indexer checkpoint
+    assert_eq!(
+        fn_checkpoint.transactions.len(),
+        indexer_checkpoint.transactions.len(),
+        "Checkpoint number {} length is not the same for FN and Indexer",
+        target_checkpoint
+    );
 
-    // Assert transactions from both indexer and FN are the same length
+    let fn_checkpoint_transactions = fn_checkpoint.transactions;
+    let indexer_checkpoint_transactions = indexer_checkpoint.transactions;
 
-    // compare FN checkpoint against IndexerCheckpoint
-    let checkpoint_transactions = checkpoint.transactions;
-    for i in 0..checkpoint_transactions.len() {
-        println!("{0:?}", checkpoint_transactions.get(i));
+    for i in 0..fn_checkpoint_transactions.len() {
+        assert_eq!(
+            fn_checkpoint_transactions.get(i),
+            indexer_checkpoint_transactions.get(i),
+            "Checkpoint transactions mismatch found in {}",
+            target_checkpoint
+        );
     }
 
     Ok(())
@@ -50,5 +61,7 @@ async fn main() -> Result<()> {
 #[clap(name = "Transactions Test")]
 pub struct TestConfig {
     #[clap(long)]
-    pub rpc_client_url: String,
+    pub fn_rpc_client_url: String,
+    #[clap(long)]
+    pub indexer_rpc_client_url: String,
 }
