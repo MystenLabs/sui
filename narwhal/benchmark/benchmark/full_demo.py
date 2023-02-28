72d5d01dded44fb772268fe45854c4b65dc316fd
Noodles <me@xiongty.com>
2023-02-28 17:29:16 +0800

    Update full_demo.py
    fix bug

--- narwhal/benchmark/benchmark/full_demo.py
+++ narwhal/benchmark/benchmark/full_demo.py
@@ -80,7 +80,7 @@ class Demo:
             for filename in primary_key_files:
                 cmd = CommandMaker.generate_key(filename).split()
                 subprocess.run(cmd, check=True)
-                cmd_pk = CommandMaker.get_pub_key(filename)
+                cmd_pk = CommandMaker.get_pub_key(filename).split()
                 pk = subprocess.check_output(cmd_pk, encoding='utf-8').strip()
                 primary_names += [pk]
 
