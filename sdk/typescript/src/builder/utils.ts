4648cba3376235218f7d886f27130f548070dcb8
Jordan Gensler <jordan@mystenlabs.com>
2023-02-27 17:14:41 -0800

    Clean up some docs

--- sdk/typescript/src/builder/utils.ts
+++ sdk/typescript/src/builder/utils.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import { create as superstructCreate, Struct } from 'superstruct';
 
 export function create<T, S>(value: T, struct: Struct<T, S>): T {
