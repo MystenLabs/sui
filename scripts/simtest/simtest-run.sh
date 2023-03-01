371b5985d1acdcb6d7ca97b2318075e70fb6e395
Eugene Boguslavsky <eugene@mystenlabs.com>
2023-03-01 15:35:10 -0800

    Fixing executing logic

--- scripts/simtest/simtest-run.sh
+++ scripts/simtest/simtest-run.sh
@@ -2,27 +2,28 @@
 # Copyright (c) Mysten Labs, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-NUM_CPUS=$(cat /proc/cpuinfo | grep processor | wc -l) # ubuntu
-# NUM_CPUS=$(sysctl -n hw.ncpu) mac
+# NUM_CPUS=$(cat /proc/cpuinfo | grep processor | wc -l) # ubuntu
+NUM_CPUS=$(sysctl -n hw.ncpu) # mac
 # NUM_CPUS=2 # testing with just two cores for now
 
 DATE=$(date +%s)
 
 # TODO: increase to 30, 1000 respectively after workflow is debugged.
-export MSIM_TEST_NUM=3
-export SIM_STRESS_TEST_DURATION_SECS=100
+export MSIM_TEST_NUM=30
+export SIM_STRESS_TEST_DURATION_SECS=1000
 
 SEED="$DATE"
-LOG_FILE="log-$SEED"
+# LOG_FILE="log-$SEED"
+
 # This command runs many different tests, so it already uses all CPUs fairly efficiently, and
 # don't need to be done inside of the for loop below.
 # TODO: this logs directly to stdout since it is not being run in parallel. is that ok?
-MSIM_TEST_SEED="$SEED" MSIM_WATCHDOG_TIMEOUT_MS=60000 scripts/simtest/cargo-simtest simtest --package sui --package sui-core
+# MSIM_TEST_SEED="$SEED" MSIM_WATCHDOG_TIMEOUT_MS=60000 scripts/simtest/cargo-simtest simtest --package sui --package sui-core
 
 for SUB_SEED in `seq 1 $NUM_CPUS`; do
   SEED="$SUB_SEED$DATE"
-  LOG_FILE="log-$SEED"
-  echo "Iteration $SUB_SEED using MSIM_TEST_SEED=$SEED, logging to $LOG_FILE"
+  #LOG_FILE="log-$SEED"
+  echo "Iteration $SUB_SEED using MSIM_TEST_SEED=$SEED" # logging to $LOG_FILE"
 
   # TODO: currently this only runs one test, (`test_simulated_load_basic`).
   # we need to add `--run-ignored all` in order to enable the ignored tests.
@@ -32,8 +33,10 @@ for SUB_SEED in `seq 1 $NUM_CPUS`; do
   # Note that because of --no-capture, even though we are running many tests, they will be
   # serialized here. So we still need the for loop / backgrounding.
   MSIM_TEST_SEED="$SEED" MSIM_TEST_NUM=1 MSIM_WATCHDOG_TIMEOUT_MS=60000 scripts/simtest/cargo-simtest simtest --package sui-benchmark --test-threads 1 & # > "$LOG_FILE" 2>&1 &
-  wait
 done
+
+wait
+
 
 # for JOB in $(jobs -p); do
 #   wait $JOB || echo "job failed"
