c959e10fa4947fc09af452fa4b4261dd85c4321a
Jordan Gensler <jordan@mystenlabs.com>
2023-02-28 18:22:29 -0800

    Add wallet kit docs site

---
+++ sdk/wallet-adapter/site/theme.config.tsx
@@ -0,0 +1,21 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Sui Wallet Kit</span>,
+  project: {
+    link: "https://github.com/MystenLabs/sui/tree/main/sdk/wallet-adapter",
+  },
+  chat: {
+    link: "https://discord.com/invite/Sui",
+  },
+  docsRepositoryBase:
+    "https://github.com/MystenLabs/sui/tree/main/sdk/wallet-adapter",
+  footer: {
+    text: "Copyright © 2023, Mysten Labs, Inc.",
+  },
+};
+
+export default config;
