f44f3e9ab1652a0402a8b6c7fe5c5868da6f6f4c
Ge Gao <ge@mystenlabs.com>
2023-03-01 12:15:32 -0500

    indexer: txn pagination

--- crates/sui-indexer/migrations/2022-11-18-195259_transactions/up.sql
+++ crates/sui-indexer/migrations/2022-11-18-195259_transactions/up.sql
@@ -2,6 +2,7 @@ CREATE TABLE transactions (
     id BIGSERIAL PRIMARY KEY,
     transaction_digest VARCHAR(255) NOT NULL,
     sender VARCHAR(255) NOT NULL,
+    recipients TEXT[] NOT NULL,
     checkpoint_sequence_number BIGINT NOT NULL,
     transaction_time TIMESTAMP,
     transaction_kinds TEXT[] NOT NULL,
@@ -11,6 +12,8 @@ CREATE TABLE transactions (
     deleted TEXT[] NOT NULL,
     unwrapped TEXT[] NOT NULL,
     wrapped TEXT[] NOT NULL,
+    -- each move call is <package>::<module>::<function>
+    move_calls TEXT[] NOT NULL,
     -- gas object related
     gas_object_id VARCHAR(255) NOT NULL,
     gas_object_sequence BIGINT NOT NULL,
@@ -34,5 +37,10 @@ CREATE TABLE transactions (
 CREATE INDEX transactions_transaction_digest ON transactions (transaction_digest);
 CREATE INDEX transactions_transaction_time ON transactions (transaction_time);
 CREATE INDEX transactions_sender ON transactions (sender);
+CREATE INDEX transactions_recipients ON transactions (recipients);
 CREATE INDEX transactions_gas_object_id ON transactions (gas_object_id);
 CREATE INDEX transactions_checkpoint_sequence_number ON transactions (checkpoint_sequence_number);
+
+CREATE INDEX transactions_move_calls ON transactions (move_calls);
+CREATE INDEX transactions_created ON transactions (created);
+CREATE INDEX transactions_mutated ON transactions (mutated);
