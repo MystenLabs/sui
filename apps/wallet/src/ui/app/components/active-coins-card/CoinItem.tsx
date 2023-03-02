c66f11ad72ecb4945ab569451267d2ff47cac54d
Jibz <godwinjibs@gmail.com>
2023-03-02 07:00:44 -0500

    combine coinItem with SelectedCoinCard

--- apps/wallet/src/ui/app/components/active-coins-card/CoinItem.tsx
+++ apps/wallet/src/ui/app/components/active-coins-card/CoinItem.tsx
@@ -5,35 +5,63 @@ import { Text } from '_app/shared/text';
 import { CoinIcon } from '_components/coin-icon';
 import { useFormatCoin } from '_hooks';
 
-export function CoinItem({
-    coinType,
-    balance,
-}: {
+type CoinItemProps = {
     coinType: string;
     balance: bigint;
-}) {
+    isActive?: boolean;
+    usd?: number;
+};
+
+export function CoinItem({ coinType, balance, isActive, usd }: CoinItemProps) {
     const [formatted, symbol] = useFormatCoin(balance, coinType);
 
     return (
         <div className="flex gap-2.5 w-full justify-center items-center">
-            <CoinIcon coinType={coinType} size="md" />
+            <CoinIcon coinType={coinType} size={isActive ? 'sm' : 'md'} />
             <div className="flex flex-1 gap-1.5 justify-between">
                 <div className="flex flex-col gap-1.5">
                     <Text variant="body" color="gray-90" weight="semibold">
-                        {symbol}
+                        {symbol} {isActive ? 'available' : ''}
                     </Text>
-                    <Text
-                        variant="subtitle"
-                        color="steel-dark"
-                        weight="semibold"
-                    >
-                        {symbol}
-                    </Text>
+                    {!isActive ? (
+                        <Text
+                            variant="subtitle"
+                            color="steel-dark"
+                            weight="semibold"
+                        >
+                            {symbol}
+                        </Text>
+                    ) : null}
                 </div>
                 <div className="flex flex-row justify-center items-center">
-                    <Text variant="body" color="gray-90" weight="semibold">
-                        {formatted} {symbol}
-                    </Text>
+                    {isActive ? (
+                        <Text
+                            variant="body"
+                            color="steel-darker"
+                            weight="medium"
+                        >
+                            {formatted}
+                        </Text>
+                    ) : (
+                        <div className="flex flex-col justify-end items-end gap-1.5">
+                            <Text
+                                variant="body"
+                                color="gray-90"
+                                weight="medium"
+                            >
+                                {formatted} {symbol}
+                            </Text>
+                            {usd && (
+                                <Text
+                                    variant="caption"
+                                    color="steel-dark"
+                                    weight="medium"
+                                >
+                                    ${usd.toLocaleString('en-US')}
+                                </Text>
+                            )}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
--- apps/wallet/src/ui/app/components/active-coins-card/index.tsx
+++ apps/wallet/src/ui/app/components/active-coins-card/index.tsx
@@ -7,39 +7,8 @@ import { Link } from 'react-router-dom';
 
 import { CoinItem } from './CoinItem';
 import { Text } from '_app/shared/text';
-import { CoinIcon } from '_components/coin-icon';
-import { useAppSelector, useFormatCoin } from '_hooks';
+import { useAppSelector } from '_hooks';
 import { accountAggregateBalancesSelector } from '_redux/slices/account';
-
-function SelectedCoinCard({
-    balance,
-    coinType,
-}: {
-    balance: bigint | number;
-    coinType: string;
-}) {
-    const [formatted, symbol] = useFormatCoin(balance, coinType);
-
-    return (
-        <Link
-            to={`/send/select?${new URLSearchParams({
-                type: coinType,
-            }).toString()}`}
-            className="bg-gray-40 rounded-2lg py-2.5 px-3 no-underline flex gap-2 items-center w-full"
-        >
-            <CoinIcon coinType={coinType} size="sm" />
-
-            <div className="flex flex-1 w-full justify-between">
-                <Text variant="body" color="steel-darker" weight="semibold">
-                    {symbol} available
-                </Text>
-                <Text variant="body" color="steel-darker" weight="medium">
-                    {formatted}
-                </Text>
-            </div>
-        </Link>
-    );
-}
 
 export function ActiveCoinsCard({
     activeCoinType = SUI_TYPE_ARG,
@@ -64,40 +33,50 @@ export function ActiveCoinsCard({
         ({ coinType }) => coinType === activeCoinType
     );
 
-    const CoinListCard = (
-        <div className="flex flex-col w-full">
-            <Text variant="caption" color="steel-darker" weight="semibold">
-                My Coins
-            </Text>
-            <div className="flex flex-col justify-between items-center mt-2">
-                {coins?.map(({ coinType, balance }) => (
-                    <Link
-                        to={`/send?${new URLSearchParams({
-                            type: coinType,
-                        }).toString()}`}
-                        key={coinType}
-                        className="py-3.75 px-1.5 no-underline flex gap-2 items-center w-full hover:bg-sui/10 group border-t border-solid border-transparent border-t-gray-45"
-                    >
-                        <CoinItem
-                            coinType={coinType}
-                            balance={BigInt(balance)}
-                        />
-                    </Link>
-                ))}
-            </div>
-        </div>
-    );
-
     return (
         <div className="flex w-full">
-            {showActiveCoin
-                ? activeCoin && (
-                      <SelectedCoinCard
-                          coinType={activeCoin.coinType}
-                          balance={activeCoin.balance}
-                      />
-                  )
-                : CoinListCard}
+            {showActiveCoin ? (
+                activeCoin && (
+                    <Link
+                        to={`/send/select?${new URLSearchParams({
+                            type: activeCoin.coinType,
+                        }).toString()}`}
+                        className="bg-gray-40 rounded-2lg py-2.5 px-3 no-underline flex gap-2 items-center w-full"
+                    >
+                        <CoinItem
+                            coinType={activeCoin.coinType}
+                            balance={activeCoin.balance}
+                            isActive
+                        />
+                    </Link>
+                )
+            ) : (
+                <div className="flex flex-col w-full">
+                    <Text
+                        variant="caption"
+                        color="steel-darker"
+                        weight="semibold"
+                    >
+                        My Coins
+                    </Text>
+                    <div className="flex flex-col justify-between items-center mt-2">
+                        {coins?.map(({ coinType, balance }) => (
+                            <Link
+                                to={`/send?${new URLSearchParams({
+                                    type: coinType,
+                                }).toString()}`}
+                                key={coinType}
+                                className="py-3.75 px-1.5 no-underline flex gap-2 items-center w-full hover:bg-sui/10 group border-t border-solid border-transparent border-t-gray-45"
+                            >
+                                <CoinItem
+                                    coinType={coinType}
+                                    balance={BigInt(balance)}
+                                />
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
