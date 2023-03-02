e5880902b7d8f39fc2d98591dae0ac3623a99190
oxade <93547199+oxade@users.noreply.github.com>
2023-03-02 17:36:41 -0500

    WIP: Natives cost 1/x (#8742)
    ## Description 
    
    Rewriting native function costs.
    
    Since Rust operations are hard to cost, I made 3 units of cost:
    **Low**: for simple opers like copying bytes
    **Medium**: for more complex opers involving function overhead
    **High**: for very complex opers like opaque crypto opers
    Costs are then written as multiples of the units above. 
    Need feedback on this approach.
    
    ## Test Plan 
    Separate logic to ensure costs trend as expected. TBD.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [x] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    * Gas costs will change

--- crates/sui-framework/src/natives/event.rs
+++ crates/sui-framework/src/natives/event.rs
@@ -1,19 +1,28 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{legacy_emit_cost, natives::object_runtime::ObjectRuntime};
+use crate::natives::object_runtime::ObjectRuntime;
 use move_binary_format::errors::{PartialVMError, PartialVMResult};
 use move_core_types::{language_storage::TypeTag, vm_status::StatusCode};
-use move_vm_runtime::native_functions::NativeContext;
+use move_vm_runtime::{
+    native_charge_gas_early_exit, native_functions::NativeContext, native_gas_total_cost,
+};
 use move_vm_types::{
     loaded_data::runtime_types::Type, natives::function::NativeResult, values::Value,
 };
 use smallvec::smallvec;
-use std::collections::VecDeque;
+use std::{collections::VecDeque, ops::Mul};
+use sui_cost_tables::natives_tables::NATIVES_COST_MID;
 use sui_types::error::VMMemoryLimitExceededSubStatusCode;
 
-/// Implementation of Move native function `event::emit<T: copy + drop>(event: T)`
-/// Adds an event to the transaction's event log
+/***************************************************************************************************
+ * native fun to_u256
+ * Implementation of the Move native function `event::emit<T: copy + drop>(event: T)`
+ * Adds an event to the transaction's event log
+ *   gas cost: NATIVES_COST_MID * event_size                      | derivation of size
+ *              + NATIVES_COST_MID * tag_size                     | converting type
+ *              + NATIVES_COST_MID * (tag_size + event_size)      | emitting the actual event
+ **************************************************************************************************/
 pub fn emit(
     context: &mut NativeContext,
     mut ty_args: Vec<Type>,
@@ -21,11 +30,20 @@ pub fn emit(
 ) -> PartialVMResult<NativeResult> {
     debug_assert!(ty_args.len() == 1);
     debug_assert!(args.len() == 1);
+    let mut gas_left = context.gas_budget();
 
     let ty = ty_args.pop().unwrap();
     let event = args.pop_back().unwrap();
 
-    let cost = legacy_emit_cost();
+    let event_size = event.legacy_size();
+
+    // Deriving event size can be expensive due to recursion overhead
+    native_charge_gas_early_exit!(
+        context,
+        gas_left,
+        NATIVES_COST_MID.mul(u64::from(event_size).into())
+    );
+
     let tag = match context.type_to_type_tag(&ty)? {
         TypeTag::Struct(s) => s,
         _ => {
@@ -35,9 +53,18 @@ pub fn emit(
             )
         }
     };
+    let tag_size = tag.abstract_size_for_gas_metering();
+
+    // Converting type to typetag be expensive due to recursion overhead
+    native_charge_gas_early_exit!(
+        context,
+        gas_left,
+        NATIVES_COST_MID.mul(u64::from(tag_size).into())
+    );
+
     let obj_runtime: &mut ObjectRuntime = context.extensions_mut().get_mut();
     let max_event_emit_size = obj_runtime.constants.max_event_emit_size;
-    let ev_size = u64::from(tag.abstract_size_for_gas_metering() + event.legacy_size());
+    let ev_size = u64::from(tag_size + event_size);
     if ev_size > max_event_emit_size {
         return Err(PartialVMError::new(StatusCode::MEMORY_LIMIT_EXCEEDED)
             .with_message(format!(
@@ -48,6 +75,12 @@ pub fn emit(
             ));
     }
     let obj_runtime: &mut ObjectRuntime = context.extensions_mut().get_mut();
+
+    // Emitting an event is cheap since its a vector push
+    native_charge_gas_early_exit!(context, gas_left, NATIVES_COST_MID.mul(ev_size.into()));
     obj_runtime.emit_event(*tag, event)?;
-    Ok(NativeResult::ok(cost, smallvec![]))
+    Ok(NativeResult::ok(
+        native_gas_total_cost!(context, gas_left),
+        smallvec![],
+    ))
 }
