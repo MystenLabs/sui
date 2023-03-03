3eb80ec972d97008d5dae2ce28e82e78059d209e
Jibz <godwinjibs@gmail.com>
2023-03-03 15:30:13 -0500

    rm useRpc hook

--- apps/wallet/src/ui/app/hooks/useObjectsOwnedByAddress.ts
+++ apps/wallet/src/ui/app/hooks/useObjectsOwnedByAddress.ts
@@ -1,15 +1,14 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { useRpcClient } from '@mysten/core';
 import { useQuery } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
-
-import { useRpc } from '_hooks';
 
 import type { SuiAddress } from '@mysten/sui.js';
 
 export function useObjectsOwnedByAddress(address?: SuiAddress | null) {
-    const rpc = useRpc();
+    const rpc = useRpcClient();
     return useQuery(
         ['objects-owned', address],
         () => rpc.getObjectsOwnedByAddress(address!),
