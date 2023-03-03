619709e1af53d374615b3f10827eede040e93e6d
Alberto Sonnino <alberto@sonnino.com>
2023-03-03 14:54:50 +0900

    Fmt

--- crates/sui-benchmark/src/drivers/bench_driver.rs
+++ crates/sui-benchmark/src/drivers/bench_driver.rs
@@ -440,9 +440,7 @@ impl Driver<(BenchmarkStats, StressStats)> for BenchDriver {
                                             metrics_cloned.num_in_flight.with_label_values(&[&payload.get_workload_type().to_string()]).dec();
                                             // let auth_sign_info = AuthorityStrongQuorumSignInfo::try_from(&cert.auth_sign_info).unwrap();
                                             // auth_sign_info.authorities(&committee_cloned).for_each(|name| metrics_cloned.validators_in_tx_cert.with_label_values(&[&name.unwrap().to_string()]).inc());
-                                            if let Some(sig_info) = effects.quorum_sig() {
-                                                sig_info.authorities(&committee_cloned).for_each(|name| metrics_cloned.validators_in_effects_cert.with_label_values(&[&name.unwrap().to_string()]).inc())
-                                            }
+                                            if let Some(sig_info) = effects.quorum_sig() { sig_info.authorities(&committee_cloned).for_each(|name| metrics_cloned.validators_in_effects_cert.with_label_values(&[&name.unwrap().to_string()]).inc()) }
                                             NextOp::Response(Some((
                                                 latency,
                                                 payload.make_new_payload(&effects),
