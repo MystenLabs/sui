5cffe4ebbfd63c14d36fd13f9e9c1ce3134ba363
Mark Logan <mark@mystenlabs.com>
2023-03-03 16:00:17 -0800

    Filter flaky tests, enable ignored simtest, set USE_MOCK_CRYPTO=1

--- scripts/simtest/simtest-run.sh
+++ scripts/simtest/simtest-run.sh
@@ -5,11 +5,13 @@
 NUM_CPUS=$(cat /proc/cpuinfo | grep processor | wc -l) # ubuntu
 # NUM_CPUS=$(sysctl -n hw.ncpu) # mac
 
+# filter out some tests that give spurious failures.
+TEST_FILTER="(not test(test_move_call_args_linter_command)) & (not test(test_package_publish_command))"
+
 DATE=$(date +%s)
 
-# TODO: increase to 30, 1000 respectively after workflow is debugged.
 export MSIM_TEST_NUM=30
-export SIM_STRESS_TEST_DURATION_SECS=1000
+export SIM_STRESS_TEST_DURATION_SECS=300
 
 SEED="$DATE"
 # LOG_FILE="log-$SEED"
@@ -17,21 +19,27 @@ SEED="$DATE"
 # This command runs many different tests, so it already uses all CPUs fairly efficiently, and
 # don't need to be done inside of the for loop below.
 # TODO: this logs directly to stdout since it is not being run in parallel. is that ok?
-MSIM_TEST_SEED="$SEED" MSIM_WATCHDOG_TIMEOUT_MS=60000 scripts/simtest/cargo-simtest simtest --package sui --package sui-core --profile simtestnightly
+MSIM_TEST_SEED="$SEED" \
+MSIM_WATCHDOG_TIMEOUT_MS=60000 \
+scripts/simtest/cargo-simtest simtest \
+  --package sui \
+  --package sui-core \
+  --profile simtestnightly
 
 for SUB_SEED in `seq 1 $NUM_CPUS`; do
   SEED="$SUB_SEED$DATE"
-  #LOG_FILE="log-$SEED"
-  echo "Iteration $SUB_SEED using MSIM_TEST_SEED=$SEED" # logging to $LOG_FILE"
+  echo "Iteration $SUB_SEED using MSIM_TEST_SEED=$SEED"
 
-  # TODO: currently this only runs one test, (`test_simulated_load_basic`).
-  # we need to add `--run-ignored all` in order to enable the ignored tests.
-  # However, the ignored tests are totally broken right now because of
-  # https://github.com/MystenLabs/sui/pull/8244.
-  #
-  # Note that because of --no-capture, even though we are running many tests, they will be
-  # serialized here. So we still need the for loop / backgrounding.
-  MSIM_TEST_SEED="$SEED" MSIM_TEST_NUM=1 MSIM_WATCHDOG_TIMEOUT_MS=60000 scripts/simtest/cargo-simtest simtest --package sui-benchmark --test-threads 1 --profile simtestnightly & # > "$LOG_FILE" 2>&1 &
+  # --test-threads 1 is important: parallelism is achieved via the for loop
+  MSIM_TEST_SEED="$SEED" \
+  MSIM_TEST_NUM=1 \
+  MSIM_WATCHDOG_TIMEOUT_MS=60000 \
+  scripts/simtest/cargo-simtest simtest \
+    --package sui-benchmark \
+    --run-ignored all \
+    --test-threads 1 \
+    --profile simtestnightly \
+    -E "$TEST_FILTER" &
 done
 
 # wait for all the jobs to end
