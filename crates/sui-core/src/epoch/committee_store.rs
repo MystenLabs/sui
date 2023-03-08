eacc8894b81a9406254aad44c1f83a82c6f8b0d9
Andrey Chursin <andrey@mystenlabs.com>
2023-03-08 15:18:17 -0800

    Do not parse public key if not found in committee (#9015)

--- crates/sui-core/src/epoch/committee_store.rs
+++ crates/sui-core/src/epoch/committee_store.rs
@@ -1,7 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use parking_lot::RwLock;
 use rocksdb::Options;
+use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use sui_storage::default_db_options;
 use sui_types::base_types::ObjectID;
@@ -15,8 +17,13 @@ use typed_store_derive::DBMapUtils;
 
 use sui_macros::nondeterministic;
 
-#[derive(DBMapUtils)]
 pub struct CommitteeStore {
+    tables: CommitteeStoreTables,
+    cache: RwLock<HashMap<EpochId, Committee>>,
+}
+
+#[derive(DBMapUtils)]
+pub struct CommitteeStoreTables {
     /// Map from each epoch ID to the committee information.
     #[default_options_override_fn = "committee_table_default_config"]
     committee_map: DBMap<EpochId, Committee>,
@@ -29,14 +36,22 @@ fn committee_table_default_config() -> DBOptions {
 
 impl CommitteeStore {
     pub fn new(path: PathBuf, genesis_committee: &Committee, db_options: Option<Options>) -> Self {
-        let committee_store =
-            Self::open_tables_read_write(path, MetricConf::default(), db_options, None);
-        if committee_store.database_is_empty() {
-            committee_store
+        let tables = CommitteeStoreTables::open_tables_read_write(
+            path,
+            MetricConf::default(),
+            db_options,
+            None,
+        );
+        let store = Self {
+            tables,
+            cache: RwLock::new(HashMap::new()),
+        };
+        if store.database_is_empty() {
+            store
                 .init_genesis_committee(genesis_committee.clone())
                 .expect("Init genesis committee data must not fail");
         }
-        committee_store
+        store
     }
 
     pub fn new_for_testing(genesis_committee: &Committee) -> Self {
@@ -47,7 +62,8 @@ impl CommitteeStore {
 
     pub fn init_genesis_committee(&self, genesis_committee: Committee) -> SuiResult {
         assert_eq!(genesis_committee.epoch, 0);
-        self.committee_map.insert(&0, &genesis_committee)?;
+        self.tables.committee_map.insert(&0, &genesis_committee)?;
+        self.cache.write().insert(0, genesis_committee);
         Ok(())
     }
 
@@ -56,18 +72,31 @@ impl CommitteeStore {
             // If somehow we already have this committee in the store, they must be the same.
             assert_eq!(&old_committee, new_committee);
         } else {
-            self.committee_map
+            self.tables
+                .committee_map
                 .insert(&new_committee.epoch, new_committee)?;
+            self.cache
+                .write()
+                .insert(new_committee.epoch, new_committee.clone());
         }
         Ok(())
     }
 
     pub fn get_committee(&self, epoch_id: &EpochId) -> SuiResult<Option<Committee>> {
-        Ok(self.committee_map.get(epoch_id)?)
+        if let Some(committee) = self.cache.read().get(epoch_id) {
+            return Ok(Some(committee.clone())); // todo use Arc
+        }
+        let committee = self.tables.committee_map.get(epoch_id)?;
+        if let Some(committee) = committee.as_ref() {
+            self.cache.write().insert(*epoch_id, committee.clone()); // todo use Arc
+        }
+        Ok(committee)
     }
 
+    // todo - make use of cache or remove this method
     pub fn get_latest_committee(&self) -> Committee {
-        self.committee_map
+        self.tables
+            .committee_map
             .iter()
             .skip_to_last()
             .next()
@@ -77,6 +106,7 @@ impl CommitteeStore {
             .1
     }
     /// Return the committee specified by `epoch`. If `epoch` is `None`, return the latest committee.
+    // todo - make use of cache or remove this method
     pub fn get_or_latest_committee(&self, epoch: Option<EpochId>) -> SuiResult<Committee> {
         Ok(match epoch {
             Some(epoch) => self
@@ -87,12 +117,13 @@ impl CommitteeStore {
     }
 
     pub fn checkpoint_db(&self, path: &Path) -> SuiResult {
-        self.committee_map
+        self.tables
+            .committee_map
             .checkpoint_db(path)
             .map_err(SuiError::StorageError)
     }
 
     fn database_is_empty(&self) -> bool {
-        self.committee_map.iter().next().is_none()
+        self.tables.committee_map.iter().next().is_none()
     }
 }
