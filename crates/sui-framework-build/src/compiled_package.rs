f89057f80cfd1f1b296fab1f7af6e11aa98c0ff7
Rijnard van Tonder <rvantonder@gmail.com>
2023-03-09 15:08:28 -0800

    chore: use std::io::sink where possible

--- crates/sui-framework-build/src/compiled_package.rs
+++ crates/sui-framework-build/src/compiled_package.rs
@@ -138,7 +138,7 @@ impl BuildConfig {
                 })?
         } else {
             self.config
-                .resolution_graph_for_package(&path, &mut Vec::new())
+                .resolution_graph_for_package(&path, &mut std::io::sink())
                 .map_err(|err| SuiError::ModuleBuildFailure {
                     error: format!("{:?}", err),
                 })?
@@ -161,7 +161,7 @@ pub fn build_from_resolution_graph(
     let result = if print_diags_to_stderr {
         BuildConfig::compile_package(resolution_graph, &mut std::io::stderr())
     } else {
-        BuildConfig::compile_package(resolution_graph, &mut Vec::new())
+        BuildConfig::compile_package(resolution_graph, &mut std::io::sink())
     };
     // write build failure diagnostics to stderr, convert `error` to `String` using `Debug`
     // format to include anyhow's error context chain.
