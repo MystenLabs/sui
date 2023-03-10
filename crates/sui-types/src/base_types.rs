1f562c4c532cb734978207de1ce0572d6eef9041
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-03-10 17:55:15 +0100

    Use default hash functions

--- crates/sui-types/src/base_types.rs
+++ crates/sui-types/src/base_types.rs
@@ -9,8 +9,8 @@ use crate::coin::COIN_MODULE_NAME;
 use crate::coin::COIN_STRUCT_NAME;
 pub use crate::committee::EpochId;
 use crate::crypto::{
-    AuthorityPublicKey, AuthorityPublicKeyBytes, KeypairTraits, PublicKey, SignatureScheme,
-    SuiPublicKey, SuiSignature,
+    AuthorityPublicKey, AuthorityPublicKeyBytes, InternalHash, KeypairTraits, PublicKey,
+    SignatureScheme, SuiPublicKey, SuiSignature, UserHash,
 };
 pub use crate::digests::{ObjectDigest, TransactionDigest, TransactionEffectsDigest};
 use crate::dynamic_field::DynamicFieldInfo;
@@ -39,7 +39,7 @@ use crate::SUI_FRAMEWORK_ADDRESS;
 use anyhow::anyhow;
 use fastcrypto::encoding::decode_bytes_hex;
 use fastcrypto::encoding::{Encoding, Hex};
-use fastcrypto::hash::{HashFunction, Sha3_256};
+use fastcrypto::hash::HashFunction;
 use move_core_types::account_address::AccountAddress;
 use move_core_types::ident_str;
 use move_core_types::identifier::IdentStr;
@@ -434,7 +434,7 @@ impl TryFrom<Vec<u8>> for SuiAddress {
 
 impl From<&AuthorityPublicKeyBytes> for SuiAddress {
     fn from(pkb: &AuthorityPublicKeyBytes) -> Self {
-        let mut hasher = Sha3_256::default();
+        let mut hasher = UserHash::default();
         hasher.update([AuthorityPublicKey::SIGNATURE_SCHEME.flag()]);
         hasher.update(pkb);
         let g_arr = hasher.finalize();
@@ -448,7 +448,7 @@ impl From<&AuthorityPublicKeyBytes> for SuiAddress {
 
 impl<T: SuiPublicKey> From<&T> for SuiAddress {
     fn from(pk: &T) -> Self {
-        let mut hasher = Sha3_256::default();
+        let mut hasher = UserHash::default();
         hasher.update([T::SIGNATURE_SCHEME.flag()]);
         hasher.update(pk);
         let g_arr = hasher.finalize();
@@ -462,7 +462,7 @@ impl<T: SuiPublicKey> From<&T> for SuiAddress {
 
 impl From<&PublicKey> for SuiAddress {
     fn from(pk: &PublicKey) -> Self {
-        let mut hasher = Sha3_256::default();
+        let mut hasher = UserHash::default();
         hasher.update([pk.flag()]);
         hasher.update(pk);
         let g_arr = hasher.finalize();
@@ -479,7 +479,7 @@ impl From<&PublicKey> for SuiAddress {
 /// of all participating public keys and its weight.
 impl From<MultiSigPublicKey> for SuiAddress {
     fn from(multisig_pk: MultiSigPublicKey) -> Self {
-        let mut hasher = Sha3_256::default();
+        let mut hasher = UserHash::default();
         hasher.update([SignatureScheme::MultiSig.flag()]);
         hasher.update(multisig_pk.threshold().to_le_bytes());
         multisig_pk.pubkeys().iter().for_each(|(pk, w)| {
@@ -892,7 +892,7 @@ impl ObjectID {
     pub fn derive_id(digest: TransactionDigest, creation_num: u64) -> Self {
         // TODO(https://github.com/MystenLabs/sui/issues/58):audit ID derivation
 
-        let mut hasher = Sha3_256::default();
+        let mut hasher = InternalHash::default();
         hasher.update(digest);
         hasher.update(creation_num.to_le_bytes());
         let hash = hasher.finalize();
