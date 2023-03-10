6a5bc304920654091b42165a721b59f86e47db5b
ade <ade@mystenlabs.com>
2023-03-09 19:52:49 -0500

    increase number of gas objects for payment

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -696,7 +696,7 @@ impl ProtocolConfig {
                 // We need this number to be at least 100x less than `max_serialized_tx_effects_size_bytes`otherwise effects can be huge
                 max_mutable_inputs: Some(1024),
                 max_serialized_tx_effects_size_bytes: Some(512 * 1024),
-                max_gas_payment_objects: Some(32),
+                max_gas_payment_objects: Some(256),
                 max_modules_in_publish: Some(128),
                 max_arguments: Some(512),
                 max_type_arguments: Some(16),
