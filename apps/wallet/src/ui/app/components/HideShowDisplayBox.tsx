85dbda75816c97018bde2172b2dd3cba37a140fc
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-03-10 00:18:52 +0000

    wallet-ext: backup view hide mnemonic by default
    * mnemonic is hidden by default in backup view and user will have to click to reveal it
    * after 3 minutes hide-show box will hide it's contents automatically in case user forgot the tab open
    * also after switching to another tab/app, moving the wallet to background it will auto hide again
    * the above will be the same for exported private key as well
    * store mnemonic in this view as an array hoping it's more difficult for an attacker to find it in memory while on this page
    * also hide-show box renders arrays in spans separated by commas for the same reason

--- apps/wallet/src/ui/app/components/HideShowDisplayBox.tsx
+++ apps/wallet/src/ui/app/components/HideShowDisplayBox.tsx
@@ -2,49 +2,98 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Copy16, EyeClose16, EyeOpen16 } from '@mysten/icons';
-import { useState } from 'react';
+import { cx } from 'class-variance-authority';
+import { useEffect, useState } from 'react';
 
 import { useCopyToClipboard } from '../hooks/useCopyToClipboard';
 import { Link } from '../shared/Link';
 import { Text } from '../shared/text';
 
+const AUTO_HIDE_INTERVAL = 3 * 60 * 1000;
+
 export type HideShowDisplayBoxProps = {
-    value: string;
+    value: string | string[];
+    hideCopy?: boolean;
     copiedMessage?: string;
 };
 
 export function HideShowDisplayBox({
     value,
+    hideCopy = false,
     copiedMessage,
 }: HideShowDisplayBoxProps) {
     const [valueHidden, setValueHidden] = useState(true);
-    const copyCallback = useCopyToClipboard(value, {
-        copySuccessMessage: copiedMessage,
-    });
+    const copyCallback = useCopyToClipboard(
+        hideCopy ? '' : typeof value === 'string' ? value : value.join(' '),
+        {
+            copySuccessMessage: copiedMessage,
+        }
+    );
+    useEffect(() => {
+        const updateOnVisibilityChange = () => {
+            if (document.visibilityState === 'hidden') {
+                setValueHidden(true);
+            }
+        };
+        document.addEventListener('visibilitychange', updateOnVisibilityChange);
+        return () => {
+            document.removeEventListener(
+                'visibilitychange',
+                updateOnVisibilityChange
+            );
+        };
+    }, []);
+    useEffect(() => {
+        let timeout: number;
+        if (!valueHidden) {
+            timeout = window.setTimeout(() => {
+                setValueHidden(true);
+            }, AUTO_HIDE_INTERVAL);
+        }
+        return () => {
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+        };
+    }, [valueHidden]);
     return (
         <div className="flex flex-col flex-nowrap items-stretch gap-2 bg-white border border-solid border-gray-60 rounded-lg overflow-hidden py-4 px-5">
-            <div className="break-all">
-                {valueHidden ? (
-                    <div className="flex flex-col gap-1.5">
-                        <div className="h-3.5 bg-gray-40 rounded-md" />
-                        <div className="h-3.5 bg-gray-40 rounded-md" />
-                        <div className="h-3.5 bg-gray-40 rounded-md w-1/2" />
+            <div className="break-all relative">
+                {valueHidden ? null : (
+                    <div className="absolute top-0">
+                        <Text variant="p1" weight="medium" color="steel-darker">
+                            {typeof value === 'string'
+                                ? value
+                                : value.map((aValue, index) => (
+                                      <span key={index}>
+                                          {(index > 0 ? ' ' : '') + aValue}
+                                      </span>
+                                  ))}
+                        </Text>
                     </div>
-                ) : (
-                    <Text variant="p1" weight="medium" color="steel-darker">
-                        {value}
-                    </Text>
                 )}
+                <div
+                    className={cx(
+                        'flex flex-col gap-1.5',
+                        valueHidden ? 'visible' : 'invisible'
+                    )}
+                >
+                    <div className="h-3.5 bg-gray-40 rounded-md" />
+                    <div className="h-3.5 bg-gray-40 rounded-md" />
+                    <div className="h-3.5 bg-gray-40 rounded-md w-1/2" />
+                </div>
             </div>
             <div className="flex flex-row flex-nowrap items-center justify-between">
                 <div>
-                    <Link
-                        color="heroDark"
-                        weight="medium"
-                        text="Copy"
-                        before={<Copy16 />}
-                        onClick={copyCallback}
-                    />
+                    {!hideCopy ? (
+                        <Link
+                            color="heroDark"
+                            weight="medium"
+                            text="Copy"
+                            before={<Copy16 />}
+                            onClick={copyCallback}
+                        />
+                    ) : null}
                 </div>
                 <div>
                     <Link
