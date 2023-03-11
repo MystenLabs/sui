2e0ad7549930eaea25e818bb17a8a0508a40494c
Mark Logan <mark@mystenlabs.com>
2023-03-10 23:18:11 -0800

    Async batch verifier

--- crates/sui-core/src/batch_bls_verifier.rs
+++ crates/sui-core/src/batch_bls_verifier.rs
@@ -1,17 +1,142 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use futures::pin_mut;
+use itertools::izip;
+use parking_lot::{Mutex, MutexGuard};
 use shared_crypto::intent::{Intent, IntentScope};
+use std::sync::Arc;
 use sui_types::{
     committee::Committee,
     crypto::{AuthoritySignInfoTrait, VerificationObligation},
-    error::SuiResult,
+    error::{SuiError, SuiResult},
     message_envelope::Message,
-    messages::CertifiedTransaction,
+    messages::{CertifiedTransaction, VerifiedCertificate},
 };
 
 use tap::TapFallible;
+use tokio::{
+    sync::oneshot,
+    time::{timeout, Duration},
+};
 use tracing::error;
+
+type Sender = oneshot::Sender<SuiResult<VerifiedCertificate>>;
+
+struct CertBuffer {
+    certs: Vec<CertifiedTransaction>,
+    senders: Vec<Sender>,
+    id: u64,
+}
+
+impl CertBuffer {
+    fn new(capacity: usize) -> Self {
+        Self {
+            certs: Vec::with_capacity(capacity),
+            senders: Vec::with_capacity(capacity),
+            id: 0,
+        }
+    }
+
+    fn take_and_replace(&mut self) -> Self {
+        let mut new = CertBuffer::new(self.capacity());
+        new.id = self.id + 1;
+        std::mem::swap(&mut new, self);
+        new
+    }
+
+    fn capacity(&self) -> usize {
+        debug_assert_eq!(self.certs.capacity(), self.senders.capacity());
+        self.certs.capacity()
+    }
+
+    fn len(&self) -> usize {
+        debug_assert_eq!(self.certs.len(), self.senders.len());
+        self.certs.len()
+    }
+
+    fn push(&mut self, tx: Sender, cert: CertifiedTransaction) {
+        self.senders.push(tx);
+        self.certs.push(cert);
+    }
+}
+
+pub struct AsyncBatchVerifier {
+    committee: Arc<Committee>,
+    queue: Mutex<CertBuffer>,
+}
+
+impl AsyncBatchVerifier {
+    pub fn new(committee: Arc<Committee>, capacity: usize) -> Self {
+        Self {
+            committee,
+            queue: Mutex::new(CertBuffer::new(capacity)),
+        }
+    }
+
+    pub async fn verify_cert(&self, cert: CertifiedTransaction) -> SuiResult<VerifiedCertificate> {
+        // this is the only innocent error we are likely to encounter - filter it before we poison
+        // a whole batch.
+        if cert.auth_sig().epoch != self.committee.epoch() {
+            return Err(SuiError::WrongEpoch {
+                expected_epoch: self.committee.epoch(),
+                actual_epoch: cert.auth_sig().epoch,
+            });
+        }
+
+        let (tx, rx) = oneshot::channel();
+        pin_mut!(rx);
+
+        let prev_id = {
+            let mut queue = self.queue.lock();
+            queue.push(tx, cert);
+            if queue.len() == queue.capacity() {
+                self.process_queue(queue);
+                // unwrap ok - process_queue will have sent the result already
+                return rx.try_recv().unwrap();
+            }
+            queue.id
+        };
+
+        if let Ok(res) = timeout(Duration::from_millis(10), &mut rx).await {
+            // unwrap ok - tx cannot have been dropped without sending a result.
+            return res.unwrap();
+        }
+
+        {
+            let queue = self.queue.lock();
+            // check if another thread took the queue while we were re-acquiring lock.
+            if prev_id == queue.id {
+                self.process_queue(queue);
+                // unwrap ok - process_queue will have sent the result already
+                return rx.try_recv().unwrap();
+            }
+        }
+
+        // unwrap ok - another took the queue while we were re-acquiring the lock and is
+        // guaranteed to process the queue immediately.
+        return rx.await.unwrap();
+    }
+
+    fn process_queue(&self, mut queue: MutexGuard<'_, CertBuffer>) {
+        let taken = queue.take_and_replace();
+        drop(queue);
+
+        let results = batch_verify_certificates(&self.committee, &taken.certs);
+        izip!(
+            results.into_iter(),
+            taken.certs.into_iter(),
+            taken.senders.into_iter(),
+        )
+        .for_each(|(result, cert, tx)| {
+            tx.send(match result {
+                Ok(()) => Ok(VerifiedCertificate::new_unchecked(cert)),
+                Err(e) => Err(e),
+            })
+            .ok();
+        });
+    }
+}
 
 /// Verifies all certificates - if any fail return error.
 pub fn batch_verify_all_certificates(
--- crates/sui-core/src/unit_tests/batch_verification_tests.rs
+++ crates/sui-core/src/unit_tests/batch_verification_tests.rs
@@ -1,20 +1,24 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use sui_types::committee::Committee;
-
 use crate::batch_bls_verifier::*;
 use crate::test_utils::{make_cert_with_large_committee, make_dummy_tx};
+use futures::future::join_all;
+use rand::{thread_rng, Rng};
+use std::sync::Arc;
 use sui_macros::sim_test;
-use sui_types::crypto::{get_key_pair, AccountKeyPair};
+use sui_types::committee::Committee;
+use sui_types::crypto::{get_key_pair, AccountKeyPair, AuthorityKeyPair};
+use sui_types::messages::CertifiedTransaction;
 
-#[sim_test]
-async fn test_batch_verify() {
-    let (committee, key_pairs) = Committee::new_simple_test_committee();
-
+fn gen_certs(
+    committee: &Committee,
+    key_pairs: &[AuthorityKeyPair],
+    count: usize,
+) -> Vec<CertifiedTransaction> {
     let (receiver, _): (_, AccountKeyPair) = get_key_pair();
 
-    let senders: Vec<_> = (0..16)
+    let senders: Vec<_> = (0..count)
         .into_iter()
         .map(|_| get_key_pair::<AccountKeyPair>())
         .collect();
@@ -24,10 +28,16 @@ async fn test_batch_verify() {
         .map(|(sender, sender_sec)| make_dummy_tx(receiver, *sender, sender_sec))
         .collect();
 
-    let certs: Vec<_> = txns
-        .iter()
-        .map(|t| make_cert_with_large_committee(&committee, &key_pairs, t))
-        .collect();
+    txns.iter()
+        .map(|t| make_cert_with_large_committee(&committee, key_pairs, t))
+        .collect()
+}
+
+#[sim_test]
+async fn test_batch_verify() {
+    let (committee, key_pairs) = Committee::new_simple_test_committee();
+
+    let certs = gen_certs(&committee, &key_pairs, 16);
 
     batch_verify_all_certificates(&committee, &certs).unwrap();
 
@@ -35,6 +45,7 @@ async fn test_batch_verify() {
     // this test is a bit much for the current implementation - it was originally written to verify
     // a bisecting fall back approach.
     for i in 0..16 {
+        let (receiver, _): (_, AccountKeyPair) = get_key_pair();
         let mut certs = certs.clone();
         let other_tx = make_dummy_tx(receiver, other_sender, &other_sender_sec);
         let other_cert = make_cert_with_large_committee(&committee, &key_pairs, &other_tx);
@@ -47,4 +58,41 @@ async fn test_batch_verify() {
             r.as_ref().unwrap();
         }
     }
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+async fn test_async_verifier() {
+    let (committee, key_pairs) = Committee::new_simple_test_committee();
+    let committee = Arc::new(committee);
+    let key_pairs = Arc::new(key_pairs);
+
+    let verifier = Arc::new(AsyncBatchVerifier::new(committee.clone(), 16));
+
+    let tasks: Vec<_> = (0..32)
+        .into_iter()
+        .map(|_| {
+            let verifier = verifier.clone();
+            let committee = committee.clone();
+            let key_pairs = key_pairs.clone();
+            tokio::task::spawn(async move {
+                let certs = gen_certs(&committee, &key_pairs, 100);
+
+                let (receiver, _): (_, AccountKeyPair) = get_key_pair();
+                let (other_sender, other_sender_sec): (_, AccountKeyPair) = get_key_pair();
+                let other_tx = make_dummy_tx(receiver, other_sender, &other_sender_sec);
+                let other_cert = make_cert_with_large_committee(&committee, &key_pairs, &other_tx);
+
+                for mut c in certs.into_iter() {
+                    if thread_rng().gen_range(0..20) == 0 {
+                        *c.auth_sig_mut_for_testing() = other_cert.auth_sig().clone();
+                        verifier.verify_cert(c).await.unwrap_err();
+                    } else {
+                        verifier.verify_cert(c).await.unwrap();
+                    }
+                }
+            })
+        })
+        .collect();
+
+    join_all(tasks).await;
 }
