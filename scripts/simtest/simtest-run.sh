9f04d86bb53a6d9ac6446dba285dedd6fab67378
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2023-03-12 10:46:52 -0700

    Simplify env vars; don't run ignored tests (#9144)

--- scripts/simtest/simtest-run.sh
+++ scripts/simtest/simtest-run.sh
@@ -18,6 +18,7 @@ SEED="$DATE"
 # TODO: this logs directly to stdout since it is not being run in parallel. is that ok?
 MSIM_TEST_SEED="$SEED" \
 MSIM_WATCHDOG_TIMEOUT_MS=60000 \
+MSIM_TEST_NUM=30 \
 scripts/simtest/cargo-simtest simtest \
   --package sui \
   --package sui-core \
@@ -35,11 +36,10 @@ for SUB_SEED in `seq 1 $NUM_CPUS`; do
 
   # --test-threads 1 is important: parallelism is achieved via the for loop
   MSIM_TEST_SEED="$SEED" \
-  MSIM_TEST_NUM=1 \
   MSIM_WATCHDOG_TIMEOUT_MS=60000 \
+  SIM_STRESS_TEST_DURATION_SECS=300 \
   scripts/simtest/cargo-simtest simtest \
     --package sui-benchmark \
-    --run-ignored all \
     --test-threads 1 \
     --profile simtestnightly \
     -E "$TEST_FILTER" > "$LOG_FILE" 2>&1 &
