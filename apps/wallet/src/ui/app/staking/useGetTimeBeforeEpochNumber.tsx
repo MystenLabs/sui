fb4761fd7780da401b31ebc5c7a4afe68d40b474
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-03-13 14:11:19 -0400

    nit

--- apps/wallet/src/ui/app/staking/useGetTimeBeforeEpochNumber.tsx
+++ apps/wallet/src/ui/app/staking/useGetTimeBeforeEpochNumber.tsx
@@ -3,12 +3,11 @@
 
 import { useSystemState } from './useSystemState';
 
-// Get start time for the current epoch and specified epoch
+// Get time between current epoch and specified epoch
 // Get the period between the current epoch and next epoch
 export function useGetTimeBeforeEpochNumber(epoch: number) {
-    // Get current epoch
     const data = useSystemState();
-    // Get current epoch
+    // Current epoch
     const currentEpoch = data.data?.epoch || 0;
     const currentEpochStartTime = data.data?.epochStartTimestampMs || 0;
     // TODO: Get the period between epochs from system state
