544275c7da03dc92d45a70a93f4cfa5c616fe7ec
Ashok Menon <ashok@mystenlabs.com>
2023-03-13 16:41:25 +0000

    Simplify move_package_tests using macro magic

--- crates/sui-core/src/unit_tests/move_package_tests.rs
+++ crates/sui-core/src/unit_tests/move_package_tests.rs
@@ -13,6 +13,38 @@ use sui_types::{
 
 use std::{collections::BTreeMap, path::PathBuf};
 
+macro_rules! type_origin_table {
+    {$($module:ident :: $type:ident => $pkg:expr),* $(,)?} => {{
+        let mut table = BTreeMap::new();
+        $(
+            table.insert(
+                ModuleStruct {
+                    module_name: stringify!($module).to_string(),
+                    struct_name: stringify!($type).to_string(),
+                },
+                $pkg,
+            );
+        )*
+        table
+    }}
+}
+
+macro_rules! linkage_table {
+    {$($original_id:expr => ($upgraded_id:expr, $version:expr)),* $(,)?} => {{
+        let mut table = BTreeMap::new();
+        $(
+            table.insert(
+                $original_id,
+                UpgradeInfo {
+                    upgraded_id: $upgraded_id,
+                    upgraded_version: $version,
+                }
+            );
+        )*
+        table
+    }}
+}
+
 #[tokio::test]
 async fn test_simple_move_package_create() {
     let (std_move_pkg, sui_move_pkg) = sui_framework::make_std_sui_move_pkgs();
@@ -25,15 +57,24 @@ async fn test_simple_move_package_create() {
     )
     .unwrap();
 
-    let mod_name = "object_basics";
     let pkg_id = move_pkg.id();
-    let type_origin_table = move_pkg.type_origin_table();
-    // this package defines 4 structs: Name, NewValueEvent, Object, and Wrapper
-    assert!(type_origin_table.len() == 4);
-    verify_object_basics_initial_types(type_origin_table, &pkg_id, mod_name).await;
+    assert_eq!(
+        move_pkg.type_origin_table(),
+        &type_origin_table! {
+            object_basics::Name => pkg_id,
+            object_basics::NewValueEvent => pkg_id,
+            object_basics::Object => pkg_id,
+            object_basics::Wrapper => pkg_id,
+        }
+    );
 
-    let linkage_table = move_pkg.linkage_table();
-    verify_object_basics_linkage(linkage_table).await;
+    assert_eq!(
+        move_pkg.linkage_table(),
+        &linkage_table! {
+            MOVE_STDLIB_OBJECT_ID => (MOVE_STDLIB_OBJECT_ID, OBJECT_START_VERSION),
+            SUI_FRAMEWORK_OBJECT_ID => (SUI_FRAMEWORK_OBJECT_ID, OBJECT_START_VERSION),
+        }
+    );
 }
 
 #[tokio::test]
@@ -49,108 +90,54 @@ async fn test_simple_move_upgraded_package_create() {
     .unwrap();
 
     let upgraded_modules = build_test_modules("object_basics_upgraded");
-    let storage_id = ObjectID::random();
+    let upgraded_id = ObjectID::random();
     let upgraded_move_pkg = move_pkg
         .new_upgraded(
-            storage_id,
+            upgraded_id,
             upgraded_modules,
             u64::MAX,
             [&std_move_pkg, &sui_move_pkg],
         )
         .unwrap();
 
-    let mod_name = "object_basics";
     let pkg_id = move_pkg.id();
-    let type_origin_table = move_pkg.type_origin_table();
-    // the original version of this package defines 4 structs: Name, NewValueEvent, Object, and Wrapper
-    assert!(type_origin_table.len() == 4);
-    verify_object_basics_initial_types(type_origin_table, &pkg_id, mod_name).await;
-    let linkage_table = move_pkg.linkage_table();
-    verify_object_basics_linkage(linkage_table).await;
-
-    let upgraded_type_origin_table = upgraded_move_pkg.type_origin_table();
-    // the upgraded version of this package defines 5 structs: Name, NewValueEvent, Object, and
-    // Wrapper (from the original version) and ObjectNewVersion (from the upgraded version)
-
-    // check entries from the original package
-    verify_object_basics_initial_types(upgraded_type_origin_table, &pkg_id, mod_name).await;
-    // check the entry added by the upgraded package
-    assert!(
-        upgraded_type_origin_table
-            .get(&ModuleStruct {
-                module_name: mod_name.to_string(),
-                struct_name: "ObjectNewVersion".to_string(),
-            })
-            .unwrap()
-            == &storage_id
+    assert_eq!(
+        move_pkg.type_origin_table(),
+        &type_origin_table! {
+            object_basics::Name => pkg_id,
+            object_basics::NewValueEvent => pkg_id,
+            object_basics::Object => pkg_id,
+            object_basics::Wrapper => pkg_id,
+        }
     );
 
-    let upgraded_linkage_table = move_pkg.linkage_table();
+    assert_eq!(
+        move_pkg.linkage_table(),
+        &linkage_table! {
+            MOVE_STDLIB_OBJECT_ID => (MOVE_STDLIB_OBJECT_ID, OBJECT_START_VERSION),
+            SUI_FRAMEWORK_OBJECT_ID => (SUI_FRAMEWORK_OBJECT_ID, OBJECT_START_VERSION),
+        }
+    );
+
+    assert_eq!(
+        upgraded_move_pkg.type_origin_table(),
+        &type_origin_table! {
+            object_basics::Name => pkg_id,
+            object_basics::NewValueEvent => pkg_id,
+            object_basics::Object => pkg_id,
+            object_basics::Wrapper => pkg_id,
+            object_basics::ObjectNewVersion => upgraded_id,
+        }
+    );
+
     // the linkage table in this upgraded package is actually the same as in the original version as
     // dependencies haven't changed
-    verify_object_basics_linkage(upgraded_linkage_table).await;
-}
-
-async fn verify_object_basics_initial_types(
-    type_origin_table: &BTreeMap<ModuleStruct, ObjectID>,
-    pkg_id: &ObjectID,
-    mod_name: &str,
-) {
-    assert!(
-        type_origin_table
-            .get(&ModuleStruct {
-                module_name: mod_name.to_string(),
-                struct_name: "Name".to_string(),
-            })
-            .unwrap()
-            == pkg_id
-    );
-    assert!(
-        type_origin_table
-            .get(&ModuleStruct {
-                module_name: mod_name.to_string(),
-                struct_name: "NewValueEvent".to_string(),
-            })
-            .unwrap()
-            == pkg_id
-    );
-    assert!(
-        type_origin_table
-            .get(&ModuleStruct {
-                module_name: mod_name.to_string(),
-                struct_name: "Object".to_string(),
-            })
-            .unwrap()
-            == pkg_id
-    );
-    assert!(
-        type_origin_table
-            .get(&ModuleStruct {
-                module_name: mod_name.to_string(),
-                struct_name: "Wrapper".to_string(),
-            })
-            .unwrap()
-            == pkg_id
-    );
-}
-
-async fn verify_object_basics_linkage(linkage_table: &BTreeMap<ObjectID, UpgradeInfo>) {
-    // this package depends on standard library and sui framework at their original versions
-    assert!(linkage_table.len() == 2);
-    assert!(
-        linkage_table.get(&MOVE_STDLIB_OBJECT_ID).unwrap()
-            == &UpgradeInfo {
-                upgraded_id: MOVE_STDLIB_OBJECT_ID,
-
-                upgraded_version: OBJECT_START_VERSION,
-            }
-    );
-    assert!(
-        linkage_table.get(&SUI_FRAMEWORK_OBJECT_ID).unwrap()
-            == &UpgradeInfo {
-                upgraded_id: SUI_FRAMEWORK_OBJECT_ID,
-                upgraded_version: OBJECT_START_VERSION,
-            }
+    assert_eq!(
+        upgraded_move_pkg.linkage_table(),
+        &linkage_table! {
+            MOVE_STDLIB_OBJECT_ID => (MOVE_STDLIB_OBJECT_ID, OBJECT_START_VERSION),
+            SUI_FRAMEWORK_OBJECT_ID => (SUI_FRAMEWORK_OBJECT_ID, OBJECT_START_VERSION),
+        }
     );
 }
 
