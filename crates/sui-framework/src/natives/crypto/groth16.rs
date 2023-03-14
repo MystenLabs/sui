e38a96953f0cedaa22c9748c825a0c68f6e9b693
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-03-14 09:21:59 +0100

    Doc

--- crates/sui-framework/src/natives/crypto/groth16.rs
+++ crates/sui-framework/src/natives/crypto/groth16.rs
@@ -15,7 +15,7 @@ use std::collections::VecDeque;
 pub const INVALID_VERIFYING_KEY: u64 = 0;
 pub const INVALID_CURVE: u64 = 1;
 
-// These must match the values in sui::groth16::Curve.
+// These must match the corresponding values in sui::groth16::Curve.
 pub const BLS12381: u8 = 0;
 pub const BN254: u8 = 1;
 
