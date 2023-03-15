7958980e716e2826b5a35a6ad5dc96fe0562fed9
Jordan Gensler <jordan@mystenlabs.com>
2023-03-14 23:22:42 -0700

    Bye bye longtext

--- apps/explorer/src/pages/transaction-result/TransactionResultType.tsx
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-// TODO: Remove this when we remove Longtext
-export type Category = 'object' | 'transaction' | 'address' | 'unknown';
--- apps/wallet/src/ui/app/components/receipt-card/StakeTxnCard.tsx
+++ apps/wallet/src/ui/app/components/receipt-card/StakeTxnCard.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { calculateAPY }from '@mysten/core';
+import { calculateAPY } from '@mysten/core';
 import { SUI_TYPE_ARG } from '@mysten/sui.js';
 import { useMemo } from 'react';
 
