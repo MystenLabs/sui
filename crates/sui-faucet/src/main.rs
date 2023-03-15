62cb1d6890db73f3c18a193af856b0d345ba3e4d
Chris Li <76067158+666lcz@users.noreply.github.com>
2023-03-15 17:54:14 -0400

    [faucet] consider reference gas price (#9376)
    ## Description 
    
    Quick fix for the gas budget to unblock testnet. Will follow up with a
    PR to change rust SDK to support deriving gas price from
    dryrun/computation bucket.
    
    ## Test Plan 
    
    CI

--- crates/sui-faucet/src/main.rs
+++ crates/sui-faucet/src/main.rs
@@ -41,7 +41,7 @@ struct FaucetConfig {
     #[clap(long, default_value = "127.0.0.1")]
     host_ip: Ipv4Addr,
 
-    #[clap(long, default_value_t = 50000)]
+    #[clap(long, default_value_t = 200_000_000)]
     amount: u64,
 
     #[clap(long, default_value_t = 5)]
