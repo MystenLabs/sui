6761a9babdee66bcf8b2cc546dc153359bd641c8
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-03-16 08:09:13 -0400

    update

--- apps/wallet/src/ui/app/staking/validators/SelectValidatorCard.tsx
+++ apps/wallet/src/ui/app/staking/validators/SelectValidatorCard.tsx
@@ -53,24 +53,29 @@ export function SelectValidatorCard() {
         if (!data) return [];
 
         const sortedAsc = data.activeValidators
-            .map((validator) => ({
-                name: validator.name,
-                address: validator.suiAddress,
-                apy: calculateAPY(validator, +data.epoch),
-                stakeShare: calculateStakeShare(
-                    BigInt(validator.stakingPoolSuiBalance),
-                    BigInt(totalStake)
-                ),
-                // flag for if the validator is at risk of being removed from the active set
-                atRiskValidator: data.atRiskValidators.some(
-                    (item) => item[0] === validator.suiAddress
-                ),
-                // flag new validators that have been activated in the current epoch
-                // for genesis validators, this will be false
-                newValidator:
-                    validator.stakingPoolActivationEpoch === +data.epoch &&
-                    data.epoch > 0,
-            }))
+            .map((validator) => {
+                const stakingPoolActivationEpoch =
+                    validator.stakingPoolActivationEpoch || 0;
+
+                return {
+                    name: validator.name,
+                    address: validator.suiAddress,
+                    apy: calculateAPY(validator, +data.epoch),
+                    stakeShare: calculateStakeShare(
+                        BigInt(validator.stakingPoolSuiBalance),
+                        BigInt(totalStake)
+                    ),
+                    // flag for if the validator is at risk of being removed from the active set
+                    atRiskValidator: data.atRiskValidators.some(
+                        (item) => item[0] === validator.suiAddress
+                    ),
+                    // flag new validators that have been activated in the current epoch
+                    // for genesis validators, this will be false
+                    newValidator:
+                        data.epoch - stakingPoolActivationEpoch <= 1 &&
+                        data.epoch !== 0,
+                };
+            })
             .sort((a, b) => {
                 if (sortKey === 'name') {
                     return a[sortKey].localeCompare(b[sortKey], 'en', {
@@ -182,6 +187,8 @@ export function SelectValidatorCard() {
                                             ? '-'
                                             : `${validator[sortKey]}%`
                                     }
+                                    newValidator={validator.newValidator}
+                                    atRisk={validator.atRiskValidator}
                                 />
                             </div>
                         ))}
