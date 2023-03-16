324aea85e9dc34c000dabf68068ff4576a0983ab
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2023-03-16 00:16:01 -0400

    indexer: configurable rpc server url and port (#9362)
    ## Description 
    
    configurable RPC server addr and port
    
    ## Test Plan 
    
    - CI
    - local run to make sure the addr and port can be configured
    ```
    cargo run --bin sui-indexer -- --db-url "postgres://postgres:postgres@localhost/gegao" --rpc-client-url "http://0.0.0.0:9000" --client-metric-host "127.0.0.1" --client-metric-port 9184 --rpc-server-port 3030
        Finished dev [unoptimized + debuginfo] target(s) in 0.57s
         Running `/Users/gegao/Documents/sui/target/debug/sui-indexer --db-url 'postgres://postgres:postgres@localhost/gegao' --rpc-client-url 'http://0.0.0.0:9000' --client-metric-host 127.0.0.1 --client-metric-port 9184 --rpc-server-port 3030`
    2023-03-15T19:02:36.943632Z  INFO sui_indexer::store::pg_indexer_store: Found 2 tables with partitions : [{"objects_history": "5", "owner_history": "5"}]
    2023-03-15T19:02:36.953308Z  INFO sui_json_rpc: acl=Const("*")
    2023-03-15T19:02:36.954266Z  INFO sui_json_rpc: Compatibility method routing enabled.
    2023-03-15T19:02:36.954544Z  INFO sui_json_rpc: Sui JSON-RPC server listening on 0.0.0.0:3030 local_addr=0.0.0.0:3030
    ```

--- crates/sui-indexer/tests/integration_tests.rs
+++ crates/sui-indexer/tests/integration_tests.rs
@@ -13,8 +13,7 @@ mod pg_integration {
     use sui_config::SUI_KEYSTORE_FILENAME;
     use sui_indexer::errors::IndexerError;
     use sui_indexer::store::{IndexerStore, PgIndexerStore};
-    use sui_indexer::PgPoolConnection;
-    use sui_indexer::{new_pg_connection_pool, Indexer};
+    use sui_indexer::{new_pg_connection_pool, Indexer, IndexerConfig, PgPoolConnection};
     use sui_json_rpc::api::{ReadApiClient, TransactionBuilderClient, WriteApiClient};
     use sui_json_rpc_types::{
         SuiMoveObject, SuiObjectDataOptions, SuiObjectResponse, SuiParsedMoveObject,
@@ -226,14 +225,19 @@ mod pg_integration {
         let store_clone = store.clone();
         let registry = Registry::default();
 
-        let rpc_url = test_cluster.rpc_url().to_string();
+        let mut config = IndexerConfig::default();
+        config.rpc_client_url = test_cluster.rpc_url().to_string();
+        let indexer_config = config.clone();
         let handle =
-            tokio::spawn(async move { Indexer::start(&rpc_url, &registry, store_clone).await });
+            tokio::spawn(
+                async move { Indexer::start(&indexer_config, &registry, store_clone).await },
+            );
 
-        // TODO: make indexer port configurable
-        let http_client = HttpClientBuilder::default()
-            .build("http://0.0.0.0:3030")
-            .unwrap();
+        let http_addr_port = format!(
+            "http://{}:{}",
+            config.rpc_server_url, config.rpc_server_port
+        );
+        let http_client = HttpClientBuilder::default().build(http_addr_port).unwrap();
 
         (test_cluster, http_client, store, handle)
     }
