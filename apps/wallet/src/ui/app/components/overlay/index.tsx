50dce939d9ed04bc9d0918efaf50243d31459e36
William Robertson <williamrobertson13@users.noreply.github.com>
2023-03-15 20:11:11 -0400

    [wallet-ext] fix toasts being hidden behind overlay screens (#9322)
    ## Description 
    
    I introduced a bug the other day where toast messages were being hidden
    on overlay screens. This PR makes a generic `Portal` component and
    portals both the overlay and toaster components inside the main page
    layout container. To get the correct stacking order, we're just going to
    use `9999` for the toaster and `999` for the overlay (it would be nice
    to figure out a better way to manage these types of stacking contexts in
    the future so these types of bugs don't happen as frequently). I did
    consider pulling out `toaster-portal-container` to the `body` of the
    document, but that came with its own positioning and layout issues.
    
    <img width="460" alt="image"
    src="https://user-images.githubusercontent.com/7453188/225321997-dea14413-ef63-45e8-a33a-2aea0436d756.png">
    
    ## Test Plan 
    - Opened an overlay and then kicked off a toast message
    - Opened the account menu and played around there
    - Tested in both full-screen and pop-up mode
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [X] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    N/A

--- apps/wallet/src/ui/app/components/overlay/index.tsx
+++ apps/wallet/src/ui/app/components/overlay/index.tsx
@@ -3,10 +3,10 @@
 
 import cl from 'classnames';
 import { useCallback } from 'react';
-import ReactDOM from 'react-dom';
 
 import useAppSelector from '../../hooks/useAppSelector';
 import { AppType } from '../../redux/slices/app/AppType';
+import { Portal } from '../../shared/Portal';
 import Icon, { SuiIcons } from '_components/icon';
 
 import type { ReactNode } from 'react';
@@ -40,31 +40,28 @@ function Overlay({
     const appType = useAppSelector((state) => state.app.appType);
     const isFullScreen = appType === AppType.fullscreen;
 
-    return ReactDOM.createPortal(
-        <>
-            {showModal ? (
-                <div
-                    className={cl(st.container, {
-                        [st.fullScreenContainer]: isFullScreen,
-                    })}
-                >
-                    <div className="bg-gray-40 h-12 w-full">
-                        <div className="text-steel-darker bg-gray-40 flex justify-center h-12 items-center text-heading4 font-semibold">
-                            {title}
-                        </div>
+    return showModal ? (
+        <Portal containerId="overlay-portal-container">
+            <div
+                className={cl(st.container, {
+                    [st.fullScreenContainer]: isFullScreen,
+                })}
+            >
+                <div className="bg-gray-40 h-12 w-full">
+                    <div className="text-steel-darker bg-gray-40 flex justify-center h-12 items-center text-heading4 font-semibold">
+                        {title}
                     </div>
-                    <div className={st.content}>{children}</div>
-                    <button className={st.closeOverlay} onClick={closeModal}>
-                        <Icon
-                            icon={closeIcon}
-                            className={cl(st.close, st[closeIcon])}
-                        />
-                    </button>
                 </div>
-            ) : null}
-        </>,
-        document.getElementById('overlay-container')!
-    );
+                <div className={st.content}>{children}</div>
+                <button className={st.closeOverlay} onClick={closeModal}>
+                    <Icon
+                        icon={closeIcon}
+                        className={cl(st.close, st[closeIcon])}
+                    />
+                </button>
+            </div>
+        </Portal>
+    ) : null;
 }
 
 export default Overlay;
