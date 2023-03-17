52203ffe0e24b847b72917b91ec0875cb847172c
Patrick Kuo <patrickkuo@me.com>
2023-03-17 19:14:43 +0000

    [indexer] - fix indexer partition manager bug (#9287) (#9479)
    ## Description -- cherrypicked from main
    
    Partition manager is crashing when creating new partition because of use
    of dummy values, this PR fixes that and add logs when a partition is
    created.
    
    ## Test Plan 
    
    Run locally and verified partitions are created without error
    ```
    2023-03-14T18:09:34.262516Z  INFO sui_indexer::handlers::checkpoint_handler: Checkpoint 309 committed with 1 transactions and 1 objects.
    2023-03-14T18:09:34.275112Z  INFO sui_indexer::handlers::checkpoint_handler: Checkpoint 310 committed with 2 transactions and 2 objects.
    2023-03-14T18:09:34.285973Z  INFO sui_indexer::store::pg_indexer_store: Created epoch partition 28 for ["objects_history", "owner_history"]
    ```

--- crates/sui-indexer/src/handlers/checkpoint_handler.rs
+++ crates/sui-indexer/src/handlers/checkpoint_handler.rs
@@ -403,6 +403,7 @@ where
                 .as_ref()
                 .map(|_epoch_change| TemporaryEpochStore {
                     owner_index: vec![],
+                    epoch_id: checkpoint.epoch,
                 });
 
         Ok((
