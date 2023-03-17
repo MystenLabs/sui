c8739ab9243e0e756e91f505dd93ef07a65b0bb9
Jian Lu <123408603+healthydeve@users.noreply.github.com>
2023-03-17 16:49:59 -0400

    [ts sdk] removing comment (#9494)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sdk/typescript/test/e2e/dynamic-fields.test.ts
+++ sdk/typescript/test/e2e/dynamic-fields.test.ts
@@ -22,7 +22,6 @@ describe('Dynamic Fields Reading API', () => {
         filter: { StructType: `${packageId}::dynamic_fields_test::Test` },
       })
       .then(function (objects) {
-        console.log(objects);
         const data = objects.data[0].details as SuiObjectData;
         parentObjectId = data.objectId;
       });
