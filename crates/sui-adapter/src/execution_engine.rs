d70f9b84db9cb75f32d940ac5f069b3c79c5d363
Sam Blackshear <sam@mystenlabs.com>
2023-03-20 14:19:33 -0700

    [hardening] updating SUI conservation check to account for dynamic fields, fixing two SUI conservation bugs
    @tnowacki previously pointed out that the previous logic for identifying dynamic field inputs was incorrect + the right way to do this is to look at `writes` and `deletes` to identify objects that are not inputs. Refactored `check_sui_conserved` to use this approach, and turn off the logic that skips any code with dynamic fields.
    
    After doing this, a lot of tests using dynamic fields started failing conservation checks. I investigated this and discovered/fixed two related conservation bugs in `charge_gas_for_storage_changes`:
    - If a dynamic field with storage rebate `N` gets mutated, we will burn `N` SUI
    - If a dynamic field with storage rebate `N` gets deleted, we will burn `N` SUI
    
    After fixing these bugs, all tests pass the conservation checks. But it would still be good to make sure we have test coverage for all {wrap/unwrap/delete}, {dynamic field/input object} scenarios.

--- crates/sui-adapter/src/execution_engine.rs
+++ crates/sui-adapter/src/execution_engine.rs
@@ -225,7 +225,7 @@ fn execute_transaction<
         #[cfg(debug_assertions)]
         {
             // ensure that this transaction did not create or destroy SUI
-            temporary_store.check_sui_conserved();
+            temporary_store.check_sui_conserved().unwrap();
         }
     }
     let cost_summary = gas_status.summary();
