b797ee684707025aaff5038e92d0fa6ebe6127c1
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2023-03-20 22:11:18 -0700

    fix doc test

--- crates/shared-crypto/src/intent.rs
+++ crates/shared-crypto/src/intent.rs
@@ -172,7 +172,7 @@ pub(crate) mod private {
 
 /// A 1-byte domain separator for hashing Object ID in Sui. It is starting from 0xf0
 /// to ensure no hashing collision for any ObjectID vs SuiAddress which is derived
-/// as the hash of `flag || pubkey`. See [`sui_types::crypto::SignatureScheme::flag()`].
+/// as the hash of `flag || pubkey`. See `sui_types::crypto::SignatureScheme::flag()`.
 #[derive(Serialize_repr, Deserialize_repr, Copy, Clone, PartialEq, Eq, Debug, Hash)]
 #[repr(u8)]
 pub enum HashingIntentScope {
