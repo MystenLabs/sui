1e062f64ca42ac614b74b9d686d1132735717252
Ge Gao <ge@mystenlabs.com>
2023-03-21 08:12:22 -0400

    indexer ops: version number and reset DB option

--- crates/sui-indexer/src/main.rs
+++ crates/sui-indexer/src/main.rs
@@ -2,9 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use clap::Parser;
+use tracing::info;
+
 use sui_indexer::errors::IndexerError;
 use sui_indexer::store::PgIndexerStore;
-use sui_indexer::{new_pg_connection_pool, Indexer, IndexerConfig};
+use sui_indexer::utils::reset_database;
+use sui_indexer::{get_pg_pool_connection, new_pg_connection_pool, Indexer, IndexerConfig};
 use sui_node::metrics::start_prometheus_server;
 
 #[tokio::main]
@@ -15,6 +18,7 @@ async fn main() -> Result<(), IndexerError> {
         .init();
 
     let indexer_config = IndexerConfig::parse();
+    info!("indexer config: {:#?}", indexer_config);
     let registry_service = start_prometheus_server(
         // NOTE: this parses the input host addr and port number for socket addr,
         // so unwrap() is safe here.
@@ -28,6 +32,16 @@ async fn main() -> Result<(), IndexerError> {
 
     let registry = registry_service.default_registry();
     let pg_connection_pool = new_pg_connection_pool(&indexer_config.db_url).await?;
+    if indexer_config.reset_db {
+        let mut conn = get_pg_pool_connection(&pg_connection_pool)?;
+        reset_database(&mut conn, /* drop_all */ true).map_err(|e| {
+            IndexerError::PostgresResetError(format!(
+                "unable to reset database with url: {:?} and err: {:?}",
+                indexer_config.db_url.clone(),
+                e
+            ))
+        })?;
+    }
     let store = PgIndexerStore::new(pg_connection_pool);
 
     Indexer::start(&indexer_config, &registry, store).await
--- crates/sui-indexer/src/utils.rs
+++ crates/sui-indexer/src/utils.rs
@@ -1,12 +1,70 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use anyhow::anyhow;
+use diesel::migration::MigrationSource;
+use diesel::{PgConnection, RunQueryDsl};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use tracing::info;
+
 use sui_json_rpc_types::SuiTransactionResponseOptions;
 use sui_sdk::apis::ReadApi as SuiReadApi;
 use sui_types::base_types::TransactionDigest;
 
 use crate::errors::IndexerError;
 use crate::types::SuiTransactionFullResponse;
+use crate::PgPoolConnection;
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
+
+/// Resets the database by reverting all migrations and reapplying them.
+///
+/// If `drop_all` is set to `true`, the function will drop all tables in the database before
+/// resetting the migrations. This option is destructive and will result in the loss of all
+/// data in the tables. Use with caution, especially in production environments.
+pub fn reset_database(conn: &mut PgPoolConnection, drop_all: bool) -> Result<(), anyhow::Error> {
+    info!("Resetting database ...");
+    if drop_all {
+        drop_all_tables(conn)
+            .map_err(|e| anyhow!("Encountering error when dropping all tables {e}"))?;
+    } else {
+        conn.revert_all_migrations(MIGRATIONS)
+            .map_err(|e| anyhow!("Error reverting all migrations {e}"))?;
+    }
+
+    conn.run_migrations(&MIGRATIONS.migrations().unwrap())
+        .map_err(|e| anyhow!("Failed to run migrations {e}"))?;
+    info!("Reset database complete.");
+    Ok(())
+}
+
+pub fn drop_all_tables(conn: &mut PgConnection) -> Result<(), diesel::result::Error> {
+    info!("Dropping all tables in the database");
+    let table_names: Vec<String> = diesel::dsl::sql::<diesel::sql_types::Text>(
+        "
+        SELECT tablename FROM pg_tables WHERE schemaname = 'public'
+    ",
+    )
+    .load(conn)?;
+
+    for table_name in table_names {
+        let drop_table_query = format!("DROP TABLE IF EXISTS {} CASCADE", table_name);
+        diesel::sql_query(drop_table_query).execute(conn)?;
+    }
+
+    // Recreate the __diesel_schema_migrations table
+    diesel::sql_query(
+        "
+        CREATE TABLE __diesel_schema_migrations (
+            version VARCHAR(50) PRIMARY KEY,
+            run_on TIMESTAMP NOT NULL DEFAULT NOW()
+        )
+    ",
+    )
+    .execute(conn)?;
+    info!("Dropped all tables in the database");
+    Ok(())
+}
 
 pub async fn multi_get_full_transactions(
     read_api: &SuiReadApi,
