1624a5a0b89a0f82ccd91b39be4857245d696c9a
Andrey Chursin <andrey@mystenlabs.com>
2023-03-21 12:29:39 -0700

    Offload batch verification to blocking pool

--- crates/sui-core/src/consensus_validator.rs
+++ crates/sui-core/src/consensus_validator.rs
@@ -8,10 +8,11 @@ use std::sync::Arc;
 
 use crate::authority::authority_per_epoch_store::AuthorityPerEpochStore;
 use crate::transaction_manager::TransactionManager;
+use async_trait::async_trait;
 use narwhal_worker::TransactionValidator;
 use sui_types::messages::{ConsensusTransaction, ConsensusTransactionKind};
 use tap::TapFallible;
-
+use tokio::runtime::Handle;
 use tracing::{info, warn};
 
 /// Allows verifying the validity of transactions
@@ -45,6 +46,7 @@ fn tx_from_bytes(tx: &[u8]) -> Result<ConsensusTransaction, eyre::Report> {
         .wrap_err("Malformed transaction (failed to deserialize)")
 }
 
+#[async_trait]
 impl TransactionValidator for SuiTxValidator {
     type Error = eyre::Report;
 
@@ -53,7 +55,7 @@ impl TransactionValidator for SuiTxValidator {
         Ok(())
     }
 
-    fn validate_batch(&self, b: &narwhal_types::Batch) -> Result<(), Self::Error> {
+    async fn validate_batch(&self, b: &narwhal_types::Batch) -> Result<(), Self::Error> {
         let _scope = monitored_scope("ValidateBatch");
         let txs = b
             .transactions
@@ -85,11 +87,16 @@ impl TransactionValidator for SuiTxValidator {
         // verify the certificate signatures as a batch
         let cert_count = cert_batch.len();
         let ckpt_count = ckpt_batch.len();
-        self.epoch_store
-            .batch_verifier
-            .verify_certs_and_checkpoints(cert_batch, ckpt_batch)
-            .tap_err(|e| warn!("batch verification error: {}", e))
-            .wrap_err("Malformed batch (failed to verify)")?;
+        let epoch_store = self.epoch_store.clone();
+        Handle::current()
+            .spawn_blocking(move || {
+                epoch_store
+                    .batch_verifier
+                    .verify_certs_and_checkpoints(cert_batch, ckpt_batch)
+                    .tap_err(|e| warn!("batch verification error: {}", e))
+                    .wrap_err("Malformed batch (failed to verify)")
+            })
+            .await??;
         self.metrics
             .certificate_signatures_verified
             .inc_by(cert_count as u64);
@@ -195,7 +202,7 @@ mod tests {
             .collect();
 
         let batch = Batch::new(transaction_bytes);
-        let res_batch = validator.validate_batch(&batch);
+        let res_batch = validator.validate_batch(&batch).await;
         assert!(res_batch.is_ok(), "{res_batch:?}");
 
         let bogus_transaction_bytes: Vec<_> = certificates
@@ -211,7 +218,7 @@ mod tests {
             .collect();
 
         let batch = Batch::new(bogus_transaction_bytes);
-        let res_batch = validator.validate_batch(&batch);
+        let res_batch = validator.validate_batch(&batch).await;
         assert!(res_batch.is_err());
     }
 }
