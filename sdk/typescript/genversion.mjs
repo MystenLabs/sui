63ba57c02d321764a79250b1fbf7147299e64e4a
Jordan Gensler <jordan@mystenlabs.com>
2023-03-21 16:45:44 -0700

    Remove import assertions

--- sdk/typescript/genversion.mjs
+++ sdk/typescript/genversion.mjs
@@ -1,13 +1,13 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { writeFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
+
 const LICENSE =
   '// Copyright (c) Mysten Labs, Inc.\n// SPDX-License-Identifier: Apache-2.0\n\n';
 
-import pkg from './package.json' assert { type: 'json' };
-
 async function main() {
+  const pkg = JSON.parse(await readFile('./package.json', 'utf8'));
   await writeFile(
     'src/pkg-version.ts',
     LICENSE + `export const pkgVersion = '${pkg.version}';\n`,
