8c43881aecdc2ddaf80d752e9d73ecaeef58bd3e
allan bailey <allan@mystenlabs.com>
2023-03-21 08:43:12 -0700

    chore: adding indexer and fullnode command scripts for containers

---
+++ docker/fullnode-x/fullnode/entry.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+unset LD_PRELOAD
+
+/usr/local/bin/sui-node --config-path /opt/sui/config/fullnode.yaml
+
---
+++ docker/fullnode-x/indexer/indexer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+/usr/local/bin/sui-indexer --db-url ${DATABASE_URL} --rpc-client-url ${RPC_CLIENT_URL}
