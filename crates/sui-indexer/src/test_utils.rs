6b1de844a0717fd949e39188bbfef909b922de26
Will Yang <willyang@mystenlabs.com>
2023-03-22 09:17:16 -0700

    remove the mds

--- crates/sui-indexer/src/test_utils.rs
+++ crates/sui-indexer/src/test_utils.rs
@@ -3,6 +3,7 @@
 
 use anyhow::anyhow;
 use prometheus::Registry;
+use sui_json_rpc_types::SuiTransactionResponse;
 use tokio::task::JoinHandle;
 
 use crate::errors::IndexerError;
@@ -31,4 +32,107 @@ pub async fn start_test_indexer(
     let store_clone = store.clone();
     let handle = tokio::spawn(async move { Indexer::start(&config, &registry, store_clone).await });
     Ok((store, handle))
+}
+
+#[derive(Clone)]
+pub struct SuiTransactionResponseBuilder<'a> {
+    response: SuiTransactionResponse,
+    full_response: &'a SuiTransactionResponse,
+    required_fields: &'a SuiTransactionResponse,
+}
+
+impl<'a> SuiTransactionResponseBuilder<'a> {
+    pub fn new(
+        full_response: &'a SuiTransactionResponse,
+        required_fields: &'a SuiTransactionResponse,
+    ) -> Self {
+        Self {
+            response: SuiTransactionResponse::default(),
+            full_response,
+            required_fields,
+        }
+    }
+
+    pub fn with_transaction(mut self) -> Self {
+        self.response = SuiTransactionResponse {
+            transaction: self.full_response.transaction.clone(),
+            ..self.response
+        };
+        self
+    }
+
+    pub fn with_raw_transaction(mut self) -> Self {
+        self.response = SuiTransactionResponse {
+            raw_transaction: self.full_response.raw_transaction.clone(),
+            ..self.response
+        };
+        self
+    }
+
+    pub fn with_effects(mut self) -> Self {
+        self.response = SuiTransactionResponse {
+            effects: self.full_response.effects.clone(),
+            ..self.response
+        };
+        self
+    }
+
+    pub fn with_events(mut self) -> Self {
+        self.response = SuiTransactionResponse {
+            events: self.full_response.events.clone(),
+            ..self.response
+        };
+        self
+    }
+
+    pub fn with_balance_changes(mut self) -> Self {
+        self.response = SuiTransactionResponse {
+            balance_changes: self.full_response.balance_changes.clone(),
+            ..self.response
+        };
+        self
+    }
+
+    pub fn with_object_changes(mut self) -> Self {
+        self.response = SuiTransactionResponse {
+            object_changes: self.full_response.object_changes.clone(),
+            ..self.response
+        };
+        self
+    }
+
+    pub fn with_input_and_changes(mut self) -> Self {
+        self.response = SuiTransactionResponse {
+            transaction: self.full_response.transaction.clone(),
+            balance_changes: self.full_response.balance_changes.clone(),
+            object_changes: self.full_response.object_changes.clone(),
+            ..self.response
+        };
+        self
+    }
+
+    pub fn with_all(mut self) -> Self {
+        self.response = SuiTransactionResponse {
+            transaction: self.full_response.transaction.clone(),
+            raw_transaction: self.full_response.raw_transaction.clone(),
+            effects: self.full_response.effects.clone(),
+            events: self.full_response.events.clone(),
+            balance_changes: self.full_response.balance_changes.clone(),
+            object_changes: self.full_response.object_changes.clone(),
+            ..self.response
+        };
+        self
+    }
+
+    pub fn build(self) -> SuiTransactionResponse {
+        SuiTransactionResponse {
+            transaction: self.response.transaction,
+            raw_transaction: self.response.raw_transaction,
+            effects: self.response.effects,
+            events: self.response.events,
+            balance_changes: self.response.balance_changes,
+            object_changes: self.response.object_changes,
+            ..self.required_fields.clone()
+        }
+    }
 }
