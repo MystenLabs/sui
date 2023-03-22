3dae40025996811363b7bb8114baf6e5b4abe127
plam-ml <127577476+plam-ml@users.noreply.github.com>
2023-03-21 19:29:35 -0700

    [Explorer]: Programmable Txns Input and Command section (#9395)
    ## Description 
    
    - First draft for Prog Txns
    https://mysten.atlassian.net/browse/APPS-557
    more changes to follow after https://github.com/MystenLabs/sui/pull/9448
    
    <img width="697" alt="Screenshot 2023-03-19 at 6 28 54 PM"
    src="https://user-images.githubusercontent.com/127577476/226226457-a991b1d8-322e-4101-9cee-5d3c5fb8814a.png">
    <img width="419" alt="Screenshot 2023-03-19 at 6 28 50 PM"
    src="https://user-images.githubusercontent.com/127577476/226226459-9fe8aed7-9f85-4422-a209-abdc7bdaa195.png">
    <img width="507" alt="Screenshot 2023-03-19 at 6 28 41 PM"
    src="https://user-images.githubusercontent.com/127577476/226226463-0aebd5ad-cd49-4584-bffd-a342970a7d2c.png">
    <img width="497" alt="Screenshot 2023-03-19 at 6 28 38 PM"
    src="https://user-images.githubusercontent.com/127577476/226226464-39834c5b-8940-4abc-9970-881841032f48.png">
    <img width="181" alt="Screenshot 2023-03-19 at 6 28 34 PM"
    src="https://user-images.githubusercontent.com/127577476/226226465-94cf029d-a4c3-407a-8925-0ce1546b75de.png">
    
    
    
    ## Pending questions
    
    - Need advice on formatting data, will connect with Ux on specifics
    - Should we check for Address vs Object types here. I am seeing them
    being all Addresses so far. Open for suggestions
    
    
    <img width="1442" alt="Screenshot 2023-03-15 at 7 48 40 PM"
    src="https://user-images.githubusercontent.com/127577476/225497514-819f988b-b690-4be3-b59a-1ba2e38c4054.png">
    
    
    
    ## Test Plan 
    
    - Tests will follow on subsequent PRs
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    
    Co-authored-by: Phong Lam <phonglam@Phongs-Laptop.attlocal.net>

--- sdk/typescript/src/types/transactions.ts
+++ sdk/typescript/src/types/transactions.ts
@@ -16,13 +16,7 @@ import {
   assign,
   nullable,
 } from 'superstruct';
-import { SuiEvent } from './events';
-import {
-  ObjectDigest,
-  SuiGasData,
-  SuiMovePackage,
-  SuiObjectRef,
-} from './objects';
+
 import {
   ObjectId,
   ObjectOwner,
@@ -32,6 +26,13 @@ import {
   TransactionDigest,
   TransactionEventDigest,
 } from './common';
+import { SuiEvent } from './events';
+import {
+  ObjectDigest,
+  SuiGasData,
+  SuiMovePackage,
+  SuiObjectRef,
+} from './objects';
 
 // TODO: support u64
 export const EpochId = number();
@@ -65,17 +66,19 @@ export const SuiArgument = union([
   object({ Result: number() }),
   object({ NestedResult: tuple([number(), number()]) }),
 ]);
+export type SuiArgument = Infer<typeof SuiArgument>;
+
+export const MoveCallSuiCommand = object({
+  arguments: optional(array(SuiArgument)),
+  type_arguments: optional(array(string())),
+  package: ObjectId,
+  module: string(),
+  function: string(),
+});
+export type MoveCallSuiCommand = Infer<typeof MoveCallSuiCommand>;
 
 export const SuiCommand = union([
-  object({
-    MoveCall: object({
-      arguments: array(SuiArgument),
-      type_arguments: array(string()),
-      package: ObjectId,
-      module: string(),
-      function: string(),
-    }),
-  }),
+  object({ MoveCall: MoveCallSuiCommand }),
   object({ TransferObjects: tuple([array(SuiArgument), SuiArgument]) }),
   object({ SplitCoins: tuple([SuiArgument, array(SuiArgument)]) }),
   object({ MergeCoins: tuple([SuiArgument, array(SuiArgument)]) }),
@@ -107,10 +110,11 @@ export const SuiCallArg = union([
 export type SuiCallArg = Infer<typeof SuiCallArg>;
 
 export const ProgrammableTransaction = object({
-  commands: array(),
+  commands: array(SuiCommand),
   inputs: array(SuiCallArg),
 });
 export type ProgrammableTransaction = Infer<typeof ProgrammableTransaction>;
+export type ProgrammableTransactionCommand = Infer<typeof SuiCommand>;
 
 /**
  * 1. WaitForEffectsCert: waits for TransactionEffectsCert and then returns to the client.
