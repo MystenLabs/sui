a2f34f2e3750b8720b266822bb64de4b6df709e7
Jos Dehaes <jos.dehaes@gmail.com>
2023-03-23 14:42:33 +0100

    chore: remove narwhal/Docker narwhal/scripts narwhal/examples and scripts

--- narwhal/examples/build.rs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(not(target_env = "msvc"))]
-    std::env::set_var("PROTOC", protobuf_src::protoc());
-    tonic_build::compile_protos("protos/narwhal.proto")?;
-    Ok(())
-}
