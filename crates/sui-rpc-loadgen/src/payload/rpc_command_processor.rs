db744f63f7d08d80e86e44ff5e6ccac6fc171eef
Will Yang <willyang@mystenlabs.com>
2023-03-24 12:08:56 -0700

    Implement queryTransactions

--- crates/sui-rpc-loadgen/src/payload/rpc_command_processor.rs
+++ crates/sui-rpc-loadgen/src/payload/rpc_command_processor.rs
@@ -1,16 +1,18 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use async_trait::async_trait;
 use futures::future::join_all;
 use shared_crypto::intent::{Intent, IntentMessage};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 use sui_json_rpc_types::{
-    BigInt, CheckpointId, ObjectChange, SuiObjectDataOptions, SuiTransactionResponseOptions,
+    BigInt, CheckpointId, ObjectChange, Page, SuiObjectDataOptions, SuiTransactionResponse,
+    SuiTransactionResponseOptions, SuiTransactionResponseQuery, TransactionsPage,
 };
 use sui_types::digests::TransactionDigest;
+use sui_types::query::TransactionFilter;
 use tokio::sync::RwLock;
 use tokio::time::sleep;
 use tracing::log::warn;
@@ -26,7 +28,7 @@ use sui_types::messages_checkpoint::CheckpointSequenceNumber;
 
 use crate::payload::{
     Command, CommandData, DryRun, GetCheckpoints, PaySui, Payload, ProcessPayload, Processor,
-    SignerInfo,
+    QueryTransactions, SignerInfo,
 };
 
 const DEFAULT_GAS_BUDGET: u64 = 100_000;
@@ -62,6 +64,7 @@ impl RpcCommandProcessor {
             CommandData::DryRun(ref v) => self.process(v, signer_info).await,
             CommandData::GetCheckpoints(ref v) => self.process(v, signer_info).await,
             CommandData::PaySui(ref v) => self.process(v, signer_info).await,
+            CommandData::QueryTransactions(ref v) => self.process(v, signer_info).await,
         }
     }
 
@@ -441,6 +444,20 @@ async fn get_coin_ids(
     results
 }
 
+async fn query_transactions(
+    client: &SuiClient,
+    query: SuiTransactionResponseQuery,
+    cursor: Option<TransactionDigest>,
+    limit: Option<usize>, // TODO: we should probably set a limit and paginate
+) -> Result<Page<SuiTransactionResponse, TransactionDigest>> {
+    let transactions = client
+        .read_api()
+        .query_transactions(query, cursor, limit, true)
+        .await
+        .unwrap();
+    Ok(transactions)
+}
+
 // todo: this and check_transactions can be generic
 pub async fn check_objects(
     clients: &Arc<RwLock<Vec<SuiClient>>>,
@@ -524,6 +541,86 @@ fn cross_validate_entities<T, U>(
                 entity_name, i, key_name, keys[i], a, b
             );
         }
+    }
+}
+
+#[async_trait]
+impl<'a> ProcessPayload<'a, &'a QueryTransactions> for RpcCommandProcessor {
+    async fn process(
+        &'a self,
+        op: &'a QueryTransactions,
+        _signer_info: &Option<SignerInfo>,
+    ) -> Result<()> {
+        let clients = self.get_clients().await?;
+        let filter = match (op.from_address, op.to_address) {
+            (Some(_), Some(_)) => {
+                return Err(anyhow!("Cannot specify both from_address and to_address"));
+            }
+            (Some(address), None) => Some(TransactionFilter::FromAddress(address)),
+            (None, Some(address)) => Some(TransactionFilter::ToAddress(address)),
+            (None, None) => None,
+        };
+        let query = SuiTransactionResponseQuery {
+            filter,
+            options: None, // not supported on indexer
+        };
+
+        let results: Vec<TransactionsPage> = Vec::new();
+
+        // Paginate results, if any
+        while results.is_empty() || results.iter().any(|r| r.has_next_page) {
+            let cursor = if results.is_empty() {
+                None
+            } else {
+                match (
+                    results.get(0).unwrap().next_cursor,
+                    results.get(1).unwrap().next_cursor,
+                ) {
+                    (Some(first_cursor), Some(second_cursor)) => {
+                        if first_cursor != second_cursor {
+                            warn!("Cursors are not the same, received {} vs {}. Selecting the first cursor to continue", first_cursor, second_cursor);
+                        }
+                        Some(first_cursor)
+                    }
+                    (Some(cursor), None) | (None, Some(cursor)) => Some(cursor),
+                    (None, None) => None,
+                }
+            };
+
+            let results = join_all(clients.iter().enumerate().map(|(i, client)| {
+                let with_query = query.clone();
+                async move {
+                    let start_time = Instant::now();
+                    let transactions = query_transactions(client, with_query, cursor, None)
+                        .await
+                        .unwrap();
+                    let elapsed_time = start_time.elapsed();
+                    debug!(
+                        "QueryTransactions Request latency {:.4} for rpc at url {i}",
+                        elapsed_time.as_secs_f64()
+                    );
+                    transactions
+                }
+            }))
+            .await;
+
+            // compare results
+            let digests = results[0]
+                .data
+                .iter()
+                .map(|transaction| transaction.digest)
+                .collect::<Vec<TransactionDigest>>();
+
+            cross_validate_entities(
+                &digests,
+                &results[0].data,
+                &results[1].data,
+                "TransactionDigest",
+                "Transaction",
+            );
+        }
+
+        Ok(())
     }
 }
 
