1fd5cb768596cdd0a7b64d1ec6512077bfa43884
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-03-25 09:50:37 -0400

    comment update

--- apps/core/src/hooks/useGetRollingAverageApys.ts
+++ apps/core/src/hooks/useGetRollingAverageApys.ts
@@ -6,15 +6,14 @@ import { useMemo } from 'react';
 import { useGetValidatorsEvents } from './useGetValidatorsEvents';
 import { roundFloat } from '../utils/roundFloat';
 
-// Calculate the APY and return the data for a given validator
 // recentEpochRewards is list of the last 30 epoch rewards for a specific validator
 // APY_e = (1 + epoch_rewards / stake)^365-1
-//  APY_e_30rollingaverage = average(APY_e,APY_e-1,…,APY_e-29);
+// APY_e_30rollingaverage = average(APY_e,APY_e-1,…,APY_e-29);
 
 const ROLLING_AVERAGE = 30;
 const DEFAULT_APY_DECIMALS = 4;
 
-//Since parsedJson is a type, we need to define the type of the object
+// define the type parsedJson response
 type ParsedJson = {
     commission_rate: string;
     epoch: string;
@@ -40,8 +39,8 @@ export interface ApyByValidator {
 }
 
 export function useGetRollingAverageApys(numberOfValidators: number | null) {
-    // Fetch the limit to the number of validators  * the rolling average
-    // Order the validators in descending order so that the most recent validators are at the top
+    // Set the limit to the number of validators  * the rolling average
+    // Order the response in descending order so that the most recent epoch are at the top
     const validatorEpochEvents = useGetValidatorsEvents({
         limit: numberOfValidators ? numberOfValidators * ROLLING_AVERAGE : null,
         order: 'descending',
@@ -52,9 +51,7 @@ export function useGetRollingAverageApys(numberOfValidators: number | null) {
             return null;
         }
         const apyGroups: ApyGroups = {};
-
         validatorEpochEvents.data.data.forEach(({ parsedJson }) => {
-            // parsedJson is not a type, so we need to define the type of the object
             const { stake, pool_staking_reward, validator_address } =
                 parsedJson as ParsedJson;
             const apy = Math.pow(1 + +pool_staking_reward / +stake, 365) - 1;
@@ -62,7 +59,7 @@ export function useGetRollingAverageApys(numberOfValidators: number | null) {
             if (!apyGroups[validator_address]) {
                 apyGroups[validator_address] = [];
             }
-            // If the APY is greater than 10000%, we set it to 0
+            // If the APY is greater than 10000% or isNAN, set it to 0
             apyGroups[validator_address].push(
                 Number.isNaN(apy) || apy > 10_000 ? 0 : apy
             );
