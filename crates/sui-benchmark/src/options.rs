b5b1d08266c6a4a05a57d681170b9d04eb22fdeb
ade <ade@mystenlabs.com>
2023-03-28 13:32:11 -0400

    nit

--- crates/sui-benchmark/src/options.rs
+++ crates/sui-benchmark/src/options.rs
@@ -165,7 +165,7 @@ pub enum RunSpec {
         // Format is "{adversarial_type}-{load_factor}".
         // `load_factor` is a number between 0.0 and 1.0 which dictates how much load per tx
         // Default is (0-0.5) implying random load at 50% load. See `AdversarialPayloadType` enum for `adversarial_type`
-        #[clap(long, default_value = "0-100")]
+        #[clap(long, default_value = "0-1.0")]
         adversarial_cfg: String,
 
         // --- generic options ---
