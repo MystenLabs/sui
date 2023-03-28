832dfa9c3b2280f2a25843a03637b1cc2d0bcf80
Alberto Sonnino <alberto@sonnino.com>
2023-03-28 14:34:51 +0900

    debug

--- crates/sui/src/sui_commands.rs
+++ crates/sui/src/sui_commands.rs
@@ -402,7 +402,9 @@ async fn genesis(
                 keystore.save()?;
 
                 // Make a new genesis config from the provided ip addresses.
-                GenesisConfig::new_for_benchmarks(&ips)
+                let mut y = GenesisConfig::new_for_benchmarks(&ips);
+                y.parameters.chain_start_timestamp_ms = 0;
+                y
             } else if keystore_path.exists() {
                 let existing_keys = FileBasedKeystore::new(&keystore_path)?.addresses();
                 GenesisConfig::for_local_testing_with_addresses(existing_keys)
