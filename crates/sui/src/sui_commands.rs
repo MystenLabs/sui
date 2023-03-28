b50bdf4d0a69c0bb209cf95aac119d79d5d77928
Alberto Sonnino <alberto@sonnino.com>
2023-03-28 13:39:58 +0900

    rebug

--- crates/sui/src/sui_commands.rs
+++ crates/sui/src/sui_commands.rs
@@ -385,12 +385,9 @@ async fn genesis(
             keystore.add_key(gas_key)?;
             keystore.save()?;
 
-            let mut x: GenesisConfig = PersistedConfig::read(&path)?;
-            x.parameters.chain_start_timestamp_ms = 0;
-            let mut y = GenesisConfig::new_for_benchmarks(&benchmark_ips.unwrap());
-            y.parameters.chain_start_timestamp_ms = 0;
-            assert_eq!(x, y);
-            y
+            // let mut x: GenesisConfig = PersistedConfig::read(&path)?;
+            // x.parameters.chain_start_timestamp_ms = 0;
+            GenesisConfig::new_for_benchmarks(&benchmark_ips.unwrap())
         }
         None => {
             if let Some(ips) = benchmark_ips {
