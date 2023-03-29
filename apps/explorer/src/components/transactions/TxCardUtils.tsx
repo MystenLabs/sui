13a03d273a79fd6d17635d7335e1533e3f332a9f
Jordan Gensler <jordan@mystenlabs.com>
2023-03-29 15:32:31 -0700

    [explorer] Tweak to show transactions count (#10054)
    ## Description 
    
    Reworks transaction block table to remove amounts and show transaction count.
    
    ## Test Plan 
    
    Ran locally.

--- apps/explorer/src/components/transactions/TxCardUtils.tsx
+++ apps/explorer/src/components/transactions/TxCardUtils.tsx
@@ -14,7 +14,6 @@ import {
 import clsx from 'clsx';
 import { type ReactNode } from 'react';
 
-import { getAmount } from '../../utils/getAmount';
 import { TxTimeType } from '../tx-time/TxTimeType';
 
 import styles from './RecentTxCard.module.css';
@@ -76,13 +75,6 @@ export const genTableDataFromTxData = (
 ) => ({
     data: results.map((transaction) => {
         const status = getExecutionStatusType(transaction);
-        const transfer = getAmount({
-            txnData: transaction,
-            suiCoinOnly: true,
-        })[0];
-
-        // use only absolute value of sui amount
-        const suiAmount = transfer?.amount ? Math.abs(transfer.amount) : null;
         const sender = getTransactionSender(transaction);
 
         return {
@@ -105,9 +97,13 @@ export const genTableDataFromTxData = (
                     />
                 </TxTableCol>
             ),
-            amounts: (
+            txns: (
                 <TxTableCol>
-                    <SuiAmount amount={suiAmount} />
+                    {transaction.transaction?.data.transaction.kind ===
+                    'ProgrammableTransaction'
+                        ? transaction.transaction.data.transaction.transactions
+                              .length
+                        : '--'}
                 </TxTableCol>
             ),
             gas: (
@@ -132,8 +128,8 @@ export const genTableDataFromTxData = (
             accessorKey: 'sender',
         },
         {
-            header: () => <TxTableHeader label="Amount" />,
-            accessorKey: 'amounts',
+            header: () => <TxTableHeader label="Txns" />,
+            accessorKey: 'txns',
         },
         {
             header: () => <TxTableHeader label="Gas" />,
