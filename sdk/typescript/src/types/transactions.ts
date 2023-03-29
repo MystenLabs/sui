3317bff72bcddaa05c2c2f32aeee677edf65db4c
Will Yang <willyang@mystenlabs.com>
2023-03-28 20:27:41 -0700

    make Publish match

--- sdk/typescript/src/types/transactions.ts
+++ sdk/typescript/src/types/transactions.ts
@@ -81,7 +81,7 @@ export const SuiTransaction = union([
   object({ TransferObjects: tuple([array(SuiArgument), SuiArgument]) }),
   object({ SplitCoins: tuple([SuiArgument, array(SuiArgument)]) }),
   object({ MergeCoins: tuple([SuiArgument, array(SuiArgument)]) }),
-  object({ Publish: SuiMovePackage }),
+  object({ Publish: tuple([SuiMovePackage, array(ObjectId)]) }),
   object({ MakeMoveVec: tuple([nullable(string()), array(SuiArgument)]) }),
 ]);
 
