33af9b16c8b5d1c4177673e70129e4141ffec486
Ge Gao <ge@mystenlabs.com>
2023-03-30 15:57:21 -0400

    indexer: remove unnecessary column index

--- crates/sui-indexer/migrations/2022-11-28-204251_addresses/up.sql
+++ crates/sui-indexer/migrations/2022-11-28-204251_addresses/up.sql
@@ -5,5 +5,3 @@ CREATE TABLE addresses
     first_appearance_time BIGINT        NOT NULL
 );
 CREATE INDEX addresses_account_address ON addresses (account_address);
-CREATE INDEX addresses_first_appearance_tx ON addresses (first_appearance_tx);
-CREATE INDEX addresses_first_appearance_time ON addresses (first_appearance_time);
