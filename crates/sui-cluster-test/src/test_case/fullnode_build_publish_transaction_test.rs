9be2799afc7341073bf82ff79aa0c4150029d831
Tim Zakian <2895723+tzakian@users.noreply.github.com>
2023-03-31 16:27:03 -0700

    Fix benchmark simtests (#10251)
    Fix benchmark simtests

--- crates/sui-cluster-test/src/test_case/fullnode_build_publish_transaction_test.rs
+++ crates/sui-cluster-test/src/test_case/fullnode_build_publish_transaction_test.rs
@@ -32,7 +32,7 @@ impl TestCaseImpl for FullNodeBuildPublishTransactionTest {
             all_module_bytes,
             dependencies,
             None::<ObjectID>,
-            1_000_000 * gas_price
+            300_000_000 * gas_price
         ];
 
         let data = ctx
