04f29b4170b9a52a73e5a2b9639c2b593b2c308c
Theodore Chaikalis <teo@mystenlabs.com>
2023-03-31 12:55:18 +0300

    Renaming sui_subscribeEvent to suix_* | Updated readme of SDKs to comply with new address format (#10201)
    ## Description 
    
    - Renamed `sui_subscribeEvent` -> `suix_subscribeEvent`
    - Renamed `sui_unsubscribeEvent` -> `suix_unsubscribeEvent` 
    - Updated SDK `README.md` to use new address lengths and Object IDs
    everywhere

--- crates/sui/tests/full_node_tests.rs
+++ crates/sui/tests/full_node_tests.rs
@@ -571,7 +571,7 @@ async fn test_full_node_sub_and_query_move_event_ok() -> Result<(), anyhow::Erro
         .subscribe(
             "suix_subscribeEvent",
             rpc_params![EventFilter::MoveEventType(struct_tag.clone())],
-            "suix_unsubscribeEvents",
+            "suix_unsubscribeEvent",
         )
         .await
         .unwrap();
