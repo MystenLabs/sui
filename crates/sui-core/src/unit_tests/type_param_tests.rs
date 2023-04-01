444361e615c012a9c0a0eb74cb3133d35ea7459a
Adam Welc <adam@mystenlabs.com>
2023-04-01 09:10:05 -0700

    Added cross-package tests

--- crates/sui-core/src/unit_tests/type_param_tests.rs
+++ crates/sui-core/src/unit_tests/type_param_tests.rs
@@ -261,3 +261,147 @@ async fn test_nested_type_param_different_module() {
 
     assert!(effects.status().is_ok());
 }
+
+#[tokio::test]
+#[cfg_attr(msim, ignore)]
+async fn test_different_package_type_param() {
+    let (sender, sender_key): (_, AccountKeyPair) = get_key_pair();
+    let gas = ObjectID::random();
+    let authority = init_state_with_ids(vec![(sender, gas)]).await;
+
+    let package = build_and_publish_test_package(
+        &authority,
+        &sender,
+        &sender_key,
+        &gas,
+        "type_params",
+        /* with_unpublished_deps */ true,
+    )
+    .await;
+
+    let package_extra = build_and_publish_test_package(
+        &authority,
+        &sender,
+        &sender_key,
+        &gas,
+        "type_params_extra",
+        /* with_unpublished_deps */ true,
+    )
+    .await;
+
+    let effects = call_move(
+        &authority,
+        &gas,
+        &sender,
+        &sender_key,
+        &package.0,
+        "m2",
+        "create_and_transfer",
+        vec![],
+        vec![
+            TestCallArg::Pure(bcs::to_bytes(&(16_u64)).unwrap()),
+            TestCallArg::Pure(bcs::to_bytes(&sender).unwrap()),
+        ],
+    )
+    .await
+    .unwrap();
+
+    let created_object_id = effects.created()[0].0 .0;
+    let type_param =
+        TypeTag::from_str(format!("{}::m2::AnotherObject", package.0).as_str()).unwrap();
+
+    let effects = call_move(
+        &authority,
+        &gas,
+        &sender,
+        &sender_key,
+        // a different package than the one where the type was defined
+        &package_extra.0,
+        "m1",
+        "transfer_object",
+        vec![type_param],
+        vec![
+            TestCallArg::Object(created_object_id),
+            TestCallArg::Pure(bcs::to_bytes(&sender).unwrap()),
+        ],
+    )
+    .await
+    .unwrap();
+
+    assert!(effects.status().is_ok());
+}
+
+#[tokio::test]
+#[cfg_attr(msim, ignore)]
+async fn test_nested_type_param_different_package() {
+    let (sender, sender_key): (_, AccountKeyPair) = get_key_pair();
+    let gas = ObjectID::random();
+    let authority = init_state_with_ids(vec![(sender, gas)]).await;
+
+    let package = build_and_publish_test_package(
+        &authority,
+        &sender,
+        &sender_key,
+        &gas,
+        "type_params",
+        /* with_unpublished_deps */ true,
+    )
+    .await;
+
+    let package_extra = build_and_publish_test_package(
+        &authority,
+        &sender,
+        &sender_key,
+        &gas,
+        "type_params_extra",
+        /* with_unpublished_deps */ true,
+    )
+    .await;
+
+    let effects = call_move(
+        &authority,
+        &gas,
+        &sender,
+        &sender_key,
+        &package.0,
+        "m1",
+        "create_and_transfer_gen",
+        vec![],
+        vec![
+            TestCallArg::Pure(bcs::to_bytes(&(16_u64)).unwrap()),
+            TestCallArg::Pure(bcs::to_bytes(&sender).unwrap()),
+        ],
+    )
+    .await
+    .unwrap();
+
+    let created_object_id = effects.created()[0].0 .0;
+    let type_param = TypeTag::from_str(
+        format!(
+            "{}::m1::GenObject<{}::m2::AnotherObject>",
+            package.0, package.0
+        )
+        .as_str(),
+    )
+    .unwrap();
+
+    let effects = call_move(
+        &authority,
+        &gas,
+        &sender,
+        &sender_key,
+        // a different package than those where types where defined
+        &package_extra.0,
+        "m1",
+        "transfer_object",
+        vec![type_param],
+        vec![
+            TestCallArg::Object(created_object_id),
+            TestCallArg::Pure(bcs::to_bytes(&sender).unwrap()),
+        ],
+    )
+    .await
+    .unwrap();
+
+    assert!(effects.status().is_ok());
+}
