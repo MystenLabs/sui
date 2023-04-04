7fbf3bf8dfe8c228ef733d696f7bef0ad090c197
Godwin JIbs <126525197+Jibz-Mysten@users.noreply.github.com>
2023-04-04 15:29:07 -0400

    Update 0% APY conditional checker  (#10256)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    This addresses the feedback from previous PR
    https://github.com/MystenLabs/sui/pull/10232
    
    - Create a util that displays APY% or null fallback `--`
    - replace `rollingAverageApys?.[validator.suiAddress] ||
    rollingAverageApys?.[validator.suiAddress] === 0
    ?rollingAverageApys?.[validator.suiAddress] : null,` with
    `rollingAverageApys?.[validator.suiAddress] ?? null`
    
    
     
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [x] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/components/receipt-card/StakeTxnCard.tsx
+++ apps/wallet/src/ui/app/components/receipt-card/StakeTxnCard.tsx
@@ -1,7 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useGetRollingAverageApys } from '@mysten/core';
+import {
+    formatPercentageDisplay,
+    useGetRollingAverageApys,
+} from '@mysten/core';
 import { SUI_TYPE_ARG } from '@mysten/sui.js';
 
 import { useGetTimeBeforeEpochNumber } from '_app/staking/useGetTimeBeforeEpochNumber';
@@ -30,11 +33,7 @@ export function StakeTxnCard({ event }: StakeTxnCardProps) {
         system?.activeValidators?.length || null
     );
 
-    const apy =
-        rollingAverageApys?.[validatorAddress] ||
-        rollingAverageApys?.[validatorAddress] === 0
-            ? rollingAverageApys?.[validatorAddress]
-            : null;
+    const apy = rollingAverageApys?.[validatorAddress] ?? null;
 
     // Reward will be available after 2 epochs
     // TODO: Get epochStartTimestampMs/StartDate for staking epoch + NUM_OF_EPOCH_BEFORE_EARNING
@@ -76,7 +75,7 @@ export function StakeTxnCard({ event }: StakeTxnCardProps) {
                         <IconTooltip tip="This is the Annualized Percentage Yield of the a specific validator’s past operations. Note there is no guarantee this APY will be true in the future." />
                     </div>
                     <Text variant="body" weight="medium" color="steel-darker">
-                        {apy === null ? '--' : apy + ' %'}
+                        {formatPercentageDisplay(apy)}
                     </Text>
                 </div>
                 <div className="flex justify-between w-full py-3.5">
