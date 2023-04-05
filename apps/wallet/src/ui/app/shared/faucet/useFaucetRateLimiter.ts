2ff232661c3f992e499ac82caac6486ae2fbe9ff
William Robertson <williamrobertson@mystenlabs.com>
2023-04-05 17:19:41 -0400

    fix

--- apps/wallet/src/ui/app/shared/faucet/useFaucetRateLimiter.ts
+++ apps/wallet/src/ui/app/shared/faucet/useFaucetRateLimiter.ts
@@ -2,70 +2,38 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useEffect, useState } from 'react';
-import Browser from 'webextension-polyfill';
 
-const IS_RATE_LIMITED_FROM_FAUCET_STORAGE_KEY = 'is_rate_limited_from_faucet';
-
-const FAUCET_RATE_LIMIT_EXPIRY_TIME_STORAGE_KEY =
-    'faucet_rate_limit_expiry_time';
+const FAUCET_RATE_LIMIT_EXPIRY_TIME_KEY = 'faucet_rate_limit_expiry_time';
 
 // We'll rate limit users for 20 minutes which should
 // more-or-less mock how the faucet API rate limits users
 const rateLimitExpiryTime = 20 * 60 * 1000;
 
 export function useFaucetRateLimiter() {
-    const [isRateLimited, setRateLimited] = useState(false);
+    const [isRateLimited, setRateLimited] = useState(() => {
+        const expiryTime = localStorage.getItem(
+            FAUCET_RATE_LIMIT_EXPIRY_TIME_KEY
+        );
+        const currTime = new Date().getTime();
+        return currTime <= Number(expiryTime);
+    });
 
     const rateLimit = () => {
-        Browser.storage.local.set({
-            [IS_RATE_LIMITED_FROM_FAUCET_STORAGE_KEY]: true,
-            [FAUCET_RATE_LIMIT_EXPIRY_TIME_STORAGE_KEY]:
-                new Date().getTime() + rateLimitExpiryTime,
-        });
+        const currTime = new Date().getTime();
+        const expiryTime = currTime + rateLimitExpiryTime;
+
+        localStorage.setItem(
+            FAUCET_RATE_LIMIT_EXPIRY_TIME_KEY,
+            String(expiryTime)
+        );
+        setRateLimited(true);
     };
 
     useEffect(() => {
-        const onChanged = (
-            changes: Browser.Storage.StorageAreaOnChangedChangesType
-        ) => {
-            if (IS_RATE_LIMITED_FROM_FAUCET_STORAGE_KEY in changes) {
-                const { newValue } =
-                    changes[IS_RATE_LIMITED_FROM_FAUCET_STORAGE_KEY];
-
-                setRateLimited(Boolean(newValue));
-            }
-        };
-
-        Browser.storage.local.onChanged.addListener(onChanged);
-        return () => {
-            Browser.storage.local.onChanged.removeListener(onChanged);
-        };
-    }, []);
-
-    useEffect(() => {
-        Browser.storage.local
-            .get({
-                [IS_RATE_LIMITED_FROM_FAUCET_STORAGE_KEY]: false,
-                [FAUCET_RATE_LIMIT_EXPIRY_TIME_STORAGE_KEY]: null,
-            })
-            .then(
-                ({
-                    [IS_RATE_LIMITED_FROM_FAUCET_STORAGE_KEY]: isRateLimited,
-                    [FAUCET_RATE_LIMIT_EXPIRY_TIME_STORAGE_KEY]: expiryTime,
-                }) => {
-                    const currTime = new Date().getTime();
-                    if (expiryTime && currTime > expiryTime) {
-                        Browser.storage.local.set({
-                            [IS_RATE_LIMITED_FROM_FAUCET_STORAGE_KEY]: false,
-                            [FAUCET_RATE_LIMIT_EXPIRY_TIME_STORAGE_KEY]: null,
-                        });
-                        setRateLimited(false);
-                    } else {
-                        setRateLimited(isRateLimited);
-                    }
-                }
-            );
-    }, []);
+        if (!isRateLimited) {
+            localStorage.removeItem(FAUCET_RATE_LIMIT_EXPIRY_TIME_KEY);
+        }
+    }, [isRateLimited]);
 
     return [isRateLimited, rateLimit] as const;
 }
