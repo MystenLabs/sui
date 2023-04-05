10e409444add0d57133a8fdf0980272d9996a55b
William Robertson <williamrobertson@mystenlabs.com>
2023-04-05 14:29:52 -0400

    fix

--- apps/wallet/src/ui/app/shared/faucet/useFaucetRateLimiter.ts
+++ apps/wallet/src/ui/app/shared/faucet/useFaucetRateLimiter.ts
@@ -9,7 +9,9 @@ const IS_RATE_LIMITED_FROM_FAUCET_STORAGE_KEY = 'is_rate_limited_from_faucet';
 const FAUCET_RATE_LIMIT_EXPIRY_TIME_STORAGE_KEY =
     'faucet_rate_limit_expiry_time';
 
-const rateLimitExpiryTime = 24 * 60 * 60 * 1000;
+// We'll rate limit users for 20 minutes which should
+// more-or-less mock how the faucet API rate limits users
+const rateLimitExpiryTime = 20 * 60 * 1000;
 
 export function useFaucetRateLimiter() {
     const [isRateLimited, setRateLimited] = useState(false);
