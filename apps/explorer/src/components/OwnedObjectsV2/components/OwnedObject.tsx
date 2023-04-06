5a1657ddcba4e7c3ed07732ba535384fb54a7664
Nikhil Mattu <nikhil@mystenlabs.com>
2023-04-06 15:57:07 -0400

    update

--- apps/explorer/src/components/OwnedObjectsV2/components/OwnedObject.tsx
+++ apps/explorer/src/components/OwnedObjectsV2/components/OwnedObject.tsx
@@ -28,12 +28,12 @@ function OwnedObject({ obj }: OwnedObjectTypes) {
         <div
             className="w-[50%] lg:flex lg:flex-wrap lg:justify-between"
         >
-            <div className="my-2 flex h-fit min-h-[72px] w-[100%] items-center overflow-x-hidden text-ellipsis whitespace-nowrap break-all sm:my-[1vh]">
+            <div className="my-2 flex h-fit min-h-[72px] w-full items-center truncate break-all sm:my-[1vh]">
                 <div className="mr-[20px] h-[60px] min-w-[60px] max-w-[60px]">
                     <DisplayBox display={display} />
                 </div>
                 <div className="overflow-hidden sm:pr-[20px]">
-                    <div className="overflow-hidden text-ellipsis whitespace-nowrap text-[13px] font-medium leading-[130%] text-gray-90">
+                    <div className="truncate text-[13px] font-medium leading-[130%] text-gray-90">
                         {extractName(displayMeta)}
                     </div>
                     <div>
