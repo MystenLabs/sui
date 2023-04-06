2d36dee028f9fd2ed1b792a663049145304a7d1e
Nikhil Mattu <nikhil@mystenlabs.com>
2023-04-06 16:59:26 -0400

    updates

--- apps/explorer/src/components/OwnedCoins/OwnedCoins.tsx
+++ apps/explorer/src/components/OwnedCoins/OwnedCoins.tsx
@@ -45,37 +45,40 @@ function OwnedCoins({ id }: { id: string }) {
     }
 
     return (
-        <div className="flex flex-col space-y-5 pt-5 text-left xl:pr-10">
-            <Heading color="gray-90" variant="heading4/semibold">
-                Coins
-            </Heading>
-            {isLoading && <LoadingSpinner />}
-            <div className="flex max-h-80 flex-col overflow-auto">
-                <div className="grid grid-cols-3 py-2 uppercase tracking-wider text-gray-80">
-                    <Text variant="caption/medium">Type</Text>
-                    <Text variant="caption/medium">Objects</Text>
-                    <Text variant="caption/medium">Balance</Text>
-                </div>
-                <div>
-                    {uniqueCoins
-                        .slice(
-                            (currentSlice - 1) * COINS_PER_PAGE,
-                            currentSlice * COINS_PER_PAGE
-                        )
-                        .map((coin) => (
-                            <OwnedCoinView id={id} key={coin.coinType} coin={coin} />
-                        ))}
-                </div>
-            </div>
-            {uniqueCoins.length > COINS_PER_PAGE && <Pagination
-                onNext={() => setCurrentSlice(currentSlice + 1)}
-                hasNext={currentSlice !==
-                    Math.ceil(uniqueCoins.length / COINS_PER_PAGE)}
-                hasPrev={currentSlice !== 1}
-                onPrev={() => setCurrentSlice(currentSlice - 1)}
-                onFirst={() => setCurrentSlice(1)}
-            />}
-        </div>
+        <>
+            {isLoading ? <LoadingSpinner /> :
+                <div className="flex flex-col space-y-5 pt-5 text-left xl:pr-10">
+                    <Heading color="gray-90" variant="heading4/semibold">
+                        Coins
+                    </Heading>
+                    <div className="flex max-h-80 flex-col overflow-auto">
+                        <div className="grid grid-cols-3 py-2 uppercase tracking-wider text-gray-80">
+                            <Text variant="caption/medium">Type</Text>
+                            <Text variant="caption/medium">Objects</Text>
+                            <Text variant="caption/medium">Balance</Text>
+                        </div>
+                        <div>
+                            {uniqueCoins
+                                .slice(
+                                    (currentSlice - 1) * COINS_PER_PAGE,
+                                    currentSlice * COINS_PER_PAGE
+                                )
+                                .map((coin) => (
+                                    <OwnedCoinView id={id} key={coin.coinType} coin={coin} />
+                                ))}
+                        </div>
+                    </div>
+                    {uniqueCoins.length > COINS_PER_PAGE && <Pagination
+                        onNext={() => setCurrentSlice(currentSlice + 1)}
+                        hasNext={currentSlice !==
+                            Math.ceil(uniqueCoins.length / COINS_PER_PAGE)}
+                        hasPrev={currentSlice !== 1}
+                        onPrev={() => setCurrentSlice(currentSlice - 1)}
+                        onFirst={() => setCurrentSlice(1)}
+                    />}
+                </div>}
+        </>
+
     );
 }
 
