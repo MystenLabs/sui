cda2f141cb6db7e5610bb599a79c8a8025a47b38
Nikhil Mattu <nikhil@mystenlabs.com>
2023-04-07 11:24:50 -0400

    updates

--- apps/explorer/sw.js
+++ apps/explorer/sw.js
@@ -1,11 +1,15 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 const cacheName = 'site-cache-v1';
 const assetsToCache = ['index.html'];
 
 self.addEventListener('install', (event) => {
-    self.skipWaiting(); // skip waiting
     event.waitUntil(
         caches.open(cacheName).then((cache) => {
             return cache.addAll(assetsToCache);
         })
     );
 });
+
+self.addEventListener('fetch', (event) => {});
