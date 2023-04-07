07deb0f797ddfe2de20fa25bbb8853cbf14aaec3
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-04-07 15:58:19 -0400

    before setting the state check if activeFieldName is an empty string

--- apps/explorer/src/components/owned-objects/views/ObjectFieldsCard.tsx
+++ apps/explorer/src/components/owned-objects/views/ObjectFieldsCard.tsx
@@ -51,7 +51,7 @@ export function ObjectFieldsCard({ id }: ObjectFieldsProps) {
         module: moduleName,
         struct: functionName,
         onSuccess: (data) => {
-            if (data?.fields) {
+            if (data?.fields && activeFieldName === '') {
                 setActiveFieldName(data.fields[0].name);
             }
         },
