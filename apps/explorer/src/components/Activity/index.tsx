82fdec289e9b3871d7ff7948a3e962a383751c2d
mamos-mysten <122397493+mamos-mysten@users.noreply.github.com>
2023-04-08 17:59:47 -0400

    [explorer] - disable play/pause button (#10616)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/explorer/src/components/Activity/index.tsx
+++ apps/explorer/src/components/Activity/index.tsx
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useState } from 'react';
-import toast from 'react-hot-toast';
+// import toast from 'react-hot-toast';
 
 import { Transactions } from '../transactions';
 
 import { CheckpointsTable } from '~/pages/checkpoints/CheckpointsTable';
 import { EpochsTable } from '~/pages/epochs/EpochsTable';
-import { PlayPause } from '~/ui/PlayPause';
+// import { PlayPause } from '~/ui/PlayPause';
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '~/ui/Tabs';
 
 type Props = {
@@ -17,7 +17,7 @@ type Props = {
     disablePagination?: boolean;
 };
 
-const AUTO_REFRESH_ID = 'auto-refresh';
+// const AUTO_REFRESH_ID = 'auto-refresh';
 const REFETCH_INTERVAL_SECONDS = 10;
 const REFETCH_INTERVAL = REFETCH_INTERVAL_SECONDS * 1000;
 
@@ -34,20 +34,20 @@ export function Activity({
     const [selectedIndex, setSelectedIndex] = useState(
         initialTab && tabs[initialTab] ? tabs[initialTab] : 0
     );
-    const [paused, setPaused] = useState(false);
+    const [paused] = useState(false);
 
-    const handlePauseChange = () => {
-        if (paused) {
-            toast.success(
-                `Auto-refreshing on - every ${REFETCH_INTERVAL_SECONDS} seconds`,
-                { id: AUTO_REFRESH_ID }
-            );
-        } else {
-            toast.success('Auto-refresh paused', { id: AUTO_REFRESH_ID });
-        }
+    // const handlePauseChange = () => {
+    //     if (paused) {
+    //         toast.success(
+    //             `Auto-refreshing on - every ${REFETCH_INTERVAL_SECONDS} seconds`,
+    //             { id: AUTO_REFRESH_ID }
+    //         );
+    //     } else {
+    //         toast.success('Auto-refresh paused', { id: AUTO_REFRESH_ID });
+    //     }
 
-        setPaused((paused) => !paused);
-    };
+    //     setPaused((paused) => !paused);
+    // };
 
     const refetchInterval = paused ? undefined : REFETCH_INTERVAL;
 
@@ -65,10 +65,11 @@ export function Activity({
                         <Tab>Checkpoints</Tab>
                     </TabList>
                     <div className="absolute inset-y-0 -top-1 right-0 text-2xl">
-                        <PlayPause
+                        {/* todo: re-enable this when rpc is stable */}
+                        {/* <PlayPause
                             paused={paused}
                             onChange={handlePauseChange}
-                        />
+                        /> */}
                     </div>
                 </div>
                 <TabPanels>
