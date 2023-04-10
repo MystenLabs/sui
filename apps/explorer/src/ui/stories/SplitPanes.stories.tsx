8292880c7011d4487bb42b4d68131d6cfe7ae6fe
plam-ml <127577476+plam-ml@users.noreply.github.com>
2023-04-10 14:44:43 -0700

    [explorer]: add draggable panels (#10465)
    ## Description 
    
    - Add draggable panels
    - https://mysten.atlassian.net/browse/APPS-736
    
    
    ![draggable](https://user-images.githubusercontent.com/127577476/230299283-d7cccc38-c17d-45ac-9664-a4a059bcd227.gif)
    
    
    
    
    ![draggable-story](https://user-images.githubusercontent.com/127577476/230261910-6a5e3371-9b04-43c8-94af-1e149a66b9fd.gif)
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    
    Co-authored-by: Phong Lam <phonglam@Phongs-Laptop.attlocal.net>

---
+++ apps/explorer/src/ui/stories/SplitPanes.stories.tsx
@@ -0,0 +1,57 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { type Meta, type StoryObj } from '@storybook/react';
+
+import { SplitPanes, type SplitPaneProps } from '../SplitPanes';
+
+export default {
+    component: SplitPanes,
+} as Meta;
+
+const panels = [
+    <div
+        key={1}
+        style={{ whiteSpace: 'nowrap' }}
+        className="h-full w-[1000px] bg-success-light"
+    >
+        {'long text here '.repeat(5)}
+    </div>,
+    <div key={2} className="h-full w-[1000px] bg-issue-light">
+        Second
+    </div>,
+    <div key={3} className="h-full w-[1000px] w-full bg-sui">
+        Third
+    </div>,
+];
+
+const SplitPanesStory: StoryObj<SplitPaneProps> = {
+    render: (props) => (
+        <div className="h-[500px] w-[1000px]">
+            <SplitPanes {...props} panels={panels} />
+        </div>
+    ),
+};
+
+export const HorizontalSplitPanes: StoryObj<SplitPaneProps> = {
+    ...SplitPanesStory,
+    args: {
+        direction: 'horizontal',
+        defaultSizes: [10, 40, 50],
+    },
+};
+
+export const VerticalSplitPanes: StoryObj<SplitPaneProps> = {
+    ...SplitPanesStory,
+    args: {
+        direction: 'vertical',
+    },
+};
+
+export const SplitPanesWithStateSaveOnRefresh: StoryObj<SplitPaneProps> = {
+    ...SplitPanesStory,
+    args: {
+        direction: 'horizontal',
+        autoSaveId: 'split-panes',
+    },
+};
