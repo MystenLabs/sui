91c96505defd4931acbdf213043b5656ee5ef0ff
sadhan <sadhan@mystenlabs.com>
2023-04-09 21:40:32 -0700

    Add high capacity cache for point workload

--- crates/sui-storage/src/indexes.rs
+++ crates/sui-storage/src/indexes.rs
@@ -14,7 +14,7 @@ use move_core_types::language_storage::{ModuleId, StructTag};
 use serde::{de::DeserializeOwned, Serialize};
 use tracing::debug;
 
-use sui_json_rpc_types::{Balance, SuiObjectDataFilter};
+use sui_json_rpc_types::SuiObjectDataFilter;
 use sui_types::base_types::{ObjectID, SuiAddress, TransactionDigest, TxSequenceNumber};
 use sui_types::base_types::{ObjectInfo, ObjectRef};
 use sui_types::digests::TransactionEventsDigest;
@@ -23,8 +23,8 @@ use sui_types::error::{SuiError, SuiResult};
 use sui_types::messages::TransactionEvents;
 use sui_types::object::Owner;
 use sui_types::query::TransactionFilter;
-use typed_store::rocks::{DBOptions, optimized_for_high_throughput_options};
 use typed_store::rocks::{default_db_options, point_lookup_db_options, DBMap, MetricConf};
+use typed_store::rocks::{optimized_for_high_throughput_options, DBOptions};
 use typed_store::traits::Map;
 use typed_store::traits::{TableSummary, TypedStoreDebug};
 use typed_store_derive::DBMapUtils;
@@ -107,7 +107,6 @@ pub struct IndexStoreTables {
     event_by_sender: DBMap<(SuiAddress, EventId), EventIndex>,
     #[default_options_override_fn = "index_table_default_config"]
     event_by_time: DBMap<(u64, EventId), EventIndex>,
-    balance: DBMap<SuiAddress, Balance>,
 }
 
 pub struct IndexStore {
@@ -155,7 +154,6 @@ impl IndexStore {
     pub fn new(path: PathBuf) -> Self {
         let tables =
             IndexStoreTables::open_tables_read_write(path, MetricConf::default(), None, None);
-        tables.balance.rocksdb.drop_cf("balance").unwrap();
         let next_sequence_number = tables
             .transaction_order
             .iter()
