3faf0b96a1bccb367669826e279824e5d912a9dd
Nikhil Mattu <nikhil@mystenlabs.com>
2023-04-10 23:15:15 -0400

    revert

--- apps/wallet/src/ui/app/pages/home/nft-details/LabelValueItem.tsx
+++ apps/wallet/src/ui/app/pages/home/nft-details/LabelValueItem.tsx
@@ -49,7 +49,7 @@ export function LabelValueItem({
     }
     return display ? (
         <div className="flex flex-row flex-nowrap gap-1">
-            <div className="flex-1 [&>div]:leading-5">
+            <div className="flex-1 overflow-hidden [&>div]:leading-5">
                 <Text
                     color="steel-dark"
                     variant="body"
