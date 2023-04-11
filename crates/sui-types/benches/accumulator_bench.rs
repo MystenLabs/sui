d5e08b4699c0d5bc615c3d90e1a52ff1ac756b40
Mark Logan <mark@mystenlabs.com>
2023-04-11 13:30:07 -0700

    Measure throughput in accumulator bench

--- crates/sui-types/benches/accumulator_bench.rs
+++ crates/sui-types/benches/accumulator_bench.rs
@@ -1,31 +1,38 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-#[macro_use]
-extern crate criterion;
-
 use fastcrypto::hash::MultisetHash;
 use sui_types::accumulator::Accumulator;
 use sui_types::base_types::ObjectDigest;
 
-use criterion::Criterion;
+use criterion::*;
 
 fn accumulator_benchmark(c: &mut Criterion) {
-    let digests: Vec<_> = (0..1_000).map(|_| ObjectDigest::random()).collect();
-    let mut accumulator = Accumulator::default();
-
-    c.bench_function("accumulate_digests", |b| {
-        b.iter(|| accumulator.insert_all(&digests))
-    });
-
-    let mut accumulator = Accumulator::default();
-    let point = {
-        let digest = ObjectDigest::random();
+    {
+        let digests: Vec<_> = (0..1_000).map(|_| ObjectDigest::random()).collect();
         let mut accumulator = Accumulator::default();
-        accumulator.insert(digest);
-        accumulator
-    };
-    c.bench_function("sum_accumulators", |b| b.iter(|| accumulator.union(&point)));
+
+        let mut group = c.benchmark_group("accumulator");
+        group.throughput(Throughput::Elements(digests.len() as u64));
+
+        group.bench_function("accumulate_digests", |b| {
+            b.iter(|| accumulator.insert_all(&digests))
+        });
+    }
+
+    {
+        let mut group = c.benchmark_group("accumulator");
+        group.throughput(Throughput::Elements(1));
+
+        let mut accumulator = Accumulator::default();
+        let point = {
+            let digest = ObjectDigest::random();
+            let mut accumulator = Accumulator::default();
+            accumulator.insert(digest);
+            accumulator
+        };
+        group.bench_function("sum_accumulators", |b| b.iter(|| accumulator.union(&point)));
+    }
 }
 
 criterion_group!(benches, accumulator_benchmark);
