f4a752317425cd565b0cf4498ae285755cac0e9d
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-04-14 01:15:12 +0100

    vite resolve source

--- apps/wallet/tests/demo-app/vite.config.ts
+++ apps/wallet/tests/demo-app/vite.config.ts
@@ -7,4 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths({ root: '../../' })],
+    resolve: {
+        conditions: ['source'],
+    },
 });
