d1f5024a66dbefe143b8b418413f44714e38e25e
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-04-16 17:44:54 +0200

    Add debug logging (#10956)
    This combines the changes of the following commits and PR's into and
    merges them into main:
    
    -
    https://github.com/MystenLabs/sui/commit/a51401887edf62381e2ab322888dab1493894343
    -
    https://github.com/MystenLabs/sui/commit/5acb4f5c3c561e117c6cebcfeaea6264279c31f5
    -
    https://github.com/MystenLabs/sui/commit/b8bc634d312b49979b801f9d452914c333f90b95
    - https://github.com/MystenLabs/sui/pull/10895
    - https://github.com/MystenLabs/sui/pull/10900

--- crates/sui-types/src/crypto.rs
+++ crates/sui-types/src/crypto.rs
@@ -45,6 +45,7 @@ use fastcrypto::error::FastCryptoError;
 use fastcrypto::hash::{Blake2b256, HashFunction};
 pub use fastcrypto::traits::Signer;
 use std::fmt::Debug;
+use tracing::warn;
 
 #[cfg(test)]
 #[path = "unit_tests/crypto_tests.rs"]
@@ -1550,7 +1551,7 @@ impl<'a> VerificationObligation<'a> {
 
     pub fn verify_all(self) -> SuiResult<()> {
         let mut pks = Vec::with_capacity(self.public_keys.len());
-        for pk in self.public_keys {
+        for pk in self.public_keys.clone() {
             pks.push(pk.into_iter());
         }
         AggregateAuthoritySignature::batch_verify(
@@ -1558,8 +1559,42 @@ impl<'a> VerificationObligation<'a> {
             pks,
             &self.messages.iter().map(|x| &x[..]).collect::<Vec<_>>()[..],
         )
-        .map_err(|e| SuiError::InvalidSignature {
-            error: format!("Failed to batch verify aggregated auth sig {}", e),
+        .map_err(|e| {
+            let message = format!(
+                "pks: {:?}, messages: {:?}, sigs: {:?}",
+                &self.public_keys,
+                self.messages
+                    .iter()
+                    .map(Base64::encode)
+                    .collect::<Vec<String>>(),
+                &self
+                    .signatures
+                    .iter()
+                    .map(|s| Base64::encode(s.as_ref()))
+                    .collect::<Vec<String>>()
+            );
+
+            let chunk_size = 2048;
+
+            // This error message may be very long, so we print out the error in chunks of to avoid
+            // hitting a max log line length on the system.
+            for (i, chunk) in message
+                .as_bytes()
+                .chunks(chunk_size)
+                .map(std::str::from_utf8)
+                .enumerate()
+            {
+                warn!(
+                    "Failed to batch verify aggregated auth sig: {} (chunk {}): {}",
+                    e,
+                    i,
+                    chunk.unwrap()
+                );
+            }
+
+            SuiError::InvalidSignature {
+                error: format!("Failed to batch verify aggregated auth sig: {}", e),
+            }
         })?;
         Ok(())
     }
