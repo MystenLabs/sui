3b2d0e3e94a62010db489c3d1065b7233976b441
healthydeve <jian@mystenlabs.com>
2023-04-17 17:54:00 -0400

    removing pub

--- crates/sui-faucet/src/main.rs
+++ crates/sui-faucet/src/main.rs
@@ -170,7 +170,7 @@ async fn request_gas(
     }
 }
 
-pub async fn create_wallet_context(timeout_secs: u64) -> Result<WalletContext, anyhow::Error> {
+async fn create_wallet_context(timeout_secs: u64) -> Result<WalletContext, anyhow::Error> {
     let wallet_conf = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
     info!("Initialize wallet from config path: {:?}", wallet_conf);
     WalletContext::new(&wallet_conf, Some(Duration::from_secs(timeout_secs))).await
