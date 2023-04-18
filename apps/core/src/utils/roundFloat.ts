471c15c02c04d590eb937af6def81c8b4197b280
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-04-18 17:36:48 -0400

    add default precision

--- apps/core/src/utils/roundFloat.ts
+++ apps/core/src/utils/roundFloat.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export function roundFloat(num: number, precision: number) {
+const defaultPrecision = 2;
+export function roundFloat(num: number, precision = defaultPrecision) {
     return parseFloat(num.toFixed(precision));
 }
