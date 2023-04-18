6c918826d3c6b4ecd798b16c428d0505a7eb8578
Phong Lam <phonglam@Phongs-Laptop.attlocal.net>
2023-04-18 10:06:33 -0700

    remove css module

--- apps/wallet/src/ui/app/pages/welcome/index.tsx
+++ apps/wallet/src/ui/app/pages/welcome/index.tsx
@@ -1,20 +1,16 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { CheckFill16 } from '@mysten/icons';
-import cl from 'classnames';
-import { Link } from 'react-router-dom';
+import { ArrowRight16, CheckFill16 } from '@mysten/icons';
 
+import { Button } from '_app/shared/ButtonUI';
 import BottomMenuLayout, { Content } from '_app/shared/bottom-menu-layout';
 import { Heading } from '_app/shared/heading';
 import { Text } from '_app/shared/text';
-import Icon, { SuiIcons } from '_components/icon';
 import Loading from '_components/loading';
 import Logo from '_components/logo';
 import { useInitializedGuard } from '_hooks';
 import PageLayout from '_pages/layout';
-
-import st from './Welcome.module.scss';
 
 const VALUE_PROP = [
     'Send, receive tokens and NFTs',
@@ -60,7 +56,7 @@ const WelcomePage = () => {
                                         </div>
                                     </div>
 
-                                    <div className="mt-5 flex gap-2 flex-col">
+                                    <div className="mt-6 flex gap-2 flex-col">
                                         {VALUE_PROP.map((value) => (
                                             <div
                                                 key={value}
@@ -80,17 +76,14 @@ const WelcomePage = () => {
                                     </div>
                                 </div>
                             </Content>
-                            <div className={st.getStarted}>
-                                <Link
+
+                            <div className="flex sticky pb-10 m-auto w-[300px] -bottom-px bg-sui-lightest">
+                                <Button
                                     to="/initialize/select"
-                                    className={cl(st.cta, 'btn', 'primary')}
-                                >
-                                    Get Started
-                                    <Icon
-                                        icon={SuiIcons.ArrowLeft}
-                                        className={cl(st.arrowLeft)}
-                                    />
-                                </Link>
+                                    size="tall"
+                                    text="Get Started"
+                                    after={<ArrowRight16 />}
+                                />
                             </div>
                         </BottomMenuLayout>
                     </div>
