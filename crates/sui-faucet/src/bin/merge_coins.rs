edcdb88283f809ca9d8961c3a01bd61e0beb142f
Jian Lu <123408603+healthydeve@users.noreply.github.com>
2023-04-18 09:57:03 -0400

    [faucet/script] Adding helper script to clean up coins (#11013)
    ## Description 
    
    Adding a helper script for faucet.
    
    ## Test Plan 
    
    Already used to smash some coins together.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ crates/sui-faucet/src/bin/merge_coins.rs
@@ -0,0 +1,83 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+use shared_crypto::intent::Intent;
+use std::{str::FromStr, time::Duration};
+use sui::client_commands::WalletContext;
+use sui_config::{sui_config_dir, SUI_CLIENT_CONFIG};
+use sui_faucet::FaucetError;
+use sui_json_rpc_types::SuiTransactionBlockResponseOptions;
+use sui_keys::keystore::AccountKeystore;
+use sui_types::{
+    base_types::ObjectID,
+    gas_coin::GasCoin,
+    messages::{ExecuteTransactionRequestType, Transaction},
+};
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut wallet = create_wallet_context(60).await?;
+    let active_address = wallet
+        .active_address()
+        .map_err(|err| FaucetError::Wallet(err.to_string()))?;
+    println!("SimpleFaucet::new with active address: {active_address}");
+    let client = wallet.get_client().await?;
+
+    // Pick a gas coin here that isn't in use by the faucet otherwise there will be some contention.
+    let gas_coin = "0xfde0a5e733e446db0c5d2294673ee5f5a17c5aeacb7565ce82cbd5a145e15d44";
+    let small_coins = wallet
+        .gas_objects(active_address)
+        .await
+        .map_err(|e| FaucetError::Wallet(e.to_string()))?
+        .iter()
+        // Ok to unwrap() since `get_gas_objects` guarantees gas
+        .map(|q| GasCoin::try_from(&q.1).unwrap())
+        // Everything less than 1 sui
+        .filter(|coin| coin.0.balance.value() <= 10000000000)
+        .collect::<Vec<GasCoin>>();
+
+    // Smash coins togethers 254 objects at a time
+    // TODO (jian): docs are actually wrong
+    for chunk in small_coins.chunks(254) {
+        let total_balance: u64 = chunk.iter().map(|coin| coin.0.balance.value()).sum();
+
+        let mut coin_vector = chunk
+            .iter()
+            .map(|coin| *coin.id())
+            .collect::<Vec<ObjectID>>();
+
+        // prepend big gas coin instance to vector
+        coin_vector.insert(0, ObjectID::from_str(gas_coin).unwrap());
+        let target = vec![active_address];
+        let target_amount = vec![total_balance];
+
+        let tx_data = client
+            .transaction_builder()
+            .pay_sui(active_address, coin_vector, target, target_amount, 1000000)
+            .await?;
+        let signature = wallet
+            .config
+            .keystore
+            .sign_secure(&active_address, &tx_data, Intent::sui_transaction())
+            .unwrap();
+        let tx = Transaction::from_data(tx_data, Intent::sui_transaction(), vec![signature])
+            .verify()
+            .unwrap();
+        client
+            .quorum_driver()
+            .execute_transaction_block(
+                tx.clone(),
+                SuiTransactionBlockResponseOptions::new().with_effects(),
+                Some(ExecuteTransactionRequestType::WaitForLocalExecution),
+            )
+            .await?;
+    }
+
+    Ok(())
+}
+
+pub async fn create_wallet_context(timeout_secs: u64) -> Result<WalletContext, anyhow::Error> {
+    let wallet_conf = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
+    info!("Initialize wallet from config path: {:?}", wallet_conf);
+    WalletContext::new(&wallet_conf, Some(Duration::from_secs(timeout_secs))).await
+}
