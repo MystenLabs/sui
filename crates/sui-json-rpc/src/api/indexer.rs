42ab1678dcd76d5e70882b9afb30f89e64623fb5
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2023-04-20 12:28:49 -0400

    sui ns rpc: add option for resolve_name_service_address (#11105)
    ## Description 
    
    Not found should be a None instead of Err.
    
    ## Test Plan 
    
    test locally to make sure error and option are returned as expected, 
    1. register `adc`
    2. try reading `adc` and `ccc`
    ```
    curl --location --request POST http://127.0.0.1:9000 \
    --header 'Content-Type: application/json' \
    --data-raw '{
        "jsonrpc": "2.0",
        "id": 1,
        "method": "suix_resolveNameServiceAddress",
        "params": ["ccc"]
    }'
    
    {"jsonrpc":"2.0","result":null,"id":1}%                                                                                                                                                                           
    curl --location --request POST http://127.0.0.1:9000 \
    --header 'Content-Type: application/json' \
    --data-raw '{
        "jsonrpc": "2.0",
        "id": 1,
        "method": "suix_resolveNameServiceAddress",
        "params": ["adc"]
    }'
    {"jsonrpc":"2.0","result":"0x54f349899f6b521547bcfcb2605c65f307934b4f32c256fd3e2d4df6feecb870","id":1}%       
    ```
    4. register `ccc` and read again
    ```
    curl --location --request POST http://127.0.0.1:9000 \
    --header 'Content-Type: application/json' \
    --data-raw '{
        "jsonrpc": "2.0",
        "id": 1,
        "method": "suix_resolveNameServiceAddress",
        "params": ["ccc"]
    }'
    {"jsonrpc":"2.0","result":"0x8d4be6642840f781adcbabebd6a12e49f5313e5e3fb5cc3e84207e7c52726c74","id":1}%     
    ```

--- crates/sui-json-rpc/src/api/indexer.rs
+++ crates/sui-json-rpc/src/api/indexer.rs
@@ -99,7 +99,7 @@ pub trait IndexerApi {
         &self,
         /// The name to resolve
         name: String,
-    ) -> RpcResult<SuiAddress>;
+    ) -> RpcResult<Option<SuiAddress>>;
 
     /// Return the resolved names given address,
     /// if multiple names are resolved, the first one is the primary name.
