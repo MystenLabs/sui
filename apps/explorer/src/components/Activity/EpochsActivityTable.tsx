4bda1b88fe6725abdaffca393428c539e584679a
Nikhil Mattu <nikhil@mystenlabs.com>
2023-04-20 14:55:18 -0400

    prevention

--- apps/explorer/src/components/Activity/EpochsActivityTable.tsx
+++ apps/explorer/src/components/Activity/EpochsActivityTable.tsx
@@ -54,98 +54,93 @@ export function EpochsActivityTable({
             : undefined;
 
     return (
-        <div data-testid="tx">
-            <div className="flex flex-col space-y-5 text-left xl:pr-10">
-                {isLoading || isFetching || isFetchingNextPage || !cardData ? (
-                    <PlaceholderTable
-                        rowCount={limit}
-                        rowHeight="16px"
-                        colHeadings={[
-                            'Epoch',
-                            'Transaction Blocks',
-                            'Stake Rewards',
-                            'Checkpoint Set',
-                            'Storage Revenue',
-                            'Epoch End',
-                        ]}
-                        colWidths={[
-                            '100px',
-                            '120px',
-                            '40px',
-                            '204px',
-                            '90px',
-                            '38px',
-                        ]}
+        <div className="flex flex-col space-y-5 text-left xl:pr-10">
+            {isLoading || isFetching || isFetchingNextPage || !cardData ? (
+                <PlaceholderTable
+                    rowCount={limit}
+                    rowHeight="16px"
+                    colHeadings={[
+                        'Epoch',
+                        'Transaction Blocks',
+                        'Stake Rewards',
+                        'Checkpoint Set',
+                        'Storage Revenue',
+                        'Epoch End',
+                    ]}
+                    colWidths={[
+                        '100px',
+                        '120px',
+                        '40px',
+                        '204px',
+                        '90px',
+                        '38px',
+                    ]}
+                />
+            ) : (
+                <div>
+                    <TableCard
+                        data={cardData.data}
+                        columns={cardData.columns}
+                    />
+                </div>
+            )}
+
+            <div className="flex justify-between">
+                {(hasNextPage || data?.pages.length) && !disablePagination ? (
+                    <Pagination
+                        onNext={() => {
+                            if (isLoading || isFetching) {
+                                return;
+                            }
+
+                            // Make sure we are at the end before fetching another page
+                            if (
+                                data &&
+                                currentPage === data?.pages.length - 1 &&
+                                !isLoading &&
+                                !isFetching
+                            ) {
+                                fetchNextPage();
+                            }
+                            setCurrentPage(currentPage + 1);
+                        }}
+                        hasNext={
+                            Boolean(hasNextPage) &&
+                            Boolean(data?.pages[currentPage])
+                        }
+                        hasPrev={currentPage !== 0}
+                        onPrev={() => setCurrentPage(currentPage - 1)}
+                        onFirst={() => setCurrentPage(0)}
                     />
                 ) : (
-                    <div>
-                        <TableCard
-                            data={cardData.data}
-                            columns={cardData.columns}
-                        />
-                    </div>
+                    disablePagination && (
+                        <Link to="/recent?tab=epochs" after={<ArrowRight12 />}>
+                            More Epochs
+                        </Link>
+                    )
                 )}
 
-                <div className="flex justify-between">
-                    {(hasNextPage || data?.pages.length) &&
-                    !disablePagination ? (
-                        <Pagination
-                            onNext={() => {
-                                if (isLoading || isFetching) {
-                                    return;
-                                }
-
-                                // Make sure we are at the end before fetching another page
-                                if (
-                                    data &&
-                                    currentPage === data?.pages.length - 1 &&
-                                    !isLoading &&
-                                    !isFetching
-                                ) {
-                                    fetchNextPage();
-                                }
-                                setCurrentPage(currentPage + 1);
+                <div className="flex items-center space-x-3">
+                    <Text variant="body/medium" color="steel-dark">
+                        {countQuery.data
+                            ? numberSuffix(Number(countQuery.data))
+                            : '-'}
+                        {` Epochs`}
+                    </Text>
+                    {!disablePagination && (
+                        <select
+                            className="form-select rounded-md border border-gray-45 px-3 py-2 pr-8 text-bodySmall font-medium leading-[1.2] text-steel-dark shadow-button"
+                            value={limit}
+                            onChange={(e) => {
+                                setLimit(Number(e.target.value));
+                                setCurrentPage(0);
                             }}
-                            hasNext={Boolean(hasNextPage)}
-                            hasPrev={currentPage !== 0}
-                            onPrev={() => setCurrentPage(currentPage - 1)}
-                            onFirst={() => setCurrentPage(0)}
-                        />
-                    ) : (
-                        <div>
-                            {disablePagination && (
-                                <Link
-                                    to="/recent?tab=epochs"
-                                    after={<ArrowRight12 />}
-                                >
-                                    More Epochs
-                                </Link>
-                            )}
-                        </div>
+                        >
+                            <option value={20}>20 Per Page</option>
+                            <option value={40}>40 Per Page</option>
+                            <option value={60}>60 Per Page</option>
+                        </select>
                     )}
-
-                    <div className="flex items-center space-x-3">
-                        <Text variant="body/medium" color="steel-dark">
-                            {countQuery.data
-                                ? numberSuffix(Number(countQuery.data))
-                                : '-'}
-                            {` Epochs`}
-                        </Text>
-                        {!disablePagination && (
-                            <select
-                                className="form-select rounded-md border border-gray-45 px-3 py-2 pr-8 text-bodySmall font-medium leading-[1.2] text-steel-dark shadow-button"
-                                value={limit}
-                                onChange={(e) => {
-                                    setLimit(Number(e.target.value));
-                                    setCurrentPage(0);
-                                }}
-                            >
-                                <option value={20}>20 Per Page</option>
-                                <option value={40}>40 Per Page</option>
-                                <option value={60}>60 Per Page</option>
-                            </select>
-                        )}
-                    </div>
                 </div>
             </div>
         </div>
