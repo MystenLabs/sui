30dcce5e4b557fa6f4f8d06da664a7072469e87b
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-04-21 09:55:50 -0400

    comment

--- apps/core/src/utils/formatAmount.ts
+++ apps/core/src/utils/formatAmount.ts
@@ -12,20 +12,21 @@ export function formatAmountParts(
 
     let postfix = '';
     let bn = new BigNumber(amount.toString());
+    const bnAbs = bn.abs();
 
     // use absolute value to determine the postfix
-    if (bn.abs().gte(1_000_000_000)) {
+    if (bnAbs.gte(1_000_000_000)) {
         bn = bn.shiftedBy(-9);
         postfix = 'B';
-    } else if (bn.abs().gte(1_000_000)) {
+    } else if (bnAbs.gte(1_000_000)) {
         bn = bn.shiftedBy(-6);
         postfix = 'M';
-    } else if (bn.abs().gte(10_000)) {
+    } else if (bnAbs.gte(10_000)) {
         bn = bn.shiftedBy(-3);
         postfix = 'K';
     }
 
-    if (bn.abs().gte(1)) {
+    if (bnAbs.gte(1)) {
         bn = bn.decimalPlaces(2, BigNumber.ROUND_DOWN);
     }
 
