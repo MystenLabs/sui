7c9099570f084c9e4d9b82bc1b9ff0fa876716fe
ade <ade@mystenlabs.com>
2023-04-24 16:11:15 -0400

    trial

--- crates/sui-storage/src/sec.rs
+++ crates/sui-storage/src/sec.rs
@@ -2,17 +2,18 @@ use typed_store::Map;
 
 use crate::indexes::IndexStoreTables;
 use std::path::PathBuf;
-
+use typed_store::rocks::MetricConf;
 pub fn follow_index_table(path: PathBuf) {
     let index_store_read_only_handle =
-        IndexStoreTables::get_read_only_handle(db_path, None, None, MetricConf::default());
+        IndexStoreTables::get_read_only_handle(path, None, None, MetricConf::default());
 
     index_store_read_only_handle
         .owner_index
-        .try_catch_up_with_primary();
+        .try_catch_up_with_primary()
+        .unwrap();
 
     let count = 10;
-    let it = index_store_read_only_handle.owner_index.iter();
+    let mut it = index_store_read_only_handle.owner_index.iter();
 
     while count > 0 {
         println!("{:?}", it.next());
