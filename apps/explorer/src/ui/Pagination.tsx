e882f8dcb9eedeb8c92aea3ae39cebb4367a46bf
Nikhil Mattu <nikhil@mystenlabs.com>
2023-04-24 12:21:12 -0400

    revert

--- apps/explorer/src/ui/Pagination.tsx
+++ apps/explorer/src/ui/Pagination.tsx
@@ -22,6 +22,73 @@ export interface PaginationResponse<Cursor = string> {
     hasNextPage: boolean;
 }
 
+// todo: temporary solution to allow pagination for queries
+// with an upper and lower bound
+export function useBoundedPaginationStack<Cursor = string>(
+    initialCursor?: Cursor,
+    maxCursor?: Cursor
+) {
+    const [stack, setStack] = useState<Cursor[]>(
+        initialCursor ? [initialCursor] : []
+    );
+    return {
+        cursor: stack.at(-1),
+        props({
+            hasNextPage = false,
+            nextCursor = null,
+        }: Partial<PaginationResponse<Cursor>> = {}): PaginationProps {
+            return {
+                hasPrev: initialCursor ? stack.length > 1 : stack.length > 0,
+                hasNext: hasNextPage && Number(nextCursor) > Number(maxCursor),
+                onFirst() {
+                    setStack(initialCursor ? [initialCursor] : []);
+                },
+                onNext() {
+                    if (nextCursor && hasNextPage) {
+                        setStack((stack) => [...stack, nextCursor]);
+                    }
+                },
+                onPrev() {
+                    setStack((stack) => stack.slice(0, -1));
+                },
+            };
+        },
+    };
+}
+
+export function useCursorPagination<T>(query: UseInfiniteQueryResult<T>) {
+    const [currentPage, setCurrentPage] = useState(0);
+
+    return {
+        ...query,
+        data: query.data?.pages[currentPage],
+        pagination: {
+            onFirst: () => setCurrentPage(0),
+            onNext: () => {
+                if (!query.data || query.isFetchingNextPage) {
+                    return;
+                }
+
+                // Make sure we are at the end before fetching another page
+                if (currentPage >= query.data.pages.length - 1) {
+                    query.fetchNextPage();
+                }
+
+                setCurrentPage(currentPage + 1);
+            },
+            onPrev: () => {
+                setCurrentPage(Math.max(currentPage - 1, 0));
+            },
+            hasNext:
+                !query.isFetchingNextPage &&
+                (currentPage < (query.data?.pages.length ?? 0) - 1 ||
+                    !!query.hasNextPage),
+            hasPrev: currentPage !== 0,
+        } satisfies PaginationProps,
+    };
+}
+
+/** @deprecated Prefer `useCursorPagination` + `useInfiniteQuery` for pagination. */
 export function usePaginationStack<Cursor = string>() {
     const [stack, setStack] = useState<Cursor[]>([]);
 
