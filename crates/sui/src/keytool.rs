67f6a6af303dc6f81985ed78163f785df6ed4c33
siomari <maria@mystenlabs.com>
2023-04-24 15:45:49 +0300

    add comments

--- crates/sui/src/keytool.rs
+++ crates/sui/src/keytool.rs
@@ -118,26 +118,32 @@ pub enum KeyToolCommand {
         threshold: ThresholdUnit,
     },
 
+    /// Converts a Base64 encoded string to its hexademical representation.
     Base64ToHex {
         base64: String,
     },
 
+    /// Converts a hexademical string to its Base64 encoded representation.
     HexToBase64 {
         hex: String,
     },
 
+    /// Converts a hexademical string to its correspinding bytes.
     HexToBytes {
         hex: String,
     },
 
+    /// Converts an array of bytes to its hexademical string representation.
     BytesToHex {
         bytes: Vec<u8>,
     },
 
+    /// Decodes a Base64 encoded string to its corresponding bytes.
     Base64ToBytes {
         base64: String,
     },
 
+    /// Encodes an array of bytes to its Base64 string representation.
     BytesToBase64 {
         bytes: Vec<u8>,
     },
@@ -263,14 +269,14 @@ impl KeyToolCommand {
                 let bytes =
                     Base64::decode(&base64).map_err(|e| anyhow!("Invalid base64 key: {:?}", e))?;
                 let hex = Hex::from_bytes(&bytes);
-                println!("Hex {:?}", hex);
+                println!("{:?}", hex);
             }
 
             KeyToolCommand::HexToBase64 { hex } => {
                 let bytes =
                     Hex::decode(&hex).map_err(|e| anyhow!("Invalid base64 key: {:?}", e))?;
                 let base64 = Base64::from_bytes(&bytes);
-                println!("Base64 {:?}", base64);
+                println!("{:?}", base64);
             }
 
             KeyToolCommand::HexToBytes { hex } => {
@@ -281,7 +287,7 @@ impl KeyToolCommand {
 
             KeyToolCommand::BytesToHex { bytes } => {
                 let hex = Hex::from_bytes(&bytes);
-                println!("Hex {:?}", hex);
+                println!("{:?}", hex);
             }
 
             KeyToolCommand::Base64ToBytes { base64 } => {
@@ -292,7 +298,7 @@ impl KeyToolCommand {
 
             KeyToolCommand::BytesToBase64 { bytes } => {
                 let base64 = Base64::from_bytes(&bytes);
-                println!("Base64 {:?}", base64);
+                println!("{:?}", base64);
             }
 
             KeyToolCommand::LoadKeypair { file } => {
