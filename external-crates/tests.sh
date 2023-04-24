cdafe6df5ba399006cf834fd911c67a63476b036
Ashok Menon <ashok@mystenlabs.com>
2023-04-24 15:55:07 +0100

    [move-execution] Run move-execution tests with external-tests

--- external-crates/tests.sh
+++ external-crates/tests.sh
@@ -1,5 +1,18 @@
-# Run tests for external crates
+#!/bin/sh
+
+# Fail after the first error
+set -e
+
+# Run tests for move
 echo "Running Move tests in external-crates"
-cd move
 echo "Excluding prover and evm Move tests"
-cargo nextest run -E '!package(move-to-yul) and !package(move-prover) and !test(prove) and !test(simple_build_with_docs)'
+
+cargo nextest run                     \
+      --manifest-path move/Cargo.toml \
+      -E '!(package(move-to-yul) | package(move-prover) | test(prove) | test(simple_build_with_docs))'
+
+# Run tests for various versions of move-execution
+for v in move-execution/v*; do
+    echo "Running Move execution $(basename "$v") tests"
+    cargo nextest run --manifest-path "$v/Cargo.toml"
+done
