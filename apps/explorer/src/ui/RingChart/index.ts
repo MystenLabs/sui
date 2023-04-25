2187e2e4f00feeb749567a810940834e7ce01010
Phong Lam <phonglam@Phongs-Laptop.attlocal.net>
2023-04-25 10:40:29 -0700

    remove index file

--- apps/explorer/src/ui/RingChart/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-export { RingChart, type RingChartProps } from './RingChart';
-export { RingChartLegend } from './RingChartLegend';
