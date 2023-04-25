d2d3ad6f9ac7c92094c2d4d6435885b19b33f9b4
Andrey Chursin <andrey@mystenlabs.com>
2023-04-25 15:40:41 -0700

    [sui-node] Stop and wait sui node runtimes on SIGINT (#11323)
    This change makes sure we stop all sui node runtimes upon receiving
    SIGINT. This is needed to make sure rocks db databases are properly
    closed before existing sui node

---
+++ crates/sui-core/src/runtime.rs
@@ -0,0 +1,29 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use sui_config::NodeConfig;
+use tokio::runtime::Runtime;
+
+pub struct SuiRuntimes {
+    // Order in this struct is the order in which runtimes are stopped
+    pub sui_node: Runtime,
+    pub metrics: Runtime,
+    // json_rpc: Runtime,
+}
+
+impl SuiRuntimes {
+    pub fn new(_confg: &NodeConfig) -> Self {
+        let sui_node = tokio::runtime::Builder::new_multi_thread()
+            .thread_name("sui-node-runtime")
+            .enable_all()
+            .build()
+            .unwrap();
+        let metrics = tokio::runtime::Builder::new_multi_thread()
+            .thread_name("metrics-runtime")
+            .worker_threads(2)
+            .enable_all()
+            .build()
+            .unwrap();
+        Self { sui_node, metrics }
+    }
+}
