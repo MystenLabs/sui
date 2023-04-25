6c921a0cc0f86f82c114cfd21b5b24afa67f07e0
Xun Li <lxfind@gmail.com>
2023-04-25 13:16:59 -0700

    Fix release build

--- crates/sui-adapter/src/execution_engine.rs
+++ crates/sui-adapter/src/execution_engine.rs
@@ -121,7 +121,6 @@ pub fn execute_transaction_to_effects_impl<
 ) {
     let mut tx_ctx = TxContext::new_from_components(&transaction_signer, &transaction_digest, epoch_id, epoch_timestamp_ms);
 
-    #[cfg(debug_assertions)]
     let is_epoch_change = matches!(transaction_kind, TransactionKind::ChangeEpoch(_));
 
     let (gas_cost_summary, execution_result) = execute_transaction::<Mode, _>(
