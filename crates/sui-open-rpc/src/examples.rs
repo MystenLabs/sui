76a2f572e3c802b2aabbb92427e988cedf2323c4
Ronny Roland <ronmilton@mystenlabs.com>
2023-04-25 12:38:32 -0600

    Edits

--- crates/sui-open-rpc/src/examples.rs
+++ crates/sui-open-rpc/src/examples.rs
@@ -52,6 +52,8 @@ use sui_types::signature::GenericSignature;
 use sui_types::utils::to_sender_signed_transaction;
 use sui_types::{parse_sui_struct_tag, SUI_FRAMEWORK_OBJECT_ID};
 use sui_types::coin::CoinMetadata;
+use sui_types::dynamic_field::{DynamicFieldInfo, DynamicFieldName, DynamicFieldType, Field};
+use sui_types::TypeTag;
 
 
 struct Examples {
@@ -106,6 +108,7 @@ impl RpcExampleProvider {
             self.sui_get_latest_checkpoint_sequence_number(),
             self.suix_get_coins(),
             self.suix_get_total_supply(),
+            self.suix_getDynamicFieldObject(),
         ]
         .into_iter()
         .map(|example| (example.function_name, example.examples))
@@ -843,6 +846,24 @@ impl RpcExampleProvider {
                 "Get total supply for the type of coin provided.",
                 vec![("coin_type", json!(coin))],
                 json!(result)
+            )]
+        )
+    }
+
+    fn suix_getDynamicFieldObject(&mut self) -> Examples {
+        
+        
+        let name = DynamicFieldName {
+            type_: TypeTag::new("0x2::sui::SUI".to_string()),
+            value: SuiJsonValue::from("name_value").to_json_value(),
+        };
+
+        Examples::new(
+            "suix_getDynamicFieldObject",
+            vec![ExamplePairing::new(
+                "Get",
+                vec![("name", json!(name))],
+                json!("")
             )]
         )
     }
