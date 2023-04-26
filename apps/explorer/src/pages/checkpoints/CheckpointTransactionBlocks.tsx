e61ed2bac93abe0aec4868b9d726d98803e5b0a2
Nikhil-Mysten <128089541+Nikhil-Mysten@users.noreply.github.com>
2023-04-26 17:49:07 -0400

    [explorer] paginated checkpoint transactions (#11290)
    ## Description 
    
    <img width="1360" alt="image"
    src="https://user-images.githubusercontent.com/128089541/234139263-9a3c9f97-b5b7-4baa-9695-733c8dfe7145.png">
    
    
    ## Test Plan 
    
    Locally
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [x] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    Using new transaction filter to get transactions for checkpoints detail

--- apps/explorer/src/pages/checkpoints/CheckpointTransactionBlocks.tsx
+++ apps/explorer/src/pages/checkpoints/CheckpointTransactionBlocks.tsx
@@ -1,36 +1,62 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useRpcClient } from '@mysten/core';
-import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 
-import {
-    genTableDataFromTxData,
-    getDataOnTxDigests,
-} from '~/components/transactions/TxCardUtils';
+import { genTableDataFromTxData } from '~/components/transactions/TxCardUtils';
+import { useGetTransactionBlocks } from '~/hooks/useGetTransactionBlocks';
+import { Pagination, useCursorPagination } from '~/ui/Pagination';
+import { PlaceholderTable } from '~/ui/PlaceholderTable';
 import { TableCard } from '~/ui/TableCard';
 
-export function CheckpointTransactionBlocks({
-    digest,
-    transactions,
-}: {
-    digest: string;
-    transactions: string[];
-}) {
-    const rpc = useRpcClient();
-    const { data: txData, isLoading } = useQuery(
-        ['checkpoint-transactions', digest],
-        async () => {
-            // todo: replace this with `suix_queryTransactionBlocks` call when we are
-            // able to query by checkpoint digest
-            const txData = await getDataOnTxDigests(rpc, transactions!);
-            return genTableDataFromTxData(txData);
-        },
-        { enabled: !!transactions.length }
-    );
-    if (isLoading) return null;
+const DEFAULT_TRANSACTIONS_LIMIT = 20;
 
-    return txData ? (
-        <TableCard data={txData?.data} columns={txData?.columns} />
-    ) : null;
+export function CheckpointTransactionBlocks({ id }: { id: string }) {
+    const [limit, setLimit] = useState(DEFAULT_TRANSACTIONS_LIMIT);
+    const transactions = useGetTransactionBlocks(
+        {
+            Checkpoint: id,
+        },
+        limit
+    );
+
+    const { data, isFetching, pagination, isLoading } =
+        useCursorPagination(transactions);
+
+    const cardData = data ? genTableDataFromTxData(data.data) : undefined;
+
+    return (
+        <div className="flex flex-col space-y-5 text-left xl:pr-10">
+            {isLoading || isFetching || !cardData ? (
+                <PlaceholderTable
+                    rowCount={20}
+                    rowHeight="16px"
+                    colHeadings={['Digest', 'Sender', 'Txns', 'Gas', 'Time']}
+                    colWidths={['30%', '30%', '10%', '20%', '10%']}
+                />
+            ) : (
+                <div>
+                    <TableCard
+                        data={cardData.data}
+                        columns={cardData.columns}
+                    />
+                </div>
+            )}
+            <div className="flex justify-between">
+                <Pagination {...pagination} />
+                <select
+                    className="form-select rounded-md border border-gray-45 px-3 py-2 pr-8 text-bodySmall font-medium leading-[1.2] text-steel-dark shadow-button"
+                    value={limit}
+                    onChange={(e) => {
+                        setLimit(Number(e.target.value));
+                        pagination.onFirst();
+                    }}
+                >
+                    <option value={20}>20 Per Page</option>
+                    <option value={40}>40 Per Page</option>
+                    <option value={60}>60 Per Page</option>
+                </select>
+            </div>
+        </div>
+    );
 }
