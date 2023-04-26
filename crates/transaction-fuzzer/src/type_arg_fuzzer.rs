ec85b4a46b4a9c74155aef18ffd99dd4c0bc3cfe
Tim Zakian <2895723+tzakian@users.noreply.github.com>
2023-04-26 11:41:52 -0700

    [transaction fuzzer] Fuzz with valid type tags (#11339)
    Adds a fuzzer generating random valid type tags, one that uses valid
    type tags but an invalid number of type arguments, and another that
    generates almost wholly-correct typetags but possibly with some invalid
    type tags.

--- crates/transaction-fuzzer/src/type_arg_fuzzer.rs
+++ crates/transaction-fuzzer/src/type_arg_fuzzer.rs
@@ -6,7 +6,8 @@ use move_core_types::{account_address::AccountAddress, identifier::Identifier};
 use proptest::arbitrary::*;
 use proptest::prelude::*;
 
-use sui_types::messages::{TransactionData, TransactionKind};
+use sui_types::base_types::ObjectID;
+use sui_types::messages::{ProgrammableTransaction, TransactionData, TransactionKind};
 use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
 use sui_types::utils::to_sender_signed_transaction;
 use sui_types::{TypeTag, SUI_FRAMEWORK_OBJECT_ID};
@@ -60,7 +61,76 @@ pub fn gen_struct_tag() -> impl Strategy<Value = StructTag> {
         })
 }
 
-pub fn pt_for_tags(type_tags: Vec<TypeTag>) -> TransactionKind {
+pub fn generate_valid_type_factory_tags(
+    type_factory_addr: ObjectID,
+) -> impl Strategy<Value = TypeTag> {
+    let leaf = prop_oneof![
+        base_type_factory_tag_gen(type_factory_addr),
+        nested_type_factory_tag_gen(type_factory_addr),
+    ];
+
+    leaf.prop_recursive(8, 6, 10, move |inner| {
+        prop_oneof![inner.prop_map(|x| TypeTag::Vector(Box::new(x))),]
+    })
+}
+
+pub fn generate_valid_and_invalid_type_factory_tags(
+    type_factory_addr: ObjectID,
+) -> impl Strategy<Value = TypeTag> {
+    let leaf = prop_oneof![
+        any::<TypeTag>(),
+        base_type_factory_tag_gen(type_factory_addr),
+        nested_type_factory_tag_gen(type_factory_addr),
+    ];
+
+    leaf.prop_recursive(8, 6, 10, move |inner| {
+        prop_oneof![inner.prop_map(|x| TypeTag::Vector(Box::new(x))),]
+    })
+}
+
+pub fn base_type_factory_tag_gen(addr: ObjectID) -> impl Strategy<Value = TypeTag> {
+    "[A-Z]".prop_map(move |name| {
+        TypeTag::Struct(Box::new(StructTag {
+            address: AccountAddress::from(addr),
+            module: Identifier::new("type_factory").unwrap(),
+            name: Identifier::new(name).unwrap(),
+            type_params: vec![],
+        }))
+    })
+}
+
+pub fn nested_type_factory_tag_gen(addr: ObjectID) -> impl Strategy<Value = TypeTag> {
+    base_type_factory_tag_gen(addr).prop_recursive(20, 256, 10, move |inner| {
+        (inner, "[A-Z]").prop_map(move |(instantiation, name)| {
+            TypeTag::Struct(Box::new(StructTag {
+                address: AccountAddress::from(addr),
+                module: Identifier::new("type_factory").unwrap(),
+                name: Identifier::new(name.to_string() + &name).unwrap(),
+                type_params: vec![instantiation],
+            }))
+        })
+    })
+}
+
+pub fn type_factory_pt_for_tags(
+    package_id: ObjectID,
+    type_tags: Vec<TypeTag>,
+    len: usize,
+) -> ProgrammableTransaction {
+    let mut builder = ProgrammableTransactionBuilder::new();
+    builder
+        .move_call(
+            package_id,
+            Identifier::new("type_factory").unwrap(),
+            Identifier::new(format!("type_tags{}", len)).unwrap(),
+            type_tags,
+            vec![],
+        )
+        .unwrap();
+    builder.finish()
+}
+
+pub fn pt_for_tags(type_tags: Vec<TypeTag>) -> ProgrammableTransaction {
     let mut builder = ProgrammableTransactionBuilder::new();
     builder
         .move_call(
@@ -71,20 +141,17 @@ pub fn pt_for_tags(type_tags: Vec<TypeTag>) -> TransactionKind {
             vec![],
         )
         .unwrap();
-    TransactionKind::ProgrammableTransaction(builder.finish())
+    builder.finish()
 }
 
-pub fn run_type_tags(account: &AccountCurrent, exec: &mut Executor, type_tags: Vec<TypeTag>) {
-    let gas_object = account
-        .current_coins
-        .get(0)
-        .unwrap()
-        .compute_object_reference();
-    let kind = pt_for_tags(type_tags);
+pub fn run_pt(account: &mut AccountCurrent, exec: &mut Executor, pt: ProgrammableTransaction) {
+    let gas_object = account.new_gas_object(exec);
+    let gas_object_ref = gas_object.compute_object_reference();
+    let kind = TransactionKind::ProgrammableTransaction(pt);
     let tx_data = TransactionData::new(
         kind,
         account.initial_data.account.address,
-        gas_object,
+        gas_object_ref,
         GAS,
         GAS_PRICE,
     );
