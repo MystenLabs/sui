cacc1375e7b88533456462236257432b564f977a
William Robertson <williamrobertson@mystenlabs.com>
2023-04-28 17:24:25 -0400

    fix

--- apps/explorer/src/pages/home/Home.tsx
+++ apps/explorer/src/pages/home/Home.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-import { useFeatureIsOn } from '@growthbook/growthbook-react';
 import { lazy, Suspense } from 'react';
 
 import { ErrorBoundary } from '../../components/error-boundary/ErrorBoundary';
@@ -9,12 +8,10 @@ import { TopValidatorsCard } from '../../components/top-validators-card/TopValid
 
 import { Activity } from '~/components/Activity';
 import { GasPriceCard } from '~/components/GasPriceCard';
-import { HomeMetrics } from '~/components/HomeMetrics';
 import { Checkpoint } from '~/components/HomeMetrics/Checkpoint';
 import { CurrentEpoch } from '~/components/HomeMetrics/CurrentEpoch';
 import { NetworkTPS } from '~/components/HomeMetrics/NetworkTPS';
 import { OnTheNetwork } from '~/components/HomeMetrics/OnTheNetwork';
-import { SuiTokenCard } from '~/components/SuiTokenCard';
 import { Card } from '~/ui/Card';
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '~/ui/Tabs';
 
@@ -23,17 +20,13 @@ const NodeMap = lazy(() => import('../../components/node-map'));
 const TRANSACTIONS_LIMIT = 25;
 
 function Home() {
-    const isHomePageRedesignEnabled = useFeatureIsOn(
-        'explorer-home-page-redesign'
-    );
-    return isHomePageRedesignEnabled ? (
+    return (
         <div
             data-testid="home-page"
             className="grid grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-[200px,1fr] lg:grid-cols-[200px,454px,1fr]"
         >
             <NetworkTPS />
-            <SuiTokenCard />
-            <div className="overflow-hidden md:col-span-full lg:col-auto">
+            <div className="overflow-hidden md:col-span-full lg:col-span-2">
                 <OnTheNetwork />
             </div>
             <CurrentEpoch />
@@ -78,54 +71,6 @@ function Home() {
             {/* TODO: Add the popular packages component here :) */}
             {/* <div className="mt-5 bg-gray-60 md:col-span-full lg:col-auto">
                 Popular packages
-            </div> */}
-        </div>
-    ) : (
-        <div
-            data-testid="home-page"
-            className="grid grid-cols-1 gap-x-12 gap-y-10 md:grid-cols-2"
-        >
-            <ErrorBoundary>
-                <HomeMetrics />
-            </ErrorBoundary>
-
-            <ErrorBoundary>
-                <Suspense fallback={<Card />}>
-                    <NodeMap minHeight={280} />
-                </Suspense>
-            </ErrorBoundary>
-
-            <ErrorBoundary>
-                <Activity initialLimit={TRANSACTIONS_LIMIT} disablePagination />
-            </ErrorBoundary>
-
-            <div data-testid="validators-table">
-                <TabGroup>
-                    <TabList>
-                        <Tab>Validators</Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <ErrorBoundary>
-                                <TopValidatorsCard limit={10} showIcon />
-                            </ErrorBoundary>
-                        </TabPanel>
-                    </TabPanels>
-                </TabGroup>
-            </div>
-            {/* <div>
-                    <TabGroup>
-                        <TabList>
-                            <Tab>Recent Packages</Tab>
-                        </TabList>
-                        <TabPanels>
-                            <TabPanel>
-                                <ErrorBoundary>
-                                    <RecentModulesCard />
-                                </ErrorBoundary>
-                            </TabPanel>
-                        </TabPanels>
-                    </TabGroup>
             </div> */}
         </div>
     );
