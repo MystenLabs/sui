7f6917c4e4cb5cb83b599f5d4f01b476acbc2c90
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-04-28 22:56:07 +0300

    explorer: update sui logo icon

--- apps/explorer/src/components/header/Header.tsx
+++ apps/explorer/src/components/header/Header.tsx
@@ -1,7 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ReactComponent as SuiLogo } from '../../assets/Sui Logo.svg';
+import { Sui, SuiLogoTxt } from '@mysten/icons';
+
 import NetworkSelect from '../network/Network';
 import Search from '../search/Search';
 
@@ -12,8 +13,13 @@ function Header() {
         <header className="relative z-20 h-header overflow-visible bg-headerNav">
             <div className="mx-auto flex h-full max-w-[1440px] items-center px-5 2xl:p-0">
                 <div className="mr-8">
-                    <LinkWithQuery data-testid="nav-logo-button" to="/">
-                        <SuiLogo />
+                    <LinkWithQuery
+                        data-testid="nav-logo-button"
+                        to="/"
+                        className="flex flex-nowrap items-center gap-1 text-white"
+                    >
+                        <Sui className="h-[26px] w-5" />
+                        <SuiLogoTxt className="h-[17px] w-[27px]" />
                     </LinkWithQuery>
                 </div>
 
