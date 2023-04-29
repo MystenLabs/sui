73e80d54b8faa80b98b2d78b83024c8892558b29
Michael Amos <michael@mystenlabs.com>
2023-04-29 16:55:49 -0700

    update logo header/footer

--- apps/explorer/src/components/footer/Footer.tsx
+++ apps/explorer/src/components/footer/Footer.tsx
@@ -1,65 +1,131 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Sui, SuiLogoTxt } from '@mysten/icons';
+import {
+    SocialDiscord24,
+    SocialLinkedin24,
+    SocialTwitter24,
+} from '@mysten/icons';
 
-import { type FooterItems, footerLinks } from './footerLinks';
+import { ReactComponent as SuiWordmark } from '../../assets/SuiWordmark.svg';
 
 import { Link } from '~/ui/Link';
 import { Text } from '~/ui/Text';
 
-function FooterLinks({ links }: { links: FooterItems }) {
+function FooterLinks() {
     return (
-        <>
-            {links.map(({ category, items }) => (
-                <div
-                    key={category}
-                    className="flex flex-col gap-y-3.5 text-left"
-                >
-                    <Text variant="captionSmall/bold" color="gray-60">
-                        {category}
+        <ul className="flex gap-8">
+            <li>
+                <Link variant="text" href="https://medium.com/mysten-labs">
+                    <Text variant="body/medium" color="steel-darker">
+                        Blog
                     </Text>
-                    <ul className="flex flex-col gap-y-3.5">
-                        {items.map(({ title, href }) => (
-                            <li key={href}>
-                                <Link variant="text" href={href}>
-                                    <Text variant="body/medium" color="white">
-                                        {title}
-                                    </Text>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ))}
-        </>
+                </Link>
+            </li>
+            <li>
+                <Link
+                    variant="text"
+                    href="https://github.com/MystenLabs/sui/blob/main/doc/paper/sui.pdf"
+                >
+                    <Text variant="body/medium" color="steel-darker">
+                        Whitepaper
+                    </Text>
+                </Link>
+            </li>
+            <li>
+                <Link variant="text" href="https://mystenlabs.com/#community">
+                    <Text variant="body/medium" color="steel-darker">
+                        Press
+                    </Text>
+                </Link>
+            </li>
+            <li>
+                <Link variant="text" href="https://docs.sui.io/">
+                    <Text variant="body/medium" color="steel-darker">
+                        Docs
+                    </Text>
+                </Link>
+            </li>
+            <li>
+                <Link variant="text" href="https://github.com/MystenLabs">
+                    <Text variant="body/medium" color="steel-darker">
+                        GitHub
+                    </Text>
+                </Link>
+            </li>
+            <li>
+                <Link variant="text" href="https://discord.gg/sui">
+                    <SocialDiscord24 />
+                </Link>
+            </li>
+            <li>
+                <Link variant="text" href="https://twitter.com/SuiNetwork">
+                    <SocialTwitter24 />
+                </Link>
+            </li>
+            <li>
+                <Link
+                    variant="text"
+                    href="https://www.linkedin.com/company/mysten-labs/"
+                >
+                    <SocialLinkedin24 />
+                </Link>
+            </li>
+        </ul>
     );
 }
 
 function Footer() {
     return (
-        <footer className="bg-gray-75 px-5 py-10 md:px-10 md:py-14">
-            <nav className="mx-auto grid grid-cols-1 gap-8 md:mx-0 md:grid-cols-4 md:gap-10 xl:w-1/2">
-                <div className="order-last mx-auto md:order-first md:mt-0">
-                    <div className="h-full space-y-2 md:flex md:flex-col md:justify-between">
-                        <div className="flex flex-row flex-nowrap items-center gap-1 text-white">
-                            <Sui className="h-[26px] w-5" />
-                            <SuiLogoTxt className="h-4 w-[26px]" />
-                        </div>
-                        <div className="mt-auto">
-                            <Text
-                                color="white"
-                                variant="pSubtitleSmall/semibold"
-                            >
-                                &copy;
-                                {`${new Date().getFullYear()} Sui. All
-                                rights reserved.`}
-                            </Text>
-                        </div>
+        <footer className="bg-gray-40 px-5 py-10 md:px-10 md:py-14">
+            <nav className="flex flex-col gap-7.5">
+                <div className="flex w-full flex-col items-center justify-between gap-7.5 md:flex-row">
+                    <div className="flex gap-2 text-hero-dark">
+                        <SuiWordmark />
                     </div>
+                    <FooterLinks />
                 </div>
-                <div className="col-span-1 grid grid-cols-4 md:col-span-3">
-                    <FooterLinks links={footerLinks} />
+
+                <div className="h-[1px] w-full bg-gray-45" />
+                <div className="flex w-full items-center justify-between">
+                    <div className="h-full space-y-2">
+                        <Text
+                            color="steel-darker"
+                            variant="pSubtitleSmall/medium"
+                        >
+                            &copy;
+                            {`${new Date().getFullYear()} Sui. All
+                                rights reserved.`}
+                        </Text>
+                    </div>
+                    <ul className="flex gap-2">
+                        <li>
+                            <Link
+                                variant="text"
+                                href="https://mystenlabs.com/legal?content=terms"
+                            >
+                                <Text
+                                    variant="pSubtitleSmall/medium"
+                                    color="steel-darker"
+                                >
+                                    Terms & Conditions
+                                </Text>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                variant="text"
+                                href="https://mystenlabs.com/legal?content=privacy"
+                            >
+                                <Text
+                                    variant="pSubtitleSmall/medium"
+                                    color="steel-darker"
+                                >
+                                    Privacy Policy
+                                </Text>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </footer>
