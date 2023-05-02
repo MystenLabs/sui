26a9626e5f73c05875a665ca668501cd057cad61
Nikhil Mattu <nikhil@mystenlabs.com>
2023-05-01 22:13:59 -0400

    cleanup

--- apps/explorer/src/components/validator-map/index.tsx
+++ apps/explorer/src/components/validator-map/index.tsx
@@ -48,6 +48,7 @@ export default function ValidatorMap({ minHeight }: Props) {
                 `http://localhost:3003/validator-map?network=testnet`,
                 {
                     method: 'GET',
+                    cache: 'no-cache'
                 }
             );
 
