c4242ee03d128f14d4ceb3f9e6287902ca75517c
mamos-mysten <122397493+mamos-mysten@users.noreply.github.com>
2023-05-04 07:22:29 -0700

    [wallet-ext] - kick onramp providers to new tab (#11728)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/pages/home/onramp/index.tsx
+++ apps/wallet/src/ui/app/pages/home/onramp/index.tsx
@@ -5,7 +5,6 @@ import { useMutation } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { OnrampIframe } from './OnrampIframe';
 import { useOnrampProviders } from './useOnrampProviders';
 import Alert from '_src/ui/app/components/alert';
 import Overlay from '_src/ui/app/components/overlay';
@@ -32,7 +31,15 @@ export function Onramp() {
         if (providers && providers.length === 0) {
             navigate('/tokens');
         }
-    }, [providers, navigate]);
+    }, [providers, navigate, mutate]);
+
+    useEffect(() => {
+        if (preferredProvider) mutate();
+    }, [preferredProvider, mutate]);
+
+    const handleClick = async () => {
+        if (onrampUrl) window.open(onrampUrl, '_blank');
+    };
 
     if (!providers || !providers.length) return null;
 
@@ -44,48 +51,42 @@ export function Onramp() {
                 navigate('/tokens');
             }}
         >
-            {onrampUrl ? (
-                <OnrampIframe url={onrampUrl} />
-            ) : (
-                <div className="w-full">
-                    <button
-                        onClick={() => mutate()}
-                        className="w-full p-6 bg-sui/10 rounded-2xl flex items-center gap-2.5 border-0 cursor-pointer"
+            <div className="w-full">
+                <button
+                    onClick={handleClick}
+                    className="w-full p-6 bg-sui/10 rounded-2xl flex items-center gap-2.5 border-0 cursor-pointer"
+                >
+                    <preferredProvider.icon />
+                    <Heading
+                        variant="heading6"
+                        weight="semibold"
+                        color="hero-dark"
                     >
-                        <preferredProvider.icon />
-                        <Heading
-                            variant="heading6"
-                            weight="semibold"
-                            color="hero-dark"
-                        >
-                            Continue with {preferredProvider.name}
-                        </Heading>
-                    </button>
+                        Continue with {preferredProvider.name}
+                    </Heading>
+                </button>
 
-                    {!!error && (
-                        <div className="mt-2">
-                            <Alert mode="warning">
-                                An unexpected error occurred. Please try again
-                                later.
-                            </Alert>
-                        </div>
-                    )}
-
-                    <div className="flex flex-col gap-4 mt-5">
-                        {otherProviders.map((provider) => (
-                            <button
-                                key={provider.key}
-                                className="block font-medium text-body text-center text-steel bg-transparent border-none cursor-pointer"
-                                onClick={() =>
-                                    setPreferredProvider(provider.key)
-                                }
-                            >
-                                Already have a {provider.name} account?
-                            </button>
-                        ))}
+                {!!error && (
+                    <div className="mt-2">
+                        <Alert mode="warning">
+                            An unexpected error occurred. Please try again
+                            later.
+                        </Alert>
                     </div>
+                )}
+
+                <div className="flex flex-col gap-4 mt-5">
+                    {otherProviders.map((provider) => (
+                        <button
+                            key={provider.key}
+                            className="block font-medium text-body text-center text-steel bg-transparent border-none cursor-pointer"
+                            onClick={() => setPreferredProvider(provider.key)}
+                        >
+                            Already have a {provider.name} account?
+                        </button>
+                    ))}
                 </div>
-            )}
+            </div>
         </Overlay>
     );
 }
