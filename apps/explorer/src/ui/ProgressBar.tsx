565267a15058b96220bbb974546f68ff943b2e06
plam-ml <127577476+plam-ml@users.noreply.github.com>
2023-05-05 14:46:01 -0700

    [Explorer]: glowing progress bar (#11116)
    ## Description 
    
    - Add glowing progress bar 
    -
    https://www.figma.com/file/kSD0qXCIDZA4KQVNepA5hR/02-Explorer?node-id=95-26657&t=gX7cw0vmupRsveiA-0
    
    
    ![animate](https://user-images.githubusercontent.com/127577476/236564187-ed9c25b2-a493-4d76-a288-c79501836c7b.gif)
    
    <img width="686" alt="Screenshot 2023-05-05 at 2 08 34 PM"
    src="https://user-images.githubusercontent.com/127577476/236569127-08cbfaba-edc9-4f59-b00c-2eeee45ffa01.png">
    
    
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    
    ---------
    
    Co-authored-by: Phong Lam <phonglam@Phongs-Laptop.attlocal.net>

--- apps/explorer/src/ui/ProgressBar.tsx
+++ apps/explorer/src/ui/ProgressBar.tsx
@@ -1,24 +1,84 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { motion } from 'framer-motion';
+import clsx from 'clsx';
+import { motion, type Variants } from 'framer-motion';
+
+const ANIMATION_START = 0.25;
+const ANIMATION_START_THRESHOLD = 20;
+
+const getProgressBarVariant = (progress: number): Variants => ({
+    initial: {
+        width: 0,
+    },
+    animate: {
+        transition: {
+            delay: ANIMATION_START,
+            duration: 0.5,
+            delayChildren: ANIMATION_START * 8,
+        },
+        width: `${progress}%`,
+    },
+});
+
+const flashPointContainerVariant: Variants = {
+    initial: { opacity: 0, left: 0 },
+    animate: {
+        left: 0,
+        opacity: 1,
+    },
+};
+
+const getFlashPointVariant = (progress: number): Variants => ({
+    initial: {
+        left: 0,
+        opacity: 80,
+    },
+    animate: {
+        left: `${progress}%`,
+        opacity: 0,
+        transition: {
+            repeatDelay: 0.5,
+            duration: 3,
+            repeat: Infinity,
+            ease: [0, 0, 0.35, 1],
+        },
+    },
+});
 
 export interface ProgressBarProps {
     progress: number;
+    animate?: boolean;
 }
 
-export function ProgressBar({ progress }: ProgressBarProps) {
+export function ProgressBar({ progress, animate }: ProgressBarProps) {
+    const isAnimated = animate && progress > ANIMATION_START_THRESHOLD;
+
     return (
-        <div className="w-full rounded-full bg-white">
+        <div className="relative w-full rounded-full bg-white">
             <motion.div
-                className="rounded-full bg-gradient-to-r from-success via-success/50 to-success py-1"
-                initial={{ width: 0 }}
-                animate={{
-                    width: `${progress}%`,
-                    type: 'spring',
-                    transition: { delay: 0.25, duration: 0.5 },
-                }}
-            />
+                variants={getProgressBarVariant(progress)}
+                className={clsx(
+                    'rounded-full py-1',
+                    isAnimated
+                        ? 'bg-success'
+                        : 'bg-gradient-to-r from-success via-success/50 to-success'
+                )}
+                initial="initial"
+                animate="animate"
+            >
+                {isAnimated && (
+                    <motion.div
+                        variants={flashPointContainerVariant}
+                        className="aboslute left-0 motion-reduce:hidden"
+                    >
+                        <motion.div
+                            variants={getFlashPointVariant(progress)}
+                            className="absolute top-1/2 z-10 h-1 w-1 -translate-y-1/2 bg-white shadow-glow"
+                        />
+                    </motion.div>
+                )}
+            </motion.div>
         </div>
     );
 }
