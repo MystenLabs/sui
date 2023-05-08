5b4437e10272692c6e649d7a62786f4c740c0677
Rahul Nair <119884990+chargarlic@users.noreply.github.com>
2023-05-08 10:00:09 -0700

    Extend Keytool for Signing with AWS KMS (#11283)
    ## Description 
    
    Extends the sui keytool to sign a transaction with a AWS KMS (only for
    secp256k1)
    - Sets up the KMS Client
    - Constructs signing request with AWS, normalizes the signature and
    compacts it
    - Converts 64byte sig to RecoverableSignature and Recovers the PublicKey
    by trying all 4 recovery ids.
    - Prints Address for Corresponding PublicKey and Serialized Signature
    
    ## Test Plan 
    
    Mock Unit Tests are a fast follow; tried with a test KMS key and
    executed corresponding transaction.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    Extend Sui Keytool to sign with AWS KMS (secp256k1)
    
    ---------
    
    Co-authored-by: Joy Wang <108701016+joyqvq@users.noreply.github.com>

--- crates/sui/src/keytool.rs
+++ crates/sui/src/keytool.rs
@@ -5,7 +5,16 @@ use bip32::DerivationPath;
 use clap::*;
 use fastcrypto::encoding::{decode_bytes_hex, Base64, Encoding, Hex};
 use fastcrypto::hash::HashFunction;
+use fastcrypto::hash::Sha256;
+use fastcrypto::secp256k1::recoverable::Secp256k1RecoverableSignature;
+use fastcrypto::secp256k1::recoverable::Secp256k1Sig;
+use fastcrypto::secp256k1::Secp256k1Signature;
 use fastcrypto::traits::KeyPair;
+use fastcrypto::traits::RecoverableSignature;
+use fastcrypto::traits::ToFromBytes;
+use fastcrypto::traits::VerifyingKey;
+use rusoto_core::Region;
+use rusoto_kms::{Kms, KmsClient, SignRequest};
 use shared_crypto::intent::{Intent, IntentMessage};
 use std::fs;
 use std::path::{Path, PathBuf};
@@ -22,6 +31,7 @@ use sui_types::multisig::{MultiSig, MultiSigPublicKey, ThresholdUnit, WeightUnit
 use sui_types::signature::GenericSignature;
 use sui_types::transaction::TransactionData;
 use tracing::info;
+
 #[cfg(test)]
 #[path = "unit_tests/keytool_tests.rs"]
 mod keytool_tests;
@@ -70,6 +80,21 @@ pub enum KeyToolCommand {
         address: SuiAddress,
         #[clap(long)]
         data: String,
+        #[clap(long)]
+        intent: Option<Intent>,
+    },
+    /// Create signature using leveraging AWS KMS. Pass in a key-id to leverage Amazon
+    /// KMS to sign a message.
+    /// Any signature commits to a [struct IntentMessage] consisting of the Base64 encoded
+    /// of the BCS serialized transaction bytes itself (the result of
+    /// [transaction builder API](https://docs.sui.io/sui-jsonrpc) and its intent. If
+    /// intent is absent, default will be used. See [struct IntentMessage] and [struct Intent]
+    /// for more details.
+    SignKMS {
+        #[clap(long)]
+        data: String,
+        #[clap(long)]
+        keyid: String,
         #[clap(long)]
         intent: Option<Intent>,
     },
@@ -156,7 +181,7 @@ pub enum KeyToolCommand {
 }
 
 impl KeyToolCommand {
-    pub fn execute(self, keystore: &mut Keystore) -> Result<(), anyhow::Error> {
+    pub async fn execute(self, keystore: &mut Keystore) -> Result<(), anyhow::Error> {
         match self {
             KeyToolCommand::Generate {
                 key_scheme,
@@ -351,6 +376,86 @@ impl KeyToolCommand {
                         }
                     }
                 }
+            }
+            KeyToolCommand::SignKMS {
+                data,
+                keyid,
+                intent,
+            } => {
+                // Currently only supports secp256k1 keys
+                println!("Raw tx_bytes to execute: {}", data);
+                let intent = intent.unwrap_or_else(Intent::sui_transaction);
+                println!("Intent: {:?}", intent);
+                let msg: TransactionData =
+                    bcs::from_bytes(&Base64::decode(&data).map_err(|e| {
+                        anyhow!("Cannot deserialize data as TransactionData {:?}", e)
+                    })?)?;
+                let intent_msg = IntentMessage::new(intent, msg);
+                println!(
+                    "Raw intent message: {:?}",
+                    Base64::encode(bcs::to_bytes(&intent_msg)?)
+                );
+                let mut hasher = DefaultHash::default();
+                hasher.update(bcs::to_bytes(&intent_msg)?);
+                let digest = hasher.finalize().digest;
+                println!("Digest to sign: {:?}", Base64::encode(digest));
+
+                // Set up the KMS client in default region.
+                let region: Region = Region::default();
+                let kms: KmsClient = KmsClient::new(region);
+
+                // Construct the signing request.
+                let request: SignRequest = SignRequest {
+                    key_id: keyid.to_string(),
+                    message: digest.to_vec().into(),
+                    message_type: Some("RAW".to_string()),
+                    signing_algorithm: "ECDSA_SHA_256".to_string(),
+                    ..Default::default()
+                };
+
+                // Sign the message, normalize the signature and then compacts it
+                // serialize_compact is loaded as bytes for Secp256k1Sinature
+                // to convert it into a RecoverableSignature
+                let response = kms.sign(request).await?;
+                let sig_bytes_der = response
+                    .signature
+                    .map(|b| b.to_vec())
+                    .expect("Requires Asymmetric Key Generated in KMS");
+
+                let mut external_sig = Secp256k1Sig::from_der(&sig_bytes_der)?;
+                external_sig.normalize_s();
+                let sig_compact = external_sig.serialize_compact();
+                let sig = Secp256k1Signature::from_bytes(&sig_compact)?;
+
+                // Covert a 64-byte signature to 65-byte signature recoverable signature.
+                // by trying all 4 possible recovery ids as the last byte. The first 64
+                // bytes are copied over.
+                let mut n_bytes: [u8; 65] = [0u8; 65];
+                n_bytes[..64].copy_from_slice(&sig_compact[..]);
+
+                for i in 0..4 {
+                    n_bytes[64] = i;
+                    let sig_r = Secp256k1RecoverableSignature::from_bytes(&n_bytes)?;
+                    let pk = sig_r.recover_with_hash::<Sha256>(digest.as_ref())?;
+
+                    // If the recovered pk is valid, output serialized signature.
+                    if pk.verify(digest.as_ref(), &sig).is_ok() {
+                        println!(
+                            "Address For Corresponding KMS Key: {}",
+                            Into::<SuiAddress>::into(&pk),
+                        );
+                        let mut serialized_sig = vec![SignatureScheme::Secp256k1.flag()];
+                        serialized_sig.extend_from_slice(&sig_compact);
+                        serialized_sig.extend_from_slice(pk.as_ref());
+                        let serialized_sig = Base64::encode(&serialized_sig);
+                        println!(
+                            "Serialized signature (`flag || sig || pk` in Base64): {:?}",
+                            serialized_sig
+                        );
+                        return Ok(());
+                    }
+                }
+                println!("Invalid signature to recover secp256k1 public key");
             }
             KeyToolCommand::MultiSigAddress {
                 threshold,
