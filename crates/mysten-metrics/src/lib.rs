14fda8717e288560939fa7baac5ee65262fb875d
longbowlu <zl9394@gmail.com>
2023-05-09 11:06:03 -0700

    expose chain identifier as a metric

--- crates/mysten-metrics/src/lib.rs
+++ crates/mysten-metrics/src/lib.rs
@@ -309,8 +309,11 @@ impl RegistryService {
 
 /// Create a metric that measures the uptime from when this metric was constructed.
 /// The metric is labeled with the provided 'version' label (this should generally be of the
-/// format: 'semver-gitrevision').
-pub fn uptime_metric(version: &'static str) -> Box<dyn prometheus::core::Collector> {
+/// format: 'semver-gitrevision') and the provided 'chain_identifier' label.
+pub fn uptime_metric(
+    version: &'static str,
+    chain_identifier: &str,
+) -> Box<dyn prometheus::core::Collector> {
     let opts = prometheus::opts!("uptime", "uptime of the node service in seconds")
         .variable_label("version");
 
@@ -320,7 +323,7 @@ pub fn uptime_metric(version: &'static str) -> Box<dyn prometheus::core::Collect
         opts,
         prometheus_closure_metric::ValueType::Counter,
         uptime,
-        &[version],
+        &[version, chain_identifier],
     )
     .unwrap();
 
