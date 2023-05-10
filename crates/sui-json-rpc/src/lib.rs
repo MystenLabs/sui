19f95294a6013cbae09a48b6d4d01aa88530694b
Ronny Roland <ronmilton@mystenlabs.com>
2023-05-10 13:54:05 -0600

    Updating RPC spec description to include url

--- crates/sui-json-rpc/src/lib.rs
+++ crates/sui-json-rpc/src/lib.rs
@@ -66,7 +66,7 @@ pub fn sui_rpc_doc(version: &str) -> Project {
     Project::new(
         version,
         "Sui JSON-RPC",
-        "Sui JSON-RPC API for interaction with Sui Full node.",
+        "Sui JSON-RPC API for interaction with Sui Full node. Make RPC calls using https://fullnode.NETWORK.sui.io:443, where NETWORK is the network you want to use (testnet, devnet, mainnet). By default, local networks use port 9000.",
         "Mysten Labs",
         "https://mystenlabs.com",
         "build@mystenlabs.com",
