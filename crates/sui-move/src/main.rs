6bd3e96a18377b2a190061d32d3694a335927c2f
Xun Li <lxfind@gmail.com>
2023-05-16 10:21:15 -0700

    Revert git revision refactoring

--- crates/sui-move/src/main.rs
+++ crates/sui-move/src/main.rs
@@ -8,8 +8,24 @@ use colored::Colorize;
 use move_package::BuildConfig as MoveBuildConfig;
 use sui_move::execute_move_command;
 use sui_types::exit_main;
-use sui_types::software_version::VERSION;
 use tracing::debug;
+
+const GIT_REVISION: &str = {
+    if let Some(revision) = option_env!("GIT_REVISION") {
+        revision
+    } else {
+        let version = git_version::git_version!(
+            args = ["--always", "--dirty", "--exclude", "*"],
+            fallback = ""
+        );
+
+        if version.is_empty() {
+            panic!("unable to query git revision");
+        }
+        version
+    }
+};
+const VERSION: &str = const_str::concat!(env!("CARGO_PKG_VERSION"), "-", GIT_REVISION);
 
 #[derive(Parser)]
 #[clap(
