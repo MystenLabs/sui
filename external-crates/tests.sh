5d067e350bff0ddf35da8f03d9d5bf9dad0d339a
ade <ade@mystenlabs.com>
2023-05-20 18:03:26 -0400

    fixup

--- external-crates/tests.sh
+++ external-crates/tests.sh
@@ -2,4 +2,4 @@
 echo "Running Move tests in external-crates"
 cd move
 echo "Excluding prover and evm Move tests"
-cargo nextest run -E '!package(move-to-yul) and !package(move-prover) and !package(evm-exec-utils) and !test(prove) and !test(run_all::simple_build_with_docs/args.txt) and !test(run_test::nested_deps_bad_parent/Move.toml) and !test(run_test::external/Move.toml) and !test(run_test::external_dev_dep/Move.toml) and !test(run_test::reference_safety/call_function_with_many_acquires)' --workspace --no-fail-fast
+cargo nextest run -E '!package(move-to-yul) and !package(move-prover) and !package(evm-exec-utils) and !test(prove) and !test(run_all::simple_build_with_docs/args.txt) and !test(run_test::nested_deps_bad_parent/Move.toml)' --workspace --no-fail-fast
