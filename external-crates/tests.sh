34b7cd0e0e9d242e38f0f2b11a2e7b3004c6314e
ade <ade@mystenlabs.com>
2023-05-20 16:36:58 -0400

    exclude evm tests

--- external-crates/tests.sh
+++ external-crates/tests.sh
@@ -2,4 +2,4 @@
 echo "Running Move tests in external-crates"
 cd move
 echo "Excluding prover and evm Move tests"
-cargo nextest run -E '!package(move-to-yul) and !package(move-prover) and !test(prove) and !test(simple_build_with_docs)' --workspace --no-fail-fast
+cargo nextest run -E '!package(move-to-yul) and !package(move-prover) and !package(evm-exec-utils) and !test(prove) and !test(simple_build_with_docs)' --workspace --no-fail-fast
