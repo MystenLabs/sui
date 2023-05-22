0197b61b4396bda2ac16584222f6e509de355652
Nikhil Mattu <nikhil@mystenlabs.com>
2023-05-22 16:03:05 -0700

    gap

--- apps/explorer/src/components/HomeMetrics/OnTheNetwork.tsx
+++ apps/explorer/src/components/HomeMetrics/OnTheNetwork.tsx
@@ -17,8 +17,8 @@ export function OnTheNetwork() {
 
     return (
         <Card bg="lightBlue" spacing="lg" height="full">
-            <div className="flex flex-col gap-4 md:flex-row">
-                <div className="mr-5 flex-1 flex-col">
+            <div className="flex flex-col gap-4 md:flex-row gap-5">
+                <div className="flex-1 flex-col">
                     <div className="flex items-center gap-2">
                         <Text color="steel-darker" variant="caption/semibold">
                             On the Network
@@ -50,7 +50,7 @@ export function OnTheNetwork() {
                     </div>
                 </div>
 
-                <div className="mr-5 flex-1 flex-col">
+                <div className="flex-1 flex-col gap-5">
                     <div className="flex items-center gap-2">
                         <Text color="steel-darker" variant="caption/semibold">
                             Accounts
