a4ea2aece10d58b6a0d32468bacc2c8bedeb5579
Will Yang <willyang@mystenlabs.com>
2023-05-21 21:59:17 -0700

    clippy is so annoying

--- crates/sui-json-rpc/src/metrics.rs
+++ crates/sui-json-rpc/src/metrics.rs
@@ -204,7 +204,7 @@ impl Logger for MetricsLogger {
     fn on_result(
         &self,
         method_name: &str,
-        success: bool,
+        _success: bool,
         error_code: Option<i32>,
         started_at: Self::Instant,
         _transport: TransportProtocol,
