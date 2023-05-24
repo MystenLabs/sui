d349dd24c599e5e6e63f0d215ab4f1e66e4b6ad7
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2023-05-24 11:48:52 -0400

    indexer: epoch population with local object store (#12181)
    ## Description 
    
    Prev. b/c indexer temp. disabled object content population, it had to
    fetch FN for epoch data, however FN does not keep historic system state
    data, indexer would always skip old epochs and started from latest.
    
    After recent PRs, indexer now re-populates local object store, thus
    indexer epoch processing can use the local epoch store now.
    
    ## Test Plan 
    
    
    Tested locally and make sure that even following a FN that has been
    updated, indexer populates epoch starting from 0
    ```
    cargo run --bin sui-indexer -- --db-url "postgres://postgres:postgres@localhost/gegao" --rpc-client-url http://lax-mnt-rpc-12.mainnet.sui.io:9000 --fullnode-sync-worker --rpc-server-url 127.0.0.1 --rpc-server-port 3030 --reset-db
    ```
    
    ![Screenshot 2023-05-24 at 8 45 36
    AM](https://github.com/MystenLabs/sui/assets/106119108/8bfb233e-ebdc-4e40-b096-9c9d1572ca96)
    
    
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-indexer/src/handlers/checkpoint_handler.rs
+++ crates/sui-indexer/src/handlers/checkpoint_handler.rs
@@ -18,7 +18,7 @@ use tracing::{error, info, warn};
 
 use mysten_metrics::spawn_monitored_task;
 use sui_core::event_handler::SubscriptionHandler;
-use sui_json_rpc::api::{GovernanceReadApiClient, ReadApiClient};
+use sui_json_rpc::api::ReadApiClient;
 use sui_json_rpc_types::{
     OwnedObjectRef, SuiGetPastObjectRequest, SuiObjectData, SuiObjectDataOptions, SuiRawData,
     SuiTransactionBlockDataAPI, SuiTransactionBlockEffects, SuiTransactionBlockEffectsAPI,
@@ -28,6 +28,7 @@ use sui_types::base_types::{ObjectID, SequenceNumber};
 use sui_types::committee::EpochId;
 use sui_types::messages_checkpoint::{CheckpointCommitment, CheckpointSequenceNumber};
 use sui_types::sui_system_state::sui_system_state_summary::SuiSystemStateSummary;
+use sui_types::sui_system_state::{get_sui_system_state, SuiSystemStateTrait};
 use sui_types::transaction::SenderSignedData;
 use sui_types::SUI_SYSTEM_ADDRESS;
 
@@ -883,19 +884,9 @@ where
         if index_epoch {
             epoch_index = if checkpoint.epoch == 0 && checkpoint.sequence_number == 0 {
                 // very first epoch
-                // NOTE: tmp. using latest system state to save storage on indexer.
-                // let system_state = get_sui_system_state(data)?;
-                // let system_state: SuiSystemStateSummary = system_state.into_sui_system_state_summary();
-                let system_state: SuiSystemStateSummary = self
-                    .http_client
-                    .get_latest_sui_system_state()
-                    .await
-                    .map_err(|e| {
-                        IndexerError::FullNodeReadingError(format!(
-                            "Failed to get latest system state with error {:?}",
-                            e
-                        ))
-                    })?;
+                let system_state = get_sui_system_state(data)?;
+                let system_state: SuiSystemStateSummary =
+                    system_state.into_sui_system_state_summary();
                 let validators = system_state
                     .active_validators
                     .iter()
@@ -914,21 +905,9 @@ where
                     validators,
                 })
             } else if let Some(end_of_epoch_data) = &checkpoint.end_of_epoch_data {
-                // Find system state object
-                // NOTE: tmp. using latest system state to save storage on indexer.
-                // let system_state = get_sui_system_state(data)?;
-                // let system_state: SuiSystemStateSummary = system_state.into_sui_system_state_summary();
-                let system_state: SuiSystemStateSummary = self
-                    .http_client
-                    .get_latest_sui_system_state()
-                    .await
-                    .map_err(|e| {
-                        IndexerError::FullNodeReadingError(format!(
-                            "Failed to get latest system state with error {:?}",
-                            e
-                        ))
-                    })?;
-
+                let system_state = get_sui_system_state(data)?;
+                let system_state: SuiSystemStateSummary =
+                    system_state.into_sui_system_state_summary();
                 let epoch_event = transactions.iter().find_map(|tx| {
                     tx.events.data.iter().find(|ev| {
                         ev.type_.address == SUI_SYSTEM_ADDRESS
