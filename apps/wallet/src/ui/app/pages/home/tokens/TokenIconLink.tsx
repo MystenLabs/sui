a11acb97cddd761f79b78c32ad22bb21bedf7a53
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-05-25 18:03:54 -0400

    change text to Stake and Earn SUI

--- apps/wallet/src/ui/app/pages/home/tokens/TokenIconLink.tsx
+++ apps/wallet/src/ui/app/pages/home/tokens/TokenIconLink.tsx
@@ -61,7 +61,7 @@ export function TokenIconLink({
                 <div>
                     {totalActivePendingStake
                         ? 'Currently Staked'
-                        : 'Stake & Earn SUI'}
+                        : 'Stake and Earn SUI'}
                 </div>
                 {!!totalActivePendingStake && (
                     <div>
