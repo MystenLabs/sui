56ea590ac62c4440a21ab17815e3b9b24b8e2e16
Rahul Nair <119884990+chargarlic@users.noreply.github.com>
2023-05-26 13:04:53 -0700

    Update download_private.sh

--- nre/download_private.sh
+++ nre/download_private.sh
@@ -13,16 +13,6 @@ commit_sha=$1
 pub_key=https://sui-private.s3.us-west-2.amazonaws.com/sui_security_release.pem
 url=https://sui-releases.s3-accelerate.amazonaws.com/$commit_sha
 
-echo "[+] Downloading docker artifacts for $commit_sha ..."
-curl $url/sui-node-docker.tar -o sui-node-docker.tar
-curl $url/sui-tools-docker.tar -o sui-tools-docker.tar
-curl $url/sui-indexer-docker.tar -o sui-indexer-docker.tar
-
-echo "[+] Verifying docker artifacts for $commit_sha ..."
-cosign verify-blob --insecure-ignore-tlog --key $pub_key --signature $url/sui-node-docker.tar.sig sui-node-docker.tar
-cosign verify-blob --insecure-ignore-tlog --key $pub_key --signature $url/sui-tools-docker.tar.sig sui-tools-docker.tar
-cosign verify-blob --insecure-ignore-tlog --key $pub_key --signature $url/sui-indexer-docker.tar.sig sui-indexer-docker.tar
-
 echo "[+] Downloading sui binaries for $commit_sha ..."
 curl $url/sui -o sui
 curl $url/sui-indexer -o sui-indexer
