33b6a528742151c7bbbb683a6f81ca77371d37c3
Phong Lam <phong@mystenlabs.com>
2023-05-30 17:42:02 -0700

    update suggestion

--- apps/wallet/tests/utils/localnet.ts
+++ apps/wallet/tests/utils/localnet.ts
@@ -3,12 +3,20 @@
 
 // eslint-disable-next-line import/order
 import 'tsconfig-paths/register';
-import { Ed25519Keypair } from '@mysten/sui.js';
+import { Ed25519Keypair, type Keypair } from '@mysten/sui.js';
 import * as bip39 from '@scure/bip39';
 import { wordlist } from '@scure/bip39/wordlists/english';
 
+export async function generateKeypairFromMnemonic(mnemonic: string) {
+    return Ed25519Keypair.deriveKeypair(mnemonic);
+}
+
 export async function generateKeypair() {
     const mnemonic = bip39.generateMnemonic(wordlist);
-    const keypair = Ed25519Keypair.deriveKeypair(mnemonic);
+    const keypair = await generateKeypairFromMnemonic(mnemonic);
     return { mnemonic, keypair };
+}
+
+export function generateAddress(keypair: Keypair) {
+    return keypair.getPublicKey().toSuiAddress();
 }
