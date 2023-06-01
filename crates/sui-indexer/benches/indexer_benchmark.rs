5269f9836e2e2d9799e788fa55d3ec7248bab2aa
Ge Gao <ge@mystenlabs.com>
2023-06-01 16:41:54 -0400

    indexer: remove dep of diesel-async etc.

--- crates/sui-indexer/benches/indexer_benchmark.rs
+++ crates/sui-indexer/benches/indexer_benchmark.rs
@@ -39,12 +39,12 @@ fn indexer_benchmark(c: &mut Criterion) {
 
     let rt: Runtime = Runtime::new().unwrap();
     let (mut checkpoints, store) = rt.block_on(async {
-        let (blocking_cp, async_cp) = new_pg_connection_pool(&db_url).await.unwrap();
+        let blocking_cp = new_pg_connection_pool(&db_url).await.unwrap();
         reset_database(&mut blocking_cp.get().unwrap(), true).unwrap();
         let registry = Registry::default();
         let indexer_metrics = IndexerMetrics::new(&registry);
 
-        let store = PgIndexerStore::new(async_cp, blocking_cp, indexer_metrics).await;
+        let store = PgIndexerStore::new(blocking_cp, indexer_metrics).await;
 
         let checkpoints = (0..150).map(create_checkpoint).collect::<Vec<_>>();
         (checkpoints, store)
