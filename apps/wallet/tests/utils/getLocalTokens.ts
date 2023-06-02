ccd05085c951d0e9dfd6df0e221cc03897b598bd
Phong Lam <phong@mystenlabs.com>
2023-06-01 23:30:42 -0700

    commented out code

--- apps/wallet/tests/utils/getLocalTokens.ts
+++ apps/wallet/tests/utils/getLocalTokens.ts
@@ -9,12 +9,12 @@ export async function getLocalTokens(page: Page) {
     /**
      * Runs this only in local environment for debugging purposes.
      */
-    if (process.env.CI !== 'true') {
-        await page.getByTestId('menu').click();
-        await page.getByTestId('Network').click();
-        await page.getByTestId('local').click();
-        await page.getByTestId('menu').click();
-    }
+    // if (process.env.CI !== 'true') {
+    //     await page.getByTestId('menu').click();
+    //     await page.getByTestId('Network').click();
+    //     await page.getByTestId('local').click();
+    //     await page.getByTestId('menu').click();
+    // }
 
     await page.getByTestId('faucet-request-button').click();
     await expect(page.getByTestId('coin-balance')).toHaveText('1,000SUI');
