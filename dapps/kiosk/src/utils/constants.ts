2a561137d6dd74bb7db117bbba86974801d99b44
Manolis Liolios <manos@mystenlabs.com>
2023-06-05 20:26:09 +0300

    add copyright

--- dapps/kiosk/src/utils/constants.ts
+++ dapps/kiosk/src/utils/constants.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 export const TANSTACK_OWNED_KIOSK_KEY = 'TANSTACK_OWNED_KIOSK_KEY';
 export const TANSTACK_KIOSK_KEY = 'TANSTACK_KIOSK_KEY';
 export const TANSTACK_KIOSK_DATA_KEY = 'TANSTACK_KIOSK_DATA_KEY';
