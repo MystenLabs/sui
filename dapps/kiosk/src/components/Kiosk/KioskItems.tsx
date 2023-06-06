8eeebd32fd5d8cec38935f80e6a2d6bdd57cab46
Manolis Liolios <manos@mystenlabs.com>
2023-06-06 20:11:17 +0300

    [Kiosk] Demo dapp (#12335)
    ## Description 
    
    A react dapp Demo for kiosk. It is a nice demonstration for the [Kiosk
    SDK](https://github.com/mystenlabs/sui/tree/main/sdk/kiosk).
    It allows a wide variety of functionality:
    
    - Fetching user's kiosk (and allowing to create one if the address
    doesn't have one).
    - Manage kiosk (withdraw profits, place items, list items for sale, take
    items from kiosk).
    - Visit a kiosk, visualises the kiosk's contents.
    - Allows to purchase an item that's listed in a kiosk. Resolves all
    transfer policy rules based on `@mysten/kiosk` package.
    
    This is a demonstration app. It doesn't leverage caching (does many RPC
    calls), doesn't allow a selection of kiosk (if a user has > 1, defaults
    to first that's returned), doesn't have tests, and the UX could be
    improved in some cases.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ dapps/kiosk/src/components/Kiosk/KioskItems.tsx
@@ -0,0 +1,94 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useEffect, useState } from 'react';
+import { KioskItem as KioskItemCmp } from './KioskItem';
+import { ListPrice } from '../Modals/ListPrice';
+import { OwnedObjectType } from '../Inventory/OwnedObjects';
+import { Loading } from '../Base/Loading';
+import { toast } from 'react-hot-toast';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useKiosk, useOwnedKiosk } from '../../hooks/kiosk';
+
+export function KioskItems({ kioskId }: { kioskId?: string }) {
+  const location = useLocation();
+
+  const { data: walletKiosk } = useOwnedKiosk();
+  const ownedKiosk = walletKiosk?.kioskId;
+
+  const isKioskPage = location.pathname.startsWith('/kiosk/');
+
+  // checks if this is an owned kiosk.
+  // We are depending on currentAccount too, as this is what triggers the `getOwnedKioskCap()` function to change
+  // using endsWith because we support it with both 0x prefix and without.
+  const isOwnedKiosk = () => {
+    return ownedKiosk?.endsWith(kioskId || '~');
+  };
+
+  const [modalItem, setModalItem] = useState<OwnedObjectType | null>(null);
+
+  const {
+    data: kioskData,
+    isLoading,
+    isError,
+    refetch: getKioskData,
+  } = useKiosk(kioskId);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isError) return;
+    toast.error(
+      'The requested kiosk was not found. You either supplied a wrong kiosk Id or the RPC call failed.',
+    );
+    navigate('/');
+  }, [navigate, isError]);
+
+  const kioskItems = kioskData?.items || [];
+  const kioskListings = kioskData?.listings || {};
+
+  if (isLoading) return <Loading />;
+
+  if (kioskItems.length === 0)
+    return <div className="py-12">The kiosk you are viewing is empty!</div>;
+
+  return (
+    <div className="mt-12">
+      {
+        // We're hiding this when we've clicked "view kiosk" for our own kiosk.
+        isOwnedKiosk() && isKioskPage && (
+          <div className="bg-yellow-300 text-black rounded px-3 py-2 mb-6">
+            You're viewing your own kiosk
+          </div>
+        )
+      }
+      <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-5">
+        {kioskId &&
+          kioskItems.map((item: OwnedObjectType) => (
+            <KioskItemCmp
+              key={item.objectId}
+              kioskId={kioskId}
+              item={item}
+              isGuest={!isOwnedKiosk()}
+              onSuccess={() => {
+                getKioskData();
+              }}
+              listing={kioskListings && kioskListings[item.objectId]}
+              listFn={(item: OwnedObjectType) => setModalItem(item)}
+            />
+          ))}
+        {modalItem && (
+          <ListPrice
+            item={modalItem}
+            onSuccess={() => {
+              toast.success('Item listed successfully.');
+              getKioskData(); // replace with single kiosk Item search here and replace
+              setModalItem(null); // replace modal.
+            }}
+            closeModal={() => setModalItem(null)}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
