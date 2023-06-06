9d4a9f0f680ea856f6c1a39e137a2c719ade979a
Manolis Liolios <manos@mystenlabs.com>
2023-06-06 17:59:06 +0300

    remove redirection to home after logout now that we use tanstack

--- dapps/kiosk/src/components/Base/SuiConnectButton.tsx
+++ dapps/kiosk/src/components/Base/SuiConnectButton.tsx
@@ -1,20 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ConnectButton, useWalletKit } from '@mysten/wallet-kit';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { ConnectButton } from '@mysten/wallet-kit';
 
 export function SuiConnectButton() {
-  const { currentAccount } = useWalletKit();
-  // redirect user to home page if they switch accounts / disconnect, to keep state management easier.
-  // In a real dapp scenario, we'd want to use tanstack or an app state solution
-  // to keep track of these and refetch owned kiosk.
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate('/');
-  }, [currentAccount?.address]);
-
   return (
     <ConnectButton
       style={{
