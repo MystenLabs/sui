6a2a42d779d41696481d91f12deb01cd5ab92769
Manolis Liolios <manos@mystenlabs.com>
2023-06-12 22:23:58 +0300

    [Kiosk SDK] Adds `getOwnedKiosks` query (#12423)
    ## Description 
    
    - Adds `getOwnedKiosks` to Kiosk SDK to simplify finding kiosks owned by
    an address.
    - Moves some types to the correct place.
    - Updates the kiosk demo to reflect these changes.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sdk/kiosk/src/query/kiosk.ts
+++ sdk/kiosk/src/query/kiosk.ts
@@ -4,9 +4,12 @@
 import {
   JsonRpcProvider,
   ObjectId,
-  ObjectType,
   PaginationArguments,
   SuiAddress,
+  SuiObjectData,
+  SuiObjectResponse,
+  getObjectFields,
+  isValidSuiAddress,
 } from '@mysten/sui.js';
 import {
   attachListingsAndPrices,
@@ -15,62 +18,13 @@ import {
   getAllDynamicFields,
   getKioskObject,
 } from '../utils';
-import { Kiosk } from '../types';
-
-/**
- * A dynamic field `Listing { ID, isExclusive }` attached to the Kiosk.
- * Holds a `u64` value - the price of the item.
- */
-export type KioskListing = {
-  /** The ID of the Item */
-  objectId: ObjectId;
-  /**
-   * Whether or not there's a `PurchaseCap` issued. `true` means that
-   * the listing is controlled by some logic and can't be purchased directly.
-   *
-   * TODO: consider renaming the field for better indication.
-   */
-  isExclusive: boolean;
-  /** The ID of the listing */
-  listingId: ObjectId;
-  price?: string;
-};
-
-/**
- * A dynamic field `Item { ID }` attached to the Kiosk.
- * Holds an Item `T`. The type of the item is known upfront.
- */
-export type KioskItem = {
-  /** The ID of the Item */
-  objectId: ObjectId;
-  /** The type of the Item */
-  type: ObjectType;
-  /** Whether the item is Locked (there must be a `Lock` Dynamic Field) */
-  isLocked: boolean;
-  /** Optional listing */
-  listing?: KioskListing;
-};
-/**
- * Aggregated data from the Kiosk.
- */
-export type KioskData = {
-  items: KioskItem[];
-  itemIds: ObjectId[];
-  listingIds: ObjectId[];
-  kiosk?: Kiosk;
-  extensions: any[]; // type will be defined on later versions of the SDK.
-};
-
-export type PagedKioskData = {
-  data: KioskData;
-  nextCursor: string | null;
-  hasNextPage: boolean;
-};
-
-export type FetchKioskOptions = {
-  withKioskFields?: boolean;
-  withListingPrices?: boolean;
-};
+import {
+  FetchKioskOptions,
+  KIOSK_OWNER_CAP,
+  KioskListing,
+  OwnedKiosks,
+  PagedKioskData,
+} from '../types';
 
 export async function fetchKiosk(
   provider: JsonRpcProvider,
@@ -117,5 +71,60 @@ export async function fetchKiosk(
     data: kioskData,
     nextCursor: null,
     hasNextPage: false,
+  };
+}
+
+/**
+ * A function to fetch all the user's kiosk Caps
+ * And a list of the kiosk address ids.
+ * Returns a list of `kioskOwnerCapIds` and `kioskIds`.
+ * Extra options allow pagination.
+ */
+export async function getOwnedKiosks(
+  provider: JsonRpcProvider,
+  address: SuiAddress,
+  options?: {
+    pagination?: PaginationArguments<string>;
+  },
+): Promise<OwnedKiosks> {
+  if (!isValidSuiAddress(address))
+    return {
+      nextCursor: null,
+      hasNextPage: false,
+      kioskOwnerCaps: [],
+      kioskIds: [],
+    };
+
+  // fetch owned kiosk caps, paginated.
+  const { data, hasNextPage, nextCursor } = await provider.getOwnedObjects({
+    owner: address,
+    filter: { StructType: KIOSK_OWNER_CAP },
+    options: {
+      showContent: true,
+    },
+    ...(options?.pagination || {}),
+  });
+
+  // get kioskIds from the OwnerCaps.
+  const kioskIdList = data?.map(
+    (x: SuiObjectResponse) => getObjectFields(x)?.for,
+  );
+
+  // clean up data that might have an error in them.
+  // only return valid objects.
+  const filteredData = data
+    .filter((x) => 'data' in x)
+    .map((x) => x.data) as SuiObjectData[];
+
+  return {
+    nextCursor,
+    hasNextPage,
+    kioskOwnerCaps: filteredData.map((x, idx) => ({
+      digest: x.digest,
+      version: x.version,
+      objectId: x.objectId,
+      kioskId: kioskIdList[idx],
+    })),
+    kioskIds: kioskIdList,
   };
 }
