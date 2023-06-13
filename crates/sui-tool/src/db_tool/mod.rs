aa471b7ae6b517623ff65fea41a58d55d49fe371
phoenix <51927076+phoenix-o@users.noreply.github.com>
2023-06-13 21:15:11 +0200

    [pruner] remove point delete option (#12462)
    remove point delete method from the pruner

--- crates/sui-tool/src/db_tool/mod.rs
+++ crates/sui-tool/src/db_tool/mod.rs
@@ -310,7 +310,6 @@ pub fn reset_db_to_genesis(path: &Path) -> anyhow::Result<()> {
     //   num-epochs-to-retain: 18446744073709551615
     //   max-checkpoints-in-batch: 10
     //   max-transactions-in-batch: 1000
-    //   use-range-deletion: true
     let perpetual_db = AuthorityPerpetualTables::open_tables_read_write(
         path.join("store").join("perpetual"),
         MetricConf::default(),
