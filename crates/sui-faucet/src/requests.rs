3c4a908706d6e8d608a8c0f220e08943ec366751
Jian Lu <123408603+healthydeve@users.noreply.github.com>
2023-06-14 11:17:22 -0700

    [Faucet] Adding batch request to sui faucet (#12257)
    ## Description 
    
    This PR adds the `/v1/gas` api to the faucet which is the batch send
    request. The request gets put into a MCSP queue which another thread
    pulls routinely from to serve up to 500 paysui transactions at once.
    
    This also adds the `/v1/status` api to the faucet which checks on the
    status of the request based on a Uuid that is generated from the initial
    gas request. The status of any given Uuid is cached for 5 minutes after
    the request completes, and if it is deleted the user will get nothing
    back.
    
    Adding batch request on faucet: 
    
    <img width="1243" alt="image"
    src="https://github.com/MystenLabs/sui/assets/123408603/bfa62f4a-8d14-4cf6-b17a-91bbb9c54378">
    
    In Progress:
    
    ![image](https://github.com/MystenLabs/sui/assets/123408603/f6546c54-774f-44dd-b07d-a2a30d4df777)
    
    Success:
    
    ![image](https://github.com/MystenLabs/sui/assets/123408603/8ac43331-c4ae-49b1-97cd-12bd6bd7e39f)
    
    Follow up items:
    Change wallet to use new API
    
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    Local Testing / CI / CD / Will redirect discord devnet faucuet, and then
    testnet faucet onto this endpoint
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-faucet/src/requests.rs
+++ crates/sui-faucet/src/requests.rs
@@ -7,6 +7,7 @@ use sui_types::base_types::SuiAddress;
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum FaucetRequest {
     FixedAmountRequest(FixedAmountRequest),
+    GetBatchSendStatusRequest(GetBatchSendStatusRequest),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -14,10 +15,21 @@ pub struct FixedAmountRequest {
     pub recipient: SuiAddress,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct GetBatchSendStatusRequest {
+    pub task_id: String,
+}
+
 impl FaucetRequest {
     pub fn new_fixed_amount_request(recipient: impl Into<SuiAddress>) -> Self {
         Self::FixedAmountRequest(FixedAmountRequest {
             recipient: recipient.into(),
+        })
+    }
+
+    pub fn new_get_batch_send_status_request(task_id: impl Into<String>) -> Self {
+        Self::GetBatchSendStatusRequest(GetBatchSendStatusRequest {
+            task_id: task_id.into(),
         })
     }
 }
