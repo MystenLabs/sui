8cdb71db1d6315034befe6b81b8047121810d54a
phoenix <alexo@mystenlabs.com>
2023-06-19 11:44:47 +0200

    periodic compaction for effects

--- crates/sui-core/src/authority/authority_store_pruner.rs
+++ crates/sui-core/src/authority/authority_store_pruner.rs
@@ -5,13 +5,14 @@ use crate::authority::authority_store_types::{ObjectContentDigest, StoreData, St
 use crate::checkpoints::{CheckpointStore, CheckpointWatermark};
 use anyhow::anyhow;
 use mysten_metrics::{monitored_scope, spawn_monitored_task};
+use once_cell::sync::Lazy;
 use prometheus::{
     register_int_counter_with_registry, register_int_gauge_with_registry, IntCounter, IntGauge,
     Registry,
 };
 use rocksdb::LiveFile;
 use std::cmp::{max, min};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::time::SystemTime;
 use std::{sync::Arc, time::Duration};
 use sui_config::node::AuthorityStorePruningConfig;
@@ -34,6 +35,19 @@ use typed_store::{Map, TypedStoreError};
 
 use super::authority_store_tables::AuthorityPerpetualTables;
 
+static PERIODIC_PRUNING_TABLES: Lazy<HashSet<String>> = Lazy::new(|| {
+    [
+        "objects",
+        "effects",
+        "transactions",
+        "events",
+        "executed_effects",
+        "executed_transactions_to_checkpoint",
+    ]
+    .into_iter()
+    .map(|cf| cf.to_string())
+    .collect()
+});
 pub struct AuthorityStorePruner {
     _objects_pruner_cancel_handle: oneshot::Sender<()>,
 }
@@ -436,8 +450,8 @@ impl AuthorityStorePruner {
         for sst_file in perpetual_db.objects.rocksdb.live_files()? {
             let file_path = db_path.join(sst_file.name.clone().trim_matches('/'));
             let last_modified = std::fs::metadata(file_path)?.modified()?;
-            if sst_file.column_family_name != "objects"
-                || sst_file.level < 2
+            if !PERIODIC_PRUNING_TABLES.contains(&sst_file.column_family_name)
+                || sst_file.level < 1
                 || sst_file.start_key.is_none()
                 || sst_file.end_key.is_none()
                 || last_modified > time_threshold
@@ -456,9 +470,11 @@ impl AuthorityStorePruner {
                 "Manual compaction of sst file {:?}. Size: {:?}, level: {:?}",
                 sst_file.name, sst_file.size, sst_file.level
             );
-            let start_key: ObjectKey = bcs::from_bytes(sst_file.start_key.as_ref().unwrap())?;
-            let end_key: ObjectKey = bcs::from_bytes(sst_file.end_key.as_ref().unwrap())?;
-            perpetual_db.objects.compact_range(&start_key, &end_key)?;
+            perpetual_db.objects.compact_range_raw(
+                &sst_file.column_family_name,
+                sst_file.start_key.unwrap(),
+                sst_file.end_key.unwrap(),
+            )?;
         }
         Ok(())
     }
