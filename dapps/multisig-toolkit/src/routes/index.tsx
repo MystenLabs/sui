8c5d012633bf2ca2a7ffb80eba6c85d1eac64bf8
Jordan Gensler <jordan@mystenlabs.com>
2023-06-20 13:39:38 -0700

    Multisig toolkit

---
+++ dapps/multisig-toolkit/src/routes/index.tsx
@@ -0,0 +1,23 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import OfflineSigner from './offline-signer';
+import { Root } from './root';
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+		children: [
+			{
+				path: '/',
+				element: <Navigate to="offline-signer" replace />,
+			},
+			{
+				path: 'offline-signer',
+				element: <OfflineSigner />,
+			},
+		],
+	},
+]);
--- dapps/multisig-toolkit/vite.config.ts
+++ dapps/multisig-toolkit/vite.config.ts
@@ -3,10 +3,11 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
 	resolve: {
 		conditions: ['source'],
 	},
