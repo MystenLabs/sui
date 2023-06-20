9a2bcbed6f87d6783e73724ed47c93aee32b8ccd
Mingwei Tian <mingwei@mystenlabs.com>
2023-06-19 18:26:21 -0700

    [Narwhal] add more logging to proposer

--- narwhal/primary/src/proposer.rs
+++ narwhal/primary/src/proposer.rs
@@ -419,10 +419,16 @@ impl Proposer {
             let enough_digests = self.digests.len() >= self.header_num_of_batches_threshold;
             let max_delay_timed_out = max_delay_timer.is_elapsed();
             let min_delay_timed_out = min_delay_timer.is_elapsed();
+            let should_create_header = (max_delay_timed_out
+                || ((enough_digests || min_delay_timed_out) && advance))
+                && enough_parents;
 
-            if (max_delay_timed_out || ((enough_digests || min_delay_timed_out) && advance))
-                && enough_parents
-            {
+            debug!(
+                "Proposer loop starts: round={} enough_parents={} enough_digests={} advance={} max_delay_timed_out={} min_delay_timed_out={} should_create_header={}", 
+                self.round, enough_parents, enough_digests, advance, max_delay_timed_out, min_delay_timed_out, should_create_header
+            );
+
+            if should_create_header {
                 if max_delay_timed_out {
                     // It is expected that this timer expires from time to time. If it expires too often, it
                     // either means some validators are Byzantine or that the network is experiencing periods
@@ -434,6 +440,8 @@ impl Proposer {
                 // Advance to the next round.
                 self.round += 1;
                 let _ = self.tx_narwhal_round_updates.send(self.round);
+
+                debug!("Proposer advanced to round {}", self.round);
 
                 // Update the metrics
                 self.metrics.current_round.set(self.round as i64);
@@ -486,9 +494,13 @@ impl Proposer {
                 continue;
             }
 
+            debug!("Proposer waiting for update: round={}", self.round);
+
             tokio::select! {
 
                 () = &mut header_repeat_timer => {
+                    debug!("Proposer header_repeat_timer, round={}", self.round);
+
                     // If the round has not advanced within header_resend_timeout then try to
                     // re-process our own header.
                     if let Some(header) = &opt_latest_header {
@@ -505,6 +517,8 @@ impl Proposer {
                 }
 
                 Some((commit_round, commit_headers)) = self.rx_committed_own_headers.recv() => {
+                    debug!("Proposer committed own header, round={}", self.round);
+
                     // Remove committed headers from the list of pending
                     let mut max_committed_round = 0;
                     for round in commit_headers {
@@ -561,6 +575,8 @@ impl Proposer {
                 },
 
                 Some((parents, round, epoch)) = self.rx_parents.recv() => {
+                    debug!("Proposer received parents, round={} parent.round={} num_parents={}", self.round, round, parents.len());
+
                     // If the core already moved to the next epoch we should pull the next
                     // committee as well.
 
@@ -611,6 +627,7 @@ impl Proposer {
                                 .reset(timer_start);
                         },
                         Ordering::Less => {
+                            debug!("Proposer ignoring older parents, round={} parent.round={}", self.round, round);
                             // Ignore parents from older rounds.
                             continue;
                         },
@@ -634,6 +651,7 @@ impl Proposer {
                     } else {
                         false
                     };
+                    debug!("Proposer advance={} round={}", advance, self.round);
 
                     let round_type = if self.round % 2 == 0 {
                         "even"
@@ -649,6 +667,8 @@ impl Proposer {
 
                 // Receive digests from our workers.
                 Some(mut message) = self.rx_our_digests.recv() => {
+                    debug!("Proposer received digest, round={}", self.round);
+
                     // Signal back to the worker that the batch is recorded on the
                     // primary, and will be tracked until inclusion. This means that
                     // if the primary does not fail it will attempt to send the digest
@@ -661,9 +681,11 @@ impl Proposer {
 
                 // Check whether any timer expired.
                 () = &mut max_delay_timer, if !max_delay_timed_out => {
+                    debug!("Proposer reached max_delay_timer, round={}", self.round);
                     // Continue to next iteration of the loop.
                 }
                 () = &mut min_delay_timer, if !min_delay_timed_out => {
+                    debug!("Proposer reached min_delay_timer, round={}", self.round);
                     // Continue to next iteration of the loop.
                 }
 
