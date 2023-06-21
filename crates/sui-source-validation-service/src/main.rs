13036d842dcda81f50b2f17d82c0cdc3f2c50dc8
Rijnard van Tonder <rvantonder@gmail.com>
2023-06-21 12:32:40 -0700

    source service config

--- crates/sui-source-validation-service/src/main.rs
+++ crates/sui-source-validation-service/src/main.rs
@@ -1,15 +1,26 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::path::PathBuf;
+
+use clap::Parser;
+
 use sui_config::{sui_config_dir, SUI_CLIENT_CONFIG};
 use sui_sdk::wallet_context::WalletContext;
-use sui_source_validation_service::{initialize, serve};
+
+use sui_source_validation_service::{initialize, parse_config, serve};
+
+#[derive(Parser, Debug)]
+struct Args {
+    config_path: PathBuf,
+}
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let config = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
-    let context = WalletContext::new(&config, None, None).await?;
-    let package_paths = vec![];
-    initialize(&context, package_paths).await?;
+pub async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let package_config = parse_config(args.config_path)?;
+    let sui_config = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
+    let context = WalletContext::new(&sui_config, None, None).await?;
+    initialize(&context, &package_config).await?;
     serve()?.await.map_err(anyhow::Error::from)
 }
