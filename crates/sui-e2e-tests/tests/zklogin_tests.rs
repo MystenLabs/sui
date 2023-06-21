a8d44090108cb586a479f68750f1a6a249bbf1fe
Xun Li <lxfind@gmail.com>
2023-06-21 10:30:08 -0700

    Use TestCluster in zklogin tests

--- crates/sui-e2e-tests/tests/zklogin_tests.rs
+++ crates/sui-e2e-tests/tests/zklogin_tests.rs
@@ -2,32 +2,23 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use sui_core::authority_client::AuthorityAPI;
+use sui_macros::sim_test;
 use sui_types::error::{SuiError, SuiResult};
 use sui_types::utils::make_zklogin_tx;
-use test_utils::authority::{spawn_test_authorities, test_authority_configs_with_objects};
-
-use sui_core::{authority_aggregator::AuthorityAggregatorBuilder, authority_client::AuthorityAPI};
-use sui_macros::sim_test;
-use sui_types::object::generate_test_gas_objects;
+use test_utils::network::TestClusterBuilder;
 
 async fn do_zklogin_test() -> SuiResult {
-    let gas_objects = generate_test_gas_objects();
-
-    // Get the authority configs and spawn them. Note that it is important to not drop
-    // the handles (or the authorities will stop).
-    let (config, _) = test_authority_configs_with_objects(gas_objects);
-    let _handles = spawn_test_authorities(&config).await;
-
+    let test_cluster = TestClusterBuilder::new().build().await;
     let (_, tx, _) = make_zklogin_tx();
 
-    let (_agg, clients) = AuthorityAggregatorBuilder::from_network_config(&config)
-        .build()
-        .unwrap();
-
-    clients
+    test_cluster
+        .authority_aggregator()
+        .authority_clients
         .values()
         .next()
         .unwrap()
+        .authority_client()
         .handle_transaction(tx)
         .await
         .map(|_| ())
