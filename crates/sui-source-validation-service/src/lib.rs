d8de3b747ba275542825d3d7dfe78067e3c88dfc
Rijnard van Tonder <rvantonder@gmail.com>
2023-06-22 13:48:05 -0700

    move: source service clone packages (#12590)
    ## Description 
    
    On start, the server clones repositories and does sparse-checkout for
    package paths as specified in config. Tasks are spawned separately for
    each repository.
    
    Example: The sequence of `git` commands for the `mystenlabs/sui` repo
    and its packages look as follows:
    
    ```
    git clone -n --depth=1 --filter=tree:0 https://github.com/mystenlabs/sui $TMPDIR
    git sparse-checkout -C $TMPDIR set --no-cone crates/sui-framework/packages/deepbook crates/sui-framework/packages/move-stdlib crates/sui-framework/packages/sui-framework crates/sui-framework/packages/sui-system
    git -C $TMPDIR checkout
    ```
    
    ## Test Plan 
    
    Check that we produce `git` clone and checkout commands faithfully
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-source-validation-service/src/lib.rs
+++ crates/sui-source-validation-service/src/lib.rs
@@ -2,12 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::{
+    ffi::OsString,
     fs,
     path::{Path, PathBuf},
+    process::Command,
 };
 
 use actix_web::{dev::Server, web, App, HttpRequest, HttpServer, Responder};
+use anyhow::{anyhow, bail};
 use serde::Deserialize;
+use url::Url;
 
 use move_package::BuildConfig as MoveBuildConfig;
 use sui_move::build::resolve_lock_file_path;
@@ -20,10 +24,10 @@ pub struct Config {
     pub packages: Vec<Packages>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Clone, Deserialize, Debug)]
 pub struct Packages {
-    repository: String,
-    paths: Vec<String>,
+    pub repository: String,
+    pub paths: Vec<String>,
 }
 
 pub async fn verify_package(
@@ -61,16 +65,110 @@ pub fn parse_config(config_path: impl AsRef<Path>) -> anyhow::Result<Config> {
     Ok(toml::from_str(&contents)?)
 }
 
-pub async fn clone_repositories(config: &Config) -> anyhow::Result<()> {
+#[derive(Debug)]
+/// Represents a sequence of git commands to clone a repository and sparsely checkout Move packages within.
+pub struct CloneCommand {
+    /// git args
+    args: Vec<Vec<OsString>>,
+    /// report repository url in error messages
+    repo_url: String,
+}
+
+impl CloneCommand {
+    pub fn new(p: &Packages, dest: &Path) -> anyhow::Result<CloneCommand> {
+        let repo_url = Url::parse(&p.repository)?;
+        let Some(components) = repo_url.path_segments().map(|c| c.collect::<Vec<_>>()) else {
+	    bail!("Could not discover repository path in url {}", &p.repository)
+	};
+        let Some(repo_name) = components.last() else {
+	    bail!("Could not discover repository name in url {}", &p.repository)
+	};
+        let dest = dest.join(repo_name).into_os_string();
+
+        macro_rules! ostr {
+            ($arg:expr) => {
+                OsString::from($arg)
+            };
+        }
+
+        let mut args = vec![];
+        // Args to clone empty repository
+        let cmd_args: Vec<OsString> = vec![
+            ostr!("clone"),
+            ostr!("-n"),
+            ostr!("--depth=1"),
+            ostr!("--filter=tree:0"),
+            ostr!(&p.repository),
+            dest.clone(),
+        ];
+        args.push(cmd_args);
+
+        // Args to sparse checkout the package set
+        let mut cmd_args: Vec<OsString> = vec![
+            ostr!("-C"),
+            dest.clone(),
+            ostr!("sparse-checkout"),
+            ostr!("set"),
+            ostr!("--no-cone"),
+        ];
+        let path_args: Vec<OsString> = p.paths.iter().map(OsString::from).collect();
+        cmd_args.extend_from_slice(&path_args);
+        args.push(cmd_args);
+
+        // Args to checkout the default branch.
+        let cmd_args: Vec<OsString> = vec![ostr!("-C"), dest, ostr!("checkout")];
+        args.push(cmd_args);
+
+        Ok(Self {
+            args,
+            repo_url: p.repository.clone(),
+        })
+    }
+
+    pub async fn run(&self) -> anyhow::Result<()> {
+        for args in &self.args {
+            let result = Command::new("git").args(args).output().map_err(|_| {
+                anyhow!(
+                    "Error cloning {} with command `git {:#?}`",
+                    self.repo_url,
+                    args
+                )
+            })?;
+            if !result.status.success() {
+                bail!(
+                    "Nonzero exit status when cloning {} with command `git {:#?}`.\
+		     Stderr: {:?}",
+                    self.repo_url,
+                    args,
+                    result.stderr
+                )
+            }
+        }
+        Ok(())
+    }
+}
+
+/// Clones repositories and checks out packages as per `config` at the directory `dir`.
+pub async fn clone_repositories(config: &Config, dir: &Path) -> anyhow::Result<()> {
+    let mut tasks = vec![];
     for p in &config.packages {
-        let _ = p.repository;
-        let _ = p.paths;
+        let command = CloneCommand::new(p, dir)?;
+        let t = tokio::spawn(async move { command.run().await });
+        tasks.push(t);
+    }
+
+    for t in tasks {
+        t.await.unwrap()?;
     }
     Ok(())
 }
 
-pub async fn initialize(context: &WalletContext, config: &Config) -> anyhow::Result<()> {
-    clone_repositories(config).await?;
+pub async fn initialize(
+    context: &WalletContext,
+    config: &Config,
+    dir: &Path,
+) -> anyhow::Result<()> {
+    clone_repositories(config, dir).await?;
     verify_packages(context, vec![]).await?;
     Ok(())
 }
