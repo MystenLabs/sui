ea389324fb014c6acb4464d79959e2f38c0a846e
Rijnard van Tonder <rvantonder@gmail.com>
2023-06-22 13:48:05 -0700

    move: source service clone packages (#12590)
    ## Description 
    
    On start, the server clones repositories and does sparse-checkout for
    package paths as specified in config. Tasks are spawned separately for
    each repository.
    
    Example: The sequence of `git` commands for the `mystenlabs/sui` repo
    and its packages look as follows:
    
    ```
    git clone -n --depth=1 --filter=tree:0 https://github.com/mystenlabs/sui $TMPDIR
    git sparse-checkout -C $TMPDIR set --no-cone crates/sui-framework/packages/deepbook crates/sui-framework/packages/move-stdlib crates/sui-framework/packages/sui-framework crates/sui-framework/packages/sui-system
    git -C $TMPDIR checkout
    ```
    
    ## Test Plan 
    
    Check that we produce `git` clone and checkout commands faithfully
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-source-validation-service/tests/tests.rs
+++ crates/sui-source-validation-service/tests/tests.rs
@@ -1,11 +1,13 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::path::PathBuf;
+
 use expect_test::expect;
 use reqwest::Client;
 use serde::Deserialize;
 
-use sui_source_validation_service::{initialize, serve, Config};
+use sui_source_validation_service::{initialize, serve, CloneCommand, Config, Packages};
 
 use test_utils::network::TestClusterBuilder;
 
@@ -19,8 +21,9 @@ async fn test_api_route() -> anyhow::Result<()> {
     let mut cluster = TestClusterBuilder::new().build().await;
     let context = &mut cluster.wallet;
     let config = Config { packages: vec![] };
+    let tmp_dir = tempfile::tempdir()?;
 
-    initialize(context, &config).await?;
+    initialize(context, &config, tmp_dir.path()).await?;
     tokio::spawn(serve().expect("Cannot start service."));
 
     let client = Client::new();
@@ -67,5 +70,46 @@ fn test_parse_package_config() -> anyhow::Result<()> {
 }"#
     ];
     expect.assert_eq(&format!("{:#?}", config));
+    Ok(())
+}
+
+#[test]
+fn test_clone_command() -> anyhow::Result<()> {
+    let packages = Packages {
+        repository: "https://github.com/user/repo".into(),
+        paths: vec!["a".into(), "b".into()],
+    };
+
+    let command = CloneCommand::new(&packages, PathBuf::from("/tmp").as_path())?;
+    let expect = expect![
+        r#"CloneCommand {
+    args: [
+        [
+            "clone",
+            "-n",
+            "--depth=1",
+            "--filter=tree:0",
+            "https://github.com/user/repo",
+            "/tmp/repo",
+        ],
+        [
+            "-C",
+            "/tmp/repo",
+            "sparse-checkout",
+            "set",
+            "--no-cone",
+            "a",
+            "b",
+        ],
+        [
+            "-C",
+            "/tmp/repo",
+            "checkout",
+        ],
+    ],
+    repo_url: "https://github.com/user/repo",
+}"#
+    ];
+    expect.assert_eq(&format!("{:#?}", command));
     Ok(())
 }
