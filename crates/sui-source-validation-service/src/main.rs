49ddc44499c23bc6eaf172c1b087d84c9c5ad3cf
Rijnard van Tonder <rvantonder@gmail.com>
2023-06-21 18:31:41 -0700

    move: add basic telemetry to source service

--- crates/sui-source-validation-service/src/main.rs
+++ crates/sui-source-validation-service/src/main.rs
@@ -7,6 +7,7 @@ use clap::Parser;
 
 use sui_config::{sui_config_dir, SUI_CLIENT_CONFIG};
 use sui_sdk::wallet_context::WalletContext;
+use telemetry_subscribers::TelemetryConfig;
 
 use sui_source_validation_service::{initialize, parse_config, serve};
 
@@ -18,6 +19,7 @@ struct Args {
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    let _logging_guard = TelemetryConfig::new().with_env().init();
     let package_config = parse_config(args.config_path)?;
     let sui_config = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
     let context = WalletContext::new(&sui_config, None, None).await?;
