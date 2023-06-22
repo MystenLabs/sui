ed73f92e1afe0ff89244c64d779651ea7a0bd7fe
Rijnard van Tonder <rvantonder@gmail.com>
2023-06-21 18:08:42 -0700

    source service clone packages

--- crates/sui-source-validation-service/src/lib.rs
+++ crates/sui-source-validation-service/src/lib.rs
@@ -4,10 +4,13 @@
 use std::{
     fs,
     path::{Path, PathBuf},
+    process::Command,
 };
 
 use actix_web::{dev::Server, web, App, HttpRequest, HttpServer, Responder};
+use anyhow::{anyhow, bail};
 use serde::Deserialize;
+use url::Url;
 
 use move_package::BuildConfig as MoveBuildConfig;
 use sui_move::build::resolve_lock_file_path;
@@ -20,10 +23,10 @@ pub struct Config {
     pub packages: Vec<Packages>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Clone, Deserialize, Debug)]
 pub struct Packages {
-    repository: String,
-    paths: Vec<String>,
+    pub repository: String,
+    pub paths: Vec<String>,
 }
 
 pub async fn verify_package(
@@ -61,16 +64,108 @@ pub fn parse_config(config_path: impl AsRef<Path>) -> anyhow::Result<Config> {
     Ok(toml::from_str(&contents)?)
 }
 
-pub async fn clone_repositories(config: &Config) -> anyhow::Result<()> {
+#[derive(Debug)]
+/// Represents a sequence of git commands to clone a repository and sparsely checkout Move packages within.
+pub struct CloneCommand {
+    /// git args
+    args: Vec<Vec<String>>,
+    /// report repository url in error messages
+    repo_url: String,
+}
+
+impl CloneCommand {
+    pub fn new(p: &Packages, dest: &Path) -> anyhow::Result<CloneCommand> {
+        let repo_url = Url::parse(&p.repository)?;
+        let Some(components) = repo_url.path_segments().map(|c| c.collect::<Vec<_>>()) else {
+	    bail!("Could not discover repository path in url {}", &p.repository)
+	};
+        let Some(repo_name) = components.last() else {
+	    bail!("Could not discover repository name in url {}", &p.repository)
+	};
+        let dest = dest
+            .join(repo_name)
+            .into_os_string()
+            .into_string()
+            .map_err(|_| {
+                anyhow!(
+                    "Could not create path to clone repsository {}",
+                    &p.repository
+                )
+            })?;
+
+        let mut args = vec![];
+        // Args to clone empty repository
+        args.push(
+            [
+                "clone",
+                "-n",
+                "--depth=1",
+                "--filter=tree:0",
+                &p.repository,
+                &dest,
+            ]
+            .iter()
+            .map(|&s| s.into())
+            .collect(),
+        );
+
+        // Args to sparse checkout the package set
+        let mut prefix: Vec<String> = ["-C", &dest, "sparse-checkout", "set", "--no-cone"]
+            .iter()
+            .map(|&s| s.into())
+            .collect();
+        prefix.extend_from_slice(&p.paths);
+        args.push(prefix);
+
+        // Args to checkout the default branch.
+        args.push(
+            ["-C", &dest, "checkout"]
+                .iter()
+                .map(|&s| s.into())
+                .collect(),
+        );
+
+        Ok(Self {
+            args,
+            repo_url: p.repository.clone(),
+        })
+    }
+
+    pub async fn run(&self) -> anyhow::Result<()> {
+        for args in &self.args {
+            Command::new("git").args(args).output().map_err(|_| {
+                anyhow!(
+                    "Error cloning package(s) for {} with command git {:#?}",
+                    self.repo_url,
+                    args
+                )
+            })?;
+        }
+        Ok(())
+    }
+}
+
+/// Clones repositories and checks out packages as per `config` at the directory `dir`.
+pub async fn clone_repositories(config: &Config, dir: &Path) -> anyhow::Result<()> {
+    let mut tasks = vec![];
     for p in &config.packages {
-        let _ = p.repository;
-        let _ = p.paths;
+        let command = CloneCommand::new(p, dir)?;
+        let t = tokio::spawn(async move { command.run().await });
+        tasks.push(t);
+    }
+
+    for t in tasks {
+        t.await.unwrap()?;
     }
     Ok(())
 }
 
-pub async fn initialize(context: &WalletContext, config: &Config) -> anyhow::Result<()> {
-    clone_repositories(config).await?;
+pub async fn initialize(
+    context: &WalletContext,
+    config: &Config,
+    dir: &Path,
+) -> anyhow::Result<()> {
+    clone_repositories(config, dir).await?;
     verify_packages(context, vec![]).await?;
     Ok(())
 }
--- crates/sui-source-validation-service/src/main.rs
+++ crates/sui-source-validation-service/src/main.rs
@@ -21,6 +21,7 @@ pub async fn main() -> anyhow::Result<()> {
     let package_config = parse_config(args.config_path)?;
     let sui_config = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
     let context = WalletContext::new(&sui_config, None, None).await?;
-    initialize(&context, &package_config).await?;
+    let tmp_dir = tempfile::tempdir()?;
+    initialize(&context, &package_config, tmp_dir.path()).await?;
     serve()?.await.map_err(anyhow::Error::from)
 }
