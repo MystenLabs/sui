63a09eea21527bab7b57c71df4e0de46fac8188c
Will Yang <willyang@mystenlabs.com>
2023-06-22 17:05:19 -0700

    clippy stuff

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -65,7 +65,7 @@ use sui_types::crypto::{
 };
 use sui_types::digests::ChainIdentifier;
 use sui_types::digests::TransactionEventsDigest;
-use sui_types::dynamic_field::{DynamicFieldInfo, DynamicFieldName, DynamicFieldType, Field};
+use sui_types::dynamic_field::{DynamicFieldInfo, DynamicFieldName, DynamicFieldType};
 use sui_types::effects::{
     SignedTransactionEffects, TransactionEffects, TransactionEffectsAPI, TransactionEvents,
     VerifiedCertifiedTransactionEffects, VerifiedSignedTransactionEffects,
