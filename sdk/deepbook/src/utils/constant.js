786b46e02705682c099285497bb0b4bbe79df590
0xdeadbeef <18015565550@189.cn>
2023-06-25 20:40:35 +0800

    remove unneed files

--- sdk/deepbook/src/utils/constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.F = exports.defaultGasBudget = void 0;
-exports.defaultGasBudget = 2000000000;
-exports.F = 1000000000;
