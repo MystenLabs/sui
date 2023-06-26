dfaab76226dbb9060ef9942f9a3e4e6580cbf119
Ge Gao <ge@mystenlabs.com>
2023-06-26 15:33:11 -0400

    indexer: populate queried tx based on options

--- crates/sui-indexer/src/apis/indexer_api.rs
+++ crates/sui-indexer/src/apis/indexer_api.rs
@@ -5,6 +5,7 @@ use std::sync::Arc;
 
 use anyhow::anyhow;
 use async_trait::async_trait;
+use futures::future::join_all;
 use jsonrpsee::core::RpcResult;
 use jsonrpsee::http_client::HttpClient;
 use jsonrpsee::types::SubscriptionResult;
@@ -19,8 +20,8 @@ use sui_json_rpc::indexer_api::spawn_subscription;
 use sui_json_rpc::SuiRpcModule;
 use sui_json_rpc_types::{
     DynamicFieldPage, EventFilter, EventPage, ObjectsPage, Page, SuiObjectDataFilter,
-    SuiObjectResponse, SuiObjectResponseQuery, SuiTransactionBlockResponse,
-    SuiTransactionBlockResponseQuery, TransactionBlocksPage, TransactionFilter,
+    SuiObjectResponse, SuiObjectResponseQuery, SuiTransactionBlockResponseQuery,
+    TransactionBlocksPage, TransactionFilter,
 };
 use sui_open_rpc::Module;
 use sui_types::base_types::{ObjectID, SuiAddress};
@@ -75,53 +76,29 @@ impl<S: IndexerStore> IndexerApi<S> {
         let limit = validate_limit(limit, *QUERY_MAX_RESULT_LIMIT)?;
         let is_descending = descending_order.unwrap_or_default();
         let cursor_str = cursor.map(|digest| digest.to_string());
-        let mut tx_digests_from_db = match query.filter {
+        let mut tx_vec_from_db = match query.filter {
             None => {
                 let indexer_seq_number = self
                     .state
                     .get_transaction_sequence_by_digest(cursor_str, is_descending)
                     .await?;
-                let tx_vec = self
-                    .state
+                self.state
                     .get_all_transaction_page(indexer_seq_number, limit + 1, is_descending)
-                    .await?;
-                tx_vec
-                    .into_iter()
-                    .map(|tx| {
-                        tx.transaction_digest.parse().map_err(|e| {
-                            IndexerError::InsertableParsingError(format!(
-                                "Failed to parse transaction digest {} : {:?}",
-                                tx.transaction_digest, e
-                            ))
-                        })
-                    })
-                    .collect::<Result<Vec<TransactionDigest>, _>>()?
+                    .await
             }
             Some(TransactionFilter::Checkpoint(checkpoint_id)) => {
                 let indexer_seq_number = self
                     .state
                     .get_transaction_sequence_by_digest(cursor_str, is_descending)
                     .await?;
-                let tx_vec = self
-                    .state
+                self.state
                     .get_transaction_page_by_checkpoint(
                         checkpoint_id as i64,
                         indexer_seq_number,
                         limit + 1,
                         is_descending,
                     )
-                    .await?;
-                tx_vec
-                    .into_iter()
-                    .map(|tx| {
-                        tx.transaction_digest.parse().map_err(|e| {
-                            IndexerError::InsertableParsingError(format!(
-                                "Failed to parse transaction digest {} : {:?}",
-                                tx.transaction_digest, e
-                            ))
-                        })
-                    })
-                    .collect::<Result<Vec<TransactionDigest>, _>>()?
+                    .await
             }
             Some(TransactionFilter::MoveFunction {
                 package,
@@ -157,7 +134,7 @@ impl<S: IndexerStore> IndexerApi<S> {
                         limit + 1,
                         is_descending,
                     )
-                    .await?
+                    .await
             }
             Some(TransactionFilter::InputObject(input_obj_id)) => {
                 let input_obj_seq = self
@@ -172,7 +149,7 @@ impl<S: IndexerStore> IndexerApi<S> {
                         limit + 1,
                         is_descending,
                     )
-                    .await?
+                    .await
             }
             Some(TransactionFilter::ChangedObject(mutated_obj_id)) => {
                 let indexer_seq_number = self
@@ -187,7 +164,7 @@ impl<S: IndexerStore> IndexerApi<S> {
                         limit + 1,
                         is_descending,
                     )
-                    .await?
+                    .await
             }
             // NOTE: more efficient to run this query over transactions table
             Some(TransactionFilter::FromAddress(sender_address)) => {
@@ -195,26 +172,14 @@ impl<S: IndexerStore> IndexerApi<S> {
                     .state
                     .get_transaction_sequence_by_digest(cursor_str, is_descending)
                     .await?;
-                let tx_vec = self
-                    .state
+                self.state
                     .get_transaction_page_by_sender_address(
                         sender_address.to_string(),
                         indexer_seq_number,
                         limit + 1,
                         is_descending,
                     )
-                    .await?;
-                tx_vec
-                    .into_iter()
-                    .map(|tx| {
-                        tx.transaction_digest.parse().map_err(|e| {
-                            IndexerError::InsertableParsingError(format!(
-                                "Failed to parse transaction digest {} : {:?}",
-                                tx.transaction_digest, e
-                            ))
-                        })
-                    })
-                    .collect::<Result<Vec<TransactionDigest>, _>>()?
+                    .await
             }
             Some(TransactionFilter::ToAddress(recipient_address)) => {
                 let recipient_seq_number = self
@@ -229,7 +194,7 @@ impl<S: IndexerStore> IndexerApi<S> {
                         limit + 1,
                         is_descending,
                     )
-                    .await?
+                    .await
             }
             Some(TransactionFilter::FromAndToAddress { from, to }) => {
                 let recipient_seq_number = self
@@ -244,7 +209,7 @@ impl<S: IndexerStore> IndexerApi<S> {
                         limit + 1,
                         is_descending,
                     )
-                    .await?
+                    .await
             }
             Some(TransactionFilter::FromOrToAddress { addr }) => {
                 let start_sequence = self
@@ -253,47 +218,53 @@ impl<S: IndexerStore> IndexerApi<S> {
                     .await?;
                 self.state
                     .get_transaction_page_by_address(addr, start_sequence, limit, is_descending)
-                    .await?
+                    .await
             }
             Some(TransactionFilter::TransactionKind(tx_kind_name)) => {
                 let indexer_seq_number = self
                     .state
                     .get_transaction_sequence_by_digest(cursor_str, is_descending)
                     .await?;
-                let tx_vec = self
-                    .state
+                self.state
                     .get_transaction_page_by_transaction_kind(
                         tx_kind_name,
                         indexer_seq_number,
                         limit + 1,
                         is_descending,
                     )
-                    .await?;
-                tx_vec
-                    .into_iter()
-                    .map(|tx| {
-                        tx.transaction_digest.parse().map_err(|e| {
-                            IndexerError::InsertableParsingError(format!(
-                                "Failed to parse transaction digest {} : {:?}",
-                                tx.transaction_digest, e
-                            ))
-                        })
-                    })
-                    .collect::<Result<Vec<TransactionDigest>, _>>()?
+                    .await
             }
-        };
+        }?;
 
-        let has_next_page = tx_digests_from_db.len() > limit;
-        tx_digests_from_db.truncate(limit);
-        let next_cursor = tx_digests_from_db.last().cloned();
+        let has_next_page = tx_vec_from_db.len() > limit;
+        tx_vec_from_db.truncate(limit);
+        let next_cursor = tx_vec_from_db
+            .last()
+            .cloned()
+            .map(|tx| {
+                let digest = tx.transaction_digest;
+                let tx_digest: Result<TransactionDigest, _> = digest.parse();
+                tx_digest.map_err(|e| {
+                    IndexerError::SerdeError(format!(
+                        "Failed to deserialize transaction digest: {:?} with error {:?}",
+                        digest, e
+                    ))
+                })
+            })
+            .transpose()?
+            .map_or(cursor, Some);
 
-        let sui_tx_resps = tx_digests_from_db
+        let tx_resp_futures = tx_vec_from_db.into_iter().map(|tx| {
+            self.state
+                .compose_sui_transaction_block_response(tx, query.options.as_ref())
+        });
+        let sui_tx_resp_vec = join_all(tx_resp_futures)
+            .await
             .into_iter()
-            .map(SuiTransactionBlockResponse::new)
-            .collect();
+            .collect::<Result<Vec<_>, _>>()?;
 
         Ok(Page {
-            data: sui_tx_resps,
+            data: sui_tx_resp_vec,
             next_cursor,
             has_next_page,
         })
