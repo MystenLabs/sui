2b59dabbb1f36b2df85501b3e614c6005940f354
Ashok Menon <ashok@mystenlabs.com>
2023-06-26 17:16:01 -0700

    [sui-execution] Cut v0 (#12698)
    ## Description 
    
    Add a script to cut a new version of `sui-execution` and
    `move-execution` (based on the `sui-execution-cut` crate), and use it to
    cut a v0. Also introduces protocol version 17 to switch from using `v0`
    to using `latest` (both are identical as of this commit).
    
    Note that `v0` is **identical** to `latest` at the time of this PR --
    there will be follow-up changes to `latest` that can take advantage of
    its decoupling with the implementation used in previous versions.
    
    ## Test Plan 
    
    Run all tests:
    
    ```
    $ cargo simtest
    $ env SUI_SKIP_SIMTESTS=1 cargo nextest run
    ```
    
    - Once with all the changes except the protocol version change,
    - Once including the protocol change
    
    (To test both cuts).
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [x] protocol change
    - [x] user-visible impact
    - [ ] breaking change for a client SDKs
    - [x] breaking change for FNs (FN binary must upgrade)
    - [x] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    
    Introduces Protocol Version 17, which includes a new versioning scheme
    for the execution layer.

--- sui-execution/src/lib.rs
+++ sui-execution/src/lib.rs
@@ -13,17 +13,29 @@ pub mod executor;
 pub mod verifier;
 
 mod latest;
+mod v0;
 
 pub fn executor(
     protocol_config: &ProtocolConfig,
     paranoid_type_checks: bool,
     silent: bool,
 ) -> SuiResult<Arc<dyn Executor + Send + Sync>> {
-    Ok(Arc::new(latest::Executor::new(
-        protocol_config,
-        paranoid_type_checks,
-        silent,
-    )?))
+    let version = protocol_config.execution_version_as_option().unwrap_or(0);
+    Ok(match version {
+        0 => Arc::new(v0::Executor::new(
+            protocol_config,
+            paranoid_type_checks,
+            silent,
+        )?),
+
+        1 => Arc::new(latest::Executor::new(
+            protocol_config,
+            paranoid_type_checks,
+            silent,
+        )?),
+
+        v => panic!("Unsupported execution version {v}"),
+    })
 }
 
 pub fn verifier<'m>(
@@ -31,5 +43,10 @@ pub fn verifier<'m>(
     is_metered: bool,
     metrics: &'m Arc<BytecodeVerifierMetrics>,
 ) -> Box<dyn Verifier + 'm> {
-    Box::new(latest::Verifier::new(protocol_config, is_metered, metrics))
+    let version = protocol_config.execution_version_as_option().unwrap_or(0);
+    match version {
+        0 => Box::new(v0::Verifier::new(protocol_config, is_metered, metrics)),
+        1 => Box::new(latest::Verifier::new(protocol_config, is_metered, metrics)),
+        v => panic!("Unsupported execution version {v}"),
+    }
 }
