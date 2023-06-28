3c77c6e589cac5c203c7bf05b63f7d3bcb41152e
plam-ml <127577476+plam-ml@users.noreply.github.com>
2023-06-28 15:17:18 -0700

    [Explorer]: Update CopyToClipboard Icon (#12750)
    ## Description 
    
    https://mysten.atlassian.net/browse/APPS-1375
    - Update `CopyToClipboard` with `lg` being Copy18
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ apps/icons/src/Copy18.tsx
@@ -0,0 +1,29 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import { SVGProps } from 'react';
+const SvgCopy18 = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="1em"
+		height="1em"
+		fill="none"
+		viewBox="0 0 19 19"
+		{...props}
+	>
+		<rect
+			width={13.227}
+			height={13.227}
+			x={0.75}
+			y={5.023}
+			stroke="#A0B6C3"
+			strokeWidth={1.5}
+			rx={2.25}
+		/>
+		<path
+			stroke="#A0B6C3"
+			strokeWidth={1.5}
+			d="M4.91 3.455V3a2 2 0 0 1 2-2H16a2 2 0 0 1 2 2v9.09a2 2 0 0 1-2 2h-.454"
+		/>
+	</svg>
+);
+export default SvgCopy18;
