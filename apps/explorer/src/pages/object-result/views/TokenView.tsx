3687df2d8c587d3cdf548049a7e4cf4fff1191db
Nikhil Mattu <nikhil@mystenlabs.com>
2023-06-30 12:15:41 -0400

    updates

--- apps/explorer/src/pages/object-result/views/TokenView.tsx
+++ apps/explorer/src/pages/object-result/views/TokenView.tsx
@@ -19,7 +19,7 @@ import { trimStdLibPrefix, genFileTypeMsg } from '../../../utils/stringUtils';
 import { LinkOrTextDescriptionItem } from '../LinkOrTextDescriptionItem';
 import { DynamicFieldsCard } from '~/components/Object/DynamicFieldsCard';
 import { ObjectFieldsCard } from '~/components/Object/ObjectFieldsCard';
-import TransactionBlocksForAddress from '~/components/TransactionBlocksForAddress/TransactionBlocksForAddress';
+import TransactionBlocksForObject from '~/components/TransactionBlocksForObject/TransactionBlocksForObject';
 import { useResolveVideo } from '~/hooks/useResolveVideo';
 import { DescriptionList, DescriptionItem } from '~/ui/DescriptionList';
 import { AddressLink, ObjectLink, TransactionLink } from '~/ui/InternalLink';
@@ -147,7 +147,7 @@ export function TokenView({ data }: { data: SuiObjectResponse }) {
 			</TabHeader>
 			<ObjectFieldsCard id={objectId} />
 			<DynamicFieldsCard id={objectId} />
-			<TransactionBlocksForAddress address={objectId} isObject />
+			<TransactionBlocksForObject address={objectId} />
 		</div>
 	);
 }
