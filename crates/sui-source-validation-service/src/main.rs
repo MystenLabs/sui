61ec528e301a24209b1eece691227ef2adf4739d
Rijnard van Tonder <rvantonder@gmail.com>
2023-07-04 00:51:08 -0600

    move: minimal end-to-end source service

--- crates/sui-source-validation-service/src/main.rs
+++ crates/sui-source-validation-service/src/main.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::path::PathBuf;
+use tracing::info;
 
 use clap::Parser;
 
@@ -9,7 +10,7 @@ use sui_config::{sui_config_dir, SUI_CLIENT_CONFIG};
 use sui_sdk::wallet_context::WalletContext;
 use telemetry_subscribers::TelemetryConfig;
 
-use sui_source_validation_service::{initialize, parse_config, serve};
+use sui_source_validation_service::{initialize, parse_config, serve, AppState};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -24,6 +25,9 @@ pub async fn main() -> anyhow::Result<()> {
     let sui_config = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
     let context = WalletContext::new(&sui_config, None, None).await?;
     let tmp_dir = tempfile::tempdir()?;
-    initialize(&context, &package_config, tmp_dir.path()).await?;
-    serve()?.await.map_err(anyhow::Error::from)
+    let sources = initialize(&context, &package_config, tmp_dir.path()).await?;
+    info!("verification complete, serving...");
+    serve(AppState { sources })?
+        .await
+        .map_err(anyhow::Error::from)
 }
