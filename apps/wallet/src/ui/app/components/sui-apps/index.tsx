fa4d5fe3c8d6db329b43f1655650c1e0bb436b14
Nikhil Mattu <nikhil@mystenlabs.com>
2023-07-06 18:55:21 -0400

    fix

--- apps/wallet/src/ui/app/components/sui-apps/index.tsx
+++ apps/wallet/src/ui/app/components/sui-apps/index.tsx
@@ -51,7 +51,7 @@ function AppsPlayGround() {
 			</div>
 
 			{BullsharkInterstitialEnabled && (
-				<div className="font-frankfurter flex flex-col w-full border-2 border-black border-solid bg-[#99DBFB] p-3 rounded-lg items-center text-white gap-1 [-webkit-text-stroke:1px_black] mb-2">
+				<div className="font-frankfurter flex flex-col w-full border-2 border-black border-solid bg-[#99DBFB] p-3 rounded-lg items-center text-white gap-1 [-webkit-text-stroke:1px_black] mb-3">
 					<div className="text-heading4">Join bullsharks quests!</div>
 					<div className="text-heading3">5 million sui prize pool!</div>
 					<a
