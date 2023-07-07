3778c254a70e1897a15a1e7297e55056ebf93f0c
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-07-07 19:16:28 +0300

    enable transaction filter only for local and mainnet

--- apps/explorer/src/components/Activity/index.tsx
+++ apps/explorer/src/components/Activity/index.tsx
@@ -2,15 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Filter16 } from '@mysten/icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import toast from 'react-hot-toast';
 
 import { EpochsActivityTable } from './EpochsActivityTable';
 import { TransactionsActivityTable } from './TransactionsActivityTable';
 import { CheckpointsTable } from '../checkpoints/CheckpointsTable';
 // import { PlayPause } from '~/ui/PlayPause';
+import { useNetwork } from '~/context';
 import { DropdownMenu, DropdownMenuCheckboxItem } from '~/ui/DropdownMenu';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/ui/Tabs';
+import { Network } from '~/utils/api/DefaultRpcClient';
 
 const VALID_TABS = ['transactions', 'epochs', 'checkpoints'];
 
@@ -44,7 +46,17 @@ export function Activity({ initialTab, initialLimit, disablePagination }: Props)
 	// };
 
 	const refetchInterval = paused ? undefined : REFETCH_INTERVAL;
-	const [showSystemTransactions, setShowSystemTransaction] = useState(false);
+	// TODO remove network check when querying transactions with TransactionKind filter is fixed on devnet and testnet
+	const [network] = useNetwork();
+	const isTransactionKindFilterEnabled = Network.MAINNET === network || Network.LOCAL === network;
+	const [showSystemTransactions, setShowSystemTransaction] = useState(
+		!isTransactionKindFilterEnabled,
+	);
+	useEffect(() => {
+		if (!isTransactionKindFilterEnabled) {
+			setShowSystemTransaction(true);
+		}
+	}, [isTransactionKindFilterEnabled]);
 
 	return (
 		<div>
@@ -56,7 +68,7 @@ export function Activity({ initialTab, initialLimit, disablePagination }: Props)
 						<TabsTrigger value="checkpoints">Checkpoints</TabsTrigger>
 					</TabsList>
 					<div className="absolute inset-y-0 -top-1 right-0 flex items-center gap-3 text-2xl">
-						{activeTab === 'transactions' ? (
+						{activeTab === 'transactions' && isTransactionKindFilterEnabled ? (
 							<DropdownMenu
 								trigger={<Filter16 className="p-1" />}
 								content={
