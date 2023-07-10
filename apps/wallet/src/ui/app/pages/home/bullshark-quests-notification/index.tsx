44471d495cd27c7f2d0b9e16cfad2dfc968eab07
Nikhil-Mysten <128089541+Nikhil-Mysten@users.noreply.github.com>
2023-07-10 16:00:42 -0400

    [wallet-ext] bullshark modal followups (#12899)
    ## Description 
    
    scrollable container
    
    ## Test Plan 
    
    Locally
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/pages/home/bullshark-quests-notification/index.tsx
+++ apps/wallet/src/ui/app/pages/home/bullshark-quests-notification/index.tsx
@@ -3,10 +3,14 @@
 
 import { X32 } from '@mysten/icons';
 
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Portal } from '../../../shared/Portal';
 import { ampli } from '_src/shared/analytics/ampli';
 import ExternalLink from '_src/ui/app/components/external-link';
+
+const setInterstitialDismissed = () =>
+	localStorage.setItem('bullshark-interstitial-dismissed', 'true');
 
 const InterstitialHeading = ({ text }: { text: string }) => {
 	return <div className="[-webkit-text-stroke:2px_black] text-heading1">{text}</div>;
@@ -24,15 +28,23 @@ const InterstitialBanner = ({ lines }: { lines: string[] }) => {
 
 function BullsharkQuestsNotification({ onClose }: { onClose: () => void }) {
 	const navigate = useNavigate();
-	const setInterstitialDismissed = () => {
-		localStorage.setItem('bullshark-interstitial-dismissed', 'true');
+
+	useEffect(() => {
+		const t = setTimeout(setInterstitialDismissed, 1000);
+		return () => clearTimeout(t);
+	}, []);
+
+	const closeInterstitial = () => {
+		setInterstitialDismissed();
+		onClose();
+		navigate('/apps');
 	};
 
 	return (
 		<Portal containerId="overlay-portal-container">
 			<div className="flex flex-col justify-center flex-nowrap items-center bg-[#99dbfb] border-solid border-4 border-black rounded-lg z-50 overflow-hidden absolute top-0 bottom-0 left-0 right-0 backdrop-blur-sm">
 				<div className="flex flex-col font-frankfurter w-full h-full items-center p-4 text-white text-center">
-					<div className="flex flex-col py-6 px-7 border-4 border-black border-solid w-full rounded-md h-full items-center">
+					<div className="flex flex-col py-6 px-7 border-4 border-black border-solid w-full rounded-md h-full items-center overflow-auto">
 						<div className="flex flex-col items-center">
 							<InterstitialHeading text="Join Bullshark Quests!" />
 							<div className="bg-[url('https://quests.mystenlabs.com/_next/static/media/logo.81b4eb8f.svg')] h-40 w-40 bg-cover" />
@@ -46,7 +58,10 @@ function BullsharkQuestsNotification({ onClose }: { onClose: () => void }) {
 						<div className="flex flex-col items-center gap-4 [-webkit-text-stroke:1px_black] w-full mt-5">
 							<ExternalLink
 								href="https://tech.mystenlabs.com/introducing-bullsharks-quests/"
-								onClick={() => ampli.clickedBullsharkQuestsCta({ sourceFlow: 'Interstitial' })}
+								onClick={() => {
+									ampli.clickedBullsharkQuestsCta({ sourceFlow: 'Interstitial' });
+									closeInterstitial();
+								}}
 								className="appearance-none no-underline text-white bg-[#EA3389] rounded-lg py-2 w-60 [-webkit-text-stroke:1px_black] leading-none text-heading6"
 							>
 								Read more on the blog
@@ -54,11 +69,7 @@ function BullsharkQuestsNotification({ onClose }: { onClose: () => void }) {
 							<button
 								data-testid="bullshark-dismiss"
 								className="appearance-none bg-transparent border-none cursor-pointer mt-1"
-								onClick={() => {
-									setInterstitialDismissed();
-									onClose();
-									navigate('/apps');
-								}}
+								onClick={closeInterstitial}
 							>
 								<X32 className="text-sui-dark h-8 w-8" />
 							</button>
