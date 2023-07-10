9ad4679d7852f19f36d59a14b5992777ff592163
Stefan Stanciulescu <stefan.stanciulescu@mystenlabs.com>
2023-07-10 12:04:31 -0700

    Refactor Rust SDK, add doc comments and examples

---
+++ crates/sui-sdk/examples/coin_read_api.rs
@@ -0,0 +1,100 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+use futures::future;
+use futures::stream::StreamExt;
+
+mod utils;
+use sui_sdk::SuiClientBuilder;
+use utils::sui_address_for_examples;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let sui_local = SuiClientBuilder::default().build_localnet().await?; // local Sui network
+    println!("Sui local version{:?}", sui_local.api_version());
+    // create a random Sui address for examples.
+    // Check utils module if you want to use a local wallet, or use SuiAddress::from_str("sui_address") for a specific address
+    let active_address = sui_address_for_examples().await?;
+
+    // ************ COIN READ API ************ //
+
+    // Get coins
+    let coins = sui_local
+        .coin_read_api()
+        .get_coins(active_address, None, None, Some(5)) // get the first five coins. Note that this can be filtered by coin type: coin_type: Some("0x2::sui::SUI".to_string())
+        .await?;
+    println!(" *** Coins ***");
+    println!("{:?}", coins);
+    println!(" *** Coins ***\n");
+
+    // Get all coins
+    let all_coins = sui_local
+        .coin_read_api()
+        .get_all_coins(active_address, None, Some(5)) // get the first five coins
+        .await?;
+    println!(" *** All coins ***");
+    println!("{:?}", all_coins);
+    println!(" *** All coins ***\n");
+
+    // Get coins as a stream
+    let coins_stream = sui_local
+        .coin_read_api()
+        .get_coins_stream(active_address, None);
+
+    println!(" *** Coins Stream ***");
+    coins_stream
+        .for_each(|coin| {
+            println!("{:?}", coin);
+            future::ready(())
+        })
+        .await;
+    println!(" *** Coins Stream ***\n");
+
+    // Select coins
+    let select_coins = sui_local
+        .coin_read_api()
+        .select_coins(active_address, Some("0x2::sui::SUI".to_string()), 1, vec![])
+        .await?;
+
+    println!(" *** Select Coins ***");
+    println!("{:?}", select_coins);
+    println!("{:?}", select_coins);
+    println!(" *** Select Coins ***\n");
+
+    // Balance
+    let balance = sui_local
+        .coin_read_api()
+        .get_balance(active_address, None)
+        .await?;
+    // Total balance
+    let total_balance = sui_local
+        .coin_read_api()
+        .get_balance(active_address, None)
+        .await?;
+    println!(" *** Balance + Total Balance *** ");
+    println!("Balance: {:?}", balance);
+    println!("Total Balance: {:?}", total_balance);
+    println!(" *** Balance + Total Balance ***\n ");
+
+    // Coin Metadata
+    let coin_metadata = sui_local
+        .coin_read_api()
+        .get_coin_metadata("0x2::sui::SUI".to_string())
+        .await?;
+
+    println!(" *** Coin Metadata *** ");
+    println!("{:?}", coin_metadata);
+    println!(" *** Coin Metadata ***\n ");
+
+    // Total Supply
+    let total_supply = sui_local
+        .coin_read_api()
+        .get_total_supply("0x2::sui::SUI".to_string())
+        .await?;
+    println!(" *** Total Supply *** ");
+    println!("{:?}", total_supply);
+    println!(" *** Total Supply ***\n ");
+
+    // ************ END OF COIN READ API ************ //
+
+    Ok(())
+}
---
+++ crates/sui-sdk/examples/utils/mod.rs
@@ -0,0 +1,44 @@
+// use sui_config::{sui_config_dir, SUI_CLIENT_CONFIG}; // uncomment if using a local wallet
+// use sui_sdk::{wallet_context::WalletContext}; // uncomment if using a local wallet
+
+use std::collections::HashMap;
+
+use reqwest::Client;
+use sui_types::base_types::SuiAddress;
+
+#[allow(dead_code)]
+pub(crate) async fn sui_address_for_examples() -> Result<SuiAddress, anyhow::Error> {
+    // If there is an existing wallet you want to use then the following code can be uncommented
+
+    // let wallet_conf = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
+    // let mut wallet =
+    //     WalletContext::new(&wallet_conf, Some(std::time::Duration::from_secs(60)), None).await?;
+    // let active_address = wallet.active_address()?;
+
+    // If a wallet does exist, then comment out the code after these comments and uncomment the code above
+    // Generate a random Sui Address, and add some coins to it. Request tokens (coins) from the faucet JSON RPC API, which requires a JSON body like this
+    // "FixedAmountRequest" : {
+    //    "recipient": "SUI_ADDRESS"
+    // }
+
+    let active_address: SuiAddress = SuiAddress::random_for_testing_only();
+    request_tokens_from_faucet(active_address).await?;
+    Ok(active_address)
+}
+
+pub(crate) async fn request_tokens_from_faucet(address: SuiAddress) -> Result<(), anyhow::Error> {
+    let mut map = HashMap::new();
+    let mut recipient = HashMap::new();
+    recipient.insert("recipient", address.to_string());
+    map.insert("FixedAmountRequest", recipient);
+
+    // make the request to the faucet JSON RPC API for coins
+    let client = Client::new();
+    let _res = client
+        .post("http://127.0.0.1:9123/gas")
+        .header("Content-Type", "application/json")
+        .json(&map)
+        .send()
+        .await?;
+    Ok(())
+}
