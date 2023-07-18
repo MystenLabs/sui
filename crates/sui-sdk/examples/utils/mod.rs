76dee90193ec52304846e6c2b34df3568220b0a0
Stefan Stanciulescu <stefan.stanciulescu@mystenlabs.com>
2023-07-18 13:45:12 -0700

    Change to consts for Faucet's urls

--- crates/sui-sdk/examples/utils/mod.rs
+++ crates/sui-sdk/examples/utils/mod.rs
@@ -7,6 +7,9 @@ use sui_types::base_types::SuiAddress;
 // use sui_config::{sui_config_dir, SUI_CLIENT_CONFIG}; // uncomment if using a local wallet
 // use sui_sdk::{wallet_context::WalletContext}; // uncomment if using a local wallet
 
+const SUI_FAUCET_TESTNET: &str = "https://faucet.testnet.sui.io/gas";
+const _SUI_FAUCET_LOCALNET: &str = "http://127.0.0.1:9123/gas"; // if you use the sui-test-validator; if it does not work, try with port 5003.
+
 #[allow(dead_code)]
 pub(crate) async fn sui_address_for_examples() -> Result<SuiAddress, anyhow::Error> {
     // If there is an existing wallet you want to use then the following code can be uncommented
@@ -32,15 +35,17 @@ pub(crate) async fn request_tokens_from_faucet(address: SuiAddress) -> Result<()
     let mut recipient = HashMap::new();
     recipient.insert("recipient", address.to_string());
     map.insert("FixedAmountRequest", recipient);
-
+    // {Fixed Amout - recipient: address}
     // make the request to the faucet JSON RPC API for coins
     let client = Client::new();
+
     let _res = client
-        // .post("http://127.0.0.1:9123/gas")
-        .post("https://faucet.devnet.sui.io/gas")
+        .post(SUI_FAUCET_TESTNET)
         .header("Content-Type", "application/json")
         .json(&map)
         .send()
         .await?;
+    println!("{:?}", _res);
+
     Ok(())
 }
