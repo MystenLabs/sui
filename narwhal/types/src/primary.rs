cdf5349c650e59281631da8aa62bf170d38aadfa
Arun Koshy <97870774+arun-koshy@users.noreply.github.com>
2023-07-20 14:49:03 -0700

    Remove BatchV1 and related code gating BatchV2 (#13077)
    ## Description 
    
    We have moved past protocol version 12 so we can remove this old code.
    
    ## Test Plan 
    
    Unit Tests + Deploy to private-testnet

--- narwhal/types/src/primary.rs
+++ narwhal/types/src/primary.rs
@@ -140,23 +140,16 @@ impl MetadataAPI for MetadataV1 {
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Arbitrary)]
 #[enum_dispatch(BatchAPI)]
 pub enum Batch {
-    V1(BatchV1),
     V2(BatchV2),
 }
 
 impl Batch {
     pub fn new(transactions: Vec<Transaction>, protocol_config: &ProtocolConfig) -> Self {
-        // TODO: Remove once we have upgraded to protocol version 12.
-        if protocol_config.narwhal_versioned_metadata() {
-            Self::V2(BatchV2::new(transactions, protocol_config))
-        } else {
-            Self::V1(BatchV1::new(transactions))
-        }
+        Self::V2(BatchV2::new(transactions, protocol_config))
     }
 
     pub fn size(&self) -> usize {
         match self {
-            Batch::V1(data) => data.size(),
             Batch::V2(data) => data.size(),
         }
     }
@@ -167,7 +160,6 @@ impl Hash<{ crypto::DIGEST_LENGTH }> for Batch {
 
     fn digest(&self) -> BatchDigest {
         match self {
-            Batch::V1(data) => data.digest(),
             Batch::V2(data) => data.digest(),
         }
     }
@@ -177,59 +169,11 @@ impl Hash<{ crypto::DIGEST_LENGTH }> for Batch {
 pub trait BatchAPI {
     fn transactions(&self) -> &Vec<Transaction>;
     fn transactions_mut(&mut self) -> &mut Vec<Transaction>;
-    fn metadata(&self) -> &Metadata;
-    fn metadata_mut(&mut self) -> &mut Metadata;
-
-    // BatchV2 APIs
     fn versioned_metadata(&self) -> &VersionedMetadata;
     fn versioned_metadata_mut(&mut self) -> &mut VersionedMetadata;
 }
 
 pub type Transaction = Vec<u8>;
-#[derive(Clone, Serialize, Deserialize, Default, Debug, PartialEq, Eq, Arbitrary)]
-pub struct BatchV1 {
-    pub transactions: Vec<Transaction>,
-    pub metadata: Metadata,
-}
-
-impl BatchAPI for BatchV1 {
-    fn transactions(&self) -> &Vec<Transaction> {
-        &self.transactions
-    }
-
-    fn transactions_mut(&mut self) -> &mut Vec<Transaction> {
-        &mut self.transactions
-    }
-
-    fn metadata(&self) -> &Metadata {
-        &self.metadata
-    }
-
-    fn metadata_mut(&mut self) -> &mut Metadata {
-        &mut self.metadata
-    }
-
-    fn versioned_metadata(&self) -> &VersionedMetadata {
-        unimplemented!("BatchV1 does not have a VersionedMetadata field");
-    }
-
-    fn versioned_metadata_mut(&mut self) -> &mut VersionedMetadata {
-        unimplemented!("BatchV1 does not have a VersionedMetadata field");
-    }
-}
-
-impl BatchV1 {
-    pub fn new(transactions: Vec<Transaction>) -> Self {
-        Self {
-            transactions,
-            metadata: Metadata::default(),
-        }
-    }
-
-    pub fn size(&self) -> usize {
-        self.transactions.iter().map(|t| t.len()).sum()
-    }
-}
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Arbitrary)]
 pub struct BatchV2 {
@@ -245,14 +189,6 @@ impl BatchAPI for BatchV2 {
 
     fn transactions_mut(&mut self) -> &mut Vec<Transaction> {
         &mut self.transactions
-    }
-
-    fn metadata(&self) -> &Metadata {
-        unimplemented!("BatchV2 does not have a Metadata field");
-    }
-
-    fn metadata_mut(&mut self) -> &mut Metadata {
-        unimplemented!("BatchV2 does not have a Metadata field");
     }
 
     fn versioned_metadata(&self) -> &VersionedMetadata {
@@ -275,35 +211,6 @@ impl BatchV2 {
     pub fn size(&self) -> usize {
         self.transactions.iter().map(|t| t.len()).sum()
     }
-}
-
-// TODO: Remove once we have upgraded to protocol version 12.
-pub fn validate_batch_version(
-    batch: &Batch,
-    protocol_config: &ProtocolConfig,
-) -> anyhow::Result<()> {
-    // If network has advanced to using version 12, which sets narwhal_versioned_metadata
-    // to true, we will start using BatchV2 locally and so we will only accept
-    // BatchV2 from the network. Otherwise BatchV1 is used.
-    match batch {
-        Batch::V1(_) => {
-            if protocol_config.narwhal_versioned_metadata() {
-                return Err(anyhow::anyhow!(format!(
-                    "Received {batch:?} but network is at {:?} and this batch version is no longer supported",
-                    protocol_config.version
-                )));
-            }
-        }
-        Batch::V2(_) => {
-            if !protocol_config.narwhal_versioned_metadata() {
-                return Err(anyhow::anyhow!(format!(
-                    "Received {batch:?} but network is at {:?} and this batch version is not supported yet",
-                    protocol_config.version
-                )));
-            }
-        }
-    };
-    Ok(())
 }
 
 #[derive(
@@ -353,16 +260,6 @@ impl AsRef<[u8]> for BatchDigest {
 impl BatchDigest {
     pub fn new(val: [u8; crypto::DIGEST_LENGTH]) -> BatchDigest {
         BatchDigest(val)
-    }
-}
-
-impl Hash<{ crypto::DIGEST_LENGTH }> for BatchV1 {
-    type TypedDigest = BatchDigest;
-
-    fn digest(&self) -> Self::TypedDigest {
-        BatchDigest::new(
-            crypto::DefaultHashFunction::digest_iterator(self.transactions.iter()).into(),
-        )
     }
 }
 
@@ -1449,15 +1346,6 @@ impl fmt::Display for BlockErrorKind {
     }
 }
 
-// TODO: Remove once we have upgraded to protocol version 12.
-/// Used by worker to inform primary it sealed a new batch.
-#[derive(Clone, Serialize, Deserialize, Eq, PartialEq, Debug)]
-pub struct WorkerOurBatchMessage {
-    pub digest: BatchDigest,
-    pub worker_id: WorkerId,
-    pub metadata: Metadata,
-}
-
 /// Used by worker to inform primary it sealed a new batch.
 #[derive(Clone, Serialize, Deserialize, Eq, PartialEq, Debug)]
 pub struct WorkerOwnBatchMessage {
@@ -1536,30 +1424,13 @@ impl From<&Vote> for VoteInfo {
 
 #[cfg(test)]
 mod tests {
-    use crate::{
-        Batch, BatchAPI, BatchV1, BatchV2, Metadata, MetadataAPI, MetadataV1, Timestamp,
-        VersionedMetadata,
-    };
+    use crate::{Batch, BatchAPI, BatchV2, MetadataAPI, MetadataV1, Timestamp, VersionedMetadata};
     use std::time::Duration;
-    use sui_protocol_config::{Chain, ProtocolConfig, ProtocolVersion};
     use test_utils::latest_protocol_version;
     use tokio::time::sleep;
 
     #[tokio::test]
     async fn test_elapsed() {
-        // TODO: Remove once we have upgraded to protocol version 12.
-        // BatchV1
-        let batch = Batch::new(
-            vec![],
-            &ProtocolConfig::get_for_version(ProtocolVersion::new(11), Chain::Unknown),
-        );
-        assert!(batch.metadata().created_at > 0);
-
-        sleep(Duration::from_secs(2)).await;
-
-        assert!(batch.metadata().created_at.elapsed().as_secs_f64() >= 2.0);
-
-        // BatchV2
         let batch = Batch::new(vec![], &latest_protocol_version());
 
         assert!(*batch.versioned_metadata().created_at() > 0);
@@ -1580,17 +1451,6 @@ mod tests {
 
     #[test]
     fn test_elapsed_when_newer_than_now() {
-        // TODO: Remove once we have upgraded to protocol version 12.
-        // BatchV1
-        let batch = Batch::V1(BatchV1 {
-            transactions: vec![],
-            metadata: Metadata {
-                created_at: 2999309726980, // something in the future - Fri Jan 16 2065 05:35:26
-            },
-        });
-
-        assert_eq!(batch.metadata().created_at.elapsed().as_secs_f64(), 0.0);
-
         // BatchV2
         let batch = Batch::V2(BatchV2 {
             transactions: vec![],
--- narwhal/types/src/tests/batch_serde.rs
+++ narwhal/types/src/tests/batch_serde.rs
@@ -2,17 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::worker::batch_serde::Token::NewtypeVariant;
-use crate::{Batch, BatchV1, Metadata};
+use crate::{Batch, BatchV2, MetadataV1, VersionedMetadata};
 use serde_test::{assert_tokens, Token};
 #[test]
 fn test_serde_batch() {
     let tx = || vec![1; 5];
 
-    let batch = Batch::V1(BatchV1 {
+    let batch = Batch::V2(BatchV2 {
         transactions: (0..2).map(|_| tx()).collect(),
-        metadata: Metadata {
-            created_at: 1666205365890,
-        },
+        versioned_metadata: VersionedMetadata::V1(MetadataV1 {
+            created_at: 2999309726980,
+            received_at: None,
+        }),
     });
 
     assert_tokens(
@@ -20,10 +21,10 @@ fn test_serde_batch() {
         &[
             NewtypeVariant {
                 name: "Batch",
-                variant: "V1",
+                variant: "V2",
             },
             Token::Struct {
-                name: "BatchV1",
+                name: "BatchV2",
                 len: 2,
             },
             Token::Str("transactions"),
@@ -43,13 +44,19 @@ fn test_serde_batch() {
             Token::U8(1),
             Token::SeqEnd,
             Token::SeqEnd,
-            Token::Str("metadata"),
+            Token::Str("versioned_metadata"),
+            Token::NewtypeVariant {
+                name: "VersionedMetadata",
+                variant: "V1",
+            },
             Token::Struct {
-                name: "Metadata",
-                len: 1,
+                name: "MetadataV1",
+                len: 2,
             },
             Token::Str("created_at"),
-            Token::U64(1666205365890),
+            Token::U64(2999309726980),
+            Token::Str("received_at"),
+            Token::None,
             Token::StructEnd,
             Token::StructEnd,
         ],
