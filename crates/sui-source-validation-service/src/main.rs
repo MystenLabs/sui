8113b67b590d510e287b90d667f3820a33b3b0b6
Rijnard van Tonder <rvantonder@gmail.com>
2023-07-20 11:39:28 -0600

    move: source service logging (#13065)
    ## Description 
    
    Some logging improvements as I'm working on Dockerfile for deployments
    
    ## Test Plan 
    
    N/A non-functional change
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-source-validation-service/src/main.rs
+++ crates/sui-source-validation-service/src/main.rs
@@ -21,8 +21,9 @@ pub async fn main() -> anyhow::Result<()> {
     let _logging_guard = TelemetryConfig::new().with_env().init();
     let package_config = parse_config(args.config_path)?;
     let tmp_dir = tempfile::tempdir()?;
+    let start = tokio::time::Instant::now();
     let sources = initialize(&package_config, tmp_dir.path()).await?;
-    info!("verification complete");
+    info!("verification complete in {:?}", start.elapsed());
     info!("serving on {}", host_port());
     serve(AppState { sources })?
         .await
