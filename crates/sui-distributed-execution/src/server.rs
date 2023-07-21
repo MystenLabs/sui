97d94693b141c23008f821eccf5c7aad74661ba5
Quentin Kniep <quentin.kniep@mystenlabs.com>
2023-07-21 21:41:47 +0200

    use relative path to load server config

--- crates/sui-distributed-execution/src/server.rs
+++ crates/sui-distributed-execution/src/server.rs
@@ -10,12 +10,13 @@ use tokio::time::{sleep, Duration};
 
 use super::agents::*;
 use super::types::*;
+const FILE_PATH: &str = "./crates/sui-distributed-execution/src/configs/config.json";
 
 pub struct Server<T: Agent<M>, M: Debug + Message + Send + 'static> {
-    global_config: GlobalConfig,    // global configuration from parsing json
+    global_config: GlobalConfig, // global configuration from parsing json
     my_id: UniqueId,
-    agent_type: PhantomData<T>,     // type of agent living on this server
-    msg_type: PhantomData<M>        // type of message used by agent
+    agent_type: PhantomData<T>, // type of agent living on this server
+    msg_type: PhantomData<M>,   // type of message used by agent
 }
 
 impl<T: Agent<M>, M: Debug + Message + Send + 'static> Server<T, M> {
@@ -30,11 +31,14 @@ impl<T: Agent<M>, M: Debug + Message + Send + 'static> Server<T, M> {
 
     // Helper function to initialize Agent
     // Outputs ingress and egress channels of the Agent
-    fn init_agent(id: UniqueId, conf: ServerConfig) 
-    -> (T,
+    fn init_agent(
+        id: UniqueId,
+        conf: ServerConfig,
+    ) -> (
+        T,
         mpsc::Sender<NetworkMessage<M>>,
-        mpsc::Receiver<NetworkMessage<M>>,) 
-    {
+        mpsc::Receiver<NetworkMessage<M>>,
+    ) {
         let (in_send, in_recv) = mpsc::channel(100);
         let (out_send, out_recv) = mpsc::channel(100);
         let agent = T::new(id, in_recv, out_send, conf.attrs);
@@ -43,7 +47,6 @@ impl<T: Agent<M>, M: Debug + Message + Send + 'static> Server<T, M> {
 
     // Server main function
     pub async fn run(&mut self) {
-
         // Initialize map from id to address
         let mut addr_table: HashMap<UniqueId, (IpAddr, u16)> = HashMap::new();
         for (id, entry) in &self.global_config {
@@ -54,13 +57,10 @@ impl<T: Agent<M>, M: Debug + Message + Send + 'static> Server<T, M> {
         // Initialize Agent and Network Manager
         // Network manager connects to agent through channels
         let agent_config = self.global_config.get(&self.my_id).unwrap().clone();
-        let (mut agent, in_sender, out_receiver) = 
-            Self::init_agent(self.my_id, agent_config);
-             
-        let mut network_manager = NetworkManager::new(
-            self.my_id, addr_table, 
-            in_sender, 
-            out_receiver);
+        let (mut agent, in_sender, out_receiver) = Self::init_agent(self.my_id, agent_config);
+
+        let mut network_manager =
+            NetworkManager::new(self.my_id, addr_table, in_sender, out_receiver);
 
         // Run the Network Manager
         tokio::spawn(async move {
@@ -75,8 +75,6 @@ impl<T: Agent<M>, M: Debug + Message + Send + 'static> Server<T, M> {
     }
 }
 
-
-
 /*****************************************************************************************
  *                                     Network Manager                                   *
  *****************************************************************************************/
@@ -84,8 +82,8 @@ impl<T: Agent<M>, M: Debug + Message + Send + 'static> Server<T, M> {
 // Network Manager spawns and manages TCP connections for the server.
 struct NetworkManager<M: Debug + Message + Send> {
     my_id: UniqueId,
-    my_addr: IpAddr,    // listening addr
-    my_port: u16,       // listening port
+    my_addr: IpAddr, // listening addr
+    my_port: u16,    // listening port
     addr_table: HashMap<UniqueId, (IpAddr, u16)>,
     // channel to pipe incoming messages for server
     application_in: mpsc::Sender<NetworkMessage<M>>,
@@ -98,7 +96,7 @@ impl<M: Debug + Message + Send + 'static> NetworkManager<M> {
         my_id: UniqueId,
         addr_table: HashMap<UniqueId, (IpAddr, u16)>,
         application_in: mpsc::Sender<NetworkMessage<M>>,
-        application_out: mpsc::Receiver<NetworkMessage<M>>
+        application_out: mpsc::Receiver<NetworkMessage<M>>,
     ) -> Self {
         NetworkManager {
             my_id,
@@ -112,11 +110,11 @@ impl<M: Debug + Message + Send + 'static> NetworkManager<M> {
 
     // Procedure to handle a single TCP connection
     async fn handle_connection(
-        my_id:UniqueId,
-        socket: TcpStream, 
-        in_sender: mpsc::Sender<NetworkMessage<M>>,   // Send channel from link to Network Manager
-        receiver_table: Arc<RwLock<HashMap<UniqueId, mpsc::Receiver<NetworkMessage<M>>>>>) 
-    {
+        my_id: UniqueId,
+        socket: TcpStream,
+        in_sender: mpsc::Sender<NetworkMessage<M>>, // Send channel from link to Network Manager
+        receiver_table: Arc<RwLock<HashMap<UniqueId, mpsc::Receiver<NetworkMessage<M>>>>>,
+    ) {
         let mut stream = BufReader::new(socket);
 
         // First perform handshake. Send my id, receive remote_id, and pick appropriate
@@ -129,7 +127,7 @@ impl<M: Debug + Message + Send + 'static> NetworkManager<M> {
         let remote_id = line.trim().parse().unwrap();
         println!("Established connection with {}", remote_id);
 
-        let mut out_receiver: mpsc::Receiver<NetworkMessage<M>>; 
+        let mut out_receiver: mpsc::Receiver<NetworkMessage<M>>;
         {
             let mut w_receiver_table = receiver_table.write().unwrap();
             out_receiver = w_receiver_table.remove(&remote_id).unwrap();
@@ -159,17 +157,17 @@ impl<M: Debug + Message + Send + 'static> NetworkManager<M> {
         }
     }
 
-    async fn run(&mut self) {     
-
+    async fn run(&mut self) {
         // Initialize routing table
         let mut routing_table = HashMap::<UniqueId, mpsc::Sender<NetworkMessage<M>>>::new();
-        let receiver_table = Arc::new(RwLock::new(
-            HashMap::<UniqueId, mpsc::Receiver<NetworkMessage<M>>>::new()
-        ));
+        let receiver_table = Arc::new(RwLock::new(HashMap::<
+            UniqueId,
+            mpsc::Receiver<NetworkMessage<M>>,
+        >::new()));
         {
             let mut w_receiver_table = receiver_table.write().unwrap();
             for (&id, _) in &self.addr_table {
-                // Channel from Network Manager to link handler, inserted into 
+                // Channel from Network Manager to link handler, inserted into
                 // routing_table and receiver_table.
                 // Each handle_connection task then grabs its corresponding receiver from receiver_table,
                 // after establishing the id of its remote peer.
@@ -178,9 +176,9 @@ impl<M: Debug + Message + Send + 'static> NetworkManager<M> {
                 w_receiver_table.insert(id, out_receiver);
             }
         }
-       
+
         let receiver_table_clone = receiver_table.clone();
-   
+
         // Channel from link handlers to Network Manager
         let (in_sender, mut in_receiver) = mpsc::channel::<NetworkMessage<M>>(100);
         let in_sender_clone = in_sender.clone();
@@ -192,14 +190,15 @@ impl<M: Debug + Message + Send + 'static> NetworkManager<M> {
         tokio::spawn(async move {
             let listener = TcpListener::bind(listener_address).await.unwrap();
             println!("Server {} listening on {}", my_id, listener_address);
-            
+
             // Accept incoming connections and spawn a handle_connection task for each
             while let Ok((socket, _)) = listener.accept().await {
                 let in_sender_clone = in_sender_clone.clone();
                 let receiver_table_clone = receiver_table_clone.clone();
 
                 tokio::spawn(async move {
-                    Self::handle_connection(my_id, socket, in_sender_clone, receiver_table_clone).await;
+                    Self::handle_connection(my_id, socket, in_sender_clone, receiver_table_clone)
+                        .await;
                 });
             }
         });
@@ -213,12 +212,13 @@ impl<M: Debug + Message + Send + 'static> NetworkManager<M> {
                 let receiver_table_clone = receiver_table.clone();
 
                 tokio::spawn(async move {
-                    Self::handle_connection(my_id, socket, in_sender_clone, receiver_table_clone).await;
+                    Self::handle_connection(my_id, socket, in_sender_clone, receiver_table_clone)
+                        .await;
                 });
             }
         }
 
-        // Receive loop to manage incoming TCP messages, 
+        // Receive loop to manage incoming TCP messages,
         // and route outgoing messages from application to the right link
         loop {
             tokio::select! {
@@ -227,7 +227,7 @@ impl<M: Debug + Message + Send + 'static> NetworkManager<M> {
                     self.application_in.send(message).await.expect("send failed");
                 }
                 // NetManager from application
-                // TODO: What happens when Agent sends to itself? Spawn a task to stick 
+                // TODO: What happens when Agent sends to itself? Spawn a task to stick
                 // the message back to the Agent ingress channel?
                 Some(message) = self.application_out.recv() => {
                     let dst = message.dst;
