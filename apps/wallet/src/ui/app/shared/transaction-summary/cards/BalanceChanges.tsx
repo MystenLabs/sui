35c5c46c3fcd174e91ee0495c47c5d1983979333
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-07-21 11:10:24 -0400

    move to share function

--- apps/wallet/src/ui/app/shared/transaction-summary/cards/BalanceChanges.tsx
+++ apps/wallet/src/ui/app/shared/transaction-summary/cards/BalanceChanges.tsx
@@ -4,9 +4,9 @@ import {
 	type BalanceChangeSummary,
 	CoinFormat,
 	useFormatCoin,
+	useCoinMetadata,
 	type BalanceChange,
 } from '@mysten/core';
-import { parseStructTag, normalizeSuiObjectId } from '@mysten/sui.js';
 import { useMemo } from 'react';
 
 import { CoinsStack } from './CoinStack';
@@ -14,62 +14,42 @@ import { Card } from '../Card';
 import { OwnerFooter } from '../OwnerFooter';
 import Alert from '_components/alert';
 import { CoinIcon } from '_src/ui/app/components/coin-icon';
-import { useRecognizedPackages } from '_src/ui/app/hooks/useRecognizedPackages';
 import { Text } from '_src/ui/app/shared/text';
 
 interface BalanceChangesProps {
 	changes?: BalanceChangeSummary;
 }
 
-function BalanceChangeEntry({
-	change,
-	isUnRecognizedToken,
-}: {
-	change: BalanceChange;
-	isUnRecognizedToken?: boolean;
-}) {
-	const { amount, coinType } = change;
+function BalanceChangeEntry({ change }: { change: BalanceChange }) {
+	const { amount, coinType, unRecognizedToken } = change;
 	const isPositive = BigInt(amount) > 0n;
-	const [formatted, symbol, queryResult] = useFormatCoin(amount, coinType, CoinFormat.FULL);
-
+	const [formatted, symbol] = useFormatCoin(amount, coinType, CoinFormat.FULL);
+	const { data: coinMetaData } = useCoinMetadata(coinType);
 	return (
 		<div className="flex flex-col gap-2">
-			<div className="flex flex-col gap-2">
-				{isUnRecognizedToken && (
-					<div className="border-t border-gray-45 pt-2">
-						<Text variant="pSubtitleSmall" weight="normal" color="steel-dark">
-							Coins below are not recognized by <span className="text-hero">Sui Foundation.</span>
+			<div className="flex justify-between">
+				<div className="flex gap-2">
+					<div className="w-5">
+						<CoinIcon size="sm" coinType={coinType} />
+					</div>
+					<div className="flex flex-wrap gap-2 gap-y-1 sm:max-w-full">
+						<Text variant="pBody" weight="semibold" color="steel-darker">
+							{coinMetaData?.name || symbol}
 						</Text>
+						{unRecognizedToken && (
+							<Alert mode="warning">
+								<div className="item-center block max-w-[70px] truncate break-normal text-captionSmallExtra font-medium uppercase tracking-wider sm:max-w-full">
+									Unrecognized
+								</div>
+							</Alert>
+						)}
 					</div>
-				)}
-				<div className="flex justify-between">
-					<div className="flex gap-2">
-						<div className="w-5">
-							<CoinIcon size="sm" coinType={coinType} />
-						</div>
-						<div className="flex max-w-[90px] flex-wrap gap-2 gap-y-1 sm:max-w-full">
-							<Text variant="pBody" weight="semibold" color="steel-darker">
-								{queryResult.data?.name || symbol}
-							</Text>
-							{isUnRecognizedToken && (
-								<Alert mode="warning">
-									<div className="item-center truncate break-normal text-captionSmallExtra font-medium uppercase tracking-wider">
-										Unrecognized
-									</div>
-								</Alert>
-							)}
-						</div>
-					</div>
-					<div className="flex">
-						<Text
-							variant="pBody"
-							weight="medium"
-							color={isPositive ? 'success-dark' : 'issue-dark'}
-						>
-							{isPositive ? '+' : ''}
-							{formatted} {symbol}
-						</Text>
-					</div>
+				</div>
+				<div className="flex">
+					<Text variant="pBody" weight="medium" color={isPositive ? 'success-dark' : 'issue-dark'}>
+						{isPositive ? '+' : ''}
+						{formatted} {symbol}
+					</Text>
 				</div>
 			</div>
 		</div>
@@ -77,24 +57,18 @@ function BalanceChangeEntry({
 }
 
 function BalanceChangeEntries({ changes }: { changes: BalanceChange[] }) {
-	const recognizedPackagesList = useRecognizedPackages();
-	const normalizedRecognizedPackages = useMemo(
-		() => recognizedPackagesList.map((itm) => normalizeSuiObjectId(itm)),
-		[recognizedPackagesList],
-	);
 	const { recognizedTokenChanges, unRecognizedTokenChanges } = useMemo(() => {
 		const recognizedTokenChanges = [];
 		const unRecognizedTokenChanges = [];
 		for (let change of changes) {
-			const { address: packageId } = parseStructTag(change.coinType);
-			if (normalizedRecognizedPackages.includes(packageId)) {
-				recognizedTokenChanges.push(change);
-			} else {
+			if (change.unRecognizedToken) {
 				unRecognizedTokenChanges.push(change);
+			} else {
+				recognizedTokenChanges.push(change);
 			}
 		}
 		return { recognizedTokenChanges, unRecognizedTokenChanges };
-	}, [changes, normalizedRecognizedPackages]);
+	}, [changes]);
 
 	return (
 		<div className="flex flex-col gap-2">
@@ -103,18 +77,9 @@ function BalanceChangeEntries({ changes }: { changes: BalanceChange[] }) {
 					<BalanceChangeEntry change={change} key={change.coinType + change.amount} />
 				))}
 				{unRecognizedTokenChanges.length > 0 && (
-					<div className="flex flex-col gap-2">
-						<div className="flex border-t border-gray-45 pt-2">
-							<Text variant="pSubtitleSmall" weight="medium" color="steel-dark">
-								Coins below are not recognized by <span className="text-hero">Sui Foundation.</span>
-							</Text>
-						</div>
+					<div className="flex flex-col gap-2 border-t border-gray-45 pt-2">
 						{unRecognizedTokenChanges.map((change, index) => (
-							<BalanceChangeEntry
-								change={change}
-								key={change.coinType + index}
-								isUnRecognizedToken
-							/>
+							<BalanceChangeEntry change={change} key={change.coinType + index} />
 						))}
 					</div>
 				)}
