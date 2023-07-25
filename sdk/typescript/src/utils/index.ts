e22b1ce96ceee6f3aca63b2d67e7c194ae8d8783
Michael Hayes <michael.hayes@mystenlabs.com>
2023-07-25 16:03:09 -0700

    deprecate remaining root exports

--- sdk/typescript/src/utils/index.ts
+++ sdk/typescript/src/utils/index.ts
@@ -28,3 +28,13 @@ export {
 	parseStructTag,
 	SUI_ADDRESS_LENGTH,
 };
+
+export const SUI_DECIMALS = 9;
+export const MIST_PER_SUI = BigInt(1000000000);
+
+export const MOVE_STDLIB_ADDRESS = '0x1';
+export const SUI_FRAMEWORK_ADDRESS = '0x2';
+export const SUI_SYSTEM_ADDRESS = '0x3';
+export const SUI_CLOCK_OBJECT_ID = normalizeSuiObjectId('0x6');
+export const SUI_SYSTEM_MODULE_NAME = 'sui_system';
+export const SUI_TYPE_ARG = `${SUI_FRAMEWORK_ADDRESS}::sui::SUI`;
--- sdk/typescript/src/utils/verify.ts
+++ sdk/typescript/src/utils/verify.ts
@@ -7,7 +7,7 @@ import { messageWithIntent } from '../cryptography/intent.js';
 import type { SerializedSignature } from '../cryptography/signature.js';
 import { blake2b } from '@noble/hashes/blake2b';
 import { toSingleSignaturePubkeyPair } from '../cryptography/utils.js';
-import { bcs } from '../types/sui-bcs.js';
+import { bcs } from '../bcs/index.js';
 
 // TODO: This might actually make sense to eventually move to the `Keypair` instances themselves, as
 // it could allow the Sui.js to be tree-shaken a little better, possibly allowing keypairs that are
