ab44787cd89c1787e07d39e6fd96fc3a100efe2b
William Robertson <williamrobertson@mystenlabs.com>
2023-07-26 23:42:21 -0400

    Revert "add navigate"
    This reverts commit 3f6001ab8ed0cee71e17cb37b460577d55b4b149.

--- apps/wallet/src/ui/app/pages/accounts/ProtectAccountPage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/ProtectAccountPage.tsx
@@ -1,13 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useNavigate } from 'react-router-dom';
 import { ProtectAccountForm } from '../../components/accounts/ProtectAccountForm';
 import { Heading } from '../../shared/heading';
 import { Text } from '_app/shared/text';
 
 export function ProtectAccountPage() {
-	const navigate = useNavigate();
 	return (
 		<div className="rounded-20 bg-sui-lightest shadow-wallet-content flex flex-col items-center px-6 py-10 h-full">
 			<Text variant="caption" color="steel-dark" weight="semibold">
@@ -26,7 +24,6 @@ export function ProtectAccountPage() {
 							'TODO: Do something when the user submits the form successfully',
 							formValues,
 						);
-						navigate('/');
 					}}
 				/>
 			</div>
