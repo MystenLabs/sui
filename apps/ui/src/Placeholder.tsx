81824c57432d04a964d296857ef1d83618158963
Phong Lam <phong@mystenlabs.com>
2023-07-26 20:19:58 -0700

    add loading indicator

--- apps/ui/src/Placeholder.tsx
+++ apps/ui/src/Placeholder.tsx
@@ -1,16 +1,31 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export interface PlaceholderProps {
+import { cva, VariantProps } from 'class-variance-authority';
+
+const placeholderStyle = cva(
+	'h-[1em] w-full animate-shimmer rounded-[3px] bg-placeholderShimmer bg-[length:1000px_100%]',
+	{
+		variants: {
+			rounded: {
+				'3px': 'rounded-[3px]',
+				lg: 'rounded-lg',
+				xl: 'rounded-xl',
+			},
+		},
+		defaultVariants: {
+			rounded: '3px',
+		},
+	},
+);
+
+type PlaceholderStyleProps = VariantProps<typeof placeholderStyle>;
+
+export interface PlaceholderProps extends PlaceholderStyleProps {
 	width?: string;
 	height?: string;
 }
 
-export function Placeholder({ width = '100%', height = '1em' }: PlaceholderProps) {
-	return (
-		<div
-			className="h-[1em] w-full animate-shimmer rounded-[3px] bg-placeholderShimmer bg-[length:1000px_100%]"
-			style={{ width, height }}
-		/>
-	);
+export function Placeholder({ rounded, width = '100%', height = '1em' }: PlaceholderProps) {
+	return <div className={placeholderStyle({ rounded })} style={{ width, height }} />;
 }
