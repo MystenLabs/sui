d9e632952fdfec61904adccf09e14a727749a53b
Phong Lam <phong@mystenlabs.com>
2023-07-28 11:58:40 -0700

    move network banner onto header component

--- apps/explorer/src/components/Layout/PageLayout.tsx
+++ apps/explorer/src/components/Layout/PageLayout.tsx
@@ -1,12 +1,17 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { useAppsBackend } from '@mysten/core';
 import { LoadingIndicator } from '@mysten/ui';
+import { useQuery } from '@tanstack/react-query';
 import clsx from 'clsx';
 import { type ReactNode } from 'react';
 
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
+import { useNetworkContext } from '~/context';
+import { Banner } from '~/ui/Banner';
+import { Network } from '~/utils/api/DefaultRpcClient';
 
 export type PageLayoutProps = {
 	gradient?: {
@@ -26,6 +31,19 @@ export function PageLayout({
 	isError,
 	backgroundGradient,
 }: PageLayoutProps) {
+	const [network] = useNetworkContext();
+	const { request } = useAppsBackend();
+	const { data } = useQuery({
+		queryKey: ['apps-backend', 'monitor-network'],
+		queryFn: () =>
+			request<{ degraded: boolean }>('monitor-network', {
+				project: 'EXPLORER',
+			}),
+		// Keep cached for 2 minutes:
+		staleTime: 2 * 60 * 1000,
+		retry: false,
+		enabled: network === Network.MAINNET,
+	});
 	const isGradientVisible = !!gradient;
 
 	return (
@@ -36,7 +54,15 @@ export function PageLayout({
 				isGradientVisible && backgroundGradient && !isError && 'bg-gradients-graph-cards-start',
 			)}
 		>
-			<Header />
+			<section className="sticky top-0 z-20 flex flex-col">
+				{network === Network.MAINNET && data?.degraded && (
+					<Banner rounded={false} align="center" variant="warning" fullWidth>
+						The explorer is running slower than usual. We&rsquo;re working to fix the issue and
+						appreciate your patience.
+					</Banner>
+				)}
+				<Header />
+			</section>
 			{loading && (
 				<div className="absolute left-1/2 right-0 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform justify-center">
 					<LoadingIndicator variant="lg" />
