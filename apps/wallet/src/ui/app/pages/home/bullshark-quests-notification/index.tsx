22e75a20a594b412b800732eacc29fd0239e8aa4
Nikhil-Mysten <128089541+Nikhil-Mysten@users.noreply.github.com>
2023-07-28 12:32:30 -0400

    [wallet-ext] update quests blog post link (#13202)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/pages/home/bullshark-quests-notification/index.tsx
+++ apps/wallet/src/ui/app/pages/home/bullshark-quests-notification/index.tsx
@@ -57,7 +57,7 @@ function BullsharkQuestsNotification({ onClose }: { onClose: () => void }) {
 
 						<div className="flex flex-col items-center gap-4 [-webkit-text-stroke:1px_black] w-full mt-5">
 							<ExternalLink
-								href="https://tech.mystenlabs.com/introducing-bullsharks-quests/"
+								href="https://tech.mystenlabs.com/bullshark-quest-2"
 								onClick={() => {
 									ampli.clickedBullsharkQuestsCta({ sourceFlow: 'Interstitial' });
 									closeInterstitial();
