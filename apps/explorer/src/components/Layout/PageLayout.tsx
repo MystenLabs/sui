bfdf8bf740d9e45f21d26f86f07979d37891e4c2
Phong Lam <phong@mystenlabs.com>
2023-07-28 11:57:35 -0700

    move network banner onto header component

--- apps/explorer/src/components/Layout/PageLayout.tsx
+++ apps/explorer/src/components/Layout/PageLayout.tsx
@@ -1,17 +1,12 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useAppsBackend } from '@mysten/core';
 import { LoadingIndicator } from '@mysten/ui';
-import { useQuery } from '@tanstack/react-query';
 import clsx from 'clsx';
 import { type ReactNode } from 'react';
 
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
-import { useNetworkContext } from '~/context';
-import { Banner } from '~/ui/Banner';
-import { Network } from '~/utils/api/DefaultRpcClient';
 
 export type PageLayoutProps = {
 	gradient?: {
@@ -31,19 +26,6 @@ export function PageLayout({
 	isError,
 	backgroundGradient,
 }: PageLayoutProps) {
-	const [network] = useNetworkContext();
-	const { request } = useAppsBackend();
-	const { data } = useQuery({
-		queryKey: ['apps-backend', 'monitor-network'],
-		queryFn: () =>
-			request<{ degraded: boolean }>('monitor-network', {
-				project: 'EXPLORER',
-			}),
-		// Keep cached for 2 minutes:
-		staleTime: 2 * 60 * 1000,
-		retry: false,
-		enabled: network === Network.MAINNET,
-	});
 	const isGradientVisible = !!gradient;
 
 	return (
@@ -54,16 +36,6 @@ export function PageLayout({
 				isGradientVisible && backgroundGradient && !isError && 'bg-gradients-graph-cards-start',
 			)}
 		>
-			{/*{network === Network.MAINNET && data?.degraded && (*/}
-			<div className={clsx(isGradientVisible && 'bg-gradients-graph-cards-bg')}>
-				<div className="">
-					<Banner rounded={false} align="center" variant="warning" fullWidth>
-						The explorer is running slower than usual. We&rsquo;re working to fix the issue and
-						appreciate your patience.
-					</Banner>
-				</div>
-			</div>
-			{/*)}*/}
 			<Header />
 			{loading && (
 				<div className="absolute left-1/2 right-0 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform justify-center">
--- apps/explorer/src/components/header/Header.tsx
+++ apps/explorer/src/components/header/Header.tsx
@@ -7,9 +7,15 @@ import { useEffect, useState } from 'react';
 
 import NetworkSelect from '../network/Network';
 import Search from '../search/Search';
+import { useNetworkContext } from '~/context';
+import { useGetNetworkDegrade } from '~/hooks/useGetNetworkDegrade';
+import { Banner } from '~/ui/Banner';
 import { LinkWithQuery } from '~/ui/utils/LinkWithQuery';
+import { Network } from '~/utils/api/DefaultRpcClient';
 
 function Header() {
+	const [network] = useNetworkContext();
+	const { data } = useGetNetworkDegrade();
 	const [isScrolled, setIsScrolled] = useState(window.scrollY > 0);
 	useEffect(() => {
 		const callback = () => {
@@ -23,23 +29,31 @@ function Header() {
 	return (
 		<header
 			className={clsx(
-				'sticky top-0 z-20 flex h-header justify-center overflow-visible bg-white/40 backdrop-blur-xl transition-shadow',
+				'sticky top-0 z-20 flex flex-col overflow-visible bg-white/40 backdrop-blur-xl transition-shadow',
 				isScrolled && 'shadow-mistyEdge',
 			)}
 		>
-			<div className="flex h-full max-w-[1440px] flex-1 items-center gap-5 px-5 2xl:p-0">
-				<LinkWithQuery
-					data-testid="nav-logo-button"
-					to="/"
-					className="flex flex-nowrap items-center gap-1 text-hero-darkest"
-				>
-					<Sui className="h-[26px] w-5" />
-					<SuiLogoTxt className="h-[17px] w-[27px]" />
-				</LinkWithQuery>
-				<div className="flex-1">
-					<Search />
+			{network === Network.MAINNET && data?.degraded && (
+				<Banner rounded={false} align="center" variant="warning" fullWidth>
+					The explorer is running slower than usual. We&rsquo;re working to fix the issue and
+					appreciate your patience.
+				</Banner>
+			)}
+			<div className="flex h-header justify-center">
+				<div className="flex h-full h-header max-w-[1440px] flex-1 items-center gap-5 px-5 2xl:p-0">
+					<LinkWithQuery
+						data-testid="nav-logo-button"
+						to="/"
+						className="flex flex-nowrap items-center gap-1 text-hero-darkest"
+					>
+						<Sui className="h-[26px] w-5" />
+						<SuiLogoTxt className="h-[17px] w-[27px]" />
+					</LinkWithQuery>
+					<div className="flex-1">
+						<Search />
+					</div>
+					<NetworkSelect />
 				</div>
-				<NetworkSelect />
 			</div>
 		</header>
 	);
