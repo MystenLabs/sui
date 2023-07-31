169eb75bce1e0e4fb7fc5f3fe942c22042e8678c
plam-ml <127577476+plam-ml@users.noreply.github.com>
2023-07-31 16:48:37 -0700

    [Explorer]: add gradient header for address page (#13229)
    ## Description 
    
    https://mysten.atlassian.net/browse/APPS-1517
    
    <img width="1500" alt="Screenshot 2023-07-31 at 10 08 11 AM"
    src="https://github.com/MystenLabs/sui/assets/127577476/2806ee2c-fabd-4382-94d4-7cd57103459f">
    
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/explorer/src/pages/transaction-result/TransactionResult.tsx
+++ apps/explorer/src/pages/transaction-result/TransactionResult.tsx
@@ -63,7 +63,6 @@ export default function TransactionResult() {
 				),
 				size: 'md',
 			}}
-			backgroundGradient={!!data}
 			isError={!!txnErrorText}
 			content={
 				getTxnErrorBool || !data ? (
