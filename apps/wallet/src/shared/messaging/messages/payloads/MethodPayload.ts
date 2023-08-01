7a813f49707bceb252687fe6a9d822ae63d8a911
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-08-01 15:05:10 +0300

    allow only mnemonic sources to be created from UI

--- apps/wallet/src/shared/messaging/messages/payloads/MethodPayload.ts
+++ apps/wallet/src/shared/messaging/messages/payloads/MethodPayload.ts
@@ -3,10 +3,7 @@
 
 import { type ExportedKeypair, type SerializedSignature } from '@mysten/sui.js/cryptography';
 import { isBasePayload } from './BasePayload';
-import {
-	type AccountSourceSerializedUI,
-	type AccountSourceType,
-} from '_src/background/account-sources/AccountSource';
+import { type AccountSourceSerializedUI } from '_src/background/account-sources/AccountSource';
 import { type SerializedUIAccount } from '_src/background/accounts/Account';
 
 import type { Payload } from './Payload';
@@ -17,7 +14,7 @@ type MethodPayloads = {
 	getStoredEntities: { type: UIAccessibleEntityType };
 	storedEntitiesResponse: { entities: any; type: UIAccessibleEntityType };
 	createAccountSource: {
-		type: AccountSourceType;
+		type: 'mnemonic';
 		params: {
 			password: string;
 			entropy?: string;
