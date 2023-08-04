be0748b42081fee1572caf6d6c652a7d2912b209
Phong Lam <phong@mystenlabs.com>
2023-08-03 23:08:44 -0700

    progress

--- apps/explorer/src/ui/SplitPanes.tsx
+++ apps/explorer/src/ui/SplitPanes.tsx
@@ -51,9 +51,9 @@ function ResizeHandle({
 		>
 			<div
 				className={clsx(
-					'relative bg-gray-45 group-hover/container:border-hero group-hover/container:bg-hero-dark',
+					'relative bg-gray-45 group-hover/container:bg-hero',
 					isHorizontal ? 'h-full w-px' : 'h-px',
-					noHoverHidden && !isCollapsed && 'border-transparent',
+					noHoverHidden && !isCollapsed && 'bg-transparent',
 				)}
 			>
 				{collapsibleButton && (
@@ -64,7 +64,7 @@ function ResizeHandle({
 						className={clsx([
 							'group/button',
 							'flex h-6 w-6 cursor-pointer items-center justify-center rounded-full',
-							'border-2 border-gray-45 bg-white text-gray-70 group-hover/container:bg-hero-dark',
+							'border-2 border-gray-45 bg-white text-gray-70 group-hover/container:border-hero-dark',
 							'hover:bg-hero-dark hover:text-white',
 							isHorizontal
 								? 'absolute left-1/2 top-10 -translate-x-2/4'
@@ -74,7 +74,7 @@ function ResizeHandle({
 					>
 						<ChevronButton
 							className={clsx(
-								'h-4 w-4 text-gray-45 group-hover/button:!text-white group-hover/container:text-hero',
+								'h-4 w-4 text-gray-45 group-hover/button:!text-white group-hover/container:text-hero-dark',
 								isCollapsed && 'rotate-180',
 							)}
 						/>
