973140869089a8c5029678dce768508d8b5fffcb
mamos-mysten <122397493+mamos-mysten@users.noreply.github.com>
2023-08-08 14:29:59 -0700

    [wallet-ext] update login flow naming (#13325)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/index.tsx
+++ apps/wallet/src/ui/app/index.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useFeatureIsOn } from '@growthbook/growthbook-react';
 import { useEffect } from 'react';
 import { Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom';
 
@@ -10,12 +9,12 @@ import { useStorageMigrationStatus } from './hooks/useStorageMigrationStatus';
 import { StorageMigrationPage } from './pages/StorageMigrationPage';
 import { ProtectAccountPage } from './pages/accounts/ProtectAccountPage';
 import { AccountsDev } from './pages/accounts-dev';
-import { TokensV2 } from './pages/enoki-onboarding/TokensV2';
 import AssetsPage from './pages/home/assets';
 import { QredoConnectInfoPage } from './pages/qredo-connect/QredoConnectInfoPage';
 import { SelectQredoAccountsPage } from './pages/qredo-connect/SelectQredoAccountsPage';
 import { RestrictedPage } from './pages/restricted';
 import WelcomePage from './pages/welcome';
+// import { TokensV2 } from './pages/zklogin/TokensV2';
 import { AppType } from './redux/slices/app/AppType';
 import { Staking } from './staking/home';
 import ForgotPasswordPage from '_app/wallet/forgot-password-page';
@@ -28,7 +27,6 @@ import { ImportLedgerAccountsPage } from '_pages/accounts/ImportLedgerAccountsPa
 import { ImportPassphrasePage } from '_pages/accounts/ImportPassphrasePage';
 import { ImportPrivateKeyPage } from '_pages/accounts/ImportPrivateKeyPage';
 import { ApprovalRequestPage } from '_pages/approval-request';
-import { WelcomePage as WelcomePageV2 } from '_pages/enoki-onboarding/WelcomePage';
 import HomePage, {
 	TokensPage,
 	TransactionBlocksPage,
@@ -49,6 +47,7 @@ import SelectPage from '_pages/initialize/select';
 import SiteConnectPage from '_pages/site-connect';
 import { setNavVisibility } from '_redux/slices/app';
 import { NEW_ACCOUNTS_ENABLED } from '_src/shared/constants';
+// import { WelcomePage as WelcomePageV2 } from '_src/ui/app/pages/zklogin/WelcomePage';
 
 const HIDDEN_MENU_PATHS = [
 	'/nft-details',
@@ -61,7 +60,6 @@ const HIDDEN_MENU_PATHS = [
 
 const App = () => {
 	const dispatch = useAppDispatch();
-	const isSocialSignInEnabled = useFeatureIsOn('enoki-social-sign-in');
 	const isPopup = useAppSelector((state) => state.app.appType === AppType.popup);
 	useEffect(() => {
 		document.body.classList.remove('app-initializing');
@@ -103,7 +101,7 @@ const App = () => {
 					</Route>
 				</>
 			) : null}
-			{/* <------ */}
+
 			<Route path="/*" element={<HomePage />}>
 				<Route path="tokens/*" element={<TokensPage />} />
 				<Route path="nfts/*" element={<AssetsPage />} />
@@ -120,6 +118,20 @@ const App = () => {
 				<Route path="*" element={<Navigate to="/tokens" replace={true} />} />
 			</Route>
 
+			<Route path="welcome" element={<WelcomePage />} />
+
+			{/* <Route path="welcome" element={<WelcomePageV2 />} /> */}
+			<Route path="/account">
+				<Route path="forgot-password" element={<ForgotPasswordPageV2 />} />
+			</Route>
+			<Route path="/accounts" element={<AccountsPage />}>
+				<Route path="add-account" element={<AddAccountPage />} />
+				<Route path="protect-account" element={<ProtectAccountPage />} />
+				<Route path="import-ledger-accounts" element={<ImportLedgerAccountsPage />} />
+				<Route path="import-passphrase" element={<ImportPassphrasePage />} />
+				<Route path="import-private-key" element={<ImportPrivateKeyPage />} />
+			</Route>
+
 			<Route path="/dapp/*" element={<HomePage disableNavigation />}>
 				<Route path="connect/:requestID" element={<SiteConnectPage />} />
 				<Route path="approve/:requestID" element={<ApprovalRequestPage />} />
@@ -127,24 +139,6 @@ const App = () => {
 				<Route path="qredo-connect/:id/select" element={<SelectQredoAccountsPage />} />
 			</Route>
 
-			<Route path="welcome" element={isSocialSignInEnabled ? <WelcomePageV2 /> : <WelcomePage />} />
-			{isSocialSignInEnabled && (
-				<>
-					<Route path="/*" element={<HomePage />}>
-						<Route path="home" element={<TokensV2 />} />
-					</Route>
-					<Route path="/account">
-						<Route path="forgot-password" element={<ForgotPasswordPageV2 />} />
-					</Route>
-					<Route path="/accounts" element={<AccountsPage />}>
-						<Route path="add-account" element={<AddAccountPage />} />
-						<Route path="protect-account" element={<ProtectAccountPage />} />
-						<Route path="import-ledger-accounts" element={<ImportLedgerAccountsPage />} />
-						<Route path="import-passphrase" element={<ImportPassphrasePage />} />
-						<Route path="import-private-key" element={<ImportPrivateKeyPage />} />
-					</Route>
-				</>
-			)}
 			<Route path="/initialize" element={<InitializePage />}>
 				<Route path="select" element={<SelectPage />} />
 				<Route path="create" element={<CreatePage />} />
