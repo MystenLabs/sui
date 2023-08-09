babdc0f9cb3f9b5f1fb803574510cd491d18c603
oxade <93547199+oxade@users.noreply.github.com>
2023-08-09 22:08:25 +0200

    [graphql/rpc] schema export and snapshot tests (#13339)
    ## Description 
    
    * Adds snapshot tests to ensure schema stability
    
    * Adds command to dump the schema
    
    ```
    $  ./target/debug/sui-graphql-rpc generate-schema
    
    type Query {
            chainIdentifier: String!
    }
    
    
    schema {
            query: Query
    }
    
    ```
    
    ## Test Plan 
    
    Manual
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-graphql-rpc/src/lib.rs
+++ crates/sui-graphql-rpc/src/lib.rs
@@ -1,4 +1,14 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-mod types;
+use async_graphql::*;
+
+pub mod commands;
+pub mod types;
+
+use crate::types::query::Query;
+
+pub fn schema_sdl_export() -> String {
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    schema.sdl()
+}
--- crates/sui-graphql-rpc/src/main.rs
+++ crates/sui-graphql-rpc/src/main.rs
@@ -1,4 +1,15 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-fn main() {}
+use clap::Parser;
+use sui_graphql_rpc::commands::Command;
+use sui_graphql_rpc::schema_sdl_export;
+
+fn main() {
+    let cmd: Command = Command::parse();
+    match cmd {
+        Command::GenerateSchema => {
+            println!("{}", &schema_sdl_export());
+        }
+    }
+}
