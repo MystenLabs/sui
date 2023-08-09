12e4ce2f24a691d377997db60a5da6c38def7a51
Nikhil Mattu <nikhil@mystenlabs.com>
2023-08-09 11:06:33 -0400

    [wallet-ext] new select dropdown

---
+++ apps/wallet/src/ui/app/components/SelectDropdown/index.tsx
@@ -0,0 +1,66 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { ChevronDown12 } from '@mysten/icons';
+import * as Select from '@radix-ui/react-select';
+import { Text } from '_app/shared/text';
+
+export interface SelectDropdownProps {
+	placeholder?: string;
+	dropdownOptions?: string[];
+	onValueChange?: (value: string) => void;
+	value?: string;
+}
+
+export function SelectDropdown({
+	placeholder,
+	dropdownOptions,
+	onValueChange,
+	value,
+}: SelectDropdownProps) {
+	return (
+		<div className="z-5">
+			<Select.Root onValueChange={onValueChange} value={value}>
+				<Select.Trigger
+					className="flex items-center border border-solid border-gray-45 shadow-md rounded-md bg-transparent px-4 py-2 gap-1.5 focus:outline-none"
+					aria-label="Food"
+				>
+					<Select.Value>
+						<Text variant="body" weight="semibold" color="steel">
+							{value || placeholder}
+						</Text>
+					</Select.Value>
+					<Select.Icon className="flex items-center">
+						<ChevronDown12 className="text-steel" />
+					</Select.Icon>
+				</Select.Trigger>
+				<Select.Portal>
+					<Select.Content
+						className="z-10 min-w-[100px] bg-transparent"
+						position="popper"
+						side="bottom"
+						sideOffset={-35}
+						align="end"
+					>
+						<Select.Viewport className="bg-white p-2 border border-solid border-gray-45 rounded-md shadow-md">
+							{dropdownOptions?.map((option) => {
+								return (
+									<Select.Item
+										value={option}
+										className="flex items-center hover:border-none hover:outline-none hover:cursor-pointer w-full hover:bg-hero-darkest hover:bg-opacity-5 p-2 rounded-sm"
+									>
+										<Select.ItemText>
+											<Text variant="body" weight="semibold" color="steel">
+												{option}
+											</Text>
+										</Select.ItemText>
+									</Select.Item>
+								);
+							})}
+						</Select.Viewport>
+					</Select.Content>
+				</Select.Portal>
+			</Select.Root>
+		</div>
+	);
+}
