ab7f75d97a785a097601f815d95bc194394ee0c7
Nikhil-Mysten <128089541+Nikhil-Mysten@users.noreply.github.com>
2023-08-10 11:30:22 -0400

    [wallet-ext] auto lock inputs (#13334)
    ## Description 
    
    <img width="396" alt="image"
    src="https://github.com/MystenLabs/sui/assets/128089541/f466042a-37b9-46c6-b046-9fce1cf38be6">
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ apps/wallet/src/ui/app/shared/forms/SelectField.tsx
@@ -0,0 +1,37 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { forwardRef } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './controls/Select';
+
+type SelectFieldProps = {
+	name: string;
+	options: string[];
+};
+
+export const SelectField = forwardRef<HTMLButtonElement, SelectFieldProps>(
+	({ name, options, ...props }, forwardedRef) => {
+		const { control } = useFormContext();
+		return (
+			<Controller
+				control={control}
+				name={name}
+				render={({ field }) => (
+					<Select onValueChange={field.onChange} defaultValue={field.value} {...props}>
+						<SelectTrigger ref={forwardedRef}>
+							<SelectValue />
+						</SelectTrigger>
+						<SelectContent position="popper" sideOffset={-41} align="end">
+							{options.map((option, index) => (
+								<SelectItem value={option} key={index}>
+									{option}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</Select>
+				)}
+			/>
+		);
+	},
+);
