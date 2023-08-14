3279788a26d44e4c983808a116479af192d0351f
Nikhil Mattu <nikhil@mystenlabs.com>
2023-08-14 19:00:33 -0400

    design updates

--- apps/wallet/src/ui/app/components/quests-banner/index.tsx
+++ apps/wallet/src/ui/app/components/quests-banner/index.tsx
@@ -7,11 +7,11 @@ import CapyOverlay from '_src/ui/assets/images/capy-overlay.svg';
 
 export function QuestsBanner() {
 	return (
-		<div className="flex flex-col p-2 border-2 border-red border-solid mb-3 rounded-lg bg-brand-buttercup relative">
-			<div className="flex flex-col font-arialRoundedBold w-full border-2 border-black border-solid bg-[#99DBFB] p-4 rounded-lg items-center text-white">
+		<div className="flex flex-col p-1 border-2 border-red border-solid mb-3 rounded-[12px] bg-brand-buttercup relative">
+			<div className="flex flex-col font-arialRoundedBold w-full border-2 border-black border-solid bg-[#99DBFB] p-3 rounded-lg items-center text-white">
 				<div className="text-heading6 text-black tracking-[0.42px] uppercase">Join quests 2!</div>
 				<div
-					className="text-heading3 font-frankfurter mt-2"
+					className="text-[22px] font-frankfurter mt-2"
 					style={{
 						WebkitTextStroke: '1px black',
 					}}
@@ -21,15 +21,15 @@ export function QuestsBanner() {
 				<ExternalLink
 					href="https://tech.mystenlabs.com/bullshark-quest-2"
 					onClick={() => ampli.clickedBullsharkQuestsCta({ sourceFlow: 'Banner - Apps tab' })}
-					className="flex text-black appearance-none no-underline bg-brand-avocado-500 rounded-[36px] pt-2.5 pb-2 px-3 text-[16px] border-[3px] border-solid border-black items-center text-center mt-6 uppercase tracking-[0.42px]"
+					className="flex text-black appearance-none no-underline bg-brand-avocado-500 rounded-[36px] pt-2.5 pb-2 px-4 text-caption border-[3px] border-solid border-black items-center text-center mt-6 uppercase tracking-[0.42px]"
 				>
 					Read the blog
 				</ExternalLink>
 			</div>
-			<div className="flex items-center justify-center absolute bottom-[3px] left-[-1px]">
+			<div className="flex items-center justify-center absolute bottom-[-1px] left-[-1px]">
 				<CapyOverlay role="img" />
 			</div>
-			<div className="flex items-center justify-center absolute bottom-[2.5px] right-0">
+			<div className="flex items-center justify-center absolute bottom-[-1.5px] right-0">
 				<BullsharkOverlay role="img" />
 			</div>
 		</div>
