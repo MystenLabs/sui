6a439d52ee68fa3eef75f35aba40eb5419fac8c0
wlmyng <127570466+wlmyng@users.noreply.github.com>
2023-08-17 16:34:17 -0700

    [graphql] unit tests for scalar types (#13447)

--- crates/sui-graphql-rpc/src/types/base64.rs
+++ crates/sui-graphql-rpc/src/types/base64.rs
@@ -10,18 +10,14 @@ use fastcrypto::encoding::Encoding as FastCryptoEncoding;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub(crate) struct Base64(Vec<u8>);
 
-// TODO: implement Base64 scalar type
-// TODO: unit tests
 #[Scalar]
 impl ScalarType for Base64 {
     fn parse(value: Value) -> InputValueResult<Self> {
-        // TODO: improve errors
         match value {
-            Value::String(s) => {
-                Ok(Base64(FastCryptoBase64::decode(&s).map_err(|r| {
-                    InputValueError::custom(format!("Invalid Base64: {}", r))
-                })?))
-            }
+            Value::String(s) => Ok(Base64(
+                FastCryptoBase64::decode(&s)
+                    .map_err(|r| InputValueError::custom(format!("{r}")))?,
+            )),
             _ => Err(InputValueError::expected_type(value)),
         }
     }
@@ -50,5 +46,81 @@ impl From<&Vec<u8>> for Base64 {
 impl From<Vec<u8>> for Base64 {
     fn from(bytes: Vec<u8>) -> Self {
         Base64::from(&bytes)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async_graphql::Value;
+
+    fn assert_input_value_error<T, U>(result: Result<T, InputValueError<U>>) {
+        match result {
+            Err(InputValueError { .. }) => {}
+            _ => panic!("Expected InputValueError"),
+        }
+    }
+
+    #[test]
+    fn test_parse_valid_base64() {
+        let input = Value::String("SGVsbG8gd29ybGQ=".to_string());
+        let parsed = <Base64 as ScalarType>::parse(input).unwrap();
+        assert_eq!(parsed.0, b"Hello world");
+    }
+
+    #[test]
+    fn test_parse_invalid_base64() {
+        let input = Value::String("SGVsbG8gd29ybGQ@".to_string());
+        let parsed = <Base64 as ScalarType>::parse(input);
+        assert_input_value_error(parsed);
+    }
+
+    #[test]
+    fn test_parse_invalid_boolean_value() {
+        let input = Value::Boolean(true);
+        let parsed = <Base64 as ScalarType>::parse(input);
+        assert_input_value_error(parsed);
+    }
+
+    #[test]
+    fn test_parse_invalid_number() {
+        let input = Value::Number(1.into());
+        let parsed = <Base64 as ScalarType>::parse(input);
+        assert_input_value_error(parsed);
+    }
+
+    #[test]
+    fn test_to_value() {
+        let base64 = Base64(b"Hello world".to_vec());
+        let value = <Base64 as ScalarType>::to_value(&base64);
+        assert_eq!(value, Value::String("SGVsbG8gd29ybGQ=".to_string()));
+    }
+
+    #[test]
+    fn test_from_str_valid() {
+        let base64_str = "SGVsbG8gd29ybGQ=";
+        let base64 = Base64::from_str(base64_str).unwrap();
+        assert_eq!(base64.0, b"Hello world");
+    }
+
+    #[test]
+    fn test_from_str_invalid() {
+        let base64_str = "SGVsbG8gd29ybGQ@";
+        let result = Base64::from_str(base64_str);
+        assert_input_value_error(result);
+    }
+
+    #[test]
+    fn test_from_vec_reference() {
+        let vec = vec![1, 2, 3, 4, 5];
+        let base64 = Base64::from(&vec);
+        assert_eq!(base64.0, vec);
+    }
+
+    #[test]
+    fn test_from_vec() {
+        let vec = vec![1, 2, 3, 4, 5];
+        let base64 = Base64::from(vec.clone());
+        assert_eq!(base64.0, vec);
     }
 }
