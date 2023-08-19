97da156eaa5ebf95e12458248d1f235fb7da44bd
oxade <93547199+oxade@users.noreply.github.com>
2023-08-19 20:55:13 +0200

    [graphql/rpc] Graphql impl date time scalar (#13471)
    ## Description 
    
    `DateTime` scalar and concrete `NameService` type
    
    ## Test Plan 
    
    Unit, manual
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-graphql-rpc/src/types/date_time.rs
+++ crates/sui-graphql-rpc/src/types/date_time.rs
@@ -1,21 +1,65 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::{fmt::Display, str::FromStr};
+
 use async_graphql::*;
+use chrono::{
+    prelude::{DateTime as ChronoDateTime, Utc as ChronoUtc},
+    ParseError as ChronoParseError, TimeZone,
+};
 
-// ISO-8601 Date and Time
-// Encoded as a 64-bit unix timestamp
-struct DateTime(u64);
+// ISO-8601 Date and Time: RFC3339 in UTC
+// YYYY-MM-DDTHH:MM:SS.mmmZ
+// Encoded as a 64-bit unix timestamp nanoseconds
+struct DateTime(i64);
 
-// TODO: unit tests
-// TODO: implement DateTime scalar type
 #[Scalar]
 impl ScalarType for DateTime {
-    fn parse(_value: Value) -> InputValueResult<Self> {
-        unimplemented!()
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::String(s) => DateTime::from_str(&s)
+                .map_err(|e| InputValueError::custom(format!("Error parsing DateTime: {}", e))),
+            _ => Err(InputValueError::expected_type(value)),
+        }
     }
 
     fn to_value(&self) -> Value {
-        unimplemented!()
+        Value::String(format!("{}", self))
+    }
+}
+
+impl FromStr for DateTime {
+    type Err = ChronoParseError;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        Ok(DateTime(
+            s.parse::<ChronoDateTime<ChronoUtc>>()?.timestamp_nanos(),
+        ))
+    }
+}
+
+impl Display for DateTime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(&format!("{:?}", ChronoUtc.timestamp_nanos(self.0)), f)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let dt: &str = "2023-08-19T15:37:24.761850Z";
+        let date_time = DateTime::from_str(dt).unwrap();
+        assert_eq!(dt, &format!("{}", date_time));
+
+        let dt: &str = "2023-08-19T15:37:24.700Z";
+        let date_time = DateTime::from_str(dt).unwrap();
+        assert_eq!(dt, &format!("{}", date_time));
+
+        let dt: &str = "2023-08-";
+        assert!(DateTime::from_str(dt).is_err());
     }
 }
