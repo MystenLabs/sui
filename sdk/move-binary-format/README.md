db85853d9a62787b994bbfb97cdcb768b8caa7e0
Damir Shamanaev <damirka.ru@gmail.com>
2023-08-21 19:55:56 +0300

    we can now update identifiers

--- sdk/move-binary-format/README.md
+++ sdk/move-binary-format/README.md
@@ -21,7 +21,7 @@ console.assert(json == bytes, '(de)serialization failed!');
 
 ## Build locally
 
-To build the binary, you need to install the `wasm-pack`. The installation script [can be found here](https://rustwasm.github.io/wasm-pack/).
+To build the binary, you need to have Rust installed and then the `wasm-pack`. The installation script [can be found here](https://rustwasm.github.io/wasm-pack/).
 
 Building for test (nodejs) environment - required for tests.
 ```
@@ -31,4 +31,13 @@ pnpm build:dev
 Building for web environment.
 ```
 pnpm build:release
+```
+
+## Running tests
+
+Local tests can only be run on the `dev` build. To run tests, follow these steps:
+
+```
+pnpm build:dev
+pnpm test
 ```
--- sdk/move-binary-format/src/lib.rs
+++ sdk/move-binary-format/src/lib.rs
@@ -1,7 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use move_binary_format::CompiledModule;
+use std::collections::HashMap;
+
+use move_core_types::identifier::Identifier;
+use move_binary_format::{CompiledModule, file_format::StructFieldInformation};
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::to_value;
 use wasm_bindgen::{prelude::*, JsValue};
@@ -21,6 +24,78 @@ pub fn deserialize(binary: String) -> Result<JsValue, JsErr> {
     let compiled_module = CompiledModule::deserialize_with_defaults(&bytes[..])?;
     let serialized = serde_json::to_string(&compiled_module)?;
     Ok(to_value(&serialized)?)
+}
+
+#[wasm_bindgen]
+/// Perform an operation on a bytecode string - deserialize, patch the identifiers
+/// and serialize back to a bytecode string.
+pub fn update_identifiers(binary: String, map: JsValue) -> Result<JsValue, JsErr> {
+    let bytes = hex::decode(binary)?;
+    let updates: HashMap<String, String> = serde_wasm_bindgen::from_value(map)?;
+    let mut compiled_module = CompiledModule::deserialize_with_defaults(&bytes[..])?;
+
+    // First update the identifiers.
+    for ident in compiled_module.identifiers.iter_mut() {
+        let old = ident.to_string();
+        if let Some(new) = updates.get(&old) {
+            *ident = Identifier::new(new.clone()).map_err(|err| {
+                JsErr {
+                    display: format!("{}", err),
+                    message: err.to_string(),
+                }
+            })?;
+        }
+    }
+
+    // Then sort and collect updated indexes.
+    let mut indexes: Vec<usize> = (0..compiled_module.identifiers.len()).collect();
+    indexes.sort_by_key(|x| &compiled_module.identifiers[*x]);
+    compiled_module.identifiers.sort();
+
+    // Then create a function to find the new index of an identifier.
+    let find_pos = |a: u16| indexes.iter().position(|x| *x == a as usize).unwrap() as u16;
+
+    // Then update the rest of the struct.
+    compiled_module.module_handles
+        .iter_mut()
+        .for_each(|handle| {
+            handle.name.0 = find_pos(handle.name.0);
+        });
+
+    compiled_module.struct_handles
+        .iter_mut()
+        .for_each(|handle| {
+            handle.name.0 = find_pos(handle.name.0);
+        });
+
+    compiled_module.function_handles
+        .iter_mut()
+        .for_each(|handle| {
+            handle.name.0 = find_pos(handle.name.0);
+        });
+
+    compiled_module.struct_defs
+        .iter_mut()
+        .for_each(|def| {
+            def.struct_handle.0 = find_pos(def.struct_handle.0);
+            if let StructFieldInformation::Declared(definitions) = &mut def.field_information {
+                definitions
+                    .iter_mut()
+                    .for_each(|field| {
+                        field.name.0 = find_pos(field.name.0);
+                    });
+            }
+        });
+
+    let mut binary = Vec::new();
+    compiled_module
+        .serialize(&mut binary)
+        .map_err(|err| JsErr {
+            display: format!("{}", err),
+            message: err.to_string(),
+        })?;
+
+    Ok(to_value(&hex::encode(binary))?)
 }
 
 #[wasm_bindgen]
