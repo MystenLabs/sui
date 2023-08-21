194d63a2cb4d2fb464d0693f76cab217a8c93b3e
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2023-08-21 12:29:21 +0200

    wallet-ext: zk onboarding (#13403)
    ## Description 
    
    enables onboarding with zk Google account
    
    
    
    https://github.com/MystenLabs/sui/assets/10210143/8faf7a3f-e3cd-40e9-a9a0-eac65093cc92
    
    part of APPS-1501
    
    ## Test Plan 
    
    👀 
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/pages/WelcomePage.tsx
+++ apps/wallet/src/ui/app/pages/WelcomePage.tsx
@@ -1,7 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { SocialButton } from '../../shared/SocialButton';
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useCreateAccountsMutation } from '../hooks/useCreateAccountMutation';
+import { SocialButton } from '../shared/SocialButton';
+import { Toaster } from '../shared/toaster';
 import { Button } from '_app/shared/ButtonUI';
 import { Heading } from '_app/shared/heading';
 import { Text } from '_app/shared/text';
@@ -9,19 +13,22 @@ import Loading from '_components/loading';
 import Logo from '_components/logo';
 import { useInitializedGuard } from '_hooks';
 import PageLayout from '_pages/layout';
+import { type ZkProvider } from '_src/background/accounts/zk/providers';
 import { ampli } from '_src/shared/analytics/ampli';
 import WelcomeSplash from '_src/ui/assets/images/WelcomeSplash.svg';
 
 export function WelcomePage() {
-	const checkingInitialized = useInitializedGuard(false);
+	const isInitializedLoading = useInitializedGuard(false);
+	const createAccountsMutation = useCreateAccountsMutation();
+	const [createInProgressProvider, setCreateInProgressProvider] = useState<ZkProvider | null>(null);
 	return (
 		<PageLayout forceFullscreen>
-			<Loading loading={checkingInitialized}>
-				<div className="rounded-20 bg-sui-lightest shadow-wallet-content flex flex-col items-center p-10 h-full">
+			<Loading loading={isInitializedLoading}>
+				<div className="rounded-20 bg-sui-lightest shadow-wallet-content flex flex-col items-center px-7 py-6 h-full overflow-auto">
 					<div className="shrink-0">
 						<Logo />
 					</div>
-					<div className="text-center mx-auto mt-4">
+					<div className="text-center mx-auto mt-2">
 						<Heading variant="heading2" color="gray-90" as="h1" weight="bold">
 							Welcome to Sui Wallet
 						</Heading>
@@ -31,10 +38,10 @@ export function WelcomePage() {
 							</Text>
 						</div>
 					</div>
-					<div className="w-full h-full mt-10">
+					<div className="w-full h-full mt-3.5 flex justify-center">
 						<WelcomeSplash role="img" />
 					</div>
-					<div className="flex flex-col gap-4 mt-7.5 w-full items-center">
+					<div className="flex flex-col gap-3 mt-3.5 w-full items-center">
 						<Text variant="pBody" color="steel-dark" weight="medium">
 							Sign in with your preferred service
 						</Text>
@@ -43,13 +50,31 @@ export function WelcomePage() {
 								<SocialButton
 									provider="google"
 									onClick={() => {
-										// eslint-disable-next-line no-console
-										console.log('TODO: Open OAuth flow');
+										setCreateInProgressProvider('Google');
 										ampli.clickedSocialSignInButton({
 											signInProvider: 'Google',
 											sourceFlow: 'Onboarding',
 										});
+										createAccountsMutation.mutate(
+											{
+												type: 'zk',
+												provider: 'Google',
+											},
+											{
+												onError: (error) => {
+													toast.error(
+														(error as Error)?.message ||
+															'Failed to create account. (Unknown error)',
+													);
+												},
+												onSettled: () => {
+													setCreateInProgressProvider(null);
+												},
+											},
+										);
 									}}
+									disabled={createAccountsMutation.isLoading}
+									loading={createInProgressProvider === 'Google'}
 								/>
 							</div>
 							<div className="flex-1">
@@ -63,6 +88,7 @@ export function WelcomePage() {
 											sourceFlow: 'Onboarding',
 										});
 									}}
+									disabled
 								/>
 							</div>
 							<div className="flex-1">
@@ -76,6 +102,7 @@ export function WelcomePage() {
 											sourceFlow: 'Onboarding',
 										});
 									}}
+									disabled
 								/>
 							</div>
 							<div className="flex-1">
@@ -89,6 +116,7 @@ export function WelcomePage() {
 											sourceFlow: 'Onboarding',
 										});
 									}}
+									disabled
 								/>
 							</div>
 						</div>
@@ -97,10 +125,12 @@ export function WelcomePage() {
 							size="tall"
 							variant="secondary"
 							text="More Options"
+							disabled
 						/>
 					</div>
 				</div>
 			</Loading>
+			<Toaster />
 		</PageLayout>
 	);
 }
