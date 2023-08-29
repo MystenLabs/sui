5cd3ad85ea469cb1ef1387dd56ab7de02d1b4367
Brandon Williams <brandon@mystenlabs.com>
2023-08-29 16:57:52 -0500

    m

--- crates/sui-rest-api/src/checkpoints.rs
+++ crates/sui-rest-api/src/checkpoints.rs
@@ -149,10 +149,15 @@ pub struct CheckpointData {
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CheckpointTransaction {
+    /// The input Transaction
     pub transaction: Transaction,
+    /// The effects produced by executing this transaction
     pub effects: TransactionEffects,
+    /// The events, if any, emitted by this transaciton during execution
     pub events: Option<TransactionEvents>,
+    /// The state of all inputs to this transaction as they were prior to execution.
     pub input_objects: Vec<Object>,
+    /// The state of all output objects created or mutated by this transaction.
     pub output_objects: Vec<Object>,
 }
 
