ba1c8db4907f177e242c5c19eb851022bd44c8fa
Nikhil Mattu <nikhil@mystenlabs.com>
2023-08-29 10:25:07 -0400

    fixed

--- apps/wallet/src/ui/app/components/accounts/EditNickname.tsx
+++ apps/wallet/src/ui/app/components/accounts/EditNickname.tsx
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useZodForm } from '@mysten/core';
-import { useCallback } from 'react';
 import toast from 'react-hot-toast';
 import { useNavigate, useParams } from 'react-router-dom';
 import { z } from 'zod';
