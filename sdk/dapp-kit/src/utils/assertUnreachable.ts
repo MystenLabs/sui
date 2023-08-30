accb66bf814e8a2136c71fbe5f8003024344aa3b
William Robertson <williamrobertson@mystenlabs.com>
2023-08-30 14:28:45 -0400

    doc

--- sdk/dapp-kit/src/utils/assertUnreachable.ts
+++ sdk/dapp-kit/src/utils/assertUnreachable.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /**
- * 
+ * Utility for compile-time exhaustiveness checking.
  */
 export function assertUnreachable(value: never): never {
 	throw new Error(`ERROR! Encountered an unexpected value: ${value}`);
