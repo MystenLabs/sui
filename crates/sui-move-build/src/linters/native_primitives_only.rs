644ace38e1afbf66fab1beab51b2fd5a4e001f8b
StefanoDalMas <stefano.dalmas01@gmail.com>
2023-08-30 10:33:42 +0200

    fixed .move file, removed useless imports and some suggestion implemented

--- crates/sui-move-build/src/linters/native_primitives_only.rs
+++ crates/sui-move-build/src/linters/native_primitives_only.rs
@@ -4,23 +4,17 @@
 //! This analysis flags uses of the sui::coin::Coin struct in fields of other structs. In most cases
 //! it's preferable to use sui::balance::Balance instead to save space.
 
-use move_command_line_common::{address::NumericalAddress, parser::NumberFormat};
 use move_compiler::{
     diag,
     diagnostics::codes::{custom, DiagnosticInfo, Severity},
-    expansion::ast as E,
     naming::ast as N,
-    shared::{CompilationEnv, Identifier},
+    shared::CompilationEnv,
     typing::{ast as T, core::ProgramInfo, visitor::TypingVisitor},
 };
-use move_core_types::account_address::AccountAddress;
 use move_ir_types::location::Loc;
 use move_symbol_pool::Symbol;
 
-use super::{
-    LinterDiagCategory, COIN_MOD_NAME, COIN_STRUCT_NAME, LINTER_DEFAULT_DIAG_CODE,
-    LINT_WARNING_PREFIX, SUI_PKG_NAME,
-};
+use super::{LinterDiagCategory, LINTER_DEFAULT_DIAG_CODE, LINT_WARNING_PREFIX};
 
 const NATIVE_PRIMITIVE_DIAG: DiagnosticInfo = custom(
     LINT_WARNING_PREFIX,
@@ -39,7 +33,7 @@ impl TypingVisitor for NativePrimitivesOnlyVisitor {
         _program_info: &ProgramInfo,
         program: &mut T::Program,
     ) {
-        for (_, _, mdef) in program.modules.iter() {
+        for (_, _, mdef) in &program.modules {
             env.add_warning_filter_scope(mdef.warning_filter.clone());
             mdef.functions
                 .iter()
@@ -51,44 +45,27 @@ impl TypingVisitor for NativePrimitivesOnlyVisitor {
 
 fn check_native(env: &mut CompilationEnv, fname: Symbol, fun: &T::Function, sloc: Loc) {
     match fun.body.value {
-        T::FunctionBody_::Defined(_) => {
+        T::FunctionBody_::Defined(_) => {}
+        T::FunctionBody_::Native => {
             let parameters = &fun.signature.parameters;
-            for (_var, data) in parameters.iter() {
-                if !is_native_or_ref(data.value.clone()) {
+            for (var, type_) in parameters.iter() {
+                if !is_native_or_ref(&type_.value) {
                     //This is a non-native type, so we prepare the message
-                    let c = _var.value.name;
+                    let c = var.value.name;
                     let msg =
                         format!("The parameter '{c}' of '{fname}' is not native or a reference");
                     let uid_msg = "Only native types or references are allowed";
-                    let mut d = diag!(NATIVE_PRIMITIVE_DIAG, (sloc, msg),);
+                    let mut d = diag!(NATIVE_PRIMITIVE_DIAG, (sloc, msg));
                     d.add_note(uid_msg);
                     env.add_diag(d);
 
-                    println!("Object in definition, time to shoot");
+                    // println!("Object in definition, time to shoot");
                 }
             }
         }
-        T::FunctionBody_::Native => {
-            //Uncomment and comment the other piece of code to try it out
-            // let parameters = &fun.signature.parameters;
-            // for (_var, data) in parameters.iter() {
-            //     if !is_native_or_ref(data.value.clone()) {
-            //         //This is a non-native type, so we prepare the message
-            //         let c = _var.value.name;
-            //         let msg =
-            //             format!("The parameter '{c}' of '{fname}' is not native or a reference");
-            //         let uid_msg = "Only native types or references are allowed";
-            //         let mut d = diag!(NATIVE_PRIMITIVE_DIAG, (sloc, msg),);
-            //         d.add_note(uid_msg);
-            //         env.add_diag(d);
-            //
-            //         println!("Object in definition, time to shoot");
-            //     }
-            // }
-        }
     }
 }
-fn is_native_or_ref(element: N::Type_) -> bool {
+fn is_native_or_ref(element: &N::Type_) -> bool {
     match element {
         N::Type_::Apply(_option, typename, _stype) => match typename.value {
             N::TypeName_::Builtin(_) => {
--- crates/sui-move-build/tests/linter/native_primitives_only.move
+++ crates/sui-move-build/tests/linter/native_primitives_only.move
@@ -1,20 +1,18 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 
-#[allow(unused_variable)]
-module 0x42::test1{
+module 0x42::test1 {
     //has copy needed because it's a vector
 
     struct S1 has copy{}
     
-    struct Coolstruct has copy,drop{
+    struct Coolstruct has copy ,drop {
         a: bool,
         b: u64,
     }
 
     #[allow(unused_function)]
-    fun returns_something(a:bool,b:u64,c:Coolstruct,d:&Coolstruct) : (bool,u64){
-        let x = b;
-        (a,x)
-    }
+    native fun returns_something(a:bool,b:u64,c:Coolstruct,d:&Coolstruct) : (bool,u64);
 
     public entry fun main(){
         let (_x,_y) = returns_something(true,42,Coolstruct{a:true,b:42},&Coolstruct{a:true,b:42});
