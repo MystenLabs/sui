98b87f393a78cb0be11e90adde1eed54cd5c9016
Blaize Security <104074712+blaize-security@users.noreply.github.com>
2023-08-31 22:46:25 +0300

    test(unit): minor refactoring

--- sdk/typescript/test/unit/cryptography/publickey.test.ts
+++ sdk/typescript/test/unit/cryptography/publickey.test.ts
@@ -56,15 +56,13 @@ describe('Publickey', () => {
 	});
 
 	it('`toBase64()` should return a valid base-64 representation', async () => {
-		expect(pk2.toBase64())
-			.toEqual(toB64(pk2.toRawBytes()))
-			.toEqual('Ah0VIwfGtysO0EGLDnDNgOf1KVuNhvVyLT9SE/vSOU82');
+		expect(pk2.toBase64()).toEqual(toB64(pk2.toRawBytes()));
+		expect(pk2.toBase64()).toEqual('Ah0VIwfGtysO0EGLDnDNgOf1KVuNhvVyLT9SE/vSOU82');
 	});
 
 	it('`toSuiPublicKey()` should return a valid sui representation', async () => {
-		expect(pk2.toSuiPublicKey())
-			.toEqual(toB64(pk2.toSuiBytes()))
-			.toEqual('AQIdFSMHxrcrDtBBiw5wzYDn9SlbjYb1ci0/UhP70jlPNg==');
+		expect(pk2.toSuiPublicKey()).toEqual(toB64(pk2.toSuiBytes()));
+		expect(pk2.toSuiPublicKey()).toEqual('AQIdFSMHxrcrDtBBiw5wzYDn9SlbjYb1ci0/UhP70jlPNg==');
 	});
 
 	it('`verifyWithIntent()` should correctly verify a signed message', async () => {
@@ -113,8 +111,8 @@ describe('Publickey', () => {
 		pk1SuiBytes.set([0x00]);
 		pk1SuiBytes.set(pk1.toRawBytes(), 1);
 
+		expect(pk1.toSuiBytes()).toEqual(pk1SuiBytes);
 		expect(pk1.toSuiBytes())
-			.toEqual(pk1SuiBytes)
 			.toEqual(
 				new Uint8Array([
 					0, 90, 226, 32, 180, 178, 246, 94, 151, 124, 18, 237, 230, 21, 121, 255, 81, 112, 182,
@@ -126,8 +124,8 @@ describe('Publickey', () => {
 		pk2SuiBytes.set([0x01]);
 		pk2SuiBytes.set(pk2.toRawBytes(), 1);
 
+		expect(pk2.toSuiBytes()).toEqual(pk2SuiBytes);
 		expect(pk2.toSuiBytes())
-			.toEqual(pk2SuiBytes)
 			.toEqual(
 				new Uint8Array([
 					1, 2, 29, 21, 35, 7, 198, 183, 43, 14, 208, 65, 139, 14, 112, 205, 128, 231, 245, 41, 91,
@@ -139,8 +137,8 @@ describe('Publickey', () => {
 		pk3SuiBytes.set([0x02]);
 		pk3SuiBytes.set(pk3.toRawBytes(), 1);
 
+		expect(pk3.toSuiBytes()).toEqual(pk3SuiBytes);
 		expect(pk3.toSuiBytes())
-			.toEqual(pk3SuiBytes)
 			.toEqual(
 				new Uint8Array([
 					2, 2, 39, 50, 43, 58, 137, 26, 10, 40, 13, 107, 193, 251, 44, 187, 35, 210, 143, 84, 144,
@@ -159,14 +157,13 @@ describe('Publickey', () => {
 		const pk3SuiAddress = normalizeSuiAddress(
 			bytesToHex(blake2b(pk3.toSuiBytes(), { dkLen: 32 })).slice(0, SUI_ADDRESS_LENGTH * 2),
 		);
-		expect(k1.toSuiAddress())
-			.toEqual(pk1SuiAddress)
-			.toEqual('0xafedf3bc60bd296aa6830d7c48ca44e0f7a32478ae4bd7b9a6ac1dc81ff7b29b');
-		expect(k2.toSuiAddress())
-			.toEqual(pk2SuiAddress)
-			.toEqual('0x7e4f9a35bf3b5383802d990956d6f3c93e6184ebbbcf0820c124ab3a59ef77ac');
-		expect(k3.toSuiAddress())
-			.toEqual(pk3SuiAddress)
-			.toEqual('0x318f591092f10b67a81963954fb9539ea3919444417726be4e1b95ce44fe2fc0');
+		expect(k1.toSuiAddress()).toEqual(pk1SuiAddress);
+		expect(k1.toSuiAddress()).toEqual('0xafedf3bc60bd296aa6830d7c48ca44e0f7a32478ae4bd7b9a6ac1dc81ff7b29b');
+
+		expect(k2.toSuiAddress()).toEqual(pk2SuiAddress);
+		expect(k2.toSuiAddress()).toEqual('0x7e4f9a35bf3b5383802d990956d6f3c93e6184ebbbcf0820c124ab3a59ef77ac');
+		
+		expect(k3.toSuiAddress()).toEqual(pk3SuiAddress);
+		expect(k3.toSuiAddress()).toEqual('0x318f591092f10b67a81963954fb9539ea3919444417726be4e1b95ce44fe2fc0');
 	});
 });
