8ed2ed71d4f0657ad2fb4403b1572a55ced4648d
Phong Lam <phong@mystenlabs.com>
2023-09-01 15:09:38 -0700

    progress

--- apps/explorer/src/components/Object/ObjectFieldsCard.tsx
+++ apps/explorer/src/components/Object/ObjectFieldsCard.tsx
@@ -7,11 +7,12 @@ import { Search24 } from '@mysten/icons';
 import { type SuiMoveNormalizedType } from '@mysten/sui.js/client';
 import { Text, LoadingIndicator, Combobox, ComboboxInput, ComboboxList, Button } from '@mysten/ui';
 import clsx from 'clsx';
-import { useEffect, useState } from 'react';
+import { type ReactNode, useEffect, useState } from 'react';
 
 import { FieldItem } from './FieldItem';
 import { ScrollToViewCard } from './ScrollToViewCard';
 import { getFieldTypeValue } from './utils';
+import { FieldsCollapsible } from '~/components/Object/FieldsCollapsible';
 import { Banner } from '~/ui/Banner';
 import { Card } from '~/ui/Card';
 import { DescriptionItem } from '~/ui/DescriptionList';
@@ -20,34 +21,12 @@ import { TabHeader } from '~/ui/Tabs';
 import { ListItem, VerticalList } from '~/ui/VerticalList';
 import { CollapsibleSection } from '~/ui/collapsible/CollapsibleSection';
 
-function ObjectFieldsCollapsibleSection({
-	name,
-	fieldValue,
-	objectType,
-	type,
-}: {
-	name: string;
-	fieldValue: string | number | object;
-	objectType: string;
-	type: SuiMoveNormalizedType;
-}) {
-	const [open, setOpen] = useState(true);
-
-	return (
-		<div className={clsx(open ? 'mb-10' : 'mb-4')}>
-			<CollapsibleSection title={name} onOpenChange={setOpen}>
-				<FieldItem value={fieldValue} objectType={objectType} type={type} />
-			</CollapsibleSection>
-		</div>
-	);
-}
-
 interface ObjectFieldsProps {
 	id: string;
-	setFieldsCount: (count: number) => void;
+	setCount: (count: number) => void;
 }
 
-export function ObjectFieldsCard({ id, setFieldsCount }: ObjectFieldsProps) {
+export function ObjectFieldsCard({ id, setCount }: ObjectFieldsProps) {
 	const { data, isLoading, isError } = useGetObject(id);
 	const [query, setQuery] = useState('');
 	const [activeFieldName, setActiveFieldName] = useState('');
@@ -82,9 +61,9 @@ export function ObjectFieldsCard({ id, setFieldsCount }: ObjectFieldsProps) {
 
 	useEffect(() => {
 		if (normalizedStruct?.fields) {
-			setFieldsCount(normalizedStruct.fields.length);
+			setCount(normalizedStruct.fields.length);
 		}
-	}, [normalizedStruct?.fields, setFieldsCount]);
+	}, [normalizedStruct?.fields, setCount]);
 
 	if (isLoading || loadingNormalizedStruct) {
 		return (
@@ -119,105 +98,66 @@ export function ObjectFieldsCard({ id, setFieldsCount }: ObjectFieldsProps) {
 	}
 
 	return (
-		<div className="mt-4 flex flex-col gap-5 overflow-auto rounded-xl border border-gray-45 bg-objectCard py-6 pl-6 pr-4">
-			<div className="flex flex-col gap-10 md:flex-row md:flex-nowrap">
-				<div className="w-full md:w-1/5">
-					<Combobox value={query} onValueChange={setQuery}>
-						<div className="flex w-full justify-between rounded-lg border border-white/50 bg-white py-1 pl-3 shadow-dropdownContent">
-							<ComboboxInput placeholder="Search" className="w-full border-none focus:outline-0" />
-							<button className="border-none bg-inherit pr-2" type="submit">
-								<Search24 className="h-4.5 w-4.5 cursor-pointer fill-steel align-middle text-gray-60" />
-							</button>
-						</div>
-						<ComboboxList
-							showResultsCount
-							options={filteredFieldNames.map((item) => ({
-								value: item.name,
-								label: item.name,
-							}))}
-							onSelect={({ value }) => {
-								setActiveFieldName(value);
-							}}
-						/>
-					</Combobox>
-					<div className="max-h-600 overflow-y-auto overflow-x-clip py-4.5">
-						<VerticalList>
-							<div className="flex flex-col gap-4 pl-3">
-								{normalizedStruct?.fields?.map(({ name, type }) => (
-									<button
-										type="button"
-										key={name}
-										className="mt-0.5"
-										// active={activeFieldName === name}
-										onClick={() => setActiveFieldName(name)}
-									>
-										<DescriptionItem
-											descriptionJustify="end"
-											labelWidth="md"
-											title={
-												<Text variant="body/medium" color="steel-darker">
-													{name}
-												</Text>
-											}
-										>
-											<Text uppercase variant="subtitle/normal" color="steel" truncate>
-												{getFieldTypeValue(type, objectType).displayName}
-											</Text>
-										</DescriptionItem>
-									</button>
-								))}
-							</div>
-						</VerticalList>
+		<div className="flex flex-col gap-10 md:flex-row md:flex-nowrap">
+			<div className="w-full md:w-1/5">
+				<Combobox value={query} onValueChange={setQuery}>
+					<div className="flex w-full justify-between rounded-lg border border-white/50 bg-white py-1 pl-3 shadow-dropdownContent">
+						<ComboboxInput placeholder="Search" className="w-full border-none focus:outline-0" />
+						<button className="border-none bg-inherit pr-2" type="submit">
+							<Search24 className="h-4.5 w-4.5 cursor-pointer fill-steel align-middle text-gray-60" />
+						</button>
 					</div>
+					<ComboboxList
+						showResultsCount
+						options={filteredFieldNames.map((item) => ({
+							value: item.name,
+							label: item.name,
+						}))}
+						onSelect={({ value }) => {
+							setActiveFieldName(value);
+						}}
+					/>
+				</Combobox>
+				<div className="max-h-600 overflow-y-auto overflow-x-clip py-4.5">
+					<VerticalList>
+						<div className="flex flex-col gap-4 pl-3">
+							{normalizedStruct?.fields?.map(({ name, type }) => (
+								<button
+									type="button"
+									key={name}
+									className="mt-0.5"
+									onClick={() => setActiveFieldName(name)}
+								>
+									<DescriptionItem
+										descriptionJustify="end"
+										labelWidth="md"
+										title={
+											<Text variant="body/medium" color="steel-darker">
+												{name}
+											</Text>
+										}
+									>
+										<Text uppercase variant="subtitle/normal" color="steel" truncate>
+											{getFieldTypeValue(type, objectType).displayName}
+										</Text>
+									</DescriptionItem>
+								</button>
+							))}
+						</div>
+					</VerticalList>
 				</div>
+			</div>
 
-				<div className="h-100 w-full overflow-auto rounded-xl border-transparent bg-transparent px-2">
-					<Card shadow bg="white">
-						{normalizedStruct?.fields.map(({ name, type }) => (
-							<ScrollToViewCard key={name} inView={name === activeFieldName}>
-								<ObjectFieldsCollapsibleSection
-									name={name}
-									fieldValue={fieldsData[name]}
-									objectType={objectType}
-									type={type}
-								/>
-							</ScrollToViewCard>
-						))}
-					</Card>
-				</div>
-
-				{/*<div className="flex max-h-600 flex-col gap-5 overflow-y-auto pb-5">*/}
-				{/*	{normalizedStruct?.fields.map(({ name, type }) => (*/}
-				{/*		<ScrollToViewCard key={name} inView={name === activeFieldName}>*/}
-				{/*			<DisclosureBox*/}
-				{/*				title={*/}
-				{/*					<div className="min-w-fit max-w-[60%] truncate break-words text-body font-medium leading-relaxed text-steel-dark">*/}
-				{/*						{name}:*/}
-				{/*					</div>*/}
-				{/*				}*/}
-				{/*				preview={*/}
-				{/*					<div className="flex items-center gap-1 truncate break-all">*/}
-				{/*						{typeof fieldsData[name] === 'object' ? (*/}
-				{/*							<Text variant="body/medium" color="gray-90">*/}
-				{/*								Click to view*/}
-				{/*							</Text>*/}
-				{/*						) : (*/}
-				{/*							<FieldItem*/}
-				{/*								value={fieldsData[name]}*/}
-				{/*								truncate*/}
-				{/*								type={type}*/}
-				{/*								objectType={objectType}*/}
-				{/*							/>*/}
-				{/*						)}*/}
-				{/*					</div>*/}
-				{/*				}*/}
-				{/*				variant="outline"*/}
-				{/*			>*/}
-				{/*				<FieldItem value={fieldsData[name]} objectType={objectType} type={type} />*/}
-				{/*			</DisclosureBox>*/}
-				{/*		</ScrollToViewCard>*/}
-				{/*	))}*/}
-				{/*</div>*/}
+			<div className="h-100 w-full overflow-auto rounded-xl border-transparent bg-transparent px-2">
+				<Card shadow bg="white">
+					{normalizedStruct?.fields.map(({ name, type }) => (
+						<ScrollToViewCard key={name} inView={name === activeFieldName}>
+							<FieldsCollapsible name={name}>
+								<FieldItem value={fieldsData[name]} objectType={objectType} type={type} />
+							</FieldsCollapsible>
+						</ScrollToViewCard>
+					))}
+				</Card>
 			</div>
 		</div>
 	);
--- apps/explorer/src/pages/object-result/views/TokenView.tsx
+++ apps/explorer/src/pages/object-result/views/TokenView.tsx
@@ -1,14 +1,23 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { useGetDynamicFields } from '@mysten/core';
 import { type SuiObjectResponse } from '@mysten/sui.js/client';
 import { Heading } from '@mysten/ui';
-import { useState } from 'react';
+import { type ReactNode, useState } from 'react';
 
 import { DynamicFieldsCard } from '~/components/Object/DynamicFieldsCard';
 import { ObjectFieldsCard } from '~/components/Object/ObjectFieldsCard';
 import TransactionBlocksForAddress from '~/components/TransactionBlocksForAddress/TransactionBlocksForAddress';
-import { Tabs, TabsList, TabsTrigger } from '~/ui/Tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/ui/Tabs';
+
+function FieldsContainer({ children }: { children: ReactNode }) {
+	return (
+		<div className="mt-4 flex flex-col gap-5 overflow-auto rounded-xl border border-gray-45 bg-objectCard py-6 pl-6 pr-4">
+			{children}
+		</div>
+	);
+}
 
 enum TABS_VALUES {
 	FIELDS = 'fields',
@@ -18,31 +27,45 @@ enum TABS_VALUES {
 export function TokenView({ data }: { data: SuiObjectResponse }) {
 	const [fieldsCount, setFieldsCount] = useState(0);
 
-	const TABS = [
-		{ label: 'Fields', value: TABS_VALUES.FIELDS, count: fieldsCount },
-		{ label: 'Dynamic Fields', value: TABS_VALUES.DYNAMIC_FIELDS },
-	];
-
 	const [activeTab, setActiveTab] = useState<string>(TABS_VALUES.FIELDS);
 	const objectId = data.data?.objectId!;
+
+	const { data: dynamicFieldsData } = useGetDynamicFields(objectId);
+
+	const renderDynamicFields = !!dynamicFieldsData?.pages?.[0].data.length;
 
 	return (
 		<div className="flex flex-col flex-nowrap gap-14">
 			<Tabs size="lg" value={activeTab} onValueChange={setActiveTab}>
 				<TabsList>
-					{TABS.map(({ label, value, count }) =>
-						value !== TABS_VALUES.FIELDS || count ? (
-							<TabsTrigger key={value} value={value}>
-								<Heading variant="heading4/semibold">
-									{count !== undefined ? count : ''} {label}
-								</Heading>
-							</TabsTrigger>
-						) : null,
+					{!!fieldsCount && (
+						<TabsTrigger value={TABS_VALUES.FIELDS}>
+							<Heading variant="heading4/semibold">{fieldsCount} Fields</Heading>
+						</TabsTrigger>
+					)}
+
+					{renderDynamicFields && (
+						<TabsTrigger value={TABS_VALUES.DYNAMIC_FIELDS}>
+							<Heading variant="heading4/semibold">Dynamic Fields</Heading>
+						</TabsTrigger>
 					)}
 				</TabsList>
+
+				<TabsContent value={TABS_VALUES.FIELDS}>
+					<FieldsContainer>
+						<ObjectFieldsCard id={objectId} setCount={setFieldsCount} />
+					</FieldsContainer>
+				</TabsContent>
+				{renderDynamicFields && (
+					<TabsContent value={TABS_VALUES.DYNAMIC_FIELDS}>
+						<FieldsContainer>
+							<DynamicFieldsCard id={objectId} />
+						</FieldsContainer>
+					</TabsContent>
+				)}
 			</Tabs>
-			<ObjectFieldsCard id={objectId} setFieldsCount={setFieldsCount} />
 			<DynamicFieldsCard id={objectId} />
+
 			<TransactionBlocksForAddress address={objectId} isObject />
 		</div>
 	);
