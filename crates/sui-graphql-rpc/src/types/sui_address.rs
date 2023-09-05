40367ddd94d3e90b2d0c519908c3a66b9f995140
ade <ade@mystenlabs.com>
2023-09-05 14:37:49 -0700

    allow short addresses on input

--- crates/sui-graphql-rpc/src/types/sui_address.rs
+++ crates/sui-graphql-rpc/src/types/sui_address.rs
@@ -20,15 +20,18 @@ impl ScalarType for SuiAddress {
                         "Invalid SuiAddress. Missing 0x prefix",
                     ));
                 }
-
-                let bytes = hex::decode(s)?;
-                if bytes.len() != SUI_ADDRESS_LENGTH {
+                if s.is_empty() || s.len() > SUI_ADDRESS_LENGTH * 2 {
                     return Err(InputValueError::custom(format!(
-                        "Expected SuiAddress of length {}, received {}.",
+                        "Expected SuiAddress string ranging from length 1 up to {} ({} bytes) or less, received {}.",
+                        SUI_ADDRESS_LENGTH * 2,
                         SUI_ADDRESS_LENGTH,
-                        bytes.len()
+                        s.len()
                     )));
                 }
+                // Pad to SUI_ADDRESS_LENGTH*2 width
+                s = format!("{:0>width$}", s, width = SUI_ADDRESS_LENGTH * 2);
+
+                let bytes = hex::decode(s)?;
                 let mut arr = [0u8; SUI_ADDRESS_LENGTH];
                 arr.copy_from_slice(&bytes);
                 Ok(SuiAddress(arr))
