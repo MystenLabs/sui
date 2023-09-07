8f7ed521c2d8521615e79499a55a05ddd6620835
oxade <93547199+oxade@users.noreply.github.com>
2023-09-07 13:10:47 +0200

    [graphql/rpc] query complexity limiting: add request time limiter (#13683)

---
+++ crates/sui-graphql-rpc/src/extensions/timeout.rs
@@ -0,0 +1,60 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use async_graphql::{
+    extensions::{Extension, ExtensionContext, ExtensionFactory, NextRequest},
+    Response, ServerError,
+};
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::time::timeout;
+
+// 10s
+const DEFAULT_REQUEST_TIMEOUT: Duration = Duration::from_millis(10_000);
+
+#[derive(Clone, Debug, Copy)]
+pub struct TimeoutConfig {
+    pub request_timeout: Duration,
+}
+
+impl Default for TimeoutConfig {
+    fn default() -> Self {
+        Self {
+            request_timeout: DEFAULT_REQUEST_TIMEOUT,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct Timeout {
+    config: TimeoutConfig,
+}
+
+impl ExtensionFactory for Timeout {
+    fn create(&self) -> Arc<dyn Extension> {
+        Arc::new(TimeoutExtension {
+            config: self.config,
+        })
+    }
+}
+
+struct TimeoutExtension {
+    config: TimeoutConfig,
+}
+
+#[async_trait::async_trait]
+impl Extension for TimeoutExtension {
+    async fn request(&self, ctx: &ExtensionContext<'_>, next: NextRequest<'_>) -> Response {
+        timeout(self.config.request_timeout, next.run(ctx))
+            .await
+            .unwrap_or_else(|_| {
+                Response::from_errors(vec![ServerError::new(
+                    format!(
+                        "Request timed out. Limit: {}s",
+                        self.config.request_timeout.as_secs_f32()
+                    ),
+                    None,
+                )])
+            })
+    }
+}
