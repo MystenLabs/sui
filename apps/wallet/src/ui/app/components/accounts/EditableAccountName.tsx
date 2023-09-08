81ea94ba030cafa151b303cfd0956dc17ae866b5
Nikhil-Mysten <128089541+Nikhil-Mysten@users.noreply.github.com>
2023-09-08 16:44:42 -0400

    [wallet-ext] inline nickname edit submit handling (#13713)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/components/accounts/EditableAccountName.tsx
+++ apps/wallet/src/ui/app/components/accounts/EditableAccountName.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useZodForm } from '@mysten/core';
-import { type ComponentProps, forwardRef, useRef } from 'react';
+import { type ComponentProps, forwardRef } from 'react';
 import toast from 'react-hot-toast';
 import { z } from 'zod';
 import { useBackgroundClient } from '../../hooks/useBackgroundClient';
@@ -32,8 +32,6 @@ export function EditableAccountName({ accountID, name }: { accountID: string; na
 		},
 	});
 	const { register } = form;
-	const { ref, ...rest } = register('nickname');
-	const inputRef = useRef<HTMLInputElement | null>(null);
 
 	const onSubmit = async ({ nickname }: { nickname: string }) => {
 		if (accountID) {
@@ -42,31 +40,19 @@ export function EditableAccountName({ accountID, name }: { accountID: string; na
 					id: accountID,
 					nickname: nickname || null,
 				});
+				const activeElement = document.activeElement as HTMLElement;
+				activeElement?.blur();
 			} catch (e) {
 				toast.error((e as Error).message || 'Failed to set nickname');
 			}
 		}
 	};
 
-	const handleKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
-		if (e.key === 'Enter') {
-			e.preventDefault();
-			form.handleSubmit(onSubmit)();
-			inputRef.current?.blur();
-		}
-	};
-
 	return (
 		<div>
-			<Form className="flex flex-col" form={form} onSubmit={onSubmit} onKeyDown={handleKeyDown}>
-				<Input
-					{...rest}
-					onBlur={() => form.handleSubmit(onSubmit)()}
-					ref={(e) => {
-						ref(e);
-						inputRef.current = e;
-					}}
-				/>
+			<Form className="flex flex-col" form={form} onSubmit={onSubmit}>
+				<Input {...register('nickname')} onBlur={() => form.handleSubmit(onSubmit)()} />
+				<button className="hidden" type="submit" />
 			</Form>
 		</div>
 	);
