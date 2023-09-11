d7d3d8cc28ca9fa4f8e0eb43a7c34c1cd66b6f42
Dario Russi <113150618+dariorussi@users.noreply.github.com>
2023-09-11 11:57:32 -0500

    Add Move calls tables (#13716)
    ## Description 
    
    Add move calls table
    
    ## Test Plan 
    
    Nothing so far
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-analytics-indexer/src/csv_writer.rs
+++ crates/sui-analytics-indexer/src/csv_writer.rs
@@ -18,7 +18,9 @@ pub(crate) struct CSVWriter {
     transaction_object_csv: Writer<File>,
     object_csv: Writer<File>,
     event_csv: Writer<File>,
+    move_call_csv: Writer<File>,
 }
+use crate::tables::MoveCallEntry;
 use tracing::info;
 
 impl CSVWriter {
@@ -38,12 +40,14 @@ impl CSVWriter {
             dir.join(format!("transaction_object_{}.csv", starting_checkpoint));
         let object_csv = dir.join(format!("object_{}.csv", starting_checkpoint));
         let event_csv = dir.join(format!("event_{}.csv", starting_checkpoint));
+        let move_call_csv = dir.join(format!("move_call_{}.csv", starting_checkpoint));
         Ok(Self {
             checkpoint_csv: Writer::from_path(checkpoint).unwrap(),
             transaction_csv: Writer::from_path(transaction).unwrap(),
             transaction_object_csv: Writer::from_path(transaction_object).unwrap(),
             object_csv: Writer::from_path(object_csv).unwrap(),
             event_csv: Writer::from_path(event_csv).unwrap(),
+            move_call_csv: Writer::from_path(move_call_csv).unwrap(),
         })
     }
 }
@@ -87,5 +91,13 @@ impl TableWriter for CSVWriter {
             self.event_csv.serialize(entry).unwrap();
         }
         self.event_csv.flush().unwrap();
+    }
+
+    fn write_move_calls(&mut self, move_call_entries: &[MoveCallEntry]) {
+        info!("Write move calls");
+        for entry in move_call_entries {
+            self.move_call_csv.serialize(entry).unwrap();
+        }
+        self.move_call_csv.flush().unwrap();
     }
 }
