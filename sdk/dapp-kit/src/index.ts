4cdbb11226bcac8d3140058656ed666f9d1d8590
William Robertson <williamrobertson@mystenlabs.com>
2023-09-11 16:13:52 -0400

    fix

--- sdk/dapp-kit/src/index.ts
+++ sdk/dapp-kit/src/index.ts
@@ -7,6 +7,7 @@ export * from './components/WalletProvider.js';
 export * from './hooks/useRpcApiVersion.js';
 export * from './hooks/rpc/index.js';
 export * from './hooks/wallet/useConnectWallet.js';
+export * from './hooks/wallet/useDisconnectWallet.js';
 export * from './hooks/wallet/useWallet.js';
 export * from './hooks/useSuiClientMutation.js';
 export * from './hooks/useSuiClientQuery.js';
