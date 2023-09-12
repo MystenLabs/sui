d4fa231a3d2e7ee35d99d7c3edba4fe26f5f1698
Phong Lam <phong@mystenlabs.com>
2023-09-11 21:29:06 -0700

    progress

--- apps/explorer/src/pages/object-result/views/ObjectView.tsx
+++ apps/explorer/src/pages/object-result/views/ObjectView.tsx
@@ -3,7 +3,7 @@
 
 import { CoinFormat, useFormatCoin } from '@mysten/core';
 import { ArrowUpRight16 } from '@mysten/icons';
-import { type SuiObjectResponse } from '@mysten/sui.js/client';
+import { type ObjectOwner, type SuiObjectResponse } from '@mysten/sui.js/client';
 import { parseStructTag, SUI_TYPE_ARG } from '@mysten/sui.js/utils';
 import { Heading, Text } from '@mysten/ui';
 import { useQuery } from '@tanstack/react-query';
@@ -99,6 +99,125 @@ function LinkWebsite({ value }: { value: string }) {
 	);
 }
 
+function DescriptionCard({
+	name,
+	display,
+	objectType,
+	objectId,
+}: {
+	name?: string | null;
+	display?: any;
+	objectType: string;
+	objectId: string;
+}) {
+	const { address, module } = parseStructTag(objectType);
+
+	return (
+		<ObjectViewCard>
+			{name && (
+				<Heading variant="heading4/semibold" color="steel-darker">
+					{name}
+				</Heading>
+			)}
+			{display?.description && (
+				<Text variant="pBody/normal" color="steel-darker">
+					{display.description}
+				</Text>
+			)}
+
+			<ObjectViewItem title="Object ID">
+				<ObjectLink objectId={objectId} />
+			</ObjectViewItem>
+
+			<ObjectViewItem title="Type" align="start">
+				<ObjectLink
+					label={<div className="text-right">{trimStdLibPrefix(objectType)}</div>}
+					objectId={`${address}?module=${module}`}
+				/>
+			</ObjectViewItem>
+		</ObjectViewCard>
+	);
+}
+
+function VersionCard({ version, digest }: { version?: string; digest: string }) {
+	return (
+		<ObjectViewCard>
+			<ObjectViewItem title="Version">
+				<Text variant="pBodySmall/medium" color="gray-90">
+					{version}
+				</Text>
+			</ObjectViewItem>
+
+			<ObjectViewItem title="Last Transaction Block Digest">
+				<TransactionLink digest={digest} />
+			</ObjectViewItem>
+		</ObjectViewCard>
+	);
+}
+
+function OwnerCard({ objOwner }: { objOwner?: ObjectOwner | null }) {
+	if (!objOwner) {
+		return null;
+	}
+
+	return (
+		<ObjectViewCard>
+			<ObjectViewItem title="Owner">
+				{objOwner === 'Immutable' ? (
+					'Immutable'
+				) : 'Shared' in objOwner ? (
+					'Shared'
+				) : 'ObjectOwner' in objOwner ? (
+					<ObjectLink objectId={objOwner.ObjectOwner} />
+				) : (
+					<AddressLink address={objOwner.AddressOwner} />
+				)}
+			</ObjectViewItem>
+		</ObjectViewCard>
+	);
+}
+
+function DisplayLinksCard({
+	display,
+}: {
+	display?: {
+		[key: string]: string;
+	} | null;
+}) {
+	return (
+		display &&
+		(display.link || display.project_url) && (
+			<ObjectViewCard>
+				{display.link && (
+					<ObjectViewItem title="Link">
+						<LinkWebsite value={display.link} />
+					</ObjectViewItem>
+				)}
+
+				{display.project_url && (
+					<ObjectViewItem title="Website">
+						<LinkWebsite value={display.project_url} />
+					</ObjectViewItem>
+				)}
+			</ObjectViewCard>
+		)
+	);
+}
+
+function StorageRebateCard({ storageRebate }: { storageRebate?: string | null }) {
+	const [storageRebateFormatted] = useFormatCoin(storageRebate, SUI_TYPE_ARG, CoinFormat.FULL);
+
+	return (
+		<ObjectViewCard>
+			<ObjectViewItem title="Storage Rebate">
+				<Text variant="pBodySmall/medium" color="steel-darker">
+					-{storageRebateFormatted}
+				</Text>
+			</ObjectViewItem>
+		</ObjectViewCard>
+	);
+}
+
 interface ObjectViewProps {
 	data: SuiObjectResponse;
 }
@@ -112,10 +231,6 @@ export function ObjectView({ data }: ObjectViewProps) {
 	const objectType = parseObjectType(data);
 	const objOwner = data.data?.owner;
 	const storageRebate = data.data?.storageRebate;
-
-	const [storageRebateFormatted] = useFormatCoin(storageRebate, SUI_TYPE_ARG, CoinFormat.FULL);
-
-	const { address, module } = parseStructTag(objectType);
 
 	const { data: imageData } = useQuery({
 		queryKey: ['image-file-type', imgUrl],
@@ -142,83 +257,22 @@ export function ObjectView({ data }: ObjectViewProps) {
 
 			<div className="flex h-full w-full flex-row gap-6">
 				<div className="flex min-w-[50%] basis-1/2 flex-col gap-4">
-					<ObjectViewCard>
-						{name && (
-							<Heading variant="heading4/semibold" color="steel-darker">
-								{name}
-							</Heading>
-						)}
-						{display?.description && (
-							<Text variant="pBody/normal" color="steel-darker">
-								{display.description}
-							</Text>
-						)}
+					<DescriptionCard
+						name={name}
+						objectType={objectType}
+						objectId={data.data?.objectId!}
+						display={display}
+					/>
 
-						<ObjectViewItem title="Object ID">
-							<ObjectLink objectId={data.data?.objectId!} />
-						</ObjectViewItem>
-
-						<ObjectViewItem title="Type" align="start">
-							<ObjectLink
-								label={<div className="text-right">{trimStdLibPrefix(objectType)}</div>}
-								objectId={`${address}?module=${module}`}
-							/>
-						</ObjectViewItem>
-					</ObjectViewCard>
-
-					<ObjectViewCard>
-						<ObjectViewItem title="Version">
-							<Text variant="pBodySmall/medium" color="gray-90">
-								{data.data?.version}
-							</Text>
-						</ObjectViewItem>
-
-						<ObjectViewItem title="Last Transaction Block Digest">
-							<TransactionLink digest={data.data?.previousTransaction!} />
-						</ObjectViewItem>
-					</ObjectViewCard>
+					<VersionCard version={data.data?.version} digest={data.data?.previousTransaction!} />
 				</div>
 
 				<div className="flex min-w-[50%] basis-1/2 flex-col gap-4">
-					{objOwner && (
-						<ObjectViewCard>
-							<ObjectViewItem title="Owner">
-								{objOwner === 'Immutable' ? (
-									'Immutable'
-								) : 'Shared' in objOwner ? (
-									'Shared'
-								) : 'ObjectOwner' in objOwner ? (
-									<ObjectLink objectId={objOwner.ObjectOwner} />
-								) : (
-									<AddressLink address={objOwner.AddressOwner} />
-								)}
-							</ObjectViewItem>
-						</ObjectViewCard>
-					)}
+					<OwnerCard objOwner={objOwner} />
 
-					{display && (display.link || display.project_url) && (
-						<ObjectViewCard>
-							{display.link && (
-								<ObjectViewItem title="Link">
-									<LinkWebsite value={display.link} />
-								</ObjectViewItem>
-							)}
+					<DisplayLinksCard display={display} />
 
-							{display.project_url && (
-								<ObjectViewItem title="Website">
-									<LinkWebsite value={display.project_url} />
-								</ObjectViewItem>
-							)}
-						</ObjectViewCard>
-					)}
-
-					<ObjectViewCard>
-						<ObjectViewItem title="Storage Rebate">
-							<Text variant="pBodySmall/medium" color="steel-darker">
-								-{storageRebateFormatted}
-							</Text>
-						</ObjectViewItem>
-					</ObjectViewCard>
+					<StorageRebateCard storageRebate={storageRebate} />
 				</div>
 			</div>
 		</div>
