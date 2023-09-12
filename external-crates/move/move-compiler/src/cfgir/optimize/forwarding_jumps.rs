0ee11fbf69bd30b825f0b1954bf4b346581a45b4
Cameron Swords <cameron.swords@mystenlabs.com>
2023-09-12 13:59:32 -0700

    Fix small bugs

--- external-crates/move/move-compiler/src/cfgir/optimize/forwarding_jumps.rs
+++ external-crates/move/move-compiler/src/cfgir/optimize/forwarding_jumps.rs
@@ -77,8 +77,8 @@ fn find_forwarding_jump_destinations(blocks: &BasicBlocks) -> LabelMap {
         let mut target = *start;
         let mut seen = BTreeSet::new();
         while let Some(next_target) = forwarding_jumps.get(&target) {
-            if let Some(final) = final_jumps.get(next_target) {
-                target = final;
+            if let Some(final_jump) = final_jumps.get(next_target) {
+                target = *final_jump;
                 break;
             } else if start == next_target {
                 target = *start;
