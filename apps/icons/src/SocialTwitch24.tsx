83a609977a1d367e66b644b6380e6e0dd39e74b9
mamos-mysten <122397493+mamos-mysten@users.noreply.github.com>
2023-09-12 08:58:00 -0700

    [wallet-ext] - fix twitch icon on welcome page (#13747)
    ## Description 
    
    Fixes a regression with the Twitch icon on the welcome page after
    updating the source SVG in a previous PR
    
    Before:
    <img width="395" alt="image"
    src="https://github.com/MystenLabs/sui/assets/122397493/b14f2b21-52bc-47bc-a829-885ec038d2fb">
    
    
    After: 
    <img width="395" alt="image"
    src="https://github.com/MystenLabs/sui/assets/122397493/7057077d-3e1b-4dbc-9e26-9021f7dd736a">
    
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/icons/src/SocialTwitch24.tsx
+++ apps/icons/src/SocialTwitch24.tsx
@@ -11,7 +11,7 @@ const SvgSocialTwitch24 = (props: SVGProps<SVGSVGElement>) => (
 		{...props}
 	>
 		<path
-			fill="#6441A5"
+			fill="currentColor"
 			fillRule="evenodd"
 			d="m11.458 10.771 2.286-2.286V1.3H2.973v9.472h2.943v1.957l1.957-1.957h3.585ZM1 2.614 1.658 0h13.4v9.143L9.83 14.372H7.2L5.57 16H3.943v-1.628H1V2.614Zm6.857 5.214h-1.3V3.914h1.3v3.914Zm3.6 0h-1.299V3.914h1.3v3.914Z"
 			clipRule="evenodd"
