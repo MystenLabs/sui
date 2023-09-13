d027d998b8b32bea99f8e258a604fcf6740733d9
ade <ade@mystenlabs.com>
2023-09-12 23:53:33 -0700

    simplify tests

--- crates/sui-graphql-rpc/src/server/builder.rs
+++ crates/sui-graphql-rpc/src/server/builder.rs
@@ -5,30 +5,25 @@ use crate::{
     server::version::{check_version_middleware, set_version_middleware},
     types::query::{Query, SuiGraphQLSchema},
 };
-use async_graphql::{extensions::ExtensionFactory, SchemaBuilder};
+use async_graphql::{extensions::ExtensionFactory, Schema, SchemaBuilder};
 use async_graphql::{EmptyMutation, EmptySubscription};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 use axum::middleware;
 use axum::{routing::IntoMakeService, Router};
+use hyper::server::conn::AddrIncoming as HyperAddrIncoming;
+use hyper::Server as HyperServer;
 use std::any::Any;
 
 pub(crate) const DEFAULT_PORT: u16 = 8000;
 pub(crate) const DEFAULT_HOST: &str = "127.0.0.1";
 
 pub(crate) struct Server {
-    pub server: hyper::Server<hyper::server::conn::AddrIncoming, IntoMakeService<Router>>,
-    #[cfg(test)]
-    pub schema: async_graphql::Schema<Query, EmptyMutation, EmptySubscription>,
+    pub server: HyperServer<HyperAddrIncoming, IntoMakeService<Router>>,
 }
 
 impl Server {
     pub async fn run(self) {
         self.server.await.unwrap();
-    }
-
-    #[cfg(test)]
-    pub async fn execute_bypass_network(&self, query: &str) -> async_graphql::Response {
-        self.schema.execute(query).await
     }
 }
 
@@ -44,7 +39,7 @@ impl ServerBuilder {
         Self {
             port: None,
             host: None,
-            schema: async_graphql::Schema::build(Query, EmptyMutation, EmptySubscription),
+            schema: Schema::build(Query, EmptyMutation, EmptySubscription),
         }
     }
 
@@ -76,20 +71,21 @@ impl ServerBuilder {
         self
     }
 
-    #[allow(clippy::redundant_clone)]
+    fn build_schema(self) -> Schema<Query, EmptyMutation, EmptySubscription> {
+        self.schema.finish()
+    }
+
     pub fn build(self) -> Server {
         let address = self.address();
-        let schema = self.schema.finish();
+        let schema = self.build_schema();
 
         let app = axum::Router::new()
             .route("/", axum::routing::get(graphiql).post(graphql_handler))
-            .layer(axum::extract::Extension(schema.clone()))
+            .layer(axum::extract::Extension(schema))
             .layer(middleware::from_fn(check_version_middleware))
             .layer(middleware::from_fn(set_version_middleware));
         Server {
             server: axum::Server::bind(&address.parse().unwrap()).serve(app.into_make_service()),
-            #[cfg(test)]
-            schema,
         }
     }
 }
@@ -111,24 +107,26 @@ async fn graphiql() -> impl axum::response::IntoResponse {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::{
         context_data::{data_provider::DataProvider, sui_sdk_data_provider::sui_sdk_client_v0},
         extensions::timeout::{Timeout, TimeoutConfig},
-        server::builder::ServerBuilder,
     };
-    use async_graphql::{extensions::ExtensionFactory, Response};
+    use async_graphql::{
+        extensions::{Extension, ExtensionContext, NextExecute},
+        Response,
+    };
+
+    use std::time::Duration;
 
     #[tokio::test]
     async fn test_timeout() {
-        use async_graphql::extensions::ExtensionContext;
-        let sdk = sui_sdk_client_v0("https://fullnode.testnet.sui.io:443/").await;
-
         struct TimedExecuteExt {
-            pub min_req_delay: std::time::Duration,
+            pub min_req_delay: Duration,
         }
 
         impl ExtensionFactory for TimedExecuteExt {
-            fn create(&self) -> std::sync::Arc<dyn async_graphql::extensions::Extension> {
+            fn create(&self) -> std::sync::Arc<dyn Extension> {
                 std::sync::Arc::new(TimedExecuteExt {
                     min_req_delay: self.min_req_delay,
                 })
@@ -136,50 +134,42 @@ mod tests {
         }
 
         #[async_trait::async_trait]
-        impl async_graphql::extensions::Extension for TimedExecuteExt {
+        impl Extension for TimedExecuteExt {
             async fn execute(
                 &self,
                 ctx: &ExtensionContext<'_>,
                 operation_name: Option<&str>,
-                next: async_graphql::extensions::NextExecute<'_>,
+                next: NextExecute<'_>,
             ) -> Response {
                 tokio::time::sleep(self.min_req_delay).await;
                 next.run(ctx, operation_name).await
             }
         }
 
-        let timeout = std::time::Duration::from_millis(1000);
-        let delay = std::time::Duration::from_millis(100);
+        async fn test_timeout(delay: Duration, timeout: Duration) -> Response {
+            let sdk = sui_sdk_client_v0("https://fullnode.testnet.sui.io:443/").await;
+            let data_provider: Box<dyn DataProvider> = Box::new(sdk);
+            let schema = ServerBuilder::new()
+                .context_data(data_provider)
+                .extension(TimedExecuteExt {
+                    min_req_delay: delay,
+                })
+                .extension(Timeout::new(TimeoutConfig {
+                    request_timeout: timeout,
+                }))
+                .build_schema();
+            schema.execute("{ chainIdentifier }").await
+        }
 
-        let data_provider: Box<dyn DataProvider> = Box::new(sdk.clone());
+        let timeout = Duration::from_millis(1000);
+        let delay = Duration::from_millis(100);
 
-        let server = ServerBuilder::new()
-            .port(8000)
-            .context_data(data_provider)
-            .extension(TimedExecuteExt {
-                min_req_delay: delay,
-            })
-            .extension(Timeout::new(TimeoutConfig {
-                request_timeout: timeout,
-            }))
-            .build();
-        let resp = server.execute_bypass_network("{ chainIdentifier }").await;
+        // Should complete successfully
+        let resp = test_timeout(delay, timeout).await;
         assert!(resp.is_ok());
 
-        let delay = std::time::Duration::from_millis(1000);
-        let data_provider: Box<dyn DataProvider> = Box::new(sdk.clone());
-
-        let server = ServerBuilder::new()
-            .port(8001)
-            .context_data(data_provider)
-            .extension(TimedExecuteExt {
-                min_req_delay: delay,
-            })
-            .extension(Timeout::new(TimeoutConfig {
-                request_timeout: delay,
-            }))
-            .build();
-        let resp = server.execute_bypass_network("{ chainIdentifier }").await;
+        // Should timeout
+        let resp = test_timeout(timeout, timeout).await;
         assert!(resp.is_err());
     }
 }
