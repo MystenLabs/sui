2aa532902e7dad6722d889b7fe31dde5d78e9b1b
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-09-15 22:09:24 +0100

    pr comments

--- apps/wallet/src/ui/app/pages/accounts/ExportAccountPage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/ExportAccountPage.tsx
@@ -4,7 +4,6 @@
 import { fromB64 } from '@mysten/sui.js/utils';
 import { bytesToHex } from '@noble/hashes/utils';
 import { useMutation } from '@tanstack/react-query';
-import { useMemo } from 'react';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 
 import { HideShowDisplayBox } from '../../components/HideShowDisplayBox';
@@ -18,10 +17,7 @@ import { useBackgroundClient } from '_src/ui/app/hooks/useBackgroundClient';
 export function ExportAccountPage() {
 	const { accountID } = useParams();
 	const { data: allAccounts, isLoading } = useAccounts();
-	const account = useMemo(
-		() => allAccounts?.find(({ id }) => accountID === id) || null,
-		[allAccounts, accountID],
-	);
+	const account = allAccounts?.find(({ id }) => accountID === id) || null;
 	const backgroundClient = useBackgroundClient();
 	const exportMutation = useMutation({
 		mutationKey: ['export-account', accountID],
@@ -29,29 +25,29 @@ export function ExportAccountPage() {
 			if (!account) {
 				return null;
 			}
-			return await backgroundClient.exportAccountKeyPair({ password, accountID: account.id });
+			const {
+				keyPair: { privateKey },
+			} = await backgroundClient.exportAccountKeyPair({
+				password,
+				accountID: account.id,
+			});
+			return `0x${bytesToHex(fromB64(privateKey))}`;
 		},
 	});
-	const privateKey = useMemo(() => {
-		if (exportMutation.data?.keyPair) {
-			return `0x${bytesToHex(fromB64(exportMutation.data.keyPair.privateKey).slice(0, 32))}`;
-		}
-		return null;
-	}, [exportMutation.data?.keyPair]);
 	const navigate = useNavigate();
 	if (!account && !isLoading) {
-		return <Navigate to="../manage" replace />;
+		return <Navigate to="/accounts/manage" replace />;
 	}
 	return (
 		<Overlay title="Account Private Key" closeOverlay={() => navigate(-1)} showModal>
 			<Loading loading={isLoading}>
-				{privateKey ? (
+				{exportMutation.data ? (
 					<div className="flex flex-col flex-nowrap items-stretch gap-3">
 						<Alert>
 							<div>Do not share your Private Key!</div>
 							<div>It provides full control of your account.</div>
 						</Alert>
-						<HideShowDisplayBox value={privateKey} copiedMessage="Private key copied" />
+						<HideShowDisplayBox value={exportMutation.data} copiedMessage="Private key copied" />
 					</div>
 				) : (
 					<VerifyPasswordModal
