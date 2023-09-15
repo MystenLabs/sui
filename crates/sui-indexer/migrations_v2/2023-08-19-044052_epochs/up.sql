c042897debbfc868003b38a7404528e6e55a0c4f
Lu Zhang <8418040+longbowlu@users.noreply.github.com>
2023-09-15 14:16:48 -0700

    [indexer-v2 1/n] add schemas and typesadd schemas and types (#13760)
    ## Description 
    
    Breaking [this](https://github.com/MystenLabs/sui/pull/13574) into
    smaller pieces.
    This is first PR to define schemas and basic data models. Addressed most
    comments in #13574
    
    
    Stack:
    1. https://github.com/MystenLabs/sui/pull/13760
    2. https://github.com/MystenLabs/sui/pull/13774
    3. https://github.com/MystenLabs/sui/pull/13775
    4. https://github.com/MystenLabs/sui/pull/13778
    
    ## Test Plan 
    
    Testing is done in a live indexer with the big PR.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    [indexer-v2 1/n] add schemas and types

---
+++ crates/sui-indexer/migrations_v2/2023-08-19-044052_epochs/up.sql
@@ -0,0 +1,29 @@
+CREATE TABLE epochs
+(
+    epoch                           BIGINT      PRIMARY KEY,
+    validators                      bytea[]     NOT NULL,
+    epoch_total_transactions        BIGINT      NOT NULL,
+    first_checkpoint_id             BIGINT      NOT NULL,
+    epoch_start_timestamp           BIGINT      NOT NULL,
+    reference_gas_price             BIGINT      NOT NULL,
+    protocol_version                BIGINT      NOT NULL,
+    -- The following fields are nullable because they are filled in
+    -- only at the end of an epoch.
+    last_checkpoint_id              BIGINT,
+    epoch_end_timestamp             BIGINT,
+    -- The following fields are from SystemEpochInfoEvent emitted
+    -- **after** advancing to the next epoch
+    storage_fund_reinvestment       BIGINT,
+    storage_charge                  BIGINT,
+    storage_rebate                  BIGINT,
+    storage_fund_balance            BIGINT,
+    stake_subsidy_amount            BIGINT,
+    total_gas_fees                  BIGINT,
+    total_stake_rewards_distributed BIGINT,
+    leftover_storage_fund_inflow    BIGINT,
+    -- total stake after advancing to the next epoch
+    new_total_stake                 BIGINT,
+    -- bcs serialized Vec<EpochCommitment> bytes, found in last CheckpointSummary
+    -- of the epoch
+    epoch_commitments               bytea
+);
