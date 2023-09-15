9d79654d955ec51419fddc1695f3efa26a69295c
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2023-09-15 20:57:05 +0100

    wallet-ext: update max height (#13781)
    ## Description 
    
    * now wallet can expand to 680px if there is available space
    
    
    
    https://github.com/MystenLabs/sui/assets/10210143/b9e7ed20-e618-4fcf-b181-da91e150bdc9
    
    
    closes APPS-1637
    
    ## Test Plan 
    
    👀 
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/pages/accounts/ProtectAccountPage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/ProtectAccountPage.tsx
@@ -79,7 +79,7 @@ export function ProtectAccountPage() {
 		return <Navigate to="/" replace />;
 	}
 	return (
-		<div className="rounded-20 bg-sui-lightest shadow-wallet-content flex flex-col items-center px-6 py-10 overflow-auto w-popup-width h-popup-height">
+		<div className="rounded-20 bg-sui-lightest shadow-wallet-content flex flex-col items-center px-6 py-10 overflow-auto w-popup-width max-h-popup-height min-h-popup-minimum h-screen">
 			<Loading loading={showVerifyPasswordView === null}>
 				{showVerifyPasswordView ? (
 					<VerifyPasswordModal
