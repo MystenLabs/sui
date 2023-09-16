f40d2725a6261ee7fd2f515953acc89d24ea32cb
Adam Welc <adam@mystenlabs.com>
2023-09-16 11:01:17 +0200

    [move-compiler] Print request for linter feedback if linter warnings present (#13795)
    ## Description 
    
    We would like to present to the developer a place where they could
    provide feedback on the available linters, with the information about
    how to report feedback being immediately available whenever the linter
    warnings are generated
    
    ## Test Plan 
    
    All existing tests must pass plus manually tested that the message gets
    printed.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [x] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    
    When linter warnings get generated, information about a place where the
    feedback on linters can be provided will be printed alongside the
    warning messages.

--- crates/sui-move-build/src/lib.rs
+++ crates/sui-move-build/src/lib.rs
@@ -58,6 +58,7 @@ use crate::linters::{
     coin_field::CoinFieldVisitor, collection_equality::CollectionEqualityVisitor,
     custom_state_change::CustomStateChangeVerifier, freeze_wrapped::FreezeWrappedVisitor,
     known_filters, self_transfer::SelfTransferVerifier, share_owned::ShareOwnedVerifier,
+    LINT_WARNING_PREFIX,
 };
 
 #[cfg(test)]
@@ -155,15 +156,23 @@ impl BuildConfig {
             };
             match units_res {
                 Ok((units, warning_diags)) => {
+                    let any_linter_warnings = warning_diags.any_with_prefix(LINT_WARNING_PREFIX);
                     report_warnings(&files, warning_diags);
+                    if any_linter_warnings {
+                        eprintln!("Please report feedback on the linter warnings at https://forums.sui.io\n");
+                    }
                     fn_info = Some(Self::fn_info(&units));
                     Ok((files, units))
                 }
                 Err(error_diags) => {
                     assert!(!error_diags.is_empty());
+                    let any_linter_warnings = error_diags.any_with_prefix(LINT_WARNING_PREFIX);
                     let diags_buf = report_diagnostics_to_color_buffer(&files, error_diags);
-                    if let Err(err) = std::io::stdout().write_all(&diags_buf) {
+                    if let Err(err) = std::io::stderr().write_all(&diags_buf) {
                         anyhow::bail!("Cannot output compiler diagnostics: {}", err);
+                    }
+                    if any_linter_warnings {
+                        eprintln!("Please report feedback on the linter warnings at https://forums.sui.io\n");
                     }
                     anyhow::bail!("Compilation error");
                 }
