11b4c668f97ac8da22bdef201f07549d7ee4e0fe
Phong Lam <phong@mystenlabs.com>
2023-09-18 14:41:55 -0700

    [Explorer]: Object page minor fixes

--- apps/explorer/src/pages/object-result/views/ObjectView.tsx
+++ apps/explorer/src/pages/object-result/views/ObjectView.tsx
@@ -49,7 +49,9 @@ function HeroVideoImage({ title, subtitle, src, video }: HeroVideoImageProps) {
 				setOpen={setOpen}
 				rounded="xl"
 			/>
-			<ArrowUpRight16 className="right-5 top-5 hidden group-hover:absolute group-hover:block" />
+			<div className="absolute right-3 top-3 hidden h-8 w-8 items-center justify-center rounded-md bg-white/40 backdrop-blur group-hover:flex">
+				<ArrowUpRight16 />
+			</div>
 		</div>
 	);
 }
@@ -101,15 +103,17 @@ function DescriptionCard({
 	};
 
 	const normalizedStructTag = normalizeStructTag(structTag);
+	const objectNameDisplay = name || display?.description;
+	const renderDescription = name && display?.description;
 
 	return (
 		<ObjectViewCard>
-			{name && (
+			{objectNameDisplay && (
 				<Heading variant="heading4/semibold" color="steel-darker">
-					{name}
+					{objectNameDisplay}
 				</Heading>
 			)}
-			{display?.description && (
+			{renderDescription && (
 				<Text variant="pBody/normal" color="steel-darker">
 					{display.description}
 				</Text>
