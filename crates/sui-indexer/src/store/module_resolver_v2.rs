f4713a494d1554a9b842b77d83ac5ba6d5f0d17c
longbowlu <zl9394@gmail.com>
2023-09-18 15:15:13 -0700

    add indexing caches and InterimModuleResolver

--- crates/sui-indexer/src/store/module_resolver_v2.rs
+++ crates/sui-indexer/src/store/module_resolver_v2.rs
@@ -1,13 +1,20 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::handlers::tx_processor::IndexingPackageCache;
+use crate::metrics::IndexerMetrics;
 use crate::schema_v2::packages;
+use crate::types_v2::IndexedPackage;
 use diesel::ExpressionMethods;
 use diesel::QueryDsl;
 use diesel::RunQueryDsl;
+use move_binary_format::CompiledModule;
+use move_bytecode_utils::module_cache::GetModule;
 use move_core_types::language_storage::ModuleId;
 use move_core_types::resolver::ModuleResolver;
+use std::sync::{Arc, Mutex};
 use sui_types::base_types::ObjectID;
+use sui_types::messages_checkpoint::CheckpointSequenceNumber;
 use sui_types::move_package::MovePackage;
 
 use crate::errors::{Context, IndexerError};
@@ -56,5 +63,54 @@ impl ModuleResolver for IndexerStoreModuleResolver {
             .serialized_module_map()
             .get(&module_name)
             .cloned())
+    }
+}
+
+/// InterimModuleResolver consists of a backup ModuleResolver
+/// (e.g. IndexerStoreModuleResolver) and an in-mem package cache.
+pub struct InterimModuleResolver<GM> {
+    backup: GM,
+    package_cache: Arc<Mutex<IndexingPackageCache>>,
+    metrics: IndexerMetrics,
+}
+
+impl<GM> InterimModuleResolver<GM> {
+    // TODO remove this after integration is done
+    #[allow(dead_code)]
+    pub fn new(
+        backup: GM,
+        package_cache: Arc<Mutex<IndexingPackageCache>>,
+        new_packages: &[IndexedPackage],
+        checkpoint_seq: CheckpointSequenceNumber,
+        metrics: IndexerMetrics,
+    ) -> Self {
+        package_cache
+            .lock()
+            .unwrap()
+            .insert_packages(new_packages, checkpoint_seq);
+        Self {
+            backup,
+            package_cache,
+            metrics,
+        }
+    }
+}
+
+impl<GM> GetModule for InterimModuleResolver<GM>
+where
+    GM: GetModule<Item = Arc<CompiledModule>, Error = anyhow::Error>,
+{
+    type Error = IndexerError;
+    type Item = Arc<CompiledModule>;
+
+    fn get_module_by_id(&self, id: &ModuleId) -> Result<Option<Arc<CompiledModule>>, Self::Error> {
+        if let Some(m) = self.package_cache.lock().unwrap().get_module_by_id(id) {
+            self.metrics.indexing_module_resolver_in_mem_hit.inc();
+            Ok(Some(m))
+        } else {
+            self.backup
+                .get_module_by_id(id)
+                .map_err(|e| IndexerError::ModuleResolutionError(e.to_string()))
+        }
     }
 }
