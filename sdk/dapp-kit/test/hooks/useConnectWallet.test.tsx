6f232ef0aeb6ba45ae9d835b2695fd9b6648d384
William Robertson <williamrobertson@mystenlabs.com>
2023-09-18 17:28:19 -0400

    fix

--- sdk/dapp-kit/test/hooks/useConnectWallet.test.tsx
+++ sdk/dapp-kit/test/hooks/useConnectWallet.test.tsx
@@ -4,27 +4,9 @@
 import { renderHook, waitFor, act } from '@testing-library/react';
 import { useConnectWallet, useCurrentWallet, useCurrentAccount } from 'dapp-kit/src';
 import { createWalletProviderContextWrapper, registerMockWallet } from '../test-utils.js';
-import { WalletAlreadyConnectedError } from 'dapp-kit/src/errors/walletErrors.js';
 import type { Mock } from 'vitest';
 
 describe('useConnectWallet', () => {
-	test('throws an error when connecting to a wallet when a connection is already active', async () => {
-		const { unregister, mockWallet } = registerMockWallet({ walletName: 'Mock Wallet 1' });
-
-		const wrapper = createWalletProviderContextWrapper();
-		const { result } = renderHook(() => useConnectWallet(), { wrapper });
-
-		result.current.mutate({ wallet: mockWallet });
-		await waitFor(() => expect(result.current.isSuccess).toBe(true));
-
-		result.current.mutate({ wallet: mockWallet });
-		await waitFor(() => expect(result.current.error).toBeInstanceOf(WalletAlreadyConnectedError));
-
-		act(() => {
-			unregister();
-		});
-	});
-
 	test('throws an error when a user fails to connect their wallet', async () => {
 		const { unregister, mockWallet } = registerMockWallet({ walletName: 'Mock Wallet 1' });
 
