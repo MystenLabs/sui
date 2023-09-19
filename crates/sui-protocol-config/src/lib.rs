fe6c8c7d967d67a054b112f5b6ffdacf41ec9092
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-09-19 16:08:20 +0200

    Update protocol version

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -76,6 +76,7 @@ const MAX_PROTOCOL_VERSION: u64 = 26;
 // Version 25: Add sui::table_vec::swap and sui::table_vec::swap_remove to system packages.
 // Version 26: New gas model version.
 //             Add support for receiving objects off of other objects in devnet only.
+//             Add sui::zklogin::verify_zklogin_id and related functions to sui framework.
 
 #[derive(Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ProtocolVersion(u64);
