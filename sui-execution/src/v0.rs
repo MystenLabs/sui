0ed13fd8442b53001f9c2d5d1d69cefa5a3bf46e
Dario Russi <113150618+dariorussi@users.noreply.github.com>
2023-09-19 07:13:25 -0700

    Remove usage of hardcoded link to latest execution version (#13806)
    ## Description 
    
    Few crates were hardcoding references to the execution version which
    makes difficult to change the execution versioning with a `v_last_` as a
    production version and a `latest` as a working version.
    This PR removes those dependencies.
    The entire story for the verifier as a cli command is a bit questionable
    but I think it moves us forward so it should be good for now
    
    ## Test Plan 
    
    Existing tests
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sui-execution/src/v0.rs
+++ sui-execution/src/v0.rs
@@ -21,6 +21,7 @@ use sui_types::{
     type_resolver::LayoutResolver,
 };
 
+use move_bytecode_verifier_v0::meter::Scope;
 use move_vm_runtime_v0::move_vm::MoveVM;
 use sui_adapter_v0::adapter::{
     default_verifier_config, new_move_vm, run_metered_move_bytecode_verifier,
@@ -35,6 +36,7 @@ use sui_verifier_v0::meter::SuiVerifierMeter;
 
 use crate::executor;
 use crate::verifier;
+use crate::verifier::{VerifierMeteredValues, VerifierOverrides};
 
 pub(crate) struct Executor(Arc<MoveVM>);
 
@@ -192,5 +194,33 @@ impl<'m> verifier::Verifier for Verifier<'m> {
             &mut self.meter,
             self.metrics,
         )
+    }
+
+    fn meter_compiled_modules_with_overrides(
+        &mut self,
+        modules: &[CompiledModule],
+        protocol_config: &ProtocolConfig,
+        config_overrides: &VerifierOverrides,
+    ) -> SuiResult<VerifierMeteredValues> {
+        let mut config = self.config.clone();
+        let max_per_fun_meter_current = config.max_per_fun_meter_units;
+        let max_per_mod_meter_current = config.max_per_mod_meter_units;
+        config.max_per_fun_meter_units = config_overrides.max_per_fun_meter_units;
+        config.max_per_mod_meter_units = config_overrides.max_per_mod_meter_units;
+        run_metered_move_bytecode_verifier(
+            modules,
+            protocol_config,
+            &config,
+            &mut self.meter,
+            self.metrics,
+        )?;
+        let fun_meter_units_result = self.meter.get_usage(Scope::Function);
+        let mod_meter_units_result = self.meter.get_usage(Scope::Function);
+        Ok(VerifierMeteredValues::new(
+            max_per_fun_meter_current,
+            max_per_mod_meter_current,
+            fun_meter_units_result,
+            mod_meter_units_result,
+        ))
     }
 }
