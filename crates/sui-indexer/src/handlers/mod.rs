b3c68709484e5bb39f9b18318d5dbd8480e8ba25
Lu Zhang <8418040+longbowlu@users.noreply.github.com>
2023-09-19 23:47:37 -0700

    [Indexer-v2 5/n] add indexer_store_v2 trait and DataToCommit structs (#13832)
    ## Description 
    
    This PR adds `IndexerStoreV2` trait and a few helper structs.
    `IndexerStoreV2` only contains the necessary functions for now, and will
    be immediately used in next PR in the stack.
    
    
    Stack:
    1. https://github.com/MystenLabs/sui/pull/13760
    2. https://github.com/MystenLabs/sui/pull/13774
    3. https://github.com/MystenLabs/sui/pull/13804
    4. https://github.com/MystenLabs/sui/pull/13805
    5. https://github.com/MystenLabs/sui/pull/13832
    6. https://github.com/MystenLabs/sui/pull/13834
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-indexer/src/handlers/mod.rs
+++ crates/sui-indexer/src/handlers/mod.rs
@@ -3,3 +3,33 @@
 
 pub mod checkpoint_handler;
 pub mod tx_processor;
+
+use sui_types::base_types::ObjectRef;
+
+use crate::types_v2::{
+    IndexedCheckpoint, IndexedEpochInfo, IndexedEvent, IndexedObject, IndexedPackage,
+    IndexedTransaction, TxIndex,
+};
+
+#[derive(Debug)]
+pub struct CheckpointDataToCommit {
+    pub checkpoint: IndexedCheckpoint,
+    pub transactions: Vec<IndexedTransaction>,
+    pub events: Vec<IndexedEvent>,
+    pub tx_indices: Vec<TxIndex>,
+    pub object_changes: TransactionObjectChangesToCommit,
+    pub packages: Vec<IndexedPackage>,
+    pub epoch: Option<EpochToCommit>,
+}
+
+#[derive(Debug)]
+pub struct TransactionObjectChangesToCommit {
+    pub changed_objects: Vec<IndexedObject>,
+    pub deleted_objects: Vec<ObjectRef>,
+}
+
+#[derive(Debug)]
+pub struct EpochToCommit {
+    pub last_epoch: Option<IndexedEpochInfo>,
+    pub new_epoch: IndexedEpochInfo,
+}
