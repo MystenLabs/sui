0f909a82f61b08fbc81716cb82c19d6d81f2b1ed
ade <ade@mystenlabs.com>
2023-09-20 13:28:42 -0700

    show limits and usage

--- crates/sui-graphql-rpc/src/extensions/mod.rs
+++ crates/sui-graphql-rpc/src/extensions/mod.rs
@@ -1,5 +1,6 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+pub(crate) mod limits_info;
 pub(crate) mod logger;
 pub(crate) mod timeout;
--- crates/sui-graphql-rpc/src/functional_group.rs
+++ crates/sui-graphql-rpc/src/functional_group.rs
@@ -1,11 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 /// Logical Groups categorise APIs exposed by GraphQL.  Groups can be enabled or disabled based on
 /// settings in the RPC's TOML configuration file.
-#[derive(Deserialize, Debug, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Ord, PartialOrd)]
 #[serde(rename_all = "kebab-case")]
 pub(crate) enum FunctionalGroup {
     /// Statistics about how the network was running (TPS, top packages, APY, etc)
