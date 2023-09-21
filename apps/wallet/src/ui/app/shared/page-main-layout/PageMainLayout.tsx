73e6d6840c3748a35216773e63aa765a6276a660
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-09-21 19:19:48 +0100

    addresses pr comments

--- apps/wallet/src/ui/app/shared/page-main-layout/PageMainLayout.tsx
+++ apps/wallet/src/ui/app/shared/page-main-layout/PageMainLayout.tsx
@@ -46,15 +46,17 @@ export function PageMainLayout({
 				middleContent={dappStatusEnabled ? <DappStatus /> : <div ref={setTitlePortalContainer} />}
 				rightContent={topNavMenuEnabled ? <WalletSettingsButton /> : undefined}
 			/>
-			<div className="relative flex flex-col flex-nowrap bg-white flex-grow overflow-y-auto overflow-x-hidden rounded-xl rounded-b-none">
-				<main className={cn('flex-grow w-full', { 'p-5': bottomNavEnabled })}>
-					<PageMainLayoutContext.Provider value={titlePortalContainer}>
-						<ErrorBoundary>{children}</ErrorBoundary>
-					</PageMainLayoutContext.Provider>
-				</main>
-				{bottomNavEnabled ? <Navigation /> : null}
-				{topNavMenuEnabled ? <MenuContent /> : null}
-				<Toaster bottomNavEnabled={bottomNavEnabled} />
+			<div className="flex flex-col flex-nowrap flex-grow overflow-hidden rounded-t-xl">
+				<div className="flex flex-col flex-nowrap bg-white flex-grow overflow-y-auto overflow-x-hidden rounded-t-xl">
+					<main className={cn('flex-grow w-full', { 'p-5': bottomNavEnabled })}>
+						<PageMainLayoutContext.Provider value={titlePortalContainer}>
+							<ErrorBoundary>{children}</ErrorBoundary>
+						</PageMainLayoutContext.Provider>
+					</main>
+					{bottomNavEnabled ? <Navigation /> : null}
+					{topNavMenuEnabled ? <MenuContent /> : null}
+					<Toaster bottomNavEnabled={bottomNavEnabled} />
+				</div>
 			</div>
 		</div>
 	);
