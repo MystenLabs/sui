90232081e364819575b1c3a15d54d703c2ccd7ce
William Robertson <williamrobertson13@users.noreply.github.com>
2023-09-23 21:10:14 -0400

    [dapp-kit] add build pipeline for bundling UI code with vanilla-extract for styling (#13845)
    ## Description 
    
    This PR sets up the build infrastructure required to package our UI
    components that'll leverage vanilla-extract for styling. We don't want
    external consumers to have to worry about what styling technologies
    we're using, so we're going to pre-bundle our CSS in `dapp-kit` at build
    time 🏗️ . For Sui Explorer, we can still support source imports by
    bundling the CSS in dapp-kit from Sui Explorer!
    
    ## Test Plan 
    - [ESM version] Tested in Sui Explorer that importing the
    `@mysten/dapp-kit/index.css` correctly imports the test CSS I added +
    `ConnectButton` renders successfully
    - [Source version] Tested in Sui Explorer that we can forgo importing
    index.css and bundle the CSS ourselves using the vite vanilla-extract
    plugin + `ConnectButton` renders successfully
    - CI
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ sdk/build-scripts/src/build-dapp-kit.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env tsx
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { buildPackage } from './utils/buildPackage';
+import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
+import autoprefixer from 'autoprefixer';
+import postcss from 'postcss';
+
+buildPackage({
+	plugins: [
+		vanillaExtractPlugin({
+			async processCss(css) {
+				const result = await postcss([autoprefixer]).process(css, {
+					// Suppress source map warning
+					from: undefined,
+				});
+				return result.css;
+			},
+		}),
+	],
+	packages: 'external',
+	bundle: true,
+}).catch((error) => {
+	console.error(error);
+	process.exit(1);
+});
