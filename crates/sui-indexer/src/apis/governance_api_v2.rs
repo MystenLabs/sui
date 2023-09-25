31f6ab3088bbb475d75cb7cd865c6c689dbb5e9a
Brandon Williams <brandon@mystenlabs.com>
2023-09-25 09:22:52 -0500

    indexer: implement part of the governance api

--- crates/sui-indexer/src/apis/governance_api_v2.rs
+++ crates/sui-indexer/src/apis/governance_api_v2.rs
@@ -5,27 +5,44 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-use crate::store::PgIndexerStoreV2;
+use crate::errors::IndexerError;
+use crate::indexer_reader::IndexerReader;
 use async_trait::async_trait;
 use jsonrpsee::core::RpcResult;
 use jsonrpsee::RpcModule;
 
 use sui_json_rpc::api::GovernanceReadApiServer;
 use sui_json_rpc::SuiRpcModule;
-use sui_json_rpc_types::SuiCommittee;
 use sui_json_rpc_types::{DelegatedStake, ValidatorApys};
+use sui_json_rpc_types::{EpochInfo, SuiCommittee};
 use sui_open_rpc::Module;
 use sui_types::base_types::{ObjectID, SuiAddress};
+use sui_types::committee::EpochId;
 use sui_types::sui_serde::BigInt;
 use sui_types::sui_system_state::sui_system_state_summary::SuiSystemStateSummary;
 
+#[derive(Clone)]
 pub(crate) struct GovernanceReadApiV2 {
-    pg_store: PgIndexerStoreV2,
+    inner: IndexerReader,
 }
 
 impl GovernanceReadApiV2 {
-    pub fn new(pg_store: PgIndexerStoreV2) -> Self {
-        Self { pg_store }
+    pub fn new(inner: IndexerReader) -> Self {
+        Self { inner }
+    }
+
+    async fn get_epoch_info(&self, epoch: Option<EpochId>) -> Result<EpochInfo, IndexerError> {
+        match self
+            .inner
+            .spawn_blocking(move |this| this.get_epoch_info(epoch))
+            .await
+        {
+            Ok(Some(epoch_info)) => Ok(epoch_info),
+            Ok(None) => Err(IndexerError::InvalidArgumentError(format!(
+                "Missing epoch {epoch:?}"
+            ))),
+            Err(e) => Err(e),
+        }
     }
 }
 
@@ -35,25 +52,40 @@ impl GovernanceReadApiServer for GovernanceReadApiV2 {
         &self,
         staked_sui_ids: Vec<ObjectID>,
     ) -> RpcResult<Vec<DelegatedStake>> {
+        // Need Dynamic field queries
         unimplemented!()
     }
+
     async fn get_stakes(&self, owner: SuiAddress) -> RpcResult<Vec<DelegatedStake>> {
+        // Need Dynamic field queries
         unimplemented!()
     }
 
     async fn get_committee_info(&self, epoch: Option<BigInt<u64>>) -> RpcResult<SuiCommittee> {
-        unimplemented!()
+        let epoch = self.get_epoch_info(epoch.as_deref().copied()).await?;
+        Ok(epoch.committee().map_err(IndexerError::from)?.into())
     }
 
     async fn get_latest_sui_system_state(&self) -> RpcResult<SuiSystemStateSummary> {
-        unimplemented!()
+        self.inner
+            .spawn_blocking(|this| this.get_latest_sui_system_state())
+            .await
+            .map_err(Into::into)
     }
 
     async fn get_reference_gas_price(&self) -> RpcResult<BigInt<u64>> {
-        unimplemented!()
+        let epoch = self.get_epoch_info(None).await?;
+        Ok(BigInt::from(epoch.reference_gas_price.ok_or_else(
+            || {
+                IndexerError::PersistentStorageDataCorruptionError(
+                    "missing latest reference gas price".to_owned(),
+                )
+            },
+        )?))
     }
 
     async fn get_validators_apy(&self) -> RpcResult<ValidatorApys> {
+        // Need Dynamic field queries
         unimplemented!()
     }
 }
