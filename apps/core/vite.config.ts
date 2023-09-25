e6fcd61cca4fe6d29201f2b9d09640cb1239a761
Manolis Liolios <manos@mystenlabs.com>
2023-09-25 14:58:07 +0300

    Fix CI failing on apps/core project

--- apps/core/vite.config.ts
+++ apps/core/vite.config.ts
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { defineConfig } from 'vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 process.env.VITE_VERCEL_ENV = process.env.VERCEL_ENV || 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	plugins: [vanillaExtractPlugin()],
 	resolve: {
 		conditions: ['source'],
 	},
