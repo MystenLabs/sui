cd98afa85dff558a6f5455256afd0f0619eb837c
Will Yang <willyang@mystenlabs.com>
2023-09-29 11:17:11 -0700

    use indexerReader for convenience

--- crates/sui-graphql-rpc/src/context_data/db_data_provider.rs
+++ crates/sui-graphql-rpc/src/context_data/db_data_provider.rs
@@ -1,106 +1,83 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{
-    context_data::db_data_provider::diesel_macro::read_only_blocking, error::Error,
-    types::digest::Digest,
-};
-use diesel::{
-    r2d2::{self, ConnectionManager},
-    ExpressionMethods, OptionalExtension, PgConnection, QueryDsl, RunQueryDsl,
-};
-use move_bytecode_utils::module_cache::SyncModuleCache;
-use std::{env, str::FromStr, sync::Arc};
+use crate::{error::Error, types::digest::Digest};
+use diesel::{ExpressionMethods, OptionalExtension, PgConnection, QueryDsl, RunQueryDsl};
+use std::str::FromStr;
 use sui_indexer::{
+    indexer_reader::IndexerReader,
     models_v2::{
         checkpoints::StoredCheckpoint, epoch::StoredEpochInfo, transactions::StoredTransaction,
     },
     schema_v2::{checkpoints, epochs, transactions},
+    PgConnectionPoolConfig,
 };
-pub type PgConnectionPool = diesel::r2d2::Pool<ConnectionManager<PgConnection>>;
-pub type PgPoolConnection = diesel::r2d2::PooledConnection<ConnectionManager<PgConnection>>;
 
-use super::module_resolver::PgModuleResolver;
-
-pub(crate) mod diesel_macro {
-    macro_rules! read_only_blocking {
-        ($pool:expr, $query:expr) => {{
-            let mut pg_pool_conn =
-                crate::context_data::db_data_provider::get_pg_pool_connection($pool)?;
-            pg_pool_conn
-                .build_transaction()
-                .read_only()
-                .run($query)
-                .map_err(|e| Error::Internal(e.to_string()))
-        }};
-    }
-    pub(crate) use read_only_blocking;
-}
-
-pub fn get_pg_pool_connection(pool: &PgConnectionPool) -> Result<PgPoolConnection, Error> {
-    pool.get().map_err(|e| {
-        Error::Internal(format!(
-            "Failed to get connection from PG connection pool with error: {:?}",
-            e
-        ))
-    })
-}
-
-pub fn establish_connection_pool() -> PgConnectionPool {
-    let database_url = env::var("PG_DB_URL").expect("PG_DB_URL must be set");
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
-    r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.")
-}
-
-#[allow(dead_code)]
 pub(crate) struct PgManager {
-    pub pool: PgConnectionPool,
-    pub module_cache: Arc<SyncModuleCache<PgModuleResolver>>,
+    pub inner: IndexerReader,
 }
 
 impl PgManager {
-    pub(crate) fn new() -> Self {
-        let pool = establish_connection_pool();
-        Self {
-            pool: pool.clone(),
-            module_cache: Arc::new(SyncModuleCache::new(PgModuleResolver::new(pool))),
-        }
+    pub(crate) fn new<T: Into<String>>(
+        db_url: T,
+        config: Option<PgConnectionPoolConfig>,
+    ) -> Result<Self, Error> {
+        // TODO (wlmyng): support config
+        let mut config = config.unwrap_or(PgConnectionPoolConfig::default());
+        config.set_pool_size(30);
+        let inner = IndexerReader::new_with_config(db_url, config)
+            .map_err(|e| Error::Internal(e.to_string()))?;
+
+        Ok(Self { inner })
+    }
+
+    pub async fn run_query_async<T, E, F>(&self, query: F) -> Result<T, Error>
+    where
+        F: FnOnce(&mut PgConnection) -> Result<T, E> + Send + 'static,
+        E: From<diesel::result::Error> + std::error::Error + Send + 'static,
+        T: Send + 'static,
+    {
+        self.inner
+            .run_query_async(query)
+            .await
+            .map_err(|e| Error::Internal(e.to_string()))
     }
 
     pub(crate) async fn fetch_tx(&self, digest: &str) -> Result<Option<StoredTransaction>, Error> {
-        let digest = Digest::from_str(digest)?;
-        read_only_blocking!(&self.pool, |conn| {
+        let digest = Digest::from_str(digest)?.into_vec();
+
+        self.run_query_async(|conn| {
             transactions::dsl::transactions
-                .filter(transactions::dsl::transaction_digest.eq(digest.into_vec()))
+                .filter(transactions::dsl::transaction_digest.eq(digest))
                 .get_result::<StoredTransaction>(conn) // Expect exactly 0 to 1 result
                 .optional()
         })
+        .await
     }
 
     pub(crate) async fn fetch_latest_epoch(&self) -> Result<StoredEpochInfo, Error> {
-        read_only_blocking!(&self.pool, |conn| {
+        self.run_query_async(|conn| {
             epochs::dsl::epochs
                 .order_by(epochs::dsl::epoch.desc())
                 .limit(1)
                 .first::<StoredEpochInfo>(conn)
         })
+        .await
     }
 
     pub(crate) async fn fetch_epoch(
         &self,
         epoch_id: u64,
     ) -> Result<Option<StoredEpochInfo>, Error> {
-        let bigint_e = i64::try_from(epoch_id)
+        let epoch_id = i64::try_from(epoch_id)
             .map_err(|_| Error::Internal("Failed to convert epoch id to i64".to_string()))?;
-
-        read_only_blocking!(&self.pool, |conn| {
+        self.run_query_async(move |conn| {
             epochs::dsl::epochs
-                .filter(epochs::dsl::epoch.eq(bigint_e))
+                .filter(epochs::dsl::epoch.eq(epoch_id))
                 .get_result::<StoredEpochInfo>(conn) // Expect exactly 0 to 1 result
                 .optional()
         })
+        .await
     }
 
     pub(crate) async fn fetch_epoch_strict(&self, epoch_id: u64) -> Result<StoredEpochInfo, Error> {
@@ -112,12 +89,13 @@ impl PgManager {
     }
 
     pub(crate) async fn fetch_latest_checkpoint(&self) -> Result<StoredCheckpoint, Error> {
-        read_only_blocking!(&self.pool, |conn| {
+        self.run_query_async(|conn| {
             checkpoints::dsl::checkpoints
                 .order_by(checkpoints::dsl::sequence_number.desc())
                 .limit(1)
                 .first::<StoredCheckpoint>(conn)
         })
+        .await
     }
 
     pub(crate) async fn fetch_checkpoint(
@@ -129,8 +107,8 @@ impl PgManager {
 
         match (digest, sequence_number) {
             (Some(digest), None) => {
-                let digest = Digest::from_str(digest)?;
-                query = query.filter(checkpoints::dsl::checkpoint_digest.eq(digest.into_vec()));
+                let digest = Digest::from_str(digest)?.into_vec();
+                query = query.filter(checkpoints::dsl::checkpoint_digest.eq(digest));
             }
             (None, Some(sequence_number)) => {
                 query = query.filter(checkpoints::dsl::sequence_number.eq(sequence_number as i64));
@@ -138,8 +116,7 @@ impl PgManager {
             _ => (), // No-op if invalid input
         }
 
-        read_only_blocking!(&self.pool, |conn| {
-            query.get_result::<StoredCheckpoint>(conn).optional()
-        })
+        self.run_query_async(|conn| query.get_result::<StoredCheckpoint>(conn).optional())
+            .await
     }
 }
