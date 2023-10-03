5879b693221a3edeb3034a1c991d9e9b881b747e
William Robertson <williamrobertson@mystenlabs.com>
2023-10-03 17:24:56 -0400

    fix

--- sdk/dapp-kit/src/components/connect-modal/InfoSection.css.ts
+++ sdk/dapp-kit/src/components/connect-modal/InfoSection.css.ts
@@ -3,14 +3,8 @@
 
 import { style } from '@vanilla-extract/css';
 
-import { themeVars } from '../../themes/themeContract.js';
-
 export const container = style({
 	display: 'flex',
 	flexDirection: 'column',
 	gap: 4,
-});
-
-export const description = style({
-	color: themeVars.colors.bodyMuted,
 });
