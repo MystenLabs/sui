31dbfb4e624f9e3ac5ff7489607d8e5c0122c8fc
Xun Li <lxfind@gmail.com>
2023-10-02 17:08:45 -0700

    Clean up some AuthorityServer functions (#14016)
    ## Description 
    
    A function that operates on the first parameter as the primary entity is
    best defined inside that type.
    This PR makes the following refactorings:
    1. Move "handle_certificate" into AuthorityState, because it really
    belongs there.
    2. Move the execution overload check into TransactionManager as it
    depends on its internal state.
    3. Move the consensus overload check into ConsensusHandler.
    
    Also cleaned up some strange Arc clones.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-core/src/consensus_adapter.rs
+++ crates/sui-core/src/consensus_adapter.rs
@@ -48,6 +48,7 @@ use mysten_metrics::{spawn_monitored_task, GaugeGuard, GaugeGuardFutureExt};
 use sui_simulator::anemo::PeerId;
 use sui_simulator::narwhal_network::connectivity::ConnectionStatus;
 use sui_types::base_types::AuthorityName;
+use sui_types::fp_ensure;
 use sui_types::messages_consensus::ConsensusTransaction;
 use sui_types::messages_consensus::ConsensusTransactionKind;
 use tokio::time::Duration;
@@ -424,7 +425,7 @@ impl ConsensusAdapter {
         committee: &Committee,
         tx_digest: &TransactionDigest,
     ) -> (Duration, usize, usize, usize) {
-        let (mut position, positions_moved, preceding_disconected) =
+        let (mut position, positions_moved, preceding_disconnected) =
             self.submission_position(committee, tx_digest);
 
         const MAX_LATENCY: Duration = Duration::from_secs(5 * 60);
@@ -450,7 +451,7 @@ impl ConsensusAdapter {
             delay_step * position as u32,
             position,
             positions_moved,
-            preceding_disconected,
+            preceding_disconnected,
         )
     }
 
@@ -565,6 +566,14 @@ impl ConsensusAdapter {
         }
         // Then check if submit_semaphore has permits
         self.submit_semaphore.available_permits() > 0
+    }
+
+    pub(crate) fn check_consensus_overload(&self) -> SuiResult {
+        fp_ensure!(
+            self.check_limits(),
+            SuiError::TooManyTransactionsPendingConsensus
+        );
+        Ok(())
     }
 
     fn submit_unchecked(
