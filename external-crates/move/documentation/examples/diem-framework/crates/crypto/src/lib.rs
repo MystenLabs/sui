a73ea215cbe5481d66b24b8b1f726798b9d850cc
Brandon Williams <brandon@mystenlabs.com>
2023-10-04 23:22:27 -0500

    move: remove mutually exclusive features
    In preparation for merging the move and sui workspaces, this eliminates
    mutually exclusive features from a number of of the move crates. (eg
    the ability to specify the length of a move address). This is in order
    to enable the ability to easily perform tests, as is done today in sui,
    by passing the `--all-features` to various cargo build commands.

--- external-crates/move/documentation/examples/diem-framework/crates/crypto/src/lib.rs
+++ external-crates/move/documentation/examples/diem-framework/crates/crypto/src/lib.rs
@@ -35,34 +35,6 @@ pub use once_cell as _once_cell;
 #[doc(hidden)]
 pub use serde_name as _serde_name;
 
-// We use formally verified arithmetic
-// in maintained forks of the dalek suite of libraries ({curve, ed,
-// x}25519-dalek). This is controlled by a feature in the forked crates
-// ('fiat_u64_backend'), which we turn on by default.
-#[cfg(not(any(feature = "fiat", feature = "u64", feature = "u32")))]
-compile_error!(
-    "no dalek arithmetic backend cargo feature enabled! \
-     please enable one of: fiat, u64, u32"
-);
-
-#[cfg(all(feature = "fiat", feature = "u64"))]
-compile_error!(
-    "at most one dalek arithmetic backend cargo feature should be enabled! \
-     please enable exactly one of: fiat, u64, u32"
-);
-
-#[cfg(all(feature = "fiat", feature = "u32"))]
-compile_error!(
-    "at most one dalek arithmetic backend cargo feature should be enabled! \
-     please enable exactly one of: fiat, u64, u32"
-);
-
-#[cfg(all(feature = "u64", feature = "u32"))]
-compile_error!(
-    "at most one dalek arithmetic backend cargo feature should be enabled! \
-     please enable exactly one of: fiat, u64, u32"
-);
-
 // MIRAI's tag analysis makes use of the incomplete const_generics feature, so the module
 // containing the definitions of MIRAI tag types should not get compiled in a release build.
 // The code below fails a build of the crate if mirai is on but debug_assertions is not.
