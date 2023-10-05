f1561bd8bdaabd53b02ead84e750c2b5f098789f
Brandon Williams <brandon@mystenlabs.com>
2023-10-04 23:22:27 -0500

    move: remove mutually exclusive features
    In preparation for merging the move and sui workspaces, this eliminates
    mutually exclusive features from a number of of the move crates. (eg
    the ability to specify the length of a move address). This is in order
    to enable the ability to easily perform tests, as is done today in sui,
    by passing the `--all-features` to various cargo build commands.

--- external-crates/move/move-stdlib/src/lib.rs
+++ external-crates/move/move-stdlib/src/lib.rs
@@ -7,7 +7,6 @@ use move_command_line_common::{
     address::NumericalAddress,
     files::{extension_equals, find_filenames, MOVE_EXTENSION},
 };
-use move_core_types::account_address::AccountAddress;
 use std::{collections::BTreeMap, path::PathBuf};
 
 #[cfg(test)]
@@ -54,12 +53,7 @@ pub fn move_nursery_docs_full_path() -> String {
 }
 
 pub fn move_stdlib_errmap_full_path() -> String {
-    let prefix = if AccountAddress::LENGTH == 16 {
-        "".to_string()
-    } else {
-        format!("address{}-", AccountAddress::LENGTH)
-    };
-    format!("{}/{}{}", env!("CARGO_MANIFEST_DIR"), &prefix, ERRMAP_FILE)
+    format!("{}/{}", env!("CARGO_MANIFEST_DIR"), ERRMAP_FILE)
 }
 
 pub fn move_stdlib_files() -> Vec<String> {
@@ -162,13 +156,7 @@ pub fn build_error_code_map(output_path: &str) {
     move_prover::run_move_prover_errors_to_stderr(options).unwrap();
 }
 
-const ERROR_DESCRIPTIONS: &[u8] = if cfg!(feature = "address20") {
-    include_bytes!("../address20-error_description.errmap")
-} else if cfg!(feature = "address32") {
-    include_bytes!("../address32-error_description.errmap")
-} else {
-    include_bytes!("../error_description.errmap")
-};
+const ERROR_DESCRIPTIONS: &[u8] = include_bytes!("../error_description.errmap");
 
 pub fn error_descriptions() -> &'static [u8] {
     ERROR_DESCRIPTIONS
