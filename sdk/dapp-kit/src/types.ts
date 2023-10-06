9a1351ba3511888b02620444da1f971e900b1ac8
Michael Hayes <michael.hayes@mystenlabs.com>
2023-10-05 17:11:38 -0700

    add docs on dapp-kit themes

--- sdk/dapp-kit/src/types.ts
+++ sdk/dapp-kit/src/types.ts
@@ -2,3 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export type { SignedPersonalMessage, SignedTransactionBlock } from '@mysten/wallet-standard';
+export type { ThemeVars } from './themes/themeContract.js';
