98778db6e3e8fae21502be70e9aba1423066c389
ade <ade@mystenlabs.com>
2023-10-10 11:33:12 -0700

    impl node state getter for simulacrum

--- crates/sui-rest-api/src/node_state_getter.rs
+++ crates/sui-rest-api/src/node_state_getter.rs
@@ -118,3 +118,92 @@ impl NodeStateGetter for AuthorityState {
         self.database.get_object(object_id)
     }
 }
+
+impl sui_rest_api::node_state_getter::NodeStateGetter for simulacrum::Simulacrum<StdRng> {
+    fn get_verified_checkpoint_by_sequence_number(
+        &self,
+        sequence_number: CheckpointSequenceNumber,
+    ) -> SuiResult<VerifiedCheckpoint> {
+        self.store()
+            .get_checkpoint_by_sequence_number(sequence_number)
+            .cloned()
+            .ok_or(SuiError::UserInputError {
+                error: UserInputError::VerifiedCheckpointNotFound(sequence_number),
+            })
+    }
+
+    fn get_latest_checkpoint_sequence_number(&self) -> SuiResult<CheckpointSequenceNumber> {
+        Ok(self
+            .store()
+            .get_highest_checkpint()
+            .map(|checkpoint| *checkpoint.sequence_number())
+            .unwrap_or(0))
+    }
+
+    fn get_checkpoint_contents(
+        &self,
+        content_digest: CheckpointContentsDigest,
+    ) -> SuiResult<CheckpointContents> {
+        self.store()
+            .get_checkpoint_contents(&content_digest)
+            .cloned()
+            .ok_or(SuiError::UserInputError {
+                error: UserInputError::CheckpointContentsNotFound(content_digest),
+            })
+    }
+
+    fn multi_get_transaction_blocks(
+        &self,
+        tx_digests: &[TransactionDigest],
+    ) -> SuiResult<Vec<Option<VerifiedTransaction>>> {
+        Ok(tx_digests
+            .iter()
+            .map(|digest| self.store().get_transaction(digest).cloned())
+            .collect())
+    }
+
+    fn multi_get_executed_effects(
+        &self,
+        digests: &[TransactionDigest],
+    ) -> SuiResult<Vec<Option<TransactionEffects>>> {
+        Ok(digests
+            .iter()
+            .map(|digest| self.store().get_transaction_effects(digest).cloned())
+            .collect())
+    }
+
+    fn multi_get_events(
+        &self,
+        event_digests: &[TransactionEventsDigest],
+    ) -> SuiResult<Vec<Option<TransactionEvents>>> {
+        Ok(event_digests
+            .iter()
+            .map(|digest| self.store().get_transaction_events(digest).cloned())
+            .collect())
+    }
+
+    fn multi_get_object_by_key(
+        &self,
+        object_keys: &[ObjectKey],
+    ) -> Result<Vec<Option<Object>>, SuiError> {
+        object_keys
+            .iter()
+            .map(|key| ObjectStore::get_object_by_key(&self.store(), &key.0, key.1))
+            .collect::<Result<Vec<_>, SuiError>>()
+    }
+
+    fn get_object_by_key(
+        &self,
+        object_id: &ObjectID,
+        version: SequenceNumber,
+    ) -> Result<Option<Object>, SuiError> {
+        Ok(self
+            .store()
+            .get_object_at_version(object_id, version)
+            .cloned())
+    }
+
+    fn get_object(&self, object_id: &ObjectID) -> Result<Option<Object>, SuiError> {
+        Objectstore::get_object(&self.store(), object_id)
+    }
+}
