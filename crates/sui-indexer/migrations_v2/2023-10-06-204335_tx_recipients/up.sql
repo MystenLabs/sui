4712d7359b64f3098c54bafd7a1448d5f3795122
longbowlu <zl9394@gmail.com>
2023-10-10 21:25:41 -0700

    rework some indices

--- crates/sui-indexer/migrations_v2/2023-10-06-204335_tx_recipients/up.sql
+++ crates/sui-indexer/migrations_v2/2023-10-06-204335_tx_recipients/up.sql
@@ -1,6 +1,10 @@
 -- Your SQL goes here
 CREATE TABLE tx_recipients (
     tx_sequence_number          BIGINT       NOT NULL,
+<<<<<<< HEAD
+=======
+    checkpoint_sequence_number  BIGINT       NOT NULL,
+>>>>>>> 86f2b5c57 (rework some indices)
     -- SuiAddress in bytes.
     recipient                   BYTEA        NOT NULL,
     PRIMARY KEY(recipient, tx_sequence_number)
