ec408f249e35d3d2af3008a854554e7229ad0766
Nikhil Mattu <nikhil@mystenlabs.com>
2023-10-12 16:40:16 -0400

    tests

--- apps/wallet/tests/utils/auth.ts
+++ apps/wallet/tests/utils/auth.ts
@@ -15,6 +15,7 @@ export async function createWallet(page: Page, extensionUrl: string) {
 	await page.getByRole('button', { name: /Create Wallet/ }).click();
 	await page.locator('label', { has: page.locator('input[type=checkbox]') }).click();
 	await page.getByRole('link', { name: /Open Sui Wallet/ }).click();
+	await page.getByTestId('bullshark-dismiss').click();
 }
 
 export async function importWallet(page: Page, extensionUrl: string, mnemonic: string | string[]) {
@@ -30,4 +31,5 @@ export async function importWallet(page: Page, extensionUrl: string, mnemonic: s
 	await page.getByLabel('Confirm Account Password').fill(PASSWORD);
 	await page.getByLabel('I read and agreed to the').click();
 	await page.getByRole('button', { name: /Create Wallet/ }).click();
+	await page.getByTestId('bullshark-dismiss').click();
 }
