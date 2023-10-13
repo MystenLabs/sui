ecc719454eec0a2074289d47d9ee5ae9eb9e8eba
Jordan Gensler <jordan@mystenlabs.com>
2023-10-13 15:59:57 -0700

    :(

--- sdk/zklogin/src/nonce.ts
+++ sdk/zklogin/src/nonce.ts
@@ -1,32 +1,26 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { toB64 } from '@mysten/bcs';
 import { PublicKey } from '@mysten/sui.js/cryptography';
 import { toBigEndianBytes } from '@mysten/sui.js/zklogin';
 import { randomBytes } from '@noble/hashes/utils';
 import { base64url } from 'jose';
 
 import { poseidonHash } from './poseidon.js';
-import { toBigIntBE, unknownToBigInt } from './utils.js';
+import { toBigIntBE } from './utils.js';
 
 const NONCE_LENGTH = 27;
 
 export function generateRandomness() {
 	// Once Node 20 enters LTS, we can just use crypto.getRandomValues(new Uint8Array(16)), but until then this improves compatibility:
-	return toB64(randomBytes(16));
+	return String(toBigIntBE(randomBytes(16)));
 }
 
 export function generateNonce(publicKey: PublicKey, maxEpoch: number, randomness: bigint | string) {
 	const publicKeyBytes = toBigIntBE(publicKey.toSuiBytes());
 	const eph_public_key_0 = publicKeyBytes / 2n ** 128n;
 	const eph_public_key_1 = publicKeyBytes % 2n ** 128n;
-	const bigNum = poseidonHash([
-		eph_public_key_0,
-		eph_public_key_1,
-		maxEpoch,
-		unknownToBigInt(randomness),
-	]);
+	const bigNum = poseidonHash([eph_public_key_0, eph_public_key_1, maxEpoch, BigInt(randomness)]);
 	const Z = toBigEndianBytes(bigNum, 20);
 	const nonce = base64url.encode(Z);
 	if (nonce.length !== NONCE_LENGTH) {
--- sdk/zklogin/src/utils.ts
+++ sdk/zklogin/src/utils.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { fromB64, toB64, toHEX } from '@mysten/bcs';
+import { toHEX } from '@mysten/bcs';
 import { PublicKey } from '@mysten/sui.js/cryptography';
 
 import { poseidonHash } from './poseidon.js';
@@ -12,7 +12,7 @@ export const MAX_AUD_VALUE_LENGTH = 145;
 const PACK_WIDTH = 248;
 
 export function getExtendedEphemeralPublicKey(publicKey: PublicKey) {
-	return toB64(publicKey.toSuiBytes());
+	return String(toBigIntBE(publicKey.toSuiBytes()));
 }
 
 export function toBigIntBE(bytes: Uint8Array) {
@@ -21,19 +21,6 @@ export function toBigIntBE(bytes: Uint8Array) {
 		return BigInt(0);
 	}
 	return BigInt(`0x${hex}`);
-}
-
-/** Takes either a bigint, a string-encoded bigint, or a string-encoded Base64 value, and returns a bigint */
-export function unknownToBigInt(value: bigint | string) {
-	if (typeof value === 'bigint') {
-		return value;
-	}
-
-	if (/^\d+$/.test(value)) {
-		return BigInt(value);
-	}
-
-	return toBigIntBE(fromB64(value));
 }
 
 /**
@@ -92,6 +79,6 @@ export function genAddressSeed(
 		hashASCIIStrToField(name, max_name_length),
 		hashASCIIStrToField(value, max_value_length),
 		hashASCIIStrToField(aud, max_aud_length),
-		poseidonHash([unknownToBigInt(salt)]),
+		poseidonHash([BigInt(salt)]),
 	]);
 }
