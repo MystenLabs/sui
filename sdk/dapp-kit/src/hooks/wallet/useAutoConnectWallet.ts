7f8b787dd2ace1a29dac9ff0cd786e64ead9ab91
William Robertson <williamrobertson@mystenlabs.com>
2023-10-18 13:55:16 -0400

    fix

--- sdk/dapp-kit/src/hooks/wallet/useAutoConnectWallet.ts
+++ sdk/dapp-kit/src/hooks/wallet/useAutoConnectWallet.ts
@@ -4,18 +4,27 @@
 import { useEffect } from 'react';
 
 import { useConnectWallet } from './useConnectWallet.js';
+import { useCurrentWallet } from './useCurrentWallet.js';
 import { useWallets } from './useWallets.js';
 import { useWalletStore } from './useWalletStore.js';
 
 export function useAutoConnectWallet(autoConnectEnabled: boolean) {
 	const { mutate: connectWallet } = useConnectWallet();
-	const wallets = useWallets();
 	const setAutoConnectionStatus = useWalletStore((state) => state.setAutoConnectionStatus);
 	const lastConnectedWalletName = useWalletStore((state) => state.lastConnectedWalletName);
 	const lastConnectedAccountAddress = useWalletStore((state) => state.lastConnectedAccountAddress);
+	const wallets = useWallets();
+	const { isDisconnected } = useCurrentWallet();
 
 	useEffect(() => {
-		if (!autoConnectEnabled || !lastConnectedWalletName || !lastConnectedAccountAddress) return;
+		if (
+			!autoConnectEnabled ||
+			!lastConnectedWalletName ||
+			!lastConnectedAccountAddress ||
+			!isDisconnected
+		) {
+			return;
+		}
 
 		const wallet = wallets.find((wallet) => wallet.name === lastConnectedWalletName);
 		if (wallet) {
@@ -33,6 +42,7 @@ export function useAutoConnectWallet(autoConnectEnabled: boolean) {
 	}, [
 		autoConnectEnabled,
 		connectWallet,
+		isDisconnected,
 		lastConnectedAccountAddress,
 		lastConnectedWalletName,
 		setAutoConnectionStatus,
