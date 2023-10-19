8373f2e541a2adb7fa162e7a49c22fe2c51940f7
wlmyng <127570466+wlmyng@users.noreply.github.com>
2023-10-18 21:13:00 -0600

    [graphql] implement checkpoint and transaction connection resolvers on epoch (#14339)
    ## Description 
    
    implement resolvers + apply sorting 
    
    ## Test Plan 
    
    existing tests
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-graphql-rpc/src/types/epoch.rs
+++ crates/sui-graphql-rpc/src/types/epoch.rs
@@ -2,11 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::context_data::context_ext::DataProviderContextExt;
+use crate::context_data::db_data_provider::PgManager;
+use crate::error::Error;
 
 use super::big_int::BigInt;
+use super::checkpoint::Checkpoint;
 use super::date_time::DateTime;
 use super::protocol_config::ProtocolConfigs;
+use super::transaction_block::{TransactionBlock, TransactionBlockFilter};
 use super::validator_set::ValidatorSet;
+use async_graphql::connection::Connection;
 use async_graphql::*;
 
 #[derive(Clone, Debug, PartialEq, Eq, SimpleObject)]
@@ -29,5 +34,60 @@ impl Epoch {
                 .fetch_protocol_config(Some(self.protocol_version))
                 .await?,
         ))
+    }
+
+    async fn checkpoint_connection(
+        &self,
+        ctx: &Context<'_>,
+        first: Option<u64>,
+        after: Option<String>,
+        last: Option<u64>,
+        before: Option<String>,
+    ) -> Result<Option<Connection<String, Checkpoint>>> {
+        ctx.data_unchecked::<PgManager>()
+            .fetch_checkpoints(first, after, last, before, Some(self.epoch_id))
+            .await
+            .extend()
+    }
+
+    async fn transaction_block_connection(
+        &self,
+        ctx: &Context<'_>,
+        first: Option<u64>,
+        after: Option<String>,
+        last: Option<u64>,
+        before: Option<String>,
+        filter: Option<TransactionBlockFilter>,
+    ) -> Result<Option<Connection<String, TransactionBlock>>> {
+        let stored_epoch = ctx
+            .data_unchecked::<PgManager>()
+            .get_epoch(Some(self.epoch_id as i64))
+            .await
+            .extend()?
+            .ok_or(Error::Internal(
+                "Epoch should be able to find itself".to_string(),
+            ))?;
+
+        let existing_filter = filter.unwrap_or_default();
+
+        // TODO (wlmyng): The combination of ordering by tx_sequence_number and filtering by checkpoint_sequence_number is too slow
+        let filter = if last.is_some() {
+            TransactionBlockFilter {
+                before_checkpoint: stored_epoch.last_checkpoint_id.map(|id| id as u64),
+                after_checkpoint: stored_epoch.last_checkpoint_id.map(|id| (id - 10) as u64),
+                ..existing_filter
+            }
+        } else {
+            TransactionBlockFilter {
+                after_checkpoint: Some((stored_epoch.first_checkpoint_id - 1) as u64),
+                before_checkpoint: Some((stored_epoch.first_checkpoint_id + 10) as u64),
+                ..existing_filter
+            }
+        };
+
+        ctx.data_unchecked::<PgManager>()
+            .fetch_txs(first, after, last, before, Some(filter))
+            .await
+            .extend()
     }
 }
