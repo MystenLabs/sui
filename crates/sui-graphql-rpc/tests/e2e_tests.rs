d8ddd4f53dd7455c1cf4e1abdc98e88e68036a98
oxade <93547199+oxade@users.noreply.github.com>
2023-10-23 11:44:22 -0700

    [graphql/rpc]: initial use of simulator in tests (#14366)
    ## Description 
    
    This PR adds a test which uses the deterministic simulator.
    Will migrate other tests to use this
    
    ## Test Plan 
    
    e2e included
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-graphql-rpc/tests/e2e_tests.rs
+++ crates/sui-graphql-rpc/tests/e2e_tests.rs
@@ -5,11 +5,12 @@
 mod tests {
     use sui_graphql_rpc::config::ConnectionConfig;
 
+    #[ignore]
     #[tokio::test]
-    async fn test_simple_client() {
+    async fn test_simple_client_validator_cluster() {
         let connection_config = ConnectionConfig::ci_integration_test_cfg();
 
-        let cluster = sui_graphql_rpc::cluster::start_cluster(connection_config).await;
+        let cluster = sui_graphql_rpc::cluster::start_cluster(connection_config, None).await;
 
         // Wait for servers to start and catchup
         tokio::time::sleep(std::time::Duration::from_secs(10)).await;
@@ -37,6 +38,48 @@ mod tests {
             "{{\"data\":{{\"chainIdentifier\":\"{}\"}}}}",
             chain_id_actual
         );
+        assert_eq!(&format!("{}", res), &exp);
+    }
+
+    #[tokio::test]
+    async fn test_simple_client_simulator_cluster() {
+        use rand::rngs::StdRng;
+        use rand::SeedableRng;
+        use simulacrum::Simulacrum;
+        use std::sync::Arc;
+        use sui_types::digests::ChainIdentifier;
+        let rng = StdRng::from_seed([12; 32]);
+        let mut sim = Simulacrum::new_with_rng(rng);
+
+        sim.create_checkpoint();
+        sim.create_checkpoint();
+
+        let genesis_checkpoint_digest1 = sim
+            .store()
+            .get_checkpoint_by_sequence_number(0)
+            .unwrap()
+            .digest();
+
+        let chain_id_actual = format!("{}", ChainIdentifier::from(*genesis_checkpoint_digest1));
+        let exp = format!(
+            "{{\"data\":{{\"chainIdentifier\":\"{}\"}}}}",
+            chain_id_actual
+        );
+        let connection_config = ConnectionConfig::ci_integration_test_cfg();
+        let cluster =
+            sui_graphql_rpc::cluster::serve_simulator(connection_config, 3000, Arc::new(sim)).await;
+
+        let query = r#"
+            query {
+                chainIdentifier
+            }
+        "#;
+        let res = cluster
+            .graphql_client
+            .execute(query.to_string(), vec![])
+            .await
+            .unwrap();
+
         assert_eq!(&format!("{}", res), &exp);
     }
 }
