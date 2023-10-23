b310b2176d7924649be4e46b78edcd927a27a7e0
ade <ade@mystenlabs.com>
2023-10-23 10:31:34 -0700

    fix comments

--- crates/sui-graphql-rpc/src/types/big_int.rs
+++ crates/sui-graphql-rpc/src/types/big_int.rs
@@ -37,9 +37,8 @@ impl FromStr for BigInt {
         let mut r = s;
         let mut signed = false;
         // check that all are digits and first can start with -
-        if s.starts_with('-') {
-            // Ok to unwrap because we know it starts with -
-            r = s.strip_prefix('-').unwrap();
+        if let Some(suffix) = s.strip_prefix('-') {
+            r = suffix;
             signed = true;
         }
         r = r.trim_start_matches('0');
