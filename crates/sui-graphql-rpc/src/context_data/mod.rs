13f255a0398cf03db5c8e669637eb9ca4b2f33fa
Ashok Menon <ashok@mystenlabs.com>
2023-10-24 22:58:00 +0100

    [GraphQL] Introducing PackageCache (#14319)
    ## Description
    
    This data source handles random access queries for packages, initially
    to calculate type layouts, but eventually to serve all requests that are
    related to package information (modules, structs, functions, types,
    layouts, signatures, etc). It is **upgrade aware**, meaning it takes
    into account a package's linkage and type origin tables to relocate the
    package IDs of structs that it references.
    
    The store is implemented as an LRU cache on top of an underlying data
    source, which currently calls out to the existing RPC using `SuiClient`,
    but will eventually read from the DB -- the underlying data source is
    abstracted behind a trait (mainly for testability), but this should also
    make it simple to adopt the DB when the time comes.
    
    The cache already supports "Reinterpreting Package IDs", an extension
    feature that allows a type to be referred to at any package ID after the
    one that it was first defined at, because it was easier to add support
    for it than prevent the behaviour.
    
    Although this implementation is currently in the `sui-graphql-rpc`
    trait, it is self-contained (it only depends on core/underlying crates)
    and could also be re-used to do upgrade-aware type and layout
    calculation in the indexer codebase.
    
    The following related work will be handled in follow-up PRs, to keep the
    size of this PR as small as possible.
    
    - Moving the cache's configuration into `ServiceConfig`.
    - Integrating `PackageCache` into the server (using it to handle queries
    for a type's layout).
    - Implementations of `OpenMoveType`, and its `OpenMoveTypeSignature` in
    GraphQL.
    
    There are also some further TODOs highlighting potential future
    optimisations that are not planned for the immediate future, but that we
    can revisit if we need to.
    
    ## Test Plan
    
    New unit tests:
    
    ```
    sui-graphql-rpc$ cargo nextest run -- package_cache
    ```
    
    ## Stack
    
    - #14376 
    - #14377 
    - #14378

--- crates/sui-graphql-rpc/src/context_data/mod.rs
+++ crates/sui-graphql-rpc/src/context_data/mod.rs
@@ -3,6 +3,8 @@
 
 pub(crate) mod data_provider;
 pub(crate) mod db_data_provider;
+#[allow(dead_code)]
+pub(crate) mod package_cache; // TODO: Remove annotation once integrated
 pub(crate) mod sui_sdk_data_provider;
 
 pub const DEFAULT_PAGE_SIZE: u64 = 10;
