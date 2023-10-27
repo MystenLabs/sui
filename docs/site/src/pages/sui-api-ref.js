dc9ebe6f5d13af772588b25b9685f4e176d6f427
ronny-mysten <118224482+ronny-mysten@users.noreply.github.com>
2023-10-27 14:46:58 -0600

    [WIP] Doc site and content update  (#14480)
    ## Description 
    
    Site and content to support updated website.
    
    ## Test Plan 
    
    Local
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [X] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    
    The documentation site, docs.sui.io, will update soon to one using a
    Docusaurus framework. This change will likely break bookmarks as the
    content has been restructured to better meet users needs. The site URL
    will not change.

---
+++ docs/site/src/pages/sui-api-ref.js
@@ -0,0 +1,17 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import React from "react";
+import Layout from "@theme/Layout";
+import API from "../components/API";
+
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
+export default function JsonRpc() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`Sui API Reference | ${siteConfig.title}`}>
+      <API />
+    </Layout>
+  );
+}
