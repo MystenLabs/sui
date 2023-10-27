a9d102750134565d6eb2c6a8415a413e9b3d5573
Anastasios Kichidis <tasos@mystenlabs.com>
2023-10-27 10:47:39 +0100

    [refactor] enable flag for consensus submission. Tuned ranges

--- crates/sui-core/src/consensus_throughput_calculator.rs
+++ crates/sui-core/src/consensus_throughput_calculator.rs
@@ -80,7 +80,7 @@ impl ThroughputProfileRanges {
         match chain_id.chain() {
             Chain::Mainnet => ThroughputProfileRanges::new(&to_profiles(500, 2_000)),
             Chain::Testnet => ThroughputProfileRanges::new(&to_profiles(500, 2_000)),
-            Chain::Unknown => ThroughputProfileRanges::new(&to_profiles(1_000, 2_000)),
+            Chain::Unknown => ThroughputProfileRanges::new(&to_profiles(500, 2_000)),
         }
     }
 
