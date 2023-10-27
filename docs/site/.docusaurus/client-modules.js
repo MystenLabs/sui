edcb2f5157f50491ab07f0627e30a65feecfd605
Ronny Roland <ronmilton@mystenlabs.com>
2023-10-27 08:25:53 -0600

    Doc site and content update

---
+++ docs/site/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/ronnyroland/Code/sui/node_modules/.pnpm/infima@0.2.0-alpha.43/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/ronnyroland/Code/sui/node_modules/.pnpm/@docusaurus+theme-classic@2.4.3_react-dom@17.0.2_react@17.0.2_typescript@4.9.5/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/ronnyroland/Code/sui/node_modules/.pnpm/@docusaurus+theme-classic@2.4.3_react-dom@17.0.2_react@17.0.2_typescript@4.9.5/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/ronnyroland/Code/sui/docs/site/src/css/custom.css'),
+];
