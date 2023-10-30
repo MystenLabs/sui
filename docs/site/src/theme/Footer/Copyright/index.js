85d94fc4b278561c9894efa42d7104e5e09c8dea
Ronny Roland <ronmilton@mystenlabs.com>
2023-10-29 21:15:08 -0600

    More merge conflicts fixed

--- docs/site/src/theme/Footer/Copyright/index.js
+++ docs/site/src/theme/Footer/Copyright/index.js
@@ -4,11 +4,7 @@ import React from "react";
 export default function FooterCopyright({ copyright }) {
   return (
     <div
-<<<<<<< HEAD
       className="text-sm lg:text-xs xl:text-sm mt-2"
-=======
-      className="text-sm"
->>>>>>> main
       // Developer provided the HTML, so assume it's safe.
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: copyright }}
