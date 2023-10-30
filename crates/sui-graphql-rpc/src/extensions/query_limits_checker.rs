40379dafff3ecf4c53da18b4be44b5859bdc03d0
oxade <93547199+oxade@users.noreply.github.com>
2023-10-30 14:42:48 -0700

    [graphql/rpc] add fragment limits inline (#14474)
    ## Description 
    
    This counts fragments in the query complexity
    Fragments can be defined but not used, causing them to still be parsed.
    We will limit these early on too in subseq PR.
    
    
    ## Test Plan 
    
    Manual, coming next
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-graphql-rpc/src/extensions/query_limits_checker.rs
+++ crates/sui-graphql-rpc/src/extensions/query_limits_checker.rs
@@ -1,14 +1,21 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::config::Limits;
 use crate::config::ServiceConfig;
+use crate::error::code::INTERNAL_SERVER_ERROR;
+use crate::error::graphql_error_at_pos;
 use crate::metrics::RequestMetrics;
 use async_graphql::extensions::NextParseQuery;
 use async_graphql::extensions::NextRequest;
 use async_graphql::parser::types::ExecutableDocument;
-use async_graphql::parser::types::Selection::Field;
+use async_graphql::parser::types::FragmentDefinition;
+use async_graphql::parser::types::Selection;
+use async_graphql::parser::types::SelectionSet;
 use async_graphql::value;
+use async_graphql::Name;
 use async_graphql::Pos;
+use async_graphql::Positioned;
 use async_graphql::Response;
 use async_graphql::ServerResult;
 use async_graphql::Variables;
@@ -19,6 +26,7 @@ use async_graphql::{
 use axum::headers;
 use axum::http::HeaderName;
 use axum::http::HeaderValue;
+use std::collections::HashMap;
 use std::collections::VecDeque;
 use std::sync::Arc;
 use tokio::sync::Mutex;
@@ -66,6 +74,22 @@ impl ExtensionFactory for QueryLimitsChecker {
     }
 }
 
+struct ComponentCost {
+    pub num_nodes: u32,
+    pub depth: u32,
+}
+
+impl std::ops::Add for ComponentCost {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            num_nodes: self.num_nodes + rhs.num_nodes,
+            depth: self.depth + rhs.depth,
+        }
+    }
+}
+
 #[async_trait::async_trait]
 impl Extension for QueryLimitsChecker {
     async fn request(&self, ctx: &ExtensionContext<'_>, next: NextRequest<'_>) -> Response {
@@ -96,6 +120,8 @@ impl Extension for QueryLimitsChecker {
         next: NextParseQuery<'_>,
     ) -> ServerResult<ExecutableDocument> {
         // TODO: limit number of variables, fragments, etc
+        // TODO: limit/ban directives for now
+        // TODO: limit overall query size
 
         let cfg = ctx
             .data::<ServiceConfig>()
@@ -124,60 +150,33 @@ impl Extension for QueryLimitsChecker {
             ));
         }
 
-        // Use BFS to analyze the query and
-        // count the number of nodes and the depth of the query
+        // TODO: Limit the complexity of fragments early on
 
-        // Queue to store the nodes at each level
-        let mut que = VecDeque::new();
-        // Number of nodes in the query
-        let mut num_nodes: u32 = 0;
-        // Depth of the query
-        let mut depth: u32 = 0;
-        // Number of nodes at each level
-        let mut level_len;
+        let mut running_costs = ComponentCost {
+            depth: 0,
+            num_nodes: 0,
+        };
 
         for (_name, oper) in doc.operations.iter() {
-            for sel in oper.node.selection_set.node.items.iter() {
-                que.push_back(sel);
-                num_nodes += 1;
-                self.check_limits(cfg, num_nodes, depth, Some(sel.pos))?;
-            }
+            self.analyze_selection_set(
+                &cfg.limits,
+                &doc.fragments,
+                &oper.node.selection_set,
+                &mut running_costs,
+            )?;
         }
-        // Track the number of nodes at first level if any
-        level_len = que.len();
 
-        while !que.is_empty() {
-            // Signifies the start of a new level
-            depth += 1;
-            self.check_limits(cfg, num_nodes, depth, None)?;
-            while level_len > 0 {
-                // Ok to unwrap since we checked for empty queue
-                // and level_len > 0
-                let sel = que.pop_front().unwrap();
-                // TODO: check for fragments, variables, etc
-                if let Field(f) = &sel.node {
-                    // TODO: check for directives, variables, etc
-                    for sel in f.node.selection_set.node.items.iter() {
-                        que.push_back(sel);
-                        num_nodes += 1;
-                        self.check_limits(cfg, num_nodes, depth, Some(sel.pos))?;
-                    }
-                }
-                level_len -= 1;
-            }
-            level_len = que.len();
-        }
         if ctx.data_opt::<ShowUsage>().is_some() {
             *self.validation_result.lock().await = Some(ValidationRes {
-                num_nodes,
-                depth,
+                num_nodes: running_costs.num_nodes,
+                depth: running_costs.depth,
                 num_variables: variables.len() as u32,
                 num_fragments: doc.fragments.len() as u32,
             });
         }
         if let Some(metrics) = ctx.data_opt::<Arc<RequestMetrics>>() {
-            metrics.num_nodes.observe(num_nodes as f64);
-            metrics.query_depth.observe(depth as f64);
+            metrics.num_nodes.observe(running_costs.num_nodes as f64);
+            metrics.query_depth.observe(running_costs.depth as f64);
             metrics.query_payload_size.observe(query.len() as f64);
         }
         Ok(doc)
@@ -185,33 +184,107 @@ impl Extension for QueryLimitsChecker {
 }
 
 impl QueryLimitsChecker {
-    fn check_limits(
+    fn analyze_selection_set(
         &self,
-        cfg: &ServiceConfig,
-        nodes: u32,
-        depth: u32,
-        pos: Option<Pos>,
+        limits: &Limits,
+        fragment_defs: &HashMap<Name, Positioned<FragmentDefinition>>,
+        sel_set: &Positioned<SelectionSet>,
+        cost: &mut ComponentCost,
     ) -> ServerResult<()> {
-        if nodes > cfg.limits.max_query_nodes {
-            return Err(ServerError::new(
-                format!(
-                    "Query has too many nodes. The maximum allowed is {}",
-                    cfg.limits.max_query_nodes
-                ),
-                pos,
-            ));
+        // Use BFS to analyze the query and count the number of nodes and the depth of the query
+
+        // Queue to store the nodes at each level
+        let mut que = VecDeque::new();
+
+        for top_level_sel in sel_set.node.items.iter() {
+            que.push_back(top_level_sel);
+            cost.num_nodes += 1;
+            check_limits(limits, cost.num_nodes, cost.depth, Some(top_level_sel.pos))?;
         }
 
-        if depth > cfg.limits.max_query_depth {
-            return Err(ServerError::new(
-                format!(
-                    "Query has too many levels of nesting. The maximum allowed is {}",
-                    cfg.limits.max_query_depth
-                ),
-                pos,
-            ));
-        }
+        // Track the number of nodes at first level if any
+        let mut level_len = que.len();
 
+        while !que.is_empty() {
+            // Signifies the start of a new level
+            cost.depth += 1;
+            check_limits(limits, cost.num_nodes, cost.depth, None)?;
+            while level_len > 0 {
+                // Ok to unwrap since we checked for empty queue
+                // and level_len > 0
+                let curr_sel = que.pop_front().unwrap();
+
+                match &curr_sel.node {
+                    Selection::Field(f) => {
+                        for field_sel in f.node.selection_set.node.items.iter() {
+                            que.push_back(field_sel);
+                            cost.num_nodes += 1;
+                            check_limits(limits, cost.num_nodes, cost.depth, Some(field_sel.pos))?;
+                        }
+                    }
+                    Selection::FragmentSpread(fs) => {
+                        let frag_name = &fs.node.fragment_name.node;
+                        let frag_def = fragment_defs.get(frag_name).ok_or_else(|| {
+                            graphql_error_at_pos(
+                                INTERNAL_SERVER_ERROR,
+                                format!(
+                                    "Fragment {} not found but present in fragment list",
+                                    frag_name
+                                ),
+                                fs.pos,
+                            )
+                        })?;
+
+                        // TODO: this is inefficient as we might loop over same fragment multiple times
+                        // Ideally web should cache the costs of fragments we've seen before
+                        // Will do as enhancement
+                        for frag_sel in frag_def.node.selection_set.node.items.iter() {
+                            que.push_back(frag_sel);
+                            cost.num_nodes += 1;
+                            check_limits(limits, cost.num_nodes, cost.depth, Some(frag_sel.pos))?;
+                        }
+                    }
+                    Selection::InlineFragment(fs) => {
+                        for in_frag_sel in fs.node.selection_set.node.items.iter() {
+                            que.push_back(in_frag_sel);
+                            cost.num_nodes += 1;
+                            check_limits(
+                                limits,
+                                cost.num_nodes,
+                                cost.depth,
+                                Some(in_frag_sel.pos),
+                            )?;
+                        }
+                    }
+                }
+                level_len -= 1;
+            }
+            level_len = que.len();
+        }
         Ok(())
     }
+}
+
+fn check_limits(limits: &Limits, nodes: u32, depth: u32, pos: Option<Pos>) -> ServerResult<()> {
+    if nodes > limits.max_query_nodes {
+        return Err(ServerError::new(
+            format!(
+                "Query has too many nodes. The maximum allowed is {}",
+                limits.max_query_nodes
+            ),
+            pos,
+        ));
+    }
+
+    if depth > limits.max_query_depth {
+        return Err(ServerError::new(
+            format!(
+                "Query has too many levels of nesting. The maximum allowed is {}",
+                limits.max_query_depth
+            ),
+            pos,
+        ));
+    }
+
+    Ok(())
 }
