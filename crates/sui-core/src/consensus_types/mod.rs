c82205760fc3c0270325cc607b2bcc9da0711ec6
Xun Li <lxfind@gmail.com>
2023-10-31 17:13:27 -0700

    Make reputation score abstract

---
+++ crates/sui-core/src/consensus_types/mod.rs
@@ -0,0 +1,10 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+pub(crate) mod committee_api;
+pub(crate) mod consensus_output_api;
+
+/// An unique integer ID for a validator used by consensus.
+/// In Narwhal, this is the inner value of the `AuthorityIdentifier` type.
+/// In Mysticeti, this is used the same way as the AuthorityIndex type there.
+pub type AuthorityIndex = u16;
