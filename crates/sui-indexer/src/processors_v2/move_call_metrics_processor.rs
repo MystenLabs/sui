6f88a25f0fbc7a5f5868a156d069c903e70bb521
Ge Gao <ge@mystenlabs.com>
2023-11-02 13:03:22 -0700

    parallel lookup with rayon

--- crates/sui-indexer/src/processors_v2/move_call_metrics_processor.rs
+++ crates/sui-indexer/src/processors_v2/move_call_metrics_processor.rs
@@ -1,8 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-// similar to AddressMetricsProcessor in address_metrics_processor.rs
-
+use rayon::prelude::*;
 use std::collections::HashMap;
 use tracing::info;
 
@@ -51,6 +50,10 @@ where
                     last_end_cp_seq + MOVE_CALL_PROCESSOR_BATCH_SIZE + 1,
                 )
                 .await?;
+            info!(
+                "Downloaded checkpoints for move call metrics on checkpoint {}",
+                last_end_cp_seq + MOVE_CALL_PROCESSOR_BATCH_SIZE
+            );
             let end_cp = cps
                 .last()
                 .ok_or(IndexerError::PostgresReadError(
@@ -58,9 +61,13 @@ where
                 ))?
                 .clone();
             let cp_epoch_map = cps
-                .iter()
+                .par_iter()
                 .map(|cp| (cp.sequence_number, cp.epoch))
                 .collect::<HashMap<_, _>>();
+            info!(
+                "Constructed cp to epoch maps for move call metrics on checkpoint {}",
+                last_end_cp_seq + MOVE_CALL_PROCESSOR_BATCH_SIZE
+            );
             let tx_checkpoints = self
                 .store
                 .get_tx_checkpoints_in_checkpoint_range(
@@ -68,8 +75,12 @@ where
                     end_cp.sequence_number + 1,
                 )
                 .await?;
+            info!(
+                "Downloaded transactions for checkpoint: {}",
+                end_cp.sequence_number
+            );
             let tx_cp_map = tx_checkpoints
-                .iter()
+                .par_iter()
                 .map(|tx| (tx.tx_sequence_number, tx.checkpoint_sequence_number))
                 .collect::<HashMap<_, _>>();
             let start_tx_seq = tx_checkpoints
@@ -84,13 +95,17 @@ where
                     "Cannot read last tx from PG for move call metrics".to_string(),
                 ))?
                 .tx_sequence_number;
+            info!(
+                "Constructed tx to cp maps for move call metrics on checkpoint {}",
+                end_cp.sequence_number
+            );
 
             let stored_move_calls = self
                 .store
                 .get_move_calls_in_tx_range(start_tx_seq, end_tx_seq + 1)
                 .await?;
             let move_calls_to_commit = stored_move_calls
-                .into_iter()
+                .into_par_iter()
                 .filter_map(|call| {
                     let cp = tx_cp_map.get(&call.tx_sequence_number)?;
                     let epoch = cp_epoch_map.get(cp)?;
@@ -112,18 +127,20 @@ where
                 move_call_count, end_cp_seq
             );
 
-            self.store.persist_move_calls(move_calls_to_commit).await?;
-            info!(
-                "Persisted {} move_calls for checkpoint: {}",
-                move_call_count, end_cp_seq
-            );
-
-            let move_call_metrics = self.store.calculate_move_call_metrics(end_cp).await?;
+            let persist_move_call_handle = self.store.persist_move_calls(move_calls_to_commit);
+            let calculate_move_call_metrics_handle = self.store.calculate_move_call_metrics(end_cp);
+            let (persist_move_call_res, calculate_move_call_metrics_res) =
+                tokio::join!(persist_move_call_handle, calculate_move_call_metrics_handle);
+            persist_move_call_res?;
+            let move_call_metrics = calculate_move_call_metrics_res?;
             self.store
                 .persist_move_call_metrics(move_call_metrics)
                 .await?;
             last_end_cp_seq = end_cp_seq;
-            info!("Persisted move_call_metrics for checkpoint: {}", end_cp_seq);
+            info!(
+                "Persisted {} move_calls and move_call_metrics for checkpoint: {}",
+                move_call_count, end_cp_seq
+            );
         }
     }
 }
