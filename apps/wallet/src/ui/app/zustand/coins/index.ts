6446eac6906a76109e354840ab9f9626d3bfc56f
Phong Lam <phong@mystenlabs.com>
2023-11-03 11:35:44 -0700

    add zustand

--- apps/wallet/src/ui/app/zustand/coins/index.ts
+++ apps/wallet/src/ui/app/zustand/coins/index.ts
@@ -2,16 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { createSelectors } from '_app/zustand/createSelectors';
+import { set as idbSet } from 'idb-keyval';
 import { create } from 'zustand';
 
 interface CoinsState {
 	pinnedCoinTypes: string[];
-	setPinnedCoinTypes: (pinnedCoins: string[]) => void;
+	setPinnedCoinTypes: (key: string, pinnedCoins: string[]) => void;
 }
 
-const useCoinsStoreBase = create<CoinsState>()((set) => ({
+const coinsStoreBase = create<CoinsState>();
+
+const useCoinsStoreBase = coinsStoreBase((set) => ({
 	pinnedCoinTypes: [],
-	setPinnedCoinTypes: (pinnedCoins: string[]) => set((state) => ({ pinnedCoinTypes: pinnedCoins })),
+	setPinnedCoinTypes: async (key, pinnedCoins) => {
+		await idbSet(key, pinnedCoins);
+		set(() => ({ pinnedCoinTypes: pinnedCoins }));
+	},
 }));
 
 export const useCoinsStore = createSelectors(useCoinsStoreBase);
