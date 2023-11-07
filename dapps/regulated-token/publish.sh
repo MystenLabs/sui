c6ecd6e8cd52bdad70b8c126945e33ab6f05ee2e
Damir Shamanaev <damirka.ru@gmail.com>
2023-11-08 01:22:12 +0300

    adds missing license in .sh file

--- dapps/regulated-token/publish.sh
+++ dapps/regulated-token/publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright (c) Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
 
 PUBLISH=$(sui client publish --gas-budget 100000000 --skip-dependency-verification --with-unpublished-dependencies --json);
 
