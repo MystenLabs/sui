2b0cc3220d16e906d3a5843d533e88136df5f1ad
Will Yang <willyang@mystenlabs.com>
2023-11-08 12:10:08 -0500

    first go - i think we can lazily resolve supply

--- crates/sui-graphql-rpc/src/types/coin.rs
+++ crates/sui-graphql-rpc/src/types/coin.rs
@@ -1,11 +1,16 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::{context_data::db_data_provider::PgManager, error::Error};
+
 use super::big_int::BigInt;
 use super::move_object::MoveObject;
 use async_graphql::*;
 
+use sui_json_rpc::coin_api::parse_to_struct_tag;
+use sui_types::balance::Supply;
 use sui_types::coin::Coin as NativeCoin;
+use sui_types::gas_coin::{GAS, TOTAL_SUPPLY_SUI};
 
 #[derive(Clone)]
 pub(crate) struct Coin {
@@ -47,5 +52,38 @@ impl TryFrom<&MoveObject> for Coin {
             native: bcs::from_bytes(move_object.native.contents())
                 .map_err(CoinDowncastError::Bcs)?,
         })
+    }
+}
+
+#[derive(SimpleObject)]
+#[graphql(complex)]
+pub(crate) struct CoinMetadata {
+    pub decimals: Option<u8>,
+    pub name: Option<String>,
+    pub symbol: Option<String>,
+    pub description: Option<String>,
+    pub icon_url: Option<String>,
+    #[graphql(skip)]
+    pub coin_type: String,
+}
+
+#[ComplexObject]
+impl CoinMetadata {
+    async fn supply(&self, ctx: &Context<'_>) -> Result<Option<BigInt>> {
+        let coin_struct = parse_to_struct_tag(&self.coin_type)?;
+        let total_supply = if GAS::is_gas(&coin_struct) {
+            Supply {
+                value: TOTAL_SUPPLY_SUI,
+            }
+        } else {
+            ctx.data_unchecked::<PgManager>()
+                .inner
+                .get_total_supply_in_blocking_task(coin_struct)
+                .await
+                .map_err(|e| Error::Internal(e.to_string()))
+                .extend()?
+        };
+
+        Ok(Some(BigInt::from(total_supply.value)))
     }
 }
