072347161a1e8825f7f2ce5e953e836f88d981d6
Ronny Roland <ronmilton@mystenlabs.com>
2023-11-07 18:01:14 -0700

    Create directory because empty ones are not created on vercel

--- docs/site/src/utils/getopenrpcspecs.js
+++ docs/site/src/utils/getopenrpcspecs.js
@@ -5,6 +5,29 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
+// Create directory
+
+const topdir = path.join(__dirname, "../open-spec");
+const mainnetdir = path.join(topdir, "mainnet");
+const testnetdir = path.join(topdir, "testnet");
+const devnetdir = path.join(topdir, "devnet");
+
+if (!fs.existsSync(topdir)){
+    fs.mkdirSync(topdir);
+}
+
+if (!fs.existsSync(mainnetdir)){
+    fs.mkdirSync(mainnetdir);
+}
+
+if (!fs.existsSync(testnetdir)){
+    fs.mkdirSync(testnetdir);
+}
+
+if (!fs.existsSync(devnetdir)){
+    fs.mkdirSync(devnetdir);
+}
+
 // Download Mainnet OpenRPC spec
 axios({
     method: "get",
@@ -12,8 +35,8 @@ axios({
     responseType: "blob"
 }).then((res) => {
 
-    if (fs.existsSync(path.join(__dirname, "../open-spec/mainnet/openrpc_backup.json"))){
-        fs.unlink(path.join(__dirname, "../open-spec/mainnet/openrpc_backup.json"), (err) => {
+    if (fs.existsSync(path.join(mainnetdir, "openrpc_backup.json"))){
+        fs.unlink(path.join(mainnetdir, "openrpc_backup.json"), (err) => {
             if (err) {
                 return console.log(err);
             }
@@ -22,11 +45,11 @@ axios({
     } else {
         console.log("Backup file does not exist.")
     }
-    if (fs.existsSync(path.join(__dirname, "../open-spec/mainnet/openrpc.json"))){
-        fs.renameSync(path.join(__dirname, "../open-spec/mainnet/openrpc.json"), path.join(__dirname, "../open-spec/mainnet/openrpc_backup.json"));
+    if (fs.existsSync(path.join(mainnetdir, "openrpc.json"))){
+        fs.renameSync(path.join(mainnetdir, "openrpc.json"), path.join(mainnetdir, "openrpc_backup.json"));
     }
     
-    fs.writeFileSync(path.join(__dirname, "../open-spec/mainnet/openrpc.json"), res.data, 'utf8');
+    fs.writeFileSync(path.join(mainnetdir, "openrpc.json"), res.data, 'utf8');
 }).catch(err => {
     console.log("Error downloading Mainnet openrpc spec.");
     console.error(err);
@@ -39,8 +62,8 @@ axios({
     responseType: "blob"
 }).then((res) => {
 
-    if (fs.existsSync(path.join(__dirname, "../open-spec/testnet/openrpc_backup.json"))){
-        fs.unlink(path.join(__dirname, "../open-spec/testnet/openrpc_backup.json"), (err) => {
+    if (fs.existsSync(path.join(testnetdir, "openrpc_backup.json"))){
+        fs.unlink(path.join(testnetdir, "openrpc_backup.json"), (err) => {
             if (err) {
                 return console.log(err);
             }
@@ -49,11 +72,11 @@ axios({
     } else {
         console.log("Backup file does not exist.")
     }
-    if (fs.existsSync(path.join(__dirname, "../open-spec/testnet/openrpc.json"))){
-        fs.renameSync(path.join(__dirname, "../open-spec/testnet/openrpc.json"), path.join(__dirname, "../open-spec/testnet/openrpc_backup.json"));
+    if (fs.existsSync(path.join(testnetdir, "openrpc.json"))){
+        fs.renameSync(path.join(testnetdir, "openrpc.json"), path.join(testnetdir, "openrpc_backup.json"));
     }
     
-    fs.writeFileSync(path.join(__dirname, "../open-spec/testnet/openrpc.json"), res.data, 'utf8');
+    fs.writeFileSync(path.join(testnetdir, "openrpc.json"), res.data, 'utf8');
 }).catch(err => {
     console.log("Error downloading Testnet openrpc spec.");
     console.error(err);
@@ -66,8 +89,8 @@ axios({
     responseType: "blob"
 }).then((res) => {
 
-    if (fs.existsSync(path.join(__dirname, "../open-spec/devnet/openrpc_backup.json"))){
-        fs.unlink(path.join(__dirname, "../open-spec/devnet/openrpc_backup.json"), (err) => {
+    if (fs.existsSync(path.join(devnetdir, "openrpc_backup.json"))){
+        fs.unlink(path.join(devnetdir, "openrpc_backup.json"), (err) => {
             if (err) {
                 return console.log(err);
             }
@@ -76,11 +99,11 @@ axios({
     } else {
         console.log("Backup file does not exist.")
     }
-    if (fs.existsSync(path.join(__dirname, "../open-spec/devnet/openrpc.json"))){
-        fs.renameSync(path.join(__dirname, "../open-spec/devnet/openrpc.json"), path.join(__dirname, "../open-spec/devnet/openrpc_backup.json"));
+    if (fs.existsSync(path.join(devnetdir, "openrpc.json"))){
+        fs.renameSync(path.join(devnetdir, "openrpc.json"), path.join(devnetdir, "openrpc_backup.json"));
     }
     
-    fs.writeFileSync(path.join(__dirname, "../open-spec/devnet/openrpc.json"), res.data, 'utf8');
+    fs.writeFileSync(path.join(devnetdir, "openrpc.json"), res.data, 'utf8');
 }).catch(err => {
     console.log("Error downloading Devnet openrpc spec.");
     console.error(err);
