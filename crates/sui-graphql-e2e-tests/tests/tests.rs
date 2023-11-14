3329158814afcb78513957d8ade38342477f7b46
ade <ade@mystenlabs.com>
2023-11-13 21:53:46 -0800

    ci

--- crates/sui-graphql-e2e-tests/tests/tests.rs
+++ crates/sui-graphql-e2e-tests/tests/tests.rs
@@ -4,13 +4,14 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 use std::path::Path;
-use sui_transactional_test_runner::{test_adapter::{SuiTestAdapter, PRE_COMPILED}, run_test_impl};
+use sui_transactional_test_runner::{
+    run_test_impl,
+    test_adapter::{SuiTestAdapter, PRE_COMPILED},
+};
 pub const TEST_DIR: &str = "tests";
 
 datatest_stable::harness!(run_test, TEST_DIR, r".*\.(mvir|move)$");
 
-#[cfg_attr(not(msim), tokio::main)]
-#[cfg_attr(msim, msim::main)]
 pub async fn run_test(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     if cfg!(feature = "pg_integration") {
         run_test_impl::<SuiTestAdapter>(path, Some(&*PRE_COMPILED)).await?;
