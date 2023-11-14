fdbd561d999f66d46e8265db4d1ae242eed21030
ade <ade@mystenlabs.com>
2023-11-13 21:41:40 -0800

    ci

--- crates/sui-graphql-e2e-tests/tests/tests.rs
+++ crates/sui-graphql-e2e-tests/tests/tests.rs
@@ -1,10 +1,19 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg(feature = "pg_integration")]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+use std::path::Path;
+use sui_transactional_test_runner::{test_adapter::{SuiTestAdapter, PRE_COMPILED}, run_test_impl};
 pub const TEST_DIR: &str = "tests";
-#[cfg(feature = "pg_integration")]
-use sui_transactional_test_runner::run_test;
 
-#[cfg(feature = "pg_integration")]
 datatest_stable::harness!(run_test, TEST_DIR, r".*\.(mvir|move)$");
+
+#[cfg_attr(not(msim), tokio::main)]
+#[cfg_attr(msim, msim::main)]
+pub async fn run_test(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    if cfg!(feature = "pg_integration") {
+        run_test_impl::<SuiTestAdapter>(path, Some(&*PRE_COMPILED)).await?;
+    }
+    Ok(())
+}
--- crates/sui-transactional-test-runner/src/lib.rs
+++ crates/sui-transactional-test-runner/src/lib.rs
@@ -8,7 +8,7 @@ pub mod programmable_transaction_test_parser;
 mod simulator_persisted_store;
 pub mod test_adapter;
 
-use move_transactional_test_runner::framework::run_test_impl;
+pub use move_transactional_test_runner::framework::run_test_impl;
 use rand::rngs::StdRng;
 use simulacrum::Simulacrum;
 use simulacrum::SimulatorStore;
