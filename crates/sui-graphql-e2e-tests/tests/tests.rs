fc20b183dba960c3b0432c409a61d37f03eb9cdf
ade <ade@mystenlabs.com>
2023-11-13 21:07:54 -0800

    ci enable

--- crates/sui-graphql-e2e-tests/tests/tests.rs
+++ crates/sui-graphql-e2e-tests/tests/tests.rs
@@ -1,7 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+#[cfg(feature = "pg_integration")]
 pub const TEST_DIR: &str = "tests";
+#[cfg(feature = "pg_integration")]
 use sui_transactional_test_runner::run_test;
 
 #[cfg(feature = "pg_integration")]
