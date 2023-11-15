6875bbf8835060b3e37695214ef9cc7e4676322f
Jordan Gensler <jordan@mystenlabs.com>
2023-11-15 09:34:46 -0800

    Tweak autoconnect wallet behavior (#14789)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sdk/dapp-kit/test/hooks/useAutoConnectWallet.test.tsx
+++ sdk/dapp-kit/test/hooks/useAutoConnectWallet.test.tsx
@@ -3,26 +3,35 @@
 
 import { act, renderHook, waitFor } from '@testing-library/react';
 
-import { useAutoConnectionStatus } from '../../src/hooks/wallet/useAutoConnectionStatus.js';
+import { useAutoConnectWallet } from '../../src/hooks/wallet/useAutoConnectWallet.js';
 import { useConnectWallet } from '../../src/index.js';
 import { createMockAccount } from '../mocks/mockAccount.js';
 import { suiFeatures } from '../mocks/mockFeatures.js';
 import { createWalletProviderContextWrapper, registerMockWallet } from '../test-utils.js';
 
-describe('useAutoConnectStatus', () => {
+function withResolvers<T = any>() {
+	let resolve, reject;
+	const promise = new Promise<T>((res, rej) => {
+		resolve = res;
+		reject = rej;
+	});
+	return { promise, reject: reject!, resolve: resolve! };
+}
+
+describe('useAutoConnectWallet', () => {
 	test('returns "disabled" when the auto-connect functionality is disabled', async () => {
 		const wrapper = createWalletProviderContextWrapper();
-		const { result } = renderHook(() => useAutoConnectionStatus(), { wrapper });
+		const { result } = renderHook(() => useAutoConnectWallet(), { wrapper });
 		expect(result.current).toBe('disabled');
 	});
 
-	test(`returns "idle" when we haven't yet made an auto-connection attempt`, async () => {
+	test(`returns "attempted" immediately when there is no last connected wallet`, async () => {
 		const wrapper = createWalletProviderContextWrapper({ autoConnect: true });
-		const { result } = renderHook(() => useAutoConnectionStatus(), { wrapper });
-		expect(result.current).toBe('idle');
+		const { result } = renderHook(() => useAutoConnectWallet(), { wrapper });
+		expect(result.current).toBe('attempted');
 	});
 
-	test('returns "settled" when we have made a successful auto-connection attempt', async () => {
+	test('returns "attempted" when we have made a successful auto-connection attempt', async () => {
 		const { unregister, mockWallet } = registerMockWallet({
 			walletName: 'Mock Wallet 1',
 			accounts: [createMockAccount(), createMockAccount()],
@@ -45,10 +54,18 @@ describe('useAutoConnectStatus', () => {
 		// Now unmount our component tree to simulate someone leaving the page.
 		unmount();
 
-		// Render our component tree again and auto-connect to our previously connected wallet account.
-		const { result: updatedResult } = renderHook(() => useAutoConnectionStatus(), { wrapper });
+		const { promise, resolve } = withResolvers();
+		mockWallet.mocks.connect.mockImplementation(async () => {
+			console.log('Waiting to connect');
+			return promise;
+		});
 
-		await waitFor(() => expect(updatedResult.current).toBe('settled'));
+		// Render our component tree again and auto-connect to our previously connected wallet account.
+		const { result: updatedResult } = renderHook(() => useAutoConnectWallet(), { wrapper });
+		expect(updatedResult.current).toBe('idle');
+		resolve({ accounts: mockWallet.accounts });
+
+		await waitFor(() => expect(updatedResult.current).toBe('attempted'));
 
 		act(() => unregister());
 	});
