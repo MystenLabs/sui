ccae92b085888e99ebd0f3bfb0ab7a51a2486ee3
Jordan Gensler <jordan@mystenlabs.com>
2023-11-16 13:42:59 -0800

    Remove source conditions from sdks (#14881)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sdk/wallet-kit/example/src/index.tsx
+++ sdk/wallet-kit/example/src/index.tsx
@@ -3,9 +3,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
+
 import { WalletKitProvider } from '@mysten/wallet-kit';
+
+import App from './App';
 
 export const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
--- sdk/zklogin/vitest.config.ts
+++ sdk/zklogin/vitest.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	resolve: {
-		conditions: ['source'],
+		alias: {
+			'@mysten/bcs': new URL('../bcs/src', import.meta.url).toString(),
+			'@mysten/sui.js': new URL('../typescript/src', import.meta.url).toString(),
+		},
 	},
 });
