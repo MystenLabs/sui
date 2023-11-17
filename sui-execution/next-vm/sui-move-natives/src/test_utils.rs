487489896871936b4ebf0966d50376b298d63c7e
Tim Zakian <2895723+tzakian@users.noreply.github.com>
2023-11-17 10:58:24 -0800

    [execution-layer] Cut new version for VM development work (#14874)
    ## Description 
    
    Cuts a new execution version for use future VM and execution
    development. Note that this is not turned on for any network, and is for
    development only.
    
    ## Test Plan 
    
    Make sure existing tests pass. 
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ sui-execution/next-vm/sui-move-natives/src/test_utils.rs
@@ -0,0 +1,49 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{legacy_test_cost, types::is_otw_struct};
+use move_binary_format::errors::PartialVMResult;
+use move_core_types::{gas_algebra::InternalGas, runtime_value::MoveTypeLayout};
+use move_vm_runtime::native_functions::NativeContext;
+use move_vm_types::{
+    loaded_data::runtime_types::Type, natives::function::NativeResult, values::Value,
+};
+use smallvec::smallvec;
+use std::collections::VecDeque;
+
+pub fn destroy(
+    _context: &mut NativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    args.pop_back();
+    Ok(NativeResult::ok(legacy_test_cost(), smallvec![]))
+}
+
+pub fn create_one_time_witness(
+    context: &mut NativeContext,
+    mut ty_args: Vec<Type>,
+    args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(ty_args.len() == 1);
+    debug_assert!(args.is_empty());
+
+    let ty = ty_args.pop().unwrap();
+    let type_tag = context.type_to_type_tag(&ty)?;
+    let type_layout = context.type_to_type_layout(&ty)?;
+
+    let Some(MoveTypeLayout::Struct(struct_layout)) = type_layout else {
+        return Ok(NativeResult::err(InternalGas::new(1), 0));
+    };
+
+    if is_otw_struct(&struct_layout, &type_tag) {
+        Ok(NativeResult::ok(
+            legacy_test_cost(),
+            smallvec![Value::struct_(move_vm_types::values::Struct::pack(vec![
+                Value::bool(true)
+            ]))],
+        ))
+    } else {
+        Ok(NativeResult::err(InternalGas::new(1), 1))
+    }
+}
