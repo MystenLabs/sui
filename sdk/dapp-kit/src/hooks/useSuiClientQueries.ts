8b875be565a8388aa2175cb2ebcb83d4bfd80d67
jovicheng <jovicheng@foxmail.com>
2023-11-21 03:29:02 +0800

    [fix] More Strict Type Definitions

--- sdk/dapp-kit/src/hooks/useSuiClientQueries.ts
+++ sdk/dapp-kit/src/hooks/useSuiClientQueries.ts
@@ -7,32 +7,48 @@ import { useQueries } from '@tanstack/react-query';
 import { useSuiClientContext } from './useSuiClient.js';
 import type { SuiRpcMethods, UseSuiClientQueryOptions } from './useSuiClientQuery.js';
 
+type BaseSuiClientQueriesArgs = {
+	method: keyof SuiRpcMethods;
+	params?: object;
+	options?: UseSuiClientQueryOptions<keyof SuiRpcMethods, unknown>;
+}[];
+
+type TypedSuiClientQueriesArgs<Args extends BaseSuiClientQueriesArgs> = {
+	[K in keyof Args]: Args[K] extends { method: infer T extends keyof SuiRpcMethods }
+		? undefined extends SuiRpcMethods[T]['params']
+			? {
+					method: T;
+					params?: SuiRpcMethods[T]['params'];
+					options?: UseSuiClientQueryOptions<T, SuiRpcMethods[T]['result']>;
+			  }
+			: {
+					method: T;
+					params: SuiRpcMethods[T]['params'];
+					options?: UseSuiClientQueryOptions<T, SuiRpcMethods[T]['result']>;
+			  }
+		: never;
+};
+
 export function useSuiClientQueries<
-	T extends keyof SuiRpcMethods,
-	TData = SuiRpcMethods[T]['result'],
+	Args extends BaseSuiClientQueriesArgs,
+	TypedArgs extends TypedSuiClientQueriesArgs<Args>,
 >(
-	...args: undefined extends SuiRpcMethods[T]['params']
-		? [
-				method: T,
-				params?: SuiRpcMethods[T]['params'],
-				options?: UseSuiClientQueryOptions<T, TData>,
-		  ][]
-		: [
-				method: T,
-				params: SuiRpcMethods[T]['params'],
-				options?: UseSuiClientQueryOptions<T, TData>,
-		  ][]
-): UseQueryResult<TData, Error>[] {
+	...args: Args extends TypedArgs ? Args : TypedArgs
+): {
+	[K in keyof Args]: Args[K] extends { method: infer T extends keyof SuiRpcMethods }
+		? UseQueryResult<SuiRpcMethods[T]['result'], Error>
+		: never;
+} {
 	const queries = args;
 
 	const suiContext = useSuiClientContext();
 
 	return useQueries({
 		queries: queries.map((query) => {
-			const [method, params, { queryKey = [], ...options } = {}] = query;
+			const { method, params, options: { queryKey = [], ...restOptions } = {} } = query;
 
 			return {
-				...options,
+				...restOptions,
 				queryKey: [suiContext.network, method, params, ...queryKey],
 				queryFn: async () => {
 					return await suiContext.client[method](params as never);
