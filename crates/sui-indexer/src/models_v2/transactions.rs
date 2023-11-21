4716e56a630e158666f85db7c66dc0217bbeba5c
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2023-11-21 15:14:23 -0500

    indexer v2 analytical: faster move call and address processors (#14730)
    ## Description 
    
    The overall idea is that, much of time in the past were spent on passing
    data from DB and then write them back, even with parallelism on many
    steps, instead this PR does that directly in DB, on testing this will
    reduce backfill time of move call metrics and DAU related metrics from
    days to hours.
    
    ## Test Plan 
    
    local run and test speed on a separate testing DB
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-indexer/src/models_v2/transactions.rs
+++ crates/sui-indexer/src/models_v2/transactions.rs
@@ -12,7 +12,6 @@ use sui_json_rpc_types::SuiTransactionBlockResponse;
 use sui_json_rpc_types::SuiTransactionBlockResponseOptions;
 use sui_types::digests::TransactionDigest;
 use sui_types::effects::TransactionEffects;
-use sui_types::effects::TransactionEffectsAPI;
 use sui_types::effects::TransactionEvents;
 use sui_types::event::Event;
 use sui_types::transaction::SenderSignedData;
@@ -39,6 +38,17 @@ pub struct StoredTransaction {
     pub success_command_count: i16,
 }
 
+#[derive(Debug, Queryable)]
+pub struct TxSeq {
+    pub seq: i64,
+}
+
+impl Default for TxSeq {
+    fn default() -> Self {
+        Self { seq: -1 }
+    }
+}
+
 #[derive(Clone, Debug, Queryable)]
 pub struct StoredTransactionTimestamp {
     pub tx_sequence_number: i64,
@@ -61,14 +71,6 @@ pub struct StoredTransactionSuccessCommandCount {
 
 impl From<&IndexedTransaction> for StoredTransaction {
     fn from(tx: &IndexedTransaction) -> Self {
-        let cmd_count = tx
-            .sender_signed_data
-            .intent_message()
-            .value
-            .execution_parts()
-            .0
-            .num_commands();
-
         StoredTransaction {
             tx_sequence_number: tx.tx_sequence_number as i64,
             transaction_digest: tx.tx_digest.into_inner().to_vec(),
@@ -92,7 +94,7 @@ impl From<&IndexedTransaction> for StoredTransaction {
                 .collect(),
             timestamp_ms: tx.timestamp_ms as i64,
             transaction_kind: tx.transaction_kind.clone() as i16,
-            success_command_count: tx.effects.status().is_ok() as i16 * cmd_count as i16,
+            success_command_count: tx.successful_tx_num as i16,
         }
     }
 }
