63b547b0d55a30d36cef57cbb0c1f15ffbac15c1
Will Riches <will@riches.im>
2023-11-21 21:15:23 +0000

    Add TOML support to Prism

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -189,7 +189,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ["rust", "typescript"],
+        additionalLanguages: ["rust", "typescript", "toml"],
       },
     }),
 };
