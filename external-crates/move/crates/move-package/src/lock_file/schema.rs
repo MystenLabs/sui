eaedb4555cea37ee4d0401993c76b8fcdc7566de
Rijnard van Tonder <rvantonder@gmail.com>
2023-11-22 12:38:28 -0800

    address feedback

--- external-crates/move/crates/move-package/src/lock_file/schema.rs
+++ external-crates/move/crates/move-package/src/lock_file/schema.rs
@@ -7,7 +7,7 @@
 
 use std::io::{Read, Seek, SeekFrom, Write};
 
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{bail, Context, Result};
 use serde::{Deserialize, Serialize};
 use tempfile::NamedTempFile;
 use toml::value::Value;
@@ -112,20 +112,23 @@ impl Packages {
 
 impl ToolchainVersioning {
     /// Read toolchain versioning info from the lock file. Returns successfully with None if
-    /// parsing the lock file succeeds but an entry for `[toolchain-version]` does not exist.
+    /// parsing the lock file succeeds but an entry for `[toolchain-versioning]` does not exist.
     pub fn read(lock: &mut impl Read) -> Result<Option<ToolchainVersioning>> {
-        let mut buf = String::new();
-        lock.read_to_string(&mut buf).context("Reading lock file")?;
-        let toml: toml::Value = toml::from_str(&buf)?;
-        let move_ = toml.get("move").ok_or_else(|| anyhow!("No [move] found"))?;
-        match move_.get("toolchain-versioning") {
-            None => Ok(None), // parsing succeeds, no entry for `toolchain-versioning`
-            Some(v) => v
-                .clone()
-                .try_into()
-                .map(Some)
-                .map_err(|e| anyhow!("Error deserializing: {e}")),
+        let contents = {
+            let mut buf = String::new();
+            lock.read_to_string(&mut buf).context("Reading lock file")?;
+            buf
+        };
+
+        #[derive(Deserialize)]
+        struct TV {
+            #[serde(rename = "toolchain-versioning")]
+            toolchain_versioning: Option<ToolchainVersioning>,
         }
+        let Schema { move_: value } = toml::de::from_str::<Schema<TV>>(&contents)
+            .context("Deserializing toolchain version")?;
+
+        Ok(value.toolchain_versioning)
     }
 }
 
