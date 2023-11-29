6bf725f71f78d8bbb741d16330fb84c98e918413
Xun Li <lxfind@gmail.com>
2023-11-28 16:47:22 -0800

    Enable effects v2 on mainnet (#15056)
    ## Description 
    
    Effects v2 has been running on testnet and working well so far.
    This PR enables effects v2 on mainnet.
    
    ## Test Plan 
    
    devnet + testnet soak
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [x ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [x ] breaking change for FNs (FN binary must upgrade)
    - [x ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    This PR changes how we store transaction effects on node databases. The
    new structure is cleaner and more efficient, also allows for better
    implementation in the execution and other layers. This should not
    affects SDKs because we translate it to SDK effects types at json rpc
    layer. Detailed design is described in
    https://github.com/MystenLabs/sui/issues/12813.

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -97,6 +97,7 @@ const MAX_PROTOCOL_VERSION: u64 = 32;
 //             Enable transfer to object in testnet.
 //             Enable Narwhal CertificateV2 on mainnet
 //             Make critbit tree and order getters public in deepbook.
+//             Enable effects v2 on mainnet.
 
 #[derive(Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ProtocolVersion(u64);
@@ -1670,6 +1671,8 @@ impl ProtocolConfig {
 
                     // enable nw cert v2 on mainnet
                     cfg.feature_flags.narwhal_certificate_v2 = true;
+
+                    cfg.feature_flags.enable_effects_v2 = true;
                 }
                 // Use this template when making changes:
                 //
