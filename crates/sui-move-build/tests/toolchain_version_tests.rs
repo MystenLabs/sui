95bc8db2d223535b897edfd2e1340268598f7c35
Rijnard van Tonder <rvantonder@gmail.com>
2023-11-29 23:59:53 -0800

    assumed that Move.toml sets edition but it doesn't seem to, and we can't override anything other than Sui, so I'm going to simplify the logic and come back to this if we need override behavior

--- crates/sui-move-build/tests/toolchain_version_tests.rs
+++ crates/sui-move-build/tests/toolchain_version_tests.rs
@@ -1,15 +1,76 @@
 use std::path::PathBuf;
 
-use sui_move_build::BuildConfig;
+use expect_test::expect;
+use move_compiler::editions::{Edition, Flavor};
+use move_package::lock_file::schema::ToolchainVersion;
+use sui_move_build::{resolve_toolchain_version, BuildConfig, CompiledPackage};
 
 #[test]
-fn resolve_toolchain_version() {
+fn no_toolchain_version() {
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.extend([
         "tests",
         "toolchain_version",
         "fixture",
-        "a_resolve_move_lock",
+        "a_no_toolchain_version",
     ]);
-    let pkg = BuildConfig::new_for_testing().build(path).unwrap();
+    let mut build_config = BuildConfig::new_for_testing();
+    build_config.config.lock_file = Some(path.join("Move.lock"));
+    let result = build_config.build(path).unwrap();
+    assert_eq!((Flavor::Sui, Edition::LEGACY), flavor_and_edition(&result));
+}
+
+#[test]
+fn toolchain_version_2024() {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.extend([
+        "tests",
+        "toolchain_version",
+        "fixture",
+        "b_toolchain_version_2024",
+    ]);
+    let mut build_config = BuildConfig::new_for_testing();
+    build_config.config.lock_file = Some(path.join("Move.lock"));
+    println!("running on path {:#?}", build_config.config.lock_file);
+    let result = build_config.build(path).unwrap();
+    assert_eq!(
+        (Flavor::Sui, Edition::E2024_ALPHA),
+        flavor_and_edition(&result)
+    );
+}
+
+#[test]
+fn toolchain_version_lock_override() {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.extend([
+        "tests",
+        "toolchain_version",
+        "fixture",
+        "c_toolchain_version_lock_override",
+    ]);
+    // The Move.lock file sets edition `legacy` but we expect `2024.alpha` that the Move.toml overrides.
+    let mut build_config = BuildConfig::new_for_testing();
+    build_config.config.lock_file = Some(path.join("Move.lock"));
+    let result = build_config.build(path).unwrap();
+    assert_eq!(
+        (Flavor::Sui, Edition::E2024_ALPHA),
+        flavor_and_edition(&result)
+    );
+}
+
+fn flavor_and_edition(pkg: &CompiledPackage) -> (Flavor, Edition) {
+    let flavor = pkg
+        .package
+        .compiled_package_info
+        .build_flags
+        .default_flavor
+        .unwrap();
+    let edition = pkg
+        .package
+        .compiled_package_info
+        .build_flags
+        .default_edition
+        .unwrap();
+
+    (flavor, edition)
 }
