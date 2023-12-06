6ca9722b265ea3ed872c93d4b2a4ea60bd31ec20
Ashok Menon <ashok@mystenlabs.com>
2023-12-06 19:26:31 +0000

    fixup: rm 'the'

--- crates/sui-graphql-rpc/src/types/transaction_block_kind/genesis.rs
+++ crates/sui-graphql-rpc/src/types/transaction_block_kind/genesis.rs
@@ -20,8 +20,7 @@ use crate::{
 #[derive(Clone, PartialEq, Eq)]
 pub(crate) struct GenesisTransaction(pub NativeGenesisTransaction);
 
-/// System transaction that initializes the network and the writes the initial set of objects
-/// on-chain.
+/// System transaction that initializes the network and writes the initial set of objects on-chain.
 #[Object]
 impl GenesisTransaction {
     /// Objects to be created during genesis.
