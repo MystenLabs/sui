39bb7f1a0ac23bded92d247b975a899f2f187ab4
Anastasios Kichidis <tasos@mystenlabs.com>
2023-12-06 12:44:27 +0000

    update mct ref. Increase leader timeout.

--- crates/sui-core/src/consensus_manager/mysticeti_manager.rs
+++ crates/sui-core/src/consensus_manager/mysticeti_manager.rs
@@ -256,7 +256,7 @@ fn mysticeti_parameters(committee: &narwhal_config::Committee) -> Parameters {
         identifiers,
         enable_pipelining: true,
         number_of_leaders: 2,
-        leader_timeout: Duration::from_millis(200),
+        leader_timeout: Duration::from_millis(2000),
         enable_cleanup: true,
         synchronizer_parameters: SynchronizerParameters {
             sample_precision: Duration::from_millis(100),
