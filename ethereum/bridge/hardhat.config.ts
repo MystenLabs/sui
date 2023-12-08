6e09b2dd5990318d320559a6fa66953f623899c6
George Digkas <george.digkas@mystenlabs.com>
2023-12-08 13:47:31 +0200

    add sui eth node

--- ethereum/bridge/hardhat.config.ts
+++ ethereum/bridge/hardhat.config.ts
@@ -8,7 +8,9 @@ import { HardhatUserConfig } from "hardhat/config";
 dotEnvConfig();
 
 // read MNEMONIC from file or from env variable
-let mnemonic = process.env.MNEMONIC!;
+const mnemonic = process.env.MNEMONIC;
+// read X-goog-api-key from file or from env variable
+const xGoogApiKey = process.env.X_GOOG_API_KEY!;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -37,11 +39,15 @@ const config: HardhatUserConfig = {
     },
     sepoliasui: {
       url: `https://eth-rpc.testnet.sui.io:443`,
-      accounts: { mnemonic: process.env.MNEMONIC! },
+      accounts: { mnemonic: mnemonic },
     },
-    goerlihh: {
-      url: "https://rpc.ankr.com/eth_goerli",
-      chainId: 5,
+    ethsui: {
+      accounts: { mnemonic: mnemonic },
+      url: `http://json-rpc.ap1cmylexnxy1eadewp3v97e8.blockchainnodeengine.com`,
+      // Use the X-goog-api-key as an http header
+      httpHeaders: {
+        "X-goog-api-key": xGoogApiKey,
+      },
     },
     coverage: {
       url: "http://127.0.0.1:8555",
