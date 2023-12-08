b429a639dfec148d39c9735274e9b830fb39780d
Stefan Stanciulescu <stefan.stanciulescu@mystenlabs.com>
2023-12-08 07:38:00 -0800

    Add clap support for parsing CLI PTB commands

--- crates/sui/src/main.rs
+++ crates/sui/src/main.rs
@@ -3,8 +3,9 @@
 
 use clap::*;
 use colored::Colorize;
+use sui::client_commands::SuiClientCommands::ReplayTransaction;
 use sui::client_commands::SuiClientCommands::{ProfileTransaction, ReplayTransaction};
-
+use sui::client_commands::PTB;
 use sui::sui_commands::SuiCommand;
 use sui_types::exit_main;
 use tracing::debug;
@@ -48,49 +49,59 @@ async fn main() {
     #[cfg(windows)]
     colored::control::set_virtual_terminal(true).unwrap();
 
-    let args = Args::parse();
-    let _guard = match args.command {
-        SuiCommand::Console { .. } | SuiCommand::KeyTool { .. } | SuiCommand::Move { .. } => {
-            telemetry_subscribers::TelemetryConfig::new()
+    let mut args = Args::command();
+    // Hacky way to handle PTB commands, because we need to get matches,
+    // instead of parse the args
+    if args
+        .get_matches_mut()
+        .subcommand_matches("client")
+        .is_some_and(|x| x.subcommand_matches("ptb").is_some())
+    {
+        exit_main!(PTB::execute(args.get_matches_mut()).await);
+    } else {
+        let args = Args::parse();
+        let _guard = match args.command {
+            SuiCommand::Console { .. } | SuiCommand::KeyTool { .. } | SuiCommand::Move { .. } => {
+                telemetry_subscribers::TelemetryConfig::new()
+                    .with_log_level("error")
+                    .with_env()
+                    .init()
+            }
+
+            SuiCommand::Client {
+                cmd:
+                    Some(ReplayTransaction {
+                        gas_info, ptb_info, ..
+                    }),
+                ..
+            } => {
+                let mut config = telemetry_subscribers::TelemetryConfig::new()
+                    .with_log_level("info")
+                    .with_env();
+                if gas_info {
+                    config = config.with_trace_target("replay_gas_info");
+                }
+                if ptb_info {
+                    config = config.with_trace_target("replay_ptb_info");
+                }
+                config.init()
+            }
+            SuiCommand::Client {
+                cmd: Some(ProfileTransaction { .. }),
+                ..
+            } => {
+                // enable full logging for ProfileTransaction and ReplayTransaction
+                telemetry_subscribers::TelemetryConfig::new()
+                    .with_env()
+                    .init()
+            }
+
+            _ => telemetry_subscribers::TelemetryConfig::new()
                 .with_log_level("error")
                 .with_env()
-                .init()
-        }
-
-        SuiCommand::Client {
-            cmd: Some(ReplayTransaction {
-                gas_info, ptb_info, ..
-            }),
-            ..
-        } => {
-            let mut config = telemetry_subscribers::TelemetryConfig::new()
-                .with_log_level("info")
-                .with_env();
-            if gas_info {
-                config = config.with_trace_target("replay_gas_info");
-            }
-            if ptb_info {
-                config = config.with_trace_target("replay_ptb_info");
-            }
-            config.init()
-        }
-        SuiCommand::Client {
-            cmd: Some(ProfileTransaction { .. }),
-            ..
-        } => {
-            // enable full logging for ProfileTransaction and ReplayTransaction
-            telemetry_subscribers::TelemetryConfig::new()
-                .with_env()
-                .init()
-        }
-
-        _ => telemetry_subscribers::TelemetryConfig::new()
-            .with_log_level("error")
-            .with_env()
-            .init(),
-    };
-
-    debug!("Sui CLI version: {VERSION}");
-
-    exit_main!(args.command.execute().await);
+                .init(),
+        };
+        debug!("Sui CLI version: {VERSION}");
+        exit_main!(args.command.execute().await);
+    }
 }
