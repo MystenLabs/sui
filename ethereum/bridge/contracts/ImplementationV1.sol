513b38aa6bd0b8794340cf5dee066c8842da521c
George Digkas <george.digkas@mystenlabs.com>
2023-12-11 17:32:12 +0200

    cleanup

--- ethereum/bridge/contracts/ImplementationV1.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
-
-contract ImplementationV1 {
-	// The function that returns the version of the implementation
-	function getVersion() public pure returns (uint256) {
-		return 1;
-	}
-}
