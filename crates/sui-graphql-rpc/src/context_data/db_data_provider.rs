c4420e7c7027090cf9dd94cac579862b8893aa74
wlmyng <127570466+wlmyng@users.noreply.github.com>
2023-12-11 08:57:45 -0800

    [gql] update cursor logic for coins and objects (#15260)
    ## Description 
    
    Last of the multi get queries backing connections. Fix results returned
    for last + before queries to return results in ascending order.
    
    ## Test Plan 
    
    objects/pagination.move
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-graphql-rpc/src/context_data/db_data_provider.rs
+++ crates/sui-graphql-rpc/src/context_data/db_data_provider.rs
@@ -237,9 +237,10 @@ impl PgManager {
         before: Option<String>,
     ) -> Result<Option<(Vec<StoredObject>, bool)>, Error> {
         let limit = self.validate_page_limit(first, last)?;
-        let descending_order = last.is_some();
-        let cursor = after
-            .or(before)
+        let before = before
+            .map(|cursor| self.parse_obj_cursor(&cursor))
+            .transpose()?;
+        let after = after
             .map(|cursor| self.parse_obj_cursor(&cursor))
             .transpose()?;
         let coin_type = parse_to_type_tag(Some(coin_type))
@@ -249,8 +250,8 @@ impl PgManager {
             .run_query_async_with_cost(
                 move || {
                     Ok(QueryBuilder::multi_get_coins(
-                        cursor.clone(),
-                        descending_order,
+                        before.clone(),
+                        after.clone(),
                         limit,
                         address.clone(),
                         coin_type.clone(),
@@ -265,6 +266,10 @@ impl PgManager {
                 let has_next_page = stored_objs.len() as i64 > limit;
                 if has_next_page {
                     stored_objs.pop();
+                }
+
+                if last.is_some() {
+                    stored_objs.reverse();
                 }
 
                 Ok((stored_objs, has_next_page))
@@ -516,16 +521,17 @@ impl PgManager {
         owner_type: Option<OwnerType>,
     ) -> Result<Option<(Vec<StoredObject>, bool)>, Error> {
         let limit = self.validate_page_limit(first, last)?;
-        let descending_order = last.is_some();
-        let cursor = after
-            .or(before)
+        let before = before
+            .map(|cursor| self.parse_obj_cursor(&cursor))
+            .transpose()?;
+        let after = after
             .map(|cursor| self.parse_obj_cursor(&cursor))
             .transpose()?;
 
         let query = move || {
             QueryBuilder::multi_get_objs(
-                cursor.clone(),
-                descending_order,
+                before.clone(),
+                after.clone(),
                 limit,
                 filter.clone(),
                 owner_type,
@@ -535,11 +541,16 @@ impl PgManager {
         let result: Option<Vec<StoredObject>> = self
             .run_query_async_with_cost(query, |query| move |conn| query.load(conn).optional())
             .await?;
+
         result
             .map(|mut stored_objs| {
                 let has_next_page = stored_objs.len() as i64 > limit;
                 if has_next_page {
                     stored_objs.pop();
+                }
+
+                if last.is_some() {
+                    stored_objs.reverse();
                 }
 
                 Ok((stored_objs, has_next_page))
