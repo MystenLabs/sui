b9f093ccf17a1647297604e9ba890eb4bb91ea5a
George Digkas <george.digkas@mystenlabs.com>
2023-12-13 16:06:08 +0200

    reformat scripts

--- ethereum/bridge/scripts/signature.ts
+++ ethereum/bridge/scripts/signature.ts
@@ -5,12 +5,11 @@ import { ethers } from 'ethers'
             '0x3f614a2b69459e93371336703571a74af91cab7ba05fd56a66b55eb1ba24ce55'
         let wallet = new ethers.Wallet(privateKey)
 
-
         const messageType = 2
         const messageVersion = 1
         const sequenceNumber = 0
         const sourceChain = 0
-        const payload = "0x00"
+        const payload = '0x00'
 
         const messageHash = ethers.solidityPackedKeccak256(
             ['uint8', 'uint8', 'uint64', 'uint8', 'bytes'],
