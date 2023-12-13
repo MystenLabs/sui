43e9dc89b4ac133cf52a6f6b8b3fe3d4d5c065be
longbowlu <zl9394@gmail.com>
2023-12-12 20:57:49 -0800

    add BridgeAuthorityAggregator

--- crates/sui-bridge/src/test_utils.rs
+++ crates/sui-bridge/src/test_utils.rs
@@ -1,17 +1,24 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::server::mock_handler::run_mock_server;
 use crate::{
-    crypto::{BridgeAuthorityKeyPair, BridgeAuthorityPublicKey},
+    crypto::{BridgeAuthorityKeyPair, BridgeAuthorityPublicKey, BridgeAuthoritySignInfo},
     events::EmittedSuiToEthTokenBridgeV1,
-    types::{BridgeAction, BridgeAuthority, BridgeChainId, SuiToEthBridgeAction, TokenId},
+    server::mock_handler::BridgeRequestMockHandler,
+    types::{
+        BridgeAction, BridgeAuthority, BridgeChainId, SignedBridgeAction, SuiToEthBridgeAction,
+        TokenId,
+    },
 };
 use ethers::types::Address as EthAddress;
 use fastcrypto::traits::KeyPair;
-use std::{pin::Pin, sync::Arc};
-use sui_types::{
-    base_types::SuiAddress, crypto::get_key_pair, digests::TransactionDigest, multiaddr::Multiaddr,
-};
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
+use sui_config::local_ip_utils;
+use sui_types::{base_types::SuiAddress, crypto::get_key_pair, digests::TransactionDigest};
+use tokio::task::JoinHandle;
 
 pub fn get_test_authority_and_key(
     voting_power: u64,
@@ -19,20 +26,18 @@ pub fn get_test_authority_and_key(
 ) -> (
     BridgeAuthority,
     BridgeAuthorityPublicKey,
-    Pin<Arc<BridgeAuthorityKeyPair>>,
+    BridgeAuthorityKeyPair,
 ) {
     let (_, kp): (_, fastcrypto::secp256k1::Secp256k1KeyPair) = get_key_pair();
     let pubkey = kp.public().clone();
     let authority = BridgeAuthority {
         pubkey: pubkey.clone(),
         voting_power,
-        bridge_network_address: Multiaddr::try_from(format!("/ip4/127.0.0.1/tcp/{}/http", port))
-            .unwrap(),
+        base_url: format!("http://127.0.0.1:{}", port),
         is_blocklisted: false,
     };
-    let secret = Arc::pin(kp);
 
-    (authority, pubkey, secret)
+    (authority, pubkey, kp)
 }
 
 pub fn get_test_sui_to_eth_bridge_action(
@@ -54,4 +59,52 @@ pub fn get_test_sui_to_eth_bridge_action(
             amount,
         },
     })
+}
+
+pub fn run_mock_bridge_server(
+    mock_handlers: Vec<BridgeRequestMockHandler>,
+) -> (Vec<JoinHandle<()>>, Vec<u16>) {
+    let mut handles = vec![];
+    let mut ports = vec![];
+    for mock_handler in mock_handlers {
+        let localhost = local_ip_utils::localhost_for_testing();
+        let port = local_ip_utils::get_available_port(&localhost);
+        // start server
+        let server_handle = run_mock_server(
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port),
+            mock_handler.clone(),
+        );
+        ports.push(port);
+        handles.push(server_handle);
+    }
+    (handles, ports)
+}
+
+pub fn get_test_authorities_and_run_mock_bridge_server(
+    voting_power: Vec<u64>,
+    mock_handlers: Vec<BridgeRequestMockHandler>,
+) -> (
+    Vec<JoinHandle<()>>,
+    Vec<BridgeAuthority>,
+    Vec<BridgeAuthorityKeyPair>,
+) {
+    assert_eq!(voting_power.len(), mock_handlers.len());
+    let (handles, ports) = run_mock_bridge_server(mock_handlers);
+    let mut authorites = vec![];
+    let mut secrets = vec![];
+    for (port, vp) in ports.iter().zip(voting_power) {
+        let (authority, _, secret) = get_test_authority_and_key(vp, *port);
+        authorites.push(authority);
+        secrets.push(secret);
+    }
+
+    (handles, authorites, secrets)
+}
+
+pub fn sign_action_with_key(
+    action: &BridgeAction,
+    secret: &BridgeAuthorityKeyPair,
+) -> SignedBridgeAction {
+    let sig = BridgeAuthoritySignInfo::new(action, secret);
+    SignedBridgeAction::new_from_data_and_sig(action.clone(), sig)
 }
