12c6d152ae61a74b26325875ddec48b039d3044a
Andrew Schran <aschran@mystenlabs.com>
2023-12-13 17:57:06 -0500

    Implement crash recovery for narwhal random beacon protocol (#15109)
    ## Description 
    
    Uses durable storage for relevant DKG and random beacon state.
    
    ## Test Plan 
    
    Manual inspection on private-testnet and unit test.

--- narwhal/primary/src/state_handler.rs
+++ narwhal/primary/src/state_handler.rs
@@ -16,6 +16,7 @@ use network::anemo_ext::NetworkExt;
 use std::collections::BTreeMap;
 use std::sync::{Arc, OnceLock};
 use std::time::Duration;
+use storage::RandomnessStore;
 use sui_protocol_config::ProtocolConfig;
 use tap::TapFallible;
 use tokio::sync::watch;
@@ -62,30 +63,47 @@ pub struct StateHandler {
 }
 
 // Internal state for randomness DKG and generation.
-// TODO: Write a brief protocol description.
+//
+// DKG protocol:
+// 1. This node sends out a `Message` to all other nodes.
+// 2. Once sufficient valid `Message`s are received from other nodes via consensus and processed,
+//    this node sends out a `Confirmation` to all other nodes.
+// 3. Once sufficient `Confirmation`s are received from other nodes via consensus and processed,
+//    they are combined to form a public VSS key and local private key shares.
+// 4. Randomness generation begins.
+//
+// Randomness generation:
+// 1. Randomness round begins at zero.
+// 2. All nodes send `RandomnessPartialSignature`s for their shares on the round number to the next
+//    Bullshark leader. This is repeated each time the leader changes.
+// 3. Once sufficient partial signatures are received to reconstruct the full signature, it is sent
+//    through consensus.
+// 4. Once the full signature is received via consensus (verified with public VSS key),
+//    randomness round is incremented.
 struct RandomnessState {
+    store: RandomnessStore,
+
     // A channel to send system messages to the proposer.
     tx_system_messages: Sender<SystemMessage>,
 
     // State for DKG.
     party: dkg::Party<PkG, EncG>,
-    processed_messages: Vec<dkg::ProcessedMessage<PkG, EncG>>,
-    used_messages: Option<dkg::UsedProcessedMessages<PkG, EncG>>,
-    confirmations: Vec<dkg::Confirmation<EncG>>,
-    dkg_output: Option<dkg::Output<PkG, EncG>>,
+    has_sent_confirmation: bool, // enables re-sending Confirmation after crash recovery
     vss_key_output: Arc<OnceLock<PublicVssKey>>,
+    dkg_output: Option<dkg::Output<PkG, EncG>>,
 
     // State for randomness generation.
     authority_id: AuthorityIdentifier,
     leader_schedule: LeaderSchedule,
     network: anemo::Network,
     last_randomness_round_sent: Option<RandomnessRound>,
-    randomness_round: RandomnessRound,
     last_narwhal_round_sent: Round,
     narhwal_round: Round,
     // Partial signatures are expensive to compute, cached in case we need to re-send.
     cached_sigs: Option<(RandomnessRound, Vec<RandomnessPartialSignature>)>,
     // Partial sig storage is keyed on (randomness round, authority ID).
+    // No need to save these for crash recovery since they are re-sent every time the next
+    // Bullshark leader changes.
     partial_sigs: BTreeMap<(RandomnessRound, AuthorityIdentifier), Vec<RandomnessPartialSignature>>,
     partial_sig_sender: Option<JoinHandle<()>>,
 }
@@ -105,6 +123,7 @@ impl RandomnessState {
         // Writes the VSS public key to this lock once DKG completes.
         vss_key_output: Arc<OnceLock<PublicVssKey>>,
         tx_system_messages: Sender<SystemMessage>,
+        store: RandomnessStore,
     ) -> Option<Self> {
         if !protocol_config.random_beacon() {
             info!("random beacon: disabled");
@@ -164,25 +183,30 @@ impl RandomnessState {
         info!(
             "random beacon: state initialized with authority_id={authority_id}, total_weight={total_weight}, t={t}, num_nodes={num_nodes}, oracle initial_prefix={prefix_str:?}",
         );
+
+        let dkg_output = store.dkg_output();
         Some(Self {
+            store,
             tx_system_messages,
             party,
-            processed_messages: Vec::new(),
-            used_messages: None,
-            confirmations: Vec::new(),
-            dkg_output: None,
+            has_sent_confirmation: false,
             vss_key_output,
+            dkg_output,
             authority_id,
             leader_schedule,
             network,
             last_randomness_round_sent: None,
-            randomness_round: RandomnessRound(0),
             last_narwhal_round_sent: 0,
             narhwal_round: 0,
             cached_sigs: None,
             partial_sigs: BTreeMap::new(),
             partial_sig_sender: None,
         })
+    }
+
+    fn set_dkg_output(&mut self, output: dkg::Output<PkG, EncG>) {
+        self.store.set_dkg_output(&output);
+        self.dkg_output = Some(output);
     }
 
     async fn start_dkg(&self) {
@@ -202,14 +226,14 @@ impl RandomnessState {
     }
 
     fn add_message(&mut self, msg: dkg::Message<PkG, EncG>) {
-        if self.used_messages.is_some() {
+        if self.store.has_used_messages() {
             // We've already sent a `Confirmation`, so we can't add any more messages.
             return;
         }
         match self.party.process_message(msg, &mut rand::thread_rng()) {
-            Ok(processed) => {
-                self.processed_messages.push(processed);
-            }
+            Ok(processed) => self
+                .store
+                .add_processed_message(processed.message.sender, processed),
             Err(err) => {
                 debug!("random beacon: error while processing DKG Message: {err:?}");
             }
@@ -217,7 +241,7 @@ impl RandomnessState {
     }
 
     fn add_confirmation(&mut self, conf: dkg::Confirmation<EncG>) {
-        if self.used_messages.is_none() {
+        if !self.store.has_used_messages() {
             // We should never see a `Confirmation` before we've sent our `Message` because
             // DKG messages are processed in consensus order.
             return;
@@ -226,21 +250,22 @@ impl RandomnessState {
             // Once we have completed DKG, no more `Confirmation`s are needed.
             return;
         }
-        self.confirmations.push(conf)
+        self.store.add_confirmation(conf.sender, conf)
     }
 
     // Generates the next SystemMessage needed to advance the random beacon DKG protocol, if
     // possible, and sends it to the proposer.
     async fn advance_dkg(&mut self) {
         // Once we have enough ProcessedMessages, send a Confirmation.
-        if self.used_messages.is_none() && !self.processed_messages.is_empty() {
-            match self.party.merge(&self.processed_messages) {
+        if !self.store.has_used_messages() || !self.has_sent_confirmation {
+            match self.party.merge(&self.store.processed_messages()) {
                 Ok((conf, used_msgs)) => {
                     info!(
                         "random beacon: sending DKG Confirmation with {} complaints",
                         conf.complaints.len()
                     );
-                    self.used_messages = Some(used_msgs);
+                    self.store.set_used_messages(used_msgs);
+                    self.has_sent_confirmation = true;
                     let _ = self
                         .tx_system_messages
                         .send(SystemMessage::DkgConfirmation(
@@ -255,13 +280,10 @@ impl RandomnessState {
         }
 
         // Once we have enough Confirmations, process them and update shares.
-        if self.dkg_output.is_none()
-            && !self.confirmations.is_empty()
-            && self.used_messages.is_some()
-        {
+        if self.dkg_output.is_none() && self.store.has_used_messages() {
             match self.party.complete(
-                self.used_messages.as_ref().expect("checked above"),
-                &self.confirmations,
+                self.store.used_messages().as_ref().expect("checked above"),
+                &self.store.confirmations(),
                 self.party.t() * 2 - 1, // t==f+1, we want 2f+1
                 &mut rand::thread_rng(),
             ) {
@@ -270,7 +292,7 @@ impl RandomnessState {
                         error!("random beacon: unable to write VSS key to output: {e:?}")
                     }
                     let num_shares = output.shares.as_ref().map_or(0, |shares| shares.len());
-                    self.dkg_output = Some(output);
+                    self.set_dkg_output(output);
                     info!("random beacon: DKG complete with {num_shares} shares for this node");
                 }
                 Err(fastcrypto::error::FastCryptoError::NotEnoughInputs) => (), // wait for more input
@@ -293,38 +315,36 @@ impl RandomnessState {
     }
 
     async fn update_randomness_round(&mut self, round: RandomnessRound) {
-        if round <= self.randomness_round {
+        if round <= self.store.randomness_round() {
             // Don't go backwards.
             return;
         }
         debug!("random beacon: updating local randomness round to {round:?}");
-        self.randomness_round = round;
-        self.partial_sigs
-            .retain(|&(round, _), _| round >= self.randomness_round);
+        self.store.set_randomness_round(round);
+        self.partial_sigs.retain(|&(r, _), _| r >= round);
         self.send_partial_signatures().await;
     }
 
     async fn send_partial_signatures(&mut self) {
-        let Some(dkg_output) = &self.dkg_output else {
-            error!("random beacon: called send_partial_signatures before DKG completed");
-            return;
-        };
-        let shares = match &dkg_output.shares {
-            Some(shares) => shares,
-            None => return, // can't participate in randomness generation without shares
-        };
-        debug!(
-            "random beacon: sending partial signatures for round {}",
-            self.randomness_round
-        );
-        if self.cached_sigs.is_none()
-            || self.cached_sigs.as_ref().unwrap().0 != self.randomness_round
-        {
+        let randomness_round = self.store.randomness_round();
+        debug!("random beacon: sending partial signatures for round {randomness_round}",);
+
+        if self.cached_sigs.is_none() || self.cached_sigs.as_ref().unwrap().0 != randomness_round {
+            let shares = {
+                let Some(dkg_output) = &self.dkg_output else {
+                    error!("random beacon: called send_partial_signatures before DKG completed");
+                    return;
+                };
+                match &dkg_output.shares {
+                    Some(shares) => shares,
+                    None => return, // can't participate in randomness generation without shares
+                }
+            };
             self.cached_sigs = Some((
-                self.randomness_round,
+                randomness_round,
                 ThresholdBls12381MinSig::partial_sign_batch(
                     shares,
-                    &self.randomness_round.signature_message(),
+                    &randomness_round.signature_message(),
                 ),
             ));
         }
@@ -337,7 +357,7 @@ impl RandomnessState {
         let leader = self.leader_schedule.leader(next_leader_narwhal_round);
         if self.authority_id == leader.id() {
             // We're the next leader, no need to send an RPC.
-            self.receive_partial_signatures(self.authority_id, self.randomness_round, sigs)
+            self.receive_partial_signatures(self.authority_id, randomness_round, sigs)
                 .await;
             return;
         }
@@ -347,7 +367,7 @@ impl RandomnessState {
         let mut client = PrimaryToPrimaryClient::new(peer);
         const SEND_PARTIAL_SIGNATURES_TIMEOUT: Duration = Duration::from_secs(10);
         let request = anemo::Request::new(SendRandomnessPartialSignaturesRequest {
-            round: self.randomness_round,
+            round: randomness_round,
             sigs,
         })
         .with_timeout(SEND_PARTIAL_SIGNATURES_TIMEOUT);
@@ -375,17 +395,17 @@ impl RandomnessState {
         round: RandomnessRound,
         sigs: Vec<RandomnessPartialSignature>,
     ) {
-        let dkg_output = match &self.dkg_output {
-            Some(dkg_output) => dkg_output,
-            None => {
+        let vss_pk = {
+            let Some(dkg_output) = &self.dkg_output else {
                 error!("random beacon: called receive_partial_signatures before DKG completed");
                 return;
-            }
+            };
+            &dkg_output.vss_pk
         };
-        if round < self.randomness_round {
+        let randomness_round = self.store.randomness_round();
+        if round < randomness_round {
             debug!(
-                "random beacon: ignoring partial signatures for old round {round} (we are at {})",
-                self.randomness_round
+                "random beacon: ignoring partial signatures for old round {round} (we are at {randomness_round})",
             );
             return;
         }
@@ -393,15 +413,14 @@ impl RandomnessState {
         // Instead of throwing them away, we store up to a couple rounds ahead so they can
         // be used if we catch up.
         const MAX_ROUND_DELTA: u64 = 2;
-        if round > self.randomness_round + MAX_ROUND_DELTA {
+        if round > randomness_round + MAX_ROUND_DELTA {
             debug!(
-                "random beacon: ignoring partial signatures for too-new round {round} (we are at {})",
-                self.randomness_round
+                "random beacon: ignoring partial signatures for too-new round {round} (we are at {randomness_round})",
             );
             return;
         }
         if let Some(last_sent) = self.last_randomness_round_sent {
-            if last_sent >= self.randomness_round {
+            if last_sent >= randomness_round {
                 debug!(
                     "random beacon: ignoring partial signatures for already-finished round {round}"
                 );
@@ -425,7 +444,7 @@ impl RandomnessState {
             &self
                 .partial_sigs
                 .iter()
-                .filter(|&((round, _), _)| *round == self.randomness_round)
+                .filter(|&((round, _), _)| *round == randomness_round)
                 .flat_map(|(_, sigs)| sigs)
                 .cloned()
                 .collect::<Vec<_>>(),
@@ -439,15 +458,13 @@ impl RandomnessState {
         };
 
         // Try to verify the aggregated signature all at once. (Should work in the happy path.)
-        if ThresholdBls12381MinSig::verify(dkg_output.vss_pk.c0(), &round.signature_message(), &sig)
-            .is_err()
-        {
+        if ThresholdBls12381MinSig::verify(vss_pk.c0(), &round.signature_message(), &sig).is_err() {
             // If verifiation fails, some of the inputs must be invalid. We have to go through
             // one-by-one to find which.
             // TODO: add test for individual sig verification.
             self.partial_sigs.retain(|&(r, authority_id), partial_sigs| {
                 if ThresholdBls12381MinSig::partial_verify_batch(
-                    &dkg_output.vss_pk,
+                    vss_pk,
                     &r.signature_message(),
                      partial_sigs.as_slice(),
                     &mut rand::thread_rng(),
@@ -467,7 +484,7 @@ impl RandomnessState {
                 &self
                     .partial_sigs
                     .iter()
-                    .filter(|&((round, _), _)| *round == self.randomness_round)
+                    .filter(|&((round, _), _)| *round == randomness_round)
                     .flat_map(|(_, sigs)| sigs)
                     .cloned()
                     .collect::<Vec<_>>(),
@@ -479,11 +496,9 @@ impl RandomnessState {
                     return;
                 }
             };
-            if let Err(e) = ThresholdBls12381MinSig::verify(
-                dkg_output.vss_pk.c0(),
-                &round.signature_message(),
-                &sig,
-            ) {
+            if let Err(e) =
+                ThresholdBls12381MinSig::verify(vss_pk.c0(), &round.signature_message(), &sig)
+            {
                 error!("Error while verifying randomness partial signatures after removing invalid partials: {e:?}");
                 return;
             }
@@ -492,11 +507,11 @@ impl RandomnessState {
         let _ = self
             .tx_system_messages
             .send(SystemMessage::RandomnessSignature(
-                self.randomness_round,
+                randomness_round,
                 bcs::to_bytes(&sig).expect("signature serialization should not fail"),
             ))
             .await;
-        self.last_randomness_round_sent = Some(self.randomness_round);
+        self.last_randomness_round_sent = Some(randomness_round);
     }
 }
 
@@ -521,6 +536,7 @@ impl StateHandler {
         randomness_private_key: RandomnessPrivateKey,
         leader_schedule: LeaderSchedule,
         network: anemo::Network,
+        randomness_store: RandomnessStore,
     ) -> JoinHandle<()> {
         let state_handler = Self {
             authority_id,
@@ -539,6 +555,7 @@ impl StateHandler {
                 network.clone(),
                 vss_key_output,
                 tx_system_messages,
+                randomness_store,
             ),
             network,
         };
@@ -607,7 +624,6 @@ impl StateHandler {
                     }
                 }
                 // Advance the random beacon DKG protocol if possible after each certificate.
-                // TODO: Implement/audit crash recovery for random beacon.
                 randomness_state.advance_dkg().await;
             }
         }
@@ -620,8 +636,9 @@ impl StateHandler {
         );
 
         // Kick off randomness DKG if enabled.
-        if let Some(ref randomness_state) = self.randomness_state {
+        if let Some(randomness_state) = self.randomness_state.as_mut() {
             randomness_state.start_dkg().await;
+            randomness_state.advance_dkg().await; // for crash recovery
         }
 
         loop {
