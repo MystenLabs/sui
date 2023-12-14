dcb86a7fc4acbbc4237c4b55b5ee6788529a2093
Zhe Wu <halfprice@users.noreply.github.com>
2023-12-14 15:32:46 -0800

    Create safe_range_iter and replace range_iter (#15376)
    ## Description 
    
    Create a `safe_range_iter` and replace existing `range_iter` with it.
    With `safe_range_iter`, it returns RocksDB erros encountered while
    iterating through specified key ranges.
    
    ## Test Plan 
    
    Added more unit test to test range iterator.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-core/src/authority/authority_store_tables.rs
+++ crates/sui-core/src/authority/authority_store_tables.rs
@@ -162,14 +162,18 @@ impl AuthorityPerpetualTables {
     ) -> Option<Object> {
         let Ok(iter) = self
             .objects
-            .range_iter(ObjectKey::min_for_id(&object_id)..=ObjectKey::max_for_id(&object_id))
+            .safe_range_iter(ObjectKey::min_for_id(&object_id)..=ObjectKey::max_for_id(&object_id))
             .skip_prior_to(&ObjectKey(object_id, version))
         else {
             return None;
         };
-        iter.reverse()
-            .next()
-            .and_then(|(key, o)| self.object(&key, o).ok().flatten())
+        iter.reverse().next().and_then(|db_result| match db_result {
+            Ok((key, o)) => self.object(&key, o).ok().flatten(),
+            Err(err) => {
+                warn!("Object iterator encountered RocksDB error {:?}", err);
+                None
+            }
+        })
     }
 
     fn construct_object(
