8873bfa31e78c6321ec6093ca182c87bb93e8333
longbowlu <zl9394@gmail.com>
2023-12-17 22:09:59 -0800

    simple handle_sui_tx_digest

--- crates/sui-bridge/src/server/mod.rs
+++ crates/sui-bridge/src/server/mod.rs
@@ -26,9 +26,9 @@ pub const APPLICATION_JSON: &str = "application/json";
 pub const ETH_TO_SUI_TX_PATH: &str = "/sign/bridge_tx/eth/sui/:tx_hash/:event_index";
 pub const SUI_TO_ETH_TX_PATH: &str = "/sign/bridge_tx/sui/eth/:tx_digest/:event_index";
 
-pub async fn run_server(socket_address: &SocketAddr) {
+pub async fn run_server(socket_address: &SocketAddr, handler: BridgeRequestHandler) {
     axum::Server::bind(socket_address)
-        .serve(make_router(Arc::new(BridgeRequestHandler::new())).into_make_service())
+        .serve(make_router(Arc::new(handler)).into_make_service())
         .await
         .unwrap();
 }
