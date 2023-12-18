291555552ac8f256536b218e7e73204c3904f4fc
Will Yang <willyang@mystenlabs.com>
2023-12-18 12:20:01 -0800

    address nits

--- crates/sui-graphql-rpc/src/types/display.rs
+++ crates/sui-graphql-rpc/src/types/display.rs
@@ -9,12 +9,24 @@ use sui_types::collection_types::VecMap;
 use crate::error::Error;
 use sui_json_rpc_types::SuiMoveValue;
 
-use super::{json::Json, move_value::try_to_json_value};
-
 #[derive(Debug, SimpleObject)]
 pub(crate) struct DisplayEntry {
     pub key: String,
     pub value: String,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum DisplayRenderError {
+    #[error("Display template value cannot be empty")]
+    TemplateValueEmpty,
+    #[error("Display template value of {0} exceeds maximum depth of {1}")]
+    ExceedsLookupDepth(usize, u64),
+    #[error("Vector of name {0} is not supported as a Display value")]
+    Vector(String),
+    #[error("Field '{0}' not found")]
+    FieldNotFound(String),
+    #[error("Unexpected MoveValue")]
+    UnexpectedMoveValue,
 }
 
 pub(crate) fn get_rendered_fields(
@@ -34,7 +46,12 @@ pub(crate) fn get_rendered_fields(
     Ok(rendered_fields)
 }
 
-// handles the PART = '{' CHAIN '}'
+/// Handles the PART of the grammar, defined as:
+/// PART   ::= '{' CHAIN '}'
+///          | '\{' | '\}'
+///          | [:utf8:]
+/// Defers resolution down to the IDENT to get_value_from_move_struct,
+/// and substitutes the result into the PART template.
 fn parse_template(template: &str, move_struct: &MoveStruct) -> Result<String, Error> {
     let mut output = template.to_string();
     let mut var_name = String::new();
@@ -53,8 +70,9 @@ fn parse_template(template: &str, move_struct: &MoveStruct) -> Result<String, Er
             }
             '}' if !escaped => {
                 in_braces = false;
-                let value = get_value_from_move_struct(move_struct, &var_name)?;
-                output = output.replace(&format!("{{{}}}", var_name), &format!("{}", value));
+                let value = get_value_from_move_struct(move_struct, &var_name)
+                    .map_err(|e| Error::Internal(e.to_string()))?;
+                output = output.replace(&format!("{{{}}}", var_name), &value.to_string());
             }
             _ if !escaped => {
                 if in_braces {
@@ -69,24 +87,21 @@ fn parse_template(template: &str, move_struct: &MoveStruct) -> Result<String, Er
     Ok(output.replace('\\', ""))
 }
 
+/// Handles the CHAIN and IDENT of the grammar, defined as:
+/// CHAIN  ::= IDENT | CHAIN '.' IDENT
+/// IDENT  ::= /* Move identifier */
 pub(crate) fn get_value_from_move_struct(
     move_struct: &MoveStruct,
     var_name: &str,
-) -> Result<Json, Error> {
-    // Supports CHAIN . IDENT today
+) -> Result<String, DisplayRenderError> {
     let parts: Vec<&str> = var_name.split('.').collect();
     if parts.is_empty() {
-        // todo: custom error
-        Err(Error::Internal(
-            "Display template value cannot be empty".to_string(),
-        ))?;
+        return Err(DisplayRenderError::TemplateValueEmpty);
     }
-    // todo: new limit on config
+    // todo: 10 is a carry-over from the sui-json-rpc implementation
+    // we should introduce this as a new limit on the config
     if parts.len() > 10 {
-        Err(Error::Internal(format!(
-            "Display template value nested depth cannot exist {}",
-            10
-        )))?;
+        return Err(DisplayRenderError::ExceedsLookupDepth(parts.len(), 10));
     }
 
     // update this as we iterate through the parts
@@ -105,31 +120,19 @@ pub(crate) fn get_value_from_move_struct(
                         None
                     }
                 })
-                .ok_or_else(|| Error::Internal(format!("Field '{}' not found", part))),
-            _ => Err(Error::Internal("Unexpected MoveValue".to_string())),
+                .ok_or_else(|| DisplayRenderError::FieldNotFound(part.to_string())),
+            _ => Err(DisplayRenderError::UnexpectedMoveValue),
         })?;
 
     // TODO: implement Display for MoveData and use that instead
-    match result {
-        MoveValue::Vector(_) => Err(Error::Internal(format!(
-            "Vector is not supported as a Display value {}",
-            var_name
-        )))?,
-        _ => Ok(try_to_json_value(result.clone())?.into()),
+    let sui_move_value: SuiMoveValue = result.clone().into();
+
+    match sui_move_value {
+        SuiMoveValue::Option(move_option) => match move_option.as_ref() {
+            Some(move_value) => Ok(move_value.to_string()),
+            None => Ok("".to_string()),
+        },
+        SuiMoveValue::Vector(_) => Err(DisplayRenderError::Vector(var_name.to_string())),
+        _ => Ok(sui_move_value.to_string()),
     }
-
-    // let sui_move_value: SuiMoveValue = result.clone().into();
-    // // MoveValue::json_impl(&self, layout: A::MoveTypeLayout)
-
-    // match sui_move_value {
-    //     SuiMoveValue::Option(move_option) => match move_option.as_ref() {
-    //         Some(move_value) => Ok(move_value.to_string()),
-    //         None => Ok("".to_string()),
-    //     },
-    //     SuiMoveValue::Vector(_) => Err(Error::Internal(format!(
-    //         "Vector is not supported as a Display value {}",
-    //         var_name
-    //     )))?,
-
-    //     _ => Ok(sui_move_value.to_string()),
 }
