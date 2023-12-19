a8d242eb3bda55d1e8f4c12668d1ebebf31eae3d
suiwombat <joe@mystenlabs.com>
2023-12-19 14:26:57 -0600

    remove recreate func and clone config instead

--- crates/sui-node/src/metrics.rs
+++ crates/sui-node/src/metrics.rs
@@ -45,21 +45,6 @@ impl MetricsPushClient {
     pub fn client(&self) -> &reqwest::Client {
         &self.client
     }
-
-    /// recreate will return a new instance of self.  we only call this when we hit an error condition
-    /// on posting data. NB do not call this in a fast loop.  we have this to work around the move block
-    pub fn recreate(&self) -> Self {
-        let certificate = self.certificate.to_owned();
-        let identity = certificate.reqwest_identity();
-        let client = reqwest::Client::builder()
-            .identity(identity)
-            .build()
-            .unwrap();
-        MetricsPushClient {
-            certificate,
-            client,
-        }
-    }
 }
 
 /// Starts a task to periodically push metrics to a configured endpoint if a metrics push endpoint
@@ -84,7 +69,9 @@ pub fn start_metrics_push_task(config: &sui_config::NodeConfig, registry: Regist
         _ => return,
     };
 
-    let mut client = MetricsPushClient::new(config.network_key_pair().copy());
+    // make a copy so we can make a new client later when we hit errors posting metrics
+    let config_copy = config.clone();
+    let mut client = MetricsPushClient::new(config_copy.network_key_pair().copy());
 
     async fn push_metrics(
         client: &MetricsPushClient,
@@ -153,7 +140,7 @@ pub fn start_metrics_push_task(config: &sui_config::NodeConfig, registry: Regist
             if let Err(error) = push_metrics(&client, &url, &registry).await {
                 // aggressively recreate our client connection if we hit an error
                 // since our tick interval is only every min, this should not be racey
-                client = client.recreate();
+                client = MetricsPushClient::new(config_copy.network_key_pair().copy());
                 tracing::warn!("unable to push metrics: {error}");
             }
         }
