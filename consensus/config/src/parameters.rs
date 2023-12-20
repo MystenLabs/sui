f221eb62be864d6711de381db0b5a1509a2444d0
mwtian <81660174+mwtian@users.noreply.github.com>
2023-12-20 08:46:28 -0800

    [Consensus] add initial directory structure (#15440)
    ## Description 
    
    Create the `consensus/` directory as discussed.
    
    Also add the initial `Parameters` type.
    
    ## Test Plan 
    
    CI
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ consensus/config/src/parameters.rs
@@ -0,0 +1,33 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::time::Duration;
+
+use serde::{Deserialize, Serialize};
+
+/// Operational configurations of a consensus authority.
+///
+/// All fields should tolerate inconsistencies among authorities, without affecting safety of the
+/// protocol. Otherwise, they need to be part of Sui protocol config or epoch state on-chain.
+///
+/// NOTE: default values should make sense, so most operators should not need to specify any field.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Parameters {
+    /// Time to wait for parent round leader before sealing a block.
+    #[serde(default = "Parameters::default_leader_timeout")]
+    pub leader_timeout: Duration,
+}
+
+impl Parameters {
+    pub fn default_leader_timeout() -> Duration {
+        Duration::from_millis(250)
+    }
+}
+
+impl Default for Parameters {
+    fn default() -> Self {
+        Self {
+            leader_timeout: Parameters::default_leader_timeout(),
+        }
+    }
+}
