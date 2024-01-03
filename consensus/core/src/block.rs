b06fe706e5fc5d5eaa0c7f46a120bca8113786d7
MW Tian <mingwei@mystenlabs.com>
2024-01-03 13:32:28 -0800

    .

--- consensus/core/src/block.rs
+++ consensus/core/src/block.rs
@@ -3,13 +3,14 @@
 
 use std::{
     cell::OnceCell,
+    fmt,
     hash::{Hash, Hasher},
 };
 
-use fastcrypto::hash::HashFunction;
+use fastcrypto::hash::{Digest, HashFunction};
 use serde::{Deserialize, Serialize};
 
-use consensus_config::{AuthorityIndex, DefaultHashFunction};
+use consensus_config::{AuthorityIndex, DefaultHashFunction, DIGEST_LENGTH};
 
 /// Round number of a block.
 pub type Round = u32;
@@ -17,12 +18,23 @@ pub type Round = u32;
 /// Block proposal timestamp in milliseconds.
 pub type BlockTimestampMs = u64;
 
-/// One validator can produce at most one block per round.
 /// A block includes references to previous round blocks and transactions that the validator
 /// considers valid.
+/// Well behaved validators produce at most one block per round, but malicious validators can
+/// equivocate.
 #[derive(Clone, Deserialize, Serialize)]
 pub enum Block {
     V1(BlockV1),
+}
+
+impl fastcrypto::hash::Hash<{ DIGEST_LENGTH }> for Block {
+    type TypedDigest = BlockDigest;
+
+    fn digest(&self) -> BlockDigest {
+        match self {
+            Block::V1(block) => block.digest(),
+        }
+    }
 }
 
 pub trait BlockAPI {
@@ -31,7 +43,7 @@ pub trait BlockAPI {
     fn round(&self) -> Round;
     fn author(&self) -> AuthorityIndex;
     fn timestamp(&self) -> BlockTimestampMs;
-    fn includes(&self) -> &[BlockRef];
+    fn ancestors(&self) -> &[BlockRef];
     // TODO: add accessor for transactions.
 }
 
@@ -40,7 +52,7 @@ pub struct BlockV1 {
     round: Round,
     author: AuthorityIndex,
     timestamp: BlockTimestampMs,
-    includes: Vec<BlockRef>,
+    ancestors: Vec<BlockRef>,
 
     #[serde(skip)]
     digest: OnceCell<BlockDigest>,
@@ -75,12 +87,12 @@ impl BlockAPI for BlockV1 {
         self.timestamp
     }
 
-    fn includes(&self) -> &[BlockRef] {
-        &self.includes
+    fn ancestors(&self) -> &[BlockRef] {
+        &self.ancestors
     }
 }
 
-/// BlockRef uniquely identifies a block.
+/// BlockRef is the minimum info that uniquely identify a block.
 #[derive(Clone, Copy, Serialize, Deserialize, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub struct BlockRef {
     pub round: Round,
@@ -94,6 +106,14 @@ impl Hash for BlockRef {
     }
 }
 
+impl fastcrypto::hash::Hash<{ DIGEST_LENGTH }> for BlockRef {
+    type TypedDigest = BlockDigest;
+
+    fn digest(&self) -> BlockDigest {
+        self.digest
+    }
+}
+
 /// Hash of a block, covers all fields except signature.
 #[derive(Clone, Copy, Serialize, Deserialize, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub struct BlockDigest([u8; consensus_config::DIGEST_LENGTH]);
@@ -101,5 +121,21 @@ pub struct BlockDigest([u8; consensus_config::DIGEST_LENGTH]);
 impl Hash for BlockDigest {
     fn hash<H: Hasher>(&self, state: &mut H) {
         state.write(&self.0[..8]);
+    }
+}
+
+impl From<BlockDigest> for Digest<{ DIGEST_LENGTH }> {
+    fn from(hd: BlockDigest) -> Self {
+        Digest::new(hd.0)
+    }
+}
+
+impl fmt::Debug for BlockDigest {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(
+            f,
+            "{}",
+            base64::Engine::encode(&base64::engine::general_purpose::STANDARD, self.0)
+        )
     }
 }
