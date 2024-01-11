f705c299513800776410c1bfecf30e3d24200f2b
techdebt-99 <150741822+techdebt-99@users.noreply.github.com>
2024-01-11 08:42:02 -0700

    Update docusaurus.config.js
    Context search breaks all

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -119,7 +119,7 @@ const config = {
         indexName: "sui",
 
         // Optional: see doc section below
-        contextualSearch: true,
+        contextualSearch: false,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         // externalUrlRegex: "external\\.com|domain\\.com",
