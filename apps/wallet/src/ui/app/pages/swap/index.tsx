f17fc63e3d5876392e6843ff698ec8e9079e0762
plam-ml <127577476+plam-ml@users.noreply.github.com>
2024-01-22 19:42:02 -0800

    [wallet]: add sentry tracking for swap coins (#15811)
    Add sentry tracking for swap coins
    
    https://mysten.atlassian.net/browse/APPS-2077

--- apps/wallet/src/ui/app/pages/swap/index.tsx
+++ apps/wallet/src/ui/app/pages/swap/index.tsx
@@ -275,6 +275,7 @@ export function SwapPageContent() {
 		baseConversionRate: USDC_CONVERSION_RATE,
 		quoteConversionRate: SUI_CONVERSION_RATE,
 		enabled: isValid,
+		amount,
 	});
 
 	const recognizedPackagesList = useRecognizedPackages();
