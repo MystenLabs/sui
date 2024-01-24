ba77a7fe397e7ac84122d2a20f4ebc366971311e
Igor Zablotchi <igor@mystenlabs.com>
2024-01-24 16:20:25 +0100

    Reducing computation to 25

--- crates/sui-distributed-execution/src/setup.rs
+++ crates/sui-distributed-execution/src/setup.rs
@@ -16,7 +16,7 @@ use sui_types::{
 pub const WORKLOAD: WorkloadKind = WorkloadKind::Move {
     num_input_objects: 2,
     num_dynamic_fields: 0,
-    computation: 50,
+    computation: 25,
 };
 pub const COMPONENT: Component = Component::PipeTxsToChannel;
 
