04e4d067a07e70a2f5e50122497ac5ca5ba1d952
suiwombat <joe@mystenlabs.com>
2024-01-26 16:32:11 -0600

    timings and data

--- nre/hammertime/cmd/main.go
+++ nre/hammertime/cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"math/rand"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/Khan/genqlient/graphql"
 	// "github.com/davecgh/go-spew/spew"
@@ -17,50 +18,90 @@ func main() {
   var wg sync.WaitGroup
   wg.Add(routines)
 
-  funcs := []func(int, *sync.WaitGroup){
+  funcs := []func(int, chan result, *sync.WaitGroup){
     runEpochCheckpointsTXBlocks,
     runActiveValidators,
   }
 
-  for i:=1; i <= routines; i++ {
-    go funcs[rand.Intn(len(funcs))](i, &wg)
+  results := make(chan result)
+  for i := 1; i <= routines; i++ {
+    go funcs[rand.Intn(len(funcs))](i, results, &wg)
   }
-  wg.Wait()
-  fmt.Println("all routines finished")
+  go func() {
+    wg.Wait()
+    close(results)
+  }()
+  avg := 0.0
+  min := 0.0
+  max := 0.0
+  failed := 0.0
+  for result := range results {
+    avg = avg + result.duration
+    if min == 0 || result.duration < min {
+      min = result.duration
+    }
+    if result.duration > max {
+      max = result.duration
+    }
+    if result.err != nil {
+      failed = failed + 1.0
+    }
+  }
+  success := 100.0
+  if failed > 0 {
+    success = failed / float64(routines) * success
+  }
+  avg = avg / float64(routines)
+  if failed == 0.0 {
+    fmt.Println("all routines finished")
+  } else {
+    fmt.Println("not all routines finished")
+  }
+  fmt.Printf("min %f avg %f max %f success rate %.2f%%\n", min, avg, max, success)
 }
 
+type result struct {
+  id       int
+  duration float64
+  err      error
+}
 
-func runActiveValidators(id int, wg *sync.WaitGroup) {
+func runActiveValidators(id int, out chan result, wg *sync.WaitGroup) {
   defer wg.Done()
-
+  startTime := time.Now()
   ctx := context.Background()
   tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
+    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+  }
   hc := &http.Client{Transport: tr}
   gc := graphql.NewClient("https://sui-testnet.mystenlabs.com/graphql", hc)
   _, err := active_validators(ctx, gc)
+  stop := time.Since(startTime).Seconds()
   if err != nil {
-    fmt.Printf("runActiveValidators: %v\n", err)
+    out <- result{id: id, duration: stop, err: err}
+    fmt.Printf("runActiveValidators %d: %v\n", id, err)
     return
   }
-
-  fmt.Printf("runActiveValidators %d: finished\n", id)
+  out <- result{id: id, duration: stop}
+  fmt.Printf("runActiveValidators %d: finished %.2f sec \n", id, stop)
 }
 
-func runEpochCheckpointsTXBlocks(id int, wg *sync.WaitGroup) {
+func runEpochCheckpointsTXBlocks(id int, out chan result, wg *sync.WaitGroup) {
   defer wg.Done()
-
+  startTime := time.Now()
   ctx := context.Background()
   tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
+    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+  }
   hc := &http.Client{Transport: tr}
   gc := graphql.NewClient("https://sui-testnet.mystenlabs.com/graphql", hc)
   _, err := epoch_checkpoints_tx_blocks(ctx, gc)
+  stop := time.Since(startTime).Seconds()
   if err != nil {
-    fmt.Printf("runEpochCheckpointsTXBlocks: %v\n", err)
+    out <- result{id: id, duration: stop, err: err}
+    fmt.Printf("runEpochCheckpointsTXBlocks %d: %v\n", id, err)
     return
   }
-  fmt.Printf("runEpochCheckpointsTXBlocks %d: finished\n", id)
+  out <- result{id: id, duration: stop}
+  fmt.Printf("runEpochCheckpointsTXBlocks %d: finished %.2f sec \n", id, time.Since(startTime).Seconds())
 }
