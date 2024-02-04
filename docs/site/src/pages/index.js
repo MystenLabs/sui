aa58b2704e6890b365a4cb4e997ec959e7e71f74
Vinay Pallegar <vp@ipalmedia.com>
2024-02-03 20:48:49 -0800

    homepage desktop edits

--- docs/site/src/pages/index.js
+++ docs/site/src/pages/index.js
@@ -6,6 +6,7 @@ import { useHistory } from "@docusaurus/router";
 
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
+import styles from "./index.module.css";
 
 export default function Home() {
   const history = useHistory();
@@ -59,69 +60,69 @@ export default function Home() {
         className={`p-px col-span-3 bg-gradient-to-b from-sui-white/40 from-20% hover:from-30% via-sui-white/0 to-sui-white/10 to-80% rounded-sui w-[350px] h-[350px]`}
       >
         <div
-          className={`${
-            aux ? "bg-sui-ghost-dark" : "bg-sui-card-dark"
-          } rounded-sui w-full h-full p-8 max-w-[350px] max-h-[350px]`}
+          className={styles.card}
         >
-          <p className="text-4xl text-white table-cell align-bottom pb-2 h-16 pb-8">
+          <h4 className="text-white">
             {title}
-          </p>
-          {children}
+          </h4>
+          <div
+            className={styles.cardLinksContainer}
+          >
+            {children}
+          </div>
         </div>
       </div>
     );
   };
 
-  const cardlinks =
-    "block py-3 border-0 border-t border-solid border-color-white text-sui-blue-lighter bg-[url(../static/img/index/right-arrow.svg)] bg-no-repeat bg-right hover:no-underline hover:text-sui-blue-lighter hover:bg-[url(../static/img/index/right-arrow-blue.svg)] pr-10";
   const darkcardclass =
     "p-px col-span-4 bg-gradient-to-b from-sui-white/40 from-20% hover:from-30% via-sui-white/0 to-sui-white/10 to-80% rounded-sui";
   return (
     <Layout>
       <div className="bg-sui-black overflow-hidden">
         <div className="w-full mt-24 mb-12 mx-auto bg-sui-black">
-          <div className="text-center">
-            <p className="lg:text-8xl md:text-6xl text-4xl text-white">
+          <div className={styles.heroText}>
+            <h1 className="center-text text-white">
               Sui Documentation
-            </p>
-            <p className="text-lg sm:text-xl lg:text-3xl mb-0 px-8 md:w-1/2 w-auto mx-auto text-sui-blue-lighter inline-block">
+            </h1>
+            <h2 className="text-gray center-text h3">
               Discover the power of Sui through examples, guides, and concepts
-            </p>
+            </h2>
           </div>
         </div>
         <div className="flex flex-row flex-wrap justify-center gap-2 max-w-[1066px] mx-auto">
           <HomeCard title="About Sui">
             <Link
-              className={`${cardlinks} text-2xl`}
+              className={styles.cardLink}
               to="./concepts/tokenomics"
             >
               Tokenomics
             </Link>
             <Link
-              className={`${cardlinks} text-2xl`}
+              className={styles.cardLink}
               to="./concepts/cryptography"
             >
               Cryptography
             </Link>
-            <Link className={`${cardlinks} text-2xl`} to="standards">
+            <Link className={styles.cardLink} to="standards">
               Standards
             </Link>
           </HomeCard>
           <HomeCard title="Developers">
             <Link
-              className={`${cardlinks} text-2xl`}
+              className={styles.cardLink}
               to="./guides/developer/getting-started"
             >
               Getting started
             </Link>
             <Link
-              className={`${cardlinks} text-2xl`}
+              className={styles.cardLink}
               to="./guides/developer/sui-101"
             >
               Sui Developer Basics
             </Link>
             <Link
-              className={`${cardlinks} text-2xl`}
+              className={styles.cardLink}
               to="./concepts/sui-move-concepts"
             >
               Move
@@ -129,13 +130,13 @@ export default function Home() {
           </HomeCard>
           <HomeCard title="Validators and Node operators">
             <Link
-              className={`${cardlinks} text-2xl`}
+              className={styles.cardLink}
               to="./guides/operator/validator-config"
             >
               Validator configuration
             </Link>
             <Link
-              className={`${cardlinks} text-2xl`}
+              className={styles.cardLink}
               to="./guides/operator/sui-full-node"
             >
               Run a Sui Full node
@@ -144,22 +145,22 @@ export default function Home() {
           </HomeCard>
           <HomeCard title="References" aux>
             <Link
-              className={`${cardlinks} text-md`}
+              className={styles.cardLink}
               to="https://sdk.mystenlabs.com/dapp-kit?ref=blog.sui.io"
             >
               Sui dApp Kit
             </Link>
-            <Link className={`${cardlinks} text-md`} to="/references/sui-api">
+            <Link className={styles.cardLink} to="/references/sui-api">
               Sui API
             </Link>
             <Link
-              className={`${cardlinks} text-md`}
+              className={styles.cardLink}
               to="https://github.com/MystenLabs/sui/tree/main/crates/sui-framework/docs"
             >
               Sui framework (GitHub)
             </Link>
             <Link
-              className={`${cardlinks} text-md`}
+              className={styles.cardLink}
               to="https://github.com/MystenLabs/sui/tree/main/crates/sui-sdk"
             >
               Rust SDK (GitHub)
@@ -167,16 +168,16 @@ export default function Home() {
           </HomeCard>
           <HomeCard title="Resources" aux>
             <Link
-              className={`${cardlinks} text-md`}
+              className={styles.cardLink}
               to="https://sui.directory/?_project_type=api%2Cdeveloper-tools%2Cinfrastructure%2Csdk"
             >
               Sui ecosystem directory
             </Link>
-            <Link className={`${cardlinks} text-md`} to="https://blog.sui.io/">
+            <Link className={styles.cardLink} to="https://blog.sui.io/">
               Sui blog
             </Link>
             <Link
-              className={`${cardlinks} text-md`}
+              className={styles.cardLink}
               to="guides/developer/dev-cheat-sheet"
             >
               Sui dev cheat sheet
@@ -187,63 +188,71 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="lg:w-[1066px] w-auto my-8 md:mt-24 text-white mx-auto text-center">
-          <h1 className="mb-4 lg:text-8xl md:text-6xl text-4xl font-normal">
+        <div className="lg:w-[1066px] w-auto my-8 md:mt-24 text-white mx-auto text-center mb-20">
+          <h2 className="h1 pb-6">
             Why Sui?
-          </h1>
-          <p className="text-lg sm:text-xl lg:text-3xl mb-0 px-8 md:w-1/2 w-auto mx-auto text-sui-blue-lighter inline-block">
+          </h2>
+          <h3 class="text-gray">
             Sui is the first internet-scale programmable blockchain platform
-          </p>
+          </h3>
         </div>
-        <div className="lg:w-[1066px] width-auto mx-auto flex-none md:flex md:flex-row items-center">
-          <div className="text-center mx-auto mb-8 md:mb-0">
+        <div className={styles.why}>
+          <div className="">
             <img
-              className="lg:w-[350px] w-[250px]"
+              height={"100%"}
               src="/img/index/blocks.png"
               alt="Decorative visual"
             />
           </div>
-          <div className="lg:w-[676px] mx-8 md:ml-8 md:mr-0 lg:block sm:block md:hidden">
-            <p className="text-xl text-sui-blue-lighter border-solid border-0 border-t border-sui-white/50">
-              <span className="inline-block mt-7 mb-3 ml-4">
+          <div className={styles.cardsB}>
+            <p className={styles.cardB}>
+              <span>
                 Unmatched scalability, instant settlement
               </span>
             </p>
-            <p className="text-xl text-sui-blue-lighter border-solid border-0 border-t border-sui-white/50">
-              <span className="inline-block mt-7 mb-3 ml-4">
+            <p className={styles.cardB}>
+              <span>
                 A safe smart contract language accessible to mainstream
                 developers
               </span>
             </p>
-            <p className="text-xl text-sui-blue-lighter border-solid border-0 border-t border-sui-white/50">
-              <span className="inline-block mt-7 mb-3 ml-4">
+            <p className={styles.cardB}>
+              <span>
                 Ability to define rich and composable on-chain assets
               </span>
             </p>
-            <p className="text-xl text-sui-blue-lighter border-solid border-0 border-t border-sui-white/50">
-              <span className="inline-block mt-7 mb-3 ml-4">
+            <p className={styles.cardB}>
+              <span>
                 Better user experience for web3 apps
               </span>
             </p>
           </div>
         </div>
-        <div className="sm:w-[840] lg:w-[1066px] w-auto my-12 text-white mx-auto md:block hidden">
-          <ContentItem title="Scalability">
+        <div className={styles.TwoColParagraph}>
+         <div className={styles.TwoColItem}>
+          <span>Scalability</span>
+          <p>
             Sui scales horizontally to meet the demands of applications. Network
             capacity grows in proportion to the increase in Sui validators'
             processing power by adding workers, resulting in low gas fees even
             during high network traffic. This scalability characteristic is in
             sharp contrast to other blockchains with rigid bottlenecks.
-          </ContentItem>
-          <ContentItem title="Move">
+          </p>
+          </div>
+         <div className={styles.TwoColItem}>
+          <span>Move</span>
+          <p>
             Move design prevents issues such as reentrancy vulnerabilities,
             poison tokens, and spoofed token approvals that attackers have
             leveraged to steal millions on other platforms. The emphasis on
             safety and expressivity provides a more straightforward transition
             from web 2.0 to web3 for developers, without the need to understand
             the intricacies of the underlying infrastructure.
-          </ContentItem>
-          <ContentItem title="On-chain assets">
+          </p>
+          </div>
+         <div className={styles.TwoColItem}>
+          <span>On-chain assets</span>
+          <p>
             Rich on-chain assets enable new applications and economies based on
             utility without relying solely on artificial scarcity. Developers
             can implement dynamic NFTs that you can upgrade, bundle, and group
@@ -252,15 +261,19 @@ export default function Home() {
             stronger in-game economies as NFT behavior gets fully reflected
             on-chain, making NFTs more valuable and delivering more engaging
             feedback loops.
-          </ContentItem>
-          <ContentItem title="Built for Web3">
+          </p>
+          </div>
+         <div className={styles.TwoColItem}>
+         <span>Built for Web3</span>
+          <p>
             Sui aims to be the most accessible smart contract platform,
             empowering developers to create great user experiences in web3. To
             usher in the next billion users, Sui empowers developers with
             various tools to take advantage of the power of the Sui blockchain.
             The Sui Development Kit (SDK) will enable developers to build
             without boundaries.
-          </ContentItem>
+          </p>
+          </div>
         </div>
       </div>
     </Layout>
