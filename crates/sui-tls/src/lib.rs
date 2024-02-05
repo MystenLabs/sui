ff407acb37d7968748e8b1a58bdf008448a2b893
suiwombat <joe@mystenlabs.com>
2024-02-05 15:40:54 -0600

    cargo fmt

--- crates/sui-tls/src/lib.rs
+++ crates/sui-tls/src/lib.rs
@@ -9,7 +9,9 @@ pub const SUI_VALIDATOR_SERVER_NAME: &str = "sui";
 
 pub use acceptor::{TlsAcceptor, TlsConnectionInfo};
 pub use certgen::SelfSignedCertificate;
-pub use verifier::{AllowAll, Allower, CertVerifier, HashSetAllow, ValidatorAllowlist, public_key_from_certificate};
+pub use verifier::{
+    public_key_from_certificate, AllowAll, Allower, CertVerifier, HashSetAllow, ValidatorAllowlist,
+};
 
 pub use rustls;
 
