f735500dff3594b86e40c6e98cc3359c9b9dd5eb
Manolis Liolios <manos@mystenlabs.com>
2024-02-05 20:39:57 +0200

    remove non used code

--- dapps/multisig-toolkit/src/components/preview-effects/context.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-import { SuiClient } from '@mysten/sui.js/src/client';
-import { createContext } from 'react';
-
-export const EffectsPreviewContext = createContext<SuiClient | undefined>(undefined);
