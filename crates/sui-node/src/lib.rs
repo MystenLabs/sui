ed9981dbb949949d75c78cfd57ac463248f1696e
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2024-02-09 14:14:50 -0800

    Remove AuthorityStore from RocksdbStore (#16175)

--- crates/sui-node/src/lib.rs
+++ crates/sui-node/src/lib.rs
@@ -507,7 +507,6 @@ impl SuiNode {
         );
 
         let state_sync_store = RocksDbStore::new(
-            store.clone(),
             execution_cache.clone(),
             committee_store.clone(),
             checkpoint_store.clone(),
