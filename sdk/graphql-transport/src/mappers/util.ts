9cdf02459c30bbadbe8273043ef5b230e2c5a089
Michael Hayes <michael.hayes@mystenlabs.com>
2024-02-13 13:41:59 -0800

    Fix flaky test caused by address normalization

--- sdk/graphql-transport/src/mappers/util.ts
+++ sdk/graphql-transport/src/mappers/util.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export function toShortTypeString<T extends string | null | undefined>(type?: T): T {
-	return type?.replace(/0x0+(\d)/g, '0x$1').replace(/,\b/g, ', ') as T;
+	return type?.replace(/0x0{31,}(\d)/g, '0x$1').replace(/,\b/g, ', ') as T;
 }
 
 export function isNumericString(value: string) {
