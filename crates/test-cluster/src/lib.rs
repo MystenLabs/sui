94caddfd74ceb6a0c8890d466abb8eb97a083b28
Zhe Wu <zhe.wu@mystenlabs.com>
2024-02-15 13:35:50 -0800

    Rename OverloadThresholdConfig to AuthorityOverloadConfig

--- crates/test-cluster/src/lib.rs
+++ crates/test-cluster/src/lib.rs
@@ -12,7 +12,7 @@ use std::num::NonZeroUsize;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use sui_config::node::{DBCheckpointConfig, OverloadThresholdConfig, RunWithRange};
+use sui_config::node::{AuthorityOverloadConfig, DBCheckpointConfig, RunWithRange};
 use sui_config::{Config, SUI_CLIENT_CONFIG, SUI_NETWORK_CONFIG};
 use sui_config::{NodeConfig, PersistedConfig, SUI_KEYSTORE_FILENAME};
 use sui_core::authority_aggregator::AuthorityAggregator;
@@ -720,7 +720,7 @@ pub struct TestClusterBuilder {
     jwk_fetch_interval: Option<Duration>,
     config_dir: Option<PathBuf>,
     default_jwks: bool,
-    overload_threshold_config: Option<OverloadThresholdConfig>,
+    authority_overload_config: Option<AuthorityOverloadConfig>,
     data_ingestion_dir: Option<PathBuf>,
     fullnode_run_with_range: Option<RunWithRange>,
 }
@@ -742,7 +742,7 @@ impl TestClusterBuilder {
             jwk_fetch_interval: None,
             config_dir: None,
             default_jwks: false,
-            overload_threshold_config: None,
+            authority_overload_config: None,
             data_ingestion_dir: None,
             fullnode_run_with_range: None,
         }
@@ -890,9 +890,9 @@ impl TestClusterBuilder {
         self
     }
 
-    pub fn with_overload_threshold_config(mut self, config: OverloadThresholdConfig) -> Self {
+    pub fn with_authority_overload_config(mut self, config: AuthorityOverloadConfig) -> Self {
         assert!(self.network_config.is_none());
-        self.overload_threshold_config = Some(config);
+        self.authority_overload_config = Some(config);
         self
     }
 
@@ -992,8 +992,8 @@ impl TestClusterBuilder {
             builder = builder.with_network_config(network_config);
         }
 
-        if let Some(overload_threshold_config) = self.overload_threshold_config.take() {
-            builder = builder.with_overload_threshold_config(overload_threshold_config);
+        if let Some(authority_overload_config) = self.authority_overload_config.take() {
+            builder = builder.with_authority_overload_config(authority_overload_config);
         }
 
         if let Some(fullnode_rpc_port) = self.fullnode_rpc_port {
