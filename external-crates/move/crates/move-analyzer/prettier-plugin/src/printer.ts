497c551c01e69b51cd5abb24becba79a26e78ef7
Adam Welc <adam@mystenlabs.com>
2024-02-23 17:32:35 -0800

    [move-format] Fixed constand formatting (#16367)
    ## Description 
    
    This PR fixes constants formatting. It also contains a minor fix to test
    harness (to print the diffs correctly) and a re-formatting of the
    printer.ts file to better conform to TS convention.
    
    ## Test Plan 
    
    Tests have been adjusted and all of them should pass

--- external-crates/move/crates/move-analyzer/prettier-plugin/src/printer.ts
+++ external-crates/move/crates/move-analyzer/prettier-plugin/src/printer.ts
@@ -12,53 +12,48 @@ export function print(path: AstPath, options: ParserOptions, print: printFn) {
     const node = path.getValue()
 
     switch (node.type) {
-    case 'source_file':
-        return join(hardline, path.map(print, 'children'));
-    case 'module_definition':
-        return [
-            'module ',
-            path.call(print, 'namedChildren', 0), // module_identity
-            ' ',
-            path.call(print, 'namedChildren', 1), // module_body
-            hardline
-        ];
-    case 'module_identity':
-        return [
-            path.call(print, 'namedChildren', 0),
-            '::',
-            path.call(print, 'namedChildren', 1)
-        ];
-    case 'module_body':
-        if (node.children.length == 2) {
-            // empty module (the only children are curlies)
-            return [ '{}' ];
-        } else {
+        case 'source_file':
+            return join(hardline, path.map(print, 'children'));
+        case 'module_definition':
             return [
-                '{',
-                indent([hardline, join(hardline, path.map(print, 'namedChildren'))]),
-                hardline,
-                '}'
+                'module ',
+                path.call(print, 'namedChildren', 0), // module_identity
+                ' ',
+                path.call(print, 'namedChildren', 1), // module_body
+                hardline
             ];
-        }
-    case 'constant':
-        // The reason for indent call and multiple indentIfBreak calls is that constant definition
-        // should be able break after `const` if the name is too long, after `:` if type name is too
-        // long, and after `=` if the value is too long, all these with increasing amount of
-        // indentation. In other words, if `const c: u64 = 42;` needed all three breaks, it would
-        // look as follows:
-        //
-        // const
-        //    c:
-        //        u64 =
-        //            42;
-        const nid = Symbol('cname');
-        const tid = Symbol('tname');
-        return [
-            group(['const', indent([line, path.call(print, 'namedChildren', 0)])], {id: nid}),
-            group([':', indentIfBreak(indent([line, path.call(print, 'namedChildren', 1)]), {groupId: nid})], {id: tid}),
-            group([' =', indentIfBreak(indentIfBreak(indent([line, path.call(print, 'namedChildren', 2)]), {groupId: tid}), {groupId: tid}), ';']),
-        ];
-    default:
-        return node.text;
+        case 'module_identity':
+            return [
+                path.call(print, 'namedChildren', 0),
+                '::',
+                path.call(print, 'namedChildren', 1)
+            ];
+        case 'module_body':
+            if (node.children.length == 2) {
+                // empty module (the only children are curlies)
+                return [ '{}' ];
+            } else {
+                return [
+                    '{',
+                    indent([hardline, join(hardline, path.map(print, 'namedChildren'))]),
+                    hardline,
+                    '}'
+                ];
+            }
+        case 'constant':
+            // break and indent only on the equal sign
+            const nid = Symbol('cname');
+            const tid = Symbol('tname');
+            return group([
+                'const ',
+                path.call(print, 'namedChildren', 0),
+                ': ',
+                path.call(print, 'namedChildren', 1),
+                ' =',
+                indent([line, path.call(print, 'namedChildren', 2)]),
+                ';',
+            ]);
+        default:
+            return node.text;
     }
 }
