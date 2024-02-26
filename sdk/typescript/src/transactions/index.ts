560f310edfbeb28208c8f3be66c84e54e35c437e
Michael Hayes <michael.hayes@mystenlabs.com>
2024-02-26 15:39:55 -0800

    Add helpers for serializing pure values based on openMoveTypeSignature and normalizedMoveType

--- sdk/typescript/src/transactions/index.ts
+++ sdk/typescript/src/transactions/index.ts
@@ -1,7 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { getPureSerializationType } from './serializer.js';
+export {
+	getPureSerializationType,
+	normalizedTypeToMoveTypeSignature,
+	pureBcsSchemaFromOpenMoveTypeSignatureBody,
+} from './serializer.js';
 
 export { Inputs } from './Inputs.js';
 export {
@@ -18,5 +22,3 @@ export {
 	type TransactionObjectArgument,
 	type TransactionResult,
 } from './TransactionBlock.js';
-
-export { getPureSerializationType };
