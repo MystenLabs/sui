a53eba4f9eae2c3d676b5a6183379281933f7038
Ajit Dubey <ajit.kumar@netsetsoftware.com>
2024-02-26 18:01:58 +0530

    Issue Refactor Coin class to use CoinStruct instead of ObjectData #7920

--- apps/wallet/src/ui/app/redux/slices/sui-objects/Coin.ts
+++ apps/wallet/src/ui/app/redux/slices/sui-objects/Coin.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import type { SuiMoveObject, SuiObjectData } from '@mysten/sui.js/client';
+import type { CoinStruct } from '@mysten/sui.js/client';
 
 const COIN_TYPE = '0x2::coin::Coin';
 const COIN_TYPE_ARG_REGEX = /^0x2::coin::Coin<(.+)>$/;
@@ -11,17 +11,17 @@ export const GAS_SYMBOL = 'SUI';
 
 // TODO use sdk
 export class Coin {
-	public static isCoin(obj: SuiObjectData) {
-		const type = obj?.content?.dataType === 'package' ? 'package' : obj?.content?.type;
+	public static isCoin(obj: CoinStruct) {
+		const type = obj?.coinType === 'package' ? 'package' : obj?.coinType;
 		return type?.startsWith(COIN_TYPE) ?? false;
 	}
 
-	public static getCoinTypeArg(obj: SuiMoveObject) {
-		const res = obj.type.match(COIN_TYPE_ARG_REGEX);
+	public static getCoinTypeArg(obj: CoinStruct) {
+		const res = obj.coinType.match(COIN_TYPE_ARG_REGEX);
 		return res ? res[1] : null;
 	}
 
-	public static isSUI(obj: SuiMoveObject) {
+	public static isSUI(obj: CoinStruct) {
 		const arg = Coin.getCoinTypeArg(obj);
 		return arg ? Coin.getCoinSymbol(arg) === 'SUI' : false;
 	}
@@ -30,12 +30,12 @@ export class Coin {
 		return coinTypeArg.substring(coinTypeArg.lastIndexOf(':') + 1);
 	}
 
-	public static getBalance(obj: SuiMoveObject): bigint {
-		return BigInt((obj.fields as { balance: string }).balance);
+	public static getBalance(obj: CoinStruct): bigint {
+		return BigInt(obj.balance);
 	}
 
-	public static getID(obj: SuiMoveObject): string {
-		return (obj.fields as { id: { id: string } }).id.id;
+	public static getID(obj: CoinStruct): string {
+		return obj.coinObjectId;
 	}
 
 	public static getCoinTypeFromArg(coinTypeArg: string) {
