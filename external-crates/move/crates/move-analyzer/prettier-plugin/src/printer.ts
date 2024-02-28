a53227da625926e37fcd6972dafa01f236dd91d1
Adam Welc <adam@mystenlabs.com>
2024-02-28 14:05:55 -0800

    [move-format] Added struct formatting (#16421)
    ## Description 
    
    Added formatting for structs. It also fixes module formatting by adding
    an extra line between module member definitions. It also includes a
    small change to test harness (do not automatically generate expected
    output file if it does not exist - makes testing during development
    easier).
    
    ## Test Plan 
    
    Added new tests

--- external-crates/move/crates/move-analyzer/prettier-plugin/src/printer.ts
+++ external-crates/move/crates/move-analyzer/prettier-plugin/src/printer.ts
@@ -4,7 +4,7 @@
 import type { AstPath, Doc, ParserOptions } from 'prettier';
 import * as prettier from 'prettier';
 
-const { hardline, indent, join, line, group, indentIfBreak } = prettier.doc.builders;
+const { hardline, indent, join, line, softline, group, ifBreak } = prettier.doc.builders;
 
 type printFn = (path: AstPath) => Doc;
 
@@ -35,15 +35,16 @@ export function print(path: AstPath, options: ParserOptions, print: printFn) {
             } else {
                 return [
                     '{',
-                    indent([hardline, join(hardline, path.map(print, 'namedChildren'))]),
+                    indent([[hardline, hardline], join([hardline, hardline], path.map(print, 'namedChildren'))]),
                     hardline,
                     '}'
                 ];
             }
         case 'constant':
-            // break and indent only on the equal sign
-            const nid = Symbol('cname');
-            const tid = Symbol('tname');
+            // break and indent only on the equal sign so long form looks as follows:
+            //
+            // const c: u64 =
+            //    42;
             return group([
                 'const ',
                 path.call(print, 'namedChildren', 0),
@@ -53,6 +54,81 @@ export function print(path: AstPath, options: ParserOptions, print: printFn) {
                 indent([line, path.call(print, 'namedChildren', 2)]),
                 ';',
             ]);
+        case 'struct_definition':
+            // type parameters are on separate lines if they don't fit on one, but fields are always
+            // on separate lines:
+            //
+            // struct SomeStruct<T1: key, T2: drop> has key {
+            //     f: u64,
+            // }
+            //
+            // struct AnotherStruct<
+            //     T1: store + drop + key,
+            //     T2: store + drop + key,
+            //     T3: store + drop + key,
+            // > has key, store {
+            //     f1: u64,
+            //     f2: u64,
+            // }
+            return [
+                'struct ',
+                path.call(print, 'namedChildren', 0),
+                path.call(print, 'namedChildren', 1),
+                path.call(print, 'namedChildren', 2),
+                path.call(print, 'namedChildren', 3),
+            ]
+        case 'native_struct_definition':
+            // same formatting as "regular" struct but (of course) without fields
+            return [
+                'struct ',
+                path.call(print, 'namedChildren', 0),
+                path.call(print, 'namedChildren', 1),
+                path.call(print, 'namedChildren', 2),
+                ';',
+            ]
+        case 'ability_decls':
+            return [
+                ' has ',
+                join(', ', path.map(print, 'namedChildren'))
+            ];
+        case 'type_parameters':
+            const tparams = Symbol('tparams');
+            return [
+                '<',
+                group([
+                    indent(softline),
+                    indent(join([',', line], path.map(print, 'namedChildren'))),
+                ], {id: tparams}),
+                ifBreak([',', softline], '', {groupId: tparams}),
+                '>',
+            ];
+        case 'type_parameter':
+            let abilities = [];
+            for (let i = 1; i < node.namedChildren.length; i++) {
+                abilities.push(path.call(print, 'namedChildren', i));
+            }
+            return [
+                path.call(print, 'firstNamedChild'),
+                node.namedChildren.length > 1 ? ': ' : '' ,
+                join(' + ', abilities),
+            ];
+        case 'struct_def_fields':
+            return node.namedChildren.length == 0
+                ? ' {}'
+                : [
+                    ' {',
+                    indent(hardline),
+                    indent(join([',', hardline], path.map(print, 'namedChildren'))),
+                    ',',
+                    hardline,
+                    '}',
+                ];
+        case 'field_annotation':
+            return [
+                path.call(print, 'namedChildren', 0),
+                ': ',
+                path.call(print, 'namedChildren', 1),
+            ];
         default:
             return node.text;
     }
