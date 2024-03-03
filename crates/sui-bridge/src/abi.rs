4bc6de58ffbc86f4a087b6ccb67df8244e2f914a
longbowlu <zl9394@gmail.com>
2024-03-02 23:12:58 -0800

    update abi and eth event structs

--- crates/sui-bridge/src/abi.rs
+++ crates/sui-bridge/src/abi.rs
@@ -26,6 +26,30 @@ abigen!(
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
+abigen!(
+    EthBridgeCommittee,
+    "abi/bridge_committee.json",
+    event_derives(serde::Deserialize, serde::Serialize)
+);
+
+abigen!(
+    EthBridgeVault,
+    "abi/bridge_vault.json",
+    event_derives(serde::Deserialize, serde::Serialize)
+);
+
+abigen!(
+    EthBridgeLimiter,
+    "abi/bridge_limiter.json",
+    event_derives(serde::Deserialize, serde::Serialize)
+);
+
+abigen!(
+    EthBridgeConfig,
+    "abi/bridge_config.json",
+    event_derives(serde::Deserialize, serde::Serialize)
+);
+
 impl EthBridgeEvent {
     pub fn try_from_eth_log(log: &EthLog) -> Option<EthBridgeEvent> {
         let raw_log = RawLog {
@@ -51,7 +75,7 @@ impl EthBridgeEvent {
         match self {
             EthBridgeEvent::EthSuiBridgeEvents(event) => {
                 match event {
-                    EthSuiBridgeEvents::TokensBridgedToSuiFilter(event) => {
+                    EthSuiBridgeEvents::TokensDepositedFilter(event) => {
                         let bridge_event = match EthToSuiTokenBridgeV1::try_from(&event) {
                             Ok(bridge_event) => bridge_event,
                             // This only happens when solidity code does not align with rust code.
@@ -59,7 +83,7 @@ impl EthBridgeEvent {
                             // We log error here.
                             // TODO: add metrics and alert
                             Err(e) => {
-                                tracing::error!(?eth_tx_hash, eth_event_index, "Failed to convert TokensBridgedToSui log to EthToSuiTokenBridgeV1. This indicates incorrect parameters or a bug in the code: {:?}. Err: {:?}", event, e);
+                                tracing::error!(?eth_tx_hash, eth_event_index, "Failed to convert TokensDepositedFilter log to EthToSuiTokenBridgeV1. This indicates incorrect parameters or a bug in the code: {:?}. Err: {:?}", event, e);
                                 return None;
                             }
                         };
@@ -70,14 +94,19 @@ impl EthBridgeEvent {
                             eth_bridge_event: bridge_event,
                         }))
                     }
-                    _ => None,
+                    EthSuiBridgeEvents::TokensClaimedFilter(_event) => None,
+                    EthSuiBridgeEvents::PausedFilter(_event) => None,
+                    EthSuiBridgeEvents::UnpausedFilter(_event) => None,
+                    EthSuiBridgeEvents::UpgradedFilter(_event) => None,
+                    EthSuiBridgeEvents::InitializedFilter(_event) => None,
                 }
             }
         }
     }
 }
 
-// Sanity checked version of TokensBridgedToSuiFilter
+/// The event emitted when tokens are deposited into the bridge on Ethereum.
+/// Sanity checked version of TokensDepositedFilter
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Hash)]
 pub struct EthToSuiTokenBridgeV1 {
     pub nonce: u64,
@@ -86,20 +115,20 @@ pub struct EthToSuiTokenBridgeV1 {
     pub sui_address: SuiAddress,
     pub eth_address: EthAddress,
     pub token_id: TokenId,
-    pub amount: u64,
+    pub sui_adjusted_amount: u64,
 }
 
-impl TryFrom<&TokensBridgedToSuiFilter> for EthToSuiTokenBridgeV1 {
+impl TryFrom<&TokensDepositedFilter> for EthToSuiTokenBridgeV1 {
     type Error = BridgeError;
-    fn try_from(event: &TokensBridgedToSuiFilter) -> BridgeResult<Self> {
+    fn try_from(event: &TokensDepositedFilter) -> BridgeResult<Self> {
         Ok(Self {
             nonce: event.nonce,
             sui_chain_id: BridgeChainId::try_from(event.destination_chain_id)?,
             eth_chain_id: BridgeChainId::try_from(event.source_chain_id)?,
-            sui_address: SuiAddress::from_bytes(event.target_address.as_ref())?,
-            eth_address: event.source_address,
-            token_id: TokenId::try_from(event.token_code)?,
-            amount: event.sui_adjusted_amount,
+            sui_address: SuiAddress::from_bytes(event.recipient_address.as_ref())?,
+            eth_address: event.sender_address,
+            token_id: TokenId::try_from(event.token_id)?,
+            sui_adjusted_amount: event.sui_adjusted_amount,
         })
     }
 }
