6c5506bf51656daf7f825b85af929dbe4798f6d4
Damir Shamanaev <damirka.ru@gmail.com>
2024-03-09 20:36:41 +0300

    all new and shiny

--- sdk/move-bytecode-template/README.md
+++ sdk/move-bytecode-template/README.md
@@ -1,15 +1,56 @@
-# Move Binary Format
+# Move Bytecode Template
 
-This crate builds a WASM binary for the `move-language/move-binary-format` allowing bytecode serialization and deserialization in various environments. The main environment this package targets is "web".
+Move Bytecode Template allows updating a pre-compiled bytecode, so that a standard template could be customized and used to publish new modules on Sui directly in the browser. Hence, removing the need for a backend to compile new modules.
+
+This crate builds a WASM binary for the `move-language/move-binary-format` allowing bytecode serialization and deserialization in various environments. The main target for this package is "web".
+
+## Applications
+
+This package is a perfect fit for the following applications:
+
+- Publishing new Coins
+- Publishing TransferPolicies
+- Initializing any base type with a custom sub-type
+
+## Example of a Template Module
+
+The following code is a close-copy of the `Coin` example in the [Move by Example](https://examples.sui.io/samples/coin.html).
+
+```move
+module 0x0::template {
+    use std::option;
+    use sui::coin;
+    use sui::transfer;
+    use sui::tx_context::{Self, TxContext};
+
+    /// The OTW for the Coin
+    struct TEMPLATE has drop {}
+
+    const DECIMALS: u8 = 0;
+    const SYMBOL: vector<u8> = b"TMPL";
+    const NAME: vector<u8> = b"Template Coin";
+    const DESCRIPTION: vector<u8> = b"Template Coin Description";
+
+    /// Init the Coin
+    fun init(witness: TEMPLATE, ctx: &mut TxContext) {
+        let (treasury, metadata) = coin::create_currency(
+            witness, DECIMALS, SYMBOL, NAME, DESCRIPTION, option::none(), ctx
+        );
+
+        transfer::public_transfer(treasury, tx_context::sender(ctx));
+        transfer::public_transfer(metadata, tx_context::sender(ctx));
+    }
+}
+```
 
 ## Usage in Web applications
 
 The package consists of code and a wasm binary. While the former can be imported directly, the latter should be made available in static / public assets as a Web application. Initialization needs to be performed via a URL, and once completed, other functions become available.
 
 ```ts
-import init, initSync, * as wasm from '@mysten/move-binary-format';
+import init, initSync, * as wasm from '@mysten/move-bytecode-template';
 
-await init('...path to /move_binary_format_bg.wasm');
+await init('path/to/move_binary_format_bg.wasm');
 // alternatively initSync(...);
 
 let version = wasm.version();
@@ -17,6 +58,34 @@ let json = wasm.deserialize('a11ceb0b06....');
 let bytes = wasm.serialize(json);
 
 console.assert(json == bytes, '(de)serialization failed!');
+```
+
+## Using with Vite
+
+To use this package with Vite, you need to import the source file and the wasm binary.
+
+```ts
+import init, * as bytecodeTemplate from "@mysten/move-bytecode-template";
+import url from '@mysten/move-bytecode-template/move_bytecode_template_bg.wasm?url';
+```
+
+Later, you can initialize the package with the URL.
+
+```ts
+await init(url);
+```
+
+Lastly, once the package is initialized, you can use the functions as described in the previous section.
+
+```ts
+const template = "a11ceb0b06....";
+
+bytecodeTemplate.deserialize(template);
+bytecodeTemplate.version();
+bytecodeTemplate.update_identifiers(template, {
+    "TEMPLATE": "MY_MODULE",
+    "template": "my_module"
+});
 ```
 
 ## Build locally
--- sdk/move-bytecode-template/src/lib.rs
+++ sdk/move-bytecode-template/src/lib.rs
@@ -6,7 +6,7 @@ use std::collections::HashMap;
 use move_binary_format::{file_format::StructFieldInformation, CompiledModule};
 use move_core_types::identifier::Identifier;
 use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::to_value;
+use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::{prelude::*, JsValue};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -19,20 +19,17 @@ pub fn version() -> String {
 
 #[wasm_bindgen]
 /// Deserialize the bytecode into a JSON string.
-pub fn deserialize(binary: String) -> Result<JsValue, JsErr> {
-    let bytes = hex::decode(binary)?;
-    let compiled_module = CompiledModule::deserialize_with_defaults(&bytes[..])?;
-    let serialized = serde_json::to_string(&compiled_module)?;
-    Ok(to_value(&serialized)?)
+pub fn deserialize(binary: &[u8]) -> Result<JsValue, JsErr> {
+    let compiled_module = CompiledModule::deserialize_with_defaults(binary)?;
+    Ok(to_value(&compiled_module)?)
 }
 
 #[wasm_bindgen]
 /// Perform an operation on a bytecode string - deserialize, patch the identifiers
 /// and serialize back to a bytecode string.
-pub fn update_identifiers(binary: String, map: JsValue) -> Result<JsValue, JsErr> {
-    let bytes = hex::decode(binary)?;
+pub fn update_identifiers(binary: &[u8], map: JsValue) -> Result<Box<[u8]>, JsErr> {
     let updates: HashMap<String, String> = serde_wasm_bindgen::from_value(map)?;
-    let mut compiled_module = CompiledModule::deserialize_with_defaults(&bytes[..])?;
+    let mut compiled_module = CompiledModule::deserialize_with_defaults(binary)?;
 
     // First update the identifiers.
     for ident in compiled_module.identifiers.iter_mut() {
@@ -91,13 +88,25 @@ pub fn update_identifiers(binary: String, map: JsValue) -> Result<JsValue, JsErr
             message: err.to_string(),
         })?;
 
-    Ok(to_value(&hex::encode(binary))?)
+    Ok(binary.into())
 }
 
 #[wasm_bindgen]
-/// Serialize the JSON module into a HEX string.
-pub fn serialize(json_module: String) -> Result<JsValue, JsErr> {
-    let compiled_module: CompiledModule = serde_json::from_str(json_module.as_str())?;
+/// Updates a constant in the constant pool.
+pub fn update_constants(
+    binary: &[u8],
+    new_value: &[u8],
+    expected_value: &[u8],
+    expected_type: String,
+) -> Result<Box<[u8]>, JsErr> {
+    let mut compiled_module = CompiledModule::deserialize_with_defaults(&binary)?;
+
+    compiled_module.constant_pool.iter_mut().for_each(|handle| {
+        if handle.data == expected_value && expected_type == format!("{:?}", handle.type_) {
+            handle.data = new_value.to_vec();
+        };
+    });
+
     let mut binary = Vec::new();
     compiled_module
         .serialize(&mut binary)
@@ -105,7 +114,48 @@ pub fn serialize(json_module: String) -> Result<JsValue, JsErr> {
             display: format!("{}", err),
             message: err.to_string(),
         })?;
-    Ok(to_value(&hex::encode(binary))?)
+
+    Ok(binary.into())
+}
+
+#[wasm_bindgen]
+#[derive(Serialize, Deserialize)]
+/// A transformed constant from the constant pool.
+pub struct Constant {
+    type_: String,
+    value_bcs: Box<[u8]>,
+}
+
+#[wasm_bindgen]
+/// Convenience method to analyze the constant pool; returns all constants in order
+/// with their type and BCS value. The index can be used to update a constant.
+pub fn get_constants(binary: &[u8]) -> Result<JsValue, JsErr> {
+    let compiled_module = CompiledModule::deserialize_with_defaults(&binary)?;
+    let constants: Vec<Constant> = compiled_module
+        .constant_pool
+        .into_iter()
+        .map(|constant| Constant {
+            type_: format!("{:?}", constant.type_),
+            value_bcs: constant.data.into(),
+        })
+        .collect();
+
+    Ok(to_value(&constants)?)
+}
+
+#[wasm_bindgen]
+/// Serialize the JSON module into a HEX string.
+pub fn serialize(json_module: JsValue) -> Result<Box<[u8]>, JsErr> {
+    let compiled_module: CompiledModule = from_value(json_module)?;
+    let mut binary = Vec::new();
+    compiled_module
+        .serialize(&mut binary)
+        .map_err(|err| JsErr {
+            display: format!("{}", err),
+            message: err.to_string(),
+        })?;
+
+    Ok(binary.into())
 }
 
 #[derive(Serialize, Deserialize)]
@@ -117,17 +167,17 @@ pub struct JsErr {
     display: String,
 }
 
+impl Into<JsValue> for JsErr {
+    fn into(self) -> JsValue {
+        to_value(&self).unwrap()
+    }
+}
+
 impl<T: std::error::Error> From<T> for JsErr {
     fn from(err: T) -> Self {
         JsErr {
             display: format!("{}", err),
             message: err.to_string(),
         }
-    }
-}
-
-impl From<JsErr> for JsValue {
-    fn from(err: JsErr) -> Self {
-        to_value(&err).unwrap()
     }
 }
