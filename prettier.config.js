2e997f35a304e406c74333fbc6b02a66fd41b13a
Alexlesch98 <160536953+Alexlesch98@users.noreply.github.com>
2024-03-12 18:59:27 +0100

    Update prettier.config.js
    1. Adjusted indentation and spacing for better readability.
    2. Kept the original structure and content intact while making formatting improvements.
    3. Added comments to sections for clarity and documentation purposes.

--- prettier.config.js
+++ prettier.config.js
@@ -2,35 +2,36 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = {
-	printWidth: 100,
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'all',
-	useTabs: true,
-	plugins: ['@ianvs/prettier-plugin-sort-imports'],
-	importOrder: [
-		'<BUILT_IN_MODULES>',
-		'<THIRD_PARTY_MODULES>',
-		'',
-		'^@/(.*)$',
-		'^~/(.*)$',
-		'',
-		'^[.]',
-	],
-	overrides: [
-		{
-			files: 'apps/explorer/**/*',
-			options: {
-				plugins: ['prettier-plugin-tailwindcss'],
-				tailwindConfig: './apps/explorer/tailwind.config.ts',
-			},
-		},
-		{
-			files: 'sdk/**/*',
-			options: {
-				proseWrap: 'always',
-			},
-		},
-	],
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: true,
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<BUILT_IN_MODULES>',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/(.*)$',
+    '^~/(.*)$',
+    '',
+    '^[.]',
+  ],
+  overrides: [
+    {
+      files: 'apps/explorer/**/*',
+      options: {
+        plugins: ['prettier-plugin-tailwindcss'],
+        tailwindConfig: './apps/explorer/tailwind.config.ts',
+      },
+    },
+    {
+      files: 'sdk/**/*',
+      options: {
+        proseWrap: 'always',
+      },
+    },
+  ],
 };
+
