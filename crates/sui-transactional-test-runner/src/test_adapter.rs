4e23ef6d72b31e29efc960d07c482212da45f730
Cameron Swords <cameron.swords@mystenlabs.com>
2024-03-13 13:45:51 -0700

    Clippy fix

--- crates/sui-transactional-test-runner/src/test_adapter.rs
+++ crates/sui-transactional-test-runner/src/test_adapter.rs
@@ -1863,8 +1863,7 @@ pub static PRE_COMPILED: Lazy<FullyCompiledProgram> = Lazy::new(|| {
         buf.extend(["packages", "deepbook", "sources"]);
         buf.to_string_lossy().to_string()
     };
-    let mut config = PackageConfig::default();
-    config.edition = Edition::E2024_BETA;
+    let config = PackageConfig { edition: Edition::E2024_BETA, ..Default::default() };
     let fully_compiled_res = move_compiler::construct_pre_compiled_lib(
         vec![PackagePaths {
             name: Some(("sui-framework".into(), config)),
