008aaa8f077bd5d106caef55d33db90087839688
jamedzung <dung.dinhnguyen@digitalavenues.com>
2024-03-23 01:43:33 +0700

    implement rules

--- external-crates/move/crates/move-compiler/src/linters/mod.rs
+++ external-crates/move/crates/move-compiler/src/linters/mod.rs
@@ -3,8 +3,11 @@
 
 use move_symbol_pool::Symbol;
 
-use crate::{command_line::compiler::Visitor, diagnostics::codes::WarningFilter};
-
+use crate::{
+    command_line::compiler::Visitor, diagnostics::codes::WarningFilter,
+    linters::shift_overflow::ShiftOperationOverflow, typing::visitor::TypingVisitor,
+};
+pub mod shift_overflow;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum LintLevel {
     // No linters
@@ -17,16 +20,33 @@ pub enum LintLevel {
 
 pub const ALLOW_ATTR_CATEGORY: &str = "lint";
 pub const LINT_WARNING_PREFIX: &str = "Lint ";
+pub const SHILF_OVERFLOW_FILTER_NAME: &str = "shift_overflow";
+
+pub const LINTER_DEFAULT_DIAG_CODE: u8 = 1;
+
+pub enum LinterDiagCategory {
+    ShiftOperationOverflow,
+}
 
 pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
-    (Some(ALLOW_ATTR_CATEGORY.into()), vec![])
+    (
+        Some(ALLOW_ATTR_CATEGORY.into()),
+        vec![WarningFilter::code(
+            Some(LINT_WARNING_PREFIX),
+            LinterDiagCategory::ShiftOperationOverflow as u8,
+            LINTER_DEFAULT_DIAG_CODE,
+            Some(SHILF_OVERFLOW_FILTER_NAME),
+        )],
+    )
 }
 
 pub fn linter_visitors(level: LintLevel) -> Vec<Visitor> {
     match level {
         LintLevel::None => vec![],
         LintLevel::Default | LintLevel::All => {
-            vec![]
+            vec![shift_overflow::ShiftOperationOverflow::visitor(
+                ShiftOperationOverflow,
+            )]
         }
     }
 }
