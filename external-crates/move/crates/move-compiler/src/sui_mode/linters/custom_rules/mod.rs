ddc32d1d92ad48f427234ff001de9bca11c08421
jamedzung <dung.dinhnguyen@digitalavenues.com>
2024-03-22 17:31:32 +0700

    implement ref deref rules
    refactor code
    
    add implement constant naming
    
    move to right place
    
    fixing code comment
    
    refactor
    
    refactored
    
    revert to main
    
    add annotation testcase
    
    add module suppressed testcase
    
    refactored
    
    remove unnecessary files
    
    Update move_check_testsuite.rs
    
    refactor
    
    add implement constant naming
    
    move to right place
    
    refactor
    
    refactored
    
    revert to main
    
    add annotation testcase
    
    add module suppressed testcase
    
    refactored
    
    refactor code
    
    remove unnecessary files
    
    Update move_check_testsuite.rs
    
    refactor
    
    Update redundant_ref_deref.rs

--- external-crates/move/crates/move-compiler/src/sui_mode/linters/custom_rules/mod.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) The Diem Core Contributors
-// Copyright (c) The Move Contributors
-// SPDX-License-Identifier: Apache-2.0
-
-pub mod constant_naming;
-pub mod redundant_ref_deref;
-pub mod shift_overflow;
