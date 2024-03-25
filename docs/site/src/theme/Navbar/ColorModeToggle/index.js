393f29e04ced4df6cbc5cfb36475cb60e219dc50
Ronny Roland <ronmilton@mystenlabs.com>
2024-03-24 19:00:45 -0600

    Misc fixes; breaking local to test remote

--- docs/site/src/theme/Navbar/ColorModeToggle/index.js
+++ docs/site/src/theme/Navbar/ColorModeToggle/index.js
@@ -1,3 +1,5 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 import React from "react";
 import { useColorMode, useThemeConfig } from "@docusaurus/theme-common";
 import ColorModeToggle from "@theme/ColorModeToggle";
--- docs/site/src/theme/Navbar/index.js
+++ docs/site/src/theme/Navbar/index.js
@@ -1,3 +1,5 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 import React from "react";
 import NavbarLayout from "@theme/Navbar/Layout";
 import NavbarContent from "@theme/Navbar/Content";
