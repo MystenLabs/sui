cb63e86259f7d97b7483d70e5fd834ed850afd16
Lu Zhang <8418040+longbowlu@users.noreply.github.com>
2024-03-28 09:59:13 -0700

    [bridge 62/n] make `bridge_client_gas_object` optional, add tests for starting bridge node (#16771)
    ## Description 
    
    This PR does two things:
    1. make `bridge_client_gas_object` in BridgeConfig optional. If no gas
    object is provided, then we look for any gas object owned by the client
    address.
    2. add tests for starting bridge node.
    3. move key generation helper functions to
    `crates/sui-bridge/src/utils.rs`
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and do not break anything, you can
    skip the following section. Otherwise, please briefly describe what has
    changed under the Release Notes section.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/sui-bridge/src/node.rs
+++ crates/sui-bridge/src/node.rs
@@ -81,6 +81,7 @@ async fn start_client_components(
         } else if let Some(cursor) = cursor {
             sui_modules_to_watch.insert(module.clone(), cursor);
         } else {
+            // TODO: for sui the query can start from genesis
             return Err(anyhow::anyhow!(
                 "No cursor found for sui bridge module {} in storage or config override",
                 module
@@ -113,6 +114,7 @@ async fn start_client_components(
             // +1: The stored value is the last block that was processed, so we start from the next block.
             eth_contracts_to_watch.insert(*contract, cursor + 1);
         } else {
+            // TODO: can we not rely on this when node starts for the first time?
             return Err(anyhow::anyhow!(
                 "No cursor found for eth contract {} in storage or config override",
                 contract
@@ -161,4 +163,235 @@ async fn start_client_components(
 
     all_handles.extend(orchestrator.run(bridge_action_executor));
     Ok(all_handles)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::BTreeMap;
+    use std::process::Child;
+
+    use super::*;
+    use crate::BRIDGE_ENABLE_PROTOCOL_VERSION;
+    use crate::{config::BridgeNodeConfig, server::APPLICATION_JSON};
+    use fastcrypto::secp256k1::Secp256k1KeyPair;
+    use sui_config::local_ip_utils::get_available_port;
+    use sui_types::base_types::SuiAddress;
+    use sui_types::crypto::get_key_pair;
+    use sui_types::crypto::EncodeDecodeBase64;
+    use sui_types::crypto::KeypairTraits;
+    use sui_types::crypto::SuiKeyPair;
+    use sui_types::digests::TransactionDigest;
+    use sui_types::event::EventID;
+    use test_cluster::TestClusterBuilder;
+
+    const DUMMY_ETH_ADDRESS: &str = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984";
+
+    #[tokio::test]
+    async fn test_starting_bridge_node() {
+        telemetry_subscribers::init_for_testing();
+        let (test_cluster, anvil_port, _child) = setup().await;
+
+        // prepare node config (server only)
+        let tmp_dir = std::env::temp_dir();
+        let authority_key_path = "test_starting_bridge_node_bridge_authority_key";
+        let server_listen_port = get_available_port("127.0.0.1");
+        let kp = test_cluster.bridge_authority_keys.as_ref().unwrap()[0].copy();
+        let base64_encoded = kp.encode_base64();
+        std::fs::write(tmp_dir.join(authority_key_path), base64_encoded).unwrap();
+
+        let config = BridgeNodeConfig {
+            server_listen_port,
+            metrics_port: get_available_port("127.0.0.1"),
+            bridge_authority_key_path_base64_raw: tmp_dir.join(authority_key_path),
+            sui_rpc_url: test_cluster.fullnode_handle.rpc_url,
+            eth_rpc_url: format!("http://127.0.0.1:{}", anvil_port),
+            eth_addresses: vec![DUMMY_ETH_ADDRESS.into()],
+            approved_governance_actions: vec![],
+            run_client: false,
+            bridge_client_key_path_base64_sui_key: None,
+            bridge_client_gas_object: None,
+            sui_bridge_modules: None,
+            db_path: None,
+            eth_bridge_contracts_start_block_override: None,
+            sui_bridge_modules_last_processed_event_id_override: None,
+        };
+        // Spawn bridge node in memory
+        tokio::spawn(async move {
+            run_bridge_node(config).await.unwrap();
+        });
+
+        let server_url = format!("http://127.0.0.1:{}", server_listen_port);
+        // Now we expect to see the server to be up and running.
+        wait_for_server_to_be_up(server_url, 3).await;
+    }
+
+    #[tokio::test]
+    async fn test_starting_bridge_node_with_client() {
+        telemetry_subscribers::init_for_testing();
+        let (test_cluster, anvil_port, _child) = setup().await;
+
+        // prepare node config (server + client)
+        let tmp_dir = std::env::temp_dir();
+        let db_path = tmp_dir.join("test_starting_bridge_node_with_client_db");
+        let authority_key_path = "test_starting_bridge_node_with_client_bridge_authority_key";
+        let server_listen_port = get_available_port("127.0.0.1");
+
+        let kp = test_cluster.bridge_authority_keys.as_ref().unwrap()[0].copy();
+        let base64_encoded = kp.encode_base64();
+        std::fs::write(tmp_dir.join(authority_key_path), base64_encoded).unwrap();
+
+        let client_sui_address = SuiAddress::from(kp.public());
+        // send some gas to this address
+        test_cluster
+            .transfer_sui_must_exceeed(client_sui_address, 1000000000)
+            .await;
+
+        let config = BridgeNodeConfig {
+            server_listen_port,
+            metrics_port: get_available_port("127.0.0.1"),
+            bridge_authority_key_path_base64_raw: tmp_dir.join(authority_key_path),
+            sui_rpc_url: test_cluster.fullnode_handle.rpc_url,
+            eth_rpc_url: format!("http://127.0.0.1:{}", anvil_port),
+            eth_addresses: vec![DUMMY_ETH_ADDRESS.into()],
+            approved_governance_actions: vec![],
+            run_client: true,
+            bridge_client_key_path_base64_sui_key: None,
+            bridge_client_gas_object: None,
+            sui_bridge_modules: Some(vec!["bridge".into()]),
+            db_path: Some(db_path),
+            eth_bridge_contracts_start_block_override: Some(BTreeMap::from_iter(vec![(
+                DUMMY_ETH_ADDRESS.into(),
+                0,
+            )])),
+            sui_bridge_modules_last_processed_event_id_override: Some(BTreeMap::from_iter(vec![(
+                "bridge".into(),
+                EventID {
+                    tx_digest: TransactionDigest::random(),
+                    event_seq: 0,
+                },
+            )])),
+        };
+        // Spawn bridge node in memory
+        let config_clone = config.clone();
+        tokio::spawn(async move {
+            run_bridge_node(config_clone).await.unwrap();
+        });
+
+        let server_url = format!("http://127.0.0.1:{}", server_listen_port);
+        // Now we expect to see the server to be up and running.
+        // client components are spawned earlier than server, so as long as the server is up,
+        // we know the client components are already running.
+        wait_for_server_to_be_up(server_url, 3).await;
+    }
+
+    #[tokio::test]
+    async fn test_starting_bridge_node_with_client_and_separate_client_key() {
+        telemetry_subscribers::init_for_testing();
+        let (test_cluster, anvil_port, _child) = setup().await;
+
+        // prepare node config (server + client)
+        let tmp_dir = std::env::temp_dir();
+        let db_path =
+            tmp_dir.join("test_starting_bridge_node_with_client_and_separate_client_key_db");
+        let authority_key_path =
+            "test_starting_bridge_node_with_client_and_separate_client_key_bridge_authority_key";
+        let server_listen_port = get_available_port("127.0.0.1");
+
+        // prepare bridge authority key
+        let kp = test_cluster.bridge_authority_keys.as_ref().unwrap()[0].copy();
+        let base64_encoded = kp.encode_base64();
+        std::fs::write(tmp_dir.join(authority_key_path), base64_encoded).unwrap();
+
+        // prepare bridge client key
+        let (_, kp): (_, Secp256k1KeyPair) = get_key_pair();
+        let kp = SuiKeyPair::from(kp);
+        let client_key_path =
+            "test_starting_bridge_node_with_client_and_separate_client_key_bridge_client_key";
+        std::fs::write(tmp_dir.join(client_key_path), kp.encode_base64()).unwrap();
+        let client_sui_address = SuiAddress::from(&kp.public());
+
+        // send some gas to this address
+        let gas_obj = test_cluster
+            .transfer_sui_must_exceeed(client_sui_address, 1000000000)
+            .await;
+
+        let config = BridgeNodeConfig {
+            server_listen_port,
+            metrics_port: get_available_port("127.0.0.1"),
+            bridge_authority_key_path_base64_raw: tmp_dir.join(authority_key_path),
+            sui_rpc_url: test_cluster.fullnode_handle.rpc_url,
+            eth_rpc_url: format!("http://127.0.0.1:{}", anvil_port),
+            eth_addresses: vec![DUMMY_ETH_ADDRESS.into()],
+            approved_governance_actions: vec![],
+            run_client: true,
+            bridge_client_key_path_base64_sui_key: Some(tmp_dir.join(client_key_path)),
+            bridge_client_gas_object: Some(gas_obj),
+            sui_bridge_modules: Some(vec!["bridge".into()]),
+            db_path: Some(db_path),
+            eth_bridge_contracts_start_block_override: Some(BTreeMap::from_iter(vec![(
+                DUMMY_ETH_ADDRESS.into(),
+                0,
+            )])),
+            sui_bridge_modules_last_processed_event_id_override: Some(BTreeMap::from_iter(vec![(
+                "bridge".into(),
+                EventID {
+                    tx_digest: TransactionDigest::random(),
+                    event_seq: 0,
+                },
+            )])),
+        };
+        // Spawn bridge node in memory
+        let config_clone = config.clone();
+        tokio::spawn(async move {
+            run_bridge_node(config_clone).await.unwrap();
+        });
+
+        let server_url = format!("http://127.0.0.1:{}", server_listen_port);
+        // Now we expect to see the server to be up and running.
+        // client components are spawned earlier than server, so as long as the server is up,
+        // we know the client components are already running.
+        wait_for_server_to_be_up(server_url, 3).await;
+    }
+
+    async fn setup() -> (test_cluster::TestCluster, u16, Child) {
+        let test_cluster: test_cluster::TestCluster = TestClusterBuilder::new()
+            .with_protocol_version((BRIDGE_ENABLE_PROTOCOL_VERSION).into())
+            .with_epoch_duration_ms(10000)
+            .build_with_bridge()
+            .await;
+
+        test_cluster
+            .wait_for_next_epoch_and_assert_bridge_committee_initialized()
+            .await;
+
+        // Start eth node with anvil
+        let anvil_port = get_available_port("127.0.0.1");
+        let eth_node_process = std::process::Command::new("anvil")
+            .arg("--port")
+            .arg(anvil_port.to_string())
+            .spawn()
+            .expect("Failed to start anvil");
+
+        (test_cluster, anvil_port, eth_node_process)
+    }
+
+    async fn wait_for_server_to_be_up(server_url: String, timeout_sec: u64) {
+        let now = std::time::Instant::now();
+        // Now we expect to see the server to be up and running, the max time to wait is 3 seconds.
+        loop {
+            if let Ok(true) = reqwest::Client::new()
+                .get(server_url.clone())
+                .header(reqwest::header::ACCEPT, APPLICATION_JSON)
+                .send()
+                .await
+                .map(|res| res.status().is_success())
+            {
+                break;
+            }
+            if now.elapsed().as_secs() > timeout_sec {
+                panic!("Server is not up and running after 3 seconds");
+            }
+            tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+        }
+    }
 }
