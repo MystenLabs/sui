336255bb561593307d48f72c477f4af9fe58c27e
Michael Hayes <michael.hayes@mystenlabs.com>
2024-03-27 22:58:59 -0700

    Add plugin stack for data resolvers

--- sdk/typescript/src/transactions/TransactionBlockDataResolver.ts
+++ sdk/typescript/src/transactions/TransactionBlockDataResolver.ts
@@ -7,17 +7,10 @@ import type { OpenMoveTypeSignature } from './blockData/v2.js';
 import { normalizedTypeToMoveTypeSignature } from './serializer.js';
 import type { TransactionBlockDataBuilder } from './TransactionBlockData.js';
 
-export type MaybePromise<T> = T | Promise<T>;
-export type TransactionBlockPluginMethod<Options> = (
-	blockData: TransactionBlockDataBuilder,
-	options: Options,
-	next: (options?: Options) => MaybePromise<void>,
-) => MaybePromise<void>;
-
 export type TransactionBlockStep = (
 	blockData: TransactionBlockDataBuilder,
 	dataResolver: TransactionBlockDataResolver,
-) => MaybePromise<void>;
+) => Promise<void>;
 
 // The maximum objects that can be fetched at once using multiGetObjects.
 const MAX_OBJECTS_PER_FETCH = 50;
@@ -63,85 +56,34 @@ const chunk = <T>(arr: T[], size: number): T[][] =>
 		arr.slice(i * size, i * size + size),
 	);
 
-export abstract class TransactionBlockDataResolver {
+type TransactionBlockDataResolverPlugin = {
+	[K in keyof TransactionBlockDataResolver as TransactionBlockDataResolver[K] extends (
+		...args: any[]
+	) => unknown
+		? K
+		: never]?: TransactionBlockDataResolver[K] extends (...args: infer Args) => infer R
+		? (
+				dataResolver: TransactionBlockDataResolver,
+				...args: Args
+		  ) => R extends Promise<infer T> ? T | null | Promise<T | null> : R | null
+		: never;
+};
+
+export class TransactionBlockDataResolver {
 	protected options: SerializeTransactionBlockOptions;
+	protected plugins: TransactionBlockDataResolverPlugin[];
 
-	constructor(options: SerializeTransactionBlockOptions = {}) {
+	constructor({
+		plugins = [],
+		...options
+	}: SerializeTransactionBlockOptions & {
+		plugins?: TransactionBlockDataResolverPlugin[];
+	} = {}) {
 		this.options = options;
+		this.plugins = plugins;
 	}
 
-	getLimit(key: keyof typeof LIMITS) {
-		// Use the limits definition if that exists:
-		if (this.options.limits && typeof this.options.limits[key] === 'number') {
-			return this.options.limits[key]!;
-		}
-
-		if (!this.options.protocolConfig) {
-			return DefaultOfflineLimits[key];
-		}
-
-		// Fallback to protocol config:
-		const attribute = this.options.protocolConfig?.attributes[LIMITS[key]];
-		if (!attribute) {
-			throw new Error(`Missing expected protocol config: "${LIMITS[key]}"`);
-		}
-
-		const value =
-			'u64' in attribute
-				? attribute.u64
-				: 'u32' in attribute
-				? attribute.u32
-				: 'u16' in attribute
-				? attribute.u16
-				: attribute.f64;
-
-		if (!value) {
-			throw new Error(`Unexpected protocol config value found for: "${LIMITS[key]}"`);
-		}
-
-		// NOTE: Technically this is not a safe conversion, but we know all of the values in protocol config are safe
-		return Number(value);
-	}
-
-	loadData(blockData: TransactionBlockDataBuilder): MaybePromise<void> {}
-
-	abstract getGasPrice(blockData: TransactionBlockDataBuilder): MaybePromise<bigint>;
-
-	abstract getGasBudget(blockData: TransactionBlockDataBuilder): MaybePromise<bigint>;
-
-	abstract getGasCoins(
-		blockData: TransactionBlockDataBuilder,
-		owner: string,
-	): MaybePromise<
-		{
-			objectId: string;
-			digest: string;
-			version: string;
-		}[]
-	>;
-
-	abstract getCoins(owner: string, coinType: string): MaybePromise<CoinStruct[]>;
-
-	abstract getObjects(ids: string[]): MaybePromise<
-		{
-			objectId: string;
-			digest: string;
-			version: string;
-			initialSharedVersion: string | null;
-		}[]
-	>;
-
-	abstract getMoveFunctionDefinition(ref: {
-		package: string;
-		module: string;
-		function: string;
-	}): MaybePromise<{
-		parameters: OpenMoveTypeSignature[];
-	}>;
-}
-
-export class SuiClientTransactionBlockDataResolver extends TransactionBlockDataResolver {
-	getClient(): SuiClient {
+	#getClient(): SuiClient {
 		if (!this.options.client) {
 			throw new Error(
 				`No provider passed to Transaction#build, but transaction data was not sufficient to build offline.`,
@@ -151,144 +93,257 @@ export class SuiClientTransactionBlockDataResolver extends TransactionBlockDataR
 		return this.options.client;
 	}
 
-	override getGasPrice() {
-		return this.getClient().getReferenceGasPrice();
-	}
+	async #runPlugins<T extends keyof TransactionBlockDataResolverPlugin>(
+		method: T,
+		args: Parameters<TransactionBlockDataResolver[T]>,
+		cb: () => ReturnType<TransactionBlockDataResolver[T]>,
+	): Promise<ReturnType<TransactionBlockDataResolver[T]>> {
+		for (const plugin of this.plugins) {
+			if (method in plugin) {
+				const result = await (
+					plugin[method as never] as (
+						...args: unknown[]
+					) => ReturnType<TransactionBlockDataResolver[T]>
+				)(this, ...args);
 
-	override async getGasBudget(blockData: TransactionBlockDataBuilder) {
-		const dryRunResult = await this.getClient().dryRunTransactionBlock({
-			transactionBlock: blockData.build({
-				maxSizeBytes: this.options.limits?.maxTxSizeBytes,
-				overrides: {
-					gasData: {
-						budget: String(this.getLimit('maxTxGas')),
-						payment: [],
-					},
-				},
-			}),
-		});
-
-		if (dryRunResult.effects.status.status !== 'success') {
-			throw new Error(
-				`Dry run failed, could not automatically determine a budget: ${dryRunResult.effects.status.error}`,
-				{ cause: dryRunResult },
-			);
-		}
-
-		const safeOverhead = GAS_SAFE_OVERHEAD * BigInt(blockData.gasConfig.price || 1n);
-
-		const baseComputationCostWithOverhead =
-			BigInt(dryRunResult.effects.gasUsed.computationCost) + safeOverhead;
-
-		const gasBudget =
-			baseComputationCostWithOverhead +
-			BigInt(dryRunResult.effects.gasUsed.storageCost) -
-			BigInt(dryRunResult.effects.gasUsed.storageRebate);
-
-		return gasBudget > baseComputationCostWithOverhead
-			? gasBudget
-			: baseComputationCostWithOverhead;
-	}
-
-	override async getGasCoins(blockData: TransactionBlockDataBuilder, owner: string) {
-		const maxGasObjects = this.getLimit('maxGasObjects');
-
-		const coins = await this.getClient().getCoins({
-			owner,
-			coinType: SUI_TYPE_ARG,
-		});
-
-		const paymentCoins = coins.data
-			// Filter out coins that are also used as input:
-			.filter((coin) => {
-				const matchingInput = blockData.inputs.find((input) => {
-					if (input.Object?.ImmOrOwnedObject) {
-						return coin.coinObjectId === input.Object.ImmOrOwnedObject.objectId;
-					}
-
-					return false;
-				});
-
-				return !matchingInput;
-			})
-			.slice(0, maxGasObjects - 1)
-			.map((coin) => ({
-				objectId: coin.coinObjectId,
-				digest: coin.digest,
-				version: coin.version,
-			}));
-
-		if (!paymentCoins.length) {
-			throw new Error('No valid gas coins found for the transaction.');
-		}
-
-		return paymentCoins;
-	}
-
-	override async getObjects(ids: string[]) {
-		const objectChunks = chunk(ids, MAX_OBJECTS_PER_FETCH);
-		const objects = (
-			await Promise.all(
-				objectChunks.map((chunk) =>
-					this.getClient().multiGetObjects({
-						ids: chunk,
-						options: { showOwner: true },
-					}),
-				),
-			)
-		).flat();
-
-		const objectsById = new Map(
-			ids.map((id, index) => {
-				return [id, objects[index]];
-			}),
-		);
-
-		const invalidObjects = Array.from(objectsById)
-			.filter(([_, obj]) => obj.error)
-			.map(([id, _]) => id);
-
-		if (invalidObjects.length) {
-			throw new Error(`The following input objects are invalid: ${invalidObjects.join(', ')}`);
-		}
-
-		return objects.map((object) => {
-			if (object.error || !object.data) {
-				throw new Error(`Failed to fetch object: ${object.error}`);
+				if (result != null) {
+					return result;
+				}
 			}
-			const owner = object.data.owner;
-			const initialSharedVersion =
-				owner && typeof owner === 'object' && 'Shared' in owner
-					? owner.Shared.initial_shared_version
-					: null;
+		}
 
-			return {
-				objectId: object.data.objectId,
-				digest: object.data.digest,
-				version: object.data.version,
-				initialSharedVersion,
-			};
+		return cb();
+	}
+
+	#runPluginsSync<T extends keyof TransactionBlockDataResolver>(
+		method: T,
+		args: Parameters<TransactionBlockDataResolver[T]>,
+		cb: () => ReturnType<TransactionBlockDataResolver[T]>,
+	): ReturnType<TransactionBlockDataResolver[T]> {
+		for (const plugin of this.plugins) {
+			if (method in plugin) {
+				const result = (
+					plugin[method as never] as (
+						...args: unknown[]
+					) => ReturnType<TransactionBlockDataResolver[T]>
+				)(this, ...args);
+
+				if (result != null) {
+					return result;
+				}
+			}
+		}
+
+		return cb();
+	}
+
+	getLimit(key: keyof typeof LIMITS): number {
+		return this.#runPluginsSync('getLimit', [key], () => {
+			// Use the limits definition if that exists:
+			if (this.options.limits && typeof this.options.limits[key] === 'number') {
+				return this.options.limits[key]!;
+			}
+
+			if (!this.options.protocolConfig) {
+				return DefaultOfflineLimits[key];
+			}
+
+			// Fallback to protocol config:
+			const attribute = this.options.protocolConfig?.attributes[LIMITS[key]];
+			if (!attribute) {
+				throw new Error(`Missing expected protocol config: "${LIMITS[key]}"`);
+			}
+
+			const value =
+				'u64' in attribute
+					? attribute.u64
+					: 'u32' in attribute
+					? attribute.u32
+					: 'u16' in attribute
+					? attribute.u16
+					: attribute.f64;
+
+			if (!value) {
+				throw new Error(`Unexpected protocol config value found for: "${LIMITS[key]}"`);
+			}
+
+			// NOTE: Technically this is not a safe conversion, but we know all of the values in protocol config are safe
+			return Number(value);
 		});
 	}
 
-	override async getMoveFunctionDefinition(ref: {
+	async loadData(blockData: TransactionBlockDataBuilder): Promise<void> {
+		await this.#runPlugins('loadData', [blockData], async () => {});
+	}
+
+	async getGasPrice(blockData: TransactionBlockDataBuilder): Promise<bigint> {
+		return this.#runPlugins('getGasPrice', [blockData], async () => {
+			return this.#getClient().getReferenceGasPrice();
+		});
+	}
+
+	async getGasBudget(blockData: TransactionBlockDataBuilder): Promise<bigint> {
+		return this.#runPlugins('getGasBudget', [blockData], async () => {
+			const dryRunResult = await this.#getClient().dryRunTransactionBlock({
+				transactionBlock: blockData.build({
+					maxSizeBytes: this.options.limits?.maxTxSizeBytes,
+					overrides: {
+						gasData: {
+							budget: String(this.getLimit('maxTxGas')),
+							payment: [],
+						},
+					},
+				}),
+			});
+
+			if (dryRunResult.effects.status.status !== 'success') {
+				throw new Error(
+					`Dry run failed, could not automatically determine a budget: ${dryRunResult.effects.status.error}`,
+					{ cause: dryRunResult },
+				);
+			}
+
+			const safeOverhead = GAS_SAFE_OVERHEAD * BigInt(blockData.gasConfig.price || 1n);
+
+			const baseComputationCostWithOverhead =
+				BigInt(dryRunResult.effects.gasUsed.computationCost) + safeOverhead;
+
+			const gasBudget =
+				baseComputationCostWithOverhead +
+				BigInt(dryRunResult.effects.gasUsed.storageCost) -
+				BigInt(dryRunResult.effects.gasUsed.storageRebate);
+
+			return gasBudget > baseComputationCostWithOverhead
+				? gasBudget
+				: baseComputationCostWithOverhead;
+		});
+	}
+
+	async getGasCoins(
+		blockData: TransactionBlockDataBuilder,
+		owner: string,
+	): Promise<
+		{
+			objectId: string;
+			digest: string;
+			version: string;
+		}[]
+	> {
+		return this.#runPlugins('getGasCoins', [blockData, owner], async () => {
+			const maxGasObjects = this.getLimit('maxGasObjects');
+
+			const coins = await this.#getClient().getCoins({
+				owner,
+				coinType: SUI_TYPE_ARG,
+			});
+
+			const paymentCoins = coins.data
+				// Filter out coins that are also used as input:
+				.filter((coin) => {
+					const matchingInput = blockData.inputs.find((input) => {
+						if (input.Object?.ImmOrOwnedObject) {
+							return coin.coinObjectId === input.Object.ImmOrOwnedObject.objectId;
+						}
+
+						return false;
+					});
+
+					return !matchingInput;
+				})
+				.slice(0, maxGasObjects - 1)
+				.map((coin) => ({
+					objectId: coin.coinObjectId,
+					digest: coin.digest,
+					version: coin.version,
+				}));
+
+			if (!paymentCoins.length) {
+				throw new Error('No valid gas coins found for the transaction.');
+			}
+
+			return paymentCoins;
+		});
+	}
+
+	async getCoins(owner: string, coinType: string): Promise<CoinStruct[]> {
+		return this.#runPlugins('getCoins', [owner, coinType], async () => {
+			const coins = await this.#getClient().getCoins({
+				owner,
+				coinType,
+			});
+
+			return coins.data;
+		});
+	}
+
+	async getObjects(ids: string[]): Promise<
+		{
+			objectId: string;
+			digest: string;
+			version: string;
+			initialSharedVersion: string | null;
+		}[]
+	> {
+		return this.#runPlugins('getObjects', [ids], async () => {
+			const objectChunks = chunk(ids, MAX_OBJECTS_PER_FETCH);
+			const objects = (
+				await Promise.all(
+					objectChunks.map((chunk) =>
+						this.#getClient().multiGetObjects({
+							ids: chunk,
+							options: { showOwner: true },
+						}),
+					),
+				)
+			).flat();
+
+			const objectsById = new Map(
+				ids.map((id, index) => {
+					return [id, objects[index]];
+				}),
+			);
+
+			const invalidObjects = Array.from(objectsById)
+				.filter(([_, obj]) => obj.error)
+				.map(([id, _]) => id);
+
+			if (invalidObjects.length) {
+				throw new Error(`The following input objects are invalid: ${invalidObjects.join(', ')}`);
+			}
+
+			return objects.map((object) => {
+				if (object.error || !object.data) {
+					throw new Error(`Failed to fetch object: ${object.error}`);
+				}
+				const owner = object.data.owner;
+				const initialSharedVersion =
+					owner && typeof owner === 'object' && 'Shared' in owner
+						? owner.Shared.initial_shared_version
+						: null;
+
+				return {
+					objectId: object.data.objectId,
+					digest: object.data.digest,
+					version: object.data.version,
+					initialSharedVersion,
+				};
+			});
+		});
+	}
+
+	async getMoveFunctionDefinition(ref: {
 		package: string;
 		module: string;
 		function: string;
-	}) {
-		const definition = await this.getClient().getNormalizedMoveFunction(ref);
+	}): Promise<{
+		parameters: OpenMoveTypeSignature[];
+	}> {
+		return this.#runPlugins('getMoveFunctionDefinition', [ref], async () => {
+			const definition = await this.#getClient().getNormalizedMoveFunction(ref);
 
-		return {
-			parameters: definition.parameters.map((param) => normalizedTypeToMoveTypeSignature(param)),
-		};
-	}
-
-	override async getCoins(owner: string, coinType: string) {
-		const coins = await this.getClient().getCoins({
-			owner,
-			coinType,
+			return {
+				parameters: definition.parameters.map((param) => normalizedTypeToMoveTypeSignature(param)),
+			};
 		});
-
-		return coins.data;
 	}
 }
