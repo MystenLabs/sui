be823a7a4768f2c52742230a138d038cbc995a77
Stefan Stanciulescu <stefan.stanciulescu@mystenlabs.com>
2024-03-29 11:12:06 -0700

    Workaround for failing to do cargo build on Windows, where it complains about boilerplate being a file and not a directory.

---
+++ crates/suiop-cli/build.rs
@@ -0,0 +1,17 @@
+//! `build.rs`
+
+fn main() {
+    let manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let mut project_path = std::path::PathBuf::from(&manifest);
+    project_path.push("boilerplate");
+
+    if project_path.exists() && project_path.is_file() {
+        std::fs::remove_file(project_path.as_path()).unwrap();
+        std::fs::create_dir_all(project_path.as_path()).unwrap();
+        return;
+    }
+
+    if !project_path.exists() {
+        std::fs::create_dir_all(project_path.as_path()).unwrap();
+    }
+}
