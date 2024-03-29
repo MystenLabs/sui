b448ddfcd6f3452c201af69bc36c102c75f6b212
Bridgerz <bridgerzoske@gmail.com>
2024-03-28 19:53:50 -0700

    re-add update token regression test

--- bridge/evm/test/BridgeConfigTest.t.sol
+++ bridge/evm/test/BridgeConfigTest.t.sol
@@ -120,4 +120,77 @@ contract BridgeConfigTest is BridgeBaseTest {
 
         assertEq(encodedMessage, expectedEncodedMessage);
     }
+
+    // An e2e update token price regression test covering message ser/de and signature verification
+    function testUpdateTokenPriceRegressionTestWithSigVerficiation() public {
+        address[] memory _committee = new address[](4);
+        _committee[0] = 0x68B43fD906C0B8F024a18C56e06744F7c6157c65;
+        _committee[1] = 0xaCAEf39832CB995c4E049437A3E2eC6a7bad1Ab5;
+        _committee[2] = 0x8061f127910e8eF56F16a2C411220BaD25D61444;
+        _committee[3] = 0x508F3F1ff45F4ca3D8e86CDCC91445F00aCC59fC;
+        uint8 sendingChainID = 1;
+        uint8[] memory _supportedChains = new uint8[](1);
+        _supportedChains[0] = sendingChainID;
+        uint8 chainID = 11;
+        uint16[] memory _stake = new uint16[](4);
+        _stake[0] = 2500;
+        _stake[1] = 2500;
+        _stake[2] = 2500;
+        _stake[3] = 2500;
+        committee = new BridgeCommittee();
+        committee.initialize(_committee, _stake, minStakeRequired);
+        uint8[] memory _supportedDestinationChains = new uint8[](1);
+        _supportedDestinationChains[0] = sendingChainID;
+
+        config = new BridgeConfig();
+        config.initialize(
+            address(committee), chainID, supportedTokens, tokenPrices, _supportedChains
+        );
+
+        committee.initializeConfig(address(config));
+
+        vault = new BridgeVault(wETH);
+        skip(2 days);
+
+        uint64[] memory totalLimits = new uint64[](1);
+        totalLimits[0] = 1000000;
+
+        limiter = new BridgeLimiter();
+        limiter.initialize(address(committee), _supportedDestinationChains, totalLimits);
+        bridge = new SuiBridge();
+        bridge.initialize(address(committee), address(vault), address(limiter), wETH);
+        vault.transferOwnership(address(bridge));
+        limiter.transferOwnership(address(bridge));
+
+        // BTC -> 600_000_000 ($60k)
+        bytes memory payload = hex"010000000023c34600";
+
+        // Create update token price message
+        BridgeUtils.Message memory message = BridgeUtils.Message({
+            messageType: BridgeUtils.UPDATE_TOKEN_PRICE,
+            version: 1,
+            nonce: 0,
+            chainID: chainID,
+            payload: payload
+        });
+        bytes memory encodedMessage = BridgeUtils.encodeMessage(message);
+        bytes memory expectedEncodedMessage =
+            hex"5355495f4252494447455f4d455353414745040100000000000000000b010000000023c34600";
+
+        assertEq(encodedMessage, expectedEncodedMessage);
+
+        bytes[] memory signatures = new bytes[](3);
+
+        signatures[0] =
+            hex"eb81068c2214c01bf5d89e6bd748c0d184ae68f74d365174657053af916dcd335960737eb724560a3481bb77b7df4169d8305a034143e1c749fd9f9bcda6cc1601";
+        signatures[1] =
+            hex"116ad7d7bb705374328f85613020777d636fa092f98aa59a1d58f12f36d96f0e7aacfeb8ff356289da8d0d75278ccad8c19ec878db0b836f96ab544e91de1fed01";
+        signatures[2] =
+            hex"b0229b50b0fe3fd4cdb05b31c7689d99e3181f9f11069cb457d73112985865ff504d9a9959c367d02b18b2d78312a012f194798499198410880351ab0a241a0c00";
+
+        committee.verifySignatures(signatures, message);
+
+        config.updateTokenPriceWithSignatures(signatures, message);
+        assertEq(config.tokenPrices(BridgeUtils.BTC), 600_000_000);
+    }
 }
