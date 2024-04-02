05f34a2f1e1285f0f0141ea7d1a8c840c327700b
Carlos Baez <cb@mamoru.ai>
2024-04-02 19:00:25 +0200

    Added WIT modules to parse WIT components
    Signed-off-by: Carlos Baez <cb@mamoru.ai>

--- crates/mamoru-sui-sniffer/src/lib.rs
+++ crates/mamoru-sui-sniffer/src/lib.rs
@@ -38,6 +38,8 @@ use sui_types::{
     transaction::{Command, ProgrammableTransaction, TransactionDataAPI, TransactionKind},
 };
 
+//use mamoru_core::daemon::wit::component::guest::sui_ctx::SuiEvent;
+
 mod error;
 
 pub struct SuiSniffer {
@@ -99,6 +101,7 @@ impl SuiSniffer {
 
         let events_timer = Instant::now();
         register_events(ctx_builder.data_mut(), layout_resolver, seq, events);
+        register_wit_events(ctx_builder.data_mut(), layout_resolver, seq, events);
 
         info!(
             duration_ms = events_timer.elapsed().as_millis(),
@@ -106,7 +109,8 @@ impl SuiSniffer {
         );
 
         let call_traces_timer = Instant::now();
-        register_call_traces(ctx_builder.data_mut(), seq, call_traces);
+        register_call_traces(ctx_builder.data_mut(), seq, call_traces.clone());
+        register_wit_call_traces(ctx_builder.data_mut(), seq, call_traces);
 
         info!(
             duration_ms = call_traces_timer.elapsed().as_millis(),
@@ -231,6 +235,32 @@ fn register_programmable_transaction(ctx: &mut SuiCtx, tx: &ProgrammableTransact
     }
 }
 
+fn register_wit_call_traces(ctx: &mut SuiCtx, tx_seq: u64, move_call_traces: Vec<MoveCallTrace>) {
+    let call_traces: Vec<_> = move_call_traces
+        .into_iter()
+        .zip(0..)
+        .map(|(trace, trace_seq)| {
+            let trace_seq = trace_seq as u64;
+
+            let call_trace = mamoru_sui_types::SuiCalltrace {
+                seq: trace_seq,
+                tx_seq,
+                depth: trace.depth,
+                call_type: match trace.call_type {
+                    MoveCallType::Call => 0,
+                    MoveCallType::CallGeneric => 1,
+                },
+                gas_used: trace.gas_used,
+                transaction_module: trace.module_id.map(|module| module.short_str_lossless()),
+                function: trace.function.to_string(),
+            };
+            call_trace
+        })
+        .collect();
+
+    ctx.wit_calltraces.extend(call_traces);
+}
+
 fn register_call_traces(ctx: &mut SuiCtx, tx_seq: u64, move_call_traces: Vec<MoveCallTrace>) {
     let mut call_trace_args_len = ctx.call_trace_args.len();
     let mut call_trace_type_args_len = ctx.call_trace_type_args.len();
@@ -334,6 +364,107 @@ fn register_events(
         .collect();
 
     data.events.extend(mamoru_events);
+}
+
+fn register_wit_events(
+    data: &mut SuiCtx,
+    layout_resolver: &mut dyn LayoutResolver,
+    tx_seq: u64,
+    events: &[Event],
+) {
+    let mamoru_events: Vec<_> = events
+        .iter()
+        .filter_map(|event| {
+            let Ok(event_struct_layout) = layout_resolver.get_annotated_layout(&event.type_) else {
+                warn!(%event.type_, "Can't fetch layout by type");
+                return None;
+            };
+
+            let Ok(event_struct) =
+                Event::move_event_to_move_struct(&event.contents, event_struct_layout)
+            else {
+                warn!(%event.type_, "Can't parse event contents");
+                return None;
+            };
+
+            let mut stack_data: Vec<mamoru_sui_types::ValueType> = Vec::new();
+            match to_wit_value_data(&MoveValue::Struct(event_struct), &mut stack_data) {
+                Ok(value_data) => Some(mamoru_sui_types::SuiEvent {
+                    tx_seq,
+                    package_id: format_object_id(event.package_id),
+                    transaction_module: event.transaction_module.clone().into_string(),
+                    sender: format_object_id(event.sender),
+                    typ: event.type_.to_canonical_string(true),
+                    contents: mamoru_sui_types::ValueData {
+                        data: if stack_data.is_empty() {
+                            None
+                        } else {
+                            Some(stack_data)
+                        },
+                        value: value_data,
+                    },
+                }),
+                Err(mesg) => {
+                    warn!(%event.type_, "Can't convert event contents to wit: ValueData");
+                    warn!(mesg);
+                    return None;
+                }
+            }
+        })
+        .collect();
+
+    data.wit_events.extend(mamoru_events);
+    println!("{:}", data.wit_events.len());
+}
+
+const MAX_STACK: usize = 500;
+
+fn to_wit_value_data(
+    data: &MoveValue,
+    stack: &mut Vec<mamoru_sui_types::ValueType>,
+) -> Result<mamoru_sui_types::ValueType, &'static str> {
+    if stack.len() > MAX_STACK {
+        return Err("Maximum stack capacity achieved");
+    }
+
+    let result = match data {
+        MoveValue::Bool(value) => mamoru_sui_types::ValueType::Bool(*value),
+        MoveValue::U8(value) => mamoru_sui_types::ValueType::U64(*value as u64),
+        MoveValue::U16(value) => mamoru_sui_types::ValueType::U64(*value as u64),
+        MoveValue::U32(value) => mamoru_sui_types::ValueType::U64(*value as u64),
+        MoveValue::U64(value) => mamoru_sui_types::ValueType::U64(*value),
+        MoveValue::U128(value) => mamoru_sui_types::ValueType::String(format!("{:#x}", value)),
+        MoveValue::U256(value) => mamoru_sui_types::ValueType::String(format!("{:#x}", value)),
+        MoveValue::Address(addr) | MoveValue::Signer(addr) => {
+            mamoru_sui_types::ValueType::String(format_object_id(addr))
+        }
+        MoveValue::Vector(value) => {
+            let list_values = value
+                .iter()
+                .map(|elem| -> Result<u64, &'static str> {
+                    let parsed_type = to_wit_value_data(elem, stack)?;
+                    let index = stack.len();
+                    (*stack).push(parsed_type);
+                    Ok(index as u64)
+                })
+                .collect::<Result<Vec<u64>, &'static str>>()?;
+            mamoru_sui_types::ValueType::List(list_values)
+        }
+        MoveValue::Struct(value) => {
+            let MoveStruct { type_, fields } = value;
+            let elems: Vec<(String, u64)> = fields
+                .iter()
+                .map(|(field, value)| {
+                    let parsed_type = to_wit_value_data(&value, stack)?;
+                    let index = stack.len();
+                    (*stack).push(parsed_type);
+                    Ok((field.clone().into_string(), index as u64))
+                })
+                .collect::<Result<Vec<(String, u64)>, &'static str>>()?;
+            mamoru_sui_types::ValueType::Struct((type_.to_canonical_string(true), elems))
+        }
+    };
+    return Ok(result);
 }
 
 fn register_object_changes(
