35e044c259d49f4cd0f76ea3919bd163ba2938ae
Bridgerz <bridgerzoske@gmail.com>
2024-04-02 15:12:56 -0700

    EVM Unit Tests Pre-Land (#16972)

--- bridge/evm/test/BridgeBaseTest.t.sol
+++ bridge/evm/test/BridgeBaseTest.t.sol
@@ -27,6 +27,7 @@ contract BridgeBaseTest is Test {
 
     address deployer;
 
+    // token addresses on mainnet
     address wETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
