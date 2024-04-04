9542386f6c9238aff4db78721bbae852d1271945
Timothy Zakian <tim@mystenlabs.com>
2024-04-03 17:21:37 -0700

    [move] Human readable object output in transactional test runner

--- crates/sui-transactional-test-runner/src/test_adapter.rs
+++ crates/sui-transactional-test-runner/src/test_adapter.rs
@@ -652,7 +652,7 @@ impl<'a> MoveTestAdapter<'a> for SuiTestAdapter {
                                 .unwrap();
 
                         self.stabilize_str(format!(
-                            "Owner: {}\nVersion: {}\nContents: {}",
+                            "Owner: {}\nVersion: {}\nContents: {:#}",
                             &obj.owner,
                             obj.version().value(),
                             move_struct
