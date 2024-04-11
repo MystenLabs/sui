e871c7d84461d797f4716e6cd648bfc1394a9725
jamedzung <dung.dinhnguyen@digitalavenues.com>
2024-04-11 15:44:46 +0700

    implement rules

--- external-crates/move/crates/move-compiler/src/linters/mod.rs
+++ external-crates/move/crates/move-compiler/src/linters/mod.rs
@@ -5,9 +5,9 @@ use move_symbol_pool::Symbol;
 
 use crate::{
     command_line::compiler::Visitor, diagnostics::codes::WarningFilter,
-    linters::shift_overflow::ShiftOperationOverflow, typing::visitor::TypingVisitor,
+    linters::impossible_comparisons::ImpossibleDoubleComparison, typing::visitor::TypingVisitor,
 };
-pub mod shift_overflow;
+pub mod impossible_comparisons;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum LintLevel {
     // No linters
@@ -20,12 +20,13 @@ pub enum LintLevel {
 
 pub const ALLOW_ATTR_CATEGORY: &str = "lint";
 pub const LINT_WARNING_PREFIX: &str = "Lint ";
-pub const SHILF_OVERFLOW_FILTER_NAME: &str = "shift_overflow";
+pub const DOUBLE_COMPARISON_FILTER_NAME: &str = "double_comparison";
 
 pub const LINTER_DEFAULT_DIAG_CODE: u8 = 1;
+pub const LINTER_DOUBLE_COMPARISON_DIAG_CODE: u8 = 11;
 
 pub enum LinterDiagCategory {
-    ShiftOperationOverflow,
+    Correctness,
 }
 
 pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
@@ -33,9 +34,9 @@ pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
         Some(ALLOW_ATTR_CATEGORY.into()),
         vec![WarningFilter::code(
             Some(LINT_WARNING_PREFIX),
-            LinterDiagCategory::ShiftOperationOverflow as u8,
-            LINTER_DEFAULT_DIAG_CODE,
-            Some(SHILF_OVERFLOW_FILTER_NAME),
+            LinterDiagCategory::Correctness as u8,
+            LINTER_DOUBLE_COMPARISON_DIAG_CODE,
+            Some(DOUBLE_COMPARISON_FILTER_NAME),
         )],
     )
 }
@@ -44,8 +45,8 @@ pub fn linter_visitors(level: LintLevel) -> Vec<Visitor> {
     match level {
         LintLevel::None => vec![],
         LintLevel::Default | LintLevel::All => {
-            vec![shift_overflow::ShiftOperationOverflow::visitor(
-                ShiftOperationOverflow,
+            vec![impossible_comparisons::ImpossibleDoubleComparison::visitor(
+                ImpossibleDoubleComparison,
             )]
         }
     }
