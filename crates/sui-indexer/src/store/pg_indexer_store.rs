d35c1f786e134a17afbbab44a5db6d33fcdecfd7
Ge Gao <ge@mystenlabs.com>
2024-04-15 14:14:36 -0400

    indexer: backfill snapshot mutation and deletion together

--- crates/sui-indexer/src/store/pg_indexer_store.rs
+++ crates/sui-indexer/src/store/pg_indexer_store.rs
@@ -31,8 +31,8 @@ use crate::models::display::StoredDisplay;
 use crate::models::epoch::StoredEpochInfo;
 use crate::models::events::StoredEvent;
 use crate::models::objects::{
-    StoredDeletedHistoryObject, StoredDeletedObject, StoredDeletedObjectSnapshot,
-    StoredHistoryObject, StoredObject, StoredObjectSnapshot,
+    StoredDeletedHistoryObject, StoredDeletedObject, StoredHistoryObject, StoredObject,
+    StoredObjectSnapshot,
 };
 use crate::models::packages::StoredPackage;
 use crate::models::transactions::StoredTransaction;
@@ -280,16 +280,14 @@ impl PgIndexerStore {
             .metrics
             .checkpoint_db_commit_latency_objects_snapshot_chunks
             .start_timer();
-        let mut mutated_objects: Vec<StoredObjectSnapshot> = vec![];
-        let mut deleted_object_ids: Vec<StoredDeletedObjectSnapshot> = vec![];
-
+        let mut objects_snapshot: Vec<StoredObjectSnapshot> = vec![];
         for object in objects {
             match object {
                 ObjectChangeToCommit::MutatedObject(stored_object) => {
-                    mutated_objects.push(stored_object.into());
+                    objects_snapshot.push(stored_object.into());
                 }
                 ObjectChangeToCommit::DeletedObject(stored_deleted_object) => {
-                    deleted_object_ids.push(stored_deleted_object.into());
+                    objects_snapshot.push(stored_deleted_object.into());
                 }
             }
         }
@@ -297,11 +295,11 @@ impl PgIndexerStore {
         transactional_blocking_with_retry!(
             &self.blocking_cp,
             |conn| {
-                for mutated_object_change_chunk in
-                    mutated_objects.chunks(PG_COMMIT_CHUNK_SIZE_INTRA_DB_TX)
+                for objects_snapshot_chunk in
+                    objects_snapshot.chunks(PG_COMMIT_CHUNK_SIZE_INTRA_DB_TX)
                 {
                     diesel::insert_into(objects_snapshot::table)
-                        .values(mutated_object_change_chunk)
+                        .values(objects_snapshot_chunk)
                         .on_conflict(objects_snapshot::object_id)
                         .do_update()
                         .set((
@@ -333,28 +331,6 @@ impl PgIndexerStore {
                         .map_err(IndexerError::from)
                         .context("Failed to write object mutations to objects_snapshot in DB.")?;
                 }
-
-                for deleted_objects_chunk in
-                    deleted_object_ids.chunks(PG_COMMIT_CHUNK_SIZE_INTRA_DB_TX)
-                {
-                    diesel::insert_into(objects_snapshot::table)
-                        .values(deleted_objects_chunk)
-                        .on_conflict(objects_snapshot::object_id)
-                        .do_update()
-                        .set((
-                            objects_snapshot::object_id.eq(excluded(objects_snapshot::object_id)),
-                            objects_snapshot::object_version
-                                .eq(excluded(objects_snapshot::object_version)),
-                            objects_snapshot::object_status
-                                .eq(excluded(objects_snapshot::object_status)),
-                            objects_snapshot::checkpoint_sequence_number
-                                .eq(excluded(objects_snapshot::checkpoint_sequence_number)),
-                        ))
-                        .execute(conn)
-                        .map_err(IndexerError::from)
-                        .context("Failed to write object deletions to objects_snapshot in DB.")?;
-                }
-
                 Ok::<(), IndexerError>(())
             },
             Duration::from_secs(60)
@@ -364,7 +340,7 @@ impl PgIndexerStore {
             info!(
                 elapsed,
                 "Persisted {} chunked objects snapshot",
-                mutated_objects.len() + deleted_object_ids.len(),
+                objects_snapshot.len(),
             )
         })
     }
@@ -952,6 +928,7 @@ impl IndexerStore for PgIndexerStore {
         let objects = make_final_list_of_objects_to_commit(object_changes);
         let len = objects.len();
         let chunks = chunk!(objects, self.parallel_objects_chunk_size);
+        // TODO if deletion is indeed the thing, then we should split mutation and deletion first and then batch separately
         let futures = chunks
             .into_iter()
             .map(|c| self.spawn_blocking_task(move |this| this.persist_objects_chunk(c)))
