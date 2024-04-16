cd3c4a8f2a638707f004a77979f2d6293efdcef2
Zhe Wu <zhe.wu@mystenlabs.com>
2024-04-16 14:20:16 -0700

    Reduce per shared object max queue length to 100

--- crates/sui-config/src/node.rs
+++ crates/sui-config/src/node.rs
@@ -791,7 +791,7 @@ fn default_max_transaction_manager_queue_length() -> usize {
 }
 
 fn default_max_transaction_manager_per_object_queue_length() -> usize {
-    200
+    100
 }
 
 impl Default for AuthorityOverloadConfig {
