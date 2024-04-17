9c0e3dce254e31f64d7c83f74a5e1b5b9675222b
hayes-mysten <135670682+hayes-mysten@users.noreply.github.com>
2024-04-17 14:35:53 -0700

    Add graphQL client to zklogin public identifier (#17215)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- sdk/typescript/src/verify/index.ts
+++ sdk/typescript/src/verify/index.ts
@@ -1,16 +1,15 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { toB64 } from '@mysten/bcs';
-
 import type { PublicKey, SerializedSignature, SignatureScheme } from '../cryptography/index.js';
 import { parseSerializedSignature } from '../cryptography/index.js';
+import type { SuiGraphQLClient } from '../graphql/client.js';
 import { Ed25519PublicKey } from '../keypairs/ed25519/publickey.js';
 import { Secp256k1PublicKey } from '../keypairs/secp256k1/publickey.js';
 import { Secp256r1PublicKey } from '../keypairs/secp256r1/publickey.js';
 // eslint-disable-next-line import/no-cycle
 import { MultiSigPublicKey } from '../multisig/publickey.js';
-import { graphqlVerifyZkLoginSignature, ZkLoginPublicIdentifier } from '../zklogin/publickey.js';
+import { ZkLoginPublicIdentifier } from '../zklogin/publickey.js';
 
 export async function verifySignature(
 	bytes: Uint8Array,
@@ -28,22 +27,9 @@ export async function verifySignature(
 export async function verifyPersonalMessage(
 	message: Uint8Array,
 	signature: SerializedSignature,
+	options: { client?: SuiGraphQLClient } = {},
 ): Promise<PublicKey> {
-	const parsedSignature = parseSignature(signature);
-
-	if (parsedSignature.signatureScheme === 'ZkLogin') {
-		if (
-			!(await graphqlVerifyZkLoginSignature({
-				address: parsedSignature.zkLogin.address,
-				bytes: toB64(message),
-				signature,
-			}))
-		) {
-			throw new Error(`Signature is not valid for the provided message`);
-		}
-
-		return parsedSignature.publicKey;
-	}
+	const parsedSignature = parseSignature(signature, options);
 
 	if (
 		!(await parsedSignature.publicKey.verifyPersonalMessage(
@@ -75,7 +61,10 @@ export async function verifyTransactionBlock(
 	return parsedSignature.publicKey;
 }
 
-function parseSignature(signature: SerializedSignature) {
+function parseSignature(
+	signature: SerializedSignature,
+	options: { client?: SuiGraphQLClient } = {},
+) {
 	const parsedSignature = parseSerializedSignature(signature);
 
 	if (parsedSignature.signatureScheme === 'MultiSig') {
@@ -88,6 +77,7 @@ function parseSignature(signature: SerializedSignature) {
 	const publicKey = publicKeyFromRawBytes(
 		parsedSignature.signatureScheme,
 		parsedSignature.publicKey,
+		options,
 	);
 	return {
 		...parsedSignature,
@@ -98,6 +88,7 @@ function parseSignature(signature: SerializedSignature) {
 export function publicKeyFromRawBytes(
 	signatureScheme: SignatureScheme,
 	bytes: Uint8Array,
+	options: { client?: SuiGraphQLClient } = {},
 ): PublicKey {
 	switch (signatureScheme) {
 		case 'ED25519':
@@ -109,7 +100,7 @@ export function publicKeyFromRawBytes(
 		case 'MultiSig':
 			return new MultiSigPublicKey(bytes);
 		case 'ZkLogin':
-			return new ZkLoginPublicIdentifier(bytes);
+			return new ZkLoginPublicIdentifier(bytes, options);
 		default:
 			throw new Error(`Unsupported signature scheme ${signatureScheme}`);
 	}
