be0499262697635804d8aca903b526480507c528
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2024-04-17 11:15:58 -0400

    feat: use graphql endpoint for zklogin verify personal msg

--- sdk/typescript/src/zklogin/publickey.ts
+++ sdk/typescript/src/zklogin/publickey.ts
@@ -1,12 +1,16 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { fromB64 } from '@mysten/bcs';
+import { fromB64, toB64 } from '@mysten/bcs';
 
 import { PublicKey } from '../cryptography/publickey.js';
 import type { PublicKeyInitData } from '../cryptography/publickey.js';
 import { SIGNATURE_SCHEME_TO_FLAG } from '../cryptography/signature-scheme.js';
 import type { SerializedSignature } from '../cryptography/signature.js';
+import { SuiGraphQLClient } from '../graphql/client.js';
+import { graphql } from '../graphql/schemas/2024-01/index.js';
+import { extractClaimValue } from './jwt-utils.js';
+import { parseZkLoginSignature } from './signature.js';
 import { toPaddedBigEndianBytes } from './utils.js';
 
 /**
@@ -55,11 +59,14 @@ export class ZkLoginPublicIdentifier extends PublicKey {
 	/**
 	 * Verifies that the signature is valid for for the provided message
 	 */
-	async verify(
-		_message: Uint8Array,
-		_signature: Uint8Array | SerializedSignature,
-	): Promise<boolean> {
-		throw Error('does not support');
+	async verify(message: Uint8Array, signature: Uint8Array | SerializedSignature): Promise<boolean> {
+		const parsedSignature = parseSerializedZkLoginSignature(signature);
+
+		return await graphqlVerifyZkLoginSignature({
+			address: parsedSignature.zkLogin!.address,
+			bytes: toB64(message),
+			signature: parsedSignature.serializedSignature,
+		});
 	}
 }
 
@@ -76,4 +83,85 @@ export function toZkLoginPublicIdentifier(
 	tmp.set(issBytes, 1);
 	tmp.set(addressSeedBytesBigEndian, 1 + issBytes.length);
 	return new ZkLoginPublicIdentifier(tmp);
+}
+
+const VerifyZkLoginSignatureQuery = graphql(`
+	query Zklogin(
+		$bytes: Base64!
+		$signature: Base64!
+		$intent_scope: ZkLoginIntentScope!
+		$author: SuiAddress!
+	) {
+		verifyZkloginSignature(
+			bytes: $bytes
+			signature: $signature
+			intentScope: $intent_scope
+			author: $author
+		) {
+			errors
+		}
+	}
+`);
+
+export async function graphqlVerifyZkLoginSignature({
+	address,
+	bytes,
+	signature,
+	network,
+	fetch: fetchFn,
+}: {
+	address: string;
+	bytes: string;
+	signature: string;
+	network?: 'mainnet' | 'testnet';
+	fetch?: typeof fetch;
+}) {
+	const gqlClient = new SuiGraphQLClient({
+		url:
+			network === 'testnet'
+				? 'https://sui-testnet.mystenlabs.com/graphql'
+				: 'https://sui-mainnet.mystenlabs.com/graphql',
+		fetch: fetchFn,
+	});
+
+	const resp = await gqlClient.query({
+		query: VerifyZkLoginSignatureQuery,
+		variables: {
+			bytes,
+			signature,
+			intent_scope: 'PERSONAL_MESSAGE',
+			author: address,
+		},
+	});
+
+	return resp.data?.verifyZkloginSignature.errors.length === 0;
+}
+
+export function parseSerializedZkLoginSignature(signature: Uint8Array | SerializedSignature) {
+	const bytes = typeof signature === 'string' ? fromB64(signature) : signature;
+
+	if (bytes[0] !== SIGNATURE_SCHEME_TO_FLAG.ZkLogin) {
+		throw new Error('Invalid signature scheme');
+	}
+
+	const signatureBytes = bytes.slice(1);
+	const { inputs, maxEpoch, userSignature } = parseZkLoginSignature(signatureBytes);
+	const { issBase64Details, addressSeed } = inputs;
+	const iss = extractClaimValue<string>(issBase64Details, 'iss');
+	const publicIdentifer = toZkLoginPublicIdentifier(BigInt(addressSeed), iss);
+	const address = publicIdentifer.toSuiAddress();
+	return {
+		serializedSignature: toB64(bytes),
+		signatureScheme: 'ZkLogin' as const,
+		zkLogin: {
+			inputs,
+			maxEpoch,
+			userSignature,
+			iss,
+			address,
+			addressSeed: BigInt(addressSeed),
+		},
+		signature: bytes,
+		publicKey: publicIdentifer.toRawBytes(),
+	};
 }
