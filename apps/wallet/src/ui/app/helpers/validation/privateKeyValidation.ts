bcea50f87802ccb391395f3518d9e1119e8234bf
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2024-04-17 13:21:42 -0400

    wallet: disable hex import for private key

--- apps/wallet/src/ui/app/helpers/validation/privateKeyValidation.ts
+++ apps/wallet/src/ui/app/helpers/validation/privateKeyValidation.ts
@@ -1,8 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { decodeSuiPrivateKey, encodeSuiPrivateKey } from '@mysten/sui.js/cryptography/keypair';
-import { hexToBytes } from '@noble/hashes/utils';
+import { decodeSuiPrivateKey } from '@mysten/sui.js/cryptography/keypair';
 import { z } from 'zod';
 
 export const privateKeyValidation = z
@@ -10,36 +9,13 @@ export const privateKeyValidation = z
 	.trim()
 	.nonempty('Private Key is required.')
 	.transform((privateKey, context) => {
-		if (!privateKey.startsWith('suiprivkey')) {
-			const hexValue = privateKey.startsWith('0x') ? privateKey.slice(2) : privateKey;
-			let privateKeyBytes: Uint8Array | undefined;
-
-			try {
-				privateKeyBytes = hexToBytes(hexValue);
-			} catch (error) {
-				context.addIssue({
-					code: 'custom',
-					message: 'Invalid Private Key, please use a Bech32 encoded 33-byte string.',
-				});
-				return z.NEVER;
-			}
-
-			if (![32, 64].includes(privateKeyBytes.length)) {
-				context.addIssue({
-					code: 'custom',
-					message: 'Hex encoded Private Key must be either 32 or 64 bytes.',
-				});
-				return z.NEVER;
-			}
-
-			return encodeSuiPrivateKey(privateKeyBytes.slice(0, 32), 'ED25519');
-		}
 		try {
 			decodeSuiPrivateKey(privateKey);
 		} catch (error) {
 			context.addIssue({
 				code: 'custom',
-				message: 'Invalid Private Key, please use a Bech32 encoded 33-byte string',
+				message:
+					'Invalid Private Key, please use a Bech32 encoded 33-byte string. Learn more: https://github.com/sui-foundation/sips/blob/main/sips/sip-15.md',
 			});
 			return z.NEVER;
 		}
