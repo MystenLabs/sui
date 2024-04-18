53e4885d5c692b821b9703654f6cfd1e04bc220a
ronny-mysten <118224482+ronny-mysten@users.noreply.github.com>
2024-04-18 14:34:30 -0600

    [docs] Updated trustless swap example name (#17228)
    ## Description 
    
    The "token" in trustless token swap does not accurately convey the
    example. Removing token from the title.
    
    ## Test plan 
    
    👓 
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- docs/content/sidebars/guides.js
+++ docs/content/sidebars/guides.js
@@ -166,15 +166,15 @@ const guides = [
 					'guides/developer/app-examples/tic-tac-toe',
 					{
 						type: 'category',
-						label: 'Trustless Token Swap',
+						label: 'Trustless Swap',
 						link: {
 							type: 'doc',
-							id: 'guides/developer/app-examples/trustless-token-swap',
+							id: 'guides/developer/app-examples/trustless-swap',
 						},
 						items: [
-							'guides/developer/app-examples/trustless-token-swap/backend',
-							'guides/developer/app-examples/trustless-token-swap/indexer-api',
-							'guides/developer/app-examples/trustless-token-swap/frontend',
+							'guides/developer/app-examples/trustless-swap/backend',
+							'guides/developer/app-examples/trustless-swap/indexer-api',
+							'guides/developer/app-examples/trustless-swap/frontend',
 						],
 					},
 					'guides/developer/app-examples/trusted-swap',
