55e16a659e4daabed9c7cfeb2be11d7f579508f6
Michael Hayes <michael.hayes@mystenlabs.com>
2024-04-19 10:40:26 -0700

    PR feedback

--- sdk/typescript/src/utils/suins.ts
+++ sdk/typescript/src/utils/suins.ts
@@ -1,9 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-const SUI_NS_NAME_REGEX = /^(?:(?:[a-z0-9][a-z0-9-]{0,62})\.?)*@[a-z0-9][a-z0-9-]{0,62}$/i;
-const SUI_NS_DOMAIN_REGEX = /^(?:(?:[a-z0-9][a-z0-9-]{0,62})\.?)+.sui$/i;
-const MAX_SUI_NS_NAME_LENGTH = 200;
+const SUI_NS_NAME_REGEX =
+	/^(?:[a-z0-9][a-z0-9-]{0,62}(?:\.[a-z0-9][a-z0-9-]{0,62})*)?@[a-z0-9][a-z0-9-]{0,62}$/i;
+const SUI_NS_DOMAIN_REGEX = /^(?:[a-z0-9][a-z0-9-]{0,62}\.)+sui$/i;
+const MAX_SUI_NS_NAME_LENGTH = 235;
 
 export function isValidSuiNSName(name: string): boolean {
 	if (name.length > MAX_SUI_NS_NAME_LENGTH) {
