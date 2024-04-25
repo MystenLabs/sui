30bb4f09a3581ce6cf1044827a7277e0daac440e
Ronny Roland <ronmilton@mystenlabs.com>
2024-04-24 23:30:10 -0600

    Get named fun/struct

--- docs/site/src/plugins/inject-code/injectLoader.js
+++ docs/site/src/plugins/inject-code/injectLoader.js
@@ -24,8 +24,10 @@ const addCodeInject = function (source) {
     if (matches) {
       matches.forEach((match) => {
         const replacer = new RegExp(match, "g");
-        if (match.startsWith("{@inject: ")) {
-          const injectFileFull = match.substring(10, match.length - 1);
+        const key = "{@inject: ";
+        // {@inject: code.move#function=sword_create}
+        if (match.startsWith(key)) {
+          const injectFileFull = match.substring(key.length, match.length - 1);
           const injectFile = injectFileFull.substring(
             0,
             injectFileFull.indexOf("#") > 0
@@ -63,100 +65,198 @@ const addCodeInject = function (source) {
               injectFileFull.indexOf("#") > 0
                 ? injectFileFull.substring(injectFileFull.indexOf("#"))
                 : null;
+
             if (marker) {
-              const regexStr = `\\/\\/\\s?docs::${marker.trim()}\\b([\\s\\S]*)\\/\\/\\s*docs::\\/\\s?${marker.trim()}\\b`;
-              const closingsStr = `\\/\\/\\s?docs::\\/${marker.trim()}\\b([)};]*)`;
+              const checkBracesBalance = (str) => {
+                const openBraces = str.match(/{/g) || [];
+                const closeBraces = str.match(/}/g) || [];
 
-              const closingRE = new RegExp(closingsStr, "g");
+                return openBraces.length === closeBraces.length;
+              };
+              //const removeLeadingSpaces = (str, numSpaces) => {
 
-              const regex = new RegExp(regexStr, "g");
-              const match = regex.exec(injectFileContent);
-              const closingStr = closingRE.exec(injectFileContent);
-              if (match) {
-                injectFileContent = match[1];
-              }
-              if (closingStr) {
-                const closingTotal = closingStr[1].length;
-                let closingArray = [];
-                for (let i = 0; i < closingTotal; i++) {
-                  const currentChar = closingStr[1][i];
-                  const nextChar = closingStr[1][i + 1];
+              const removeLeadingSpaces = (matchArray, prepend = "") => {
+                const numSpaces = matchArray[1] ? matchArray[1].length - 1 : 0;
+                if (numSpaces === 0) {
+                  return prepend + matchArray[0];
+                }
+                const lines = matchArray[0].split("\n");
 
-                  if (nextChar === ";") {
-                    closingArray.push(currentChar + nextChar);
-                    i++;
+                return [
+                  prepend,
+                  lines.map((line) => line.substring(numSpaces)).join("\n"),
+                ].join("\n");
+              };
+              const funKey = "#function=";
+              const structKey = "#struct=";
+              const funName =
+                marker.indexOf(funKey) >= 0
+                  ? marker
+                      .substring(marker.indexOf(funKey) + funKey.length)
+                      .trim()
+                  : null;
+              const structName =
+                marker.indexOf(structKey) >= 0
+                  ? marker
+                      .substring(marker.indexOf(structKey) + structKey.length)
+                      .trim()
+                  : null;
+              if (funName) {
+                const funStr = `^(\\s*)*(public )?fun ${funName}\\b(?=[^\\w]).*?}\\n(?=\\n)`;
+                const funRE = new RegExp(funStr, "ms");
+                const funMatch = funRE.exec(injectFileContent);
+                if (funMatch) {
+                  const numSpaces = funMatch[1] ? funMatch[1].length - 1 : 0;
+                  let preFun = injectFileContent.substring(
+                    0,
+                    funMatch.index - 1,
+                  );
+                  const lines = preFun.split("\n");
+                  let pre = [];
+                  for (let x = lines.length - 1; x > 0; x--) {
+                    if (lines[x].trim() === "}") {
+                      break;
+                    } else {
+                      pre.push(lines[x].substring(numSpaces));
+                    }
+                  }
+                  preFun = pre.reverse().join("\n");
+                  if (!checkBracesBalance(injectFileContent)) {
+                    injectFileContent =
+                      "Could not find valid function definition. If code is formatted correctly, consider using code comments instead.";
                   } else {
-                    closingArray.push(currentChar);
+                    injectFileContent = removeLeadingSpaces(funMatch, preFun);
                   }
                 }
-                const totClosings = closingArray.length;
-
-                // Process any closing elements added in the closing comment of source code
-                let closing = "";
-                for (let j = 0; j < totClosings; j++) {
-                  let space = "  ".repeat(totClosings - 1 - j);
-                  closing += `\n${space}${closingArray[j]}`;
-                }
-                injectFileContent = injectFileContent.trim() + closing;
-              }
-
-              // Start searching for the pause doc comments
-              // Must be in form: // docs::#idName-pause: optional replacement text
-              // Must be followed at some point by: // docs::#idName-resume
-              // Replace text in the middle with optional replacement text or blankness
-              const pauseStr = `\\/\\/\\s?docs::${marker.trim()}-pause:?(.*)`;
-              const pauseRE = new RegExp(pauseStr, "g");
-              let matches = injectFileContent.match(pauseRE);
-              for (let match in matches) {
-                const test = matches[match].replace(
-                  /[.*+?^${}()|[\]\\]/g,
-                  "\\$&",
-                );
-                let replacer = "";
-                if (matches[match].indexOf("-pause:") > 0) {
-                  replacer = matches[match].substring(
-                    matches[match].indexOf("-pause") + 8,
-                  );
-                }
-
-                const newRE = new RegExp(test);
-                const resumeRE = new RegExp(
-                  `\\/\\/\\s?docs::${marker.trim()}-resume`,
-                );
-                let paused;
-                if (replacer !== "") {
-                  paused = new RegExp(
-                    newRE.source.replace(":?(.*)", "") +
-                      ".*?" +
-                      resumeRE.source,
-                    "gs",
-                  );
+              } else if (structName) {
+                const structStr = `^(\\s*)*(public )?struct \\b${structName}(?=[^\\w]).*?}\\n(?=\\n)`;
+                const structRE = new RegExp(structStr, "ms");
+                const structMatch = structRE.exec(injectFileContent);
+                if (structMatch) {
+                  if (!checkBracesBalance(structMatch[0])) {
+                    injectFileContent =
+                      "Could not find valid struct definition. If code is formatted correctly, consider using code comments instead";
+                  } else {
+                    injectFileContent = removeLeadingSpaces(structMatch);
+                  }
                 } else {
-                  paused = new RegExp(
-                    newRE.source.replace(":?(.*)", "") +
-                      "(?!:).*?" +
-                      resumeRE.source,
-                    "gs",
+                  injectFileContent = "Struct not found.";
+                }
+              } else {
+                const regexStr = `\\/\\/\\s?docs::${marker.trim()}\\b([\\s\\S]*)\\/\\/\\s*docs::\\/\\s?${marker.trim()}\\b`;
+                const closingsStr = `\\/\\/\\s?docs::\\/${marker.trim()}\\b([)};]*)`;
+
+                const closingRE = new RegExp(closingsStr, "g");
+
+                const regex = new RegExp(regexStr, "g");
+                const match = regex.exec(injectFileContent);
+                const closingStr = closingRE.exec(injectFileContent);
+                if (match) {
+                  injectFileContent = match[1];
+                }
+                if (closingStr) {
+                  const closingTotal = closingStr[1].length;
+                  let closingArray = [];
+                  for (let i = 0; i < closingTotal; i++) {
+                    const currentChar = closingStr[1][i];
+                    const nextChar = closingStr[1][i + 1];
+
+                    if (nextChar === ";") {
+                      closingArray.push(currentChar + nextChar);
+                      i++;
+                    } else {
+                      closingArray.push(currentChar);
+                    }
+                  }
+                  const totClosings = closingArray.length;
+
+                  // Process any closing elements added in the closing comment of source code
+                  let closing = "";
+                  for (let j = 0; j < totClosings; j++) {
+                    let space = "  ".repeat(totClosings - 1 - j);
+                    closing += `\n${space}${closingArray[j]}`;
+                  }
+                  injectFileContent = injectFileContent.trim() + closing;
+                }
+
+                // Start searching for the pause doc comments
+                // Must be in form: // docs::#idName-pause: optional replacement text
+                // Must be followed at some point by: // docs::#idName-resume
+                // Replace text in the middle with optional replacement text or blankness
+                const pauseStr = `\\/\\/\\s?docs::${marker.trim()}-pause:?(.*)`;
+                const pauseRE = new RegExp(pauseStr, "g");
+                let matches = injectFileContent.match(pauseRE);
+                for (let match in matches) {
+                  const test = matches[match].replace(
+                    /[.*+?^${}()|[\]\\]/g,
+                    "\\$&",
+                  );
+                  let replacer = "";
+                  if (matches[match].indexOf("-pause:") > 0) {
+                    replacer = matches[match].substring(
+                      matches[match].indexOf("-pause") + 8,
+                    );
+                  }
+
+                  const newRE = new RegExp(test);
+                  const resumeRE = new RegExp(
+                    `\\/\\/\\s?docs::${marker.trim()}-resume`,
+                  );
+                  let paused;
+                  if (replacer !== "") {
+                    paused = new RegExp(
+                      newRE.source.replace(":?(.*)", "") +
+                        ".*?" +
+                        resumeRE.source,
+                      "gs",
+                    );
+                  } else {
+                    paused = new RegExp(
+                      newRE.source.replace(":?(.*)", "") +
+                        "(?!:).*?" +
+                        resumeRE.source,
+                      "gs",
+                    );
+                  }
+                  injectFileContent = injectFileContent.replace(
+                    paused,
+                    replacer,
                   );
                 }
-                injectFileContent = injectFileContent.replace(paused, replacer);
               }
+
+              // Replace all the //docs:: lines in code
+              injectFileContent = injectFileContent.replace(
+                /^\s*\/\/\s*docs::\/?.*\r?$\n?/gm,
+                "",
+              );
+
+              const trimContent = (content) => {
+                let arr = content.split("\n");
+                let start = 0;
+                let end = arr.length;
+
+                while (start < end && arr[start] === "") {
+                  start++;
+                }
+
+                while (end > start && arr[end - 1] === "") {
+                  end--;
+                }
+
+                return arr.slice(start, end).join("\n");
+              };
+
+              injectFileContent = trimContent(injectFileContent);
+              injectFileContent = `\`\`\`${language} title=${injectFile}\n${injectFileContent}\n\`\`\``;
+
+              res = res.replace(replacer, injectFileContent);
+              res = addMarkdownIncludes(res);
             }
-
-            // Replace all the //docs:: lines in code
-            injectFileContent = injectFileContent.replace(
-              /^\s*\/\/\s*docs::\/?.*\r?$\n?/gm,
-              "",
-            );
-
-            injectFileContent = `\`\`\`${language} title=${injectFile}\n${injectFileContent}\n\`\`\``;
-
-            res = res.replace(replacer, injectFileContent);
-            res = addMarkdownIncludes(res);
           } else {
             res = res.replace(
               replacer,
-              `\n> code to inject not found: ${injectFile} --> ${fullPath}\n`,
+              `\n> Code to inject not found: ${injectFile} --> ${fullPath}\n`,
             );
           }
         } else {
