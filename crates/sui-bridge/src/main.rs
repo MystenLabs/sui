fea58deaea4ce7452bd34ea5b2d84b5366330a57
Brandon Williams <brandon@mystenlabs.com>
2024-05-02 11:42:58 -0500

    sui-bridge: use bin-version

--- crates/sui-bridge/src/main.rs
+++ crates/sui-bridge/src/main.rs
@@ -12,23 +12,8 @@ use sui_bridge::node::run_bridge_node;
 use sui_config::Config;
 use tracing::info;
 
-// TODO consolidate this with sui-node/src/main.rs, but where to put it?
-const GIT_REVISION: &str = {
-    if let Some(revision) = option_env!("GIT_REVISION") {
-        revision
-    } else {
-        let version = git_version::git_version!(
-            args = ["--always", "--abbrev=12", "--dirty", "--exclude", "*"],
-            fallback = ""
-        );
-
-        if version.is_empty() {
-            panic!("unable to query git revision");
-        }
-        version
-    }
-};
-const VERSION: &str = const_str::concat!(env!("CARGO_PKG_VERSION"), "-", GIT_REVISION);
+// Define the `GIT_REVISION` and `VERSION` consts
+bin_version::bin_version!();
 
 #[derive(Parser)]
 #[clap(rename_all = "kebab-case")]
