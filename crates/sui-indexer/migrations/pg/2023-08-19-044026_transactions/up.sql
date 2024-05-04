716d66156c2d7d00228414542cbb4d737a9242b6
Will Yang <willyang@mystenlabs.com>
2024-05-03 17:27:43 -0700

    cleanup txs tables part 1

--- crates/sui-indexer/migrations/pg/2023-08-19-044026_transactions/up.sql
+++ crates/sui-indexer/migrations/pg/2023-08-19-044026_transactions/up.sql
@@ -16,12 +16,10 @@ CREATE TABLE transactions (
     -- SystemTransaction/ProgrammableTransaction. See types.rs
     transaction_kind            smallint     NOT NULL,
     -- number of successful commands in this transaction, bound by number of command
-    -- in a programmaable transaction.
+    -- in a programmable transaction.
     success_command_count       smallint     NOT NULL,
     PRIMARY KEY (tx_sequence_number, checkpoint_sequence_number)
 ) PARTITION BY RANGE (checkpoint_sequence_number);
 CREATE TABLE transactions_partition_0 PARTITION OF transactions FOR VALUES FROM (0) TO (MAXVALUE);
-CREATE INDEX transactions_transaction_digest ON transactions (transaction_digest);
 CREATE INDEX transactions_checkpoint_sequence_number ON transactions (checkpoint_sequence_number);
--- only create index for system transactions (0). See types.rs
-CREATE INDEX transactions_transaction_kind ON transactions (transaction_kind) WHERE transaction_kind = 0;
+CREATE INDEX transactions_transaction_kind ON transactions (transaction_kind, tx_sequence_number, checkpoint_sequence_number);
