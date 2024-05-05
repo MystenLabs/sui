b5b192625b7836ea8a30bd415a7f1ab840fc186b
Vu Viet Tai <vuviettai@gmail.com>
2024-05-05 17:08:05 +0700

    Update Mysten Labs license

--- crates/sui-core/src/scalaris/mod.rs
+++ crates/sui-core/src/scalaris/mod.rs
@@ -1,4 +1,5 @@
-// Copyright (c) 2024, Scalaris.
+// Copyright (c) 2021, Facebook, Inc. and its affiliates
+// Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
 mod consensus_output_api;
