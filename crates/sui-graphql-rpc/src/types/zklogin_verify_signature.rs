444d0bd4a98b6bcc61e94501f9b10faa716c11b0
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2024-05-13 10:45:10 -0400

    fix: use derived author for zklogin verify signature

--- crates/sui-graphql-rpc/src/types/zklogin_verify_signature.rs
+++ crates/sui-graphql-rpc/src/types/zklogin_verify_signature.rs
@@ -125,14 +125,11 @@ pub(crate) async fn verify_zklogin_signature(
             let tx_data: TransactionData = bcs::from_bytes(&bytes)
                 .map_err(|_| Error::Client("Invalid tx data bytes".to_string()))?;
             let intent_msg = IntentMessage::new(Intent::sui_transaction(), tx_data.clone());
-            let tx_sender = tx_data.execution_parts().1;
-            if tx_sender != author.into() {
-                return Err(Error::Client("Tx sender mismatch author".to_string()));
-            }
-            match zklogin_sig.verify_authenticator(
+            let sig = GenericSignature::ZkLoginAuthenticator(zklogin_sig);
+            match sig.verify_authenticator(
                 &intent_msg,
-                tx_sender,
-                Some(curr_epoch),
+                author.into(),
+                curr_epoch,
                 &verify_params,
             ) {
                 Ok(_) => Ok(ZkLoginVerifyResult {
