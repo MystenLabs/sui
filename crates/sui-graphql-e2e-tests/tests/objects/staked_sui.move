8037531da7f178e97adfc86c6cc18f843211c8b5
Zhe Wu <zhe.wu@mystenlabs.com>
2024-05-19 12:20:49 -0700

    Update graphql e2e tests to protocol version 45. Create a script that automatically update all tests protocol version

--- crates/sui-graphql-e2e-tests/tests/objects/staked_sui.move
+++ crates/sui-graphql-e2e-tests/tests/objects/staked_sui.move
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-//# init --protocol-version 39 --simulator --accounts C
+//# init --protocol-version 45 --simulator --accounts C
 
 //# run-graphql
 { # init --protocol-version 39ial query yields only the validator's stake
---
+++ scripts/update-graphql-e2e-test-protocol-version.py
@@ -0,0 +1,46 @@
+import os
+import re
+import argparse
+
+def replace_protocol_version_in_file(file_path, old_version, new_version, yes_to_all):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    updated_content = re.sub(
+        f"init --protocol-version {old_version} --",
+        f"init --protocol-version {new_version} --",
+        content
+    )
+
+    if content != updated_content:
+        print(f"Found 'init --protocol-version {old_version}' in {file_path}")
+        if yes_to_all :
+           with open(file_path, 'w') as file:
+               file.write(updated_content)
+           print(f"Updated {file_path}")
+        else:
+            confirm = input(f"Do you want to replace '--protocol-version {old_version}' with '--protocol-version {new_version}'? (yes/no): ").strip().lower()
+            if confirm == 'yes' or confirm == 'y':
+                with open(file_path, 'w') as file:
+                    file.write(updated_content)
+                print(f"Updated {file_path}")
+            else:
+                print(f"Skipped {file_path}")
+
+def replace_protocol_version_in_repo(repo_path, old_version, new_version, yes_to_all):
+    for root, dirs, files in os.walk(repo_path):
+        for file in files:
+            if file.endswith('.move'):
+                file_path = os.path.join(root, file)
+                replace_protocol_version_in_file(file_path, old_version, new_version, yes_to_all)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Replace protocol version in .move files.')
+    parser.add_argument('--yes-to-all', action='store_true', help='Automatically say "yes to all" for all changes')
+    args = parser.parse_args()
+
+    repo_path = os.getcwd()
+    old_version = input("Enter the old protocol version (XX): ")
+    new_version = input("Enter the new protocol version (YY): ")
+    replace_protocol_version_in_repo(repo_path, old_version, new_version, args.yes_to_all)
+    print(f"Next step. Running `env UB=1 cargo nextest run --features pg_integration` in `crates/sui-graphql-e2e-tests` to update all the snapshots.")
