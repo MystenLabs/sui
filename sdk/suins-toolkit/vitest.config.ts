fa8f122981fd9d0bf72cfcdb7c672a7225613029
Michael Hayes <michael.hayes@mystenlabs.com>
2024-05-21 10:08:50 -0700

    Fix alias in vitest configs

--- sdk/suins-toolkit/vitest.config.ts
+++ sdk/suins-toolkit/vitest.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
 		hookTimeout: 1000000,
 		testTimeout: 1000000,
 	},
+	resolve: {
+		alias: {
+			'@mysten/bcs': new URL('../bcs/src', import.meta.url).pathname,
+			'@mysten/sui': new URL('../typescript/src', import.meta.url).pathname,
+		},
+	},
 });
