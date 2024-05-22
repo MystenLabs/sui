e6c65fc3dd7ece4725c81432f73eaf52d259f62d
phoenix <51927076+phoenix-o@users.noreply.github.com>
2024-05-22 08:22:32 -0400

    [data ingestion] migrate analytics to data ingestion framework (#17731)

--- crates/sui-analytics-indexer/src/handlers/event_handler.rs
+++ crates/sui-analytics-indexer/src/handlers/event_handler.rs
@@ -6,12 +6,13 @@ use fastcrypto::encoding::{Base64, Encoding};
 use sui_types::SYSTEM_PACKAGE_ADDRESSES;
 
 use std::path::Path;
+use sui_data_ingestion_core::Worker;
+use tokio::sync::Mutex;
 
 use crate::handlers::{get_move_struct, AnalyticsHandler};
 use crate::package_store::{LocalDBPackageStore, PackageCache};
 use crate::tables::EventEntry;
 use crate::FileType;
-use sui_indexer::framework::Handler;
 use sui_json_rpc_types::SuiMoveStruct;
 use sui_package_resolver::Resolver;
 use sui_rest_api::CheckpointData;
@@ -20,25 +21,27 @@ use sui_types::effects::TransactionEvents;
 use sui_types::event::Event;
 
 pub struct EventHandler {
+    state: Mutex<State>,
+}
+
+struct State {
     events: Vec<EventEntry>,
     package_store: LocalDBPackageStore,
     resolver: Resolver<PackageCache>,
 }
 
 #[async_trait::async_trait]
-impl Handler for EventHandler {
-    fn name(&self) -> &str {
-        "event"
-    }
-    async fn process_checkpoint(&mut self, checkpoint_data: &CheckpointData) -> Result<()> {
+impl Worker for EventHandler {
+    async fn process_checkpoint(&self, checkpoint_data: CheckpointData) -> Result<()> {
         let CheckpointData {
             checkpoint_summary,
             transactions: checkpoint_transactions,
             ..
         } = checkpoint_data;
+        let mut state = self.state.lock().await;
         for checkpoint_transaction in checkpoint_transactions {
             for object in checkpoint_transaction.output_objects.iter() {
-                self.package_store.update(object)?;
+                state.package_store.update(object)?;
             }
             if let Some(events) = &checkpoint_transaction.events {
                 self.process_events(
@@ -47,11 +50,13 @@ impl Handler for EventHandler {
                     checkpoint_transaction.transaction.digest(),
                     checkpoint_summary.timestamp_ms,
                     events,
+                    &mut state,
                 )
                 .await?;
             }
             if checkpoint_summary.end_of_epoch_data.is_some() {
-                self.resolver
+                state
+                    .resolver
                     .package_store()
                     .evict(SYSTEM_PACKAGE_ADDRESSES.iter().copied());
             }
@@ -62,33 +67,42 @@ impl Handler for EventHandler {
 
 #[async_trait::async_trait]
 impl AnalyticsHandler<EventEntry> for EventHandler {
-    fn read(&mut self) -> Result<Vec<EventEntry>> {
-        let cloned = self.events.clone();
-        self.events.clear();
+    async fn read(&self) -> Result<Vec<EventEntry>> {
+        let mut state = self.state.lock().await;
+        let cloned = state.events.clone();
+        state.events.clear();
         Ok(cloned)
     }
 
     fn file_type(&self) -> Result<FileType> {
         Ok(FileType::Event)
     }
+
+    fn name(&self) -> &str {
+        "event"
+    }
 }
 
 impl EventHandler {
     pub fn new(store_path: &Path, rest_uri: &str) -> Self {
         let package_store = LocalDBPackageStore::new(&store_path.join("event"), rest_uri);
-        EventHandler {
+        let state = State {
             events: vec![],
             package_store: package_store.clone(),
             resolver: Resolver::new(PackageCache::new(package_store)),
+        };
+        Self {
+            state: Mutex::new(state),
         }
     }
     async fn process_events(
-        &mut self,
+        &self,
         epoch: u64,
         checkpoint: u64,
         digest: &TransactionDigest,
         timestamp_ms: u64,
         events: &TransactionEvents,
+        state: &mut State,
     ) -> Result<()> {
         for (idx, event) in events.data.iter().enumerate() {
             let Event {
@@ -98,7 +112,7 @@ impl EventHandler {
                 type_,
                 contents,
             } = event;
-            let move_struct = get_move_struct(type_, contents, &self.resolver).await?;
+            let move_struct = get_move_struct(type_, contents, &state.resolver).await?;
             let (_struct_tag, sui_move_struct) = match move_struct.into() {
                 SuiMoveStruct::WithTypes { type_, fields } => {
                     (type_, SuiMoveStruct::WithFields(fields))
@@ -120,7 +134,7 @@ impl EventHandler {
                 event_json: event_json.to_string(),
             };
 
-            self.events.push(entry);
+            state.events.push(entry);
         }
         Ok(())
     }
