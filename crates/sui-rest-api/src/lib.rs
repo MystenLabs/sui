bb8e5092590bdeef24a2e7d32fd6b930f226e2a9
Brandon Williams <brandon@mystenlabs.com>
2024-05-26 13:19:22 -0500

    rest: get and list transactions endpoints

--- crates/sui-rest-api/src/lib.rs
+++ crates/sui-rest-api/src/lib.rs
@@ -37,6 +37,13 @@ pub const TEXT_PLAIN_UTF_8: &str = "text/plain; charset=utf-8";
 pub const APPLICATION_BCS: &str = "application/bcs";
 pub const APPLICATION_JSON: &str = "application/json";
 
+#[derive(Debug, Copy, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Direction {
+    Ascending,
+    Descending,
+}
+
 #[derive(Clone)]
 pub struct RestService {
     reader: StateReader,
@@ -91,6 +98,14 @@ impl RestService {
 
         Router::new()
             .route("/", get(info::node_info))
+            .route(
+                transactions::GET_TRANSACTION_PATH,
+                get(transactions::get_transaction),
+            )
+            .route(
+                transactions::LIST_TRANSACTIONS_PATH,
+                get(transactions::list_transactions),
+            )
             .route(
                 committee::GET_LATEST_COMMITTEE_PATH,
                 get(committee::get_latest_committee),
