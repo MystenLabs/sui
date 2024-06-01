7a56ae0138df6db51421a60fba667f0c54cdbbc5
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2024-05-31 18:17:01 -0700

    [GraphQL] Fix APY for a validator (#17701)
    ## Description 
    
    This PR fixes the APY for a validator and ensures that historical APYs
    can be computed. It also reverts some changes in the `governance_api`,
    particularly moves `exchange_rates` as a free function that can be
    called from the GraphQL crate.
    
    At system start, the task to trigger the exchange rates cache is
    executed, thus until the next trigger on epoch change, the apys can be
    fetched pretty fast (~2-3s for the standard 5 items per page).
    
    ## Test plan 
    Updated test. Manual tests and queries.
    
    Response time: ~1s
    ```graphql
    {
      epoch {
        epochId
        referenceGasPrice
        validatorSet {
          totalStake
          inactivePoolsSize
          activeValidators(first: 5) {
            nodes {
              name
              apy
            }
          }
        }
      }
    }
    ```
    
    ```json
    {
      "data": {
        "epoch": {
          "epochId": 405,
          "referenceGasPrice": "751",
          "validatorSet": {
            "totalStake": "8163113083622628156",
            "inactivePoolsSize": 4,
            "activeValidators": {
              "nodes": [
                {
                  "name": "ComingChat",
                  "apy": 310
                },
                {
                  "name": "InfStones",
                  "apy": 302
                },
                {
                  "name": "Gaucho",
                  "apy": 310
                },
                {
                  "name": "Astro-Stakers",
                  "apy": 310
                },
                {
                  "name": "H2O Nodes",
                  "apy": 310
                }
              ]
            }
          }
        }
      }
    }
    ```
    
    Response time for this query: ~2s
    ```graphql 
    {
      epoch(id: 127){
        epochId
        referenceGasPrice
        validatorSet {
          totalStake
          inactivePoolsSize
          activeValidators {
            nodes {
              name
              apy
            }
          }
        }
      }
    }
    ```
    
    ```json
    {
      "data": {
        "epoch": {
          "epochId": 127,
          "referenceGasPrice": "765",
          "validatorSet": {
            "totalStake": "7437800674887300328",
            "inactivePoolsSize": 2,
            "activeValidators": {
              "nodes": [
                {
                  "name": "ComingChat",
                  "apy": 480
                },
                {
                  "name": "InfStones",
                  "apy": 471
                },
                {
                  "name": "Gaucho",
                  "apy": 471
                },
                {
                  "name": "Astro-Stakers",
                  "apy": 485
                },
                {
                  "name": "H2O Nodes",
                  "apy": 471
                }
              ]
            }
          }
        }
      }
    }
    ```
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

---
+++ crates/sui-graphql-rpc/src/server/exchange_rates_task.rs
@@ -0,0 +1,59 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use sui_indexer::apis::{governance_api::exchange_rates, GovernanceReadApi};
+use tokio::sync::watch;
+use tokio_util::sync::CancellationToken;
+use tracing::{error, info};
+
+use crate::data::pg::PgExecutor;
+
+/// Background task for kicking on epoch change the exchange rates function on the indexer, which
+/// caches the ValidatorExchangeRates that are needed for computing APYs.
+pub(crate) struct TriggerExchangeRatesTask {
+    cancel: CancellationToken,
+    db: PgExecutor,
+    epoch_rx: watch::Receiver<u64>,
+}
+
+impl TriggerExchangeRatesTask {
+    pub(crate) fn new(
+        db: PgExecutor,
+        epoch_rx: watch::Receiver<u64>,
+        cancel: CancellationToken,
+    ) -> Self {
+        Self {
+            db,
+            epoch_rx,
+            cancel,
+        }
+    }
+
+    pub(crate) async fn run(&mut self) {
+        loop {
+            tokio::select! {
+                _ = self.cancel.cancelled() => {
+                    info!("Shutdown signal received, terminating trigger exchange rates task");
+                    return;
+                }
+
+                _ = self.epoch_rx.changed() => {
+                    info!("Detected epoch boundary, triggering call to exchange rates");
+                    let latest_sui_sytem_state = self.db.inner.spawn_blocking(move |this|
+                        this.get_latest_sui_system_state()
+                    ).await.map_err(|_| error!("Failed to fetch latest Sui system state"));
+
+                    if let Ok(latest_sui_sytem_state) = latest_sui_sytem_state {
+                        let db = self.db.clone();
+                        let governance_api = GovernanceReadApi::new(db.inner) ;
+                        exchange_rates(&governance_api, &latest_sui_sytem_state)
+                            .await
+                            .map_err(|e| error!("Failed to fetch exchange rates: {:?}", e))
+                            .ok();
+                        info!("Finished fetching exchange rates");
+                    }
+                }
+            }
+        }
+    }
+}
