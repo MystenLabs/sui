8612ad96230fd09fbfa2909629d920f866bbda30
Brandon Williams <brandon@mystenlabs.com>
2024-06-08 14:06:21 -0500

    types: add CheckpointTransaction::removed_objects
    Add removed_objects method for providing an efficient iterator over
    objects that were either deleted for wrapped.

--- crates/sui-types/src/full_checkpoint_content.rs
+++ crates/sui-types/src/full_checkpoint_content.rs
@@ -1,10 +1,13 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::effects::{TransactionEffects, TransactionEvents};
+use crate::effects::{
+    IDOperation, ObjectIn, ObjectOut, TransactionEffects, TransactionEvents,
+};
 use crate::messages_checkpoint::{CertifiedCheckpointSummary, CheckpointContents};
 use crate::object::Object;
 use crate::transaction::Transaction;
+use itertools::Either;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -50,4 +53,58 @@ pub struct CheckpointTransaction {
     pub input_objects: Vec<Object>,
     /// The state of all output objects created or mutated by this transaction.
     pub output_objects: Vec<Object>,
+}
+
+impl CheckpointTransaction {
+    // provide an iterator over all deleted or wrapped objects in this transaction
+    pub fn removed_objects(&self) -> impl Iterator<Item = &Object> {
+        // Iterator over id and versions for all deleted or wrapped objects
+        match &self.effects {
+            TransactionEffects::V1(v1) => Either::Left(
+                // Effects v1 has delted and wrapped objects versions as the "new" version, not the
+                // old one that was actually removed. So we need to take these and then look them
+                // up in the `modified_at_versions`.
+                v1.deleted().iter().chain(v1.wrapped()).map(|(id, _, _)| {
+                    // lookup the old version for mutated objects
+                    let (_, old_version) = v1
+                        .modified_at_versions()
+                        .iter()
+                        .find(|(oid, _old_version)| oid == id)
+                        .expect("deleted/wrapped object must have entry in 'modified_at_versions'");
+                    (id, old_version)
+                }),
+            ),
+            TransactionEffects::V2(v2) => {
+                Either::Right(v2.changed_objects().iter().filter_map(|(id, change)| {
+                    match (
+                        &change.input_state,
+                        &change.output_state,
+                        &change.id_operation,
+                    ) {
+                        // Deleted Objects
+                        (
+                            ObjectIn::Exist(((version, _d), _o)),
+                            ObjectOut::NotExist,
+                            IDOperation::Deleted,
+                        ) => Some((id, version)),
+
+                        // Wrapped Objects
+                        (
+                            ObjectIn::Exist(((version, _), _)),
+                            ObjectOut::NotExist,
+                            IDOperation::None,
+                        ) => Some((id, version)),
+                        _ => None,
+                    }
+                }))
+            }
+        }
+        // Use id and version to lookup in input Objects
+        .map(|(id, version)| {
+            self.input_objects
+                .iter()
+                .find(|o| &o.id() == id && &o.version() == version)
+                .expect("all removed objects should show up in input objects")
+        })
+    }
 }
