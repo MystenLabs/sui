3cd2ea40d31f5a9842aa053a3bdb40df4d63198c
Dario Russi <113150618+dariorussi@users.noreply.github.com>
2024-06-14 10:30:56 +0200

    Remove events from the Move VM (#18235)
    ## Description 
    
    Remove code related to events
    
    ## Test plan 
    
    existing tests
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- external-crates/move/crates/move-unit-test/src/test_runner.rs
+++ external-crates/move/crates/move-unit-test/src/test_runner.rs
@@ -290,7 +290,7 @@ impl SharedTestingConfig {
                 .into(),
         );
         match session.finish_with_extensions().0 {
-            Ok((cs, _, extensions)) => (Ok(cs), Ok(extensions), return_result, test_run_info),
+            Ok((cs, extensions)) => (Ok(cs), Ok(extensions), return_result, test_run_info),
             Err(err) => (Err(err.clone()), Err(err), return_result, test_run_info),
         }
     }
