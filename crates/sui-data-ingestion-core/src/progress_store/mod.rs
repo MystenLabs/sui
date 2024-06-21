3dc3237222c41da9e47108184abc97dcaded4d4b
phoenix <alexo@mystenlabs.com>
2024-06-21 12:02:48 -0400

    [data ingestion] only update watermark if you made more progress than other hosts

--- crates/sui-data-ingestion-core/src/progress_store/mod.rs
+++ crates/sui-data-ingestion-core/src/progress_store/mod.rs
@@ -38,10 +38,12 @@ impl<P: ProgressStore> ProgressStore for ProgressStoreWrapper<P> {
         task_name: String,
         checkpoint_number: CheckpointSequenceNumber,
     ) -> Result<()> {
-        self.progress_store
-            .save(task_name.clone(), checkpoint_number)
-            .await?;
-        self.pending_state.insert(task_name, checkpoint_number);
+        if checkpoint_number > self.load(task_name.clone()).await? {
+            self.progress_store
+                .save(task_name.clone(), checkpoint_number)
+                .await?;
+            self.pending_state.insert(task_name, checkpoint_number);
+        }
         Ok(())
     }
 }
