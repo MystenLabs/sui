64b2cd4bc2e815a18d3d0772e705edea2e656d3f
Ashok Menon <ashok@mystenlabs.com>
2024-06-24 12:25:14 +0100

    [pick #18347][GraphQL] Fix bug where config types "zero out" fields not in TOML
    ## Description
    
    When a field is labeled with `#[serde(default)]` it means that if that
    field is not present, it is populated using the `Default` impl for the
    field's type:
    
    ```rust
    #[derive(Serialize, Deserialize, Clone, Debug)]
    struct Foo {
        #[serde(default)]
        bar: Bar,
    }
    
    Foo { bar: Bar::default() }
    ```
    
    This is not the behaviour we want, however. We want fields that have not
    been supplied to be populated with their value from the `Default` impl
    of the type they are part of:
    
    ```rust
    Foo { bar: Foo::default().bar }
    ```
    
    Implementing this manually requires a lot of boilerplate -- each field
    needs to have an associated function that returns its default value:
    
    ```rust
    #[derive(Serialize, Deserialize, Clone, Debug)]
    struct Foo {
        #[serde(default = "Foo::__default_bar")]
        bar: Bar,
    }
    ```
    
    So this PR introduces an attribute proc macro to perform this
    transformation:
    
    ```rust
    #[GraphQLConfig]
    struct Foo {
        bar: Bar,
    }
    ```
    
    It also performs some other related clean-ups:
    
    - Moves default values inline into the respective `Default` impls, to
    prevent them being used in isolation.
    - Move the documentation for what the field is onto the struct
    definition, and the documentation for how the default was chosen onto
    the `Default` impl.
    - Moved the implementation of `Display` for `Version`.
    - Got rid of `ConnectionConfig::ci_integration_test_cfg` as it is the
    same as its `Default` impl now.
    - Improved various docs about what various configs are.
    
    ## Test plan
    
    Added a test for reading a `ServiceConfig` from an incomplete/partial
    TOML:
    
    ```sh
    sui-graphql-rpc$ cargo nextest run -- read_partial_in_service_config
    ```
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol:
    - [ ] Nodes (Validators and Full nodes):
    - [ ] Indexer:
    - [ ] JSON-RPC:
    - [x] GraphQL: Fix a bug where starting the service using a config with
    not all fields set would result in the unset fields being zeroed out
    rather than taking their usual default values (as would happen if no
    config had been explicitly supplied).
    - [ ] CLI:
    - [ ] Rust SDK:

--- crates/sui-graphql-rpc/src/data/pg.rs
+++ crates/sui-graphql-rpc/src/data/pg.rs
@@ -183,7 +183,7 @@ mod query_cost {
 #[cfg(all(test, feature = "pg_integration"))]
 mod tests {
     use super::*;
-    use crate::config::DEFAULT_SERVER_DB_URL;
+    use crate::config::ConnectionConfig;
     use diesel::QueryDsl;
     use sui_framework::BuiltInFramework;
     use sui_indexer::{
@@ -193,7 +193,8 @@ mod tests {
 
     #[test]
     fn test_query_cost() {
-        let pool = new_pg_connection_pool_impl(DEFAULT_SERVER_DB_URL, Some(5)).unwrap();
+        let connection_config = ConnectionConfig::default();
+        let pool = new_pg_connection_pool_impl(&connection_config.db_url, Some(5)).unwrap();
         let mut conn = get_pg_pool_connection(&pool).unwrap();
         reset_database(&mut conn, /* drop_all */ true, /* use_v2 */ true).unwrap();
 
--- crates/sui-graphql-rpc/tests/examples_validation_tests.rs
+++ crates/sui-graphql-rpc/tests/examples_validation_tests.rs
@@ -107,10 +107,8 @@ mod tests {
 
         sim.create_checkpoint();
 
-        let connection_config = ConnectionConfig::ci_integration_test_cfg();
-
         let cluster = sui_graphql_rpc::test_infra::cluster::serve_executor(
-            connection_config,
+            ConnectionConfig::default(),
             DEFAULT_INTERNAL_DATA_SOURCE_PORT,
             Arc::new(sim),
             None,
@@ -172,10 +170,8 @@ mod tests {
 
         sim.create_checkpoint();
 
-        let connection_config = ConnectionConfig::ci_integration_test_cfg();
-
         let cluster = sui_graphql_rpc::test_infra::cluster::serve_executor(
-            connection_config,
+            ConnectionConfig::default(),
             DEFAULT_INTERNAL_DATA_SOURCE_PORT,
             Arc::new(sim),
             None,
