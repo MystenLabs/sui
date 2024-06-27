56f9c65db3d3f9927ee0b043071eb37546f6e342
Rijnard van Tonder <rvantonder@gmail.com>
2024-06-27 10:55:09 -0700

    move: support automated address management (#17995)
    ## Description 
    
    This PR adds automated address management support for `sui client
    publish` and `sui client upgrade`. The `chain_id` is resolved early in
    sui commands and propagated down where published ID resolution is
    needed.
    
    High-level:
    
    - If a package contains a `Move.lock` that contains info to support a
    package upgrade, then that is used, and a user does not have to (nor
    should they) maintain a `published-at` address in their `Move.toml`.
    
    - If a package does _not_ have any `Move.lock` information to support a
    package upgrade, we will still fallback to `Move.toml` as we do today.
    Nothing changes.
    
    - If a package has both a `Move.lock` and `Move.toml`, then the
    `Move.lock` will be used (the user is free to delete `published-at` in
    their `Move.toml`). Caveat: If a published address is in both of these
    files, they must be the same (we will raise a `Conflict` error message
    otherwise).
     
    - Note: docs to follow and merge at the same time as this PR,
    particularly [this section on
    upgrades](https://docs.sui.io/concepts/sui-move-concepts/packages/upgrade#example)
    
    - Note after this PR it is still required to set `[addresses]` to `0x0`
    in `Move.toml`. This requirement will be removed subsequently (which
    also requires setting to `0x0` still). I didn't want to meddle with that
    logic all at once.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    We cover three cases:
    
    - Upgrade with `Move.toml` only (existing package upgrade test)
    - Upgrade with `Move.lock` only (extension of existing package
    management test)
    - Upgrade with `Move.lock` and `Move.toml` with conflicting addresses
    error condition (new test)
    
    - Existing e2e tests pass (and SDK tests now resolve chain ID for
    publish commands)
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [x] CLI: Running `sui client publish` or `sui client upgrade` will now
    cause package addresses to be tracked in the `Move.lock` file. This
    removes the need to manually record or edit `published-at` addresses in
    the `Move.toml` file going forward. See docs PR
    https://github.com/MystenLabs/sui/pull/18433 for full details.
    - [ ] Rust SDK:

--- crates/sui-package-management/src/lib.rs
+++ crates/sui-package-management/src/lib.rs
@@ -2,15 +2,35 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::{bail, Context};
+use std::fs::File;
 use std::path::PathBuf;
+use std::str::FromStr;
 
-use move_package::lock_file::{self, LockFile};
+use move_package::{
+    lock_file::{self, schema::ManagedPackage, LockFile},
+    resolution::resolution_graph::Package,
+    source_package::layout::SourcePackageLayout,
+};
+use move_symbol_pool::Symbol;
 use sui_json_rpc_types::{get_new_package_obj_from_response, SuiTransactionBlockResponse};
 use sui_sdk::wallet_context::WalletContext;
+use sui_types::base_types::ObjectID;
+
+const PUBLISHED_AT_MANIFEST_FIELD: &str = "published-at";
 
 pub enum LockCommand {
     Publish,
     Upgrade,
+}
+
+#[derive(Debug, Clone)]
+pub enum PublishedAtError {
+    Invalid(String),
+    NotPresent,
+    Conflict {
+        id_lock: String,
+        id_manifest: String,
+    },
 }
 
 /// Update the `Move.lock` file with automated address management info.
@@ -72,4 +92,67 @@ pub async fn update_lock_file(
     }?;
     lock.commit(lock_file)?;
     Ok(())
+}
+
+/// Find the published on-chain ID in the `Move.lock` or `Move.toml` file.
+/// A chain ID of `None` means that we will only try to resolve a published ID from the Move.toml.
+/// The published ID is resolved from the `Move.toml` if the Move.lock does not exist.
+/// Else, we resolve from the `Move.lock`, where addresses are automatically
+/// managed. If conflicting IDs are found in the `Move.lock` vs. `Move.toml`, a
+/// "Conflict" error message returns.
+pub fn resolve_published_id(
+    package: &Package,
+    chain_id: Option<String>,
+) -> Result<ObjectID, PublishedAtError> {
+    // Look up a valid `published-at` in the `Move.toml` first, which we'll
+    // return if the Move.lock does not manage addresses.
+    let published_id_in_manifest = match published_at_property(package) {
+        Ok(v) => Some(v),
+        Err(PublishedAtError::NotPresent) => None,
+        Err(e) => return Err(e), // An existing but invalid `published-at` in `Move.toml` should fail early.
+    };
+
+    let lock = package.package_path.join(SourcePackageLayout::Lock.path());
+    let Ok(mut lock_file) = File::open(lock.clone()) else {
+        return match published_id_in_manifest {
+            Some(v) => {
+                ObjectID::from_str(v.as_str()).map_err(|_| PublishedAtError::Invalid(v.to_owned()))
+            }
+            None => Err(PublishedAtError::NotPresent),
+        };
+    };
+    let managed_packages = ManagedPackage::read(&mut lock_file).ok();
+    // Find the environment and ManagedPackage data for this chain_id.
+    let env_for_chain_id = managed_packages
+        .and_then(|m| {
+            let chain_id = chain_id.as_ref()?;
+            m.into_iter().find(|(_, v)| v.chain_id == *chain_id)
+        })
+        .map(|(k, v)| (k, v.latest_published_id));
+
+    let package_id = match (env_for_chain_id, published_id_in_manifest) {
+        (Some((_env, id_lock)), Some(id_manifest)) if id_lock != id_manifest => {
+            return Err(PublishedAtError::Conflict {
+                id_lock,
+                id_manifest,
+            })
+        }
+        (Some((_, id_lock)), _) => id_lock,
+        (None, Some(id_manifest)) => id_manifest, /* No info in Move.lock: Fall back to manifest */
+        _ => return Err(PublishedAtError::NotPresent), /* Neither in Move.toml nor Move.lock */
+    };
+    ObjectID::from_str(package_id.as_str())
+        .map_err(|_| PublishedAtError::Invalid(package_id.to_owned()))
+}
+
+fn published_at_property(package: &Package) -> Result<String, PublishedAtError> {
+    let Some(value) = package
+        .source_package
+        .package
+        .custom_properties
+        .get(&Symbol::from(PUBLISHED_AT_MANIFEST_FIELD))
+    else {
+        return Err(PublishedAtError::NotPresent);
+    };
+    Ok(value.to_string())
 }
