5adf58737f9b0222527b1e1f260465b4b49270ea
Andrew Schran <aschran@mystenlabs.com>
2024-07-09 15:44:02 -0400

    fix enum order

--- crates/sui-types/src/execution_status.rs
+++ crates/sui-types/src/execution_status.rs
@@ -205,9 +205,6 @@ pub enum ExecutionFailureStatus {
     #[error("Certificate is cancelled due to congestion on shared objects: {congested_objects}")]
     ExecutionCancelledDueToSharedObjectCongestion { congested_objects: CongestedObjects },
 
-    #[error("Certificate is cancelled because randomness could not be generated this epoch")]
-    ExecutionCancelledDueToRandomnessUnavailable,
-
     #[error("Address {address:?} is denied for coin {coin_type}")]
     AddressDeniedForCoin {
         address: SuiAddress,
@@ -216,6 +213,9 @@ pub enum ExecutionFailureStatus {
 
     #[error("Coin type is globally paused for use: {coin_type}")]
     CoinTypeGlobalPause { coin_type: String },
+
+    #[error("Certificate is cancelled because randomness could not be generated this epoch")]
+    ExecutionCancelledDueToRandomnessUnavailable,
     // NOTE: if you want to add a new enum,
     // please add it at the end for Rust SDK backward compatibility.
 }
