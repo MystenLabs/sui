765afd3850693f89b52364d2483d995141838c88
nikos.kitmeridis <nikos.kitmeridis@mystenlabs.com>
2024-07-15 10:41:48 +0300

    Adds support for coins other than SUI on the Rosetta account/balance endpoint (WIP) * Changes: SUI becomes the default currency on the request if none provided * Changes: Refactor of the balance request method to reduce code duplication * Pending: Handle multiple coins on the same request * Pending: Add more tests

--- crates/sui-rosetta/tests/gas_budget_tests.rs
+++ crates/sui-rosetta/tests/gas_budget_tests.rs
@@ -67,9 +67,7 @@ async fn pay_with_gas_budget(budget: u64) -> TransactionIdentifierResponseResult
             "operation_identifier":{"index":0},
             "type":"PaySui",
             "account": { "address" : recipient.to_string() },
-            "amount" : {
-                "value": "1000000000",
-            },
+            "amount" : { "value": "1000000000" , "currency": { "symbol": "SUI", "decimals": 9}}
         },{
             "operation_identifier":{"index":1},
             "type":"PaySui",
