5472bd97025a0bb11152eb2263093de81e2a1b56
Andrew Schran <aschran@mystenlabs.com>
2024-07-17 16:29:26 -0400

    add test for byzantine peer handling

--- crates/sui-network/src/randomness/tests.rs
+++ crates/sui-network/src/randomness/tests.rs
@@ -366,6 +366,118 @@ async fn test_restart_recovery() {
     }
 }
 
+#[tokio::test]
+async fn test_byzantine_peer_handling() {
+    telemetry_subscribers::init_for_testing();
+    let committee_fixture = CommitteeFixture::generate(rand::rngs::OsRng, 0, 4);
+    let committee = committee_fixture.committee();
+
+    let mut randomness_rxs = Vec::new();
+    let mut networks: Vec<anemo::Network> = Vec::new();
+    let mut nodes = Vec::new();
+    let mut handles = Vec::new();
+    let mut authority_info = HashMap::new();
+
+    for (authority, stake) in committee.members() {
+        let mut config = RandomnessConfig::default();
+        config.max_ignored_peer_weight_factor = Some(0.3);
+
+        let (tx, rx) = mpsc::channel(3);
+        randomness_rxs.push(rx);
+        let (unstarted, router) = Builder::new(*authority, tx).config(config).build();
+
+        let network = utils::build_network(|r| r.merge(router));
+        for n in networks.iter() {
+            network.connect(n.local_addr()).await.unwrap();
+        }
+        networks.push(network.clone());
+
+        let node = node_from_committee(committee, authority, *stake);
+        authority_info.insert(*authority, (network.peer_id(), node.id));
+        nodes.push(node);
+
+        let (r, handle) = unstarted.build(network);
+        handles.push((authority, handle));
+
+        let span = tracing::span!(
+            tracing::Level::INFO,
+            "RandomnessEventLoop",
+            authority = ?authority.concise(),
+        );
+        tokio::spawn(r.start().instrument(span));
+    }
+    info!(?authority_info, "authorities constructed");
+
+    let nodes = nodes::Nodes::new(nodes).unwrap();
+
+    // Send partial sigs from authorities 0 and 1, but give them different DKG output so they think
+    // each other are byzantine.
+    for (i, (authority, handle)) in handles.iter().enumerate() {
+        let mock_dkg_output = mocked_dkg::generate_mocked_output::<PkG, EncG>(
+            nodes.clone(),
+            committee.validity_threshold().try_into().unwrap(),
+            if i < 2 { 100 + i as u128 } else { 0 },
+            committee
+                .authority_index(authority)
+                .unwrap()
+                .try_into()
+                .unwrap(),
+        );
+        handle.send_partial_signatures(0, RandomnessRound(0));
+        handle.update_epoch(
+            0,
+            authority_info.clone(),
+            mock_dkg_output,
+            committee.validity_threshold().try_into().unwrap(),
+            None,
+        );
+    }
+    for rx in &mut randomness_rxs[2..] {
+        // Validators (2, 3) can communicate normally.
+        let (epoch, round, bytes) = rx.recv().await.unwrap();
+        assert_eq!(0, epoch);
+        assert_eq!(0, round.0);
+        assert_ne!(0, bytes.len());
+    }
+    for rx in &mut randomness_rxs[..2] {
+        // Validators (0, 1) are byzantine.
+        assert!(rx.try_recv().is_err());
+    }
+
+    // New epoch, they should forgive each other.
+    for (authority, handle) in handles.iter().take(2) {
+        let mock_dkg_output = mocked_dkg::generate_mocked_output::<PkG, EncG>(
+            nodes.clone(),
+            committee.validity_threshold().try_into().unwrap(),
+            0,
+            committee
+                .authority_index(authority)
+                .unwrap()
+                .try_into()
+                .unwrap(),
+        );
+        handle.send_partial_signatures(1, RandomnessRound(0));
+        handle.update_epoch(
+            1,
+            authority_info.clone(),
+            mock_dkg_output,
+            committee.validity_threshold().try_into().unwrap(),
+            None,
+        );
+    }
+    for rx in &mut randomness_rxs[..2] {
+        // Validators (0, 1) can communicate normally in new epoch.
+        let (epoch, round, bytes) = rx.recv().await.unwrap();
+        assert_eq!(1, epoch);
+        assert_eq!(0, round.0);
+        assert_ne!(0, bytes.len());
+    }
+    for rx in &mut randomness_rxs[2..] {
+        // Validators (2, 3) are still on old epoch.
+        assert!(rx.try_recv().is_err());
+    }
+}
+
 fn node_from_committee(
     committee: &Committee,
     authority: &AuthorityPublicKeyBytes,
