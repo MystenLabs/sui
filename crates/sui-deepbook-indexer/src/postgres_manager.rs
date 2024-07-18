6e3ae1156af4b2b8cfe93c638cb5f5efd3090ae1
Dario Russi <113150618+dariorussi@users.noreply.github.com>
2024-07-18 17:56:38 +0200

    Delete crates/sui-deepbook-indexer/del_me
    useless

--- crates/sui-deepbook-indexer/src/postgres_manager.rs
+++ crates/sui-deepbook-indexer/src/postgres_manager.rs
@@ -1,11 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{models::Deepbook, schema::deepbook};
 use diesel::{
     pg::PgConnection,
     r2d2::{ConnectionManager, Pool},
-    Connection, RunQueryDsl,
 };
 
 pub type PgPool = Pool<ConnectionManager<PgConnection>>;
@@ -16,15 +14,4 @@ pub fn get_connection_pool(database_url: String) -> PgPool {
         .test_on_check_out(true)
         .build(manager)
         .expect("Could not build Postgres DB connection pool")
-}
-
-pub fn write(pool: &PgPool, data: Vec<Deepbook>) -> Result<(), anyhow::Error> {
-    let connection = &mut pool.get()?;
-    connection.transaction(|conn| {
-        diesel::insert_into(deepbook::table)
-            .values(&data)
-            .on_conflict_do_nothing()
-            .execute(conn)
-    })?;
-    Ok(())
 }
