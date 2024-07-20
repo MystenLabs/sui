e17ca397ea25cb6f2147960952d7ff49665c5b34
William Smith <williamprincesmith@gmail.com>
2024-07-19 22:14:53 -0400

    [TrafficControl] Support multi-hop forwarding (#18699)
    ## Description 
    
    Sometimes there can be more than one proxy between the client and the
    server. In such cases, the `x-forwarded-for` header is appended with the
    full path of IPs, ostensibly with the client IP being the first. For
    this reason, we need to fix the parsing to accommodate a list of IPs.
    However, we additionally have the problem of knowing which IP in the
    list is the client IP. We cannot assume the first IP in the list is the
    client IP, as a malicious client could attempt to spoof by writing a
    junk value in `x-forwarded-for` in the request, to which the internal
    LBs would append. There are two possible solutions to this:
    
    a) define a custom header and configure our gcs LBS to write to this and
    overwrite any existing value for that header.
    b) add configuration such that the node operator defines the number of
    intermediate load balancers (`num_hops`) it runs.
    
    In such a case, for a `num_hops = N`, the client IP should always be the
    `N`th **to last**.
    To illustrate (note that for N proxies, only the IP addresses of proxy
    1, ..., N-1 will be contained in the header, as proxy N
    is directly connected to the server and does not write its own IP):
    ```
    [
      <whatever the client may have written>, 
      <actual client IP>, <-- we want this
      <1>, 
      ..., 
      <N - 1>
    ]
    ```
    
    The first solution would require extra configuration on their infra by
    the node operator, and would be specific to gcs (we'd have to confirm
    that something similar is supported in other cloud hosting services). So
    this PR implements option (2).
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/sui-core/src/authority_server.rs
+++ crates/sui-core/src/authority_server.rs
@@ -1008,24 +1008,44 @@ macro_rules! handle_with_decoration {
                     None
                 }
             }
-            ClientIdSource::XForwardedFor => {
+            ClientIdSource::XForwardedFor(num_hops) => {
                 let do_header_parse = |op: &MetadataValue<Ascii>| {
                     match op.to_str() {
                         Ok(header_val) => {
-                            match header_val.parse::<SocketAddr>() {
-                                Ok(socket_addr) => Some(socket_addr.ip()),
-                                Err(err) => {
+                            let header_contents = header_val.split(',').map(str::trim).collect::<Vec<_>>();
+                            if *num_hops == 0 {
+                                error!(
+                                    "x-forwarded-for: 0 specified. x-forwarded-for contents: {:?}. Please assign nonzero value for \
+                                    number of hops here, or use `socket-addr` client-id-source type if requests are not being proxied \
+                                    to this node. Skipping traffic controller request handling.",
+                                    header_contents,
+                                );
+                                return None;
+                            }
+                            let contents_len = header_contents.len();
+                            let Some(client_ip) = header_contents.get(contents_len - num_hops) else {
+                                error!(
+                                    "x-forwarded-for header value of {:?} contains {} values, but {} hops were specificed. \
+                                    Expected at least {} values. Skipping traffic controller request handling.",
+                                    header_contents,
+                                    contents_len,
+                                    num_hops,
+                                    contents_len,
+                                );
+                                return None;
+                            };
+                            client_ip.parse::<IpAddr>().ok().or_else(|| {
+                                client_ip.parse::<SocketAddr>().ok().map(|socket_addr| socket_addr.ip()).or_else(|| {
                                     $self.metrics.forwarded_header_parse_error.inc();
                                     error!(
-                                        "Failed to parse x-forwarded-for header value of {:?} to ip address: {:?}. \
+                                        "Failed to parse x-forwarded-for header value of {:?} to ip address or socket. \
                                         Please ensure that your proxy is configured to resolve client domains to an \
                                         IP address before writing header",
-                                        header_val,
-                                        err,
+                                        client_ip,
                                     );
                                     None
-                                }
-                            }
+                                })
+                            })
                         }
                         Err(e) => {
                             // TODO: once we have confirmed that no legitimate traffic
@@ -1043,7 +1063,7 @@ macro_rules! handle_with_decoration {
                     do_header_parse(op)
                 } else {
                     $self.metrics.forwarded_header_not_included.inc();
-                    error!("x-forwarded-header not present for request despite node configuring XForwardedFor tracking type");
+                    error!("x-forwarded-for header not present for request despite node configuring x-forwarded-for tracking type");
                     None
                 }
             }
