ca992a7c84053190ddc3446a17d49735acc8e388
tx-tomcat <dungnd8594@gmail.com>
2024-07-22 02:21:35 +0700

    refactor code base

--- external-crates/move/crates/move-compiler/src/linters/mod.rs
+++ external-crates/move/crates/move-compiler/src/linters/mod.rs
@@ -5,16 +5,16 @@ use move_symbol_pool::Symbol;
 
 use crate::{
     command_line::compiler::Visitor, diagnostics::codes::WarningFilter,
-    linters::constant_naming::ConstantNamingVisitor, typing::visitor::TypingVisitor,
+    linters::constant_naming::ConstantNamingVisitor,
+    typing::visitor::TypingVisitor,
 };
+
 pub mod constant_naming;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum LintLevel {
-    // No linters
     None,
-    // Run only the default linters
     Default,
-    // Run all linters
     All,
 }
 
@@ -32,19 +32,14 @@ pub enum LinterDiagnosticCategory {
 pub const ALLOW_ATTR_CATEGORY: &str = "lint";
 pub const LINT_WARNING_PREFIX: &str = "Lint ";
 pub const CONSTANT_NAMING_FILTER_NAME: &str = "constant_naming";
-
 pub const CONSTANT_NAMING_DIAG_CODE: u8 = 1;
-
-pub enum LinterDiagCategory {
-    Style,
-}
 
 pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
     (
         Some(ALLOW_ATTR_CATEGORY.into()),
         vec![WarningFilter::code(
             Some(LINT_WARNING_PREFIX),
-            LinterDiagCategory::Style as u8,
+            LinterDiagnosticCategory::Style as u8,
             CONSTANT_NAMING_DIAG_CODE,
             Some(CONSTANT_NAMING_FILTER_NAME),
         )],
@@ -53,8 +48,7 @@ pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
 
 pub fn linter_visitors(level: LintLevel) -> Vec<Visitor> {
     match level {
-        LintLevel::None => vec![],
-        LintLevel::Default => vec![],
+        LintLevel::None | LintLevel::Default => vec![],
         LintLevel::All => {
             vec![constant_naming::ConstantNamingVisitor::visitor(
                 ConstantNamingVisitor,
