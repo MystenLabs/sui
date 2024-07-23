7a0f7d31c89e2d25b455ea071a465a7e5ca558e2
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2024-07-23 09:00:35 -0700

    Refactor consensus output to write to an intermediate store before going to the db (#18447)
    This PR should have no behavior changes. It introduces a new struct,
    `ConsensusCommitOutput`, which stores all writes generated while
    processing a consensus commit, prior to writing them to the db.
    
    This is the first stage of caching/quarantining consensus-specific
    epoch-db state. The next step will be to hold `ConsensusCommitOutput`
    structs in memory until the checkpoints created for the commit have been
    certified. This will also require reading from `ConsensusCommitOutput`
    (or more likely, a broader caching struct which holds information from a
    set of `ConsensusCommitOutput` objects), since required information will
    not always be available from the db.

--- crates/sui-core/src/consensus_handler.rs
+++ crates/sui-core/src/consensus_handler.rs
@@ -644,7 +644,7 @@ impl From<SerializableSequencedConsensusTransactionKind> for SequencedConsensusT
     }
 }
 
-#[derive(Serialize, Deserialize, Clone, Hash, PartialEq, Eq, Debug)]
+#[derive(Serialize, Deserialize, Clone, Hash, PartialEq, Eq, Debug, Ord, PartialOrd)]
 pub enum SequencedConsensusTransactionKey {
     External(ConsensusTransactionKey),
     System(TransactionDigest),
