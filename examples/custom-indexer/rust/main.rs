8169f0529f477c52a2565404e406a70ce3aefa8e
Zihe Huang <zihehuang@gmail.com>
2024-07-23 16:20:15 -0700

    [docs] add custom indexer example to doc page (#18711)
    ## Description 
    
    Linking custom indexer example to doc page
    
    ## Test plan 
    
    manually
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:
    
    ---------
    
    Co-authored-by: ronny-mysten <118224482+ronny-mysten@users.noreply.github.com>

---
+++ examples/custom-indexer/rust/main.rs
@@ -0,0 +1,32 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use async_trait::async_trait;
+use sui_types::full_checkpoint_content::CheckpointData;
+use sui_data_ingestion_core::{Worker, setup_single_workflow};
+
+struct CustomWorker;
+
+#[async_trait]
+impl Worker for CustomWorker {
+    async fn process_checkpoint(&self, checkpoint: CheckpointData) -> Result<()> {
+        // custom processing logic
+        // print out the checkpoint number
+        println!("Processing checkpoint: {}", checkpoint.checkpoint_summary.to_string());
+        Ok(())
+    }
+}
+                    
+#[tokio::main]
+async fn main() -> Result<()> {
+    let (executor, term_sender) = setup_single_workflow(
+        CustomWorker,
+        "https://checkpoints.testnet.sui.io".to_string(),
+        0, /* initial checkpoint number */
+        5, /* concurrency */
+        None, /* extra reader options */
+    ).await?;
+    executor.await?;
+    Ok(())
+}
