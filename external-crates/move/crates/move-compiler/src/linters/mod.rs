21aeeae879548b5a6974021d178b91548aada216
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2024-07-25 01:52:45 +0700

    [Linter] Refactor: Optimize Linter Configuration and Visitor Generation (#18755)
    ## Description 
    
    Function Updates:
    Updated the known_filters function to use the
    LinterDiagnosticCategory::Style enum variant.
    Simplified the linter_visitors function by combining the None and
    Default cases into a single match arm, reducing redundancy.
    
    Naming Consistency:
    Ensured consistent naming conventions throughout the code.
    
    Removed Unused Code:
    Eliminated any unused code or imports to improve code cleanliness.
    
    ## Test plan 
    
    Existing linter unit tests
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- external-crates/move/crates/move-compiler/src/linters/mod.rs
+++ external-crates/move/crates/move-compiler/src/linters/mod.rs
@@ -8,6 +8,7 @@ use crate::{
     linters::constant_naming::ConstantNamingVisitor, typing::visitor::TypingVisitor,
 };
 pub mod constant_naming;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum LintLevel {
     // No linters
@@ -32,19 +33,14 @@ pub enum LinterDiagnosticCategory {
 pub const ALLOW_ATTR_CATEGORY: &str = "lint";
 pub const LINT_WARNING_PREFIX: &str = "Lint ";
 pub const CONSTANT_NAMING_FILTER_NAME: &str = "constant_naming";
-
 pub const CONSTANT_NAMING_DIAG_CODE: u8 = 1;
-
-pub enum LinterDiagCategory {
-    Style,
-}
 
 pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
     (
         Some(ALLOW_ATTR_CATEGORY.into()),
         vec![WarningFilter::code(
             Some(LINT_WARNING_PREFIX),
-            LinterDiagCategory::Style as u8,
+            LinterDiagnosticCategory::Style as u8,
             CONSTANT_NAMING_DIAG_CODE,
             Some(CONSTANT_NAMING_FILTER_NAME),
         )],
@@ -53,8 +49,7 @@ pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
 
 pub fn linter_visitors(level: LintLevel) -> Vec<Visitor> {
     match level {
-        LintLevel::None => vec![],
-        LintLevel::Default => vec![],
+        LintLevel::None | LintLevel::Default => vec![],
         LintLevel::All => {
             vec![constant_naming::ConstantNamingVisitor::visitor(
                 ConstantNamingVisitor,
