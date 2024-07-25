a6b3440e484b62cc29b298d23958afe3a5b76f03
Brandon Williams <brandon@mystenlabs.com>
2024-07-25 12:08:19 -0500

    chore: update anemo and quinn

--- crates/sui-network/src/state_sync/server.rs
+++ crates/sui-network/src/state_sync/server.rs
@@ -224,7 +224,9 @@ where
             struct SemaphoreExtension(OwnedSemaphorePermit);
             inner.call(req).await.map(move |mut response| {
                 // Insert permit as extension so it's not dropped until the response is sent.
-                response.extensions_mut().insert(SemaphoreExtension(permit));
+                response
+                    .extensions_mut()
+                    .insert(Arc::new(SemaphoreExtension(permit)));
                 response
             })
         };
