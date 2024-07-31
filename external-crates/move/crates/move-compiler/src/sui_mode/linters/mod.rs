5a0febd2d09cc6a9a42236577e6238ef02bf21e0
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2024-08-01 03:55:21 +0700

    [Linter] Warn against freezing a type T with a capability-like name (ends in Cap or Capability) and capability-like usage (one or more functions gated with &T inside the package that declares T) (#17273)
    ## Description 
    The lint identifies function calls that may incorrectly freeze such
    types, which can lead to design issues.
    Key Features:
    
    1. Checks for specific freezing functions defined in constants.
    2. Uses regex to identify capability-like type names.
    3. Reports warnings for potential misuse of freezing on capability-like
    types.
    
    Implementation Details:
    
    The lint focuses on public_freeze and freeze functions in the
    sui::transfer module.
    It uses a regex pattern to match type names ending with "Cap",
    "Capability", or similar variations.
    When a match is found, it reports a warning with a custom diagnostic
    message.
    
    # Test plan
    Added more use case including false positive, false negative case
    
    ## Release notes
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [X] CLI: Move will now lint against freezing a potential capability object
    - [ ] Rust SDK:
    
    ---------
    
    Co-authored-by: jamedzung <dung.dinhnguyen@digitalavenues.com>
    Co-authored-by: Todd Nowacki <tmn@mystenlabs.com>

--- external-crates/move/crates/move-compiler/src/sui_mode/linters/mod.rs
+++ external-crates/move/crates/move-compiler/src/sui_mode/linters/mod.rs
@@ -18,6 +18,7 @@ pub mod coin_field;
 pub mod collection_equality;
 pub mod custom_state_change;
 pub mod freeze_wrapped;
+pub mod freezing_capability;
 pub mod missing_key;
 pub mod public_random;
 pub mod self_transfer;
@@ -70,6 +71,7 @@ pub const FREEZE_WRAPPED_FILTER_NAME: &str = "freeze_wrapped";
 pub const COLLECTION_EQUALITY_FILTER_NAME: &str = "collection_equality";
 pub const PUBLIC_RANDOM_FILTER_NAME: &str = "public_random";
 pub const MISSING_KEY_FILTER_NAME: &str = "missing_key";
+pub const FREEZING_CAPABILITY_FILTER_NAME: &str = "freezing_capability";
 
 pub const RANDOM_MOD_NAME: &str = "random";
 pub const RANDOM_STRUCT_NAME: &str = "Random";
@@ -87,6 +89,7 @@ pub enum LinterDiagnosticCode {
     CollectionEquality,
     PublicRandom,
     MissingKey,
+    FreezingCapability,
 }
 
 pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
@@ -140,6 +143,12 @@ pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
             LinterDiagnosticCode::MissingKey as u8,
             Some(MISSING_KEY_FILTER_NAME),
         ),
+        WarningFilter::code(
+            Some(LINT_WARNING_PREFIX),
+            LinterDiagnosticCategory::Sui as u8,
+            LinterDiagnosticCode::FreezingCapability as u8,
+            Some(FREEZING_CAPABILITY_FILTER_NAME),
+        ),
     ];
 
     (Some(ALLOW_ATTR_CATEGORY.into()), filters)
@@ -148,17 +157,20 @@ pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
 pub fn linter_visitors(level: LintLevel) -> Vec<Visitor> {
     match level {
         LintLevel::None => vec![],
-        LintLevel::Default | LintLevel::All => {
-            vec![
-                share_owned::ShareOwnedVerifier.visitor(),
-                self_transfer::SelfTransferVerifier.visitor(),
-                custom_state_change::CustomStateChangeVerifier.visitor(),
-                coin_field::CoinFieldVisitor.visitor(),
-                freeze_wrapped::FreezeWrappedVisitor.visitor(),
-                collection_equality::CollectionEqualityVisitor.visitor(),
-                public_random::PublicRandomVisitor.visitor(),
-                missing_key::MissingKeyVisitor.visitor(),
-            ]
+        LintLevel::Default => vec![
+            share_owned::ShareOwnedVerifier.visitor(),
+            self_transfer::SelfTransferVerifier.visitor(),
+            custom_state_change::CustomStateChangeVerifier.visitor(),
+            coin_field::CoinFieldVisitor.visitor(),
+            freeze_wrapped::FreezeWrappedVisitor.visitor(),
+            collection_equality::CollectionEqualityVisitor.visitor(),
+            public_random::PublicRandomVisitor.visitor(),
+            missing_key::MissingKeyVisitor.visitor(),
+        ],
+        LintLevel::All => {
+            let mut visitors = linter_visitors(LintLevel::Default);
+            visitors.extend([freezing_capability::WarnFreezeCapability.visitor()]);
+            visitors
         }
     }
 }
