0caa4028c197c2b61513313ea7701a1089e64489
Anastasios Kichidis <tasos@mystenlabs.com>
2024-08-02 13:46:11 +0100

    [refactor] consensus submission - return when transactions have been processed either via consensus or via checkpoints

--- crates/sui-core/src/consensus_adapter.rs
+++ crates/sui-core/src/consensus_adapter.rs
@@ -50,8 +50,8 @@ use sui_simulator::anemo::PeerId;
 use sui_simulator::narwhal_network::connectivity::ConnectionStatus;
 use sui_types::base_types::AuthorityName;
 use sui_types::fp_ensure;
-use sui_types::messages_consensus::ConsensusTransaction;
 use sui_types::messages_consensus::ConsensusTransactionKind;
+use sui_types::messages_consensus::{ConsensusTransaction, ConsensusTransactionKey};
 use tokio::time::Duration;
 use tracing::{debug, info, warn};
 
@@ -79,6 +79,7 @@ pub struct ConsensusAdapterMetrics {
     pub sequencing_certificate_authority_position: Histogram,
     pub sequencing_certificate_positions_moved: Histogram,
     pub sequencing_certificate_preceding_disconnected: Histogram,
+    pub sequencing_certificate_processed: IntCounterVec,
     pub sequencing_in_flight_semaphore_wait: IntGauge,
     pub sequencing_in_flight_submissions: IntGauge,
     pub sequencing_estimated_latency: IntGauge,
@@ -146,6 +147,12 @@ impl ConsensusAdapterMetrics {
                 "The number of authorities that were hashed to an earlier position that were filtered out due to being disconnected when submitting to consensus.",
                 SEQUENCING_CERTIFICATE_POSITION_BUCKETS.to_vec(),
                 registry,
+            ).unwrap(),
+            sequencing_certificate_processed: register_int_counter_vec_with_registry!(
+                "sequencing_certificate_processed",
+                "The number of certificates that have been processed either by consensus or checkpoint.",
+                &["source"],
+                registry
             ).unwrap(),
             sequencing_in_flight_semaphore_wait: register_int_gauge_with_registry!(
                 "sequencing_in_flight_semaphore_wait",
@@ -277,7 +284,7 @@ pub struct ConsensusAdapter {
     consensus_throughput_profiler: ArcSwapOption<ConsensusThroughputProfiler>,
     /// A structure to register metrics
     metrics: ConsensusAdapterMetrics,
-    /// Semaphore limiting parallel submissions to narwhal
+    /// Semaphore limiting parallel submissions to consensus
     submit_semaphore: Semaphore,
     latency_observer: LatencyObserver,
     protocol_config: ProtocolConfig,
@@ -715,29 +722,29 @@ impl ConsensusAdapter {
             "soft_bundle"
         };
 
-        let processed_waiter = epoch_store
-            .consensus_messages_processed_notify(transaction_keys.clone())
-            .boxed();
+        let mut guard = InflightDropGuard::acquire(&self, tx_type);
 
-        pin_mut!(processed_waiter);
-
+        // Create the waiter until the node's turn comes to submit to consensus
         let (await_submit, position, positions_moved, preceding_disconnected) =
             self.await_submit_delay(epoch_store.committee(), &transactions[..]);
 
-        let mut guard = InflightDropGuard::acquire(&self, tx_type);
+        // Create the waiter until the transaction is processed by consensus or via checkpoint
+        let processed_via_consensus_or_checkpoint =
+            self.await_consensus_or_checkpoint(transaction_keys.clone(), epoch_store);
+        pin_mut!(processed_via_consensus_or_checkpoint);
+
         let processed_waiter = tokio::select! {
             // We need to wait for some delay until we submit transaction to the consensus
-            _ = await_submit => Some(processed_waiter),
+            _ = await_submit => Some(processed_via_consensus_or_checkpoint),
 
             // If epoch ends, don't wait for submit delay
             _ = epoch_store.user_certs_closed_notify() => {
                 warn!(epoch = ?epoch_store.epoch(), "Epoch ended, skipping submission delay");
-                Some(processed_waiter)
+                Some(processed_via_consensus_or_checkpoint)
             }
 
-            // If transaction is received by consensus while we wait, we are done.
-            processed = &mut processed_waiter => {
-                processed.expect("Storage error when waiting for consensus message processed");
+            // If transaction is received by consensus or checkpoint while we wait, we are done.
+            _ = &mut processed_via_consensus_or_checkpoint => {
                 None
             }
         };
@@ -769,6 +776,7 @@ impl ConsensusAdapter {
         } else {
             None
         };
+
         if let Some(processed_waiter) = processed_waiter {
             debug!("Submitting {:?} to consensus", transaction_keys);
 
@@ -844,7 +852,6 @@ impl ConsensusAdapter {
                     processed_waiter.await
                 }
             }
-            .expect("Storage error when waiting for consensus message processed");
         }
         debug!("{transaction_keys:?} processed by consensus");
 
@@ -894,6 +901,39 @@ impl ConsensusAdapter {
             .sequencing_certificate_success
             .with_label_values(&[tx_type])
             .inc();
+    }
+
+    /// Waits for transactions to appear either to consensus output or been executed via a checkpoint (state sync).
+    async fn await_consensus_or_checkpoint(
+        self: &Arc<Self>,
+        transaction_keys: Vec<SequencedConsensusTransactionKey>,
+        epoch_store: &Arc<AuthorityPerEpochStore>,
+    ) {
+        // Filter only the transaction digests to check for occurance executed checkpoint.
+        let transaction_digests = transaction_keys
+            .iter()
+            .flat_map(|key| {
+                if let SequencedConsensusTransactionKey::External(
+                    ConsensusTransactionKey::Certificate(digest),
+                ) = key
+                {
+                    Some(*digest)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+
+        tokio::select! {
+            processed = epoch_store.consensus_messages_processed_notify(transaction_keys) => {
+                processed.expect("Storage error when waiting for consensus message processed");
+                self.metrics.sequencing_certificate_processed.with_label_values(&["consensus"]).inc();
+            },
+            processed = epoch_store.transaction_executed_in_checkpoint_notify(transaction_digests) => {
+                processed.expect("Storage error when waiting for transaction executed in checkpoint");
+                self.metrics.sequencing_certificate_processed.with_label_values(&["checkpoint"]).inc();
+            }
+        }
     }
 }
 
