c4343a297240fe8c684efd815ffe01d953d6a2d2
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2024-08-15 06:01:33 +0700

    [Linter] Unnecessary while loop (#16876)
    # Description
    This linter encourages replacing `while(true)` loops with the more
    idiomatic loop construct. Here's a breakdown of how it works:
    It checks each expression in the AST.
    If the expression is a While loop, it examines the condition.
    If the condition is always true (using the `is_condition_always_true`
    function), it reports a diagnostic suggesting to use loop instead.
    
    The `is_condition_always_true` function checks if the condition is a
    boolean literal with the value true.
    
    ## Test plan
    Added more use case including false positive, false negative case
    
    ## Release notes
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [X] CLI: Move will now lint against `while (true)`, which should be
    replaced by `loop`
    - [ ] Rust SDK:
    
    ---------
    
    Co-authored-by: jamedzung <dung.dinhnguyen@digitalavenues.com>
    Co-authored-by: Todd Nowacki <tmn@mystenlabs.com>

--- external-crates/move/crates/move-compiler/src/linters/mod.rs
+++ external-crates/move/crates/move-compiler/src/linters/mod.rs
@@ -8,6 +8,7 @@ use crate::{
     linters::constant_naming::ConstantNamingVisitor, typing::visitor::TypingVisitor,
 };
 pub mod constant_naming;
+mod unnecessary_while_loop;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum LintLevel {
@@ -34,16 +35,26 @@ pub const ALLOW_ATTR_CATEGORY: &str = "lint";
 pub const LINT_WARNING_PREFIX: &str = "Lint ";
 pub const CONSTANT_NAMING_FILTER_NAME: &str = "constant_naming";
 pub const CONSTANT_NAMING_DIAG_CODE: u8 = 1;
+pub const WHILE_TRUE_TO_LOOP_FILTER_NAME: &str = "while_true";
+pub const WHILE_TRUE_TO_LOOP_DIAG_CODE: u8 = 4;
 
 pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
     (
         Some(ALLOW_ATTR_CATEGORY.into()),
-        vec![WarningFilter::code(
-            Some(LINT_WARNING_PREFIX),
-            LinterDiagnosticCategory::Style as u8,
-            CONSTANT_NAMING_DIAG_CODE,
-            Some(CONSTANT_NAMING_FILTER_NAME),
-        )],
+        vec![
+            WarningFilter::code(
+                Some(LINT_WARNING_PREFIX),
+                LinterDiagnosticCategory::Style as u8,
+                CONSTANT_NAMING_DIAG_CODE,
+                Some(CONSTANT_NAMING_FILTER_NAME),
+            ),
+            WarningFilter::code(
+                Some(LINT_WARNING_PREFIX),
+                LinterDiagnosticCategory::Complexity as u8,
+                WHILE_TRUE_TO_LOOP_DIAG_CODE,
+                Some(WHILE_TRUE_TO_LOOP_FILTER_NAME),
+            ),
+        ],
     )
 }
 
@@ -51,9 +62,12 @@ pub fn linter_visitors(level: LintLevel) -> Vec<Visitor> {
     match level {
         LintLevel::None | LintLevel::Default => vec![],
         LintLevel::All => {
-            vec![constant_naming::ConstantNamingVisitor::visitor(
-                ConstantNamingVisitor,
-            )]
+            vec![
+                constant_naming::ConstantNamingVisitor::visitor(ConstantNamingVisitor),
+                unnecessary_while_loop::WhileTrueToLoop::visitor(
+                    unnecessary_while_loop::WhileTrueToLoop,
+                ),
+            ]
         }
     }
 }
