8102b1ba337db0903b60583785b14afb78c267d1
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2024-08-15 18:02:42 -0400

    pick(19005): merge idx-breaking-change-park to main
    ## Description
    
    Cherry pick merge of indexer breaking changes to previous GraphQL
    release to unbreak backward compatibility.
    
    ## Test plan
    
    ```
    sui$ cargo build -p sui-indexer
    sui$ cargo nextest run -p sui-graphql-rpc
    sui$ cargo nextest run -p sui-graphql-e2e-tests --features pg_integration
    ```
    
    ---------
    
    Co-authored-by: Emma Zhong <emma@mystenlabs.com>
    Co-authored-by: Ashok Menon <ashok@mystenlabs.com>
    Co-authored-by: wlmyng <127570466+wlmyng@users.noreply.github.com>
    Co-authored-by: Emma Zhong <emmazhongjy@gmail.com>

--- crates/sui-indexer/src/models/tx_indices.rs
+++ crates/sui-indexer/src/models/tx_indices.rs
@@ -3,7 +3,8 @@
 
 use crate::{
     schema::{
-        tx_calls, tx_changed_objects, tx_digests, tx_input_objects, tx_recipients, tx_senders,
+        tx_calls_fun, tx_calls_mod, tx_calls_pkg, tx_changed_objects, tx_digests, tx_input_objects,
+        tx_kinds, tx_recipients, tx_senders,
     },
     types::TxIndex,
 };
@@ -24,7 +25,6 @@ pub struct TxDigest {
 #[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
 #[diesel(table_name = tx_senders)]
 pub struct StoredTxSenders {
-    pub cp_sequence_number: i64,
     pub tx_sequence_number: i64,
     pub sender: Vec<u8>,
 }
@@ -32,35 +32,52 @@ pub struct StoredTxSenders {
 #[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
 #[diesel(table_name = tx_recipients)]
 pub struct StoredTxRecipients {
-    pub cp_sequence_number: i64,
     pub tx_sequence_number: i64,
     pub recipient: Vec<u8>,
+    pub sender: Vec<u8>,
 }
 
 #[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
 #[diesel(table_name = tx_input_objects)]
 pub struct StoredTxInputObject {
-    pub cp_sequence_number: i64,
     pub tx_sequence_number: i64,
     pub object_id: Vec<u8>,
+    pub sender: Vec<u8>,
 }
 
 #[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
 #[diesel(table_name = tx_changed_objects)]
 pub struct StoredTxChangedObject {
-    pub cp_sequence_number: i64,
     pub tx_sequence_number: i64,
     pub object_id: Vec<u8>,
+    pub sender: Vec<u8>,
 }
 
 #[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
-#[diesel(table_name = tx_calls)]
-pub struct StoredTxCalls {
-    pub cp_sequence_number: i64,
+#[diesel(table_name = tx_calls_pkg)]
+pub struct StoredTxPkg {
+    pub tx_sequence_number: i64,
+    pub package: Vec<u8>,
+    pub sender: Vec<u8>,
+}
+
+#[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
+#[diesel(table_name = tx_calls_mod)]
+pub struct StoredTxMod {
+    pub tx_sequence_number: i64,
+    pub package: Vec<u8>,
+    pub module: String,
+    pub sender: Vec<u8>,
+}
+
+#[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
+#[diesel(table_name = tx_calls_fun)]
+pub struct StoredTxFun {
     pub tx_sequence_number: i64,
     pub package: Vec<u8>,
     pub module: String,
     pub func: String,
+    pub sender: Vec<u8>,
 }
 
 #[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
@@ -68,7 +85,13 @@ pub struct StoredTxCalls {
 pub struct StoredTxDigest {
     pub tx_digest: Vec<u8>,
     pub tx_sequence_number: i64,
-    pub cp_sequence_number: i64,
+}
+
+#[derive(Queryable, Insertable, Selectable, Debug, Clone, Default)]
+#[diesel(table_name = tx_kinds)]
+pub struct StoredTxKind {
+    pub tx_kind: i16,
+    pub tx_sequence_number: i64,
 }
 
 #[allow(clippy::type_complexity)]
@@ -80,71 +103,109 @@ impl TxIndex {
         Vec<StoredTxRecipients>,
         Vec<StoredTxInputObject>,
         Vec<StoredTxChangedObject>,
-        Vec<StoredTxCalls>,
+        Vec<StoredTxPkg>,
+        Vec<StoredTxMod>,
+        Vec<StoredTxFun>,
         Vec<StoredTxDigest>,
+        Vec<StoredTxKind>,
     ) {
         let tx_sequence_number = self.tx_sequence_number as i64;
-        let cp_sequence_number = self.checkpoint_sequence_number as i64;
-        let tx_senders = self
-            .senders
-            .iter()
-            .map(|s| StoredTxSenders {
-                cp_sequence_number,
-                tx_sequence_number,
-                sender: s.to_vec(),
-            })
-            .collect();
+        let tx_sender = StoredTxSenders {
+            tx_sequence_number,
+            sender: self.sender.to_vec(),
+        };
         let tx_recipients = self
             .recipients
             .iter()
             .map(|s| StoredTxRecipients {
-                cp_sequence_number,
                 tx_sequence_number,
                 recipient: s.to_vec(),
+                sender: self.sender.to_vec(),
             })
             .collect();
         let tx_input_objects = self
             .input_objects
             .iter()
             .map(|o| StoredTxInputObject {
-                cp_sequence_number,
                 tx_sequence_number,
                 object_id: bcs::to_bytes(&o).unwrap(),
+                sender: self.sender.to_vec(),
             })
             .collect();
         let tx_changed_objects = self
             .changed_objects
             .iter()
             .map(|o| StoredTxChangedObject {
-                cp_sequence_number,
                 tx_sequence_number,
                 object_id: bcs::to_bytes(&o).unwrap(),
+                sender: self.sender.to_vec(),
             })
             .collect();
-        let tx_calls = self
+
+        let mut packages = Vec::new();
+        let mut packages_modules = Vec::new();
+        let mut packages_modules_funcs = Vec::new();
+
+        for (pkg, pkg_mod, pkg_mod_func) in self
             .move_calls
             .iter()
-            .map(|(p, m, f)| StoredTxCalls {
-                cp_sequence_number,
+            .map(|(p, m, f)| (*p, (*p, m.clone()), (*p, m.clone(), f.clone())))
+        {
+            packages.push(pkg);
+            packages_modules.push(pkg_mod);
+            packages_modules_funcs.push(pkg_mod_func);
+        }
+
+        let tx_pkgs = packages
+            .iter()
+            .map(|p| StoredTxPkg {
+                tx_sequence_number,
+                package: p.to_vec(),
+                sender: self.sender.to_vec(),
+            })
+            .collect();
+
+        let tx_mods = packages_modules
+            .iter()
+            .map(|(p, m)| StoredTxMod {
+                tx_sequence_number,
+                package: p.to_vec(),
+                module: m.to_string(),
+                sender: self.sender.to_vec(),
+            })
+            .collect();
+
+        let tx_calls = packages_modules_funcs
+            .iter()
+            .map(|(p, m, f)| StoredTxFun {
                 tx_sequence_number,
                 package: p.to_vec(),
                 module: m.to_string(),
                 func: f.to_string(),
+                sender: self.sender.to_vec(),
             })
             .collect();
+
         let stored_tx_digest = StoredTxDigest {
             tx_digest: self.transaction_digest.into_inner().to_vec(),
             tx_sequence_number,
-            cp_sequence_number,
+        };
+
+        let tx_kind = StoredTxKind {
+            tx_kind: self.tx_kind as i16,
+            tx_sequence_number,
         };
 
         (
-            tx_senders,
+            vec![tx_sender],
             tx_recipients,
             tx_input_objects,
             tx_changed_objects,
+            tx_pkgs,
+            tx_mods,
             tx_calls,
             vec![stored_tx_digest],
+            vec![tx_kind],
         )
     }
 }
