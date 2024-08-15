f20d9eec0b8b29d7b1a18004537e10840f11419b
Ashok Menon <ashok@mystenlabs.com>
2024-08-15 14:53:12 +0100

    fixup: semver for deprecation version

--- crates/sui-json-rpc-types/src/sui_transaction.rs
+++ crates/sui-json-rpc-types/src/sui_transaction.rs
@@ -181,7 +181,7 @@ impl SuiTransactionBlockResponseOptions {
     }
 
     #[deprecated(
-        since = "1.31",
+        since = "1.31.0",
         note = "Balance and object changes no longer require local execution"
     )]
     pub fn require_local_execution(&self) -> bool {
