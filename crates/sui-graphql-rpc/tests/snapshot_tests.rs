b3d3d7ad680fa993c03ec959046632ec0a32500e
Ashok Menon <ashok@mystenlabs.com>
2024-08-19 11:41:40 +0100

    [chore][GraphQL] Declutter schemas (#18288)
    ## Description
    
    Remove `draft_target_schema.graphql` and promote
    `current_progress_schema.graphql` to be the canonical schema for the
    service -- move it to the top-level of the `sui-graphql-rpc` crate to
    make it easier to find. This is to avoid confusion about source of truth
    for the GraphQL schema.
    
    Because the TS SDK references the schema at multiple GraphQL versions,
    we will need to cherry-pick this change to release branches when it
    lands.
    
    ## Test plan
    
    CI
    
    ## Stack
    
    - #17543
    - #17692 
    - #17693 
    - #17696 
    - #18287 
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [x] GraphQL: The schema file has been moved from
    `crates/sui-graphql-rpc/schemas/current_progress_schema.graphql` to
    `crates/sui-graphql-rpc/schema.graphql`.
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/sui-graphql-rpc/tests/snapshot_tests.rs
+++ crates/sui-graphql-rpc/tests/snapshot_tests.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use insta::assert_snapshot;
-use std::fs::write;
+use std::fs;
 use std::path::PathBuf;
 use sui_graphql_rpc::server::builder::export_schema;
 
@@ -11,9 +11,8 @@ fn test_schema_sdl_export() {
     let sdl = export_schema();
 
     // update the current schema file
-    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.extend(["schema", "current_progress_schema.graphql"]);
-    write(path, &sdl).unwrap();
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("schema.graphql");
+    fs::write(path, &sdl).unwrap();
 
     assert_snapshot!(sdl);
 }
