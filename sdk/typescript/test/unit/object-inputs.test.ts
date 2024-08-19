143cd9dc29896c1e042c4b5bc2c176fa306c5552
hayes-mysten <135670682+hayes-mysten@users.noreply.github.com>
2024-08-19 12:04:28 -0700

    [ts sdk] Add new tx.object methods (#18940)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- sdk/typescript/test/unit/object-inputs.test.ts
+++ sdk/typescript/test/unit/object-inputs.test.ts
@@ -4,39 +4,37 @@
 import { toB58 } from '@mysten/bcs';
 import { describe, expect, it } from 'vitest';
 
-import { Arguments, Transaction } from '../../src/transactions';
+import { Transaction } from '../../src/transactions';
 
-describe('Arguments helpers', () => {
-	it('can use Arguments for building a transaction', async () => {
-		const args = [
-			Arguments.object('0x123'),
-			Arguments.receivingRef({
-				objectId: '1',
-				version: '123',
-				digest: toB58(new Uint8Array(32).fill(0x1)),
-			}),
-			Arguments.sharedObjectRef({
-				objectId: '2',
-				mutable: true,
-				initialSharedVersion: '123',
-			}),
-			Arguments.objectRef({
-				objectId: '3',
-				version: '123',
-				digest: toB58(new Uint8Array(32).fill(0x1)),
-			}),
-			Arguments.pure.address('0x2'),
-			Arguments.object.system(),
-			Arguments.object.clock(),
-			Arguments.object.random(),
-			Arguments.object.denyList(),
-		];
-
+describe('Transaction inputs', () => {
+	it('can use tx methods for creating inputs', async () => {
 		const tx = new Transaction();
 
 		tx.moveCall({
 			target: '0x2::foo::bar',
-			arguments: args,
+			arguments: [
+				tx.object('0x123'),
+				tx.receivingRef({
+					objectId: '1',
+					version: '123',
+					digest: toB58(new Uint8Array(32).fill(0x1)),
+				}),
+				tx.sharedObjectRef({
+					objectId: '2',
+					mutable: true,
+					initialSharedVersion: '123',
+				}),
+				tx.objectRef({
+					objectId: '3',
+					version: '123',
+					digest: toB58(new Uint8Array(32).fill(0x1)),
+				}),
+				tx.pure.address('0x2'),
+				tx.object.system(),
+				tx.object.clock(),
+				tx.object.random(),
+				tx.object.denyList(),
+			],
 		});
 
 		expect(tx.getData()).toMatchInlineSnapshot(`
