24049ef1f50cc4f8facb13427fc94d5fa93f8ac9
George Danezis <4999882+gdanezis@users.noreply.github.com>
2024-08-19 15:32:29 +0100

    [light-client] Add standalone verification function (#18971)
    ## Description 
    
    Add a standalone light client verification function. It takes a
    structure that contains some targets to verify and a proof material, and
    returns Ok() if they verify or an error otherwise. It takes dependencies
    only on Sui types for the moment.
    
    ## Test plan 
    
    We have new unit tests.

---
+++ crates/sui-light-client/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod construct;
+pub mod proof;
+
+#[doc(inline)]
+pub use proof::*;
+
+#[doc(inline)]
+pub use construct::*;
