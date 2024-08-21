eb3b97c2bf6f01667fb276c0479f6493f1e047e5
MW Tian <mingwei@mystenlabs.com>
2024-08-21 15:37:45 -0700

    [Consensus] panic in tests when transaction exceeds consensus size limit

--- consensus/core/src/lib.rs
+++ consensus/core/src/lib.rs
@@ -44,7 +44,7 @@ pub use authority_node::ConsensusAuthority;
 pub use block::{BlockAPI, Round};
 pub use commit::{CommitDigest, CommitIndex, CommitRef, CommittedSubDag};
 pub use commit_consumer::{CommitConsumer, CommitConsumerMonitor};
-pub use transaction::{TransactionClient, TransactionVerifier, ValidationError};
+pub use transaction::{ClientError, TransactionClient, TransactionVerifier, ValidationError};
 
 #[cfg(test)]
 #[path = "tests/randomized_tests.rs"]
