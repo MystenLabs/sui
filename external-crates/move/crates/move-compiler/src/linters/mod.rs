c724ed0d6dd062e1a0e04e4e33ff7b8f497f5829
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2024-08-24 00:49:12 +0700

    [Linter] Meaningless Math Operation (#16626)
    ## Description 
    The lint identifies operations that have no effect on the result, such
    as multiplying by 0 or 1, adding or subtracting 0, or shifting by 0.
    These operations are considered redundant and can be simplified to
    improve code clarity.
    
    Main Logic:
    
    For binary operations, it checks the operator and the right-hand side
    operand.
    It identifies specific patterns of meaningless operations.
    
    Detected Patterns:
    The lint checks for the following meaningless operations:
    
    Multiplication or division by 0
    Multiplication by 1
    Addition or subtraction of 0
    Left or right shift by 0
    
    ## Test plan
    Added more use case including true positive, true negative, false
    positive, false negative case
    
    ## Release notes
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [X] CLI: Move will now lint against unnecessary math operations in
    many cases.
    - [ ] Rust SDK:
    
    ---------
    
    Co-authored-by: jamedzung <dung.dinhnguyen@digitalavenues.com>
    Co-authored-by: Todd Nowacki <tmn@mystenlabs.com>

--- external-crates/move/crates/move-compiler/src/linters/mod.rs
+++ external-crates/move/crates/move-compiler/src/linters/mod.rs
@@ -4,10 +4,11 @@
 use move_symbol_pool::Symbol;
 
 use crate::{
-    command_line::compiler::Visitor, diagnostics::codes::WarningFilter,
-    linters::constant_naming::ConstantNamingVisitor, typing::visitor::TypingVisitor,
+    cfgir::visitor::CFGIRVisitor, command_line::compiler::Visitor,
+    diagnostics::codes::WarningFilter, typing::visitor::TypingVisitor,
 };
 pub mod constant_naming;
+pub mod meaningless_math_operation;
 mod unnecessary_while_loop;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -37,6 +38,8 @@ pub const CONSTANT_NAMING_FILTER_NAME: &str = "constant_naming";
 pub const CONSTANT_NAMING_DIAG_CODE: u8 = 1;
 pub const WHILE_TRUE_TO_LOOP_FILTER_NAME: &str = "while_true";
 pub const WHILE_TRUE_TO_LOOP_DIAG_CODE: u8 = 4;
+pub const MEANINGLESS_MATH_FILTER_NAME: &str = "unnecessary_math";
+pub const MEANINGLESS_MATH_DIAG_CODE: u8 = 8;
 
 pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
     (
@@ -54,6 +57,12 @@ pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
                 WHILE_TRUE_TO_LOOP_DIAG_CODE,
                 Some(WHILE_TRUE_TO_LOOP_FILTER_NAME),
             ),
+            WarningFilter::code(
+                Some(LINT_WARNING_PREFIX),
+                LinterDiagnosticCategory::Complexity as u8,
+                MEANINGLESS_MATH_DIAG_CODE,
+                Some(MEANINGLESS_MATH_FILTER_NAME),
+            ),
         ],
     )
 }
@@ -63,10 +72,9 @@ pub fn linter_visitors(level: LintLevel) -> Vec<Visitor> {
         LintLevel::None | LintLevel::Default => vec![],
         LintLevel::All => {
             vec![
-                constant_naming::ConstantNamingVisitor::visitor(ConstantNamingVisitor),
-                unnecessary_while_loop::WhileTrueToLoop::visitor(
-                    unnecessary_while_loop::WhileTrueToLoop,
-                ),
+                constant_naming::ConstantNamingVisitor.visitor(),
+                unnecessary_while_loop::WhileTrueToLoop.visitor(),
+                meaningless_math_operation::MeaninglessMathOperation.visitor(),
             ]
         }
     }
