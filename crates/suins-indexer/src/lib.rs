adcce3935dfff058a50c861397f909590e90ed93
Brandon Williams <brandon@mystenlabs.com>
2024-08-26 10:22:31 -0500

    suins-indexer: convert to using diesel-async

--- crates/suins-indexer/src/lib.rs
+++ crates/suins-indexer/src/lib.rs
@@ -5,22 +5,29 @@ pub mod indexer;
 pub mod models;
 pub mod schema;
 
-use diesel::pg::PgConnection;
-use diesel::r2d2::{ConnectionManager, Pool};
 use dotenvy::dotenv;
 use std::env;
 
-pub type PgConnectionPool = diesel::r2d2::Pool<ConnectionManager<PgConnection>>;
-pub type PgPoolConnection = diesel::r2d2::PooledConnection<ConnectionManager<PgConnection>>;
+use diesel_async::pooled_connection::bb8::Pool;
+use diesel_async::pooled_connection::AsyncDieselConnectionManager;
+use diesel_async::AsyncPgConnection;
 
-pub fn get_connection_pool() -> PgConnectionPool {
+pub type PgConnectionPool =
+    diesel_async::pooled_connection::bb8::Pool<diesel_async::AsyncPgConnection>;
+pub type PgPoolConnection<'a> =
+    diesel_async::pooled_connection::bb8::PooledConnection<'a, AsyncPgConnection>;
+
+pub async fn get_connection_pool() -> PgConnectionPool {
+    //Pool<AsyncPgConnection> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+    let manager =
+        AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(database_url);
 
     Pool::builder()
-        .test_on_check_out(true)
         .build(manager)
+        .await
         .expect("Could not build Postgres DB connection pool")
 }
