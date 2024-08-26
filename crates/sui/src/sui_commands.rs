9f60d0827f6001d40d5bcc2eb2689230b0dad2f7
Brandon Williams <brandon@mystenlabs.com>
2024-08-26 15:26:35 -0500

    sui, cluster-test: remove direct dependency on diesel

--- crates/sui/src/sui_commands.rs
+++ crates/sui/src/sui_commands.rs
@@ -700,7 +700,7 @@ async fn start(
             .map_err(|_| anyhow!("Invalid indexer host and port"))?;
         tracing::info!("Starting the indexer service at {indexer_address}");
         // Start in writer mode
-        start_test_indexer::<diesel::PgConnection>(
+        start_test_indexer(
             Some(pg_address.clone()),
             fullnode_url.clone(),
             ReaderWriterConfig::writer_mode(None),
@@ -710,7 +710,7 @@ async fn start(
         info!("Indexer in writer mode started");
 
         // Start in reader mode
-        start_test_indexer::<diesel::PgConnection>(
+        start_test_indexer(
             Some(pg_address.clone()),
             fullnode_url.clone(),
             ReaderWriterConfig::reader_mode(indexer_address.to_string()),
