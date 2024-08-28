292031e819838ba66a173e51160e7cbda78af132
Lu Zhang <8418040+longbowlu@users.noreply.github.com>
2024-08-27 20:17:58 -0700

    [bridge] e2e tests optimizations (#18892)
    ## Description 
    
    This PR does a few things to make e2e tests run faster:
    1. instead of having every node running the client, only having one of
    them do so.
    2. allow custom number of bridge nodes, and reduce the amount of nodes
    for some tests
    3. reduce the epoch length to 1 slot from 3 slots
    
    These jointly reduce the duration of the basic e2e transfer test from
    100+s to 70s.
    
    
    ## Test plan 
    
    existing tests
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/sui-bridge/src/e2e_tests/test_utils.rs
+++ crates/sui-bridge/src/e2e_tests/test_utils.rs
@@ -180,6 +180,14 @@ impl BridgeTestClusterBuilder {
         let bridge_client = SuiBridgeClient::new(&test_cluster.fullnode_handle.rpc_url)
             .await
             .unwrap();
+        info!(
+            "Bridge committee: {:?}",
+            bridge_client
+                .get_bridge_committee()
+                .await
+                .unwrap()
+                .to_string()
+        );
         BridgeTestCluster {
             num_validators: self.num_validators,
             test_cluster,
@@ -207,7 +215,6 @@ impl BridgeTestClusterBuilder {
         test_cluster
             .trigger_reconfiguration_if_not_yet_and_assert_bridge_committee_initialized()
             .await;
-        info!("Bridge committee is finalized");
         test_cluster
     }
 
@@ -519,7 +526,7 @@ pub(crate) async fn deploy_sol_contract(
     };
 
     let serialized_config = serde_json::to_string_pretty(&deploy_config).unwrap();
-    tracing::debug!(
+    tracing::info!(
         "Serialized config written to {:?}: {:?}",
         deploy_config_path,
         serialized_config
@@ -658,7 +665,7 @@ impl EthBridgeEnvironment {
             .arg("--block-time")
             .arg("1") // 1 second block time
             .arg("--slots-in-an-epoch")
-            .arg("3") // 3 slots in an epoch
+            .arg("1") // 1 slots in an epoch
             .spawn()
             .expect("Failed to start anvil");
 
@@ -753,7 +760,7 @@ pub(crate) async fn start_bridge_cluster(
             metrics_port: get_available_port("127.0.0.1"),
             bridge_authority_key_path: authority_key_path,
             approved_governance_actions,
-            run_client: true,
+            run_client: i == 0,
             db_path: Some(db_path),
             eth: EthConfig {
                 eth_rpc_url: eth_environment.rpc_url.clone(),
