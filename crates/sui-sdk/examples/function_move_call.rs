50eb98afb9606ed4e00d30000bbe27acc6b83e89
Tony Lee <chunglee2018@u.northwestern.edu>
2024-08-28 16:03:34 -0400

    update calls

--- crates/sui-sdk/examples/function_move_call.rs
+++ crates/sui-sdk/examples/function_move_call.rs
@@ -40,43 +40,68 @@ async fn main() -> Result<(), anyhow::Error> {
     // for the transaction, and find the coin we use as gas
     let (sui, sender, _recipient) = setup_for_write().await?;
 
-    // we need to find the coin we will use as gas
-    let coins = sui
-        .coin_read_api()
-        .get_coins(sender, None, None, None)
-        .await?;
+    // Find a coin to use as gas
+    let coins = sui.coin_read_api().get_coins(sender, None, None, None).await?;
     let coin = coins.data.into_iter().next().unwrap();
 
-    // 2) create a programmable transaction builder to add commands and create a PTB
+    // Create a programmable transaction builder (PTB)
     let mut ptb = ProgrammableTransactionBuilder::new();
 
-    // Create an Argument::Input for Pure 6 value of type u64
-    let input_value = 10u64;
-    let input_argument = CallArg::Pure(bcs::to_bytes(&input_value).unwrap());
+    // Define the pool address, baseCoin type, and quoteCoin type
+    let pool_address = "0x2decc59a6f05c5800e5c8a1135f9d133d1746f562bf56673e6e81ef4f7ccd3b7";
+    let pool_object_id = ObjectID::from_hex_literal(pool_address)?;
+    let pool_input = CallArg::Object(bcs::to_bytes(&pool_address).unwrap());
+    let pool_object_ref = sui.read_api().get_object(pool_object_id).await?.reference();
+    let pool_input = CallArg::Object(pool_object_ref.into());
+    ptb.input(pool_input)?;
+
+    // Create an Argument::Input for Pure 5 value of type u64
+    let tick_from_mid = 5u64;
+    let input_argument = CallArg::Pure(bcs::to_bytes(&tick_from_mid).unwrap());
 
     // Add this input to the builder
     ptb.input(input_argument)?;
 
-    // 3) add a move call to the PTB
-    // Replace the pkg_id with the package id you want to call
-    let pkg_id = "0x883393ee444fb828aa0e977670cf233b0078b41d144e6208719557cb3888244d";
+    let base_coin_type = "0x36dbef866a1d62bf7328989a10fb2f07d769f4ee587c0de4a0a256e57e0a58a8::deep::DEEP";  // Example base coin type
+    let quote_coin_type = "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI";  // Example quote coin type
+
+    // Add the Move call to the PTB
+    let pkg_id = "0xc89b2bd6172c077aec6e8d7ba201e99c32f9770cdae7be6dac9d95132fff8e8e";
     let package = ObjectID::from_hex_literal(pkg_id).map_err(|e| anyhow!(e))?;
-    let module = Identifier::new("hello_wolrd").map_err(|e| anyhow!(e))?;
-    let function = Identifier::new("hello_world").map_err(|e| anyhow!(e))?;
+    let module = Identifier::new("pool").map_err(|e| anyhow!(e))?;
+    let function = Identifier::new("get_level2_ticks_from_mid").map_err(|e| anyhow!(e))?;
+    // Convert the sui_clock_object_id string to ObjectID
+    let sui_clock_object_id = ObjectID::from_hex_literal(
+        "0x0000000000000000000000000000000000000000000000000000000000000006"
+    )?;
+
+    // Fetch the object reference using the Sui API
+    let sui_clock_object_ref = sui.read_api().get_object(sui_clock_object_id).await?.reference();
+
+    // Use the full object reference to create CallArg::Object
+    let clock_input = CallArg::Object(sui_clock_object_ref.into());
+    ptb.input(clock_input)?;
+
     ptb.command(Command::MoveCall(Box::new(ProgrammableMoveCall {
         package,
         module,
         function,
-        type_arguments: vec![],
-        arguments: vec![Argument::Input(0)],
+        type_arguments: vec![
+            Identifier::new(base_coin_type).map_err(|e| anyhow!(e))?,
+            Identifier::new(quote_coin_type).map_err(|e| anyhow!(e))?,
+        ],
+        arguments: vec![
+            Argument::Input(0), // pool.address
+            Argument::Input(1), // tickFromMid
+            Argument::Input(2), // SUI_CLOCK_OBJECT_ID
+        ],
     })));
 
-    // build the transaction block by calling finish on the ptb
+    // Build the transaction block
     let builder = ptb.finish();
 
     let gas_budget = 10_000_000;
     let gas_price = sui.read_api().get_reference_gas_price().await?;
-    // create the transaction data that will be sent to the network
     let tx_data = TransactionData::new_programmable(
         sender,
         vec![coin.object_ref()],
@@ -85,11 +110,11 @@ async fn main() -> Result<(), anyhow::Error> {
         gas_price,
     );
 
-    // 4) sign transaction
+    // Sign the transaction
     let keystore = FileBasedKeystore::new(&sui_config_dir()?.join(SUI_KEYSTORE_FILENAME))?;
     let signature = keystore.sign_secure(&sender, &tx_data, Intent::sui_transaction())?;
 
-    // 5) execute the transaction
+    // Execute the transaction
     print!("Executing the transaction...");
     let transaction_response = sui
         .quorum_driver_api()
