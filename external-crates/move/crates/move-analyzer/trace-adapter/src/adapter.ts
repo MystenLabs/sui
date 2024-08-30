e4481e94dfa7e911f676be5bf5ec574ed52fda03
Adam Welc <adam@mystenlabs.com>
2024-08-29 17:01:39 -0700

    [trace-view] Initial version of debug adapter and trace view extension (#19106)
    ## Description 
    
    Move VM tracing is currently in development
    (https://github.com/MystenLabs/sui/pull/18730) and this PR contains the
    first version of both the Move VM trace viewing VSCode extension and the
    IDE-independent [Debug Adapter Protocol
    ](https://microsoft.github.io/debug-adapter-protocol/)(DAP)
    implementation that is is responsible for actual trace analysis and
    visualization.
    
    Reviewing advice - I would suggest focusing on TypeScript files as the
    rest is largely configuration and boilerplate to get a TypeScript
    project (and, by extension, a VSCode project) going.
    
    
    ## Test plan 
    
    Tested manually by both packaging/installing extension and making sure
    that the extension itself can be debugged correctly.

---
+++ external-crates/move/crates/move-analyzer/trace-adapter/src/adapter.ts
@@ -0,0 +1,217 @@
+import {
+    Logger,
+    logger,
+    LoggingDebugSession,
+    InitializedEvent,
+    TerminatedEvent,
+    StoppedEvent,
+    Thread,
+    StackFrame,
+    Source
+} from '@vscode/debugadapter';
+import { DebugProtocol } from '@vscode/debugprotocol';
+import * as path from 'path';
+import { Runtime, RuntimeEvents } from './runtime';
+
+const enum LogLevel {
+    Log = 'log',
+    Verbose = 'verbose',
+    None = 'none'
+}
+
+/**
+ * Converts a log level string to a Logger.LogLevel.
+ *
+ * @param level log level as string
+ * @returns log level as Logger.LogLevel
+ */
+function convertLoggerLogLevel(level: string): Logger.LogLevel {
+    switch (level) {
+        case LogLevel.Log:
+            return Logger.LogLevel.Log;
+        case LogLevel.Verbose:
+            return Logger.LogLevel.Verbose;
+        default:
+            return Logger.LogLevel.Stop;
+    }
+}
+
+/**
+ * This interface describes the move-debug specific launch attributes
+ * (which are not part of the Debug Adapter Protocol).
+ * The schema for these attributes lives in the package.json of the move-debug extension.
+ * The interface should always match this schema.
+ */
+interface ILaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
+    /** An absolute path to the Move source file whose traces are to be viewed. */
+    source: string;
+    /** Trace selected for viewing. */
+    traceInfo: string;
+    /** Automatically stop target after launch. If not specified, target does not stop. */
+    stopOnEntry?: boolean;
+    /** enable logging the Debug Adapter Protocol */
+    logLevel?: string;
+}
+
+export class MoveDebugSession extends LoggingDebugSession {
+
+    private static THREAD_ID = 1;
+
+    private runtime: Runtime;
+
+    public constructor() {
+        super();
+        this.setDebuggerLinesStartAt1(false);
+        this.setDebuggerColumnsStartAt1(false);
+        this.runtime = new Runtime();
+
+        // setup event handlers
+
+        this.runtime.on(RuntimeEvents.stopOnStep, () => {
+            this.sendEvent(new StoppedEvent('step', MoveDebugSession.THREAD_ID));
+        });
+        this.runtime.on(RuntimeEvents.end, () => {
+            this.sendEvent(new TerminatedEvent());
+        });
+
+    }
+
+    protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
+
+        // build and return the capabilities of this debug adapter (enable as needed)
+        response.body = response.body || {};
+
+        // the adapter implements the configurationDone request
+        response.body.supportsConfigurationDoneRequest = false;
+
+        // make VS Code use 'evaluate' when hovering over source
+        response.body.supportsEvaluateForHovers = false;
+
+        // make VS Code show a 'step back' button
+        response.body.supportsStepBack = false;
+
+        // make VS Code support data breakpoints
+        response.body.supportsDataBreakpoints = false;
+
+        // make VS Code support completion in REPL
+        response.body.supportsCompletionsRequest = false;
+        response.body.completionTriggerCharacters = [];
+
+        // make VS Code send cancel request
+        response.body.supportsCancelRequest = false;
+
+        // make VS Code send the breakpointLocations request
+        response.body.supportsBreakpointLocationsRequest = false;
+
+        // make VS Code provide "Step in Target" functionality
+        response.body.supportsStepInTargetsRequest = false;
+
+        // the adapter defines two exceptions filters, one with support for conditions.
+        response.body.supportsExceptionFilterOptions = false;
+        response.body.exceptionBreakpointFilters = [];
+
+        // make VS Code send exceptionInfo request
+        response.body.supportsExceptionInfoRequest = false;
+
+        // make VS Code send setVariable request
+        response.body.supportsSetVariable = false;
+
+        // make VS Code send setExpression request
+        response.body.supportsSetExpression = false;
+
+        // make VS Code send disassemble request
+        response.body.supportsDisassembleRequest = false;
+        response.body.supportsSteppingGranularity = false;
+        response.body.supportsInstructionBreakpoints = false;
+
+        // make VS Code able to read and write variable memory
+        response.body.supportsReadMemoryRequest = false;
+        response.body.supportsWriteMemoryRequest = false;
+
+        response.body.supportSuspendDebuggee = false;
+        response.body.supportTerminateDebuggee = false;
+        response.body.supportsFunctionBreakpoints = false;
+        response.body.supportsDelayedStackTraceLoading = false;
+
+        this.sendResponse(response);
+        this.sendEvent(new InitializedEvent());
+    }
+
+    protected async launchRequest(response: DebugProtocol.LaunchResponse, args: ILaunchRequestArguments): Promise<void> {
+        logger.setup(convertLoggerLogLevel(args.logLevel ?? LogLevel.None), false);
+        logger.log("Launching trace viewer for file: " + args.source + " and trace: " + args.traceInfo);
+        try {
+            await this.runtime.start(args.source, args.traceInfo, args.stopOnEntry || false);
+        } catch (err) {
+            response.success = false;
+            response.message = err instanceof Error ? err.message : String(err);
+        }
+        this.sendResponse(response);
+        this.sendEvent(new StoppedEvent("entry", MoveDebugSession.THREAD_ID));
+    }
+
+    protected configurationDoneRequest(response: DebugProtocol.ConfigurationDoneResponse, args: DebugProtocol.ConfigurationDoneArguments): void {
+        this.sendResponse(response);
+    }
+
+    protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
+        response.body = {
+            threads: [
+                new Thread(MoveDebugSession.THREAD_ID, "Main Thread")
+            ]
+        };
+        this.sendResponse(response);
+    }
+
+    protected stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments): void {
+        try {
+            const runtimeStack = this.runtime.stack();
+            response.body = {
+                stackFrames: runtimeStack.frames.map(frame => {
+                    const fileName = path.basename(frame.file);
+                    return new StackFrame(frame.id, frame.name, new Source(fileName, frame.file), frame.line);
+                }).reverse(),
+                totalFrames: runtimeStack.frames.length
+            };
+        } catch (err) {
+            response.success = false;
+            response.message = err instanceof Error ? err.message : String(err);
+        }
+        this.sendResponse(response);
+    }
+
+    protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
+        try {
+            this.runtime.step(true);
+        } catch (err) {
+            response.success = false;
+            response.message = err instanceof Error ? err.message : String(err);
+        }
+        this.sendResponse(response);
+    }
+
+    protected stepInRequest(response: DebugProtocol.StepInResponse, args: DebugProtocol.StepInArguments): void {
+        let terminate = false;
+        try {
+            terminate = this.runtime.step(false);
+        } catch (err) {
+            response.success = false;
+            response.message = err instanceof Error ? err.message : String(err);
+        }
+        if (terminate) {
+            this.sendEvent(new TerminatedEvent());
+        }
+        this.sendResponse(response);
+    }
+
+    protected stepOutRequest(response: DebugProtocol.StepOutResponse, args: DebugProtocol.StepOutArguments): void {
+        this.runtime.stepOut();
+        this.sendResponse(response);
+    }
+
+    protected disconnectRequest(response: DebugProtocol.DisconnectResponse, args: DebugProtocol.DisconnectArguments): void {
+        // Cleanup and terminate the debug session
+        this.sendEvent(new TerminatedEvent());
+        this.sendResponse(response);
+    }
+}
