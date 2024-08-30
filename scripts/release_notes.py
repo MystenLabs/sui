b2eecc55f3bc24af65b6ad8139a0bf05f7729367
Eugene Boguslavsky <eugene@mystenlabs.com>
2024-08-30 13:38:30 -0700

    Move release notes validator into its own workflow (#19148)
    ## Description 
    Move the release notes validator into its own workflow
    
    ## Test plan 
    ```
    eugene@eugene-dev ~/code/sui/ (ebmifa/fix_release_notes) $ ./scripts/release_notes.py check 19148
    Found issues with release notes in 19148:
     - 'Protocol' is checked but has no release note.
    ```
    ![Screenshot 2024-08-30 at 9 07
    27 AM](https://github.com/user-attachments/assets/7665bad5-4b36-4faf-9e41-8195dc935156)
    
    ```
    eugene@eugene-dev ~/code/sui/ (ebmifa/fix_release_notes) $ ./scripts/release_notes.py generate releases/sui-v1.31.0-release releases/sui-v1.32.0-release
    ## Protocol
    #### Sui Protocol Version in this release: `55`
    
    https://github.com/MystenLabs/sui/pull/19031:
    Enable Move enums in mainnet
    
    ## Indexer
    
    https://github.com/MystenLabs/sui/pull/18899:
    ....
    ```
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [x] Protocol:
    - [x] Nodes (Validators and Full nodes): and here
    - [x] Indexer: and here
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- scripts/release_notes.py
+++ scripts/release_notes.py
@@ -61,11 +61,9 @@ NOTE_ORDER = [
     "REST API",
 ]
 
-
 class Note(NamedTuple):
     checked: bool
     note: str
-
 
 def parse_args():
     """Parse command line arguments."""
@@ -99,47 +97,27 @@ def parse_args():
     check_p = sub_parser.add_parser(
         "check",
         description=(
-            "Check if the release notes section of a givne commit is complete, "
+            "Check if the release notes section of a the PR is complete, "
             "i.e. that every impacted component has a non-empty note."
         ),
     )
 
     check_p.add_argument(
-        "commit",
+        "pr",
         nargs="?",
-        default="HEAD",
-        help="The commit to check, defaults to HEAD.",
+        help="The PR to check.",
     )
 
     return vars(parser.parse_args())
-
 
 def git(*args):
     """Run a git command and return the output as a string."""
     return subprocess.check_output(["git"] + list(args)).decode().strip()
 
-
-def extract_notes(commit):
-    """Get release notes from a commit message.
-
-    Find the 'Release notes' section in the commit message, and
-    extract the notes for each impacted area (area that has been
-    ticked).
-
-    Returns a tuple of the PR number and a dictionary of impacted
-    areas mapped to their release note. Each release note indicates
-    whether it has a note and whether it was checked (ticked).
-
-    """
-    message = git("show", "-s", "--format=%B", commit)
-
-    # Extract PR number
-    match = RE_PR.match(message)
-    pr = match.group(1) if match else None
+def parse_notes(pr, notes):
+    # # Find the release notes section
     result = {}
-
-    # Find the release notes section
-    match = RE_HEADING.search(message)
+    match = RE_HEADING.search(notes)
     if not match:
         return pr, result
 
@@ -168,6 +146,50 @@ def extract_notes(commit):
 
     return pr, result
 
+def extract_notes_for_pr(pr):
+    """Get release notes from a body of the PR
+
+    Find the 'Release notes' section in PR body, and
+    extract the notes for each impacted area (area that has been
+    ticked).
+
+    Returns a tuple of the PR number and a dictionary of impacted
+    areas mapped to their release note. Each release note indicates
+    whether it has a note and whether it was checked (ticked).
+
+    """
+
+    url = f"https://api.github.com/repos/MystenLabs/sui/pulls/{pr}"
+    curl_command = [
+        "curl", "-s",
+        "-H", "Accept: application/vnd.github.groot-preview+json",
+        url
+    ]
+
+    # Execute the curl command
+    result = subprocess.run(curl_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    json_data = json.loads(result.stdout)
+    notes = json_data.get("body")
+    return parse_notes(pr, notes)
+
+def extract_notes_for_commit(commit):
+    """Get release notes from a commit message.
+
+    Find the 'Release notes' section in the commit message, and
+    extract the notes for each impacted area (area that has been
+    ticked).
+
+    Returns a tuple of the PR number and a dictionary of impacted
+    areas mapped to their release note. Each release note indicates
+    whether it has a note and whether it was checked (ticked).
+
+    """
+    message = git("show", "-s", "--format=%B", commit)
+
+    # Extract PR number
+    match = RE_PR.match(message)
+    pr = match.group(1) if match else None
+    return parse_notes(pr, message)
 
 def extract_protocol_version(commit):
     """Find the max protocol version at this commit.
@@ -189,23 +211,22 @@ def extract_protocol_version(commit):
 
         return match[0]
 
-
 def print_changelog(pr, log):
     if pr:
         print(f"https://github.com/MystenLabs/sui/pull/{pr}:")
     print(log)
 
 
-def do_check(commit):
-    """Check if the release notes section of a given commit is complete.
+def do_check(pr):
+    """Check if the release notes section of a given PR is complete.
 
     This means that every impacted component has a non-empty note,
     every note is attached to a checked checkbox, and every impact
     area is known.
 
     """
-
-    _, notes = extract_notes(commit)
+    
+    pr, notes = extract_notes_for_pr(pr)
     issues = []
     for impacted, note in notes.items():
         if impacted not in NOTE_ORDER:
@@ -222,11 +243,10 @@ def do_check(commit):
     if not issues:
         return
 
-    print(f"Found issues with release notes in {commit}:")
+    print(f"Found issues with release notes in PR {pr}:")
     for issue in issues:
         print(issue)
     sys.exit(1)
-
 
 def do_generate(from_, to):
     """Generate release notes from git commits.
@@ -261,7 +281,7 @@ def do_generate(from_, to):
         return
 
     for commit in commits.split("\n"):
-        pr, notes = extract_notes(commit)
+        pr, notes = extract_notes_for_commit(commit)
         for impacted, note in notes.items():
             if note.checked:
                 results[impacted].append((pr, note.note))
@@ -289,9 +309,8 @@ def do_generate(from_, to):
             print_changelog(pr, note)
             print()
 
-
 args = parse_args()
 if args["command"] == "generate":
     do_generate(args["from"], args["to"])
 elif args["command"] == "check":
-    do_check(args["commit"])
+    do_check(args["pr"])
