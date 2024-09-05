39fc97d488487afc78f3de4f7c49fe8258e51152
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2024-09-05 17:14:11 -0400

    expose api

--- crates/sui-light-client/src/lib.rs
+++ crates/sui-light-client/src/lib.rs
@@ -9,3 +9,41 @@ pub use proof::*;
 
 #[doc(inline)]
 pub use construct::*;
+use serde_json::{json, Value};
+
+pub async fn query_last_checkpoint_of_epoch(graphql_url: &str, epoch_id: u64) -> anyhow::Result<u64> {
+    // GraphQL query to get the last checkpoint of an epoch
+    let query = json!({
+        "query": "query ($epochID: Int) { epoch(id: $epochID) { checkpoints(last: 1) { nodes { sequenceNumber } } } }",
+        "variables": { "epochID": epoch_id }
+    });
+
+    // Submit the query by POSTing to the GraphQL endpoint
+    let client = reqwest::Client::new();
+    let resp = client
+        .post(graphql_url)
+        .header("Content-Type", "application/json")
+        .body(query.to_string())
+        .send()
+        .await
+        .expect("Cannot connect to graphql")
+        .text()
+        // .json::<HashMap<String, String>>()
+        .await
+        .expect("Cannot parse response");
+
+    // Parse the JSON response to get the last checkpoint of the epoch
+    let v: Value = serde_json::from_str(resp.as_str()).expect("Incorrect JSON response");
+    let checkpoint_number = v["data"]["epoch"]["checkpoints"]["nodes"][0]["sequenceNumber"]
+        .as_u64()
+        .unwrap();
+
+    Ok(checkpoint_number)
+}
+
+// The list of checkpoints at the end of each epoch
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub struct CheckpointsList {
+    // List of end of epoch checkpoints
+    pub checkpoints: Vec<u64>,
+}
--- crates/sui-light-client/src/main.rs
+++ crates/sui-light-client/src/main.rs
@@ -6,6 +6,7 @@ use async_trait::async_trait;
 use move_core_types::account_address::AccountAddress;
 use sui_json_rpc_types::{SuiObjectDataOptions, SuiTransactionBlockResponseOptions};
 
+use sui_light_client::{query_last_checkpoint_of_epoch, CheckpointsList};
 use sui_rest_api::CheckpointData;
 use sui_types::{
     base_types::ObjectID,
@@ -32,8 +33,6 @@ use std::{io::Read, sync::Arc};
 use log::info;
 use object_store::parse_url;
 use object_store::path::Path;
-use serde_json::json;
-use serde_json::Value;
 use url::Url;
 
 /// A light client for the Sui blockchain
@@ -122,43 +121,6 @@ struct Config {
 
     /// GraphQL endpoint
     graphql_url: String,
-}
-
-pub async fn query_last_checkpoint_of_epoch(graphql_url: &str, epoch_id: u64) -> anyhow::Result<u64> {
-    // GraphQL query to get the last checkpoint of an epoch
-    let query = json!({
-        "query": "query ($epochID: Int) { epoch(id: $epochID) { checkpoints(last: 1) { nodes { sequenceNumber } } } }",
-        "variables": { "epochID": epoch_id }
-    });
-
-    // Submit the query by POSTing to the GraphQL endpoint
-    let client = reqwest::Client::new();
-    let resp = client
-        .post(graphql_url)
-        .header("Content-Type", "application/json")
-        .body(query.to_string())
-        .send()
-        .await
-        .expect("Cannot connect to graphql")
-        .text()
-        // .json::<HashMap<String, String>>()
-        .await
-        .expect("Cannot parse response");
-
-    // Parse the JSON response to get the last checkpoint of the epoch
-    let v: Value = serde_json::from_str(resp.as_str()).expect("Incorrect JSON response");
-    let checkpoint_number = v["data"]["epoch"]["checkpoints"]["nodes"][0]["sequenceNumber"]
-        .as_u64()
-        .unwrap();
-
-    Ok(checkpoint_number)
-}
-
-// The list of checkpoints at the end of each epoch
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
-struct CheckpointsList {
-    // List of end of epoch checkpoints
-    checkpoints: Vec<u64>,
 }
 
 fn read_checkpoint_list(config: &Config) -> anyhow::Result<CheckpointsList> {
