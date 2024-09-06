96a7b2315ec4771b51fe1f63edb02526ac8af1ea
Brandon Williams <brandon@mystenlabs.com>
2024-09-06 15:08:12 -0500

    indexer: add async transaction_with_retry function
    Introduce an async version of the transactional_blocking_with_retry
    macro but in function form for improved readability.

--- crates/sui-indexer/src/store/mod.rs
+++ crates/sui-indexer/src/store/mod.rs
@@ -1,8 +1,13 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::time::Duration;
+
+use diesel_async::{scoped_futures::ScopedBoxFuture, AsyncPgConnection};
 pub(crate) use indexer_store::*;
 pub use pg_indexer_store::PgIndexerStore;
+
+use crate::{database::ConnectionPool, errors::IndexerError};
 
 pub mod indexer_store;
 pub mod package_resolver;
@@ -231,4 +236,43 @@ pub mod diesel_macro {
     pub use run_query_repeatable_async;
     pub use spawn_read_only_blocking;
     pub use transactional_blocking_with_retry;
+}
+
+pub async fn transaction_with_retry<'a, Q, T>(
+    pool: &ConnectionPool,
+    timeout: Duration,
+    query: Q,
+) -> Result<T, IndexerError>
+where
+    Q: for<'r> FnOnce(
+            &'r mut AsyncPgConnection,
+        ) -> ScopedBoxFuture<'a, 'r, Result<T, IndexerError>>
+        + Send,
+    Q: Clone,
+    T: 'a,
+{
+    let backoff = backoff::ExponentialBackoff {
+        max_elapsed_time: Some(timeout),
+        ..Default::default()
+    };
+    backoff::future::retry(backoff, || async {
+        let mut connection = pool.get().await.map_err(|e| backoff::Error::Transient {
+            err: IndexerError::PostgresWriteError(e.to_string()),
+            retry_after: None,
+        })?;
+
+        connection
+            .build_transaction()
+            .read_write()
+            .run(query.clone())
+            .await
+            .map_err(|e| {
+                tracing::error!("Error with persisting data into DB: {:?}, retrying...", e);
+                backoff::Error::Transient {
+                    err: IndexerError::PostgresWriteError(e.to_string()),
+                    retry_after: None,
+                }
+            })
+    })
+    .await
 }
