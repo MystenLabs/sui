695869360cc3bd951c4544e89369012d46707a88
Brandon Williams <brandon@mystenlabs.com>
2024-09-10 09:41:48 -0500

    m

--- crates/sui-graphql-e2e-tests/tests/tests.rs
+++ crates/sui-graphql-e2e-tests/tests/tests.rs
@@ -12,7 +12,7 @@ pub const TEST_DIR: &str = "tests";
 
 datatest_stable::harness!(run_test, TEST_DIR, r".*\.(mvir|move)$");
 
-#[cfg_attr(not(msim), tokio::main)]
+#[tokio::main]
 async fn run_test(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     telemetry_subscribers::init_for_testing();
     run_test_impl::<SuiTestAdapter>(path, Some(Arc::new(PRE_COMPILED.clone()))).await?;
