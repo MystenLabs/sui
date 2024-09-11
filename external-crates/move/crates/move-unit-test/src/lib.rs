b281fbbd9de33f050660421c36bc40daab459ab2
Tim Zakian <2895723+tzakian@users.noreply.github.com>
2024-09-11 11:07:35 -0700

    [move] Tracing tests and plumbing into the Move CLI (_not_ Sui CLI yet!) (#18730)
    ## Description 
    
    Plumbs tracing into the Move unit tests, and Move CLI (_not_ the Sui CLI
    just yet!).
    
    You can now run with `move test --trace-execution` or `move test
    --trace-execution <dir>`. If the Move CLI binary was compiled with the
    `gas-profiled` feature flag enabled this will result in a trace file for
    each unit test either under `<move-pkg>/traces/` or under
    `<move-pkg>/<dir>/`. Once we're happy with this, and we've kicked the
    tires a bit more + moved over test coverage and gas profiling plumbing
    this up into the Sui CLI is something we should do.
    
    
    This additionally adds a new test to the `./tests.sh` script for
    external crates to compile and run tracing tests with the `gas-profiler`
    flag set.
    
    ### PR Stack:
    1. #18727
    2. #18729
    3. #18730 **<<<<< You are here**
    
    
    ## Test plan 
    
    Added a number of tests.
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- external-crates/move/crates/move-unit-test/src/lib.rs
+++ external-crates/move/crates/move-unit-test/src/lib.rs
@@ -32,6 +32,7 @@ const DEFAULT_RAND_ITERS: u64 = 10;
 
 const RAND_NUM_ITERS_FLAG: &str = "rand-num-iters";
 const SEED_FLAG: &str = "seed";
+const TRACE_FLAG: &str = "trace-execution";
 
 #[derive(Debug, Parser, Clone)]
 #[clap(author, version, about)]
@@ -111,6 +112,10 @@ pub struct UnitTestingConfig {
     // WARNING: You should only use this flag for debugging and meta-testing purposes!
     #[clap(skip)]
     pub deterministic_generation: bool,
+
+    // Enable tracing for tests
+    #[clap(long = TRACE_FLAG, value_name = "PATH")]
+    pub trace_execution: Option<Option<String>>,
 }
 
 fn format_module_id(
@@ -141,6 +146,7 @@ impl UnitTestingConfig {
             rand_num_iters: Some(DEFAULT_RAND_ITERS),
             seed: None,
             deterministic_generation: false,
+            trace_execution: None,
         }
     }
 
@@ -237,6 +243,11 @@ impl UnitTestingConfig {
         }
 
         writeln!(shared_writer.lock().unwrap(), "Running Move unit tests")?;
+        let trace_location = match &self.trace_execution {
+            Some(None) => Some("traces".to_string()),
+            Some(Some(path)) => Some(path.clone()),
+            None => None,
+        };
         let mut test_runner = TestRunner::new(
             self.gas_limit.unwrap_or(DEFAULT_EXECUTION_BOUND),
             self.num_threads,
@@ -244,6 +255,7 @@ impl UnitTestingConfig {
             self.seed,
             rand_num_iters,
             self.deterministic_generation,
+            trace_location,
             test_plan,
             native_function_table,
             cost_table,
