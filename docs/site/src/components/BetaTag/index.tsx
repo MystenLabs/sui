d5dc35488063c4f40ee9c758d84949aab3bf52e1
Ronny Roland <ronmilton@mystenlabs.com>
2024-09-13 11:59:31 -0600

    Updates from feedback

--- docs/site/src/components/BetaTag/index.tsx
+++ docs/site/src/components/BetaTag/index.tsx
@@ -20,16 +20,19 @@ export default function BetaTag(props) {
       className="!my-12 bg-sui-blue-light border-sui-blue-dark dark:bg-sui-blue-dark dark:border-sui-blue-light"
     >
       <p className="pt-2">
-        The content in this topic describes a beta feature. Beta features are in
-        active development, so feature details are subject to change before the
-        feature is merged into Mainnet.
+        The content in this topic describes a beta feature or service. Beta
+        features and services are in active development, so details are likely
+        to change.
       </p>
-      {(beta.includes("testnet") || beta.includes("devnet")) && (
+      {(beta.includes("testnet") ||
+        beta.includes("devnet") ||
+        beta.includes("mainnet")) && (
         <p>
-          This feature is currently available in
+          This feature or service is currently available in
           <ul className="mt-4">
             {beta.includes("devnet") && <li className="font-bold">Devnet</li>}
             {beta.includes("testnet") && <li className="font-bold">Testnet</li>}
+            {beta.includes("mainnet") && <li className="font-bold">Mainnet</li>}
           </ul>
         </p>
       )}
