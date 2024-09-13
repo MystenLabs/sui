e5703e337b613a5acc4bcc6c87de89c6d5456235
nikos-kitmeridis <145983019+nikos-kitmeridis@users.noreply.github.com>
2024-09-13 09:25:28 +0300

    Adds configuration for One (#19339)
    ## Description 
    
    Adds configuration for One
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/sui-config/src/node.rs
+++ crates/sui-config/src/node.rs
@@ -272,6 +272,7 @@ pub fn default_zklogin_oauth_providers() -> BTreeMap<Chain, BTreeSet<String>> {
         "Credenza3".to_string(),
         "Playtron".to_string(),
         "Threedos".to_string(),
+        "Onefc".to_string(),
         "AwsTenant-region:us-east-1-tenant_id:us-east-1_LPSLCkC3A".to_string(), // test tenant in mysten aws
         "AwsTenant-region:us-east-1-tenant_id:us-east-1_qPsZxYqd8".to_string(), // ambrus, external partner
     ]);
