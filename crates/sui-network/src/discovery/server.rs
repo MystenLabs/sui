90f67311a2f03fece8ac13ac1b0ddc813ab0d64d
Brandon Williams <brandon@mystenlabs.com>
2024-09-23 09:32:20 -0500

    discovery: limit the number of peers shared during discovery

--- crates/sui-network/src/discovery/server.rs
+++ crates/sui-network/src/discovery/server.rs
@@ -1,10 +1,14 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use super::{Discovery, NodeInfo, State};
+use super::{Discovery, NodeInfo, State, MAX_PEERS_TO_SEND};
 use anemo::{Request, Response};
+use rand::seq::IteratorRandom;
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, RwLock};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GetKnownPeersResponse {
@@ -27,7 +31,32 @@ impl Discovery for Server {
             .our_info
             .clone()
             .ok_or_else(|| anemo::rpc::Status::internal("own_info has not been initialized yet"))?;
-        let known_peers = state.known_peers.values().cloned().collect();
+
+        // prefer returning peers that we are connected to as they are known-good
+        let mut known_peers = state
+            .connected_peers
+            .keys()
+            .filter_map(|peer_id| state.known_peers.get(peer_id))
+            .map(|info| (info.peer_id, info))
+            .take(MAX_PEERS_TO_SEND)
+            .collect::<HashMap<_, _>>();
+
+        if known_peers.len() <= MAX_PEERS_TO_SEND {
+            // Fill the remaining space with other peers, randomly sampling at most MAX_PEERS_TO_SEND
+            for info in state
+                .known_peers
+                .values()
+                .choose_multiple(&mut rand::thread_rng(), MAX_PEERS_TO_SEND)
+            {
+                if known_peers.len() >= MAX_PEERS_TO_SEND {
+                    break;
+                }
+
+                known_peers.insert(info.peer_id, info);
+            }
+        }
+
+        let known_peers = known_peers.into_values().cloned().collect();
 
         Ok(Response::new(GetKnownPeersResponse {
             own_info,
