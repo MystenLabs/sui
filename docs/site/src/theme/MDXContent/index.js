372fbfb273462bebf560e6f834194b2847a07edd
Ronny Roland <ronmilton@mystenlabs.com>
2024-09-23 19:02:54 -0600

    Removing ref to component not yet ready

--- docs/site/src/theme/MDXContent/index.js
+++ docs/site/src/theme/MDXContent/index.js
@@ -8,7 +8,6 @@ import TabItem from "@theme/TabItem";
 import { Card, Cards } from "@site/src/components/Cards";
 import EffortBox from "@site/src/components/EffortBox";
 import BetaTag from "@site/src/components/BetaTag";
-import { GlobalDataProvider } from "@site/src/components/GlobalDataProvider";
 export default function MDXContent({ children }) {
   const suiComponents = {
     ...MDXComponents,
@@ -18,7 +17,6 @@ export default function MDXContent({ children }) {
     TabItem,
     EffortBox,
     BetaTag,
-    GlobalDataProvider,
   };
   return <MDXProvider components={suiComponents}>{children}</MDXProvider>;
 }
