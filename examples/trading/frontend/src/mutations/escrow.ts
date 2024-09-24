4896747babb94170326c4e09de6eb34f09047e48
Ronny Roland <ronmilton@mystenlabs.com>
2024-09-24 08:50:49 -0600

    Missing header

--- examples/trading/frontend/src/mutations/escrow.ts
+++ examples/trading/frontend/src/mutations/escrow.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 import { CONSTANTS, QueryKey } from "@/constants";
 import { useTransactionExecution } from "@/hooks/useTransactionExecution";
 import { ApiEscrowObject, ApiLockedObject } from "@/types/types";
