1caa931f3480e98ea8de107d08c4f6ceb487e3bc
Rijnard van Tonder <rvantonder@gmail.com>
2024-09-26 12:48:54 -0700

    move: external resolver reads multiple lock contents

---
+++ external-crates/move/crates/move-package/tests/test_sources/resolvers/successful_package_batch_response.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# Copyright (c) The Move Contributors
+# SPDX-License-Identifier: Apache-2.0
+
+ROOT="$(git rev-parse --show-toplevel)"
+TYPE="dependencies"
+PACKAGE="$2"
+
+# Print Diagnostics for test output
+cat <<EOF >&2
+Successful External Resolver
+PWD:     $(pwd | sed "s,^$ROOT,\$ROOT,")
+Type:    $TYPE
+Package: $PACKAGE
+EOF
+
+foo=$(cat <<'EOF'
+[move]
+version = 3
+manifest_digest = "42"
+deps_digest = "7"
+dependencies = [
+    { id = "foo", name = "foo" },
+]
+
+[[move.package]]
+id = "foo"
+source = { local = "./deps_only/foo" }
+EOF
+)
+
+bar=$(cat <<'EOF'
+[move]
+version = 3
+manifest_digest = "42"
+deps_digest = "7"
+dependencies = [
+    { id = "bar", name = "bar" },
+]
+
+[[move.package]]
+id = "bar"
+source = { local = "./deps_only/bar" }
+EOF
+)
+
+# Echo the two separate graph contents twice with a null separator in between
+echo "$foo\0$bar"
