d8daa73812ac73c83a8344795ef4bfbf0ea958fa
Ge Gao <ge@mystenlabs.com>
2024-09-27 17:31:58 -0400

    seg trait and snap

--- crates/sui-indexer/src/handlers/mod.rs
+++ crates/sui-indexer/src/handlers/mod.rs
@@ -3,11 +3,18 @@
 
 use std::collections::BTreeMap;
 
+use async_trait::async_trait;
+use futures::{FutureExt, StreamExt};
+
+use sui_rest_api::CheckpointData;
+use tokio_util::sync::CancellationToken;
+
 use crate::{
+    errors::IndexerError,
     models::{display::StoredDisplay, obj_indices::StoredObjectVersion},
     types::{
         EventIndex, IndexedCheckpoint, IndexedDeletedObject, IndexedEpochInfo, IndexedEvent,
-        IndexedObject, IndexedPackage, IndexedTransaction, TxIndex,
+        IndexedObject, IndexedPackage, IndexedTransaction, IndexerResult, TxIndex,
     },
 };
 
@@ -16,6 +23,8 @@ pub mod committer;
 pub mod objects_snapshot_processor;
 pub mod pruner;
 pub mod tx_processor;
+
+pub(crate) const CHECKPOINT_COMMIT_BATCH_SIZE: usize = 100;
 
 #[derive(Debug)]
 pub struct CheckpointDataToCommit {
@@ -43,4 +52,104 @@ pub struct EpochToCommit {
     pub last_epoch: Option<IndexedEpochInfo>,
     pub new_epoch: IndexedEpochInfo,
     pub network_total_transactions: u64,
+}
+
+#[async_trait]
+pub trait Handler<T>: Send + Sync {
+    async fn start_transform_and_load(
+        &self,
+        cp_receiver: mysten_metrics::metered_channel::Receiver<CheckpointData>,
+        cancel: CancellationToken,
+    ) -> IndexerResult<()> {
+        let checkpoint_commit_batch_size = std::env::var("CHECKPOINT_COMMIT_BATCH_SIZE")
+            .unwrap_or(CHECKPOINT_COMMIT_BATCH_SIZE.to_string())
+            .parse::<usize>()
+            .unwrap();
+        let mut stream = mysten_metrics::metered_channel::ReceiverStream::new(cp_receiver)
+            .ready_chunks(checkpoint_commit_batch_size);
+
+        let mut unprocessed = BTreeMap::new();
+        let mut batch: Vec<CheckpointData> = vec![];
+        let mut next_cp_to_process = self
+            .get_watermark_hi()
+            .await?
+            .map(|n| n.saturating_add(1))
+            .unwrap_or_default();
+
+        loop {
+            if cancel.is_cancelled() {
+                return Ok(());
+            }
+
+            // Try to fetch new checkpoints from the stream
+            match stream.next().now_or_never() {
+                Some(Some(indexed_checkpoint_batch)) => {
+                    if cancel.is_cancelled() {
+                        return Ok(());
+                    }
+                    for checkpoint in indexed_checkpoint_batch {
+                        unprocessed
+                            .insert(checkpoint.checkpoint_summary.sequence_number, checkpoint);
+                    }
+                }
+                Some(None) => break, // Stream has ended
+                None => {}           // No new checkpoints available right now
+            }
+
+            // Process unprocessed checkpoints, even no new checkpoints from stream
+            let checkpoint_lag_limiter = self.get_checkpoint_lag_limiter().await?;
+            while next_cp_to_process <= checkpoint_lag_limiter {
+                if let Some(checkpoint) = unprocessed.remove(&next_cp_to_process) {
+                    batch.push(checkpoint);
+                    next_cp_to_process += 1;
+                } else {
+                    break;
+                }
+            }
+
+            if !batch.is_empty() {
+                let last_checkpoint_seq = batch.last().unwrap().checkpoint_summary.sequence_number;
+                let transformed_data = self.transform(batch).await.map_err(|e| {
+                    IndexerError::DataTransformationError(format!(
+                        "Failed to transform checkpoint batch: {}. Handler: {}",
+                        e,
+                        self.name()
+                    ))
+                })?;
+                self.load(transformed_data).await.map_err(|e| {
+                    IndexerError::PostgresWriteError(format!(
+                        "Failed to load transformed data into DB for handler {}: {}",
+                        self.name(),
+                        e
+                    ))
+                })?;
+                self.set_watermark_hi(last_checkpoint_seq).await?;
+                batch = vec![];
+            }
+        }
+        Err(IndexerError::ChannelClosed(format!(
+            "Checkpoint channel is closed unexpectedly for handler {}",
+            self.name()
+        )))
+    }
+
+    /// return handler name
+    fn name(&self) -> String;
+
+    /// transform data from `CheckpointData` to .*ToCommit
+    async fn transform(&self, cp_batch: Vec<CheckpointData>) -> IndexerResult<Vec<T>>;
+
+    /// commit .*ToCommit to DB
+    async fn load(&self, tranformed_data: Vec<T>) -> IndexerResult<()>;
+
+    /// read high watermark of the table DB
+    async fn get_watermark_hi(&self) -> IndexerResult<Option<u64>>;
+
+    /// set high watermark of the table DB, also update metrics.
+    async fn set_watermark_hi(&self, watermark_hi: u64) -> IndexerResult<()>;
+
+    /// get checkpoint lag limiter, for handlers without lag limiter, return u64::MAX
+    async fn get_checkpoint_lag_limiter(&self) -> IndexerResult<u64> {
+        Ok(u64::MAX)
+    }
 }
--- crates/sui-indexer/src/handlers/objects_snapshot_processor.rs
+++ crates/sui-indexer/src/handlers/objects_snapshot_processor.rs
@@ -2,13 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use async_trait::async_trait;
-use futures::StreamExt;
 use mysten_metrics::get_metrics;
-use mysten_metrics::metered_channel::{Receiver, Sender};
+use mysten_metrics::metered_channel::Sender;
 use mysten_metrics::spawn_monitored_task;
 use sui_data_ingestion_core::Worker;
 use sui_rest_api::CheckpointData;
-use sui_types::messages_checkpoint::CheckpointSequenceNumber;
 use tokio_util::sync::CancellationToken;
 use tracing::info;
 
@@ -16,14 +14,16 @@ use crate::config::SnapshotLagConfig;
 use crate::store::PgIndexerStore;
 use crate::types::IndexerResult;
 use crate::{metrics::IndexerMetrics, store::IndexerStore};
-use std::collections::HashMap;
 
 use super::checkpoint_handler::CheckpointHandler;
+use super::Handler;
 use super::TransactionObjectChangesToCommit;
 
-pub struct ObjectsSnapshotProcessor {
+#[derive(Clone)]
+pub struct ObjectsSnapshotHandler {
     pub store: PgIndexerStore,
-    pub indexed_obj_sender: Sender<CheckpointObjectChanges>,
+    pub cp_sender: Sender<CheckpointData>,
+    snapshot_config: SnapshotLagConfig,
     metrics: IndexerMetrics,
 }
 
@@ -33,154 +33,103 @@ pub struct CheckpointObjectChanges {
 }
 
 #[async_trait]
-impl Worker for ObjectsSnapshotProcessor {
+impl Worker for ObjectsSnapshotHandler {
+    // ?? change Worker trait to use Arc<CheckpointData> to avoid clone
     async fn process_checkpoint(&self, checkpoint: &CheckpointData) -> anyhow::Result<()> {
-        let checkpoint_sequence_number = checkpoint.checkpoint_summary.sequence_number;
-        // Index the object changes and send them to the committer.
-        let object_changes: TransactionObjectChangesToCommit =
-            CheckpointHandler::index_objects(checkpoint, &self.metrics).await?;
-        self.indexed_obj_sender
-            .send(CheckpointObjectChanges {
-                checkpoint_sequence_number,
-                object_changes,
-            })
-            .await?;
+        self.cp_sender.send(checkpoint.clone()).await?;
         Ok(())
     }
 }
 
-// Start both the ingestion pipeline and committer for objects snapshot table.
-pub async fn start_objects_snapshot_processor(
+#[async_trait]
+impl Handler<TransactionObjectChangesToCommit> for ObjectsSnapshotHandler {
+    fn name(&self) -> String {
+        "objects_snapshot_handler".to_string()
+    }
+
+    async fn transform(
+        &self,
+        cp_batch: Vec<CheckpointData>,
+    ) -> IndexerResult<Vec<TransactionObjectChangesToCommit>> {
+        futures::future::join_all(cp_batch.into_iter().map(|checkpoint| {
+            let metrics = self.metrics.clone();
+            async move { CheckpointHandler::index_objects(&checkpoint, &metrics).await }
+        }))
+        .await
+        .into_iter()
+        .collect::<Result<Vec<TransactionObjectChangesToCommit>, _>>()
+    }
+
+    async fn load(
+        &self,
+        transformed_data: Vec<TransactionObjectChangesToCommit>,
+    ) -> IndexerResult<()> {
+        self.store
+            .persist_objects_snapshot(transformed_data)
+            .await?;
+        Ok(())
+    }
+
+    // TODO: read watermark table when it's ready.
+    async fn get_watermark_hi(&self) -> IndexerResult<Option<u64>> {
+        self.store
+            .get_latest_object_snapshot_checkpoint_sequence_number()
+            .await
+    }
+
+    // TODO: update watermark table when it's ready.
+    async fn set_watermark_hi(&self, watermark_hi: u64) -> IndexerResult<()> {
+        self.metrics
+            .latest_object_snapshot_sequence_number
+            .set(watermark_hi as i64);
+        Ok(())
+    }
+
+    async fn get_checkpoint_lag_limiter(&self) -> IndexerResult<u64> {
+        let latest_checkpoint = self.store.get_latest_checkpoint_sequence_number().await?;
+        Ok(latest_checkpoint
+            .map(|seq| seq.saturating_sub(self.snapshot_config.snapshot_min_lag as u64))
+            .unwrap_or_default()) // hold snapshot handler until at least one checkpoint is in DB
+    }
+}
+
+pub async fn start_objects_snapshot_handler(
     store: PgIndexerStore,
     metrics: IndexerMetrics,
     snapshot_config: SnapshotLagConfig,
     cancel: CancellationToken,
-) -> IndexerResult<(ObjectsSnapshotProcessor, u64)> {
-    info!("Starting object snapshot processor...");
-
-    let watermark = store
-        .get_latest_object_snapshot_checkpoint_sequence_number()
-        .await
-        .expect("Failed to get latest snapshot checkpoint sequence number from DB")
-        .map(|seq| seq + 1)
-        .unwrap_or_default();
+) -> IndexerResult<(ObjectsSnapshotHandler, u64)> {
+    info!("Starting object snapshot handler...");
 
     let global_metrics = get_metrics().unwrap();
-    // Channel for actually communicating indexed object changes between the ingestion pipeline and the committer.
-    let (indexed_obj_sender, indexed_obj_receiver) = mysten_metrics::metered_channel::channel(
-        // TODO: placeholder for now
+    let (cp_sender, cp_receiver) = mysten_metrics::metered_channel::channel(
         600,
         &global_metrics
             .channel_inflight
-            .with_label_values(&["obj_indexing_for_snapshot"]),
+            .with_label_values(&["objects_snapshot_handler_checkpoint_data"]),
     );
 
-    // Start an ingestion pipeline with the objects snapshot processor as a worker.
-    let worker = ObjectsSnapshotProcessor::new(store.clone(), indexed_obj_sender, metrics.clone());
+    let objects_snapshot_handler =
+        ObjectsSnapshotHandler::new(store.clone(), cp_sender, metrics.clone(), snapshot_config);
 
-    // Now start the task that will commit the indexed object changes to the store.
-    spawn_monitored_task!(ObjectsSnapshotProcessor::commit_objects_snapshot(
-        store,
-        watermark,
-        indexed_obj_receiver,
-        metrics,
-        snapshot_config,
-        cancel,
-    ));
-    Ok((worker, watermark))
+    let watermark_hi = objects_snapshot_handler.get_watermark_hi().await?;
+    let handler_clone = objects_snapshot_handler.clone();
+    spawn_monitored_task!(handler_clone.start_transform_and_load(cp_receiver, cancel,));
+    Ok((objects_snapshot_handler, watermark_hi.unwrap_or_default()))
 }
 
-impl ObjectsSnapshotProcessor {
+impl ObjectsSnapshotHandler {
     pub fn new(
         store: PgIndexerStore,
-        indexed_obj_sender: Sender<CheckpointObjectChanges>,
+        cp_sender: Sender<CheckpointData>,
         metrics: IndexerMetrics,
-    ) -> ObjectsSnapshotProcessor {
+        snapshot_config: SnapshotLagConfig,
+    ) -> ObjectsSnapshotHandler {
         Self {
             store,
-            indexed_obj_sender,
+            cp_sender,
             metrics,
-        }
-    }
-
-    // Receives object changes from the ingestion pipeline and commits them to the store,
-    // keeping the appropriate amount of checkpoint lag behind the rest of the indexer.
-    pub async fn commit_objects_snapshot(
-        store: PgIndexerStore,
-        watermark: CheckpointSequenceNumber,
-        indexed_obj_receiver: Receiver<CheckpointObjectChanges>,
-        metrics: IndexerMetrics,
-        config: SnapshotLagConfig,
-        cancel: CancellationToken,
-    ) -> IndexerResult<()> {
-        let batch_size = 100;
-        let mut stream = mysten_metrics::metered_channel::ReceiverStream::new(indexed_obj_receiver)
-            .ready_chunks(batch_size);
-
-        let mut start_cp = watermark;
-        // To prevent the processor from committing more data than allowed by the min lag, keep an
-        // in-memory buffer of changes that should not be committed to `objects_snapshot` yet.
-        let mut unprocessed = HashMap::new();
-        let mut batch = vec![];
-
-        info!("Starting objects snapshot committer...");
-        loop {
-            tokio::select! {
-                _ = cancel.cancelled() => {
-                    info!("Shutdown signal received, terminating object snapshot processor");
-                    return Ok(());
-                }
-                _ = tokio::time::sleep(std::time::Duration::from_secs(config.sleep_duration)) => {
-                    let latest_indexer_cp = store
-                        .get_latest_checkpoint_sequence_number()
-                        .await?
-                        .unwrap_or_default();
-
-                    // We update the snapshot table when it falls behind the rest of the indexer by
-                    // more than the min_lag. When `latest_indexer_cp = start_cp +
-                    // config.snapshot_min_lag`, we have not actually indexed `start_cp` yet, hence
-                    // why the condition is `>=`.
-                    while latest_indexer_cp >= start_cp + config.snapshot_min_lag as u64 {
-                        // The maximum checkpoint sequence number that can be committed to the
-                        // `objects_snapshot` table.
-                        let max_allowed_cp = latest_indexer_cp - config.snapshot_min_lag as u64;
-
-                        if let Some(new_changes) = stream.next().await {
-                            for checkpoint in new_changes {
-                                unprocessed.insert(checkpoint.checkpoint_sequence_number, checkpoint);
-                            }
-                        }
-
-                        // Collect the checkpoint object changes to write to `objects_snapshot`,
-                        // stopping when there are gaps in the sequence of unprocessed checkpoints.
-                        // This is an inclusive range, so if `start_cp` is equal to
-                        // `max_allowed_cp`, we'll still index the one checkpoint.
-                        for cp in start_cp..=max_allowed_cp {
-                            if let Some(checkpoint) = unprocessed.remove(&cp) {
-                                batch.push(checkpoint);
-                            }
-                            else {
-                                break;
-                            }
-                        }
-
-                        if !batch.is_empty() {
-                            let first_checkpoint_seq = batch.first().as_ref().unwrap().checkpoint_sequence_number;
-                            let last_checkpoint_seq = batch.last().as_ref().unwrap().checkpoint_sequence_number;
-                            info!("Objects snapshot processor is updating objects snapshot table from {} to {}", first_checkpoint_seq, last_checkpoint_seq);
-
-                            store.persist_objects_snapshot(batch.drain(..).map(|obj| obj.object_changes).collect())
-                                .await
-                                .unwrap_or_else(|_| panic!("Failed to backfill objects snapshot from {} to {}", first_checkpoint_seq, last_checkpoint_seq));
-                            start_cp = last_checkpoint_seq + 1;
-
-                            metrics
-                                .latest_object_snapshot_sequence_number
-                                .set(last_checkpoint_seq as i64);
-                        }
-                    }
-                }
-            }
+            snapshot_config,
         }
     }
 }
