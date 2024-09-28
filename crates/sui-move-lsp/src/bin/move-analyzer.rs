4ee061c3f394687e9e4307f665a4a4439be9ee2f
Krešimir Klas <github@kklas.net>
2024-09-28 10:33:44 +0000

    make on-chain hooks work with move-analyzer

--- crates/sui-move-lsp/src/bin/move-analyzer.rs
+++ crates/sui-move-lsp/src/bin/move-analyzer.rs
@@ -3,6 +3,9 @@
 
 use clap::*;
 use move_analyzer::analyzer;
+use sui::package_hooks::SuiPackageHooks;
+use sui_config::{sui_config_dir, SUI_CLIENT_CONFIG};
+use sui_sdk::wallet_context::WalletContext;
 
 // Define the `GIT_REVISION` and `VERSION` consts
 bin_version::bin_version!();
@@ -16,7 +19,16 @@ bin_version::bin_version!();
 )]
 struct App {}
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = sui_config_dir()?.join(SUI_CLIENT_CONFIG);
+    let context = WalletContext::new(&config, None, None)?;
+    let rt = tokio::runtime::Runtime::new().expect("Failed to create Tokio runtime");
+    rt.block_on(async {
+        SuiPackageHooks::register_from_ctx(&context).await.unwrap();
+    });
+
     App::parse();
     analyzer::run();
+
+    Ok(())
 }
