5ee7eb53f1f1d311c4161c1ec76377913f09c5e0
Jort <jordan.jennings@mystenlabs.com>
2024-10-03 10:08:07 -0700

    ValidateMetadata Move struct doesn't match typo (#19647)
    ## Description 
    
    rename to "match" for typo in validator error string
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- sui-execution/latest/sui-move-natives/src/validator.rs
+++ sui-execution/latest/sui-move-natives/src/validator.rs
@@ -53,7 +53,7 @@ pub fn validate_metadata_bcs(
     let validator_metadata =
         bcs::from_bytes::<ValidatorMetadataV1>(&metadata_bytes).map_err(|_| {
             PartialVMError::new(StatusCode::MALFORMED).with_message(
-                "ValidateMetadata Move struct does not much internal ValidateMetadata struct"
+                "ValidateMetadata Move struct does not match internal ValidateMetadata struct"
                     .to_string(),
             )
         })?;
