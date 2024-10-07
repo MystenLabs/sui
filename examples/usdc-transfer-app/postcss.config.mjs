d43cb2de011b3ed46457ae24bdc173664afe012f
Ronny Roland <ronmilton@mystenlabs.com>
2024-10-07 13:33:36 -0600

    Prettier and licenses

--- examples/usdc-transfer-app/postcss.config.mjs
+++ examples/usdc-transfer-app/postcss.config.mjs
@@ -1,8 +1,11 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+	plugins: {
+		tailwindcss: {},
+	},
 };
 
 export default config;
--- examples/usdc-transfer-app/tailwind.config.ts
+++ examples/usdc-transfer-app/tailwind.config.ts
@@ -1,19 +1,22 @@
-import type { Config } from "tailwindcss";
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
