b2ff92ba2ab24ecfe25bdb476872c3c34facf3ac
Ronny Roland <ronmilton@mystenlabs.com>
2024-10-08 14:52:02 -0600

    Licenses and module version

--- examples/usdc-transfer-app/App.js
+++ examples/usdc-transfer-app/App.js
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 "use client";
 import React, { useState, useEffect } from "react";
 import {
--- examples/usdc-transfer-app/index.js
+++ examples/usdc-transfer-app/index.js
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
