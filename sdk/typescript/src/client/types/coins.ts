b6a76f1dc7ffa456d6985abac8f265ec5f0bd6b6
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2024-10-09 10:25:01 -0700

    Try

--- sdk/typescript/src/client/types/coins.ts
+++ sdk/typescript/src/client/types/coins.ts
@@ -1,9 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+// Coin Balance type
 export type CoinBalance = {
-	coinType: string;
-	coinObjectCount: number;
-	totalBalance: string;
-	lockedBalance: Record<string, string>;
+  coinType: string;
+  coinObjectCount: number;
+  totalBalance: string;
+  lockedBalance: Record<string, string>;
 };
