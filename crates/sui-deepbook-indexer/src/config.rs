441f993236d6efbcb45e6fc523b871a2eab6286c
longbowlu <zl9394@gmail.com>
2024-10-09 22:02:25 -0700

    remove unused fields

--- crates/sui-deepbook-indexer/src/config.rs
+++ crates/sui-deepbook-indexer/src/config.rs
@@ -17,7 +17,6 @@ pub struct IndexerConfig {
     pub deepbook_genesis_checkpoint: u64,
     pub concurrency: u64,
     pub metric_port: u16,
-    pub resume_from_checkpoint: Option<u64>,
 }
 
 impl sui_config::Config for IndexerConfig {}
