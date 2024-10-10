75a3e8f7a3b065c95d3e329c057b0e098d1ac952
Aslan Tashtanov <aslan.tashtanov@mystenlabs.com>
2024-10-10 16:41:18 -0400

    endpoints

--- crates/sui-deepbook-indexer/src/server.rs
+++ crates/sui-deepbook-indexer/src/server.rs
@@ -2,13 +2,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    error::DeepBookError, models::Pools, schema, sui_deepbook_indexer::PgDeepbookPersistent,
+    error::DeepBookError,
+    models::{OrderFill, Pools},
+    schema,
+    sui_deepbook_indexer::PgDeepbookPersistent,
 };
-use axum::{debug_handler, extract::State, http::StatusCode, routing::get, Json, Router};
-use diesel::{query_dsl::methods::SelectDsl, SelectableHelper};
+use axum::{
+    debug_handler,
+    extract::{Path, State},
+    http::StatusCode,
+    routing::get,
+    Json, Router,
+};
+use diesel::prelude::*;
+use diesel::{ExpressionMethods, SelectableHelper};
 use diesel_async::RunQueryDsl;
 use std::net::SocketAddr;
+use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::{net::TcpListener, task::JoinHandle};
+
+pub const GET_POOLS_PATH: &str = "/get_pools";
+pub const GET_24HR_VOLUME_PATH: &str = "/get_24hr_volume/:pool_id";
+pub const GET_24HR_VOLUME_BY_BALANCE_MANAGER_ID: &str =
+    "/get_24hr_volume_by_balance_manager_id/:pool_id/:balance_manager_id";
 
 pub fn run_server(socket_address: SocketAddr, state: PgDeepbookPersistent) -> JoinHandle<()> {
     tokio::spawn(async move {
@@ -20,7 +36,12 @@ pub fn run_server(socket_address: SocketAddr, state: PgDeepbookPersistent) -> Jo
 pub(crate) fn make_router(state: PgDeepbookPersistent) -> Router {
     Router::new()
         .route("/", get(health_check))
-        .route("/get_pools", get(get_pools))
+        .route(GET_POOLS_PATH, get(get_pools))
+        .route(GET_24HR_VOLUME_PATH, get(get_24hr_volume))
+        .route(
+            GET_24HR_VOLUME_BY_BALANCE_MANAGER_ID,
+            get(get_24hr_volume_by_balance_manager_id),
+        )
         .with_state(state)
 }
 
@@ -60,4 +81,60 @@ async fn get_pools(
         .await?;
 
     Ok(Json(results))
+}
+
+async fn get_24hr_volume(
+    Path(pool_id): Path<String>,
+    State(state): State<PgDeepbookPersistent>,
+) -> Result<Json<i64>, DeepBookError> {
+    let connection = &mut state.pool.get().await?;
+    let unix_ts = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as i64;
+    let day_ago = unix_ts - 24 * 60 * 60 * 1000;
+    let results: Vec<OrderFill> = schema::order_fills::table
+        .select(OrderFill::as_select())
+        .filter(schema::order_fills::pool_id.eq(pool_id))
+        .filter(schema::order_fills::onchain_timestamp.gt(day_ago))
+        .load(connection)
+        .await?;
+
+    let mut total_vol = 0;
+    for order_fill in results {
+        total_vol += order_fill.base_quantity;
+    }
+
+    Ok(Json(total_vol))
+}
+
+async fn get_24hr_volume_by_balance_manager_id(
+    Path((pool_id, balance_manager_id)): Path<(String, String)>,
+    State(state): State<PgDeepbookPersistent>,
+) -> Result<Json<Vec<i64>>, DeepBookError> {
+    let connection = &mut state.pool.get().await?;
+    let unix_ts = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as i64;
+    let day_ago = unix_ts - 24 * 60 * 60 * 1000;
+    let results: Vec<OrderFill> = schema::order_fills::table
+        .select(OrderFill::as_select())
+        .filter(schema::order_fills::pool_id.eq(pool_id))
+        .filter(schema::order_fills::onchain_timestamp.gt(day_ago))
+        .load(connection)
+        .await?;
+
+    let mut maker_vol = 0;
+    let mut taker_vol = 0;
+    for order_fill in results {
+        if order_fill.maker_balance_manager_id == balance_manager_id {
+            maker_vol += order_fill.base_quantity;
+        };
+        if order_fill.taker_balance_manager_id == balance_manager_id {
+            taker_vol += order_fill.base_quantity;
+        };
+    }
+
+    Ok(Json(vec![maker_vol, taker_vol]))
 }
