a9710f3e6e8fbc2a45f7dc5236cf0a5b687a58fa
Ashok Menon <ashok@mystenlabs.com>
2024-10-12 17:40:34 +0100

    indexer-alt: Fetch checkpoints from remote store

--- crates/sui-indexer-alt/src/args.rs
+++ crates/sui-indexer-alt/src/args.rs
@@ -5,6 +5,11 @@ use url::Url;
 
 #[derive(clap::Parser, Debug, Clone)]
 pub struct Args {
+    /// First checkpoint to start indexing from.
+    #[arg(long, default_value_t = 0)]
+    pub start: u64,
+
+    /// Remote Store to fetch CheckpointData from.
     #[arg(long)]
     pub remote_store_url: Url,
 }
