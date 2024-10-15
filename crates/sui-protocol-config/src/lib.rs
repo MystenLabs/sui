2e681a0063cecb4c31ff7aa3c42f1821d8237799
Arun Koshy <97870774+arun-koshy@users.noreply.github.com>
2024-10-14 21:56:42 -0700

    [consensus] Enable distributed vote scoring in mainnet v63 (#19799)
    ## Description 
    
    Bump to protocol version 63 
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    Has been running nightly in private-testnet and also running
    successfully in testnet https://metrics.sui.io/goto/KDyf-3kHR?orgId=1
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [X] Protocol: Enable distributed vote scoring in mainnet v63 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -18,7 +18,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-const MAX_PROTOCOL_VERSION: u64 = 62;
+const MAX_PROTOCOL_VERSION: u64 = 63;
 
 // Record history of protocol version allocations here:
 //
@@ -184,6 +184,7 @@ const MAX_PROTOCOL_VERSION: u64 = 62;
 //             Further reduce minimum number of random beacon shares.
 //             Add feature flag for Mysticeti fastpath.
 // Version 62: Makes the event's sending module package upgrade-aware.
+// Version 63: Switch to distributed vote scoring in consensus in mainnet
 
 #[derive(Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ProtocolVersion(u64);
@@ -2815,6 +2816,13 @@ impl ProtocolConfig {
                 }
                 62 => {
                     cfg.feature_flags.relocate_event_module = true;
+                }
+                63 => {
+                    cfg.feature_flags.relocate_event_module = true;
+
+                    // Enable distributed vote scoring for mainnet
+                    cfg.feature_flags
+                        .consensus_distributed_vote_scoring_strategy = true;
                 }
                 // Use this template when making changes:
                 //
