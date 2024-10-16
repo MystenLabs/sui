75ae28b872f43972953e1858296175e6eb7e3653
Ashok Menon <ashok@mystenlabs.com>
2024-10-16 23:40:45 +0100

    refactor(indexer-alt): Introduce Indexer abstraction
    ## Description
    
    Introduce an `Indexer` type to reduce the boilerplate of introducing a
    new pipeline, and gracefully shutting everything down.
    
    This also prepares the codebase for watermarks support: The initial
    checkpoint to start ingestion at can be calculated by taking the MIN
    checkpoint in the watermark table across all pipelines.
    
    ## Test plan
    
    Unit tests:
    
    ```
    sui$ cargo nextest run -p sui-indexer-alt
    ```
    
    And run the indexer locally.

--- crates/sui-indexer-alt/src/args.rs
+++ crates/sui-indexer-alt/src/args.rs
@@ -1,22 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::net::SocketAddr;
-
-use crate::{db::DbConfig, handlers::CommitterConfig, ingestion::IngestionConfig};
+use crate::IndexerConfig;
 
 #[derive(clap::Parser, Debug, Clone)]
 pub struct Args {
     #[command(flatten)]
-    pub ingestion: IngestionConfig,
-
-    #[command(flatten)]
-    pub db: DbConfig,
-
-    #[command(flatten)]
-    pub committer: CommitterConfig,
-
-    /// Address to serve Prometheus Metrics from.
-    #[arg(long, default_value = "0.0.0.0:9184")]
-    pub metrics_address: SocketAddr,
+    pub indexer_config: IndexerConfig,
 }
