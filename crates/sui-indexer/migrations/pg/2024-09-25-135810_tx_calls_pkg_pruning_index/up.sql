a1146c1b551ed766bfa485630bc4f2f3f11dc7f3
Ge Gao <ge@mystenlabs.com>
2024-10-16 14:55:47 -0400

    remove index adding for experiment

--- crates/sui-indexer/migrations/pg/2024-09-25-135810_tx_calls_pkg_pruning_index/up.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS
-    tx_calls_pkg_tx_sequence_number
-ON  tx_calls_pkg (tx_sequence_number);
--- crates/sui-indexer/migrations/pg/2024-09-25-135811_tx_changed_objects_pruning_index/down.sql
+++
@@ -1,1 +0,0 @@
-DROP INDEX CONCURRENTLY IF EXISTS tx_changed_objects_tx_sequence_number;
