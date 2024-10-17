555bd241e5aa7c4373b402600f98be87b660b3b2
Ashok Menon <ashok@mystenlabs.com>
2024-10-17 16:27:26 +0100

    indexer-alt: support checkpoint upperbound for graceful shutdown
    ## Description
    Previously, the service would just hang after having wound down all
    pipelines during a graceful shutdown. This is because the graceful
    shutdown process was left waiting for an explicit interrupt signal to
    unblock the whole shutdown process.
    
    This change relaxes that constraint: If the service is already shutting
    down, then there is no need to wait for the explicit Ctrl-C. This makes
    it feasible to run the indexer for specific ranges of checkpoints.
    
    Note that we don't want to call the global cancellation token once
    ingestion is done, because that might stop a downstream task before it
    has acted on the downloaded checkpoints. Instead, the ingestion service
    communicates completion by closing its channel, which propagates through
    the handlers and committers.
    
    ## Test plan
    
    ```
    RUST_LOG="info,sui_indexer_alt=debug" cargo run -p sui-indexer-alt --release --  \
      --database-url "postgres://postgres:postgrespw@localhost:5432/sui_indexer_alt" \
      --remote-store-url https://checkpoints.mainnet.sui.io                          \
      --last-checkpoint 1000
    ```

--- crates/sui-indexer-alt/src/task.rs
+++ crates/sui-indexer-alt/src/task.rs
@@ -4,7 +4,7 @@
 use std::iter;
 
 use futures::future::{self, Either};
-use tokio::{signal, task::JoinHandle};
+use tokio::{signal, sync::oneshot, task::JoinHandle};
 use tokio_util::sync::CancellationToken;
 
 /// Manages cleanly exiting the process, either because one of its constituent services has stopped
@@ -13,10 +13,16 @@ pub async fn graceful_shutdown(
     services: impl IntoIterator<Item = JoinHandle<()>>,
     cancel: CancellationToken,
 ) {
+    // If the service is naturalling winding down, we don't need to wait for an interrupt signal.
+    // This channel is used to short-circuit the await in that case.
+    let (cancel_ctrl_c_tx, cancel_ctrl_c_rx) = oneshot::channel();
+
     let interrupt = async {
-        signal::ctrl_c()
-            .await
-            .expect("Failed to listen for Ctrl-C signal");
+        tokio::select! {
+            _ = cancel_ctrl_c_rx => {}
+            _ = cancel.cancelled() => {}
+            _ = signal::ctrl_c() => cancel.cancel(),
+        }
 
         Ok(())
     };
@@ -30,7 +36,7 @@ pub async fn graceful_shutdown(
 
     // Wait for the first service to finish, or for an interrupt signal.
     let (_, _, rest) = future::select_all(futures).await;
-    cancel.cancel();
+    let _ = cancel_ctrl_c_tx.send(());
 
     // Wait for the remaining services to finish.
     let _ = future::join_all(rest).await;
