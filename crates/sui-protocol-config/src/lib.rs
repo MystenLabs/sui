7763b61e958b6464b3101f20b6a9afa06bc09b7a
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2024-10-18 09:59:26 +0200

    Update snapshots...

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -18,7 +18,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-const MAX_PROTOCOL_VERSION: u64 = 64;
+const MAX_PROTOCOL_VERSION: u64 = 65;
 
 // Record history of protocol version allocations here:
 //
