f1d010f35203d33d39c149a3df40527cab6783ce
Ashok Menon <ashok@mystenlabs.com>
2024-10-19 18:12:20 +0100

    indexer-alt: simplify event indexing
    ## Description
    
    Instead of having a separate table for each component of the cascading
    index, have a single table, and add multiple indices to it for each of
    the cascading cases.
    
    This should reduce the footprint and ingress to the DB, but mildly
    increases the risk that the DB picks a bad query plan.
    
    ## Test plan
    
    Run all the existing tests, and also run an experiment to confirm that
    the DB can successfully plan queries against this kind of schema.
    
    Our initial fear was that if we had multiple indices on a single table,
    then the DB may pick the wrong index, and there is still a chance that
    might happen if we add indices for disparate filters to the same table
    (i.e. we combine the event emit module and event struct instantiation
    fields into one table), but we can guarantee that the reader will only
    issue one query to each of these merged tables, and it should entirely
    overlap with one of its indices.

--- crates/sui-indexer-alt/migrations/2024-10-19-113135_ev_indices/down.sql
+++ crates/sui-indexer-alt/migrations/2024-10-19-113135_ev_indices/down.sql
@@ -1,6 +1,2 @@
-DROP TABLE IF EXISTS ev_emit_pkg;
 DROP TABLE IF EXISTS ev_emit_mod;
-DROP TABLE IF EXISTS ev_struct_pkg;
-DROP TABLE IF EXISTS ev_struct_mod;
-DROP TABLE IF EXISTS ev_struct_name;
 DROP TABLE IF EXISTS ev_struct_inst;
--- crates/sui-indexer-alt/migrations/2024-10-19-113135_ev_indices/up.sql
+++ crates/sui-indexer-alt/migrations/2024-10-19-113135_ev_indices/up.sql
@@ -1,17 +1,3 @@
-CREATE TABLE IF NOT EXISTS ev_emit_pkg
-(
-    package                     BYTEA,
-    tx_sequence_number          BIGINT,
-    sender                      BYTEA         NOT NULL,
-    PRIMARY KEY(package, tx_sequence_number)
-);
-
-CREATE INDEX IF NOT EXISTS ev_emit_pkg_tx_sequence_number
-ON ev_emit_pkg (tx_sequence_number);
-
-CREATE INDEX IF NOT EXISTS ev_emit_pkg_sender
-ON ev_emit_pkg (sender, package, tx_sequence_number);
-
 CREATE TABLE IF NOT EXISTS ev_emit_mod
 (
     package                     BYTEA,
@@ -27,50 +13,11 @@ ON ev_emit_mod (tx_sequence_number);
 CREATE INDEX IF NOT EXISTS ev_emit_mod_sender
 ON ev_emit_mod (sender, package, module, tx_sequence_number);
 
-CREATE TABLE IF NOT EXISTS ev_struct_pkg
-(
-    package                     BYTEA,
-    tx_sequence_number          BIGINT,
-    sender                      BYTEA         NOT NULL,
-    PRIMARY KEY(package, tx_sequence_number)
-);
+CREATE INDEX IF NOT EXISTS ev_emit_pkg
+ON ev_emit_mod (package, tx_sequence_number);
 
-CREATE INDEX IF NOT EXISTS ev_struct_pkg_tx_sequence_number
-ON ev_struct_pkg (tx_sequence_number);
-
-CREATE INDEX IF NOT EXISTS ev_struct_pkg_sender
-ON ev_struct_pkg (sender, package, tx_sequence_number);
-
-CREATE TABLE IF NOT EXISTS ev_struct_mod
-(
-    package                     BYTEA,
-    module                      TEXT,
-    tx_sequence_number          BIGINT,
-    sender                      BYTEA         NOT NULL,
-    PRIMARY KEY(package, module, tx_sequence_number)
-);
-
-CREATE INDEX IF NOT EXISTS ev_struct_mod_tx_sequence_number
-ON ev_struct_mod (tx_sequence_number);
-
-CREATE INDEX IF NOT EXISTS ev_struct_mod_sender
-ON ev_struct_mod (sender, package, module, tx_sequence_number);
-
-CREATE TABLE IF NOT EXISTS ev_struct_name
-(
-    package                     BYTEA,
-    module                      TEXT,
-    name                        TEXT,
-    tx_sequence_number          BIGINT,
-    sender                      BYTEA         NOT NULL,
-    PRIMARY KEY(package, module, name, tx_sequence_number)
-);
-
-CREATE INDEX IF NOT EXISTS ev_struct_name_tx_sequence_number
-ON ev_struct_name (tx_sequence_number);
-
-CREATE INDEX IF NOT EXISTS ev_struct_name_sender
-ON ev_struct_name (sender, package, module, name, tx_sequence_number);
+CREATE INDEX IF NOT EXISTS ev_emit_pkg_sender
+ON ev_emit_mod (sender, package, tx_sequence_number);
 
 CREATE TABLE IF NOT EXISTS ev_struct_inst
 (
@@ -81,11 +28,29 @@ CREATE TABLE IF NOT EXISTS ev_struct_inst
     instantiation               BYTEA,
     tx_sequence_number          BIGINT,
     sender                      BYTEA         NOT NULL,
-    PRIMARY KEY(package, module, instantiation, tx_sequence_number)
+    PRIMARY KEY(package, module, name, instantiation, tx_sequence_number)
 );
 
 CREATE INDEX IF NOT EXISTS ev_struct_inst_tx_sequence_number
 ON ev_struct_inst (tx_sequence_number);
 
 CREATE INDEX IF NOT EXISTS ev_struct_inst_sender
-ON ev_struct_inst (sender, package, module, instantiation, tx_sequence_number);
+ON ev_struct_inst (sender, package, module, name, instantiation, tx_sequence_number);
+
+CREATE INDEX IF NOT EXISTS ev_struct_name
+ON ev_struct_inst (package, module, name, tx_sequence_number);
+
+CREATE INDEX IF NOT EXISTS ev_struct_name_sender
+ON ev_struct_inst (sender, package, module, name, tx_sequence_number);
+
+CREATE INDEX IF NOT EXISTS ev_struct_mod
+ON ev_struct_inst (package, module, tx_sequence_number);
+
+CREATE INDEX IF NOT EXISTS ev_struct_mod_sender
+ON ev_struct_inst (sender, package, module, tx_sequence_number);
+
+CREATE INDEX IF NOT EXISTS ev_struct_pkg
+ON ev_struct_inst (package, tx_sequence_number);
+
+CREATE INDEX IF NOT EXISTS ev_struct_pkg_sender
+ON ev_struct_inst (sender, package, tx_sequence_number);
