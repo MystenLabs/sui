75ac3809083d45c5edd56eb76a724b343d82e71f
Cosmin Radoi <cosmin.radoi@gmail.com>
2024-10-21 11:31:54 -0700

    fix

--- external-crates/move/crates/move-compiler/src/parser/verification_attribute_filter.rs
+++ external-crates/move/crates/move-compiler/src/parser/verification_attribute_filter.rs
@@ -42,10 +42,9 @@ impl FilterContext for Context<'_> {
     // An AST element should be removed if:
     // * It is annotated #[verify_only] and verify mode is not set
     fn should_remove_by_attributes(&mut self, attrs: &[P::Attributes]) -> bool {
-        if !self.env.flags().is_verifying() {
-            return true;
+        if self.env.flags().is_verifying() {
+            return false;
         }
-
         use known_attributes::VerificationAttribute;
         let flattened_attrs: Vec<_> = attrs.iter().flat_map(verification_attributes).collect();
         let is_verify_only_loc = flattened_attrs
