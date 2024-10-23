fa8fa971d891bb22b6518fc81a27181d88f77a3c
Ge Gao <ge@mystenlabs.com>
2024-10-23 16:15:56 -0400

    dep in other crates

--- crates/sui-graphql-rpc/src/test_infra/cluster.rs
+++ crates/sui-graphql-rpc/src/test_infra/cluster.rs
@@ -132,6 +132,8 @@ pub async fn start_network_cluster() -> NetworkCluster {
         None,
         Some(data_ingestion_path.path().to_path_buf()),
         Some(cancellation_token.clone()),
+        None, /* start_checkpoint */
+        None, /* end_checkpoint */
     )
     .await;
 
