c33374e735257b2a460a2f882e062619f8958bc5
wlmyng <127570466+wlmyng@users.noreply.github.com>
2024-10-31 12:51:47 -0700

    [mvr] indexer schema subset and reduced persist tasks (#20100)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/sui-mvr-indexer/tests/ingestion_tests.rs
+++ crates/sui-mvr-indexer/tests/ingestion_tests.rs
@@ -8,11 +8,8 @@ use diesel_async::RunQueryDsl;
 use simulacrum::Simulacrum;
 use sui_mvr_indexer::errors::IndexerError;
 use sui_mvr_indexer::handlers::TransactionObjectChangesToCommit;
-use sui_mvr_indexer::models::{
-    checkpoints::StoredCheckpoint, objects::StoredObject, objects::StoredObjectSnapshot,
-    transactions::StoredTransaction,
-};
-use sui_mvr_indexer::schema::{checkpoints, objects, objects_snapshot, transactions};
+use sui_mvr_indexer::models::{checkpoints::StoredCheckpoint, objects::StoredObjectSnapshot};
+use sui_mvr_indexer::schema::{checkpoints, objects_snapshot};
 use sui_mvr_indexer::store::indexer_store::IndexerStore;
 use sui_mvr_indexer::test_utils::{
     set_up, set_up_with_start_and_end_checkpoints, wait_for_checkpoint, wait_for_objects_snapshot,
@@ -22,56 +19,7 @@ use sui_mvr_indexer::types::IndexedDeletedObject;
 use sui_mvr_indexer::types::IndexedObject;
 use sui_mvr_indexer::types::TxIndex;
 use sui_types::base_types::SuiAddress;
-use sui_types::effects::TransactionEffectsAPI;
-use sui_types::gas_coin::GasCoin;
-use sui_types::SUI_FRAMEWORK_PACKAGE_ID;
 use tempfile::tempdir;
-
-#[tokio::test]
-pub async fn test_transaction_table() -> Result<(), IndexerError> {
-    let tempdir = tempdir().unwrap();
-    let mut sim = Simulacrum::new();
-    let data_ingestion_path = tempdir.path().to_path_buf();
-    sim.set_data_ingestion_path(data_ingestion_path.clone());
-
-    // Execute a simple transaction.
-    let transfer_recipient = SuiAddress::random_for_testing_only();
-    let (transaction, _) = sim.transfer_txn(transfer_recipient);
-    let (effects, err) = sim.execute_transaction(transaction.clone()).unwrap();
-    assert!(err.is_none());
-
-    // Create a checkpoint which should include the transaction we executed.
-    let checkpoint = sim.create_checkpoint();
-
-    let (_, pg_store, _, _database) = set_up(Arc::new(sim), data_ingestion_path).await;
-
-    // Wait for the indexer to catch up to the checkpoint.
-    wait_for_checkpoint(&pg_store, 1).await?;
-
-    let digest = effects.transaction_digest();
-
-    // Read the transaction from the database directly.
-    let mut connection = pg_store.pool().dedicated_connection().await.unwrap();
-    let db_txn: StoredTransaction = transactions::table
-        .filter(transactions::transaction_digest.eq(digest.inner().to_vec()))
-        .first::<StoredTransaction>(&mut connection)
-        .await
-        .expect("Failed reading transaction from PostgresDB");
-
-    // Check that the transaction was stored correctly.
-    assert_eq!(db_txn.tx_sequence_number, 1);
-    assert_eq!(db_txn.transaction_digest, digest.inner().to_vec());
-    assert_eq!(
-        db_txn.raw_transaction,
-        bcs::to_bytes(&transaction.data()).unwrap()
-    );
-    assert_eq!(db_txn.raw_effects, bcs::to_bytes(&effects).unwrap());
-    assert_eq!(db_txn.timestamp_ms, checkpoint.timestamp_ms as i64);
-    assert_eq!(db_txn.checkpoint_sequence_number, 1);
-    assert_eq!(db_txn.transaction_kind, 1);
-    assert_eq!(db_txn.success_command_count, 2); // split coin + transfer
-    Ok(())
-}
 
 #[tokio::test]
 pub async fn test_checkpoint_range_ingestion() -> Result<(), IndexerError> {
@@ -136,53 +84,6 @@ pub async fn test_checkpoint_range_ingestion() -> Result<(), IndexerError> {
         end_checkpoint
     );
 
-    Ok(())
-}
-
-#[tokio::test]
-pub async fn test_object_type() -> Result<(), IndexerError> {
-    let tempdir = tempdir().unwrap();
-    let mut sim = Simulacrum::new();
-    let data_ingestion_path = tempdir.path().to_path_buf();
-    sim.set_data_ingestion_path(data_ingestion_path.clone());
-
-    // Execute a simple transaction.
-    let transfer_recipient = SuiAddress::random_for_testing_only();
-    let (transaction, _) = sim.transfer_txn(transfer_recipient);
-    let (_, err) = sim.execute_transaction(transaction.clone()).unwrap();
-    assert!(err.is_none());
-
-    // Create a checkpoint which should include the transaction we executed.
-    let _ = sim.create_checkpoint();
-
-    let (_, pg_store, _, _database) = set_up(Arc::new(sim), data_ingestion_path).await;
-
-    // Wait for the indexer to catch up to the checkpoint.
-    wait_for_checkpoint(&pg_store, 1).await?;
-
-    let obj_id = transaction.gas()[0].0;
-
-    // Read the transaction from the database directly.
-    let mut connection = pg_store.pool().dedicated_connection().await.unwrap();
-    let db_object: StoredObject = objects::table
-        .filter(objects::object_id.eq(obj_id.to_vec()))
-        .first::<StoredObject>(&mut connection)
-        .await
-        .expect("Failed reading object from PostgresDB");
-
-    let obj_type_tag = GasCoin::type_();
-
-    // Check that the different components of the event type were stored correctly.
-    assert_eq!(
-        db_object.object_type,
-        Some(obj_type_tag.to_canonical_string(true))
-    );
-    assert_eq!(
-        db_object.object_type_package,
-        Some(SUI_FRAMEWORK_PACKAGE_ID.to_vec())
-    );
-    assert_eq!(db_object.object_type_module, Some("coin".to_string()));
-    assert_eq!(db_object.object_type_name, Some("Coin".to_string()));
     Ok(())
 }
 
