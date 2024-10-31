ef0d78c638e3ef0d0a3b28787b618ea56a140284
Ge Gao <106119108+gegaowp@users.noreply.github.com>
2024-10-31 19:30:51 -0400

    add sui-field-count for sui-indexer-alt (#20126)
    ## Description 
    
    title, so that we can replace hard-coded field count in pipelines.
    a crate did very similar things but it's archived, so prob better to
    have our own impl, also b/c the functionality is relatively simple
    https://github.com/discosultan/field-count
    
    ## Test plan 
    
    added tests in models/
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/sui-indexer-alt/src/models/checkpoints.rs
+++ crates/sui-indexer-alt/src/models/checkpoints.rs
@@ -3,8 +3,9 @@
 
 use crate::schema::kv_checkpoints;
 use diesel::prelude::*;
+use sui_field_count::FieldCount;
 
-#[derive(Insertable, Debug, Clone)]
+#[derive(Insertable, Debug, Clone, FieldCount)]
 #[diesel(table_name = kv_checkpoints)]
 pub struct StoredCheckpoint {
     pub sequence_number: i64,
@@ -12,4 +13,14 @@ pub struct StoredCheckpoint {
     pub certified_checkpoint: Vec<u8>,
     /// BCS serialized CheckpointContents
     pub checkpoint_contents: Vec<u8>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_stored_checkpoint_field_count() {
+        assert_eq!(StoredCheckpoint::field_count(), 3);
+    }
 }
