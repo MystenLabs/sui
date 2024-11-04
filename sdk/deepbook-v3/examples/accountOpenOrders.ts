d2b8e3ddfa35cdfc86d9e5db8fb9f25dbbba53c0
Tony Lee <chunglee2018@u.northwestern.edu>
2024-11-04 12:21:25 -0800

    open orders

---
+++ sdk/deepbook-v3/examples/accountOpenOrders.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
+
+import { DeepBookClient } from '../src/index.js'; // Adjust import source accordingly
+
+(async () => {
+	const env = 'mainnet';
+
+	// Initialize with balance managers if needed
+	const balanceManagers = {
+		AMBER: {
+			address: '0x344c2734b1d211bd15212bfb7847c66a3b18803f3f5ab00f5ff6f87b6fe6d27d',
+			tradeCap: '',
+		},
+	};
+
+	const dbClient = new DeepBookClient({
+		address: '0x0',
+		env: env,
+		client: new SuiClient({
+			url: getFullnodeUrl(env),
+		}),
+		balanceManagers: balanceManagers,
+	});
+
+	const pools = ['SUI_USDC', 'DEEP_SUI', 'DEEP_USDC', 'WUSDT_USDC'];
+	for (const pool of pools) {
+		console.log(pool);
+		console.log(await dbClient.accountOpenOrders(pool, 'AMBER'));
+	}
+})();
