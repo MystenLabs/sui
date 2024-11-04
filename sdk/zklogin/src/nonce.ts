f51e40f67948a6b65cfd9d407b2bfc6449f0fbcf
panda <919401990@qq.com>
2024-11-04 14:55:39 +0700

    [sdk:zklogin]perf:use crypto.getRandomValues instead randomBytes

--- sdk/zklogin/src/nonce.ts
+++ sdk/zklogin/src/nonce.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import crypto from 'node:crypto';
 import { toHex } from '@mysten/bcs';
 import type { PublicKey } from '@mysten/sui/cryptography';
 import { toPaddedBigEndianBytes } from '@mysten/sui/zklogin';
-import { randomBytes } from '@noble/hashes/utils';
 import { base64url } from 'jose';
 
 import { poseidonHash } from './poseidon.js';
@@ -20,8 +20,7 @@ function toBigIntBE(bytes: Uint8Array) {
 }
 
 export function generateRandomness() {
-	// Once Node 20 enters LTS, we can just use crypto.getRandomValues(new Uint8Array(16)), but until then we use `randomBytes` to improve compatibility:
-	return String(toBigIntBE(randomBytes(16)));
+	return String(toBigIntBE(crypto.getRandomValues(new Uint8Array(16))));
 }
 
 export function generateNonce(publicKey: PublicKey, maxEpoch: number, randomness: bigint | string) {
