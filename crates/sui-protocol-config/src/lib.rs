5d21169b25da6875d38ca970162ab9d6e2ce7956
Anastasios Kichidis <tasos@mystenlabs.com>
2024-11-13 14:28:03 +0000

    [fix] format

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -1732,7 +1732,10 @@ impl ProtocolConfig {
             
     pub fn consensus_linearizer_collect_subdag_v2(&self) -> bool {
         let res = self.feature_flags.consensus_linearizer_collect_subdag_v2;
-        assert!(!res || self.gc_depth() > 0, "The consensus linearizer collect sub dag V2 requires GC to be enabled");
+        assert!(
+            !res || self.gc_depth() > 0,
+            "The consensus linearizer collect sub dag V2 requires GC to be enabled"
+        );
         res
     }
 }
