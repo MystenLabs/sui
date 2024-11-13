91dec7c02d776f3e68a5a1e5b86851c7d289c206
Ashok Menon <ashok@mystenlabs.com>
2024-11-13 14:00:52 +0000

    indexer-alt: remove watermarks.epoch_lo (#20218)
    ## Description
    
    This field turns out not to be used by the new pruner implementation,
    because it is entirely based on checkpoints.
    
    ## Test plan
    
    CI
    
    ## Stack 
    
    - #20149 
    - #20150 
    - #20166 
    - #20216 
    - #20217 
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/sui-indexer-alt/src/schema.rs
+++ crates/sui-indexer-alt/src/schema.rs
@@ -233,7 +233,6 @@ diesel::table! {
         checkpoint_hi_inclusive -> Int8,
         tx_hi -> Int8,
         timestamp_ms_hi_inclusive -> Int8,
-        epoch_lo -> Int8,
         reader_lo -> Int8,
         pruner_timestamp -> Timestamp,
         pruner_hi -> Int8,
