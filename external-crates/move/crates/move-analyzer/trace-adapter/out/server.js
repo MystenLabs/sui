c1a9b34e35c94f20d48c0fb973a040ec3b83de99
Adam Welc <adam@mystenlabs.com>
2024-11-14 15:00:46 -0800

    Removed accidentally added output directory

--- external-crates/move/crates/move-analyzer/trace-adapter/out/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const debugadapter_1 = require("@vscode/debugadapter");
-const adapter_1 = require("./adapter");
-// Run the MoveDebugSession debug adapter using stdio
-debugadapter_1.DebugSession.run(adapter_1.MoveDebugSession);
-//# sourceMappingURL=server.js.map
