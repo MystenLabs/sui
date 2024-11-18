8f020117d18243dc555a6a353fe4f212d19e4db8
Ronny Roland <ronmilton@mystenlabs.com>
2024-11-18 09:48:01 -0700

    init

--- docs/site/sidebars.js
+++ docs/site/sidebars.js
@@ -1,14 +1,14 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-//const why_sui = require("../content/sidebars/why_sui.js");
+const getStarted = require("../content/sidebars/getStarted.js");
 const guides = require("../content/sidebars/guides.js");
 const concepts = require("../content/sidebars/concepts.js");
 const standards = require("../content/sidebars/standards.js");
 const references = require("../content/sidebars/references.js");
 
 const sidebars = {
-  //whySuiSidebar: why_sui,
+  getStartedSidebar: getStarted,
   guidesSidebar: guides,
   conceptsSidebar: concepts,
   standardsSidebar: standards,
