114b5837d5cd7c13dbceefc2b8e232e55d4ff07b
Michael Hayes <michael.hayes@mystenlabs.com>
2024-11-22 12:24:06 -0800

    [docker] enable multi-arch builds for docker images

--- docker/sui-tools/build.sh
+++ docker/sui-tools/build.sh
@@ -19,7 +19,7 @@ echo "build date: \t$BUILD_DATE"
 echo "git revision: \t$GIT_REVISION"
 echo
 
-docker build -f "$DOCKERFILE" "$REPO_ROOT" \
+docker buildx build --platform linux/amd64,linux/arm64 -f "$DOCKERFILE" "$REPO_ROOT" \
 	--build-arg GIT_REVISION="$GIT_REVISION" \
 	--build-arg BUILD_DATE="$BUILD_DATE" \
 	"$@"
