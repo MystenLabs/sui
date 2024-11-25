0deef1dfdd014dc3f4c78a0cea10b88a00c6a382
nikos-terzo <nikos.terzo@mystenlabs.com>
2024-11-25 19:34:27 +0200

    Stake: fn stake_pt

--- crates/sui-rosetta/src/types/internal_operation/stake.rs
+++ crates/sui-rosetta/src/types/internal_operation/stake.rs
@@ -2,11 +2,18 @@ use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 
 use sui_sdk::SuiClient;
-use sui_types::base_types::SuiAddress;
+use sui_types::base_types::ObjectRef;
+use sui_types::governance::ADD_STAKE_FUN_NAME;
+use sui_types::sui_system_state::SUI_SYSTEM_MODULE_NAME;
+use sui_types::transaction::{Argument, CallArg, Command, ObjectArg, ProgrammableTransaction};
+use sui_types::SUI_SYSTEM_PACKAGE_ID;
+use sui_types::{
+    base_types::SuiAddress, programmable_transaction_builder::ProgrammableTransactionBuilder,
+};
 
 use crate::errors::Error;
 
-use super::{TransactionAndObjectData, TryConstructTransaction};
+use super::{TransactionAndObjectData, TryConstructTransaction, MAX_COMMAND_ARGS};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Stake {
@@ -25,4 +32,54 @@ impl TryConstructTransaction for Stake {
     ) -> Result<TransactionAndObjectData, Error> {
         todo!();
     }
+}
+
+pub fn stake_pt(
+    validator: SuiAddress,
+    amount: u64,
+    stake_all: bool,
+    coins_to_merge: &[ObjectRef],
+) -> anyhow::Result<ProgrammableTransaction> {
+    let mut builder = ProgrammableTransactionBuilder::new();
+    if !coins_to_merge.is_empty() {
+        // TODO: test and test that this won't mess with the workaround
+        // below
+        // We need to merge the rest of the coins.
+        // Each merge has a limit of 511 arguments.
+        coins_to_merge
+            .chunks(MAX_COMMAND_ARGS)
+            .try_for_each(|chunk| -> anyhow::Result<()> {
+                let to_merge = chunk
+                    .iter()
+                    .map(|&o| builder.obj(ObjectArg::ImmOrOwnedObject(o)))
+                    .collect::<Result<Vec<Argument>, anyhow::Error>>()?;
+                builder.command(Command::MergeCoins(Argument::GasCoin, to_merge));
+                Ok(())
+            })?;
+    };
+
+    // [WORKAROUND] - this is a hack to work out if the staking ops is for a selected amount or None amount (whole wallet).
+    // if amount is none, validator input will be created after the system object input
+    let amount = builder.pure(amount)?;
+    let (validator, system_state) = if !stake_all {
+        let validator = builder.input(CallArg::Pure(bcs::to_bytes(&validator)?))?;
+        let state = builder.input(CallArg::SUI_SYSTEM_MUT)?;
+        (validator, state)
+    } else {
+        let state = builder.input(CallArg::SUI_SYSTEM_MUT)?;
+        let validator = builder.input(CallArg::Pure(bcs::to_bytes(&validator)?))?;
+        (validator, state)
+    };
+    let coin = builder.command(Command::SplitCoins(Argument::GasCoin, vec![amount]));
+
+    let arguments = vec![system_state, coin, validator];
+
+    builder.command(Command::move_call(
+        SUI_SYSTEM_PACKAGE_ID,
+        SUI_SYSTEM_MODULE_NAME.to_owned(),
+        ADD_STAKE_FUN_NAME.to_owned(),
+        vec![],
+        arguments,
+    ));
+    Ok(builder.finish())
 }
