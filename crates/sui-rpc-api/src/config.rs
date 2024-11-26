1291a1d02afb557a11be771eb8f146d5285ee480
Brandon Williams <brandon@mystenlabs.com>
2024-11-26 09:35:36 -0600

    rpc: add config for enabling new indexes instad of reusing the jsonrpc config flag

--- crates/sui-rpc-api/src/config.rs
+++ crates/sui-rpc-api/src/config.rs
@@ -10,6 +10,18 @@ pub struct Config {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub enable_unstable_apis: Option<bool>,
 
+    /// Enable indexing of transactions and objects
+    ///
+    /// This enables indexing of transactions and objects which allows for a slightly richer rpc
+    /// api. There are some APIs which will be disabled/enabled based on this config while others
+    /// (eg GetTransaction) will still be enabled regardless of this config but may return slight
+    /// less data (eg GetTransaction won't return the checkpoint that includes the requested
+    /// transaction).
+    ///
+    /// Defaults to `false`, with indexing and APIs which require indexes being disabled
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub enable_indexing: Option<bool>,
+
     // Only include this till we have another field that isn't set with a non-default value for
     // testing
     #[doc(hidden)]
@@ -23,5 +35,9 @@ impl Config {
         // Until the rest service as a whole is "stabalized" with a sane set of default stable
         // apis, have the default be to enable all apis
         self.enable_unstable_apis.unwrap_or(true)
+    }
+
+    pub fn enable_indexing(&self) -> bool {
+        self.enable_indexing.unwrap_or(false)
     }
 }
