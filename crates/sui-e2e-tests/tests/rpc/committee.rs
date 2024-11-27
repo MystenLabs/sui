666015e2b7569e4356a6a676d8e207cb86ff4bc0
Brandon Williams <brandon@mystenlabs.com>
2024-11-26 21:53:46 -0600

    rest: remove support for bcs response types from various endpoints
    Remove support for bcs response types from various endpoints, a few are
    kept due to existing clients, as support for a binary response type will
    be supported via a gRPC service that will be added in a future patch.

--- crates/sui-e2e-tests/tests/rpc/committee.rs
+++ crates/sui-e2e-tests/tests/rpc/committee.rs
@@ -3,7 +3,6 @@
 
 use sui_macros::sim_test;
 use sui_rpc_api::client::sdk::Client;
-use sui_sdk_types::types::ValidatorCommittee;
 use test_cluster::TestClusterBuilder;
 
 #[sim_test]
@@ -14,39 +13,4 @@ async fn get_committee() {
 
     let _committee = client.get_committee(0).await.unwrap();
     let _committee = client.get_current_committee().await.unwrap();
-
-    async fn raw_request(url: &str) {
-        let client = reqwest::Client::new();
-
-        // Make sure list works with json
-        let _object = client
-            .get(url)
-            .header(reqwest::header::ACCEPT, sui_rpc_api::rest::APPLICATION_JSON)
-            .send()
-            .await
-            .unwrap()
-            .json::<ValidatorCommittee>()
-            .await
-            .unwrap();
-
-        // TODO remove this once the BCS format is no longer supported by the rest endpoint and clients
-        // wanting binary have migrated to grpc
-        let bytes = client
-            .get(url)
-            .header(reqwest::header::ACCEPT, sui_rpc_api::rest::APPLICATION_BCS)
-            .send()
-            .await
-            .unwrap()
-            .bytes()
-            .await
-            .unwrap();
-        let _committee = bcs::from_bytes::<ValidatorCommittee>(&bytes).unwrap();
-    }
-
-    let url = format!("{}/v2/system/committee", test_cluster.rpc_url(),);
-
-    raw_request(&url).await;
-
-    let url = format!("{}/v2/system/committee/0", test_cluster.rpc_url());
-    raw_request(&url).await;
 }
--- crates/sui-e2e-tests/tests/rpc/transactions.rs
+++ crates/sui-e2e-tests/tests/rpc/transactions.rs
@@ -3,7 +3,7 @@
 
 use sui_macros::sim_test;
 use sui_rpc_api::client::sdk::Client;
-use sui_rpc_api::rest::transactions::{ListTransactionsQueryParameters, TransactionResponse};
+use sui_rpc_api::rest::transactions::ListTransactionsQueryParameters;
 use test_cluster::TestClusterBuilder;
 
 use crate::transfer_coin;
@@ -17,36 +17,6 @@ async fn get_transaction() {
     let client = Client::new(test_cluster.rpc_url()).unwrap();
 
     let _transaction = client.get_transaction(&transaction_digest).await.unwrap();
-
-    let client = reqwest::Client::new();
-    let url = format!(
-        "{}/v2/transactions/{}",
-        test_cluster.rpc_url(),
-        transaction_digest,
-    );
-    // Make sure it works with json
-    let _transaction = client
-        .get(&url)
-        .header(reqwest::header::ACCEPT, sui_rpc_api::rest::APPLICATION_JSON)
-        .send()
-        .await
-        .unwrap()
-        .json::<TransactionResponse>()
-        .await
-        .unwrap();
-
-    // TODO remove this once the BCS format is no longer supported by the rest endpoint and clients
-    // wanting binary have migrated to grpc
-    let bytes = client
-        .get(&url)
-        .header(reqwest::header::ACCEPT, sui_rpc_api::rest::APPLICATION_BCS)
-        .send()
-        .await
-        .unwrap()
-        .bytes()
-        .await
-        .unwrap();
-    let _transaction = bcs::from_bytes::<TransactionResponse>(&bytes).unwrap();
 }
 
 #[sim_test]
@@ -64,30 +34,4 @@ async fn list_checkpoint() {
         .into_inner();
 
     assert!(!transactions.is_empty());
-
-    let client = reqwest::Client::new();
-    let url = format!("{}/v2/transactions", test_cluster.rpc_url());
-    // Make sure it works with json
-    let _transactions = client
-        .get(&url)
-        .header(reqwest::header::ACCEPT, sui_rpc_api::rest::APPLICATION_JSON)
-        .send()
-        .await
-        .unwrap()
-        .json::<Vec<TransactionResponse>>()
-        .await
-        .unwrap();
-
-    // TODO remove this once the BCS format is no longer supported by the rest endpoint and clients
-    // wanting binary have migrated to grpc
-    let bytes = client
-        .get(&url)
-        .header(reqwest::header::ACCEPT, sui_rpc_api::rest::APPLICATION_BCS)
-        .send()
-        .await
-        .unwrap()
-        .bytes()
-        .await
-        .unwrap();
-    let _transactions = bcs::from_bytes::<Vec<TransactionResponse>>(&bytes).unwrap();
 }
