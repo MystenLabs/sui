dfafcbc5b2c6ab5de06649576f5ed2345fb9c19d
nikos-terzo <nikos.terzo@mystenlabs.com>
2024-11-27 21:40:14 +0200

    Move test in test_coin_utils one level up to avoid double testing

--- crates/sui-rosetta/tests/custom_coins_tests.rs
+++ crates/sui-rosetta/tests/custom_coins_tests.rs
@@ -7,10 +7,11 @@ mod rosetta_client;
 mod test_coin_utils;
 
 use serde_json::json;
+use sui_types::object::Owner;
 use std::num::NonZeroUsize;
 use std::path::Path;
 use sui_json_rpc_types::{
-    SuiExecutionStatus, SuiTransactionBlockEffectsAPI, SuiTransactionBlockResponseOptions,
+    ObjectChange, SuiExecutionStatus, SuiTransactionBlockEffectsAPI, SuiTransactionBlockResponseOptions
 };
 use sui_rosetta::operations::Operations;
 use sui_rosetta::types::{
@@ -24,6 +25,52 @@ use test_cluster::TestClusterBuilder;
 use test_coin_utils::{init_package, mint};
 
 use crate::rosetta_client::{start_rosetta_test_server, RosettaEndpoint};
+
+#[tokio::test]
+async fn test_mint() {
+    const COIN1_BALANCE: u64 = 100_000_000;
+    const COIN2_BALANCE: u64 = 200_000_000;
+    let test_cluster = TestClusterBuilder::new().build().await;
+    let client = test_cluster.wallet.get_client().await.unwrap();
+    let keystore = &test_cluster.wallet.config.keystore;
+
+    let sender = test_cluster.get_address_0();
+    let init_ret = init_package(
+        &client,
+        keystore,
+        sender,
+        Path::new("tests/custom_coins/test_coin"),
+    )
+    .await
+    .unwrap();
+
+    let address1 = test_cluster.get_address_1();
+    let address2 = test_cluster.get_address_2();
+    let balances_to = vec![(COIN1_BALANCE, address1), (COIN2_BALANCE, address2)];
+
+    let mint_res = mint(&client, keystore, init_ret, balances_to)
+        .await
+        .unwrap();
+    let coins = mint_res
+        .object_changes
+        .unwrap()
+        .into_iter()
+        .filter_map(|change| {
+            if let ObjectChange::Created {
+                object_type, owner, ..
+            } = change
+            {
+                Some((object_type, owner))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    let coin1 = coins.iter().find(|coin| coin.1 == Owner::AddressOwner(address1)).unwrap();
+    let coin2 = coins.iter().find(|coin| coin.1 == Owner::AddressOwner(address2)).unwrap();
+    assert!(coin1.0.to_string().contains("::test_coin::TEST_COIN"));
+    assert!(coin2.0.to_string().contains("::test_coin::TEST_COIN"));
+}
 
 #[tokio::test]
 async fn test_custom_coin_balance() {
