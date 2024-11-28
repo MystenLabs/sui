31459d7de3f753447d2cd7e94991fe3b92430806
nikos-terzo <nikos.terzo@mystenlabs.com>
2024-11-28 21:06:47 +0200

    Some pay custom coin dust tests

--- crates/sui-rosetta/tests/dust_tests.rs
+++ crates/sui-rosetta/tests/dust_tests.rs
@@ -1,7 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+#[path = "dust_tests/pay_coin_with_dust.rs"]
+mod pay_coin_with_dust;
 #[path = "dust_tests/pay_with_dust.rs"]
 mod pay_with_dust;
+#[allow(dead_code)]
+mod rosetta_client;
 #[path = "dust_tests/split_coin.rs"]
 mod split_coin;
