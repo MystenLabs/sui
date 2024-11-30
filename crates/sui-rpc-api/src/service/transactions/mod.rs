f95db627196373b3405f8fa4a48973f5c7b2d61d
Brandon Williams <brandon@mystenlabs.com>
2024-11-30 15:51:22 -0600

    rpc: factor out get_transaction logic

---
+++ crates/sui-rpc-api/src/service/transactions/mod.rs
@@ -0,0 +1,57 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::types::GetTransactionOptions;
+use crate::types::TransactionResponse;
+use crate::Result;
+use crate::RpcService;
+use sui_sdk_types::types::TransactionDigest;
+use tap::Pipe;
+
+impl RpcService {
+    pub fn get_transaction(
+        &self,
+        transaction_digest: TransactionDigest,
+        options: &GetTransactionOptions,
+    ) -> Result<TransactionResponse> {
+        let crate::reader::TransactionRead {
+            digest,
+            transaction,
+            signatures,
+            effects,
+            events,
+            checkpoint,
+            timestamp_ms,
+        } = self.reader.get_transaction_read(transaction_digest)?;
+
+        let transaction_bcs = options
+            .include_transaction_bcs()
+            .then(|| bcs::to_bytes(&transaction))
+            .transpose()?;
+
+        let effects_bcs = options
+            .include_effects_bcs()
+            .then(|| bcs::to_bytes(&effects))
+            .transpose()?;
+
+        let events_bcs = options
+            .include_events_bcs()
+            .then(|| events.as_ref().map(bcs::to_bytes))
+            .flatten()
+            .transpose()?;
+
+        TransactionResponse {
+            digest,
+            transaction: options.include_transaction().then_some(transaction),
+            transaction_bcs,
+            signatures: options.include_signatures().then_some(signatures),
+            effects: options.include_effects().then_some(effects),
+            effects_bcs,
+            events: options.include_events().then_some(events).flatten(),
+            events_bcs,
+            checkpoint,
+            timestamp_ms,
+        }
+        .pipe(Ok)
+    }
+}
