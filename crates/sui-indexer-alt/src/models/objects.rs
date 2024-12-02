ad5af0c53d111fd77e3a12343cf9fff94b230e0d
Ashok Menon <ashok@mystenlabs.com>
2024-12-02 01:00:04 +0000

    indexer-alt: use `NULL`, instead of `DEFAULT VALUE`
    ## Description
    
    Add `#[diesel(treat_none_as_default_value = false)]` to model types that
    include optional fields. This affects how those fields are written out
    to SQL when they contain `None`. Previously (and by default), those
    fields would be represented by the keyword `DEFAULT VALUE`, and after
    this change, they will be represented by a parameter binding, which will
    be bound to `NULL`.
    
    This is semantically identical in our case, because we don't set default
    values, but it also results in less variety in prepared statements
    (because regardless of the content of fields, they will now all be
    represented by a binding), which will improve grouping of statistics
    per-statement, and could also improve performance, if those prepared
    statements can be cached and re-used.
    
    ## Test plan
    
    Re-run indexer on first 100000 checkpoints.

--- crates/sui-indexer-alt/src/models/objects.rs
+++ crates/sui-indexer-alt/src/models/objects.rs
@@ -15,6 +15,7 @@ use crate::schema::{
 
 #[derive(Insertable, Debug, Clone, FieldCount)]
 #[diesel(table_name = kv_objects, primary_key(object_id, object_version))]
+#[diesel(treat_none_as_default_value = false)]
 pub struct StoredObject {
     pub object_id: Vec<u8>,
     pub object_version: i64,
@@ -62,7 +63,6 @@ pub enum StoredOwnerKind {
 
 #[derive(Insertable, Debug, Clone, FieldCount)]
 #[diesel(table_name = sum_coin_balances, primary_key(object_id))]
-#[diesel(treat_none_as_default_value = false)]
 pub struct StoredSumCoinBalance {
     pub object_id: Vec<u8>,
     pub object_version: i64,
@@ -87,6 +87,7 @@ pub struct StoredSumObjType {
 
 #[derive(Insertable, Debug, Clone)]
 #[diesel(table_name = wal_coin_balances, primary_key(object_id, object_version))]
+#[diesel(treat_none_as_default_value = false)]
 pub struct StoredWalCoinBalance {
     pub object_id: Vec<u8>,
     pub object_version: i64,
@@ -98,6 +99,7 @@ pub struct StoredWalCoinBalance {
 
 #[derive(Insertable, Debug, Clone)]
 #[diesel(table_name = wal_obj_types, primary_key(object_id, object_version))]
+#[diesel(treat_none_as_default_value = false)]
 pub struct StoredWalObjType {
     pub object_id: Vec<u8>,
     pub object_version: i64,
