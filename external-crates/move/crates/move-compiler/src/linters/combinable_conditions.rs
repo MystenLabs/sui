be60d7b4cdc579cedaf7a26670d64338b803c2a8
Todd Nowacki <tmn@mystenlabs.com>
2024-12-04 19:46:32 -0800

    rewrite

--- external-crates/move/crates/move-compiler/src/linters/combinable_conditions.rs
+++ external-crates/move/crates/move-compiler/src/linters/combinable_conditions.rs
@@ -6,139 +6,318 @@
 //! This rule focuses on simplifying expressions involving `==`, `<`, `>`, and `!=` operators to improve code readability.
 
 use crate::{
+    cfgir::visitor::{same_value_exp, simple_visitor},
     diag,
+    hlir::ast::{self as H, UnannotatedExp_},
     linters::StyleCodes,
-    parser::ast::BinOp_,
-    typing::{
-        ast::{self as T, UnannotatedExp_},
-        visitor::simple_visitor,
-    },
+    parser::ast::{BinOp, BinOp_},
 };
-use lazy_static::lazy_static;
-use move_ir_types::location::Loc;
-use std::collections::HashMap;
+use move_ir_types::location::*;
 
 #[derive(Debug, Clone, Copy)]
 enum Simplification {
-    Contradiction,
-    UseComparison,
-    UseEquality,
+    Reducible(InnerOp_),
+    AlwaysTrue,
+    AlwaysFalse,
 }
 
-impl Simplification {
-    fn message(&self) -> &'static str {
-        match self {
-            Simplification::Contradiction => {
-                "This is always contradictory and can be simplified to false"
-            }
-            Simplification::UseComparison => "Consider simplifying to `<=` or `>=` respectively.",
-            Simplification::UseEquality => "Consider simplifying to `==`.",
-        }
-    }
-}
+// impl Simplification {
+//     fn message(&self) -> &'static str {
+//         match self {
+//             Simplification::SameOp
+//             Simplification::Contradiction => {
+//                 "This is always contradictory and can be simplified to false"
+//             }
+//             Simplification::UseComparison => "Consider simplifying to `<=` or `>=` respectively.",
+//             Simplification::UseEquality => "Consider simplifying to `==`.",
+//         }
+//     }
+// }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-enum Operator {
+#[derive(Debug, Clone, Copy)]
+enum OuterOp_ {
+    And,
+    Or,
+}
+type OuterOp = Spanned<OuterOp_>;
+
+#[derive(Debug, Clone, Copy)]
+enum InnerOp_ {
     Eq,
+    Neq,
     Lt,
     Gt,
     Le,
     Ge,
-    And,
-    Or,
 }
-
-impl From<&BinOp_> for Operator {
-    fn from(op: &BinOp_) -> Self {
-        match op {
-            BinOp_::Eq => Operator::Eq,
-            BinOp_::Lt => Operator::Lt,
-            BinOp_::Gt => Operator::Gt,
-            BinOp_::Le => Operator::Le,
-            BinOp_::Ge => Operator::Ge,
-            BinOp_::And => Operator::And,
-            BinOp_::Or => Operator::Or,
-            _ => panic!("Unexpected operator"),
-        }
-    }
-}
-
-lazy_static! {
-    static ref OPERATOR_COMBINATIONS: HashMap<(Operator, Operator, Operator), Simplification> = {
-        let mut m = HashMap::new();
-        // Contradictions
-        for ops in [(Operator::Eq, Operator::Lt), (Operator::Eq, Operator::Gt)] {
-            m.insert((ops.0, ops.1, Operator::And), Simplification::Contradiction);
-            m.insert((ops.1, ops.0, Operator::And), Simplification::Contradiction);
-        }
-        // Use comparison operators
-        for ops in [(Operator::Eq, Operator::Lt), (Operator::Eq, Operator::Gt)] {
-            m.insert((ops.0, ops.1, Operator::Or), Simplification::UseComparison);
-            m.insert((ops.1, ops.0, Operator::Or), Simplification::UseComparison);
-        }
-        // Use equality
-        for ops in [(Operator::Ge, Operator::Eq), (Operator::Le, Operator::Eq)] {
-            m.insert((ops.0, ops.1, Operator::And), Simplification::UseEquality);
-            m.insert((ops.1, ops.0, Operator::And), Simplification::UseEquality);
-        }
-        m
-    };
-}
+type InnerOp = Spanned<InnerOp_>;
 
 simple_visitor!(
     CombinableBoolConditionsVisitor,
-    fn visit_exp_custom(&mut self, exp: &T::Exp) -> bool {
-        if let UnannotatedExp_::BinopExp(e1, op, _, e2) = &exp.exp.value {
-            if let (
-                UnannotatedExp_::BinopExp(lhs1, op1, _, rhs1),
-                UnannotatedExp_::BinopExp(lhs2, op2, _, rhs2),
-            ) = (&e1.exp.value, &e2.exp.value)
-            {
-                check_combinable_conditions(
-                    self,
-                    exp.exp.loc,
-                    lhs1,
-                    rhs1,
-                    lhs2,
-                    rhs2,
-                    &op1.value,
-                    &op2.value,
-                    &op.value,
-                );
+    fn visit_exp_custom(&mut self, exp: &H::Exp) -> bool {
+        use H::UnannotatedExp_ as E;
+        let E::BinopExp(outer_l, outer_bop, outer_r) = &exp.exp.value else {
+            return false;
+        };
+        let E::BinopExp(l1, op_l, r1) = &outer_l.exp.value else {
+            return false;
+        };
+        let E::BinopExp(l2, op_r, r2) = &outer_r.exp.value else {
+            return false;
+        };
+        let Some((outer, inner_l, inner_r)) = binop_case(outer_bop, l1, op_l, r1, l2, op_r, r2)
+        else {
+            return false;
+        };
+        let simplification = match outer.value {
+            OuterOp_::And => simplify_and(inner_l, inner_r),
+            OuterOp_::Or => simplify_or(inner_l, inner_r),
+        };
+        let msg = match simplification {
+            Simplification::Reducible(inner_op) => {
+                format!("to just the operation '{}'", inner_op)
             }
-        }
+            Simplification::AlwaysTrue => "is always 'true'".to_string(),
+            Simplification::AlwaysFalse => "is always 'false'".to_string(),
+        };
+        self.reporter.add_diag(diag!(
+            StyleCodes::CombinableBoolConditions.diag_info(),
+            (exp.exp.loc, format!("This comparison {msg}")),
+        ));
 
         false
     }
 );
 
-fn check_combinable_conditions(
-    context: &mut Context,
-    loc: Loc,
-    lhs1: &T::Exp,
-    rhs1: &T::Exp,
-    lhs2: &T::Exp,
-    rhs2: &T::Exp,
-    op1: &BinOp_,
-    op2: &BinOp_,
-    parent_op: &BinOp_,
-) {
-    if lhs1 == lhs2 && rhs1 == rhs2 && !is_module_call(lhs1) && !is_module_call(rhs1) {
-        let key = (
-            Operator::from(op1),
-            Operator::from(op2),
-            Operator::from(parent_op),
-        );
-        if let Some(simplification) = OPERATOR_COMBINATIONS.get(&key) {
-            let diagnostic = diag!(
-                StyleCodes::CombinableBoolConditions.diag_info(),
-                (loc, simplification.message())
-            );
-            context.add_diag(diagnostic); // Using context instead of self
+fn simplify_and(op1: InnerOp, op2: InnerOp) -> Simplification {
+    use InnerOp_ as I;
+    Simplification::Reducible(match (op1.value, op2.value) {
+        // same operation
+        (I::Eq, I::Eq)
+        | (I::Neq, I::Neq)
+        | (I::Ge, I::Ge)
+        | (I::Le, I::Le)
+        | (I::Lt, I::Lt)
+        | (I::Gt, I::Gt) => op1.value,
+
+        // contradiction
+        (I::Lt, I::Gt)
+        | (I::Gt, I::Lt)
+        | (I::Lt, I::Ge)
+        | (I::Ge, I::Lt)
+        | (I::Le, I::Gt)
+        | (I::Gt, I::Le)
+        | (I::Eq, I::Lt)
+        | (I::Lt, I::Eq)
+        | (I::Eq, I::Gt)
+        | (I::Gt, I::Eq)
+        | (I::Neq, I::Eq)
+        | (I::Eq, I::Neq) => return Simplification::AlwaysFalse,
+
+        // ==
+        (I::Le, I::Ge)
+        | (I::Ge, I::Le)
+        | (I::Ge, I::Eq)
+        | (I::Eq, I::Ge)
+        | (I::Le, I::Eq)
+        | (I::Eq, I::Le) => I::Eq,
+
+        // <
+        (I::Lt, I::Le)
+        | (I::Le, I::Lt)
+        | (I::Lt, I::Neq)
+        | (I::Neq, I::Lt)
+        | (I::Le, I::Neq)
+        | (I::Neq, I::Le) => I::Lt,
+        // >
+        (I::Gt, I::Ge)
+        | (I::Ge, I::Gt)
+        | (I::Gt, I::Neq)
+        | (I::Neq, I::Gt)
+        | (I::Ge, I::Neq)
+        | (I::Neq, I::Ge) => I::Gt,
+    })
+}
+
+fn simplify_or(op1: InnerOp, op2: InnerOp) -> Simplification {
+    use InnerOp_ as I;
+    Simplification::Reducible(match (op1.value, op2.value) {
+        // same operation
+        (I::Eq, I::Eq)
+        | (I::Neq, I::Neq)
+        | (I::Ge, I::Ge)
+        | (I::Le, I::Le)
+        | (I::Lt, I::Lt)
+        | (I::Gt, I::Gt) => op1.value,
+
+        // tautology
+        (I::Neq, I::Le)
+        | (I::Neq, I::Ge)
+        | (I::Le, I::Neq)
+        | (I::Ge, I::Neq)
+        | (I::Gt, I::Le)
+        | (I::Le, I::Gt)
+        | (I::Lt, I::Ge)
+        | (I::Ge, I::Lt)
+        | (I::Ge, I::Le)
+        | (I::Le, I::Ge)
+        | (I::Neq, I::Eq)
+        | (I::Eq, I::Neq) => return Simplification::AlwaysTrue,
+
+        // !=
+        (I::Neq, I::Lt)
+        | (I::Neq, I::Gt)
+        | (I::Lt, I::Neq)
+        | (I::Gt, I::Neq)
+        | (I::Lt, I::Gt)
+        | (I::Gt, I::Lt) => I::Neq,
+
+        // <=
+        (I::Lt, I::Le)
+        | (I::Le, I::Lt)
+        | (I::Eq, I::Lt)
+        | (I::Lt, I::Eq)
+        | (I::Eq, I::Le)
+        | (I::Le, I::Eq) => I::Le,
+        // >=
+        (I::Gt, I::Ge)
+        | (I::Ge, I::Gt)
+        | (I::Eq, I::Gt)
+        | (I::Eq, I::Ge)
+        | (I::Gt, I::Eq)
+        | (I::Ge, I::Eq) => I::Ge,
+    })
+}
+
+fn outer(sp!(loc, bop_): &BinOp) -> Option<OuterOp> {
+    let op_ = match bop_ {
+        BinOp_::And => OuterOp_::And,
+        BinOp_::Or => OuterOp_::Or,
+        BinOp_::Eq
+        | BinOp_::Lt
+        | BinOp_::Gt
+        | BinOp_::Le
+        | BinOp_::Ge
+        | BinOp_::Add
+        | BinOp_::Sub
+        | BinOp_::Mul
+        | BinOp_::Mod
+        | BinOp_::Div
+        | BinOp_::BitOr
+        | BinOp_::BitAnd
+        | BinOp_::Xor
+        | BinOp_::Shl
+        | BinOp_::Shr
+        | BinOp_::Range
+        | BinOp_::Implies
+        | BinOp_::Iff
+        | BinOp_::Neq => return None,
+    };
+    Some(sp(*loc, op_))
+}
+
+fn inner(sp!(loc, bop_): &BinOp) -> Option<InnerOp> {
+    let op_ = match bop_ {
+        BinOp_::Eq => InnerOp_::Eq,
+        BinOp_::Neq => InnerOp_::Neq,
+        BinOp_::Lt => InnerOp_::Lt,
+        BinOp_::Gt => InnerOp_::Gt,
+        BinOp_::Le => InnerOp_::Le,
+        BinOp_::Ge => InnerOp_::Ge,
+
+        BinOp_::Add
+        | BinOp_::Sub
+        | BinOp_::Mul
+        | BinOp_::Mod
+        | BinOp_::Div
+        | BinOp_::BitOr
+        | BinOp_::BitAnd
+        | BinOp_::Xor
+        | BinOp_::Shl
+        | BinOp_::Shr
+        | BinOp_::Range
+        | BinOp_::Implies
+        | BinOp_::Iff
+        | BinOp_::And
+        | BinOp_::Or => return None,
+    };
+    Some(sp(*loc, op_))
+}
+
+fn flip(sp!(loc, op_): InnerOp) -> InnerOp {
+    sp(
+        loc,
+        match op_ {
+            InnerOp_::Eq => InnerOp_::Eq,
+            InnerOp_::Neq => InnerOp_::Neq,
+            InnerOp_::Lt => InnerOp_::Gt,
+            InnerOp_::Gt => InnerOp_::Lt,
+            InnerOp_::Le => InnerOp_::Ge,
+            InnerOp_::Ge => InnerOp_::Le,
+        },
+    )
+}
+
+fn binop_case(
+    outer_bop: &BinOp,
+    l1: &H::Exp,
+    op_l: &BinOp,
+    r1: &H::Exp,
+    l2: &H::Exp,
+    op_r: &BinOp,
+    r2: &H::Exp,
+) -> Option<(OuterOp, InnerOp, InnerOp)> {
+    let outer = outer(outer_bop)?;
+    let inner_l = inner(op_l)?;
+    let inner_r = inner(op_r)?;
+    let (inner_l, inner_r) = operand_case(l1, inner_l, r1, l2, inner_r, r2)?;
+    Some((outer, inner_l, inner_r))
+}
+
+fn operand_case(
+    l1: &H::Exp,
+    op1: InnerOp,
+    r1: &H::Exp,
+    l2: &H::Exp,
+    op2: InnerOp,
+    r2: &H::Exp,
+) -> Option<(InnerOp, InnerOp)> {
+    if same_value_exp(l1, l2) && same_value_exp(r1, r2) {
+        // a1 := l1
+        // a2 := l2
+        // b1 := r1
+        // b2 := r2
+        if same_value_exp(l1, r1) {
+            // Covered by EqualOperands
+            None
+        } else {
+            Some((op1, op2))
         }
+    } else if same_value_exp(l1, r2) && same_value_exp(r1, l2) {
+        // a1 := l1
+        // a2 := r2
+        // b1 := r1
+        // b2 := l2
+        Some((op1, flip(op2)))
+    } else {
+        None
     }
 }
 
-fn is_module_call(exp: &T::Exp) -> bool {
-    matches!(exp.exp.value, UnannotatedExp_::ModuleCall(_))
+impl std::fmt::Display for InnerOp_ {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use InnerOp_ as I;
+        write!(
+            f,
+            "{}",
+            match self {
+                I::Eq => "==",
+                I::Neq => "!=",
+                I::Lt => "<",
+                I::Gt => ">",
+                I::Le => "<=",
+                I::Ge => ">=",
+            }
+        )
+    }
 }
