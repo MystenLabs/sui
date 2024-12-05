c777f43d117e840c64ad884c7ca49208f16a392e
Andrew Schran <aschran@mystenlabs.com>
2024-12-05 10:38:17 -0500

    separate index changes into a different migration

--- crates/sui-indexer-alt/migrations/2024-12-02-185822_add_coin_balance_owner_kind/up.sql
+++ crates/sui-indexer-alt/migrations/2024-12-02-185822_add_coin_balance_owner_kind/up.sql
@@ -1,14 +1,6 @@
 ALTER TABLE sum_coin_balances
 ADD COLUMN coin_owner_kind SMALLINT NOT NULL DEFAULT 1;
 
-DROP INDEX sum_coin_balances_owner_type;
-CREATE INDEX sum_coin_balances_owner_type
-ON sum_coin_balances (coin_owner_kind, owner_id, coin_type, coin_balance, object_id, object_version);
-
 ALTER TABLE wal_coin_balances
 ADD COLUMN coin_owner_kind SMALLINT;
-UPDATE wal_coin_balances SET owner_kind = 1 WHERE owner_id IS NOT NULL;
-
-DROP INDEX wal_coin_balances_owner_type;
-CREATE INDEX wal_coin_balances_owner_type
-ON wal_coin_balances (coin_owner_kind, owner_id, coin_type, coin_balance, object_id, object_version);
+UPDATE wal_coin_balances SET coin_owner_kind = 1 WHERE owner_id IS NOT NULL;
