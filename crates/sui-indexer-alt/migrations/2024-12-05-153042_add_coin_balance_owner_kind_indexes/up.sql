e0a59447dfbd26919bf4001ae47c934b9b1cee45
Andrew Schran <aschran@mystenlabs.com>
2024-12-05 11:30:01 -0500

    separate migrations even further

--- crates/sui-indexer-alt/migrations/2024-12-05-153042_add_coin_balance_owner_kind_indexes/up.sql
+++ crates/sui-indexer-alt/migrations/2024-12-05-153042_add_coin_balance_owner_kind_indexes/up.sql
@@ -1,7 +1,1 @@
 DROP INDEX CONCURRENTLY IF EXISTS sum_coin_balances_owner_type;
-CREATE INDEX CONCURRENTLY IF NOT EXISTS sum_coin_balances_owner_kind_owner_id_type
-ON sum_coin_balances (coin_owner_kind, owner_id, coin_type, coin_balance, object_id, object_version);
-
-DROP INDEX CONCURRENTLY IF EXISTS wal_coin_balances_owner_type;
-CREATE INDEX CONCURRENTLY IF NOT EXISTS wal_coin_balances_owner_kind_owner_id_type
-ON wal_coin_balances (coin_owner_kind, owner_id, coin_type, coin_balance, object_id, object_version);
