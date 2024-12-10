c823d1a7baa2ded3f2ed3dff11e9c09428b6b81d
Daniel Poelzleithner <git@poelzi.org>
2024-12-11 00:44:27 +0100

    Use env to run scripts
    Use /usr/bin/env as it is the most compatible way to run any executable in PATH
    as a shebang

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, fenix, nixpkgs }: {
+    packages.x86_64-linux.default = fenix.packages.x86_64-linux.minimal.toolchain;
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [ fenix.overlays.default ];
+          environment.systemPackages = with pkgs; [
+            (fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            rust-analyzer-nightly
+          ];
+        })
+      ];
+    };
+  };
+}
