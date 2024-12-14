9f5e865a8b11e331de01104d526f4de321f559c0
Michael George <mike.george@mystenlabs.com>
2024-12-14 04:45:07 -0500

    converted yaml snapshots to inline (it's a lot less confusing); added remaining tests; commented out tests for unimplemented features

--- crates/sui-move/tests/new_tests.rs
+++ crates/sui-move/tests/new_tests.rs
@@ -3,7 +3,7 @@
 
 use anyhow;
 use cli_sandbox::Project;
-use insta::{assert_snapshot, assert_yaml_snapshot};
+use insta::{assert_debug_snapshot, assert_snapshot};
 use insta_cmd::{assert_cmd_snapshot, get_cargo_bin};
 use std::io::Read;
 use std::path::Path;
@@ -14,6 +14,7 @@ use std::{fs, fs::File, path::PathBuf, process::Command};
 
 /// # Test infrastructure //////////////////////////////////////////////////////////////////////////
 
+/// create a `sui-move` Command that runs in a new temporary directory
 fn sui_move() -> (Project, Command) {
     let project = Project::new().unwrap();
     let mut cmd = Command::new(get_cargo_bin("sui-move"));
@@ -77,12 +78,24 @@ fn test_new_file_recursive_paths() {
     new_file(&mut proj, "qux.txt", "qux").unwrap();
     new_file(&mut proj, "quux/.foo", "qux").unwrap();
 
-    assert_yaml_snapshot!(recursive_paths(proj.path()));
+    assert_debug_snapshot!(recursive_paths(proj.path()), @r###"
+    [
+        "",
+        "foo",
+        "foo/bar.txt",
+        "foo/baz",
+        "foo/baz/baz.txt",
+        "quux",
+        "quux/.foo",
+        "qux.txt",
+    ]
+    "###);
 }
 
 /// # Tests for `sui move new` /////////////////////////////////////////////////////////////////////
 
 #[test]
+/// Check files created by `sui-move new example`, where `example/` doesn't exist
 fn test_new_basic() {
     let (mut proj, mut cmd) = sui_move();
 
@@ -96,15 +109,27 @@ fn test_new_basic() {
     "###);
 
     // check list of files
-    assert_yaml_snapshot!(recursive_paths(proj.path().join("example")));
+    assert_debug_snapshot!(recursive_paths(proj.path().join("example")), @r###"
+    [
+        "",
+        ".gitignore",
+        "Move.toml",
+        "sources",
+        "sources/example.move",
+        "tests",
+        "tests/example_tests.move",
+    ]
+    "###);
 
     // check .gitignore contents
     assert_snapshot!(slurp_file(&mut proj, "example/.gitignore").expect("sui move new creates .gitignore"), @r###"
     build/*
-    "###)
+    "###);
 }
 
 #[test]
+/// `sui-move new example` when `example/.gitignore` exists: it should be modified rather than
+/// replaced
 fn test_new_gitignore_exists() {
     let (mut proj, mut cmd) = sui_move();
 
@@ -121,7 +146,17 @@ fn test_new_gitignore_exists() {
     "###);
 
     // check list of files
-    assert_yaml_snapshot!(recursive_paths(proj.path().join("example")));
+    assert_debug_snapshot!(recursive_paths(proj.path().join("example")), @r###"
+    [
+        "",
+        ".gitignore",
+        "Move.toml",
+        "sources",
+        "sources/example.move",
+        "tests",
+        "tests/example_tests.move",
+    ]
+    "###);
 
     // check .gitignore contents
     assert_snapshot!(slurp_file(&mut proj, "example/.gitignore").expect("sui move new updates .gitignore"), @r###"
@@ -131,27 +166,156 @@ fn test_new_gitignore_exists() {
 }
 
 #[test]
-fn test_new_sources_exists() {
-    // TODO
-}
-
-#[test]
-fn test_new_tests_exists() {
-    // TODO
-}
-
-#[test]
-fn test_new_move_toml_exists() {
+/// `sui-move new example` when `example/.gitignore` already contains `build/*`: it should be
+/// unchanged
+fn test_new_gitignore_has_build() {
     let (mut proj, mut cmd) = sui_move();
 
-    // create .gitignore file
-    new_file(&mut proj, "example/Move.toml", "dummy").unwrap();
+    // create .gitignore file containing `build/*`
+    new_file(
+        &mut proj,
+        "example/.gitignore",
+        r###"
+        first_ignore
+        build/*
+        another_ignore
+        "###,
+    )
+    .unwrap();
 
     // sui move new example
     assert_cmd_snapshot!(cmd.arg("new").arg("example"), @r###"
-    success: false
-    "###);
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    "###,
+    );
 
     // check list of files
-    assert_yaml_snapshot!(recursive_paths(proj.path().join("example")));
+    assert_debug_snapshot!(recursive_paths(proj.path().join("example")), @r###"
+    [
+        "",
+        ".gitignore",
+        "Move.toml",
+        "sources",
+        "sources/example.move",
+        "tests",
+        "tests/example_tests.move",
+    ]
+    "###);
+
+    // check .gitignore contents
+    assert_snapshot!(slurp_file(&mut proj, "example/.gitignore").expect("sui move new updates .gitignore"), @r###"
+    first_ignore
+    build/*
+    another_ignore
+    "###);
 }
+
+// TODO: implement this functionality (linear: https://linear.app/mysten-labs/issue/DVX-486/sui-move-new-will-clobber-existing-files)
+//
+// #[test]
+// /// `sui-move new example` when `example/sources` exists should not generate any new example source
+// /// but should otherwise operate normally
+// fn test_new_sources_exists() {
+//     let (mut proj, mut cmd) = sui_move();
+//
+//     // create .gitignore and sources/ files
+//     new_file(&mut proj, "example/.gitignore", "existing_ignore\n").unwrap();
+//     new_file(&mut proj, "example/sources/dummy.txt", "").unwrap();
+//
+//     // sui move new example
+//     assert_cmd_snapshot!(cmd.arg("new").arg("example"), @r###"
+//     success: true
+//     exit_code: 0
+//     ----- stdout -----
+//
+//     ----- stderr -----
+//     "###);
+//
+//     // check list of files - no new source files
+//     assert_debug_snapshot!(recursive_paths(proj.path().join("example")), @r###"
+//     [
+//         "",
+//         ".gitignore",
+//         "Move.toml",
+//         "sources",
+//         "sources/dummy.txt",
+//         "tests",
+//     ]
+//     "###);
+//
+//     // check .gitignore contents
+//     assert_snapshot!(slurp_file(&mut proj, "example/.gitignore").expect("sui move new creates .gitignore"), @r###"
+//     existing_ignore
+//     build/*
+//     "###);
+// }
+//
+// #[test]
+// /// `sui-move new example` when `example/tests` exists should not generate any new example source
+// /// but should otherwise operate normally
+// fn test_new_tests_exists() {
+//     let (mut proj, mut cmd) = sui_move();
+//
+//     // create .gitignore and tests/ files
+//     new_file(&mut proj, "example/.gitignore", "existing_ignore\n").unwrap();
+//     new_file(&mut proj, "example/tests/dummy.txt", "").unwrap();
+//
+//     // sui move new example
+//     assert_cmd_snapshot!(cmd.arg("new").arg("example"), @r###"
+//     success: true
+//     exit_code: 0
+//     ----- stdout -----
+//
+//     ----- stderr -----
+//     "###);
+//
+//     // check list of files - no new source files
+//     assert_debug_snapshot!(recursive_paths(proj.path().join("example")), @r###"
+//     [
+//         "",
+//         ".gitignore",
+//         "Move.toml",
+//         "sources",
+//         "tests",
+//         "tests/dummy.txt",
+//     ]
+//     "###);
+//
+//     // check .gitignore contents
+//     assert_snapshot!(slurp_file(&mut proj, "example/.gitignore").expect("sui move new creates .gitignore"), @r###"
+//     existing_ignore
+//     build/*
+//     "###);
+// }
+//
+// #[test]
+// /// `sui-move new example` when `example/Move.toml` exists should fail and modify nothing
+// fn test_new_move_toml_exists() {
+//     let (mut proj, mut cmd) = sui_move();
+//
+//     // create files
+//     new_file(&mut proj, "example/Move.toml", "dummy").unwrap();
+//     new_file(&mut proj, "example/.gitignore", "existing_ignore\n").unwrap();
+//
+//     // sui move new example
+//     assert_cmd_snapshot!(cmd.arg("new").arg("example"), @r###"
+//     success: false
+//     "###);
+//
+//     // check list of files - should be unchanged
+//     assert_debug_snapshot!(recursive_paths(proj.path().join("example")), @r###"
+//     [
+//          "",
+//          "Move.toml",
+//          ".gitignore",
+//     ]
+//     "###);
+//
+//     // check that files are unchanged
+//     assert_snapshot!(slurp_file(&mut proj, "example/Move.toml").expect("Move.toml should exist"), @"dummy");
+//     assert_snapshot!(slurp_file(&mut proj, "example/.gitignore").expect(".gitignore should exist"), @"existing_ignore\n");
+// }
