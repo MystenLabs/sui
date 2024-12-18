1e5bbcdd760fd3fc51747da33728facf92309bd7
Ge Gao <ge@mystenlabs.com>
2024-12-18 13:14:56 -0500

    indexer alt restorer: init commit

---
+++ crates/sui-indexer-alt-restorer/src/archives.rs
@@ -0,0 +1,58 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::num::NonZeroUsize;
+
+use prometheus::Registry;
+use sui_types::digests::CheckpointDigest;
+use tracing::info;
+
+use sui_archival::reader::{ArchiveReader, ArchiveReaderMetrics};
+use sui_config::node::ArchiveReaderConfig;
+use sui_config::object_storage_config::{ObjectStoreConfig, ObjectStoreType};
+
+use crate::Args;
+
+#[derive(Clone, Debug)]
+pub(crate) struct ArchivalCheckpointInfo {
+    pub next_checkpoint_after_epoch: u64,
+    #[allow(unused)]
+    pub chain_identifier: CheckpointDigest,
+}
+
+pub(crate) async fn read_archival_checkpoint_info(
+    args: &Args,
+) -> Result<ArchivalCheckpointInfo, anyhow::Error> {
+    let archive_store_config = ObjectStoreConfig {
+        object_store: Some(ObjectStoreType::GCS),
+        bucket: Some(args.archive_bucket.clone()),
+        object_store_connection_limit: args.concurrency,
+        no_sign_request: false,
+        ..Default::default()
+    };
+    let archive_reader_config = ArchiveReaderConfig {
+        remote_store_config: archive_store_config,
+        download_concurrency: NonZeroUsize::new(args.concurrency).unwrap(),
+        use_for_pruning_watermark: false,
+    };
+    let metrics = ArchiveReaderMetrics::new(&Registry::default());
+    let archive_reader = ArchiveReader::new(archive_reader_config, &metrics)?;
+    archive_reader.sync_manifest_once().await?;
+    let manifest = archive_reader.get_manifest().await?;
+    let next_checkpoint_after_epoch = manifest.next_checkpoint_after_epoch(args.start_epoch);
+    info!(
+        "Read from archives: next checkpoint sequence after epoch {} is: {}",
+        args.start_epoch, next_checkpoint_after_epoch
+    );
+    let cp_summaries = archive_reader
+        .get_summaries_for_list_no_verify(vec![0])
+        .await?;
+    let first_cp = cp_summaries
+        .first()
+        .ok_or_else(|| anyhow::anyhow!("No checkpoint found"))?;
+    let chain_identifier = *first_cp.digest();
+    Ok(ArchivalCheckpointInfo {
+        next_checkpoint_after_epoch,
+        chain_identifier,
+    })
+}
---
+++ crates/sui-indexer-alt-restorer/src/lib.rs
@@ -0,0 +1,44 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+mod archives;
+mod snapshot;
+
+use archives::read_archival_checkpoint_info;
+use clap::Parser;
+use sui_pg_db::DbArgs;
+
+use crate::snapshot::SnapshotRestorer;
+
+#[derive(Parser, Debug, Clone)]
+#[clap(name = "sui-indexer-restorer")]
+pub struct Args {
+    #[clap(long, env = "START_EPOCH", required = true)]
+    pub start_epoch: u64,
+
+    #[clap(long, env = "ENDPOINT", required = true)]
+    pub endpoint: String,
+
+    #[clap(long, env = "SNAPSHOT_BUCKET", required = true)]
+    pub snapshot_bucket: String,
+
+    #[clap(long, env = "ARCHIVE_BUCKET", required = true)]
+    pub archive_bucket: String,
+
+    #[clap(long, env = "SNAPSHOT_LOCAL_DIR", required = true)]
+    pub snapshot_local_dir: String,
+
+    #[clap(long, env = "CONCURRENCY", default_value_t = 50)]
+    pub concurrency: usize,
+
+    #[clap(flatten)]
+    pub db_args: DbArgs,
+}
+
+pub async fn restore(args: &Args) -> anyhow::Result<()> {
+    let archival_checkpoint_info = read_archival_checkpoint_info(args).await?;
+    let mut snapshot_restorer =
+        SnapshotRestorer::new(args, archival_checkpoint_info.next_checkpoint_after_epoch).await?;
+    snapshot_restorer.restore().await?;
+    Ok(())
+}
