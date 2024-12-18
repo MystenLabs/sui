f9a8636f4507606c3778492fc56b7c657fe48f4a
Brandon Williams <brandon@mystenlabs.com>
2024-12-18 16:30:15 -0600

    rpc: add grpc tests for GetTransaction

--- crates/sui-rpc-api/src/proto/node.rs
+++ crates/sui-rpc-api/src/proto/node.rs
@@ -334,6 +334,114 @@ impl From<GetCheckpointOptions> for crate::types::GetCheckpointOptions {
 // GetTransactionOptions
 //
 
+impl GetTransactionOptions {
+    pub fn all() -> Self {
+        Self {
+            transaction: Some(true),
+            transaction_bcs: Some(true),
+            signatures: Some(true),
+            signatures_bytes: Some(true),
+            effects: Some(true),
+            effects_bcs: Some(true),
+            events: Some(true),
+            events_bcs: Some(true),
+        }
+    }
+
+    pub fn none() -> Self {
+        Self {
+            transaction: Some(false),
+            transaction_bcs: Some(false),
+            signatures: Some(false),
+            signatures_bytes: Some(false),
+            effects: Some(false),
+            effects_bcs: Some(false),
+            events: Some(false),
+            events_bcs: Some(false),
+        }
+    }
+
+    pub fn with_transaction(mut self) -> Self {
+        self.transaction = Some(true);
+        self
+    }
+
+    pub fn without_transaction(mut self) -> Self {
+        self.transaction = Some(false);
+        self
+    }
+
+    pub fn with_transaction_bcs(mut self) -> Self {
+        self.transaction_bcs = Some(true);
+        self
+    }
+
+    pub fn without_transaction_bcs(mut self) -> Self {
+        self.transaction_bcs = Some(false);
+        self
+    }
+
+    pub fn with_signatures(mut self) -> Self {
+        self.signatures = Some(true);
+        self
+    }
+
+    pub fn without_signatures(mut self) -> Self {
+        self.signatures = Some(false);
+        self
+    }
+
+    pub fn with_signatures_bytes(mut self) -> Self {
+        self.signatures_bytes = Some(true);
+        self
+    }
+
+    pub fn without_signatures_bytes(mut self) -> Self {
+        self.signatures_bytes = Some(false);
+        self
+    }
+
+    pub fn with_effects(mut self) -> Self {
+        self.effects = Some(true);
+        self
+    }
+
+    pub fn without_effects(mut self) -> Self {
+        self.effects = Some(false);
+        self
+    }
+
+    pub fn with_effects_bcs(mut self) -> Self {
+        self.effects_bcs = Some(true);
+        self
+    }
+
+    pub fn without_effects_bcs(mut self) -> Self {
+        self.effects_bcs = Some(false);
+        self
+    }
+
+    pub fn with_events(mut self) -> Self {
+        self.events = Some(true);
+        self
+    }
+
+    pub fn without_events(mut self) -> Self {
+        self.events = Some(false);
+        self
+    }
+
+    pub fn with_events_bcs(mut self) -> Self {
+        self.events_bcs = Some(true);
+        self
+    }
+
+    pub fn without_events_bcs(mut self) -> Self {
+        self.events_bcs = Some(false);
+        self
+    }
+}
+
 impl From<crate::types::GetTransactionOptions> for GetTransactionOptions {
     fn from(
         crate::types::GetTransactionOptions {
@@ -383,6 +491,24 @@ impl From<GetTransactionOptions> for crate::types::GetTransactionOptions {
             events,
             events_bcs,
         }
+    }
+}
+
+//
+// GetTransactionRequest
+//
+
+impl GetTransactionRequest {
+    pub fn new<T: Into<super::types::Digest>>(digest: T) -> Self {
+        Self {
+            digest: Some(digest.into()),
+            options: None,
+        }
+    }
+
+    pub fn with_options(mut self, options: GetTransactionOptions) -> Self {
+        self.options = Some(options);
+        self
     }
 }
 
