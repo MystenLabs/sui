92d7d628762d9f21276a3e47897dfec92cddfddb
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2024-12-19 16:41:55 -0800

    Try to fix faucet

--- crates/sui-faucet/src/server.rs
+++ crates/sui-faucet/src/server.rs
@@ -58,7 +58,7 @@ pub async fn start_faucet(
 
     let governor_cfg = Arc::new(
         GovernorConfigBuilder::default()
-            .const_period(Duration::from_secs(2))
+            .const_period(Duration::from_secs(10))
             .burst_size(max_request_per_second as u32)
             .key_extractor(GlobalKeyExtractor)
             .finish()
