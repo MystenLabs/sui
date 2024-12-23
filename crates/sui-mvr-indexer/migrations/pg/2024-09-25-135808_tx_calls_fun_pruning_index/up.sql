e09bd2bb4734ce01825583b509fb025db0920757
wlmyng <127570466+wlmyng@users.noreply.github.com>
2024-12-23 11:21:13 -0800

    remove sui-mvr-indexer crate as it is now unused (#20582)
    ## Description 
    
    remove sui-mvr-indexer crate now that we've rolled mvr into indexer
    through mvr-mode
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/sui-mvr-indexer/migrations/pg/2024-09-25-135808_tx_calls_fun_pruning_index/up.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS
-    tx_calls_fun_tx_sequence_number
-ON  tx_calls_fun (tx_sequence_number);
--- crates/sui-mvr-indexer/migrations/pg/2024-10-09-180628_add_network_total_transactions_to_epochs/up.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE epochs ADD COLUMN first_tx_sequence_number bigint;
