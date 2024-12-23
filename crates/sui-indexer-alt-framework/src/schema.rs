bb077c2e361ba72a7c041a95c50688bde77303d4
Will Yang <willyang@mystenlabs.com>
2024-12-23 13:58:57 -0500

    but if i allow diesel schema as is, then cargofmt would complain

--- crates/sui-indexer-alt-framework/src/schema.rs
+++ crates/sui-indexer-alt-framework/src/schema.rs
@@ -23,4 +23,7 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(cp_sequence_numbers, watermarks,);
+diesel::allow_tables_to_appear_in_same_query!(
+    cp_sequence_numbers,
+    watermarks,
+);
