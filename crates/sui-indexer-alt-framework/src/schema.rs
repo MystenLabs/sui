eb2090400faf346c5494c2456df4c1d610252cdb
Will Yang <willyang@mystenlabs.com>
2024-12-23 13:59:24 -0500

    cargo fmt

--- crates/sui-indexer-alt-framework/src/schema.rs
+++ crates/sui-indexer-alt-framework/src/schema.rs
@@ -23,7 +23,4 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(
-    cp_sequence_numbers,
-    watermarks,
-);
+diesel::allow_tables_to_appear_in_same_query!(cp_sequence_numbers, watermarks,);
