4b59afd90e5d9067f792d20d83a959489bff1dbf
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2024-12-23 16:01:55 -0800

    Increase default backpressure threshold (#20726)
    Should fix performance regression at high TPS

--- crates/sui-config/src/node.rs
+++ crates/sui-config/src/node.rs
@@ -391,7 +391,7 @@ impl ExecutionCacheConfig {
                 ExecutionCacheConfig::WritebackCache {
                     backpressure_threshold,
                     ..
-                } => backpressure_threshold.unwrap_or(10000),
+                } => backpressure_threshold.unwrap_or(100_000),
             })
     }
 
