b1dc2aa075f5db11649bfc432646f491338b6ceb
Adam Welc <adam@mystenlabs.com>
2024-12-26 18:55:27 +0100

    Updated Sui dependency to the global one

---
+++ external-crates/move/crates/move-analyzer/trace-adapter/out/utils.js
@@ -0,0 +1,12 @@
+"use strict";
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FRAME_LIFETIME = void 0;
+/**
+ * If end of lifetime for a local has this value,
+ * it means that it lives until the end of the current
+ * frame.
+ */
+exports.FRAME_LIFETIME = -1;
+//# sourceMappingURL=utils.js.map
