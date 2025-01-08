2519c827cc8785e05a561b08e02179c032a4afb7
mwtian <81660174+mwtian@users.noreply.github.com>
2025-01-07 17:57:53 -0800

    [consensus] logging and metrics improvements (#20807)
    ## Description 
    
    - Include peer hostname in round prober errors.
    - Fix block proposal interval metric.
    - Log missing blocks and peers fetched from in info.
    - Add `synchronizer_current_missing_blocks_by_authority` metric.
    
    ## Test plan 
    
    CI
    PT
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- consensus/core/src/core.rs
+++ consensus/core/src/core.rs
@@ -497,8 +497,25 @@ impl Core {
             .node_metrics
             .proposed_block_size
             .observe(serialized.len() as f64);
-        // Unnecessary to verify own blocks.
+        // Own blocks are assumed to be valid.
         let verified_block = VerifiedBlock::new_verified(signed_block, serialized);
+
+        // Record the interval from last proposal, before accepting the proposed block.
+        let last_proposed_block = self.last_proposed_block();
+        if last_proposed_block.round() > 0 {
+            self.context
+                .metrics
+                .node_metrics
+                .block_proposal_interval
+                .observe(
+                    Duration::from_millis(
+                        verified_block
+                            .timestamp_ms()
+                            .saturating_sub(last_proposed_block.timestamp_ms()),
+                    )
+                    .as_secs_f64(),
+                );
+        }
 
         // Accept the block into BlockManager and DagState.
         let (accepted_blocks, missing) = self
@@ -512,18 +529,6 @@ impl Core {
 
         // Ensure the new block and its ancestors are persisted, before broadcasting it.
         self.dag_state.write().flush();
-
-        let current_proposal_duration = Duration::from_millis(verified_block.timestamp_ms());
-        let previous_proposal_duration = Duration::from_millis(self.last_proposed_timestamp_ms());
-        self.context
-            .metrics
-            .node_metrics
-            .block_proposal_interval
-            .observe(
-                current_proposal_duration
-                    .saturating_sub(previous_proposal_duration)
-                    .as_secs_f64(),
-            );
 
         // Now acknowledge the transactions for their inclusion to block
         ack_transactions(verified_block.reference());
