46e1a3e8f5ade69d9fb0f2bc7f920e31dc5609f6
Michael Hayes <michael.hayes@mystenlabs.com>
2025-01-08 16:47:14 -0800

    remove sdks

--- sdk/typescript/src/keypairs/passkey/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-export { PasskeyKeypair, BrowserPasskeyProvider } from './keypair.js';
-export type { PasskeyProvider, BrowserPasswordProviderOptions } from './keypair.js';
-export { PasskeyPublicKey } from './publickey.js';
