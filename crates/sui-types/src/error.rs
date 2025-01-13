3d7a58be8ce70248bfe7e4d531580f4877b37db8
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2025-01-13 17:05:06 -0500

    feat: add base verify and parse attestation functions

--- crates/sui-types/src/error.rs
+++ crates/sui-types/src/error.rs
@@ -675,6 +675,9 @@ pub enum SuiError {
 
     #[error("The request did not contain a certificate")]
     NoCertificateProvidedError,
+
+    #[error("Enclave attestation failed: {0}")]
+    AttestationFailedToVerify(String),
 }
 
 #[repr(u64)]
