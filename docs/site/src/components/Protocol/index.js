be29f35d0267d4497fd4335bf0a703e5c3ddd588
Ronny Roland <ronmilton@mystenlabs.com>
2025-01-23 09:30:28 -0700

    test

--- docs/site/src/components/Protocol/index.js
+++ docs/site/src/components/Protocol/index.js
@@ -1,15 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 import React from "react";
-import { useDocusaurusContext } from "@docusaurus/core";
+
 
 export default function Protocol({ modules: { jsonData } }) {
   const jsonContent = require(jsonData);
-
-  return (
-    <div>
-      <h1>{jsonContent.title}</h1>
-      <p>{jsonContent.content}</p>
-    </div>
-  );
+  console.log(jsonContent);
+  return <></>;
 }
