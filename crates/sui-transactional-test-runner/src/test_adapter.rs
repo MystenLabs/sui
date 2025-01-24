7311dba41ba14af09779b39c4d7e8b98c707ca33
Jort <jordan.jennings@mystenlabs.com>
2025-01-24 13:54:12 -0800

    add files for expected tests

--- crates/sui-transactional-test-runner/src/test_adapter.rs
+++ crates/sui-transactional-test-runner/src/test_adapter.rs
@@ -1415,7 +1415,6 @@ impl SuiTestAdapter {
         gas_budget: Option<u64>,
         policy: u8,
         gas_price: u64,
-        // dry_run: bool,
     ) -> anyhow::Result<Option<String>> {
         let modules_bytes = modules
             .iter()
