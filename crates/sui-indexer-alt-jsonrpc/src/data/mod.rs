4988a868c118c4cbeb49741f3875f5d29a3ef7ca
Ashok Menon <ashok@mystenlabs.com>
2025-01-25 22:14:57 +0000

    rpc-alt: suix_getLatestSuiSystemState
    ## Description
    
    Implement `suix_getLatestSuiSystemState` from the governance API. This
    works by fetching the latest version of a particular object, using just
    the `obj_versions` table.
    
    This would not work for arbitrary objects, because `obj_versions` is not
    updated when an object is deleted or wrapped. This means that when
    asking for the latest version of an object that is currently deleted or
    wrapped, we will see the version prior to the deletion/wrap. This should
    be fine for the system state wrapper and its inner object because these
    cannot be deleted or wrapped.
    
    There are two more simplifications that have been undertaken here, by
    ignoring consistency and watermarking. Although we do need to implement
    both of these for GraphQL, we can get away with not doing that yet (for
    JSONRPC) because our current JSONRPC implementation does neither of
    these things, and so therefore this should not be a regression.
    
    ## Test plan
    
    New E2E tests show inspecting the system state after operations that
    would tweak it (staking, changing the epoch, withdrawing stake):
    
    ```
    sui$ cargo nextest run -p sui-indexer-alt-e2e-tests -- system_state
    ```

--- crates/sui-indexer-alt-jsonrpc/src/data/mod.rs
+++ crates/sui-indexer-alt-jsonrpc/src/data/mod.rs
@@ -1,6 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+pub(crate) mod object_versions;
 pub(crate) mod objects;
 pub(crate) mod package_resolver;
 pub(crate) mod reader;
--- crates/sui-indexer-alt-schema/src/objects.rs
+++ crates/sui-indexer-alt-schema/src/objects.rs
@@ -21,7 +21,7 @@ pub struct StoredObject {
     pub serialized_object: Option<Vec<u8>>,
 }
 
-#[derive(Insertable, Debug, Clone, FieldCount)]
+#[derive(Insertable, Selectable, Debug, Clone, FieldCount, Queryable)]
 #[diesel(table_name = obj_versions, primary_key(object_id, object_version))]
 pub struct StoredObjVersion {
     pub object_id: Vec<u8>,
