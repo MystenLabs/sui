1a797055352ea3d5740c31d2c69c1e9e2c8c75db
Ashok Menon <ashok@mystenlabs.com>
2025-01-25 22:14:57 +0000

    rpc-alt: suix_getLatestSuiSystemState
    ## Description
    
    Implement `suix_getLatestSuiSystemState` from the governance API. This
    works by fetching the latest version of a particular object, using just
    the `obj_versions` table.
    
    This would not work for arbitrary objects, because `obj_versions` is not
    updated when an object is deleted or wrapped. This means that when
    asking for the latest version of an object that is currently deleted or
    wrapped, we will see the version prior to the deletion/wrap. This should
    be fine for the system state wrapper and its inner object because these
    cannot be deleted or wrapped.
    
    There are two more simplifications that have been undertaken here, by
    ignoring consistency and watermarking. Although we do need to implement
    both of these for GraphQL, we can get away with not doing that yet (for
    JSONRPC) because our current JSONRPC implementation does neither of
    these things, and so therefore this should not be a regression.
    
    ## Test plan
    
    New E2E tests show inspecting the system state after operations that
    would tweak it (staking, changing the epoch, withdrawing stake):
    
    ```
    sui$ cargo nextest run -p sui-indexer-alt-e2e-tests -- system_state
    ```

--- crates/sui-indexer-alt-jsonrpc/src/api/governance.rs
+++ crates/sui-indexer-alt-jsonrpc/src/api/governance.rs
@@ -3,13 +3,32 @@
 
 use diesel::{ExpressionMethods, QueryDsl};
 
-use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use jsonrpsee::{
+    core::{DeserializeOwned, RpcResult},
+    proc_macros::rpc,
+};
 use sui_indexer_alt_schema::schema::kv_epoch_starts;
 use sui_open_rpc::Module;
 use sui_open_rpc_macros::open_rpc;
-use sui_types::sui_serde::BigInt;
+use sui_types::{
+    base_types::ObjectID,
+    dynamic_field::{derive_dynamic_field_id, Field},
+    object::Object,
+    sui_serde::BigInt,
+    sui_system_state::{
+        sui_system_state_inner_v1::SuiSystemStateInnerV1,
+        sui_system_state_inner_v2::SuiSystemStateInnerV2,
+        sui_system_state_summary::SuiSystemStateSummary, SuiSystemStateTrait,
+        SuiSystemStateWrapper,
+    },
+    TypeTag, SUI_SYSTEM_STATE_OBJECT_ID,
+};
 
-use crate::{context::Context, error::internal_error};
+use crate::{
+    context::Context,
+    data::{object_versions::LatestObjectKey, objects::ObjectVersionKey},
+    error::{internal_error, rpc_bail},
+};
 
 use super::rpc_module::RpcModule;
 
@@ -19,6 +38,10 @@ trait GovernanceApi {
     /// Return the reference gas price for the network as of the latest epoch.
     #[method(name = "getReferenceGasPrice")]
     async fn get_reference_gas_price(&self) -> RpcResult<BigInt<u64>>;
+
+    /// Return a summary of the latest version of the Sui System State object (0x5), on-chain.
+    #[method(name = "getLatestSuiSystemState")]
+    async fn get_latest_sui_system_state(&self) -> RpcResult<SuiSystemStateSummary>;
 }
 
 pub(crate) struct Governance(pub Context);
@@ -40,6 +63,82 @@ impl GovernanceApiServer for Governance {
             .map_err(internal_error)?;
 
         Ok((rgp as u64).into())
+    }
+
+    async fn get_latest_sui_system_state(&self) -> RpcResult<SuiSystemStateSummary> {
+        let Self(ctx) = self;
+
+        /// Fetch the latest version of the object at ID `object_id`, and deserialize its contents
+        /// as a Rust type `T`, assuming that it is a Move object (not a package).
+        ///
+        /// This function extracts the common parts of object loading for this API, but it does not
+        /// generalize beyond that, because:
+        ///
+        /// - It assumes that the objects being loaded are never deleted or wrapped (because it
+        ///   loads using `LatestObjectKey` directly without checking the live object set).
+        ///
+        /// - It first fetches one record from `obj_versions` and then fetches its contents. It is
+        ///   easy to misuse this API to fetch multiple objects in sequence, in a loop, rather than
+        ///   fetching them concurrently.
+        async fn fetch_latest<T: DeserializeOwned>(
+            ctx: &Context,
+            object_id: ObjectID,
+        ) -> Result<T, String> {
+            let id_display = object_id.to_canonical_display(/* with_prefix */ true);
+            let loader = ctx.loader();
+
+            let latest_version = loader
+                .load_one(LatestObjectKey(object_id))
+                .await
+                .map_err(|e| e.to_string())?
+                .ok_or_else(|| format!("Failed to load latest version for {id_display}"))?;
+
+            let stored = loader
+                .load_one(ObjectVersionKey(
+                    object_id,
+                    latest_version.object_version as u64,
+                ))
+                .await
+                .map_err(|e| e.to_string())?
+                .ok_or_else(|| format!("Failed to load object for {id_display}"))?
+                .serialized_object
+                .ok_or_else(|| format!("Failed to load contents for {id_display}"))?;
+
+            let object: Object = bcs::from_bytes(&stored).map_err(|e| e.to_string())?;
+
+            let move_object = object
+                .data
+                .try_as_move()
+                .ok_or_else(|| format!("{id_display} is not a Move object"))?;
+
+            bcs::from_bytes(move_object.contents())
+                .map_err(|e| format!("Failed to deserialize contents for {id_display}: {e}"))
+        }
+
+        let wrapper: SuiSystemStateWrapper = fetch_latest(ctx, SUI_SYSTEM_STATE_OBJECT_ID)
+            .await
+            .map_err(internal_error)?;
+
+        let inner_id = derive_dynamic_field_id(
+            SUI_SYSTEM_STATE_OBJECT_ID,
+            &TypeTag::U64,
+            &bcs::to_bytes(&wrapper.version).map_err(internal_error)?,
+        )
+        .map_err(internal_error)?;
+
+        Ok(match wrapper.version {
+            1 => fetch_latest::<Field<u64, SuiSystemStateInnerV1>>(ctx, inner_id)
+                .await
+                .map_err(internal_error)?
+                .value
+                .into_sui_system_state_summary(),
+            2 => fetch_latest::<Field<u64, SuiSystemStateInnerV2>>(ctx, inner_id)
+                .await
+                .map_err(internal_error)?
+                .value
+                .into_sui_system_state_summary(),
+            v => rpc_bail!(internal_error("Unexpected inner system state version: {v}")),
+        })
     }
 }
 
