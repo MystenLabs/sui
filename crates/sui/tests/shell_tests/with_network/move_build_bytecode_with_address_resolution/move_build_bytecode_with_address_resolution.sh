3f2d63b29acd219edb58c19d7f37397fa29a471e
Michael George <mike.george@mystenlabs.com>
2025-01-28 16:47:58 -0500

    removed some copy-pasta

--- crates/sui/tests/shell_tests/with_network/move_build_bytecode_with_address_resolution/move_build_bytecode_with_address_resolution.sh
+++ crates/sui/tests/shell_tests/with_network/move_build_bytecode_with_address_resolution/move_build_bytecode_with_address_resolution.sh
@@ -3,9 +3,6 @@
 
 # fixing issue https://github.com/MystenLabs/sui/issues/6546
 
-COIN=$(sui client --client.config $CONFIG objects   --json | jq '.[0].data.objectId')
-ADDR=$(sui client --client.config $CONFIG addresses --json | jq '.addresses[0][1]')
-
 sui client --client.config $CONFIG \
   publish simple \
   --json | jq '.effects.status'
