d8fbd44b5b889f58daed8aa0761d771c97606d81
Ashok Menon <ashok@mystenlabs.com>
2025-01-28 23:44:30 +0000

    rpc-alt: queryTransactionBlocks tx_calls filters
    ## Descriptioon
    
    Filter transactions based on the functions they call.
    
    ## Test plan
    
    New E2E tests:
    
    ```
    sui$ cargo nextest run -p sui-indexer-alt-e2e-tests -- transactions/query
    ```

--- crates/sui-indexer-alt-jsonrpc/src/api/transactions/filter.rs
+++ crates/sui-indexer-alt-jsonrpc/src/api/transactions/filter.rs
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Context as _;
-use diesel::{ExpressionMethods, QueryDsl};
-use sui_indexer_alt_schema::schema::tx_digests;
+use diesel::{ExpressionMethods, JoinOnDsl, QueryDsl};
+use sui_indexer_alt_schema::schema::{tx_calls, tx_digests};
 use sui_json_rpc_types::{Page as PageResponse, TransactionFilter};
-use sui_types::digests::TransactionDigest;
+use sui_types::{base_types::ObjectID, digests::TransactionDigest};
 
 use crate::{
     error::{invalid_params, rpc_bail, RpcError},
@@ -36,6 +36,12 @@ pub(super) async fn transactions(
     use TransactionFilter as F;
     let mut refs = match filter {
         None => all_transactions(ctx, &page).await?,
+
+        Some(F::MoveFunction {
+            package,
+            module,
+            function,
+        }) => tx_calls(ctx, &page, package, module.as_ref(), function.as_ref()).await?,
 
         Some(F::TransactionKind(_) | F::TransactionKindIn(_)) => {
             return unsupported("TransactionKind filter is not supported")
@@ -98,6 +104,71 @@ async fn all_transactions(
         .select((d::tx_sequence_number, d::tx_digest))
         .limit(page.limit + 1)
         .into_boxed();
+
+    if let Some(Cursor(tx)) = page.cursor {
+        if page.descending {
+            query = query.filter(d::tx_sequence_number.lt(tx as i64));
+        } else {
+            query = query.filter(d::tx_sequence_number.gt(tx as i64));
+        }
+    }
+
+    if page.descending {
+        query = query.order(d::tx_sequence_number.desc());
+    } else {
+        query = query.order(d::tx_sequence_number.asc());
+    }
+
+    let mut conn = ctx
+        .reader()
+        .connect()
+        .await
+        .context("Failed to connect to database")?;
+
+    let refs: Vec<(i64, Vec<u8>)> = conn
+        .results(query)
+        .await
+        .context("Failed to fetch matching transaction digests")?;
+
+    Ok(refs)
+}
+
+/// Fetch a page of transaction digests that called the described function(s). Functions can be
+/// selected by just their package, their module, or their fully-qualified name. It is an error to
+/// supply a package and function, but no module.
+///
+/// Fetches one more result than was requested to detect a next page.
+async fn tx_calls(
+    ctx: &Context,
+    page: &Page<u64>,
+    package: &ObjectID,
+    module: Option<&String>,
+    function: Option<&String>,
+) -> Result<Vec<(i64, Vec<u8>)>, RpcError<Error>> {
+    use tx_calls::dsl as c;
+    use tx_digests::dsl as d;
+
+    if let (None, Some(function)) = (module, function) {
+        return Err(invalid_params(Error::MissingModule {
+            function: function.clone(),
+        }));
+    }
+
+    let mut query = d::tx_digests
+        .inner_join(c::tx_calls.on(d::tx_sequence_number.eq(c::tx_sequence_number)))
+        .select((d::tx_sequence_number, d::tx_digest))
+        .limit(page.limit + 1)
+        .into_boxed();
+
+    query = query.filter(c::package.eq(package.as_slice()));
+
+    if let Some(module) = module {
+        query = query.filter(c::module.eq(module.as_str()));
+    }
+
+    if let Some(function) = function {
+        query = query.filter(c::function.eq(function.as_str()));
+    }
 
     if let Some(Cursor(tx)) = page.cursor {
         if page.descending {
