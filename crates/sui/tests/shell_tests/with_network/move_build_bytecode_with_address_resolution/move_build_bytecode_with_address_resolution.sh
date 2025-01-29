56b8b5f62fac0b41f255b665c7480d1a10779ff7
Michael George <mike.george@mystenlabs.com>
2025-01-29 15:22:36 -0500

    copy pasta

--- crates/sui/tests/shell_tests/with_network/move_build_bytecode_with_address_resolution/move_build_bytecode_with_address_resolution.sh
+++ crates/sui/tests/shell_tests/with_network/move_build_bytecode_with_address_resolution/move_build_bytecode_with_address_resolution.sh
@@ -1,8 +1,6 @@
 # Copyright (c) Mysten Labs, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-# fixing issue https://github.com/MystenLabs/sui/issues/6546
-
 sui client --client.config $CONFIG \
   publish simple \
   --json | jq '.effects.status'
