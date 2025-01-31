088df993fb14ae5105468a365e6a7ee19b088c51
Bridgerz <bridgerzoske@gmail.com>
2025-01-31 11:57:22 -0800

    [Bridge] Support wbtc vault balance

--- crates/sui-bridge/src/sui_bridge_watchdog/metrics.rs
+++ crates/sui-bridge/src/sui_bridge_watchdog/metrics.rs
@@ -10,6 +10,7 @@ use prometheus::{
 pub struct WatchdogMetrics {
     pub eth_vault_balance: IntGauge,
     pub usdt_vault_balance: IntGauge,
+    pub wbtc_vault_balance: IntGauge,
     pub total_supplies: IntGaugeVec,
     pub eth_bridge_paused: IntGauge,
     pub sui_bridge_paused: IntGauge,
@@ -27,6 +28,12 @@ impl WatchdogMetrics {
             usdt_vault_balance: register_int_gauge_with_registry!(
                 "bridge_usdt_vault_balance",
                 "Current balance of usdt eth vault",
+                registry,
+            )
+            .unwrap(),
+            wbtc_vault_balance: register_int_gauge_with_registry!(
+                "bridge_wbtc_vault_balance",
+                "Current balance of wbtc eth vault",
                 registry,
             )
             .unwrap(),
