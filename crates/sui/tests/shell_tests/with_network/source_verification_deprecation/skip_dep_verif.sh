cb470236a8f898d965ecb472cfe866c45dfa4557
Michael George <mike.george@mystenlabs.com>
2025-02-03 17:47:41 -0500

    added upgrade to tests; improved coverage

--- crates/sui/tests/shell_tests/with_network/source_verification_deprecation/skip_dep_verif.sh
+++ crates/sui/tests/shell_tests/with_network/source_verification_deprecation/skip_dep_verif.sh
@@ -1,24 +1,36 @@
 # Copyright (c) Mysten Labs, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-# test that publishing with `--skip-dependency-verification` succeeds cleanly, even if the dependencies don't match
+# check that --skip-dependency-verification has the right behavior on publish and upgrade
 
-echo munge various Move.toml files
+echo "=== munge Move.toml files ===" | tee /dev/stderr
 FRAMEWORK_DIR=$(echo $CARGO_MANIFEST_DIR | sed 's#/crates/sui#/crates/sui-framework/packages/sui-framework#g')
 for i in dependency/Move.toml example/Move.toml
 do
-  cat $i | sed "s#FRAMEWORK_DIR#$FRAMEWORK_DIR#g" > Move.toml
-  mv Move.toml $i
+  cat $i | sed "s#FRAMEWORK_DIR#$FRAMEWORK_DIR#g" > Move.toml \
+    && mv Move.toml $i
 done
 
-echo publish dependency
-sui client --client.config $CONFIG publish dependency --skip-dependency-verification \
+echo "=== publish dependency ===" | tee /dev/stderr
+sui client --client.config $CONFIG publish "dependency" --skip-dependency-verification \
   --json | jq '.effects.status'
 
-echo modify dependency
+echo "=== publish package v0 (should NOT warn) ===" | tee /dev/stderr
+UPGRADE_CAP=$(sui client --client.config $CONFIG publish "example" --skip-dependency-verification \
+  --json | jq -r '.objectChanges.[] | select(.objectType == "0x2::package::UpgradeCap") | .objectId')
+
+echo "=== upgrade package (should NOT warn) ===" | tee /dev/stderr
+sui client --client.config $CONFIG upgrade --upgrade-capability $UPGRADE_CAP example --skip-dependency-verification \
+  --json | jq '.effects.status'
+
+echo "=== modify dependency ===" | tee /dev/stderr
 cat dependency/sources/dependency.move | sed 's#0#1#g' > dependency.move
 mv dependency.move dependency/sources/dependency.move
 
-echo publish example
-sui client --client.config $CONFIG publish example --skip-dependency-verification \
+echo "=== try to publish with modified dep (should succeed) ===" | tee /dev/stderr
+UPGRADE_CAP=$(sui client --client.config $CONFIG publish "example" --skip-dependency-verification \
+  --json | jq -r '.objectChanges.[] | select(.objectType == "0x2::package::UpgradeCap") | .objectId')
+
+echo "=== try to upgrade with modified dep (should succeed) ===" | tee /dev/stderr
+sui client --client.config $CONFIG upgrade --upgrade-capability $UPGRADE_CAP example --skip-dependency-verification \
   --json | jq '.effects.status'
