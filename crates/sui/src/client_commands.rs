4f5a9748484d94f7e6d4001ae510a05315e1b1e3
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-02-03 21:36:03 -0800

    Remove unneeded code

--- crates/sui/src/client_commands.rs
+++ crates/sui/src/client_commands.rs
@@ -1063,7 +1063,6 @@ impl SuiClientCommands {
                     )?;
                 }
                 let compiled_package = compile_result?;
-                eprintln!("deps {:?}", compiled_package.dependency_ids);
                 let compiled_modules =
                     compiled_package.get_package_bytes(with_unpublished_dependencies);
                 let dependencies = compiled_package.dependency_ids;
