8f671d242f29e10a11237dc3535df65be097090c
Brandon Williams <brandon@mystenlabs.com>
2025-02-05 14:32:58 -0600

    rpc: remove rest checkpoint endpoints

--- crates/sui-e2e-tests/tests/rpc/checkpoints.rs
+++ crates/sui-e2e-tests/tests/rpc/checkpoints.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use sui_macros::sim_test;
-use sui_rpc_api::client::sdk::Client;
 use sui_rpc_api::client::Client as CoreClient;
 use sui_rpc_api::proto::node::v2::node_service_client::NodeServiceClient;
 use sui_rpc_api::proto::node::v2::{
@@ -15,23 +14,12 @@ use test_cluster::TestClusterBuilder;
 use crate::{stake_with_validator, transfer_coin};
 
 #[sim_test]
-async fn list_checkpoint() {
+async fn get_latest_checkpoint() {
     let test_cluster = TestClusterBuilder::new().build().await;
 
     let _transaction_digest = transfer_coin(&test_cluster.wallet).await;
 
-    let client = Client::new(test_cluster.rpc_url()).unwrap();
     let core_client = CoreClient::new(test_cluster.rpc_url()).unwrap();
-
-    let checkpoints = client
-        .list_checkpoints(&Default::default())
-        .await
-        .unwrap()
-        .into_inner();
-
-    assert!(!checkpoints.is_empty());
-
-    let _latest = client.get_latest_checkpoint().await.unwrap().into_inner();
 
     let _latest = core_client.get_latest_checkpoint().await.unwrap();
 }
@@ -41,14 +29,6 @@ async fn get_checkpoint() {
     let test_cluster = TestClusterBuilder::new().build().await;
 
     let _transaction_digest = transfer_coin(&test_cluster.wallet).await;
-
-    let client = Client::new(test_cluster.rpc_url()).unwrap();
-
-    let latest = client.get_latest_checkpoint().await.unwrap().into_inner();
-    let _ = client
-        .get_checkpoint(latest.checkpoint.sequence_number)
-        .await
-        .unwrap();
 
     let mut grpc_client = NodeServiceClient::connect(test_cluster.rpc_url().to_owned())
         .await
@@ -168,12 +148,15 @@ async fn get_full_checkpoint() {
 
     let transaction_digest = stake_with_validator(&test_cluster).await;
 
-    let client = Client::new(test_cluster.rpc_url()).unwrap();
     let core_client = CoreClient::new(test_cluster.rpc_url()).unwrap();
 
-    let latest = client.get_latest_checkpoint().await.unwrap().into_inner();
+    let latest = core_client
+        .get_latest_checkpoint()
+        .await
+        .unwrap()
+        .into_data();
     let _ = core_client
-        .get_full_checkpoint(latest.checkpoint.sequence_number)
+        .get_full_checkpoint(latest.sequence_number)
         .await
         .unwrap();
 
