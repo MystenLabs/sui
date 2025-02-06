b5a2d134ce4ae5ddb89650295bb63cccc148f94e
Ashok Menon <ashok@mystenlabs.com>
2025-02-06 19:33:31 +0000

    rpc-alt: getNormalizedMoveFunction
    ## Description
    Fetch a function's signature. Note that this implementation uses the
    package resolver, so unlike the current JSON-RPC, this will correctly
    relocate package IDs for types that are introduced in upgraded packages.
    
    ## Test plan
    New E2E tests:
    
    ```
    sui$ cargo nextest run -p sui-indexer-alt-e2e-tests \
      -- move/functions
    ```

--- crates/sui-indexer-alt-jsonrpc/src/lib.rs
+++ crates/sui-indexer-alt-jsonrpc/src/lib.rs
@@ -7,6 +7,7 @@ use std::sync::Arc;
 use anyhow::Context as _;
 use api::checkpoints::Checkpoints;
 use api::dynamic_fields::DynamicFields;
+use api::move_utils::MoveUtils;
 use api::objects::{Objects, ObjectsConfig, QueryObjects};
 use api::rpc_module::RpcModule;
 use api::transactions::{QueryTransactions, Transactions, TransactionsConfig};
@@ -228,6 +229,7 @@ pub async fn start_rpc(
     rpc.add_module(Checkpoints(context.clone()))?;
     rpc.add_module(DynamicFields(context.clone()))?;
     rpc.add_module(Governance(context.clone()))?;
+    rpc.add_module(MoveUtils(context.clone()))?;
     rpc.add_module(Objects(context.clone(), objects_config.clone()))?;
     rpc.add_module(QueryObjects(context.clone(), objects_config))?;
     rpc.add_module(QueryTransactions(context.clone(), transactions_config))?;
