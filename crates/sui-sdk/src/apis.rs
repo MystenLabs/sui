7086e667cdd849d3410c7cf171399d8579efbcef
Brandon Williams <brandon@mystenlabs.com>
2025-02-07 16:50:16 -0600

    m

--- crates/sui-sdk/src/apis.rs
+++ crates/sui-sdk/src/apis.rs
@@ -824,7 +824,7 @@ impl CoinReadApi {
             ),
             move |(mut data, cursor, has_next_page, coin_type)| async move {
                 if let Some(item) = data.pop() {
-                    Some((item, (data, cursor, /* has_next_page */ true, coin_type)))
+                    Some((item, (data, cursor, has_next_page, coin_type)))
                 } else if has_next_page {
                     let page = self
                         .get_coins(owner, coin_type.clone(), cursor, Some(100))
