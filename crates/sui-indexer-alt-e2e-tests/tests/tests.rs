d9511e358743d8fb1a9d0173a97cd2929b97f1bb
Ashok Menon <ashok@mystenlabs.com>
2025-02-08 16:56:59 +0000

    refactor(rpc-alt): Extract OffchainCluster
    ## Description
    
    Pull out `OffChainCluster` from the transactional test runner set-up
    code, so that it can be used for non-transactional tests.
    
    ## Test plan
    
    Run existing tests:
    
    ```
    sui$ cargo nextest run -p sui-indexer-alt-e2e-tests
    ```

--- crates/sui-indexer-alt-e2e-tests/tests/tests.rs
+++ crates/sui-indexer-alt-e2e-tests/tests/tests.rs
@@ -3,7 +3,6 @@
 
 use std::{
     error::Error,
-    net::{IpAddr, Ipv4Addr, SocketAddr},
     path::Path,
     sync::{
         atomic::{AtomicUsize, Ordering},
@@ -18,32 +17,21 @@ use diesel_async::RunQueryDsl;
 use prometheus::Registry;
 use reqwest::Client;
 use serde_json::{json, Value};
-use sui_indexer_alt::{config::IndexerConfig, start_indexer};
+use sui_indexer_alt::config::IndexerConfig;
+use sui_indexer_alt_e2e_tests::OffchainCluster;
 use sui_indexer_alt_framework::{ingestion::ClientArgs, schema::watermarks, IndexerArgs};
 use sui_indexer_alt_jsonrpc::{
-    config::RpcConfig, data::system_package_task::SystemPackageTaskArgs, start_rpc, RpcArgs,
+    config::RpcConfig, data::system_package_task::SystemPackageTaskArgs,
 };
-use sui_pg_db::{
-    temp::{get_available_port, TempDb},
-    Db, DbArgs,
-};
+use sui_pg_db::{Db, DbArgs};
 use sui_transactional_test_runner::{
     create_adapter,
     offchain_state::{OffchainStateReader, TestResponse},
     run_tasks_with_adapter,
     test_adapter::{OffChainConfig, SuiTestAdapter, PRE_COMPILED},
 };
-use tokio::task::JoinHandle;
 use tokio_util::sync::CancellationToken;
 use url::Url;
-
-struct OffchainCluster {
-    rpc_listen_address: SocketAddr,
-    indexer: JoinHandle<()>,
-    jsonrpc: JoinHandle<()>,
-    database: TempDb,
-    cancel: CancellationToken,
-}
 
 struct OffchainReader {
     db: Db,
@@ -53,119 +41,6 @@ struct OffchainReader {
 }
 
 datatest_stable::harness!(run_test, "tests", r".*\.move$");
-
-impl OffchainCluster {
-    /// Create a new off-chain cluster consisting of a temporary database, Indexer, and JSONRPC
-    /// service, to serve requests from E2E tests.
-    ///
-    /// NOTE: this cluster does not honour the following fields in `OffChainConfig`, because they
-    /// do not map to to how its components are implemented:
-    ///
-    /// - `snapshot_config.sleep_duration` -- there are multiple consistent pipelines, and each
-    ///   controls the interval at which it runs.
-    /// - `retention_config`, as retention is not measured in epochs for this pipeline (it is
-    ///    measured in checkpoints).
-    async fn new(config: &OffChainConfig) -> Self {
-        let cancel = CancellationToken::new();
-
-        let rpc_port = get_available_port();
-        let rpc_listen_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), rpc_port);
-
-        // We don't expose metrics in these tests, but we create a registry to collect them anyway.
-        let registry = Registry::new();
-
-        let database = TempDb::new().expect("Failed to create temporary database");
-
-        let db_args = DbArgs {
-            database_url: database.database().url().clone(),
-            ..Default::default()
-        };
-
-        let client_args = ClientArgs {
-            local_ingestion_path: Some(config.data_ingestion_path.clone()),
-            remote_store_url: None,
-        };
-
-        // The example config includes every pipeline, and we configure its consistent range using
-        // the off-chain config that was passed in.
-        let mut indexer_config = IndexerConfig::example();
-        indexer_config.consistency.retention = Some(config.snapshot_config.snapshot_min_lag as u64);
-
-        let rpc_args = RpcArgs {
-            rpc_listen_address,
-            ..Default::default()
-        };
-
-        let rpc_config = RpcConfig::example();
-
-        // This configuration controls how often the RPC service checks for changes to system
-        // packages. The default polling interval is probably too slow for changes to get picked
-        // up, so tests that rely on this behaviour will always fail, but this is better than flaky
-        // behavior.
-        let system_package_task_args = SystemPackageTaskArgs::default();
-
-        let with_genesis = true;
-        let indexer = start_indexer(
-            db_args.clone(),
-            IndexerArgs::default(),
-            client_args,
-            indexer_config,
-            with_genesis,
-            &registry,
-            cancel.child_token(),
-        )
-        .await
-        .expect("Failed to start indexer");
-
-        let jsonrpc = start_rpc(
-            db_args,
-            rpc_args,
-            system_package_task_args,
-            rpc_config,
-            &registry,
-            cancel.child_token(),
-        )
-        .await
-        .expect("Failed to start JSON-RPC server");
-
-        Self {
-            rpc_listen_address,
-            indexer,
-            jsonrpc,
-            database,
-            cancel,
-        }
-    }
-
-    /// An implementation of the API that the test cluster uses to send reads to the off-chain
-    /// set-up.
-    async fn reader(&self) -> Box<dyn OffchainStateReader> {
-        let db = Db::for_read(DbArgs {
-            database_url: self.database.database().url().clone(),
-            ..Default::default()
-        })
-        .await
-        .expect("Failed to connect to database");
-
-        let rpc_url = Url::parse(&format!("http://{}/", self.rpc_listen_address))
-            .expect("Failed to parse RPC URL");
-
-        Box::new(OffchainReader {
-            db,
-            rpc_url,
-            client: Client::new(),
-            queries: AtomicUsize::new(0),
-        })
-    }
-
-    /// Triggers cancellation of all downstream services, waits for them to stop and cleans up the
-    /// temporary database.
-    async fn stopped(self) {
-        self.cancel.cancel();
-        let _ = self.indexer.await;
-        let _ = self.jsonrpc.await;
-    }
-}
 
 impl OffchainReader {
     /// Wait indefinitely until all pipelines have caught up with `checkpoint`.
@@ -255,6 +130,58 @@ impl OffchainStateReader for OffchainReader {
     }
 }
 
+async fn cluster(config: &OffChainConfig) -> OffchainCluster {
+    let cancel = CancellationToken::new();
+    let registry = Registry::new();
+
+    let indexer_args = IndexerArgs::default();
+
+    let client_args = ClientArgs {
+        local_ingestion_path: Some(config.data_ingestion_path.clone()),
+        remote_store_url: None,
+    };
+
+    // This configuration controls how often the RPC service checks for changes to system packages.
+    // The default polling interval is probably too slow for changes to get picked up, so tests
+    // that rely on this behaviour will always fail, but this is better than flaky behavior.
+    let system_package_task_args = SystemPackageTaskArgs::default();
+
+    // The example config includes every pipeline, and we configure its consistent range using the
+    // off-chain config that was passed in.
+    let mut indexer_config = IndexerConfig::example();
+    indexer_config.consistency.retention = Some(config.snapshot_config.snapshot_min_lag as u64);
+
+    let rpc_config = RpcConfig::example();
+
+    OffchainCluster::new(
+        indexer_args,
+        client_args,
+        system_package_task_args,
+        indexer_config,
+        rpc_config,
+        &registry,
+        cancel,
+    )
+    .await
+    .expect("Failed to create off-chain cluster")
+}
+
+async fn reader(cluster: &OffchainCluster) -> Box<dyn OffchainStateReader> {
+    let db = Db::for_read(DbArgs {
+        database_url: cluster.db_url(),
+        ..Default::default()
+    })
+    .await
+    .expect("Failed to connect to database");
+
+    Box::new(OffchainReader {
+        db,
+        rpc_url: cluster.rpc_url(),
+        client: Client::new(),
+        queries: AtomicUsize::new(0),
+    })
+}
+
 #[cfg_attr(not(msim), tokio::main)]
 #[cfg_attr(msim, msim::main)]
 async fn run_test(path: &Path) -> Result<(), Box<dyn Error>> {
@@ -269,13 +196,13 @@ async fn run_test(path: &Path) -> Result<(), Box<dyn Error>> {
         create_adapter::<SuiTestAdapter>(path, Some(Arc::new(PRE_COMPILED.clone()))).await?;
 
     // configure access to the off-chain reader
-    let cluster = OffchainCluster::new(adapter.offchain_config.as_ref().unwrap()).await;
-    adapter.with_offchain_reader(cluster.reader().await);
+    let c = cluster(adapter.offchain_config.as_ref().unwrap()).await;
+    adapter.with_offchain_reader(reader(&c).await);
 
     // run the tasks in the test
     run_tasks_with_adapter(path, adapter, output).await?;
 
     // clean-up the off-chain cluster
-    cluster.stopped().await;
+    c.stopped().await;
     Ok(())
 }
