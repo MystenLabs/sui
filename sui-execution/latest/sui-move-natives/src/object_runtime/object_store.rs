2f16b4b2da7c695902f8dead850b6a8bc74f6bff
Todd Nowacki <tmn@mystenlabs.com>
2025-02-11 15:51:54 -0800

    Cleanup protocol config checks

--- sui-execution/latest/sui-move-natives/src/object_runtime/object_store.rs
+++ sui-execution/latest/sui-move-natives/src/object_runtime/object_store.rs
@@ -595,20 +595,7 @@ impl<'a> ChildObjectStore<'a> {
                         ),
                 );
             }
-            if self.inner.protocol_config.loaded_child_object_format() {
-                // double check format did not change
-                if !self.inner.protocol_config.loaded_child_object_format_type() && child_ty != &ty
-                {
-                    let msg = format!("Type changed for child {child} when setting the value back");
-                    return Err(
-                        PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-                            .with_message(msg),
-                    );
-                }
-                value
-            } else {
-                GlobalValue::none()
-            }
+            value
         } else {
             GlobalValue::none()
         };
