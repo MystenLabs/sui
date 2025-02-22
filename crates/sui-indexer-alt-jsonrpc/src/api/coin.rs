68c228835a121b00b8b4db5a80bd71dfdaa4081d
Emma Zhong <emma@mystenlabs.com>
2025-02-21 17:06:59 -0800

    [rpc-alt] add getAllBalances (#21094)
    ## Description 
    
    Add initial implementation for `getAllBalances`, reusing most of the
    infra already built for other APIs. Note that this method does not
    support pagination and we may have to add optimizations later to improve
    performance when fetching whale's balances, based on benchmarking
    results.
    
    ## Test plan 
    
    Added e2e tests.
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/sui-indexer-alt-jsonrpc/src/api/coin.rs
+++ crates/sui-indexer-alt-jsonrpc/src/api/coin.rs
@@ -1,8 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::collections::{BTreeMap, HashMap};
+
 use anyhow::Context as _;
 use futures::future;
+use sui_types::object::Object;
 
 use super::rpc_module::RpcModule;
 use crate::context::Context;
@@ -17,7 +20,7 @@ use move_core_types::language_storage::TypeTag;
 use serde::{Deserialize, Serialize};
 use sui_indexer_alt_schema::objects::StoredCoinOwnerKind;
 use sui_indexer_alt_schema::schema::coin_balance_buckets;
-use sui_json_rpc_types::{Coin, Page as PageResponse};
+use sui_json_rpc_types::{Balance, Coin, Page as PageResponse};
 use sui_open_rpc::Module;
 use sui_open_rpc_macros::open_rpc;
 use sui_types::{
@@ -42,6 +45,14 @@ trait CoinsApi {
         /// maximum number of items per page
         limit: Option<usize>,
     ) -> RpcResult<PageResponse<Coin, String>>;
+
+    /// Return the total coin balance for all coin types, owned by the address owner.
+    #[method(name = "getAllBalances")]
+    async fn get_all_balances(
+        &self,
+        /// the owner's Sui address
+        owner: SuiAddress,
+    ) -> RpcResult<Vec<Balance>>;
 }
 
 pub(crate) struct Coins(pub Context, pub CoinsConfig);
@@ -102,7 +113,7 @@ impl CoinsApiServer for Coins {
         )?;
 
         // We get all the qualified coin ids first.
-        let coin_id_page = filter_coins(ctx, owner, coin_type_tag, page).await?;
+        let coin_id_page = filter_coins(ctx, owner, Some(coin_type_tag), Some(page)).await?;
 
         let coin_futures = coin_id_page.data.iter().map(|id| coin_response(ctx, *id));
 
@@ -118,6 +129,43 @@ impl CoinsApiServer for Coins {
             next_cursor: coin_id_page.next_cursor,
             has_next_page: coin_id_page.has_next_page,
         })
+    }
+
+    async fn get_all_balances(&self, owner: SuiAddress) -> RpcResult<Vec<Balance>> {
+        let Self(ctx, _) = self;
+        let coin_ids = filter_coins(ctx, owner, None, None).await?;
+        let coin_futures = coin_ids
+            .data
+            .iter()
+            .map(|id| object_with_coin_data(ctx, *id));
+        let coins = future::join_all(coin_futures)
+            .await
+            .into_iter()
+            .zip(coin_ids.data)
+            .map(|(r, c)| {
+                let id = c;
+                r.with_internal_context(|| format!("Failed to get object {id}"))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        // Using a BTreeMap so that the ordering of keys is deterministic.
+        let mut balance_map: BTreeMap<String, (usize, u128)> = BTreeMap::new();
+        for (_, coin_type, balance) in coins {
+            let entry = balance_map.entry(coin_type).or_insert((0, 0));
+            entry.0 += 1;
+            entry.1 += balance as u128;
+        }
+        let balances: Vec<Balance> = balance_map
+            .into_iter()
+            .map(|(coin_type, (coin_object_count, total_balance))| Balance {
+                coin_type,
+                coin_object_count,
+                total_balance,
+                // LockedCoin is deprecated
+                locked_balance: HashMap::new(),
+            })
+            .collect();
+        Ok(balances)
     }
 }
 
@@ -143,8 +191,8 @@ impl Default for CoinsConfig {
 async fn filter_coins(
     ctx: &Context,
     owner: SuiAddress,
-    coin_type_tag: TypeTag,
-    page: Page<Cursor>,
+    coin_type_tag: Option<TypeTag>,
+    page: Option<Page<Cursor>>,
 ) -> Result<PageResponse<ObjectID, String>, RpcError<Error>> {
     use coin_balance_buckets::dsl as cb;
 
@@ -153,11 +201,6 @@ async fn filter_coins(
         .connect()
         .await
         .context("Failed to connect to database")?;
-
-    let limit = page.limit;
-
-    let serialized_coin_type =
-        bcs::to_bytes(&coin_type_tag).context("Failed to serialize coin type tag")?;
 
     // We use two aliases of coin_balance_buckets to make the query more readable.
     let (candidates, newer) = diesel::alias!(
@@ -193,11 +236,18 @@ async fn filter_coins(
         .filter(newer!(object_id).is_null())
         .filter(candidates!(owner_kind).eq(StoredCoinOwnerKind::Fastpath))
         .filter(candidates!(owner_id).eq(owner.to_vec()))
-        .filter(candidates!(coin_type).eq(serialized_coin_type))
         .into_boxed();
 
+    if let Some(coin_type_tag) = coin_type_tag {
+        let serialized_coin_type =
+            bcs::to_bytes(&coin_type_tag).context("Failed to serialize coin type tag")?;
+        query = query.filter(candidates!(coin_type).eq(serialized_coin_type));
+    }
+
+    let (cursor, limit) = page.map_or((None, None), |p| (p.cursor, Some(p.limit)));
+
     // If the cursor is specified, we filter by it.
-    if let Some(c) = page.cursor {
+    if let Some(c) = cursor {
         query = query.filter(
             sql::<Bool>(r"(candidates.coin_balance_bucket, candidates.cp_sequence_number, candidates.object_id) < (")
                 .bind::<SmallInt, _>(c.coin_balance_bucket as i16)
@@ -213,16 +263,23 @@ async fn filter_coins(
     query = query
         .order_by(candidates!(coin_balance_bucket).desc())
         .then_order_by(candidates!(cp_sequence_number).desc())
-        .then_order_by(candidates!(object_id).desc())
-        .limit(limit + 1);
+        .then_order_by(candidates!(object_id).desc());
+
+    if let Some(limit) = limit {
+        query = query.limit(limit + 1);
+    }
 
     let mut buckets: Vec<(Vec<u8>, i64, i16)> =
         conn.results(query).await.context("Failed to query coins")?;
 
-    // Now gather pagination info.
-    let has_next_page = buckets.len() > limit as usize;
-    if has_next_page {
-        buckets.truncate(limit as usize);
+    let mut has_next_page = false;
+
+    if let Some(limit) = limit {
+        // Now gather pagination info.
+        has_next_page = buckets.len() > limit as usize;
+        if has_next_page {
+            buckets.truncate(limit as usize);
+        }
     }
 
     let next_cursor = buckets
@@ -252,14 +309,31 @@ async fn filter_coins(
 }
 
 async fn coin_response(ctx: &Context, id: ObjectID) -> Result<Coin, RpcError<Error>> {
-    let object = load_latest(ctx, id)
-        .await?
-        .ok_or_else(|| anyhow::anyhow!("Failed to load latest object {}", id))?;
+    let (object, coin_type, balance) = object_with_coin_data(ctx, id).await?;
 
     let coin_object_id = object.id();
     let digest = object.digest();
     let version = object.version();
     let previous_transaction = object.as_inner().previous_transaction;
+
+    Ok(Coin {
+        coin_type,
+        coin_object_id,
+        version,
+        digest,
+        balance,
+        previous_transaction,
+    })
+}
+
+async fn object_with_coin_data(
+    ctx: &Context,
+    id: ObjectID,
+) -> Result<(Object, String, u64), RpcError<Error>> {
+    let object = load_latest(ctx, id)
+        .await?
+        .ok_or_else(|| anyhow::anyhow!("Failed to load latest object {}", id))?;
+
     let coin = object
         .as_coin_maybe()
         .context("Object is expected to be a coin")?;
@@ -267,12 +341,5 @@ async fn coin_response(ctx: &Context, id: ObjectID) -> Result<Coin, RpcError<Err
         .coin_type_maybe()
         .context("Object is expected to have a coin type")?
         .to_canonical_string(/* with_prefix */ true);
-    Ok(Coin {
-        coin_type,
-        coin_object_id,
-        version,
-        digest,
-        balance: coin.balance.value(),
-        previous_transaction,
-    })
+    Ok((object, coin_type, coin.balance.value()))
 }
