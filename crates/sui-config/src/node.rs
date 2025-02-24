b129eeb050bf8fa4bd39a15338f152fc2ba62937
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2025-02-24 11:02:28 -0500

    chore: remove test tenant (#21335)

--- crates/sui-config/src/node.rs
+++ crates/sui-config/src/node.rs
@@ -476,7 +476,6 @@ pub fn default_zklogin_oauth_providers() -> BTreeMap<Chain, BTreeSet<String>> {
         "Threedos".to_string(),
         "Onefc".to_string(),
         "FanTV".to_string(),
-        "AwsTenant-region:us-east-1-tenant_id:us-east-1_LPSLCkC3A".to_string(), // test tenant in mysten aws
         "AwsTenant-region:us-east-1-tenant_id:us-east-1_qPsZxYqd8".to_string(), // Ambrus, external partner
         "Arden".to_string(),                                                    // Arden partner
         "AwsTenant-region:eu-west-3-tenant_id:eu-west-3_gGVCx53Es".to_string(), // Trace, external partner
