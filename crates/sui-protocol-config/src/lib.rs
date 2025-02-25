deb0b8e7464b9067740239c8fe5a6c16c4895842
Anastasios Kichidis <tasos@mystenlabs.com>
2025-02-25 12:31:15 +0000

    [chore] enable the median based timestamp.

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -3362,6 +3362,7 @@ impl ProtocolConfig {
                     if chain != Chain::Mainnet {
                         cfg.feature_flags.passkey_auth = true;
                     }
+                    cfg.feature_flags.consensus_median_based_timestamp = true;
                 }
                 76 => {
                     if chain != Chain::Mainnet && chain != Chain::Testnet {
