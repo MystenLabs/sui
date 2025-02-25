68f214d577aef083c62dad9553ab3d550f573161
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2025-02-24 16:07:27 -0800

    Make ExecutionTiming Copy + Clone (#21340)

--- crates/sui-types/src/execution.rs
+++ crates/sui-types/src/execution.rs
@@ -234,7 +234,7 @@ impl ExecutionTimeObservationKey {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 pub enum ExecutionTiming {
     Success(Duration),
     Abort(Duration),
