8d2d3b0447221d9d0afc121b5ccdb5e9f693f6db
Damir Shamanaev <damirka.ru@gmail.com>
2025-03-05 17:18:25 +0300

    more fixes

--- external-crates/move/tooling/prettier-move/src/cst/expression/if_expression.ts
+++ external-crates/move/tooling/prettier-move/src/cst/expression/if_expression.ts
@@ -4,7 +4,7 @@
 import { Node } from '../..';
 import { MoveOptions, printFn, treeFn } from '../../printer';
 import { AstPath, Doc, doc } from 'prettier';
-const { group, softline, ifBreak, indentIfBreak, line, indent } = doc.builders;
+const { group, softline, line, ifBreak, indent, hardlineWithoutBreakParent } = doc.builders;
 
 /** The type of the node implemented in this file */
 export const NODE_TYPE = 'if_expression';
@@ -34,26 +34,29 @@ export default function (path: AstPath<Node>): treeFn | null {
  * }
  *
  * // multi line + single line
- * if (cond)
+ * if (cond) {
  * 	return long_expr;
+ * }
  *
  * // multi line + single line + long expr
  * if (
  * 	long_cond ||
  *  long_cond
- * )
+ * ) {
  * 	return long_expr &&
  * 		long_expr;
+ * }
  * ```
  */
 function printIfExpression(path: AstPath<Node>, options: MoveOptions, print: printFn): Doc {
-	const groupId = Symbol('if_expression');
 	const hasElse = path.node.children.some((e) => e.type == 'else');
-
 	const condition = path.node.nonFormattingChildren[0];
 	const trueBranch = path.node.nonFormattingChildren[1];
-	const result: Doc[] = [
-		// condition group
+	const groupId = Symbol('if_expression_true');
+	const result: Doc[] = [];
+
+	// condition group
+	result.push(
 		group([
 			'if (',
 			condition?.isList
@@ -65,40 +68,56 @@ function printIfExpression(path: AstPath<Node>, options: MoveOptions, print: pri
 					],
 			')',
 		]),
-		// body group
-		group(
-			[
-				trueBranch?.isList
-					? [' ', path.call(print, 'nonFormattingChildren', 1)]
-					: [indent(line), path.call(print, 'nonFormattingChildren', 1)],
-			],
-			{ id: groupId },
-		),
-	];
+	);
+
+	// true branch group
+	if (trueBranch?.isList) {
+		const shouldBreak =
+			trueBranch.leadingComment.some((e) => e.type == 'line_comment') ||
+			trueBranch.trailingComment?.type == 'line_comment';
+
+		result.push([' ', path.call(print, 'nonFormattingChildren', 1)]);
+		hasElse && result.push(shouldBreak ? [line, 'else'] : ' else');
+	} else {
+		result.push(
+			group(
+				[
+					ifBreak([' {', indent(hardlineWithoutBreakParent)], ' '),
+					indent(path.call(print, 'nonFormattingChildren', 1)),
+					ifBreak([hardlineWithoutBreakParent, '}'], ''),
+				],
+				{ id: groupId },
+			),
+		);
+
+		// link group breaking to the true branch, add `else` either with a
+		// newline or without - depends on whether true branch is converted into
+		// a block
+		hasElse && result.push([ifBreak([' else'], [line, 'else'], { groupId })]);
+	}
 
 	// else block
 	if (hasElse) {
-		const isTrueBlock = trueBranch?.type === 'block';
 		const elseNode = path.node.nonFormattingChildren[2]!;
 		const printed = path.call(print, 'nonFormattingChildren', 2);
 		const shouldBreak =
 			elseNode.leadingComment.some((e) => e.type == 'line_comment') ||
 			elseNode.trailingComment?.type == 'line_comment';
 
-		if (elseNode.isList || elseNode.isControlFlow) {
-			result.push(group([
-				isTrueBlock ? ' ' : line,
-				'else ',
-				printed,
-			]))
+		// special casing chained `if_expression` with the expectation that the
+		// expression can handle breaking / nesting itself.
+		if (elseNode.isList || elseNode.type == 'if_expression') {
+			result.push([' ', printed]);
 		} else {
 			result.push(
-				isTrueBlock ? ' ' : line,
-				'else',
-				group([
-					indent(line),
-					indent(path.call(print, 'nonFormattingChildren', 2)),
-				], { shouldBreak }),
+				group(
+					[
+						ifBreak([' {', indent(hardlineWithoutBreakParent)], ' '),
+						indent(path.call(print, 'nonFormattingChildren', 2)),
+						ifBreak([hardlineWithoutBreakParent, '}'], ''),
+					],
+					{ shouldBreak },
+				),
 			);
 		}
 	}
