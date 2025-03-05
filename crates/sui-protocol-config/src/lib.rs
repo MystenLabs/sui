f0e20a8db5ae002c968e51ba2e8937dc9406aadf
Anastasios Kichidis <tasos@mystenlabs.com>
2025-03-05 13:04:08 +0000

    [fix] renamed flag.

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -3312,7 +3312,7 @@ impl ProtocolConfig {
                     if chain != Chain::Mainnet {
                         cfg.feature_flags.passkey_auth = true;
                     }
-                    cfg.feature_flags.consensus_median_based_timestamp = true;
+                    cfg.feature_flags.consensus_median_based_commit_timestamp = true;
                 }
                 76 => {
                     if chain != Chain::Mainnet && chain != Chain::Testnet {
