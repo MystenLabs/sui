855b69d2d265f22c657f2c57452ffe738bce8856
Ashok Menon <ashok@mystenlabs.com>
2025-03-16 21:07:49 +0000

    graphql-alt: metrics service
    ## Description
    
    Add a metrics service and track basic metrics about requests
    (latencies and counts).
    
    ## Test plan
    
    Run the service, try out the following queries:
    
    ```
    query { hello }
    query { boom }
    ```
    
    ...and visit `http://localhost:9184/metrics.

--- crates/sui-indexer-alt-graphql/src/api/query.rs
+++ crates/sui-indexer-alt-graphql/src/api/query.rs
@@ -1,14 +1,19 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use async_graphql::Object;
+use async_graphql::{Error, Object, Result};
 
 pub struct Query;
 
 #[Object]
 impl Query {
-    /// Test query
+    /// Test query that always succeeds
     async fn hello(&self) -> String {
         "Hello, GraphQL!".to_string()
+    }
+
+    /// Test query that always fails
+    async fn boom(&self) -> Result<String> {
+        Err(Error::new("Boom!"))
     }
 }
