3a41d51936600e5b549db5ab9c31925d003c66eb
Ashok Menon <ashok@mystenlabs.com>
2025-03-19 13:23:17 +0000

    graphql-alt: track parsing and validation errors
    ## Description
    
    Annotate parsing and validation errors with a code, just like errors we
    get during execution.
    
    This change also moves the extent of time being measured to account for
    the full request (not just execution).
    
    ## Test plan
    
    Try the following queries, which introduce various different kinds of
    error:
    
    ```graphql
    # User error
    query { boom }
    
    # Internal error
    query { uhOh }
    
    # Validation error (variable not used)
    query($foo: String!) { uhOh }
    
    # Parsing error (no close paren)
    query { uhOh
    ```
    
    And note that the response is correctly annotated with a code, and the
    metrics correctly track these new codes as well.

--- crates/sui-indexer-alt-graphql/src/extensions/logging.rs
+++ crates/sui-indexer-alt-graphql/src/extensions/logging.rs
@@ -8,26 +8,31 @@ use std::{
 
 use async_graphql::{
     extensions::{
-        Extension, ExtensionContext, ExtensionFactory, NextExecute, NextParseQuery, NextResolve,
-        ResolveInfo,
+        Extension, ExtensionContext, ExtensionFactory, NextParseQuery, NextPrepareRequest,
+        NextRequest, NextResolve, NextValidation, ResolveInfo,
     },
     parser::types::ExecutableDocument,
-    Response, ServerResult, Value, Variables,
+    Request, Response, ServerError, ServerResult, ValidationResult, Value, Variables,
 };
 use serde_json::json;
 use tracing::{debug, info, warn};
 use uuid::Uuid;
 
-use crate::{error::code, metrics::RpcMetrics};
+use crate::{
+    error::{code, fill_error_code},
+    metrics::RpcMetrics,
+};
 
 /// Context data that tracks the session UUID and the client's address, to associate logs with a
 /// particular request.
+#[derive(Copy, Clone)]
 pub(crate) struct Session(Uuid, SocketAddr);
 
 /// This extension is responsible for tracing and recording metrics for various GraphQL queries.
 pub(crate) struct Logging(pub Arc<RpcMetrics>);
 
 struct LoggingExt {
+    session: Mutex<Option<Session>>,
     query: Mutex<Option<String>>,
     metrics: Arc<RpcMetrics>,
 }
@@ -41,6 +46,7 @@ impl Session {
 impl ExtensionFactory for Logging {
     fn create(&self) -> Arc<dyn Extension> {
         Arc::new(LoggingExt {
+            session: Mutex::new(None),
             query: Mutex::new(None),
             metrics: self.0.clone(),
         })
@@ -49,30 +55,24 @@ impl ExtensionFactory for Logging {
 
 #[async_trait::async_trait]
 impl Extension for LoggingExt {
-    /// Track query-wide metrics
-    async fn execute(
-        &self,
-        ctx: &ExtensionContext<'_>,
-        operation_name: Option<&str>,
-        next: NextExecute<'_>,
-    ) -> Response {
-        let Session(uuid, addr) = ctx.data_unchecked();
-
+    async fn request(&self, ctx: &ExtensionContext<'_>, next: NextRequest<'_>) -> Response {
         self.metrics.queries_received.inc();
         self.metrics.queries_in_flight.inc();
-
         let guard = self.metrics.query_latency.start_timer();
-        let response = next.run(ctx, operation_name).await;
+        let response = next.run(ctx).await;
         let elapsed_ms = guard.stop_and_record() * 1000.0;
-
         self.metrics.queries_in_flight.dec();
+
+        // SAFETY: This is set by `prepare_request`.
+        let Session(uuid, addr) = self.session.lock().unwrap().unwrap();
+
         if response.is_ok() {
             info!(%uuid, %addr, elapsed_ms, "Request succeeded");
             self.metrics.queries_succeeded.inc();
         } else {
             let codes = error_codes(&response);
 
-            // Log internal errors and timeouts at a higher log level than other errors.
+            // Log internal errors, timeouts, and unknown errors at a higher log level than other errors.
             if is_loud_query(&codes) {
                 warn!(%uuid, %addr, query = self.query.lock().unwrap().as_ref().unwrap(), "Query");
             } else {
@@ -94,10 +94,24 @@ impl Extension for LoggingExt {
         }
 
         debug!(%uuid, %addr, response = %json!(response), "Response");
+
         response
     }
 
-    /// debug! trace the query.
+    /// Capture Session information from the Context so that the `request` handler can use it for
+    /// logging, once it has finished executing.
+    async fn prepare_request(
+        &self,
+        ctx: &ExtensionContext<'_>,
+        request: Request,
+        next: NextPrepareRequest<'_>,
+    ) -> ServerResult<Request> {
+        let session = ctx.data_unchecked();
+        *self.session.lock().unwrap() = Some(*session);
+        next.run(ctx, request).await
+    }
+
+    /// Check for parse errors and capture the query in case we need to log it.
     async fn parse_query(
         &self,
         ctx: &ExtensionContext<'_>,
@@ -105,10 +119,27 @@ impl Extension for LoggingExt {
         variables: &Variables,
         next: NextParseQuery<'_>,
     ) -> ServerResult<ExecutableDocument> {
-        let doc = next.run(ctx, query, variables).await?;
+        let doc = next.run(ctx, query, variables).await.map_err(|mut err| {
+            fill_error_code(&mut err.extensions, code::GRAPHQL_PARSE_FAILED);
+            err
+        })?;
+
         let query = ctx.stringify_execute_doc(&doc, variables);
         *self.query.lock().unwrap() = Some(query);
         Ok(doc)
+    }
+
+    async fn validation(
+        &self,
+        ctx: &ExtensionContext<'_>,
+        next: NextValidation<'_>,
+    ) -> Result<ValidationResult, Vec<ServerError>> {
+        next.run(ctx).await.map_err(|mut errs| {
+            for err in &mut errs {
+                fill_error_code(&mut err.extensions, code::GRAPHQL_VALIDATION_FAILED);
+            }
+            errs
+        })
     }
 
     /// Track metrics per field
@@ -154,7 +185,78 @@ fn error_codes(response: &Response) -> Vec<&str> {
 /// Whether the query should be logged at a "louder" level (e.g. `warn!` instead of `debug!`),
 /// because it's related to some problem that we should probably investigate.
 fn is_loud_query(codes: &[&str]) -> bool {
-    codes
-        .iter()
-        .any(|c| matches!(*c, code::REQUEST_TIMEOUT | code::INTERNAL_SERVER_ERROR))
+    codes.is_empty()
+        || codes
+            .iter()
+            .any(|c| matches!(*c, code::REQUEST_TIMEOUT | code::INTERNAL_SERVER_ERROR))
+}
+
+#[cfg(test)]
+mod tests {
+    use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
+    use prometheus::Registry;
+
+    use super::*;
+
+    struct Query;
+
+    #[Object]
+    impl Query {
+        async fn op(&self) -> bool {
+            true
+        }
+    }
+
+    #[tokio::test]
+    async fn parsing_error_code() {
+        let registry = Registry::new();
+        let metrics = RpcMetrics::new(&registry);
+
+        let request = Request::from("{ op").data(Session::new("0.0.0.0:0".parse().unwrap()));
+        let response = Schema::build(Query, EmptyMutation, EmptySubscription)
+            .extension(Logging(metrics.clone()))
+            .finish()
+            .execute(request)
+            .await;
+
+        assert!(response.is_err());
+        assert_eq!(error_codes(&response), vec![code::GRAPHQL_PARSE_FAILED]);
+        assert_eq!(metrics.queries_received.get(), 1);
+        assert_eq!(
+            metrics
+                .queries_failed
+                .with_label_values(&[code::GRAPHQL_PARSE_FAILED])
+                .get(),
+            1
+        );
+    }
+
+    #[tokio::test]
+    async fn validation_error_code() {
+        let registry = Registry::new();
+        let metrics = RpcMetrics::new(&registry);
+
+        let request = Request::from("query ($foo: String) { op }")
+            .data(Session::new("0.0.0.0:0".parse().unwrap()));
+
+        let response = Schema::build(Query, EmptyMutation, EmptySubscription)
+            .extension(Logging(metrics.clone()))
+            .finish()
+            .execute(request)
+            .await;
+
+        assert!(response.is_err());
+        assert_eq!(
+            error_codes(&response),
+            vec![code::GRAPHQL_VALIDATION_FAILED]
+        );
+        assert_eq!(metrics.queries_received.get(), 1);
+        assert_eq!(
+            metrics
+                .queries_failed
+                .with_label_values(&[code::GRAPHQL_VALIDATION_FAILED])
+                .get(),
+            1
+        );
+    }
 }
