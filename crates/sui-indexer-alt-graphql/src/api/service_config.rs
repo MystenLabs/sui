1597415c048bc958d00407d1f0fe0c35a6e5fb0d
Ashok Menon <ashok@mystenlabs.com>
2025-03-20 14:36:40 +0000

    graphql-alt: configs for query limits
    ## Description
    
    Add configs that will be used by the query limits checker and expose
    them in the query's `ServiceConfig`.
    
    Note that these configs are not used yet by the validation pass yet.
    
    ## Test plan
    
    :eyes:

--- crates/sui-indexer-alt-graphql/src/api/service_config.rs
+++ crates/sui-indexer-alt-graphql/src/api/service_config.rs
@@ -20,4 +20,82 @@ impl ServiceConfig {
         let config: &RpcConfig = ctx.data()?;
         Ok(config.limits.query_timeout_ms)
     }
+
+    /// Maximum depth of a GraphQL query that can be accepted by this service.
+    async fn max_query_depth(&self, ctx: &Context<'_>) -> Result<u32, RpcError> {
+        let config: &RpcConfig = ctx.data()?;
+        Ok(config.limits.max_query_depth)
+    }
+
+    /// The maximum number of nodes (field names) the service will accept in a single query.
+    async fn max_query_nodes(&self, ctx: &Context<'_>) -> Result<u32, RpcError> {
+        let config: &RpcConfig = ctx.data()?;
+        Ok(config.limits.max_query_nodes)
+    }
+
+    /// Maximum number of estimated output nodes in a GraphQL response.
+    ///
+    /// The estimate is an upperbound of how many nodes there would be in the output assuming every requested field is present, paginated requests return full page sizes, and multi-get queries find all requested keys. Below is a worked example query:
+    ///
+    /// ```graphql
+    /// |  0: query {                            # 273 = 2 + 6 + 265
+    /// |  1:   checkpoint {                     #   2 = 1 + 1
+    /// |  2:     sequenceNumber                 #     1
+    /// |  3:   }
+    /// |  4:
+    /// |  5:   multiGetObjects([$a, $b, $c]) {  #   6 = 1 + 3 * (1 + 1)
+    /// |  6:     address                        #     1
+    /// |  7:     digest                         #     1
+    /// |  8:   }
+    /// |  9:
+    /// | 10:   # default page size is 20
+    /// | 11:   transactions {                   #   265 = 1 + 3 + 1 + 20 * (1 + 12)
+    /// | 12:     pageInfo {                     #     3 = 1 + 1 + 1
+    /// | 13:       hasNextPage                  #       1
+    /// | 14:       endCursor                    #       1
+    /// | 15:     }
+    /// | 16:
+    /// | 17:     nodes {                        #     1
+    /// | 18:       digest                       #       1
+    /// | 19:       effects {                    #       12 = 1 + 11
+    /// | 20:         objectChanges(first: 10) { #         11 = 1 + 10 * (1)
+    /// | 21:           nodes {                  #         1
+    /// | 22:             address                #           1
+    /// | 23:           }
+    /// | 24:         }
+    /// | 25:       }
+    /// | 26:     }
+    /// | 27:   }
+    /// | 28: }
+    /// ```
+    async fn max_output_nodes(&self, ctx: &Context<'_>) -> Result<u32, RpcError> {
+        let config: &RpcConfig = ctx.data()?;
+        Ok(config.limits.max_output_nodes)
+    }
+
+    /// Maximum size in bytes allowed for the `txBytes` and `signatures` parameters of an `executeTransaction` or `simulateTransaction` field, or the `bytes` and `signature` parameters of a `verifyZkLoginSignature` field.
+    ///
+    /// This is cumulative across all matching fields in a single GraphQL request.
+    async fn max_transaction_payload_size(&self, ctx: &Context<'_>) -> Result<u32, RpcError> {
+        let config: &RpcConfig = ctx.data()?;
+        Ok(config.limits.max_tx_payload_size)
+    }
+
+    /// Maximum size in bytes of a single GraphQL request, excluding the elements covered by `maxTransactionPayloadSize`.
+    async fn max_query_payload_size(&self, ctx: &Context<'_>) -> Result<u32, RpcError> {
+        let config: &RpcConfig = ctx.data()?;
+        Ok(config.limits.max_query_payload_size)
+    }
+
+    /// By default, paginated queries will return this many elements if a page size is not provided. This may be overridden for paginated queries that are limited by the protocol.
+    async fn default_page_size(&self, ctx: &Context<'_>) -> Result<u32, RpcError> {
+        let config: &RpcConfig = ctx.data()?;
+        Ok(config.limits.default_page_size)
+    }
+
+    /// By default, paginated queries can return at most this many elements. A request to fetch more elements will result in an error. This limit may be superseded when the field being paginated is limited by the protocol (e.g. object changes for a transaction).
+    async fn max_page_size(&self, ctx: &Context<'_>) -> Result<u32, RpcError> {
+        let config: &RpcConfig = ctx.data()?;
+        Ok(config.limits.max_page_size)
+    }
 }
--- crates/sui-indexer-alt-graphql/src/config.rs
+++ crates/sui-indexer-alt-graphql/src/config.rs
@@ -4,6 +4,7 @@
 use std::{mem, time::Duration};
 
 use sui_default_config::DefaultConfig;
+use sui_protocol_config::{Chain, ProtocolConfig, ProtocolVersion};
 use tracing::warn;
 
 use crate::extensions::timeout::TimeoutConfig;
@@ -33,6 +34,35 @@ pub struct Limits {
     /// Time (in milliseconds) to wait for a read request from the GraphQL service. Requests that
     /// take longer than this time to return a result will return a timeout error.
     pub query_timeout_ms: u32,
+
+    /// Maximum depth of a GraphQL query that can be accepted by this service.
+    pub max_query_depth: u32,
+
+    /// The maximum number of nodes (field names) the service will accept in a single query.
+    pub max_query_nodes: u32,
+
+    /// Maximum number of estimated output nodes in a GraphQL response.
+    pub max_output_nodes: u32,
+
+    /// Maximum size in bytes allowed for the `txBytes` and `signatures` parameters of an
+    /// `executeTransaction` or `simulateTransaction` field, or the `bytes` and `signature`
+    /// parameters of a `verifyZkLoginSignature` field.
+    ///
+    /// This is cumulative across all matching fields in a single GraphQL request.
+    pub max_tx_payload_size: u32,
+
+    /// Maximum size in bytes of a single GraphQL request, excluding the elements covered by
+    /// `max_transaction_payload_size`.
+    pub max_query_payload_size: u32,
+
+    /// By default, paginated queries will return this many elements if a page size is not
+    /// provided. This may be overridden for paginated queries that are limited by the protocol.
+    pub default_page_size: u32,
+
+    /// By default, paginated queries can return at most this many elements. A request to fetch
+    /// more elements will result in an error. This limit may be superseded when the field being
+    /// paginated is limited by the protocol (e.g. object changes for a transaction).
+    pub max_page_size: u32,
 }
 
 #[DefaultConfig]
@@ -40,6 +70,13 @@ pub struct Limits {
 pub struct LimitsLayer {
     pub mutation_timeout_ms: Option<u32>,
     pub query_timeout_ms: Option<u32>,
+    pub max_query_depth: Option<u32>,
+    pub max_query_nodes: Option<u32>,
+    pub max_output_nodes: Option<u32>,
+    pub max_tx_payload_size: Option<u32>,
+    pub max_query_payload_size: Option<u32>,
+    pub default_page_size: Option<u32>,
+    pub max_page_size: Option<u32>,
 
     #[serde(flatten)]
     pub extra: toml::Table,
@@ -76,6 +113,15 @@ impl LimitsLayer {
         Limits {
             mutation_timeout_ms: self.mutation_timeout_ms.unwrap_or(base.mutation_timeout_ms),
             query_timeout_ms: self.query_timeout_ms.unwrap_or(base.query_timeout_ms),
+            max_query_depth: self.max_query_depth.unwrap_or(base.max_query_depth),
+            max_query_nodes: self.max_query_nodes.unwrap_or(base.max_query_nodes),
+            max_output_nodes: self.max_output_nodes.unwrap_or(base.max_output_nodes),
+            max_tx_payload_size: self.max_tx_payload_size.unwrap_or(base.max_tx_payload_size),
+            max_query_payload_size: self
+                .max_query_payload_size
+                .unwrap_or(base.max_query_payload_size),
+            default_page_size: self.default_page_size.unwrap_or(base.default_page_size),
+            max_page_size: self.max_page_size.unwrap_or(base.max_page_size),
         }
     }
 }
@@ -85,6 +131,13 @@ impl From<Limits> for LimitsLayer {
         Self {
             mutation_timeout_ms: Some(value.mutation_timeout_ms),
             query_timeout_ms: Some(value.query_timeout_ms),
+            max_query_depth: Some(value.max_query_depth),
+            max_query_nodes: Some(value.max_query_nodes),
+            max_output_nodes: Some(value.max_output_nodes),
+            max_tx_payload_size: Some(value.max_tx_payload_size),
+            max_query_payload_size: Some(value.max_query_payload_size),
+            default_page_size: Some(value.default_page_size),
+            max_page_size: Some(value.max_page_size),
             extra: Default::default(),
         }
     }
@@ -92,6 +145,9 @@ impl From<Limits> for LimitsLayer {
 
 impl Default for Limits {
     fn default() -> Self {
+        let max_tx_size_bytes = max_across_protocol(ProtocolConfig::max_tx_size_bytes_as_option)
+            .unwrap_or(u32::MAX as u64) as u32;
+
         Self {
             // This default was picked as the sum of pre- and post- quorum timeouts from
             // [sui_core::authority_aggregator::TimeoutConfig], with a 10% buffer.
@@ -99,6 +155,14 @@ impl Default for Limits {
             // <https://github.com/MystenLabs/sui/blob/eaf05fe5d293c06e3a2dfc22c87ba2aef419d8ea/crates/sui-core/src/authority_aggregator.rs#L84-L85>
             mutation_timeout_ms: 74_000,
             query_timeout_ms: 40_000,
+            max_query_depth: 20,
+            max_query_nodes: 300,
+            max_output_nodes: 100_000,
+            // Add a 30% buffer to the protocol limit, rounded up to account Base64 overhead.
+            max_tx_payload_size: (max_tx_size_bytes * 4).div_ceil(3) as u32,
+            max_query_payload_size: 5_000,
+            default_page_size: 20,
+            max_page_size: 50,
         }
     }
 }
@@ -113,4 +177,18 @@ fn check_extra(pos: &str, extra: toml::Table) {
             extra,
         )
     }
+}
+
+/// Fetch the maximum value of a protocol config across all chains and versions.
+fn max_across_protocol<T: Ord>(f: impl Fn(&ProtocolConfig) -> Option<T>) -> Option<T> {
+    let mut x = None;
+    let mut v = ProtocolVersion::MIN;
+    while v <= ProtocolVersion::MAX {
+        x = x.max(f(&ProtocolConfig::get_for_version(v, Chain::Unknown)));
+        x = x.max(f(&ProtocolConfig::get_for_version(v, Chain::Mainnet)));
+        x = x.max(f(&ProtocolConfig::get_for_version(v, Chain::Testnet)));
+        v = v + 1;
+    }
+
+    x
 }
