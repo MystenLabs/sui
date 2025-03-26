8fb260e104f0b2ee8adfe4681fbe099d4d53d5e7
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2025-03-25 22:03:38 -0700

    Better monitored_scope coverage (#21608)

--- crates/sui-core/src/checkpoints/mod.rs
+++ crates/sui-core/src/checkpoints/mod.rs
@@ -1228,6 +1228,7 @@ impl CheckpointBuilder {
         &self,
         pendings: Vec<PendingCheckpointV2>,
     ) -> anyhow::Result<CheckpointSequenceNumber> {
+        let _scope = monitored_scope("CheckpointBuilder::make_checkpoint");
         let last_details = pendings.last().unwrap().details().clone();
 
         // Keeps track of the effects that are already included in the current checkpoint.
@@ -1265,6 +1266,8 @@ impl CheckpointBuilder {
         roots: Vec<TransactionKey>,
         effects_in_current_checkpoint: &mut BTreeSet<TransactionDigest>,
     ) -> SuiResult<Vec<TransactionEffects>> {
+        let _scope = monitored_scope("CheckpointBuilder::resolve_checkpoint_transactions");
+
         self.metrics
             .checkpoint_roots_count
             .inc_by(roots.len() as u64);
@@ -1279,8 +1282,6 @@ impl CheckpointBuilder {
             .notify_read_executed_effects(&root_digests)
             .in_monitored_scope("CheckpointNotifyRead")
             .await;
-
-        let _scope = monitored_scope("CheckpointBuilder");
 
         let consensus_commit_prologue = if self
             .epoch_store
