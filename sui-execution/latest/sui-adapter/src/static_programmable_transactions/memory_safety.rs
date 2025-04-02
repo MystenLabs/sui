219cb557dd98c3219096397be2e745222e6c4442
Todd Nowacki <tmn@mystenlabs.com>
2025-04-02 13:54:08 -0700

    typing progress

--- sui-execution/latest/sui-adapter/src/static_programmable_transactions/memory_safety.rs
+++ sui-execution/latest/sui-adapter/src/static_programmable_transactions/memory_safety.rs
@@ -2,24 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use super::{ast as T, env::Env};
-use std::collections::{BTreeMap, BTreeSet};
 use sui_types::error::ExecutionError;
-
-struct Context {
-    root: Ref,
-    gas_coin: Value,
-    inputs: Vec<Value>,
-    results: Vec<Vec<Value>>,
-}
-
-enum Value {
-    // No value
-    Invalid,
-    // A non reference value
-    NonRef,
-    // A reference value
-    Ref(Ref),
-}
 
 pub fn verify(env: &Env, txn: &T::Transaction) -> Result<(), ExecutionError> {
     todo!()
