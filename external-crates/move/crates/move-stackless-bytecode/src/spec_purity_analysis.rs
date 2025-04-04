cd52c1fdb6204eeff9b78f3f4e24592b45757338
Andrii <andrii@asymptotic.tech>
2025-04-04 17:35:12 +0300

    feat: fixes

--- external-crates/move/crates/move-stackless-bytecode/src/spec_purity_analysis.rs
+++ external-crates/move/crates/move-stackless-bytecode/src/spec_purity_analysis.rs
@@ -3,9 +3,7 @@ use std::collections::BTreeSet;
 use codespan_reporting::diagnostic::Severity;
 use move_model::model::{FunId, FunctionEnv, GlobalEnv, Loc, QualifiedId};
 
-use crate::{
-    function_data_builder::FunctionDataBuilder, function_target::{FunctionData, FunctionTarget}, function_target_pipeline::{FunctionTargetProcessor, FunctionTargetsHolder, FunctionVariant}, stackless_bytecode::{Bytecode, Operation}
-};
+use crate::{function_target::{FunctionData, FunctionTarget}, function_target_pipeline::{FunctionTargetProcessor, FunctionTargetsHolder, FunctionVariant}, stackless_bytecode::{Bytecode, Operation}};
 
 pub const RESTRICTED_MODULES: [&str; 3] = ["transfer", "event", "emit"];
 
@@ -26,7 +24,6 @@ impl SpecPurityAnalysis {
     pub fn new() -> Box<Self> {
         Box::new(Self())
     }
-
     pub fn find_operation_in_function(&self, target_id: QualifiedId<FunId>, code: &[Bytecode]) -> Option<Operation> {
         for cp in code {
             match cp {
@@ -53,16 +50,17 @@ impl SpecPurityAnalysis {
         code: &[Bytecode], 
         func_env: &FunctionEnv,
         targets: &FunctionTargetsHolder,
-        builder: &FunctionDataBuilder, 
+        target: &FunctionTarget, 
         env: &GlobalEnv, 
-        skip: &Option<Operation>, 
+        skip: &Option<Operation>,
     ) -> BTreeSet<Loc> {
         let mut results = BTreeSet::new();
-        let mut visited_funcs: BTreeSet<FunId> = BTreeSet::new();
 
-        for param in func_env.get_parameters() {
-            if param.1.is_mutable_reference() {
-                results.insert(func_env.get_loc()); 
+        if !targets.is_spec(&func_env.get_qualified_id()) {
+            for param in func_env.get_parameters() {
+                if param.1.is_mutable_reference() {
+                    results.insert(func_env.get_loc()); 
+                }
             }
         }
 
@@ -74,10 +72,6 @@ impl SpecPurityAnalysis {
                     }
                     match operation {
                         Operation::Function(mod_id, func_id, _) => {
-                            if !visited_funcs.insert(func_id.clone()) {
-                                continue;
-                            }
-
                             let module = env.get_module(*mod_id); 
                             let module_name = env.symbol_pool().string(module.get_name().name());
 
@@ -86,7 +80,7 @@ impl SpecPurityAnalysis {
                             }
 
                             if RESTRICTED_MODULES.contains(&module_name.as_str()) {
-                                results.insert(builder.get_loc(*attr)); 
+                                results.insert(target.get_bytecode_loc(*attr)); 
                             }
 
                             let internal_data = targets.get_data(&mod_id.qualified(*func_id), &FunctionVariant::Baseline);
@@ -100,7 +94,7 @@ impl SpecPurityAnalysis {
                                 .expect("Function expect to be already scanned");
 
                             if !annotation.is_pure {
-                                results.insert(builder.get_loc(*attr)); 
+                                results.insert(target.get_bytecode_loc(*attr)); 
                             }
                         },
                         _ => {}
@@ -112,30 +106,20 @@ impl SpecPurityAnalysis {
 
         results
     }
-}
 
-impl FunctionTargetProcessor for SpecPurityAnalysis {
-    fn process(
-        &self,
-        targets: &mut FunctionTargetsHolder,
-        func_env: &FunctionEnv,
-        mut data: FunctionData,
-        _scc_opt: Option<&[FunctionEnv]>,
-    ) -> FunctionData {        
+    fn analyse(&self, func_env: &FunctionEnv, targets: &FunctionTargetsHolder, data: &FunctionData) -> PurityVerificationInfo {
+        let is_spec = targets.is_spec(&func_env.get_qualified_id());
         let env = func_env.module_env.env;
-        let builder = FunctionDataBuilder::new(func_env, data.clone());
-        let func_target = FunctionTarget::new(func_env, &builder.data);
+        let func_target = FunctionTarget::new(func_env, data);
 
         let underlying_func_id = targets.get_fun_by_spec(&func_env.get_qualified_id());
 
         let code = func_target.get_bytecode();
 
         let call_operation = if underlying_func_id.is_some() { self.find_operation_in_function(*underlying_func_id.unwrap(), code) } else { None };
-        let modif_locations = self.find_modifiable_locations_in_function(code, &func_env, targets, &builder, &env, &call_operation);
+        let modif_locations = self.find_modifiable_locations_in_function(code, &func_env, targets, &func_target, &env, &call_operation);
 
-        Self::mark(&mut data, modif_locations.len() == 0);
-
-        if targets.is_spec(&func_env.get_qualified_id()) {
+        if is_spec {
             for loc in modif_locations.iter() {
                 env.diag(
                     Severity::Error,
@@ -145,20 +129,42 @@ impl FunctionTargetProcessor for SpecPurityAnalysis {
             }
         }
 
+        PurityVerificationInfo { is_pure: modif_locations.len() == 0 }
+    }
+}
+
+impl FunctionTargetProcessor for SpecPurityAnalysis {
+    fn process(
+        &self,
+        targets: &mut FunctionTargetsHolder,
+        func_env: &FunctionEnv,
+        mut data: FunctionData,
+        scc_opt: Option<&[FunctionEnv]>,
+    ) -> FunctionData {
+        let annotation = data
+            .annotations
+            .get::<PurityVerificationInfo>();
+
+        let annotation_data = if annotation.is_some() { 
+            annotation.unwrap().clone()
+         } else { 
+            self.analyse(func_env, targets, &data)
+        };
+    
+        let fixedpoint = match scc_opt {
+            None => true,
+            Some(_) => match data.annotations.get::<PurityVerificationInfo>() {
+                None => false,
+                Some(old_annotation) => old_annotation.is_pure == annotation_data.is_pure
+            },
+        };
+
+        data.annotations.set::<PurityVerificationInfo>(annotation_data, fixedpoint);
+
         data
     }
 
     fn name(&self) -> String {
         "spec_purity_analysis".to_string()
-    }
-}
-
-impl SpecPurityAnalysis {
-    fn mark(data: &mut FunctionData, pure: bool) {
-        let info = data
-            .annotations
-            .get_or_default_mut::<PurityVerificationInfo>(true);
-        
-        info.is_pure = pure;
     }
 }
