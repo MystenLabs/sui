08eb70b00c4169e8b5922c0ed347fd50d565f32e
Cam Swords <cameronswords@gmail.com>
2025-04-04 17:04:44 -0700

    [vm][move-vm][rewrite] Add message for VM errors when available in transactional tests (#21702)
    ## Description 
    
    Add `message` for VM errors when available in transactional tests
    
    ## Test plan 
    
    Tests updated
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    
    Co-authored-by: Cameron Swords <cgswords@CameronacStudio.hsd1.wa.comcast.net>

--- external-crates/move/crates/move-transactional-test-runner/src/vm_test_harness.rs
+++ external-crates/move/crates/move-transactional-test-runner/src/vm_test_harness.rs
@@ -442,14 +442,22 @@ pub fn format_vm_error(e: &VMError) -> String {
         Location::Undefined => "undefined".to_owned(),
         Location::Module(id) => format!("0x{}::{}", id.address().short_str_lossless(), id.name()),
     };
-    // message: {message:?},
+    let message = if let Some(msg) = e.message() {
+        format!(
+            "
+    message: {:?},",
+            msg
+        )
+    } else {
+        "".to_string()
+    };
     format!(
         "{{
     major_status: {major_status:?},
     sub_status: {sub_status:?},
     location: {location_string},
     indices: {indices:?},
-    offsets: {offsets:?},
+    offsets: {offsets:?},{message}
 }}",
         major_status = e.major_status(),
         sub_status = e.sub_status(),
@@ -457,7 +465,7 @@ pub fn format_vm_error(e: &VMError) -> String {
         // TODO maybe include source map info?
         indices = e.indices(),
         offsets = e.offsets(),
-        // message = e.message(),
+        message = message,
     )
 }
 
