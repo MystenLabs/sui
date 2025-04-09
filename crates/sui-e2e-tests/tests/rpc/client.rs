47bf616ea7d5f169277adf955c5060cd1af20f4d
Brandon Williams <brandon@mystenlabs.com>
2025-04-09 15:39:42 -0500

    rpc: migrate tests for the v2beta interface
    Migrate all of the e2e tests for grpc from testing the old prototype
    implementation (which will be removed in a future patch) to the new
    v2beta apis.

--- crates/sui-e2e-tests/tests/rpc/client.rs
+++ crates/sui-e2e-tests/tests/rpc/client.rs
@@ -1,14 +1,32 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::transfer_coin;
 use sui_macros::sim_test;
 use sui_rpc_api::Client;
 use sui_sdk_types::BalanceChange;
+use sui_sdk_types::ObjectId;
 use sui_test_transaction_builder::make_transfer_sui_transaction;
 use sui_types::base_types::SuiAddress;
 use sui_types::effects::TransactionEffectsAPI;
 use sui_types::transaction::TransactionDataAPI;
 use test_cluster::TestClusterBuilder;
+
+#[sim_test]
+async fn get_object() {
+    let test_cluster = TestClusterBuilder::new().build().await;
+
+    let id: ObjectId = "0x5".parse().unwrap();
+
+    let client = Client::new(test_cluster.rpc_url()).unwrap();
+
+    let _object = client.get_object(id.into()).await.unwrap();
+
+    let _object = client
+        .get_object_with_version(id.into(), 1.into())
+        .await
+        .unwrap();
+}
 
 #[sim_test]
 async fn execute_transaction_transfer() {
@@ -48,4 +66,19 @@ async fn execute_transaction_transfer() {
     actual.sort_by_key(|e| e.address);
 
     assert_eq!(actual, expected);
+}
+
+#[sim_test]
+async fn get_full_checkpoint() {
+    let test_cluster = TestClusterBuilder::new().build().await;
+
+    let _transaction_digest = transfer_coin(&test_cluster.wallet).await;
+
+    let client = Client::new(test_cluster.rpc_url()).unwrap();
+
+    let latest = client.get_latest_checkpoint().await.unwrap().into_data();
+    let _ = client
+        .get_full_checkpoint(latest.sequence_number)
+        .await
+        .unwrap();
 }
