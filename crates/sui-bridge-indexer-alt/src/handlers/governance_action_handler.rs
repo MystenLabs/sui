20b1a9d57c534ec7a5012d09b7b1bd818281f080
patrick <patrickkuo@me.com>
2025-04-11 10:38:30 +0100

    license

--- crates/sui-bridge-indexer-alt/src/handlers/governance_action_handler.rs
+++ crates/sui-bridge-indexer-alt/src/handlers/governance_action_handler.rs
@@ -1,3 +1,5 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 use crate::handlers::{BRIDGE, COMMITTEE, LIMITER, TREASURY};
 use crate::struct_tag;
 use async_trait::async_trait;
