34f4200635eaf9354ef2373a40a58ba92500f5ce
Ashok Menon <ashok@mystenlabs.com>
2025-04-11 23:50:28 +0100

    graphql-alt: TransactionEffects.effectsBcs
    ## Description
    
    Fetch transaction contents and use that to return raw transaction
    effects BCS.
    
    ## Test plan
    
    Updated E2E tests:
    
    ```
    sui$ cargo nextest run -p sui-indexer-alt-e2e-tests -- graphql/transactions
    ```

--- crates/sui-indexer-alt-graphql/src/api/types/transaction_effects.rs
+++ crates/sui-indexer-alt-graphql/src/api/types/transaction_effects.rs
@@ -1,15 +1,26 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use async_graphql::Object;
+use std::sync::Arc;
+
+use anyhow::Context as _;
+use async_graphql::{Context, Object};
 use fastcrypto::encoding::{Base58, Encoding};
+use sui_indexer_alt_reader::kv_loader::{KvLoader, TransactionContents};
 use sui_types::digests::TransactionDigest;
 
-use crate::{api::scalars::digest::Digest, error::RpcError};
+use crate::{
+    api::scalars::{base64::Base64, digest::Digest},
+    error::RpcError,
+};
 
 pub(crate) struct TransactionEffects {
     digest: TransactionDigest,
+    contents: EffectsContents,
 }
+
+#[derive(Clone)]
+pub(crate) struct EffectsContents(Option<Arc<TransactionContents>>);
 
 /// The results of executing a transaction.
 #[Object]
@@ -20,21 +31,58 @@ impl TransactionEffects {
     async fn digest(&self) -> String {
         Base58::encode(self.digest)
     }
+
+    #[graphql(flatten)]
+    async fn contents(&self, ctx: &Context<'_>) -> Result<EffectsContents, RpcError> {
+        Ok(if self.contents.0.is_some() {
+            self.contents.clone()
+        } else {
+            EffectsContents::fetch(ctx, self.digest).await?
+        })
+    }
+}
+
+#[Object]
+impl EffectsContents {
+    /// The Base64-encoded BCS serialization of these effects, as `TransactionEffects`.
+    async fn effects_bcs(&self) -> Result<Option<Base64>, RpcError> {
+        let Some(content) = &self.0 else {
+            return Ok(None);
+        };
+
+        Ok(Some(Base64(content.raw_effects()?)))
+    }
 }
 
 impl TransactionEffects {
-    /// Construct a transaction that is represented by just its identifier (its transaction
-    /// digest). This does not check whether the transaction exists, so should not be used to
-    /// "fetch" a transaction based on a digest provided as user input.
-    pub(crate) fn with_id(digest: TransactionDigest) -> Self {
-        Self { digest }
-    }
+    /// Load the effects from the store, and return it fully inflated (with contents already
+    /// fetched). Returns `None` if the effects do not exist (either never existed or were pruned
+    /// from the store).
+    pub(crate) async fn fetch(ctx: &Context<'_>, digest: Digest) -> Result<Option<Self>, RpcError> {
+        let contents = EffectsContents::fetch(ctx, digest.into()).await?;
+        let Some(tx) = &contents.0 else {
+            return Ok(None);
+        };
 
-    /// Load the transaction from the store, and return it fully inflated (with contents already
-    /// fetched). Returns `None` if the transaction does not exist (either never existed or was
-    /// pruned from the store).
-    pub(crate) async fn fetch(digest: Digest) -> Result<Option<Self>, RpcError> {
-        // TODO: Actually fetch the transaction effects to check whether it exists.
-        Ok(Some(TransactionEffects::with_id(digest.into())))
+        Ok(Some(TransactionEffects {
+            digest: tx.digest()?,
+            contents,
+        }))
+    }
+}
+
+impl EffectsContents {
+    pub(crate) async fn fetch(
+        ctx: &Context<'_>,
+        digest: TransactionDigest,
+    ) -> Result<Self, RpcError> {
+        let kv_loader: &KvLoader = ctx.data()?;
+
+        let transaction = kv_loader
+            .load_one_transaction(digest)
+            .await
+            .context("Failed to fetch transaction contents")?;
+
+        Ok(Self(transaction.map(Arc::new)))
     }
 }
