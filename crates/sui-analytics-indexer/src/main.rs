01add008db5d0de02e15729cc77bff9b51572d24
Nick <nickvikeras@gmail.com>
2025-04-17 10:24:36 -0500

    Make num cpus configurable

--- crates/sui-analytics-indexer/src/main.rs
+++ crates/sui-analytics-indexer/src/main.rs
@@ -11,8 +11,7 @@ use sui_data_ingestion_core::{
 use tokio::{signal, sync::oneshot};
 use tracing::info;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     let _guard = telemetry_subscribers::TelemetryConfig::new()
         .with_env()
         .init();
@@ -24,68 +23,76 @@ async fn main() -> Result<()> {
     let config: JobConfig = serde_yaml::from_str(&std::fs::read_to_string(&args[1])?)?;
     info!("Parsed config: {:#?}", config);
 
-    // Setup metrics
-    let registry_service = mysten_metrics::start_prometheus_server(
-        format!(
-            "{}:{}",
-            config.client_metric_host, config.client_metric_port
-        )
-        .parse()
-        .unwrap(),
-    );
-    let registry: Registry = registry_service.default_registry();
-    mysten_metrics::init_metrics(&registry);
-    let metrics = AnalyticsMetrics::new(&registry);
+    let num_cpus = config.num_cpus.unwrap_or(num_cpus::get());
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(num_cpus)
+        .enable_all()
+        .build()?;
 
-    let remote_store_url = config.remote_store_url.clone();
-    let batch_size = config.batch_size;
+    runtime.block_on(async {
+        // Setup metrics
+        let registry_service = mysten_metrics::start_prometheus_server(
+            format!(
+                "{}:{}",
+                config.client_metric_host, config.client_metric_port
+            )
+            .parse()
+            .unwrap(),
+        );
+        let registry: Registry = registry_service.default_registry();
+        mysten_metrics::init_metrics(&registry);
+        let metrics = AnalyticsMetrics::new(&registry);
 
-    let processors = config.create_checkpoint_processors(metrics).await?;
+        let remote_store_url = config.remote_store_url.clone();
+        let batch_size = config.batch_size;
 
-    let mut watermarks = HashMap::new();
-    for processor in processors.iter() {
-        let watermark = processor
-            .last_committed_checkpoint()
-            .map(|seq_num| seq_num + 1)
-            .unwrap_or(0);
-        watermarks.insert(processor.task_name.clone(), watermark);
-    }
+        let processors = config.create_checkpoint_processors(metrics).await?;
 
-    let progress_store = ShimIndexerProgressStore::new(watermarks);
-    let mut executor = IndexerExecutor::new(
-        progress_store,
-        processors.len(),
-        DataIngestionMetrics::new(&Registry::new()),
-    );
+        let mut watermarks = HashMap::new();
+        for processor in processors.iter() {
+            let watermark = processor
+                .last_committed_checkpoint()
+                .map(|seq_num| seq_num + 1)
+                .unwrap_or(0);
+            watermarks.insert(processor.task_name.clone(), watermark);
+        }
 
-    for processor in processors {
-        let task_name = processor.task_name.clone();
-        let worker_pool = WorkerPool::new(processor, task_name, 1);
-        executor.register(worker_pool).await?;
-    }
+        let progress_store = ShimIndexerProgressStore::new(watermarks);
+        let mut executor = IndexerExecutor::new(
+            progress_store,
+            processors.len(),
+            DataIngestionMetrics::new(&Registry::new()),
+        );
 
-    let reader_options = ReaderOptions {
-        batch_size,
-        ..Default::default()
-    };
+        for processor in processors {
+            let task_name = processor.task_name.clone();
+            let worker_pool = WorkerPool::new(processor, task_name, 1);
+            executor.register(worker_pool).await?;
+        }
 
-    let (exit_sender, exit_receiver) = oneshot::channel();
-    let executor_progress = executor.run(
-        tempfile::tempdir()?.into_path(),
-        Some(remote_store_url),
-        vec![],
-        reader_options,
-        exit_receiver,
-    );
+        let reader_options = ReaderOptions {
+            batch_size,
+            ..Default::default()
+        };
 
-    tokio::spawn(async {
-        signal::ctrl_c()
-            .await
-            .expect("Failed to install Ctrl+C handler");
-        exit_sender
-            .send(())
-            .expect("Failed to gracefully process shutdown");
-    });
-    executor_progress.await?;
-    Ok(())
+        let (exit_sender, exit_receiver) = oneshot::channel();
+        let executor_progress = executor.run(
+            tempfile::tempdir()?.into_path(),
+            Some(remote_store_url),
+            vec![],
+            reader_options,
+            exit_receiver,
+        );
+
+        tokio::spawn(async {
+            signal::ctrl_c()
+                .await
+                .expect("Failed to install Ctrl+C handler");
+            exit_sender
+                .send(())
+                .expect("Failed to gracefully process shutdown");
+        });
+        executor_progress.await?;
+        Ok(())
+    })
 }
