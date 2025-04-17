48e3bd49f744d2d3a09e6f2ac1eaf09fc2943146
Nick <nickvikeras@gmail.com>
2025-04-17 16:00:51 -0500

    jemalloc-sys

--- crates/sui-analytics-indexer/src/heap_profiler.rs
+++ crates/sui-analytics-indexer/src/heap_profiler.rs
@@ -1,23 +1,13 @@
-use std::{ffi::CString, os::raw::c_char, ptr};
-
-extern "C" {
-    fn mallctl(
-        name: *const c_char,
-        oldp: *mut std::ffi::c_void,
-        oldlenp: *mut usize,
-        newp: *mut std::ffi::c_void,
-        newlen: usize,
-    ) -> i32;
-}
+use jemalloc_sys::mallctl;
 
 pub fn dump_heap_profile_now() {
-    let name = CString::new("prof.dump").unwrap();
+    let name = std::ffi::CString::new("prof.dump").unwrap();
     unsafe {
         mallctl(
             name.as_ptr(),
-            ptr::null_mut(),
-            ptr::null_mut(),
-            ptr::null_mut(),
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
             0,
         );
     }
