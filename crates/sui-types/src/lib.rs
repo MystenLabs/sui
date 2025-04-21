85ce58c23d62d2ddc30814cfafa0b57e57786817
Brandon Williams <brandon@mystenlabs.com>
2025-04-21 10:27:38 -0500

    proto: implement json rendering of move values
    Using the move layout Visitor infrastructure, implement json rendering
    of arbitrary move values which can then be embedded in protobuf messages
    using google.protobuf.Value.

--- crates/sui-types/src/lib.rs
+++ crates/sui-types/src/lib.rs
@@ -73,6 +73,7 @@ pub mod nitro_attestation;
 pub mod object;
 pub mod passkey_authenticator;
 pub mod programmable_transaction_builder;
+pub mod proto_value;
 pub mod quorum_driver_types;
 pub mod randomness_state;
 pub mod signature;
