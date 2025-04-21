c3e3f0292d665bd32c4d0ce109cbdb737b30cbb0
Ashok Menon <ashok@mystenlabs.com>
2025-04-21 15:27:24 +0100

    indexer-reader: resolve packages using kv_packages
    ## Description
    
    Cut over from `sum_packages` to `kv_packages` for fetching the latest
    version of a package, removing support for `sum_packages` altogether
    from the reader and indexer codebases.
    
    This change needs to wait for the following to happen before it can
    land:
    
    - Indexing changes to introduce `kv_packages` need to land first, and
      backfill.
    - The old `sum_packages` pipelines need to be wound down, to remove a
      dependency on the code being removed.
    
    ## Test plan
    
    Existing tests.

--- crates/sui-indexer-alt/src/handlers/mod.rs
+++ crates/sui-indexer-alt/src/handlers/mod.rs
@@ -26,7 +26,6 @@ pub(crate) mod kv_transactions;
 pub(crate) mod obj_info;
 pub(crate) mod obj_versions;
 pub(crate) mod sum_displays;
-pub(crate) mod sum_packages;
 pub(crate) mod tx_affected_addresses;
 pub(crate) mod tx_affected_objects;
 pub(crate) mod tx_balance_changes;
