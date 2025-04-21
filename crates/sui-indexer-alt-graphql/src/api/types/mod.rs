89b72e361a3ba1822ce35f8a8b5ee22926c31fbb
Ashok Menon <ashok@mystenlabs.com>
2025-04-21 23:41:58 +0100

    graphql-alt: IAddressable interface
    ## Description
    
    Pull out the common trait representing anything that has an address.
    
    ## Test plan
    
    Existing tests.

--- crates/sui-indexer-alt-graphql/src/api/types/mod.rs
+++ crates/sui-indexer-alt-graphql/src/api/types/mod.rs
@@ -1,6 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+pub(crate) mod addressable;
 pub(crate) mod object;
 pub(crate) mod object_change;
 pub(crate) mod service_config;
