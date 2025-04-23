26b42da2b26201110fe390f63dfd805b3d50dd6b
Nick <nickvikeras@gmail.com>
2025-04-23 13:26:06 -0500

    Rewrite parquet writer to use less memory

--- crates/sui-analytics-indexer/src/analytics_metrics.rs
+++ crates/sui-analytics-indexer/src/analytics_metrics.rs
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 #![allow(dead_code)]
 use prometheus::{
-    register_int_counter_vec_with_registry, register_int_gauge_vec_with_registry, IntCounterVec,
-    IntGaugeVec, Registry,
+    register_histogram_vec_with_registry, register_int_counter_vec_with_registry,
+    register_int_gauge_vec_with_registry, HistogramOpts, HistogramVec, IntCounterVec, IntGaugeVec,
+    Registry,
 };
 
 #[derive(Clone)]
@@ -12,6 +13,7 @@ pub struct AnalyticsMetrics {
     pub last_uploaded_checkpoint: IntGaugeVec,
     pub max_checkpoint_on_store: IntGaugeVec,
     pub total_too_large_to_deserialize: IntCounterVec,
+    pub file_size_bytes: HistogramVec,
 }
 
 impl AnalyticsMetrics {
@@ -42,6 +44,25 @@ impl AnalyticsMetrics {
                 "total_too_large_to_deserialize",
                 "Total number of rows skipped due to size.",
                 &["data_type"],
+                registry,
+            )
+            .unwrap(),
+            file_size_bytes: register_histogram_vec_with_registry!(
+                HistogramOpts::new("file_size_bytes", "Size of generated files in bytes.",)
+                    .buckets(vec![
+                        1_000.0,         // 1 KB
+                        10_000.0,        // 10 KB
+                        100_000.0,       // 100 KB
+                        1_000_000.0,     // 1 MB
+                        10_000_000.0,    // 10 MB
+                        50_000_000.0,    // 50 MB
+                        100_000_000.0,   // 100 MB
+                        250_000_000.0,   // 250 MB
+                        500_000_000.0,   // 500 MB
+                        1_000_000_000.0, // 1 GB
+                        2_000_000_000.0, // 2 GB
+                    ]),
+                &["source"],
                 registry,
             )
             .unwrap(),
