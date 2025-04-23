c5b7c930fd41977fa378506f38305a5c0c77f282
Andrey Chursin <andrey@mystenlabs.com>
2025-04-23 16:45:20 -0700

    Correctly initialize prometheus registry for tidehunter (#21965)

--- crates/typed-store/src/tidehunter_util.rs
+++ crates/typed-store/src/tidehunter_util.rs
@@ -1,8 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::DBMetrics;
 use bincode::Options;
-use prometheus::Registry;
 use serde::de::DeserializeOwned;
 use std::path::Path;
 use std::sync::Arc;
@@ -28,8 +28,7 @@ pub struct ThConfig {
 
 pub fn open(path: &Path, key_shape: KeyShape) -> Arc<Db> {
     std::fs::create_dir_all(path).expect("failed to open tidehunter db");
-    // TODO: fix metrics initialization
-    let metrics = Metrics::new_in(&Registry::default());
+    let metrics = Metrics::new_in(&DBMetrics::get().registry);
     let db = Db::open(path, key_shape, Arc::new(thdb_config()), metrics)
         .expect("failed to open tidehunter db");
     db.start_periodic_snapshot();
