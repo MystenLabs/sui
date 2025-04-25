8fdc2ae9f5b116aa9fc04fe3891f8887d0aab0b7
quentin <quentus123@gmail.com>
2025-04-25 17:50:21 +0200

    made requested changes

--- crates/sui-types/src/unit_tests/programmable_transaction_builder_tests.rs
+++ crates/sui-types/src/unit_tests/programmable_transaction_builder_tests.rs
@@ -1,3 +1,5 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 use crate::base_types::random_object_ref;
 use crate::programmable_transaction_builder::ProgrammableTransactionBuilder;
 use crate::transaction::Argument::Input;
