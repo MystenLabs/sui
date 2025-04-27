6966d9d1118878beb56b2847d6d97cbee8c6d79a
Mingwei Tian <mingwei@mystenlabs.com>
2025-04-27 13:10:03 -0700

    Wait for replaying consensus commits before checkpoint builder starts

--- crates/sui-core/src/unit_tests/mysticeti_manager_tests.rs
+++ crates/sui-core/src/unit_tests/mysticeti_manager_tests.rs
@@ -46,7 +46,7 @@ pub fn checkpoint_service_for_testing(state: Arc<AuthorityState>) -> Arc<Checkpo
         3,
         100_000,
     );
-    checkpoint_service.spawn().now_or_never().unwrap();
+    checkpoint_service.spawn(None).now_or_never().unwrap();
     checkpoint_service
 }
 
