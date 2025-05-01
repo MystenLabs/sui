c9a861dd6c831bd8de4be7bfeb33877bf5dd8015
Nick <nickvikeras@gmail.com>
2025-05-01 13:48:33 +0400

    Refactor

--- crates/sui-analytics-indexer/src/handlers/transaction_bcs_handler.rs
+++ crates/sui-analytics-indexer/src/handlers/transaction_bcs_handler.rs
@@ -3,100 +3,77 @@
 
 use anyhow::Result;
 use fastcrypto::encoding::{Base64, Encoding};
-use sui_data_ingestion_core::Worker;
-use tokio::sync::Mutex;
 
-use sui_types::full_checkpoint_content::{CheckpointData, CheckpointTransaction};
+use sui_types::full_checkpoint_content::CheckpointData;
 
 use crate::handlers::AnalyticsHandler;
 use crate::tables::TransactionBCSEntry;
 use crate::FileType;
 
-pub struct TransactionBCSHandler {
-    pub(crate) state: Mutex<State>,
-}
+#[derive(Clone)]
+pub struct TransactionBCSHandler {}
 
-pub(crate) struct State {
-    pub(crate) transactions: Vec<TransactionBCSEntry>,
-}
+impl TransactionBCSHandler {
+    pub fn new() -> Self {
+        TransactionBCSHandler {}
+    }
 
-#[async_trait::async_trait]
-impl Worker for TransactionBCSHandler {
-    type Result = ();
+    async fn process_transactions(
+        &self,
+        checkpoint_data: &CheckpointData,
+    ) -> Result<Vec<TransactionBCSEntry>> {
+        let txn_len = checkpoint_data.transactions.len();
+        let mut entries = Vec::with_capacity(txn_len);
 
-    async fn process_checkpoint(&self, checkpoint_data: &CheckpointData) -> Result<()> {
-        let CheckpointData {
-            checkpoint_summary,
-            transactions: checkpoint_transactions,
-            ..
-        } = checkpoint_data;
-        let mut state = self.state.lock().await;
-        for checkpoint_transaction in checkpoint_transactions {
-            self.process_transaction(
-                checkpoint_summary.epoch,
-                checkpoint_summary.sequence_number,
-                checkpoint_summary.timestamp_ms,
-                checkpoint_transaction,
-                &mut state,
-            )?;
+        for idx in 0..txn_len {
+            let transaction = &checkpoint_data.transactions[idx];
+            let epoch = checkpoint_data.checkpoint_summary.epoch;
+            let checkpoint_seq = checkpoint_data.checkpoint_summary.sequence_number;
+            let timestamp_ms = checkpoint_data.checkpoint_summary.timestamp_ms;
+
+            let txn = &transaction.transaction;
+            let txn_data = txn.transaction_data();
+            let transaction_digest = txn.digest().base58_encode();
+
+            let entry = TransactionBCSEntry {
+                transaction_digest,
+                checkpoint: checkpoint_seq,
+                epoch,
+                timestamp_ms,
+                bcs: Base64::encode(bcs::to_bytes(&txn_data).unwrap()),
+            };
+
+            entries.push(entry);
         }
-        Ok(())
+
+        Ok(entries)
     }
 }
 
 #[async_trait::async_trait]
 impl AnalyticsHandler<TransactionBCSEntry> for TransactionBCSHandler {
-    async fn read(&self) -> Result<Vec<TransactionBCSEntry>> {
-        let mut state = self.state.lock().await;
-        Ok(std::mem::take(&mut state.transactions))
+    async fn process_checkpoint(
+        &self,
+        checkpoint_data: &CheckpointData,
+    ) -> Result<Vec<TransactionBCSEntry>> {
+        self.process_transactions(checkpoint_data).await
     }
 
     fn file_type(&self) -> Result<FileType> {
         Ok(FileType::TransactionBCS)
     }
 
-    fn name(&self) -> &str {
+    fn name(&self) -> &'static str {
         "transaction_bcs"
-    }
-}
-
-impl TransactionBCSHandler {
-    pub fn new() -> Self {
-        let state = Mutex::new(State {
-            transactions: vec![],
-        });
-        TransactionBCSHandler { state }
-    }
-    fn process_transaction(
-        &self,
-        epoch: u64,
-        checkpoint: u64,
-        timestamp_ms: u64,
-        checkpoint_transaction: &CheckpointTransaction,
-        state: &mut State,
-    ) -> Result<()> {
-        let transaction = &checkpoint_transaction.transaction;
-        let txn_data = transaction.transaction_data();
-        let transaction_digest = transaction.digest().base58_encode();
-
-        let entry = TransactionBCSEntry {
-            transaction_digest,
-            checkpoint,
-            epoch,
-            timestamp_ms,
-            bcs: Base64::encode(bcs::to_bytes(&txn_data).unwrap()),
-        };
-        state.transactions.push(entry);
-        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::handlers::transaction_bcs_handler::TransactionBCSHandler;
+    use crate::handlers::AnalyticsHandler;
     use fastcrypto::encoding::{Base64, Encoding};
     use simulacrum::Simulacrum;
-    use sui_data_ingestion_core::Worker;
     use sui_types::base_types::SuiAddress;
     use sui_types::storage::ReadStore;
 
@@ -118,8 +95,7 @@ mod tests {
                 .unwrap(),
         )?;
         let txn_handler = TransactionBCSHandler::new();
-        txn_handler.process_checkpoint(&checkpoint_data).await?;
-        let transaction_entries = txn_handler.state.lock().await.transactions.clone();
+        let transaction_entries = txn_handler.process_checkpoint(&checkpoint_data).await?;
         assert_eq!(transaction_entries.len(), 1);
         let db_txn = transaction_entries.first().unwrap();
 
--- crates/sui-analytics-indexer/src/handlers/transaction_objects_handler.rs
+++ crates/sui-analytics-indexer/src/handlers/transaction_objects_handler.rs
@@ -2,151 +2,99 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
-use sui_data_ingestion_core::Worker;
-use tokio::sync::Mutex;
 
-use sui_types::base_types::ObjectID;
-use sui_types::effects::TransactionEffects;
-use sui_types::full_checkpoint_content::{CheckpointData, CheckpointTransaction};
+use sui_types::full_checkpoint_content::CheckpointData;
 use sui_types::transaction::TransactionDataAPI;
 
 use crate::handlers::{AnalyticsHandler, InputObjectTracker, ObjectStatusTracker};
 use crate::tables::TransactionObjectEntry;
 use crate::FileType;
 
-pub struct TransactionObjectsHandler {
-    state: Mutex<State>,
-}
+#[derive(Clone)]
+pub struct TransactionObjectsHandler {}
 
-struct State {
-    transaction_objects: Vec<TransactionObjectEntry>,
-}
+impl TransactionObjectsHandler {
+    pub fn new() -> Self {
+        TransactionObjectsHandler {}
+    }
 
-#[async_trait::async_trait]
-impl Worker for TransactionObjectsHandler {
-    type Result = ();
+    async fn process_transactions(
+        &self,
+        checkpoint_data: &CheckpointData,
+    ) -> Result<Vec<TransactionObjectEntry>> {
+        let txn_len = checkpoint_data.transactions.len();
+        let mut entries = Vec::new();
 
-    async fn process_checkpoint(&self, checkpoint_data: &CheckpointData) -> Result<()> {
-        let CheckpointData {
-            checkpoint_summary,
-            transactions: checkpoint_transactions,
-            ..
-        } = checkpoint_data;
-        let mut state = self.state.lock().await;
-        for checkpoint_transaction in checkpoint_transactions {
-            self.process_transaction(
-                checkpoint_summary.epoch,
-                checkpoint_summary.sequence_number,
-                checkpoint_summary.timestamp_ms,
-                checkpoint_transaction,
-                &checkpoint_transaction.effects,
-                &mut state,
-            );
+        for idx in 0..txn_len {
+            let transaction = &checkpoint_data.transactions[idx];
+            let epoch = checkpoint_data.checkpoint_summary.epoch;
+            let checkpoint_seq = checkpoint_data.checkpoint_summary.sequence_number;
+            let timestamp_ms = checkpoint_data.checkpoint_summary.timestamp_ms;
+
+            let transaction_digest = transaction.transaction.digest().base58_encode();
+            let txn_data = transaction.transaction.transaction_data();
+            let effects = &transaction.effects;
+
+            let input_object_tracker = InputObjectTracker::new(txn_data);
+            let object_status_tracker = ObjectStatusTracker::new(effects);
+
+            // Process input objects
+            for object in txn_data
+                .input_objects()
+                .expect("Input objects must be valid")
+                .iter()
+            {
+                let object_id = object.object_id();
+                let version = object.version().map(|v| v.value());
+                let entry = TransactionObjectEntry {
+                    object_id: object_id.to_string(),
+                    version,
+                    transaction_digest: transaction_digest.clone(),
+                    checkpoint: checkpoint_seq,
+                    epoch,
+                    timestamp_ms,
+                    input_kind: input_object_tracker.get_input_object_kind(&object_id),
+                    object_status: object_status_tracker.get_object_status(&object_id),
+                };
+                entries.push(entry);
+            }
+
+            // Process output objects
+            for object in transaction.output_objects.iter() {
+                let object_id = object.id();
+                let version = Some(object.version().value());
+                let entry = TransactionObjectEntry {
+                    object_id: object_id.to_string(),
+                    version,
+                    transaction_digest: transaction_digest.clone(),
+                    checkpoint: checkpoint_seq,
+                    epoch,
+                    timestamp_ms,
+                    input_kind: input_object_tracker.get_input_object_kind(&object_id),
+                    object_status: object_status_tracker.get_object_status(&object_id),
+                };
+                entries.push(entry);
+            }
         }
-        Ok(())
+
+        Ok(entries)
     }
 }
 
 #[async_trait::async_trait]
 impl AnalyticsHandler<TransactionObjectEntry> for TransactionObjectsHandler {
-    async fn read(&self) -> Result<Vec<TransactionObjectEntry>> {
-        let mut state = self.state.lock().await;
-        Ok(std::mem::take(&mut state.transaction_objects))
+    async fn process_checkpoint(
+        &self,
+        checkpoint_data: &CheckpointData,
+    ) -> Result<Vec<TransactionObjectEntry>> {
+        self.process_transactions(checkpoint_data).await
     }
 
     fn file_type(&self) -> Result<FileType> {
         Ok(FileType::TransactionObjects)
     }
 
-    fn name(&self) -> &str {
+    fn name(&self) -> &'static str {
         "transaction_objects"
-    }
-}
-
-impl TransactionObjectsHandler {
-    pub fn new() -> Self {
-        TransactionObjectsHandler {
-            state: Mutex::new(State {
-                transaction_objects: vec![],
-            }),
-        }
-    }
-    fn process_transaction(
-        &self,
-        epoch: u64,
-        checkpoint: u64,
-        timestamp_ms: u64,
-        checkpoint_transaction: &CheckpointTransaction,
-        effects: &TransactionEffects,
-        state: &mut State,
-    ) {
-        let transaction = &checkpoint_transaction.transaction;
-        let transaction_digest = transaction.digest().base58_encode();
-        let txn_data = transaction.transaction_data();
-        let input_object_tracker = InputObjectTracker::new(txn_data);
-        let object_status_tracker = ObjectStatusTracker::new(effects);
-        // input
-        txn_data
-            .input_objects()
-            .expect("Input objects must be valid")
-            .iter()
-            .map(|object| (object.object_id(), object.version().map(|v| v.value())))
-            .for_each(|(object_id, version)| {
-                self.process_transaction_object(
-                    epoch,
-                    checkpoint,
-                    timestamp_ms,
-                    transaction_digest.clone(),
-                    &object_id,
-                    version,
-                    &input_object_tracker,
-                    &object_status_tracker,
-                    state,
-                )
-            });
-        // output
-        checkpoint_transaction
-            .output_objects
-            .iter()
-            .map(|object| (object.id(), Some(object.version().value())))
-            .for_each(|(object_id, version)| {
-                self.process_transaction_object(
-                    epoch,
-                    checkpoint,
-                    timestamp_ms,
-                    transaction_digest.clone(),
-                    &object_id,
-                    version,
-                    &input_object_tracker,
-                    &object_status_tracker,
-                    state,
-                )
-            });
-    }
-    // Transaction object data.
-    // Builds a view of the object in input and output of a transaction.
-    fn process_transaction_object(
-        &self,
-        epoch: u64,
-        checkpoint: u64,
-        timestamp_ms: u64,
-        transaction_digest: String,
-        object_id: &ObjectID,
-        version: Option<u64>,
-        input_object_tracker: &InputObjectTracker,
-        object_status_tracker: &ObjectStatusTracker,
-        state: &mut State,
-    ) {
-        let entry = TransactionObjectEntry {
-            object_id: object_id.to_string(),
-            version,
-            transaction_digest,
-            checkpoint,
-            epoch,
-            timestamp_ms,
-            input_kind: input_object_tracker.get_input_object_kind(object_id),
-            object_status: object_status_tracker.get_object_status(object_id),
-        };
-        state.transaction_objects.push(entry);
     }
 }
