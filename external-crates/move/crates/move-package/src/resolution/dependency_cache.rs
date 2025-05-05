2913dd696026975e38958a968472290420b5c0d4
Andrii <andrii@asymptotic.tech>
2025-05-05 20:30:31 +0300

    feat: git deps fetching update

--- external-crates/move/crates/move-package/src/resolution/dependency_cache.rs
+++ external-crates/move/crates/move-package/src/resolution/dependency_cache.rs
@@ -90,7 +90,13 @@ impl DependencyCache {
                     )?;
                     // If the cached folder does not exist, download and clone accordingly
                     if let Ok(mut output) = Command::new("git")
-                        .args([OsStr::new("clone"), os_git_url, git_path.as_os_str()])
+                        .args([
+                            OsStr::new("clone"),
+                            OsStr::new("--depth=1"),
+                            OsStr::new("--single-branch"),
+                            os_git_url,
+                            git_path.as_os_str(),
+                        ])
                         .stdin(Stdio::null())
                         .spawn()
                     {
