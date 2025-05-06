114993f73f85301b99e66bef3013017a5f5005b3
MoFahimKh <fk0534872@gmail.com>
2025-05-06 17:03:06 +0530

    fix(examples): update SyntaxHighlighter to follow Docusaurus theme

--- docs/site/src/components/API/api-ref/examples.js
+++ docs/site/src/components/API/api-ref/examples.js
@@ -1,36 +1,19 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Markdown from "markdown-to-jsx";
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import js from "react-syntax-highlighter/dist/esm/languages/hljs/json";
 import docco from "react-syntax-highlighter/dist/esm/styles/hljs/docco";
 import dark from "react-syntax-highlighter/dist/esm/styles/hljs/dracula";
+import { useColorMode } from "@docusaurus/theme-common";
 
 SyntaxHighlighter.registerLanguage("json", js);
 
 const Examples = (props) => {
-  const [light, setLight] = useState(true);
-
-  useEffect(() => {
-    const checkTheme = () => {
-      const theme = localStorage.getItem("theme");
-
-      if (theme !== "light") {
-        setLight(false);
-      }
-    };
-
-    window.addEventListener("storage", checkTheme);
-
-    return () => {
-      window.removeEventListener("storage", checkTheme);
-    };
-  }, [light]);
-
   const { method, examples } = props;
-
+  const { colorMode } = useColorMode(); // "light" or "dark"
   const request = {
     jsonrpc: "2.0",
     id: 1,
@@ -38,8 +21,7 @@ const Examples = (props) => {
     params: [],
   };
 
-  let keyedParams = examples[0].params;
-
+  const keyedParams = examples[0].params;
   keyedParams.forEach((item) => {
     request.params.push(item.value);
   });
@@ -49,31 +31,36 @@ const Examples = (props) => {
 
   const response = {
     jsonrpc: "2.0",
-    result: {},
+    result: examples[0].result.value,
     id: 1,
   };
 
-  response.result = examples[0].result.value;
+  const isLightTheme = colorMode === "light";
 
   return (
     <div className="mx-4">
       <p className="my-2">
         <Markdown>{examples[0].name}</Markdown>
       </p>
+
       {examples[0].params && (
         <div>
           <p className="font-bold mt-4 text-sui-gray-80 dark:text-sui-gray-50">
             Request
           </p>
-          <pre className="p-2 pb-0 max-h-96	dark:bg-sui-ghost-dark bg-sui-ghost-white rounded-lg mt-4 overflow-x-auto border dark:border-sui-gray-75">
+          <pre className="p-2 pb-0 max-h-96 dark:bg-sui-ghost-dark bg-sui-ghost-white rounded-lg mt-4 overflow-x-auto border dark:border-sui-gray-75">
             <code className="text-base">
-              <SyntaxHighlighter language={js} style={light ? docco : dark}>
+              <SyntaxHighlighter
+                language="json"
+                style={isLightTheme ? docco : dark}
+              >
                 {stringRequest}
               </SyntaxHighlighter>
             </code>
           </pre>
         </div>
       )}
+
       {examples[0].result.value && (
         <div>
           <p className="font-bold mt-6 text-sui-gray-80 dark:text-sui-gray-50">
@@ -81,7 +68,10 @@ const Examples = (props) => {
           </p>
           <pre className="p-2 pb-0 max-h-96 dark:bg-sui-ghost-dark bg-sui-ghost-white rounded-lg mt-4 overflow-x-auto border dark:border-sui-gray-75">
             <code className="text-base">
-              <SyntaxHighlighter language={js} style={light ? docco : dark}>
+              <SyntaxHighlighter
+                language="json"
+                style={isLightTheme ? docco : dark}
+              >
                 {JSON.stringify(response, null, 2)}
               </SyntaxHighlighter>
             </code>
