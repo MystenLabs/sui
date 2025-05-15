d9224eeae3d8f46d91b19e7462999a78e16f492a
Adam Welc <adam@mystenlabs.com>
2025-05-15 16:58:23 +0200

    Coins are now represented as Move values in the trace

--- crates/sui-types/src/ptb_trace.rs
+++ crates/sui-types/src/ptb_trace.rs
@@ -1,11 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use move_trace_format::format::TypeTagWithRefs;
+use move_trace_format::{format::TypeTagWithRefs, value::SerializableMoveValue};
 
 use serde::Serialize;
-
-use crate::base_types::ObjectID;
 
 /// Information about the PTB commands to be stored in the PTB start event.
 /// It contains only the information needed to provide a summary of the command.
@@ -39,22 +37,20 @@ pub enum PTBExternalEvent {
     Upgrade,     // TODO
 }
 
-/// Information about the coin store in external trace events.
+/// Information about an object stored in external trace events.
 #[derive(Clone, Debug, Serialize)]
-pub struct CoinInfo {
-    /// Identifier of the coin object.
-    pub object_id: ObjectID,
-    /// Coin balance.
-    pub balance: u64,
+pub struct ObjectInfo {
+    /// Coin type.
+    pub type_: TypeTagWithRefs,
+    /// Move value representation of the coin.
+    pub value: SerializableMoveValue,
 }
 
 /// Information about the SplitCoins external event.
 #[derive(Clone, Debug, Serialize)]
 pub struct SplitCoinsEvent {
-    /// Type of both input and output coins.
-    pub type_: TypeTagWithRefs,
     /// Input coin.
-    pub input: CoinInfo,
+    pub input: ObjectInfo,
     /// Output coins.
-    pub result: Vec<CoinInfo>,
+    pub result: Vec<ObjectInfo>,
 }
