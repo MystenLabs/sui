3ef6fe80517063424a7b1d7e481897b49d0afad9
Tim Zakian <2895723+tzakian@users.noreply.github.com>
2025-05-22 18:08:24 -0700

    Revert "[3/n][object runtime type tags] Add type tags to object runtime (part 2) (#22169)" (#22213)
    This reverts commit 84b515d4028ede7020742d13b89a04c54a2022e7.
    
    Snapshot CI tests are still failing, so reverting to prevent noise while
    I fix them.

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -700,10 +700,6 @@ struct FeatureFlags {
     // Allow objects created or mutated in system transactions to exceed the max object size limit.
     #[serde(skip_serializing_if = "is_false")]
     allow_unbounded_system_objects: bool,
-
-    // Signifies the cut-over of using type tags instead of `Type`s in the object runtime.
-    #[serde(skip_serializing_if = "is_false")]
-    type_tags_in_object_runtime: bool,
 }
 
 fn is_false(b: &bool) -> bool {
@@ -1998,10 +1994,6 @@ impl ProtocolConfig {
 
     pub fn allow_unbounded_system_objects(&self) -> bool {
         self.feature_flags.allow_unbounded_system_objects
-    }
-
-    pub fn type_tags_in_object_runtime(&self) -> bool {
-        self.feature_flags.type_tags_in_object_runtime
     }
 }
 
@@ -3588,7 +3580,6 @@ impl ProtocolConfig {
                     if chain != Chain::Mainnet && chain != Chain::Testnet {
                         cfg.feature_flags.enable_party_transfer = true;
                     }
-                    cfg.feature_flags.type_tags_in_object_runtime = true;
                 }
                 // Use this template when making changes:
                 //
--- sui-execution/latest/sui-adapter/src/programmable_transactions/linkage_view.rs
+++ sui-execution/latest/sui-adapter/src/programmable_transactions/linkage_view.rs
@@ -1,31 +1,32 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::programmable_transactions::data_store::PackageStore;
+use std::{
+    cell::RefCell,
+    collections::{BTreeMap, HashMap, HashSet, hash_map::Entry},
+    str::FromStr,
+};
+
+use crate::execution_value::SuiResolver;
 use move_core_types::{
     account_address::AccountAddress,
     identifier::{IdentStr, Identifier},
     language_storage::ModuleId,
     resolver::{LinkageResolver, ModuleResolver},
 };
-use std::{
-    cell::RefCell,
-    collections::{BTreeMap, HashMap, HashSet, hash_map::Entry},
-    rc::Rc,
-    str::FromStr,
-};
+use sui_types::storage::{PackageObject, get_module};
 use sui_types::{
     base_types::ObjectID,
     error::{ExecutionError, SuiError, SuiResult},
     move_package::{MovePackage, TypeOrigin, UpgradeInfo},
+    storage::BackingPackageStore,
 };
 
 /// Exposes module and linkage resolution to the Move runtime.  The first by delegating to
 /// `resolver` and the second via linkage information that is loaded from a move package.
 pub struct LinkageView<'state> {
-    /// Interface to resolve packages, modules and resources directly from the store, and possibly
-    /// from other sources (e.g., packages just published).
-    resolver: Box<dyn PackageStore + 'state>,
+    /// Interface to resolve packages, modules and resources directly from the store.
+    resolver: Box<dyn SuiResolver + 'state>,
     /// Information used to change module and type identities during linkage.
     linkage_info: Option<LinkageInfo>,
     /// Cache containing the type origin information from every package that has been set as the
@@ -39,11 +40,6 @@ pub struct LinkageView<'state> {
     /// Cache of past package addresses that have been the link context -- if a package is in this
     /// set, then we will not try to load its type origin table when setting it as a context (again).
     past_contexts: RefCell<HashSet<ObjectID>>,
-    /// A mapping from the defining ID of a type to a valid linkage context that has already been
-    /// set. This is used to avoid double-loading packages for types that are already in the
-    /// cache. Note that there may be multiple "valid" resolutions for a given defining ID, but we
-    /// only care about having one of them so first one wins.
-    context_resolution_cache: RefCell<BTreeMap<ObjectID, ObjectID>>,
 }
 
 #[derive(Debug)]
@@ -56,13 +52,12 @@ pub struct LinkageInfo {
 pub struct SavedLinkage(LinkageInfo);
 
 impl<'state> LinkageView<'state> {
-    pub fn new(resolver: Box<dyn PackageStore + 'state>) -> Self {
+    pub fn new(resolver: Box<dyn SuiResolver + 'state>) -> Self {
         Self {
             resolver,
             linkage_info: None,
             type_origin_cache: RefCell::new(HashMap::new()),
             past_contexts: RefCell::new(HashSet::new()),
-            context_resolution_cache: RefCell::new(BTreeMap::new()),
         }
     }
 
@@ -132,16 +127,6 @@ impl<'state> LinkageView<'state> {
             package: defining_id,
         } in context.type_origin_table()
         {
-            if !self
-                .context_resolution_cache
-                .borrow()
-                .contains_key(defining_id)
-            {
-                self.context_resolution_cache
-                    .borrow_mut()
-                    .insert(*defining_id, storage_id);
-            }
-
             let Ok(module_name) = Identifier::from_str(module_name) else {
                 invariant_violation!("Module name isn't an identifier: {module_name}");
             };
@@ -155,13 +140,6 @@ impl<'state> LinkageView<'state> {
         }
 
         Ok(runtime_id)
-    }
-
-    pub fn linkage_context_for_defining_id(&self, defining_id: ObjectID) -> Option<ObjectID> {
-        self.context_resolution_cache
-            .borrow()
-            .get(&defining_id)
-            .cloned()
     }
 
     pub fn original_package_id(&self) -> Option<AccountAddress> {
@@ -261,7 +239,7 @@ impl<'state> LinkageView<'state> {
         }
 
         let storage_id = ObjectID::from(*self.relocate(runtime_id)?.address());
-        let Some(package) = self.resolver.get_package(&storage_id)? else {
+        let Some(package) = self.resolver.get_package_object(&storage_id)? else {
             invariant_violation!("Missing dependent package in store: {storage_id}",)
         };
 
@@ -269,7 +247,7 @@ impl<'state> LinkageView<'state> {
             module_name,
             datatype_name: struct_name,
             package,
-        } in package.type_origin_table()
+        } in package.move_package().type_origin_table()
         {
             if module_name == runtime_id.name().as_str() && struct_name == struct_.as_str() {
                 self.add_type_origin(runtime_id.clone(), struct_.to_owned(), *package)?;
@@ -279,7 +257,7 @@ impl<'state> LinkageView<'state> {
 
         invariant_violation!(
             "{runtime_id}::{struct_} not found in type origin table in {storage_id} (v{})",
-            package.version(),
+            package.move_package().version(),
         )
     }
 }
@@ -314,23 +292,18 @@ impl LinkageResolver for LinkageView<'_> {
     }
 }
 
+// Remaining implementations delegated to state_view
+
 impl ModuleResolver for LinkageView<'_> {
     type Error = SuiError;
 
     fn get_module(&self, id: &ModuleId) -> Result<Option<Vec<u8>>, Self::Error> {
-        Ok(self
-            .get_package(&ObjectID::from(*id.address()))?
-            .and_then(|package| {
-                package
-                    .serialized_module_map()
-                    .get(id.name().as_str())
-                    .cloned()
-            }))
+        get_module(self, id)
     }
 }
 
-impl PackageStore for LinkageView<'_> {
-    fn get_package(&self, package_id: &ObjectID) -> SuiResult<Option<Rc<MovePackage>>> {
-        self.resolver.get_package(package_id)
+impl BackingPackageStore for LinkageView<'_> {
+    fn get_package_object(&self, package_id: &ObjectID) -> SuiResult<Option<PackageObject>> {
+        self.resolver.get_package_object(package_id)
     }
 }
