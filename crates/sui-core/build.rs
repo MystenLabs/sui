4e5483c0852a2ef36005171cf7db6ee82fb7ce74
andll <andll@danasoft.ws>
2025-05-23 15:42:03 -0700

    Add license

--- crates/sui-core/build.rs
+++ crates/sui-core/build.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 fn main() {
     // This build script enables tidehunter config used to gate tidehunter in sui codebase
     println!("cargo::rerun-if-env-changed=USE_TIDEHUNTER");
