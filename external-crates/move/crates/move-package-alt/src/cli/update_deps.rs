962246f8f5c1f24fffd84048c0dd464fe2fcf295
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-06-03 15:30:23 -0700

    WIP

--- external-crates/move/crates/move-package-alt/src/cli/update_deps.rs
+++ external-crates/move/crates/move-package-alt/src/cli/update_deps.rs
@@ -19,15 +19,20 @@ pub struct UpdateDeps {
     /// Path to the project
     #[arg(name = "path", short = 'p', long = "path", default_value = ".")]
     path: Option<PathBuf>,
+
+    #[arg(name = "environment", short = 'e', long = "environment")]
+    environment: Option<String>,
 }
 
 impl UpdateDeps {
     pub async fn execute(&self) -> PackageResult<()> {
         let path = PackagePath::new(self.path.clone().unwrap_or_else(|| PathBuf::from(".")))?;
-        let manifest = Manifest::<Vanilla>::read_from_file(&path.manifest_path())?;
 
-        let envs = manifest.environments();
-        let package_graph = PackageGraph::<Vanilla>::load(&path, envs.clone()).await?;
+        let package = Package::<Vanilla>::load_root(&path.path()).await?;
+        let envs = package.manifest().environments();
+
+        // update the lockfile
+        // let package_graph = PackageGraph::<Vanilla>::load(&path, envs.clone()).await?;
 
         Ok(())
     }
