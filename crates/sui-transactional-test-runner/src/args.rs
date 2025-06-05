60e2ea1e68378f177e8168a23399a8a9812d4148
Jort <jordan.jennings@mystenlabs.com>
2025-06-05 14:57:59 -0700

    Extra entry, gas, upgrade and publish transactional tests (#22238)
    ## Description 
    
    Filling out the last tests for #20109 
    
    - extra gas testing, checking smashing and deletion rebates are
    calculated at the correct time during dry run and added compute heavy
    transaction (along side the storage heavy transaction)
    - basic pass/fail upgrade test for dry run and normal execution
    - small fix for upgrade dry_run option in test framework
    - basic pass/fail publish test dry run and normal execution
    - dry_run argument for publish in test framework
    - tests for `InvalidArgumentToPrivateEntryFunction` for dry and dev inspect
    
    
    ## Test plan 
    
    Transactional test runner
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/sui-transactional-test-runner/src/args.rs
+++ crates/sui-transactional-test-runner/src/args.rs
@@ -47,6 +47,8 @@ pub struct SuiPublishArgs {
     pub dependencies: Vec<String>,
     #[clap(long = "gas-price")]
     pub gas_price: Option<u64>,
+    #[clap(long = "dry-run")]
+    pub dry_run: bool,
 }
 
 #[derive(Debug, clap::Parser)]
