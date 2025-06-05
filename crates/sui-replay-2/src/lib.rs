0771c5497af2bf02696189f1d3dbd58e77c830e5
Dario Russi <113150618+dariorussi@users.noreply.github.com>
2025-06-05 16:28:20 -0500

    Replay tool over the new schema (#22307)
    _[notice] it may be easier to review the files entirely as it is a
    significant change from what was there_
    
    ## Description 
    
    This is the "rewrite" of the replay tool to work against the new GQL
    schema.
    Things look pretty good. I cannot find yet a transaction that fails and
    we will see once we replay (and redefine) the interesting transactions.
    I have run about 2K transaction a mix of system and non system
    transactions (`SELECT transaction_digest FROM transaction_parquet WHERE
    date(timestamp_ms) >= current_date - 14 AND transaction_count> 20 AND
    move_calls > 10 AND is_system_txn = FALSE AND created > 0 AND deleted >
    0 AND mutated > 0` and sevaral variation of `SELECT transaction_digest
    FROM transaction_parquet WHERE date(timestamp_ms) >= current_date - 14
    AND is_system_txn = TRUE AND transaction_kind !=
    'ConsensusCommitPrologueV3' AND transaction_kind !=
    'RandomnessStateUpdate'`.
    I have seen one failure because of limits reached by the GQL but we'll
    look how to solve those at a later time.
    @awelc I moved the tracing code in its own file, it should be
    straightforward. Also I changed the default directory to be .replay.
    Next steps are interesting transactions and building the cache for local
    replay (no network access)
    
    ## Test plan 
    
    manual for now, the interesting transactions (or likely a subset of it)
    will be the test code later
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    
    ---------
    
    Co-authored-by: Adam Welc <adam@mystenlabs.com>

--- crates/sui-replay-2/src/lib.rs
+++ crates/sui-replay-2/src/lib.rs
@@ -2,17 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use clap::Parser;
-use similar::{ChangeTag, TextDiff};
+use std::path::PathBuf;
 use std::str::FromStr;
-use sui_types::{effects::TransactionEffects, supported_protocol_versions::Chain};
+use sui_types::supported_protocol_versions::Chain;
 
 pub mod data_store;
-pub mod environment;
-pub mod errors;
 pub mod execution;
 pub mod gql_queries;
+pub mod replay_interface;
 pub mod replay_txn;
+pub mod tracing;
 
+/// Arguments to the replay tool.
+/// It allows to replay a single transaction by digest or
+/// a file containing multiple digests, one per line.
+/// This may evolve to something very different in time and
+/// it's not meant to be stable.
+/// The options available are very convenient for the current
+/// development cycle.
 #[derive(Parser, Clone, Debug)]
 #[clap(
     name = "Sui Replay Tool",
@@ -20,28 +27,34 @@ pub mod replay_txn;
     rename_all = "kebab-case"
 )]
 pub struct ReplayConfig {
+    /// Transaction digest to replay.
+    #[arg(long, short)]
+    pub digest: Option<String>,
+    /// File containing a list of digest, one per line.
+    #[arg(long)]
+    pub digests_path: Option<PathBuf>,
     /// RPC of the fullnode used to replay the transaction.
     #[arg(long, short, default_value = "mainnet")]
     pub node: Node,
-    /// Transaction digest to replay.
-    #[arg(long, short)]
-    pub tx_digest: String,
     /// Show transaction effects.
     #[arg(long, short, default_value = "false")]
     pub show_effects: bool,
     /// Verify transaction execution matches what was executed on chain.
     #[arg(long, short, default_value = "false")]
     pub verify: bool,
-    // Enable tracing for tests
-    #[arg(long = "trace-execution", default_value = None)]
-    pub trace_execution: Option<Option<String>>,
+    /// Provide a directory to collect tracing. Or defaults to `<cur_dir>/.replay/<digest>`
+    #[arg(long = "trace", default_value = None)]
+    pub trace: Option<Option<PathBuf>>,
 }
 
+/// Enum around rpc gql endpoints.
 #[derive(Clone, Debug)]
 pub enum Node {
     Mainnet,
-    Testnet,
-    Devnet,
+    // TODO: define once we have stable end points.
+    //       Use `Custom` for now.
+    // Testnet,
+    // Devnet,
     Custom(String),
 }
 
@@ -49,8 +62,8 @@ impl Node {
     pub fn chain(&self) -> Chain {
         match self {
             Node::Mainnet => Chain::Mainnet,
-            Node::Testnet => Chain::Testnet,
-            Node::Devnet => Chain::Unknown,
+            // Node::Testnet => Chain::Testnet,
+            // Node::Devnet => Chain::Unknown,
             Node::Custom(_) => Chain::Unknown,
         }
     }
@@ -62,31 +75,9 @@ impl FromStr for Node {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "mainnet" => Ok(Node::Mainnet),
-            "testnet" => Ok(Node::Testnet),
-            "devnet" => Ok(Node::Devnet),
+            // "testnet" => Ok(Node::Testnet),
+            // "devnet" => Ok(Node::Devnet),
             _ => Ok(Node::Custom(s.to_string())),
         }
     }
-}
-
-/// Utility to diff effects in a human readable format
-pub fn diff_effects(
-    expected_effect: &TransactionEffects,
-    txn_effects: &TransactionEffects,
-) -> String {
-    let expected = format!("{:#?}", expected_effect);
-    let result = format!("{:#?}", txn_effects);
-    let mut res = vec![];
-
-    let diff = TextDiff::from_lines(&expected, &result);
-    for change in diff.iter_all_changes() {
-        let sign = match change.tag() {
-            ChangeTag::Delete => "---",
-            ChangeTag::Insert => "+++",
-            ChangeTag::Equal => "   ",
-        };
-        res.push(format!("{}{}", sign, change));
-    }
-
-    res.join("")
 }
