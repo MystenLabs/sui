703e6abf37cde6c0cd146db051fe28d94bc32d09
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2025-06-09 17:41:07 -0700

    [Cherry-pick] Increase utilization target and burst limit

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -3684,6 +3684,18 @@ impl ProtocolConfig {
                     cfg.feature_flags
                         .record_consensus_determined_version_assignments_in_prologue_v2 = true;
                     cfg.feature_flags.disallow_self_identifier = true;
+
+                    cfg.feature_flags.per_object_congestion_control_mode =
+                        PerObjectCongestionControlMode::ExecutionTimeEstimate(
+                            ExecutionTimeEstimateParams {
+                                target_utilization: 50,
+                                allowed_txn_cost_overage_burst_limit_us: 500_000, // 500 ms
+                                randomness_scalar: 20,
+                                max_estimate_us: 1_500_000, // 1.5s
+                                stored_observations_num_included_checkpoints: 10,
+                                stored_observations_limit: 20,
+                            },
+                        );
                 }
                 // Use this template when making changes:
                 //
