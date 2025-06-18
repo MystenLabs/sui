57c2575115ae3a72eb9c6f65d13dca23b081f706
Nick <nickvikeras@gmail.com>
2025-06-18 14:14:52 -0500

    Add license comment

--- crates/sui-rpc-api/src/grpc/v2alpha/get_package.rs
+++ crates/sui-rpc-api/src/grpc/v2alpha/get_package.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::{
     proto::google::rpc::bad_request::FieldViolation,
     proto::rpc::v2alpha::{GetPackageRequest, GetPackageResponse, Module, Package},
