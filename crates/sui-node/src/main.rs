ee9596150c743065c7e8c948de37500b8986cbb5
John Naulty <jnaulty@mystenlabs.com>
2025-06-26 14:48:36 +0300

    feat: conditionally use jemalloc on non-MSVC targets
    Fixes Windows build issues by excluding jemalloc on MSVC while
    consolidating dependencies on tikv-jemalloc ecosystem.

--- crates/sui-node/src/main.rs
+++ crates/sui-node/src/main.rs
@@ -43,6 +43,7 @@ struct Args {
     run_with_range_checkpoint: Option<CheckpointSequenceNumber>,
 }
 
+#[cfg(not(target_env = "msvc"))]
 #[global_allocator]
 static JEMALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
