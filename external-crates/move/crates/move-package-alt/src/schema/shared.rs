700a4460af322c9678d1612eeb74f423d65695a1
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-06-28 20:50:12 -0700

    [pkg-alt] Refactor published and original ids into their own types (#22538)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- external-crates/move/crates/move-package-alt/src/schema/shared.rs
+++ external-crates/move/crates/move-package-alt/src/schema/shared.rs
@@ -1,10 +1,16 @@
-use std::path::PathBuf;
+use std::{fmt::Debug, fmt::Display, path::PathBuf};
 
 use move_core_types::{account_address::AccountAddress, identifier::Identifier};
 use serde::{Deserialize, Serialize};
 
 pub type EnvironmentName = String;
 pub type PackageName = Identifier;
+
+#[derive(Clone, Deserialize)]
+pub struct PublishedID(pub AccountAddress);
+
+#[derive(Clone, Deserialize)]
+pub struct OriginalID(pub AccountAddress);
 
 /// A serialized dependency of the form `{ local = <path> }`
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
@@ -42,25 +48,53 @@ impl From<ConstTrue> for bool {
     }
 }
 
-/// Serialize an `AccountAddress` as a hex literal string, which will have the 0x prefix.
-pub fn ser_account<S>(account: &AccountAddress, serializer: S) -> Result<S::Ok, S::Error>
+/// Serialize an `AccountAddress` to its canonical string representation
+fn ser_account<S>(account: &AccountAddress, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: serde::Serializer,
 {
-    let canonical_string = account.to_hex_literal();
+    let canonical_string = account.to_canonical_string(true);
     serializer.serialize_str(&canonical_string)
 }
 
-pub fn ser_opt_account<S>(
-    account: &Option<AccountAddress>,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    if let Some(account) = account {
-        ser_account(account, serializer)
-    } else {
-        serializer.serialize_none()
+impl Serialize for OriginalID {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        ser_account(&self.0, serializer)
+    }
+}
+
+impl Serialize for PublishedID {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        ser_account(&self.0, serializer)
+    }
+}
+
+impl Display for PublishedID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0.to_canonical_string(true))
+    }
+}
+
+impl Debug for PublishedID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0.to_canonical_string(true))
+    }
+}
+
+impl Display for OriginalID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0.to_canonical_string(true))
+    }
+}
+
+impl Debug for OriginalID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0.to_canonical_string(true))
     }
 }
