209b1f66551148f40f25f171046c80408645ab6e
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-06-30 09:54:42 -0700

    [pkg-alt] Add some testing infrastructure around git repos (#22527)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

---
+++ external-crates/move/crates/move-package-alt/src/test_utils/git.rs
@@ -0,0 +1,227 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copied and adapted from
+// <https://github.com/rust-lang/cargo/tree/master/crates/cargo-test-support/src> at SHA
+// 4ac865d3d7b62281ad4dcb92406c816b6f1aeceb
+
+//! # Git Testing Support
+//!
+//! ## Creating a git dependency
+//! [`new()`] is an easy way to create a new git repository containing a
+//! project that you can then use as a dependency. It will automatically add all
+//! the files you specify in the project and commit them to the repository.
+//!
+//! ### Example:
+//!
+//! ```no_run
+//! let git_project = git::new("dep1", |project| {
+//!     project
+//!         .file("Move.toml", &basic_manifest("dep1", "1.0.0"))
+//!         .file("sources/dep1.move", r#"module dep1::dep1 { public fun f() { } }"#)
+//! });
+//!
+//! // Use the `root()` or `root_path()` method to get the FS path to the new repository.
+//! let p = project()
+//!     .file("Move.toml", &format!(r#"
+//!         [package]
+//!         name = "a"
+//!         version = "1.0.0"
+//!         edition = "2025"
+//!
+//!         [dependencies]
+//!         dep1 = {{ git = '{}' }}
+//!     "#, git_project.root_path()))
+//!     .file("sources/a.move", "module a::a { public fun t() { dep1::f(); } }")
+//!     .build();
+//! ```
+//!
+//! ## Manually creating repositories
+//!
+//! [`repo()`] can be used to create a [`RepoBuilder`] which provides a way of
+//! adding files to a blank repository and committing them.
+//!
+//! If you want to then manipulate the repository (such as adding new files or
+//! tags), you can use `git2::Repository::open()` to open the repository and then
+//! use some of the helper functions in this file to interact with the repository.
+
+use std::fs;
+use std::path::{Path, PathBuf};
+use url::Url;
+
+use crate::t;
+use crate::test_utils::panic_error;
+use crate::test_utils::paths::*;
+use crate::test_utils::*;
+use git2::RepositoryInitOptions;
+use std::env::consts;
+
+/// Manually construct a [`Repository`]
+///
+/// See also [`new`], [`repo`]
+#[must_use]
+pub struct RepoBuilder {
+    repo: git2::Repository,
+    files: Vec<PathBuf>,
+}
+
+/// See [`new`]
+pub struct Repository(git2::Repository);
+
+/// Create a [`RepoBuilder`] to build a new git repository.
+///
+/// Call [`RepoBuilder::build()`] to finalize and create the repository.
+pub fn repo(p: &Path) -> RepoBuilder {
+    RepoBuilder::init(p)
+}
+
+impl RepoBuilder {
+    pub fn init(p: &Path) -> RepoBuilder {
+        t!(fs::create_dir_all(p.parent().unwrap()));
+        let repo = init(p);
+        RepoBuilder {
+            repo,
+            files: Vec::new(),
+        }
+    }
+
+    /// Add a file to the repository.
+    pub fn file(self, path: &str, contents: &str) -> RepoBuilder {
+        let mut me = self.nocommit_file(path, contents);
+        me.files.push(PathBuf::from(path));
+        me
+    }
+
+    /// Add a file that will be left in the working directory, but not added
+    /// to the repository.
+    pub fn nocommit_file(self, path: &str, contents: &str) -> RepoBuilder {
+        let dst = self.repo.workdir().unwrap().join(path);
+        t!(fs::create_dir_all(dst.parent().unwrap()));
+        t!(fs::write(&dst, contents));
+        self
+    }
+
+    /// Create the repository and commit the new files.
+    pub fn build(self) -> Repository {
+        {
+            let mut index = t!(self.repo.index());
+            for file in self.files.iter() {
+                t!(index.add_path(file));
+            }
+            t!(index.write());
+            let id = t!(index.write_tree());
+            let tree = t!(self.repo.find_tree(id));
+            let sig = t!(self.repo.signature());
+            t!(self
+                .repo
+                .commit(Some("HEAD"), &sig, &sig, "Initial commit", &tree, &[]));
+        }
+        let RepoBuilder { repo, .. } = self;
+        Repository(repo)
+    }
+}
+
+impl Repository {
+    pub fn root(&self) -> &Path {
+        self.0.workdir().unwrap()
+    }
+
+    pub fn url(&self) -> Url {
+        self.0.workdir().unwrap().to_url()
+    }
+
+    pub fn revparse_head(&self) -> String {
+        self.0
+            .revparse_single("HEAD")
+            .expect("revparse HEAD")
+            .id()
+            .to_string()
+    }
+}
+
+/// *(`git2`)* Initialize a new repository at the given path.
+pub fn init(path: &Path) -> git2::Repository {
+    // default_search_path();
+    let mut opts = RepositoryInitOptions::new();
+    opts.initial_head("main");
+    let repo = t!(git2::Repository::init_opts(path, &opts));
+    default_repo_cfg(&repo);
+    repo
+}
+
+fn default_repo_cfg(repo: &git2::Repository) {
+    let mut cfg = t!(repo.config());
+    t!(cfg.set_str("user.email", "foo@bar.com"));
+    t!(cfg.set_str("user.name", "Foo Bar"));
+}
+
+/// Create a new [`Project`] in a git [`Repository`]
+pub fn new<F>(name: &str, callback: F) -> Project
+where
+    F: FnOnce(ProjectBuilder) -> ProjectBuilder,
+{
+    new_repo(name, callback).0
+}
+
+/// Create a new [`Project`] with access to the [`Repository`]
+pub fn new_repo<F>(name: &str, callback: F) -> (Project, git2::Repository)
+where
+    F: FnOnce(ProjectBuilder) -> ProjectBuilder,
+{
+    let mut git_project = project().at(name);
+    git_project = callback(git_project);
+    let git_project = git_project.build();
+
+    let repo = init(&git_project.root());
+    add(&repo);
+    commit(&repo);
+    (git_project, repo)
+}
+
+/// *(`git2`)* Add all files in the working directory to the git index
+pub fn add(repo: &git2::Repository) {
+    let mut index = t!(repo.index());
+    t!(index.add_all(["*"].iter(), git2::IndexAddOption::DEFAULT, None));
+    t!(index.write());
+}
+
+/// *(`git2`)* Commit changes to the git repository
+pub fn commit(repo: &git2::Repository) -> git2::Oid {
+    let tree_id = t!(t!(repo.index()).write_tree());
+    let sig = t!(repo.signature());
+    let mut parents = Vec::new();
+    if let Some(parent) = repo.head().ok().map(|h| h.target().unwrap()) {
+        parents.push(t!(repo.find_commit(parent)))
+    }
+    let parents = parents.iter().collect::<Vec<_>>();
+    t!(repo.commit(
+        Some("HEAD"),
+        &sig,
+        &sig,
+        "test",
+        &t!(repo.find_tree(tree_id)),
+        &parents
+    ))
+}
+
+/// *(`git2`)* Create a new tag in the git repository
+pub fn tag(repo: &git2::Repository, name: &str) {
+    let head = repo.head().unwrap().target().unwrap();
+    t!(repo.tag(
+        name,
+        &t!(repo.find_object(head, None)),
+        &t!(repo.signature()),
+        "make a new tag",
+        false
+    ));
+}
+
+// / *(`git2`)* Get all commits in the repository, starting from HEAD
+pub fn commits(repo: &git2::Repository) -> Vec<git2::Commit> {
+    let mut revwalk = t!(repo.revwalk());
+    t!(revwalk.push_head());
+    revwalk
+        .map(|oid| t!(repo.find_commit(oid.unwrap())))
+        .collect()
+}
---
+++ external-crates/move/crates/move-package-alt/src/test_utils/mod.rs
@@ -0,0 +1,239 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copied and adapted from
+// <https://github.com/rust-lang/cargo/tree/master/crates/cargo-test-support/src> at SHA
+// 4ac865d3d7b62281ad4dcb92406c816b6f1aeceb
+
+pub mod git;
+mod paths;
+
+use paths::PathExt;
+use paths::root;
+use std::env;
+use std::ffi::OsStr;
+use std::fmt::Write;
+use std::fs;
+use std::os;
+use std::path::Path;
+use std::path::PathBuf;
+
+#[macro_export]
+macro_rules! t {
+    ($e:expr) => {
+        match $e {
+            Ok(e) => e,
+            Err(e) => panic_error(&format!("failed running {}", stringify!($e)), e),
+        }
+    };
+}
+
+/// `panic!`, reporting the specified error , see also [`t!`]
+#[track_caller]
+pub fn panic_error(what: &str, err: impl Into<anyhow::Error>) -> ! {
+    let err = err.into();
+    pe(what, err);
+    #[track_caller]
+    fn pe(what: &str, err: anyhow::Error) -> ! {
+        let mut result = format!("{}\nerror: {}", what, err);
+        for cause in err.chain().skip(1) {
+            let _ = writeln!(result, "\nCaused by:");
+            let _ = write!(result, "{}", cause);
+        }
+        panic!("\n{}", result);
+    }
+}
+#[derive(PartialEq, Clone)]
+struct FileBuilder {
+    path: PathBuf,
+    body: String,
+    executable: bool,
+}
+
+impl FileBuilder {
+    pub fn new(path: PathBuf, body: &str, executable: bool) -> FileBuilder {
+        FileBuilder {
+            path,
+            body: body.to_string(),
+            executable,
+        }
+    }
+
+    fn mk(&mut self) {
+        if self.executable {
+            let mut path = self.path.clone().into_os_string();
+            write!(path, "{}", env::consts::EXE_SUFFIX).unwrap();
+            self.path = path.into();
+        }
+
+        self.dirname().mkdir_p();
+        fs::write(&self.path, &self.body)
+            .unwrap_or_else(|e| panic!("could not create file {}: {}", self.path.display(), e));
+
+        #[cfg(unix)]
+        if self.executable {
+            use std::os::unix::fs::PermissionsExt;
+
+            let mut perms = fs::metadata(&self.path).unwrap().permissions();
+            let mode = perms.mode();
+            perms.set_mode(mode | 0o111);
+            fs::set_permissions(&self.path, perms).unwrap();
+        }
+    }
+
+    fn dirname(&self) -> &Path {
+        self.path.parent().unwrap()
+    }
+}
+
+/// A directory to run tests against.
+///
+/// See [`ProjectBuilder`] to get started.
+#[derive(Debug)]
+pub struct Project {
+    root: PathBuf,
+}
+
+/// Create a project to run tests against but you need to add a manifest either via basic_manifest
+/// or through your own content.
+///
+/// To get started, see:
+/// - [`project`]
+#[must_use]
+pub struct ProjectBuilder {
+    root: Project,
+    files: Vec<FileBuilder>,
+}
+
+impl ProjectBuilder {
+    /// Root of the project
+    pub fn root(&self) -> PathBuf {
+        self.root.root()
+    }
+
+    /// Create project in `root`
+    pub fn new(root: PathBuf) -> ProjectBuilder {
+        ProjectBuilder {
+            root: Project { root },
+            files: vec![],
+        }
+    }
+
+    /// Create project, relative to [`paths::root`]
+    pub fn at<P: AsRef<Path>>(mut self, path: P) -> Self {
+        self.root = Project {
+            root: root().join(path),
+        };
+        self
+    }
+
+    /// Adds a file to the project.
+    pub fn file<B: AsRef<Path>>(mut self, path: B, body: &str) -> Self {
+        self._file(path.as_ref(), body, false);
+        self
+    }
+
+    /// Adds an executable file to the project.
+    pub fn executable<B: AsRef<Path>>(mut self, path: B, body: &str) -> Self {
+        self._file(path.as_ref(), body, true);
+        self
+    }
+
+    fn _file(&mut self, path: &Path, body: &str, executable: bool) {
+        self.files.push(FileBuilder::new(
+            self.root.root().join(path),
+            body,
+            executable,
+        ));
+    }
+
+    /// Creates the project.
+    pub fn build(mut self) -> Project {
+        // First, clean the directory if it already exists
+        self.rm_root();
+
+        // Create the empty directory
+        self.root.root().mkdir_p();
+
+        for file in self.files.iter_mut() {
+            file.mk();
+        }
+
+        let ProjectBuilder { root, .. } = self;
+        root
+    }
+
+    fn rm_root(&self) {
+        self.root.root().rm_rf()
+    }
+}
+
+impl Project {
+    /// Try to get the git commits in the project, but it will panic if this is not a git
+    /// repository.
+    pub fn commits(&self) -> Vec<String> {
+        let repo = git2::Repository::open(self.root.clone())
+            .unwrap_or_else(|_| panic!("failed to open git repository at {}", self.root.display()));
+        git::commits(&repo)
+            .into_iter()
+            .map(|c| c.id().to_string())
+            .collect()
+    }
+
+    /// Root of the project
+    pub fn root(&self) -> PathBuf {
+        self.root.clone()
+    }
+
+    /// Root of the project as a string. This will panic if root does not exist.
+    pub fn root_path(&self) -> &str {
+        self.root.to_str().unwrap()
+    }
+
+    /// Overwrite a file with new content
+    pub fn change_file(&self, path: impl AsRef<Path>, body: &str) {
+        FileBuilder::new(self.root().join(path), body, false).mk()
+    }
+
+    /// Returns the contents of `Move.lock`.
+    pub fn read_lockfile(&self) -> String {
+        self.read_file("Move.lock")
+    }
+
+    /// Returns the contents of a path in the project root
+    pub fn read_file(&self, path: impl AsRef<Path>) -> String {
+        let full = self.root().join(path);
+        fs::read_to_string(&full)
+            .unwrap_or_else(|e| panic!("could not read file {}: {}", full.display(), e))
+    }
+
+    /// Modifies `Move.toml` to remove all commented lines.
+    pub fn uncomment_root_manifest(&self) {
+        let contents = self.read_file("Move.toml").replace("#", "");
+        fs::write(self.root().join("Move.toml"), contents).unwrap();
+    }
+}
+
+/// Generates a project layout, see [`ProjectBuilder`]
+pub fn project() -> ProjectBuilder {
+    ProjectBuilder::new(root().join("foo"))
+}
+
+/// Generate a basic `Move.toml` content
+pub fn basic_manifest(name: &str, version: &str) -> String {
+    format!(
+        r#"
+        [package]
+        name = "{}"
+        version = "{}"
+        authors = []
+        edition = "2025"
+
+        [environments]
+        mainnet = "35834a8a"
+        testnet = "4c78adac"
+    "#,
+        name, version
+    )
+}
