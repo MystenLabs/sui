16cc383d574830edc98a2004a2f8122e0adff46f
Ronny Roland <ronmilton@mystenlabs.com>
2025-07-02 14:05:19 -0600

    updated styling

--- docs/site/src/components/Search/index.tsx
+++ docs/site/src/components/Search/index.tsx
@@ -12,6 +12,7 @@ import {
   Index,
   Pagination,
 } from "react-instantsearch";
+import { useHistory } from "@docusaurus/router";
 
 const { decode } = require("he");
 
@@ -37,17 +38,33 @@ function getDeepestHierarchyLabel(hierarchy) {
   return lastValue || hierarchy.lvl6 || "";
 }
 
-function CustomHitsContent() {
+function CustomHitsContent({ name }) {
   const { hits: items } = useHits();
+  const history = useHistory();
+  const currentHost = typeof window !== "undefined" ? window.location.host : "";
+
+  let siteToVisit = "Try your search again with different keywords";
+  if (name === "sui_docs") {
+    siteToVisit = `${siteToVisit}. If you are unable to find the information you need, try one of the official Sui support channels: <a href="https://github.com/MystenLabs/sui/issues/new/choose" target="_blank">GitHub</a>, <a href="https://discord.gg/Sui" target="_blank">Discord</a>, or <a href="https://t.me/SuiTokenNetwork" target="_blank">Telegram</a>.`;
+  } else if (name === "suins_docs") {
+    siteToVisit = `${siteToVisit} or visit the official <a href="https://docs.suins.io" target="_blank">SuiNS doc</a> site.`;
+  } else if (name === "move_book") {
+    siteToVisit = `${siteToVisit} or visit <a href="https://move-book.com/" target="_blank">The Move Book</a> dedicated site.`;
+  } else if (name === "dapp_kit") {
+    siteToVisit = `${siteToVisit} or visit the official <a href="https://sdk.mystenlabs.com/dapp-kit" target="_blank">dApp Kit</a> site.`;
+  } else {
+    siteToVisit = `${siteToVisit}.`;
+  }
 
   if (items.length === 0) {
     return (
       <>
         <p>No results found.</p>
-        <p>
-          Try your search again with different keywords or visit such and such
-          site.
-        </p>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: `${siteToVisit}`,
+          }}
+        />
       </>
     );
   }
@@ -66,27 +83,50 @@ function CustomHitsContent() {
     <>
       {Object.entries(grouped).map(([key, group], index) => {
         return (
-          <div className="border border-solid p-4 mb-4 rounded-lg" key={index}>
-            <div className="font-bold text-left w-full">
+          <div
+            className="mb-8 pb-8 border-2 border-solid border-transparent border-b-sui-gray-50 dark:border-b-sui-gray-80"
+            key={index}
+          >
+            <div className="text-2xl text-sui-gray-80 text-bold mb-2">
               {group[0].hierarchy?.lvl1 || "[no title]"}
             </div>
-            {group.map((hit, i) => {
-              const level = hit.type;
-              let sectionTitle = hit.lvl0;
-              if (level === "content") {
-                sectionTitle = getDeepestHierarchyLabel(hit.hierarchy);
-              } else {
-                sectionTitle = hit.hierarchy?.[level] || level;
-              }
-              return (
-                <div key={i} className="mb-2">
-                  <a href={hit.url} className="text-sm text-blue-600 underline">
-                    {sectionTitle}
-                  </a>
-                  <p>{hit.content ? truncateAtWord(hit.content) : ""}</p>
-                </div>
-              );
-            })}
+            <div className="ml-4">
+              {group.map((hit, i) => {
+                const level = hit.type;
+                let sectionTitle = hit.lvl0;
+                if (level === "content") {
+                  sectionTitle = getDeepestHierarchyLabel(hit.hierarchy);
+                } else {
+                  sectionTitle = hit.hierarchy?.[level] || level;
+                }
+
+                const hitHost = new URL(hit.url).host;
+                const isInternal = hitHost === currentHost;
+
+                return (
+                  <div key={i} className="mb-2">
+                    {isInternal ? (
+                      <button
+                        onClick={() => history.push(new URL(hit.url).pathname)}
+                        className="text-lg text-blue-600 underline text-left"
+                      >
+                        {sectionTitle}
+                      </button>
+                    ) : (
+                      <a
+                        href={hit.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-lg text-blue-600 underline"
+                      >
+                        {sectionTitle}
+                      </a>
+                    )}
+                    <p>{hit.content ? truncateAtWord(hit.content) : ""}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         );
       })}
@@ -102,17 +142,34 @@ function getQueryParam(key) {
 }
 
 function TabbedResults({ activeTab, onChange, tabs }) {
+  const suitooltip = "Search results from the official Sui Docs";
+  const suinstooltip = "Search results from Sui Name Service";
+  const movetooltip = "Search results from The Move Book";
+  const dapptooltip = "Search results from the Sui dApp Kit (TypeScript SDK)";
   return (
-    <div className="mb-4 flex justify-start">
+    <div className="mb-4 flex justify-start border-2 border-solid border-white dark:border-sui-black border-b-sui-gray-50 dark:border-b-sui-gray-80">
       {tabs.map(({ label, indexName, count }) => (
-        <button
-          key={indexName}
-          className="mr-4 text-lg border-none bg-white"
-          onClick={() => onChange(indexName)}
-          active={activeTab === indexName}
-        >
-          {label} | <span className="text-sm">{count} hits</span>
-        </button>
+        <div className="relative group inline-block">
+          <button
+            key={indexName}
+            className={`mr-4 text-sm lg:text-md xl:text-lg bg-white dark:bg-sui-black cursor-pointer ${activeTab === indexName ? "text-sui-blue border-2 border-solid border-transparent border-b-sui-blue-dark dark:border-b-sui-blue" : "border-transparent"}`}
+            onClick={() => onChange(indexName)}
+          >
+            {label}{" "}
+            <span className="text-xs bg-sui-gray-50 dark:bg-sui-gray-80 rounded-full py-1 px-2">
+              {count}
+            </span>
+          </button>
+          <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max max-w-xs px-2 py-1 text-sm text-white bg-gray-800 rounded tooltip-delay">
+            {label === "Sui"
+              ? suitooltip
+              : label === "SuiNS"
+                ? suinstooltip
+                : label === "The Move Book and Reference"
+                  ? movetooltip
+                  : dapptooltip}
+          </div>
+        </div>
       ))}
     </div>
   );
@@ -133,27 +190,39 @@ function IndexStatsCollector({
 }
 
 function RefinementSection() {
-  const { nbHits } = useStats();
-
-  if (nbHits === 0) return null;
-
+  const { hits } = useHits();
+  if (hits.length === 0) return null;
   return (
-    <div className="py-4 border border-solid rounded-lg mb-4">
-      <h2 className="pl-4 text-lg">Filter by category</h2>
+    <div className="bg-sui-gray-50 dark:bg-sui-gray-80 p-4 sticky top-20 z-10">
+      <h2 className="text-lg">Refine results</h2>
       <RefinementList attribute="source" />
     </div>
   );
 }
 
+function ConditionalPagination() {
+  const { hits } = useHits();
+  if (hits.length === 0) return null;
+  return <Pagination />;
+}
+
 function TabbedIndex({ indexName }) {
-  const { nbHits } = useStats();
-  console.log(nbHits);
-  if (nbHits === 0) return null;
+  const { hits } = useHits();
   return (
     <Index indexName={indexName}>
-      <RefinementSection />
-      <CustomHitsContent />
-      <Pagination />
+      <div className="grid grid-cols-12 gap-4">
+        {hits.length > 0 && (
+          <div className="col-span-12 md:col-span-4 xl:col-span-3">
+            <RefinementSection />
+          </div>
+        )}
+        <div className="col-span-12 md:col-span-8 xl:col-span-9">
+          <CustomHitsContent name={indexName} />
+        </div>
+        <div className="col-span-12">
+          <ConditionalPagination />
+        </div>
+      </div>
     </Index>
   );
 }
@@ -171,10 +240,10 @@ export default function Search() {
   });
 
   const tabs = [
-    { label: "Sui docs", indexName: "sui_docs" },
-    { label: "SuiNS docs", indexName: "suins_docs" },
+    { label: "Sui", indexName: "sui_docs" },
+    { label: "SuiNS", indexName: "suins_docs" },
     { label: "The Move Book and Reference", indexName: "move_book" },
-    { label: "SDK docs", indexName: "dapp_kit" },
+    { label: "dApp Kit", indexName: "dapp_kit" },
   ];
 
   const handleVisibility = React.useCallback(
@@ -208,7 +277,7 @@ export default function Search() {
 
       <div className="grid grid-cols-12 gap-4 sui-search">
         <div className="col-span-12">
-          <SearchBox />
+          <SearchBox placeholder="Start typing to begin your search..." />
         </div>
         <div className="col-span-12">
           <TabbedResults
@@ -224,9 +293,7 @@ export default function Search() {
           {tabs.map((tab) => (
             <div
               key={tab.indexName}
-              style={{
-                display: activeTab === tab.indexName ? "block" : "none",
-              }}
+              className={`flex ${activeTab === tab.indexName ? "block" : "hidden"}`}
             >
               <TabbedIndex indexName={tab.indexName} />
             </div>
