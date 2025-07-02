56e84f6dfd2c799033718a493ace85011749b19d
Nick <nickvikeras@gmail.com>
2025-07-02 15:25:37 -0500

    Revert "Static Link Jemalloc (#22517)"
    This reverts commit 2ff9193990a8d2cd6c160f6d391d8127b0f82c26.

--- crates/sui-node/src/main.rs
+++ crates/sui-node/src/main.rs
@@ -43,10 +43,6 @@ struct Args {
     run_with_range_checkpoint: Option<CheckpointSequenceNumber>,
 }
 
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static JEMALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
-
 fn main() {
     antithesis_sdk::antithesis_init();
 
