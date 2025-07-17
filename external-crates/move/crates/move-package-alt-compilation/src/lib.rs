4efd680a0341bbde038f41fcee0f4fe2770a175d
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-07-17 11:35:03 -0700

    Add compilaltion crate

---
+++ external-crates/move/crates/move-package-alt-compilation/src/lib.rs
@@ -0,0 +1,35 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{build_config::BuildConfig, build_plan::BuildPlan, compiled_package::CompiledPackage};
+use move_package_alt::{
+    errors::PackageResult, flavor::MoveFlavor, package::RootPackage, schema::Environment,
+};
+use std::{io::Write, path::Path};
+
+pub mod build_config;
+pub mod build_plan;
+pub mod compiled_package;
+pub mod layout;
+pub mod lint_flag;
+pub mod on_disk_package;
+
+pub async fn compile_package<W: Write, F: MoveFlavor>(
+    path: &Path,
+    build_config: &BuildConfig,
+    env: &Environment,
+    writer: &mut W,
+) -> PackageResult<CompiledPackage> {
+    let root_pkg = RootPackage::<F>::load(path, env.clone()).await?;
+    BuildPlan::create(root_pkg, build_config, env)?.compile(writer, |compiler| compiler)
+}
+
+pub async fn compile_from_root_package<W: Write, F: MoveFlavor>(
+    root_pkg: RootPackage<F>,
+    build_config: &BuildConfig,
+    env: &Environment,
+    writer: &mut W,
+) -> PackageResult<CompiledPackage> {
+    BuildPlan::create(root_pkg, build_config, env)?.compile(writer, |compiler| compiler)
+}
