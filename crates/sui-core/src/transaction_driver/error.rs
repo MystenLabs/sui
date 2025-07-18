1f90aa7d1a076f70297162afbcd3fb7d1ab446e1
Mingwei Tian <mingwei@mystenlabs.com>
2025-07-18 10:23:32 -0700

    [TD] add common components to retry submission and getting full effects

--- crates/sui-core/src/transaction_driver/error.rs
+++ crates/sui-core/src/transaction_driver/error.rs
@@ -2,33 +2,38 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use serde::{Deserialize, Serialize};
-use sui_types::error::SuiError;
+use sui_types::{committee::EpochId, error::SuiError};
 use thiserror::Error;
 
 /// Client facing errors regarding transaction submission via Transaction Driver.
 /// Every invariant needs detailed content to instruct client handling.
 #[derive(Eq, PartialEq, Clone, Debug, Serialize, Deserialize, Error, Hash)]
 pub enum TransactionDriverError {
-    #[error("Serialization error: {0}")]
-    SerializationError(SuiError),
-    #[error("Deserialization error: {0}")]
-    DeserializationError(SuiError),
+    // Errors against individual validators.
     #[error("Transaction timed out getting consensus position")]
-    TimeoutSubmittingTransaction,
-    #[error("Transaction timed out while acknowledging effects")]
-    TimeoutAcknowledgingEffects,
+    TimedOutSubmittingTransaction,
     #[error("Transaction timed out while getting full effects")]
-    TimeoutGettingFullEffects,
-    #[error("Failed to call validator {0}: {1}")]
-    RpcFailure(String, String),
+    TimedOutGettingFullEffectsAtValidator,
     #[error("Failed to find execution data: {0}")]
     ExecutionDataNotFound(String),
-    #[error("Transaction rejected with reason: {0}")]
-    TransactionRejected(String),
-    #[error("Transaction status expired at peer validator")]
-    TransactionStatusExpired,
-    #[error("Transaction rejected with reason: {0} & expired with reason: {1}")]
-    TransactionRejectedOrExpired(String, String),
+    #[error("Transaction rejected at peer validator")]
+    TransactionRejectedAtValidator(String),
+    #[error("Transaction status expired at peer validator, currently at epoch {0} round {1}")]
+    TransactionStatusExpired(EpochId, u32),
+    #[error("Validator internal error: {0}")]
+    ValidatorInternalError(SuiError),
+
+    // TODO(fastpath): after proper error aggregation, there errors should not exist.
+    #[error("No more targets to retry")]
+    NoMoreTargets,
+    #[error("Transaction driver failed after retries. See logs for details.")]
+    TransactionDriverFailure,
+
+    // TODO(fastpath): Move these aggregated errors to a different status.
+    #[error("Transaction rejected with stake {0} & reasons: {1}")]
+    TransactionRejected(u64, String),
+    #[error("Transaction rejected with stake {0} and expired with stake {1}, reasons: {2}")]
+    TransactionRejectedOrExpired(u64, u64, String),
     #[error("Forked execution results: total_responses_weight {total_responses_weight}, executed_weight {executed_weight}, rejected_weight {rejected_weight}, expired_weight {expired_weight}, Errors: {errors:?}")]
     ForkedExecution {
         total_responses_weight: u64,
