1eca5ef67775a1b04119e76b02967dbd9768e944
andll <andll@danasoft.ws>
2025-07-22 11:48:54 -0700

    fmt

--- crates/typed-store/src/tidehunter_util.rs
+++ crates/typed-store/src/tidehunter_util.rs
@@ -1,11 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::env;
 use crate::DBMetrics;
 use bincode::Options;
 use prometheus::Registry;
 use serde::de::DeserializeOwned;
+use std::env;
 use std::path::Path;
 use std::sync::Arc;
 use tidehunter::config::Config;
@@ -59,7 +59,10 @@ fn thdb_config() -> Config {
     let frag_size = if let Ok(frag_size) = env::var("TH_FRAG_SIZE") {
         let frag_size = frag_size.parse().expect("Failed to parse TH_FRAG_SIZE");
         assert!(frag_size > 0, "TH_FRAG_SIZE must be more then 0");
-        assert!(frag_size % (4 * 1024) == 0, "TH_FRAG_SIZE must be page aligned({frag_size} given)");
+        assert!(
+            frag_size % (4 * 1024) == 0,
+            "TH_FRAG_SIZE must be page aligned({frag_size} given)"
+        );
         println!("Using frag size from env variable {frag_size}");
         frag_size
     } else {
