e258b982e53b25e2f60b289f6f1d387c11dad111
Henry Chen <henry.chen@mystenlabs.com>
2025-07-22 19:33:10 -0700

    use data loader for cp_sequence_numbers

--- crates/sui-indexer-alt-graphql/src/api/types/transaction_bounds.rs
+++ crates/sui-indexer-alt-graphql/src/api/types/transaction_bounds.rs
@@ -1,15 +1,18 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use diesel::{ExpressionMethods, QueryDsl};
+use async_graphql::{dataloader::DataLoader, Context};
+use std::sync::Arc;
+use sui_indexer_alt_reader::cp_sequence_numbers::CpSequenceNumberKey;
 use sui_indexer_alt_reader::kv_loader::KvLoader;
-use sui_indexer_alt_reader::pg_reader::Connection;
+use sui_indexer_alt_reader::pg_reader::PgReader;
 
 use crate::api::types::checkpoint_bounds::CheckpointBounds;
 use crate::error::RpcError;
 use anyhow::Context as _;
-use sui_indexer_alt_schema::schema::cp_sequence_numbers;
 
+/// Bounds on transaction sequence number, imposed by filters. The outermost bounds are determined
+/// by the checkpoint filters. These get translated into bounds in terms of transaction sequence numbers:
 pub(crate) struct TransactionBounds {
     tx_lo: u64,
     tx_hi_exclusive: u64,
@@ -21,27 +24,23 @@ impl TransactionBounds {
     ///  - Queries the KV store for the upper checkpoint to determine the exclusive upper bound using the `network_total_transactions` field
     ///
     pub(crate) async fn fetch_transaction_bounds(
-        conn: &mut Connection<'_>,
-        kv_loader: &KvLoader,
+        ctx: &Context<'_>,
         checkpoint_bounds: CheckpointBounds,
     ) -> Result<TransactionBounds, RpcError> {
-        use cp_sequence_numbers::dsl as cp;
+        let pg_loader: &Arc<DataLoader<PgReader>> = ctx.data()?;
 
-        let cp_tx_lo_query = cp::cp_sequence_numbers
-            .select(cp::tx_lo)
-            .filter(cp::cp_sequence_number.eq(checkpoint_bounds.lower() as i64))
-            .limit(1);
-
-        let tx_lo_record: Vec<i64> = conn
-            .results(cp_tx_lo_query)
+        // Load the lower checkpoint sequence number data
+        let cp_sequence_lo = pg_loader
+            .load_one(CpSequenceNumberKey(checkpoint_bounds.lower()))
             .await
-            .context("Failed to query checkpoint sequence numbers")?;
+            .context("Failed to query checkpoint bounds")?;
 
-        let tx_lo = match tx_lo_record.first().copied() {
-            Some(val) => val,
+        let tx_lo = match cp_sequence_lo {
+            Some(stored_cp) => stored_cp.tx_lo as u64,
             None => return Err(anyhow::anyhow!("No valid lower checkpoint bound found").into()),
         };
 
+        let kv_loader: &KvLoader = ctx.data()?;
         let contents = kv_loader
             .load_one_checkpoint(checkpoint_bounds.upper())
             .await
@@ -49,16 +48,17 @@ impl TransactionBounds {
 
         // tx_hi_exclusive is the network_total_transactions of the highest checkpoint bound.
         let tx_hi_exclusive = if let Some((summary, _, _)) = contents.as_ref() {
-            summary.network_total_transactions as i64
+            summary.network_total_transactions as u64
         } else {
             return Err(anyhow::anyhow!("No valid upper tx upper bound found").into());
         };
 
         Ok(Self {
-            tx_lo: tx_lo as u64,
-            tx_hi_exclusive: tx_hi_exclusive as u64,
+            tx_lo,
+            tx_hi_exclusive,
         })
     }
+
     /// Get the lower tx bound
     pub(crate) fn lower(&self) -> u64 {
         self.tx_lo
