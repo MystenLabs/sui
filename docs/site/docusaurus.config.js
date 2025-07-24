9fcd4f04421d53786a252a74b5afd6f96313c01b
ronny-mysten <118224482+ronny-mysten@users.noreply.github.com>
2025-07-24 16:30:08 -0600

    [docs] Search (#22477)
    ## Description 
    
    Improves the full search experience. Adds move book, sdks, suins, and
    walrus to results.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -177,35 +177,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      algolia: {
-        // The application ID provided by Algolia
-        appId: "ZF283DJAYX",
-
-        // Public API key: it is safe to commit it
-        apiKey: "7f24db6c4ec06d6905592deb228f4460",
-
-        indexName: "sui",
-
-        // Optional: see doc section below
-        contextualSearch: false,
-
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        // externalUrlRegex: "external\\.com|domain\\.com",
-
-        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-        //replaceSearchResultPathname: {
-        //from: "/docs/", // or as RegExp: /\/docs\//
-        //to: "/",
-        //},
-
-        // Optional: Algolia search parameters
-        //searchParameters: {},
-
-        // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: "search",
-
-        //... other Algolia params
-      },
       image: "img/sui-doc-og.png",
       docs: {
         sidebar: {
--- docs/site/src/theme/Navbar/Content/index.js
+++ docs/site/src/theme/Navbar/Content/index.js
@@ -1,6 +1,6 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-import React from "react";
+import React, { useState } from "react";
 import { useThemeConfig, ErrorCauseBoundary } from "@docusaurus/theme-common";
 import {
   splitNavbarItems,
@@ -8,16 +8,18 @@ import {
 } from "@docusaurus/theme-common/internal";
 import NavbarItem from "@theme/NavbarItem";
 import ThemeToggle from "@site/src/components/ThemeToggle";
-import SearchBar from "@theme/SearchBar";
+//import SearchBar from "@theme/SearchBar";
 import NavbarMobileSidebarToggle from "@theme/Navbar/MobileSidebar/Toggle";
 import NavbarLogo from "@theme/Navbar/Logo";
 import NavbarSearch from "@theme/Navbar/Search";
 import GetStartedLink from "@site/src/components/GetStartedLink";
+import SearchModal from "@site/src/components/Search/SearchModal";
 
 function useNavbarItems() {
   // TODO temporary casting until ThemeConfig type is improved
   return useThemeConfig().navbar.items;
 }
+
 function NavbarItems({ items }) {
   return (
     <div className="flex flex-[8_1_0%] items-center justify-start gap-8 min-[1100px]:gap-16">
@@ -39,6 +41,7 @@ ${JSON.stringify(item, null, 2)}`,
     </div>
   );
 }
+
 function NavbarContentLayout({ left, right }) {
   return (
     <div className="navbar__inner">
@@ -47,12 +50,52 @@ function NavbarContentLayout({ left, right }) {
     </div>
   );
 }
+
+function SearchLauncher() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        className="DocSearch DocSearch-Button flex items-center cursor-pointer"
+        onClick={() => setOpen(true)}
+      >
+        <span class="DocSearch-Button-Container flex">
+          <svg
+            width="20"
+            height="20"
+            class="DocSearch-Search-Icon"
+            viewBox="0 0 20 20"
+            aria-hidden="true"
+          >
+            <path
+              d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+              stroke="currentColor"
+              fill="none"
+              fill-rule="evenodd"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <span class="DocSearch-Button-Placeholder font-semibold">Search</span>
+        </span>
+      </button>
+      <SearchModal isOpen={open} onClose={() => setOpen(false)} />
+    </>
+  );
+}
+
 export default function NavbarContent() {
   const mobileSidebar = useNavbarMobileSidebar();
   const items = useNavbarItems();
   const [leftItems, rightItems] = splitNavbarItems(items);
   const searchBarItem = items.find((item) => item.type === "search");
-
+  try {
+    window.initCookbook();
+  } catch (e) {
+    // Gracefully ignore errors if something goes wrong
+    console.error("Error initializing Ask Cookbook", e);
+  }
   return (
     <NavbarContentLayout
       left={
@@ -71,7 +114,7 @@ export default function NavbarContent() {
           <ThemeToggle />
           {!searchBarItem && (
             <NavbarSearch>
-              <SearchBar />
+              <SearchLauncher />
               <GetStartedLink />
             </NavbarSearch>
           )}
