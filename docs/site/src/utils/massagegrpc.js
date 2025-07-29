c6ba9b6442be06e397e318208e767ba7d771ff34
Ronny Roland <ronmilton@mystenlabs.com>
2025-07-29 14:49:57 -0600

    fix link

---
+++ docs/site/src/utils/massagegrpc.js
@@ -0,0 +1,21 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.resolve(
+  __dirname,
+  "../../../content/references/sui-api/sui-graphql/beta/reference/types/objects/checkpoint.mdx",
+);
+
+let content = fs.readFileSync(filePath, "utf8");
+
+content = content.replace(
+  /\/references\/sui-api\/sui-graphql\/beta\/reference\/types\/objects\/query\.mdx/g,
+  "/references/sui-api/sui-graphql/beta/reference/operations/queries/checkpoint.mdx",
+);
+
+fs.writeFileSync(filePath, content, "utf8");
+
+console.log("✅ Patched query link in checkpoint.mdx");
