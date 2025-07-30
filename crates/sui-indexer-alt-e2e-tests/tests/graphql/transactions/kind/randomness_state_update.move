cb0c8ce7b7ff29ee10436a3d2c91b32a75c8dadd
Joey Pham <joey.pham@mystenlabs.com>
2025-07-30 10:32:04 -0400

    [graphql-alt] Support RandomnessStateUpdate kind for TransactionKind [3/n]

---
+++ crates/sui-indexer-alt-e2e-tests/tests/graphql/transactions/kind/randomness_state_update.move
@@ -0,0 +1,26 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//# init --protocol-version 70 --simulator
+
+//# advance-epoch --create-random-state
+
+//# set-random-state --randomness-round 0 --random-bytes SGVsbG8gU3VpIFJhbmRvbW5lc3M= --randomness-initial-version 2
+
+//# create-checkpoint
+
+//# run-graphql
+{
+  # Test the RandomnessStateUpdate transaction created by set-random-state
+  randomnessStateUpdateTransaction: transaction(digest: "@{digest_2}") {
+    digest
+    kind {
+      ... on RandomnessStateUpdateTransaction {
+        epoch
+        randomnessRound
+        randomBytes
+        randomnessObjInitialSharedVersion
+      }
+    }
+  }
+} 
--- crates/sui-indexer-alt-graphql/src/api/types/transaction_kind/mod.rs
+++ crates/sui-indexer-alt-graphql/src/api/types/transaction_kind/mod.rs
@@ -8,16 +8,19 @@ use crate::scope::Scope;
 
 use self::{
     consensus_commit_prologue::ConsensusCommitPrologueTransaction, genesis::GenesisTransaction,
+    randomness_state_update::RandomnessStateUpdateTransaction,
 };
 
 pub(crate) mod consensus_commit_prologue;
 pub(crate) mod genesis;
+pub(crate) mod randomness_state_update;
 
 /// Different types of transactions that can be executed on the Sui network.
 #[derive(Union, Clone)]
 pub enum TransactionKind {
     Genesis(GenesisTransaction),
     ConsensusCommitPrologue(ConsensusCommitPrologueTransaction),
+    RandomnessStateUpdate(RandomnessStateUpdateTransaction),
 }
 
 impl TransactionKind {
@@ -39,6 +42,11 @@ impl TransactionKind {
             K::ConsensusCommitPrologueV4(ccp) => Some(T::ConsensusCommitPrologue(
                 ConsensusCommitPrologueTransaction::from_v4(ccp, scope),
             )),
+            K::RandomnessStateUpdate(rsu) => {
+                Some(T::RandomnessStateUpdate(RandomnessStateUpdateTransaction {
+                    native: rsu,
+                }))
+            }
             // Other types will return None for now
             _ => None,
         }
