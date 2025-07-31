9e9bb77baf771433c6288efa64666ab43a34a8b2
Joey Pham <joey.pham@mystenlabs.com>
2025-07-31 17:47:45 -0400

    [graphql-alt] Support ConsensusCommitPrologueTransaction kind for TransactionKind [2/n] (#22950)
    ## Description 
    
    Adding the new `ConsensusCommitPrologueTransaction` as a new enum field
    within `TransactionKind` union
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ```
    cargo nextest run -p sui-indexer-alt-graphql
    cargo nextest run -p sui-indexer-alt-reader
    cargo nextest run -p sui-indexer-alt-e2e-tests -- graphql
    ```
    
    ---
    
    ## Stack 
    - #22652 
    - #22718 
    - #22788 
    - #22943
    - #22950 ⬅️
    
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/sui-indexer-alt-graphql/src/api/types/transaction_kind/mod.rs
+++ crates/sui-indexer-alt-graphql/src/api/types/transaction_kind/mod.rs
@@ -6,14 +6,18 @@ use sui_types::transaction::TransactionKind as NativeTransactionKind;
 
 use crate::scope::Scope;
 
-use self::genesis::GenesisTransaction;
+use self::{
+    consensus_commit_prologue::ConsensusCommitPrologueTransaction, genesis::GenesisTransaction,
+};
 
+pub(crate) mod consensus_commit_prologue;
 pub(crate) mod genesis;
 
 /// Different types of transactions that can be executed on the Sui network.
 #[derive(Union, Clone)]
 pub enum TransactionKind {
     Genesis(GenesisTransaction),
+    ConsensusCommitPrologue(ConsensusCommitPrologueTransaction),
 }
 
 impl TransactionKind {
@@ -23,7 +27,19 @@ impl TransactionKind {
 
         match kind {
             K::Genesis(g) => Some(T::Genesis(GenesisTransaction { native: g, scope })),
-            // For now, only support Genesis - other types will return None
+            K::ConsensusCommitPrologue(ccp) => Some(T::ConsensusCommitPrologue(
+                ConsensusCommitPrologueTransaction::from_v1(ccp, scope),
+            )),
+            K::ConsensusCommitPrologueV2(ccp) => Some(T::ConsensusCommitPrologue(
+                ConsensusCommitPrologueTransaction::from_v2(ccp, scope),
+            )),
+            K::ConsensusCommitPrologueV3(ccp) => Some(T::ConsensusCommitPrologue(
+                ConsensusCommitPrologueTransaction::from_v3(ccp, scope),
+            )),
+            K::ConsensusCommitPrologueV4(ccp) => Some(T::ConsensusCommitPrologue(
+                ConsensusCommitPrologueTransaction::from_v4(ccp, scope),
+            )),
+            // Other types will return None for now
             _ => None,
         }
     }
