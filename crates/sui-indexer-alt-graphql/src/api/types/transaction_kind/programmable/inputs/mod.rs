38ad1773dcb9648654f06d5a0850162fc9924c24
Joey Pham <joey.pham@mystenlabs.com>
2025-08-01 12:55:06 -0400

    [graphql-alt] Support Object Transaction Input for TransactionKind [2/n]

--- crates/sui-indexer-alt-graphql/src/api/types/transaction_kind/programmable/inputs/mod.rs
+++ crates/sui-indexer-alt-graphql/src/api/types/transaction_kind/programmable/inputs/mod.rs
@@ -1,30 +1,57 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+pub mod object;
 pub mod pure;
 
 use async_graphql::*;
 
 use crate::{api::scalars::base64::Base64, scope::Scope};
+pub use object::{OwnedOrImmutable, Receiving, SharedInput};
 pub use pure::Pure;
 
 /// Input argument to a Programmable Transaction Block (PTB) command.
-#[derive(Union, Clone)]
+#[derive(Union)]
 pub enum TransactionInput {
     Pure(Pure),
+    OwnedOrImmutable(OwnedOrImmutable),
+    SharedInput(SharedInput),
+    Receiving(Receiving),
+    // TODO: Add BalanceWithdraw variant
 }
 
 impl TransactionInput {
-    pub fn from(input: sui_types::transaction::CallArg, _scope: Scope) -> Self {
-        use sui_types::transaction::CallArg;
+    pub fn from(input: sui_types::transaction::CallArg, scope: Scope) -> Self {
+        use sui_types::transaction::{CallArg, ObjectArg};
 
         match input {
             CallArg::Pure(bytes) => Self::Pure(Pure {
                 bytes: Some(Base64::from(bytes)),
             }),
-            // TODO: Handle other input types
-            _ => Self::Pure(Pure {
-                bytes: Some(Base64::from(b"TODO: Unsupported input type".to_vec())),
+            CallArg::Object(obj_arg) => match obj_arg {
+                ObjectArg::ImmOrOwnedObject((object_id, version, digest)) => {
+                    Self::OwnedOrImmutable(OwnedOrImmutable::from_object_ref(
+                        object_id, version, digest, scope,
+                    ))
+                }
+                ObjectArg::SharedObject {
+                    id,
+                    initial_shared_version,
+                    mutable,
+                } => Self::SharedInput(SharedInput::from_shared_object(
+                    id,
+                    initial_shared_version,
+                    mutable,
+                )),
+                ObjectArg::Receiving((object_id, version, digest)) => Self::Receiving(
+                    Receiving::from_object_ref(object_id, version, digest, scope),
+                ),
+            },
+            // TODO: Handle BalanceWithdraw
+            CallArg::BalanceWithdraw(_) => Self::Pure(Pure {
+                bytes: Some(Base64::from(
+                    b"TODO: BalanceWithdraw not supported".to_vec(),
+                )),
             }),
         }
     }
