5a5792729a0d33b5954891b6269a2e43d9d58526
Anastasios Kichidis <tasos@mystenlabs.com>
2025-08-12 15:38:56 +0300

    [fix] pass the committee authority names as display names during fullnode reconfig observer. Remove tx id from some traces.

--- crates/sui-benchmark/src/fullnode_reconfig_observer.rs
+++ crates/sui-benchmark/src/fullnode_reconfig_observer.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use async_trait::async_trait;
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 use sui_core::{
     authority_aggregator::{AuthAggMetrics, AuthorityAggregator},
     authority_client::NetworkAuthorityClient,
@@ -74,11 +74,11 @@ impl ReconfigObserver<NetworkAuthorityClient> for FullNodeReconfigObserver {
                             .committee_store
                             .insert_new_committee(new_committee.committee());
                         let auth_agg = AuthorityAggregator::new_from_committee(
-                            sui_system_state.get_sui_committee_for_benchmarking(),
+                            new_committee,
                             &self.committee_store,
                             self.safe_client_metrics_base.clone(),
                             self.auth_agg_metrics.clone(),
-                            Arc::new(HashMap::new()),
+                            Arc::new(sui_system_state.get_committee_authority_names_to_hostnames()),
                         );
                         driver.update_authority_aggregator(Arc::new(auth_agg));
                     } else {
