04910e8a2c2693133c3bfbdf569b6f5798f27d48
mwtian <81660174+mwtian@users.noreply.github.com>
2025-08-14 08:21:16 -0700

    Update latency buckets (#23137)
    ## Description 
    
    Specifically, make client latency buckets more fine grained. Client
    latency should match QD and TD latencies when plotted together.
    
    ## Test plan 
    
    CI
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/sui-core/src/quorum_driver/metrics.rs
+++ crates/sui-core/src/quorum_driver/metrics.rs
@@ -9,14 +9,6 @@ use prometheus::{
     Registry,
 };
 
-const FINALITY_LATENCY_SEC_BUCKETS: &[f64] = &[
-    0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85,
-    0.9, 0.95, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6,
-    2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5,
-    7.0, 7.5, 8.0, 8.5, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0,
-    25.0,
-];
-
 #[derive(Clone)]
 pub struct QuorumDriverMetrics {
     pub(crate) total_requests: IntCounter,
@@ -99,7 +91,7 @@ impl QuorumDriverMetrics {
                 "quorum_driver_settlement_finality_latency",
                 "Settlement finality latency observed from quorum driver",
                 &["tx_type"],
-                FINALITY_LATENCY_SEC_BUCKETS.to_vec(),
+                mysten_metrics::LATENCY_SEC_BUCKETS.to_vec(),
                 registry,
             )
             .unwrap(),
