831e07844fd642fe56c6f2ac249a2e5daa550d8a
Ashok Menon <ashok@mystenlabs.com>
2025-08-25 15:38:40 +0100

    graphql-alt: name service APIs
    ## Description
    
    Add APIs to map a SuiNS name to an `Address`, and return the default
    SuiNS name for an address, taking into account expiration.
    
    The implementation is based on the equivalent API in `jsonrpc-alt`, but
    accounting for GraphQL's methods for fetching objects, in a consistent
    way.
    
    ## Test plan
    
    New E2E tests -- these need to be implemented in Rust, so that a custom
    name service config can be set:
    
    ```
    $ cargo nextest run            \
      -p sui-indexer-alt-e2e-tests \
      --test graphql_name_service_tests
    ```
    
    The tests are adapted from the equivalent tests for `jsonrpc-alt`.

--- crates/sui-indexer-alt-graphql/src/api/scalars/mod.rs
+++ crates/sui-indexer-alt-graphql/src/api/scalars/mod.rs
@@ -6,6 +6,7 @@ pub(crate) mod big_int;
 pub(crate) mod cursor;
 pub(crate) mod date_time;
 pub(crate) mod digest;
+pub(crate) mod domain;
 pub(crate) mod json;
 pub(crate) mod owner_kind;
 pub(crate) mod sui_address;
