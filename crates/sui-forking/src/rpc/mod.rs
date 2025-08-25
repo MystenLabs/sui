e22d31e1acd3d93f455ffe0f9b360f14d12b50f1
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-08-25 09:41:53 -0700

    WIP

--- crates/sui-forking/src/rpc/mod.rs
+++ crates/sui-forking/src/rpc/mod.rs
@@ -11,8 +11,13 @@ use prometheus::Registry;
 use tokio_util::sync::CancellationToken;
 
 use simulacrum::Simulacrum;
+use sui_indexer_alt_jsonrpc::NodeArgs;
 use sui_indexer_alt_jsonrpc::RpcArgs;
 use sui_indexer_alt_jsonrpc::RpcService;
+use sui_indexer_alt_jsonrpc::config::RpcConfig;
+use sui_indexer_alt_reader::bigtable_reader::BigtableArgs;
+use sui_indexer_alt_reader::system_package_task::SystemPackageTaskArgs;
+use sui_pg_db::DbArgs;
 use tokio::task::JoinHandle;
 
 pub(crate) async fn start_rpc(
@@ -23,7 +28,22 @@ pub(crate) async fn start_rpc(
     let registry = Registry::new_custom(Some("sui_forking".into()), None)
         .context("Failed to create Prometheus registry.")?;
 
-    let mut rpc = RpcService::new(RpcArgs::default(), &registry, cancel.clone()).unwrap();
+    let mut rpc = sui_indexer_alt_jsonrpc::basic_rpc(
+        None,
+        None,
+        DbArgs::default(),
+        BigtableArgs::default(),
+        RpcArgs::default(),
+        NodeArgs {
+            fullnode_rpc_url: None,
+        },
+        SystemPackageTaskArgs::default(),
+        RpcConfig::default(),
+        &registry,
+        cancel.child_token(),
+    )
+    .await?;
+    // let mut rpc = RpcService::new(RpcArgs::default(), &registry, cancel.clone()).unwrap();
     rpc.add_module(write::Write(simulacrum))?;
 
     let h_rpc = rpc.run().await.context("Failed to start RPC service")?;
