dfd52fae56a5e22710eb81d925f8ae61ed0247ec
Michael George <mike.george@mystenlabs.com>
2025-08-26 10:38:07 -0400

    added version to lockfile; removed old ephemeral lockfile parsing

--- external-crates/move/crates/move-package-alt/src/package/lockfile.rs
+++ external-crates/move/crates/move-package-alt/src/package/lockfile.rs
@@ -18,8 +18,6 @@ use super::{EnvironmentName, paths::PackagePath};
 pub struct Lockfiles<F: MoveFlavor> {
     main: ParsedLockfile<F>,
     file: FileHandle,
-    ephemeral: BTreeMap<EnvironmentName, Publication<F>>,
-    // TODO: probably should have separate file handles for ephemerals?
 }
 
 // TODO: instead of a Lockfile module, we should maybe have a publication information module I think; the
@@ -39,32 +37,10 @@ impl<F: MoveFlavor> Lockfiles<F> {
         let file_id = FileHandle::new(lockfile_name)?;
         let main: ParsedLockfile<F> = toml_edit::de::from_str(file_id.source())?;
 
-        // Parse all `.Move.<env>.lock`
-        debug!("reading ephemeral lockfiles");
-        let mut ephemeral = BTreeMap::new();
-        for (env, path) in path.env_lockfiles()? {
-            let file_id = FileHandle::new(path)?;
-            let metadata = toml_edit::de::from_str(file_id.source())?;
-
-            ephemeral.insert(env.clone(), metadata);
-        }
-
         Ok(Some(Lockfiles {
             main,
-            ephemeral,
             file: file_id,
         }))
-    }
-
-    /// Serialize [self] into `Move.lock` and `Move.<env>.lock`.
-    pub fn write_to(&self, path: &PackagePath) -> PackageResult<()> {
-        std::fs::write(path.lockfile_path(), self.main.render_as_toml())?;
-
-        for (env, entry) in &self.ephemeral {
-            std::fs::write(path.lockfile_by_env_path(env), entry.render_as_toml())?;
-        }
-
-        Ok(())
     }
 
     pub fn pins_for_env(&self, env: &EnvironmentName) -> Option<&BTreeMap<PackageID, Pin>> {
@@ -75,25 +51,8 @@ impl<F: MoveFlavor> Lockfiles<F> {
         self.file
     }
 
-    // TODO: handle ephemerals correctly
-    /// Return the published metadata for all environments.
-    pub fn published(&self) -> &BTreeMap<EnvironmentName, Publication<F>> {
-        &self.main.published
-    }
-
-    // TODO: handle ephemerals correctly
     /// Return the published metadata for a specific environment.
     pub fn published_for_env(&self, env: &EnvironmentName) -> Option<Publication<F>> {
         self.main.published.get(env).cloned()
     }
-
-    // TODO: ignores ephemerals and should probably be removed
-    pub fn render_main_lockfile(&self) -> String {
-        self.main.render_as_toml()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    // TODO
 }
