d7c345027e4fb68ba0f8d66e4c3e7fef4da8759f
Michael George <mike.george@mystenlabs.com>
2025-08-27 10:30:49 -0400

    added publish metadata to vanilla; test (and fix) folding in pubfile render

--- external-crates/move/crates/move-package-alt/src/flavor/vanilla.rs
+++ external-crates/move/crates/move-package-alt/src/flavor/vanilla.rs
@@ -7,6 +7,8 @@
 
 use std::{collections::BTreeMap, iter::empty};
 
+use serde::{Deserialize, Serialize};
+
 use crate::schema::{
     Environment, EnvironmentID, EnvironmentName, PackageName, ReplacementDependency,
 };
@@ -25,8 +27,21 @@ pub fn default_environment() -> Environment {
 #[derive(Debug)]
 pub struct Vanilla;
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "kebab-case")]
+pub struct PublishedMetadata {
+    #[serde(default)]
+    build_config: Option<SavedBuildConfig>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct SavedBuildConfig {
+    edition: String,
+    flavor: String,
+}
+
 impl MoveFlavor for Vanilla {
-    type PublishedMetadata = ();
+    type PublishedMetadata = PublishedMetadata;
     type PackageMetadata = ();
     type AddressInfo = ();
 
