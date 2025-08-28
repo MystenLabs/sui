cfaf708c8215c385efeebca3768df735b9e21f85
Deepak Maram <deepak@mystenlabs.com>
2025-08-28 15:44:47 -0400

    New data structures enabling checkpoint artifacts

--- crates/sui-core/src/checkpoints/mod.rs
+++ crates/sui-core/src/checkpoints/mod.rs
@@ -2083,6 +2083,7 @@ impl CheckpointBuilder {
                 end_of_epoch_data,
                 timestamp_ms,
                 matching_randomness_rounds,
+                vec![],
             );
             summary.report_checkpoint_age(
                 &self.metrics.last_created_checkpoint_age,
