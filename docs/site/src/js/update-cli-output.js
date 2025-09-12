ca6223f03ce4d815b48d4a0bbb55590b2cdb475b
Jessie Mongeon <133128541+jessiemongeon1@users.noreply.github.com>
2025-09-12 16:50:26 -0500

    Apply suggestions from code review

--- docs/site/src/js/update-cli-output.js
+++ docs/site/src/js/update-cli-output.js
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
