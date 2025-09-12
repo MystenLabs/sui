c2c6e0211ffb32ef55ec0e2d9ba73e9153cc5e21
Todd Nowacki <tmn@mystenlabs.com>
2025-09-11 19:35:01 -0700

    stable sort

--- crates/sui/tests/shell_tests/with_network/summaries/summarize_json.sh
+++ crates/sui/tests/shell_tests/with_network/summaries/summarize_json.sh
@@ -3,9 +3,9 @@
 
 # Default output format is JSON
 sui move --client.config $CONFIG summary --package-id 0x2 --bytecode
-ls -1 package_summaries
-ls -1 package_summaries/0x0000000000000000000000000000000000000000000000000000000000000002
-ls -1 package_summaries/0x0000000000000000000000000000000000000000000000000000000000000001
+ls -1 package_summaries | LC_ALL=C sort -f
+ls -1 package_summaries/0x0000000000000000000000000000000000000000000000000000000000000002 | LC_ALL=C sort -f
+ls -1 package_summaries/0x0000000000000000000000000000000000000000000000000000000000000001 | LC_ALL=C sort -f
 cat package_summaries/root_package_metadata.json
 echo
 cat package_summaries/address_mapping.json
