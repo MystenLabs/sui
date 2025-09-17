94c3b1eb8923df3690d058d3d5a68806636e558f
dongowu <jason_w96@163.com>
2025-09-17 16:55:46 +0800

    chore: remove redundant words in comments and documentation &&moditify the .graphql file

--- sui-execution/v1/sui-adapter/src/execution_engine.rs
+++ sui-execution/v1/sui-adapter/src/execution_engine.rs
@@ -368,7 +368,7 @@ mod checked {
                 result = Err(conservation_err);
                 gas_charger.reset(temporary_store);
                 gas_charger.charge_gas(temporary_store, &mut result);
-                // check conservation once more more
+                // check conservation once more
                 if let Err(recovery_err) = {
                     temporary_store
                         .check_sui_conserved(simple_conservation_checks, cost_summary)
