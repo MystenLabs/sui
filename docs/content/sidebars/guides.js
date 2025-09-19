88fb5a9217573f26431c169ddf0d7ecb445de254
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2025-09-19 08:43:05 -0500

    getting started restructure

--- docs/content/sidebars/guides.js
+++ docs/content/sidebars/guides.js
@@ -7,195 +7,182 @@ const guides = [
 		label: 'Guides',
 		id: 'guides',
 	},
-	{
-		type: 'category',
-		label: 'Developer Guides',
-		collapsed: false,
-		link: {
-			type: 'doc',
-			id: 'guides/developer',
+		{
+			type: 'category',
+			label: 'Getting Started',
+			collapsed: false,
+			link: {
+				type: 'doc',
+				id: 'guides/developer/getting-started',
+			},
+			items: [
+				{
+					type: 'category',
+					label: 'Install Sui',
+					collapsed: false,
+					link: {
+						type: 'doc',
+						id: 'guides/developer/getting-started/sui-install',
+					},
+					items: [
+						'guides/developer/getting-started/install-source',
+						'guides/developer/getting-started/install-binaries',
+						'guides/developer/getting-started/upgrade-sui',
+					],
+				},
+				'guides/developer/getting-started/configure-sui-client',
+				'guides/developer/getting-started/get-address',
+				'guides/developer/getting-started/get-coins',
+				'guides/developer/getting-started/hello-world',
+				'guides/developer/getting-started/next-steps',
+			],
 		},
-		items: [
-			{
-				type: 'category',
-				label: 'Getting Started',
-				collapsed: false,
-				link: {
-					type: 'doc',
-					id: 'guides/developer/getting-started',
-				},
-				items: [
-					'guides/developer/getting-started/sui-install',
-					'guides/developer/getting-started/connect',
-					'guides/developer/getting-started/local-network',
-					'guides/developer/getting-started/get-address',
-					'guides/developer/getting-started/get-coins',
-					'guides/developer/getting-started/data-serving',
-				],
+		{
+			type: 'category',
+			label: 'Sui Essentials',
+			link: {
+				type: 'doc',
+				id: 'guides/developer/sui-101',
 			},
-			{
-				type: 'category',
-				label: 'Your First Sui dApp',
-				link: {
-					type: 'doc',
-					id: 'guides/developer/first-app',
-				},
-				items: [
-					'guides/developer/first-app/write-package',
-					'guides/developer/first-app/build-test',
-					'guides/developer/first-app/publish',
-					'guides/developer/first-app/debug',
-					'guides/developer/first-app/client-tssdk',
-				],
-			},
-			{
-				type: 'category',
-				label: 'Sui 101',
-				link: {
-					type: 'doc',
-					id: 'guides/developer/sui-101',
-				},
-				items: [
-					'guides/developer/sui-101/object-ownership',
-					'guides/developer/sui-101/using-events',
-					'guides/developer/sui-101/access-time',
-					'guides/developer/sui-101/sign-and-send-txn',
-					'guides/developer/sui-101/sponsor-txn',
-					'guides/developer/sui-101/avoid-equivocation',
-					{
-						type: 'category',
-						label: 'Working with PTBs',
-						link: {
-							type: 'doc',
-							id: 'guides/developer/sui-101/working-with-ptbs',
-						},
-						items: [
-							'guides/developer/sui-101/building-ptb',
-							'guides/developer/sui-101/coin-mgt',
-							'guides/developer/sui-101/simulating-refs',
-						],
+			items: [
+				'guides/developer/sui-101/object-ownership',
+				'guides/developer/sui-101/using-events',
+				'guides/developer/sui-101/access-time',
+				'guides/developer/sui-101/sign-and-send-txn',
+				'guides/developer/sui-101/sponsor-txn',
+				'guides/developer/sui-101/avoid-equivocation',
+				{
+					type: 'category',
+					label: 'Working with PTBs',
+					link: {
+						type: 'doc',
+						id: 'guides/developer/sui-101/working-with-ptbs',
 					},
-				],
-			},
-			{
-				type: 'category',
-				label: 'Coins and Tokens',
-				link: {
-					type: 'doc',
-					id: 'guides/developer/coin-index',
+					items: [
+						'guides/developer/sui-101/building-ptb',
+						'guides/developer/sui-101/coin-mgt',
+						'guides/developer/sui-101/simulating-refs',
+					],
 				},
-				items: [
-					'guides/developer/coin',
-					'guides/developer/coin/regulated',
-					'guides/developer/coin/in-game-token',
-					'guides/developer/coin/loyalty',
-				],
+			],
+		},
+		{
+			type: 'category',
+			label: 'Coins and Tokens',
+			link: {
+				type: 'doc',
+				id: 'guides/developer/coin-index',
 			},
-			'guides/developer/stablecoins',
-			{
-				type: 'category',
-				label: 'NFTs',
-				link: {
-					type: 'doc',
-					id: 'guides/developer/nft-index',
-				},
-				items: [
-					'guides/developer/nft',	
-					'guides/developer/nft/nft-soulbound', 
-					'guides/developer/nft/nft-rental', 
-					'guides/developer/nft/asset-tokenization'
-				],
+			items: [
+				'guides/developer/coin',
+				'guides/developer/coin/regulated',
+				'guides/developer/coin/in-game-token',
+				'guides/developer/coin/loyalty',
+			],
+		},
+		'guides/developer/stablecoins',
+		{
+			type: 'category',
+			label: 'NFTs',
+			link: {
+				type: 'doc',
+				id: 'guides/developer/nft-index',
 			},
-			{
-				type: 'category',
-				label: 'Cryptography',
-				link: {
-					type: 'doc',
-					id: 'guides/developer/cryptography',
-				},
-				items: [
-					'guides/developer/cryptography/signing',
-					'guides/developer/cryptography/groth16',
-					'guides/developer/cryptography/hashing',
-					'guides/developer/cryptography/ecvrf',
-					'guides/developer/cryptography/multisig',
-					{
-						type: 'category',
-						label: 'zkLogin Integration Guide',
-						link: {
-							type: 'doc',
-							id: 'guides/developer/cryptography/zklogin-integration',
-						},
-						items: [
-							'guides/developer/cryptography/zklogin-integration/developer-account',
-							'guides/developer/cryptography/zklogin-integration/zklogin-example',
-						],
+			items: [
+				'guides/developer/nft',	
+				'guides/developer/nft/nft-soulbound', 
+				'guides/developer/nft/nft-rental', 
+				'guides/developer/nft/asset-tokenization'
+			],
+		},
+		{
+			type: 'category',
+			label: 'Cryptography',
+			link: {
+				type: 'doc',
+				id: 'guides/developer/cryptography',
+			},
+			items: [
+				'guides/developer/cryptography/signing',
+				'guides/developer/cryptography/groth16',
+				'guides/developer/cryptography/hashing',
+				'guides/developer/cryptography/ecvrf',
+				'guides/developer/cryptography/multisig',
+				{
+					type: 'category',
+					label: 'zkLogin Integration Guide',
+					link: {
+						type: 'doc',
+						id: 'guides/developer/cryptography/zklogin-integration',
 					},
-				],
-			},
-			{
-				type: 'category',
-				label: 'Advanced Topics',
-				link: {
-					type: 'doc',
-					id: 'guides/developer/advanced',
+					items: [
+						'guides/developer/cryptography/zklogin-integration/developer-account',
+						'guides/developer/cryptography/zklogin-integration/zklogin-example',
+					],
 				},
-				items: [
-					/*{
-						type: 'category',
-						label: 'Efficient Smart Contracts',
-						link: {
-							type: 'doc',
-							id: 'guides/developer/advanced/efficient-smart-contracts',
-						},
-						items: ['guides/developer/advanced/min-gas-fees'],
-					},*/
-					'guides/developer/advanced/move-2024-migration',
-					'guides/developer/advanced/custom-indexer',
-					'guides/developer/advanced/custom-indexer/indexer-walrus',
-					'guides/developer/advanced/randomness-onchain',
-					'guides/developer/getting-started/graphql-rpc',
-					'guides/developer/advanced/local-fee-markets',
-				],
+			],
+		},
+		{
+			type: 'category',
+			label: 'Advanced Topics',
+			link: {
+				type: 'doc',
+				id: 'guides/developer/advanced',
 			},
-			{
-				type: 'category',
-				label: 'App Examples',
-				link: {
-					type: 'doc',
-					id: 'guides/developer/app-examples',
-				},
-				items: [
-					'guides/developer/app-examples/e2e-counter',
-					'guides/developer/app-examples/trustless-swap',
-					'guides/developer/app-examples/coin-flip',
-					'guides/developer/app-examples/reviews-rating',
-					'guides/developer/app-examples/blackjack',
-					'guides/developer/app-examples/plinko',
-					'guides/developer/app-examples/tic-tac-toe',
-					{
-						type: 'category',
-						label: 'Oracles',
-						link: {
-							type: 'doc',
-							id: 'guides/developer/app-examples/oracle',
-						},
-						items: [
-							'guides/developer/app-examples/weather-oracle',
-							'guides/developer/app-examples/meta-pricing-oracle',
-						],
+			items: [
+				/*{
+					type: 'category',
+					label: 'Efficient Smart Contracts',
+					link: {
+						type: 'doc',
+						id: 'guides/developer/advanced/efficient-smart-contracts',
 					},
-					// 'guides/developer/app-examples/recaptcha',
-					// 'guides/developer/app-examples/trusted-swap',
-					// 'guides/developer/app-examples/turnip-town',
-					// 'guides/developer/app-examples/auction',
-				],
+					items: ['guides/developer/advanced/min-gas-fees'],
+				},*/
+				'guides/developer/advanced/move-2024-migration',
+				'guides/developer/advanced/custom-indexer',
+				'guides/developer/advanced/custom-indexer/indexer-walrus',
+				'guides/developer/advanced/randomness-onchain',
+				'guides/developer/getting-started/graphql-rpc',
+				'guides/developer/advanced/local-fee-markets',
+			],
+		},
+		{
+			type: 'category',
+			label: 'App Examples',
+			link: {
+				type: 'doc',
+				id: 'guides/developer/app-examples',
 			},
-			'guides/developer/starter-templates',
-			'guides/developer/zklogin-onboarding',
-			'guides/developer/dev-cheat-sheet',
-		],
-	},
+			items: [
+				'guides/developer/app-examples/e2e-counter',
+				'guides/developer/app-examples/trustless-swap',
+				'guides/developer/app-examples/coin-flip',
+				'guides/developer/app-examples/reviews-rating',
+				'guides/developer/app-examples/blackjack',
+				'guides/developer/app-examples/plinko',
+				'guides/developer/app-examples/tic-tac-toe',
+				{
+					type: 'category',
+					label: 'Oracles',
+					link: {
+						type: 'doc',
+						id: 'guides/developer/app-examples/oracle',
+					},
+					items: [
+						'guides/developer/app-examples/weather-oracle',
+						'guides/developer/app-examples/meta-pricing-oracle',
+					],
+				},
+				// 'guides/developer/app-examples/recaptcha',
+				// 'guides/developer/app-examples/trusted-swap',
+				// 'guides/developer/app-examples/turnip-town',
+				// 'guides/developer/app-examples/auction',
+			],
+		},
+		'guides/developer/starter-templates',
+		'guides/developer/zklogin-onboarding',
+		'guides/developer/dev-cheat-sheet',
 	{
 		type: 'category',
 		label: 'Operator Guides',
