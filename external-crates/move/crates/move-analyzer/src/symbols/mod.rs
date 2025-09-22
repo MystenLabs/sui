249b3614de355c9a6d3c55b0d6c5d387f7242113
Adam Welc <adam@mystenlabs.com>
2025-09-22 15:40:54 +0200

    [move-ide] Incremental symbolication fix (#23666)
    ## Description 
    
    This fixes a problem with incremental symbolication. What we attempted
    before was to collect information about which files are modified (i.e.,
    which ones have to be fully compiled) from the messages sent by the
    client to the LSP server. In some cases, however, this seemed to have
    led to problems.
    
    In particular, a file might have been deemed unmodified by the analysis
    but be actually modified before mapped files structure (including file
    hashes) used throughout analysis was constructed. This would result in
    the discrepancy in the hash for this file seen by the compiler (as it
    would used cached data for this file) and mapped files structure (as it
    would have a hash after modification). This could cause `move-analyzer`
    to crash.
    
    This PR implements a much more robust way of determining which files are
    modified that manages to introducing this race condition
    
    ## Test plan 
    
    This was difficult to reproduce, but the original reporter of this bug
    confirms that after trying `move-analyzer` with this fix, the crashes he
    used to observe no longer happen

--- external-crates/move/crates/move-analyzer/src/symbols/mod.rs
+++ external-crates/move/crates/move-analyzer/src/symbols/mod.rs
@@ -147,17 +147,10 @@ pub type FileModules = BTreeMap<PathBuf, BTreeSet<ModuleDefs>>;
 /// correctly computed symbols should be a replacement for the old set - if symbols are not
 /// actually (re)computed and the diagnostics are returned, the old symbolic information should
 /// be retained even if it's getting out-of-date.
-///
-/// Takes `modified_files` as an argument to indicate if we can retain (portion of) the cached
-/// user code. If `modified_files` is `None`, we can't retain any cached user code (need to recompute)
-/// everything. If `modified_files` is `Some`, we can retain cached user code for all Move files other than
-/// the ones in `modified_files` (if `modified_paths` contains a path not representing
-/// a Move file but rather a directory, then we conservatively do not re-use any cached info).
 pub fn get_symbols(
     packages_info: Arc<Mutex<CachedPackages>>,
     ide_files_root: VfsPath,
     pkg_path: &Path,
-    modified_files: Option<Vec<PathBuf>>,
     lint: LintLevel,
     cursor_info: Option<(&PathBuf, Position)>,
     implicit_deps: Dependencies,
@@ -188,7 +181,6 @@ pub fn get_symbols(
             packages_info.clone(),
             ide_files_root.clone(),
             pkg_path,
-            modified_files.clone(),
             lint,
             implicit_deps.clone(),
         )?;
@@ -228,6 +220,12 @@ pub fn compute_symbols(
         .file_name_mapping()
         .iter()
         .map(|(fhash, fpath)| (*fhash, fpath.clone()))
+        .collect::<BTreeMap<_, _>>();
+    let user_file_hashes = compiled_pkg_info
+        .mapped_files
+        .file_name_mapping()
+        .iter()
+        .map(|(fhash, fpath)| (fpath.clone(), *fhash))
         .collect::<BTreeMap<_, _>>();
     let mut symbols_computation_data = SymbolsComputationData::new();
     let typed_mod_named_address_maps = compiled_pkg_info
@@ -270,6 +268,7 @@ pub fn compute_symbols(
                     deps_symbols_data,
                     program: Arc::new(program),
                     file_paths: Arc::new(file_paths),
+                    user_file_hashes: Arc::new(user_file_hashes),
                     edition,
                     compiler_info,
                     lsp_diags,
