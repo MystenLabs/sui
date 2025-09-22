540240254502b2f3cc94d28b1bccb576111abefe
Brandon Williams <brandon@mystenlabs.com>
2025-09-22 10:41:01 -0500

    kv-grpc: delay v2 release (#23690)

--- crates/sui-kv-rpc/src/main.rs
+++ crates/sui-kv-rpc/src/main.rs
@@ -72,7 +72,7 @@ async fn main() -> Result<()> {
             sui_rpc_api::proto::google::protobuf::FILE_DESCRIPTOR_SET,
         )
         .register_encoded_file_descriptor_set(sui_rpc_api::proto::google::rpc::FILE_DESCRIPTOR_SET)
-        .register_encoded_file_descriptor_set(sui_rpc::proto::sui::rpc::v2::FILE_DESCRIPTOR_SET)
+        // .register_encoded_file_descriptor_set(sui_rpc::proto::sui::rpc::v2::FILE_DESCRIPTOR_SET)
         .register_encoded_file_descriptor_set(
             sui_rpc::proto::sui::rpc::v2beta2::FILE_DESCRIPTOR_SET,
         )
@@ -82,7 +82,7 @@ async fn main() -> Result<()> {
             sui_rpc_api::proto::google::protobuf::FILE_DESCRIPTOR_SET,
         )
         .register_encoded_file_descriptor_set(sui_rpc_api::proto::google::rpc::FILE_DESCRIPTOR_SET)
-        .register_encoded_file_descriptor_set(sui_rpc::proto::sui::rpc::v2::FILE_DESCRIPTOR_SET)
+        // .register_encoded_file_descriptor_set(sui_rpc::proto::sui::rpc::v2::FILE_DESCRIPTOR_SET)
         .register_encoded_file_descriptor_set(
             sui_rpc::proto::sui::rpc::v2beta2::FILE_DESCRIPTOR_SET,
         )
@@ -101,9 +101,9 @@ async fn main() -> Result<()> {
             Arc::new(RpcMetrics::new(&registry)),
         )))
         .add_service(LedgerServiceServer::new(server.clone()))
-        .add_service(
-            sui_rpc::proto::sui::rpc::v2::ledger_service_server::LedgerServiceServer::new(server),
-        )
+        // .add_service(
+        //     sui_rpc::proto::sui::rpc::v2::ledger_service_server::LedgerServiceServer::new(server),
+        // )
         .add_service(reflection_v1)
         .add_service(reflection_v1alpha)
         .serve(addr)
