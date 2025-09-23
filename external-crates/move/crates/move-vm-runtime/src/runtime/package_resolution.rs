c523c336514865f6d0e128ae11b13d30aff19d8a
Tim Zakian <timzakian@gmail.com>
2025-09-23 16:35:29 -0700

    [11/n][bella-ciao][move] Rip out majority of `TransactionDataCache`

--- external-crates/move/crates/move-vm-runtime/src/runtime/package_resolution.rs
+++ external-crates/move/crates/move-vm-runtime/src/runtime/package_resolution.rs
@@ -11,12 +11,14 @@ use crate::{
     cache::move_cache::{self, MoveCache, Package, ResolvedPackageResult},
     dbg_println, jit,
     natives::functions::NativeFunctions,
-    runtime::data_cache::TransactionDataCache,
-    shared::{data_store::DataStore, logging::expect_no_verification_errors, types::VersionId},
+    shared::{logging::expect_no_verification_errors, types::VersionId},
     validation::{validate_package, verification},
 };
 use move_binary_format::errors::{Location, PartialVMError, VMResult};
-use move_core_types::{resolver::ModuleResolver, vm_status::StatusCode};
+use move_core_types::{
+    resolver::{ModuleResolver, SerializedPackage},
+    vm_status::StatusCode,
+};
 use move_vm_config::runtime::VMConfig;
 use tracing::error;
 
@@ -28,18 +30,12 @@ use std::{
 // Retrieves a package from the cache, attempting to load it from the data store if
 // it is not present.
 pub fn resolve_package(
+    store: impl ModuleResolver,
     cache: &MoveCache,
     natives: &NativeFunctions,
-    module_resolver: impl ModuleResolver,
     package_to_read: VersionId,
 ) -> VMResult<ResolvedPackageResult> {
-    let data_store = TransactionDataCache::new(module_resolver);
-    let mut packages = resolve_packages(
-        cache,
-        natives,
-        &data_store,
-        BTreeSet::from([package_to_read]),
-    )?;
+    let mut packages = resolve_packages(store, cache, natives, BTreeSet::from([package_to_read]))?;
 
     if packages.is_empty() {
         return Ok(ResolvedPackageResult::NotFound);
@@ -77,9 +73,9 @@ pub fn resolve_package(
 // Retrieves a set of packages from the cache, attempting to load them from the data store if
 // they are not present.
 pub fn resolve_packages(
+    store: impl ModuleResolver,
     cache: &MoveCache,
     natives: &NativeFunctions,
-    data_store: &impl DataStore,
     packages_to_read: BTreeSet<VersionId>,
 ) -> VMResult<BTreeMap<VersionId, Arc<move_cache::Package>>> {
     dbg_println!("loading {packages_to_read:#?}");
@@ -102,9 +98,9 @@ pub fn resolve_packages(
     // Load and cache anything that wasn't already there.
     // NB: packages can be loaded out of order here (e.g., in parallel) if so desired.
     for pkg in load_and_verify_packages(
+        store,
         &cache.vm_config,
         natives,
-        data_store,
         allow_loading_failure,
         &pkgs_to_cache,
     )? {
@@ -124,14 +120,14 @@ pub fn resolve_packages(
 // Read the package from the data store, deserialize it, and verify it (internally).
 // NB: Does not perform cyclic dependency verification or linkage checking.
 pub fn load_and_verify_packages(
+    store: impl ModuleResolver,
     vm_config: &VMConfig,
     natives: &NativeFunctions,
-    data_store: &impl DataStore,
     allow_loading_failure: bool,
     packages_to_read: &BTreeSet<VersionId>,
 ) -> VMResult<Vec<verification::ast::Package>> {
     let packages = packages_to_read.iter().cloned().collect::<Vec<_>>();
-    let packages = match data_store.load_packages(&packages) {
+    let packages = match load_packages(store, &packages) {
         Ok(packages) => packages,
         Err(err) if allow_loading_failure => return Err(err),
         Err(err) => {
@@ -145,6 +141,45 @@ pub fn load_and_verify_packages(
         .into_iter()
         .map(|pkg| validate_package(natives, vm_config, pkg))
         .collect()
+}
+
+// Loads a set of packages from the data store, converting any underlying storage errors into VM errors.
+// If any package is not found, an error is returned.
+// If there is an error loading any package, an error is returned.
+// The order of the returned packages matches the order of the provided version ids.
+fn load_packages(
+    store: impl ModuleResolver,
+    ids: &[VersionId],
+) -> VMResult<Vec<SerializedPackage>> {
+    let pkgs = match store.get_packages(ids) {
+        Ok(pkgs) => pkgs
+            .into_iter()
+            .enumerate()
+            .map(|(idx, pkg)| {
+                pkg.ok_or_else(|| {
+                    PartialVMError::new(StatusCode::LINKER_ERROR)
+                        .with_message(format!("Cannot find package {:?} in data cache", ids[idx],))
+                        .finish(Location::Undefined)
+                })
+            })
+            .collect::<VMResult<Vec<_>>>()?,
+        Err(err) => {
+            let msg = format!("Unexpected storage error: {:?}", err);
+            return Err(
+                PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
+                    .with_message(msg)
+                    .finish(Location::Undefined),
+            );
+        }
+    };
+
+    // Should all be the same length, the the ordering should be preserved.
+    debug_assert_eq!(pkgs.len(), ids.len());
+    for (pkg, id) in pkgs.iter().zip(ids.iter()) {
+        debug_assert_eq!(pkg.version_id, *id);
+    }
+
+    Ok(pkgs)
 }
 
 // Retrieve a JIT-compiled package from the cache, or compile and add it to the cache.
@@ -192,118 +227,3 @@ pub fn jit_and_cache_package(
             .finish(Location::Undefined)
     })
 }
-
-// -------------------------------------------------------------------------------------------------
-// Old Code, Preserved for Utility and Possible Reinstatement
-// -------------------------------------------------------------------------------------------------
-
-//  /// Retrieves a single module from the cache. NOTE: this package is _not_ checked for cyclic
-//  /// dependency verification or linkage, simply retrieved from the cache (or loaded, if
-//  /// necessary). Also, this may trigger subsequent loads for package dependencies to build up
-//  /// the type cache.
-//  pub fn get_module(
-//      &self,
-//      data_store: &impl DataStore,
-//      link_context: &LinkageContext,
-//      module_id: &ModuleId,
-//  ) -> VMResult<(Arc<CompiledModule>, Arc<Module>)> {
-//      let module_id = link_context.relocate(module_id).map_err(|err| {
-//          err.with_message("Could not relocate module in data store".to_string())
-//              .finish(Location::Undefined)
-//      })?;
-//      let (package, ident) = module_id.clone().into();
-//      let packages =
-//          self.retrieve_packages(data_store, link_context, BTreeSet::from([package]))?;
-//      let Some(package) = packages.get(&package) else {
-//          return Err(
-//              PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-//                  .with_message("Package not found in loaded cache".to_string())
-//                  .finish(Location::Undefined),
-//          );
-//      };
-//      let Some(compiled_module) = package.verified.modules.get(&module_id) else {
-//          return Err(
-//              PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-//                  .with_message("Module not found in package".to_string())
-//                  .finish(Location::Undefined),
-//          );
-//      };
-//      let Some(loaded_module) = package.runtime.loaded_modules.get(&ident) else {
-//          return Err(
-//              PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-//                  .with_message("Module not found in package".to_string())
-//                  .finish(Location::Undefined),
-//          );
-//      };
-//      // FIXME: Needs more arcs elsehwere
-//      Ok((Arc::new(compiled_module.value.clone()), loaded_module.clone()))
-//  }
-
-//  /// Retries a function defnitionn from the cache, handing back the contianing module, function,
-//  /// and paraemter and return type information. NOTE: this package is _not_ checked for cyclic
-//  /// dependency verification or linkage, simply retrieved from the cache (or loaded, if
-//  /// necessary). Also, this may trigger subsequent loads for package dependencies to build up
-//  /// the type cache.
-//  pub fn get_function(
-//      &self,
-//      data_store: &impl DataStore,
-//      link_context: &LinkageContext,
-//      module_id: &ModuleId,
-//      function_name: &IdentStr,
-//      ty_args: &[Type],
-//  ) -> VMResult<LoadedFunction> {
-//      let (compiled_module, loaded_module) =
-//          self.get_module(data_store, link_context, module_id)?;
-//      let module_id = link_context.relocate(module_id).map_err(|err| {
-//          err.with_message("Could not relocate module in data store".to_string())
-//              .finish(Location::Undefined)
-//      })?;
-//      let Some(function) = loaded_module.function_map.get(function_name).copied() else {
-//          return Err(
-//              PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-//                  .with_message("Module not found in package".to_string())
-//                  .finish(Location::Undefined),
-//          );
-//      };
-
-//      let fun_ref = function.to_ref();
-
-//      let parameters = compiled_module
-//          .signature_at(fun_ref.parameters)
-//          .0
-//          .iter()
-//          .map(|tok| {
-//              self.type_cache()└─➤
-//                  .read()
-//                  .make_type(&compiled_module, tok, link_context)
-//          })
-//          .collect::<PartialVMResult<Vec<_>>>()
-//          .map_err(|err| err.finish(Location::Undefined))?;
-
-//      let return_ = compiled_module
-//          .signature_at(fun_ref.return_)
-//          .0
-//          .iter()
-//          .map(|tok| {
-//              self.type_cache()
-//                  .read()
-//                  .make_type(&compiled_module, tok, link_context)
-//          })
-//          .collect::<PartialVMResult<Vec<_>>>()
-//          .map_err(|err| err.finish(Location::Undefined))?;
-
-//      // verify type arguments
-//      self.type_cache()
-//          .read()
-//          .verify_ty_args(fun_ref.type_parameters(), ty_args)
-//          .map_err(|e| e.finish(Location::Module(module_id.clone())))?;
-
-//      let loaded_function = LoadedFunction {
-//          compiled_module,
-//          loaded_module,
-//          function,
-//          parameters,
-//          return_,
-//      };
-//      Ok(loaded_function)
-//  }
