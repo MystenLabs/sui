417d72c97c30daf0e2f041da5f87f90c3a105b5d
Ashok Menon <ashok@mystenlabs.com>
2025-10-01 00:16:22 +0100

    display: new syntax for dynamic field access.
    ## Description
    
    Differentiate the syntax for vector access from the syntax for dynamic
    field and dynamic object field access so that we can always determine
    from the syntax which accesses are going to load objects. The new syntax
    is as follows:
    
    ```
    e[f]    # vector or VecMap access
    e->[f]  # dynamic field access
    e=>[f]  # dynamic object field access
    ```
    
    In future, we may also supporting "dereferencing" an object by its ID,
    and fetching a field from that value, with the following syntax:
    
    ```
    e->f
    ```
    
    And it is TBD what the syntax would be for loading a value, wholesale.
    
    ## Test plan
    
    ```
    $ cargo nextest run -p sui-display
    ```

--- crates/sui-display/src/v2/lexer.rs
+++ crates/sui-display/src/v2/lexer.rs
@@ -32,6 +32,10 @@ pub(crate) struct OwnedLexeme(pub bool, pub Token, pub usize, pub String);
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub(crate) enum Token {
+    /// '->'
+    Arrow,
+    /// '=>'
+    AArrow,
     /// '@'
     At,
     /// ':'
@@ -130,6 +134,10 @@ impl<'s> Lexer<'s> {
 
         use Token as T;
         Some(match bytes.first()? {
+            b'-' if bytes.get(1) == Some(&b'>') => self.take(ws, T::Arrow, 2),
+
+            b'=' if bytes.get(1) == Some(&b'>') => self.take(ws, T::AArrow, 2),
+
             b'@' => self.take(ws, T::At, 1),
 
             b':' if bytes.get(1) == Some(&b':') => self.take(ws, T::CColon, 2),
@@ -283,6 +291,8 @@ impl fmt::Display for OwnedLexeme {
         }
 
         match self {
+            L(_, T::Arrow, _, _) => write!(f, "'->'"),
+            L(_, T::AArrow, _, _) => write!(f, "'=>'"),
             L(_, T::At, _, _) => write!(f, "'@'"),
             L(_, T::Colon, _, _) => write!(f, "':'"),
             L(_, T::CColon, _, _) => write!(f, "'::'"),
@@ -330,6 +340,8 @@ impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use Token as T;
         match self {
+            T::Arrow => write!(f, "'->'"),
+            T::AArrow => write!(f, "'=>'"),
             T::At => write!(f, "'@'"),
             T::Colon => write!(f, "':'"),
             T::CColon => write!(f, "'::'"),
@@ -559,12 +571,11 @@ mod tests {
         "###);
     }
 
-    // Display supports two kinds of index -- `foo[i]` and `bar[[j]]`. Unlike braces, doubly nested
-    // brackets do not have their own token. The two cases are distinguished by the parser, which
-    // uses significant whitespace to distinguish between two separate `]`'s vs a single `]]`.
+    // Display supports three kinds of index -- `foo[i]`, `bar->[j]`, and `baz=>[k]`, representing
+    // vector/VecMap, dynamic field, and dynamic object field access respectively.
     #[test]
     fn test_indices() {
-        assert_snapshot!(lexemes(r#"foo {bar[baz].qux[[quy]][quz]}"#), @r###"
+        assert_snapshot!(lexemes(r#"foo {bar[baz].qux=>[quy]->[quz]}"#), @r###"
         L(false, Text, 0, "foo ")
         L(false, LBrace, 4, "{")
         L(false, Ident, 5, "bar")
@@ -573,15 +584,15 @@ mod tests {
         L(false, RBracket, 12, "]")
         L(false, Dot, 13, ".")
         L(false, Ident, 14, "qux")
-        L(false, LBracket, 17, "[")
-        L(false, LBracket, 18, "[")
-        L(false, Ident, 19, "quy")
-        L(false, RBracket, 22, "]")
+        L(false, AArrow, 17, "=>")
+        L(false, LBracket, 19, "[")
+        L(false, Ident, 20, "quy")
         L(false, RBracket, 23, "]")
-        L(false, LBracket, 24, "[")
-        L(false, Ident, 25, "quz")
-        L(false, RBracket, 28, "]")
-        L(false, RBrace, 29, "}")
+        L(false, Arrow, 24, "->")
+        L(false, LBracket, 26, "[")
+        L(false, Ident, 27, "quz")
+        L(false, RBracket, 30, "]")
+        L(false, RBrace, 31, "}")
         "###);
     }
 
