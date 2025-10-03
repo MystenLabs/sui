d3e27eca5eaf4dc476ac9d9e3f77d4ff8837ec82
akmysti <222086987+akmysti@users.noreply.github.com>
2025-10-02 19:02:21 -0700

    Move tests to unit_tests directory

--- external-crates/move/crates/move-vm-runtime/src/unit_tests/mod.rs
+++ external-crates/move/crates/move-vm-runtime/src/unit_tests/mod.rs
@@ -11,6 +11,7 @@ mod exec_func_effects_tests;
 mod function_arg_tests;
 mod instantiation_tests;
 mod invariant_violation_tests;
+mod jump_table_tests;
 mod leak_tests;
 mod loader_tests;
 mod nested_loop_tests;
