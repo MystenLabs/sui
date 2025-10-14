0496bb14a59370691edc3cb4121420cf50a3ea9e
Ashok Menon <ashok@mystenlabs.com>
2025-10-15 00:12:57 +0100

    display: dynamic field support
    ## Description
    
    Support dynamic field and dynamic object field access to Display v2.
    This also makes use of the `store` in the interpreter. Making use of the
    `store` surfaced lifetime constraint issues that have also been fixed in
    this change.
    
    ## Test plan
    
    New unit tests for dynamic field accesses:
    
    ```
    $ cargo nextest run -p sui-display
    ```

--- crates/sui-display/src/v2/interpreter.rs
+++ crates/sui-display/src/v2/interpreter.rs
@@ -1,11 +1,20 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{borrow::Cow, fmt::Write as _, sync::atomic::AtomicUsize};
+use std::{
+    borrow::Cow,
+    fmt::Write as _,
+    sync::{Arc, atomic::AtomicUsize},
+};
 
 use futures::{
     future::{OptionFuture, join_all},
     join,
+};
+use move_core_types::account_address::AccountAddress;
+use sui_types::dynamic_field::{
+    DynamicFieldInfo, DynamicFieldType, derive_dynamic_field_id,
+    visitor::{self as DFV, FieldVisitor},
 };
 
 use crate::v2::extractor::Extractor;
@@ -18,29 +27,30 @@ use super::{
 };
 
 /// The interpreter is responsible for evaluating expressions inside format strings into values.
-pub(crate) struct Interpreter<'a> {
-    #[allow(unused)]
-    store: Box<dyn Store<'a>>,
-    root: Slice<'a>,
+pub(crate) struct Interpreter<'s, S: Store<'s>> {
+    root: Slice<'s>,
+    store: S,
+    max_output_size: usize,
+    used_output: AtomicUsize,
 }
 
-impl<'a> Interpreter<'a> {
-    pub(crate) fn new(store: Box<dyn Store<'a>>, root: Slice<'a>) -> Self {
-        Self { store, root }
+impl<'s, S: Store<'s>> Interpreter<'s, S> {
+    pub(crate) fn new(root: Slice<'s>, store: S, max_output_size: usize) -> Self {
+        Self {
+            root,
+            store,
+            max_output_size,
+            used_output: AtomicUsize::new(0),
+        }
     }
 
-    /// Entrypoint to evaluate a single format string.
-    ///
-    /// The size of the output is bounded by `max_output_size`, and measured in a shared counter --
-    /// `used_output`.
+    /// Entrypoint to evaluate a single format string, represented as a sequence of its strands.
     pub(crate) async fn eval(
         &self,
-        used_output: &AtomicUsize,
-        max_output_size: usize,
-        strands: &'a [P::Strand<'a>],
+        strands: &'s [P::Strand<'s>],
     ) -> Result<serde_json::Value, FormatError> {
         // TODO(amnn): Support nested display and JSON transform.
-        let mut writer = BoundedWriter::new(used_output, max_output_size);
+        let mut writer = BoundedWriter::new(&self.used_output, self.max_output_size);
 
         for strand in strands {
             match strand {
@@ -68,7 +78,7 @@ impl<'a> Interpreter<'a> {
     ///
     /// Returns the result from the first chain that produces a value, or `Ok(None)` if none do.
     /// Propagates any errors encountered during evaluation.
-    async fn eval_alts(&self, alts: &'a [P::Chain<'a>]) -> Result<Option<Value<'a>>, FormatError> {
+    async fn eval_alts(&self, alts: &'s [P::Chain<'s>]) -> Result<Option<Value<'s>>, FormatError> {
         for chain in alts {
             if let Some(v) = self.eval_chain(chain).await? {
                 return Ok(Some(v));
@@ -88,7 +98,7 @@ impl<'a> Interpreter<'a> {
     /// describing exists.
     ///
     /// Any errors encountered during evaluation are propagated.
-    async fn eval_chain(&self, chain: &'a P::Chain<'a>) -> Result<Option<Value<'a>>, FormatError> {
+    async fn eval_chain(&self, chain: &'s P::Chain<'s>) -> Result<Option<Value<'s>>, FormatError> {
         use Accessor as A;
         use Value as V;
 
@@ -121,6 +131,78 @@ impl<'a> Interpreter<'a> {
         accessors.reverse();
         while let Some(accessor) = accessors.last() {
             match (root, accessor) {
+                (V::Address(a), A::DFIndex(i)) => {
+                    let bytes = bcs::to_bytes(&i)?;
+                    let type_ = i.type_();
+                    let df_id = derive_dynamic_field_id(a, &type_, &bytes)?;
+
+                    let Some(field) = self
+                        .store
+                        .object(df_id.into())
+                        .await
+                        .map_err(|e| FormatError::Store(Arc::new(e)))?
+                    else {
+                        return Ok(None);
+                    };
+
+                    let field = match FieldVisitor::deserialize(field.bytes, field.layout) {
+                        Ok(f) => f,
+                        Err(DFV::Error::Visitor(e)) => return Err(FormatError::Visitor(e)),
+                        Err(_) => return Ok(None),
+                    };
+
+                    if field.kind != DynamicFieldType::DynamicField {
+                        return Ok(None);
+                    }
+
+                    accessors.pop();
+                    root = V::Slice(Slice {
+                        bytes: field.value_bytes,
+                        layout: field.value_layout,
+                    });
+                }
+
+                (V::Address(a), A::DOFIndex(i)) => {
+                    let bytes = bcs::to_bytes(&i)?;
+                    let type_ = DynamicFieldInfo::dynamic_object_field_wrapper(i.type_()).into();
+                    let df_id = derive_dynamic_field_id(a, &type_, &bytes)?;
+
+                    let Some(field) = self
+                        .store
+                        .object(df_id.into())
+                        .await
+                        .map_err(|e| FormatError::Store(Arc::new(e)))?
+                    else {
+                        return Ok(None);
+                    };
+
+                    let field = match FieldVisitor::deserialize(field.bytes, field.layout) {
+                        Ok(f) => f,
+                        Err(DFV::Error::Visitor(e)) => return Err(FormatError::Visitor(e)),
+                        Err(_) => return Ok(None),
+                    };
+
+                    if field.kind != DynamicFieldType::DynamicObject {
+                        return Ok(None);
+                    }
+
+                    let Ok(id) = AccountAddress::from_bytes(field.value_bytes) else {
+                        return Ok(None);
+                    };
+
+                    let Some(slice) = self
+                        .store
+                        .object(id)
+                        .await
+                        .map_err(|e| FormatError::Store(Arc::new(e)))?
+                    else {
+                        return Ok(None);
+                    };
+
+                    accessors.pop();
+                    root = V::Slice(slice);
+                }
+
                 // Fetch a single byte from a byte array, as long as the accessor evaluates to a
                 // numeric index.
                 (V::Bytes(bs), accessor) => {
@@ -169,11 +251,10 @@ impl<'a> Interpreter<'a> {
                 }
 
                 // Use the remaining accessors to extract a value from a slice of a serialized
-                // value. This can consume multiple accessors.
+                // value. This can consume multiple accessors, but will pause if it encounters a
+                // dynamic (object) field access.
                 (V::Slice(slice), _) => {
-                    let Some(value) = Extractor::deserialize_slice(slice, &mut accessors)
-                        .map_err(FormatError::Visitor)?
-                    else {
+                    let Some(value) = Extractor::deserialize_slice(slice, &mut accessors)? else {
                         return Ok(None);
                     };
 
@@ -207,8 +288,8 @@ impl<'a> Interpreter<'a> {
     /// errors or `None` values.
     async fn eval_accessor(
         &self,
-        acc: &'a P::Accessor<'a>,
-    ) -> Result<Option<Accessor<'a>>, FormatError> {
+        acc: &'s P::Accessor<'s>,
+    ) -> Result<Option<Accessor<'s>>, FormatError> {
         use Accessor as VA;
         use P::Accessor as PA;
 
@@ -227,8 +308,8 @@ impl<'a> Interpreter<'a> {
     /// otherwise it propagates errors or `None` values.
     async fn eval_literal(
         &self,
-        lit: &'a P::Literal<'a>,
-    ) -> Result<Option<Value<'a>>, FormatError> {
+        lit: &'s P::Literal<'s>,
+    ) -> Result<Option<Value<'s>>, FormatError> {
         use P::Literal as L;
         use Value as V;
 
@@ -275,8 +356,8 @@ impl<'a> Interpreter<'a> {
     /// it propagates errors or `None` values.
     async fn eval_fields(
         &self,
-        field: &'a P::Fields<'a>,
-    ) -> Result<Option<Fields<'a>>, FormatError> {
+        field: &'s P::Fields<'s>,
+    ) -> Result<Option<Fields<'s>>, FormatError> {
         Ok(match field {
             P::Fields::Positional(fs) => self.eval_chains(fs).await?.map(Fields::Positional),
             P::Fields::Named(fs) => self
@@ -292,8 +373,8 @@ impl<'a> Interpreter<'a> {
     /// propagates errors or `None` values.
     async fn eval_chains(
         &self,
-        chains: impl IntoIterator<Item = &'a P::Chain<'a>>,
-    ) -> Result<Option<Vec<Value<'a>>>, FormatError> {
+        chains: impl IntoIterator<Item = &'s P::Chain<'s>>,
+    ) -> Result<Option<Vec<Value<'s>>>, FormatError> {
         let values = chains
             .into_iter()
             .map(|chain| Box::pin(self.eval_chain(chain)));
