f96a8a650e0cc37db1cd6c44796baa6b19a1ce41
Abhinav Garg <abhi.garg@mystenlabs.com>
2025-10-17 14:29:52 -0400

    docs: adding a page for Nautilus and Seal pattern

--- docs/content/sidebars/concepts.js
+++ docs/content/sidebars/concepts.js
@@ -156,6 +156,7 @@ const concepts = [
 				items: [
 					'concepts/cryptography/nautilus/nautilus-design',
 					'concepts/cryptography/nautilus/using-nautilus',
+					'concepts/cryptography/nautilus/nautilus-seal',
 				],
 			},
 			'concepts/cryptography/system/checkpoint-verification',
