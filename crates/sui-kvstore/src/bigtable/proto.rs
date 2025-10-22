959204892f6f20f1e4fb5f1cf56fbd9d8ba499fa
Brandon Williams <brandon@mystenlabs.com>
2025-10-22 11:06:21 -0600

    chore: update tonic to 0.14 (#24032)
    This patch updates tonic to 0.14 as well as adds support for building
    the bigtable client from proto files directly (instead of just having a
    the generated protos checked in).

--- crates/sui-kvstore/src/bigtable/proto.rs
+++ crates/sui-kvstore/src/bigtable/proto.rs
@@ -1,14 +1,23 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 #![allow(clippy::all)]
+#![allow(unused)]
+
 #[path = "proto"]
 pub mod bigtable {
+    #[rustfmt::skip]
     #[path = "google.bigtable.v2.rs"]
     pub mod v2;
 }
 
+#[rustfmt::skip]
 #[path = "proto/google.rpc.rs"]
 pub mod rpc;
 
+#[rustfmt::skip]
 #[path = "proto/google.api.rs"]
 pub mod api;
+
+#[rustfmt::skip]
+#[path = "proto/google.r#type.rs"]
+pub mod r#type;
