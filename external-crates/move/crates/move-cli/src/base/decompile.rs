045821aca93418bb6fffa4bd24b8368dd5432aa7
cgswords <cameron.swords@mystenlabs.com>
2025-10-23 14:17:16 -0700

    Test cleanup

--- external-crates/move/crates/move-cli/src/base/decompile.rs
+++ external-crates/move/crates/move-cli/src/base/decompile.rs
@@ -29,7 +29,6 @@ enum Input {
 
 impl Decompile {
     pub fn execute(self, _path: Option<&Path>, _config: BuildConfig) -> anyhow::Result<()> {
-        println!("Starting decompilation...");
         let Self { input, output_path } = self;
         // Ensure the input file exists
         let input_path = Path::new(&input);
@@ -71,10 +70,7 @@ impl Decompile {
         };
 
         // Ensure the output path exists
-        let output_path = Path::new(&output_path)
-            .canonicalize()
-            .expect("Invalid output path");
-        let output_path = PathBuf::from(output_path);
+        let output_path = Path::new(&output_path);
         std::fs::create_dir_all(&output_path).map_err(|_| anyhow!("Failed to create directory"))?;
 
         // Decompile the files
