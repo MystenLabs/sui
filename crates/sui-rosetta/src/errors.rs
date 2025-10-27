f0c13244f4cbefd7e4ae65a3aafa2a22c641d347
Brandon Williams <brandon@mystenlabs.com>
2025-10-27 09:11:15 -0600

    sui-rosetta: remove embedded fullnode mode (#24102)

--- crates/sui-rosetta/src/errors.rs
+++ crates/sui-rosetta/src/errors.rs
@@ -21,7 +21,6 @@ use strum::EnumProperty;
 use strum_macros::Display;
 use strum_macros::EnumDiscriminants;
 use thiserror::Error;
-use typed_store::TypedStoreError;
 
 /// Sui-Rosetta specific error types.
 /// This contains all the errors returns by the sui-rosetta server.
@@ -75,8 +74,6 @@ pub enum Error {
     SuiRpcError(#[from] sui_sdk::error::Error),
     #[error(transparent)]
     EncodingError(#[from] eyre::Report),
-    #[error(transparent)]
-    DBError(#[from] TypedStoreError),
     #[error(transparent)]
     JsonExtractorRejection(#[from] JsonRejection),
 
