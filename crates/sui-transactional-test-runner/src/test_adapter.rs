97c5f380f91e5c817009b98536b72a99f5110bb1
Todd Nowacki <tmn@mystenlabs.com>
2025-10-28 11:50:34 -0700

    Small fixes to transactional tests (#24118)
    ## Description 
    
    - Added an error for setting the protocol version to the max, or past
    the max
    - Added a flag to hide the contents of an object with --hide-contents,
    which will help with noisy tests
    
    ## Test plan 
    
    - 👀 
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/sui-transactional-test-runner/src/test_adapter.rs
+++ crates/sui-transactional-test-runner/src/test_adapter.rs
@@ -253,6 +253,17 @@ impl AdapterInitConfig {
             .unwrap_or_default();
 
         let mut protocol_config = if let Some(protocol_version) = protocol_version {
+            assert!(
+                protocol_version <= ProtocolVersion::max().as_u64(),
+                "Cannot set the protocol version to {}, since it is higher than the max version {}",
+                protocol_version,
+                ProtocolVersion::max().as_u64(),
+            );
+            assert!(
+                protocol_version != ProtocolVersion::max().as_u64(),
+                "Do not set the protocol version to the max {}. It can lead to unanticipated test changes once the max version is bumped. Instead, leave it unset to always use the max version.",
+                protocol_version,
+            );
             ProtocolConfig::get_for_version(protocol_version.into(), Chain::Unknown)
         } else {
             ProtocolConfig::get_for_max_version_UNSAFE()
@@ -390,7 +401,7 @@ impl MoveTestAdapter<'_> for SuiTestAdapter {
             Some((init_cmd, sui_args)) => AdapterInitConfig::from_args(init_cmd, sui_args),
             None => AdapterInitConfig::default(),
         };
-        let enabled_ptb_v2 = protocol_config.version >= ProtocolVersion::max()
+        let enabled_ptb_v2 = protocol_config.version == ProtocolVersion::max()
             && ENABLE_PTB_V2.get().copied().unwrap_or(false);
         protocol_config.set_enable_ptb_execution_v2_for_testing(enabled_ptb_v2);
 
@@ -890,7 +901,10 @@ impl MoveTestAdapter<'_> for SuiTestAdapter {
                     current_epoch, round
                 )))
             }
-            SuiSubcommand::ViewObject(ViewObjectCommand { id: fake_id }) => {
+            SuiSubcommand::ViewObject(ViewObjectCommand {
+                id: fake_id,
+                hide_contents,
+            }) => {
                 let obj = get_obj!(fake_id);
                 Ok(Some(match &obj.data {
                     object::Data::Move(move_obj) => {
@@ -899,12 +913,14 @@ impl MoveTestAdapter<'_> for SuiTestAdapter {
                             BoundedVisitor::deserialize_struct(move_obj.contents(), &layout)
                                 .unwrap();
 
-                        self.stabilize_str(format!(
-                            "Owner: {}\nVersion: {}\nContents: {:#}",
-                            &obj.owner,
-                            obj.version().value(),
-                            move_struct
-                        ))
+                        let msg =
+                            format!("Owner: {}\nVersion: {}", &obj.owner, obj.version().value());
+                        let msg = if hide_contents {
+                            format!("{msg}\nType: {:#}", &move_struct.type_)
+                        } else {
+                            format!("{msg}\nContents: {move_struct:#}",)
+                        };
+                        self.stabilize_str(msg)
                     }
                     object::Data::Package(package) => {
                         let num_modules = package.serialized_module_map().len();
