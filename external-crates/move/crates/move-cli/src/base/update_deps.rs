0d83e0acd9e635fd336aa2a2a281effe4887e1a0
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-11-03 12:38:04 -0800

    Fix the logic to allow updating all deps

--- external-crates/move/crates/move-cli/src/base/update_deps.rs
+++ external-crates/move/crates/move-cli/src/base/update_deps.rs
@@ -19,7 +19,7 @@ pub struct UpdateDeps {
     /// The environment to update dependencies for. If none is provided, all environments'
     /// dependencies will be updated.
     #[arg(name = "environment", short = 'e', long = "environment")]
-    environment: EnvironmentName,
+    environment: Option<EnvironmentName>,
     /// Arbitrary mode -- this will be used to enable or filter user-defined `#[mode(<MODE>)]`
     /// annodations during compiltaion.
     #[arg(
@@ -34,15 +34,21 @@ impl UpdateDeps {
     pub async fn execute<F: MoveFlavor>(&self, path: Option<&Path>) -> anyhow::Result<()> {
         let default = PathBuf::from(".");
         let path = path.unwrap_or(&default);
-        let envs = RootPackage::<F>::environments(&path)?;
+        let mut envs = RootPackage::<F>::environments(&path)?;
 
-        let Some(chain_id) = envs.get(&self.environment) else {
-            bail!("Environment {} not found", self.environment);
-        };
-        let environment = Environment::new(self.environment.clone(), chain_id.clone());
-        let mut root_package =
-            RootPackage::<F>::load_force_repin(&path, environment, self.modes.clone()).await?;
-        root_package.save_lockfile_to_disk()?;
+        if let Some(env) = self.environment.as_ref() {
+            let Some(_) = envs.get(env) else {
+                bail!("Environment {} not found", env);
+            };
+            envs.retain(|env_name, _| env_name == env);
+        }
+
+        for e in envs {
+            let env = Environment::new(e.0.clone(), e.1.clone());
+            let mut root_package =
+                RootPackage::<F>::load_force_repin(&path, env, self.modes.clone()).await?;
+            root_package.save_lockfile_to_disk()?;
+        }
 
         Ok(())
     }
