3a67d2e14293989e2953d48d43d1dbbdf93d7cdf
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-11-10 12:28:42 -0800

    Remove caching chain ids for now

--- crates/sui/src/sui_commands.rs
+++ crates/sui/src/sui_commands.rs
@@ -1666,8 +1666,6 @@ async fn prompt_if_no_config(
             .persisted(wallet_conf_path)
             .save()?;
 
-            let context = WalletContext::new(wallet_conf_path)?;
-            let _ = context.cache_chain_id(&context.get_client().await?).await?;
         }
     }
     Ok(())
