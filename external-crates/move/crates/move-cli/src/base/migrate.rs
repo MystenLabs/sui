2152704a1f7fb878434daf60c4af6c4ac64a1d43
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-11-11 08:15:33 -0800

    Fix the code to work with simtests; we need it to be Send as StdinLock is not Send due to a MutexGuard which is not Send

--- external-crates/move/crates/move-cli/src/base/migrate.rs
+++ external-crates/move/crates/move-cli/src/base/migrate.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use clap::*;
-use std::path::Path;
+use std::{io::{self, BufRead, BufReader}, path::Path};
 
 use move_package_alt::flavor::MoveFlavor;
 use move_package_alt_compilation::{build_config::BuildConfig, find_env};
@@ -22,14 +22,9 @@ impl Migrate {
     ) -> anyhow::Result<()> {
         let rerooted_path = reroot_path(path)?;
         let env = find_env::<F>(&rerooted_path, &config.clone())?;
-
+        let mut reader: Box<dyn BufRead + Send> = Box::new(BufReader::new(io::stdin()));
         config
-            .migrate_package::<F, _, _>(
-                &rerooted_path,
-                env,
-                &mut std::io::stdout(),
-                &mut std::io::stdin().lock(),
-            )
+            .migrate_package::<F, _, _>(&rerooted_path, env, &mut io::stdout(), &mut reader)
             .await?;
         Ok(())
     }
