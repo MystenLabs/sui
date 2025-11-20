c1f1ae650fb9f9248b39a569400b4420820868db
Damir Shamanaev <damirka.ru@gmail.com>
2025-11-20 16:22:32 +0400

    [cherry-pick] framework changes to coin.move (#24343)
    ## Description 
    
    Change is already in main with the same protocol version 103.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] Indexing Framework:

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -23,7 +23,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-const MAX_PROTOCOL_VERSION: u64 = 102;
+const MAX_PROTOCOL_VERSION: u64 = 103;
 
 // Record history of protocol version allocations here:
 //
@@ -275,6 +275,7 @@ const MAX_PROTOCOL_VERSION: u64 = 102;
 // Version 100: Framework update
 // Version 101: Framework update
 //              Set max updates per settlement txn to 100.
+// Version 103: Framework update: internal Coin methods
 
 #[derive(Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ProtocolVersion(u64);
@@ -4265,6 +4266,7 @@ impl ProtocolConfig {
                         );
                     cfg.feature_flags.deprecate_global_storage_ops = true;
                 }
+                103 => {}
                 // Use this template when making changes:
                 //
                 //     // modify an existing constant.
