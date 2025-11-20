a43e5ec213bb44bf6418349ec471bc404bdf0311
Michael George <mike.george@mystenlabs.com>
2025-11-20 15:00:57 -0500

    added sui move cache-package command

---
+++ crates/sui-move/src/cache_package.rs
@@ -0,0 +1,40 @@
+use clap::Parser;
+use move_package_alt::{
+    cache_package,
+    schema::{Environment, ManifestDependencyInfo},
+};
+use serde::Deserialize;
+use sui_package_alt::SuiFlavor;
+
+/// Download a package and return information about it
+#[derive(Parser)]
+pub struct CachePackage {
+    /// The environment name to use for resolution
+    environment_name: String,
+
+    /// The chain ID to use for resolution
+    environment_id: String,
+
+    /// A string containing the dependency as it would appear in Move.toml (e.g. `{ local = "../dep" }`)
+    dependency: String,
+}
+
+#[derive(Deserialize)]
+struct DepSpec {
+    dep: ManifestDependencyInfo,
+}
+
+impl CachePackage {
+    pub async fn execute(&self) -> anyhow::Result<()> {
+        let str = format!("dep = {}", self.dependency);
+        let dep: DepSpec = toml::from_str(&str)?;
+        let env = Environment {
+            name: self.environment_name.clone(),
+            id: self.environment_id.clone(),
+        };
+        let info = cache_package::<SuiFlavor>(&env, &dep.dep).await?;
+        println!("{}", serde_json::to_string(&info)?);
+
+        Ok(())
+    }
+}
