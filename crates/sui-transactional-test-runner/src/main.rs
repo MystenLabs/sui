8f1a3e1fb04e7c7bea265b65326591307b5d931d
Todd Nowacki <tmn@mystenlabs.com>
2025-11-21 13:40:20 -0800

    fix

--- crates/sui-transactional-test-runner/src/main.rs
+++ crates/sui-transactional-test-runner/src/main.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use std::collections::BTreeMap;
 
 use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
