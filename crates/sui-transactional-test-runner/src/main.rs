26382b3da6cba9b2e926d0a90a43b72d9d10efc4
Todd Nowacki <tmn@mystenlabs.com>
2025-11-21 13:19:02 -0800

    cleanup

--- crates/sui-transactional-test-runner/src/main.rs
+++ crates/sui-transactional-test-runner/src/main.rs
@@ -1,12 +1,16 @@
 use std::collections::BTreeMap;
 
-use sui_types::{
-    base_types::SuiAddress, programmable_transaction_builder::ProgrammableTransactionBuilder,
-};
+use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
 
 fn main() -> anyhow::Result<()> {
-    let mut ptb = ProgrammableTransactionBuilder::new();
+    let ptb = ProgrammableTransactionBuilder::new();
     let ptb = ptb.finish();
+    if ptb.commands.is_empty() {
+        println!(
+            "No commands in the programmable transaction. This is a small utility to generate transactional tests via a Rust ProgrammableTransactionBuilder. Add commands here before using."
+        );
+        return Ok(());
+    }
     let mut output = String::new();
     sui_transactional_test_runner::programmable_transaction_test_parser::printer::commands(
         &BTreeMap::new(),
