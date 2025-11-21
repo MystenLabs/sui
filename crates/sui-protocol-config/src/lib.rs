535d31495666c0e2aa7ea14cf5c65544c0d1f9fd
Todd Nowacki <tmn@mystenlabs.com>
2025-11-20 18:32:10 -0800

    remove protocol config test functions

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -2283,20 +2283,7 @@ impl ProtocolConfig {
     }
 
     pub fn enable_ptb_execution_v2(&self) -> bool {
-        let enabled = self.feature_flags.enable_ptb_execution_v2;
-        // PTB execution v2 requires gas model version > 10 and the translation charges to be set.
-        if enabled {
-            debug_assert!(self.translation_per_command_base_charge.is_some());
-            debug_assert!(self.translation_per_input_base_charge.is_some());
-            debug_assert!(self.translation_pure_input_per_byte_charge.is_some());
-            debug_assert!(self.translation_per_type_node_charge.is_some());
-            debug_assert!(self.translation_per_reference_node_charge.is_some());
-            debug_assert!(self.translation_per_linkage_entry_charge.is_some());
-            debug_assert!(self.feature_flags.abstract_size_in_object_runtime);
-            debug_assert!(self.feature_flags.object_runtime_charge_cache_load_gas);
-            debug_assert!(self.gas_model_version.is_some_and(|version| version > 10));
-        }
-        enabled
+        self.feature_flags.enable_ptb_execution_v2
     }
 
     pub fn better_adapter_type_resolution_errors(&self) -> bool {
@@ -4268,7 +4255,6 @@ impl ProtocolConfig {
                     cfg.translation_pure_input_per_byte_charge = Some(1);
                     cfg.translation_per_type_node_charge = Some(1);
                     cfg.translation_per_reference_node_charge = Some(1);
-                    cfg.translation_metering_step_resolution = Some(1000);
                     cfg.translation_per_linkage_entry_charge = Some(10);
                     cfg.gas_model_version = Some(11);
                     cfg.feature_flags.abstract_size_in_object_runtime = true;
