d1cc9730878b750dbbe7e1ab3e6eb88191aa7e65
Patrick D. Lloyd <patrick@distrust.co>
2025-11-29 23:24:52 -0800

    Reverted hard-coded release profile

--- docker/sui-node-deterministic/build.sh
+++ docker/sui-node-deterministic/build.sh
@@ -12,15 +12,13 @@ DOCKERFILE="$DIR/Dockerfile"
 GIT_REVISION="$(git describe --always --abbrev=12 --dirty --exclude '*')"
 BUILD_DATE="$(date -u +'%Y-%m-%d')"
 PLATFORM="linux/amd64"
-#1="asdf"
 # option to build using debug symbols
-#if [ "$1" = "--debug-symbols" ]; then
-#	PROFILE="bench"
-#	echo "Building with full debug info enabled ... WARNING: binary size might significantly increase"
-#	shift
-#else
 PROFILE="release"
-#fi
+if [ "$1" = "--debug-symbols" ]; then
+	PROFILE="bench"
+	echo "Building with full debug info enabled ... WARNING: binary size might significantly increase"
+	shift
+fi
 
 echo
 echo "Building sui-node-deterministic docker image"
