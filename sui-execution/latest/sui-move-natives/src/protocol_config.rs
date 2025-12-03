a1e79047a5242e1c5cee7c14d5400002148ee13d
Xun Li <lxfind@gmail.com>
2025-12-02 16:04:46 -0800

    Remove invariant violations

--- sui-execution/latest/sui-move-natives/src/protocol_config.rs
+++ sui-execution/latest/sui-move-natives/src/protocol_config.rs
@@ -3,8 +3,7 @@
 
 use crate::get_extension;
 use crate::object_runtime::ObjectRuntime;
-use move_binary_format::errors::{PartialVMError, PartialVMResult};
-use move_core_types::vm_status::StatusCode;
+use move_binary_format::errors::PartialVMResult;
 use move_vm_runtime::native_functions::NativeContext;
 use move_vm_types::{
     loaded_data::runtime_types::Type,
@@ -36,28 +35,26 @@ pub fn is_feature_enabled(
     let feature_flag_name_bytes = pop_arg!(args, Vector);
     let bytes = feature_flag_name_bytes.to_vec_u8()?;
 
-    let feature_flag_name = match String::from_utf8(bytes.to_vec()) {
-        Ok(s) => s,
-        Err(_) => {
-            debug_assert!(false);
-            return Err(
-                PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-                    .with_message("Feature flag name is not a valid UTF-8 string".to_owned()),
-            );
-        }
-    };
-
     let protocol_config = &get_extension!(context, ObjectRuntime)?.protocol_config;
 
-    // Use the auto-generated lookup_feature method to find the feature flag
-    let is_enabled = match protocol_config.lookup_feature(feature_flag_name) {
-        Some(value) => value,
-        None => {
+    let is_enabled = match String::from_utf8(bytes.to_vec()) {
+        Ok(s) => {
+            // Use the auto-generated lookup_feature method to find the feature flag
+            match protocol_config.lookup_feature(s) {
+                Some(value) => value,
+                None => {
+                    debug_assert!(false);
+                    // We don't distinguish between feature flags that are not present and feature flags that are present but disabled.
+                    // This is to handle the case where we accidentally shipped a framework upgrade that check a feature flag that is not present
+                    // in the binary yet.
+                    false
+                }
+            }
+        }
+        Err(_) => {
             debug_assert!(false);
-            return Err(
-                PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-                    .with_message("Feature flag not found".to_owned()),
-            );
+            // Invalid UTF feature flags are treated as disabled feature flags.
+            false
         }
     };
 
