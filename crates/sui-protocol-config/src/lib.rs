88a3dcfeed467df75e783c7c5cafe6d1d832af32
Mingwei Tian <mingwei@mystenlabs.com>
2025-12-04 11:18:20 -0800

    Disable skipping GC'ed vote

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -4287,7 +4287,6 @@ impl ProtocolConfig {
 
                     cfg.poseidon_bn254_cost_base = Some(260);
 
-                    cfg.feature_flags.consensus_skip_gced_accept_votes = true;
                     if chain != Chain::Mainnet {
                         cfg.feature_flags
                             .enable_nitro_attestation_all_nonzero_pcrs_parsing = true;
