077c7bb565506812a9dd77198e1a6cb046a4ac03
Michael George <mike.george@mystenlabs.com>
2025-12-06 08:47:04 -0500

    fixed tests

--- crates/sui/tests/shell_tests/dont_repin/dont-repin-comment.sh
+++ crates/sui/tests/shell_tests/dont_repin/dont-repin-comment.sh
@@ -4,4 +4,4 @@
 # This should succeed - the manifest has a broken dep, but the lockfile has it pinned to the correct location
 # Repinning shouldn't be retriggered just from an added comment
 echo '# comment' >> Move.toml
-sui move build
+sui move --client.config $CONFIG build
