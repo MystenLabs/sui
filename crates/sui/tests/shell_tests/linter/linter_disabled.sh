3fa2591067948cbdfab8b391d36629703f3c6c52
Michael George <mike.george@mystenlabs.com>
2025-12-08 08:40:03 -0500

    removed copy of sui framework

--- crates/sui/tests/shell_tests/linter/linter_disabled.sh
+++ crates/sui/tests/shell_tests/linter/linter_disabled.sh
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Should succeed with linting disabled (but stats should be summarized)
-sui move --client.config $CONFIG test --silence-warnings
+sui move --client.config $CONFIG test -p example --silence-warnings
