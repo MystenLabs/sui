226c4eaf9f9c7717aa44eaf1f14d4177c39a990e
Michael George <mike.george@mystenlabs.com>
2025-12-15 16:34:24 -0500

    improved redacting

--- crates/sui/tests/shell_tests/default_config/prompt.sh
+++ crates/sui/tests/shell_tests/default_config/prompt.sh
@@ -3,9 +3,10 @@
 
 # If the config file doesn't exist, we prompt and then create it
 echo "" | sui client --client.config ./client.yaml envs \
-  | sed 's/Generated new keypair.*$/Generated new keypair <REDACTED>/g'
+  | sed 's/Generated new keypair.*$/Generated new keypair <REDACTED>/g' \
+  | sed 's/recovery phrase : \[.*\]/recovery phrase : <REDACTED>/g'
 
-sed 's/active_address:.*$/active_address: XXX/g' client.yaml
+sed 's/active_address:.*$/active_address: <REDACTED>/g' client.yaml
 
 echo ""
 echo "check for keystore:"
