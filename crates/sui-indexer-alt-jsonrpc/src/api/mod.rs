44a8634323c1de644707bfb354a0f2911a2217ca
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-12-22 11:53:18 -0800

    Open up checkpoints RPC module

--- crates/sui-indexer-alt-jsonrpc/src/api/mod.rs
+++ crates/sui-indexer-alt-jsonrpc/src/api/mod.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-pub(crate) mod checkpoints;
+pub mod checkpoints;
 pub mod coin;
 pub(crate) mod dynamic_fields;
 pub mod governance;
