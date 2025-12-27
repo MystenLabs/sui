4b9fd343b9cf6614c3574550739cf1c2d99b072f
Ashok Menon <ashok@mystenlabs.com>
2025-12-27 01:37:24 +0000

    graphql: Infer types for pure PTB call args
    ## Description
    
    Use existing functionality in the package resolver to represent pure
    transaction arguments as `MoveValue`s if we can infer their type
    (otherwise, keep using `Pure` call args).
    
    ## Test plan
    
    Updated unit tests:
    
    ```
    sui$ cargo nextest run         \
      -p sui-indexer-alt-e2e-tests \
      -- transactions/kind/programmable
    ```

--- crates/sui-indexer-alt-graphql/src/api/types/transaction_kind/programmable/mod.rs
+++ crates/sui-indexer-alt-graphql/src/api/types/transaction_kind/programmable/mod.rs
@@ -41,13 +41,20 @@ impl ProgrammableTransaction {
     ) -> Option<Result<Connection<String, TransactionInput>, RpcError>> {
         Some(
             async {
-                let pagination = ctx.data::<PaginationConfig>()?;
+                let pagination: &PaginationConfig = ctx.data()?;
                 let limits = pagination.limits("ProgrammableTransaction", "inputs");
                 let page = Page::from_params(limits, first, after, last, before)?;
+
+                let resolver = self.scope.package_resolver();
+                let pure_layouts = match resolver.pure_input_layouts(&self.native).await {
+                    Ok(layouts) => layouts,
+                    Err(_) => vec![None; self.native.inputs.len()],
+                };
 
                 page.paginate_indices(self.native.inputs.len(), |i| {
                     Ok(TransactionInput::from(
                         self.native.inputs[i].clone(),
+                        pure_layouts[i].clone(),
                         self.scope.clone(),
                     ))
                 })
@@ -67,7 +74,7 @@ impl ProgrammableTransaction {
     ) -> Option<Result<Connection<String, Command>, RpcError>> {
         Some(
             async {
-                let pagination = ctx.data::<PaginationConfig>()?;
+                let pagination: &PaginationConfig = ctx.data()?;
                 let limits = pagination.limits("ProgrammableTransaction", "commands");
                 let page = Page::from_params(limits, first, after, last, before)?;
 
