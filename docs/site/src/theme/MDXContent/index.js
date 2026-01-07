f17a90f50e713358048b7e3f0d1ecdda52877703
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2026-01-07 16:45:32 -0600

    fix subtrees

--- docs/site/src/theme/MDXContent/index.js
+++ docs/site/src/theme/MDXContent/index.js
@@ -1,37 +1,37 @@
+/*
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
+*/
 import React from "react";
-import { MDXProvider } from "@mdx-js/react";
-import MDXComponents from "@theme/MDXComponents";
+import MDXComponentsOriginal from "@theme-original/MDXComponents";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
 import { Card, Cards } from "@site/src/shared/components/Cards";
-import EffortBox from "@site/src/components/EffortBox";
-import BetaTag from "@site/src/components/BetaTag";
 import CodeBlock from "@theme/CodeBlock";
 import DocCardList from "@theme/DocCardList";
-import ProtocolConfig from "@site/src/components/ProtocolConfig";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import UnsafeLink from "@site/src/shared/components/UnsafeLink";
 import RelatedLink from "@site/src/shared/components/RelatedLink";
 import ImportContent from "@site/src/shared/components/ImportContent";
 
-export default function MDXContent({ children }) {
-  const suiComponents = {
-    ...MDXComponents,
-    Card,
-    Cards,
-    Tabs,
-    TabItem,
-    EffortBox,
-    BetaTag,
-    CodeBlock,
-    DocCardList,
-    ProtocolConfig,
-    BrowserOnly,
-    UnsafeLink,
-    RelatedLink,
-    ImportContent,
-  };
-  return <MDXProvider components={suiComponents}>{children}</MDXProvider>;
-}
+// Site-specific components - these may need to stay in each site
+// import EffortBox from "@site/src/components/EffortBox";
+// import BetaTag from "@site/src/components/BetaTag";
+// import ProtocolConfig from "@site/src/components/ProtocolConfig";
+
+export default {
+  ...MDXComponentsOriginal,
+  Card,
+  Cards,
+  Tabs,
+  TabItem,
+  CodeBlock,
+  DocCardList,
+  BrowserOnly,
+  UnsafeLink,
+  RelatedLink,
+  ImportContent,
+  // EffortBox,
+  // BetaTag,
+  // ProtocolConfig,
+};
