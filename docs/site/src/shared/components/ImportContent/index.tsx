ee5ba494690da1d73b18e4aa043a65c179b067d4
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2026-01-07 17:01:14 -0600

    fix subtrees

--- docs/site/src/shared/components/ImportContent/index.tsx
+++ docs/site/src/shared/components/ImportContent/index.tsx
@@ -6,7 +6,8 @@ import React from "react";
 import CodeBlock from "@theme/CodeBlock";
 import utils from "./utils";
 import MarkdownIt from "markdown-it";
-import { importContentMap } from "@site/.generated/ImportContentMap";
+
+import { importContentMap } from "../../../.generated/ImportContentMap";
 
 /// <reference types="webpack-env" />
 
