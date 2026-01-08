a16b6fef736babfb893279be545ad86121b06b2b
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2026-01-08 11:22:46 -0600

    misc

--- docs/site/src/theme/MDXComponents/index.jsx
+++ docs/site/src/theme/MDXComponents/index.jsx
@@ -1,11 +1,42 @@
+/*
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-//
-// This code ensures <Tabs> <TabItems> are in the MDX scope globally
-
+*/
 import React from "react";
-import MDXComponents from "@theme-original/MDXComponents";
+import MDXComponentsOriginal from "@theme-original/MDXComponents";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
+import { Card, Cards } from "@site/src/shared/components/Cards";
+import CodeBlock from "@theme/CodeBlock";
+import DocCardList from "@theme/DocCardList";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import UnsafeLink from "@site/src/shared/components/UnsafeLink";
+import RelatedLink from "@site/src/shared/components/RelatedLink";
+import ImportContent from "@site/src/shared/components/ImportContent";
+
+// Debug: Log what each import actually is
+console.log("[MDXComponents Debug]");
+console.log("Tabs:", typeof Tabs, Tabs);
+console.log("TabItem:", typeof TabItem, TabItem);
+console.log("Card:", typeof Card, Card);
+console.log("Cards:", typeof Cards, Cards);
+console.log("CodeBlock:", typeof CodeBlock, CodeBlock);
+console.log("DocCardList:", typeof DocCardList, DocCardList);
+console.log("BrowserOnly:", typeof BrowserOnly, BrowserOnly);
+console.log("UnsafeLink:", typeof UnsafeLink, UnsafeLink);
+console.log("RelatedLink:", typeof RelatedLink, RelatedLink);
+console.log("ImportContent:", typeof ImportContent, ImportContent);
 
 export default {
-  ...MDXComponents,
+  ...MDXComponentsOriginal,
+  Card,
+  Cards,
+  Tabs,
+  TabItem,
+  CodeBlock,
+  DocCardList,
+  BrowserOnly,
+  UnsafeLink,
+  RelatedLink,
+  ImportContent,
 };
--- docs/site/src/theme/MDXContent/index.js
+++ docs/site/src/theme/MDXContent/index.js
@@ -1,37 +1,12 @@
-/*
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-*/
-import React from "react";
-import MDXComponentsOriginal from "@theme-original/MDXComponents";
-import Tabs from "@theme/Tabs";
-import TabItem from "@theme/TabItem";
-import { Card, Cards } from "@site/src/shared/components/Cards";
-import CodeBlock from "@theme/CodeBlock";
-import DocCardList from "@theme/DocCardList";
-import BrowserOnly from "@docusaurus/BrowserOnly";
-import UnsafeLink from "@site/src/shared/components/UnsafeLink";
-import RelatedLink from "@site/src/shared/components/RelatedLink";
-import ImportContent from "@site/src/shared/components/ImportContent";
-
-// Site-specific components - these may need to stay in each site
-// import EffortBox from "@site/src/components/EffortBox";
-// import BetaTag from "@site/src/components/BetaTag";
-// import ProtocolConfig from "@site/src/components/ProtocolConfig";
-
-export default {
-  ...MDXComponentsOriginal,
-  Card,
-  Cards,
-  Tabs,
-  TabItem,
-  CodeBlock,
-  DocCardList,
-  BrowserOnly,
-  UnsafeLink,
-  RelatedLink,
-  ImportContent,
-  // EffortBox,
-  // BetaTag,
-  // ProtocolConfig,
-};
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from 'react';
+import {MDXProvider} from '@mdx-js/react';
+import MDXComponents from '@theme/MDXComponents';
+export default function MDXContent({children}) {
+  return <MDXProvider components={MDXComponents}>{children}</MDXProvider>;
+}
