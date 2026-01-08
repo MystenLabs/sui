6532e7c689303ce050072b640715e24793467189
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2026-01-08 11:30:00 -0600

    misc

--- docs/site/src/theme/MDXContent/index.js
+++ docs/site/src/theme/MDXContent/index.js
@@ -1,9 +1,8 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+/*
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+*/
+
 import React from 'react';
 import {MDXProvider} from '@mdx-js/react';
 import MDXComponents from '@theme/MDXComponents';
