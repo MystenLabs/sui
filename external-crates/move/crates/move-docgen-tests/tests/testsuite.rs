c30f1ebb41d8703779552ca41859cf076aa1fefe
Michael George <mike.george@mystenlabs.com>
2026-01-13 15:19:20 -0500

    missed a rename

--- external-crates/move/crates/move-docgen-tests/tests/testsuite.rs
+++ external-crates/move/crates/move-docgen-tests/tests/testsuite.rs
@@ -51,7 +51,7 @@ fn test_impl(toml_path: &Path, flags: DocgenFlags, test_case: &str) -> datatest_
     let mut w = Vec::new();
 
     // Block on the async function
-    let env = move_package_alt::flavor::vanilla::default_environment();
+    let env = Vanilla::default_environment();
     let root_pkg: RootPackage<Vanilla> = config
         .package_loader(toml_path.parent().unwrap(), &env)
         .load_sync()?;
