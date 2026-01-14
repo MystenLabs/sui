69171329724ea9a78d8de7743788274a70812e38
Todd Nowacki <tmn@mystenlabs.com>
2026-01-14 11:42:16 -0800

    cp snap files

--- external-crates/move/crates/bytecode-verifier-transactional-tests/tests/tests.rs
+++ external-crates/move/crates/bytecode-verifier-transactional-tests/tests/tests.rs
@@ -2,13 +2,8 @@
 // Copyright (c) The Move Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-pub const TEST_DIR: &str = "tests";
 use move_transactional_test_runner::vm_test_harness::run_test;
 
-datatest_stable::harness!(run_test, TEST_DIR, r".*\.(mvir|move)$");
+pub const TEST_DIR: &str = "tests";
 
-datatest_stable::harness!(
-    run_test_with_regex_reference_safety,
-    TEST_DIR,
-    r".*\.(mvir|move)$"
-);
+datatest_stable::harness!(run_test, TEST_DIR, r".*\.(mvir|move)$");
