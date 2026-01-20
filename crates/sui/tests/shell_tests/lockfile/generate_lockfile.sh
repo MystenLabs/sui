e4328c10a00d3f89cc3a0d2db917eef7236af8c1
Michael George <mike.george@mystenlabs.com>
2026-01-20 12:02:23 -0500

    windows

--- crates/sui/tests/shell_tests/lockfile/generate_lockfile.sh
+++ crates/sui/tests/shell_tests/lockfile/generate_lockfile.sh
@@ -6,7 +6,12 @@ sui move --client.config $CONFIG build -p a
 
 echo "Lockfile"
 echo "==="
-cat a/Move.lock
+# On windows, the paths in the lockfile contain `\` so when they are written to TOML, toml_edit uses
+# raw strings (' delimited) instead of regular strings (" delimited); thus we use `sed` instead of `cat`
+#
+# Note: we don't need to change the `\` because the shell tests do that
+# automatically on the entire output
+sed "s/'/\"/g" a/Move.lock
 cp a/Move.lock a/Move.lock.tmp
 echo "==="
 
