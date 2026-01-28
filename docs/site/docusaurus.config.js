88935140b17155ed132b54f2dcea8c9c894a4237
Jessie Mongeon <133128541+jessiemongeon1@users.noreply.github.com>
2026-01-28 17:34:36 -0600

    docs: Add Docusaurus subtree for shared components (#24827)
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] Indexing Framework:
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -5,9 +5,8 @@ import { fileURLToPath } from "url";
 import path from "path";
 import math from "remark-math";
 import katex from "rehype-katex";
-//import rehypeRawFiles from "./src/rehype/rehype-raw-only.mjs";
-//import rehypeTabsMd from "./src/rehype/rehype-tabs.mjs";
-//import rehypeFixAnchorUrls from "./src/rehype/rehype-fix-anchor-urls.mjs";
+import remarkGlossary from "./src/shared/plugins/remark-glossary.js";
+
 const npm2yarn = require("@docusaurus/remark-plugin-npm2yarn");
 
 const effortRemarkPlugin = require("./src/plugins/effort");
@@ -56,7 +55,7 @@ const config = {
     //require.resolve('./src/plugins/framework'),
     "docusaurus-plugin-copy-page-button",
     [
-      require.resolve("./src/plugins/plausible"),
+      require.resolve("./src/shared/plugins/plausible"),
       {
         domain: "docs.sui.io",
         enableInDev: false,
@@ -83,7 +82,7 @@ const config = {
                     {
                       loader: path.resolve(
                         __dirname,
-                        "./src/plugins/inject-code/stepLoader.js",
+                        "./src/shared/plugins/inject-code/stepLoader.js",
                       ),
                     },
                   ],
@@ -117,7 +116,7 @@ const config = {
         },
       },
     ],
-    //require.resolve("./src/plugins/tabs-md-client/index.mjs"),
+    //require.resolve("./src/shared/plugins/tabs-md-client/index.mjs"),
     async function myPlugin(context, options) {
       return {
         name: "docusaurus-tailwindcss",
@@ -129,7 +128,7 @@ const config = {
         },
       };
     },
-    path.resolve(__dirname, `./src/plugins/descriptions`),
+    path.resolve(__dirname, `./src/shared/plugins/descriptions`),
     path.resolve(__dirname, `./src/plugins/framework`),
     path.resolve(__dirname, `./src/plugins/askcookbook`),
     path.resolve(__dirname, `./src/plugins/protocol`),
@@ -160,9 +159,8 @@ const config = {
             [npm2yarn, { sync: true, converters: ["yarn", "pnpm"] }],
             effortRemarkPlugin,
             betaRemarkPlugin,
+            [remarkGlossary, { glossaryFile: path.resolve(__dirname, "static/glossary.json") }],
           ],
-          //beforeDefaultRehypePlugins: [rehypeFixAnchorUrls],
-          //rehypePlugins: [katex, rehypeRawFiles, rehypeTabsMd],
           rehypePlugins: [katex],
         },
         theme: {
@@ -172,6 +170,9 @@ const config = {
             require.resolve("./src/css/details.css"),
           ],
         },
+        pages: {
+          remarkPlugins: [[remarkGlossary, { glossaryFile: path.resolve(__dirname, "static/glossary.json") }]],
+        }
       },
     ],
   ],
