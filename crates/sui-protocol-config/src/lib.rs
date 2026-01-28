c235a7a008ea69b65caa578de6644fb13a2459a1
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2026-01-28 08:11:53 -0800

    wip

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -2849,8 +2849,8 @@ impl ProtocolConfig {
             event_emit_auth_stream_cost: None,
 
             // `accumulator` module
-            accumulator_emit_cost_base: Some(52),
-            accumulator_pending_balance_cost_base: Some(52),
+            accumulator_emit_cost_base: None,
+            accumulator_pending_balance_cost_base: None,
 
             //  `object` module
             // Cost params for the Move native function `borrow_uid<T: key>(obj: &T): &UID`
@@ -4524,6 +4524,9 @@ impl ProtocolConfig {
                     }
                     cfg.feature_flags.validate_zklogin_public_identifier = true;
                     cfg.feature_flags.fix_checkpoint_signature_mapping = true;
+
+                    cfg.accumulator_emit_cost_base = Some(52);
+                    cfg.accumulator_pending_balance_cost_base = Some(52);
                 }
                 // Use this template when making changes:
                 //
@@ -5089,10 +5092,9 @@ mod test {
         );
 
         // We didnt have this in version 1
-        assert!(
-            prot.lookup_attr("max_move_identifier_len".to_string())
-                .is_none()
-        );
+        assert!(prot
+            .lookup_attr("max_move_identifier_len".to_string())
+            .is_none());
 
         // But we did in version 9
         let prot: ProtocolConfig =
@@ -5105,12 +5107,11 @@ mod test {
         let prot: ProtocolConfig =
             ProtocolConfig::get_for_version(ProtocolVersion::new(1), Chain::Unknown);
         // We didnt have this in version 1
-        assert!(
-            prot.attr_map()
-                .get("max_move_identifier_len")
-                .unwrap()
-                .is_none()
-        );
+        assert!(prot
+            .attr_map()
+            .get("max_move_identifier_len")
+            .unwrap()
+            .is_none());
         // We had this in version 1
         assert!(
             prot.attr_map().get("max_arguments").unwrap()
@@ -5121,17 +5122,14 @@ mod test {
         let prot: ProtocolConfig =
             ProtocolConfig::get_for_version(ProtocolVersion::new(1), Chain::Unknown);
         // Does not exist
-        assert!(
-            prot.feature_flags
-                .lookup_attr("some random string".to_owned())
-                .is_none()
-        );
-        assert!(
-            !prot
-                .feature_flags
-                .attr_map()
-                .contains_key("some random string")
-        );
+        assert!(prot
+            .feature_flags
+            .lookup_attr("some random string".to_owned())
+            .is_none());
+        assert!(!prot
+            .feature_flags
+            .attr_map()
+            .contains_key("some random string"));
 
         // Was false in v1
         assert!(
