ea200ae5cefcc7f5f96651a5d40c96117f0619ed
wlmyng <127570466+wlmyng@users.noreply.github.com>
2026-01-28 13:01:53 -0800

    [jsonrpc-alt] port coins to consistent store (#24558)
    Update existing reads against `coin_balance_buckets` to instead read
    from the Consistent Store using the consistent reader. This has several
    benefits:
    1. We can deprecate the `coin_balance_buckets` pipeline.
    2. Coins returned are fully ordered by descending balance order, rather
    than inconsistent balances within the same magnitude.
    3. Enables forking tool to read from forked data in consistent-store:
    DelegationCoin apis don't need to delegate to fullnode anymore, as the
    Consistent Store can answer balance-related questions.
    
    We'll need to provide consistent reader args and redeploy our
    jsonrpc-alt service.
    
    - Update existing test snapshots
    - replace existing fullnode delegation tests with balance-specific
    tests, notably around transfer and delete (merge)
    
    ---
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol:
    - [ ] Nodes (Validators and Full nodes):
    - [ ] gRPC:
    - [] JSON-RPC:
    - [ ] GraphQL:
    - [ ] CLI:
    - [ ] Rust SDK:
    - [ ] Indexing Framework:

--- crates/sui-indexer-alt-jsonrpc/src/context.rs
+++ crates/sui-indexer-alt-jsonrpc/src/context.rs
@@ -7,6 +7,8 @@ use async_graphql::dataloader::DataLoader;
 use prometheus::Registry;
 use sui_indexer_alt_reader::bigtable_reader::BigtableArgs;
 use sui_indexer_alt_reader::bigtable_reader::BigtableReader;
+use sui_indexer_alt_reader::consistent_reader::ConsistentReader;
+use sui_indexer_alt_reader::consistent_reader::ConsistentReaderArgs;
 use sui_indexer_alt_reader::kv_loader::KvLoader;
 use sui_indexer_alt_reader::package_resolver::DbPackageStore;
 use sui_indexer_alt_reader::package_resolver::PackageCache;
@@ -21,6 +23,9 @@ use crate::metrics::RpcMetrics;
 /// A bundle of different interfaces to data, for use by JSON-RPC method implementations.
 #[derive(Clone)]
 pub(crate) struct Context {
+    /// Access to the Consistent Store.
+    consistent_reader: ConsistentReader,
+
     /// Direct access to the database, for running SQL queries.
     pg_reader: PgReader,
 
@@ -54,6 +59,7 @@ impl Context {
         bigtable_instance: Option<String>,
         db_args: DbArgs,
         bigtable_args: BigtableArgs,
+        consistent_reader_args: ConsistentReaderArgs,
         config: RpcConfig,
         metrics: Arc<RpcMetrics>,
         registry: &Registry,
@@ -81,7 +87,12 @@ impl Context {
             config.package_resolver.clone(),
         ));
 
+        let consistent_reader =
+            ConsistentReader::new(Some("jsonrpc_consistent"), consistent_reader_args, registry)
+                .await?;
+
         Ok(Self {
+            consistent_reader,
             pg_reader,
             pg_loader,
             kv_loader,
@@ -89,6 +100,11 @@ impl Context {
             metrics,
             config: Arc::new(config),
         })
+    }
+
+    /// For performing reads against the Consistent Store.
+    pub(crate) fn consistent_reader(&self) -> &ConsistentReader {
+        &self.consistent_reader
     }
 
     /// For performing arbitrary SQL queries on the Postgres db.
@@ -101,9 +117,8 @@ impl Context {
         &self.pg_loader
     }
 
-    /// For performing point look-ups on the kv store.
-    /// Depends on the configuration of the indexer, the kv store may be backed by
-    /// eitherBigtable or Postgres.
+    /// For performing point look-ups on the kv store. Depends on the configuration of the indexer,
+    /// the kv store may be backed by either Bigtable or Postgres.
     pub(crate) fn kv_loader(&self) -> &KvLoader {
         &self.kv_loader
     }
