03e6ae4599573cac47f5b602781245aaef440a06
Nick Vikeras <nick.vikeras@mystenlabs.com>
2026-02-19 15:57:43 -0600

    increase MAX_PENDING_ROWS for busy pipelines (#25543)
    ## Description
    
    These are just based on observed reasonable values from testing

--- crates/sui-kvstore/src/handlers/transactions.rs
+++ crates/sui-kvstore/src/handlers/transactions.rs
@@ -55,4 +55,5 @@ impl Processor for TransactionsPipeline {
 impl BigTableProcessor for TransactionsPipeline {
     const TABLE: &'static str = tables::transactions::NAME;
     const FANOUT: usize = 100;
+    const MAX_PENDING_ROWS: usize = 100_000;
 }
