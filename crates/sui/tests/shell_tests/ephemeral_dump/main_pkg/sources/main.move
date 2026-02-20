f6e321200015fb9e5c27f45a3b7859134de17b55
Michael George <mike.george@mystenlabs.com>
2026-02-20 11:34:32 -0500

    checked output bytecode for addresses; moved missing pubfile tests into with_network (with a TODO)

--- crates/sui/tests/shell_tests/ephemeral_dump/main_pkg/sources/main.move
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-module main_pkg::main {
-
-    public fun f(): u64 {
-        0
-    }
-}
---
+++ crates/sui/tests/shell_tests/with_network/ephemeral_dump/dump_with_missing_pubfile.sh
@@ -0,0 +1,35 @@
+# Copyright (c) Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# Test sui move build --dump with --pubfile-path for ephemeral publication
+# where the pubfile doesn't exist; this should fail without `-e`, and succeed with `-e`
+#
+# TODO dvx-1992: this only works because the package has no implicit
+#                dependencies; otherwise they would need to be in the pubfile
+
+# Build with --dump using nonexisting ephemeral pubfile and no build environment
+echo "=== should fail because of unknown build-env ==="
+sui move --client.config $CONFIG \
+  build -p dep_pkg \
+  --dump --pubfile-path Pub.missing.toml --no-tree-shaking \
+  2>&1 > output.txt || cat output.txt
+
+# Build with --dump using build environment
+echo
+echo "=== should succeed ==="
+sui move --client.config $CONFIG \
+  build -p dep_pkg \
+  --dump --pubfile-path Pub.missing.toml --no-tree-shaking \
+  -e testnet \
+  2>&1 > output.json || cat output.json
+
+cat output.json | sed 's/.*"modules":\["\([^"]*\)".*/\1/' | base64 -d > main.mv
+sui move disassemble main.mv > main.move
+
+echo
+echo "=== decompiled bytecode should have main module at address 0 ==="
+grep module main.move
+
+echo
+echo "=== Pubfile should not have been written ==="
+# TODO DVX-2009: cat Pub.missing.toml
