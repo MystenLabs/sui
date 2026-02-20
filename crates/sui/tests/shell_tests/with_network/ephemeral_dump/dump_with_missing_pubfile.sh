98ce138e331dc4650aa034a32f915d5023fcf6b3
Michael George <mike.george@mystenlabs.com>
2026-02-20 12:26:01 -0500

    redaction

--- crates/sui/tests/shell_tests/with_network/ephemeral_dump/dump_with_missing_pubfile.sh
+++ crates/sui/tests/shell_tests/with_network/ephemeral_dump/dump_with_missing_pubfile.sh
@@ -8,11 +8,12 @@
 #                dependencies; otherwise they would need to be in the pubfile
 
 # Build with --dump using nonexisting ephemeral pubfile and no build environment
+# TODO dvx-1994: redaction is needed because error message contains localnet chain ID
 echo "=== should fail because of unknown build-env ==="
 sui move --client.config $CONFIG \
   build -p dep_pkg \
   --dump --pubfile-path Pub.missing.toml --no-tree-shaking \
-  2>&1 > output.txt || cat output.txt
+  2>&1 | grep -v "localnet = "
 
 # Build with --dump using build environment
 echo
