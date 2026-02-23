0fbef8cf99ed39877110a6e95f9ff8c35ec0c0f1
andll <andll@danasoft.ws>
2026-02-23 14:36:30 -0800

    Fix sequence_key key type

--- crates/sui-core/src/authority/authority_per_epoch_store.rs
+++ crates/sui-core/src/authority/authority_per_epoch_store.rs
@@ -647,14 +647,13 @@ impl AuthorityEpochTables {
         let bloom_config = KeySpaceConfig::new().with_bloom_filter(0.001, 32_000);
         let lru_bloom_config = bloom_config.clone().with_value_cache_size(value_cache_size);
         let lru_only_config = KeySpaceConfig::new().with_value_cache_size(value_cache_size);
-        let pending_checkpoint_signatures_config = KeySpaceConfig::new()
+        let builder_checkpoint_summary_v2_config = KeySpaceConfig::new()
             .disable_unload()
             .with_value_cache_size(default_value_cache_size());
-        let builder_checkpoint_summary_v2_config = pending_checkpoint_signatures_config.clone();
         let object_ref_indexing = KeyIndexing::Hash;
         let tx_digest_indexing = KeyIndexing::key_reduction(32, 0..16);
         let uniform_key = KeyType::uniform(default_cells_per_mutex());
-        let sequence_key = KeyType::from_prefix_bits(1 * 8 + 4);
+        let sequence_key = KeyType::from_prefix_bits(6 * 8 + 4);
         let configs = vec![
             (
                 "signed_transactions".to_string(),
@@ -764,7 +763,7 @@ impl AuthorityEpochTables {
                     8 + 8,
                     mutexes,
                     sequence_key,
-                    pending_checkpoint_signatures_config,
+                    KeySpaceConfig::default(),
                 ),
             ),
             (
