MystenLabs/sui

commit 3a5fd2a0aceb0e17b054dd055b7bdb4153b003e8

[move-compiler] Fix Pack/PackVariant ICE. Fix `assert` arity ICE (#25525)
## Description 

- #25453 PackVariant could be reached in HLIR with invalid arguments if
invoked with arguments (even if none were expected). As such, I made
several errors non-blocking and fixed the downstream issues.
- #25459 Fixed overly tight checks on assert arity. Like with
PackVariant, this could be reached in some cases even with the block
errors.

## Test plan 

- Added tests
- Updated tests 

---

## Release notes

Check each box that your changes affect. If none of the boxes relate to
your changes, release notes aren't required.

For each box you select, include information after the relevant heading
that describes the impact of your changes that a user might notice and
any actions they must take to implement updates.

- [ ] Protocol: 
- [ ] Nodes (Validators and Full nodes): 
- [ ] gRPC:
- [ ] JSON-RPC: 
- [ ] GraphQL: 
- [ ] CLI: 
- [ ] Rust SDK:
- [ ] Indexing Framework:

external-crates/move/crates/move-compiler/src/hlir/match_compilation.rs
@@ -157,7 +157,7 @@ fn build_match_tree(
         return MatchTree::Failure;
     }
 
-    if let Some(leaf) = matrix.wild_tree_opt(&fringe) {
+    if let Some(leaf) = matrix.wild_tree_opt(context.env, &mut context.reporter, &fringe) {
         debug_print!(context.debug.match_specialization, (msg "wild leaf"), ("matrix" => matrix));
         return MatchTree::Leaf(leaf);
     }

external-crates/move/crates/move-compiler/src/shared/matching.rs
@@ -5,7 +5,7 @@ use crate::{
     diagnostics::DiagnosticReporter,
     expansion::ast::{Fields, ModuleIdent, Mutability, Value},
     hlir::translate::NEW_NAME_DELIM,
-    ice,
+    ice, ice_assert,
     naming::ast::{self as N, Type, Var},
     parser::ast::{BinOp_, ConstantName, Field, VariantName},
     shared::{CompilationEnv, program_info::ProgramInfo, unique_map::UniqueMap},
@@ -154,9 +154,14 @@ impl PatternArm {
             .all(|pat| matches!(pat.pat.value, TP::Wildcard | TP::Binder(_, _)))
     }
 
-    fn all_wild_arm(&mut self, fringe: &VecDeque<FringeEntry>) -> Option<ArmResult> {
+    fn all_wild_arm<'env>(
+        &mut self,
+        env: &'env CompilationEnv,
+        reporter: &mut DiagnosticReporter<'env>,
+        fringe: &VecDeque<FringeEntry>,
+    ) -> Option<ArmResult> {
         if self.is_wild_arm() {
-            let bindings = self.make_arm_bindings(fringe);
+            let bindings = self.make_arm_bindings(env, reporter, fringe);
             let PatternArm {
                 pats: _,
                 guard,
@@ -174,9 +179,20 @@ impl PatternArm {
         }
     }
 
-    fn make_arm_bindings(&mut self, fringe: &VecDeque<FringeEntry>) -> PatBindings {
+    fn make_arm_bindings<'env>(
+        &mut self,
+        env: &'env CompilationEnv,
+        reporter: &mut DiagnosticReporter<'env>,
+        fringe: &VecDeque<FringeEntry>,
+    ) -> PatBindings {
         let mut bindings = BTreeMap::new();
-        assert!(self.pats.len() == fringe.len());
+        // If the lengths don't match, we have an error from typing
+        ice_assert!(
+            reporter,
+            self.pats.len() == fringe.len() || env.has_errors(),
+            self.arm.orig_pattern.pat.loc,
+            "mismatched length should have caused an error in typing"
+        );
         for (pmut, subject) in self.pats.iter_mut().zip(fringe.iter()) {
             if let TP::Binder(mut_, x) = pmut.pat.value {
                 if bindings.insert(x, (mut_, subject.clone())).is_some() {
@@ -519,17 +535,22 @@ impl PatternMatrix {
             .any(|pat| pat.is_wild_arm() && pat.guard.is_none())
     }
 
-    pub fn wild_tree_opt(&mut self, fringe: &VecDeque<FringeEntry>) -> Option<Vec<ArmResult>> {
+    pub fn wild_tree_opt<'env>(
+        &mut self,
+        env: &'env CompilationEnv,
+        reporter: &mut DiagnosticReporter<'env>,
+        fringe: &VecDeque<FringeEntry>,
+    ) -> Option<Vec<ArmResult>> {
         // NB: If the first row is all wild, we need to collect _all_ wild rows that have guards
         // until we find one that does not. If we do not find one without a guard, then this isn't
         // a wild tree.
-        if let Some(arm) = self.patterns[0].all_wild_arm(fringe) {
+        if let Some(arm) = self.patterns[0].all_wild_arm(env, reporter, fringe) {
             if arm.guard.is_none() {
                 return Some(vec![arm]);
             }
             let mut result = vec![arm];
             for pat in self.patterns[1..].iter_mut() {
-                if let Some(arm) = pat.all_wild_arm(fringe) {
+                if let Some(arm) = pat.all_wild_arm(env, reporter, fringe) {
                     let has_guard = arm.guard.is_some();
                     result.push(arm);
                     if !has_guard {

external-crates/move/crates/move-compiler/tests/move_2024/typing/unbound_field.move
@@ -1,0 +1,36 @@
+module 0x0::m;
+
+public struct S has drop { a: u8 }
+public struct T has drop { a: u8, b: u8 }
+
+public enum E has drop {
+    A {},
+    B { a: u8 },
+    C { a: u8, b: u8 },
+}
+
+fun t() {
+    // an unbound field, but missing fields from the declaration
+    S { f: false };
+    T { f: false, a: 0 };
+    E::B { f: false };
+    E::C { a: 0, f: false };
+
+    // an extra unbound field
+    S { a: 0, f: false };
+    S { f: false, a: 0 };
+    T { a: 0, b: 0, f: false };
+    T { f: false, a: 0, b: 0 };
+    T { a: 0, f: false, b: 0 };
+    E::A { f: false };
+    E::B { a: 0, f: false };
+    E::B { f: false, a: 0 };
+    E::C { a: 0, b: 0, f: false };
+    E::C { a: 0, f: false, b: 0 };
+    E::C { f: false, a: 0, b: 0 };
+
+    // A small use after move error to make sure we get this far in the compiler
+    let x = 0u64;
+    move x;
+    move x;
+}

external-crates/move/crates/move-compiler/tests/move_2024/typing/unbound_field.snap
@@ -1,0 +1,131 @@
+---
+source: crates/move-compiler/tests/move_check_testsuite.rs
+info:
+  flavor: core
+  edition: 2024.alpha
+  lint: false
+---
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:14:5
+   │
+14 │     S { f: false };
+   │     ^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::S'
+
+error[E04016]: too few arguments
+   ┌─ tests/move_2024/typing/unbound_field.move:14:5
+   │
+14 │     S { f: false };
+   │     ^^^^^^^^^^^^^^ Missing argument for field 'a' in '0x0::m::S'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:15:5
+   │
+15 │     T { f: false, a: 0 };
+   │     ^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E04016]: too few arguments
+   ┌─ tests/move_2024/typing/unbound_field.move:15:5
+   │
+15 │     T { f: false, a: 0 };
+   │     ^^^^^^^^^^^^^^^^^^^^ Missing argument for field 'b' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:16:5
+   │
+16 │     E::B { f: false };
+   │     ^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::B'
+
+error[E04016]: too few arguments
+   ┌─ tests/move_2024/typing/unbound_field.move:16:5
+   │
+16 │     E::B { f: false };
+   │     ^^^^^^^^^^^^^^^^^ Missing argument for field 'a' in '0x0::m::E::B'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:17:5
+   │
+17 │     E::C { a: 0, f: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::C'
+
+error[E04016]: too few arguments
+   ┌─ tests/move_2024/typing/unbound_field.move:17:5
+   │
+17 │     E::C { a: 0, f: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^ Missing argument for field 'b' in '0x0::m::E::C'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:20:5
+   │
+20 │     S { a: 0, f: false };
+   │     ^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::S'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:21:5
+   │
+21 │     S { f: false, a: 0 };
+   │     ^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::S'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:22:5
+   │
+22 │     T { a: 0, b: 0, f: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:23:5
+   │
+23 │     T { f: false, a: 0, b: 0 };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:24:5
+   │
+24 │     T { a: 0, f: false, b: 0 };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:25:5
+   │
+25 │     E::A { f: false };
+   │     ^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::A'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:26:5
+   │
+26 │     E::B { a: 0, f: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::B'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:27:5
+   │
+27 │     E::B { f: false, a: 0 };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::B'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:28:5
+   │
+28 │     E::C { a: 0, b: 0, f: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::C'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:29:5
+   │
+29 │     E::C { a: 0, f: false, b: 0 };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::C'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:30:5
+   │
+30 │     E::C { f: false, a: 0, b: 0 };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::C'
+
+error[E06002]: use of unassigned variable
+   ┌─ tests/move_2024/typing/unbound_field.move:35:5
+   │
+34 │     move x;
+   │     ------
+   │     │
+   │     The value of 'x' was previously moved here.
+   │     Suggestion: use 'copy x' to avoid the move.
+35 │     move x;
+   │     ^^^^^^ Invalid usage of previously moved variable 'x'.

external-crates/move/crates/move-compiler/src/naming/translate.rs
@@ -4282,20 +4282,22 @@ fn resolve_call(
                     }
                 }
                 B::Assert(_) => {
+                    let function_call_help = || {
+                        format!(
+                            "Replace with '{0}!'. '{0}' has been replaced with a '{0}!' built-in \
+                            macro so that arguments are no longer eagerly evaluated",
+                            B::ASSERT_MACRO
+                        )
+                    };
                     if is_macro.is_none() {
                         let dep_msg = format!(
                             "'{}' function syntax has been deprecated and will be removed",
                             B::ASSERT_MACRO
                         );
-                        // TODO make this a tip/hint?
-                        let help_msg = format!(
-                            "Replace with '{0}!'. '{0}' has been replaced with a '{0}!' built-in \
-                            macro so that arguments are no longer eagerly evaluated",
-                            B::ASSERT_MACRO
-                        );
                         let mut diag =
                             diag!(Uncategorized::DeprecatedWillBeRemoved, (call_loc, dep_msg),);
-                        diag.add_note(help_msg);
+                        // TODO make this a tip/hint?
+                        diag.add_note(function_call_help());
                         context.add_diag(diag);
                     }
                     exp_types_opt_with_arity_check(
@@ -4308,12 +4310,22 @@ fn resolve_call(
                     );
                     // If no abort code is given for the assert, we add in the abort code as the
                     // bitset-line-number if `CleverAssertions` is set.
-                    if args.value.len() == 1 && is_macro.is_some() {
-                        context.check_feature(
+                    if args.value.len() == 1 {
+                        let is_supported = context.check_feature(
                             context.current_package,
                             FeatureGate::CleverAssertions,
                             subject_loc,
                         );
+                        if is_supported && is_macro.is_none() {
+                            let dep_msg = format!(
+                                "'{}' function syntax has been deprecated and cannot be used with clever assertions",
+                                B::ASSERT_MACRO
+                            );
+                            let mut diag = diag!(Editions::DeprecatedFeature, (call_loc, dep_msg));
+                            // TODO make this a tip/hint?
+                            diag.add_note(function_call_help());
+                            context.add_diag(diag);
+                        }
                         args.value.push(sp(
                             call_loc,
                             N::Exp_::ErrorConstant {

commit 7d2a6613f28b1228c0f706426ce13c3381f2a116

[move-compiler] Fix more issues with decl field indicies (#25565)
## Description 

- Fixes issue introduced in #25525 (whoops)
- Adds missing test from #25525 (whoops)
- Fixes usage of the context in matching
- Fixes #25548 in the same manner as #25525

## Test plan 

- New tests

---

## Release notes

Check each box that your changes affect. If none of the boxes relate to
your changes, release notes aren't required.

For each box you select, include information after the relevant heading
that describes the impact of your changes that a user might notice and
any actions they must take to implement updates.

- [ ] Protocol: 
- [ ] Nodes (Validators and Full nodes): 
- [ ] gRPC:
- [ ] JSON-RPC: 
- [ ] GraphQL: 
- [ ] CLI: 
- [ ] Rust SDK:
- [ ] Indexing Framework:

external-crates/move/crates/move-compiler/src/hlir/match_compilation.rs
@@ -157,7 +157,7 @@ fn build_match_tree(
         return MatchTree::Failure;
     }
 
-    if let Some(leaf) = matrix.wild_tree_opt(context.env, &mut context.reporter, &fringe) {
+    if let Some(leaf) = matrix.wild_tree_opt(context, &fringe) {
         debug_print!(context.debug.match_specialization, (msg "wild leaf"), ("matrix" => matrix));
         return MatchTree::Leaf(leaf);
     }
@@ -837,7 +837,7 @@ fn make_arm_variant_unpack_fields(
             .make_unpack_binders(decl_fields.clone(), pat_loc, field_tys);
     let fringe_exps = make_fringe_entries(&fringe_binders);
 
-    let ordered_pats = order_fields_by_decl(decl_fields, field_pats);
+    let ordered_pats = order_fields_by_decl(context.hlir_context, decl_fields, field_pats);
 
     let mut unpack_fields: Vec<(Field, Var, Type)> = vec![];
     assert!(fringe_exps.len() == ordered_pats.len());
@@ -893,7 +893,7 @@ fn make_arm_struct_unpack_fields(
             .make_unpack_binders(decl_fields.clone(), pat_loc, field_tys);
     let fringe_exps = make_fringe_entries(&fringe_binders);
 
-    let ordered_pats = order_fields_by_decl(decl_fields, field_pats);
+    let ordered_pats = order_fields_by_decl(context.hlir_context, decl_fields, field_pats);
 
     let mut unpack_fields: Vec<(Field, Var, Type)> = vec![];
     assert!(fringe_exps.len() == ordered_pats.len());

external-crates/move/crates/move-compiler/src/hlir/translate.rs
@@ -1543,7 +1543,7 @@ fn value_fields(
                         "field '{}' is unbound but there are no errors",
                         f,
                     );
-                    let decl_idx = decl_idx_opt.unwrap_or(field_len);
+                    let decl_idx = decl_idx_opt.unwrap_or(field_len + exp_idx);
                     (decl_idx, f, exp_idx, bt, tf)
                 })
                 .collect()

external-crates/move/crates/move-compiler/src/shared/matching.rs
@@ -8,7 +8,7 @@ use crate::{
     ice, ice_assert,
     naming::ast::{self as N, Type, Var},
     parser::ast::{BinOp_, ConstantName, Field, VariantName},
-    shared::{CompilationEnv, program_info::ProgramInfo, unique_map::UniqueMap},
+    shared::{CompilationEnv, Identifier, program_info::ProgramInfo, unique_map::UniqueMap},
     typing::ast::{self as T, MatchArm_, MatchPattern, UnannotatedPat_ as TP},
 };
 use move_ir_types::location::*;
@@ -65,7 +65,7 @@ pub struct ArmResult {
 
 /// A shared match context trait for use with counterexample generation in Typing and match
 /// compilation in HLIR lowering.
-pub trait MatchContext<const AFTER_TYPING: bool> {
+pub trait MatchContext<const AFTER_TYPING: bool>: Sized {
     fn env(&self) -> &CompilationEnv;
     fn reporter(&self) -> &DiagnosticReporter<'_>;
     fn new_match_var(&mut self, name: String, loc: Loc) -> N::Var;
@@ -94,7 +94,7 @@ pub trait MatchContext<const AFTER_TYPING: bool> {
             }
         }
 
-        let fields = order_fields_by_decl(decl_fields, arg_types.clone());
+        let fields = order_fields_by_decl(self, decl_fields, arg_types.clone());
         fields
             .into_iter()
             .map(|(_, field_name, field_type)| {
@@ -113,7 +113,7 @@ pub trait MatchContext<const AFTER_TYPING: bool> {
         pattern_loc: Loc,
         arg_types: Fields<N::Type>,
     ) -> Vec<(Field, N::Var, N::Type)> {
-        let fields = order_fields_by_decl(decl_fields, arg_types.clone());
+        let fields = order_fields_by_decl(self, decl_fields, arg_types.clone());
         fields
             .into_iter()
             .map(|(_, field_name, field_type)| {
@@ -154,14 +154,13 @@ impl PatternArm {
             .all(|pat| matches!(pat.pat.value, TP::Wildcard | TP::Binder(_, _)))
     }
 
-    fn all_wild_arm<'env>(
+    fn all_wild_arm<const AFTER_TYPING: bool, MC: MatchContext<AFTER_TYPING>>(
         &mut self,
-        env: &'env CompilationEnv,
-        reporter: &mut DiagnosticReporter<'env>,
+        context: &MC,
         fringe: &VecDeque<FringeEntry>,
     ) -> Option<ArmResult> {
         if self.is_wild_arm() {
-            let bindings = self.make_arm_bindings(env, reporter, fringe);
+            let bindings = self.make_arm_bindings(context, fringe);
             let PatternArm {
                 pats: _,
                 guard,
@@ -179,17 +178,16 @@ impl PatternArm {
         }
     }
 
-    fn make_arm_bindings<'env>(
+    fn make_arm_bindings<const AFTER_TYPING: bool, MC: MatchContext<AFTER_TYPING>>(
         &mut self,
-        env: &'env CompilationEnv,
-        reporter: &mut DiagnosticReporter<'env>,
+        context: &MC,
         fringe: &VecDeque<FringeEntry>,
     ) -> PatBindings {
         let mut bindings = BTreeMap::new();
         // If the lengths don't match, we have an error from typing
         ice_assert!(
-            reporter,
-            self.pats.len() == fringe.len() || env.has_errors(),
+            context.reporter(),
+            self.pats.len() == fringe.len() || context.env().has_errors(),
             self.arm.orig_pattern.pat.loc,
             "mismatched length should have caused an error in typing"
         );
@@ -320,7 +318,7 @@ impl PatternArm {
                     .program_info()
                     .enum_variant_fields(&mident, &enum_, &name)
                     .unwrap();
-                let ordered_pats = order_fields_by_decl(decl_fields, field_pats);
+                let ordered_pats = order_fields_by_decl(context, decl_fields, field_pats);
                 for (_, _, pat) in ordered_pats.into_iter().rev() {
                     output.pats.push_front(pat);
                 }
@@ -381,7 +379,7 @@ impl PatternArm {
                     .program_info()
                     .struct_fields(&mident, &struct_)
                     .unwrap();
-                let ordered_pats = order_fields_by_decl(decl_fields, field_pats);
+                let ordered_pats = order_fields_by_decl(context, decl_fields, field_pats);
                 for (_, _, pat) in ordered_pats.into_iter().rev() {
                     output.pats.push_front(pat);
                 }
@@ -560,22 +558,21 @@ impl PatternMatrix {
             .any(|pat| pat.is_wild_arm() && pat.guard.is_none())
     }
 
-    pub fn wild_tree_opt<'env>(
+    pub fn wild_tree_opt<const AFTER_TYPING: bool, MC: MatchContext<AFTER_TYPING>>(
         &mut self,
-        env: &'env CompilationEnv,
-        reporter: &mut DiagnosticReporter<'env>,
+        context: &MC,
         fringe: &VecDeque<FringeEntry>,
     ) -> Option<Vec<ArmResult>> {
         // NB: If the first row is all wild, we need to collect _all_ wild rows that have guards
         // until we find one that does not. If we do not find one without a guard, then this isn't
         // a wild tree.
-        if let Some(arm) = self.patterns[0].all_wild_arm(env, reporter, fringe) {
+        if let Some(arm) = self.patterns[0].all_wild_arm(context, fringe) {
             if arm.guard.is_none() {
                 return Some(vec![arm]);
             }
             let mut result = vec![arm];
             for pat in self.patterns[1..].iter_mut() {
-                if let Some(arm) = pat.all_wild_arm(env, reporter, fringe) {
+                if let Some(arm) = pat.all_wild_arm(context, fringe) {
                     let has_guard = arm.guard.is_some();
                     result.push(arm);
                     if !has_guard {
@@ -981,15 +978,35 @@ fn combine_pattern_fields(
 }
 
 /// Helper function for creating an ordered list of fields Field information and Fields.
-pub fn order_fields_by_decl<T: std::fmt::Debug>(
+pub fn order_fields_by_decl<
+    const AFTER_TYPING: bool,
+    MC: MatchContext<AFTER_TYPING>,
+    T: std::fmt::Debug,
+>(
+    context: &MC,
     decl_fields: UniqueMap<Field, usize>,
     fields: Fields<T>,
 ) -> Vec<(usize, Field, T)> {
+    let field_len = decl_fields.len();
     let mut texp_fields: Vec<(usize, Field, T)> = fields
         .into_iter()
-        .map(|(f, (_exp_idx, t))| (*decl_fields.get(&f).unwrap(), f, t))
+        .map(|(f, (exp_idx, t))| {
+            // If the field is not a valid one, typing will produce an error.
+            // So keep the field in a consistent order, but after all of the
+            // valid fields.
+            let decl_idx_opt = decl_fields.get(&f).copied();
+            ice_assert!(
+                context.reporter(),
+                decl_idx_opt.is_some() || context.env().has_errors(),
+                f.loc(),
+                "field '{}' is unbound but there are no errors",
+                f,
+            );
+            let decl_idx = decl_idx_opt.unwrap_or(field_len + exp_idx);
+            (decl_idx, f, t)
+        })
         .collect();
-    texp_fields.sort_by(|(decl_idx1, _, _), (decl_idx2, _, _)| decl_idx1.cmp(decl_idx2));
+    texp_fields.sort_by_key(|(decl_idx, _, _)| *decl_idx);
     texp_fields
 }
 

external-crates/move/crates/move-compiler/tests/move_2024/matching/mismatched_types.move
@@ -1,0 +1,10 @@
+module 0x42::m {
+    public struct P(bool) has drop;
+    public struct N { x: bool } has drop;
+
+    fun f(n: N): bool {
+        match (n) {
+            P(false) => false,
+        }
+    }
+}

external-crates/move/crates/move-compiler/tests/move_2024/matching/mismatched_types.snap
@@ -1,0 +1,24 @@
+---
+source: crates/move-compiler/tests/move_check_testsuite.rs
+info:
+  flavor: core
+  edition: 2024.alpha
+  lint: false
+---
+error[E04036]: non-exhaustive pattern
+  ┌─ tests/move_2024/matching/mismatched_types.move:6:16
+  │
+6 │         match (n) {
+  │                ^ Pattern 'N { 0: true }' not covered
+
+error[E04007]: incompatible types
+  ┌─ tests/move_2024/matching/mismatched_types.move:7:13
+  │
+5 │     fun f(n: N): bool {
+  │              - Expected: '0x42::m::N'
+6 │         match (n) {
+7 │             P(false) => false,
+  │             ^^^^^^^^
+  │             │
+  │             Invalid pattern
+  │             Given: '0x42::m::P'

external-crates/move/crates/move-compiler/tests/move_2024/typing/unbound_field.move
@@ -21,13 +21,13 @@ fun t() {
     S { f: false, a: 0 };
     T { a: 0, b: 0, f: false };
     T { f: false, a: 0, b: 0 };
-    T { a: 0, f: false, b: 0 };
+    T { a: 0, f: false, b: 0, g: false };
     E::A { f: false };
     E::B { a: 0, f: false };
     E::B { f: false, a: 0 };
     E::C { a: 0, b: 0, f: false };
     E::C { a: 0, f: false, b: 0 };
-    E::C { f: false, a: 0, b: 0 };
+    E::C { f: false, a: 0, b: 0, g: false };
 
     // A small use after move error to make sure we get this far in the compiler
     let x = 0u64;

external-crates/move/crates/move-compiler/tests/move_2024/typing/unbound_field.snap
@@ -80,8 +80,14 @@ error[E03010]: unbound field
 error[E03010]: unbound field
    ┌─ tests/move_2024/typing/unbound_field.move:24:5
    │
-24 │     T { a: 0, f: false, b: 0 };
-   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+24 │     T { a: 0, f: false, b: 0, g: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:24:5
+   │
+24 │     T { a: 0, f: false, b: 0, g: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'g' in '0x0::m::T'
 
 error[E03010]: unbound field
    ┌─ tests/move_2024/typing/unbound_field.move:25:5
@@ -116,8 +122,14 @@ error[E03010]: unbound field
 error[E03010]: unbound field
    ┌─ tests/move_2024/typing/unbound_field.move:30:5
    │
-30 │     E::C { f: false, a: 0, b: 0 };
-   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::C'
+30 │     E::C { f: false, a: 0, b: 0, g: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::E::C'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field.move:30:5
+   │
+30 │     E::C { f: false, a: 0, b: 0, g: false };
+   │     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Unbound field 'g' in '0x0::m::E::C'
 
 error[E06002]: use of unassigned variable
    ┌─ tests/move_2024/typing/unbound_field.move:35:5

external-crates/move/crates/move-compiler/tests/move_2024/typing/unbound_field_assignment.move
@@ -1,0 +1,39 @@
+module 0x0::m;
+
+public struct S has copy, drop { a: u8 }
+public struct T has copy, drop { a: u8, b: u8 }
+
+fun t(s: S, t: T) {
+    // an unbound field, but missing fields from the declaration
+    let S { f } = s;
+    f;
+    let T { f, a } = t;
+    f;
+    a;
+
+    // an extra unbound field
+    let S { a, f } = s;
+    a;
+    f;
+    let S { f, a } = s;
+    f;
+    a;
+    let T { a, b, f } = t;
+    a;
+    b;
+    f;
+    let T { f, a, b } = t;
+    f;
+    a;
+    b;
+    let T { a, f, b, g } = t;
+    a;
+    f;
+    b;
+    g;
+
+    // A small use after move error to make sure we get this far in the compiler
+    let x = 0u64;
+    move x;
+    move x;
+}

external-crates/move/crates/move-compiler/tests/move_2024/typing/unbound_field_assignment.snap
@@ -1,0 +1,77 @@
+---
+source: crates/move-compiler/tests/move_check_testsuite.rs
+info:
+  flavor: core
+  edition: 2024.alpha
+  lint: false
+---
+error[E03010]: unbound field
+  ┌─ tests/move_2024/typing/unbound_field_assignment.move:8:9
+  │
+8 │     let S { f } = s;
+  │         ^^^^^^^ Unbound field 'f' in '0x0::m::S'
+
+error[E04016]: too few arguments
+  ┌─ tests/move_2024/typing/unbound_field_assignment.move:8:9
+  │
+8 │     let S { f } = s;
+  │         ^^^^^^^ Missing binding for field 'a' in '0x0::m::S'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:10:9
+   │
+10 │     let T { f, a } = t;
+   │         ^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E04016]: too few arguments
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:10:9
+   │
+10 │     let T { f, a } = t;
+   │         ^^^^^^^^^^ Missing binding for field 'b' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:15:9
+   │
+15 │     let S { a, f } = s;
+   │         ^^^^^^^^^^ Unbound field 'f' in '0x0::m::S'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:18:9
+   │
+18 │     let S { f, a } = s;
+   │         ^^^^^^^^^^ Unbound field 'f' in '0x0::m::S'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:21:9
+   │
+21 │     let T { a, b, f } = t;
+   │         ^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:25:9
+   │
+25 │     let T { f, a, b } = t;
+   │         ^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:29:9
+   │
+29 │     let T { a, f, b, g } = t;
+   │         ^^^^^^^^^^^^^^^^ Unbound field 'f' in '0x0::m::T'
+
+error[E03010]: unbound field
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:29:9
+   │
+29 │     let T { a, f, b, g } = t;
+   │         ^^^^^^^^^^^^^^^^ Unbound field 'g' in '0x0::m::T'
+
+error[E06002]: use of unassigned variable
+   ┌─ tests/move_2024/typing/unbound_field_assignment.move:38:5
+   │
+37 │     move x;
+   │     ------
+   │     │
+   │     The value of 'x' was previously moved here.
+   │     Suggestion: use 'copy x' to avoid the move.
+38 │     move x;
+   │     ^^^^^^ Invalid usage of previously moved variable 'x'.