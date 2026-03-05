b47585e09f0ba0b5d551187d193e41a2fc72dfaf
andll <andll@danasoft.ws>
2026-03-05 12:18:50 -0800

    Reduce max_index_maps

--- crates/typed-store/src/tidehunter_util.rs
+++ crates/typed-store/src/tidehunter_util.rs
@@ -81,7 +81,7 @@ fn thdb_config() -> Config {
     let max_maps = 4;
     #[cfg(not(debug_assertions))]
     let max_maps = 8; // 8Gb of mapped space for prod
-    let max_index_maps = 3;
+    let max_index_maps = Some(3);
     #[cfg(debug_assertions)]
     let commit_pool_size = 0;
     #[cfg(not(debug_assertions))]
