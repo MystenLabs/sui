a0a246bb8cc10fec095e137950858a623ef16cba
Nick Vikeras <nick.vikeras@mystenlabs.com>
2026-03-06 11:49:02 -0600

    Remote store operator docs (#25702)
    ## Description
    
    Triton One was interested in running their own internal remote store, so
    documenting how to operate the indexer for them, and in case anyone else
    is interested.

--- docs/content/sidebars/guides.js
+++ docs/content/sidebars/guides.js
@@ -320,6 +320,8 @@ const guides = [
       'guides/operator/exchange-integration',
       'guides/operator/bridge-node-configuration',
       'guides/operator/indexer-stack-setup',
+      'guides/operator/archival-stack-setup',
+      'guides/operator/remote-store-setup',
       {
         type: 'category',
         label: 'Sui Validator Nodes',
