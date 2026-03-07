342e81b3c3f63790c58a5078225a29de14476e4c
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2026-03-07 10:48:44 -0600

    more misc fix

--- docs/site/scripts/generate-import-context.js
+++ docs/site/scripts/generate-import-context.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const path = require("path");
 const glob = require("glob");
 
-const SITE_ROOT = path.resolve(__dirname, "../"); // docusaurus site root
+const SITE_ROOT = path.resolve(__dirname, "."); // docusaurus site root
 const REPO_ROOT = path.resolve(SITE_ROOT, "../../.."); // monorepo root
 const OUT_FILE = path.join(SITE_ROOT, ".generated/ImportContentMap.ts");
 
