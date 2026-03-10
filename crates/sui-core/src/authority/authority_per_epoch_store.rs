60a96ed8eeb992fbc15377cccfacfa34e91a87b7
andll <andll@danasoft.ws>
2026-03-10 11:51:29 -0700

    4x for owned_object_locked_transactions mutex and more bits for executed_transaction_digests

--- crates/sui-core/src/authority/authority_per_epoch_store.rs
+++ crates/sui-core/src/authority/authority_per_epoch_store.rs
@@ -698,7 +698,7 @@ impl AuthorityEpochTables {
                 "owned_object_locked_transactions".to_string(),
                 ThConfig::new_with_config_indexing(
                     object_ref_indexing,
-                    mutexes * 2,
+                    mutexes * 8,
                     uniform_key,
                     bloom_config.clone(),
                 ),
