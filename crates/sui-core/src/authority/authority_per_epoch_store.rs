315a0ee19ecab7f0af056b6aae00bfa7800a5077
andll <andll@danasoft.ws>
2026-03-09 18:01:04 -0700

    Adjust mutex count for some tables

--- crates/sui-core/src/authority/authority_per_epoch_store.rs
+++ crates/sui-core/src/authority/authority_per_epoch_store.rs
@@ -664,7 +664,7 @@ impl AuthorityEpochTables {
                 "owned_object_locked_transactions".to_string(),
                 ThConfig::new_with_config_indexing(
                     object_ref_indexing,
-                    mutexes,
+                    mutexes * 2,
                     uniform_key,
                     bloom_config.clone(),
                 ),
@@ -707,7 +707,7 @@ impl AuthorityEpochTables {
                 "consensus_message_processed".to_string(),
                 ThConfig::new_with_config_indexing(
                     KeyIndexing::Hash,
-                    mutexes,
+                    mutexes * 2,
                     uniform_key,
                     bloom_config.clone(),
                 ),
