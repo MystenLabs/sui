a04a98a92c35a51618138cda129c42e3644d69ac
Tim Zakian <timzakian@gmail.com>
2026-03-17 13:13:50 -0700

    [move][treesitter] Refactor and update Move treesitter implementation

--- external-crates/move/tooling/tree-sitter/queries/highlights.scm
+++ external-crates/move/tooling/tree-sitter/queries/highlights.scm
@@ -26,7 +26,6 @@
 (function_definition name: (function_identifier)  @function)
 (macro_function_definition name: (function_identifier)  @macro)
 (native_function_definition name: (function_identifier)  @function)
-(usual_spec_function name: (function_identifier)  @function)
 (function_parameter name: (variable_identifier)  @variable.parameter)
 
 ;; Module definitions
@@ -40,7 +39,6 @@
 (call_expression (name_expression access: (module_access module: (module_identifier)  @namespace.module.name)))
 (call_expression (name_expression access: (module_access member: (identifier)  @function.call)))
 
-
 (label (identifier)  @label)
 
 ;; Macro calls
@@ -53,8 +51,6 @@
 (byte_string_literal) @string.byte
 (address_literal) @number.address
 
-;; Binders
-
 ;; Uses
 (use_member member: (identifier)  @include.member)
 (use_module alias: (module_identifier) @namespace.module.name)
@@ -63,9 +59,6 @@
 (use_fun (module_access member: (identifier)  @include.member))
 
 (function_identifier) @function.name
-
-;; Friends
-; (friend_access local_module: (identifier)  @namespace.module.name)
 
 ;; Structs
 (struct_definition name: (struct_identifier)  @type.definition.struct)
@@ -81,7 +74,6 @@
 (pack_expression (name_expression access: (module_access)  @constructor.name))
 
 ;; Unpacks
-;; TODO: go into variants
 (bind_unpack (name_expression)  @type.name)
 (module_access "$" (identifier)  @macro.variable)
 "$"  @macro.variable
@@ -91,11 +83,6 @@
 (abort_expression) @keyword
 (mut_ref) @keyword
 
-;; Lambdas
-; (lambda_binding bind: (bind_var (variable_identifier)  @variable.parameter))
-; (lambda_bindings (bind_var (variable_identifier)  @variable.parameter))
-
-
 ;; Operators
 (binary_operator) @operator
 (unary_op) @operator
@@ -103,23 +90,7 @@
 "@" @operator
 "->" @operator
 
-;; Specs
-(spec_block target: (identifier)  @function.spec.target)
-(spec_pragma) @preproc.annotation.spec
-(spec_condition kind: (condition_kind) @condition.spec)
-(condition_properties) @preproc.spec.condition.properties
-
-; (match_expression "match") @keyword
-
-;; Spec keywords
-; "opaque" @keyword
-; "aborts_if" @keyword
-; "abstract" @keyword
-[
- "pragma"
-] @keyword
-
-;; Source Language Keywords
+;; Keywords
 [
  "fun"
  "return"
@@ -146,6 +117,5 @@
  "break"
  "continue"
  "loop"
+ "match"
 ] @keyword
-
- "match" @keyword
