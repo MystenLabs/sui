ef81e780531877d65c3ff663f2250a31b6a4e93e
andll <andll@danasoft.ws>
2026-03-19 11:35:36 -0700

    Revert "Disable state sync"
    This reverts commit 1f60902d7640890e3212ac74fe2a3d23f331ce19.

--- crates/sui-network/src/state_sync/mod.rs
+++ crates/sui-network/src/state_sync/mod.rs
@@ -1823,7 +1823,6 @@ where
         debug!("store already contains checkpoint contents");
         return Some(contents);
     }
-    return None;
 
     // Iterate through our selected peers trying each one in turn until we're able to
     // successfully get the target checkpoint
