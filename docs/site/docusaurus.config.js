18cfaf3ff66708fe4cd6accc1424d879489a179d
Jessie Mongeon <133128541+jessiemongeon1@users.noreply.github.com>
2026-03-19 08:24:39 -0500

    Update docusaurus.config.js

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -49,6 +49,40 @@ const config = {
 
   staticDirectories: ["static", "src/open-spec"],
 
+  sitemap: {
+    // Ignore pages in the sitemap that use special /slug frontmatter, as they 
+    // don't pass the llms.txt afdocs test properly.
+    // These docs exist and are surfaced through the llms.txt file.
+    ignorePatterns: [
+      '/code-of-conduct',
+      '/concepts/architecture',
+      '/concepts/architecture/index',
+      '/contribute-to-sui-repos',
+      '/developer/guides/getting-started',
+      '/exchange-integration-guide',
+      '/guides/developer/accessing-data/custom-indexing-framework',
+      '/guides/developer/coin-index',
+      '/guides/developer/cryptography/zklogin-integration/index',
+      '/guides/developer/currency',
+      '/guides/developer/nautilus/index',
+      '/guides/developer/nft-index',
+      '/guides/developer/nft-rental',
+      '/guides/developer/nft-soulbound',
+      '/guides/developer/object-ownership/index',
+      '/guides/developer/operator',
+      '/guides/developer/packages/index',
+      '/guides/developer/suiplay0x1',
+      '/guides/developer/transactions/index',
+      '/localize-sui-docs',
+      '/references/sui-api/sui-graphql/beta/reference',
+      '/search',
+      '/style-guide',
+      '/sui-api-ref',
+      '/sui-compared',
+      '/sui-framework-reference',
+      '/sui-glossary',
+    ],
+  },
   markdown: {
     format: "detect",
     mermaid: true,
