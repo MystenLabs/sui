30bb160664532bde1be026c93613f33a6a1ef4cd
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2026-03-25 10:41:10 -0700

    First commit

---
+++ crates/sui-forking/src/lib.rs
@@ -0,0 +1,7 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Forking tool for Sui.
+
+#[allow(unused)]
+pub(crate) static VERSION: &str = env!("CARGO_PKG_VERSION");
