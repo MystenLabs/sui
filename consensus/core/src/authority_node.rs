f559fd35bcbbe69e341cece5a25cf048d116ef97
Mingwei Tian <mingwei@mystenlabs.com>
2026-03-26 13:58:37 -0700

    use fifo compaction for consensus store

--- consensus/core/src/authority_node.rs
+++ consensus/core/src/authority_node.rs
@@ -238,7 +238,10 @@ where
         ));
 
         let store_path = context.parameters.db_path.as_path().to_str().unwrap();
-        let store = Arc::new(RocksDBStore::new(store_path));
+        let store = Arc::new(RocksDBStore::new(
+            store_path,
+            context.parameters.use_fifo_compaction,
+        ));
         let dag_state = Arc::new(RwLock::new(DagState::new(context.clone(), store.clone())));
 
         let block_verifier = Arc::new(SignedBlockVerifier::new(
