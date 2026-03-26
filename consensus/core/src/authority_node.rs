80d4bdb3bfe93ae5ed066c81aa339d93ffea7684
Anastasios Kichidis <tasos@mystenlabs.com>
2026-03-26 14:59:07 +0000

    [fix] revert test change to not fail

--- consensus/core/src/authority_node.rs
+++ consensus/core/src/authority_node.rs
@@ -800,7 +800,7 @@ mod tests {
             ..Default::default()
         };
 
-        let (observer_commit_consumer, observer_commit_receiver) = CommitConsumerArgs::new(0, 0);
+        let (observer_commit_consumer, _observer_commit_receiver) = CommitConsumerArgs::new(0, 0);
         let observer = ConsensusAuthority::start(
             network_type,
             0,
@@ -816,8 +816,6 @@ mod tests {
             0,
         )
         .await;
-
-        commit_receivers.push(observer_commit_receiver);
 
         // Give Observer more time to connect and sync
         sleep(Duration::from_secs(5)).await;
