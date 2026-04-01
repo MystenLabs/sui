61378bb7afa7217c3f9f053127c792a663ded997
Ashok Menon <ashok@mystenlabs.com>
2026-04-01 11:37:36 +0100

    indexer: remove unused function
    ## Description
    
    `SequentialStore::sequential_connect` is vestigial and no longer used.
    
    ## Test plan
    
    ```
    $ cargo nextest run -p sui-indexer-alt-framework -p sui-indexer-alt
    ```

--- crates/sui-indexer-alt-framework-store-traits/src/lib.rs
+++ crates/sui-indexer-alt-framework-store-traits/src/lib.rs
@@ -3,7 +3,6 @@
 
 use std::time::Duration;
 
-use anyhow::Context;
 use async_trait::async_trait;
 use chrono::DateTime;
 use chrono::Utc;
@@ -169,13 +168,6 @@ pub trait SequentialStore: for<'c> Store<Connection<'c> = Self::SequentialConnec
     type SequentialConnection<'c>: SequentialConnection
     where
         Self: 'c;
-
-    async fn sequential_connect<'c>(&'c self) -> anyhow::Result<Self::SequentialConnection<'c>> {
-        Ok(self
-            .connect()
-            .await
-            .context("Failed to establish sequential connection to store")?)
-    }
 
     async fn transaction<'a, R, F>(&self, f: F) -> anyhow::Result<R>
     where
