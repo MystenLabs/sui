edd66650d776749e7ef37c5eb7f5051eabd8d21c
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2026-04-01 10:56:11 -0700

    Remove lib for now

--- crates/sui-forking/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-//! Forking tool for Sui.
-
-#[allow(unused)]
-pub(crate) static VERSION: &str = env!("CARGO_PKG_VERSION");
