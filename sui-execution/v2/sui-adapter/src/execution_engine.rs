9ad67e1f89957bb59c5f4600b195ddab102a8b3b
Jort <jordan.jennings@mystenlabs.com>
2026-04-09 20:12:16 -0700

    [types]: change ExecutionStatus::new_failure() to accept ExecutionFailure

--- sui-execution/v2/sui-adapter/src/execution_engine.rs
+++ sui-execution/v2/sui-adapter/src/execution_engine.rs
@@ -169,8 +169,7 @@ mod checked {
                 _ => (),
             };
 
-            let (status, command) = error.to_execution_status();
-            ExecutionStatus::new_failure(status, command)
+            ExecutionStatus::new_failure(error.to_execution_failure())
         } else {
             ExecutionStatus::Success
         };
