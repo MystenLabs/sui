08871a91be6e7bfc20f1732684a1aaad2f30bccf
Todd Nowacki <tmn@mystenlabs.com>
2026-05-04 09:59:44 -0700

    vector refactor

--- external-crates/move/crates/move-ir-to-bytecode-syntax/src/lexer.rs
+++ external-crates/move/crates/move-ir-to-bytecode-syntax/src/lexer.rs
@@ -79,13 +79,11 @@ pub enum Tok {
     Return,
     Struct,
     True,
-    VecPack(u64),
     VecLen,
     VecImmBorrow,
     VecMutBorrow,
     VecPushBack,
     VecPopBack,
-    VecUnpack(u64),
     VecSwap,
     LBrace,
     Pipe,
@@ -248,21 +246,7 @@ impl<'input> Lexer<'input> {
                             "vec_push_back" => (Tok::VecPushBack, len),
                             "vec_pop_back" => (Tok::VecPopBack, len),
                             "vec_swap" => (Tok::VecSwap, len),
-                            _ => {
-                                if let Some(stripped) = name.strip_prefix("vec_pack_") {
-                                    match stripped.parse::<u64>() {
-                                        Ok(num) => (Tok::VecPack(num), len),
-                                        Err(_) => (Tok::NameBeginTyValue, len + 1),
-                                    }
-                                } else if let Some(stripped) = name.strip_prefix("vec_unpack_") {
-                                    match stripped.parse::<u64>() {
-                                        Ok(num) => (Tok::VecUnpack(num), len),
-                                        Err(_) => (Tok::NameBeginTyValue, len + 1),
-                                    }
-                                } else {
-                                    (Tok::NameBeginTyValue, len + 1)
-                                }
-                            }
+                            _ => (Tok::NameBeginTyValue, len + 1),
                         },
                         Some('(') => match name {
                             "assert" => (Tok::Assert, len + 1),
