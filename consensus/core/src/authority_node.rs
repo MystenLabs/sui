fcbea2a088cb88f921663496b84e10b038d09194
Mingwei Tian <mingwei@mystenlabs.com>
2026-05-21 10:24:39 -0700

    Remove consensus FIFO compaction option

--- consensus/core/src/authority_node.rs
+++ consensus/core/src/authority_node.rs
@@ -270,10 +270,7 @@ where
         ));
 
         let store_path = context.parameters.db_path.as_path().to_str().unwrap();
-        let store = Arc::new(RocksDBStore::new(
-            store_path,
-            context.parameters.use_fifo_compaction,
-        ));
+        let store = Arc::new(RocksDBStore::new(store_path));
         let dag_state = Arc::new(RwLock::new(DagState::new(context.clone(), store.clone())));
 
         let block_verifier = Arc::new(SignedBlockVerifier::new(
