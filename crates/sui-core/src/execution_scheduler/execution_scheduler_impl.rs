585cf2989c506e6380cfb5ac95f87d15aafc61fb
Anastasios Kichidis <tasos@mystenlabs.com>
2026-05-28 14:48:38 +0100

    [refactor] remove the capability methods for NodeRole. Use only the runs_consensus method.

--- crates/sui-core/src/execution_scheduler/execution_scheduler_impl.rs
+++ crates/sui-core/src/execution_scheduler/execution_scheduler_impl.rs
@@ -174,8 +174,8 @@ impl ExecutionScheduler {
         scheduler_type: FundsWithdrawSchedulerType,
         address_funds_scheduler_metrics: &Arc<AddressFundsSchedulerMetrics>,
     ) -> Option<FundsWithdrawScheduler> {
-        let withdraw_scheduler_enabled = epoch_store.node_role().process_consensus_commits()
-            && epoch_store.accumulators_enabled();
+        let withdraw_scheduler_enabled =
+            epoch_store.node_role().runs_consensus() && epoch_store.accumulators_enabled();
         if !withdraw_scheduler_enabled {
             return None;
         }
