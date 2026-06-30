2ca5c3c219e13fe715c466c1ac830ec69a010416
Tim Zakian <2895723+tzakian@users.noreply.github.com>
2026-06-30 14:01:08 -0700

    [adapter] Add `PackagePayload` enum (#27009)
    ## Description 
    Add a `PackagePayload` from the loaded SPTB AST on-down, but do not
    create the `PackagePayload::Deserialized` variant yet.
    
    Plumbing for future work in the PR stacked on this that adds unified
    linkage.
    
    ## Test plan 
    
    CI
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] Indexing Framework:

--- sui-execution/latest/sui-adapter/src/static_programmable_transactions/execution/context.rs
+++ sui-execution/latest/sui-adapter/src/static_programmable_transactions/execution/context.rs
@@ -15,7 +15,7 @@ use crate::{
             values::{Local, Locals, Value},
         },
         linkage::resolved_linkage::{ExecutableLinkage, ResolvedLinkage},
-        loading::ast::Datatype,
+        loading::ast::{Datatype, DeserializedPackage, PackagePayload},
         typing::ast::{self as T, Type},
     },
 };
@@ -1022,27 +1022,27 @@ where
     //
     // Publish and Upgrade
     //
-    pub fn deserialize_modules(
-        &mut self,
-        module_bytes: &[Vec<u8>],
-    ) -> Result<Vec<CompiledModule>, Mode::Error> {
-        assert_invariant!(
-            !module_bytes.is_empty(),
-            "empty package is checked in transaction input checker"
-        );
-        let total_bytes = module_bytes.iter().map(|v| v.len()).sum();
-        self.gas_charger.charge_publish_package(total_bytes)?;
 
-        let binary_config = self.env.protocol_config.binary_config(None);
-        let modules = module_bytes
-            .iter()
-            .map(|b| {
-                CompiledModule::deserialize_with_config(b, &binary_config)
-                    .map_err(|e| e.finish(Location::Undefined))
-            })
-            .collect::<VMResult<Vec<CompiledModule>>>()
-            .map_err(|e| self.env.convert_vm_error(e))?;
-        Ok(modules)
+    pub fn deserialize_package(
+        &mut self,
+        package_payload: PackagePayload,
+        dep_ids: &[ObjectID],
+    ) -> Result<DeserializedPackage, Mode::Error> {
+        Ok(match package_payload {
+            PackagePayload::Deserialized(deserialized_pkg) => deserialized_pkg,
+            PackagePayload::Serialized(module_bytes) => {
+                // This assertion is also checked in the call to `deserialize_modules`, but we
+                // want to check it here first to keep existing behavior around checking this
+                // invariant before the charge on pre-existing pathways.
+                assert_invariant!(
+                    !module_bytes.is_empty(),
+                    "empty package is checked in transaction input checker"
+                );
+                let total_bytes = module_bytes.iter().map(|v| v.len()).sum();
+                self.gas_charger.charge_publish_package(total_bytes)?;
+                self.env.deserialize_package(&module_bytes, dep_ids)?
+            }
+        })
     }
 
     fn fetch_package(&mut self, dependency_id: &ObjectID) -> Result<Rc<MovePackage>, Mode::Error> {
