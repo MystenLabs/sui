d198ee52f99e9423fc7506faea24d6f17953321e
Anastasios Kichidis <tasos@mystenlabs.com>
2026-07-02 11:40:11 +0100

    [feat] support gas double spending transactions for the stress nodes and load generation.

--- crates/sui-benchmark/tests/simtest.rs
+++ crates/sui-benchmark/tests/simtest.rs
@@ -1444,6 +1444,7 @@ mod test {
             party: config.party_weight,
             conflicting_transfer: config.conflicting_transfer_weight,
             composite: config.composite_weight,
+            gas_double_spend: 0,
         };
 
         let workload_config = WorkloadConfig {
@@ -1461,6 +1462,7 @@ mod test {
             shared_counter_gas_price_multiplier: config.shared_counter_gas_price_multiplier,
             num_contested_objects: config.num_contested_objects,
             randomized_transaction_concurrency: config.randomized_transaction_concurrency,
+            gas_double_spend_copies: 2,
             target_qps,
             in_flight_ratio,
             duration,
