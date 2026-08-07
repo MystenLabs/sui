e166eb7baea237f82407d3b90197cb750185035a
Todd Nowacki <tmn@mystenlabs.com>
2026-08-06 19:31:37 -0700

    enable constant folding with constants in functions

--- external-crates/move/crates/move-compiler/src/cfgir/optimize/constant_fold.rs
+++ external-crates/move/crates/move-compiler/src/cfgir/optimize/constant_fold.rs
@@ -3,17 +3,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    cfgir::cfg::MutForwardCFG,
+    cfgir::{cfg::MutForwardCFG, optimize::Constants},
     diag,
     diagnostics::DiagnosticReporter,
     expansion::ast::Mutability,
     hlir::ast::{
         BaseType, BaseType_, Command, Command_, Exp, FunctionSignature, SingleType, TypeName,
-        TypeName_, UnannotatedExp_, Value, Value_, Var,
+        TypeName_, UnannotatedExp_, Value_, Var,
     },
     ice,
     naming::ast::{BuiltinTypeName, BuiltinTypeName_},
-    parser::ast::{BinOp, BinOp_, ConstantName, UnaryOp, UnaryOp_},
+    parser::ast::{BinOp, BinOp_, UnaryOp, UnaryOp_},
     shared::unique_map::UniqueMap,
 };
 use move_ir_types::location::*;
@@ -23,9 +23,9 @@ use std::{borrow::Cow, convert::TryFrom};
 /// returns true if anything changed
 pub fn optimize(
     reporter: &DiagnosticReporter,
+    constants: Constants,
     _signature: &FunctionSignature,
     _locals: &UniqueMap<Var, (Mutability, SingleType)>,
-    constants: &UniqueMap<ConstantName, Value>,
     cfg: &mut MutForwardCFG,
 ) -> bool {
     let context = Context {
@@ -54,7 +54,7 @@ pub fn optimize(
 
 struct Context<'a> {
     reporter: &'a DiagnosticReporter<'a>,
-    constants: &'a UniqueMap<ConstantName, Value>,
+    constants: Constants<'a>,
 }
 
 //**************************************************************************************************
@@ -96,6 +96,7 @@ fn optimize_cmd(context: &Context, sp!(_, cmd_): &mut Command) -> Option<bool> {
 fn optimize_exp(context: &Context, e: &mut Exp) -> bool {
     use UnannotatedExp_ as E;
     let optimize_exp = |e| optimize_exp(context, e);
+    let foldable_exp = |e: &Exp| foldable_exp(context, e);
     match &mut e.exp.value {
         //************************************
         // Pass through cases
@@ -113,7 +114,9 @@ fn optimize_exp(context: &Context, e: &mut Exp) -> bool {
             let E::Constant(name) = e_ else {
                 unreachable!()
             };
-            if let Some(value) = context.constants.get(name) {
+            if context.constants.force_inline
+                && let Some(value) = context.constants.values.get(name)
+            {
                 *e_ = E::Value(value.clone());
                 true
             } else {
@@ -455,9 +458,13 @@ const fn evalue_(loc: Loc, v: Value_) -> UnannotatedExp_ {
 // Foldable Value
 //**************************************************************************************************
 
-fn foldable_exp(e: &Exp) -> Option<&Value_> {
+/// The value of `e` if it is statically known.
+/// Constants have their values used, even when not forcibly inlined. This enables outer
+/// operations/expressions to be folded.
+fn foldable_exp(context: &Context, e: &Exp) -> Option<&Value_> {
     use UnannotatedExp_ as E;
     match &e.exp.value {
+        E::Constant(name) => context.constants.values.get(name).map(|sp!(_, v_)| v_),
         E::Value(sp!(_, v_)) => Some(v_),
         _ => None,
     }
@@ -481,7 +488,7 @@ fn ignorable_exp(e: &Exp) -> bool {
 /// `0xFFFFu16 as u8`.
 pub fn report_always_erroring_operations(
     reporter: &DiagnosticReporter,
-    constants: &UniqueMap<ConstantName, Value>,
+    constants: Constants<'_>,
     cfg: &MutForwardCFG,
 ) {
     let context = Context {
@@ -523,7 +530,7 @@ fn check_cmd(context: &Context, sp!(_, cmd_): &Command) {
 fn check_exp<'a>(context: &Context, e: &'a Exp) -> Option<Cow<'a, Value_>> {
     use UnannotatedExp_ as E;
     match &e.exp.value {
-        E::Value(_) | E::Constant(_) => foldable_exp(e).map(Cow::Borrowed),
+        E::Value(_) | E::Constant(_) => foldable_exp(context, e).map(Cow::Borrowed),
 
         E::Unit { .. }
         | E::UnresolvedError
