c2b9ad4c55ebc988c1c0b8c23cd22254460c25b6
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2026-08-27 07:04:53 -0700

    [chore] Remove a bunch of deps, update some versions, consolidate deps (#27808)
    ## Description 
    
    This PR removes a bunch of deps, updates some versions to try to
    consolidate between external-crates and normal-crates, and have fewer
    different versions of the same dependency.
    
    ## Test plan 
    
    Existing CI
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] Indexing Framework:

--- crates/sui-proxy/src/admin.rs
+++ crates/sui-proxy/src/admin.rs
@@ -10,7 +10,9 @@ use crate::peers::{AllowedPeer, SuiNodeProvider};
 use crate::var;
 use anyhow::Error;
 use anyhow::Result;
-use axum::{Extension, Router, extract::DefaultBodyLimit, middleware, routing::post};
+use axum::{
+    Extension, Router, extract::DefaultBodyLimit, http::StatusCode, middleware, routing::post,
+};
 use fastcrypto::ed25519::{Ed25519KeyPair, Ed25519PublicKey};
 use fastcrypto::traits::{KeyPair, ToFromBytes};
 use rustls::pki_types::{CertificateDer, PrivateKeyDer, pem::PemObject};
@@ -116,9 +118,10 @@ pub fn app(
         // Enforce on all routes.
         // If the request does not complete within the specified timeout it will be aborted
         // and a 408 Request Timeout response will be sent.
-        .layer(TimeoutLayer::new(Duration::from_secs(
-            timeout_secs.unwrap_or(20),
-        )))
+        .layer(TimeoutLayer::with_status_code(
+            StatusCode::REQUEST_TIMEOUT,
+            Duration::from_secs(timeout_secs.unwrap_or(20)),
+        ))
         .layer(Extension(relay))
         .layer(Extension(labels))
         .layer(Extension(client))
