cb9251f68012f4627100100107f80f31161526ab
Tim Zakian <2895723+tzakian@users.noreply.github.com>
2026-09-18 12:10:43 -0700

    [perf] Add LinkageValidationEnvironment to linkage checking (#28042)
    ## Description 
    
    Setup linkage validation to caching and reuse intermediate data
    structures instead of rebuilding them.
    
    
    ## Test plan 
    
    CI + new tests
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] Indexing Framework:

--- external-crates/move/crates/bytecode-verifier-tests/src/unit_tests/mod.rs
+++ external-crates/move/crates/bytecode-verifier-tests/src/unit_tests/mod.rs
@@ -13,6 +13,7 @@ pub mod bounds_tests;
 pub mod code_unit_tests;
 pub mod constants_tests;
 pub mod control_flow_tests;
+pub mod dependency_tests;
 pub mod duplication_tests;
 pub mod generic_ops_tests;
 pub mod large_type_test;
